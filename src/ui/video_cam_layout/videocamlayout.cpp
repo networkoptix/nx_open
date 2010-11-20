f1cb409895312fc45996dee79567678cf2f3c481
Sergey Bystrov <sbystrov@networkoptix.com>
2010-11-20 03:45:06 +0000

    fixed a little bug
    <==== Mercurial data ====>
    Branch: master

--- src/ui/video_cam_layout/videocamlayout.cpp
+++ src/ui/video_cam_layout/videocamlayout.cpp
@@ -142,8 +142,9 @@ void SceneLayout::stop_helper(bool emt)
 		// after we can wait for each thread to stop
 		cl_log.log("About to shutdown device ", (int)devitem ,"\r\n", cl_logDEBUG1);
 		devitem->stopDispay();
-		devitem->getDevice()->releaseRef();
+		CLDevice* dev = devitem->getDevice();
 		delete devitem;
+		dev->releaseRef();
 	}
 
 	m_deviceitems.clear();
