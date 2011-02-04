68c5d3edd70f96916f4bc32d9728ed61dfb316a4
Sergey Bystrov <sbystrov@networkoptix.com>
2011-02-04 01:32:32 +0000

    added missing files
    <==== Mercurial data ====>
    Branch: master

---
+++ src/StdAfx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// H264SDKExample.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
---
+++ src/StdAfx.h
@@ -0,0 +1,60 @@
+#ifndef std_afx_h_uni_client_1701
+#define std_afx_h_uni_client_1701
+
+#include <QWidget>
+#include <QCompleter>
+#include <QTimer>
+#include <QLineEdit>
+#include <QString>
+#include <QStringList>
+#include <QStringListModel>
+#include <QDateTime>
+#include <QGraphicsItem>
+#include <QObject>
+#include <QMutex>
+#include <QThread>
+#include <QSemaphore>
+#include <QQueue>
+#include <QtOpenGL/QGLWidget>
+#include <QWaitCondition>
+#include <QDialog>
+#include <QCloseEvent>
+#include <QResizeEvent>
+#include <QMessageBox>
+#include <QNetworkAddressEntry>
+#include <QHostAddress>
+#include <QGraphicsView>
+#include <QFile>
+#include <QDir>
+#include <QGraphicsProxyWidget>
+#include <QWebView>
+#include <QPropertyAnimation>
+#include <QListView>
+#include <QGraphicsSceneMouseEvent>
+#include <QDomDocument>
+#include <QWindowsStyle>
+#include <QInputDialog>
+#include <QPixmapCache>
+#include <QGroupBox>
+#include <QAuthenticator>
+#include <QCheckBox>
+#include <QAction>
+#include <QLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QAudioOutput>
+#include <QThreadPool>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QtCore/qmath.h>
+#include <QEasingCurve>
+#include <QScrollBar>
+#include <QDialogButtonBox>
+#include <QUdpSocket>
+#include <QtConcurrentmap>
+#include <QXmlQuery>
+
+
+#endif//std_afx_h_uni_client_1701
+
+
