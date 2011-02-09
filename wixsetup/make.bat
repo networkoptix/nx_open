650066534375e13a5e1f291a7c78226147cef8a8
Ivan Vigasin <ivigasin@networkoptix.com>
2011-02-09 20:05:55 +0000

    Build installer for Release (not Debug).
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/make.bat
+++ wixsetup/make.bat
@@ -1,4 +1,6 @@
 rem To run this script install WiX 3.5 from wix.sourceforge.net and add its bin dir to system path
 
-Candle.exe -out obj\Debug\ -ext WixUIExtension.dll -ext WixUtilExtension.dll *.wxs
-Light.exe -cultures:null -ext WixUIExtension.dll -ext WixUtilExtension.dll -out bin\Debug\UniversalClientSetup.msi -pdbout bin\Debug\UniversalClientSetup.wixpdb obj\Debug\*.wixobj
+call gen_fragments.bat 
+
+Candle.exe -out obj\Release\ -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll *.wxs
+Light.exe -cultures:null -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -out bin\Release\UniversalClientSetup.msi -pdbout bin\Release\UniversalClientSetup.wixpdb obj\Release\*.wixobj
