415be29b8a9b7ad084384131c11748d620091e05
Ivan Vigasin <ivigasin@networkoptix.com>
2011-03-17 15:31:15 +0000

    #78 make genskin a module
    <==== Mercurial data ====>
    Branch: master

--- src/genskin.py
+++ src/genskin.py
@@ -1,29 +1,33 @@
 import os, posixpath
 
-os.path = posixpath
+def genskin():
+  os.path = posixpath
 
-skin_qrc = open('skin.qrc', 'w')
+  skin_qrc = open('skin.qrc', 'w')
 
-print >> skin_qrc, """
-<!DOCTYPE RCC>
-<RCC version="1.0">
-<qresource prefix="/skin">
-"""
+  print >> skin_qrc, """
+  <!DOCTYPE RCC>
+  <RCC version="1.0">
+  <qresource prefix="/skin">
+  """
 
-skin_dir = '../resource/skin'
-for root, dirs, files in os.walk(skin_dir):
-  parent = root[len(skin_dir) + 1:]
-  if '.svn' in dirs:
-    dirs.remove('.svn')  # don't visit SVN directories
+  skin_dir = '../resource/skin'
+  for root, dirs, files in os.walk(skin_dir):
+    parent = root[len(skin_dir) + 1:]
+    if '.svn' in dirs:
+      dirs.remove('.svn')  # don't visit SVN directories
 
-  for f in files:
-    if f.endswith('.png'):
-      print >> skin_qrc, '<file alias="%s">%s</file>' % (os.path.join(parent, f).lower(), os.path.join(root, f).lower())
-    
+    for f in files:
+      if f.endswith('.png'):
+        print >> skin_qrc, '<file alias="%s">%s</file>' % (os.path.join(parent, f).lower(), os.path.join(root, f).lower())
+      
 
-print >> skin_qrc, """
-</qresource>
-</RCC>
-"""
+  print >> skin_qrc, """
+  </qresource>
+  </RCC>
+  """
 
-skin_qrc.close()
+  skin_qrc.close()
+
+if __name__ == '__main__':
+  genskin()
