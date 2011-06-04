91680fb45a4f3d4b38b90d1f8e5e74daae95be7a
Ivan Vigasin <ivigasin@networkoptix.com>
2011-06-05 03:05:40 +0400

    Use $HOME\Movies\EVE Media instead of C:\EVE Media by default
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/make.bat
+++ wixsetup/make.bat
@@ -1,4 +1,5 @@
 CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 MSBuild ..\src\uniclient.vcproj /t:Rebuild /p:Configuration=Release
+MSBuild PropsCA\PropsCA.vcproj /t:Rebuild /p:Configuration=Release
 
 @start /B /WAIT make.py
