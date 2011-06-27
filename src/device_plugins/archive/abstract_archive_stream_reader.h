55c3a77bee7283ff24954b0be474483b731fe712
Unknown User <john.doe@blackhole.com>
2011-06-27 03:11:42 -0700

    introduced interface for audio track selection ( will use it in ui in morning )
    <==== Mercurial data ====>
    Branch: master

--- src/device_plugins/archive/abstract_archive_stream_reader.h
+++ src/device_plugins/archive/abstract_archive_stream_reader.h
@@ -32,6 +32,13 @@ public:
     virtual void previousFrame(quint64 /*mksec*/) {}
     virtual void nextFrame() {}
 
+    // gives a list of audio tracks 
+    virtual QStringList getAudioTracksInfo() const {return QStringList();};
+
+
+    // sets certain track 
+    virtual void setAudioChannel(unsigned int num){};
+
 protected:
 	virtual void channeljumpTo(quint64 mksec, int channel) = 0;
     quint64 skipFramesToTime() const;
