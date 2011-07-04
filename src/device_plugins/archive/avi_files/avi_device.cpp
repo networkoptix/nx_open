b4ea95ff72ec21796bd16abba1adba49139812b3
Ivan Vigasin <ivigasin@networkoptix.com>
2011-07-05 00:19:21 +0400

    Use clean path when adding avi device.
    <==== Mercurial data ====>
    Branch: master

--- src/device_plugins/archive/avi_files/avi_device.cpp
+++ src/device_plugins/archive/avi_files/avi_device.cpp
@@ -1,9 +1,10 @@
 #include "avi_device.h"
 #include "avi_strem_reader.h"
+#include "util.h"
 
 CLAviDevice::CLAviDevice(const QString& file)
 {
-	m_uniqueId = file;
+    m_uniqueId = QDir::cleanPath(file);
 	m_name = QFileInfo(file).fileName();
 }
 
