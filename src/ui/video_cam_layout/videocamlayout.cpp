ac0eea8b58bd4bb130703ab70a14adbe8783fafd
Konstantin Ritt <ritt.ks@gmail.com>
2011-08-03 03:00:19 +0400

    minor
    <==== Mercurial data ====>
    Branch: master

--- src/ui/video_cam_layout/videocamlayout.cpp
+++ src/ui/video_cam_layout/videocamlayout.cpp
@@ -408,7 +408,7 @@ bool SceneLayout::addDevice(CLDevice* device, bool update_scene_rect, CLBasicLay
 {
 	if (!m_grid.isSpaceAvalable())
 	{
-		cl_log.log(QLatin1String("Cannot support so many devices "), cl_logDEBUG1);
+//		cl_log.log(QLatin1String("Cannot support so many devices "), cl_logDEBUG1);
 		return false;
 	}
 
