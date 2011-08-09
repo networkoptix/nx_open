092a8359426f3106e18707964e128849fc4f1856
Ivan Komissarov <archangel_rus@mail.ru>
2011-08-09 15:33:58 +0400

    Added volatile
    <==== Mercurial data ====>
    Branch: master

--- src/base/longrunnable.h
+++ src/base/longrunnable.h
@@ -3,74 +3,71 @@
 
 class CLLongRunnable : public QThread
 {
-	Q_OBJECT
-public slots:
-	void start ( Priority priority = InheritPriority ) 
-	{
-		if (m_runing) // already runing;
-			return;
+    Q_OBJECT
 
-		m_runing = true;
-		m_needStop = false;
-		QThread::start(priority);
-	}
+public slots:
+    void start ( Priority priority = InheritPriority )
+    {
+        if (m_runing) // already runing;
+            return;
+
+        m_runing = true;
+        m_needStop = false;
+        QThread::start(priority);
+    }
+
 public:
 
-	CLLongRunnable():
-	  m_runing(false),
-	  m_onPause(false)
-	  {
+    CLLongRunnable() : m_runing(false), m_onPause(false) {}
 
-	  }
-
-	virtual ~CLLongRunnable(){};
+    virtual ~CLLongRunnable() {}
 
     virtual void pleaseStop()
-	{
-		m_needStop = true;
-		if (m_onPause)
-			resume();
-	}
+    {
+        m_needStop = true;
+        if (m_onPause)
+            resume();
+    }
 
-	virtual bool needToStop() const
-	{
-		return m_needStop;
-	}
+    virtual bool needToStop() const
+    {
+        return m_needStop;
+    }
 
-	virtual void stop()
-	{
-		pleaseStop();
-		wait();
-		m_runing = false;
-	}
+    virtual void stop()
+    {
+        pleaseStop();
+        wait();
+        m_runing = false;
+    }
 
-	virtual void pause()
-	{
-		m_semaphore.tryAcquire(m_semaphore.available());
-		m_onPause = true;
+    virtual void pause()
+    {
+        m_semaphore.tryAcquire(m_semaphore.available());
+        m_onPause = true;
 
-	}
+    }
 
-	virtual void resume()
-	{
-		m_onPause = false;
-		m_semaphore.release();
-	}
+    virtual void resume()
+    {
+        m_onPause = false;
+        m_semaphore.release();
+    }
 
-	void pauseDelay()
-	{
-		while(m_onPause && !needToStop())
-			m_semaphore.tryAcquire(1,50);
-	}
+    void pauseDelay()
+    {
+        while(m_onPause && !needToStop())
+            m_semaphore.tryAcquire(1,50);
+    }
 
-        bool onPause() const { return m_onPause; }
+    bool onPause() const { return m_onPause; }
 
 protected:
-	bool m_runing;
-	volatile bool m_needStop;
+    bool m_runing;
+    volatile bool m_needStop;
 
-	bool m_onPause;
-	QSemaphore m_semaphore;
+    volatile bool m_onPause;
+    QSemaphore m_semaphore;
 };
 
 #endif //cl_long_runnable_146
