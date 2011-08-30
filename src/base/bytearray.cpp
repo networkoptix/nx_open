cce6161c0eedf40f87f7ff43e13b1e26d26c2cfe
Konstantin Ritt <ritt.ks@gmail.com>
2011-08-30 15:57:07 +0400

    fix crash in video frame decoding due to misaligned memory access on AMDs
    +minor refactoring;
    this also reduces the peak memory consumption on my machine in about 1.5MB per movie
    
    <==== Mercurial data ====>
    Branch: master

--- src/base/bytearray.cpp
+++ src/base/bytearray.cpp
@@ -1,154 +1,133 @@
 #include "bytearray.h"
-#include <assert.h>
-#include <memory.h>
+
 #include "log.h"
 
 CLByteArray::CLByteArray(unsigned int alignment, unsigned int capacity):
-m_alignment(alignment),
-m_size(0)
+    m_alignment(alignment),
+    m_capacity(capacity + alignment),
+    m_size(0),
+    m_ignore(0)
 {
-
-	capacity+=alignment;
-
-	m_data = new char[capacity];
-
-	int shift  = (alignment==0) ? 0 : (alignment - ((unsigned long)m_data)%alignment);
-
-	m_al_data = m_data + shift;
-
-	m_capacity = capacity - shift;
-
-	m_ignore = 0;
+    m_data = (char *)qMallocAligned(m_capacity, m_alignment);
+    Q_CHECK_PTR(m_data);
 }
 
 CLByteArray::~CLByteArray()
 {
-	delete[] m_data;
-}
-
-const char* CLByteArray::data() const
-{
-	return m_al_data + m_ignore;
-}
-
-char *CLByteArray::data()
-{
-	return m_al_data + m_ignore;
-}
-
-void CLByteArray::ignore_first_bytes(int bytes_to_ignore)
-{
-	m_ignore = bytes_to_ignore;
-}
-
-unsigned int CLByteArray::size() const
-{
-	return m_size - m_ignore;
-}
-
-unsigned int CLByteArray::capacity() const
-{
-	return m_capacity;
-}
-
-bool CLByteArray::increase_capacity(unsigned int new_capacity)
-{
-	cl_log.log(QLatin1String("CLByteArray::increase_capacity:reallocate"), cl_logWARNING);
-
-	new_capacity += m_alignment;
-
-	if (new_capacity <= m_size + m_alignment)
-		return false;
-
-	char* new_data = new char[new_capacity];
-	if (!new_data)
-		return false;
-
-	int shift = (m_alignment==0) ? 0 : (m_alignment - ((unsigned long)new_data)%m_alignment);
-	m_capacity = new_capacity - shift;
-
-	memcpy(new_data + shift, m_al_data, m_size); // copy old data to new
-
-	delete[] m_data; // delete old data
-
-	m_data = new_data;
-	m_al_data = m_data + shift;
-
-	// size remains the same
-
-	return true;
-
-}
-
-unsigned int CLByteArray::write(const char* data, unsigned int size)
-{
-	if (size > m_capacity - m_size) // if we do not have anougth space
-	{
-		if (!increase_capacity(m_capacity*2 + size))
-			return 0;
-	}
-
-	memcpy(m_al_data + m_size, data, size);
-	m_size += size;
-	return size;
-}
-
-unsigned int CLByteArray::write(const QByteArray& data)
-{
-	return write(data.constData(), data.size());
-}
-
-unsigned int CLByteArray::write(const char* data, unsigned int size, int abs_shift  )
-{
-	if (size + abs_shift > m_capacity) // if we do not have anougth space
-	{
-		if (!increase_capacity(m_capacity*2 + size))
-			return 0;
-	}
-
-	memcpy(m_al_data + abs_shift, data, size);
-
-	if (size + abs_shift > m_size)
-		m_size = size + abs_shift;
-
-	return size;
-
-}
-
-unsigned int CLByteArray::write(const CLByteArray& other )
-{
-	return write(other.data(), other.size());
+    qFreeAligned(m_data);
 }
 
 void CLByteArray::clear()
 {
-	m_size = 0;
+    m_size = 0;
+    m_ignore = 0;
 }
 
-char* CLByteArray::prepareToWrite(int size)
+const char *CLByteArray::constData() const
 {
-	if (size > m_capacity - m_size) // if we do not have anogth space
-	{
-		if (!increase_capacity(m_capacity*2 + size))
-			return 0;
-	}
+    return m_data + m_ignore;
+}
 
-	return m_al_data + m_size;
+char *CLByteArray::data()
+{
+    return m_data + m_ignore;
+}
+
+// ### consider deprecating ignore_first_bytes() as it could break data alignment
+void CLByteArray::ignore_first_bytes(int bytes_to_ignore)
+{
+    m_ignore = bytes_to_ignore;
+}
+
+unsigned int CLByteArray::size() const
+{
+    return m_size - m_ignore;
+}
+
+unsigned int CLByteArray::capacity() const
+{
+    return m_capacity;
+}
+
+bool CLByteArray::increase_capacity(unsigned int new_capacity)
+{
+    new_capacity += m_alignment;
+
+    if (new_capacity < m_size + m_alignment)
+        return false;
+
+    if (new_capacity <= m_capacity)
+        return true;
+
+    char *new_data = (char *)qReallocAligned(m_data, new_capacity, m_capacity, m_alignment);
+    if (!q_check_ptr(new_data))
+        return false; // ### would probably crash anyways
+
+    m_capacity = new_capacity;
+    m_data = new_data;
+    // size remains unchanged
+
+    return true;
+}
+
+unsigned int CLByteArray::write(const char *data, unsigned int size)
+{
+    if (size > m_capacity - m_size) // if we do not have enough space
+    {
+        if (!increase_capacity(m_capacity * 2 + size))
+        {
+            cl_log.log(QLatin1String("CLByteArray::write(): Unable to increase capacity"), cl_logWARNING);
+            return 0;
+        }
+    }
+
+    qMemCopy(m_data + m_size, data, size);
+
+    m_size += size;
+
+    return size;
+}
+
+unsigned int CLByteArray::write(const char *data, unsigned int size, int abs_shift)
+{
+    if (size > m_capacity - abs_shift) // if we do not have enough space
+    {
+        if (!increase_capacity(m_capacity * 2 + size))
+        {
+            cl_log.log(QLatin1String("CLByteArray::write(): Unable to increase capacity"), cl_logWARNING);
+            return 0;
+        }
+    }
+
+    qMemCopy(m_data + abs_shift, data, size);
+
+    if (size + abs_shift > m_size)
+        m_size = size + abs_shift;
+
+    return size;
+}
+
+char *CLByteArray::prepareToWrite(int size)
+{
+    if (size > m_capacity - m_size) // if we do not have enough space
+    {
+        if (!increase_capacity(m_capacity * 2 + size))
+        {
+            cl_log.log(QLatin1String("CLByteArray::prepareToWrite(): Unable to increase capacity"), cl_logWARNING);
+            return 0;
+        }
+    }
+
+    return m_data + m_size;
 }
 
 void CLByteArray::done(int size)
 {
-	m_size+=size;
+    m_size += size;
 }
 
 void CLByteArray::removeZerrowsAtTheEnd()
 {
-	while(1)
-	{
-		if (m_size>0 && m_al_data[m_size-1]==0)
-			m_size--;
-		else
-			break;
-	}
-
+    while (m_size>0 && m_data[m_size-1] == 0)
+        --m_size;
 }
--- src/base/bytearray.h
+++ src/base/bytearray.h
@@ -1,83 +1,83 @@
-#ifndef CL_bytearray_1207
-#define CL_bytearray_1207
+#ifndef CLBYTEARRAY_H
+#define CLBYTEARRAY_H
+
+#include <QtCore/qglobal.h>
+
+class QByteArray;
 
 // the purpose of this class is to provide array container for alignment data
 class CLByteArray
 {
 public:
+    // creates array with alignment and capacity
+    // note that capacity() function might return slightly different value from the one passed to constructor
+    explicit CLByteArray(unsigned int alignment, unsigned int capacity);
+    ~CLByteArray();
 
-	// creates array with alignment and capacity
-	// note that capacity() function might return slightly different value from the one passed to constructor 
-	explicit CLByteArray(unsigned int alignment, unsigned int capacity);
+    // sets current write position and size to 0; capacity remains unchanged
+    void clear();
 
-	~CLByteArray();
+    // returns const pointer to aligned data
+    const char *constData() const;
+    inline const char *data() const
+    { return constData(); }
 
-	// returns const pointer to alignment data
-	const char *data() const; 
+    // returns pointer to aligned data
+    char *data();
 
-	// returns pointer to alignment data
-	char *data();
+    // returns actual data size
+    unsigned int size() const;
 
-	// return actual data size
-	unsigned int size() const;
+    unsigned int capacity() const;
 
-	unsigned int capacity() const;
-
-	//writes size bytes to array starting from current position.
-	// if size > (capacity - curr_size) then capacity automatically increased by factor of 2
-	unsigned int write(const char* data, unsigned int size );
+    // writes size bytes to array starting from current position.
+    // if size > (capacity - curr_size) then capacity automatically increased by factor of 2
+    unsigned int write(const char *data, unsigned int size);
 
     //writes QByteArray to array starting from current position.
     // if size > (capacity - curr_size) then capacity automatically increased by factor of 2
-    unsigned int write(const QByteArray& data);
+    inline unsigned int write(const QByteArray &data)
+    { return write(data.constData(), data.size()); }
 
-	//writes size bytes to array starting from abs_shift position.
-	// if size + shift > (capacity) then capacity automatically increased by factor of 2
-	unsigned int write(const char* data, unsigned int size, int abs_shift );
+    //writes size bytes to array starting from abs_shift position.
+    // if size + shift > (capacity) then capacity automatically increased by factor of 2
+    unsigned int write(const char *data, unsigned int size, int abs_shift);
 
-	// writes other array to current position
-	// if other.size > (this.capacity - this.curr_size) then capacity automatically increased by factor of 2
-	unsigned int write(const CLByteArray& other );
+    // writes other array to current position
+    // if other.size > (this.capacity - this.curr_size) then capacity automatically increased by factor of 2
+    inline unsigned int write(const CLByteArray &data)
+    { return write(data.constData(), data.size()); }
 
-	// sets current write position and size to 0;
-	// capacity remains the same
-	void clear();
+    // this function makes sure that array has capacity to write size bytes
+    // returns a pointer
+    // this function is used with done()
+    char *prepareToWrite(int size);
 
-	// this function makes sure that array has capacity to write size bytes 
-	// returns a pointer
-	// this function is used with done()
-	char* prepareToWrite(int size);
+    void done(int size);
 
-	void done(int size);
+    void ignore_first_bytes(int bytes_to_ignore);
 
-	void ignore_first_bytes(int bytes_to_ignore);
+    //======
 
-	//======
-
-	//av cam
-	void removeZerrowsAtTheEnd();
+    //av cam
+    void removeZerrowsAtTheEnd();
 
 private:
-	CLByteArray(const CLByteArray&) {}
-	CLByteArray& operator=(const CLByteArray&) { return *this; }
+    Q_DISABLE_COPY(CLByteArray)
 
-	// this function realocates data and changes capacity.
-	// if new capacity smaller than current size+alignment this function does nothing 
-	// return false if can not allocate new memorry
-	bool increase_capacity(unsigned int new_capacity);
+    // this function realocates data and changes capacity.
+    // if new capacity smaller than current size+alignment this function does nothing
+    // return false if can not allocate new memorry
+    bool increase_capacity(unsigned int new_capacity);
 
 private:
-	unsigned int m_alignment;
+    unsigned int m_alignment;
+    unsigned int m_capacity;
+    unsigned int m_size;
 
-	unsigned int m_size;
-	unsigned int m_capacity;
+    char *m_data;
 
-	char* m_data;
-
-	char* m_al_data;
-
-	int m_ignore;
-
+    int m_ignore;
 };
 
-#endif //CL_bytearray_1207
+#endif // CLBYTEARRAY_H
