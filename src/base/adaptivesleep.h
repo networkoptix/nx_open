1682fb0764fb78ab24f11ab0edd97a8378bf9f9b
Roman Vasilenko <rvasilenko@networkoptix.com>
2011-09-08 22:27:41 +0400

    1.2 coeff in max sleep interval removed
    <==== Mercurial data ====>
    Branch: master

--- src/base/adaptivesleep.h
+++ src/base/adaptivesleep.h
@@ -46,7 +46,7 @@ public:
           }
           //cl_log.log("sleep time=", havetowait/1000000.0, cl_logALWAYS);
           if (havetowait < MAX_VALID_SLEEP_TIME)
-            CLSleep::msleep(qMin(havetowait/1000, mksec/800)); // max sleep time is source delay*1.2
+            CLSleep::msleep(qMin(havetowait/1000, mksec/1000));
           else
               afterdelay();
           return havetowait;
