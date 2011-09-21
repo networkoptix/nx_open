2ad855d2b915a09324725220a977ab2037c24e2d
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2011-09-21 17:45:27 +0400

    Fixes 275
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/make.bat
+++ wixsetup/make.bat
@@ -1,4 +1,15 @@
-CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+if exist "%PROGRAMFILES(X86)%" goto gotx64 else goto gotx32
+
+:gotx32
+set "ARCH_PROGRAMFILES=%PROGRAMFILES%"
+goto build
+
+:gotx64
+set "ARCH_PROGRAMFILES=%PROGRAMFILES(X86)%"
+goto build
+
+:build
+CALL "%ARCH_PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 MSBuild ..\src\uniclient.vcproj /t:Rebuild /p:Configuration=Release
 MSBuild PropsCA\PropsCA.vcproj /t:Rebuild /p:Configuration=Release
 MSBuild EveAssocCA\EveAssocCA.vcproj /t:Rebuild /p:Configuration=Release
