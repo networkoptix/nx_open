166d20f7b3c40ec75bb09eee4251fe85bd6ffc40
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2011-09-27 22:09:17 +0400

    add convert.bat to make.bat
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/make.bat
+++ wixsetup/make.bat
@@ -1,5 +1,11 @@
+set "CURRENTDIR=%cd%"
+cd..
+call convert.bat
+
 call "%VS90COMNTOOLS%vsvars32.bat"
 call qtvars.bat
+
+cd %CURRENTDIR%
 
 MSBuild ..\src\uniclient.vcproj /t:Rebuild /p:Configuration=Release
 MSBuild PropsCA\PropsCA.vcproj /t:Rebuild /p:Configuration=Release
