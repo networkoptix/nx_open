a79e3953b0b5c796cbe6c66e94bb006004253903
Roman Vasilenko <rvasilenko@networkoptix.com>
2011-11-09 22:04:33 +0400

    missed files added
    <==== Mercurial data ====>
    Branch: master

---
+++ eveCommon/src/api/VideoSessionManager.cpp
@@ -0,0 +1,29 @@
+#include "VideoSessionManager.h"
+#include <QDebug>
+#include <QNetworkReply>
+#include <QDomDocument>
+#include <QXmlInputSource>
+#include <QSharedPointer>
+#include "QStdIStream.h"
+#include "AppSessionManager.h"
+
+int VideoServerSessionManager::getTimePeriods(const QnRequestParamList& params, QnApiTimePeriodListResponsePtr& timePeriodList)
+{
+    QByteArray reply;
+    if(sendGetRequest("RecordedTimePeriods", params, reply) == 0)
+    {
+        try
+        {
+            QTextStream stream(reply);
+            QStdIStream is(stream.device());
+
+            QnApiTimePeriodListResponsePtr timePeriodList = QnApiResourceTypeResponsePtr(xsd::api::timePeriodList::TimePeriodList (is, xml_schema::flags::dont_validate).release());
+
+            return 0;
+        } catch (const xml_schema::exception& e)
+        {
+            qDebug(e.what());
+        }
+    }
+    return 1;
+}
