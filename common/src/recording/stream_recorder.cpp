da1be20d8ef5a7bbfaed3f7cf025fbf31a36eb08
Roman Vasilenko <rvasilenko@networkoptix.com>
2011-11-12 21:50:24 +0400

    small change for recorded file duration
    <==== Mercurial data ====>
    Branch: master

--- common/src/recording/stream_recorder.cpp
+++ common/src/recording/stream_recorder.cpp
@@ -99,7 +99,7 @@ bool QnStreamRecorder::processData(QnAbstractDataPacketPtr data)
         if (m_truncateInterval > 0 && md->timestamp - m_firstTimestamp > m_currentChunkLen)
         {
             m_currentChunkLen += m_truncateInterval - (md->timestamp - m_firstTimestamp);
-            while (m_currentChunkLen < 0)
+            while (m_currentChunkLen <= 0)
                 m_currentChunkLen += m_truncateInterval;
             cl_log.log("chunkLen=" , m_currentChunkLen/1000000.0, cl_logALWAYS);
             m_prevDateTime = m_currentDateTime;
