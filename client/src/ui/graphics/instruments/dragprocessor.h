79e45b7ca5efdf0f3afdbaa2960c63885a1a20b6
Alexander Fokin <afokin@networkoptix.com>
2011-11-19 19:52:53 +0400

    More docs.
    <==== Mercurial data ====>
    Branch: new_scene_integration2

--- client/src/ui/graphics/instruments/dragprocessor.h
+++ client/src/ui/graphics/instruments/dragprocessor.h
@@ -15,18 +15,63 @@ class QGraphicsScene;
 
 class DragProcessor;
 
+/**
+ * Interface for handling drag process state changes.
+ */
 class DragProcessHandler {
 public:
     DragProcessHandler();
     virtual ~DragProcessHandler();
 
 protected:
+    /**
+     * This function is called whenever drag process starts. It usually happens
+     * when the user presses a mouse button.
+     * 
+     * It is guaranteed that <tt>finishDragProcess()</tt> will also be called.
+     */
     virtual void startDragProcess() {};
+
+    /**
+     * This function is called whenever drag starts. It usually happens when
+     * user moves the mouse far enough from the point where it was pressed,
+     * or if he holds it pressed long enough.
+     * 
+     * Note that this function may not be called if the user releases the
+     * mouse button before the drag could start.
+     * 
+     * It is guaranteed that if this function was called, then 
+     * <tt>finishDrag()</tt> will also be called.
+     */
     virtual void startDrag() {};
+
+    /**
+     * This function is called each time a mouse position changes while drag is
+     * in progress.
+     */
     virtual void drag() {};
+
+    /**
+     * This function is called whenever drag ends. It usually happens when the
+     * user releases the mouse button that started the drag.
+     * 
+     * If <tt>startDrag()</tt> was called, then it is guaranteed that this 
+     * function will also be called.
+     */
     virtual void finishDrag() {};
+
+    /**
+     * This function is called whenever drag process ends. It usually happens 
+     * right after the actual drag ends.
+     * 
+     * If <tt>startDragProcess()</tt> was called, then it is guaranteed that
+     * this function will also be called.
+     */
     virtual void finishDragProcess() {};
 
+    /**
+     * \returns                         Drag processor associated with this handler. 
+     */
     DragProcessor *processor() const {
         return m_processor;
     }
@@ -38,19 +83,40 @@ private:
 };
 
 
+/**
+ * Drag processor implements logic that is common to drag-based instruments such
+ * as hand scrolling instrument, dragging instrument, and even click and double
+ * click detection instruments.
+ * 
+ * Dragging is not a simple process and drag distance and drag time must be 
+ * taken into account. Drag processor takes care of all these problems
+ * abstracting away the gory details.
+ * 
+ * Drag processor presents several event handling function that are to be
+ * called from user code. Note that only one set of functions can be used for
+ * a single drag processor (e.g. a single drag processor cannot be used to
+ * track scene- and view-level dragging at the same time). This is not checked at
+ * runtime, so be careful.
+ * 
+ * User code is notified about state changes through the <tt>DragProcessHandler</tt>
+ * interface. It is guaranteed that no matter what happens, functions of this
+ * interface will be called in proper order. For example, if <tt>startDrag()</tt>
+ * function was called, then <tt>finishDrag()</tt> will also be called even
+ * if this drag processor or target surface is destroyed.
+ */
 class DragProcessor: public QObject, protected QnSceneUtility {
     Q_OBJECT;
 public:
     enum State {
-        WAITING,
-        PREPAIRING,
-        DRAGGING
+        WAITING,    /**< Waiting for the drag process to start. */
+        PREPAIRING, /**< Drag process started, waiting for actual drag to start. */
+        DRAGGING    /**< Drag is in progress. */
     };
 
     enum Flag {
         /** By default, drag processor will compress consecutive drag operations 
          * if scene and view mouse coordinates didn't change between them. 
-         * This flag disables this compression. */
+         * This flag disables the compression. */
         DONT_COMPRESS = 0x1 
     };
     Q_DECLARE_FLAGS(Flags, Flag);
@@ -89,41 +155,86 @@ public:
 
     void setHandler(DragProcessHandler *handler);
 
+    /**
+     * Resets this drag processor, moving it into <tt>WAITING</tt> state. 
+     */
     void reset();
 
+    /**
+     * \returns                         Position of the mouse pointer at the moment the 
+     *                                  mouse button that initiated this drag process was pressed, 
+     *                                  in screen coordinates.
+     */
     const QPoint &mousePressScreenPos() const {
         return m_mousePressScreenPos;
     }
 
+    /**
+     * \returns                         Position of the mouse pointer at the last call to <tt>drag()</tt> 
+     *                                  handler function, in screen coordinates.
+     *                                  When <tt>drag()</tt> is called for the first time, 
+     *                                  this function returns mouse press position.
+     */
     const QPoint &lastMouseScreenPos() const {
         return m_lastMouseScreenPos;
     }
 
+    /**
+     * \returns                         Current position of the mouse pointer, in screen coordinates.
+     */
     const QPoint &mouseScreenPos() const {
         return m_mouseScreenPos;
     }
 
+    /**
+     * \returns                         Current position of the mouse pointer, in viewport coordinates. 
+     */
     QPoint mouseViewportPos() const;
 
+    /**
+     * \returns                         Position of the mouse pointer at the moment the 
+     *                                  mouse button that initiated this drag process was pressed, 
+     *                                  in scene coordinates.
+     */
     const QPointF &mousePressScenePos() const {
         return m_mousePressScenePos;
     }
 
+    /**
+     * \returns                         Position of the mouse pointer at the last call to <tt>drag()</tt> 
+     *                                  handler function, in scene coordinates.
+     *                                  When <tt>drag()</tt> is called for the first time, 
+     *                                  this function returns mouse press position.
+     */
     const QPointF &lastMouseScenePos() const {
         return m_lastMouseScenePos;
     }
 
+    /**
+     * \returns                         Current position of the mouse pointer, in scene coordinates. 
+     */
     const QPointF &mouseScenePos() const {
         return m_mouseScenePos;
     }
 
+    /**
+     * \returns                         Current keyboard modifiers. 
+     */
     Qt::KeyboardModifiers modifiers() const {
         return m_modifiers;
     }
 
+    /**
+     * Note that this function returns NULL if current graphics view is 
+     * being destroyed or if non-view set of event handler function is used.
+     *
+     * \returns                         Current graphics view. 
+     */
     QGraphicsView *view() const {
         return m_view;
     }
+
+    /* Set of view-level event handler functions that are to be used from user code. */
 
     void mousePressEvent(QWidget *viewport, QMouseEvent *event);
 
@@ -133,9 +244,17 @@ public:
 
     void paintEvent(QWidget *viewport, QPaintEvent *event);
 
+    /**
+     * Note that this function returns NULL if current graphics scene is 
+     * being destroyed or if non-scene set of event handler function is used.
+     * 
+     * \returns                         Current graphics scene.
+     */
     QGraphicsScene *scene() const {
         return m_scene;
     }
+
+    /* Set of scene-level event handler functions that are to be used from user code. */
 
     void mousePressEvent(QGraphicsScene *scene, QGraphicsSceneMouseEvent *event);
 
@@ -143,9 +262,17 @@ public:
 
     void mouseReleaseEvent(QGraphicsScene *scene, QGraphicsSceneMouseEvent *event);
 
+    /**
+     * Note that this function returns NULL if current graphics item is 
+     * being destroyed or if non-item set of event handler function is used.
+     * 
+     * \returns                         Current graphics item.
+     */
     QGraphicsItem *item() const {
         return m_item;
     }
+    
+    /* Set of item-level event handler functions that are to be used from user code. */
 
     void mousePressEvent(QGraphicsItem *item, QGraphicsSceneMouseEvent *event);
 
@@ -212,7 +339,11 @@ private:
     /** Current drag state. */
     State m_state;
 
-    /** Counter that is used to detect nested transitions. */
+    /** Counter that is used to detect nested transitions.
+     *
+     * If a complex (two states long) transition is in progress, and handler
+     * for the transition to the first state triggered another transition, then 
+     * transition to the second state must not be performed. */
     int m_transitionCounter;
 
     /** Current keyboard modifiers. */
