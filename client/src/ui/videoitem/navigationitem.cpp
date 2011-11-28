69b2ca6cbea8b8825cbd969649e97961f5aa20d7
Roman Vasilenko <rvasilenko@networkoptix.com>
2011-11-28 18:31:43 +0400

    fixed spam of DATETIME_NOW jumps from slider
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/videoitem/navigationitem.cpp
+++ client/src/ui/videoitem/navigationitem.cpp
@@ -471,7 +471,7 @@ void NavigationItem::updateSlider()
         quint64 time = m_camera->getCurrentTime();
         if (time != AV_NOPTS_VALUE)
         {
-            m_currentTime = time/1000;
+            m_currentTime = time != DATETIME_NOW ? time/1000 : time;
             m_timeSlider->setCurrentValue(m_currentTime);
         }
         m_liveButton->setVisible(!reader->onPause() && m_camera->getCamCamDisplay()->isRealTimeSource());
@@ -544,6 +544,9 @@ void NavigationItem::onValueChanged(qint64 time)
 
     QnAbstractArchiveReader *reader = static_cast<QnAbstractArchiveReader*>(m_camera->getStreamreader());
     if (reader->isSkippingFrames())
+        return;
+
+    if (reader->isRealTimeSource() && m_timeSlider->isAtEnd())
         return;
 
     smartSeek(time);
