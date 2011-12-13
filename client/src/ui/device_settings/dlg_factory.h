b79e8ade7adaf2962c12726b70d7318293499da9
Alexander Fokin <afokin@networkoptix.com>
2011-12-13 18:07:12 +0400

    Menu continued.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/device_settings/dlg_factory.h
+++ client/src/ui/device_settings/dlg_factory.h
@@ -7,22 +7,60 @@
 class QnResource;
 class CLAbstractDeviceSettingsDlg;
 
+/**
+ * Interface for creating device-specific settings dialogs. 
+ */
 class CLAbstractDlgManufacture
 {
-protected:
-    virtual ~CLAbstractDlgManufacture() {}
-
 public:
+    /**
+     * \param dev                       Resource to create settings dialog for.
+     * \returns                         Newly created settings dialog, 
+     *                                  or NULL if it could not be created.
+     */
     virtual CLAbstractDeviceSettingsDlg *createDlg(QnResourcePtr dev) = 0;
+
+    /**
+     * \param dev                       Resource to check.
+     * \returns                         Whether this manufacture can create a
+     *                                  settings dialog for the given resource.
+     */
     virtual bool canProduceDlg(QnResourcePtr dev) const = 0;
+
+protected:
+    /**
+     * Virtual destructor.
+     */
+    virtual ~CLAbstractDlgManufacture() {}
 };
 
 
+/**
+ * Factory that can be used to create a settings dialog given a resource.
+ */
 class CLDeviceSettingsDlgFactory
 {
 public:
+    /**
+     * \param dev                       Resource to check.
+     * \returns                         Whether this factory can create a 
+     *                                  settings dialog for the given resource.
+     */
     static bool canCreateDlg(QnResourcePtr dev);
+
+    /**
+     * \param dev                       Resource to create settings dialog for.
+     * \returns                         Newly created settings dialog for the
+     *                                  given resource, or NULL if the dialog
+     *                                  could not be created.
+     */
     static CLAbstractDeviceSettingsDlg *createDlg(QnResourcePtr dev);
+
+    /**
+     * Registers a dialog manufacture with this factory.
+     * 
+     * \param manufacture               Manufacture to register.
+     */
     static void registerDlgManufacture(CLAbstractDlgManufacture *manufacture);
 };
 
