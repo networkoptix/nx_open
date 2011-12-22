38c7e8312079550eb717583d1553c116e5d2c678
Alexander Fokin <afokin@networkoptix.com>
2011-12-22 19:39:56 +0400

    Fixed a crash with improper proxy style ownership management.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/proxystyle.cpp
+++ client/src/ui/proxystyle.cpp
@@ -3,31 +3,54 @@
 #include <QtGui/QApplication>
 #include <QtGui/QStyleFactory>
 
+#include <utils/common/warnings.h>
+
+/* This object takes ownership of application styles. 
+ * 
+ * Proxy style cannot take ownership of application style because application
+ * style will be destroyed once the proxy style is destroyed.
+ * 
+ * In the same time, ownership of the application style must be transferred
+ * away from the application instance, since setting the global style to
+ * proxy style will delete the current global style unless it is not
+ * owned by the application instance.
+ *
+ * Hence this little hack.
+ */
+Q_GLOBAL_STATIC(QObject, styleStorage);
+
 ProxyStyle::ProxyStyle(QStyle *baseStyle)
-    : m_style(baseStyle)
+    : m_style(0)
 {
-    setBaseStyle(baseStyle ? baseStyle : QApplication::style());
+    setBaseStyle(baseStyle);
 }
 
 ProxyStyle::ProxyStyle(const QString &baseStyle, QObject *parent)
     : m_style(0)
 {
-    QStyle *style = QStyleFactory::create(baseStyle);
-    if (!style)
-        style = QApplication::style();
-    style->setParent(parent);
-    setBaseStyle(style);
+    setParent(parent);
+    setBaseStyle(QStyleFactory::create(baseStyle));
 }
 
 QStyle *ProxyStyle::baseStyle() const
 {
     if (!m_style)
-        const_cast<ProxyStyle *>(this)->setBaseStyle(QApplication::style());
+        const_cast<ProxyStyle *>(this)->setBaseStyle(0); /* Will reinit base style with QApplication's style. */
     return m_style;
 }
 
 void ProxyStyle::setBaseStyle(QStyle *style)
 {
-    m_style = style;
-    setParent(m_style);
+    if(style == this) {
+        qnWarning("Setting base of a proxy style to itself will lead to infinite recursion, falling back to windows style.");
+        style = QStyleFactory::create(QLatin1String("windows"));
+    }
+
+    if(style) {
+        m_style = style;
+        m_style->setParent(this);
+    } else {
+        m_style = QApplication::style();
+        m_style->setParent(styleStorage());
+    }
 }
