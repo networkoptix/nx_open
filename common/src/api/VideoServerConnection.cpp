0d20b08a81607c797f6def695b613a81a8960c30
Roman Vasilenko <rvasilenko@networkoptix.com>
2011-12-23 21:40:53 +0400

    added binary format for timePeriods. Every period record now 12 bytes (it was 60+ bytes for XML)
    <==== Mercurial data ====>
    Branch: master

--- common/src/api/VideoServerConnection.cpp
+++ common/src/api/VideoServerConnection.cpp
@@ -3,28 +3,11 @@
 #include "xsd_RecordedTimePeriods.h"
 #include "VideoSessionManager.h"
 
-namespace {
-    QnTimePeriodList parseRecordedTimePeriods(const QnApiRecordedTimePeriodsResponsePtr &reply) {
-        using namespace xsd::api::recordedTimePeriods;
-
-        QnTimePeriodList result;
-        for (RecordedTimePeriods::timePeriod_const_iterator i = reply->timePeriod().begin(); i != reply->timePeriod().end(); ++i)
-            result << QnTimePeriod(i->startTime(), i->duration());
-
-        return result;
-    }
-
-} // anonymous namespace
-
-void detail::QnVideoServerConnectionReplyProcessor::at_replyReceived(int status, const QnApiRecordedTimePeriodsResponsePtr &reply, int handle)
+void detail::QnVideoServerConnectionReplyProcessor::at_replyReceived(int status, const QnTimePeriodList &result, int handle)
 {
-    QnTimePeriodList result;
-    if(status == 0) 
-        result = parseRecordedTimePeriods(reply);
     emit finished(status, result, handle);
     deleteLater();
 }
-
 
 QnVideoServerConnection::QnVideoServerConnection(const QUrl &url, QObject *parent):
     QObject(parent),
@@ -44,6 +27,7 @@ QnRequestParamList QnVideoServerConnection::createParamList(const QnNetworkResou
     result << QnRequestParam("startTime", QString::number(startTimeUSec));
     result << QnRequestParam("endTime", QString::number(endTimeUSec));
     result << QnRequestParam("detail", QString::number(detail));
+    result << QnRequestParam("format", "bin");
     for (int i = 0; i < motionRegion.rects().size(); ++i)
     {
         QRect r = motionRegion.rects().at(i);
@@ -60,16 +44,13 @@ QnRequestParamList QnVideoServerConnection::createParamList(const QnNetworkResou
 QnTimePeriodList QnVideoServerConnection::recordedTimePeriods(const QnNetworkResourceList& list, qint64 startTimeMs, qint64 endTimeMs, qint64 detail, const QRegion& motionRegion)
 {
     QnTimePeriodList result;
-    QnApiRecordedTimePeriodsResponsePtr timePeriodList;
-    if (m_sessionManager->recordedTimePeriods(createParamList(list, startTimeMs, endTimeMs, detail, motionRegion), timePeriodList) == 0)
-        result = parseRecordedTimePeriods(timePeriodList);
-
+    int status = m_sessionManager->recordedTimePeriods(createParamList(list, startTimeMs, endTimeMs, detail, motionRegion), result);
     return result;
 }
 
-long QnVideoServerConnection::asyncRecordedTimePeriods(const QnNetworkResourceList& list, qint64 startTimeMs, qint64 endTimeMs, qint64 detail, QRegion motionRegion, QObject *target, const char *slot) {
+int QnVideoServerConnection::asyncRecordedTimePeriods(const QnNetworkResourceList& list, qint64 startTimeMs, qint64 endTimeMs, qint64 detail, QRegion motionRegion, QObject *target, const char *slot) {
     detail::QnVideoServerConnectionReplyProcessor *processor = new detail::QnVideoServerConnectionReplyProcessor();
     connect(processor, SIGNAL(finished(int, const QnTimePeriodList &, int)), target, slot);
 
-    return m_sessionManager->asyncRecordedTimePeriods(createParamList(list, startTimeMs, endTimeMs, detail, motionRegion), processor, SLOT(at_replyReceived(int, const QnApiRecordedTimePeriodsResponsePtr &, int)));
+    return m_sessionManager->asyncRecordedTimePeriods(createParamList(list, startTimeMs, endTimeMs, detail, motionRegion), processor, SLOT(at_replyReceived(int, const QnTimePeriodList&, int)));
 }
