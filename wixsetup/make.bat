c5114b30debc42e0cb60b5623a9d4aa08bd4c4b9
Ivan Vigasin <ivigasin@networkoptix.com>
2011-12-28 01:26:03 +0400

    revert commented lines
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/make.bat
+++ wixsetup/make.bat
@@ -4,23 +4,23 @@ call qtvars.bat
 set "CURRENTDIR=%cd%"
 
 cd ..\common
-rem @start /B /WAIT convert.py
+@start /B /WAIT convert.py
 
 cd ..\client
-rem @start /B /WAIT convert.py
+@start /B /WAIT convert.py
 
 cd ..\server
-rem @start /B /WAIT convert.py
+@start /B /WAIT convert.py
 
 cd ..\appserver
-rem @start /B /WAIT convert.py
-rem @start /B /WAIT setup.py build
+@start /B /WAIT convert.py
+@start /B /WAIT setup.py build
 
 cd %CURRENTDIR%
 
-rem MSBuild ..\common\src\common.vcproj /t:Rebuild /p:Configuration=Release
-rem MSBuild ..\client\src\client.vcproj /t:Rebuild /p:Configuration=Release
-rem MSBuild ..\server\src\server.vcproj /t:Rebuild /p:Configuration=Release
+MSBuild ..\common\src\common.vcproj /t:Rebuild /p:Configuration=Release
+MSBuild ..\client\src\client.vcproj /t:Rebuild /p:Configuration=Release
+MSBuild ..\server\src\server.vcproj /t:Rebuild /p:Configuration=Release
 
 MSBuild PropsCA\PropsCA.vcproj /t:Rebuild /p:Configuration=Release
 MSBuild EveAssocCA\EveAssocCA.vcproj /t:Rebuild /p:Configuration=Release
