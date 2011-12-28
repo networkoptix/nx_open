1584beca6dc6e81ef6294d093c9ad1c974b7f625
Konstantin Ritt <ritt.ks@gmail.com>
2011-12-28 23:00:45 +0200

    AppStyle & Skin: improvements
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/skin/appstyle_p.h
+++ client/src/ui/skin/appstyle_p.h
@@ -10,13 +10,17 @@ class AppStyle : public ProxyStyle
 public:
     AppStyle(const QString &baseStyle, QObject *parent = 0);
 
-    int pixelMetric(PixelMetric metric, const QStyleOption *option = 0, const QWidget *widget = 0) const;
-    int styleHint(StyleHint hint, const QStyleOption *option = 0, const QWidget *widget = 0, QStyleHintReturn *returnData = 0) const;
+    void drawComplexControl(ComplexControl control, const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget = 0) const;
+
     QRect subControlRect(ComplexControl control, const QStyleOptionComplex *option, SubControl subControl, const QWidget *widget = 0) const;
-    void drawComplexControl(ComplexControl control, const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget) const;
+
+    int styleHint(StyleHint hint, const QStyleOption *option = 0, const QWidget *widget = 0, QStyleHintReturn *returnData = 0) const;
+    int pixelMetric(PixelMetric metric, const QStyleOption *option = 0, const QWidget *widget = 0) const;
+
     QPixmap standardPixmap(StandardPixmap standardPixmap, const QStyleOption *option = 0, const QWidget *widget = 0) const;
 
     void polish(QApplication *application);
+
     void unpolish(QApplication *application);
 
 protected Q_SLOTS:
