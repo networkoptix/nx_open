f1c517d878aff03d63092c9eb1bd419a4ce7feff
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-01-04 22:50:05 +0400

    autotest scripts polished
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/test.bat
+++ wixsetup/test.bat
@@ -1,3 +1,6 @@
-call install.bat
-call "c:\Program Files (x86)\Network Optix\VMS\Client\client.exe" --test-timeout 5000 --test-resource-substring Server > test.txt
-call uninstall.bat
+SET VERSION=1.2.91
+
+del /F /P c:\records
+start /B /WAIT msiexec /I "bin\VMS-%VERSION%.msi" /qb- /Lv* "install.log" SERVER_HOST="localhost" SERVER_PORT="8000" SERVER_LOGIN="appserver" SERVER_PASSWORD="123" CLIENT_APPSERVER_HOST="localhost" CLIENT_APPSERVER_PORT="8000" CLIENT_APPSERVER_LOGIN="appserver" CLIENT_APPSERVER_PASSWORD="123" SERVER_DIRECTORY="c:\records"
+"%VS90COMNTOOLS%\..\..\..\Network Optix\VMS\Client\client.exe" --test-timeout 15000 --test-resource-substring Server > test.txt
+call msiexec /x "bin\VMS-%VERSION%.msi" /qn /l*v uninstall.log
