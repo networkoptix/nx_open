9c900021a4a52045afecd89eaa05b96b14aee274
Sergey Bystrov <sbystrov@networkoptix.com>
2012-01-10 00:26:43 +0400

    dlink almost here
    <==== Mercurial data ====>
    Branch: master

---
+++ common/src/plugins/resources/d-link/dlink_resource.h
@@ -0,0 +1,34 @@
+#ifndef dlink_resource_h_2215
+#define dlink_resource_h_2215
+
+
+#include "core/resourcemanagment/security_cam_resource.h"
+#include "core/resource/camera_resource.h"
+#include "utils/network/simple_http_client.h"
+#include "core/datapacket/mediadatapacket.h"
+
+
+class QnPlDlinkResource : public QnCameraResource
+{
+public:
+
+    static const char* MANUFACTURE;
+
+    QnPlDlinkResource();
+
+    virtual bool isResourceAccessible();
+
+    virtual bool updateMACAddress();
+
+    virtual QString manufacture() const;
+
+    virtual void setIframeDistance(int frames, int timems); // sets the distance between I frames
+
+protected:
+
+    virtual QnAbstractStreamDataProvider* createLiveDataProvider();
+
+    virtual void setCropingPhysical(QRect croping);
+};
+
+#endif //dlink_resource_h_2215
