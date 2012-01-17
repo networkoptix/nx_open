2b7f34e3393850258ba7c835238c4ecf082a336d
Konstantin Ritt <ritt.ks@gmail.com>
2012-01-17 22:01:08 +0200

    minor
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resources/archive/avi_files/ufile_protocol.cpp
+++ common/src/plugins/resources/archive/avi_files/ufile_protocol.cpp
@@ -1,31 +1,35 @@
 // This is URLPrococol to allow ffmpeg use files with non-ascii filenames
 
-static QSemaphore sem(4);
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtCore/QScopedPointer>
+#include <QtCore/QSemaphore>
 
+Q_GLOBAL_STATIC_WITH_ARGS(QSemaphore, semaphore, (4))
 
-class QnSemaphoreLocker
+class SemaphoreLocker
 {
 public:
-    inline QnSemaphoreLocker(QSemaphore *sem) : m_sem(sem)
-        { m_sem->acquire(); }
-    inline ~QnSemaphoreLocker()
-        { m_sem->release(); }
+    inline SemaphoreLocker(QSemaphore *sem) : m_sem(sem)
+    { m_sem->acquire(); }
+    inline ~SemaphoreLocker()
+    { m_sem->release(); }
 
 private:
     QSemaphore *m_sem;
 };
 
+
 static int ufile_open(URLContext *h, const char *filename, int flags)
 {
-    QnSemaphoreLocker sem_locker(&sem);
+    SemaphoreLocker locker(semaphore());
 
     av_strstart(filename, "ufile:", &filename);
 
-    QFile *f = new QFile(QString::fromUtf8(filename));
-    if (!f)
-    {
+    QScopedPointer<QFile> pFile(new QFile(QString::fromUtf8(filename)));
+    if (!pFile)
         return AVERROR(ENOMEM);
-    }
 
     QFile::OpenMode mode;
 
@@ -36,80 +40,68 @@ static int ufile_open(URLContext *h, const char *filename, int flags)
     else
         mode = QIODevice::ReadOnly;
 
-    if (mode & QIODevice::WriteOnly)  {
+    if (mode & QIODevice::WriteOnly) {
         QDir dir;
         dir.mkpath(QFileInfo(filename).absolutePath());
     }
 
-    if (!f->open(mode))
-    {
-        delete f;
+    if (!pFile->open(mode))
         return AVERROR(ENOENT);
-    }
 
-    h->priv_data = (void *)f;
+    h->priv_data = (void *)pFile.take();
 
     return 0;
 }
 
 static int ufile_close(URLContext *h)
 {
-    QnSemaphoreLocker sem_locker(&sem);
+    SemaphoreLocker locker(semaphore());
 
-    QFile *f = (QFile *) h->priv_data;
+    QFile *pFile = reinterpret_cast<QFile *>(h->priv_data);
 
-    if (f)
-    {
-        f->close();
-        delete f;
-    }
+    pFile->close();
+    delete pFile;
 
     return 0;
 }
 
 static int ufile_read(URLContext *h, unsigned char *buf, int size)
 {
-    QnSemaphoreLocker sem_locker(&sem);
+    SemaphoreLocker locker(semaphore());
 
-    QFile* pFile = ((QFile *) h->priv_data);
+    QFile *pFile = reinterpret_cast<QFile *>(h->priv_data);
+
     return (int) pFile->read((char*)buf, size);
 }
 
 static int ufile_write(URLContext *h, const unsigned char *buf, int size)
 {
-    QnSemaphoreLocker sem_locker(&sem);
+    SemaphoreLocker locker(semaphore());
 
-    QFile* pFile = ((QFile *) h->priv_data);
+    QFile *pFile = reinterpret_cast<QFile *>(h->priv_data);
+
     return (int) pFile->write((const char*)buf, size);
 }
 
-
 static int64_t ufile_seek(URLContext *h, int64_t pos, int whence)
 {
-    QnSemaphoreLocker sem_locker(&sem);
+    SemaphoreLocker locker(semaphore());
 
-    QFile* pFile = ((QFile *) h->priv_data);
+    QFile *pFile = reinterpret_cast<QFile *>(h->priv_data);
 
     switch (whence)
     {
-    case SEEK_SET:
-        return pFile->seek(pos);
-
-    case SEEK_CUR:
-        return pFile->seek(pFile->pos() + pos);
-
-    case (SEEK_END):
-        return pFile->seek(pFile->size() + pos);
-
-    case (AVSEEK_SIZE):
-        return pFile->size();
+    case SEEK_SET: return pFile->seek(pos);
+    case SEEK_CUR: return pFile->seek(pFile->pos() + pos);
+    case SEEK_END: return pFile->seek(pFile->size() + pos);
+    case AVSEEK_SIZE: return pFile->size();
+    default: break;
     }
 
     return AVERROR(ENOENT);
 }
 
-URLProtocol ufile_protocol =
-{
+URLProtocol ufile_protocol = {
     "ufile",
     ufile_open,
     ufile_read,
