bef3e5e5059cac32d11f8e0195794341e6080026
Alexander Fokin <afokin@networkoptix.com>
2012-01-24 03:41:17 +0400

    Changed the way margins affect viewport size.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/workbench/workbench_display.cpp
+++ client/src/ui/workbench/workbench_display.cpp
@@ -100,6 +100,7 @@ QnWorkbenchDisplay::QnWorkbenchDisplay(QnWorkbench *workbench, QObject *parent):
     m_viewportAnimator(NULL),
     m_curtainAnimator(NULL),
     m_mode(QnWorkbench::VIEWING),
+    m_marginFlags(MARGINS_AFFECT_POSITION | MARGINS_AFFECT_SIZE),
     m_frontZ(0.0),
     m_dummyScene(new QGraphicsScene(this))
 {
@@ -610,9 +611,27 @@ void QnWorkbenchDisplay::setViewportMargins(const QMargins &margins) {
         return;
 
     m_viewportMargins = margins;
-    m_viewportAnimator->setViewportMargins(margins);
+    
+    if(m_marginFlags & MARGINS_AFFECT_SIZE)
+        m_viewportAnimator->setViewportMargins(margins);
 
     synchronizeSceneBoundsExtension();
+}
+
+QnWorkbenchDisplay::MarginFlags QnWorkbenchDisplay::marginFlags() const {
+    return m_marginFlags;
+}
+
+void QnWorkbenchDisplay::setMarginFlags(MarginFlags flags) {
+    if(m_marginFlags == flags)
+        return;
+
+    QMargins margins = m_viewportMargins;
+    setViewportMargins(QMargins(0, 0, 0, 0));
+
+    m_marginFlags = flags;
+
+    setViewportMargins(margins);
 }
 
 
@@ -855,22 +874,29 @@ void QnWorkbenchDisplay::synchronizeSceneBounds() {
 }
 
 void QnWorkbenchDisplay::synchronizeSceneBoundsExtension() {
+    if(m_marginFlags == 0)
+        return;
+
     QSizeF viewportSize = m_view->viewport()->size();
     MarginsF positionExtension = cwiseDiv(m_viewportMargins, viewportSize);
 
-    QnWorkbenchItem *zoomedItem = m_itemByRole[QnWorkbench::ZOOMED];
-    if(zoomedItem != NULL) {
-        m_boundingInstrument->setPositionBoundsExtension(m_view, positionExtension);
-    } else {
-        m_boundingInstrument->setPositionBoundsExtension(m_view, positionExtension + MarginsF(0.5, 0.5, 0.5, 0.5));
+    if(m_marginFlags & MARGINS_AFFECT_POSITION) {
+        QnWorkbenchItem *zoomedItem = m_itemByRole[QnWorkbench::ZOOMED];
+        if(zoomedItem != NULL) {
+            m_boundingInstrument->setPositionBoundsExtension(m_view, positionExtension);
+        } else {
+            m_boundingInstrument->setPositionBoundsExtension(m_view, positionExtension + MarginsF(0.5, 0.5, 0.5, 0.5));
+        }
     }
 
-    QSizeF sizeExtension = sizeDelta(positionExtension);
-    sizeExtension = cwiseDiv(sizeExtension, QSizeF(1.0, 1.0) - sizeExtension);
+    if(m_marginFlags & MARGINS_AFFECT_SIZE) {
+        QSizeF sizeExtension = sizeDelta(positionExtension);
+        sizeExtension = cwiseDiv(sizeExtension, QSizeF(1.0, 1.0) - sizeExtension);
 
-    m_boundingInstrument->setSizeBoundsExtension(m_view, sizeExtension, sizeExtension);
-    if(m_itemByRole[QnWorkbench::ZOOMED] == NULL)
-        m_boundingInstrument->stickScale(m_view);
+        m_boundingInstrument->setSizeBoundsExtension(m_view, sizeExtension, sizeExtension);
+        if(m_itemByRole[QnWorkbench::ZOOMED] == NULL)
+            m_boundingInstrument->stickScale(m_view);
+    }
 }
 
 void QnWorkbenchDisplay::synchronizeRaisedGeometry() {
