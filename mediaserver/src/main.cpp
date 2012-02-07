f3cd3796cdb331cc56cd8d90dc1572a85128b1cb
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-02-07 20:05:42 +0400

    sometimes on mediaServer startup unread ResourceTypes access is occured. Possible fixed
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/main.cpp
+++ mediaserver/src/main.cpp
@@ -261,23 +261,7 @@ int serverMain(int argc, char *argv[])
 
     defaultMsgHandler = qInstallMsgHandler(myMsgHandler);
 
-    // Create and start SessionManager
-    SessionManager* sm = SessionManager::instance();
-
-    QThread *thread = new QThread();
-    sm->moveToThread(thread);
-
-    QObject::connect(sm, SIGNAL(destroyed()), thread, SLOT(quit()));
-    QObject::connect(thread , SIGNAL(finished()), thread, SLOT(deleteLater()));
-
-    thread->start();
-    //
-
-    QnResource::startCommandProc();
-
-    QnResourcePool::instance(); // to initialize net state;
     ffmpegInit();
-
 
     // ------------------------------------------
 #ifdef TEST_RTSP_SERVER
@@ -397,6 +381,10 @@ public:
 
         if (needToStop())
             return;
+
+        QnResource::startCommandProc();
+
+        QnResourcePool::instance(); // to initialize net state;
 
         QString appserverHostString = settings.value("appserverHost", QLatin1String(DEFAULT_APPSERVER_HOST)).toString();
 
