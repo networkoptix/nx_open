ffb02a38e85282432e5dbc09d86ce34f02f4c8ef
Alexander Fokin <afokin@networkoptix.com>
2012-02-19 06:00:52 +0400

    Some refactoring in styles.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/style/app_style.cpp
+++ client/src/ui/style/app_style.cpp
@@ -1,63 +1,25 @@
 #include "app_style.h"
-
 #include <QApplication>
 #include <QPainter>
 #include <QStyleOption>
 #include <QMenu>
 #include <QAction>
-
+#include <utils/common/scoped_painter_rollback.h>
 #include "skin.h"
 
-static const float global_dialog_opacity = 0.9f;
-static const float global_menu_opacity = 0.8f;
-
+// -------------------------------------------------------------------------- //
+// AppStyle
+// -------------------------------------------------------------------------- //
 AppStyle::AppStyle(const QString &baseStyle, QObject *parent): 
     base_type(baseStyle, parent)
 {}
 
-void AppStyle::drawComplexControl(ComplexControl control, const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget) const
-{
+void AppStyle::drawComplexControl(ComplexControl control, const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget) const {
     switch (control) {
-#ifndef QT_NO_SLIDER
     case CC_Slider:
-        if (widget && widget->inherits("QAbstractSlider")) { /* For old good widgets. */
-            base_type::drawComplexControl(control, option, painter, widget);
-            break;
-        }
-
-        /* Bespin's slider painting is way to slow, so we do it our way. */
-        if (const QStyleOptionSlider *sliderOption = qstyleoption_cast<const QStyleOptionSlider *>(option)) {
-            if (sliderOption->orientation != Qt::Horizontal) {
-                qWarning("AppStyle: Non-horizontal sliders are not implemented. Falling back to the default painting.");
-                base_type::drawComplexControl(control, option, painter, widget);
-                break;
-            }
-
-            const QRect grooveRect = subControlRect(CC_Slider, option, SC_SliderGroove, widget);
-            QRect handleRect = subControlRect(CC_Slider, option, SC_SliderHandle, widget);
-            handleRect.setSize(handleRect.size() * 2);
-            handleRect.moveTopLeft(handleRect.topLeft() - QPoint(handleRect.width(), handleRect.height()) / 4);
-
-            const bool hovered = (option->state & State_Enabled) && (option->activeSubControls & SC_SliderHandle);
-
-            QPixmap grooveBorderPic = Skin::pixmap(QLatin1String("slider_groove_lborder.png"), grooveRect.size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            QPixmap grooveBodyPic = Skin::pixmap(QLatin1String("slider_groove_body.png"), grooveRect.size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            QPixmap handlePic = Skin::pixmap(hovered ? QLatin1String("slider_handle_active.png") : QLatin1String("slider_handle.png"), handleRect.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
-
-            painter->drawTiledPixmap(grooveRect.adjusted(grooveBorderPic.width(), 0, -grooveBorderPic.width(), 0), grooveBodyPic);
-            painter->drawPixmap(grooveRect.topLeft(), grooveBorderPic);
-            {
-                QTransform oldTransform = painter->transform();
-                painter->translate(grooveRect.left() + grooveRect.width(), grooveRect.top());
-                painter->scale(-1.0, 1.0);
-                painter->drawPixmap(0, 0, grooveBorderPic);
-                painter->setTransform(oldTransform);
-            }
-
-            painter->drawPixmap(handleRect, handlePic);
-        }
+        if(drawSliderComplexControl(option, painter, widget))
+            return;
         break;
-#endif // QT_NO_SLIDER
     case CC_TitleBar:
         {
             /* Draw background in order to avoid painting artifacts. */
@@ -67,50 +29,48 @@ void AppStyle::drawComplexControl(ComplexControl control, const QStyleOptionComp
                 painter->fillRect(option->rect, bgColor);
             }
         }
-        /* FALL THROUGH. */
+        break;
     default:
-        base_type::drawComplexControl(control, option, painter, widget);
+        break;
     }
+
+    base_type::drawComplexControl(control, option, painter, widget);
 }
 
 void AppStyle::drawControl(ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const {
     switch(element) {
     case CE_MenuItem:
-        /* There are cases when we want an action to be checkable, but do not want the checkbox displayed in the menu. 
-         * So we introduce an internal property for this. */
-        if(const QStyleOptionMenuItem *itemOption = qstyleoption_cast<const QStyleOptionMenuItem *>(option)) {
-            const QMenu *menu = qobject_cast<const QMenu *>(widget);
-            if(!menu) {
-                base_type::drawControl(element, option, painter, widget);
-            } else {
-                QAction *action = menu->actionAt(option->rect.center());
-                if(action && action->property(hideCheckBoxInMenuPropertyName).value<bool>()) {
-                    QStyleOptionMenuItem::CheckType checkType = itemOption->checkType;
-                    QStyleOptionMenuItem *localOption = const_cast<QStyleOptionMenuItem *>(itemOption);
-                    localOption->checkType = QStyleOptionMenuItem::NotCheckable;
-                    base_type::drawControl(element, option, painter, widget);
-                    localOption->checkType = checkType;
-                } else {
-                    base_type::drawControl(element, option, painter, widget);
-                }
-            }
-        }
+        if(drawMenuItemControl(option, painter, widget))
+            return;
         break;
     default:
-        base_type::drawControl(element, option, painter, widget);
+        break;
     }
+
+    base_type::drawControl(element, option, painter, widget);
 }
 
-int AppStyle::styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const
-{
+void AppStyle::drawPrimitive(PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const {
+    switch(element) {
+    case PE_IndicatorTabClose:
+        if(drawTabClosePrimitive(option, painter, widget))
+            return;
+        break;
+    default:
+        break;
+    }
+
+    base_type::drawPrimitive(element, option, painter, widget);
+}
+
+int AppStyle::styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const {
     if (hint == QStyle::SH_ToolTipLabel_Opacity)
         return 255;
 
     return base_type::styleHint(hint, option, widget, returnData);
 }
 
-void AppStyle::polish(QApplication *application)
-{
+void AppStyle::polish(QApplication *application) {
     base_type::polish(application);
 
     QFont menuFont;
@@ -119,24 +79,101 @@ void AppStyle::polish(QApplication *application)
     application->setFont(menuFont, "QMenu");
 }
 
-void AppStyle::unpolish(QApplication *application)
-{
+void AppStyle::unpolish(QApplication *application) {
     application->setFont(QFont(), "QMenu");
 
     base_type::unpolish(application);
 }
 
-void AppStyle::polish(QWidget *widget)
-{
+void AppStyle::polish(QWidget *widget) {
     base_type::polish(widget);
 }
 
-void AppStyle::unpolish(QWidget *widget)
-{
+void AppStyle::unpolish(QWidget *widget) {
     base_type::unpolish(widget);
 }
 
+bool AppStyle::drawMenuItemControl(const QStyleOption *option, QPainter *painter, const QWidget *widget) const {
+    const QStyleOptionMenuItem *itemOption = qstyleoption_cast<const QStyleOptionMenuItem *>(option);
+    if(!itemOption)
+        return false;
+    
+    const QMenu *menu = qobject_cast<const QMenu *>(widget);
+    if(!menu) 
+        return false;
 
+    /* There are cases when we want an action to be checkable, but do not want the checkbox displayed in the menu. 
+     * So we introduce an internal property for this. */
+    QAction *action = menu->actionAt(option->rect.center());
+    if(!action || !action->property(hideCheckBoxInMenuPropertyName).value<bool>()) 
+        return false;
+    
+    QStyleOptionMenuItem::CheckType checkType = itemOption->checkType;
+    QStyleOptionMenuItem *localOption = const_cast<QStyleOptionMenuItem *>(itemOption);
+    localOption->checkType = QStyleOptionMenuItem::NotCheckable;
+    base_type::drawControl(CE_MenuItem, option, painter, widget);
+    localOption->checkType = checkType;
+    return true;
+}
+
+bool AppStyle::drawSliderComplexControl(const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget) const {
+    /* Bespin's slider painting is way to slow, so we do it our way. */
+    const QStyleOptionSlider *sliderOption = qstyleoption_cast<const QStyleOptionSlider *>(option);
+    if (!sliderOption) 
+        return false;
+    
+    if (sliderOption->orientation != Qt::Horizontal) 
+        return false; /* Non-horizontal sliders are not implemented. */
+    
+    const QRect grooveRect = subControlRect(CC_Slider, option, SC_SliderGroove, widget);
+    QRect handleRect = subControlRect(CC_Slider, option, SC_SliderHandle, widget);
+    handleRect.setSize(handleRect.size() * 2);
+    handleRect.moveTopLeft(handleRect.topLeft() - QPoint(handleRect.width(), handleRect.height()) / 4);
+
+    const bool hovered = (option->state & State_Enabled) && (option->activeSubControls & SC_SliderHandle);
+
+    QPixmap grooveBorderPic = Skin::pixmap(QLatin1String("slider_groove_lborder.png"), grooveRect.size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QPixmap grooveBodyPic = Skin::pixmap(QLatin1String("slider_groove_body.png"), grooveRect.size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QPixmap handlePic = Skin::pixmap(hovered ? QLatin1String("slider_handle_active.png") : QLatin1String("slider_handle.png"), handleRect.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    painter->drawTiledPixmap(grooveRect.adjusted(grooveBorderPic.width(), 0, -grooveBorderPic.width(), 0), grooveBodyPic);
+    painter->drawPixmap(grooveRect.topLeft(), grooveBorderPic);
+    {
+        QTransform oldTransform = painter->transform();
+        painter->translate(grooveRect.left() + grooveRect.width(), grooveRect.top());
+        painter->scale(-1.0, 1.0);
+        painter->drawPixmap(0, 0, grooveBorderPic);
+        painter->setTransform(oldTransform);
+    }
+
+    painter->drawPixmap(handleRect, handlePic);
+    return true;
+}
+
+bool AppStyle::drawTabClosePrimitive(const QStyleOption *option, QPainter *painter, const QWidget *widget) const {
+    bool sunken = option->state & State_Sunken;
+    bool hover = (option->state & State_Enabled) && (option->state & State_MouseOver);
+    if (sunken) 
+        hover = false;
+
+    QRectF rect = option->rect;
+    qreal d = sunken ? 0.25 : (hover ? 0.1 : 0.2);
+    rect.adjust(rect.width() * d, rect.height() * d, rect.width() * -d, rect.height() * -d);
+
+    QBrush b = option->palette.text(); //Colors::mid( CCOLOR(tab.std, Bg), CCOLOR(tab.std, Fg), 3, 1+hover );
+
+    QnScopedPainterAntialiasingRollback antialiasingRollback(painter, true);
+    QnScopedPainterPenRollback penRollback(painter, QPen(b, qMin(rect.width(), rect.height()) * 0.2));
+    painter->drawEllipse(rect);
+    return true;
+
+    return false;
+}
+
+
+// -------------------------------------------------------------------------- //
+// AppProxyStyle
+// -------------------------------------------------------------------------- //
 AppProxyStyle::AppProxyStyle(QStyle *style): 
     QProxyStyle(style)
 {}
@@ -317,9 +354,9 @@ QPixmap AppProxyStyle::standardPixmap(StandardPixmap standardPixmap, const QStyl
     case SP_MediaPause:
         pm = Skin::pixmap(QLatin1String("pause_grey.png"));
         break;
-//    case SP_MediaStop:
-//        pm = Skin::pixmap(QLatin1String("stop_grey.png"));
-//        break;
+    case SP_MediaStop:
+        pm = Skin::pixmap(QLatin1String("stop_grey.png"));
+        break;
     case SP_MediaSeekForward:
         pm = Skin::pixmap(QLatin1String("forward_grey.png"));
         break;
@@ -355,7 +392,7 @@ QIcon AppProxyStyle::standardIconImplementation(StandardPixmap standardIcon, con
     case SP_TitleBarMinButton:
         icon.addPixmap(Skin::pixmap(QLatin1String("decorations/minimize.png"), QSize(16, 16), Qt::KeepAspectRatio));
         break;
-//    case SP_TitleBarMaxButton: // ###
+//    case SP_TitleBarMaxButton:
 //        icon.addPixmap(Skin::pixmap(QLatin1String("decorations/maximize.png"), QSize(16, 16), Qt::KeepAspectRatio));
 //        break;
     case SP_TitleBarCloseButton:
@@ -370,7 +407,7 @@ QIcon AppProxyStyle::standardIconImplementation(StandardPixmap standardIcon, con
     case SP_TitleBarUnshadeButton:
         icon.addPixmap(Skin::pixmap(QLatin1String("decorations/unfullscreen.png"), QSize(16, 16), Qt::KeepAspectRatio));
         break;
-//    case SP_TitleBarContextHelpButton: // ###
+//    case SP_TitleBarContextHelpButton:
 //        icon.addPixmap(Skin::pixmap(QLatin1String("decorations/help.png"), QSize(16, 16), Qt::KeepAspectRatio));
 //        break;
     case SP_ArrowLeft:
@@ -409,10 +446,10 @@ QIcon AppProxyStyle::standardIconImplementation(StandardPixmap standardIcon, con
         icon.addPixmap(Skin::pixmap(QLatin1String("pause_grey.png"), QSize(16, 16), Qt::KeepAspectRatio));
         icon.addPixmap(Skin::pixmap(QLatin1String("pause_blue.png"), QSize(16, 16), Qt::KeepAspectRatio), QIcon::Active);
         break;
-//    case SP_MediaStop:
-//        icon.addPixmap(Skin::pixmap(QLatin1String("stop_grey.png"), QSize(16, 16), Qt::KeepAspectRatio));
-//        icon.addPixmap(Skin::pixmap(QLatin1String("stop_blue.png"), QSize(16, 16), Qt::KeepAspectRatio), QIcon::Active);
-//        break;
+    case SP_MediaStop:
+        icon.addPixmap(Skin::pixmap(QLatin1String("stop_grey.png"), QSize(16, 16), Qt::KeepAspectRatio));
+        icon.addPixmap(Skin::pixmap(QLatin1String("stop_blue.png"), QSize(16, 16), Qt::KeepAspectRatio), QIcon::Active);
+        break;
     case SP_MediaSeekForward:
         icon.addPixmap(Skin::pixmap(QLatin1String("forward_grey.png"), QSize(16, 16), Qt::KeepAspectRatio));
         icon.addPixmap(Skin::pixmap(QLatin1String("forward_blue.png"), QSize(16, 16), Qt::KeepAspectRatio), QIcon::Active);
@@ -443,3 +480,4 @@ QIcon AppProxyStyle::standardIconImplementation(StandardPixmap standardIcon, con
 
     return icon;
 }
+
--- client/src/ui/style/app_style.h
+++ client/src/ui/style/app_style.h
@@ -14,6 +14,7 @@ public:
 
     virtual void drawComplexControl(ComplexControl control, const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget = 0) const override;
     virtual void drawControl(ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget = 0) const override;
+    virtual void drawPrimitive(PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget = 0) const override;
 
     virtual int styleHint(StyleHint hint, const QStyleOption *option = 0, const QWidget *widget = 0, QStyleHintReturn *returnData = 0) const override;
 
@@ -21,6 +22,11 @@ public:
     virtual void unpolish(QApplication *application) override;
     virtual void polish(QWidget *widget) override;
     virtual void unpolish(QWidget *widget) override;
+
+protected:
+    bool drawMenuItemControl(const QStyleOption *option, QPainter *painter, const QWidget *widget) const;
+    bool drawSliderComplexControl(const QStyleOptionComplex *option, QPainter *painter, const QWidget *widget) const;
+    bool drawTabClosePrimitive(const QStyleOption *option, QPainter *painter, const QWidget *widget) const;
 };
 
 
