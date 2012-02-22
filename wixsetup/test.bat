c0484cb59ce47218f02c16ffecf4a43a0ef7e1f7
Ivan Vigasin <ivigasin@networkoptix.com>
2012-02-22 21:16:14 +0400

    appserver -> admin installer fixes
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/test.bat
+++ wixsetup/test.bat
@@ -8,7 +8,7 @@ echo version = %VERSION%
 
 del /F /P c:\records
 
-start /B /WAIT msiexec /I "bin\VMS-%VERSION%.msi" /qb- /Lv* "install.log" SERVER_HOST="localhost" SERVER_PORT="8000" SERVER_LOGIN="appserver" SERVER_PASSWORD="123" CLIENT_APPSERVER_HOST="localhost" CLIENT_APPSERVER_PORT="8000" CLIENT_APPSERVER_LOGIN="appserver" CLIENT_APPSERVER_PASSWORD="123" SERVER_DIRECTORY="c:\records"
+start /B /WAIT msiexec /I "bin\VMS-%VERSION%.msi" /qb- /Lv* "install.log" SERVER_HOST="localhost" SERVER_PORT="8000" SERVER_LOGIN="admin" SERVER_PASSWORD="123" CLIENT_APPSERVER_HOST="localhost" CLIENT_APPSERVER_PORT="8000" CLIENT_APPSERVER_LOGIN="admin" CLIENT_APPSERVER_PASSWORD="123" SERVER_DIRECTORY="c:\records"
 echo installer errorlevel is %ERRORLEVEL%
 
 "%VS90COMNTOOLS%\..\..\..\Network Optix\VMS\Client\client.exe" --test-timeout 60000 --test-resource-substring Server > test.txt
