882c7e026099fddf060023e2e23a3e39b3d9189c
Alexander Fokin <afokin@networkoptix.com>
2012-03-08 03:40:10 +0400

    Forgotten files...
    <==== Mercurial data ====>
    Branch: master

---
+++ client/src/ui/widgets/tree_view.cpp
@@ -0,0 +1,40 @@
+#include "tree_view.h"
+#include <QTimerEvent>
+#include <QDragMoveEvent>
+
+QnTreeView::QnTreeView(QWidget *parent): 
+    QTreeView(parent)
+{}
+
+QnTreeView::~QnTreeView() {
+    return;
+}
+
+void QnTreeView::dragMoveEvent(QDragMoveEvent *event) {
+    if (autoExpandDelay() >= 0) {
+        m_dragMovePos = event->pos();
+        m_openTimer.start(autoExpandDelay(), this);
+    }
+
+    /* Important! Skip QTreeView's implementation. */
+    QAbstractItemView::dragMoveEvent(event);
+}
+
+void QnTreeView::dragLeaveEvent(QDragLeaveEvent *event) {
+    m_openTimer.stop();
+
+    QTreeView::dragLeaveEvent(event);
+}
+
+void QnTreeView::timerEvent(QTimerEvent *event) {
+    if (event->timerId() == m_openTimer.timerId()) {
+        QPoint pos = m_dragMovePos;
+        if (state() == QAbstractItemView::DraggingState && viewport()->rect().contains(pos)) {
+            QModelIndex index = indexAt(pos);
+            setExpanded(index, !isExpanded(index));
+        }
+        m_openTimer.stop();
+    }
+
+    QTreeView::timerEvent(event);
+}
---
+++ client/src/ui/widgets/tree_view.h
@@ -0,0 +1,29 @@
+#ifndef QN_TREE_VIEW_H
+#define QN_TREE_VIEW_H
+
+#include <QTreeView>
+#include <QBasicTimer>
+
+/**
+ * This class fixes some bugs in <tt>QTreeView</tt> related to drag and drop
+ * handling when embedded into a <tt>QGraphicsProxyWidget</tt>.
+ */
+class QnTreeView: public QTreeView {
+    Q_OBJECT;
+public:
+    QnTreeView(QWidget *parent = NULL);
+
+    virtual ~QnTreeView();
+
+protected:
+    virtual void dragMoveEvent(QDragMoveEvent *event) override;
+    virtual void dragLeaveEvent(QDragLeaveEvent *event) override;
+    virtual void timerEvent(QTimerEvent *event) override;
+
+private:
+    QBasicTimer m_openTimer;
+    QPoint m_dragMovePos;
+};
+
+
+#endif // QN_TREE_VIEW_H
