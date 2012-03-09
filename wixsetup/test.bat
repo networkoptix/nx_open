149f214053326a40c9457fc1cd03e042978cd3af
Unknown User <john.doe@blackhole.com>
2012-03-09 10:56:17 +0400

    test wait time adjusted
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/test.bat
+++ wixsetup/test.bat
@@ -6,12 +6,12 @@ SET VERSION=%APPLICATION_VERSION%.%BUILD_NUMBER%
 
 echo version = %VERSION%
 
-del /F /P c:\records
+rmdir /s /q "c:\records"
 
 start /B /WAIT msiexec /I "bin\VMS-%VERSION%.msi" /qb- /Lv* "install.log" SERVER_HOST="localhost" SERVER_PORT="8000" SERVER_LOGIN="admin" SERVER_PASSWORD="123" CLIENT_APPSERVER_HOST="localhost" CLIENT_APPSERVER_PORT="8000" CLIENT_APPSERVER_LOGIN="admin" CLIENT_APPSERVER_PASSWORD="123" SERVER_DIRECTORY="c:\records"
 echo installer errorlevel is %ERRORLEVEL%
 
-"%VS90COMNTOOLS%\..\..\..\Network Optix\VMS\Client\client.exe" --test-timeout 60000 --test-resource-substring Server > test.txt
+"%VS90COMNTOOLS%\..\..\..\Network Optix\VMS\Client\client.exe" --test-timeout 180000 --test-resource-substring Server > test.txt
 SET TESTRESULT=%ERRORLEVEL%
 echo test errorlevel is %TESTRESULT%
 
