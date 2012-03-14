d2ee8fc1fdf85c7dd557d7a0806df8502ba0d2c6
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-03-13 19:41:27 -0800

    icon size is 18
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/style/noptix_style.cpp
+++ client/src/ui/style/noptix_style.cpp
@@ -43,8 +43,7 @@ QnNoptixStyle::QnNoptixStyle(QStyle *style):
 
 int QnNoptixStyle::pixelMetric(PixelMetric metric, const QStyleOption *option, const QWidget *widget) const {
     if(metric == PM_ToolBarIconSize)
-        return 15;
-
+        return 18;
     return base_type::pixelMetric(metric, option, widget);
 }
 
