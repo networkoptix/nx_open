0898882b1e99d24ab19b9821679d5492e1233b0a
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-03-21 17:58:50 -0700

    convert is changed based on the other components
    <==== Mercurial data ====>
    Branch: master

--- testcamera/convert.py
+++ testcamera/convert.py
@@ -16,6 +16,7 @@ sys.path.insert(0, '../common')
 from convert import index_dirs, rmtree, setup_ffmpeg, instantiate_pro, copy_files, BUILDLIB, setup_tools, platform
 from convert import gen_env_sh
 from convert import convert as convert_common
+from common_version import *
 
 FFMPEG_VERSION = '2011-08-29'
 
@@ -23,32 +24,38 @@ EXCLUDE_DIRS = ('.svn', 'dxva')
 EXCLUDE_FILES = ('dxva', 'moc_', 'qrc_', 'StdAfx')
 
 def gen_version_h():
+    ORGANIZATION_NAME, APPLICATION_VERSION, BUILD_NUMBER, REVISION, FFMPEG_VERSION = set_env()
+    print >> sys.stderr, 'Revision is %s' % REVISION
+    print >> sys.stderr, 'FFMPEG version is %s' % FFMPEG_VERSION
+
     version_h = open('src/version.h', 'w')
     print >> version_h, '#ifndef UNIVERSAL_CLIENT_VERSION_H_'
     print >> version_h, '#define UNIVERSAL_CLIENT_VERSION_H_'
     print >> version_h, '// This file is generated. Go to version.py'
     print >> version_h, 'static const char* ORGANIZATION_NAME="%s";' % ORGANIZATION_NAME
     print >> version_h, 'static const char* APPLICATION_NAME="%s";' % APPLICATION_NAME
-    print >> version_h, 'static const char* APPLICATION_VERSION="%s";' % APPLICATION_VERSION
+    print >> version_h, 'static const char* APPLICATION_VERSION="%s.%s";' % (APPLICATION_VERSION, BUILD_NUMBER)
+    print >> version_h, 'const char* const APPLICATION_REVISION="%s";' % REVISION
+    print >> version_h, 'const char* const FFMPEG_VERSION="%s";' % FFMPEG_VERSION	
     print >> version_h, ''
 
     print >> version_h, '// There constans are here for windows resouce file.'
 
     app_version_commas = APPLICATION_VERSION.replace('.', ',')
-    print >> version_h, '#define VER_FILEVERSION             %s,0' % app_version_commas
-    print >> version_h, '#define VER_FILEVERSION_STR         "%s.0\0"' % APPLICATION_VERSION
+    print >> version_h, '#define VER_FILEVERSION             %s,%s' % (app_version_commas, BUILD_NUMBER)
+    print >> version_h, '#define VER_FILEVERSION_STR         "%s.%s"' % (APPLICATION_VERSION, BUILD_NUMBER)
 
-    print >> version_h, '#define VER_PRODUCTVERSION          %s' % app_version_commas
-    print >> version_h, '#define VER_PRODUCTVERSION_STR      "%s\0"' % APPLICATION_VERSION
+    print >> version_h, '#define VER_PRODUCTVERSION          %s.%s' % (app_version_commas, BUILD_NUMBER)
+    print >> version_h, '#define VER_PRODUCTVERSION_STR      "%s.%s"' % (APPLICATION_VERSION, BUILD_NUMBER)
 
     print >> version_h, '#define VER_COMPANYNAME_STR         "%s"' % ORGANIZATION_NAME
     print >> version_h, '#define VER_FILEDESCRIPTION_STR     "%s"' % APPLICATION_NAME
     print >> version_h, '#define VER_INTERNALNAME_STR        "%s"' % APPLICATION_NAME
-    print >> version_h, '#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2011 %s"' % ORGANIZATION_NAME
+    print >> version_h, '#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2012 %s"' % ORGANIZATION_NAME
     print >> version_h, '#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"'
     print >> version_h, '#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR'
-    print >> version_h, '#define VER_ORIGINALFILENAME_STR    "EvePlayer-Beta.exe"'
-    print >> version_h, '#define VER_PRODUCTNAME_STR         "EvePlayer"'
+    print >> version_h, '#define VER_ORIGINALFILENAME_STR    "mediaserver.exe"'
+    print >> version_h, '#define VER_PRODUCTNAME_STR         "HD Witness"'
 
     print >> version_h, '#define VER_COMPANYDOMAIN_STR       "networkoptix.com"'
     print >> version_h, '#endif // UNIVERSAL_CLIENT_VERSION_H_'
