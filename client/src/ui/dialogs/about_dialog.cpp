63485c13c3c53db623235a21b2226730e001b35c
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-03-22 22:43:55 -0700

    1) About dialog changed
    2) Ligin dialog - indends adjusted
    3) traytool renamings
    
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/dialogs/about_dialog.cpp
+++ client/src/ui/dialogs/about_dialog.cpp
@@ -64,8 +64,8 @@ void QnAboutDialog::retranslateUi()
             "<b>Qt v.%3</b> - Copyright (c) 2012 Nokia Corporation.<br />\n"
             "<b>FFMpeg %4</b> - Copyright (c) 2000-2012 the FFmpeg developers.<br />\n"
             "<b>Color Picker v2.6 Qt Solution</b> - Copyright (c) 2009 Nokia Corporation.<br />"
-            "<b>LAME</b> - Copyright (c) 1998-2012 the LAME developers.<br />"
-            "<b>OpenAL 1.1</b> - Copyright (c) 2000-2006 Creative Labs.<br />"
+            "<b>LAME 3.99.0</b> - Copyright (c) 1998-2012 the LAME developers.<br />"
+            "<b>OpenAL Soft 1.13</b> - Copyright (c) 2000-2006 Creative Labs.<br />"
         ).
         arg(QString::fromLatin1(ORGANIZATION_NAME) + QLatin1String("(tm)")).
         arg(QString::fromLatin1(APPLICATION_NAME)).
