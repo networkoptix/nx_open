bf1d78125a33e2f4eaf8218332b129211dc732db
Ivan Vigasin <ivigasin@networkoptix.com>
2012-04-03 22:10:42 +0400

    run convert appserver as before
    <==== Mercurial data ====>
    Branch: master

--- convert.bat
+++ convert.bat
@@ -14,7 +14,7 @@ cd traytool
 @start /B /WAIT convert.py
 cd %~dp0
 
-rem cd appserver
-rem call update.bat
-rem @start /B /WAIT convert.py
-rem cd %~dp0
+cd appserver
+call update.bat
+@start /B /WAIT convert.py
+cd %~dp0
