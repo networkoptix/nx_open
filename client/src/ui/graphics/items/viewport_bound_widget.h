6b265833756302c398b3ccd61b924f9d58265773
Alexander Fokin <afokin@networkoptix.com>
2012-04-06 04:11:40 +0400

    Independent header & footer.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/items/viewport_bound_widget.h
+++ client/src/ui/graphics/items/viewport_bound_widget.h
@@ -3,6 +3,15 @@
 
 #include <QGraphicsWidget>
 
+class Instrument;
+
+/**
+ * Graphics widget with a coordinate system that has the same scale as the
+ * viewport, regardless of its size.
+ * 
+ * Items added to this widget will not be scaled when viewport's transformation 
+ * changes.
+ */
 class QnViewportBoundWidget: public QGraphicsWidget {
     Q_OBJECT;
 
@@ -12,11 +21,28 @@ public:
     QnViewportBoundWidget(QGraphicsItem *parent = NULL);
     virtual ~QnViewportBoundWidget();
 
-    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    /**
+     * \returns                         Desired size of this graphics widget, in parent coordinates. 
+     */
+    const QSizeF &desiredSize();
+
+    /**
+     * \param desiredSize               Desired size of this graphics widget, in parent coordinates. 
+     */
+    void setDesiredSize(const QSizeF &desiredSize);
+
+protected:
+    virtual void resizeEvent(QGraphicsSceneResizeEvent *event) override;
+    virtual QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
+
+protected slots:
+    void updateScale(QGraphicsView *view = NULL);
 
 private:
-    QTransform m_sceneToViewport;
+    QSizeF m_desiredSize;
+    bool m_inUpdateScale;
     QWeakPointer<QGraphicsView> m_lastView;
+    QWeakPointer<Instrument> m_instrument;
 };
 
 
