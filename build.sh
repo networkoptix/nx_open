76353d79a63b2795d1508784262c8351774ee9a3
Ivan Vigasin <ivigasin@networkoptix.com>
2012-04-24 21:41:19 +0400

    move dependencies installation lines to separate script
    <==== Mercurial data ====>
    Branch: release_1.0

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
-set -e
-
-apt-get install -y libmp3lame-dev libssl-dev libx264-dev libxerces-c-dev libprotobuf-dev libqt4-dev libqjson-dev build-essential python-django python-pip 
-
-pushd appserver
-python ./scripts/install_others.py
-popd
+set -ex
 
 if [ -z "$1" ]
 then
