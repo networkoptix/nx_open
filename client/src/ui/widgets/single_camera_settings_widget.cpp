cc2b991dbb9130b89fdac835c181907569c5f76f
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-04-25 20:47:45 +0400

    motion UI bugfix
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/widgets/single_camera_settings_widget.cpp
+++ client/src/ui/widgets/single_camera_settings_widget.cpp
@@ -95,7 +95,7 @@ void QnSingleCameraSettingsWidget::setCamera(const QnVirtualCameraResourcePtr &c
         ui->motionWEBPageLink->setText(tr("%1").arg(webPageAddress));
         if(m_motionWidget) 
             m_motionWidget->setCamera(m_camera);
-        ui->softwareMotionButton->setChecked(m_camera->getMotionType() == MT_SoftwareGrid);
+        ui->cameraMotionButton->setChecked(m_camera->getMotionType() != MT_SoftwareGrid);
         updateMaxMotionRect();
 
         /*
