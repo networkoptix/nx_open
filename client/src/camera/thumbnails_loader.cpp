b4f7c041c0ad58f5fdd3a776b32df53925489a3c
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-05-02 14:05:53 +0400

    slider thumbnails
    <==== Mercurial data ====>
    Branch: master

--- client/src/camera/thumbnails_loader.cpp
+++ client/src/camera/thumbnails_loader.cpp
@@ -11,11 +11,15 @@ QnThumbnailsLoader::QnThumbnailsLoader(QnResourcePtr resource)
     m_endTime = 0;
     m_outWidth = 128;
     m_outHeight = 128*3/4;
+    m_scaleContext = 0;
+    m_rgbaBuffer = 0;
 }
 
 QnThumbnailsLoader::~QnThumbnailsLoader()
 {
-
+    if (m_scaleContext)
+        sws_freeContext(m_scaleContext);
+    delete [] m_rgbaBuffer;
 }
 
 void QnThumbnailsLoader::setThumbnailsSize(int width, int height)
@@ -26,17 +30,30 @@ void QnThumbnailsLoader::setThumbnailsSize(int width, int height)
 
 void QnThumbnailsLoader::loadRange(qint64 startTime, qint64 endTime, qint64 step)
 {
-    pleaseStop();
-    stop();
+    //pleaseStop();
+    //stop();
     m_images.clear();
     m_startTime = startTime;
     m_endTime = endTime;
     m_step = step;
+
+    m_rangeToLoad.clear();
+    m_rangeToLoad << QnTimePeriod(startTime, endTime-startTime);
     start();
 }
 
 void QnThumbnailsLoader::addRange(qint64 startTime, qint64 endTime)
 {
+    if (startTime < m_startTime)
+    {
+        m_rangeToLoad << QnTimePeriod(startTime, m_startTime - startTime);
+        m_startTime = startTime;
+    }
+    if (endTime > m_endTime)
+    {
+        m_rangeToLoad << QnTimePeriod(endTime, endTime - m_endTime);
+        m_endTime = endTime;
+    }
 
 }
 
@@ -50,51 +67,75 @@ QPixmap QnThumbnailsLoader::getPixmapByTime(qint64 time)
     return QPixmap();
 }
 
+void QnThumbnailsLoader::allocateScaleContext(int lineSize, int width, int height, PixelFormat format)
+{
+    if (m_scaleContext) {
+        sws_freeContext(m_scaleContext);
+        m_scaleContext = 0;
+    }
+    delete [] m_rgbaBuffer;
+
+    int numBytes = avpicture_get_size(PIX_FMT_RGBA, lineSize, height);
+    m_rgbaBuffer = (quint8*) qMallocAligned(numBytes, 32);
+    m_scaleContext = sws_getContext(width, height, format, 
+        m_outWidth, m_outHeight, PIX_FMT_BGRA, SWS_POINT, NULL, NULL, NULL);
+}
+
 void QnThumbnailsLoader::run()
 {
-    m_rtspClient->open(m_resource);
     m_rtspClient->setAdditionalAttribute("x-media-step", QByteArray::number(m_step));
-    m_rtspClient->seek(m_startTime, m_endTime);
 
-    QnCompressedVideoDataPtr frame = qSharedPointerDynamicCast<QnCompressedVideoData> (m_rtspClient->getNextData());
-    if (!frame) {
-        pleaseStop();
-        return;
-    }
-
-    CLFFmpegVideoDecoder decoder(frame->compressionType, frame, false);
-    CLVideoDecoderOutput outFrame;
-    outFrame.setUseExternalData(false);
-
-    int numBytes = avpicture_get_size(PIX_FMT_RGBA, outFrame.linesize[0], outFrame.height);
-    quint8* rgbaBuffer = (quint8*) qMallocAligned(numBytes, 32);
-    SwsContext* scaleContext = sws_getContext(outFrame.width, outFrame.height, (PixelFormat) outFrame.format, 
-                                              m_outWidth, m_outHeight, PIX_FMT_BGRA, SWS_POINT, NULL, NULL, NULL);
-
-    int dstLineSize[4];
-    quint8* dstBuffer[4];
-    dstLineSize[0] = outFrame.linesize[0]*4;
-    dstLineSize[1] = dstLineSize[2] = dstLineSize[3] = 0;
-    QImage image(rgbaBuffer, outFrame.width, outFrame.height, outFrame.linesize[0]*4, QImage::Format_ARGB32_Premultiplied);
-    dstBuffer[0] = dstBuffer[1] = dstBuffer[2] = dstBuffer[3] = rgbaBuffer;
-
-    while (!m_needStop && frame)
+    while (!m_needStop)
     {
-        if (decoder.decode(frame, &outFrame))
+        if (m_rangeToLoad.isEmpty())
         {
-            sws_scale(scaleContext,
-                outFrame.data, outFrame.linesize, 
-                0, outFrame.height, 
-                dstBuffer, dstLineSize);
+            msleep(5);
+            continue;
+        }
+        m_rtspClient->open(m_resource);
 
-            m_images.insert(frame->timestamp, QPixmap::fromImage(image));
+        
+        m_mutex.lock();
+        QnTimePeriod period = m_rangeToLoad.dequeue();
+        m_mutex.unlock();
+
+        m_rtspClient->seek(period.startTimeMs, period.endTimeMs());
+
+        QnCompressedVideoDataPtr frame = qSharedPointerDynamicCast<QnCompressedVideoData> (m_rtspClient->getNextData());
+        if (!frame) 
+            continue;
+
+        CLFFmpegVideoDecoder decoder(frame->compressionType, frame, false);
+        CLVideoDecoderOutput outFrame;
+        outFrame.setUseExternalData(false);
+
+        allocateScaleContext(outFrame.linesize[0], outFrame.width, outFrame.height, (PixelFormat) outFrame.format);
+
+
+        int dstLineSize[4];
+        quint8* dstBuffer[4];
+        dstLineSize[0] = outFrame.linesize[0]*4;
+        dstLineSize[1] = dstLineSize[2] = dstLineSize[3] = 0;
+        QImage image(m_rgbaBuffer, outFrame.width, outFrame.height, outFrame.linesize[0]*4, QImage::Format_ARGB32_Premultiplied);
+        dstBuffer[0] = dstBuffer[1] = dstBuffer[2] = dstBuffer[3] = m_rgbaBuffer;
+
+        while (!m_needStop && frame)
+        {
+            if (decoder.decode(frame, &outFrame))
+            {
+                sws_scale(m_scaleContext,
+                    outFrame.data, outFrame.linesize, 
+                    0, outFrame.height, 
+                    dstBuffer, dstLineSize);
+
+                m_images.insert(frame->timestamp, QPixmap::fromImage(image));
+            }
+
+            frame = qSharedPointerDynamicCast<QnCompressedVideoData> (m_rtspClient->getNextData());
         }
 
-        frame = qSharedPointerDynamicCast<QnCompressedVideoData> (m_rtspClient->getNextData());
+        m_rtspClient->close();
     }
-
-    qFreeAligned(rgbaBuffer);
-    sws_freeContext(scaleContext);
 }
 
 void QnThumbnailsLoader::pleaseStop()
