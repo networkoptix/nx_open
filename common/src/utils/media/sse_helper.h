b2af5de4b8d3282a309ab739d7ae0b3a642eafa1
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-05-04 18:14:37 +0400

    fixed gcc sse4 intrisicts problem
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/media/sse_helper.h
+++ common/src/utils/media/sse_helper.h
@@ -4,11 +4,32 @@
 #include <QtCore/private/qsimd_p.h>
 #include <xmmintrin.h>
 #include <emmintrin.h>
-#include <smmintrin.h>
+
 
 #if defined(Q_CC_GNU)
-#define sse4_attribute __attribute__ ((__target__ ("sse4.1")))
+
+#define __always_inline__ __always_inline__, __nodebug__
+
+#undef __STATIC_INLINE
+#ifdef __GNUC_STDC_INLINE__
+#define __STATIC_INLINE __inline
 #else
+#define __STATIC_INLINE static __inline
+#endif
+
+#define sse4_attribute __attribute__ ((__target__ ("sse4.1")))
+
+__STATIC_INLINE int __attribute__((__always_inline__)) sse4_attribute
+
+
+
+_mm_testz_si128 (__m128i __M, __m128i __V)
+{
+    return __builtin_ia32_ptestz128 ((__v2di)__M, (__v2di)__V);
+}
+
+#else
+#include <smmintrin.h>
 #define sse4_attribute 
 #endif
 
