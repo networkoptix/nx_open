eab1b5a5f0b4af29895a497c7a76d0e19df8e352
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-05-06 11:53:48 +0400

    added linux environment and build scripts
    <==== Mercurial data ====>
    Branch: master

---
+++ linux-environment.sh
@@ -0,0 +1,26 @@
+apt-get install -y mc openjdk-6-jre-headless protobuf-compiler build-essential unzip mercurial libqt4-dev libprotobuf-dev
+
+mkdir -p ~/environment
+cd ~/environment
+wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/maven.zip
+unzip ./maven.zip
+chmod 755 ~/environment/apache-maven-3.0.4/bin/mvn
+
+echo 'export environment=~/environment' >> ~/.profile
+echo 'export PATH=~/environment/apache-maven-3.0.4/bin/:'$PATH >> ~/.profile
+echo 'export JAVA_HOME=/usr' >> ~/.profile
+. ~/.profile
+
+mkdir ~/projects
+cd ~/projects
+wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/mercurial.sh
+chmod 755 mercurial.sh
+./mercurial.sh
+cd ./netoptix_vms/appserver/
+
+python ./scripts/install_pip.py
+python ./scripts/install_others.py
+
+cd ~/projects/netoptix_vms
+
+mvn clean package
