f92b24b4e7d78aa8fcdf62e6f93638e7295d7384
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-05-06 14:42:31 +0400

    1) fixed error with maven unpack and path
    2) libz added to packages
    3) deb rm is not sudo (the folder will be chown first)
    
    <==== Mercurial data ====>
    Branch: master

--- linux-environment.sh
+++ linux-environment.sh
@@ -1,15 +1,16 @@
-sudo apt-get install -y openjdk-6-jre-headless protobuf-compiler build-essential unzip libqt4-dev
+sudo apt-get install -y openjdk-6-jre-headless protobuf-compiler build-essential unzip libqt4-dev libz-dev
 
 mkdir -p ~/environment
 cd ~/environment
-wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/maven.zip
+
 mkdir maven
 cd ./maven
+wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/maven.zip
 unzip ./maven.zip
-chmod 755 ~/environment/apache-maven-3.0.4/bin/mvn
+chmod 755 ~/environment/maven/bin/mvn
 
 echo 'export environment=~/environment' >> ~/.profile
-echo 'export PATH=~/environment/apache-maven-3.0.4/bin/:'$PATH >> ~/.profile
+echo 'export PATH=~/environment/maven/bin/:'$PATH >> ~/.profile
 echo 'export JAVA_HOME=/usr' >> ~/.profile
 . ~/.profile
 
@@ -21,3 +22,5 @@ sudo python ./scripts/install_pip.py
 sudo python ./scripts/install_others.py
 
 cd ~/projects/netoptix_vms
+
+sudo chown -R 
