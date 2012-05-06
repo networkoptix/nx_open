71dd4bbbf6946a4a27ffadd100b6cc5bdf642b74
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-05-06 14:03:57 +0400

    adjusted linuz scripts
    <==== Mercurial data ====>
    Branch: master

--- linux-build-mvn.sh
+++ linux-build-mvn.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-export PATH=~/environment/apache-maven-3.0.4/bin/:'$PATH 
-sudo mvn clean package
+mvn clean package
--- linux-environment.sh
+++ linux-environment.sh
@@ -1,8 +1,10 @@
-apt-get install -y openjdk-6-jre-headless protobuf-compiler build-essential unzip libqt4-dev
+sudo apt-get install -y openjdk-6-jre-headless protobuf-compiler build-essential unzip libqt4-dev
 
 mkdir -p ~/environment
 cd ~/environment
 wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/maven.zip
+mkdir maven
+cd ./maven
 unzip ./maven.zip
 chmod 755 ~/environment/apache-maven-3.0.4/bin/mvn
 
@@ -12,15 +14,10 @@ echo 'export JAVA_HOME=/usr' >> ~/.profile
 . ~/.profile
 
 mkdir ~/projects
-#cd ~/projects
-#wget --no-check-certificate https://boris:pizdohuj@noptix.enk.me/jenkins/mercurial.sh
-#chmod 755 mercurial.sh
-#./mercurial.sh
-cd ./netoptix_vms/appserver/
 
-python ./scripts/install_pip.py
-python ./scripts/install_others.py
+cd ~/projects/netoptix_vms/appserver/
+
+sudo python ./scripts/install_pip.py
+sudo python ./scripts/install_others.py
 
 cd ~/projects/netoptix_vms
-
-mvn clean package
