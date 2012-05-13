416ec93abb8febd0d8decd464d38684caffcf9d8
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-05-13 04:57:53 +0400

    fixed images (problem with new ffmpeg)
    <==== Mercurial data ====>
    Branch: master

---
+++ common/src/plugins/resources/archive/single_shot_file_reader.cpp
@@ -0,0 +1,50 @@
+#include "single_shot_file_reader.h"
+#include "filetypesupport.h"
+#include "utils/common/synctime.h"
+
+QnSingleShotFileStreamreader::QnSingleShotFileStreamreader(QnResourcePtr resource):
+    QnAbstractMediaStreamDataProvider(resource)
+{
+}
+
+QnAbstractMediaDataPtr QnSingleShotFileStreamreader::getNextData()
+{
+	CodecID compressionType;
+    QString lowerFileName = getResource()->getUrl().toLower();
+
+    if (lowerFileName.endsWith(QLatin1String(".png")))
+        compressionType = CODEC_ID_PNG;
+    else if (lowerFileName.endsWith(QLatin1String(".jpeg")) || lowerFileName.endsWith(QLatin1String(".jpg")))
+        compressionType = CODEC_ID_MJPEG;
+    else if (lowerFileName.endsWith(QLatin1String(".tiff")) || lowerFileName.endsWith(QLatin1String(".tif")))
+        compressionType = CODEC_ID_TIFF;
+    else if (lowerFileName.endsWith(QLatin1String(".gif")))
+        compressionType = CODEC_ID_GIF;
+    else if (lowerFileName.endsWith(QLatin1String(".bmp")))
+        compressionType = CODEC_ID_BMP;
+    else
+        return QnAbstractMediaDataPtr();
+
+    QFile file(getResource()->getUrl());
+    if (!file.open(QIODevice::ReadOnly))
+        return QnAbstractMediaDataPtr();
+
+    QByteArray srcData = file.readAll();
+	QnCompressedVideoDataPtr outData(new QnCompressedVideoData(CL_MEDIA_ALIGNMENT, srcData.size()));
+    outData->data.write(srcData);
+
+	outData->compressionType = compressionType;
+    outData->flags |= AV_PKT_FLAG_KEY | QnAbstractMediaData::MediaFlags_StillImage;
+    outData->timestamp = qnSyncTime->currentMSecsSinceEpoch()*1000;
+    outData->dataProvider = this;
+	outData->channelNumber = 0;
+
+	return outData;
+}
+
+void QnSingleShotFileStreamreader::run()
+{
+    QnAbstractMediaDataPtr data = getNextData();
+    if (data)
+        putData(data);
+}
