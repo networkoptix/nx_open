0d38a7826e31ecb383f6063c9e70b2e32d79501b
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-05-13 12:52:28 +0400

    corrected backslashes
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resources/archive/single_shot_file_reader.h
+++ common/src/plugins/resources/archive/single_shot_file_reader.h
@@ -1,7 +1,7 @@
 #ifndef single_shot_file_reader_h_215
 #define single_shot_file_reader_h_215
 
-#include "core\dataprovider\media_streamdataprovider.h"
+#include "core/dataprovider/media_streamdataprovider.h"
 
 // difference between this class and pull reader is that run function does not have infinit loop
 // it quits after first getData
