6750af2933a92941f0ea2fc26486c38a7135a14c
Sergey Bystrov <sbystrov@networkoptix.com>
2012-05-16 01:43:32 -0700

    Backed out changeset: 54f4ff215c21
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resources/arecontvision/resource/av_resource.cpp
+++ common/src/plugins/resources/arecontvision/resource/av_resource.cpp
@@ -118,8 +118,6 @@ bool QnPlAreconVisionResource::setHostAddress(const QHostAddress& ip, QnDomain d
 
     if (domain == QnDomainPhysical)
     {
-        return false; // never change ip in this version 
-
         QUdpSocket sock;
 
         sock.bind(getDiscoveryAddr() , 0); // address usesd to find cam
