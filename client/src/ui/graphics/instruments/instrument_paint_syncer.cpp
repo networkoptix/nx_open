3d3fffbef09439a8e5a00ff577a51179023c6145
Alexander Fokin <afokin@networkoptix.com>
2012-06-15 20:30:36 +0400

    Updating OpenGL view 60 timer per second.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/instruments/instrument_paint_syncer.cpp
+++ client/src/ui/graphics/instruments/instrument_paint_syncer.cpp
@@ -7,7 +7,6 @@
 InstrumentPaintSyncer::InstrumentPaintSyncer(QObject *parent):
     QObject(parent),
     m_animationTimer(new QAnimationTimer(this)),
-    m_ticksSinceLastPaint(0),
     m_currentWidget(NULL)
 {
     m_animationTimer->addListener(this);
@@ -23,21 +22,13 @@ bool InstrumentPaintSyncer::eventFilter(QObject *watched, QEvent *event) {
         m_currentWidget = qobject_cast<QWidget *>(watched);
     }
 
-    m_ticksSinceLastPaint = 0;
     updateCurrentTime(QDateTime::currentMSecsSinceEpoch());
     return false;
 }
 
 void InstrumentPaintSyncer::tick(int /*deltaTime*/) {
-    m_ticksSinceLastPaint++;
-    if(m_ticksSinceLastPaint <= 1)
-        return;
-
-    if(currentWatched() != NULL && m_currentWidget != NULL) {
-        /* We better wait for the paint event than firing the animations here. 
-         * It may cause unnecessary updates, but I guess we can live with that. */
+    if(currentWatched() && m_currentWidget)
         m_currentWidget->update(); 
-    }
 }
 
 void InstrumentPaintSyncer::activatedNotify() {
