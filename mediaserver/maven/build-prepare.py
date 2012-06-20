6d7224b4f7a4821e0a1518772796bf81f8ba7816
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-06-20 22:07:23 +0400

    fix mediaserver build-prepare.py
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/maven/build-prepare.py
+++ mediaserver/maven/build-prepare.py
@@ -5,5 +5,5 @@ sys.path.insert(0, '${basedir}/../common')
 #sys.path.insert(0, os.path.join('..', '..', 'common'))
 
 from gencomp import gencomp_cpp
-os.makedirs('${project.build.directory}/build/release/generated/')
-gencomp_cpp(open('${project.build.directory}/build/release/generated/compatibility_info.cpp', 'w'))
+#os.makedirs('${project.build.directory}/build/release/generated/')
+gencomp_cpp(open('${project.build.sourceDirectory}/compatibility_info.cpp', 'w'))
