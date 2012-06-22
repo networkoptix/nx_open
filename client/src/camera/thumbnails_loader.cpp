fe075c06606415ab8a0b92808dcc56f36e8d2301
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-06-23 01:04:40 +0400

    compile error fix
    <==== Mercurial data ====>
    Branch: master

--- client/src/camera/thumbnails_loader.cpp
+++ client/src/camera/thumbnails_loader.cpp
@@ -24,6 +24,7 @@
 #include "utils/media/frame_info.h"
 
 #include "thumbnails_loader_helper.h"
+#include <limits>
 
 namespace {
     const qint64 defaultUpdateInterval = 30 * 1000; /* 30 seconds. */
