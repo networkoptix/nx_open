81a867095c55827999fb947e02ea8554caa0da6b
Ivan Vigasin <ivigasin@networkoptix.com>
2012-06-27 15:07:32 +0400

    bindToInterface(): log level to debug. print address instead of interface name.
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/network/nettools.cpp
+++ common/src/utils/network/nettools.cpp
@@ -24,7 +24,7 @@ bool bindToInterface(QUdpSocket& sock, const QnInterfaceAndAddr& iface)
 
     if (res)
     {
-        cl_log.log(cl_logWARNING, "bindToInterface(): Can't bind to interface %s: %s", iface.name.toAscii().constData(), strerror(errno));
+        cl_log.log(cl_logDEBUG1, "bindToInterface(): Can't bind to interface %s: %s", iface.address.toString().toAscii().constData(), strerror(errno));
         return false;
     }
 
