530a395935fe104a2d5fe2cd2c0ea6d90cbd70a5
Sergey Bystrov <sbystrov@networkoptix.com>
2012-07-20 20:02:33 -0700

    isd works
    <==== Mercurial data ====>
    Branch: release_1.2

--- common/src/plugins/resources/isd/isd_resource.h
+++ common/src/plugins/resources/isd/isd_resource.h
@@ -29,7 +29,7 @@ public:
 
     virtual void setIframeDistance(int frames, int timems); // sets the distance between I frames
 
-    bool hasDualStreaming() const {return false;}
+    //bool hasDualStreaming() const {return false;}
 
 
     virtual const QnResourceAudioLayout* getAudioLayout(const QnAbstractMediaStreamDataProvider* dataProvider) override;
