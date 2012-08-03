654645535670b16cec4f5ad03c0ce4e54ca65df4
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-08-03 19:52:25 +0400

    build file for Linux (several processors)
    <==== Mercurial data ====>
    Branch: master

---
+++ cpp/shared-resources/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case `uname -s` in
+    "Linux")
+        PLATFORM=linux
+        NPROCESSORS=$[$(cat /proc/cpuinfo | grep ^processor | wc -l)]
+        PRODUCTS="common mediaserver mediaproxy client"
+        ;;
+    "Darwin")
+        PLATFORM=mac
+        NPROCESSORS=`sysctl hw.ncpu | awk '{print $2}'`
+        FFMPEG_VERSION=`grep ^FFMPEG_VERSION common/convert.py | sed "s/FFMPEG_VERSION = \'\(.*\)\'/\1/g"`
+        PRODUCTS="common mediaserver mediaproxy client"
+        export DYLD_LIBRARY_PATH=$PWD/../ffmpeg/ffmpeg-git-$FFMPEG_VERSION-mac-release/lib:$DYLD_LIBRARY_PATH
+        ;;
+esac
+
+make -f Makefile.${build.configuration} -j $[NPROCESSORS+1]
