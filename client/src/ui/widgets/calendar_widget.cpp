23bfc140ed16e12f625df56902661f54ff3c9665
Alexander Fokin <afokin@networkoptix.com>
2012-08-13 20:53:34 +0400

    Removed calendar's month context menu.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/widgets/calendar_widget.cpp
+++ client/src/ui/widgets/calendar_widget.cpp
@@ -1,10 +1,17 @@
 #include "calendar_widget.h"
 
+#include <QtGui/QPainter>
+
 #define DAY 1000 * 60 * 60 * 24
 
 QnCalendarWidget::QnCalendarWidget(): 
     QCalendarWidget()
 {
+    /* Month button's drop-down menu doesn't work well with graphics scene, so we simply remove it. */
+    QToolButton *monthButton = findChild<QToolButton *>(QLatin1String("qt_calendar_monthbutton"));
+    if(monthButton)
+        monthButton->setMenu(NULL);
+
     m_currentTimeStorage.setAggregationMSecs(DAY);
     m_syncedTimeStorage.setAggregationMSecs(DAY);
 }
