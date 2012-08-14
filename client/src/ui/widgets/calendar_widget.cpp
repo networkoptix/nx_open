1d71a514d23ba1f02cd1047c2dd9836f298a2d93
Sergey Ivanov <sivanov@networkoptix.com>
2012-08-14 17:00:10 +0400

    Fixed horizontal sizing of the calendar
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/widgets/calendar_widget.cpp
+++ client/src/ui/widgets/calendar_widget.cpp
@@ -1,6 +1,10 @@
 #include "calendar_widget.h"
 
+#include "utils/common/event_processors.h"
+
 #include <QtGui/QPainter>
+#include <QtGui/QTableView>
+#include <QtGui/QHeaderView>
 
 #define DAY 1000 * 60 * 60 * 24
 
@@ -11,12 +15,25 @@ QnCalendarWidget::QnCalendarWidget():
     QToolButton *monthButton = findChild<QToolButton *>(QLatin1String("qt_calendar_monthbutton"));
     if(monthButton)
         monthButton->setMenu(NULL);
+
+    QWidget *yearEdit = findChild<QWidget *>(QLatin1String("qt_calendar_yearedit"));
+    QnSingleEventEater *contextMenuEater = new QnSingleEventEater(Qn::AcceptEvent, yearEdit);
+    contextMenuEater->setEventType(QEvent::ContextMenu);
+    yearEdit->installEventFilter(contextMenuEater);
+
+    setHorizontalHeaderFormat(SingleLetterDayNames);
+
+    QTableView *table = findChild<QTableView *>(QLatin1String("qt_calendar_calendarview"));
+    QHeaderView* header = table->horizontalHeader();
+    header->setMinimumSectionSize(18);
 }
 
 void QnCalendarWidget::setCurrentTimePeriods( Qn::TimePeriodRole type, QnTimePeriodList periods )
 {
     m_currentTimeStorage.setPeriods(type, periods);
 
+    int w = minimumSizeHint().width();
+    qDebug() << w;
     update();
 }
 
