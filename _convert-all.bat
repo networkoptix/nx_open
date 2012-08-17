c78d0c2ca54ed4a2fb7ddb9f306df0d8e3c6cd16
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2012-08-17 17:09:54 +0400

    Intel media sdk connected to the project
    <==== Mercurial data ====>
    Branch: quick_sync

--- _convert-all.bat
+++ _convert-all.bat
@@ -7,4 +7,4 @@ echo customization=%INPUT%
 if not [%1] == [] set CUSTOMIZATION=-Dcustomization=%INPUT%
 
 @echo on
-mvn compile -P!convert-clean-windows,!convert-clean,!installer %CUSTOMIZATION%
+mvn compile -U -P!convert-clean-windows,!convert-clean,!installer %CUSTOMIZATION%
--- _convert-clean-all.bat
+++ _convert-clean-all.bat
@@ -7,4 +7,4 @@ echo customization=%INPUT%
 if not [%1] == [] set CUSTOMIZATION=-Dcustomization=%INPUT%
 
 @echo on
-mvn clean compile -P!installer %CUSTOMIZATION%
+mvn clean compile -U -P!installer %CUSTOMIZATION%
