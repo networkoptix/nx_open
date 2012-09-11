ec8dfbb5e08d6043022c9fc8185fe92648deed25
Alexander Fokin <afokin@networkoptix.com>
2012-09-11 23:26:35 +0400

    Fixed a filename typo.
    <==== Mercurial data ====>
    Branch: master

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -63,7 +63,7 @@
 #include "plugins/storage/file_storage/layout_storage_resource.h"
 #include "core/resource/camera_history.h"
 #include "client_message_processor.h"
-#include "ui/workbench/workbench_tranlation_manager.h"
+#include "ui/workbench/workbench_translation_manager.h"
 
 #ifdef Q_WS_X11
     #include "utils/app_focus_listener.h"
