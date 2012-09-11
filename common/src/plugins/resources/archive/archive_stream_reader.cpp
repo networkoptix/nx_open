49ad75a4bf68476416f234c80a4f80c92bcc098d
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2012-09-11 20:15:18 +0400

    playback fix: problem with jump if camera is offline and does not has archive
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resources/archive/archive_stream_reader.cpp
+++ common/src/plugins/resources/archive/archive_stream_reader.cpp
@@ -210,21 +210,19 @@ bool QnArchiveStreamReader::init()
     {
         while (1)
         {
-            if (m_delegate->seek(requiredJumpTime, false) > 0) 
-            {
-                m_jumpMtx.lock();
-                if (m_requiredJumpTime == requiredJumpTime) {
-                    m_requiredJumpTime = AV_NOPTS_VALUE;
-                    m_jumpMtx.unlock();
+            bool seekOk = m_delegate->seek(requiredJumpTime, false) > 0;
+            m_jumpMtx.lock();
+            if (m_requiredJumpTime == requiredJumpTime) {
+                m_requiredJumpTime = AV_NOPTS_VALUE;
+                m_jumpMtx.unlock();
+                if (seekOk)
                     emit jumpOccured(requiredJumpTime);
-                }
-                else {
-                    requiredJumpTime = m_requiredJumpTime;
-                    m_jumpMtx.unlock();
-                    continue; // race condition, jump to new position again
-                }
+                else
+                    emit jumpCanceled(requiredJumpTime);
+                break;
             }
-            break;
+            requiredJumpTime = m_requiredJumpTime; // race condition. jump again
+            m_jumpMtx.unlock();
         }
     }
 
