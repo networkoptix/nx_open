cdb752b74c3fb1b69828915ed12e9f0dba117af9
Sergey Ivanov <sivanov@networkoptix.com>
2012-09-13 15:27:20 +0400

    some fixes
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/items/generic/image_button_widget.cpp
+++ client/src/ui/graphics/items/generic/image_button_widget.cpp
@@ -76,10 +76,10 @@ class QnImageButtonHoverProgressAccessor: public AbstractAccessor {
 QnImageButtonWidget::QnImageButtonWidget(QGraphicsItem *parent):
     base_type(parent),
     m_pixmapCacheValid(false),
+    m_state(0),
     m_checkable(false),
     m_cached(false),
     m_skipNextHoverEvents(false),
-    m_state(0),
     m_hoverProgress(0.0),
     m_action(NULL),
     m_actionIconOverridden(false)
