f5f1c20a24c1956f28e01dc46d1eb9757471f4c7
Ivan Vigasin <ivigasin@networkoptix.com>
2012-09-15 17:55:35 +0400

    fix ms startup
    <==== Mercurial data ====>
    Branch: master

--- debsetup/mediaserver-deb/maven/build-dist.sh
+++ debsetup/mediaserver-deb/maven/build-dist.sh
@@ -34,16 +34,6 @@ mkdir -p $ETCSTAGE
 mkdir -p $INITSTAGE
 mkdir -p $INITDSTAGE
 
-# Copy mediaserver binary
-install -m 755 $SERVER_BIN_PATH/mediaserver* $BINSTAGE
-
-# Copy mediaserver startup script
-install -m 755 bin/mediaserver $BINSTAGE
-
-# Copy upstart and sysv script
-install -m 755 init/networkoptix-mediaserver.conf $INITSTAGE/$COMPANY_NAME-mediaserver.conf
-install -m 755 init.d/networkoptix-mediaserver $INITDSTAGE/$COMPANY_NAME-mediaserver
-
 # Copy libraries
 cp -P $SERVER_LIB_PATH/*.so* $LIBSTAGE
 
@@ -57,6 +47,16 @@ done
 find $PKGSTAGE -type d -print0 | xargs -0 chmod 755
 find $PKGSTAGE -type f -print0 | xargs -0 chmod 644
 chmod -R 755 $BINSTAGE
+
+# Copy mediaserver binary
+install -m 755 $SERVER_BIN_PATH/mediaserver* $BINSTAGE
+
+# Copy mediaserver startup script
+install -m 755 bin/mediaserver $BINSTAGE
+
+# Copy upstart and sysv script
+install -m 644 init/networkoptix-mediaserver.conf $INITSTAGE/$COMPANY_NAME-mediaserver.conf
+install -m 755 init.d/networkoptix-mediaserver $INITDSTAGE/$COMPANY_NAME-mediaserver
 
 # Prepare DEBIAN dir
 mkdir -p $STAGE/DEBIAN
