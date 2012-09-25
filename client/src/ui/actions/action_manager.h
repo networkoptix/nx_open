586053ef85d78c9662674f51446e560b8db923b0
Alexander Fokin <afokin@networkoptix.com>
2012-09-25 17:42:39 +0400

    Action aliases in action manager.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/actions/action_manager.h
+++ client/src/ui/actions/action_manager.h
@@ -20,38 +20,115 @@ class QnActionFactory;
 class QnActionBuilder;
 class QnWorkbenchContext;
 
+/**
+ * Action manager stores application's actions and presents an interface for
+ * creating context menus given current action scope and parameters.
+ */
 class QnActionManager: public QObject, public QnWorkbenchContextAware {
     Q_OBJECT;
 public:
+    /**
+     * Default constructor.
+     * 
+     * \param parent                    Context-aware parent of this action manager.
+     */
     QnActionManager(QObject *parent = NULL);
 
+    /**
+     * Virtual destructor.
+     */
     virtual ~QnActionManager();
 
-    QAction *action(Qn::ActionId id) const;
+    /**
+     * \param action                    New action to register with this action manager.
+     */
+    void addAction(QnAction *action);
 
+    /**
+     * Registers action id as an alias for another action id.
+     * 
+     * \param id                        Alias id.
+     * \param targetId                  Id of the target action.
+     */
+    void addAlias(Qn::ActionId id, Qn::ActionId targetId);
+
+    /**
+     * \param id                        Id of the action to get.
+     * \returns                         Action for the given id, or NULL if no such action exists.
+     */
+    QnAction *action(Qn::ActionId id) const;
+
+    /**
+     * \returns                         List of all actions of this action manager.
+     */
     QList<QnAction *> actions() const;
 
+    /**
+     * \param id                        Id of the action to check.
+     * \param parameters                Parameters to check.
+     * \returns                         Whether the action with the given id can
+     *                                  be triggered with supplied parameters.
+     */
     bool canTrigger(Qn::ActionId id, const QnActionParameters &parameters = QnActionParameters());
 
+    /**
+     * Triggers the action with the given id.
+     * 
+     * \param id                        Id of the action to trigger.
+     * \param parameters                Parameters to pass to action handler.
+     */
     void trigger(Qn::ActionId id, const QnActionParameters &parameters = QnActionParameters());
 
+    /**
+     * \param scope                     Scope of the menu to create.
+     * \param parameters                Action parameters for menu creation.
+     * \returns                         Newly created menu for the given paramters.
+     *                                  Ownership of the created menu is passed to
+     *                                  the caller.
+     */
     QMenu *newMenu(Qn::ActionScope scope, const QnActionParameters &parameters = QnActionParameters());
 
+    /**
+     * \returns                         Action target provider that is assigned to this
+     *                                  action manager.
+     */
     QnActionTargetProvider *targetProvider() const {
         return m_targetProviderGuard ? m_targetProvider : NULL;
     }
 
+    /**
+     * \param targetProvider            New target provider for this action manager.
+     */
     void setTargetProvider(QnActionTargetProvider *targetProvider);
 
+    /**
+     * \param action                    Action that has just been activated.
+     * \returns                         Parameters with which the given action
+     *                                  was triggered.
+     */
     QnActionParameters currentParameters(QnAction *action) const;
 
+    /**
+     * This is a convenience overload to be used inside handlers of <tt>QAction</tt>'s
+     * <tt>triggered()</tt> signal.
+     *
+     * \param sender                    Action that has just been activated.
+     * \returns                         Parameters with which the given action
+     *                                  was triggered.
+     */
     QnActionParameters currentParameters(QObject *sender) const;
 
+    /**
+     * This function replaces one action in the given menu with another one.
+     * 
+     * \param menu                      Menu to replace the action in.
+     * \param targetId                  Id of the action to be replaced.
+     * \param targetAction              Replacement action.
+     */
     void redirectAction(QMenu *menu, Qn::ActionId targetId, QAction *targetAction);
 
 protected:
     friend class QnAction;
-    friend class QnActionFactory;
 
     void copyAction(QAction *dst, QnAction *src, bool forwardSignals = true);
 
@@ -64,14 +141,14 @@ private slots:
     void at_menu_aboutToShow();
 
 private:
-    /** Root action data. Also contained in the maps. */
+    /** Root action. Also contained in the maps. */
     QnAction *m_root;
 
-    /** Mapping from action id to action data. */ 
+    /** Mapping from action id to action instance. */ 
     QHash<Qn::ActionId, QnAction *> m_actionById;
 
     /** Mapping from action to action id. */
-    QHash<QAction *, Qn::ActionId> m_idByAction;
+    QHash<QnAction *, Qn::ActionId> m_idByAction;
 
     /** Mapping from a menu created by this manager to the parameters that were 
      * passed to it at construction time. NULL key is used for shortcut actions. */
