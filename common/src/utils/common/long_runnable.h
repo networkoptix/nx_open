e1e2509fb6554f949d893f299d20c86c0f545ab3
Alexander Fokin <afokin@networkoptix.com>
2012-10-05 15:47:48 +0400

    #1111: Implement runtime check that stop is called in the most-derived streamreader's destructor.
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/common/long_runnable.h
+++ common/src/utils/common/long_runnable.h
@@ -1,100 +1,98 @@
-#ifndef cl_long_runnable_146
-#define cl_long_runnable_146
+#ifndef QN_LONG_RUNNABLE_H
+#define QN_LONG_RUNNABLE_H
 
-#include <QThread>
-#include <QSemaphore>
+#include <cassert>
+#include <typeinfo>
+
+#include <QtCore/QThread>
+#include <QtCore/QSemaphore>
+
 #include "warnings.h"
 
-class QN_EXPORT QnLongRunnable : public QThread
-{
+class QN_EXPORT QnLongRunnable: public QThread {
     Q_OBJECT
+public:
+    QnLongRunnable(): 
+        m_needStop(false),
+        m_onPause(false)
+    {
+        DEBUG_CODE(m_type = NULL);
+    }
 
-signals:
-    void threadPaused();
+    virtual ~QnLongRunnable() {
+        if(isRunning())
+            qnCritical("Runnable instance was destroyed without a call to stop().");
+    }
+
+    virtual bool needToStop() const {
+        return m_needStop;
+    }
+
+    virtual void pause() {
+        m_semaphore.tryAcquire(m_semaphore.available());
+        m_onPause = true;
+    }
+
+    virtual void resume() {
+        m_onPause = false;
+        m_semaphore.release();
+    }
+
+    void pauseDelay() {
+        while(m_onPause && !needToStop())
+            m_semaphore.tryAcquire(1, 50);
+    }
+
+    void smartSleep(int ms) {
+        int n = ms / 100;
+
+        for (int i = 0; i < n; ++i)
+            if (!needToStop())
+                msleep(100);
+
+        if (!needToStop())
+            msleep(ms % 100);
+    }
+
+    bool isPaused() const { return m_onPause; }
 
 public slots:
-    void start ( Priority priority = InheritPriority )
-    {
+    void start(Priority priority = InheritPriority) {
         if (isRunning()) // already runing;
             return;
 
+        DEBUG_CODE(m_type = &typeid(*this));
+
         m_needStop = false;
         QThread::start(priority);
-        Q_ASSERT(isRunning());
+        assert(isRunning());
     }
 
-    virtual void pleaseStop()
-    {
+    virtual void pleaseStop() {
         m_needStop = true;
         if (m_onPause)
             resume();
     }
 
-    virtual void stop()
-    {
+    virtual void stop() {
+        DEBUG_CODE(
+            if(m_type) {
+                const std::type_info *type = &typeid(*this);
+                assert(*type == *m_type); /* You didn't call stop() from derived class's destructor! Die! */
+            }
+        );
+
         pleaseStop();
         wait();
     }
 
-public:
-    QnLongRunnable() : m_onPause(false) {}
-    
-    virtual ~QnLongRunnable() 
-    {
-        if(isRunning())
-            qnCritical("Runnable instance was destroyed without a call to stop().");
-    }
-
-
-    virtual bool needToStop() const
-    {
-        return m_needStop;
-    }
-
-    virtual void pause()
-    {
-        m_semaphore.tryAcquire(m_semaphore.available());
-        m_onPause = true;
-
-    }
-
-    virtual void resume()
-    {
-        m_onPause = false;
-        m_semaphore.release();
-    }
-
-    void pauseDelay()
-    {
-        if (m_onPause)
-            emit threadPaused();
-        while(m_onPause && !needToStop())
-            m_semaphore.tryAcquire(1,50);
-    }
-
-    void smartSleep(int ms) // not precise 
-    {
-        int n = ms/100;
-        
-        for (int i = 0; i < n; ++i)
-        {
-            if (!needToStop())
-                msleep(100);
-        }
-
-        if (!needToStop())
-        {
-            msleep(ms%100);
-        }
-    }
-
-    bool onPause() const { return m_onPause; }
-
 protected:
     volatile bool m_needStop;
-
     volatile bool m_onPause;
+
     QSemaphore m_semaphore;
+
+    DEBUG_CODE(const std::type_info *m_type;)
 };
 
 
@@ -119,4 +117,4 @@ struct QnRunnableCleanup
 };
 
 
-#endif //cl_long_runnable_146
+#endif // QN_LONG_RUNNABLE_H
