c9014165489750ddb9dfa3c2ba07cf49d060991f
Alexander Fokin <afokin@networkoptix.com>
2012-10-08 17:33:15 +0400

    VBO-based paused painter.
    <==== Mercurial data ====>
    Branch: opengl_performance

--- client/src/ui/graphics/painters/paused_painter.cpp
+++ client/src/ui/graphics/painters/paused_painter.cpp
@@ -1,41 +1,67 @@
 #include "paused_painter.h"
 
 #include <ui/graphics/opengl/gl_shortcuts.h>
+#include <ui/graphics/opengl/gl_buffer_stream.h>
 #include <ui/graphics/shaders/color_shader_program.h>
 
 QnPausedPainter::QnPausedPainter(const QGLContext *context):
-    m_program(QnColorShaderProgram::instance(context)) 
+    QnGlFunctions(context),
+    m_shader(QnColorShaderProgram::instance(context)) 
 {
-    /* Create display list. */
-    m_list = glGenLists(1);
+    if(context != QGLContext::currentContext())
+        qnWarning("Invalid current OpenGL context.");
 
-    /* Compile the display list. */
-    glNewList(m_list, GL_COMPILE);
-    glBegin(GL_QUADS);
-    glColor(1.0, 1.0, 1.0, 1.0);
+    QByteArray data;
 
-    qreal d = 1.0 / 3.0;
-    glVertex(-1.0, -1.0);
-    glVertex(-d,   -1.0);
-    glVertex(-d,    1.0);
-    glVertex(-1.0,  1.0);
-    glVertex( d,   -1.0);
-    glVertex( 1.0, -1.0);
-    glVertex( 1.0,  1.0);
-    glVertex( d,    1.0);
-    glEnd();
-    glEndList();
+    /* Generate vertex data. */
+    QnGlBufferStream<GLfloat> vertexStream(&data);
+    m_vertexOffset = vertexStream.offset();
+    GLfloat d = 1.0 / 3.0;
+    vertexStream 
+        << -1.0 << -1.0
+        << -d   << -1.0
+        << -d   <<  1.0
+        << -1.0 <<  1.0
+        <<  d   << -1.0
+        <<  1.0 << -1.0
+        <<  1.0 <<  1.0
+        <<  d   <<  1.0;
+    m_vertexCount = 8;
+
+    /* Generate color data. */
+    QnGlBufferStream<GLfloat> colorStream(&data);
+    m_colorOffset = colorStream.offset();
+    for(int i = 0; i < m_vertexCount; i++)
+        colorStream << QColor(255, 255, 255, 255);
+
+    /* Push data to GPU. */
+    glGenBuffers(1, &m_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer);
+    glBufferData(GL_ARRAY_BUFFER, data.size(), data.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 QnPausedPainter::~QnPausedPainter() {
-    glDeleteLists(m_list, 1);
+    glDeleteBuffers(1, &m_buffer);
 }
 
 void QnPausedPainter::paint(qreal opacity) {
-    m_program->bind();
-    m_program->setColorMultiplier(QVector4D(1.0, 1.0, 1.0, opacity));
-    glCallList(m_list);
-    m_program->release();
+    m_shader->bind();
+    m_shader->setColorMultiplier(QVector4D(1.0, 1.0, 1.0, opacity));
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableVertexAttribArray(m_shader->colorLocation());
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer);
+    glVertexPointer(2, GL_FLOAT, 0, reinterpret_cast<const GLvoid *>(m_vertexOffset));
+    glVertexAttribPointer(m_shader->colorLocation(), 4, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<const GLvoid *>(m_colorOffset));
+    glDrawArrays(GL_QUADS, 0, m_vertexCount);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glDisableVertexAttribArray(m_shader->colorLocation());
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    m_shader->release();
 }
 
 void QnPausedPainter::paint() {
--- client/src/ui/graphics/painters/paused_painter.h
+++ client/src/ui/graphics/painters/paused_painter.h
@@ -3,11 +3,13 @@
 
 #include <QtCore/QSharedPointer>
 
+#include <ui/graphics/opengl/gl_functions.h>
+
 class QGLContext;
 
 class QnColorShaderProgram;
 
-class QnPausedPainter {
+class QnPausedPainter: public QnGlFunctions {
 public:
     QnPausedPainter(const QGLContext *context);
 
@@ -18,8 +20,9 @@ public:
     void paint();
 
 private:
-    unsigned m_list;
-    QSharedPointer<QnColorShaderProgram> m_program;
+    GLuint m_buffer;
+    int m_vertexOffset, m_colorOffset, m_vertexCount;
+    QSharedPointer<QnColorShaderProgram> m_shader;
 };
 
 #endif // QN_PAUSED_PAINTER_H
