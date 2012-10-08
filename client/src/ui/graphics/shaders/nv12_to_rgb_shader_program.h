bbddec21367f2cef0a543741f670a8c0946657f1
Alexander Fokin <afokin@networkoptix.com>
2012-10-08 20:57:39 +0400

    NV12 to RGB shader.
    <==== Mercurial data ====>
    Branch: master

---
+++ client/src/ui/graphics/shaders/nv12_to_rgb_shader_program.h
@@ -0,0 +1,57 @@
+#ifndef QN_NV12_TO_RGB_SHADER_PROGRAM_H
+#define QN_NV12_TO_RGB_SHADER_PROGRAM_H
+
+#include <QtOpenGL/QGLShaderProgram>
+
+class QnNv12ToRgbShaderProgram: public QGLShaderProgram {
+    Q_OBJECT;
+public:
+    enum Colorspace {
+        YuvEbu,
+        YuvBt601,
+        YuvBt709,
+        YuvSmtp240m
+    };
+
+    QnNv12ToRgbShaderProgram(const QGLContext *context, QObject *parent = NULL);
+
+    /**
+     * \param target                    Number of the texture block that Y-plane is loaded to. 
+     *                                  It is presumed to be in <tt>GL_LUMINANCE</tt> format.
+     */
+    void setYTexture(int target) {
+        setUniformValue(m_yTextureLocation, target);
+    }
+
+    /**
+     * \param target                    Number of the texture block that UV-plane is loaded to.
+     *                                  It is presumed to be in <tt>GL_LUMINANCE_ALPHA</tt> format, 
+     *                                  U component stored as luminance, and V as alpha.
+     */
+    void setUVTexture(int target) {
+        setUniformValue(m_uvTextureLocation, target);
+    }
+
+    /**
+     * \param transform                 YUV->RGB colorspace transformation to use. 
+     */
+    template<class T>
+    void setColorTransform(const QMatrix4x4 &transform) {
+        setUniformValue(m_colorTransformLocation, transform);
+    }
+
+    void setOpacity(GLfloat opacity) {
+        setUniformValue(m_opacityLocation, opacity);
+    }
+
+    static QMatrix4x4 colorTransform(Colorspace colorspace, bool fullRange = true);
+
+private:
+    int m_yTextureLocation;
+    int m_uvTextureLocation;
+    int m_colorTransformLocation;
+    int m_opacityLocation;
+};
+
+
+#endif // QN_NV12_TO_RGB_SHADER_PROGRAM_H
--- client/src/ui/graphics/shaders/texture_transition_shader_program.h
+++ client/src/ui/graphics/shaders/texture_transition_shader_program.h
@@ -1,16 +1,24 @@
 #ifndef QN_TEXTURE_TRANSITION_SHADER_PROGRAM_H
 #define QN_TEXTURE_TRANSITION_SHADER_PROGRAM_H
 
-#include <QGLShaderProgram>
+#include <QtOpenGL/QGLShaderProgram>
 
 class QnTextureTransitionShaderProgram: public QGLShaderProgram {
     Q_OBJECT;
 public:
     QnTextureTransitionShaderProgram(const QGLContext *context, QObject *parent = NULL);
 
-    void setTexture0(int target);
-    void setTexture1(int target);
-    void setProgress(qreal progress);
+    void setTexture0(int target) {
+        setUniformValue(m_texture0Location, target);
+    }
+
+    void setTexture1(int target) {
+        setUniformValue(m_texture1Location, target);
+    }
+
+    void setProgress(GLfloat progress) {
+        setUniformValue(m_progressLocation, progress);
+    }
 
 private:
     int m_texture0Location;
