57b8cac0173cee8551e832dc060c8ca0cd41deed
Alexander Fokin <afokin@networkoptix.com>
2012-10-14 15:08:39 +0400

    Fixed #1280.
    <==== Mercurial data ====>
    Branch: master

--- common/src/core/resource/resource.cpp
+++ common/src/core/resource/resource.cpp
@@ -68,7 +68,7 @@ void QnResource::updateInner(QnResourcePtr other)
 {
     Q_ASSERT(getUniqueId() == other->getUniqueId()); // unique id MUST be the same
 
-    m_id = other->m_id;
+    m_id = other->m_id; // TODO: this is WRONG!!!!!!!!!11111111
     m_typeId = other->m_typeId;
     m_lastDiscoveredTime = other->m_lastDiscoveredTime;
     m_tags = other->m_tags;
