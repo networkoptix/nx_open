1fcdad7671a3927a87f1115090f8b562c33ec751
Alexander Fokin <afokin@networkoptix.com>
2012-10-15 17:07:08 +0400

    Added TODO for gdm.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/widgets/properties/single_camera_settings_widget.cpp
+++ client/src/ui/widgets/properties/single_camera_settings_widget.cpp
@@ -630,11 +630,11 @@ void QnSingleCameraSettingsWidget::at_motionSelectionCleared() {
 }
 
 void QnSingleCameraSettingsWidget::at_linkActivated(const QString &urlString) {
-
     bool canAuth = !m_readOnly;
 
+    // TODO: #gdm Use QDesktopServices::setUrlHandler
 #ifdef Q_OS_WIN
-    if (canAuth){
+    if (canAuth) {
         QSettings settings(QLatin1String("HKEY_CURRENT_USER\\Software\\Clients\\StartMenuInternet"), QSettings::NativeFormat);
         QString defaultBrowser = settings.value(QLatin1String("Default")).toString();
         if (defaultBrowser.isEmpty() || defaultBrowser.contains(QLatin1String("iexplore")))
