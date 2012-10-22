01636287592fddbdd72a9c60f29b27c80dd04095
Alexander Fokin <afokin@networkoptix.com>
2012-10-22 16:15:26 +0400

    Starting help.
    <==== Mercurial data ====>
    Branch: master

---
+++ client/src/help/help_topics.h
@@ -0,0 +1,68 @@
+/**
+ * \file 
+ * 
+ * This file is intended for multiple inclusion, hence no include guards.
+ * 
+ * Define <tt>QN_HELP_TOPIC</tt> macro before including this file.
+ */
+
+/**
+ * \def QN_HELP_TOPIC(ID, URL)
+ * 
+ * \param ID                            Identifier of the help topic.
+ * \param URL                           Url assigned to this help topic.
+ */
+
+#ifndef QN_HELP_TOPIC
+#   error QN_HELP_TOPIC is not defined
+#endif
+
+QN_HELP_TOPIC(MainWindow_MainMenu_Help,                         "main_menu.htm")
+QN_HELP_TOPIC(MainWindow_TabNavigator_Help,                     "tabs.htm")
+QN_HELP_TOPIC(MainWindow_Pin_Help,                              "showing_and_hiding_side_panels.htm")
+QN_HELP_TOPIC(MainWindow_Panic_Help,                            "panic_recording.htm")
+QN_HELP_TOPIC(MainWindow_ScreenRecording_Help,                  "performing_screen_recording.htm")
+QN_HELP_TOPIC(MainWindow_Fulscreen_Help,                        "full_screen_and_windowed_mode.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Help,                             "working_with_a_resource_tree.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Servers_Help,                     "configuring_media_servers_additional_settings_(ad.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Search_Help,                      "search.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Users_Help,                       "introducing_user_roles.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Layouts_Help,                     "configuring_layouts.htm")
+QN_HELP_TOPIC(MainWindow_Tree_Local_Help,                       "playing_back_local_files_in_hd_witness.htm")
+QN_HELP_TOPIC(MainWindow_Scene_Help,                            "hd_witness_user_interface_overview.htm")
+QN_HELP_TOPIC(MainWindow_Playback_Help,                         "playback_panel.htm")
+QN_HELP_TOPIC(MainWindow_LivePlayPause_Help,                    "navigating_through_archive_and.htm")
+QN_HELP_TOPIC(MainWindow_Sync_Help,                             "navigating_through_several_cameras_synchronously.htm")
+QN_HELP_TOPIC(MainWindow_Calendar_Help,                         "using_calendar.htm")
+QN_HELP_TOPIC(MainWindow_Thumbnails_Help,                       "using_thumbnails_for_better_navigation.htm")
+QN_HELP_TOPIC(MainWindow_ThumbnailSearch_Help,                  "thumbnail_search.htm")
+QN_HELP_TOPIC(MainWindow_MediaItem_Help,                        "viewing_camera_stream_information.htm")
+QN_HELP_TOPIC(MainWindow_MediaItem_SmartSearch_Help,            "performing_smart_search.htm")
+QN_HELP_TOPIC(MainWindow_MonitoringItem_Help,                   "monitoring_media_servers.htm")
+
+QN_HELP_TOPIC(SystemSettings_General_MediaFolders_Help,         "configuring_media_folders.htm")
+QN_HELP_TOPIC(SystemSettings_General_TourCycleTime_Help,        "tours.htm")
+QN_HELP_TOPIC(SystemSettings_General_ShowIpInTree_Help,         "working_with_a_resource_tree.htm")
+QN_HELP_TOPIC(SystemSettings_General_Language_Help,             "changing_interface_language.htm")
+QN_HELP_TOPIC(SystemSettings_General_NetworkInterfaces_Help,    "obtaining_the_current_network_configuration.htm")
+QN_HELP_TOPIC(SystemSettings_ScreenRecording_Help,              "setting_up_screen_recording.htm")
+QN_HELP_TOPIC(SystemSettings_Licenses_Help,                     "obtaining_and_activating_hd_witness_licenses.htm")
+
+QN_HELP_TOPIC(ServerSettings_General_Help,                      "configuring_media_servers_additional_settings_(ad.htm")
+QN_HELP_TOPIC(ServerSettings_Panic_Help,                        "panic_recording.htm")
+QN_HELP_TOPIC(ServerSettings_Storages_Help,                     "configuring_media_server_storages_(admin_only).htm")
+
+QN_HELP_TOPIC(UserSettings_Help,                                "changing_user_settings.htm")
+QN_HELP_TOPIC(NewUser_Help,                                     "creating_a_new_user_(admin_only).htm")
+
+QN_HELP_TOPIC(NewLayout_Help,                                   "creating_a_new_layout_(admin_only).htm")
+
+QN_HELP_TOPIC(CameraSettings_General_Name_Help,                 "renaming_a_camera.htm")
+QN_HELP_TOPIC(CameraSettings_General_Ip_Help,                   "obtaining_cameras_ip_and_mac.htm")
+QN_HELP_TOPIC(CameraSettings_General_Audio_Help,                "configuring_audio_for_the_camera_(admin_only).htm")
+QN_HELP_TOPIC(CameraSettings_General_Auth_Help,                 "configuring_camera_authentication_(admin_only).htm")
+QN_HELP_TOPIC(CameraSettings_Motion_Help,                       "setting_up_motion_mask_and_motion_sensitivity_(adm.htm")
+QN_HELP_TOPIC(CameraSettings_Advanced_Help,                     "configuring_proprietary_camera_parameters.htm")
+QN_HELP_TOPIC(CameraSettings_Recording_Help,                    "editing_recording_schedule.htm")
+
+#undef QN_HELP_TOPIC
