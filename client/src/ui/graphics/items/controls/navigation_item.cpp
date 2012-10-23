ceca1766c24f87cd0ea579a32f7dfc9c66fc903c
Alexander Fokin <afokin@networkoptix.com>
2012-10-23 20:30:14 +0400

    Zoom buttons not affected by resizer.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/items/controls/navigation_item.cpp
+++ client/src/ui/graphics/items/controls/navigation_item.cpp
@@ -128,11 +128,12 @@ QnNavigationItem::QnNavigationItem(QGraphicsItem *parent):
     m_zoomOutButton = new QnImageButtonWidget(m_timeSlider);
     m_zoomOutButton->setIcon(qnSkin->pixmap("item/zoom_out.png"));
     m_zoomOutButton->setPreferredSize(16, 16);
+    m_zoomOutButton->setPos(0, 8);
 
     m_zoomInButton = new QnImageButtonWidget(m_timeSlider);
     m_zoomInButton->setIcon(qnSkin->pixmap("item/zoom_in.png"));
     m_zoomInButton->setPreferredSize(16, 16);
-    m_zoomInButton->setPos(16, 0);
+    m_zoomInButton->setPos(16, 8);
 
     m_timeScrollBar = new QnTimeScrollBar(this);
     
