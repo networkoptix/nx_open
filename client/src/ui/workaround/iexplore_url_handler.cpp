f9faae25251a982c5f8d9783c4e851d8c6dee6e6
Alexander Fokin <afokin@networkoptix.com>
2012-10-24 12:45:18 +0400

    Minor!!!!111
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/workaround/iexplore_url_handler.cpp
+++ client/src/ui/workaround/iexplore_url_handler.cpp
@@ -17,13 +17,13 @@ QnIexploreUrlHandler::~QnIexploreUrlHandler() {
 bool QnIexploreUrlHandler::handleUrl(QUrl url) {
     bool canAuth = !url.userName().isEmpty();
 
-    if (canAuth){
+    if (canAuth) {
         QSettings settings(QLatin1String("HKEY_CURRENT_USER\\Software\\Clients\\StartMenuInternet"), QSettings::NativeFormat);
         QString defaultBrowser = settings.value(QLatin1String("Default")).toString().toLower();
         if (defaultBrowser.isEmpty() || defaultBrowser.contains(QLatin1String("iexplore")))
             canAuth = false;
     }
-    if (!canAuth){
+    if (!canAuth) {
         url.setUserName(QString());
         url.setPassword(QString());
     }
