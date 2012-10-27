99440a4092bb35aaa6b549caefc1a11ccf8c2fea
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-10-27 23:37:45 +0400

    business logic
    <==== Mercurial data ====>
    Branch: events

---
+++ mediaserver/src/events/mserver_business_rule_processor.cpp
@@ -0,0 +1,41 @@
+#include <QList>
+#include "mserver_business_rule_processor.h"
+#include "core/resource/media_server_resource.h"
+#include "core/resource/security_cam_resource.h"
+#include "recorder/recording_manager.h"
+
+bool QnMServerBusinessRuleProcessor::executeActionInternal(QnAbstractBusinessActionPtr action)
+{
+    if (QnBusinessRuleProcessor::executeActionInternal(action))
+        return true;
+
+    switch(action->actionType())
+    {
+    case BA_CameraOutput:
+        break;
+    case BA_Bookmark:
+        break;
+    case BA_CameraRecording:
+        return executeRecordingAction(action.dynamicCast<QnRecordingBusinessAction>());
+    case BA_PanicRecording:
+        break;
+
+    default:
+        break;
+    }
+    return false;
+};
+
+bool QnMServerBusinessRuleProcessor::executeRecordingAction(QnRecordingBusinessActionPtr action)
+{
+    QnSecurityCamResourcePtr camera = action->getResource().dynamicCast<QnSecurityCamResource>();
+    Q_ASSERT(camera);
+    if (!camera)
+        return false;
+
+    // todo: if camera is offline function return false. Need some tries on timer event
+    if (action->getToggleState() == ToggleState_On)
+        return qnRecordingManager->startForcedRecording(camera, action->getStreamQuality(), action->getFps(), action->getRecordDuration());
+    else
+        return qnRecordingManager->stopForcedRecording(camera);
+}
---
+++ mediaserver/src/events/mserver_business_rule_processor.h
@@ -0,0 +1,20 @@
+#ifndef __MSERVER_BUSINESS_RULE_PROCESSOR_H_
+#define __MSERVER_BUSINESS_RULE_PROCESSOR_H_
+
+#include "events/business_rule_processor.h"
+#include "events/recording_business_action.h"
+
+/*
+* QnMServerBusinessRuleProcessor can execute business actions
+*/
+
+class QnMServerBusinessRuleProcessor: public QnBusinessRuleProcessor
+{
+public:
+protected:
+    virtual bool executeActionInternal(QnAbstractBusinessActionPtr action) override;
+private:
+    bool executeRecordingAction(QnRecordingBusinessActionPtr action);
+};
+
+#endif // __MSERVER_BUSINESS_RULE_PROCESSOR_H_
