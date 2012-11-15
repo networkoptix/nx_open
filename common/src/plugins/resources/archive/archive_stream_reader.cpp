90dc740385d7a8ba0c3df25a1e86871e54ba96e7
Roman Vasilenko <rvasilenko@networkoptix.com>
2012-11-15 19:41:58 +0400

    newredass: some code is simplified, so some depricated functions are not longer used
    <==== Mercurial data ====>
    Branch: redass

--- common/src/plugins/resources/archive/archive_stream_reader.cpp
+++ common/src/plugins/resources/archive/archive_stream_reader.cpp
@@ -40,7 +40,6 @@ QnArchiveStreamReader::QnArchiveStreamReader(QnResourcePtr dev ) :
     m_currentTimeHint(AV_NOPTS_VALUE),
 //private section 2
     m_bofReached(false),
-    m_canChangeQuality(true),
     m_externalLocked(false),
     m_exactJumpToSpecifiedFrame(false),
     m_ignoreSkippingFrame(false),
@@ -1049,17 +1048,6 @@ void QnArchiveStreamReader::setPlaybackMask(const QnTimePeriodList& playbackMask
     m_playbackMaskHelper.setPlaybackMask(playbackMask);
 }
 
-void QnArchiveStreamReader::disableQualityChange()
-{
-    m_canChangeQuality = false;
-}
-
-void QnArchiveStreamReader::enableQualityChange()
-{
-    m_canChangeQuality = true;
-}
-
-
 void QnArchiveStreamReader::onDelegateChangeQuality(MediaQuality quality)
 {
     QMutexLocker lock(&m_jumpMtx);
@@ -1067,23 +1055,9 @@ void QnArchiveStreamReader::onDelegateChangeQuality(MediaQuality quality)
     m_oldQualityFastSwitch = m_qualityFastSwitch = true;
 }
 
-void QnArchiveStreamReader::setQualityForced(MediaQuality quality)
-{
-    if (m_quality != quality || !m_qualityFastSwitch) 
-    {
-        bool useMutex = !m_externalLocked;
-        if (useMutex)
-            m_jumpMtx.lock();
-        m_quality = quality;
-        m_qualityFastSwitch = true;
-        if (useMutex)
-            m_jumpMtx.unlock();
-    }
-}
-
 void QnArchiveStreamReader::setQuality(MediaQuality quality, bool fastSwitch)
 {
-    if (m_canChangeQuality && (m_quality != quality || fastSwitch > m_qualityFastSwitch)) 
+    if (m_quality != quality || fastSwitch > m_qualityFastSwitch)
     {
         bool useMutex = !m_externalLocked;
         if (useMutex)
