c9128f1e028a957f345e5e980f3a717a254a6c8e
Alexander Fokin <afokin@networkoptix.com>
2012-11-23 17:27:59 +0400

    Fixed shader source macro on gcc.
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/shaders/shader_source.h
+++ client/src/ui/graphics/shaders/shader_source.h
@@ -1,8 +1,16 @@
 #ifndef QN_SHADER_SOURCE_H
 #define QN_SHADER_SOURCE_H
 
+#include <QtCore/QByteArray>
+
 #include <utils/common/preprocessor.h>
 
-#define QN_SHADER_SOURCE BOOST_PP_STRINGIZE
+inline QByteArray qnUnparenthesize(const char *parenthesized) {
+    QByteArray result(parenthesized);
+    return result.mid(1, result.size() - 2);
+    return result;
+}
+
+#define QN_SHADER_SOURCE(...) qnUnparenthesize(BOOST_PP_STRINGIZE((__VA_ARGS__)))
 
 #endif // QN_SHADER_SOURCE_H
