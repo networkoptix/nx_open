7b25b96881dc6dc9b61be5859378f1476087bc42
Alexander Fokin <afokin@networkoptix.com>
2012-12-14 17:03:32 +0400

    Imported sse_helper from stable_ptz branch.
    <==== Mercurial data ====>
    Branch: stable

--- common/src/utils/media/sse_helper.h
+++ common/src/utils/media/sse_helper.h
@@ -6,6 +6,33 @@
 #include <emmintrin.h>
 
 
+#if defined(Q_CC_MSVC)
+#   include <intrin.h> /* For __cpuid. */
+#elif defined(Q_CC_GNU)
+#   ifdef __amd64__
+#       define __cpuid(res, op)                                                 \
+            __asm__ volatile(                                                   \
+            "mov %%rbx, %%rsi    \n\t"                                          \
+            "cpuid               \n\t"                                          \
+            "mov %%rbx, %1       \n\t"                                          \
+            "mov %%rsi, %%rbx    \n\t"                                          \
+            :"=a"(res[0]), "=m"(res[1]), "=c"(res[2]), "=d"(res[3])             \
+            :"0"(op) : "%rsi")
+#   else
+#       define __cpuid(res, op)                                                 \
+            __asm__ volatile(                                                   \
+            "movl %%ebx, %%esi   \n\t"                                          \
+            "cpuid               \n\t"                                          \
+            "movl %%ebx, %1      \n\t"                                          \
+            "movl %%esi, %%ebx   \n\t"                                          \
+            :"=a"(res[0]), "=m"(res[1]), "=c"(res[2]), "=d"(res[3])             \
+            :"0"(op) : "%esi")
+#   endif
+#else
+#   error __cpuid is not supported for your compiler.
+#endif
+
+
 #if defined(Q_CC_GNU) && !defined(Q_OS_MAC)
 
 /* We cannot include GCC intrinsic headers cause they cause compilation errors.
@@ -98,31 +125,10 @@ static inline bool useSSE42()
 #endif
 }
 
-#ifdef Q_OS_LINUX
-#ifdef __amd64__
-#define __cpuid(res, op)\
-  __asm__ volatile(                         \
-            "mov %%rbx, %%rsi   \n\t"		\
-            "cpuid               \n\t"		\
-            "mov %%rbx, %1      \n\t"		\
-            "mov %%rsi, %%rbx   \n\t"		\
-            :"=a"(res[0]), "=m"(res[1]), "=c"(res[2]), "=d"(res[3]) 	\
-            :"0"(op) : "%rsi")
-#else
-#define __cpuid(res, op)\
-  __asm__ volatile(                         \
-            "movl %%ebx, %%esi   \n\t"		\
-            "cpuid               \n\t"		\
-            "movl %%ebx, %1      \n\t"		\
-            "movl %%esi, %%ebx   \n\t"		\
-            :"=a"(res[0]), "=m"(res[1]), "=c"(res[2]), "=d"(res[3]) 	\
-            :"0"(op) : "%esi")
-#endif
-#endif
-
+// TODO: function too large for inlining. Move to cpp file.
 static inline QString getCPUString()
 {
-    char CPUBrandString[0x40];
+    char CPUBrandString[0x40]; 
     int CPUInfo[4] = {-1};
 
     // Calling __cpuid with 0x80000000 as the InfoType argument
@@ -132,7 +138,7 @@ static inline QString getCPUString()
     memset(CPUBrandString, 0, sizeof(CPUBrandString));
 
     // Get the information associated with each extended ID.
-    for( unsigned int i=0x80000000; i<=nExIds; ++i )
+    for(unsigned int i=0x80000000; i<=nExIds; ++i)
     {
         __cpuid(CPUInfo, i);
 
@@ -145,7 +151,7 @@ static inline QString getCPUString()
             memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
     }
 
-    if( nExIds >= 0x80000004 )
+    if(nExIds >= 0x80000004)
     {
         //printf_s("\nCPU Brand String: %s\n", CPUBrandString);
         return QString::fromAscii( CPUBrandString ).trimmed();
