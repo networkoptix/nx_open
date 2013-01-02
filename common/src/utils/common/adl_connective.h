9279f92adaea625a98a33e0b89eb111244cd1367
Alexander Fokin <afokin@networkoptix.com>
2013-01-02 16:34:07 +0400

    ADL connector works.
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/common/adl_connective.h
+++ common/src/utils/common/adl_connective.h
@@ -6,43 +6,49 @@
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
 #include <QtCore/QSharedPointer>
+#include <QtCore/QWeakPointer>
 
 namespace Qn {
-    QObject *connector(QObject *object) {
+    inline QObject *connector(QObject *object) {
         return object;
     }
 
     template<class T>
-    QObject *connector(const QScopedPointer<T> &object) {
+    inline QObject *connector(const QScopedPointer<T> &object) {
         return connector(object.data());
     }
 
     template<class T>
-    QObject *connector(const QSharedPointer<T> &object) {
+    inline QObject *connector(const QSharedPointer<T> &object) {
+        return connector(object.data());
+    }
+
+    template<class T>
+    inline QObject *connector(const QWeakPointer<T> &object) {
         return connector(object.data());
     }
 
 
     template<class T1, class T2>
-    bool connect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type = Qt::AutoConnection) {
+    inline bool connect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type = Qt::AutoConnection) {
         return QObject::connect(connector(sender), signal, connector(receiver), method, type);
     }
 
     template<class T1, class T2>
-    bool disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type) {
+    inline bool disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method) {
         return QObject::disconnect(connector(sender), signal, connector(receiver), method);
     }
 
     namespace detail {
         template<class T1, class T2>
-        bool adl_connect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type = Qt::AutoConnection) {
+        inline bool adl_connect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type = Qt::AutoConnection) {
             using Qn::connect; /* Let ADL kick in. */
 
             return connect(sender, signal, receiver, method, type);
         }
 
         template<class T1, class T2>
-        bool adl_disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type) {
+        inline bool adl_disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method) {
             using Qn::disconnect; /* Let ADL kick in. */
 
             return disconnect(sender, signal, receiver, method);
@@ -53,7 +59,7 @@ namespace Qn {
 
 
 template<class Base>
-class AdlConnective {
+class AdlConnective: public Base {
 public:
     QN_FORWARD_CONSTRUCTOR(AdlConnective, Base, {});
 
@@ -66,7 +72,7 @@ public:
     }
 
     template<class T1, class T2>
-    static bool disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method, Qt::ConnectionType type) {
+    static bool disconnect(const T1 &sender, const char *signal, const T2 &receiver, const char *method) {
         return Qn::detail::adl_disconnect(sender, signal, receiver, method);
     }
 };
