6d483c3f46a7822da7d8475af2a515b081d479ad
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2013-01-18 00:04:46 +0400

    Added retrieval of gl fence functions
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/graphics/opengl/gl_functions.cpp
+++ client/src/ui/graphics/opengl/gl_functions.cpp
@@ -50,6 +50,11 @@ namespace QnGl {
     void APIENTRY glDisableVertexAttribArray(GLuint) { WARN(); };
     void APIENTRY glEnableVertexAttribArray(GLuint) { WARN(); };
 
+    GLsync APIENTRY glFenceSync( GLenum condition, GLbitfield flags ) { WARN(); return NULL; };
+    void APIENTRY glDeleteSync( GLsync sync ) { WARN(); };
+    void APIENTRY glWaitSync( GLsync sync, GLbitfield flags, GLuint64 timeout ) { WARN(); };
+    GLAPI GLenum APIENTRY glClientWaitSync (GLsync sync, GLbitfield flags, GLuint64 timeout) { WARN(); return 0; };
+
 #undef WARN
 
 } // namespace QnGl
@@ -154,6 +159,14 @@ public:
         if(status)
             m_features |= QnGlFunctions::OpenGL2_0;
 
+        status = true;
+        RESOLVE( PFNGLFENCESYNCPROC, glFenceSync );
+        RESOLVE( PFNGLDELETESYNCPROC, glDeleteSync );
+        RESOLVE( PFNGLCLIENTWAITSYNCPROC, glClientWaitSync );
+        RESOLVE( PFNGLWAITSYNCPROC, glWaitSync );
+        if(status)
+            m_features |= QnGlFunctions::OpenGL3_2;
+
 #undef RESOLVE
 
         QByteArray renderer = reinterpret_cast<const char *>(glGetString(GL_RENDERER));
@@ -194,6 +207,11 @@ public:
     PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
     PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray;
     PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
+
+    PFNGLFENCESYNCPROC glFenceSync;
+    PFNGLDELETESYNCPROC glDeleteSync;
+    PFNGLCLIENTWAITSYNCPROC glClientWaitSync;
+    PFNGLWAITSYNCPROC glWaitSync;
 
 private:
     template<class Function>
@@ -308,6 +326,21 @@ void QnGlFunctions::glEnableVertexAttribArray(GLuint index) {
 
 void QnGlFunctions::glDisableVertexAttribArray(GLuint index) {
     d->glDisableVertexAttribArray(index);
+}
+
+GLsync QnGlFunctions::glFenceSync(GLenum condition, GLbitfield flags )
+{
+    return d->glFenceSync( condition, flags );
+}
+
+void QnGlFunctions::glDeleteSync( GLsync sync )
+{
+    d->glDeleteSync( sync );
+}
+
+void QnGlFunctions::glWaitSync( GLsync sync, GLbitfield flags, GLuint64 timeout )
+{
+    d->glWaitSync( sync, flags, timeout );
 }
 
 #ifdef Q_OS_WIN
