4e39d84ed49fd0e181f907c9732e388aafde8ba8
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-01-23 17:45:16 +0400

    time change to past bug fix
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/network/rtpsession.cpp
+++ common/src/utils/network/rtpsession.cpp
@@ -227,7 +227,7 @@ bool QnRtspTimeHelper::isLocalTimeChanged()
     } while (m_timer.elapsed() != elapsed);
     qint64 expectedLocalTime = elapsed + m_localStartTime;
     bool timeChanged = qAbs(expectedLocalTime - ct) > LOCAL_TIME_RESYNC_THRESHOLD;
-    if (!timeChanged && elapsed > 3600) {
+    if (timeChanged || elapsed > 3600) {
         m_localStartTime = qnSyncTime->currentMSecsSinceEpoch();
         m_timer.restart();
     }
@@ -318,6 +318,8 @@ qint64 QnRtspTimeHelper::getUsecTime(quint32 rtpTime, const RtspStatistic& stati
                 m_lastWarnTime = currentUsecTime;
             }
             reset();
+            if (localTimeChanged)
+                m_lastTime = AV_NOPTS_VALUE;
             return getUsecTime(rtpTime, statistics, frequency, false);
         }
         else {
