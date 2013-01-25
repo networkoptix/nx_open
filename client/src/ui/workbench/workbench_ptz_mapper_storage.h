c4e4dc68366212937703dc07ea626f7b9e115705
Alexander Fokin <afokin@networkoptix.com>
2013-01-25 20:22:35 +0400

    Ptz mapper storage started.
    <==== Mercurial data ====>
    Branch: master

---
+++ client/src/ui/workbench/workbench_ptz_mapper_storage.h
@@ -0,0 +1,15 @@
+#ifndef QN_WORKBENCH_PTZ_MAPPER_STORAGE_H
+#define QN_WORKBENCH_PTZ_MAPPER_STORAGE_H
+
+#include <QtCore/QObject>
+
+#include "workbench_context_aware.h"
+
+class QnWorkbenchPtzMapperStorage: public QObject, public QnWorkbenchContextAware {
+    Q_OBJECT
+public:
+
+};
+
+
+#endif // QN_WORKBENCH_PTZ_MAPPER_STORAGE_H
