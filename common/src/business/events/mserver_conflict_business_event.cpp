f40320562e223e8a3be4d9d4f6db5ff488f48042
Sergey Ivanov <sivanov@networkoptix.com>
2013-01-29 18:00:04 +0400

    Runtime parameters in "Conflict ECs" event
    <==== Mercurial data ====>
    Branch: master

--- common/src/business/events/mserver_conflict_business_event.cpp
+++ common/src/business/events/mserver_conflict_business_event.cpp
@@ -1,6 +1,31 @@
 #include "mserver_conflict_business_event.h"
 #include "core/resource/resource.h"
 
+namespace QnBusinessEventRuntime {
+
+    static QLatin1String currentECStr("currentEC");
+    static QLatin1String conflictECsStr("conflictECs");
+
+    static QLatin1String separator("\n");
+
+   QString getCurrentEC(const QnBusinessParams &params) {
+        return params.value(currentECStr, QString()).toString();
+    }
+
+    void setCurrentEC(QnBusinessParams* params, QString value) {
+        (*params)[currentECStr] = value;
+    }
+
+    QStringList getConflictECs(const QnBusinessParams &params) {
+        return params.value(conflictECsStr, QString()).toString().split(separator, QString::SkipEmptyParts);
+    }
+
+    void setConflictECs(QnBusinessParams* params, QStringList value) {
+        (*params)[conflictECsStr] = value.join(separator);
+    }
+
+}
+
 QnMServerConflictBusinessEvent::QnMServerConflictBusinessEvent(
         const QnResourcePtr& mServerRes,
         qint64 timeStamp,
@@ -24,4 +49,19 @@ QString QnMServerConflictBusinessEvent::toString() const
         }
     }
     return text;
+}
+
+QnBusinessParams QnMServerConflictBusinessEvent::getRuntimeParams() const {
+    QnBusinessParams params = base_type::getRuntimeParams();
+
+    if (!m_conflictList.isEmpty()) {
+        QnBusinessEventRuntime::setCurrentEC(&params, QString::fromUtf8(m_conflictList[0]));
+
+        QStringList conflictECs;
+        for (int i = 1; i < m_conflictList.size(); i++)
+            conflictECs << QString::fromUtf8(m_conflictList[i]);
+        QnBusinessEventRuntime::setConflictECs(&params, conflictECs);
+    }
+
+    return params;
 }
