9d0a87872661d2f2bb9d63dcb3e417557ff17f8d
Ivan Vigasin <ivigasin@networkoptix.com>
2013-02-05 11:49:17 -0800

    Just ignore invalid licenses. Do not reset license pool.
    <==== Mercurial data ====>
    Branch: master

--- common/src/api/serializer/pb_serializer.cpp
+++ common/src/api/serializer/pb_serializer.cpp
@@ -1165,13 +1165,15 @@ void parseResource(QnResourcePtr& resource, const pb::Resource& pb_resource, QnR
 
 void parseLicense(QnLicensePtr& license, const pb::License& pb_license)
 {
-    license = QnLicensePtr(new QnLicense(
+    QnLicensePtr rawLicense = QnLicensePtr(new QnLicense(
                             QString::fromUtf8(pb_license.name().c_str()),
                             pb_license.key().c_str(),
                             pb_license.cameracount(),
                             pb_license.hwid().c_str(),
                             pb_license.signature().c_str()
                             ));
+
+    license = rawLicense->isValid() ? rawLicense : QnLicensePtr();
 }
 
 void parseCameraServerItem(QnCameraHistoryItemPtr& historyItem, const pb::CameraServerItem& pb_cameraServerItem)
