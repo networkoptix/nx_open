535afde2283cfd925daa259a5d975952459a6168
Ivan Vigasin <ivigasin@networkoptix.com>
2013-02-13 23:52:43 -0800

    hardware id moved to ecs
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/common/hardware_id.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HARDWARE_ID_H__
-#define __HARDWARE_ID_H__
-
-#include <QString>
-
-/** Return PC hardware ID based on motherboard ID, biosID and HDD ID */
-const char* getHardwareId();
-
-#endif // __HARDWARE_ID_H__
