c1234c82fc927611c5e86a799a80e07ad4fbbf32
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-02-27 13:28:44 -0800

    vmax thumbnails protection
    <==== Mercurial data ====>
    Branch: vmax_480

--- common/src/plugins/storage/dts/vmax480/vmax480_archive_delegate.cpp
+++ common/src/plugins/storage/dts/vmax480/vmax480_archive_delegate.cpp
@@ -213,6 +213,10 @@ void QnVMax480ArchiveDelegate::calcSeekPoints(qint64 startTime, qint64 endTime, 
 
 void QnVMax480ArchiveDelegate::setRange(qint64 startTime, qint64 endTime, qint64 frameStep)
 {
+    if ((startTime-endTime)/frameStep > 60) {
+        qWarning() << "Too large thumbnails range. requested" << (startTime-endTime)/frameStep << "thumbnails. Ignoring";
+    }
+
     qDebug() << "getThumbnails range" << startTime << endTime << frameStep;
 
     m_thumbnailsMode = true;
