e297011370f19755b352c6e251e4ba14c2f74679
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-03-01 18:05:32 +0400

    auto-deploy parent POM if version is changed
    <==== Mercurial data ====>
    Branch: master

--- version.sh
+++ version.sh
@@ -8,3 +8,5 @@ echo $OLD_VERSION
 #echo "for f in `find * -name pom.xml`; do sed -i"" -e 's/$OLD_VERSION/$NEW_VERSION/g' $f; done"
 
 for f in `find * -name pom.xml`; do sed -i"" -e "s/$OLD_VERSION/$NEW_VERSION/g" $f; done
+
+mvn deploy -N
