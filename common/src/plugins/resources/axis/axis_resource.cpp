444c300f11081cc7669e763f0fd39d01a34e48bd
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-03-09 00:24:55 -0800

    add firmware for axis
    <==== Mercurial data ====>
    Branch: acti_camera

--- common/src/plugins/resources/axis/axis_resource.cpp
+++ common/src/plugins/resources/axis/axis_resource.cpp
@@ -257,6 +257,18 @@ bool QnPlAxisResource::readMotionInfo()
 bool QnPlAxisResource::initInternal()
 {
 
+    //TODO/IMPL check firmware version. it must be >= 5.0.0 to support I/O ports
+    {
+        CLSimpleHTTPClient http (getHostAddress(), QUrl(getUrl()).port(DEFAULT_AXIS_API_PORT), getNetworkTimeout(), getAuth());
+        CLHttpStatus status = http.doGET(QByteArray("axis-cgi/param.cgi?action=list&group=root.Properties.Firmware.Version"));
+        if (status == CL_HTTP_SUCCESS) {
+            QByteArray firmware;
+            http.readAll(firmware);
+            firmware = firmware.mid(firmware.indexOf('=')+1);
+            setFirmware(QString::fromUtf8(firmware));
+        }
+    }
+
     {
         // enable send motion into H.264 stream
         CLSimpleHTTPClient http (getHostAddress(), QUrl(getUrl()).port(DEFAULT_AXIS_API_PORT), getNetworkTimeout(), getAuth());
@@ -346,11 +358,11 @@ bool QnPlAxisResource::initInternal()
     //root.Image.I1.TriggerData.MotionDetectionEnabled=yes
     //root.Properties.Motion.MaxNbrOfWindows=10
 
-    //TODO/IMPL check firmware version. it must be >= 5.0.0 to support I/O ports
-
     initializeIOPorts( &http );
 
     initializePtz(&http);
+
+    // determin camera max resolution
 
     // TODO: #Elric this is totally evil, copypasta from ONVIF resource.
     {
