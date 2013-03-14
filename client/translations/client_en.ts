6628d4c01dccc88be4b8e1ddf65c75b6aba5557c
Alexander Fokin <afokin@networkoptix.com>
2013-03-14 16:01:04 +0400

    English translation.
    <==== Mercurial data ====>
    Branch: master

--- client/translations/client_en.ts
+++ client/translations/client_en.ts
@@ -5,16 +5,16 @@
     <name>QnBusinessRuleViewModel</name>
     <message numerus="yes">
         <source>%n Camera(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Camera</numerusform>
+            <numerusform>%n Cameras</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>%n Server(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Server</numerusform>
+            <numerusform>%n Servers</numerusform>
         </translation>
     </message>
 </context>
@@ -22,79 +22,79 @@
     <name>QnWorkbenchActionHandler</name>
     <message numerus="yes">
         <source>The following %n layout(s) are not saved. Do you want to save them?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>The following layout is not saved. Do you want to save it?</numerusform>
+            <numerusform>The following %n layouts are not saved. Do you want to save them?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>The following %n layout(s) are being saved.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>The following layout is being saved.</numerusform>
+            <numerusform>The following %n layouts are being saved.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Save changes to the following %n camera(s)?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Save changes to the following camera?</numerusform>
+            <numerusform>Save changes to the following %n cameras?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Are you sure you want to permanently delete these %n file(s)?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Are you sure you want to permanently delete this file?</numerusform>
+            <numerusform>Are you sure you want to permanently delete these %n files?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Are you sure you want to remove these %n item(s) from layout?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Are you sure you want to remove this item from layout?</numerusform>
+            <numerusform>Are you sure you want to remove these %n items from layout?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Do you really want to delete the following %n item(s)?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Do you really want to delete the following item?</numerusform>
+            <numerusform>Do you really want to delete the following %n items?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Exactly one item must be selected for export, but %n item(s) are currently selected.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Exactly one item must be selected for export, but %n item is currently selected.</numerusform>
+            <numerusform>Exactly one item must be selected for export, but %n items are currently selected.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Could not save the following %n layout(s) to Enterprise Controller.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Could not save the following layout to Enterprise Controller.</numerusform>
+            <numerusform>Could not save the following %n layouts to Enterprise Controller.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Do you want to restore these %n layout(s)?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Do you want to restore this layout?</numerusform>
+            <numerusform>Do you want to restore these %n layouts?</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Could not save the following %n items to Enterprise Controller.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Could not save the following item to Enterprise Controller.</numerusform>
+            <numerusform>Could not save the following %n items to Enterprise Controller.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Could not save changes made to the following %n resource(s).</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Could not save changes made to the following resource.</numerusform>
+            <numerusform>Could not save changes made to the following %n resources.</numerusform>
         </translation>
     </message>
 </context>
@@ -104,13 +104,11 @@
         <source>Could not save the following %n layout(s) to Enterprise Controller.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
-            <numerusform></numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Do you want to restore these %n layout(s)?</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
@@ -118,13 +116,11 @@
         <source>Could not save the following %n items to Enterprise Controller.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
-            <numerusform></numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>Could not save changes made to the following %n resource(s).</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
