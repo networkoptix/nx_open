e19f69d38ed884edf4c151218eda386182ed9955
Alexander Fokin <afokin@networkoptix.com>
2013-03-18 01:18:09 +0400

    Plain PTZ seems to work OK for MPTZ20X.
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resources/digitalwatchdog/digital_watchdog_resource.cpp
+++ common/src/plugins/resources/digitalwatchdog/digital_watchdog_resource.cpp
@@ -65,17 +65,35 @@ bool QnPlWatchDogResource::isDualStreamingEnabled(bool& unauth)
 bool QnPlWatchDogResource::initInternal() 
 {
     bool unauth = false;
-
     if (!isDualStreamingEnabled(unauth) && unauth==false) 
     {
         // The camera most likely is going to reset after enabling dual streaming
         enableOnvifSecondStream();
         return false;
     }
-    else 
+        
+    bool result = QnPlOnvifResource::initInternal();
+
+    // TODO: #Elric this code is totally evil. Better write it properly as soon as possible.
+    CLSimpleHTTPClient http(getHostAddress(), HTTP_PORT, getNetworkTimeout(), getAuth());
+    http.doGET(QByteArray("/cgi-bin/getconfig.cgi?action=color"));
+    QByteArray data;
+    http.readAll(data);
+
+    qreal speedX = 1.0, speedY = 1.0;
+    if(getModel() == QLatin1String("DWC-MPTZ20X"))
+        speedX *= -1.0;
+    if(data.contains("flipmode1: 1"))
+        speedY *= -1.0;
+    if(QnOnvifPtzController *ptzController = dynamic_cast<QnOnvifPtzController *>(base_type::getPtzController())) 
     {
-        return QnPlOnvifResource::initInternal();
+        QMatrix4x4 transform;
+        transform(0, 0) = speedX;
+        transform(1, 1) = speedY;
+        ptzController->setSpeedTransform(transform);
     }
+
+    return result;
 }
 
 
@@ -86,8 +104,7 @@ void QnPlWatchDogResource::enableOnvifSecondStream()
     QByteArray request;
     request.append("onvif_stream_number=2&onvif_use_service=true&onvif_service_port=8032&");
     request.append("onvif_use_discovery=true&onvif_use_security=true&onvif_security_opts=63&onvif_use_sa=true&reboot=true");
-    CLHttpStatus status = http.doPOST(QByteArray("/cgi-bin/onvifsetup.cgi"), QLatin1String(request));
-    Q_UNUSED(status);
+    http.doPOST(QByteArray("/cgi-bin/onvifsetup.cgi"), QLatin1String(request));
 
     setStatus(Offline);
     // camera rebooting ....
