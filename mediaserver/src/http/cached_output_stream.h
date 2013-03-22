fd3cf76cdb4fbfbc3d376b0f671934b8e90f5c66
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2013-03-22 16:42:46 +0400

    If not sufficient bandwidth during progressive downloading, source frames are skipped now
    <==== Mercurial data ====>
    Branch: master

---
+++ mediaserver/src/http/cached_output_stream.h
@@ -0,0 +1,46 @@
+/**********************************************************
+* 16 nov 2012
+* a.kolesnikov
+***********************************************************/
+
+#ifndef CACHED_OUTPUT_STREAM_H
+#define CACHED_OUTPUT_STREAM_H
+
+#include <queue>
+
+#include <QByteArray>
+#include <QMutex>
+
+#include <utils/common/long_runnable.h>
+#include <utils/common/threadqueue.h>
+
+
+class QnTCPConnectionProcessor;
+
+class CachedOutputStream
+:
+    public QnLongRunnable
+{
+public:
+    /*!
+        \param ssl pointer to SSL. If not NULL, ssl is used
+    */
+    CachedOutputStream( QnTCPConnectionProcessor* const tcpOutput );
+    ~CachedOutputStream();
+
+    void postPacket( const QByteArray& data );
+    bool failed() const;
+    size_t packetsInQueue() const;
+
+    virtual void pleaseStop();
+
+protected:
+    virtual void run() override;
+
+private:
+    QnTCPConnectionProcessor* const m_tcpOutput;
+    CLThreadQueue<QByteArray> m_packetsToSend;
+    bool m_failed;
+};
+
+#endif  //CACHED_OUTPUT_STREAM_H
