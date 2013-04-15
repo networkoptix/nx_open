b45a359bd7e5a466ae042c05531a479ac640e180
Ivan Vigasin <ivigasin@networkoptix.com>
2013-04-15 23:17:14 +0400

    basic archive support
    <==== Mercurial data ====>
    Branch: master

---
+++ HDWitness/HDWCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDWCalendarViewController.h
+//  HDWitness
+//
+//  Created by Ivan Vigasin on 4/15/13.
+//  Copyright (c) 2013 Ivan Vigasin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDWCalendarViewController : UIViewController
+
+@end
---
+++ HDWitness/HDWCalendarViewController.m
@@ -0,0 +1,38 @@
+//
+//  HDWCalendarViewController.m
+//  HDWitness
+//
+//  Created by Ivan Vigasin on 4/15/13.
+//  Copyright (c) 2013 Ivan Vigasin. All rights reserved.
+//
+
+#import "HDWCalendarViewController.h"
+
+@interface HDWCalendarViewController ()
+
+@end
+
+@implementation HDWCalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
