21c973c0c31fdf10dad13134be8d6fc886512c62
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2013-04-24 23:22:22 +0400

    Added some comments to make some things clear
    <==== Mercurial data ====>
    Branch: cam_sdk

--- common/src/plugins/resources/mdns/mdns_device_searcher.h
+++ common/src/plugins/resources/mdns/mdns_device_searcher.h
@@ -20,6 +20,10 @@ public:
 
     virtual QnResourceList findResources();
 protected:
+    /*!
+        \param result Just found resources. In case if same camera has been found on multiple network interfaces
+        \note Searcher MUST not duplicate resoures, already present in \a result
+    */
     virtual QList<QnNetworkResourcePtr> processPacket(QnResourceList& result, const QByteArray& responseData, const QHostAddress& discoveryAddress) = 0;
 private:
     void checkSocket(QUdpSocket& sock, QnResourceList& result, QHostAddress localAddress);
