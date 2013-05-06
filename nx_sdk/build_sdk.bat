af29f94f29d8db7281c2d01d00f392a05320d8b5
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2013-05-06 17:42:40 +0400

    Added camera integration SDK build
    <==== Mercurial data ====>
    Branch: master

---
+++ nx_sdk/build_sdk.bat
@@ -0,0 +1,30 @@
+
+set TARGET_DIR=nx_sdk
+mkdir %TARGET_DIR%
+mkdir %TARGET_DIR%\include\
+mkdir %TARGET_DIR%\include\plugins\
+mkdir %TARGET_DIR%\sample\
+mkdir %TARGET_DIR%\sample\axiscamplugin\
+copy /Y readme.txt %TARGET_DIR%\
+
+@rem Copying integration headers
+copy /Y ..\common\src\plugins\plugin_api.h %TARGET_DIR%\include\plugins\
+copy /Y ..\common\src\plugins\camera_plugin.h %TARGET_DIR%\include\plugins\
+
+@rem Copying AXIS plugin
+xcopy /Y ..\axiscamplugin\*.pro %TARGET_DIR%\sample\axiscamplugin\
+mkdir %TARGET_DIR%\sample\axiscamplugin\
+xcopy /Y ..\axiscamplugin\*.pro %TARGET_DIR%\sample\axiscamplugin\
+mkdir %TARGET_DIR%\sample\axiscamplugin\src\
+xcopy /Y ..\axiscamplugin\src\*.h %TARGET_DIR%\sample\axiscamplugin\src\
+xcopy /Y ..\axiscamplugin\src\*.cpp %TARGET_DIR%\sample\axiscamplugin\src\
+copy /Y ..\axiscamplugin\Doxyfile %TARGET_DIR%\sample\axiscamplugin\Doxyfile
+
+@rem Removing unnecessary source files
+del /Q /F %TARGET_DIR%\sample\axiscamplugin\src\compatibility_info.cpp
+
+@rem Generating documentation
+set CUR_DIR_BAK=%~dp0
+cd %TARGET_DIR%\sample\axiscamplugin\
+doxygen
+cd %CUR_DIR_BAK%
---
+++ nx_sdk/readme.txt
@@ -0,0 +1,1 @@
+See sample/axiscamplugin/docs/html/index.html
