1a32c17908a6f987f3c0769b46ca2979ff760caf
Sergey Ivanov <sivanov@networkoptix.com>
2013-05-14 14:36:18 +0400

    temporary fix for Ubuntu 13.04
    <==== Mercurial data ====>
    Branch: master

--- _convert-all.sh
+++ _convert-all.sh
@@ -1,2 +1,4 @@
 mvn compile --projects=build_environment
 mvn compile -T 4 --projects=appserver,common,applauncher,client,mediaserver,mediaproxy
+sudo chmod -R +r /usr/local/lib/python2.7/dist-packages/
+
