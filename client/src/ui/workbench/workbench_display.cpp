dd2eaf08a33226cf98c296271318c1639ca2e10f
Sergey Bystrov <sbystrov@networkoptix.com>
2013-05-22 14:33:40 +0400

    suggested opacity
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/workbench/workbench_display.cpp
+++ client/src/ui/workbench/workbench_display.cpp
@@ -551,7 +551,7 @@ void QnWorkbenchDisplay::setWidget(Qn::ItemRole role, QnResourceWidget *widget) 
         if(newWidget != NULL) {
             bringToFront(newWidget);
             synchronize(newWidget, true);
-            newWidget->setOpacity(0.7);
+            newWidget->setOpacity(0.88);
         }
         break;
     }
