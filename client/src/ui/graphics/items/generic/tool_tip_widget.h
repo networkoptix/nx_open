695f3cd10cc1a0ec945aaa51b0502dc96f07f049
Alexander Fokin <afokin@networkoptix.com>
2013-06-04 15:23:03 +0400

    More doxydocs for tool tip widget.
    <==== Mercurial data ====>
    Branch: feature_1935_Redesign_notifications

--- client/src/ui/graphics/items/generic/tool_tip_widget.h
+++ client/src/ui/graphics/items/generic/tool_tip_widget.h
@@ -9,6 +9,12 @@
 
 class QnProxyLabel;
 
+/**
+ * A generic balloon tool tip widget. It works just like any other normal widget,
+ * and almost anything can be placed on it in a layout.
+ * 
+ * Use <tt>setContentsMargins</tt> to change tool tip's content margins and rounding radius.
+ */
 class QnToolTipWidget: public QnSimpleFrameWidget {
     Q_OBJECT;
     typedef QnSimpleFrameWidget base_type;
@@ -17,23 +23,21 @@ public:
     QnToolTipWidget(QGraphicsItem *parent = NULL, Qt::WindowFlags windowFlags = 0);
     virtual ~QnToolTipWidget();
 
-    /* Use setContentsMargins to change tooltip's margins. */
-
     /**
      * \returns                         Position of balloon's tail, in local coordinates.
      */
     QPointF tailPos() const;
 
     /**
+     * \param tailPos                   New position of balloon's tail, in local coordinates.
+     */
+    void setTailPos(const QPointF &tailPos);
+
+    /**
      * \returns                         Widget's side to which balloon's tail is attached, or zero if 
      *                                  there is no tail.
      */
     Qn::Border tailBorder() const;
-
-    /**
-     * \param tailPos                   New position of balloon's tail, in local coordinates.
-     */
-    void setTailPos(const QPointF &tailPos);
 
     /**
      * \returns                         Width of the base of balloon's tail.
@@ -52,10 +56,33 @@ public:
      */
     void pointTo(const QPointF &pos);
 
+    /**
+     * A convenience accessor for tool tips that contain a text label (<tt>QnProxyLabel</tt>).
+     * 
+     * \return                          Text of this tool tip's label, or <tt>null</tt> string if 
+     *                                  this widget does not contain a label.
+     */
     QString text() const;
+
+    /**
+     * A convenience accessor that ensures that this tool tip contains a text label (<tt>QnProxyLabel</tt>)
+     * and sets this label's text. Note that if some other widgets are placed on this tool tip, they
+     * will be destroyed.
+     * 
+     * \param text                      New text for this tool tip's label.
+     */
     void setText(const QString &text);
 
+    /**
+     * \returns                         Whether this tool tip is resized automatically to match 
+     *                                  the preferred size of its contents.
+     */
     bool isAutoSize() const;
+
+    /**
+     * \param autoSize                  Whether this tool tip should be resized automatically to match
+     *                                  the preferred size of this contents.
+     */
     void setAutoSize(bool autoSize);
 
     virtual QRectF boundingRect() const override;
