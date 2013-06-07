08c843ef26219330fb9a4774a786b0410bc55277
Sergey Ivanov <sivanov@networkoptix.com>
2013-06-07 13:05:20 +0400

    gcc warning ignored in random() function
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/common/util.cpp
+++ common/src/utils/common/util.cpp
@@ -245,6 +245,12 @@ int currentTimeZone()
     return res;
 }
 
+#pragma GCC diagnostic ignored "-Woverflow"
+
+/** Returns random integer number between min and max parameters. Thread-safe.
+ *  Correctness of parameters is responsibility of the callee.
+ *  GCC overflow warning is produced 'by design' (c)Elric
+ */
 int random(int min, int max) {
     return min + static_cast<int>(static_cast<qint64>(max - min) * qrand() / (RAND_MAX + 1));
 }
