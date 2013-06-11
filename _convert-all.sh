233034ae40dba111f2b50b3b2384b647426e3168
Ivan Vigasin <ivigasin@networkoptix.com>
2013-06-11 18:34:16 +0400

    use python from buildenv on linux
    <==== Mercurial data ====>
    Branch: master

--- _convert-all.sh
+++ _convert-all.sh
@@ -1,4 +1,3 @@
 mvn compile --projects=build_environment
 mvn compile -T 4 --projects=appserver,common,applauncher,client,mediaserver,mediaproxy
-sudo chmod -R +r /usr/local/lib/python2.7/dist-packages/
 
