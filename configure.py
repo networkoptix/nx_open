5dfdd5da6c573f0a42e6ce1ff60ea1908b49453f
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-06-17 23:54:52 +0400

    introduced configure script and building appserver
    <==== Mercurial data ====>
    Branch: build-refactor

---
+++ configure.py
@@ -0,0 +1,31 @@
+import io, sys, os, platform
+import ConfigParser
+import argparse
+#from main import get_environment_variable, cd
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", help="configuration, default - release")
+    parser.add_argument("--cst", help="customization, default - HD Witness")
+    args = parser.parse_args()    
+    if args.cfg:
+        build_configuration = args.cfg
+    elif os.getenv('configuration'):
+        build_configuration = os.getenv('configuration')
+    else:
+        build_configuration = 'release'
+        
+    if args.cst:
+        build_customization = args.cst
+    elif os.getenv('customization'):
+        build_customization = os.getenv('customization')
+    else:
+        build_customization = 'default'          
+
+    f = open('configure_settings_tmp.py', 'w')
+    print >> f, \
+    'customization = "%s" \nconfiguration = "%s"' %(build_customization, build_configuration)
+
+    print '\n++++++++++++++++++++++++ CONFUGURED ++++++++++++++++++++++++\n'
+    print >> sys.stderr, 'customization is: %s' % build_customization
+    print >> sys.stderr, 'configuration: is %s' % build_configuration
--- main.py
+++ main.py
@@ -1,58 +1,54 @@
 import io, sys, os, platform
 import ConfigParser
 import argparse
+from os.path import dirname, join
 
-class Config:
-    pass
     
-def get_build_environment():   
-    config = Config()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--cfg", help="configuration, default - release")
-    parser.add_argument("--cst", help="customization, default - HD Witness")
-    #parser.add_argument("command")
-    args = parser.parse_args()
-    if platform.architecture()[0] == '64bit':
-        config.build_arch = 'x64'
-    else:
-        config.build_arch = 'x86'
-        
-    if args.cfg:
-        config.build_configuration = args.cfg
-    elif os.getenv('configuration'):
-        config.build_configuration = os.getenv('configuration')
-    else:
-        config.build_configuration = 'release'
-        
-    if args.cst:
-        config.build_customization = args.cst
-    elif os.getenv('customization'):
-        config.build_customization = os.getenv('customization')
-    else:
-        config.build_customization = 'default'        
+#class Config:
+#    pass
 
-    if sys.platform == 'win32':
-        config.build_platform = 'windows'
-    elif sys.platform == 'linux2':
-        config.build_platform = 'linux'
-    elif sys.platform == 'darwin':
-        config.build_platform = 'macosx'    
-    return config
+class cd:         
+    def __init__(self, newPath):
+        self.newPath = newPath
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
     
 def get_environment_variable(variable):
+    try:
+        with open(join(dirname(os.path.abspath(__file__)),'configure_settings_tmp.py')): from configure_settings_tmp import customization, configuration
+    except IOError:
+        print 'Please run configure.py first'
+        sys.exit(1)
+    
     if variable == 'platform':
-        return get_os_name()
-    elif os.getenv(variable):
-        return os.getenv(variable)
-    else:
-        config = ConfigParser.RawConfigParser(allow_no_value=True)
-        config.readfp(open(os.path.dirname(os.path.abspath(__file__)) + '/build-' + get_build_environment().build_customization + '.properties'))
-        return config.get("basic", variable)
-
-config = get_build_environment()        
-print >> sys.stderr, 'Python arch is %s' % config.build_arch
-print >> sys.stderr, 'platform is %s' % config.build_platform
-print >> sys.stderr, 'customization is %s' % config.build_customization
-print >> sys.stderr, 'product.name is %s' % get_environment_variable('product.name')
-print >> sys.stderr, 'build.configuration is %s' % config.build_configuration
-print >> sys.stderr, 'environment is %s' % get_environment_variable('environment')
+        if sys.platform == 'win32':
+            return 'windows'
+        elif sys.platform == 'linux2':
+            return 'linux'
+        elif sys.platform == 'darwin':
+            return 'macosx'   
+            
+    elif variable == 'arch':        
+        if platform.architecture()[0] == '64bit':
+            return 'x64'
+        else:
+            return 'x86'        
+    
+    elif variable == 'customization':
+        return customization
+    
+    elif variable == 'configuration':
+        return configuration
+        
+    else:        
+        if os.getenv(variable):
+            return os.getenv(variable)
+        else:
+            config = ConfigParser.RawConfigParser(allow_no_value=True)
+            config.readfp(open(os.path.dirname(os.path.abspath(__file__)) + '/build-' + customization + '.properties'))
+            return config.get("basic", variable)  
