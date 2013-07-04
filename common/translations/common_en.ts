d9ad5633d1d5ea7733388fd64d14273d95bed4d8
Alexander Fokin <afokin@networkoptix.com>
2013-07-04 16:08:08 +0400

    Common english translation.
    <==== Mercurial data ====>
    Branch: master

--- common/translations/common_en.ts
+++ common/translations/common_en.ts
@@ -6,8 +6,7 @@
     <message numerus="yes">
         <source>%n times since %1 %2</source>
         <comment>%1 means time, %2 means date</comment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
             <numerusform></numerusform>
         </translation>
     </message>
@@ -16,16 +15,16 @@
     <name>QnBusinessStringsHelper</name>
     <message numerus="yes">
         <source>%1 &quot;%2&quot; has detected %n storage issues</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%1 &quot;%2&quot; has detected %n storage issue</numerusform>
+            <numerusform>%1 &quot;%2&quot; has detected %n storage issues</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>%1 has experienced %n network issues with camera %2</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%1 has experienced %n network issue with camera %2</numerusform>
+            <numerusform>%1 has experienced %n network issues with camera %2</numerusform>
         </translation>
     </message>
 </context>
