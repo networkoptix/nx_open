22008b2b6284402b18bedaf6a2e2c0cf27e8fd83
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-07-08 19:57:44 +0400

    Fixed NULL pointer exception
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/rtsp/rtsp_connection.cpp
+++ mediaserver/src/rtsp/rtsp_connection.cpp
@@ -1191,7 +1191,7 @@ int QnRtspConnectionProcessor::composeSetParameter()
             d->archiveDP->setQuality(d->quality, d->qualityFastSwitch);
             return CODE_OK;
         }
-        else if (normParam.startsWith("x-send-motion"))
+        else if (normParam.startsWith("x-send-motion") && d->archiveDP)
         {
             QByteArray value = vals[1].trimmed();
             d->archiveDP->setSendMotion(value == "1" || value == "true");
