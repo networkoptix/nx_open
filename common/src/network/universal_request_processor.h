c42661fcf653898c6e66747bf8312051f51a25ac
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-08-19 20:55:35 +0400

    two way proxy in progress
    <==== Mercurial data ====>
    Branch: udp_hole

---
+++ common/src/network/universal_request_processor.h
@@ -0,0 +1,23 @@
+#ifndef __UNIVERSAL_REQUEST_PROCESSOR_H__
+#define __UNIVERSAL_REQUEST_PROCESSOR_H__
+
+#include "utils/network/tcp_connection_processor.h"
+
+class QnUniversalRequestProcessorPrivate;
+
+class QnUniversalRequestProcessor: public QnTCPConnectionProcessor
+{
+public:
+    QnUniversalRequestProcessor(TCPSocket* socket, QnTcpListener* owner);
+    QnUniversalRequestProcessor(QnUniversalRequestProcessorPrivate* priv, TCPSocket* socket, QnTcpListener* owner);
+    virtual ~QnUniversalRequestProcessor();
+
+protected:
+    virtual void run() override;
+    virtual void pleaseStop() override;
+    void processRequest();
+private:
+    Q_DECLARE_PRIVATE(QnUniversalRequestProcessor);
+};
+
+#endif // __UNIVERSAL_REQUEST_PROCESSOR_H__
