1552bb6369d0aa46b91a8c841cf5f4cbcae977c6
Sergey Ivanov <sivanov@networkoptix.com>
2013-08-20 18:01:10 +0400

    Linux build-env
    <==== Mercurial data ====>
    Branch: qt5port

--- build_environment/maven/prepare-package.sh
+++ build_environment/maven/prepare-package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export buildlib=${buildLib}
-export QTDIR=`qmake -query QT_INSTALL_LIBS`
-export QTPLUG=`qmake -query QT_INSTALL_PLUGINS`
+export QTDIR=`${environment.dir}/qt5/qtbase-${arch}/bin/qmake -query QT_INSTALL_LIBS`
+export QTPLUG=`${environment.dir}/qt5/qtbase-${arch}/bin/qmake -query QT_INSTALL_PLUGINS`
 
 
 if [[ -z $buildlib ]]; then
@@ -12,8 +12,8 @@ if [[ -z $buildlib ]]; then
 
   for i in ${qtlib1} ${qtlib2} ${qtlib3} ${qtlib4} ${qtlib5} ${qtlib6} ${qtlib7} ${qtlib8} ${qtlib9}
   do
-    cp -P `find $QTDIR -iname 'libQt'"$i"'.so*'` ${libdir}/build/bin/debug
-    cp -P `find $QTDIR -iname 'libQt'"$i"'.so*'` ${libdir}/build/bin/release  
+    cp -P `find $QTDIR -iname 'libQt5'"$i"'.so*'` ${libdir}/build/bin/debug
+    cp -P `find $QTDIR -iname 'libQt5'"$i"'.so*'` ${libdir}/build/bin/release  
   done
 
   for i in ${qtplugin1} ${qtplugin2} ${qtplugin3}
