1fb9ad9e85fd5ad939fb04f7c166110031e3dbdd
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-09-06 16:38:14 +0400

    added python scripts
    <==== Mercurial data ====>
    Branch: installer-with-client

---
+++ wixsetup/maven/wix-gen.py
@@ -0,0 +1,5 @@
+import os, sys
+os.system('${python.dir}/python generate-ec-wxs.py')
+os.system('${python.dir}/python generate-help-wxs.py')
+os.system('${python.dir}/python generate-vox-wxs.py')
+os.system('${python.dir}/python generate-bg-wxs.py')
