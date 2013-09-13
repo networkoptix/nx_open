d5dc38a8012bb652614289a8d58d7bb9cd6f5770
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-09-14 00:12:04 +0400

    moved buildenv.py to maven's environment. Added arch and toolchain to get_buildenv call
    <==== Mercurial data ====>
    Branch: build-refactor

---
+++ build_environment/maven/filter-resources/buildenv.py
@@ -0,0 +1,21 @@
+import os, sys, platform, subprocess
+from os import listdir
+from os.path import dirname, join, exists, isfile
+from pkg_resources import load_entry_point
+
+basedir = join(dirname(os.path.abspath(__file__)))
+sys.path.append(basedir + '/' + '../..')
+from main import get_executable_extension, get_environment_variable, cd
+
+basedir = join(dirname(os.path.abspath(__file__)))
+environment = get_environment_variable('environment')
+
+with cd(environment):
+        status = subprocess.call('hg pull -u', shell=True)
+        if status != 0:
+            sys.exit(status) 
+            
+with cd(environment):
+        status = subprocess.call('%s/get_buildenv.%s ${arch} ${toolchain.version}' % (environment, get_executable_extension()), shell=True)
+        if status != 0:
+            sys.exit(status)             
