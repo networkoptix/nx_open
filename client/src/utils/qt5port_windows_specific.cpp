1d04cfda86b79b9ecb8790b6d744daa236fb75c4
Alexander Fokin <afokin@networkoptix.com>
2013-09-25 16:21:16 +0400

    More Qt5 fixes.
    <==== Mercurial data ====>
    Branch: build-refactor

--- client/src/utils/qt5port_windows_specific.cpp
+++ client/src/utils/qt5port_windows_specific.cpp
@@ -1,26 +1,5 @@
 #include "qt5port_windows_specific.h"
 
-#ifdef Q_OS_WIN
-
-#include <QtGui/QGuiApplication>
-#include <QtGui/5.1.1/QtGui/qpa/qplatformnativeinterface.h>
-
-QWindow *windowForWidget(const QWidget *widget) {
-    if (QWindow *window = widget->windowHandle())
-        return window;
-    if (const QWidget *nativeParent = widget->nativeParentWidget())
-        return nativeParent->windowHandle();
-    return NULL;
-}
-
-HWND hwndForWidget(const QWidget *widget) {
-    if (QWindow *window = windowForWidget(widget))
-        if (window->handle())
-            return static_cast<HWND>(QGuiApplication::platformNativeInterface()->nativeResourceForWindow(QByteArrayLiteral("handle"), window));
-    return NULL;
-}
-#endif
-
 HWND widToHwnd(WId id) {
     return reinterpret_cast<HWND>(id);
 }
--- client/src/utils/qt5port_windows_specific.h
+++ client/src/utils/qt5port_windows_specific.h
@@ -3,16 +3,11 @@
 
 #include <QtGlobal>
 
-/** Qt5 windows-platform-dependent functions that are to be implemented. */
+/* Qt5 windows-platform-dependent functions that are to be implemented. */
 
 #ifdef Q_OS_WIN
 
-#include <QtGui/QPixmap>
-
 #include "windef.h"
-
-HWND hwndForWidget(const QWidget *widget);
-
 
 HWND widToHwnd(WId id);
 WId hwndToWid(HWND id);
