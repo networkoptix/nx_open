2a87fd422bc98db78e356e6985fcc44832faf6a0
Alexander Fokin <afokin@networkoptix.com>
2013-10-07 12:50:43 +0400

    Fixed #2385.
    <==== Mercurial data ====>
    Branch: build-refactor

--- client/src/ui/workaround/size_move_workaround_win.cpp
+++ client/src/ui/workaround/size_move_workaround_win.cpp
@@ -21,6 +21,16 @@ QnSizeMoveWorkaround::~QnSizeMoveWorkaround() {
 bool QnSizeMoveWorkaround::nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
     MSG *msg = static_cast<MSG *>(message);
     switch(msg->message) {
+    /* Workaround #1: Qt does not react to system help query. */
+    case WM_SYSCOMMAND: 
+        if(msg->wParam == SC_CONTEXTHELP) {
+            *result = 0; /* An application should return zero if it processes this message. */
+            QWhatsThis::enterWhatsThisMode();
+            return true;
+        }
+        return false;
+
+    /* Workaround #2: event loop starvation if event dispatching is done by the system. */
     case WM_ENTERSIZEMOVE:
         m_inSizeMove = true;
         return false;
