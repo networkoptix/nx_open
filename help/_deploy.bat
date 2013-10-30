3bc27d141471907cdc07b9ec7d4b87d2c7fbcf6c
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-10-30 09:57:32 +0400

    chinese help integration
    <==== Mercurial data ====>
    Branch: master

--- help/_deploy.bat
+++ help/_deploy.bat
@@ -1,4 +1,4 @@
-call mvn clean deploy -Dcustomization=nnodal
+@rem call mvn clean deploy -Dcustomization=nnodal
 call mvn clean deploy -Dcustomization=digitalwatchdog
 call mvn clean deploy 
 call mvn clean deploy -Dchild.customization=relong -Dhelp.language=chinese
