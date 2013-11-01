7be1f6115ccb8832322a246d4afdd9cf7fa99be3
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-11-01 21:04:29 +0400

    remove realtime priority
    <==== Mercurial data ====>
    Branch: stable_root

--- mediaserver/src/main.cpp
+++ mediaserver/src/main.cpp
@@ -484,7 +484,7 @@ int serverMain(int argc, char *argv[])
 
     defaultMsgHandler = qInstallMsgHandler(myMsgHandler);
 
-    platform->process(NULL)->setPriority(QnPlatformProcess::HighestPriority);
+    platform->process(NULL)->setPriority(QnPlatformProcess::HighPriority);
 
     ffmpegInit();
 
