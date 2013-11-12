392781c954188f8a4920c0e73db9bcc6df668491
Alexander Fokin <afokin@networkoptix.com>
2013-11-12 15:42:10 +0400

    Removed customization code as there are just too many of them.
    <==== Mercurial data ====>
    Branch: master

--- common/src/common/customization.h
+++ common/src/common/customization.h
@@ -9,6 +9,7 @@
 #include <version.h>
 
 struct QnProductFeatures {
+    QString customizationName;
     bool freeLicenseIsTrial;
     int freeLicenseCount;
     QString freeLicenseKey;
@@ -16,27 +17,10 @@ struct QnProductFeatures {
 
 
 namespace Qn {
-    enum Customization {
-        HdWitnessCustomization,
-        DwSpectrumCustomization,
-        NVisionCustomization
-    };
-
-    inline Qn::Customization calculateCustomization(const char *customizationName) {
-        if(std::strcmp(customizationName, "Vms") == 0 || std::strcmp(customizationName, "default") == 0) {
-            return HdWitnessCustomization;
-        } else if(std::strcmp(customizationName, "digitalwatchdog") == 0) {
-            return DwSpectrumCustomization;
-        } else if(std::strcmp(customizationName, "nnodal") == 0) {
-            return NVisionCustomization;
-        } else {
-            // assert(false); 
-            return HdWitnessCustomization;
-        }
-    }
 
     inline QnProductFeatures calculateProductFeatures() {
         QnProductFeatures result;
+        result.customizationName = QLatin1String(QN_CUSTOMIZATION_NAME);
         result.freeLicenseIsTrial = QN_FREE_LICENSE_IS_TRIAL;
         result.freeLicenseCount = QN_FREE_LICENSE_COUNT;
         result.freeLicenseKey = QLatin1String(QN_FREE_LICENSE_KEY);
@@ -46,22 +30,11 @@ namespace Qn {
 } // namespace Qn
 
 /**
- * \returns                             Customization with which this software
- *                                      version was built.
- */
-inline Qn::Customization qnCustomization() {
-    /* Note that Q_GLOBAL_STATIC and other synchronization is not needed here. */
-    static Qn::Customization result = Qn::calculateCustomization(QN_CUSTOMIZATION_NAME);
-    return result;
-}
-
-/**
  * \returns                             Product features that are available for
  *                                      customization with which this software
  *                                      version was built.
  */
 inline QnProductFeatures qnProductFeatures() {
-    /* Note that Q_GLOBAL_STATIC and other synchronization is not needed here. */
     static QnProductFeatures result = Qn::calculateProductFeatures(); // TODO: #Elric synch is actually needed!
     return result;
 }
