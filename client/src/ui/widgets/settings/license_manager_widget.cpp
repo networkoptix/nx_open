aa3fb3aec27514120bb4a0690faaa8714d4576df
Alexander Fokin <afokin@networkoptix.com>
2013-11-21 17:39:18 +0400

    Fixed #2438.
    <==== Mercurial data ====>
    Branch: release_2.0

--- client/src/ui/widgets/settings/license_manager_widget.cpp
+++ client/src/ui/widgets/settings/license_manager_widget.cpp
@@ -266,16 +266,28 @@ void QnLicenseManagerWidget::at_downloadFinished() {
 
         QByteArray replyData = reply->readAll();
 
+        // TODO: #Elric use JSON mapping here.
         // If we can deserialize JSON it means there is an error.
         QVariantMap errorMessage;
         if (QJson::deserialize(replyData, &errorMessage)) {
             QString error = errorMessage.value(lit("error")).toString();
-            // TODO: Afeksnadr Lokhin, find CORRECT message here
             QString messageId = errorMessage.value(lit("messageId")).toString();
             QString message = errorMessage.value(lit("message")).toString();
             QVariantMap arguments = errorMessage.value(lit("arguments")).toMap();
 
-            message = Mustache::renderTemplate(tr(message.toLatin1().constData()), arguments);
+            if(messageId == lit("DatabaseError")) {
+                message = tr("Database error has occurred.");
+            } else if(messageId == lit("InvalidData")) {
+                message = tr("Invalid data was received.");
+            } else if(messageId == lit("InvalidKey")) {
+                message = tr("The license key is invalid.");
+            } else if(messageId == lit("InvalidBrand")) {
+                message = tr("You are trying to activate {{brand}} license on {{db_brand}}. This is not allowed.");
+            } else if(messageId == lit("AlreadyActivated")) {
+                message = tr("This license key has been previously activated to {{hwid}} on {{time}}.");
+            }
+
+            message = Mustache::renderTemplate(message, arguments);
 
             QMessageBox::information(this, tr("License Activation"), tr("There was a problem activating your license.") + lit(" ") + message);
             ui->licenseWidget->setState(QnLicenseWidget::Normal);
