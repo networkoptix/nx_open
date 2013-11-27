b18323ef5c56c6a50a9db9acb27140513c3d2536
Sergey Ivanov <sivanov@networkoptix.com>
2013-11-27 15:11:29 +0400

    PTZ fix: model Sd83X1 added
    <==== Mercurial data ====>
    Branch: release_2.0

--- common/src/plugins/resources/onvif/onvif_ptz_controller.cpp
+++ common/src/plugins/resources/onvif/onvif_ptz_controller.cpp
@@ -111,7 +111,10 @@ int QnOnvifPtzController::stopMove()
 {
     // TODO: #Elric TOTALLY EVIL!!! Refactor properly.
     QString model = m_resource->getModel();
-    if(model == lit("SD8362") || model == lit("SD83X3") || model == lit("SD81X1"))
+    if(model == lit("SD8362") ||
+            model == lit("SD83X3") ||
+            model == lit("SD81X1") ||
+            model == lit("SD83X1"))
         return startMove(0, 0, 0);
 
     QAuthenticator auth(m_resource->getAuth());
