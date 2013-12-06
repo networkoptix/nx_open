1179a5607b19f0764fea7ee6de8eec815dccc4fc
Alexander Fokin <afokin@networkoptix.com>
2013-12-06 22:04:54 +0400

    Compiles.
    <==== Mercurial data ====>
    Branch: improved_ptz

--- client/src/client/client_model_functions.cpp
+++ client/src/client/client_model_functions.cpp
@@ -2,9 +2,9 @@
 
 #include "client_model_types.h"
 
-QN_DEFINE_STRUCT_FUNCTIONS(QnWorkbenchState, (datastream), (currentLayoutIndex)(layoutUuids), inline);
-QN_DEFINE_STRUCT_FUNCTIONS(QnServerStorageKey, (datastream)(eq)(hash), (serverUuid)(storagePath), inline);
-QN_DEFINE_STRUCT_FUNCTIONS(QnLicenseWarningState, (datastream), (lastWarningTime)(ignore), inline);
+QN_DEFINE_STRUCT_FUNCTIONS(QnWorkbenchState, (datastream), (currentLayoutIndex)(layoutUuids));
+QN_DEFINE_STRUCT_FUNCTIONS(QnServerStorageKey, (datastream)(eq)(hash), (serverUuid)(storagePath));
+QN_DEFINE_STRUCT_FUNCTIONS(QnLicenseWarningState, (datastream), (lastWarningTime)(ignore));
 
 
 
