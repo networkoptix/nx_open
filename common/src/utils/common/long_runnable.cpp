421713b827199f77b50f4b41e8529db3e2bc44a1
Alexander Fokin <afokin@networkoptix.com>
2013-12-10 14:29:01 +0400

    Long runnable pool in MS and MP.
    <==== Mercurial data ====>
    Branch: improved_ptz

--- common/src/utils/common/long_runnable.cpp
+++ common/src/utils/common/long_runnable.cpp
@@ -117,12 +117,11 @@ QnLongRunnable::QnLongRunnable():
         m_pool = pool->d;
         m_pool->createdNotify(this);
     } else {
-        // todo: #Roman add QnLongRunnable initialization to mediaServer and mediaProxy
-        //qnWarning("QnLongRunnablePool instance does not exist, lifetime of this runnable will not be tracked.");
+        qnWarning("QnLongRunnablePool instance does not exist, lifetime of this runnable will not be tracked.");
     }
 
-    connect(this, SIGNAL(started()), this, SLOT(at_started()), Qt::DirectConnection);
-    connect(this, SIGNAL(finished()), this, SLOT(at_finished()), Qt::DirectConnection);
+    connect(this, SIGNAL(started()),    this, SLOT(at_started()), Qt::DirectConnection);
+    connect(this, SIGNAL(finished()),   this, SLOT(at_finished()), Qt::DirectConnection);
 }
 
 QnLongRunnable::~QnLongRunnable() {
