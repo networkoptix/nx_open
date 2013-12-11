d6fed6158df21dbfffa01075f497eb827b52eef1
Sergey Yuldashev <syuldashev@networkoptix.com>
2013-12-11 17:03:33 +0300

    fixed archive restoring
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/recorder/storage_manager.cpp
+++ mediaserver/src/recorder/storage_manager.cpp
@@ -790,6 +790,10 @@ void QnStorageManager::addDataToCatalog(DeviceFileCatalogPtr newCatalog, const Q
 
             existingCatalog = catalog[mac] = newCatalog;
         }
+		else if (existingCatalog->isEmpty())
+		{
+			existingCatalog->m_chunks = newCatalog->m_chunks;
+		}
     }
     existingCatalog->rewriteCatalog(isLastRecordRecording);
 }
