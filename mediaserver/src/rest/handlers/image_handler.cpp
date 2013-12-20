108b48c08e5336665e98e10f0ac36bf88c0f9d08
Roman Vasilenko <rvasilenko@networkoptix.com>
2013-12-20 21:24:19 +0400

    debug code removed
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/rest/handlers/image_handler.cpp
+++ mediaserver/src/rest/handlers/image_handler.cpp
@@ -235,7 +235,7 @@ int QnImageHandler::executeGet(const QString& path, const QnRequestParamList& pa
     PixelFormat ffmpegColorFormat = PIX_FMT_RGBA;
     QImage::Format qtColorFormat = QImage::Format_ARGB32_Premultiplied;
     int pixelBytes = 4;
-    if (colorSpace == "gray8" || 1) {
+    if (colorSpace == "gray8") {
         ffmpegColorFormat = PIX_FMT_GRAY8;
         qtColorFormat = QImage::Format_Indexed8;
         pixelBytes = 1;
