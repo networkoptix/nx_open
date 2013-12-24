47aedb57699b2969da34c00360b08765ac5ef0e9
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2013-12-24 19:00:26 +0400

    get rid of installer.customization
    <==== Mercurial data ====>
    Branch: release_2.1

--- wixsetup/maven/generate-vox-wxs.py
+++ wixsetup/maven/generate-vox-wxs.py
@@ -7,6 +7,5 @@ def fixasfiles():
     open('ClientVox.wxs', 'w').write(text)
 
 if __name__ == '__main__':
-    os.system(r'heat dir ${ClientVoxSourceDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientVox.wxs -cg ClientVoxComponent -dr ${installer.customization}VoxDir -var var.ClientVoxSourceDir')
+    os.system(r'heat dir ${ClientVoxSourceDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientVox.wxs -cg ClientVoxComponent -dr ${customization}VoxDir -var var.ClientVoxSourceDir')
     #fixasfiles()
-
