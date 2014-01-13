fab9df778e060306b06d419f9fd631fc0164be39
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-01-13 18:30:27 +0400

    got rid of installer.customization
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/maven/filter-resources/generate-bg-wxs.py
+++ wixsetup/maven/filter-resources/generate-bg-wxs.py
@@ -2,7 +2,7 @@ import os, sys, subprocess
 from subprocess import Popen, PIPE
 
 if __name__ == '__main__':
-    p = subprocess.Popen(r'heat dir ${ClientBgSourceDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientBg.wxs -cg ClientBgComponent -dr ${installer.customization}BgDir -var var.ClientBgSourceDir', shell=True, stdout=PIPE)
+    p = subprocess.Popen(r'heat dir ${ClientBgSourceDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientBg.wxs -cg ClientBgComponent -dr ${customization}BgDir -var var.ClientBgSourceDir', shell=True, stdout=PIPE)
     out, err = p.communicate()
     print out
     p.wait()
