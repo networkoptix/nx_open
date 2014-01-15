a15d7efeacbe7d2cf7614c97230711d588051d81
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-01-15 21:28:03 +0400

    child customization is gone parent is here
    <==== Mercurial data ====>
    Branch: master

--- help/_deploy.bat
+++ help/_deploy.bat
@@ -1,4 +1,4 @@
 @rem call mvn clean deploy -Dcustomization=nnodal
 call mvn clean deploy -Dcustomization=digitalwatchdog
 call mvn clean deploy 
-call mvn clean deploy -Dchild.customization=relong -Dhelp.language=chinese
+call mvn clean deploy -Dcustomization=relong -Dhelp.language=chinese
