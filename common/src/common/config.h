7f2916fc1fb5528854985893a226fc1fdd6ab6e4
Alexander Fokin <afokin@networkoptix.com>
2014-01-17 16:42:15 +0400

    nullptr for mac.
    <==== Mercurial data ====>
    Branch: master

--- common/src/common/config.h
+++ common/src/common/config.h
@@ -52,15 +52,33 @@
 #ifdef __cplusplus
 #   undef NULL
 #   define NULL nullptr
+#
 #   ifdef __GNUC__
 #       undef __null
 #       define __null nullptr
 #   endif
+#
+#   if defined(__APPLE__) && !defined(_NATIVE_NULLPTR_SUPPORTED)
+/* No nullptr on this system... We do it the evil way. */
+namespace {
+    const class nullptr_t {
+    public:
+        /* Convertible to any type of null non-member pointer... */
+        template<class T>
+        inline operator T*() const { return 0; } 
+
+        /* ...or any type of null member pointer. */
+        template<class C, class T>
+        inline operator T C::*() const { return 0; } 
+
+    private:
+        /* Can't take address of nullptr. */
+        void operator&() const;  
+    } nullptr = {};
+}
+#   endif
 #endif
 
-#ifdef __APPLE__
-    #define nullptr 0
-#endif
 
 /* Define foreach */
 //#define foreach BOOST_FOREACH
