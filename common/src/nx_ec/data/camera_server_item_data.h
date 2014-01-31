5a95613026aed42b01f722312cbd4de023dce3db
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-01-31 10:48:20 -0800

    add missed files
    <==== Mercurial data ====>
    Branch: new_ec

---
+++ common/src/nx_ec/data/camera_server_item_data.h
@@ -0,0 +1,39 @@
+#include "api_data.h"
+#ifndef __CAMERA_SERVER_ITEM_DATA_H__
+#define __CAMERA_SERVER_ITEM_DATA_H__
+
+#include "ec2_resource_data.h"
+#include "core/resource/camera_history.h"
+
+namespace ec2
+{
+
+struct ApiCameraServerItemData: public ApiData
+{
+    ApiCameraServerItemData(): timestamp(0) {}
+
+    QString  physicalId;
+    QString  serverGuid;
+    qint64   timestamp; 
+
+    QN_DECLARE_STRUCT_SERIALIZATORS_BINDERS();
+};
+
+struct ApiCameraServerItemDataList: public ApiData
+{
+    std::vector<ApiCameraServerItemData> data;
+
+    QN_DECLARE_STRUCT_SERIALIZATORS();
+
+    void loadFromQuery(QSqlQuery& query);
+    void toResourceList(QnCameraHistoryList& outData) const;
+};
+
+}
+
+#define ApiCameraServerItemFields (physicalId) (serverGuid) (timestamp)
+QN_DEFINE_STRUCT_SERIALIZATORS_BINDERS (ec2::ApiCameraServerItemData, ApiCameraServerItemFields)
+QN_DEFINE_STRUCT_SERIALIZATORS (ec2::ApiCameraServerItemDataList, (data) )
+
+
+#endif // __CAMERA_SERVER_ITEM_DATA_H__
