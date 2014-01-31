f4622b9e015bd085f9181f917cc5b4c26e2b7f32
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-01-30 16:11:14 -0800

    add missed files
    <==== Mercurial data ====>
    Branch: new_ec

---
+++ common/src/nx_ec/data/mserver_data.cpp
@@ -0,0 +1,100 @@
+#include "mserver_data.h"
+#include "core/resource/media_server_resource.h"
+#include "core/resource/storage_resource.h"
+#include "core/resource/resource_type.h"
+
+namespace ec2
+{
+
+static QHostAddress stringToAddr(const QString& hostStr)
+{
+    return QHostAddress(hostStr);
+}
+
+
+static void deserializeNetAddrList(QList<QHostAddress>& netAddrList, const QString& netAddrListString)
+{
+    QStringList addListStrings = netAddrListString.split(QLatin1Char(';'));
+    std::transform(addListStrings.begin(), addListStrings.end(), std::back_inserter(netAddrList), stringToAddr);
+}
+
+static QString serializeNetAddrList(const QList<QHostAddress>& netAddrList)
+{
+    QStringList addListStrings;
+    std::transform(netAddrList.begin(), netAddrList.end(), std::back_inserter(addListStrings), std::mem_fun_ref(&QHostAddress::toString));
+    return addListStrings.join(QLatin1String(";"));
+}
+
+void ApiStorageData::toResource(QnAbstractStorageResourcePtr resource) const
+{
+    resource->setId(id);
+    resource->setName(name);
+    resource->setUrl(url);
+    resource->setSpaceLimit(spaceLimit);
+    resource->setUsedForWriting(usedForWriting);
+}
+
+QnResourceParameters ApiStorageData::toHashMap() const
+{
+    QnResourceParameters parameters;
+    parameters["id"] = id;
+    parameters["parentId"] = parentId;
+    parameters["name"] = name;
+    parameters["url"] = url;
+    parameters["spaceLimit"] = QString::number(spaceLimit);
+    parameters["usedForWriting"] = QString::number(usedForWriting);
+
+    return parameters;
+}
+
+void ApiStorageDataList::loadFromQuery(QSqlQuery& query)
+{
+    QN_QUERY_TO_DATA_OBJECT(ApiStorageData, data, ApiStorageDataFields ApiResourceDataFields)
+}
+
+void ApiMediaServerData::toResource(QnMediaServerResourcePtr resource, QnResourceFactory* factory) const
+{
+    ApiResourceData::toResource(resource);
+
+    QList<QHostAddress> resNetAddrList;
+    deserializeNetAddrList(resNetAddrList, netAddrList);
+
+    resource->setUrl(apiUrl);
+    resource->setNetAddrList(resNetAddrList);
+    resource->setReserve(reserve);
+    resource->setPanicMode((QnMediaServerResource::PanicMode) panicMode);
+    resource->setStreamingUrl(streamingUrl);
+    resource->setVersion(QnSoftwareVersion(version));
+    //resource->setAuthKey(authKey);
+
+    QnResourceTypePtr resType = qnResTypePool->getResourceTypeByName(QLatin1String("Storage"));
+    if (!resType)
+        return;
+
+    QnAbstractStorageResourceList storagesRes;
+    foreach(const ApiStorageData& storage, storages) {
+        QnAbstractStorageResourcePtr storageRes = factory->createResource(resType->getId(), storage.toHashMap()).dynamicCast<QnAbstractStorageResource>();
+        
+        storage.toResource(storageRes);
+        storagesRes.push_back(storageRes);
+    }
+    resource->setStorages(storagesRes);
+}
+
+void ApiMediaServerDataList::loadFromQuery(QSqlQuery& query)
+{
+    QN_QUERY_TO_DATA_OBJECT(ApiMediaServerData, data, medisServerDataFields ApiResourceDataFields)
+}
+
+void ApiMediaServerDataList::toResourceList(QnMediaServerResourceList& outData, QnResourceFactory* factory) const
+{
+    outData.reserve(data.size());
+    for(int i = 0; i < data.size(); ++i) 
+    {
+        QnMediaServerResourcePtr server(new QnMediaServerResource());
+        data[i].toResource(server, factory);
+        outData << server;
+    }
+}
+
+}
