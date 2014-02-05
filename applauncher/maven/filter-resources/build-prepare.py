3c6f0bb2f6ad8e58b9d2d91033c49d3e0e84ae49
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-02-05 19:03:00 +0400

    added light client compilation for dw
    <==== Mercurial data ====>
    Branch: master

--- applauncher/maven/filter-resources/build-prepare.py
+++ applauncher/maven/filter-resources/build-prepare.py
@@ -3,7 +3,3 @@
 import os, sys, posixpath
 sys.path.insert(0, '${basedir}/../common')
 #sys.path.insert(0, os.path.join('..', '..', 'common'))
-
-from gencomp import gencomp_cpp
-#os.makedirs('${project.build.directory}/build/release/generated/')
-gencomp_cpp(open('${project.build.sourceDirectory}/compatibility_info.cpp', 'w'))
