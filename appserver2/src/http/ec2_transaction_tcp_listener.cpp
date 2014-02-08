e434d80ae6ad7d5f61435c2e31f33ffc633779db
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-02-08 14:08:06 -0800

    api events
    <==== Mercurial data ====>
    Branch: new_ec

---
+++ appserver2/src/http/ec2_transaction_tcp_listener.cpp
@@ -0,0 +1,53 @@
+#include "ec2_transaction_tcp_listener.h"
+#include "utils/network/tcp_connection_priv.h"
+#include "transaction/transaction_message_bus.h"
+
+namespace ec2
+{
+
+// -------------------------- QnTransactionTcpProcessor ---------------------
+
+
+class QnTransactionTcpProcessorPrivate: public QnTCPConnectionProcessorPrivate
+{
+public:
+
+    QnTransactionTcpProcessorPrivate(): 
+        QnTCPConnectionProcessorPrivate()
+    {
+    }
+};
+
+QnTransactionTcpProcessor::QnTransactionTcpProcessor(QSharedPointer<AbstractStreamSocket> socket, QnTcpListener* _owner):
+    QnTCPConnectionProcessor(new QnTransactionTcpProcessorPrivate, socket)
+{
+    Q_UNUSED(_owner)
+    Q_D(QnTransactionTcpProcessor);
+
+    setObjectName( "QnTransactionTcpProcessor" );
+}
+
+QnTransactionTcpProcessor::~QnTransactionTcpProcessor()
+{
+    Q_D(QnTransactionTcpProcessor);
+    stop();
+}
+
+void QnTransactionTcpProcessor::run()
+{
+    Q_D(QnTransactionTcpProcessor);
+    initSystemThreadId();
+
+    if (d->clientRequest.isEmpty()) {
+        if (!readRequest())
+            return;
+    }
+    parseRequest();
+    d->chunkedMode = true;
+    sendResponse("HTTP", CODE_OK, "application/octet-stream");
+
+    QnTransactionMessageBus::instance()->gotConnectionFromRemotePeer(d->socket);
+    d->socket.clear();
+}
+
+}
