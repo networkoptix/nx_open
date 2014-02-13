f9661a27eeeef863cbf2927a5f895ee7e981f4c4
Denis Klychkov <dklychkov@networkoptix.com>
2014-02-13 19:54:40 +0400

    add light mode detection class dummy
    <==== Mercurial data ====>
    Branch: light_mode_detection

---
+++ client/src/utils/performance_test.cpp
@@ -0,0 +1,8 @@
+#include "performance_test.h"
+
+#include <QtCore/QFile>
+
+
+int QnPerformanceTest::getOptimalLightMode() {
+    return 0;
+}
---
+++ client/src/utils/performance_test.h
@@ -0,0 +1,9 @@
+#ifndef PERFORMANCE_TEST_H
+#define PERFORMANCE_TEST_H
+
+class QnPerformanceTest {
+public:
+    static int getOptimalLightMode();
+};
+
+#endif // PERFORMANCE_TEST_H
