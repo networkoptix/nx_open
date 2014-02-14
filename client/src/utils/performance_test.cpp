deeb7f0e868ff68425ec7841727079d9e4bbb0c5
Denis Klychkov <dklychkov@networkoptix.com>
2014-02-14 15:17:46 +0400

    add simple GPU name test
    <==== Mercurial data ====>
    Branch: light_mode_detection

--- client/src/utils/performance_test.cpp
+++ client/src/utils/performance_test.cpp
@@ -1,34 +1,32 @@
 #include "performance_test.h"
 
+#include <GL/gl.h>
+
 #include <QtCore/QFile>
 #include <QtCore/QRegExp>
 
-#ifdef Q_OS_LINUX
-namespace {
-    QString getCpuName() {
-        QFile cpuInfo(QLatin1String("/proc/cpuinfo"));
-        if (!cpuInfo.open(QFile::ReadOnly))
-            return QString();
-
-        QString content = QLatin1String(cpuInfo.readAll());
-        cpuInfo.close();
-
-        /* Information about cores is separated by empty lines */
-//        int coreNum = content.count("\n\n") + 1;
-
-        QRegExp modelNameRegExp(QLatin1String("model name\\s+: (.*)\n"));
-        modelNameRegExp.setMinimal(true);
-        QString modelName;
-        if (modelNameRegExp.indexIn(content) != -1)
-            modelName = modelNameRegExp.cap(1);
-        return modelName;
-    }
-}
-#endif
+#include <utils/common/performance.h>
+#include <client/client_globals.h>
 
 int QnPerformanceTest::getOptimalLightMode() {
 #ifdef Q_OS_LINUX
-    QString cpuName = getCpuName();
+    bool poorCpu = false;
+    bool poorGpu = false;
+
+    QString cpuName = QnPerformance::cpuName();
+    QRegExp atomCpuRegExp("Intel\\(R\\) Atom\\(TM\\) CPU .*");
+    if (atomCpuRegExp.exactMatch(cpuName))
+        poorCpu = true;
+
+    QString renderer = glGetString(GL_RENDERER);
+    QRegExp slowRendererRegExp("Gallium .* on llvmpipe .*");
+    if (slowRendererRegExp.exactMatch(renderer))
+        poorGpu = true;
+
+    if (poorCpu && poorGpu)
+        return Qn::LightModeFull;
+    else
+        return 0;
 #endif
     return 0;
 }
