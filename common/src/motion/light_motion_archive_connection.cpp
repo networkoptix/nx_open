fc611bebd808bd8a2970ad850606755e003773f1
Sergey Ivanov <sivanov@networkoptix.com>
2014-03-03 18:13:04 +0400

    Bug #2779: Export multi-video from multi-video works incorrectly
    <==== Mercurial data ====>
    Branch: release_2.2

--- common/src/motion/light_motion_archive_connection.cpp
+++ common/src/motion/light_motion_archive_connection.cpp
@@ -3,15 +3,15 @@
 
 QnLightMotionArchiveConnection::QnLightMotionArchiveConnection(const QnMetaDataLightVector& data, int channel):
     m_motionData(data),
-    m_channel(channel),
-    m_lastResult(new QnMetaDataV1())
+    m_channel(channel)
 {
 
 }
 
 QnMetaDataV1Ptr QnLightMotionArchiveConnection::getMotionData(qint64 timeUsec)
 {
-    if (m_motionData.empty() || m_lastResult->containTime(timeUsec))
+    if (m_motionData.empty() ||
+            (m_lastResult && m_lastResult->containTime(timeUsec)))  //TODO: #rvasilenko Is this check sane? Why do we return empty result?
         return QnMetaDataV1Ptr();
 
     qint64 timeMs = timeUsec/1000;
