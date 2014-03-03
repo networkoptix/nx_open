6cbd251be281dec9a9e8142c7a8c5b463f884ab1
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-03-03 21:34:55 +0400

    add missed files
    <==== Mercurial data ====>
    Branch: new_ec

---
+++ common/src/nx_ec/data/server_alive_data.h
@@ -0,0 +1,21 @@
+#ifndef __SERVER_ALIVE_DATA_H_
+#define __SERVER_ALIVE_DATA_H_
+
+#include "nx_ec/ec_api.h"
+#include "api_data.h"
+#include "utils/common/id.h"
+
+namespace ec2
+{
+
+struct ApiServerAliveData: public ApiData
+{
+    QnId serverId;
+    bool isAlive;
+};
+
+}
+
+QN_DEFINE_STRUCT_SERIALIZATORS (ec2::ApiServerAliveData, (serverId) (isAlive) )
+
+#endif // __SERVER_ALIVE_DATA_H_
