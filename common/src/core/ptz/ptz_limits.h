c2c4f4ea6a6a94ef55a8626038a131f5a265e134
Alexander Fokin <afokin@networkoptix.com>
2014-03-03 16:24:01 +0400

    Fixes in ptz pan range bounding.
    <==== Mercurial data ====>
    Branch: release_2.2

--- common/src/core/ptz/ptz_limits.h
+++ common/src/core/ptz/ptz_limits.h
@@ -7,6 +7,8 @@
 #include <QtCore/QMetaType>
 #include <QtGui/QVector3D>
 
+#include <utils/math/math.h>
+
 struct QnPtzLimits: public boost::equality_comparable1<QnPtzLimits> {
     QnPtzLimits(): minPan(0), maxPan(360), minTilt(-90), maxTilt(90), minFov(0), maxFov(360) {}
 
@@ -30,9 +32,39 @@ inline QVector3D qBound(const QVector3D &position, const QnPtzLimits &limits) {
     if(qFuzzyCompare(panRange, 360) || panRange > 360)
         unlimitedPan = true;
 
+    qreal pan = position.x();
+    if(!unlimitedPan && !qBetween(limits.minPan, pan, limits.maxPan)) {
+        /* Round it to the nearest boundary. */
+        qreal panBase = limits.minPan - qMod(limits.minPan, 360.0);
+        qreal panShift = qMod(pan, 360.0);
+
+        qreal bestPan = pan;
+        qreal bestDist = std::numeric_limits<qreal>::max();
+
+        pan = panBase - 360.0 + panShift;
+        for(int i = 0; i < 3; i++) {
+            qreal dist;
+            if(pan < limits.minPan) {
+                dist = limits.minPan - pan;
+            } else if(pan > limits.maxPan) {
+                dist = pan - limits.maxPan;
+            } else {
+                dist = 0.0;
+            }
+
+            if(dist < bestDist) {
+                bestDist = dist;
+                bestPan = pan;
+            }
+
+            pan += 360.0;
+        }
+
+        pan = bestPan;
+    }
+
     return QVector3D(
-        unlimitedPan ? position.x() : 
-        qBound<float>(limits.minPan,  position.x(), limits.maxPan),
+        pan,
         qBound<float>(limits.minTilt, position.y(), limits.maxTilt),
         qBound<float>(limits.minFov,  position.z(), limits.maxFov)
     );
