ff4095138978296c0b8271dad21775e910c698e2
Alexander Fokin <afokin@networkoptix.com>
2014-03-04 17:10:43 +0400

    Histogram customization.
    <==== Mercurial data ====>
    Branch: release_2.2

--- client/src/ui/widgets/histogram_widget.cpp
+++ client/src/ui/widgets/histogram_widget.cpp
@@ -24,14 +24,13 @@ void QnHistogramWidget::setHistogramParams(const ImageCorrectionParams& params)
     update();
 }
 
-void QnHistogramWidget::paintEvent(QPaintEvent *event)
+void QnHistogramWidget::paintEvent(QPaintEvent *)
 {
-    Q_UNUSED(event)
-    QPainter p(this);
+    QPainter painter(this);
     
-    p.setBrush(QBrush(Qt::Dense5Pattern));
-    p.setPen(Qt::NoPen);
-    p.drawRect(rect());
+    painter.setBrush(QBrush(Qt::Dense5Pattern));
+    painter.setPen(Qt::NoPen);
+    painter.drawRect(rect());
 
     double w = width();
     if (isEnabled() && m_params.enabled)
@@ -62,32 +61,32 @@ void QnHistogramWidget::paintEvent(QPaintEvent *event)
             prevY = curY;
         }
 
-        p.setPen(Qt::white);
-        p.drawLines(lines);
-        p.setPen(QColor(0x60, 0x60, 0x60));
-        p.drawLines(lines2);
+        painter.setPen(Qt::white);
+        painter.drawLines(lines);
+        painter.setPen(QColor(0x60, 0x60, 0x60));
+        painter.drawLines(lines2);
 
         const QColor selectionColor = qnGlobals->selectionColor();
-        p.setPen(selectionColor.lighter());
-        p.setBrush(selectionColor);
+        painter.setPen(selectionColor.lighter());
+        painter.setBrush(selectionColor);
         double xScale = w / 256;
-        p.drawRect(QRect(qAbs(m_data.bCoeff * 256.0) * xScale, 1,  256.0 / m_data.aCoeff * xScale + 0.5, height()));
+        painter.drawRect(QRect(qAbs(m_data.bCoeff * 256.0) * xScale, 1,  256.0 / m_data.aCoeff * xScale + 0.5, height()));
         
-        p.setPen(Qt::white);
-        p.drawText(QRect(2, 2, width() - 4, height() - 4), Qt::AlignRight, tr("Gamma %1").arg(m_data.gamma, 0, 'f', 2));
+        painter.setPen(Qt::white);
+        painter.drawText(QRect(2, 2, width() - 4, height() - 4), Qt::AlignRight, tr("Gamma %1").arg(m_data.gamma, 0, 'f', 2));
     }
 
     QPen pen;
     pen.setStyle(Qt::DashLine);
     pen.setWidth(1);
     pen.setBrush(QColor(0,255,0, 30));
-    p.setPen(pen);
-    p.drawLine(width()/4, 0, width()/4, height());
-    p.drawLine(width()/2, 0, width()/2, height());
-    p.drawLine(width()/4*3, 0, width()/4*3, height());
-    p.drawLine(0, height()/2, width(), height()/2);
+    painter.setPen(pen);
+    painter.drawLine(width()/4, 0, width()/4, height());
+    painter.drawLine(width()/2, 0, width()/2, height());
+    painter.drawLine(width()/4*3, 0, width()/4*3, height());
+    painter.drawLine(0, height()/2, width(), height()/2);
 
-    p.setBrush(Qt::NoBrush);
-    p.setPen(Qt::white);
-    p.drawRect(QRect(0, 0, width() - 1, height() - 1));
+    painter.setBrush(Qt::NoBrush);
+    painter.setPen(Qt::white);
+    painter.drawRect(QRect(0, 0, width() - 1, height() - 1));
 }
