05b4fa7f6912a0ceca184502bc22bb1029c7ff85
Alexander Fokin <afokin@networkoptix.com>
2014-03-04 10:09:22 +0400

    zh_TW flag.
    <==== Mercurial data ====>
    Branch: release_2.2

--- common/translations/common_zh_TW.ts
+++ common/translations/common_zh_TW.ts
@@ -41,7 +41,7 @@
         <location filename="../src/translation/translation_manager.cpp" line="156"/>
         <source>Locale Code</source>
         <extracomment>Internal. Please don&apos;t change existing translation.</extracomment>
-        <translation>地區代碼</translation>
+        <translation>zh_TW</translation>
     </message>
 </context>
 <context>
