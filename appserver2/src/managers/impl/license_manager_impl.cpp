5e5ee0eab8035a7e3f2fe4cb57227c177d806fd0
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2014-03-05 14:23:51 +0400

    Added hardcoded license for DW customization
    <==== Mercurial data ====>
    Branch: new_ec

--- appserver2/src/managers/impl/license_manager_impl.cpp
+++ appserver2/src/managers/impl/license_manager_impl.cpp
@@ -6,7 +6,7 @@
 #include "license_manager_impl.h"
 
 
-static const char TEST_LICENSE[] = 
+static const char TEST_LICENSE_NX[] = 
     "NAME=hdwitness\n"
     "SERIAL=5A7B-EYJO-914U-7MKD\n"
     "HWID=0333333333333333333333333333333333\n"
@@ -17,6 +17,19 @@ static const char TEST_LICENSE[] =
     "EXPIRATION=\n"
     "SIGNATURE=Tb7NULLsgJb6cj0cZdYLGC04Y/i2etfCQqUZbL4I6hXstWfCnoV5qQPoDvTY8gpVGR9RfPLeHLiDzbuFW3p1ZQ==\n"
     "SIGNATURE2=n2uJGJ9f/La1GgO7SHkxdTrErcs0NvK7PbYbdkUmJWAUXItKCudMNi6YPQMqG4aCc2F5XnfdWNGAPLlELVjnng==\n";
+
+static const char TEST_LICENSE_DW[] = 
+    "NAME=dwspectrum\n"
+    "SERIAL=EWXZ-OXEW-NTRP-T8RP\n"
+    "HWID=0333333333333333333333333333333333\n"
+    "COUNT=100\n"
+    "CLASS=digital\n"
+    "VERSION=20\n"
+    "BRAND=dwspectrum\n"
+    "EXPIRATION=\n"
+    "SIGNATURE=N5hqvo0eBbPcuCEGVQJ29XTYZalkpThlZTcTkSLvqKSGWfbv2KCIV/0ntOhRM+JiX0oysOhM81/2oTtK37VlDw==\n"
+    "SIGNATURE2=CrZsJh2xzOhKsMRgFA9AztwYrDLed6KgSLxdbGjEQ5cpqL0m8GxE9A/jNWbHP9MGWynfHsFGVWTeMCtnRkDIlg==\n";
+
 
 namespace ec2
 {
@@ -29,9 +42,13 @@ namespace ec2
         //TODO/IMPL
 
         ApiLicense license;
-        license.licenseBlock = TEST_LICENSE;
 
+        license.licenseBlock = TEST_LICENSE_NX;
         licList->data.push_back( license );
+
+        license.licenseBlock = TEST_LICENSE_DW;
+        licList->data.push_back( license );
+
         return ErrorCode::ok;
     }
 
