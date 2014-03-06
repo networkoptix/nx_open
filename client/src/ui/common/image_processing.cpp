6754b622d3f1b9ce64fed5ac4b8b32a26a90530f
Alexander Fokin <afokin@networkoptix.com>
2014-03-06 11:48:08 +0400

    Clock customization.
    <==== Mercurial data ====>
    Branch: release_2.2

--- client/src/ui/common/image_processing.cpp
+++ client/src/ui/common/image_processing.cpp
@@ -89,4 +89,19 @@ void gaussianBlur(const QImage &src, qreal sigma, QImage *dst) {
     gaussianBlurInternal(src.convertToFormat(QImage::Format_ARGB32), sigma, *dst);
 }
 
-
+QImage cropToAspectRatio(const QImage &source, const qreal targetAspectRatio) {
+    QImage result = source;
+    qreal aspectRatio = (qreal)source.width() / (qreal)source.height();
+    if (!qFuzzyCompare(aspectRatio, targetAspectRatio)) {
+        if (targetAspectRatio > aspectRatio) {
+            int targetHeight = qRound((qreal)source.width() / targetAspectRatio);
+            int offset = (source.height() - targetHeight) / 2;
+            result = source.copy(0, offset, source.width(), targetHeight);
+        } else {
+            int targetWidth = qRound((qreal)source.height() * targetAspectRatio);
+            int offset = (source.width() - targetWidth / 2);
+            result = source.copy(offset, 0, targetWidth, source.height());
+        }
+    }
+    return result;
+}
