9443bef2fa2aac26f5c3fb789019161f44045918
Sergey Ivanov <sivanov@networkoptix.com>
2014-03-17 18:33:07 +0400

    EC2 fix rollback
    <==== Mercurial data ====>
    Branch: feature_2318_video_wall_2.2

--- client/src/ui/models/resource_pool_model.cpp
+++ client/src/ui/models/resource_pool_model.cpp
@@ -614,11 +614,11 @@ void QnResourcePoolModel::at_resource_itemAdded(const QnLayoutResourcePtr &layou
     QnResourcePoolModelNode *node = this->node(item.uuid);
 
     QnResourcePtr resource;
-    //if(item.resource.id.isValid()) { // TODO: #EC2
-        //resource = resourcePool()->getResourceById(item.resource.id);
-    //} else {
+    if(item.resource.id.isValid()) { // TODO: #EC2
+        resource = resourcePool()->getResourceById(item.resource.id);
+    } else {
         resource = resourcePool()->getResourceByUniqId(item.resource.path);
-    //}
+    }
 
     node->setResource(resource);
     node->setParent(parentNode);
