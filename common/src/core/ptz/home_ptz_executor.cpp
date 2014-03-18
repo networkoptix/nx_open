3b97397800eb7025351e246c150fe6b87d173571
Alexander Fokin <afokin@networkoptix.com>
2014-03-18 14:10:31 +0400

    PTZ home timeout.
    <==== Mercurial data ====>
    Branch: release_2.2

--- common/src/core/ptz/home_ptz_executor.cpp
+++ common/src/core/ptz/home_ptz_executor.cpp
@@ -6,8 +6,8 @@
 #include "ptz_object.h"
 
 namespace {
-    //int homeTimeout = 5 * 60 * 1000;
-    int homeTimeout = 10 * 1000;
+    int homeTimeout = 2 * 60 * 1000;
+    //int homeTimeout = 10 * 1000;
 }
 
 // -------------------------------------------------------------------------- //
