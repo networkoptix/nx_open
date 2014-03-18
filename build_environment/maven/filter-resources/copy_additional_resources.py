9403cc363f4666b5afd1e1b735f9c8a7cb078bef
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2014-03-18 16:54:36 +0400

    Setting up build environment for ISD s2
    <==== Mercurial data ====>
    Branch: new_ec

--- build_environment/maven/filter-resources/copy_additional_resources.py
+++ build_environment/maven/filter-resources/copy_additional_resources.py
@@ -59,9 +59,9 @@ if __name__ == '__main__':
             
     if get_platform() == 'windows':        
         for arch in ('x86', 'x64'):
-            plugin_source_dir = '${environment.dir}/qt5/qtbase-%s/plugins' % arch
-            lib_source_dir = '${environment.dir}/qt5/qtbase-%s/bin' % arch
-            pdb_source_dir = '${environment.dir}/qt5/qtbase-%s/lib' % arch
+            plugin_source_dir = '${qt.dir}/plugins' % arch
+            lib_source_dir = '${qt.dir}/bin' % arch
+            pdb_source_dir = '${qt.dir}/lib' % arch
             target_dir = join('${project.build.directory}', arch, 'bin')
             lib_target_dir = join('${project.build.directory}', arch, 'lib')
             help_dir = join('${project.build.directory}', arch, 'bin/help')
@@ -114,7 +114,7 @@ if __name__ == '__main__':
     else:     
         lib_source_dir = '${qt.dir}/lib'
         lib_target_dir = join('${project.build.directory}', 'lib')
-        plugin_source_dir = '${environment.dir}/qt5/qtbase-${arch}/plugins'	
+        plugin_source_dir = '${qt.dir}/plugins'	
         target_dir = join('${project.build.directory}', 'bin')
         help_dir = join('${project.build.directory}', 'bin/help')
         
