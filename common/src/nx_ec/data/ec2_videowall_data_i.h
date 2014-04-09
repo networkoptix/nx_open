074d255ee2fbf8078d87a26b3a0dff74c1228cc5
Ivan Vigasin <ivigasin@networkoptix.com>
2014-04-09 19:35:47 +0400

    fix ios client compilation
    <==== Mercurial data ====>
    Branch: new_ec_ios

--- common/src/nx_ec/data/ec2_videowall_data_i.h
+++ common/src/nx_ec/data/ec2_videowall_data_i.h
@@ -6,6 +6,6 @@
     };
 
     #define ApiVideowallDataFields (autorun)
-    QN_DEFINE_DERIVED_STRUCT_SERIALIZATORS(ApiVideowallData, ec2::ApiResourceData, ApiVideowallDataFields);
+    QN_DEFINE_DERIVED_STRUCT_SERIALIZATORS(ApiVideowallData, ApiResourceData, ApiVideowallDataFields);
 
     QN_DEFINE_API_OBJECT_LIST_DATA(ApiVideowall)
