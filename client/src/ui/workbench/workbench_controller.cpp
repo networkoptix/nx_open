210c2df6a92fab6d3d6590f5379e637a3ee1eb80
Denis Klychkov <dklychkov@networkoptix.com>
2014-04-16 12:45:16 +0400

    fix Bug #3196: screen recording + cancel
    <==== Mercurial data ====>
    Branch: release_2.2

--- client/src/ui/workbench/workbench_controller.cpp
+++ client/src/ui/workbench/workbench_controller.cpp
@@ -652,11 +652,11 @@ void QnWorkbenchController::at_screenRecorder_recordingFinished(const QString &r
                                                       tr("Save Recording As..."),
                                                       qnSettings->lastRecordingDir() + QLatin1Char('/') + suggetion,
                                                       tr("AVI (Audio/Video Interleaved) (*.avi)")));
-        dialog->exec();
+        int dialogResult = dialog->exec();
 
         QString filePath = dialog->selectedFile();
-        QString selectedExtension = dialog->selectedExtension();
-        if (!filePath.isEmpty()) {
+        if (dialogResult == QDialog::Accepted && !filePath.isEmpty()) {
+            QString selectedExtension = dialog->selectedExtension();
             if (!filePath.endsWith(selectedExtension, Qt::CaseInsensitive))
                 filePath += selectedExtension;
 
