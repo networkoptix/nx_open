e9ebb91e3e44c63e0fc052cf8ef255653f9b8558
Alexander Fokin <afokin@networkoptix.com>
2014-05-01 15:44:16 -0700

    Fusion docz.
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/fusion/fusion_fwd.h
+++ common/src/utils/fusion/fusion_fwd.h
@@ -16,7 +16,8 @@ namespace QnFusion {
 
 
 /**
- * 
+ * Place this macro inside the class's body to allow access to the class's 
+ * private fields from the fusion binding.
  */
 #define QN_FUSION_ENABLE_PRIVATE()                                              \
     template<class T>                                                           \
@@ -29,7 +30,13 @@ namespace QnFusion {
 
 
 /**
- *
+ * This macro generates several function definitions for the provided type.
+ * Tokens for the functions to generate are passed in FUNCTION_SEQ parameter. 
+ * See QN_FUSION_DEFINE_FUNCTIONS for a list of tokens. 
+ * 
+ * \param CLASS                         Type to declare functions for.
+ * \param FUNCTION_SEQ                  Preprocessor sequence of functions to define.
+ * \param PREFIX                        Optional function definition prefix, e.g. <tt>inline</tt>.
  */
 #define QN_FUSION_DECLARE_FUNCTIONS(CLASS, FUNCTION_SEQ, ... /* PREFIX */)       \
     BOOST_PP_SEQ_FOR_EACH(QN_FUSION_DECLARE_FUNCTIONS_STEP_I, (CLASS, ##__VA_ARGS__), FUNCTION_SEQ)
@@ -39,7 +46,11 @@ namespace QnFusion {
 
 
 /**
- * TODO: Note why BOOST_PP_VARIADIC_SEQ_FOR_EACH
+ * Same as QN_FUSION_DECLARE_FUNCTIONS, but for several types.
+ * 
+ * \param FUNCTION_SEQ                  Preprocessor sequence of types to define functions for.
+ * \param FUNCTION_SEQ                  Preprocessor sequence of functions to define.
+ * \param PREFIX                        Optional function definition prefix, e.g. <tt>inline</tt>.
  */
 #define QN_FUSION_DECLARE_FUNCTIONS_FOR_TYPES(CLASS_SEQ, FUNCTION_SEQ, ... /* PREFIX */) \
     BOOST_PP_VARIADIC_SEQ_FOR_EACH(QN_FUSION_DECLARE_FUNCTIONS_FOR_TYPES_STEP_I, (FUNCTION_SEQ, ##__VA_ARGS__), CLASS_SEQ)
