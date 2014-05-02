ec7762df6ad44c69b4def2a125a924ee71f8924b
Ivan Vigasin <ivigasin@networkoptix.com>
2014-05-03 00:26:53 +0400

    thanks to Sasha, ios now compiles
    <==== Mercurial data ====>
    Branch: new_ec_ios

--- common/src/api/model/connection_info.h
+++ common/src/api/model/connection_info.h
@@ -1,15 +1,20 @@
 #ifndef QN_CONNECTION_INFO_H
 #define QN_CONNECTION_INFO_H
 
+#ifndef QN_NO_QT
 #include <QtCore/QMetaType>
 #include <QtCore/QSharedPointer>
+#endif
 
+#ifndef QN_NO_BASE
 #include <utils/common/software_version.h>
 #include <utils/common/model_functions_fwd.h>
 
 #include "compatibility_item.h"
 
 typedef QnSoftwareVersion SoftwareVersionType; // TODO: #Elric remove?
+#endif
+
 
 struct QnConnectionInfo {
     QnConnectionInfo(): proxyPort(0) {}
@@ -23,11 +28,16 @@ struct QnConnectionInfo {
     QString brand;
 };
 
+#ifndef QN_NO_QT
 QN_FUSION_DECLARE_FUNCTIONS(QnCompatibilityItem, (metatype)(json)(binary))
 QN_FUSION_DECLARE_FUNCTIONS(QnConnectionInfo, (metatype)(json)(binary))
 
 // TODO: #Elric remove shared pointer?
 typedef QSharedPointer<QnConnectionInfo> QnConnectionInfoPtr;
 Q_DECLARE_METATYPE(QnConnectionInfoPtr);
+#else
+QN_FUSION_DECLARE_FUNCTIONS(QnCompatibilityItem,(binary))
+QN_FUSION_DECLARE_FUNCTIONS(QnConnectionInfo, (binary))
+#endif
 
 #endif // QN_CONNECTION_INFO_H
