65118b2b36f086235b073185f8662164417fc20e
Alexander Fokin <afokin@networkoptix.com>
2014-05-13 13:21:53 -0700

    CSV stream fixes.
    <==== Mercurial data ====>
    Branch: master

--- common/src/utils/serialization/csv_stream.h
+++ common/src/utils/serialization/csv_stream.h
@@ -12,63 +12,62 @@ public:
     {
     }
 
-    void writeField(const QString &field) 
-    {
-        // unicode symbols will not escaped http://en.wikipedia.org/wiki/UTF-8
-        QByteArray utf8 = field.toUtf8();
-        writeLatin1Field(utf8);
+    void writeField(const QString &field) {
+        writeUtf8Field(field.toUtf8());
     }
 
-    //escaping rule: http://www.csvreader.com/csv/docs/DataStreams.Csv.CsvReader.UserSettings.EscapeMode.html
-    void writeLatin1Field(const QByteArray &field) 
-    {
+    /**
+     * Writes out the given utf-8 encoded field. Escaping is done as described here:
+     * http://www.csvreader.com/csv/docs/DataStreams.Csv.CsvReader.UserSettings.EscapeMode.html
+     * 
+     * \param field
+     */ 
+    void writeUtf8Field(const QByteArray &field) {
         bool needEscaping = false;
         unsigned char ch;
-        for (int i = 0 ; i < field.size() ; i++)
-        {
+        for (int i = 0 ; i < field.size() ; i++) {
             ch = field[i];
-            if (ch < 32 || ch == ',' || ch == '\\')
+            if (ch < 32 || ch == ',' || ch == '\\') {
                 needEscaping = true;
-        };
-        if (needEscaping)
-        {
-            QByteArray changedField;
-            for (int i = 0 ; i < field.size() ; i++)
-            {
-                ch = field[i];
-                if ( ch < 32 ) {
-                    changedField.push_back('\\');
-                    switch (ch)
-                    {
-                    case '\n':  changedField.push_back('n'); break;
-                    case '\r':  changedField.push_back('r'); break;
-                    case '\t':  changedField.push_back('t'); break;
-                    case '\b':  changedField.push_back('b'); break;
-                    case '\f':  changedField.push_back('f'); break;
-                    case  27 :  changedField.push_back('e'); break;
-                    case '\v':  changedField.push_back('v'); break;
-                    case '\a':  changedField.push_back('a'); break;
-                    default: 
-                        {
-                            changedField.push_back('x');
-                            changedField.push_back(toHexChar(ch / 16));
-                            changedField.push_back(toHexChar(ch % 16));
-                            break;
-                        }
-                    }
-                } 
-                else {
-                    if ( ch == ',' || ch == '\\' )
-                        changedField.push_back('\\');
-                    changedField.push_back(ch);
+                break;
+            }
+        }
+
+        if (!needEscaping) {
+            m_stream.write(field.data(), field.size());
+            return;
+        }
+
+        QByteArray changedField;
+        for (int i = 0 ; i < field.size() ; i++) {
+            ch = field[i];
+
+            /* Note that we won't end up escaping partial UTF-8 sequences here
+             * as all of them are in range [128, 255]. See http://en.wikipedia.org/wiki/UTF-8. */
+
+            if ( ch < 32 ) {
+                changedField.push_back('\\');
+                switch (ch) {
+                case '\n':  changedField.push_back('n'); break;
+                case '\r':  changedField.push_back('r'); break;
+                case '\t':  changedField.push_back('t'); break;
+                default:
+                    changedField.push_back('x');
+                    changedField.push_back(toHexChar(ch / 16));
+                    changedField.push_back(toHexChar(ch % 16));
+                    break;
                 }
-            };
-            m_stream.write(changedField.data(), changedField.size());
-        } else m_stream.write(field.data(), field.size());
+            } else {
+                if ( ch == ',' || ch == '\\' )
+                    changedField.push_back('\\');
+                changedField.push_back(ch);
+            }
+        }
+        m_stream.write(changedField.data(), changedField.size());
     }
 
-    void writeLatin1Field(const char *field) {
-        writeLatin1Field(QByteArray::fromRawData(field, qstrlen(field)));
+    void writeUtf8Field(const char *field) {
+        writeUtf8Field(QByteArray::fromRawData(field, qstrlen(field)));
     }
 
     void writeComma() {
@@ -78,9 +77,9 @@ public:
     void writeEndline() {
         m_stream.write("\r\n", 2); /* RFC 4180: CRLF line endings in CSV. */
     }
+
 private:
-    unsigned char toHexChar(unsigned char digit)
-    {
+    static unsigned char toHexChar(unsigned char digit) {
         return digit <= 9 ? '0' + digit : 'A' + (digit-10);
     }
 
