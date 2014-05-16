2a27c313e2e7df62e84baf4e43cc7e6f1c57b863
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2014-05-16 21:19:49 +0400

    fixed: integer constant overflow
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/streaming/streaming_chunk_transcoder_thread.cpp
+++ mediaserver/src/streaming/streaming_chunk_transcoder_thread.cpp
@@ -184,7 +184,7 @@ void StreamingChunkTranscoderThread::run()
         //protecting from discontinuity in timestamp
         if( transcodeIter->second->prevPacketTimestamp == -1 )
             transcodeIter->second->prevPacketTimestamp = srcMediaData->timestamp;
-        if( qAbs(srcMediaData->timestamp - transcodeIter->second->prevPacketTimestamp) > 3600*1000*1000L )
+        if( qAbs(srcMediaData->timestamp - transcodeIter->second->prevPacketTimestamp) > 3600*1000*1000LL ) //1 hour (usec)
             transcodeIter->second->prevPacketTimestamp = srcMediaData->timestamp;   //discontinuity
         if( srcMediaData->timestamp > transcodeIter->second->prevPacketTimestamp )
         {
