6819759603f2b4d1c5a3d55c679b5d3c7c9d0cf7
Alexander Fokin <afokin@networkoptix.com>
2014-05-21 13:50:47 -0700

    Simpler serialization of video wall operation.
    <==== Mercurial data ====>
    Branch: master

--- common/src/core/resource/videowall_control_message.cpp
+++ common/src/core/resource/videowall_control_message.cpp
@@ -2,34 +2,9 @@
 
 #include <utils/common/model_functions.h>
 
-QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
-    QnVideoWallControlMessage::Operation, 
-    (QnVideoWallControlMessage::Exit,                           "Exit")
-    (QnVideoWallControlMessage::Identify,                       "Identify")
+QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(QnVideoWallControlMessage, Operation)
 
-    (QnVideoWallControlMessage::ControlStarted,                 "ControlStarted")
-    (QnVideoWallControlMessage::ControlStopped,                 "ControlStopped")
-
-    (QnVideoWallControlMessage::ItemRoleChanged,                "ItemRoleChanged")
-    (QnVideoWallControlMessage::LayoutDataChanged,              "LayoutDataChanged")
-
-    (QnVideoWallControlMessage::LayoutItemAdded,                "LayoutItemAdded")
-    (QnVideoWallControlMessage::LayoutItemRemoved,              "LayoutItemRemoved")
-    (QnVideoWallControlMessage::LayoutItemDataChanged,          "LayoutItemDataChanged")
-
-    (QnVideoWallControlMessage::ZoomLinkAdded,                  "ZoomLinkAdded")
-    (QnVideoWallControlMessage::ZoomLinkRemoved,                "ZoomLinkRemoved")
-
-    (QnVideoWallControlMessage::NavigatorPositionChanged,       "NavigatorPositionChanged")
-    (QnVideoWallControlMessage::NavigatorSpeedChanged,          "NavigatorSpeedChanged")
-
-    (QnVideoWallControlMessage::SynchronizationChanged,         "SynchronizationChanged")
-    (QnVideoWallControlMessage::MotionSelectionChanged,         "MotionSelectionChanged")
-    (QnVideoWallControlMessage::MediaDewarpingParamsChanged,    "MediaDewarpingParamsChanged")
-    )
-
-
-    QDebug operator<<(QDebug dbg, const QnVideoWallControlMessage &message) {
-        dbg.nospace() << "QnVideoWallControlMessage(" << QnLexical::serialized(message.operation) << ") seq:" << message[QLatin1String("sequence")];
-        return dbg.space();
+QDebug operator<<(QDebug dbg, const QnVideoWallControlMessage &message) {
+    dbg.nospace() << "QnVideoWallControlMessage(" << QnLexical::serialized(message.operation) << ") seq:" << message[QLatin1String("sequence")];
+    return dbg.space();
 }
