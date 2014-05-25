03291937011657e74a6c86cf97c9665499679385
Denis Klychkov <dklychkov@networkoptix.com>
2014-05-25 23:20:58 +0400

    upload update peer task
    <==== Mercurial data ====>
    Branch: system_switch

---
+++ client/src/utils/upload_updates_peer_task.h
@@ -0,0 +1,45 @@
+#ifndef UPLOAD_UPDATES_PEER_TASK_H
+#define UPLOAD_UPDATES_PEER_TASK_H
+
+#include <utils/network_peer_task.h>
+#include <utils/common/system_information.h>
+
+class QnUpdateUploader;
+
+class QnUploadUpdatesPeerTask : public QnNetworkPeerTask {
+    Q_OBJECT
+public:
+    enum ErrorCode {
+        NoError = 0,
+        ConsistencyError,
+        UploadError
+    };
+
+    explicit QnUploadUpdatesPeerTask(QObject *parent = 0);
+
+    void setUploads(const QHash<QnSystemInformation, QString> &fileBySystemInformation);
+    void setUpdateId(const QString &updateId);
+
+protected:
+    virtual void doStart() override;
+
+private:
+    void uploadNextUpdate();
+
+private slots:
+    void at_uploader_finished();
+    void at_uploader_failed();
+    void at_uploader_progressChanged(int progress);
+    void at_uploader_peerProgressChanged(const QnId &peerId, int progress);
+
+private:
+    QString m_updateId;
+    QHash<QnSystemInformation, QString> m_fileBySystemInformation;
+    QMultiHash<QnSystemInformation, QnId> m_peersBySystemInformation;
+    QList<QnSystemInformation> m_pendingUploads;
+    QSet<QnId> m_finishedPeers;
+
+    QnUpdateUploader *m_uploader;
+};
+
+#endif // UPLOAD_UPDATES_PEER_TASK_H
