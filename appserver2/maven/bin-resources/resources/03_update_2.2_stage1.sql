5c293d20fb4ee354f7ee0e355e52edd8b310ed09
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-05-27 18:02:09 +0400

    transaction log fix
    <==== Mercurial data ====>
    Branch: master

--- appserver2/maven/bin-resources/resources/03_update_2.2_stage1.sql
+++ appserver2/maven/bin-resources/resources/03_update_2.2_stage1.sql
@@ -65,7 +65,7 @@ CREATE TABLE "transaction_log" (
 			    tran_guid   BLOB(16) NOT NULL,
 			    tran_data   BLOB  NOT NULL);
 
-CREATE UNIQUE INDEX idx_transaction_key   ON transaction_log(peer_guid, sequence);
+CREATE UNIQUE INDEX idx_transaction_key   ON transaction_log(peer_guid, db_guid, sequence);
 CREATE UNIQUE INDEX idx_transaction_hash  ON transaction_log(tran_guid);
 CREATE INDEX idx_transaction_time  ON transaction_log(timestamp);
 
