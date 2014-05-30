87fdeffc6c632e7bee1b17d279192f2d11c45c39
Denis Klychkov <dklychkov@networkoptix.com>
2014-05-30 15:29:52 +0400

    add QnDirectModuleFinder class
    <==== Mercurial data ====>
    Branch: improved_discovery

---
+++ common/src/utils/network/direct_module_finder.cpp
@@ -0,0 +1,124 @@
+#include "direct_module_finder.h"
+
+#include <QtCore/QTimer>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+
+namespace {
+
+    const int defaultMaxConnections = 30;
+    const int manualCheckIntervalMs = 15 * 1000;
+
+    QUrl makeRequestUrl(const QHostAddress &address, quint16 port) {
+        return QUrl(QString(lit("http://%1:%2/api/moduleInformation")).arg(address.toString()).arg(port));
+    }
+
+} // anonymous namespace
+
+QnDirectModuleFinder::QnDirectModuleFinder(QObject *parent) :
+    QObject(parent),
+    m_maxConnections(defaultMaxConnections),
+    m_networkAccessManager(new QNetworkAccessManager(this)),
+    m_manualCheckTimer(new QTimer(this))
+{
+    m_manualCheckTimer->setInterval(manualCheckIntervalMs);
+    connect(m_manualCheckTimer,     &QTimer::timeout,                   this,   &QnDirectModuleFinder::at_manualCheckTimer_timeout);
+    connect(m_networkAccessManager, &QNetworkAccessManager::finished,   this,   &QnDirectModuleFinder::at_reply_finished);
+}
+
+void QnDirectModuleFinder::addAddress(const QHostAddress &address, quint16 port) {
+    QUrl url = makeRequestUrl(address, port);
+    if (!m_autoAddresses.contains(url)) {
+        m_autoAddresses.insert(makeRequestUrl(address, port));
+        enqueRequest(url);
+    }
+}
+
+void QnDirectModuleFinder::removeAddress(const QHostAddress &address, quint16 port) {
+    m_autoAddresses.remove(makeRequestUrl(address, port));
+}
+
+void QnDirectModuleFinder::addIgnoredAddress(const QHostAddress &address, quint16 port) {
+    m_ignoredAddresses.insert(makeRequestUrl(address, port));
+}
+
+void QnDirectModuleFinder::removeIgnoredAddress(const QHostAddress &address, quint16 port) {
+    m_ignoredAddresses.remove(makeRequestUrl(address, port));
+}
+
+void QnDirectModuleFinder::addIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port) {
+    m_ignoredModules.insert(id, makeRequestUrl(address, port));
+}
+
+void QnDirectModuleFinder::removeIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port) {
+    m_ignoredModules.remove(id, makeRequestUrl(address, port));
+}
+
+void QnDirectModuleFinder::start() {
+    foreach (const QUrl &url, m_manualAddresses)
+        enqueRequest(url);
+
+    foreach (const QUrl &url, m_autoAddresses)
+        enqueRequest(url);
+
+    m_manualCheckTimer->start();
+}
+
+void QnDirectModuleFinder::stop() {
+    m_manualCheckTimer->stop();
+    m_requestQueue.clear();
+}
+
+void QnDirectModuleFinder::enqueRequest(const QUrl &url) {
+    if (m_activeRequests.contains(url) || m_requestQueue.contains(url))
+        return;
+
+    if (m_ignoredAddresses.contains(url))
+        return;
+
+    m_requestQueue.enqueue(url);
+    activateRequests();
+}
+
+void QnDirectModuleFinder::activateRequests() {
+    while (m_activeRequests.size() < m_maxConnections && !m_requestQueue.isEmpty()) {
+        QUrl url = m_requestQueue.dequeue();
+        m_activeRequests.insert(url);
+        m_networkAccessManager->get(QNetworkRequest(url));
+    }
+}
+
+void QnDirectModuleFinder::at_reply_finished(QNetworkReply *reply) {
+    QUrl url = reply->request().url();
+    if (!m_activeRequests.contains(url))
+        Q_ASSERT_X(0, "Reply that is not in the set of active requests has finished!", Q_FUNC_INFO);
+
+    reply->deleteLater();
+
+    m_activeRequests.remove(url);
+
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray data = reply->readAll();
+        QnModuleInformation moduleInformation;
+        // TODO: deserialize
+        if (!m_ignoredModules.contains(moduleInformation.id, url)) {
+            auto it = m_foundModules.find(moduleInformation.id);
+            if (it == m_foundModules.end())
+                m_foundModules.insert(moduleInformation.id, moduleInformation);
+            else
+                *it = moduleInformation;
+
+            m_lastPingById[moduleInformation.id] = QDateTime::currentMSecsSinceEpoch();
+            emit moduleFound(moduleInformation, url.host());
+        }
+    } else {
+
+    }
+
+    activateRequests();
+}
+
+void QnDirectModuleFinder::at_manualCheckTimer_timeout() {
+    foreach (const QUrl &url, m_manualAddresses)
+        enqueRequest(url);
+}
---
+++ common/src/utils/network/direct_module_finder.h
@@ -0,0 +1,64 @@
+#ifndef DIRECT_MODULE_FINDER_H
+#define DIRECT_MODULE_FINDER_H
+
+#include <QtCore/QObject>
+#include <QtCore/QQueue>
+#include <QtNetwork/QHostAddress>
+
+#include <utils/common/id.h>
+#include <utils/network/module_information.h>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+class QTimer;
+
+class QnDirectModuleFinder : public QObject {
+    Q_OBJECT
+public:
+    explicit QnDirectModuleFinder(QObject *parent = 0);
+
+    void addAddress(const QHostAddress &address, quint16 port);
+    void removeAddress(const QHostAddress &address, quint16 port);
+
+    void addIgnoredAddress(const QHostAddress &address, quint16 port);
+    void removeIgnoredAddress(const QHostAddress &address, quint16 port);
+
+    void addIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port);
+    void removeIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port);
+
+    void start();
+    void stop();
+
+signals:
+    //!Emitted when new enterprise controller has been found
+    void moduleFound(const QnModuleInformation &moduleInformation, const QString &remoteAddress);
+
+    //!Emmited when previously found module did not respond to request in predefined timeout
+    void moduleLost(const QnModuleInformation &moduleInformation);
+
+private:
+    void enqueRequest(const QUrl &url);
+    void activateRequests();
+
+private slots:
+    void at_reply_finished(QNetworkReply *reply);
+    void at_manualCheckTimer_timeout();
+
+private:
+    QSet<QUrl> m_autoAddresses;
+    QSet<QUrl> m_ignoredAddresses;
+    QSet<QUrl> m_manualAddresses;
+    QMultiHash<QnId, QUrl> m_ignoredModules;
+
+    int m_maxConnections;
+    QQueue<QUrl> m_requestQueue;
+    QSet<QUrl> m_activeRequests;
+
+    QHash<QnId, QnModuleInformation> m_foundModules;
+    QHash<QnId, quint64> m_lastPingById;
+
+    QNetworkAccessManager *m_networkAccessManager;
+    QTimer *m_manualCheckTimer;
+};
+
+#endif // DIRECT_MODULE_FINDER_H
