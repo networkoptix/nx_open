1419da40f9b45713263f25989116f7f025998128
Denis Klychkov <dklychkov@networkoptix.com>
2014-06-06 15:12:43 +0400

    use https when changing password
    <==== Mercurial data ====>
    Branch: improved_discovery

--- client/src/utils/join_system_tool.cpp
+++ client/src/utils/join_system_tool.cpp
@@ -133,6 +133,7 @@ void QnJoinSystemTool::joinResource() {
 
     // temporary add the auth info to the api url
     QUrl apiUrl = m_oldApiUrl;
+    apiUrl.setScheme(lit("https"));
     apiUrl.setUserName(lit("admin"));
     apiUrl.setPassword(m_password);
     m_targetServer->apiConnection()->setUrl(apiUrl);
