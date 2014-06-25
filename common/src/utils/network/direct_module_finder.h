b55362bdf095cbac15b8f22d87f20f2920149918
Denis Klychkov <dklychkov@networkoptix.com>
2014-06-25 15:48:00 +0400

    make direct module finder more "directed" (it looks for the certain modules, not only urls)
    <==== Mercurial data ====>
    Branch: network_issues

--- common/src/utils/network/direct_module_finder.h
+++ common/src/utils/network/direct_module_finder.h
@@ -19,25 +19,25 @@ public:
 
     void setCompatibilityMode(bool compatibilityMode);
 
-    void addUrl(const QUrl &url);
-    void removeUrl(const QUrl &url);
-    void addAddress(const QHostAddress &address, quint16 port);
-    void removeAddress(const QHostAddress &address, quint16 port);
+    void addUrl(const QUrl &url, const QnId &id);
+    void removeUrl(const QUrl &url, const QnId &id);
+    void addAddress(const QHostAddress &address, quint16 port, const QnId &id);
+    void removeAddress(const QHostAddress &address, quint16 port, const QnId &id);
 
-    void addManualUrl(const QUrl &url);
-    void removeManualUrl(const QUrl &url);
-    void addManualAddress(const QHostAddress &address, quint16 port);
-    void removeManualAddress(const QHostAddress &address, quint16 port);
+    void addManualUrl(const QUrl &url, const QnId &id);
+    void removeManualUrl(const QUrl &url, const QnId &id);
+    void addManualAddress(const QHostAddress &address, quint16 port, const QnId &id);
+    void removeManualAddress(const QHostAddress &address, quint16 port, const QnId &id);
+
+    void addIgnoredModule(const QUrl &url, const QnId &id);
+    void removeIgnoredModule(const QUrl &url, const QnId &id);
+    void addIgnoredModule(const QHostAddress &address, quint16 port, const QnId &id);
+    void removeIgnoredModule(const QHostAddress &address, quint16 port, const QnId &id);
 
     void addIgnoredUrl(const QUrl &url);
     void removeIgnoredUrl(const QUrl &url);
     void addIgnoredAddress(const QHostAddress &address, quint16 port);
     void removeIgnoredAddress(const QHostAddress &address, quint16 port);
-
-    void addIgnoredModule(const QnId &id, const QUrl &url);
-    void removeIgnoredModule(const QnId &id, const QUrl &url);
-    void addIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port);
-    void removeIgnoredModule(const QnId &id, const QHostAddress &address, quint16 port);
 
     void checkUrl(const QUrl &url);
 
@@ -47,10 +47,10 @@ public:
     QList<QnModuleInformation> foundModules() const;
     QnModuleInformation moduleInformation(const QnId &id) const;
 
-    QSet<QUrl> autoUrls() const;
+    QMultiHash<QUrl, QnId> autoUrls() const;
+    QMultiHash<QUrl, QnId> manualUrls() const;
+    QMultiHash<QUrl, QnId> ignoredModules() const;
     QSet<QUrl> ignoredUrls() const;
-    QSet<QUrl> manualUrls() const;
-    QMultiHash<QnId, QUrl> ignoredModules() const;
 
 signals:
     //!Emitted when new enterprise controller has been found
@@ -62,8 +62,10 @@ signals:
 private:
     void enqueRequest(const QUrl &url);
 
-    void checkAndAddUrl(const QUrl &url, QSet<QUrl> *urlSet);
-    void checkAndAddAddress(const QHostAddress &address, quint16 port, QSet<QUrl> *urlSet);
+    void checkAndAddUrl(const QUrl &url, const QnId &id, QMultiHash<QUrl, QnId> *urls);
+    void checkAndAddAddress(const QHostAddress &address, quint16 port, const QnId &id, QMultiHash<QUrl, QnId> *urls);
+
+    QnIdSet expectedModuleIds(const QUrl &url) const;
 
 private slots:
     void activateRequests();
@@ -72,10 +74,10 @@ private slots:
     void at_manualCheckTimer_timeout();
 
 private:
-    QSet<QUrl> m_autoUrls;
+    QMultiHash<QUrl, QnId> m_autoUrls;
+    QMultiHash<QUrl, QnId> m_manualUrls;
+    QMultiHash<QUrl, QnId> m_ignoredModules;
     QSet<QUrl> m_ignoredUrls;
-    QSet<QUrl> m_manualUrls;
-    QMultiHash<QnId, QUrl> m_ignoredModules;
 
     int m_maxConnections;
     QQueue<QUrl> m_requestQueue;
