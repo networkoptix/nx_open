b1763f132443845803283cd80ad75926cf5800ee
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-06-26 01:24:43 -1000

    jom is silent
    <==== Mercurial data ====>
    Branch: new_qt_build

--- cpp/maven/filter-resources/build.bat
+++ cpp/maven/filter-resources/build.bat
@@ -11,4 +11,4 @@ rem IF NOT [%1] == [] call msbuild common-${arch}.vcproj /t:Rebuild /consolelogg
 rem IF NOT [%2] == [] call msbuild common-${arch}.vcproj /t:Rebuild /consoleloggerparameters:ErrorsOnly /p:Configuration=%CONFIG2%
 
 rem call msbuild ${project.build.sourceDirectory}\${project.artifactId}-${arch}.vcxproj /t:Build /consoleloggerparameters:Summary /p:Configuration=${build.configuration}
-call ${environment.dir}\bin\jom
+call ${environment.dir}\bin\jom /S
