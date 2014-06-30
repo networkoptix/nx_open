44d301e3be2113b81f5eb19541cc0b12823ff969
Sergey Ivanov <sivanov@networkoptix.com>
2014-06-30 17:45:42 +0400

    Fancier output of the branches script - displaying TOO OLD
    <==== Mercurial data ====>
    Branch: master

--- util/branches.py
+++ util/branches.py
@@ -1,8 +1,12 @@
 #/bin/python
 
 import subprocess
+from datetime import datetime
+from datetime import timedelta
+
 output = subprocess.check_output("hg branches", shell=True)
 result = []
+curDate = datetime.now()
 
 for row in output.split('\n'):
     if ':' in row:
@@ -17,7 +21,8 @@ for row in output.split('\n'):
         result.append(branch)
 
 for branch in result:
-    info = subprocess.check_output("hg log -r " + branch['rev'], shell=True)
+    log = "hg log -r " + branch['rev'] + ' --template "date:{date|shortdate}\\nuser:{author}"'
+    info = subprocess.check_output(log, shell=True)
     for row in info.split('\n'):
         if 'user:' in row:
             key, value = row.split(':')
@@ -27,11 +32,12 @@ for branch in result:
             branch['user'] = user
         if 'date:' in row:
             date = row[5:].strip()
-            branch['date'] = date
+            branch['date'] = datetime.strptime(date, "%Y-%m-%d")
 
 result.sort(key=lambda branch: (branch['user'], branch['inactive']))
 
 prevUser = ''
+
 for branch in result:
     if prevUser != branch['user']:
         if prevUser != '':
@@ -41,4 +47,8 @@ for branch in result:
     if branch['inactive']:
         print str(branch['name']).ljust(40), branch['date'], 'INACTIVE'
     else:
-        print str(branch['name']).ljust(40), branch['date']
+        delta = curDate - branch['date']
+        if delta > timedelta(days = 30):
+            print str(branch['name']).ljust(40), branch['date'], 'TOO OLD', delta.days, 'days'
+        else:
+            print str(branch['name']).ljust(40), branch['date']
