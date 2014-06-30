b05c83712725ffcff4b8fe707e1f3bd6f4ec26ef
Sergey Ivanov <sivanov@networkoptix.com>
2014-06-30 17:17:10 +0400

    Fancier output of the branches script
    <==== Mercurial data ====>
    Branch: master

--- util/branches.py
+++ util/branches.py
@@ -8,6 +8,8 @@ for row in output.split('\n'):
     if ':' in row:
         key, value = row.split(':')
         name, rev = key.split()
+        if name == 'default':
+            continue
         branch = {'name' : name, 'rev' : rev, 'hash': value, 'inactive': False }
         if '(' in value:
             branch['hash'] = value.split('(')[0].strip()
@@ -20,6 +22,8 @@ for branch in result:
         if 'user:' in row:
             key, value = row.split(':')
             user = value.strip()
+            if '<' in user:
+                user = user.split('<')[0].strip()
             branch['user'] = user
         if 'date:' in row:
             date = row[5:].strip()
@@ -30,10 +34,11 @@ result.sort(key=lambda branch: (branch['user'], branch['inactive']))
 prevUser = ''
 for branch in result:
     if prevUser != branch['user']:
+        if prevUser != '':
+            print '--------------------------------------------------------------------------------'
         prevUser = branch['user']
-        print '----------------------------------------'
         print prevUser
     if branch['inactive']:
-        print branch['name'], '\t', branch['date'], 'INACTIVE'
+        print str(branch['name']).ljust(40), branch['date'], 'INACTIVE'
     else:
-        print branch['name'], '\t', branch['date']
+        print str(branch['name']).ljust(40), branch['date']
