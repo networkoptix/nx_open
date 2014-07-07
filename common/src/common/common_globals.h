80b6254cd78bba09f4041a1e46b407410ff0e8d2
Alexander Fokin <afokin@networkoptix.com>
2014-07-07 10:44:38 -0700

    Explicit values for serialization format.
    <==== Mercurial data ====>
    Branch: master

--- common/src/common/common_globals.h
+++ common/src/common/common_globals.h
@@ -478,11 +478,11 @@ public:
 
 
     enum SerializationFormat {
-        JsonFormat,
-        UbjsonFormat,
-        BnsFormat,
-        CsvFormat,
-        XmlFormat
+        JsonFormat      = 0,
+        UbjsonFormat    = 1,
+        BnsFormat       = 2,
+        CsvFormat       = 3,
+        XmlFormat       = 4
     };
     QN_ENABLE_ENUM_NUMERIC_SERIALIZATION(SerializationFormat)
 
