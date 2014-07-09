97ea49373028595835233ba7bb69f7923a5733c7
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-07-09 16:35:11 +0400

    (Issue #3304) :  PTZ controls stop working after using for a while
    <==== Mercurial data ====>
    Branch: release_2.2.1

--- mediaserver/src/rest/handlers/ptz_rest_handler.h
+++ mediaserver/src/rest/handlers/ptz_rest_handler.h
@@ -17,6 +17,8 @@ public:
     virtual QString description() const;
 
 private:
+    typedef std::function<int ()> AsyncFunc;
+
     int executeContinuousMove(const QnPtzControllerPtr &controller, const QnRequestParams &params, QnJsonRestResult &result);
     int executeContinuousFocus(const QnPtzControllerPtr &controller, const QnRequestParams &params, QnJsonRestResult &result);
     int executeAbsoluteMove(const QnPtzControllerPtr &controller, const QnRequestParams &params, QnJsonRestResult &result);
@@ -47,6 +49,8 @@ private:
     bool checkSequence(const QString& id, int sequence);
     void cleanupOldSequence();
 
+    static int execCommandAsync(const QString& sequence, AsyncFunc function);
+    static void asyncExecutor(const QString& sequence, AsyncFunc function);
 private:
     struct SequenceInfo {
         SequenceInfo(int seq = 0): sequence(seq) { m_timer.restart(); }
@@ -59,6 +63,18 @@ private:
     
     QMap<QString, SequenceInfo> m_sequencedRequests;
     QMutex m_sequenceMutex;
+
+    
+    struct AsyncExecInfo 
+    {
+        AsyncExecInfo(): inProgress() {}
+
+        bool inProgress;
+        AsyncFunc nextCommand;
+    };
+    static QMap<QString, AsyncExecInfo> m_workers;
+    static QMutex m_asyncExecMutex;
+    
 };
 
 #endif // QN_PTZ_REST_HANDLER_H
