77edb70502c09f06fda58b7c5f102541ce58afff
Sergey Ivanov <sivanov@networkoptix.com>
2014-07-16 18:41:07 +0400

    (Issue #3625) :  Client crash
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/workbench/handlers/workbench_action_handler.cpp
+++ client/src/ui/workbench/handlers/workbench_action_handler.cpp
@@ -1639,8 +1639,6 @@ void QnWorkbenchActionHandler::at_reconnectAction_triggered() {
     QnResource::startCommandProc();
 
     context()->setUserName(connectionData.url.userName());
-
-    at_messageProcessor_connectionOpened();
 }
 
 void QnWorkbenchActionHandler::at_disconnectAction_triggered() {
