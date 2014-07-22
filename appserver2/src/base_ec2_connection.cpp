c4a34a53f5ee7dd87295cf46b523259d135b357f
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-07-22 19:31:07 +0400

    transaction sequence refactor
    <==== Mercurial data ====>
    Branch: transaction_sequence_refactor

--- appserver2/src/base_ec2_connection.cpp
+++ appserver2/src/base_ec2_connection.cpp
@@ -161,7 +161,7 @@ namespace ec2
     template<class T>
     QnTransaction<ApiPanicModeData> BaseEc2Connection<T>::prepareTransaction( ApiCommand::Value command, const Qn::PanicMode& mode)
     {
-        QnTransaction<ApiPanicModeData> tran(command, true);
+        QnTransaction<ApiPanicModeData> tran(command);
         tran.params.mode = mode;
         return tran;
     }
@@ -192,9 +192,8 @@ namespace ec2
     template<class T>
     void ec2::BaseEc2Connection<T>::sendRuntimeData(const ec2::ApiRuntimeData &data)
     {
-        ec2::QnTransaction<ec2::ApiRuntimeData> tran(ec2::ApiCommand::runtimeInfoChanged, false);
+        ec2::QnTransaction<ec2::ApiRuntimeData> tran(ec2::ApiCommand::runtimeInfoChanged);
         tran.params = data;
-        tran.fillSequence();
         ec2::qnTransactionBus->sendTransaction(tran);
     }
 
