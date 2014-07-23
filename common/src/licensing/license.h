c86cd17bca2e20bd25c448088d80fc6b63c68b28
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-07-23 17:11:38 +0400

    (Issue #3668) :  swarm license
    <==== Mercurial data ====>
    Branch: master

--- common/src/licensing/license.h
+++ common/src/licensing/license.h
@@ -44,8 +44,8 @@ public:
         InvalidHardwareID,
         InvalidBrand,
         Expired,
-        InvalidType
-
+        InvalidType,
+        TooManyLicensesPerDevice
     };
 
     QnLicense();
