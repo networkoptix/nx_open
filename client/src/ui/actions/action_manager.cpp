a49bb9543b40ec0bfcea5c7902fb8ddea885fec3
Sergey Ivanov <sivanov@networkoptix.com>
2014-07-25 17:45:18 +0400

    (Issue #3692) :  Rename "Alarm/Event Log" to "Event Log"
    <==== Mercurial data ====>
    Branch: master

--- client/src/ui/actions/action_manager.cpp
+++ client/src/ui/actions/action_manager.cpp
@@ -736,7 +736,7 @@ QnActionManager::QnActionManager(QObject *parent):
     factory(Qn::BusinessEventsLogAction).
         flags(Qn::Main | Qn::Tree | Qn::GlobalHotkey).
         requiredPermissions(Qn::CurrentUserResourceRole, Qn::GlobalProtectedPermission).
-        text(tr("Alarm/Event Log...")).
+        text(tr("Event Log...")).
         shortcut(tr("Ctrl+L")).
         autoRepeat(false).
         condition(new QnTreeNodeTypeCondition(Qn::ServersNode, this));
