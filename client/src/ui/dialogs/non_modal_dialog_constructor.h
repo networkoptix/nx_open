7c5b09bd4982558cfc20364cea2eda02759013e6
Sergey Ivanov <sivanov@networkoptix.com>
2014-08-04 14:17:45 +0400

    (Issue #3662) :  Need to close all dialog windows if connection was lost
    "Login" dialog wiil be dsplayed after all modal windows are closed.
    
    <==== Mercurial data ====>
    Branch: client_connect_refactor

--- client/src/ui/dialogs/non_modal_dialog_constructor.h
+++ client/src/ui/dialogs/non_modal_dialog_constructor.h
@@ -1,33 +1,90 @@
 #ifndef QN_NON_MODAL_DIALOG_CONSTRUCTOR_H
 #define QN_NON_MODAL_DIALOG_CONSTRUCTOR_H
 
+class QnShowDialogHelper {
+public:
+    static void show(QWidget* dialog, const QRect &targetGeometry) {
+        dialog->show();
+        dialog->raise();
+        dialog->activateWindow(); // TODO: #Elric show raise activateWindow? Maybe we should also do grabKeyboard, grabMouse? wtf, really?
+        if (!targetGeometry.isNull())
+            dialog->setGeometry(targetGeometry);
+    }
+};
+
+/** Helper class to show target non-modal dialog as soon as top-level modal dialog is closed. */
+class QnDelayedShowHelper: public QObject, public QnShowDialogHelper {
+    Q_OBJECT
+public:
+    QnDelayedShowHelper(QWidget* targetWidget, const QRect &targetGeometry, int sourceCount, QObject *parent = NULL):
+        QObject(parent),
+        m_targetWidget(targetWidget),
+        m_targetGeometry(targetGeometry),
+        m_sourceCount(sourceCount)
+    {}
+    
+    virtual bool eventFilter(QObject *watched, QEvent *event) override {
+        if (m_targetWidget && event->type() == QEvent::Hide) {
+            watched->removeEventFilter(this);   //avoid double call
+            m_sourceCount--;
+
+            if (m_sourceCount <= 0) {
+                show(m_targetWidget.data(), m_targetGeometry);
+                deleteLater();
+            }
+            
+        }
+
+        return QObject::eventFilter(watched, event);
+    }
+
+
+private:
+    QPointer<QWidget> m_targetWidget;
+    QRect m_targetGeometry;
+    int m_sourceCount;
+};
+
+
 /** Helper class to restore geometry of the persistent dialogs. */
 template<typename T>
-class QnNonModalDialogConstructor {
+class QnNonModalDialogConstructor: public QnShowDialogHelper {
 public:
     
-    QnNonModalDialogConstructor<T>(QPointer<T> &dialog, QWidget* parent):
-        m_newlyCreated(false)
+    QnNonModalDialogConstructor<T>(QPointer<T> &dialog, QWidget* parent)
     {
         if (!dialog) {
             dialog = new T(parent);
-            m_newlyCreated = true;
+        } else {
+            /* Dialog's show() method will reset the geometry, saving it to restore afterwards. */
+            m_targetGeometry = dialog->geometry();  
         }
         m_dialog = dialog;
-        m_oldGeometry = dialog->geometry();
     }
 
     ~QnNonModalDialogConstructor() {
-        m_dialog->show();
-        m_dialog->raise();
-        m_dialog->activateWindow(); // TODO: #Elric show raise activateWindow? Maybe we should also do grabKeyboard, grabMouse? wtf, really?
-        if (!m_newlyCreated)
-            m_dialog->setGeometry(m_oldGeometry);
+        QWidgetList modalWidgets;
+
+        /* Checking all top-level widgets. */
+        foreach (QWidget* tlw, qApp->topLevelWidgets()) {
+            if (!tlw->isHidden() && tlw->isModal())
+                modalWidgets << tlw;
+        }
+
+        /* Setup dialog to show after all modal windows are closed. */
+        if (!modalWidgets.isEmpty()) {
+            QnDelayedShowHelper* helper = new QnDelayedShowHelper(m_dialog, m_targetGeometry, modalWidgets.size(), m_dialog);
+            foreach (QWidget* modalWidget, modalWidgets)
+                modalWidget->installEventFilter(helper);
+            return;
+        }
+
+        /* Or show it immediately if no modal windows are visible. */
+        show(m_dialog, m_targetGeometry);
     }
 
 private:
-    bool m_newlyCreated;
-    QRect m_oldGeometry;
+    QRect m_targetGeometry;
     QPointer<T> m_dialog;
 };
 
