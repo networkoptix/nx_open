db881f4267323c1e378463acd5f0ae3d0785bd7d
Sergey Ivanov <sivanov@networkoptix.com>
2014-08-04 14:40:17 +0400

    (Issue #3662) :  Need to close all dialog windows if connection was lost
    Fixed "Save Screenshot As..." dialog problems
    
    <==== Mercurial data ====>
    Branch: client_connect_refactor

--- common/src/utils/common/forward.h
+++ common/src/utils/common/forward.h
@@ -45,7 +45,7 @@
     template<class T0, class T1, class T2>                                      \
     CLASS(T0 &arg0, T1 &arg1, T2 &arg2): BASE(arg0, arg1, arg2) __VA_ARGS__     \
     template<class T0, class T1, class T2, class T3>                            \
-    CLASS(T0 &arg0, const T1 &arg1, const T2 &arg2, const T3 &arg3): BASE(arg0, arg1, arg2, arg3) __VA_ARGS__     \
+    CLASS(const T0 &arg0, const T1 &arg1, const T2 &arg2, const T3 &arg3): BASE(arg0, arg1, arg2, arg3) __VA_ARGS__     \
 
 
 
