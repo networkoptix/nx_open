4e0102591366a99dfe1911988fe7094e9f8bd6a1
Denis Klychkov <dklychkov@networkoptix.com>
2014-08-13 19:16:21 +0400

    some movements and cleanup
    <==== Mercurial data ====>
    Branch: mobile_client

---
+++ mobile_client/src/StdAfx.h
@@ -0,0 +1,80 @@
+#define QT_NO_CAST_FROM_ASCII
+
+#include <common/config.h>
+#ifdef __cplusplus
+#   include <common/common_globals.h>
+#endif
+
+#ifdef __cplusplus
+
+/* STL headers. */
+#include <algorithm>
+#include <functional>
+
+/* Boost headers. */
+#include <boost/foreach.hpp>
+#include <boost/array.hpp>
+
+/* QT headers. */
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtCore/QStringList>
+#include <QtCore/QStringListModel>
+#include <QtCore/QList>
+#include <QtCore/QMap>
+#include <QtCore/QPair>
+#include <QtCore/QQueue>
+#include <QtCore/QSet>
+#include <QtCore/QMutex>
+#include <QtCore/QMutexLocker>
+#include <QtCore/QWaitCondition>
+#include <QtCore/QSemaphore>
+#include <QtCore/QThread>
+#include <QtCore/QThreadPool>
+#include <QtCore/QDateTime>
+#include <QtCore/QTime>
+#include <QtCore/QTimer>
+#include <QtCore/QFileSystemWatcher>
+#include <QtCore/QDataStream>
+#include <QtCore/QTextStream>
+#include <QtCore/QIODevice>
+#include <QtCore/QBuffer>
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtCore/QFileInfoList>
+#include <QtCore/QParallelAnimationGroup>
+#include <QtCore/QPropertyAnimation>
+#include <QtCore/QPoint>
+#include <QtCore/QPointF>
+#include <QtCore/QRect>
+#include <QtCore/QRectF>
+#include <QtCore/QSize>
+#include <QtCore/QEasingCurve>
+#include <QtCore/qmath.h>
+#include <QtCore/QtDebug>
+#include <QtGui/QCloseEvent>
+#include <QtGui/QKeyEvent>
+#include <QtGui/QMouseEvent>
+#include <QtGui/QWheelEvent>
+#include <QtGui/QResizeEvent>
+#include <QtGui/QColor>
+#include <QtGui/QDesktopServices>
+#include <QtGui/QFont>
+#include <QtGui/QFontMetrics>
+#include <QtGui/QPaintEngine>
+#include <QtGui/QPainter>
+#include <QtGui/QPainterPath>
+#include <QtGui/QPixmap>
+#include <QtGui/QPixmapCache>
+#include <QtGui/QRadialGradient>
+#include <QtNetwork/QAuthenticator>
+#include <QtNetwork/QHostAddress>
+#include <QtNetwork/QNetworkAddressEntry>
+#include <QtNetwork/QNetworkInterface>
+#include <QtNetwork/QUdpSocket>
+#include <QtXml/QDomDocument>
+#include <QtXml/QDomElement>
+#include <QtConcurrent/QtConcurrentMap>
+
+#endif
