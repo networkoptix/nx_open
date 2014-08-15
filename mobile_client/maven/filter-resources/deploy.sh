4ad1325083fa5dacbf929da76c67009fe464b03f
Denis Klychkov <dklychkov@networkoptix.com>
2014-08-15 20:19:48 +0400

    remove libs dir before deployment apk
    <==== Mercurial data ====>
    Branch: mobile_client

--- mobile_client/maven/filter-resources/deploy.sh
+++ mobile_client/maven/filter-resources/deploy.sh
@@ -3,5 +3,9 @@
 BUILD_TARGET=apk
 ANDROID_NDK_ROOT=${env.environment}/android/android-ndk
 
+if [ -d "$BUILD_TARGET/libs" ]; then
+    rm -rf $BUILD_TARGET/libs
+fi
+
 make install INSTALL_ROOT=$BUILD_TARGET
 ${libdir}/bin/androiddeployqt --input android-deployment.json --output $BUILD_TARGET $*
