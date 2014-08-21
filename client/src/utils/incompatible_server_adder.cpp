513388f98a660af9d80f4f49f797b2063cc31c0a
Denis Klychkov <dklychkov@networkoptix.com>
2014-08-21 20:37:07 +0400

    fix a bug with incompatibility status
    <==== Mercurial data ====>
    Branch: master

--- client/src/utils/incompatible_server_adder.cpp
+++ client/src/utils/incompatible_server_adder.cpp
@@ -66,8 +66,6 @@ void QnIncompatibleServerAdder::at_peerChanged(const QnModuleInformation &module
             server->setSystemInfo(moduleInformation.systemInformation);
             server->setSystemName(moduleInformation.systemName);
             server->setStatus(moduleInformation.isCompatibleToCurrentSystem() ? Qn::Offline : Qn::Incompatible);
-            // now put server to 'other systems' subtree
-            server->parentIdChanged(server);
         }
         return;
     }
