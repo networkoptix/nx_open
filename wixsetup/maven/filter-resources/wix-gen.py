91186c15f26121645caf6ca7c18569893f46238e
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-09-09 16:15:08 -0700

    Client Package for Windows ok
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/maven/filter-resources/wix-gen.py
+++ wixsetup/maven/filter-resources/wix-gen.py
@@ -1,7 +1,8 @@
-import os, sys, subprocess
+import os, sys, subprocess, shutil
 from subprocess import Popen, PIPE
+from os.path import dirname, join, exists, isfile
 
-bin_source_dir = ${libdir}/${arch}
+bin_source_dir = '${libdir}/${arch}/bin/${build.configuration}'
 
 for wxs in ('dbsync', 'help', 'vox', 'bg'):
     p = subprocess.Popen('${python.dir}/python generate-%s-wxs.py' % wxs, shell=True, stdout=PIPE)
@@ -13,10 +14,10 @@ for wxs in ('dbsync', 'help', 'vox', 'bg'):
         print "failed with code: %s" % str(p.returncode) 
         sys.exit(1)
 		
-if os.path.exists(join(target_dir, '${applauncher.name}.exe')):
-    os.shutil.rmtree(join(pdb_source_dir, '${applauncher.name}.exe'))
-shutil.copy2(join(pdb_source_dir, 'applauncher.exe'), join(target_dir, '${applauncher.name}.exe'))
+if os.path.exists(join(bin_source_dir, '${product.name} Launcher.exe')):
+    os.unlink(join(bin_source_dir, '${product.name} Launcher.exe'))
+shutil.copy2(join(bin_source_dir, 'applauncher.exe'), join(bin_source_dir, '${product.name} Launcher.exe'))
     
-if os.path.exists(join(target_dir, '${client.name}.exe')):
-    os.shutil.rmtree(join(pdb_source_dir, '${client.name}.exe'))          
-shutil.copy2(join(pdb_source_dir, 'client.exe'), join(target_dir, '${client.name}.exe'))
+if os.path.exists(join(bin_source_dir, '${product.name}.exe')):
+    os.unlink(join(bin_source_dir, '${product.name}.exe'))          
+shutil.copy2(join(bin_source_dir, 'client.exe'), join(bin_source_dir, '${product.name}.exe'))
