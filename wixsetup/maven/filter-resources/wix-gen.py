358405c24958b2982772f5ca5c6e641210b20c13
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-09-09 15:59:08 -0700

    copy binaries for packing them
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/maven/filter-resources/wix-gen.py
+++ wixsetup/maven/filter-resources/wix-gen.py
@@ -1,6 +1,8 @@
 import os, sys, subprocess
 from subprocess import Popen, PIPE
 
+bin_source_dir = ${libdir}/${arch}
+
 for wxs in ('dbsync', 'help', 'vox', 'bg'):
     p = subprocess.Popen('${python.dir}/python generate-%s-wxs.py' % wxs, shell=True, stdout=PIPE)
     out, err = p.communicate()
@@ -10,3 +12,11 @@ for wxs in ('dbsync', 'help', 'vox', 'bg'):
     if p.returncode:  
         print "failed with code: %s" % str(p.returncode) 
         sys.exit(1)
+		
+if os.path.exists(join(target_dir, '${applauncher.name}.exe')):
+    os.shutil.rmtree(join(pdb_source_dir, '${applauncher.name}.exe'))
+shutil.copy2(join(pdb_source_dir, 'applauncher.exe'), join(target_dir, '${applauncher.name}.exe'))
+    
+if os.path.exists(join(target_dir, '${client.name}.exe')):
+    os.shutil.rmtree(join(pdb_source_dir, '${client.name}.exe'))          
+shutil.copy2(join(pdb_source_dir, 'client.exe'), join(target_dir, '${client.name}.exe'))
