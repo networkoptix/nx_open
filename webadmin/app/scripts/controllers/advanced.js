6e03ade01adae1e6de087d686b5ccbe35004fe46
Evgeny Balashov <ebalashov@networkoptix.com>
2014-09-12 13:11:26 +0400

    WA: advanced interface
    <==== Mercurial data ====>
    Branch: webadmin

--- webadmin/app/scripts/controllers/advanced.js
+++ webadmin/app/scripts/controllers/advanced.js
@@ -6,7 +6,22 @@ angular.module('webadminApp')
         $scope.storages = mediaserver.getStorages();
 
         $scope.storages.then(function (r) {
+
             $scope.storages = r.data.reply.storages;
+            for(var i in $scope.storages){
+                $scope.storages[i].reservedSpaceGb = Math.round(1.*$scope.storages[i].reservedSpace / (1024*1024*1024));
+            }
+            console.log($scope.storages);
+
+            $scope.$watch(function(){
+                for(var i in $scope.storages){
+                    var storage = $scope.storages[i];
+                    storage.reservedSpace = storage.reservedSpaceGb * (1024*1024*1024);
+                    storage.warning = storage.isUsedForWriting && (storage.reservedSpace <= 0 ||  storage.reservedSpace >= storage.totalSpace );
+                }
+                console.log($scope.storages);
+            })
+
         });
 
         $scope.formatSpace = function(bytes){
@@ -20,7 +35,9 @@ angular.module('webadminApp')
             }
             return Number(bytes).toFixed(precision) + " " + sizes[posttxt];
         };
-
+        $scope.toGBs = function(total){
+            return    Math.floor(total / (1024*1024*1024));
+        }
         $scope.save = function(){
             console.log("call saveSpace");
             alert("saveSpace not implemented yet");
--- webadmin/app/scripts/directives/icon.js
+++ webadmin/app/scripts/directives/icon.js
@@ -7,14 +7,34 @@ angular.module('webadminApp')
     .directive('icon', function () {
         return {
             restrict: 'E',
+            scope:{
+                for:"="
+            },
+            template:'<span ng-show="for || onFalse" class="glyphicon glyphicon-{{for?onTrue:onFalse}} {{class}}" title="{{for?titleTrue:titleFalse}}"></span>',
             link: function (scope, element, attrs) {
-                attrs.onTrue = attrs.onTrue || 'ok';
-                var isTrue = attrs.for=="true";
+                scope.onTrue = attrs.onTrue || 'ok';
+                scope.onFalse = attrs.onFalse;
+                scope.class = attrs.class;
+                scope.titleTrue = attrs.titleTrue;
+                scope.titleFalse = attrs.titleFalse;
+/*
+
+                var isTrue = scope.for=="true";
                 var icon = isTrue ? attrs.onTrue : attrs.onFalse;
                 attrs.title = isTrue? attrs.titleTrue : attrs.titleFalse;
 
                 var template = icon ? '<span class="glyphicon glyphicon-' + icon + ' ' + attrs.class + '" title="' + attrs.title + '"></span>' : '';
-                $(element).replaceWith(template);
+                //$(element).append(template);
+
+                /*scope.$watch("for",function(){
+                   // $(element).children().remove();
+                    var isTrue = scope.for=="true";
+                    var icon = isTrue ? attrs.onTrue : attrs.onFalse;
+                    attrs.title = isTrue? attrs.titleTrue : attrs.titleFalse;
+
+                    var template = icon ? '<span class="glyphicon glyphicon-' + icon + ' ' + attrs.class + '" title="' + attrs.title + '"></span>' : '';
+                    //$(element).append(template);
+                })*/
             }
         };
     });
