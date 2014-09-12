6a5acb750cf13decafe6246aac8486f0ed3bd465
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-09-13 00:00:13 +0400

    linux update pack renamed
    <==== Mercurial data ====>
    Branch: master

--- debsetup/mediaserver-deb/maven/filter-resources/build-dist.sh
+++ debsetup/mediaserver-deb/maven/filter-resources/build-dist.sh
@@ -108,6 +108,6 @@ cp -P $SERVER_LIB_PATH/*.debug ${project.build.directory}
 cp -P $SERVER_BIN_PATH/*.debug ${project.build.directory}
 cp -P $SERVER_LIB_PLUGIN_PATH/*.debug ${project.build.directory}
 tar czf ./$FINALNAME-debug-symbols.tar.gz ./*.debug
-(cd $STAGEBASE; zip -y ./server-update-${platform}-$VERSION.${buildNumber}-${arch}.zip ./* -i *.*)
-mv $STAGEBASE/server-update-${platform}-$VERSION.${buildNumber}-${arch}.zip ${project.build.directory}
+(cd $STAGEBASE; zip -y ./server-update-${platform}-${arch}-$VERSION.${buildNumber}.zip ./* -i *.*)
+mv $STAGEBASE/server-update-${platform}-${arch}-$VERSION.${buildNumber}.zip ${project.build.directory}
 echo "server.finalName=$FINALNAME" >> finalname-server.properties
