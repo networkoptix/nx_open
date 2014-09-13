9e316ffc51144f35f0aa676aa8903e817dde0d5e
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-09-12 19:00:10 -0700

    smart buildenv call (only if branch is changed)
    <==== Mercurial data ====>
    Branch: master

--- configure.py
+++ configure.py
@@ -1,7 +1,18 @@
 import io, sys, os, platform
 import ConfigParser
 import argparse
-#from main import get_environment_variable, cd
+import subprocess
+from main import get_environment_variable, cd
+from os.path import dirname, join
+
+def runProcess(exe): 
+    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while(True):
+      retcode = p.poll() #returns None while subprocess is running
+      line = p.stdout.readline()
+      yield line
+      if(retcode is not None):
+        break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,12 +41,37 @@ if __name__ == '__main__':
         build_arch = os.getenv('customization')
     else:
         build_arch = 'default'        
+
+    try:
+        with open(join(dirname(os.path.abspath(__file__)),'configure_settings_tmp.py')): 
+            from configure_settings_tmp import branch
+            print >> sys.stderr, 'old branch is: %s' % branch   
+    except IOError:
+        print 'not configured'
         
+    def run_command(cmd):
+        '''given shell command, returns communication tuple of stdout and stderr'''
+        return subprocess.Popen(cmd, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE, 
+                                stdin=subprocess.PIPE).communicate()
+
+    newbranch = run_command('hg branch')[0][:-1] 
     f = open('configure_settings_tmp.py', 'w')
     print >> f, \
-    'customization = "%s" \nconfiguration = "%s" \nbuild_arch = "%s"' %(build_customization, build_configuration, build_arch)
-
+    'customization = "%s" \nconfiguration = "%s" \nbuild_arch = "%s" \nbranch = "%s"' %(build_customization, build_configuration, build_arch, newbranch)
+    f.close()
+    
     print '\n++++++++++++++++++++++++ CONFUGURED ++++++++++++++++++++++++\n'
     print >> sys.stderr, 'customization is: %s' % build_customization
     print >> sys.stderr, 'configuration is: %s' % build_configuration
     print >> sys.stderr, 'build_arch is: %s' % build_arch
+    print >> sys.stderr, 'branch is: %s' % newbranch
+    if branch == newbranch:
+        f = open('donotrebuild', 'w')
+        f.close()
+        print "++++++++++++++++++++++++ Reconfigure is NOT required ++++++++++++++++++++++++"
+    else:
+        print "++++++++++++++++++++++++ Reconfigure is required ++++++++++++++++++++++++"
+        if os.path.isfile('donotrebuild'):
+            os.unlink('donotrebuild')
