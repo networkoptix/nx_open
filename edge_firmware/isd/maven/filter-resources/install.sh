3b616c10feba0436335a2a358f9872cb43c4c473
Olga Mironenko <omironenko@networkoptix.com>
2014-09-19 19:10:03 +0400

    changed customization's name
    <==== Mercurial data ====>
    Branch: master

--- edge_firmware/isd/maven/filter-resources/install.sh
+++ edge_firmware/isd/maven/filter-resources/install.sh
@@ -10,7 +10,7 @@ DISTRIB=$COMPANY_NAME-mediaserver-${box}-${release.version}.${buildNumber}$BETA
 
 update () {
     /etc/init.d/S99$COMPANY_NAME-mediaserver stop
-    tar xfv $DISTRIB.tar.gz  --exclude='./usr/local/apps/$COMPANY_NAME/mediaserver/etc/mediaserver.conf' -C /
+    tar xfv $DISTRIB.tar.gz  --exclude='./usr/local/apps/${deb.customization.company.name}/mediaserver/etc/mediaserver.conf' -C /
     /etc/init.d/S99$COMPANY_NAME-mediaserver start
 }
 
