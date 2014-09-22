b93f145ca899628fe860903ec07a68e56fc1a078
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-09-22 19:08:39 +0400

    (Issue #3967) :  Cannot discover Sony cameras
    <==== Mercurial data ====>
    Branch: master

--- common/src/plugins/resource/onvif/onvif_resource_searcher_wsdd.cpp
+++ common/src/plugins/resource/onvif/onvif_resource_searcher_wsdd.cpp
@@ -63,25 +63,25 @@ namespace
     }
 
     static const char STATIC_DISCOVERY_MESSAGE[] = "\
-    <s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">\
-    <s:Header>\
-    <a:Action s:mustUnderstand=\"1\">\
-    http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe\
-    </a:Action>\
-    <a:MessageID>%1</a:MessageID>\
-    <a:ReplyTo>\
-    <a:Address>\
-    http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous\
-    </a:Address>\
-    </a:ReplyTo>\
-    <a:To s:mustUnderstand=\"1\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>\
-    </s:Header>\
-    <s:Body>\
-    <Probe xmlns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\">\
-    <d:Types xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" xmlns:dp0=\"http://www.onvif.org/ver10/network/wsdl\">dp0:NetworkVideoTransmitter</d:Types>\
-    </Probe>\
-    </s:Body>\
-    </s:Envelope>";
+<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">\
+<s:Header>\
+<a:Action s:mustUnderstand=\"1\">\
+http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe\
+</a:Action>\
+<a:MessageID>%1</a:MessageID>\
+<a:ReplyTo>\
+<a:Address>\
+http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous\
+</a:Address>\
+</a:ReplyTo>\
+<a:To s:mustUnderstand=\"1\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>\
+</s:Header>\
+<s:Body>\
+<Probe xmlns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\">\
+<d:Types xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" xmlns:dp0=\"http://www.onvif.org/ver10/network/wsdl\">dp0:NetworkVideoTransmitter</d:Types>\
+</Probe>\
+</s:Body>\
+</s:Envelope>";
 
 
     // avoid SOAP select call
