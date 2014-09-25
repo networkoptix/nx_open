c758c46dab60cb4a4f72768ca0d9d089d573eba8
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-09-24 17:09:12 -0700

    fix previous fix
    <==== Mercurial data ====>
    Branch: master

--- macsetup/client-dmg/maven/bin-resources/macdeployqt.py
+++ macsetup/client-dmg/maven/bin-resources/macdeployqt.py
@@ -96,7 +96,7 @@ def fix_binary(binary, bindir, libdir, qlibdir, tlibdir):
 
 
 def main(app_path, bindir, libdir, helpdir):
-    qlibdir = libdir
+    qlibdir = os.path.dirname(libdir)
 
     appdir = os.path.basename(app_path)
     app, _ = os.path.splitext(appdir)
