a57a9486d2dee971ec11d562f11d588085a1cd09
Denis Klychkov <dklychkov@networkoptix.com>
2014-09-26 11:40:51 +0400

    place discovered peers directly in the result
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/rest/handlers/discovered_peers_rest_handler.cpp
+++ mediaserver/src/rest/handlers/discovered_peers_rest_handler.cpp
@@ -2,25 +2,13 @@
 
 #include "utils/network/tcp_connection_priv.h"
 #include "utils/network/module_finder.h"
-#include "utils/common/model_functions_fwd.h"
 #include "utils/common/model_functions.h"
-
-namespace {
-    struct DiscoveredPeersResult {
-        QList<QnModuleInformation> modules;
-    };
-    QN_FUSION_DECLARE_FUNCTIONS(DiscoveredPeersResult, (json))
-    QN_FUSION_ADAPT_STRUCT_FUNCTIONS(DiscoveredPeersResult, (json), (modules))
-}
 
 int QnDiscoveredPeersRestHandler::executeGet(const QString &path, const QnRequestParams &params, QnJsonRestResult &result) {
     Q_UNUSED(path)
     Q_UNUSED(params)
 
-    DiscoveredPeersResult jsonResult;
-    jsonResult.modules = QnModuleFinder::instance()->foundModules();
-
-    result.setReply(jsonResult);
+    result.setReply(QnModuleFinder::instance()->foundModules());
 
     return CODE_OK;
 }
