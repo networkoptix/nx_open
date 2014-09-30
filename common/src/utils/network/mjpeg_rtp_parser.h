18fe71b207f1b271c4d78d9f705beec143a082ec
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-09-30 18:23:17 +0400

    rtsp refactor in progress
    <==== Mercurial data ====>
    Branch: rtsp_track_refactor.patch

--- common/src/utils/network/mjpeg_rtp_parser.h
+++ common/src/utils/network/mjpeg_rtp_parser.h
@@ -18,8 +18,7 @@ public:
     virtual void setSDPInfo(QList<QByteArray> lines) override;
 
     virtual bool processData(quint8* rtpBufferBase, int bufferOffset, int readed, const RtspStatistic& statistics) override;
-    virtual QnAbstractMediaDataPtr nextData() override;
-
+    
 private:
     int makeHeaders(quint8 *p, int type, int w, int h, const quint8 *lqt, const quint8 *cqt, u_short dri);
     void updateHeaderTables(const quint8* lummaTable, const quint8* chromaTable);
