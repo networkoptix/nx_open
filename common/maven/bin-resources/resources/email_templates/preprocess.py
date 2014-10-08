3c43a2d37e5fd00bc4135dbb45de8f4155e7f96c
Evgeny Balashov <ebalashov@networkoptix.com>
2014-10-08 18:22:14 +0400

    E&A: email template update
    <==== Mercurial data ====>
    Branch: emailNotifications

--- common/maven/bin-resources/resources/email_templates/preprocess.py
+++ common/maven/bin-resources/resources/email_templates/preprocess.py
@@ -10,16 +10,15 @@ containter_text = containter_text.replace('{{>style_css}}', open('style_css.must
 for f in ('camera_input', 'camera_ip_conflict', 'camera_motion', 'mediaserver_conflict', 'mediaserver_failure', 'mediaserver_started', 'storage_failure'):
     newf = open(f + '.mustache', 'w')
     text = containter_text.replace('{{>body}}', open(f + '.mustache.body').read())
-    print >> newf, inline_css(text)
-
-
-
+    print >> newf, inline_css(text).encode('utf-8','replace')
 
 multi_res_containter_text = open('multi_res_container.mustache.template').read()
 multi_res_containter_text = multi_res_containter_text.replace('{{>style_css}}', open('style_css.mustache.body').read())
 
 for f in ('camera_disconnect', 'network_issue'):
+    print f
     newf = open(f + '.mustache', 'w')
     text = multi_res_containter_text.replace('{{>body}}', open(f + '.mustache.body').read())
-    print >> newf, inline_css(text)
+    print >> newf, inline_css(text).encode('utf-8','replace')
+
     
