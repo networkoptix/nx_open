aca3c5c4c3b4a814b83ec615b86fb45d1c4fd67e
Denis Klychkov <dklychkov@networkoptix.com>
2014-10-23 17:24:23 +0400

    fix some issues with layout AR (bug #4192)
    <==== Mercurial data ====>
    Branch: master

---
+++ client/src/utils/aspect_ratio.h
@@ -0,0 +1,29 @@
+#ifndef ASPECT_RATIO_H
+#define ASPECT_RATIO_H
+
+#include <QtCore/QString>
+
+#include <ui/actions/actions.h>
+
+class QnAspectRatio
+{
+public:
+    QnAspectRatio(int width = 1, int height = 1);
+
+    qreal toReal() const;
+    QString toString() const;
+
+    static QList<QnAspectRatio> standardRatios();
+    static QnAspectRatio closestStandardRatio(qreal aspectRatio);
+
+    static bool isRotated90(qreal angle);
+    static Qn::ActionId aspectRatioActionId(const QnAspectRatio &aspectRatio);
+
+    bool operator ==(const QnAspectRatio &other) const;
+
+private:
+    int m_width;
+    int m_height;
+};
+
+#endif // ASPECT_RATIO_H
