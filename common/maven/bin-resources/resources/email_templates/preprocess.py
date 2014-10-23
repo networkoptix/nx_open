b10105e042e947cb35e5acbd0ef2af0a063626d6
Roman Vasilenko <rvasilenko@networkoptix.com>
2014-10-23 17:04:30 +0400

    (Issue #3917) :  Add event "License issue
    <==== Mercurial data ====>
    Branch: master

--- common/maven/bin-resources/resources/email_templates/preprocess.py
+++ common/maven/bin-resources/resources/email_templates/preprocess.py
@@ -7,7 +7,7 @@ cssutils.log.setLevel(logging.ERROR)
 containter_text = open('container.mustache.template').read()
 containter_text = containter_text.replace('{{>style_css}}', open('style_css.mustache.body').read())
 
-for f in ('camera_input', 'camera_ip_conflict', 'camera_motion', 'mediaserver_conflict', 'mediaserver_failure', 'mediaserver_started', 'storage_failure'):
+for f in ('camera_input', 'camera_ip_conflict', 'camera_motion', 'mediaserver_conflict', 'mediaserver_failure', 'mediaserver_started', 'storage_failure', 'license_issue'):
     newf = open(f + '.mustache', 'w')
     text = containter_text.replace('{{>body}}', open(f + '.mustache.body').read())
     print >> newf, inline_css(text).encode('utf-8','replace')
