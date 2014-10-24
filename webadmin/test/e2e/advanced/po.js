743c7af13e0d59d3f39c132c8ea06a7042e64f88
Evgeny Balashov <ebalashov@networkoptix.com>
2014-10-24 17:29:07 +0400

    WA: test, bugfixes
    <==== Mercurial data ====>
    Branch: master

--- webadmin/test/e2e/advanced/po.js
+++ webadmin/test/e2e/advanced/po.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var Page = function () {
+    this.get = function () {
+        browser.get('/#/advanced');
+    };
 
+    this.storagesRows = element.all(by.repeater("storage in storages"));
 };
 
 module.exports = Page;
--- webadmin/test/e2e/advanced/spec.js
+++ webadmin/test/e2e/advanced/spec.js
@@ -3,6 +3,46 @@ var Page = require('./po.js');
 describe('Advanced Page', function () {
 
     var p = new Page();
+
+    it("should allow user to see storages: free space, limit, totalspace, enabled, some indicator",function(){
+        p.get();
+        expect(p.storagesRows.count()).toBeGreaterThan(0);
+        var storage = p.storagesRows.first();
+        expect(storage.element(by.css(".storage-url")).getText()).toMatch(/[\w\d\:]*(\/[\w\d]*)+/);
+        expect(storage.element(by.css(".progress")).getAttribute("title")).toMatch(/Reserved\:\s\d+\.?\d*\s*\wB,\s+Free\:\s\d+\.?\d*\s*\wB,\s+Occupied\:\s\d+\.?\d*\s*\wB,\s+Total:\s\d+\.?\d*\s*\wB/);
+        expect(storage.element(by.css(".glyphicon")).isDisplayed()).toBe(true);
+    });
+
+    it("should allow user to change settings: limit, enabled",function(){
+        expect("test").toBe("written");
+    });
+
+    it("should show warnings in some cases",function(){
+        expect("test").toBe("written");
+    });
+
+    it("should show confirm before saving",function(){
+        expect("test").toBe("written");
+    });
+
+
+    it("cancel button should cancel changes",function(){
+        expect("test").toBe("written");
+    });
+
+    it("should save settings and display it after reload",function(){
+        expect("test").toBe("written");
+    });
+
+    it("should show button for updating system",function(){
+        expect("test").toBe("written");
+    });
+
+    it("should upload some file and display an error",function(){
+        expect("test").toBe("written");
+    });
+
+
     it("Module",function(){
         expect("all tests").toBe("written");
     });
