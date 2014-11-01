023871b1887683fd8b3cda36adac75011e41240a
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2014-10-31 18:05:59 -0700

    fixed product names
    <==== Mercurial data ====>
    Branch: master

--- wixsetup/maven/filter-resources/sign.bat
+++ wixsetup/maven/filter-resources/sign.bat
@@ -4,8 +4,8 @@ SET CERTIFICATE=${sign.intermediate.cer}
 
 IF NOT [%PASSWORD%] == [] (
     IF NOT [%CERTIFICATE%] == [] (
-        call ${environment.dir}\bin\signtool.exe sign /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${project.build.finalName}.msi
+        call ${environment.dir}\bin\signtool.exe sign /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${finalName}.msi
     ) ELSE (
-        call ${environment.dir}\bin\signtool.exe sign /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${project.build.finalName}.msi
+        call ${environment.dir}\bin\signtool.exe sign /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${finalName}.msi
     )    
-) ELSE call ${environment.dir}\bin\signtool.exe sign /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${project.build.finalName}.msi
+) ELSE call ${environment.dir}\bin\signtool.exe sign /d "${company.name} ${display.product.name}" /f ${sign.cer} bin\${finalName}.msi
