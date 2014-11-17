e6ee67762b309183e1ee8a739a268f3fcd0a9d0d
Denis Klychkov <dklychkov@networkoptix.com>
2014-11-17 12:23:57 +0300

    add possibility to get information about all modules with api/moduleInformation
    <==== Mercurial data ====>
    Branch: master

--- mediaserver/src/rest/handlers/module_information_rest_handler.cpp
+++ mediaserver/src/rest/handlers/module_information_rest_handler.cpp
@@ -1,14 +1,25 @@
 #include "module_information_rest_handler.h"
 
+#include <core/resource_management/resource_pool.h>
+#include <core/resource/media_server_resource.h>
 #include <utils/network/tcp_connection_priv.h>
 #include <utils/network/module_information.h>
+#include <utils/common/model_functions.h>
 #include <common/common_module.h>
 
 int QnModuleInformationRestHandler::executeGet(const QString &path, const QnRequestParams &params, QnJsonRestResult &result, const QnRestConnectionProcessor*) 
 {
     Q_UNUSED(path)
-    Q_UNUSED(params)
 
-    result.setReply(qnCommon->moduleInformation());
+    bool allModules = params.value(lit("allModules")) == lit("true");
+
+    if (allModules) {
+        QList<QnModuleInformation> modules;
+        for (const QnMediaServerResourcePtr &server: qnResPool->getAllServers())
+            modules.append(server->getModuleInformation());
+        result.setReply(modules);
+    } else {
+        result.setReply(qnCommon->moduleInformation());
+    }
     return CODE_OK;
 }
