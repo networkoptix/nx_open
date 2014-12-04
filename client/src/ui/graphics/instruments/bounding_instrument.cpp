9b453ac939ec72eb2819dc535815abe8fb076619
Denis Klychkov <dklychkov@networkoptix.com>
2014-12-04 17:00:44 +0300

    (Bug #4488): Couldn't zoom in on the Scene by mouse wheel
    <==== Mercurial data ====>
    Branch: release_2.3_review

--- client/src/ui/graphics/instruments/bounding_instrument.cpp
+++ client/src/ui/graphics/instruments/bounding_instrument.cpp
@@ -164,6 +164,8 @@ public:
         m_defaultStickyLogScaleResettingThreshold = 0.7;
         m_stickyLogScaleResettingThreshold = m_defaultStickyLogScaleResettingThreshold;
         m_logScaleResettingSpeedMultiplier = 0.16;
+        m_stickScaleTimeout = 300;
+        m_zoomInStopTimeMs = 0;
     }
 
     void update() {
@@ -266,6 +268,9 @@ public:
         /* Scene to viewport transform at the end of the last tick. */
         //QTransform oldSceneToViewport = m_sceneToViewport;
 
+        if (m_view->viewportTransform().m11() > m_sceneToViewport.m11())
+            m_zoomInStopTimeMs = time;
+
         /* Correct. */
 
         /* Calculate old scale. */
@@ -321,7 +326,7 @@ public:
 
                 qreal logDirection = calculateDistance(logScale, m_stickyLogScaleLo, m_stickyLogScaleHi);
                 qreal scaleSpeed = m_logScaleSpeed;
-                if (qFuzzyIsNull(logDirection)) {
+                if (qFuzzyIsNull(logDirection) && time - m_zoomInStopTimeMs > m_stickScaleTimeout) {
                     if (logScale > m_stickyLogScaleResettingThreshold) {
                         logDirection = 1.0 - logScale;
                         scaleSpeed *= m_logScaleResettingSpeedMultiplier;
@@ -540,6 +545,12 @@ public:
 
     /** Scale speed multiplier when scale is resetting to default value (m_stickyLogScaleHi). */
     qreal m_logScaleResettingSpeedMultiplier;
+
+    /** Timestamp when zoom-in was stopped */
+    qint64 m_zoomInStopTimeMs;
+
+    /** Timeout to stick scale after zoom-in */
+    qint64 m_stickScaleTimeout;
 };
 
 
