c56001b8abb4a513073d45b7ef0da2f05fb28e5e
Evgeny Balashov <ebalashov@networkoptix.com>
2014-12-09 16:23:00 +0300

    WC: timeline - sizing and zoom
    <==== Mercurial data ====>
    Branch: wc-timeline

--- webadmin/app/scripts/directives/timeline.js
+++ webadmin/app/scripts/directives/timeline.js
@@ -18,7 +18,8 @@ angular.module('webadminApp')
                     maxTimeLineWidth: 250000, // maximum width for timeline, which browser can handle
                     // TODO: support changing boundaries on huge detailization
                     initialZoom: 0, // Начальный масштаб (степень) 0 - чтобы увидеть весь таймлайн.
-                    updateInterval: 1000 // Интервал обновления таймлайна
+                    updateInterval: 1000, // Интервал обновления таймлайна
+                    animationDuration: 200 // Скорость анимации
                 };
 
                 function initTimeline(){
@@ -29,56 +30,74 @@ angular.module('webadminApp')
                     //scope.start = scope.ruler.start;
 
                     updateView();
+                    updateDetailization();
                 }
 
                 var viewportWidth = element.find('.viewport').width();
-                function updateView(){
-                    //1. Calculate timelineWidth, frameWidth according to actualZoom
-                    var zoomLevel = Math.pow(timelineConfig.zoomStep,scope.actualZoomLevel);
-                    var initialWidth = viewportWidth * zoomLevel; // Width for initial timeline interval - base for zooming
 
-                    scope.timelineWidth = initialWidth * (scope.ruler.marksTree.end -  scope.ruler.marksTree.start)/(scope.end - scope.start) + 'px';
-                    scope.timelineStart = initialWidth * (scope.ruler.marksTree.start - scope.start)/(scope.end - scope.start) + 'px';
+                function viewPortScroll(value){
+                    return element.find('.viewport').scrollLeft(value);
+                }
 
-                    scope.frameWidth = initialWidth * ((new Date()).getTime() -  scope.start)/(scope.end - scope.start) + 'px';
+                function zoomLevel(){
+                    return Math.pow(timelineConfig.zoomStep,scope.actualZoomLevel);
+                }
+                
+                function positionToDate(position){
+                    return Math.round(scope.start + (scope.end - scope.start) * position / (viewportWidth * zoomLevel()));
+                }
 
-                    console.log('updateView',zoomLevel,initialWidth,(scope.end - scope.start),(scope.ruler.end -  scope.ruler.start),scope.timelineWidth,scope.timelineStart,scope.frameWidth);
-
-                    //2. Calculate active boundaries for detailization
-                    console.warn('Calculate active boundaries for detailization');
-
-                    // Find scrollstart as the time
-                    var scrollStart = element.find('.viewport').scrollLeft();
-
-                    console.log('scroll ',scrollStart);
-
-                    var start = Math.round(scope.start + (scope.end - scope.start) * scrollStart / initialWidth);
-                    var end = Math.round(scope.start + (scope.end - scope.start) * (scrollStart + viewportWidth)/ initialWidth);
-                    // Find scrollstart + width as the time
-                    console.log('visible time', new Date(start),new Date(end));
-
+                function dateToPosition(date){
+                    return (date - scope.start) * (viewportWidth * zoomLevel()) / (scope.end - scope.start);
+                }
+                
+                function initialWidth(){
+                    return viewportWidth * zoomLevel();
+                }
+                
+                function updateDetailization(){
+                    var scrollStart = viewPortScroll();
+                    var start = positionToDate (scrollStart);
+                    var end = positionToDate (scrollStart + viewportWidth);
+                    
                     //3. Select actual level
                     var secondsPerPixel = (scope.end-scope.start)/initialWidth/1000;
-
                     for(var i=1; i < RulerModel.levels.length ; i ++ ){
                         if((RulerModel.levels[i].interval.getSeconds() / RulerModel.levels[i].width) < secondsPerPixel){
                             break;
                         }
                     }
-
-                    console.log('test level', i, RulerModel.levels[i].name,
-                            RulerModel.levels[i].interval.getSeconds() / RulerModel.levels[i].width, secondsPerPixel);
-
                     scope.actualLevel = i-1;
-
-                    console.log('level',scope.actualLevel, RulerModel.levels[scope.actualLevel].name,
-                        RulerModel.levels[scope.actualLevel].interval.getSeconds() / RulerModel.levels[scope.actualLevel].width, secondsPerPixel);
 
                     //4. Set interval for events
                     console.warn('Set interval for events', start,end,scope.actualLevel);
 
                     //5. Set interval for ruler
                     scope.ruler.setInterval(start,end,scope.actualLevel);
+                }
+
+                function updateView(){
+                    //1. Calculate timelineWidth, frameWidth according to actualZoom
+                    var initialWidth = initialWidth();// Width for initial timeline interval - base for zooming
+
+                    scope.timelineWidth = initialWidth * (scope.ruler.marksTree.end -  scope.ruler.marksTree.start)/(scope.end - scope.start) ;
+                    scope.timelineStart = initialWidth * (scope.ruler.marksTree.start - scope.start)/(scope.end - scope.start) ;
+                    scope.frameWidth = initialWidth * ((new Date()).getTime() -  scope.start)/(scope.end - scope.start) ;
+
+                    console.warn("run animate here");
+
+                    element.find(".timeline").stop(true,false).animate({
+                        width:scope.timelineWidth,
+                        left:scope.timelineStart
+                    },timelineConfig.animationDuration);
+
+                    element.find(".frame").stop(true,false).animate({
+                        width:scope.frameWidth
+                    },timelineConfig.animationDuration);
+
+
+
+                    console.warn("run updateDetailization() after animation complete");
                 }
 
                 scope.timelineWidth = '100%';
@@ -96,19 +115,38 @@ angular.module('webadminApp')
 
                 scope.disableZoomOut = true;
                 scope.disableZoomIn = false;
-                scope.zoom = function(zoomIn){
+
+
+                scope.zoom = function(zoomIn, speed, targetScrollTime, targetScrollTimePosition){
+
+                    speed = speed || 1;
+                    if(!targetScrollTime){
+                        targetScrollTime = positionToDate(viewPortScroll() + viewportWidth/2);
+                        targetScrollTimePosition = 0.5
+                    }
+
                     if(zoomIn && !scope.disableZoomIn) {
-                        scope.actualZoomLevel ++;
+                        scope.actualZoomLevel += speed;
                     }
 
                     if(!zoomIn && !scope.disableZoomOut ) {
-                        scope.actualZoomLevel --;
+                        scope.actualZoomLevel -= speed;
                         if(scope.actualZoomLevel <= 0) {
                             scope.actualZoomLevel = 0;
                         }
                     }
 
+                    var newScrollPosition = dateToPosition(targetScrollTime) - viewportWidth * targetScrollTimePosition;
+
                     updateView();
+                    
+                    console.warn("run scroll animation here");
+                    
+                    updateDetailization();
+                    
+                    element.find(".viewport").stop(true,false).animate({
+                        scrollLeft:newScrollPosition
+                    },timelineConfig.animationDuration);
 
                     scope.disableZoomOut = scope.actualZoomLevel<=0;
 
