8ef3a4339c47ca1d34a1cf6eec0a7f35e980b277
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2014-12-17 22:12:20 +0300

    refs #4633
    fixed inefficient HTTP message size calculation
    
    <==== Mercurial data ====>
    Branch: release_2.3_review

--- common/src/utils/network/tcp_connection_processor.cpp
+++ common/src/utils/network/tcp_connection_processor.cpp
@@ -45,16 +45,25 @@ QnTCPConnectionProcessor::~QnTCPConnectionProcessor()
     delete d_ptr;
 }
 
-int QnTCPConnectionProcessor::isFullMessage(const QByteArray& message)
+int QnTCPConnectionProcessor::isFullMessage(
+    const QByteArray& message,
+    boost::optional<qint64>* const fullMessageSize )
 {
-    QByteArray lRequest = message.toLower();
+    if( fullMessageSize && fullMessageSize->is_initialized() )
+        return fullMessageSize->get() > message.size()
+            ? 0
+            : fullMessageSize->get();   //signalling that full message has been read
+
     QByteArray delimiter = "\n";
-    int pos = lRequest.indexOf(delimiter);
+    int pos = message.indexOf(delimiter);
     if (pos == -1)
         return 0;
-    if (pos > 0 && lRequest[pos-1] == '\r')
+    if (pos > 0 && message[pos-1] == '\r')
         delimiter = "\r\n";
     int contentLen = 0;
+
+    //retrieving content-length from message
+    QByteArray lRequest = message.toLower();
     int contentLenPos = lRequest.indexOf("content-length");
     if (contentLenPos >= 0)
     {
@@ -74,12 +83,15 @@ int QnTCPConnectionProcessor::isFullMessage(const QByteArray& message)
         if (posStart >= 0)
             contentLen = lRequest.mid(posStart, posEnd - posStart+1).toInt();
     }
+
     QByteArray dblDelim = delimiter + delimiter;
-    //return lRequest.endsWith(dblDelim) && (!contentLen || lRequest.indexOf(dblDelim) < lRequest.length()-dblDelim.length());
-    if (lRequest.indexOf(dblDelim) == -1)
+    const int dblDelimPos = message.indexOf(dblDelim);
+    if (dblDelimPos == -1)
         return 0;
-    int expectedSize = lRequest.indexOf(dblDelim) + dblDelim.size() + contentLen;
-    if (expectedSize > lRequest.size())
+    const int expectedSize = dblDelimPos + dblDelim.size() + contentLen;
+    if( fullMessageSize )
+        *fullMessageSize = expectedSize;
+    if (expectedSize > message.size())
         return 0;
     else 
         return expectedSize;
@@ -225,7 +237,6 @@ bool QnTCPConnectionProcessor::sendChunk( const QByteArray& chunk )
 
 bool QnTCPConnectionProcessor::sendChunk( const char* data, int size )
 {
-    Q_D(QnTCPConnectionProcessor);
     QByteArray result = QByteArray::number(size,16);
     result.append("\r\n");
     result.append(data, size);  //TODO/IMPL avoid copying by implementing writev in socket
@@ -291,6 +302,7 @@ bool QnTCPConnectionProcessor::readRequest()
     d->requestBody.clear();
     d->responseBody.clear();
 
+    boost::optional<qint64> fullHttpMessageSize;
     while (!needToStop() && d->socket->isConnected())
     {
         int readed;
@@ -299,7 +311,7 @@ bool QnTCPConnectionProcessor::readRequest()
         {
             //globalTimeout.restart();
             d->clientRequest.append((const char*) d->tcpReadBuffer, readed);
-            if (isFullMessage(d->clientRequest))
+            if (isFullMessage(d->clientRequest, &fullHttpMessageSize))
             {
                 NX_LOG( QnLog::HTTP_LOG_INDEX, QString::fromLatin1("Received request from %1:\n%2-------------------\n\n\n").
                     arg(d->socket->getPeerAddress().toString()).
@@ -311,6 +323,8 @@ bool QnTCPConnectionProcessor::readRequest()
                 qWarning() << "Too large HTTP client request. Ignoring";
                 return false;
             }
+            if( fullHttpMessageSize )
+                d->clientRequest.reserve( fullHttpMessageSize.get() );
         }
         else //if (globalTimeout.elapsed() > CONNECTION_TIMEOUT)
         {
