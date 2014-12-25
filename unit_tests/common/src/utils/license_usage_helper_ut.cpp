08b4e15b80a8da4c4d8f0ab35b234801ad768d5b
Sergey Ivanov <sivanov@networkoptix.com>
2014-12-25 19:38:49 +0300

    Basic tests for QnCamLicenseUsageHelper
    <==== Mercurial data ====>
    Branch: feature_4636

--- unit_tests/common/src/utils/license_usage_helper_ut.cpp
+++ unit_tests/common/src/utils/license_usage_helper_ut.cpp
@@ -1,12 +1,37 @@
 #include <gtest/gtest.h>
 
 #include <core/resource_management/resource_pool_scaffold.h>
+#include <licensing/license_pool_scaffold.h>
+
+#include <core/resource/camera_resource.h>
+
 #include <utils/license_usage_helper.h>
 
 TEST( QnCamLicenseUsageHelperTest, init )
 {
-    QnResourcePoolScaffold scaffold;
-    QnCamLicenseUsageHelper helper;
+    QnLicensePoolScaffold licPoolScaffold;
 
-    ASSERT_EQ( helper.isValid(), true );
+    QnResourcePoolScaffold resPoolScaffold;
+    QnCamLicenseUsageHelper helper;
+    ASSERT_TRUE( helper.isValid() );
+}
+
+TEST( QnCamLicenseUsageHelperTest, testProfessionalLicenses )
+{
+    QnLicensePoolScaffold licPoolScaffold;
+
+    QnResourcePoolScaffold resPoolScaffold;
+    resPoolScaffold.addCamera()->setScheduleDisabled(false);
+
+    QnCamLicenseUsageHelper helper;
+    ASSERT_FALSE( helper.isValid() );
+
+    licPoolScaffold.addLicense(Qn::LC_Professional, 2);
+    ASSERT_TRUE( helper.isValid() );
+
+    resPoolScaffold.addCamera()->setScheduleDisabled(false);
+    ASSERT_TRUE( helper.isValid() );
+
+    resPoolScaffold.addCamera()->setScheduleDisabled(false);
+    ASSERT_FALSE( helper.isValid() );
 }
