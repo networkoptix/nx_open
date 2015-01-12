cd12f997f462fb58b0ff1a7c2110a759f92e3e9d
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-01-12 20:01:08 +0300

    refs #4698
    added unit test for asynchronous socket operation cancellation
    
    <==== Mercurial data ====>
    Branch: bug_4698_async_socket_termination

--- unit_tests/common/src/utils/network/socket.cpp
+++ unit_tests/common/src/utils/network/socket.cpp
@@ -4,33 +4,67 @@
 ***********************************************************/
 
 #include <chrono>
+#include <thread>
 
 #include <gtest/gtest.h>
 
+#include "socket_test_helper.h"
+
+
+namespace 
+{
+    const int SECONDS_TO_WAIT_AFTER_TEST = 5;
+}
 
 /*!
     This test verifies that AbstractCommunicatingSocket::cancelAsyncIO method works fine
 */
 TEST( SocketAsyncModeTest, AsyncOperationCancellation )
 {
-    static const int MAX_SIMULTANEOUS_CONNECTIONS = 100;
-    static const int BYTES_TO_SEND_THROUGH_CONNECTION = 100*1024*1024;
-    static const int TEST_DURATION_SECONDS = 11;
+    static const int TEST_DURATION_SECONDS = 1;
+    static const int TEST_RUNS = 20;
 
-    RandomDataTcpServer server( BYTES_TO_SEND_THROUGH_CONNECTION );
-    ASSERT( server.start() );
+    for( int i = 0; i < TEST_RUNS; ++i )
+    {
+        static const int MAX_SIMULTANEOUS_CONNECTIONS = 100;
+        static const int BYTES_TO_SEND_THROUGH_CONNECTION = 100*1024*1024;
 
-    ConnectionsGenerator connectionsGenerator(
-        server.addressBeingListened(),
-        MAX_SIMULTANEOUS_CONNECTIONS,
-        BYTES_TO_SEND_THROUGH_CONNECTION );
-    ASSERT( connectionsGenerator.start() );
+        RandomDataTcpServer server( BYTES_TO_SEND_THROUGH_CONNECTION );
+        ASSERT_TRUE( server.start() );
 
-    std::this_thread::sleep_for( std::chrono::seconds( TEST_DURATION_SECONDS ) );
+        ConnectionsGenerator connectionsGenerator(
+            server.addressBeingListened(),
+            MAX_SIMULTANEOUS_CONNECTIONS,
+            BYTES_TO_SEND_THROUGH_CONNECTION );
+        ASSERT_TRUE( connectionsGenerator.start() );
 
-    connectionsGenerator.pleaseStop();
-    connectionsGenerator.join();
+        std::this_thread::sleep_for( std::chrono::seconds( TEST_DURATION_SECONDS ) );
 
-    server.pleaseStop();
-    server.join();
+        connectionsGenerator.pleaseStop();
+        connectionsGenerator.join();
+
+        server.pleaseStop();
+        server.join();
+    }
+
+    //waiting for some calls to deleted objects
+    std::this_thread::sleep_for( std::chrono::seconds( SECONDS_TO_WAIT_AFTER_TEST ) );
+}
+
+TEST( SocketAsyncModeTest, AsyncOperationCancellation2 )
+{
+    static const int TEST_RUNS = 47;
+
+    for( int i = 0; i < TEST_RUNS; ++i )
+    {
+        std::unique_ptr<AbstractStreamServerSocket> serverSocket( SocketFactory::createStreamServerSocket() );
+        ASSERT_TRUE( serverSocket->setNonBlockingMode(true) );
+        ASSERT_TRUE( serverSocket->bind(SocketAddress()) );
+        ASSERT_TRUE( serverSocket->listen() );
+        ASSERT_TRUE( serverSocket->acceptAsync( [](SystemError::ErrorCode, AbstractStreamSocket*){  } ) );
+        serverSocket->terminateAsyncIO( true );
+    }
+
+    //waiting for some calls to deleted objects
+    std::this_thread::sleep_for( std::chrono::seconds( SECONDS_TO_WAIT_AFTER_TEST ) );
 }
