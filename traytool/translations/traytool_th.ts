e07e4ae71fcb653f2c516db084de0b0dd246f947
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2015-01-27 09:50:53 -0800

    thai is ready
    <==== Mercurial data ====>
    Branch: release_2.3_review

--- traytool/translations/traytool_th.ts
+++ traytool/translations/traytool_th.ts
@@ -6,13 +6,15 @@
     <message>
         <location filename="../src/main.cpp" line="21"/>
         <source>System Tray</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>มีเดียเซิร์ฟเวอร์</translatorcomment>
+        <translation>แถบงาน</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="22"/>
         <source>There is no system tray on this system.
 Application will now quit.</source>
-        <translation type="unfinished"></translation>
+        <translation>ไม่มีแถบงานในระบบนี้
+แอพลิเคชันจะทำการปิด.</translation>
     </message>
 </context>
 <context>
@@ -20,12 +22,12 @@ Application will now quit.</source>
     <message>
         <location filename="../src/systraywindow.cpp" line="425"/>
         <source>Insufficient privileges to manage services</source>
-        <translation type="unfinished"></translation>
+        <translation>สิทธิ์ไม่เพียงพอในการจัดการบริการ</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="425"/>
         <source>UAC must be enabled to request privileges for non-admin users</source>
-        <translation type="unfinished"></translation>
+        <translation>UAC จะต้องได้รับการใช้งานขอรับสิทธิประโยชน์สำหรับผู้ใช้ที่ไม่ใช่ผู้ดูแลระบบ</translation>
     </message>
 </context>
 <context>
@@ -33,108 +35,109 @@ Application will now quit.</source>
     <message>
         <location filename="../src/systraywindow.cpp" line="157"/>
         <source>Access denied.</source>
-        <translation type="unfinished"></translation>
+        <translation>ปฏิเสธการเข้าใช้.</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="160"/>
         <source>Specified database does not exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>ฐานข้อมูลที่ระบุไม่มีอยู่.</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="163"/>
         <source>Specified parameter is invalid.</source>
-        <translation type="unfinished"></translation>
+        <translation>พารามิเตอร์ระบุไม่ถูกต้อง.</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="167"/>
         <source>Could not access installed services</source>
-        <translation type="unfinished"></translation>
+        <translation>ไม่สามารถเข้าถึงการติดตั้งได้</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="167"/>
         <source>An error has occurred while trying to access installed services:
  %1</source>
-        <translation type="unfinished"></translation>
+        <translation>เกิดข้อผิดพลาดในขณะที่พยายามที่จะเข้าถึงบริการการติดตั้ง:
+ %1</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="180"/>
         <source>No %1 services installed</source>
-        <translation type="unfinished"></translation>
+        <translation>ไม่มี %1บริการติดตั้ง</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="279"/>
         <source>Media server has been stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>มีเดียเซิร์ฟเวอร์หยุดการทำงาน</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="286"/>
         <source>Media server has been started</source>
-        <translation type="unfinished"></translation>
+        <translation>มีเดียเซิร์ฟเวอร์เริ่มการทำงาน</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="310"/>
         <source> (stopped)</source>
-        <translation type="unfinished"></translation>
+        <translation> (หยุด)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="318"/>
         <source> (starting)</source>
-        <translation type="unfinished"></translation>
+        <translation> (เริ่ม)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="326"/>
         <source> (stopping)</source>
-        <translation type="unfinished"></translation>
+        <translation> (กำลังหยุด)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="334"/>
         <source> (started)</source>
-        <translation type="unfinished"></translation>
+        <translation> (เริ่ม)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="342"/>
         <source> (resuming)</source>
-        <translation type="unfinished"></translation>
+        <translation>(กลับมาทำงานใหม่)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="350"/>
         <source> (paused)</source>
-        <translation type="unfinished"></translation>
+        <translation>(หยุดชั่วคราว)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="358"/>
         <source> (pausing)</source>
-        <translation type="unfinished"></translation>
+        <translation>(หยุดชั่วคราว)</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="386"/>
         <source>Media server is going to be stopped. Are you sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>มีเดียเซิร์ฟเวอร์กำลังหยุดทำงาน คุณแน่ใจหรือไม่?</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="465"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;ออก</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="468"/>
         <source>Show &amp;Media Server Log</source>
-        <translation type="unfinished"></translation>
+        <translation>แสดง &amp;การบันทึก มีเดียเซิร์ฟเวอร์</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="473"/>
         <source>Start Media Server</source>
-        <translation type="unfinished"></translation>
+        <translation>เริ่มต้นมีเดียเซิร์ฟเวอร์</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="478"/>
         <source>Stop Media Server</source>
-        <translation type="unfinished"></translation>
+        <translation>หยุดมีเดียวเซิร์ฟเวอร์</translation>
     </message>
     <message>
         <location filename="../src/systraywindow.cpp" line="483"/>
         <source>Media Server Web Page</source>
-        <translation type="unfinished"></translation>
+        <translation>มีเดียเซิร์ฟเวอร์  เว็บเพจ</translation>
     </message>
 </context>
 </TS>
