b787053eeb8168c38063b72a958373e546d2f519
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-02-02 20:26:21 +0300

    (Issue #4978) : Disable peer-to-peer sync in parallel mode
    <==== Mercurial data ====>
    Branch: edge_2.3.1

--- appserver2/src/transaction/transaction_message_bus.cpp
+++ appserver2/src/transaction/transaction_message_bus.cpp
@@ -485,6 +485,7 @@ void QnTransactionMessageBus::onGotTransactionSyncResponse(QnTransactionTranspor
 void QnTransactionMessageBus::onGotTransactionSyncDone(QnTransactionTransport* sender, const QnTransaction<ApiTranSyncDoneData> &tran) {
     Q_UNUSED(tran)
     sender->setSyncDone(true);
+    sender->setSyncInProgress(false);
     // propagate new data to other peers. Aka send current state, other peers should request update if need
     handlePeerAliveChanged(m_localPeer, true, true); 
     m_aliveSendTimer.restart();
@@ -808,11 +809,31 @@ void QnTransactionMessageBus::onGotTransactionSyncRequest(QnTransactionTransport
     }
 }      
 
+bool QnTransactionMessageBus::isSyncInProgress() const
+{
+    for (QnConnectionMap::const_iterator itr = m_connections.begin(); itr != m_connections.end(); ++itr)
+    {
+        QnTransactionTransport* transport = *itr;
+        if (transport->remotePeer().isServer() && transport->isSyncInProgress())
+            return true;
+    }
+    return false;
+}
+
 void QnTransactionMessageBus::queueSyncRequest(QnTransactionTransport* transport)
 {
     // send sync request
     transport->setReadSync(false);
     transport->setSyncDone(false);
+
+    if (isSyncInProgress()) {
+        transport->setNeedResync(true);
+        return;
+    }
+    
+    transport->setSyncInProgress(true);
+    transport->setNeedResync(false);
+
     QnTransaction<ApiSyncRequestData> requestTran(ApiCommand::tranSyncRequest);
     requestTran.params.persistentState = transactionLog->getTransactionsState();
     requestTran.params.runtimeState = m_runtimeTransactionLog->getTransactionsState();
@@ -1124,6 +1145,8 @@ void QnTransactionMessageBus::doPeriodicTasks()
                     qWarning() << "Transaction Transport HTTP keep-alive timeout for connection" << transport->remotePeer().id;
                     transport->setState(QnTransactionTransport::Error);
                 }
+                else if (transport->isNeedResync())
+                    queueSyncRequest(transport);
             }
         }
     }
