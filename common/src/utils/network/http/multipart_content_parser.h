db216bb537219edbe9fb9f6603993bb3aee56abd
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-02-17 23:08:36 +0300

    refs #5148 fixed HTTP multipart content parsing in case if part size is unknown
    <==== Mercurial data ====>
    Branch: release_2.3_review

--- common/src/utils/network/http/multipart_content_parser.h
+++ common/src/utils/network/http/multipart_content_parser.h
@@ -29,13 +29,21 @@ namespace nx_http
         void setBoundary( const StringType& boundary );
 
     private:
-        // TODO: #Elric #enum
         enum ParsingState
         {
             waitingBoundary,
             readingHeaders,
             readingTextData,
-            readingBinaryData
+            //!reading data with Content-Length known
+            readingSizedBinaryData,
+            //!reading data with Content-Length not known: searching for boundary
+            readingUnsizedBinaryData
+        };
+
+        enum ChunkParseState
+        {
+            waitingEndOfLine,
+            checkingForBoundaryAfterEndOfLine
         };
 
         ParsingState m_state;
@@ -44,7 +52,15 @@ namespace nx_http
         StringType m_boundary;
         StringType m_startBoundaryLine;
         StringType m_endBoundaryLine;
+        StringType m_boundaryForUnsizedBinaryParsing;
         unsigned int m_contentLength;
+        ChunkParseState m_chunkParseState;
+        nx::Buffer m_supposedBoundary;
+        nx_http::HttpHeaders m_currentFrameHeaders;
+
+        void readUnsizedBinaryData(
+            const QnByteArrayConstRef& data,
+            size_t* const offset );
     };
 }
 
