bf739af8cf9edcb704251ae73f94909eac5b2cf7
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-02-26 21:24:01 +0300

    refs #4815 if db is not stored in storage directory, than DB reference file is placed to help find DB in case of storage directory has been renamed or moved to a different server
    <==== Mercurial data ====>
    Branch: release_2.3_review

--- mediaserver/src/recorder/storage_manager.cpp
+++ mediaserver/src/recorder/storage_manager.cpp
@@ -389,15 +389,38 @@ int QnStorageManager::detectStorageIndex(const QString& p)
     }
 }
 
-static QString getDBPath( const QnStorageResourcePtr& storage )
+static const QString dbRefFileName( QLatin1String("db_ref.guid") );
+
+static bool getDBPath( const QnStorageResourcePtr& storage, QString* const dbDirectory )
 {
+    QString storagePath = storage->getPath();
+    const QString dbRefFilePath = storagePath+"/"+dbRefFileName;
+
+    QByteArray dbRefGuidStr;
+    //checking for file db_ref.guid existence
+    if( QFile::exists(dbRefFilePath) )
+    {
+        //have to use db from data directory, not from storage
+        //reading guid from file
+        QFile dbGuidFile( dbRefFilePath );
+        if( !dbGuidFile.open( QIODevice::ReadOnly ) )
+            return false;
+        dbRefGuidStr = dbGuidFile.readAll();
+    }
+
+    if( !dbRefGuidStr.isEmpty() )
+    {
+        *dbDirectory = QDir(getDataDirectory() + "/storage_db/" + dbRefGuidStr).absolutePath();
+        return true;
+    }
+
 #ifdef _WIN32
-    return storage->getPath();
+    //on windows always placing db to a storage directory
+    *dbDirectory = storagePath;
+    return true;
 #else
     //On linux, sqlite db cannot be safely placed on a network partition due to lock problem
         //So, for such storages creating DB in data dir
-
-    QString storagePath = storage->getPath();
 
     QList<QnPlatformMonitor::PartitionSpace> partitions = 
         qnPlatform->monitor()->QnPlatformMonitor::totalPartitionSpaceInfo(
@@ -408,13 +431,28 @@ static QString getDBPath( const QnStorageResourcePtr& storage )
         if( !storagePath.startsWith( partition.path ) )
             continue;
 
-        storagePath = QDir(getDataDirectory() + "/storage_db/" +
-            QCryptographicHash::hash(storagePath.toLatin1(), QCryptographicHash::Md5).toHex()).absolutePath();
-        QDir().mkpath( storagePath );
-        return storagePath;
+        dbRefGuidStr = QUuid::createUuid().toString().toLatin1();
+        if( dbRefGuidStr.size() < 2 )
+            return false;   //bad guid, somehow
+        //removing {}
+        dbRefGuidStr.remove( dbRefGuidStr.size()-1, 1 );
+        dbRefGuidStr.remove( 0, 1 );
+        //saving db ref guid to file on storage
+        QFile dbGuidFile( dbRefFilePath );
+        if( !dbGuidFile.open( QIODevice::WriteOnly | QIODevice::Truncate ) )
+            return false;
+        if( dbGuidFile.write( dbRefGuidStr ) != dbRefGuidStr.size() )
+            return false;
+        dbGuidFile.close();
+
+        storagePath = QDir(getDataDirectory() + "/storage_db/" + dbRefGuidStr).absolutePath();
+        if( !QDir().mkpath( storagePath ) )
+            return false;
+        break;
     }
 
-    return storagePath;
+    *dbDirectory = storagePath;
+    return true;
 #endif
 }
 
@@ -427,7 +465,12 @@ QnStorageDbPtr QnStorageManager::getSDB(const QnStorageResourcePtr &storage)
         QString simplifiedGUID = qnCommon->moduleGUID().toString();
         simplifiedGUID = simplifiedGUID.replace("{", "");
         simplifiedGUID = simplifiedGUID.replace("}", "");
-        const QString& dbPath = getDBPath(storage);
+        QString dbPath;
+        if( !getDBPath(storage, &dbPath) )
+        {
+            NX_LOG( lit("Failed to file path to storage DB file. Storage is not writable?"), cl_logWARNING );
+            return QnStorageDbPtr();
+        }
         QString fileName = closeDirPath(dbPath) + QString::fromLatin1("%1_media.sqlite").arg(simplifiedGUID);
         QString oldFileName = closeDirPath(dbPath) + QString::fromLatin1("media.sqlite");
         if (QFile::exists(oldFileName) && !QFile::exists(fileName))
