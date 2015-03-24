1e3ccf105f155dcce37154651f6cfbe42473c13f
Evgeny Balashov <ebalashov@networkoptix.com>
2015-03-24 13:10:12 +0300

    WC: fix mediaserver.js
    <==== Mercurial data ====>
    Branch: wc-timeline

--- webadmin/app/scripts/services/mediaserver.js
+++ webadmin/app/scripts/services/mediaserver.js
@@ -1,25 +1,40 @@
 'use strict';
 
 angular.module('webadminApp')
-    .factory('mediaserver', function ($http, $modal, ipCookie,$log) {
+    .factory('mediaserver', function ($http, $modal, $q, ipCookie,$log) {
 
         var cacheModuleInfo = null;
         var cacheCurrentUser = null;
 
+        var proxy = '';
+        if(location.search.indexOf('proxy=')>0){
+            var params = location.search.replace('?','').split('&');
+            for (var i=0;i<params.length;i++) {
+                var pair = params[i].split("=");
+                if(pair[0] === 'proxy'){
+                    proxy = '/proxy/' + pair[1];
+                    break;
+                }
+            }
+        }
+
+        ipCookie('Authorization','Digest', { path: '/' });
+
         function getSettings(){
-            return $http.get('/api/moduleInformation?salt=' + (new Date()).getTime());
+            return $http.get(proxy + '/api/moduleInformation?showAddresses=true');
         }
 
         var offlineDialog = null;
         var loginDialog = null;
         function offlineHandler(error){
 
-            //console.log(error);
-
             // Check 401 against offline
 
             if(error.status === 401) {
-                if (loginDialog === null) { //Dialog is not displayed
+
+                var isInFrame = window.self !== window.top; // If we are in frame - do not show dialog
+
+                if (!isInFrame && loginDialog === null) { //Dialog is not displayed
                     loginDialog = $modal.open({
                         templateUrl: 'views/login.html',
                         keyboard:false,
@@ -43,7 +58,7 @@ angular.module('webadminApp')
                         keyboard:false,
                         backdrop:'static'
                     });
-                    offlineDialog.result.then(function () {//Dialog closed - means server is online
+                    offlineDialog.result.then(function (info) {//Dialog closed - means server is online
                         offlineDialog = null;
                     });
                 });
@@ -54,8 +69,25 @@ angular.module('webadminApp')
             return request;
         }
         return {
+            logUrl:function(params){
+                return proxy + '/api/showLog' + (params||'');
+            },
+            hasProxy:function(){
+                return proxy !=='';
+            },
+            checkAdmin:function(){
+                var deferred = $q.defer();
+                if(this.hasProxy()){ // Proxy means read-only
+                    deferred.resolve(false);
+                }
+                this.getCurrentUser().then(function(result){
+                    var isAdmin = result.data.reply.isAdmin || (result.data.reply.permissions & Config.globalEditServersPermissions);
+                    deferred.resolve(isAdmin);
+                });
+                return deferred.promise;
+            },
             getSettings: function(url) {
-                url = url || '';
+                url = url || proxy;
                 if(url === true){//force reload cache
                     cacheModuleInfo = null;
                     return getSettings();
@@ -67,43 +99,43 @@ angular.module('webadminApp')
                     // on error - clear object to reload next time
                     return cacheModuleInfo;
                 }
-                return $http.get(url + '/api/moduleInformation',{
+                //Some another server
+                return $http.get(url + '/api/moduleInformation?showAddresses=true',{
                     timeout: 3*1000
                 });
             },
             saveSettings: function(systemName,port) {
-                return wrapRequest($http.post('/api/configure?systemName=' + systemName + '&port=' + port));
+                return wrapRequest($http.post(proxy + '/api/configure?systemName=' + systemName + '&port=' + port));
             },
             changePassword: function(password,oldPassword) {
-                return wrapRequest($http.post('/api/configure?password=' + password  + '&oldPassword=' + oldPassword));
+                return wrapRequest($http.post(proxy + '/api/configure?password=' + password  + '&oldPassword=' + oldPassword));
             },
             mergeSystems: function(url,password,currentPassword,keepMySystem){
-                return wrapRequest($http.post('/api/mergeSystems?password=' + password +
-                    '&currentPassword=' + currentPassword +
-                    '&url=' + encodeURIComponent(url) +
-                    '&takeRemoteSettings=' + (!keepMySystem)));
-            },
-            pingSystem: function(url,password){return wrapRequest($http.post('/api/pingSystem?password=' + password  + '&url=' + encodeURIComponent(url))); },
-            restart: function() { return wrapRequest($http.post('/api/restart')); },
-            getStorages: function(){ return wrapRequest($http.get('/api/storageSpace')); },
-            saveStorages:function(info){return wrapRequest($http.post('/ec2/saveStorages',info)); },
-            discoveredPeers:function(){return wrapRequest($http.get('/api/discoveredPeers')); },
-            getMediaServer: function(id){return wrapRequest($http.get('/ec2/getMediaServersEx?id=' + id.replace('{','').replace('}',''))); },
-            getMediaServers: function(){return wrapRequest($http.get('/ec2/getMediaServersEx')); },
+                return wrapRequest($http.post(proxy + '/api/mergeSystems?password=' + password
+                    + '&currentPassword=' + currentPassword
+                    + '&url=' + encodeURIComponent(url)
+                    + '&takeRemoteSettings=' + (!keepMySystem))); },
+            pingSystem: function(url,password){return wrapRequest($http.post(proxy + '/api/pingSystem?password=' + password  + '&url=' + encodeURIComponent(url))); },
+            restart: function() { return wrapRequest($http.post(proxy + '/api/restart')); },
+            getStorages: function(){ return wrapRequest($http.get(proxy + '/api/storageSpace')); },
+            saveStorages:function(info){return wrapRequest($http.post(proxy + '/ec2/saveStorages',info)); },
+            discoveredPeers:function(){return wrapRequest($http.get(proxy + '/api/discoveredPeers')); },
+            getMediaServer: function(id){return wrapRequest($http.get(proxy + '/ec2/getMediaServersEx?id=' + id.replace('{','').replace('}',''))); },
+            getMediaServers: function(){return wrapRequest($http.get(proxy + '/ec2/getMediaServersEx')); },
             getCameras:function(id){
                 if(typeof(id)!=='undefined'){
-                    return wrapRequest($http.get('/ec2/getCamerasEx?id=' + id.replace('{','').replace('}','')));
+                    return wrapRequest($http.get(proxy + '/ec2/getCamerasEx?id=' + id.replace('{','').replace('}','')));
                 }
-                return wrapRequest($http.get('/ec2/getCamerasEx'));
+                return wrapRequest($http.get(proxy + '/ec2/getCamerasEx'));
             },
-            saveMediaServer: function(info){return wrapRequest($http.post('/ec2/saveMediaServer',info)); },
+            saveMediaServer: function(info){return wrapRequest($http.post(proxy + '/ec2/saveMediaServer',info)); },
             statistics:function(url){
-                url = url || '';
+                url = url || proxy;
                 return wrapRequest($http.get(url + '/api/statistics?salt=' + (new Date()).getTime()));
             },
             getCurrentUser:function(forcereload){
                 if(cacheCurrentUser === null || forcereload){
-                    cacheCurrentUser = wrapRequest($http.get('/api/getCurrentUser'));
+                    cacheCurrentUser = wrapRequest($http.get(proxy + '/api/getCurrentUser'));
                 }
                 return cacheCurrentUser;
             },
@@ -128,7 +160,7 @@ angular.module('webadminApp')
                 }
                 //RecordedTimePeriods
                 return wrapRequest($http.get(serverUrl + 'api/RecordedTimePeriods' +
-                    '?physicalId=' + physicalId.replace('{','').replace('}','') +
+                    '?physicalId=' + physicalId +
                     '&startTime=' + startTime +
                     '&endTime=' + endTime +
                     '&detail=' + detail +
