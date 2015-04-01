5973f65284820ee8c9abd16742ca26a2cbf7bb9b
Ivan Vigasin <ivigasin@networkoptix.com>
2015-04-01 00:40:03 -0700

    fix maven release build
    <==== Mercurial data ====>
    Branch: paxton

--- plugins/acs/paxton-installer/maven/filter-resources/build-installer.bat
+++ plugins/acs/paxton-installer/maven/filter-resources/build-installer.bat
@@ -1,3 +1,2 @@
 candle -dAxSourceDir=..\..\..\..\build_environment\target\x86\bin\Release Product.wxs AxClient.wxs
 light -ext WixUIExtension.dll -cultures:en-us Product.wixobj AxClient.wixobj -out ${finalName}
-sign 
