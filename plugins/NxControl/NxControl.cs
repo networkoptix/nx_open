d0bda28fb07363334687fe8e4371778247d0f034
Ivan Vigasin <ivigasin@networkoptix.com>
2015-04-03 12:34:35 -0700

    revert invalid commit f846e8f34014
    <==== Mercurial data ====>
    Branch: paxton

--- plugins/NxControl/NxControl.cs
+++ plugins/NxControl/NxControl.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NxInterface;
-using AxInterop.NxWitness;
+using AxInterop.DWSpectrum;
 
 namespace NxControl
 {
-    public class NxControl : AxAxNxWitness, IPlugin
+    public class NxControl : AxAxDWSpectrum, IPlugin
     {
         public NxControl() {
-            base.connectionProcessed += axNxWitness1_connectedProcessed;
+            base.connectionProcessed += axDWSpectrum1_connectedProcessed;
         }
 
         public event ConnectionProcessedEventHandler connectProcessed;
-        void axNxWitness1_connectedProcessed(object sender, IAxNxWitnessEvents_connectionProcessedEvent e)
-        {
+        void axDWSpectrum1_connectedProcessed(object sender, IAxDWSpectrumEvents_connectionProcessedEvent e) {
             connectProcessed(sender, new ConnectionProcessedEvent(e.p_status, e.p_message));
         }
     }
