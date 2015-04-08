81481169eb7f429cf2f6b65e77870b4fe0e14ad9
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-04-08 18:53:10 +0300

    (Issue #5407) :  Deadlock at jaguar at v.2.3.1
    <==== Mercurial data ====>
    Branch: release_2.3.1_review

--- mediaserver/src/main.cpp
+++ mediaserver/src/main.cpp
@@ -1833,6 +1833,7 @@ void QnMain::run()
     QnResource::initAsyncPoolInstance()->setMaxThreadCount( MSSettings::roSettings()->value(
         nx_ms_conf::RESOURCE_INIT_THREADS_COUNT,
         nx_ms_conf::DEFAULT_RESOURCE_INIT_THREADS_COUNT ).toInt() );
+    QnResource::initAsyncPoolInstance()->setExpiryTimeout(-1); // default experation timeout is 30 second. But it has a bug in QT < v.5.3
 
     //============================
     std::unique_ptr<UPNPDeviceSearcher> upnpDeviceSearcher(new UPNPDeviceSearcher());
