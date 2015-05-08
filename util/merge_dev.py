8686013ab0d5017e6a0364f69b026787a99ad88b
Sergey Ivanov <sivanov@networkoptix.com>
2015-05-08 17:22:30 +0300

    (Issue #5577) :  Create auto-merge script
    <==== Mercurial data ====>
    Branch: dev_2.3.1

--- util/merge_dev.py
+++ util/merge_dev.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 #/bin/python
 
 import subprocess
 import sys
+import os
+import argparse
 
 def execCommand(command):
     print command
@@ -10,13 +13,34 @@ def execCommand(command):
         sys.exit(code)
     return code
         
-def getChangelog():
-    command = 'hg log -b dev_2.3.1 -T "{desc}\\n\\n" -r "(::dev_2.3.1 - ::prod_2.3.1)"'
+def getChangelog(revision):
+    command = 'hg log -T "{desc}\\n\\n" -r '
+    changeset = '"(::{0} - ::prod_2.3.1)"'.format(revision)
+    command = command + changeset
+    print command
     changelog = subprocess.check_output(command, shell=True)
     changes = sorted(set(changelog.split('\n\n')))
     
     return '\n'.join(changes)
+      
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--rev', type=str, help="Target revision")
+    parser.add_argument('-p', '--preview', action='store_true', help="preview changes")
+    args = parser.parse_args()
     
-execCommand('hg up prod_2.3.1')     
-execCommand('hg merge dev_2.3.1')
-execCommand('hg ci -m"{0}"'.format(getChangelog()))
+    revision = args.rev
+    if not revision:
+        revision = 'dev_2.3.1'
+   
+    if args.preview:
+        print getChangelog(revision)
+        sys.exit(0)
+        
+    execCommand('hg up prod_2.3.1')     
+    execCommand('hg merge {0}').format(revision)
+    execCommand('hg ci -m"{0}"'.format(getChangelog()))
+    sys.exit(0)
+    
+if __name__ == "__main__":
+    main()
