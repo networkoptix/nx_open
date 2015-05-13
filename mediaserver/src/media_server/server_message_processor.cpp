21fe700f66fcdafe04c9b120d7f964cf2f2cabc7
Denis Klychkov <dklychkov@networkoptix.com>
2015-05-13 20:40:08 +0300

    Bug #5586: Re-save current server resource if it has been changed from the outside.
    <==== Mercurial data ====>
    Branch: dev_2.3.1_ui

--- mediaserver/src/media_server/server_message_processor.cpp
+++ mediaserver/src/media_server/server_message_processor.cpp
@@ -71,8 +71,13 @@ void QnServerMessageProcessor::updateResource(const QnResourcePtr &resource)
 
     if (isServer) 
     {
-        if (resource->getId() != ownMediaServer->getId())
-            resource->addFlags( Qn::foreigner );
+        if (resource->getId() == ownMediaServer->getId()) {
+            QnMediaServerResourcePtr savedServer;
+            QnAppServerConnectionFactory::getConnection2()->getMediaServerManager()->saveSync(ownMediaServer, &savedServer);
+            return;
+        }
+
+        resource->addFlags(Qn::foreigner);
     }
 
     // We are always online
