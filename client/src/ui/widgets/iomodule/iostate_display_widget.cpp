f9eb0224345dcd919827f7a520f11d84d1ec97cb
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-07-01 20:31:12 +0300

    (#5826) - client IO monitor dialog
    <==== Mercurial data ====>
    Branch: dev_2.3.2_iomodule

--- client/src/ui/widgets/iomodule/iostate_display_widget.cpp
+++ client/src/ui/widgets/iomodule/iostate_display_widget.cpp
@@ -1,6 +1,7 @@
 #include "iostate_display_widget.h"
 #include "camera/iomodule/iomodule_monitor.h"
 #include "ui_iostate_display_widget.h"
+#include "ui/style/skin.h"
 
 namespace
 {
@@ -36,27 +37,113 @@ void QnIOStateDisplayWidget::at_cameraPropertyChanged(const QnResourcePtr & /*re
 
 void QnIOStateDisplayWidget::updateControls()
 {
+
     auto ioSettings = m_camera->getIOPorts();
+
+    //QGridLayout* mainLayout = new QGridLayout(this);
+    //setLayout(mainLayout);
+
+    QFont labelFont = font();
+    labelFont.setBold(true);
+
+    int row = 0;
+    for (const QnIOPortData& value: ioSettings)
+    {
+        if (value.portType != Qn::PT_Input)
+            continue;
+
+
+        QLabel* idLabel = new QLabel(this);
+        idLabel->setText(value.id);
+        idLabel->setFont(labelFont);
+        idLabel->setStyleSheet(lit("QLabel { color : #80ffffff; }"));
+        ui->gridLayout->addWidget(idLabel, row, 0);
+
+        QLabel* inputStateLabel = new QLabel(this);
+        QPixmap pixmap = qnSkin->pixmap("item/io_indicator_on.png");
+        inputStateLabel->setPixmap(pixmap);
+        inputStateLabel->setFixedSize(pixmap.size());
+        ui->gridLayout->addWidget(inputStateLabel, row, 1);
+
+        QLabel* inputTextLabel = new QLabel(this);
+        inputTextLabel->setText(value.inputName);
+        inputTextLabel->setFont(labelFont);
+        ui->gridLayout->addWidget(inputTextLabel, row, 2);
+
+        QLabel* spacerLabel = new QLabel(this);
+        spacerLabel->setFixedSize(32, 2);
+        ui->gridLayout->addWidget(spacerLabel, row, 3);
+
+        row++;
+    }
+
+    row = 0;
+    for (const QnIOPortData& value: ioSettings)
+    {
+        if (value.portType != Qn::PT_Output)
+            continue;
+
+        if (row >= ui->gridLayout->rowCount()) {
+            QLabel* spacerLabel = new QLabel(this);
+            spacerLabel->setFixedSize(32, 2);
+            ui->gridLayout->addWidget(spacerLabel, row, 3);
+        }
+
+        QLabel* idLabel = new QLabel(this);
+        idLabel->setText(value.id);
+        idLabel->setFont(labelFont);
+        idLabel->setStyleSheet(lit("QLabel { color : #80ffffff; }"));
+        ui->gridLayout->addWidget(idLabel, row, 4);
+
+        QLabel* spacerLabel = new QLabel(this);
+        spacerLabel->setFixedSize(8, 2);
+        ui->gridLayout->addWidget(spacerLabel, row, 5);
+
+        QPushButton* button = new QPushButton(this);
+        button->setText(value.outputName);
+        ui->gridLayout->addWidget(button, row, 6);
+
+        row++;
+    }
+
+    /*
     for (const QnIOPortData& value: ioSettings) 
     {
         if (value.portType == Qn::PT_Disabled)
             continue;
 
-        QHBoxLayout* layout = new QHBoxLayout(this);
+        //QHBoxLayout* layout = new QHBoxLayout(this);
+        //layout->setContentsMargins(0.0, 0.0, 0.0, 0.0);
+        //layout->setSpacing(6.0);
+
+        QFont idLabelFont = font();
+        idLabelFont.setBold(true);
 
         QLabel* idLabel = new QLabel(this);
         idLabel->setText(value.id);
+        idLabel->setFont(idLabelFont);
+        idLabel->setStyleSheet(lit("QLabel { color : #80ffffff; }"));
         layout->addWidget(idLabel);
 
-        if (value.portType == Qn::PT_Input) {
+        if (value.portType == Qn::PT_Input) 
+        {
+            QFont labelFont = font();
+            labelFont.setBold(true);
+
             QLabel* inputStateLabel = new QLabel(this);
-            //inputStateLabel->setPixmap("io_off");
+            inputStateLabel->setFont(labelFont);
+            QPixmap pixmap = qnSkin->pixmap("item/io_indicator_on.png");
+            inputStateLabel->setPixmap(pixmap);
+            inputStateLabel->setFixedSize(pixmap.size());
+            layout->addWidget(inputStateLabel);
+
             QLabel* inputTextLabel = new QLabel(this);
             inputTextLabel->setText(value.inputName);
+            inputTextLabel->setFont(labelFont);
             layout->addWidget(inputTextLabel);
             
-            QSpacerItem* spacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-            layout->addSpacerItem(spacer);
+            //QSpacerItem* spacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+            //layout->addSpacerItem(spacer);
 
             ui->inputLayout->addLayout(layout);
         }
@@ -65,15 +152,16 @@ void QnIOStateDisplayWidget::updateControls()
             button->setText(value.outputName);
             layout->addWidget(button);
 
-            QSpacerItem* spacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-            layout->addSpacerItem(spacer);
+            //QSpacerItem* spacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+            //layout->addSpacerItem(spacer);
 
             ui->outputLayout->addLayout(layout);
         }
 
-        QSpacerItem* spacer1 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
-        ui->verticalLayout->addSpacerItem(spacer1);
+        //QSpacerItem* spacer1 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
+        //ui->verticalLayout->addSpacerItem(spacer1);
     }
+    */
 
     openConnection();
 }
