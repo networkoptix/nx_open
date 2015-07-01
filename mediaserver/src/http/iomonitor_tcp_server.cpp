c9e38b32bec02063f9c979561095ba90c79ed996
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-07-01 10:59:53 +0300

    Task #5825 - Create IO monitor - server method
    <==== Mercurial data ====>
    Branch: dev_2.3.2_iomodule

--- mediaserver/src/http/iomonitor_tcp_server.cpp
+++ mediaserver/src/http/iomonitor_tcp_server.cpp
@@ -8,10 +8,12 @@
 #include "core/resource_management/resource_pool.h"
 #include "utils/serialization/json.h"
 #include <utils/common/model_functions.h>
+#include "common/common_module.h"
 
 class QnIOMonitorConnectionProcessorPrivate: public QnTCPConnectionProcessorPrivate
 {
 public:
+    QMutex dataMutex;
     QMutex waitMutex;
     QWaitCondition waitCond;
     QnIOStateDataList dataToSend;
@@ -46,38 +48,52 @@ void QnIOMonitorConnectionProcessor::run()
             sendResponse(CODE_NOT_FOUND, "multipart/x-mixed-replace; boundary=ioboundary");
             return;
         }
-        
-        sendResponse(CODE_OK, "multipart/x-mixed-replace; boundary=ioboundary");
+        connect(camera.data(), &QnSecurityCamResource::parentIdChanged, this, [this]() {pleaseStop(); }, Qt::DirectConnection);
+        connect(camera.data(), &QnSecurityCamResource::initializedChanged, this, &QnIOMonitorConnectionProcessor::at_cameraInitDone, Qt::DirectConnection);
+        connect(camera.data(), &QnSecurityCamResource::cameraInput, this, &QnIOMonitorConnectionProcessor::at_cameraIOStateChanged, Qt::DirectConnection);
 
-        connect(camera.data(), &QnSecurityCamResource::cameraInput, this, &QnIOMonitorConnectionProcessor::at_cameraInput, Qt::DirectConnection);
-        connect(camera.data(), &QnSecurityCamResource::cameraOutput, this, &QnIOMonitorConnectionProcessor::at_cameraOutput, Qt::DirectConnection);
+        if (camera->getParentId() != qnCommon->moduleGUID()) {
+            sendResponse(CODE_NOT_FOUND, "multipart/x-mixed-replace; boundary=ioboundary");
+            return;
+        }
+        else {
+            sendResponse(CODE_OK, "multipart/x-mixed-replace; boundary=ioboundary");
+        }
 
-        static const int KEEPALIVE_INTERVAL = 1000 * 5;
-
-        using namespace std::placeholders;
         static const int REQUEST_BUFFER_SIZE = 1024;
+        static const int KEEPALIVE_INTERVAL = 1000 * 5;
         d->requestBuffer.reserve(REQUEST_BUFFER_SIZE);
         d->socket->setRecvTimeout(KEEPALIVE_INTERVAL);
+
+        using namespace std::placeholders;
         d->socket->readSomeAsync( &d->requestBuffer, std::bind( &QnIOMonitorConnectionProcessor::onSomeBytesReadAsync, this, d->socket.data(), _1, _2 ) );
 
-        auto iostates = camera->ioStates();
+        setData(camera->ioStates());
         QMutexLocker lock(&d->waitMutex);
-        d->dataToSend = std::move(iostates);
-        sendMultipartData(lock);
         while (d->socket->isConnected()) 
         {
+            sendMultipartData();
             d->waitCond.wait(&d->waitMutex);
-            if (d->socket->isConnected())
-                sendMultipartData(lock);
         }
+        lock.unlock();
         d->socket->terminateAsyncIO(true);
+    }
+}
+
+void QnIOMonitorConnectionProcessor::at_cameraInitDone(const QnResourcePtr &resource)
+{
+    Q_D(QnIOMonitorConnectionProcessor);
+    QnSecurityCamResourcePtr camera = resource.dynamicCast<QnSecurityCamResource>();
+    if (camera && camera->isInitialized()) {
+        setData(camera->ioStates());
+        d->waitCond.wakeAll();
     }
 }
 
 void QnIOMonitorConnectionProcessor::onSomeBytesReadAsync( AbstractSocket* sock, SystemError::ErrorCode errorCode, size_t bytesRead )
 {
     Q_D(QnIOMonitorConnectionProcessor);
-    QMutexLocker lock(&d->waitMutex);
+    QMutexLocker lock(&d->waitMutex); // just in case to prevent socket simultaneous calls while send / read async
 
     using namespace std::placeholders;
     d->requestBuffer.resize(0);
@@ -86,38 +102,40 @@ void QnIOMonitorConnectionProcessor::onSomeBytesReadAsync( AbstractSocket* sock,
     d->waitCond.wakeAll();
 }
 
-void QnIOMonitorConnectionProcessor::at_cameraInput(
+void QnIOMonitorConnectionProcessor::at_cameraIOStateChanged(
     const QnResourcePtr& resource,
     const QString& inputPortID,
     bool value,
     qint64 timestamp )
 {
     Q_D(QnIOMonitorConnectionProcessor);
-    QMutexLocker lock(&d->waitMutex);
-    d->dataToSend.push_back(QnIOStateData(inputPortID, value, timestamp));
+    addData(QnIOStateData(inputPortID, value, timestamp));
     d->waitCond.wakeAll();
 }
 
-void QnIOMonitorConnectionProcessor::at_cameraOutput(
-    const QnResourcePtr& resource,
-    const QString& inputPortID,
-    bool value,
-    qint64 timestamp )
+void QnIOMonitorConnectionProcessor::sendMultipartData()
 {
     Q_D(QnIOMonitorConnectionProcessor);
-    QMutexLocker lock(&d->waitMutex);
-    d->dataToSend.push_back(QnIOStateData(inputPortID, value, timestamp));
-    d->waitCond.wakeAll();
-}
-
-void QnIOMonitorConnectionProcessor::sendMultipartData(QMutexLocker& lock)
-{
-    Q_D(QnIOMonitorConnectionProcessor);
-    d->response.messageBody = QJson::serialized<QnIOStateDataList>(d->dataToSend);
-    d->dataToSend.clear();
-    lock.unlock();
+    {
+        QMutexLocker lock(&d->dataMutex);
+        d->response.messageBody = QJson::serialized<QnIOStateDataList>(d->dataToSend);
+        d->dataToSend.clear();
+    }
     sendResponse(CODE_OK, "application/json", QByteArray(), "--ioboundary");
-    lock.relock();
+}
+
+void QnIOMonitorConnectionProcessor::setData(QnIOStateDataList&& value)
+{
+    Q_D(QnIOMonitorConnectionProcessor);
+    QMutexLocker lock(&d->dataMutex);
+    d->dataToSend = value;
+}
+
+void QnIOMonitorConnectionProcessor::addData(QnIOStateData&& value)
+{
+    Q_D(QnIOMonitorConnectionProcessor);
+    QMutexLocker lock(&d->dataMutex);
+    d->dataToSend.push_back(value);
 }
 
 void QnIOMonitorConnectionProcessor::pleaseStop()
--- mediaserver/src/http/iomonitor_tcp_server.h
+++ mediaserver/src/http/iomonitor_tcp_server.h
@@ -3,6 +3,7 @@
 
 #include "utils/network/tcp_connection_processor.h"
 #include "core/resource/resource_fwd.h"
+#include "api/model/api_ioport_data.h"
 
 class QnIOMonitorConnectionProcessorPrivate;
 
@@ -16,23 +17,20 @@ protected:
     virtual void run() override;
     virtual void pleaseStop() override;
 private slots:
-    void at_cameraInput(
+    void at_cameraIOStateChanged(
         const QnResourcePtr& resource,
         const QString& inputPortID,
         bool value,
         qint64 timestamp );
-
-    void at_cameraOutput(
-        const QnResourcePtr& resource,
-        const QString& inputPortID,
-        bool value,
-        qint64 timestamp );
+    void at_cameraInitDone(const QnResourcePtr &resource);
 private:
     void onSomeBytesReadAsync( AbstractSocket* sock, SystemError::ErrorCode errorCode, size_t bytesRead );
+    void setData(QnIOStateDataList&& value);
+    void addData(QnIOStateData&& value);
 private:
     Q_DECLARE_PRIVATE(QnIOMonitorConnectionProcessor);
 
-    void sendMultipartData(QMutexLocker& lock);
+    void sendMultipartData();
 };
 
 #endif // QN_IOMONITOR_REST_HANDLER_H
