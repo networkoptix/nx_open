b3d70a71517eb193c238df62f299ffe90f0ba725
Denis Klychkov <dklychkov@networkoptix.com>
2015-07-02 13:05:46 +0300

    Add comment to chunk painter (refs #5812)
    <==== Mercurial data ====>
    Branch: mobile_client_1

--- mobile_client/src/ui/timeline/timeline_chunk_painter.cpp
+++ mobile_client/src/ui/timeline/timeline_chunk_painter.cpp
@@ -30,7 +30,13 @@ void QnTimelineChunkPainter::start(qint64 startPos, qint64 centralPos, const QRe
     m_centralPosition = centralPos;
     m_centralCoordinate = xFromValue(m_centralPosition);
     m_minChunkLength = (windowEnd - windowStart) / rect.width();
-    m_geometry->allocate((chunkCount + 2) * 6);
+    int rectCount = chunkCount + 2;
+    /* +2 is for the situations like this:
+     *            chunk
+     *             \/
+     * [  +1   +++++++++++   +1  ]
+     */
+    m_geometry->allocate(rectCount * 6);
     m_points = m_geometry->vertexDataAsColoredPoint2D();
     m_index = 0;
 
