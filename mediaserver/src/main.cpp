160ec0511b2f36d3e92b26d85c1bbaedfe2e4d63
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-07-14 19:31:15 +0300

    refs #6014 added API to give plugin access to mediaserver settings
    <==== Mercurial data ====>
    Branch: dev_2.3.1

--- mediaserver/src/main.cpp
+++ mediaserver/src/main.cpp
@@ -1616,7 +1616,7 @@ void QnMain::run()
     QnMServerResourceSearcher::initStaticInstance( new QnMServerResourceSearcher() );
 
     //Initializing plugin manager
-    PluginManager::instance()->loadPlugins();
+    PluginManager::instance()->loadPlugins( MSSettings::roSettings() );
 
     if (needToStop())
         return;
