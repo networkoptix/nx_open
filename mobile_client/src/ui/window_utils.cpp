489f3dcf17ce13f0f2474522b75bd30a0adbd388
Denis Klychkov <dklychkov@networkoptix.com>
2015-07-20 17:59:21 +0300

    MOBILE-5: Compilation fix for desktop
    <==== Mercurial data ====>
    Branch: mobile_client_1

---
+++ mobile_client/src/ui/window_utils.cpp
@@ -0,0 +1,23 @@
+#include "window_utils.h"
+
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
+
+void prepareWindow() {}
+
+void hideSystemUi() {}
+
+void showSystemUi() {}
+
+int statusBarHeight() {
+    return 0;
+}
+
+int navigationBarHeight() {
+    return 0;
+}
+
+bool isTablet() {
+    return false;
+}
+
+#endif
