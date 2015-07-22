d6f0fb929354b3194bc2077b8b83a1b39849490f
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2015-07-22 09:20:32 -0700

    Fixes Bug #6058: NXTool should be built only for default. Added conditions to the installer
    <==== Mercurial data ====>
    Branch: dev_2.3.2

--- wixsetup/maven/filter-resources/wix-compile.py
+++ wixsetup/maven/filter-resources/wix-compile.py
@@ -18,21 +18,27 @@ commands = [\
 'cscript FixExitDialog.js %s/%s' % (client_msi_folder, client_msi_name),\
 'candle -ext WixBalExtension -dinstalltype="server-only" -dDbSync22SourceDir="${DbSync22Dir}" -dClientHelpSourceDir="${ClientHelpSourceDir}" -dClientVoxSourceDir="${ClientVoxSourceDir}" -dClientBgSourceDir="${ClientBgSourceDir}" -arch ${arch} -out obj\\${build.configuration}-server-only\\ -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll UninstallOptionsDlg.wxs Associations.wxs MyExitDialog.wxs PasswordShouldMatchDlg.wxs UpgradeDlg.wxs EmptyPasswordDlg.wxs AllFieldsAreMandatoryDlg.wxs InstallTypeDlg.wxs PortDuplicateDlg.wxs PortIsBusyDlg.wxs InvalidPasswordDlg.wxs MediaServerDlg.wxs ClientDlg.wxs SelectionWarning.wxs Product-server-only.wxs ClientHelp.wxs ClientVox.wxs ClientBg.wxs Client.wxs Server.wxs traytool.wxs DbSync22Files.wxs',\
 'light -ext WixBalExtension -cultures:${installer.language} -cc ${project.build.directory} -reusecab -loc CustomStrings_${installer.language}.wxl -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll -out %s/%s -pdbout bin\\${build.configuration}\\EVEMediaPlayerSetup.wixpdb obj\\${build.configuration}-server-only\\*.wixobj' % (server_msi_folder, server_msi_name),\
-'cscript FixExitDialog.js %s/%s' % (server_msi_folder, server_msi_name),\
-'candle -dinstalltype="client-only" -dDbSync22SourceDir="${DbSync22Dir}" -dClientHelpSourceDir="${ClientHelpSourceDir}" -dClientVoxSourceDir="${ClientVoxSourceDir}" -dClientBgSourceDir="${ClientBgSourceDir}" -arch ${arch} -out obj\\${build.configuration}-nxtool\\ -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll MyExitDialog.wxs UpgradeDlg.wxs NxtoolDlg.wxs SelectionWarning.wxs Product-nxtool.wxs Nxtool.wxs',\
-'light -cultures:${installer.language} -cc ${project.build.directory} -reusecab -loc CustomStrings_${installer.language}.wxl -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll -out %s/%s -pdbout bin\\${build.configuration}\\EVEMediaPlayerSetup.wixpdb obj\\${build.configuration}-nxtool\\*.wixobj' % (nxtool_msi_folder, nxtool_msi_name),\
-'cscript FixExitDialog.js %s/%s' % (nxtool_msi_folder, nxtool_msi_name)
+'cscript FixExitDialog.js %s/%s' % (server_msi_folder, server_msi_name)
 ]
+if '${nxtool}' == 'true':
+    commands += [\
+    'candle -dinstalltype="client-only" -dDbSync22SourceDir="${DbSync22Dir}" -dClientHelpSourceDir="${ClientHelpSourceDir}" -dClientVoxSourceDir="${ClientVoxSourceDir}" -dClientBgSourceDir="${ClientBgSourceDir}" -arch ${arch} -out obj\\${build.configuration}-nxtool\\ -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll MyExitDialog.wxs UpgradeDlg.wxs NxtoolDlg.wxs SelectionWarning.wxs Product-nxtool.wxs Nxtool.wxs',\
+    'light -cultures:${installer.language} -cc ${project.build.directory} -reusecab -loc CustomStrings_${installer.language}.wxl -ext WixFirewallExtension.dll -ext WixUIExtension.dll -ext WixUtilExtension.dll -ext wixext\WixSystemToolsExtension.dll -out %s/%s -pdbout bin\\${build.configuration}\\EVEMediaPlayerSetup.wixpdb obj\\${build.configuration}-nxtool\\*.wixobj' % (nxtool_msi_folder, nxtool_msi_name),\
+    'cscript FixExitDialog.js %s/%s' % (nxtool_msi_folder, nxtool_msi_name)
+    ]
 
 for command in commands:
-    p = subprocess.Popen(command, shell=True, stdout=PIPE)
-    out, err = p.communicate()
-    print ('\n++++++++++++++++++++++%s++++++++++++++++++++++' % command)
-    print out
-    p.wait()
-    if p.returncode != 0 and p.returncode != 204:  
-        print "failed with code: %s" % str(p.returncode) 
-        sys.exit(1)
+    print command
+    
+#for command in commands:
+  #  p = subprocess.Popen(command, shell=True, stdout=PIPE)
+  #  out, err = p.communicate()
+ #   print ('\n++++++++++++++++++++++%s++++++++++++++++++++++' % command)
+#    print out
+#    p.wait()
+#    if p.returncode != 0 and p.returncode != 204:  
+#        print "failed with code: %s" % str(p.returncode) 
+#        sys.exit(1)
 
 client_msi_product_code = subprocess.check_output('cscript //NoLogo productcode.js %s\\%s' % (client_msi_folder, client_msi_name)).strip()
 server_msi_product_code = subprocess.check_output('cscript //NoLogo productcode.js %s\\%s' % (server_msi_folder, server_msi_name)).strip()
