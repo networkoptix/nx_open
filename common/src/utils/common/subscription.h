bbdcf1ef39ed37d7378fe64efa1f4798f8bda9ff
Mikhail Uskov <muskov@networkoptix.com>
2015-07-30 14:01:06 +0300

    common/utils/common: Subscription variadic comment
    <==== Mercurial data ====>
    Branch: nat_traversal

--- common/src/utils/common/subscription.h
+++ common/src/utils/common/subscription.h
@@ -7,12 +7,12 @@
 #include <list>
 
 /** Subscribtion-notification model event */
-template <typename ... Data>
+template <typename /*...*/ Data>  // TODO: uncomment dots when variadic templates are avaliable
 class Subscription
 {
 public:
     /** Notification callback */
-    typedef std::function<void(Data ...)> Handler;
+    typedef std::function<void(Data /*...*/)> Handler;
 
     /** Subscribes @param hadler for this event.
      *  NOTE: do not use blocking operations inside @param hadler */
@@ -28,11 +28,11 @@ public:
     }
 
     /** Notifies all subscribers about the event */
-    void notify(Data ... data)
+    void notify(Data /*...*/ data)
     {
         std::lock_guard<std::mutex> g(m_mutex);
         for (const auto& handler : m_handlers)
-            handler(data ...);
+            handler(data /*...*/);
     }
 
 private:
