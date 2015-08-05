60c1c257668a7eead4fa556e982a9ed91b1ca8c7
Alexander Kulikov <akulikov@networkoptix.com>
2015-08-05 19:06:02 +0300

    vms-327. Windows part work in progress
    <==== Mercurial data ====>
    Branch: feature_5703_storage_2

--- nx_storage_sdk/build_sdk.bat
+++ nx_storage_sdk/build_sdk.bat
@@ -7,30 +7,39 @@ rmdir /S /Q %TARGET_DIR%
 mkdir %TARGET_DIR%
 mkdir %TARGET_DIR%\include\
 mkdir %TARGET_DIR%\include\plugins\
+mkdir %TARGET_DIR%\include\plugins\storage\
+mkdir %TARGET_DIR%\include\plugins\storage\third_party
 mkdir %TARGET_DIR%\sample\
 mkdir %TARGET_DIR%\sample\ftpstorageplugin\
+mkdir %TARGET_DIR%\sample\ftpstorageplugin\vs2012
+mkdir %TARGET_DIR%\sample\ftpstorageplugin\vs2012\ftpstorageplugin
+
 copy /Y readme.txt %TARGET_DIR%\
 
 @rem Copying integration headers
 copy /Y ..\common\src\plugins\plugin_api.h %TARGET_DIR%\include\plugins\
-copy /Y ..\common\src\plugins\storage\third_party\third_party_storage.h %TARGET_DIR%\include\
+copy /Y ..\common\src\plugins\storage\third_party\third_party_storage.h %TARGET_DIR%\include\plugins\storage\third_party\
 
 @rem Copying Ftp Storage plugin
 set PLUGIN_NAME=ftpstorageplugin
 
-xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\*.pro %TARGET_DIR%\sample\%PLUGIN_NAME%\
-mkdir %TARGET_DIR%\sample\%PLUGIN_NAME%\
-xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\*.pro %TARGET_DIR%\sample\%PLUGIN_NAME%\
 mkdir %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
 mkdir %TARGET_DIR%\sample\%PLUGIN_NAME%\src\impl
-xcopy /Y ..\plugins\%PLUGIN_NAME%\src\*.h %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
-xcopy /Y ..\plugins\%PLUGIN_NAME%\src\*.cpp %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
-xcopy /Y ..\plugins\%PLUGIN_NAME%\src\impl*.h %TARGET_DIR%\sample\%PLUGIN_NAME%\src\impl
-xcopy /Y ..\plugins\%PLUGIN_NAME%\src\impl*.cpp %TARGET_DIR%\sample\%PLUGIN_NAME%\src\impl
-copy /Y ..\plugins\%PLUGIN_NAME%\Doxyfile %TARGET_DIR%\sample\%PLUGIN_NAME%\Doxyfile
 
-@rem Removing unnecessary source files
-del /Q /F %TARGET_DIR%\sample\%PLUGIN_NAME%\src\compatibility_info.cpp
+copy /Y ..\storage_plugins\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.sln_ %TARGET_DIR%\sample\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.sln
+copy /Y ..\storage_plugins\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.vcxproj_ %TARGET_DIR%\sample\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.vcxproj
+copy /Y ..\storage_plugins\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.vcxproj.filters_ %TARGET_DIR%\sample\%PLUGIN_NAME%\vs2012\%PLUGIN_NAME%\%PLUGIN_NAME%.vcxproj.filters
+
+xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\src\CMakeLists.txt %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
+
+xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\src\ftp_library.h %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
+xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\src\ftp_library.cpp %TARGET_DIR%\sample\%PLUGIN_NAME%\src\
+
+xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\src\impl\ftplib.h %TARGET_DIR%\sample\%PLUGIN_NAME%\src\impl
+xcopy /Y ..\storage_plugins\%PLUGIN_NAME%\src\impl\ftplib.cpp %TARGET_DIR%\sample\%PLUGIN_NAME%\src\impl
+
+copy /Y ..\storage_plugins\%PLUGIN_NAME%\Doxyfile %TARGET_DIR%\sample\%PLUGIN_NAME%\Doxyfile
+
 
 @rem Generating documentation
 set CUR_DIR_BAK=%~dp0
