3b98947d21463e474b9d98aa1d42833b5b57657e
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2015-08-10 16:45:28 -0700

    NX1 OS is upgraded when 1-click upgrade is started
    <==== Mercurial data ====>
    Branch: dev_2.3.2_nx1upgrade

--- edge_firmware/rpi/maven/filter-resources/install.sh
+++ edge_firmware/rpi/maven/filter-resources/install.sh
@@ -9,9 +9,12 @@ fi
 DISTRIB=$COMPANY_NAME-mediaserver-${box}-${release.version}.${buildNumber}$BETA
 
 update () {
-    /etc/init.d/$COMPANY_NAME-mediaserver stop
-    tar xfv $DISTRIB.tar.gz --exclude='./opt/${deb.customization.company.name}/mediaserver/etc/mediaserver.conf' -C /
-    /etc/init.d/$COMPANY_NAME-mediaserver start
+  /etc/init.d/cron stop
+  /etc/init.d/$COMPANY_NAME-mediaserver stop
+  tar xfv $DISTRIB.tar.gz --exclude='./opt/${deb.customization.company.name}/mediaserver/etc/mediaserver.conf' -C /
+  if [[ "${box}" == "bpi" ]]; then /etc/init.d/upgrade; fi
+  /etc/init.d/$COMPANY_NAME-mediaserver start
+  /etc/init.d/cron start
 }
 
 if [ "$1" != "" ]
