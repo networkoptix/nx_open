616d2ac4daed77c056d4483492f610350ea703ad
Mikhail Uskov <muskov@networkoptix.com>
2015-08-11 15:09:25 +0300

    VMS-323 replace ApiClientInfoData::operator == with QJson::serialized ==
    <==== Mercurial data ====>
    Branch: dev_2.4.0

--- common/src/nx_ec/data/api_client_info_data.cpp
+++ common/src/nx_ec/data/api_client_info_data.cpp
@@ -4,19 +4,6 @@
 namespace ec2 {
 
     QN_FUSION_ADAPT_STRUCT_FUNCTIONS_FOR_TYPES((ApiClientInfoData), \
-        (ubjson)(xml)(json)(sql_record)(csv_record), _Fields)
-
-	bool ApiClientInfoData::operator == (const ApiClientInfoData& rhs) const
-	{
-        return id == rhs.id &&
-            skin == rhs.skin &&
-            systemInfo == rhs.systemInfo &&
-            cpuArchitecture == rhs.cpuArchitecture &&
-            cpuModelName == rhs.cpuModelName &&
-            phisicalMemory == rhs.phisicalMemory &&
-            openGLVersion == rhs.openGLVersion &&
-            openGLVendor == rhs.openGLVendor &&
-            openGLRenderer == rhs.openGLRenderer;
-	}
+        (ubjson)(xml)(json)(sql_record)(csv_record), Fields)
 
 } // namespace ec2
