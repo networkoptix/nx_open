1300e6d9d0151f34de742d237e8e7f50ddda211d
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-08-19 19:37:53 +0300

    VMS-497 - add client form form bRule parameters
    <==== Mercurial data ====>
    Branch: external_event_2.4.1

--- client/src/ui/widgets/business/business_event_widget_factory.cpp
+++ client/src/ui/widgets/business/business_event_widget_factory.cpp
@@ -2,6 +2,7 @@
 
 #include <ui/widgets/business/empty_business_event_widget.h>
 #include <ui/widgets/business/camera_input_business_event_widget.h>
+#include <ui/widgets/business/custom_business_event_widget.h>
 
 QnAbstractBusinessParamsWidget* QnBusinessEventWidgetFactory::createWidget(QnBusiness::EventType eventType, QWidget *parent,
                                                                            QnWorkbenchContext *context) {
@@ -9,6 +10,9 @@ QnAbstractBusinessParamsWidget* QnBusinessEventWidgetFactory::createWidget(QnBus
     switch (eventType) {
     case QnBusiness::CameraInputEvent:
         return new QnCameraInputBusinessEventWidget(parent);
+    case QnBusiness::CustomInstantEvent:
+    case QnBusiness::CustomProlongedEvent:
+        return new QnCustomBusinessEventWidget(parent);
     default:
         return new QnEmptyBusinessEventWidget(parent);
     }
