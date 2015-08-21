ba06f28dc674525da9dc77aab33172a3faf6675c
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-08-21 16:44:41 +0300

    #VMS-528 one more test for HTTP Via header
    <==== Mercurial data ====>
    Branch: dev_2.4.0

--- unit_tests/common_ut/src/utils/network/http/http_types_ut.cpp
+++ unit_tests/common_ut/src/utils/network/http/http_types_ut.cpp
@@ -311,6 +311,12 @@ TEST( HttpHeaderTest, Via_parse )
     EXPECT_EQ( via.entries[0].protoVersion, QByteArray( "1.1" ) );
     EXPECT_EQ( via.entries[0].receivedBy, QByteArray( "{47bf37a0-72a6-2890-b967-5da9c390d28a}" ) );
     EXPECT_TRUE( via.entries[0].comment.isEmpty() );
+
+
+
+    via.entries.clear();
+    EXPECT_TRUE( via.parse( " " ) );
+    EXPECT_EQ( via.entries.size(), 0 );
 }
 
 TEST( HttpHeaderTest, Via_toString )
