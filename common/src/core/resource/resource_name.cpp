5eba48397f62b82145e7f1d9a8ba840ea8eae544
Sergey Ivanov <sivanov@networkoptix.com>
2015-08-31 16:48:05 +0300

    VMS-632: Update all "camera" strings to IO MOdule where possible
    Created generic functions with translation logic
    
    <==== Mercurial data ====>
    Branch: x_VMS-632_device_strings

--- common/src/core/resource/resource_name.cpp
+++ common/src/core/resource/resource_name.cpp
@@ -7,6 +7,43 @@
 #include <core/resource/media_server_resource.h>
 #include <core/resource_management/resource_pool.h>
 
+namespace {
+
+    class QnResourceNameStrings {
+        Q_DECLARE_TR_FUNCTIONS(QnResourceNameStrings)
+    public:       
+        static QString cameras(bool capitalize, int count) { 
+            if (count == 0)
+                return capitalize   ? tr("Cameras")                     : tr("cameras"); 
+            if (count == 1)
+                return capitalize   ? tr("Camera")                      : tr("camera"); 
+            return capitalize       ? tr("%n Camera(s)", 0, count)      : tr("%n camera(s)", 0, count); 
+        }
+
+        static QString iomodules(bool capitalize, int count) {
+            if (count == 0)
+                return capitalize   ? tr("IO Modules")                  : tr("IO modules"); 
+            if (count == 1)
+                return capitalize   ? tr("IO Module")                   : tr("IO module");
+            return capitalize       ? tr("%n IO Module(s)", 0, count)   : tr("%n IO module(s)", 0, count); 
+        }
+
+        static QString devices(bool capitalize, int count) { 
+            if (count == 0)
+                return capitalize   ? tr("Devices")                     : tr("devices"); 
+            if (count == 1)
+                return capitalize   ? tr("Device")                      : tr("device");
+            return capitalize       ? tr("%n Device(s)", 0, count)      : tr("%n device(s)", 0, count); 
+        }
+
+        static QString selectDevice() {
+            return tr("Please select at least one device.");
+        }
+
+    };
+
+}
+
 QString extractHost(const QString &url) 
 {
     /* Don't go through QHostAddress/QUrl constructors as it is SLOW. 
@@ -34,4 +71,36 @@ QString getFullResourceName(const QnResourcePtr &resource, bool showIp) {
             return QString(QLatin1String("%1 (%2)")).arg(baseName).arg(host);
     }
     return baseName;
+}
+
+QString getDevicesName(const QnVirtualCameraResourceList &devices, bool capitalize /* = true*/) {
+
+    /* Quick check - if there are no io modules in the system at all. */
+    if (!qnResPool->containsIoModules())
+        return QnResourceNameStrings::cameras(capitalize, devices.size());
+
+    using boost::algorithm::any_of;
+
+    bool hasCameras = any_of(devices, [](const QnVirtualCameraResourcePtr &device) {
+        return device->hasVideo(nullptr);
+    });
+
+    bool hasIoModules = any_of(devices, [](const QnVirtualCameraResourcePtr &device) {
+        return device->isIOModule() && !device->hasVideo(nullptr);
+    });
+
+    /* Only one type of devices. */
+    if (hasCameras ^ hasIoModules) {
+        if (hasCameras)
+            return QnResourceNameStrings::cameras(capitalize, devices.size());
+        if (hasIoModules)
+            return QnResourceNameStrings::iomodules(capitalize, devices.size());
+    }
+
+    /* Both of them - or none. */
+    return QnResourceNameStrings::devices(capitalize, devices.size());
+}
+
+QString selectDevice() {
+    return QnResourceNameStrings::selectDevice();
 }
--- common/src/core/resource/resource_name.h
+++ common/src/core/resource/resource_name.h
@@ -1,5 +1,4 @@
-#ifndef RESOURCE_NAME_H
-#define RESOURCE_NAME_H
+#pragma once
 
 #include <QString>
 
@@ -9,4 +8,23 @@ QString extractHost(const QString &url);
 QString getFullResourceName(const QnResourcePtr& resource, bool showIp);
 inline QString getShortResourceName(const QnResourcePtr& resource) { return getFullResourceName(resource, false); }
 
-#endif // RESOURCE_NAME_H
+/**
+ * @brief Calculate common name for the target devices list.
+ * @details Following rules are applied:
+ * * If all devices are cameras - "%n Cameras";
+ * * If all devices are IO Modules - "%n IO Modules";
+ * * Supported single items - "Camera" or "IO Module";
+ * * For mixed or empty list "Devices" is used;
+ * @param capitalize Should the word begin from the capital letter, default value is true.
+ */
+QString getDevicesName(const QnVirtualCameraResourceList &devices = QnVirtualCameraResourceList(), bool capitalize = true);
+
+inline QString getDevicesNameUpper(const QnVirtualCameraResourceList &devices = QnVirtualCameraResourceList()) {
+    return getDevicesName(devices, true);
+}
+
+inline QString getDevicesNameLower(const QnVirtualCameraResourceList &devices = QnVirtualCameraResourceList()) {
+    return getDevicesName(devices, false);
+}
+
+QString selectDevice();
