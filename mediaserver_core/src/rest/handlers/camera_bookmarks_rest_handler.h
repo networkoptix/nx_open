aad9da296e2dd0635c2da956bf3b74682ba5319e
Sergey Ivanov <sivanov@networkoptix.com>
2015-09-23 17:30:04 +0200

    Cleanup unused code
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- mediaserver_core/src/rest/handlers/camera_bookmarks_rest_handler.h
+++ mediaserver_core/src/rest/handlers/camera_bookmarks_rest_handler.h
@@ -8,14 +8,12 @@
 class QnCameraBookmarksRestHandler: public QnJsonRestHandler {
     Q_OBJECT
 public:
-    virtual int executeGet(const QString &path, const QnRequestParams &params, QnJsonRestResult &result, const QnRestConnectionProcessor*) override;
     virtual int executePost(const QString &path, const QnRequestParams &params, const QByteArray &body, QnJsonRestResult &result, const QnRestConnectionProcessor*) override;
 
 private:
     int addCameraBookmarkAction(const QnRequestParams &params, const QByteArray &body, QnJsonRestResult &result);
     int updateCameraBookmarkAction(const QnRequestParams &params, const QByteArray &body, QnJsonRestResult &result);
     int deleteCameraBookmarkAction(const QnRequestParams &params, const QByteArray &body, QnJsonRestResult &result);
-    int getCameraBookmarksAction(const QnRequestParams & params, QnJsonRestResult &result);
 };
 
 #endif // QN_CAMERA_BOOKMARKS_REST_HANDLER_H
