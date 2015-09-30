c04e2a2ee21c55bb845b3864f55a267544513b1e
Sergey Ivanov <sivanov@networkoptix.com>
2015-09-30 16:21:48 +0300

    Test Fixture for the client unit tests
    <==== Mercurial data ====>
    Branch: dev_2.4.1_gui

--- unit_tests/client_ut/src/ui/workbench/workbench_access_controller_ut.cpp
+++ unit_tests/client_ut/src/ui/workbench/workbench_access_controller_ut.cpp
@@ -13,19 +13,53 @@
 
 #include <ui/style/skin.h>
 
+namespace {
+    const QString userName1 = QStringLiteral("unit_test_user_1");
+    const QString userName2 = QStringLiteral("unit_test_user_2");
+}
+
+class QnWorkbenchAccessControllerTest : public testing::Test {
+protected:  
+
+    // virtual void SetUp() will be called before each test is run.
+    virtual void SetUp() {
+        m_module.reset(new QnClientModule());
+        m_skin.reset(new QnSkin());
+        m_context.reset(new QnWorkbenchContext(qnResPool));
+    }
+
+    // virtual void TearDown() will be called after each test is run.
+    virtual void TearDown() {
+        m_context.clear();
+        m_skin.clear();
+        m_module.clear();
+    }
+
+    QnUserResourcePtr addUser(const QString &name, Qn::Permissions globalPermissions) {
+        QnUserResourcePtr user(new QnUserResource());
+        user->setId(QnUuid::createUuid());
+        user->setName(name);
+        user->setPermissions(globalPermissions);
+        qnResPool->addResource(user);
+
+        return user;
+    }
+
+
+
+    // Declares the variables your tests want to use.
+    QSharedPointer<QnClientModule> m_module;
+    QSharedPointer<QnSkin> m_skin;
+    QSharedPointer<QnWorkbenchContext> m_context;
+};
+
+
+
 /** Initial test. Check if empty helper is valid. */
-TEST( QnWorkbenchAccessControllerTest, init )
+TEST_F( QnWorkbenchAccessControllerTest, init )
 {
-    QScopedPointer<QnClientModule> module(new QnClientModule());
-    QScopedPointer<QnSkin> skin(new QnSkin());
-
-    QScopedPointer<QnWorkbenchContext> context(new QnWorkbenchContext(qnResPool));  
-    QnUserResourcePtr user(new QnUserResource());
-    user->setId(QnUuid::createUuid());
-    user->setName("ut");
-    qnResPool->addResource(user);
-
-    context->setUserName("ut");
+    auto user = addUser(userName1, Qn::GlobalOwnerPermissions);  
+    m_context->setUserName(userName1);
     
-    ASSERT_EQ(user, context->user());
+    ASSERT_EQ(user, m_context->user());
 }
