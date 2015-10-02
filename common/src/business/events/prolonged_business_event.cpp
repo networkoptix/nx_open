826f8106d664422064ec4d983f764a5461953c8a
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-10-02 16:29:09 +0300

    (VMS-1048) - Long action may not start on Generic Event
    <==== Mercurial data ====>
    Branch: dev_2.4.1

--- common/src/business/events/prolonged_business_event.cpp
+++ common/src/business/events/prolonged_business_event.cpp
@@ -1,4 +1,5 @@
 #include "prolonged_business_event.h"
+#include <business/actions/abstract_business_action.h>
 
 QnProlongedBusinessEvent::QnProlongedBusinessEvent(QnBusiness::EventType eventType, const QnResourcePtr& resource, QnBusiness::EventState toggleState, qint64 timeStamp):
     base_type(eventType, resource, toggleState, timeStamp)
@@ -6,7 +7,7 @@ QnProlongedBusinessEvent::QnProlongedBusinessEvent(QnBusiness::EventType eventTy
     Q_ASSERT(QnBusiness::hasToggleState(eventType));
 }
 
-bool QnProlongedBusinessEvent::checkCondition(QnBusiness::EventState state, const QnBusinessEventParameters &params) const {
+bool QnProlongedBusinessEvent::checkCondition(QnBusiness::EventState state, const QnBusinessEventParameters &params, QnBusiness::ActionType actionType) const {
     Q_UNUSED(params)
-    return state == QnBusiness::UndefinedState || state == getToggleState();
+    return state == QnBusiness::UndefinedState || state == getToggleState() || QnBusiness::hasToggleState(actionType);
 }
