a7f4eccce41b422e35d8f9b4f08e69f322469d45
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-10-05 13:21:21 +0300

    #CLOUD-103 cdb request name moved to constant shared between cdb and cdb_client
    <==== Mercurial data ====>
    Branch: nat_traversal

---
+++ nx_cloud/cloud_db_client/src/cdb_request_path.cpp
@@ -0,0 +1,27 @@
+/**********************************************************
+* Oct 5, 2015
+* akolesnikov
+***********************************************************/
+
+#include "cdb_request_path.h"
+
+
+namespace nx {
+namespace cdb {
+
+const char* ACCOUNT_REGISTER_PATH = "/account/register";
+const char* ACCOUNT_ACTIVATE_PATH = "/account/activate";
+const char* ACCOUNT_GET_PATH = "/account/get";
+
+const char* SYSTEM_BIND_PATH = "/system/bind";
+const char* SYSTEM_UNBIND_PATH = "/system/unbind";
+const char* SYSTEM_GET_PATH = "/system/get";
+const char* SYSTEM_SHARE_PATH = "/system/share";
+
+const char* AUTH_GET_NONCE_PATH = "/auth/get_nonce";
+const char* AUTH_GET_AUTHENTICATION_PATH = "/auth/get_authentication";
+
+const char* PING_PATH = "/ping";
+
+}
+}
---
+++ nx_cloud/cloud_db_client/src/cdb_request_path.h
@@ -0,0 +1,30 @@
+/**********************************************************
+* Oct 5, 2015
+* akolesnikov
+***********************************************************/
+
+#ifndef NX_CDB_CL_REQUEST_PATH_H
+#define NX_CDB_CL_REQUEST_PATH_H
+
+
+namespace nx {
+namespace cdb {
+
+extern const char* ACCOUNT_REGISTER_PATH;
+extern const char* ACCOUNT_ACTIVATE_PATH;
+extern const char* ACCOUNT_GET_PATH;
+
+extern const char* SYSTEM_BIND_PATH;
+extern const char* SYSTEM_UNBIND_PATH;
+extern const char* SYSTEM_GET_PATH;
+extern const char* SYSTEM_SHARE_PATH;
+
+extern const char* AUTH_GET_NONCE_PATH;
+extern const char* AUTH_GET_AUTHENTICATION_PATH;
+
+extern const char* PING_PATH;
+
+}
+}
+
+#endif //NX_CDB_CL_REQUEST_PATH_H
