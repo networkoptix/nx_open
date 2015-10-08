c95e1e62a665efd83454c2ea9a13ffd71f858761
Evgeny Balashov <ebalashov@networkoptix.com>
2015-10-08 16:30:42 +0300

    #CLOUD-115 empty front-end project
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/cloud/api/models.py
+++ cloud_portal/cloud/api/models.py
@@ -1,3 +1,3 @@
-hgfrom django.db import models
+from django.db import models
 
 # Create your models here.
---
+++ cloud_portal/front_end/app/scripts/app.js
@@ -0,0 +1,92 @@
+'use strict';
+
+angular.module('webadminApp', [
+    'ipCookie',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    //'ngTouch',
+    'ui.bootstrap',
+    'tc.chartjs',
+    'ngStorage'
+]).config(function ($routeProvider) {
+    $routeProvider
+        .when('/settings', {
+            templateUrl: 'views/settings.html',
+            controller: 'SettingsCtrl'
+        })
+        .when('/join', {
+            templateUrl: 'views/join.html',
+            controller: 'SettingsCtrl'
+        })
+        .when('/info', {
+            templateUrl: 'views/info.html',
+            controller: 'InfoCtrl'
+        })
+        .when('/developers', {
+            templateUrl: 'views/developers.html',
+            controller: 'MainCtrl'
+        })
+        .when('/support', {
+            templateUrl: 'views/support.html',
+            controller: 'MainCtrl'
+        })
+        .when('/help', {
+            templateUrl: 'views/help.html',
+            controller: 'MainCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html'
+        })
+        .when('/advanced', {
+            templateUrl: 'views/advanced.html',
+            controller: 'AdvancedCtrl'
+        })
+        .when('/webclient', {
+            templateUrl: 'views/webclient.html',
+            controller: 'WebclientCtrl',
+            reloadOnSearch: false
+        }).when('/view/', {
+            templateUrl: 'views/view.html',
+            controller: 'ViewCtrl',
+            reloadOnSearch: false
+        }).when('/view/:cameraId', {
+            templateUrl: 'views/view.html',
+            controller: 'ViewCtrl',
+            reloadOnSearch: false
+        })
+        .when('/sdkeula', {
+            templateUrl: 'views/sdkeula.html',
+            controller: 'SdkeulaCtrl'
+        })
+        .when('/sdkeula/:sdkFile', {
+            templateUrl: 'views/sdkeula.html',
+            controller: 'SdkeulaCtrl'
+        })
+        .when('/log', {
+            templateUrl: 'views/log.html',
+            controller: 'LogCtrl'
+        })
+        .when('/log', {
+            templateUrl: 'views/log.html',
+            controller: 'LogCtrl'
+        })
+        .otherwise({
+            redirectTo: '/settings'
+        });
+});
+
+
+angular.module('webadminApp').run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+    var original = $location.path;
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            var lastRoute = $route.current;
+            var un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute;
+                un();
+            });
+        }
+        return original.apply($location, [path]);
+    };
+}]);
