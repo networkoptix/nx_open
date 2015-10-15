ae5db3819be65085cf7f6c8a2e2d82bbb108d9cd
Denis Klychkov <dklychkov@networkoptix.com>
2015-10-15 15:00:37 +0300

    MOBILE-61: Some renamings.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- mobile_client/maven/filter-resources/deploy-ios.sh
+++ mobile_client/maven/filter-resources/deploy-ios.sh
@@ -8,5 +8,5 @@ TARGET_SDK=${TARGET_SDK:-iphoneos}
 
 /usr/bin/xcrun -sdk "${TARGET_SDK}" PackageApplication \
     -v "${libdir}/bin/${build.configuration}/${project.artifactId}.app" \
-    -o "${libdir}/bin/${build.configuration}/${product.name.short}2-${project.version.name}.${project.version.code}.ipa" \
+    -o "${libdir}/bin/${build.configuration}/${product.name.short}-${project.version.name}.${project.version.code}.ipa" \
     --embed "${provisioning_profile}"
