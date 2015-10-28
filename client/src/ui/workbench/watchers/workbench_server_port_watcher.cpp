fae9481674eaaaa6405fcfb70023c2b4b62c8676
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2015-10-28 16:05:06 +0300

    VMS-1160: Cannot change anything through client... Review Fixes
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

---
+++ client/src/ui/workbench/watchers/workbench_server_port_watcher.cpp
@@ -0,0 +1,62 @@
+
+#include "workbench_server_port_watcher.h"
+
+#include <ui/actions/action_manager.h>
+#include <ui/workbench/workbench_context.h>
+#include <ui/workbench/workbench_context_aware.h>
+
+#include <api/app_server_connection.h>
+#include <common/common_module.h>
+#include <client/client_message_processor.h>
+#include <core/resource/media_server_resource.h>
+#include <core/resource_management/resource_pool.h>
+
+QnWorkbenchServerPortWatcher::QnWorkbenchServerPortWatcher(QObject *parent)
+    : Connective<QObject>(parent)
+    , QnWorkbenchContextAware(parent)
+
+    , m_currentServerId()
+{
+    connect(qnResPool, &QnResourcePool::resourceRemoved
+        , this, [this](const QnResourcePtr &resource)
+    {
+        if (m_currentServerId.isNull() || !resource || (resource->getId() != m_currentServerId))
+            return;
+
+        const auto removedServer = resource.dynamicCast<QnMediaServerResource>();
+        if (!removedServer)
+            return;
+
+        disconnect(removedServer, nullptr, this, nullptr);
+        m_currentServerId = QnUuid();
+    });
+
+    connect(qnClientMessageProcessor, &QnCommonMessageProcessor::initialResourcesReceived
+        , this, [this]()
+    {
+        const auto currentServer = qnCommon->currentServer();
+        if (!currentServer)
+            return;
+
+        connect(currentServer, &QnMediaServerResource::portChanged, this
+            , [this](const QnResourcePtr &resource)
+        {
+            const auto currentServer = resource.dynamicCast<QnMediaServerResource>();
+            if (!currentServer)
+                return;
+
+            QUrl url = QnAppServerConnectionFactory::url();
+            if (url.isEmpty() || (url.port() == currentServer->getPort()))
+                return;
+
+            url.setPort(currentServer->getPort());
+            QnAppServerConnectionFactory::setUrl(url);
+
+            context()->menu()->trigger(Qn::ReconnectAction);
+        });
+    });
+}
+
+QnWorkbenchServerPortWatcher::~QnWorkbenchServerPortWatcher()
+{
+}
