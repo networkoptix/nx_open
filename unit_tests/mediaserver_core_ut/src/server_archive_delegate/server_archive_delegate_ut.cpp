63384b31d022db327e48578b9ac9ea2f57915937
Alexander Kulikov <akulikov@networkoptix.com>
2015-10-28 16:32:53 +0300

    #vms-1188. Backup storage. Playback ut.
    <==== Mercurial data ====>
    Branch: x_vms_1188_backup_storage_playback_ut

--- unit_tests/mediaserver_core_ut/src/server_archive_delegate/server_archive_delegate_ut.cpp
+++ unit_tests/mediaserver_core_ut/src/server_archive_delegate/server_archive_delegate_ut.cpp
@@ -23,95 +23,70 @@
 #include <thread>
 #include <chrono>
 
-const QString normalStorageUrl = lit("C/HD_Witness_Media");
-const QString backupStorageUrl = lit("D/HD_Witness_Media");
-
-const QString nLqPath = normalStorageUrl + lit("/low_quality/camera_unique_id/2015/10/22/19");
-const QString nHqPath = normalStorageUrl + lit("/hi_quality/camera_unique_id/2015/10/22/19");
-
-const QString bLqPath = backupStorageUrl + lit("/low_quality/camera_unique_id/2015/10/22/19");
-const QString bHqPath = backupStorageUrl + lit("/hi_quality/camera_unique_id/2015/10/22/19");
-
-const QString sGuid = lit("D71EC6D8-8EFD-4137-A098-0437AB2044CC");
-
-class AuxData
-{
+class AuxData {
 public:
-    AuxData() {}
+    AuxData() 
+        : normalStorageUrl(qApp->applicationDirPath() + lit("/C/HD_Witness_Media")),
+          backupStorageUrl(qApp->applicationDirPath() + lit("/D/HD_Witness_Media")),
+          nLqPath(normalStorageUrl + lit("/low_quality/camera_unique_id/2015/10/22/19")),
+          nHqPath(normalStorageUrl + lit("/hi_quality/camera_unique_id/2015/10/22/19")),
+          bLqPath(backupStorageUrl + lit("/low_quality/camera_unique_id/2015/10/22/19")),
+          bHqPath(backupStorageUrl + lit("/hi_quality/camera_unique_id/2015/10/22/19")),
+          sGuid(lit("D71EC6D8-8EFD-4137-A098-0437AB2044CC")) {
+        cleanWorkDirs();
+    }
 
-    int prepare()
-    {
+    int prepare() {
+        if (!createTestPhysicalData()) {
+            return -1;
+        }
         m_resourcePool.reset(new QnResourcePool);
         m_resourceTypePool.reset(new QnResourceTypePool);
-
         m_commonModule.reset(new QnCommonModule);
-        m_commonModule->setModuleGUID(sGuid);
-        
+        m_commonModule->setModuleGUID(sGuid);        
         //m_mediaServer = QnMediaServerResourcePtr(new QnMediaServerResource(qnResTypePool));
         //m_mediaServer->setId(sGuid);
         //m_resourcePool->addResource(m_mediaServer);
-
         m_runnablePool.reset(new QnLongRunnablePool);
         m_normalManager.reset(new QnStorageManager(QnServer::StoragePool::Normal));
         m_backupManager.reset(new QnStorageManager(QnServer::StoragePool::Backup));
-        //m_scheduleSync.reset(new QnScheduleSync);
-
-        //qnNormalStorageMan->initDone();
-        //qnBackupStorageMan->initDone();
-
+        m_scheduleSync.reset(new QnScheduleSync);
         m_normalStorage = QnStorageResourcePtr(new QnFileStorageResource);
         m_normalStorage->setUrl(normalStorageUrl);
-
-        m_backupStorage = QnStorageResourcePtr(new QnFileStorageResource);
-        m_backupStorage->setUrl(backupStorageUrl);
-
+        m_normalStorage->setId("{FEAA4B27-6076-4B57-A2B2-5CEC43894CFE}");
+        m_normalStorage->setStatus(Qn::Online);
+        //m_backupStorage = QnStorageResourcePtr(new QnFileStorageResource);
+        //m_backupStorage->setUrl(backupStorageUrl);
+        //m_backupStorage->setStatus(Qn::Online);
+        //m_backupStorage->setId("{9D220DF5-5FD4-48E7-B42A-BBC5D10AF1CE}");
         qnNormalStorageMan->addStorage(m_normalStorage);
-        qnNormalStorageMan->addStorage(m_backupStorage);
-
-        if (!createTestPhysicalData())
-            return -1;
-
+        //qnBackupStorageMan->addStorage(m_backupStorage);
+        qnNormalStorageMan->rebuildCatalogAsync();
         return 0;
     }
 
-    ~AuxData() 
-    {
-        //cleanup();
+    ~AuxData() {
+        destroyObjects();
+        cleanWorkDirs();
     }
 
-    void cleanup()
-    {
-        qnNormalStorageMan->stopAsyncTasks();
-        qnBackupStorageMan->stopAsyncTasks();
-
-        m_normalManager.reset();
-        m_backupManager.reset();
-        m_scheduleSync.reset();
-        //m_mediaServer.reset();
-        m_commonModule.reset();
-        m_runnablePool.reset();
-        m_resourceTypePool.reset();
-
+    void cleanWorkDirs() {
         std::function<bool (const QString &)> recursiveClean =
-            [&](const QString &path) 
-            {
+            [&](const QString &path) {
                 QDir dir(path);
                 QFileInfoList entryList = dir.entryInfoList(
-                    QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot, 
-                    QDir::DirsFirst
-                );
-                
-                for (auto &entry : entryList) 
-                {
+                        QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot, 
+                        QDir::DirsFirst);                
+                for (auto &entry : entryList) {
                     auto tmp = entry.absoluteFilePath();
-                    if (entry.isDir()) 
-                    {
+                    if (entry.isDir()) {
                          if (!recursiveClean(entry.absoluteFilePath()))
                             return false;
                     } 
-                    else if (entry.isFile()) 
-                    {
-                        bool result = dir.remove(entry.absoluteFilePath());
+                    else if (entry.isFile()) {
+                        QFile f(entry.absoluteFilePath());
+                        f.setPermissions(QFile::ReadOther | QFile::WriteOther);
+                        auto result = f.remove();
                         if (!result)
                             return false;
                     }
@@ -119,19 +94,28 @@ public:
                 dir.rmpath(path);
                 return true;
             };
-
         recursiveClean(normalStorageUrl);
         recursiveClean(backupStorageUrl);
     }
 
+    void destroyObjects() {
+        qnNormalStorageMan->stopAsyncTasks();
+        qnBackupStorageMan->stopAsyncTasks();
+        m_normalManager.reset();
+        m_backupManager.reset();
+        m_scheduleSync.reset();
+        //m_mediaServer.reset();
+        m_commonModule.reset();
+        m_runnablePool.reset();
+        m_resourceTypePool.reset();
+    }
+
 private:
-    bool createTestPhysicalData()
-    {
+    bool createTestPhysicalData() {
         if (!QDir().mkpath(nLqPath) || !QDir().mkpath(nHqPath) || 
             !QDir().mkpath(bLqPath) || !QDir().mkpath(bHqPath)) {            
             return false;
         }
-
         if (!QFile::copy(":/1445529661948_77158.mkv", nLqPath + lit("/1445529661948_77158.mkv")) || 
             !QFile::copy(":/1445529661948_77158.mkv", nHqPath + lit("/1445529661948_77158.mkv"))) {
             return false;
@@ -143,29 +127,37 @@ private:
     {
     }
 
+public:
+    const QString normalStorageUrl;
+    const QString backupStorageUrl;
+    const QString nLqPath;
+    const QString nHqPath;
+    const QString bLqPath;
+    const QString bHqPath;
+    const QString sGuid;
+
 private:
-    QnStorageResourcePtr                m_normalStorage;
-    QnStorageResourcePtr                m_backupStorage;
-    std::unique_ptr<QnCommonModule>     m_commonModule;
+    QnStorageResourcePtr m_normalStorage;
+    QnStorageResourcePtr m_backupStorage;
+    std::unique_ptr<QnCommonModule> m_commonModule;
     std::unique_ptr<QnLongRunnablePool> m_runnablePool;
-    std::unique_ptr<QnResourcePool>     m_resourcePool;
-    std::unique_ptr<QnScheduleSync>     m_scheduleSync;
-    std::unique_ptr<QnStorageManager>   m_normalManager;
-    std::unique_ptr<QnStorageManager>   m_backupManager;
+    std::unique_ptr<QnResourcePool> m_resourcePool;
+    std::unique_ptr<QnScheduleSync> m_scheduleSync;
+    std::unique_ptr<QnStorageManager> m_normalManager;
+    std::unique_ptr<QnStorageManager> m_backupManager;
     std::unique_ptr<QnResourceTypePool> m_resourceTypePool;
     //QnMediaServerResourcePtr            m_mediaServer;    
-    QnResourceStatusDictionary          m_rdict;
+    QnResourceStatusDictionary m_rdict;
     //QnMediaServerUserAttributesPool     m_mediaServerAttrPool;
+
 #ifndef _WIN32
     QnPlatformAbstraction platformAbstraction;
 #endif
-
 };
 
-TEST(ServerArchiveDelegateTest, Main) 
-{
+TEST(ServerArchiveDelegateTest, Main) {
     AuxData auxData;
+    auxData.cleanWorkDirs();
     ASSERT_TRUE(auxData.prepare() == 0) << "Prepare test data failed";
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    auxData.cleanup();
+    std::this_thread::sleep_for(std::chrono::seconds(30));
 }
