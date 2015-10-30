1d222faab3db7e03f0972fc54d6cdd27fa6b89bb
Roman Vasilenko <rvasilenko@networkoptix.com>
2015-10-30 19:26:05 +0300

    VMS-1240. reduce bookmarks cleanup interval.
    <==== Mercurial data ====>
    Branch: dev_2.5

--- mediaserver_core/src/recorder/storage_manager.cpp
+++ mediaserver_core/src/recorder/storage_manager.cpp
@@ -50,7 +50,7 @@
 namespace {
     static const qint64 MSECS_PER_DAY = 1000ll * 3600ll * 24ll;
     static const qint64 MOTION_CLEANUP_INTERVAL = 1000ll * 3600;
-    static const qint64 BOOKMARK_CLEANUP_INTERVAL = 1000ll * 3600;
+    static const qint64 BOOKMARK_CLEANUP_INTERVAL = 1000ll * 60;
     static const qint64 EMPTY_DIRS_CLEANUP_INTERVAL = 1000ll * 3600;
     static const QString SCAN_ARCHIVE_FROM(lit("SCAN_ARCHIVE_FROM"));
 
