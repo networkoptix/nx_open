a6bf806d6f3827597a16f046d41cf828ec0c7f53
Denis Klychkov <dklychkov@networkoptix.com>
2015-11-04 12:18:35 +0300

    MOBILE: Fix non-beta android build.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- mobile_client/maven/filter-resources/deploy-android.sh
+++ mobile_client/maven/filter-resources/deploy-android.sh
@@ -2,6 +2,7 @@
 
 BUILD_TARGET=apk
 ANDROID_NDK_ROOT=${env.environment}/android/android-ndk
+TARGET_APK=${product.name.short}-${project.version.name}.${project.version.code}-${build.configuration}.apk
 
 if [ -d "$BUILD_TARGET/libs" ]; then
     rm -rf $BUILD_TARGET/libs
@@ -9,14 +10,21 @@ fi
 
 BUILD_TYPE=
 SIGN=
+SOURCE_APK=
 if [[ ${build.configuration} == "release" ]]; then
     BUILD_TYPE=--release
 
     if [[ ${skip.sign} != true ]]; then
         SIGN="--sign ${google.keystore} ${google.alias} --storepass ${google.storepass} --keypass ${google.keypass}"
+        SOURCE_APK=apk/bin/QtApp-release-signed.apk
+    else
+        SOURCE_APK=apk/bin/QtApp-release-unsigned.apk
     fi
+else
+    SOURCE_APK=apk/bin/QtApp-debug.apk
 fi
-
 
 make install --makefile=Makefile.${build.configuration} INSTALL_ROOT=$BUILD_TARGET
 ${libdir}/bin/androiddeployqt $BUILD_TYPE $SIGN --input android-deployment.json --output $BUILD_TARGET $*
+
+cp $SOURCE_APK $TARGET_APK
