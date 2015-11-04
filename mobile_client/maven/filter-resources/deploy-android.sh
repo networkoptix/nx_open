7eb33b5b7c5ca65ada6305abd79b4078cd7cac03
Denis Klychkov <dklychkov@networkoptix.com>
2015-11-04 12:52:37 +0300

    MOBILE: APK-IPA renamings.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- mobile_client/maven/filter-resources/deploy-android.sh
+++ mobile_client/maven/filter-resources/deploy-android.sh
@@ -2,27 +2,36 @@
 
 BUILD_TARGET=apk
 ANDROID_NDK_ROOT=${env.environment}/android/android-ndk
-TARGET_APK=${product.name.short}-${project.version.name}.${project.version.code}-${build.configuration}.apk
 
 if [ -d "$BUILD_TARGET/libs" ]; then
     rm -rf $BUILD_TARGET/libs
 fi
 
+APK_SUFFIX=
 BUILD_TYPE=
 SIGN=
 SOURCE_APK=
-if [[ ${build.configuration} == "release" ]]; then
+if [[ "${build.configuration}" == "release" ]]; then
     BUILD_TYPE=--release
 
-    if [[ ${skip.sign} != true ]]; then
+    if [[ "${skip.sign}" != "true" ]]; then
         SIGN="--sign ${google.keystore} ${google.alias} --storepass ${google.storepass} --keypass ${google.keypass}"
         SOURCE_APK=apk/bin/QtApp-release-signed.apk
     else
         SOURCE_APK=apk/bin/QtApp-release-unsigned.apk
     fi
+
+    if [[ "${beta}" == "true" ]]; then
+        APK_SUFFIX="beta"
+    else
+        APK_SUFFIX="release"
+    fi
 else
     SOURCE_APK=apk/bin/QtApp-debug.apk
+    APK_SUFFIX="debug"
 fi
+
+TARGET_APK=${product.name.short}-${project.version.name}.${project.version.code}-$APK_SUFFIX.apk
 
 make install --makefile=Makefile.${build.configuration} INSTALL_ROOT=$BUILD_TARGET
 ${libdir}/bin/androiddeployqt $BUILD_TYPE $SIGN --input android-deployment.json --output $BUILD_TARGET $*
