3231d2378ce22eae8f278422b64436a466232515
Denis Klychkov <dklychkov@networkoptix.com>
2015-11-05 17:20:33 +0300

    Fix some warnings generated by cppcheck.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- common/src/utils/common/string.h
+++ common/src/utils/common/string.h
@@ -68,7 +68,7 @@ qint64 parseDateTime( const QString& dateTime );
  * \param pattern                       Pattern to use for result construction. 
  *                                      <tt>%1</tt> will be replaced with size in resulting units, and <tt>%2</tt> with unit name.
  */
-QString formatFileSize(qint64 size, int precision = 1, int prefixThreshold = 1, Qn::MetricPrefix minPrefix = Qn::NoPrefix, Qn::MetricPrefix maxPrefix = Qn::YottaPrefix, bool useBinaryPrefixes = true, const QString pattern = QLatin1String("%1 %2"));
+QString formatFileSize(qint64 size, int precision = 1, int prefixThreshold = 1, Qn::MetricPrefix minPrefix = Qn::NoPrefix, Qn::MetricPrefix maxPrefix = Qn::YottaPrefix, bool useBinaryPrefixes = true, const QString &pattern = QLatin1String("%1 %2"));
 
 int naturalStringCompare(const QString &lhs, const QString &rhs, Qt::CaseSensitivity caseSensitive = Qt::CaseSensitive, bool enableFloat = false);
 QStringList naturalStringSort(const QStringList &list, Qt::CaseSensitivity caseSensitive = Qt::CaseSensitive);
