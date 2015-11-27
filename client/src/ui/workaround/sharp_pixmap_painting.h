3ae6068a8d3649105ad65919943327d0a19ca970
Sergey Ivanov <sivanov@networkoptix.com>
2015-11-27 14:14:05 +0300

    Comment added
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- client/src/ui/workaround/sharp_pixmap_painting.h
+++ client/src/ui/workaround/sharp_pixmap_painting.h
@@ -1,3 +1,7 @@
 #pragma once
 
+/**
+ * Utility method to paint pixmap on the painter, leaving it sharp.
+ * Actual if the bitmap contains text or small details.
+ */
 void paintPixmapSharp( QPainter *painter, const QPixmap &pixmap, const QPointF &position = QPointF());
