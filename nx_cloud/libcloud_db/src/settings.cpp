1661978c06da0977017f4707a8b82c7427872044
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2015-12-09 13:42:52 +0300

    #CLOUD-137 const names made conformant with coding conventions
    <==== Mercurial data ====>
    Branch: dev_3.0

--- nx_cloud/libcloud_db/src/settings.cpp
+++ nx_cloud/libcloud_db/src/settings.cpp
@@ -19,57 +19,57 @@
 namespace
 {
     //log settings
-    static const QLatin1String LOG_LEVEL( "log/logLevel" );
+    const QLatin1String kLogLevel( "log/logLevel" );
 #ifdef _DEBUG
-    static const QLatin1String DEFAULT_LOG_LEVEL( "DEBUG" );
+    const QLatin1String kDefaultLogLevel( "DEBUG" );
 #else
-    static const QLatin1String DEFAULT_LOG_LEVEL( "INFO" );
+    const QLatin1String kDefaultLogLevel( "INFO" );
 #endif
-    static const QLatin1String LOG_DIR( "log/logDir" );
+    const QLatin1String kLogDir( "log/logDir" );
 
-    static const QLatin1String ENDPOINTS_TO_LISTEN( "listenOn" );
-    static const QLatin1String DEFAULT_ENDPOINTS_TO_LISTEN( "0.0.0.0:3346" );
+    const QLatin1String kEndpointsToListen( "listenOn" );
+    const QLatin1String kDefaultEndpointsToListen( "0.0.0.0:3346" );
 
-    static const QLatin1String DATA_DIR( "dataDir" );
+    const QLatin1String kDataDir( "dataDir" );
 
     //DB options
-    static const QLatin1String DB_DRIVER_NAME( "db/driverName" );
-    static const QLatin1String DEFAULT_DB_DRIVER_NAME( "QMYSQL" );
+    const QLatin1String kDbDriverName( "db/driverName" );
+    const QLatin1String kDefaultDbDriverName( "QMYSQL" );
 
-    static const QLatin1String DB_HOST_NAME( "db/hostName" );
-    static const QLatin1String DEFAULT_DB_HOST_NAME( "127.0.0.1" );
+    const QLatin1String kDbHostName( "db/hostName" );
+    const QLatin1String kDefaultDbHostName( "127.0.0.1" );
 
-    static const QLatin1String DB_PORT( "db/port" );
-    static const int DEFAULT_DB_PORT = 3306;
+    const QLatin1String kDbPort( "db/port" );
+    const int kDefaultDbPort = 3306;
 
-    static const QLatin1String DB_DB_NAME( "db/name" );
-    static const QLatin1String DEFAULT_DB_DB_NAME( "nx_cloud" );
+    const QLatin1String kDbName( "db/name" );
+    const QLatin1String kDefaultDbName( "nx_cloud" );
 
-    static const QLatin1String DB_USER_NAME( "db/userName" );
-    static const QLatin1String DEFAULT_DB_USER_NAME( "root" );
+    const QLatin1String kDbUserName( "db/userName" );
+    const QLatin1String kDefaultDbUserName( "root" );
 
-    static const QLatin1String DB_PASSWORD( "db/password" );
-    static const QLatin1String DEFAULT_DB_PASSWORD( "" );
+    const QLatin1String kDbPassword( "db/password" );
+    const QLatin1String kDefaultDbPassword( "" );
 
-    static const QLatin1String DB_CONNECT_OPTIONS( "db/connectOptions" );
-    static const QLatin1String DEFAULT_DB_CONNECT_OPTIONS( "" );
+    const QLatin1String kDbConnectOptions( "db/connectOptions" );
+    const QLatin1String kDefaultDbConnectOptions( "" );
 
-    static const QLatin1String DB_MAX_CONNECTIONS( "db/maxConnections" );
-    static const QLatin1String DEFAULT_DB_MAX_CONNECTIONS( "1" );
+    const QLatin1String kDbMaxConnections( "db/maxConnections" );
+    const QLatin1String kDefaultDbMaxConnections( "1" );
 
-    static const QLatin1String CLOUD_DB_URL( "cloudDbUrl" );
-    static const QLatin1String DEFAULT_CLOUD_DB_URL( "http://nowhere.com:3346/" );
+    const QLatin1String kCloudDbUrl( "cloudDbUrl" );
+    const QLatin1String kDefaultCloudDbUrl( "http://nowhere.com:3346/" );
 
-    static const QLatin1String CHANGE_USER( "changeUser" );
+    const QLatin1String kChangeUser( "changeUser" );
 
     //notification settings
-    static const QLatin1String NOTIFICATION_ENABLED("notification/enabled");
-    static const QLatin1String DEFAULT_NOTIFICATION_ENABLED("true");
+    const QLatin1String kNotificationEnabled("notification/enabled");
+    const QLatin1String kDefaultNotificationEnabled("true");
 
     //auth settings
-    static const QLatin1String AUTH_XML_PATH("auth/rulesXmlPath");
-    static const QLatin1String DEFAULT_AUTH_XML_PATH(":/authorization_rules.xml");
-    static const QString MODULE_NAME = lit( "cloud_db" );
+    const QLatin1String kAuthXmlPath("auth/rulesXmlPath");
+    const QLatin1String kDefaultAuthXmlPath(":/authorization_rules.xml");
+    const QString kModuleName = lit( "cloud_db" );
 }
 
 
@@ -87,7 +87,7 @@ Settings::Settings()
         QN_APPLICATION_NAME ),
 #else
     m_settings( lit("/opt/%1/%2/etc/%2.conf" )
-                .arg( VER_LINUX_ORGANIZATION_NAME ).arg( MODULE_NAME ),
+                .arg( VER_LINUX_ORGANIZATION_NAME ).arg( kModuleName ),
                 QSettings::IniFormat ),
 #endif
     m_showHelp( false )
@@ -132,8 +132,8 @@ const QString& Settings::changeUser() const
 std::list<SocketAddress> Settings::endpointsToListen() const
 {
     const QStringList& httpAddrToListenStrList = m_settings.value(
-        ENDPOINTS_TO_LISTEN,
-        DEFAULT_ENDPOINTS_TO_LISTEN ).toString().split( ',' );
+        kEndpointsToListen,
+        kDefaultEndpointsToListen ).toString().split( ',' );
     std::list<SocketAddress> httpAddrToListenList;
     std::transform(
         httpAddrToListenStrList.begin(),
@@ -146,13 +146,13 @@ std::list<SocketAddress> Settings::endpointsToListen() const
 
 QString Settings::dataDir() const
 {
-    const QString& dataDirFromSettings = m_settings.value( DATA_DIR ).toString();
+    const QString& dataDirFromSettings = m_settings.value( kDataDir ).toString();
     if( !dataDirFromSettings.isEmpty() )
         return dataDirFromSettings;
 
 #ifdef Q_OS_LINUX
     QString defVarDirName = QString( "/opt/%1/%2/var" )
-            .arg( VER_LINUX_ORGANIZATION_NAME ).arg( MODULE_NAME );
+            .arg( VER_LINUX_ORGANIZATION_NAME ).arg( kModuleName );
     QString varDirName = m_settings.value( "varDir", defVarDirName ).toString();
     return varDirName;
 #else
@@ -183,31 +183,31 @@ void Settings::fillSupportedCmdParameters()
 void Settings::loadConfiguration()
 {
     //log
-    m_logging.logLevel = m_settings.value( LOG_LEVEL, DEFAULT_LOG_LEVEL ).toString();
-    m_logging.logDir = m_settings.value( LOG_DIR, dataDir() + lit( "/log/" ) ).toString();
+    m_logging.logLevel = m_settings.value( kLogLevel, kDefaultLogLevel ).toString();
+    m_logging.logDir = m_settings.value( kLogDir, dataDir() + lit( "/log/" ) ).toString();
 
     //DB
-    m_dbConnectionOptions.driverName = m_settings.value( DB_DRIVER_NAME, DEFAULT_DB_DRIVER_NAME ).toString();
-    m_dbConnectionOptions.hostName = m_settings.value( DB_HOST_NAME, DEFAULT_DB_HOST_NAME ).toString();
-    m_dbConnectionOptions.port = m_settings.value( DB_PORT, DEFAULT_DB_PORT ).toInt();
-    m_dbConnectionOptions.dbName = m_settings.value( DB_DB_NAME, DEFAULT_DB_DB_NAME ).toString();
-    m_dbConnectionOptions.userName = m_settings.value( DB_USER_NAME, DEFAULT_DB_USER_NAME ).toString();
-    m_dbConnectionOptions.password = m_settings.value( DB_PASSWORD, DEFAULT_DB_PASSWORD ).toString();
-    m_dbConnectionOptions.connectOptions = m_settings.value( DB_CONNECT_OPTIONS, DEFAULT_DB_CONNECT_OPTIONS ).toString();
-    m_dbConnectionOptions.maxConnectionCount = m_settings.value( DB_MAX_CONNECTIONS, DEFAULT_DB_MAX_CONNECTIONS ).toUInt();
+    m_dbConnectionOptions.driverName = m_settings.value( kDbDriverName, kDefaultDbDriverName ).toString();
+    m_dbConnectionOptions.hostName = m_settings.value( kDbHostName, kDefaultDbHostName ).toString();
+    m_dbConnectionOptions.port = m_settings.value( kDbPort, kDefaultDbPort ).toInt();
+    m_dbConnectionOptions.dbName = m_settings.value( kDbName, kDefaultDbName ).toString();
+    m_dbConnectionOptions.userName = m_settings.value( kDbUserName, kDefaultDbUserName ).toString();
+    m_dbConnectionOptions.password = m_settings.value( kDbPassword, kDefaultDbPassword ).toString();
+    m_dbConnectionOptions.connectOptions = m_settings.value( kDbConnectOptions, kDefaultDbConnectOptions ).toString();
+    m_dbConnectionOptions.maxConnectionCount = m_settings.value( kDbMaxConnections, kDefaultDbMaxConnections ).toUInt();
     if( m_dbConnectionOptions.maxConnectionCount == 0 )
         m_dbConnectionOptions.maxConnectionCount = std::thread::hardware_concurrency();
 
-    m_cloudBackendUrl = m_settings.value( CLOUD_DB_URL, DEFAULT_CLOUD_DB_URL ).toString();
-    m_changeUser = m_settings.value( CHANGE_USER ).toString();
+    m_cloudBackendUrl = m_settings.value( kCloudDbUrl, kDefaultCloudDbUrl ).toString();
+    m_changeUser = m_settings.value( kChangeUser ).toString();
 
     //email
     m_notification.enabled = m_settings.value(
-        NOTIFICATION_ENABLED,
-        DEFAULT_NOTIFICATION_ENABLED).toString() == "true";
+        kNotificationEnabled,
+        kDefaultNotificationEnabled).toString() == "true";
 
     //auth
-    m_auth.rulesXmlPath = m_settings.value(AUTH_XML_PATH, DEFAULT_AUTH_XML_PATH).toString();
+    m_auth.rulesXmlPath = m_settings.value(kAuthXmlPath, kDefaultAuthXmlPath).toString();
 }
 
 }   //conf
