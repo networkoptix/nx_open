96bad239609c25e963f69a909afb805c6d9685b5
Denis Klychkov <dklychkov@networkoptix.com>
2015-12-16 17:47:46 +0300

    MOBILE: Review fix.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- mobile_client/src/camera/camera_thumbnail_cache.h
+++ mobile_client/src/camera/camera_thumbnail_cache.h
@@ -1,5 +1,4 @@
-#ifndef QNCAMERATHUMBNAILCACHE_H
-#define QNCAMERATHUMBNAILCACHE_H
+#pragma once
 
 #include <QtCore/QObject>
 #include <QtCore/QElapsedTimer>
@@ -8,12 +7,16 @@
 #include <api/helpers/thumbnail_request_data.h>
 #include <utils/common/id.h>
 #include <utils/common/singleton.h>
+#include <utils/thread/mutex.h>
 #include <camera/thumbnail_cache_base.h>
 
-class QnCameraThumbnailCache : public QObject, public QnThumbnailCacheBase, public Singleton<QnCameraThumbnailCache> {
+class QnCameraThumbnailCache : public QObject, public QnThumbnailCacheBase, public Singleton<QnCameraThumbnailCache>
+{
     Q_OBJECT
+
 public:
-    struct ThumbnailData {
+    struct ThumbnailData
+    {
         QString thumbnailId;
         qint64 time;
         bool loading;
@@ -46,11 +49,9 @@ private:
     void refreshThumbnail(const QnUuid &id);
 
 private:
-    mutable QMutex m_mutex;
+    mutable QnMutex m_mutex;
     QElapsedTimer m_elapsedTimer;
     QHash<QnUuid, ThumbnailData> m_thumbnailByResourceId;
     QHash<QString, QPixmap> m_pixmaps;
     QnThumbnailRequestData m_request;
 };
-
-#endif // QNCAMERATHUMBNAILCACHE_H
