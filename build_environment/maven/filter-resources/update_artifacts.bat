6d71184d974ee17291f22d690e6e420e4ca4822e
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2015-12-21 22:48:55 -0800

    ENV-52: works for Windows
    <==== Mercurial data ====>
    Branch: tricom_demo_features

---
+++ build_environment/maven/filter-resources/update_artifacts.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set SOURCE=%NX_RSYNC_SOURCE%
+if "%NX_RSYNC_SOURCE%" == "" (
+  set SOURCE=enk.me
+)
+
+set CURRENTDIR=%cd%
+set RSYNC=%CURRENTDIR%\rsync-win32\rsync -rtvL
+
+mkdir %CURRENTDIR%\artifacts\qt\${qt_version}\${platform}\${arch}
+cd %CURRENTDIR%\artifacts\qt\${qt_version}\${platform}
+%RSYNC% --delete rsync://%SOURCE%/buildenv/artifacts/qt/${qt_version}/${platform}/${arch} .
+cd %CURRENTDIR%
