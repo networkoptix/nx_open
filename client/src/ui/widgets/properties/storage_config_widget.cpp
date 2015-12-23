5460943202e18756b14716c0da728a190993c78d
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2015-12-23 17:26:36 +0300

    VMS-1728: Do not disable button "Backup now" if at least one camera selected
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- client/src/ui/widgets/properties/storage_config_widget.cpp
+++ client/src/ui/widgets/properties/storage_config_widget.cpp
@@ -126,7 +126,6 @@ QnStorageConfigWidget::QnStorageConfigWidget(QWidget* parent)
     , m_updating(false)
     , m_quality(qnGlobalSettings->backupQualities())
     , m_camerasToBackup()
-    , m_currentServerCamerasToBackup()
 {
     ui->setupUi(this);
 
@@ -263,7 +262,7 @@ void QnStorageConfigWidget::initQualitiesCombo()
 
     resetQualities();
 
-    connect(ui->qualityComboBox, QnComboboxCurrentIndexChanged, this, [this](int index)
+    connect(ui->qualityComboBox, QnComboboxCurrentIndexChanged, this, [this](int /* index */)
     {
         m_quality = extractQuality(ui->qualityComboBox);
         emit hasChangesChanged();
@@ -477,7 +476,7 @@ void QnStorageConfigWidget::updateRebuildInfo() {
 
 void QnStorageConfigWidget::updateBackupInfo() {
     updateBackupUi(qnServerStorageManager->backupStatus(m_server)
-        , m_camerasToBackup.size(), m_currentServerCamerasToBackup.size());
+        , m_camerasToBackup.size());
 }
 
 void QnStorageConfigWidget::applyStoragesChanges(QnStorageResourceList& result, const QnStorageModelInfoList &storages) const {
@@ -737,8 +736,7 @@ void QnStorageConfigWidget::updateBackupWidgetsVisibility() {
 }
 
 void QnStorageConfigWidget::updateBackupUi(const QnBackupStatusData& reply
-    , int overallSelectedCameras
-    , int currentServerSelectedCameras)
+    , int overallSelectedCameras)
 {
     QString status;
 
@@ -750,7 +748,7 @@ void QnStorageConfigWidget::updateBackupUi(const QnBackupStatusData& reply
 
     QString backupInfo;
     bool canStartBackup = this->canStartBackup(
-        reply, currentServerSelectedCameras, &backupInfo);
+        reply, overallSelectedCameras, &backupInfo);
     ui->backupWarningLabel->setText(backupInfo);
 
     bool realtime = m_backupSchedule.backupType == Qn::Backup_RealTime;
@@ -813,13 +811,6 @@ void QnStorageConfigWidget::updateCamerasForBackup(const QnVirtualCameraResource
         return;
 
     m_camerasToBackup = cameras;
-
-    const auto isCurrentServerFilter = [this](const QnVirtualCameraResourcePtr &resource)
-    {
-        return (resource->getParentServer() == m_server);
-    };
-
-    m_currentServerCamerasToBackup = m_camerasToBackup.filtered(isCurrentServerFilter);
 
     updateBackupInfo();
     emit hasChangesChanged();
@@ -904,7 +895,7 @@ void QnStorageConfigWidget::at_serverBackupStatusChanged( const QnMediaServerRes
     if (server != m_server)
         return;
 
-    updateBackupUi(status, m_camerasToBackup.size(), m_currentServerCamerasToBackup.size());
+    updateBackupUi(status, m_camerasToBackup.size());
     updateRebuildInfo();
 }
 
