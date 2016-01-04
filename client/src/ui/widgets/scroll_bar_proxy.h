0232cd178042768d826f043f7b2410055c7436c7
Denis Klychkov <dklychkov@networkoptix.com>
2016-01-04 21:44:34 +0300

    Introduce scroll bar proxy.
    <==== Mercurial data ====>
    Branch: new_style

---
+++ client/src/ui/widgets/scroll_bar_proxy.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QtWidgets/QScrollBar>
+
+class QAbstractScrollArea;
+
+class QnScrollBarProxy : public QScrollBar
+{
+    typedef QScrollBar base_type;
+
+public:
+    QnScrollBarProxy(QWidget *parent = nullptr);
+    QnScrollBarProxy(QScrollBar *scrollBar, QWidget *parent = nullptr);
+    ~QnScrollBarProxy();
+
+    void setScrollBar(QScrollBar *scrollBar);
+
+    QSize sizeHint() const override;
+    QSize minimumSizeHint() const override;
+
+    bool event(QEvent *event) override;
+
+    static void makeProxy(QScrollBar *scrollBar, QAbstractScrollArea *scrollArea);
+
+protected:
+    void paintEvent(QPaintEvent *) override;
+
+private:
+    void setScrollBarVisible(bool visible);
+
+private:
+    QScrollBar *m_scrollBar;
+    bool m_visible;
+};
