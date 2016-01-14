f82d911dffb89617079a51dcfbe66e7eb6c068c5
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-01-14 20:25:47 +0300

    network tests moved to nx_network_ut project
    <==== Mercurial data ====>
    Branch: dev_3.0

--- unit_tests/nx_network_ut/src/cloud/cloud_stream_socket_ut.cpp
+++ unit_tests/nx_network_ut/src/cloud/cloud_stream_socket_ut.cpp
@@ -3,7 +3,7 @@
 #include <nx/network/cloud/cloud_stream_socket.h>
 #include <nx/network/cloud/cloud_server_socket.h>
 
-#include "utils/network/simple_socket_test_helper.h"
+#include "simple_socket_test_helper.h"
 
 namespace nx {
 namespace cc {
