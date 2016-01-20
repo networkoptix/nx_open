0d210f9ebcfc4f45160be23db6cd38900947b170
Evgeny Balashov <ebalashov@networkoptix.com>
2016-01-20 14:15:07 +0300

    #WEB-236 - better reloading after changing settings
    <==== Mercurial data ====>
    Branch: dev_2.5.0_web

--- webadmin/app/scripts/controllers/settings.js
+++ webadmin/app/scripts/controllers/settings.js
@@ -76,7 +76,7 @@ angular.module('webadminApp')
                 }
             } else {
                 alert('Settings saved');
-                if( $scope.settings.port !==  window.location.port ) {
+                if( $scope.settings.port !=  window.location.port ) {
                     window.location.href = (window.location.protocol + '//' + window.location.hostname + ':' + $scope.settings.port + window.location.pathname + window.location.hash);
                 }else{
                     window.location.reload();
