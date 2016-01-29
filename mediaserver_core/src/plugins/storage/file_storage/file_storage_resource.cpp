2c4e6fb31d3d369dbcebd975e6f17c8d28b8960b
Mikhail Uskov <muskov@networkoptix.com>
2016-01-29 17:08:11 +0300

    fix possible memory leak on getline
    <==== Mercurial data ====>
    Branch: dev_2.5

--- mediaserver_core/src/plugins/storage/file_storage/file_storage_resource.cpp
+++ mediaserver_core/src/plugins/storage/file_storage/file_storage_resource.cpp
@@ -690,6 +690,9 @@ static bool readTabFile( const QString& filePath, QStringList* const mountPoints
     size_t length = 0;
     while( !feof(tabFile) )
     {
+        // man getline 3:
+        // This buffer should be freed by the user program even if getline() failed.
+        std::unique_ptr< void, decltype( free )* >( line, &free );
         if( getline( &line, &length, tabFile ) == -1 )
             break;
 
@@ -702,7 +705,6 @@ static bool readTabFile( const QString& filePath, QStringList* const mountPoints
             mountPoints->push_back( fields[1] );
     }
 
-    free( line );
     fclose( tabFile );
 
     return true;
