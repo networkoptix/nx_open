e4e54221f0817a682c77d2e4a8a3cf7982f981a9
Sergey Ivanov <sivanov@networkoptix.com>
2016-02-02 14:26:01 +0300

    VMS-1937: Client crash when the first bookmark created
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- unit_tests/client_ut/src/camera/camera_bookmark_aggregation_ut.cpp
+++ unit_tests/client_ut/src/camera/camera_bookmark_aggregation_ut.cpp
@@ -4,11 +4,14 @@
 
 namespace
 {
-    QnCameraBookmark createValidBookmark() {
+    const qint64 kDefaultBookmarkDurationMs = 60 * 1000;
+
+    QnCameraBookmark createValidBookmark(qint64 startTimeMs = 0) {
         QnCameraBookmark result;
         result.guid = QnUuid::createUuid();
         result.cameraId = "testcamera";
-        result.durationMs = 60 * 1000;
+        result.startTimeMs = startTimeMs;
+        result.durationMs = kDefaultBookmarkDurationMs;
         Q_ASSERT_X(result.isValid(), Q_FUNC_INFO, "This function must create valid bookmarks");
 
         return result;
@@ -31,19 +34,42 @@ TEST( QnCameraBookmarkAggregationTest, add )
     QnCameraBookmark bookmark(createValidBookmark());
     ASSERT_TRUE(bookmark.isValid());
 
-    list.addBookmark(bookmark);
-    ASSERT_EQ(list.bookmarkList().size(), 1);
+    bool changed = list.addBookmark(bookmark);
+    ASSERT_EQ(1, list.bookmarkList().size());
+    ASSERT_TRUE(changed);
 }
 
-/** Test if invalid bookmark is not added. */
-TEST( QnCameraBookmarkAggregationTest, addInvalid )
+/** Test if null bookmark is not added. */
+TEST( QnCameraBookmarkAggregationTest, addNull )
 {
     QnCameraBookmarkAggregation list;
 
     QnCameraBookmark bookmark;
-    ASSERT_FALSE(bookmark.isValid());
+    ASSERT_TRUE(bookmark.isNull());
+
+#ifdef _DEBUG
+    ASSERT_DEATH(list.addBookmark(bookmark), "");
+#else
+    /* Following checks are actual in release mode. */
+    bool changed = list.addBookmark(bookmark);
+    ASSERT_FALSE(changed);
+#endif
+
+    ASSERT_EQ(0, list.bookmarkList().size());
+}
+
+/** Test if bookmark is not duplicated. */
+TEST( QnCameraBookmarkAggregationTest, addSame )
+{
+    QnCameraBookmarkAggregation list;
+
+    QnCameraBookmark bookmark(createValidBookmark());
+    ASSERT_TRUE(bookmark.isValid());
 
     list.addBookmark(bookmark);
 
-    ASSERT_EQ(list.bookmarkList().size(), 0);
+    bool changed = list.addBookmark(bookmark);
+
+    ASSERT_EQ(1, list.bookmarkList().size());
+    ASSERT_FALSE(changed);
 }
