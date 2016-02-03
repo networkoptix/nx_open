0dfc3aef1bb76f119d4552595c0b0b0307a9e2cb
Sergey Ivanov <sivanov@networkoptix.com>
2016-02-03 18:20:50 +0300

    VMS-1928: Bookmarks look like being sorted by "Name" but really sorted by "Start Time"
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- client/src/ui/dialogs/private/search_bookmarks_dialog_p.cpp
+++ client/src/ui/dialogs/private/search_bookmarks_dialog_p.cpp
@@ -100,6 +100,9 @@ QnSearchBookmarksDialogPrivate::QnSearchBookmarksDialogPrivate(const QString &fi
     m_ui->setupUi(m_owner);
     m_ui->gridBookmarks->setModel(m_model);
 
+    QnBookmarkSortOrder sortOrder = QnSearchBookmarksModel::defaultSortOrder();
+    int column = QnSearchBookmarksModel::sortFieldToColumn(sortOrder.column);
+    m_ui->gridBookmarks->horizontalHeader()->setSortIndicator(column, sortOrder.order);
 
     const auto updateFilterText = [this]()
     {
@@ -250,6 +253,8 @@ bool QnSearchBookmarksDialogPrivate::fillActionParameters(QnActionParameters &pa
     for (const QModelIndex &index: selection)
     {
         const auto bookmark = bookmarkFromIndex(index);
+
+        /* Add invalid bookmarks, user must be able to delete them. */
         if (!bookmark.isValid())
             continue;
 
@@ -268,6 +273,10 @@ bool QnSearchBookmarksDialogPrivate::fillActionParameters(QnActionParameters &pa
         return true;
 
     QnCameraBookmark currentBookmark = bookmarks.first();
+    /* User should not be able to export or open invalid bookmark. */
+    if (!currentBookmark.isValid())
+        return true;
+
     params.setArgument(Qn::CameraBookmarkRole, currentBookmark);
 
     auto camera = availableCameraByUniqueId(currentBookmark.cameraId);
