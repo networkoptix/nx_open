4b82dbc3ee882fbca6f03a49fc788bdc69fa072f
Evgeny Balashov <ebalashov@networkoptix.com>
2016-02-08 17:25:19 +0300

    New email templates: minify
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/cloud/notifications/static/templates/src/preprocess.py
+++ cloud_portal/cloud/notifications/static/templates/src/preprocess.py
@@ -1,5 +1,6 @@
 import cssutils
 import logging
+import htmlmin
 from inlinestyler.utils import inline_css
 
 import os
@@ -14,7 +15,7 @@ for file in os.listdir("."):
         template_name = file.replace('.mustache.body', '')
         new_file = open('../' + template_name + '.mustache', 'w')
         text = containter_text.replace('{{>body}}', open(template_name + '.mustache.body').read())
-        print >> new_file, inline_css(text).encode('utf-8', 'replace')
+        print >> new_file, htmlmin.minify(inline_css(text), remove_comments=True, remove_empty_space=True).encode('utf-8', 'replace')
 
 """
 for f in ('camera_input', 'camera_ip_conflict', 'camera_motion', 'mediaserver_conflict', 'generic_event', 'mediaserver_failure', 'mediaserver_started', 'storage_failure', 'license_issue'):
