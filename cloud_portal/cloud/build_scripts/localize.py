96711c93d6e808c2c81256cba2fa6a3be9d6fe77
Evgeny Balashov <ebalashov@networkoptix.com>
2016-02-09 19:10:28 +0300

    Localization: process front end and templates at once
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/cloud/build_scripts/localize.py
+++ cloud_portal/cloud/build_scripts/localize.py
@@ -2,35 +2,36 @@ import os
 import xml.etree.ElementTree as eTree
 
 # 1. read ts into xml
-xml_file = 'cloud_portal.ts'
+xml_files = ('cloud_portal.ts', 'templates.ts')
 root_directory = ''
 
-tree = eTree.parse(xml_file)
-root = tree.getroot()
+for xml_file in xml_files:
+    tree = eTree.parse(xml_file)
+    root = tree.getroot()
 
-# 2. go file by file
-active_filename = ''
-active_content = ''
-for context in root.iter('context'):
-    for message in context.iter('message'):
-        location = message.find('location')
-        if location is not None and location.get('filename'):
-            if active_filename:
-                # save old file
-                with open(active_filename, "w") as file_descriptor:
-                    file_descriptor.write(active_content)
+    # 2. go file by file
+    active_filename = ''
+    active_content = ''
+    for context in root.iter('context'):
+        for message in context.iter('message'):
+            location = message.find('location')
+            if location is not None and location.get('filename'):
+                if active_filename:
+                    # save old file
+                    with open(active_filename, "w") as file_descriptor:
+                        file_descriptor.write(active_content)
 
-            active_filename = os.path.join(root_directory, location.get('filename'))
+                active_filename = os.path.join(root_directory, location.get('filename'))
 
-            with open(active_filename, 'r') as file_descriptor:
-                active_content = file_descriptor.read()
+                with open(active_filename, 'r') as file_descriptor:
+                    active_content = file_descriptor.read()
 
-        source = message.find('source').text
+            source = message.find('source').text
 
-        translation = message.find('translation').text
+            translation = message.find('translation').text
 
-        if translation and translation.strip():
-            # 3. replace string in file
+            if translation and translation.strip():
+                # 3. replace string in file
 
-            print("replacing:", active_filename, source, translation)
-            active_content = active_content.replace(source, translation)
+                print("replacing:", active_filename, source, translation)
+                active_content = active_content.replace(source, translation)
