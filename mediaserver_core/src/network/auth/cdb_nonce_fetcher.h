8642480a209e008adc2043ee2c2e4e2909e541e2
Sergey Ivanov <sivanov@networkoptix.com>
2016-02-09 16:01:52 +0300

    VS 2015 compile fixes
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- mediaserver_core/src/network/auth/cdb_nonce_fetcher.h
+++ mediaserver_core/src/network/auth/cdb_nonce_fetcher.h
@@ -72,7 +72,7 @@ private:
     TimerManager::TimerGuard m_timerID;
     std::random_device m_rd;
     std::default_random_engine m_randomEngine;
-    std::uniform_int_distribution<char> m_nonceTrailerRandomGenerator;
+    std::uniform_int_distribution<short> m_nonceTrailerRandomGenerator;
 
     void fetchCdbNonceAsync();
     void gotNonce(nx::cdb::api::ResultCode resCode, nx::cdb::api::NonceData nonce);
