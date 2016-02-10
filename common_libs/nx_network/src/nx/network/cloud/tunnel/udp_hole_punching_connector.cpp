d20582c6ff41aae1f7989258d5d83417351d53ba
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-02-10 14:54:27 +0300

    #CLOUD-26 draft implementing udp hole punching connector
    <==== Mercurial data ====>
    Branch: dev_3.0

--- common_libs/nx_network/src/nx/network/cloud/tunnel/udp_hole_punching_connector.cpp
+++ common_libs/nx_network/src/nx/network/cloud/tunnel/udp_hole_punching_connector.cpp
@@ -5,15 +5,27 @@
 
 #include "udp_hole_punching_connector.h"
 
+#include <nx/utils/log/log.h>
+#include <nx/utils/log/log_message.h>
+
+#include <utils/serialization/lexical.h>
+
 
 namespace nx {
 namespace network {
 namespace cloud {
 
+using namespace nx::hpm;
+
+//TODO #ak this is a temporary address
+const char* mediatorEndpoint = "cloud-demo.hdw.mx:3345";
+
 UdpHolePunchingTunnelConnector::UdpHolePunchingTunnelConnector(
     AddressEntry targetHostAddress)
 :
-    m_targetHostAddress(std::move(targetHostAddress))
+    m_targetHostAddress(std::move(targetHostAddress)),
+    m_connectSessionID(QnUuid::createUuid().toByteArray()),
+    m_mediatorUdpClient(mediatorEndpoint)
 {
 }
 
@@ -34,12 +46,49 @@ void UdpHolePunchingTunnelConnector::connect(
         SystemError::ErrorCode errorCode,
         std::unique_ptr<AbstractTunnelConnection>)> handler)
 {
-    //TODO #ak
+    NX_LOGX(lm("connect to %1 with timeout %2 ms, connect session %3").
+        arg(m_targetHostAddress.host.toString()).arg(timeout.count()).
+        arg(m_connectSessionID),
+        cl_logDEBUG2);
+
+    m_completionHandler = std::move(handler);
+
+    api::ConnectRequest connectRequest;
+    connectRequest.originatingPeerID = QnUuid::createUuid().toByteArray();
+    connectRequest.connectSessionID = m_connectSessionID;
+    connectRequest.connectionMethods = api::ConnectionMethod::udpHolePunching;
+    connectRequest.destinationHostName = m_targetHostAddress.host.toString().toUtf8();
+    using namespace std::placeholders;
+    m_mediatorUdpClient.connect(
+        connectRequest,
+        std::bind(&UdpHolePunchingTunnelConnector::onConnectResponse, this, _1, _2));
 }
     
 const AddressEntry& UdpHolePunchingTunnelConnector::targetPeerAddress() const
 {
     return m_targetHostAddress;
+}
+
+void UdpHolePunchingTunnelConnector::onConnectResponse(
+    api::ResultCode resultCode,
+    api::ConnectResponse response)
+{
+    //if failed, reporting error
+    if (resultCode != api::ResultCode::ok)
+    {
+        NX_LOGX(lm("mediator reported %1 error code on connect request to %2").
+            arg(QnLexical::serialized(resultCode)).
+            arg(m_targetHostAddress.host.toString().toUtf8()),
+            cl_logDEBUG1);
+        return;
+    }
+
+    //if succeeded - initiating cross-nat UDT connection
+}
+
+void UdpHolePunchingTunnelConnector::holePunchingDone()
+{
+    //TODO #ak reporting result to mediator
 }
 
 } // namespace cloud
