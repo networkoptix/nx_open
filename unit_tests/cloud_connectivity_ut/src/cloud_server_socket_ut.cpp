5e8f9db73d8187018404cf69f90b949cc6f1f871
Mikhail Uskov <muskov@networkoptix.com>
2016-02-12 18:31:40 +0300

    CLOUD-168 CloudServerSocketStressTcpTest enhancements
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- unit_tests/cloud_connectivity_ut/src/cloud_server_socket_ut.cpp
+++ unit_tests/cloud_connectivity_ut/src/cloud_server_socket_ut.cpp
@@ -10,8 +10,12 @@ namespace nx {
 namespace network {
 namespace cloud {
 
-/** Accepts usual TCP connections */
-struct FakeTcpTunnelConnection : AbstractTunnelConnection
+/**
+ * Accepts usual TCP connections
+ */
+struct FakeTcpTunnelConnection
+:
+    AbstractTunnelConnection
 {
     FakeTcpTunnelConnection(
         String remotePeerId, SocketAddress address, size_t sockets = 1000)
@@ -41,36 +45,43 @@ struct FakeTcpTunnelConnection : AbstractTunnelConnection
     void accept(SocketHandler handler) override
     {
         if (m_sockets == 0)
-            return m_server->post([handler]()
-            {
-                handler(SystemError::connectionReset, nullptr, false);
-            });
+            return m_server->post(
+                [handler]()
+                {
+                    handler(SystemError::connectionReset, nullptr, false);
+                });
 
-        m_server->acceptAsync([this, handler](
-            SystemError::ErrorCode c, AbstractStreamSocket* s)
-        {
-            EXPECT_EQ(c, SystemError::noError);
-            --m_sockets;
-            handler(c, std::unique_ptr<AbstractStreamSocket>(s), m_sockets);
-        });
+        m_server->acceptAsync(
+            [this, handler](
+                SystemError::ErrorCode c, AbstractStreamSocket* s)
+            {
+                EXPECT_EQ(c, SystemError::noError);
+                --m_sockets;
+                handler(c, std::unique_ptr<AbstractStreamSocket>(s), m_sockets);
+            });
     }
 
     void pleaseStop(std::function<void()> handler) override
     {
-        m_server->pleaseStop([this, handler]
-        {
-            NX_LOGX(lm("exhausted"), cl_logDEBUG1);
-            m_server.reset();
-            handler();
-        });
+        m_server->pleaseStop(
+            [this, handler]
+            {
+                NX_LOGX(lm("exhausted"), cl_logDEBUG1);
+                m_server.reset();
+                handler();
+            });
     }
 
     size_t m_sockets;
     std::unique_ptr<AbstractStreamServerSocket> m_server;
 };
 
-/** Creates several \class FakeTcpTunnelConnection */
-struct FakeTcpTunnelAcceptor : public AbstractTunnelAcceptor
+/**
+ * Creates several \class FakeTcpTunnelConnection
+ */
+struct FakeTcpTunnelAcceptor
+:
+    public AbstractTunnelAcceptor
 {
     FakeTcpTunnelAcceptor(
         String remotePeerId, boost::optional<SocketAddress> address,
@@ -109,9 +120,13 @@ struct FakeTcpTunnelAcceptor : public AbstractTunnelAcceptor
     std::unique_ptr<AbstractCommunicatingSocket> m_ioThreadSocket;
 };
 
-/** Creates @param nAcceptors @class FakeTcpTunnelAcceptor(s) instead of real ones */
+/**
+ * Creates @param nAcceptors @class FakeTcpTunnelAcceptor(s) instead of real ones
+ */
 template<quint16 nAcceptors>
-struct CloudServerSocketTcpTester: CloudServerSocket
+struct CloudServerSocketTcpTester
+:
+    CloudServerSocket
 {
     CloudServerSocketTcpTester(): CloudServerSocket(nullptr) {}
 
@@ -137,7 +152,8 @@ struct CloudServerSocketTcpTester: CloudServerSocket
 
 template<typename ServerSocket>
 struct CloudServerSocketTcpTest
-    : public ::testing::Test
+:
+    public ::testing::Test
 {
 protected:
     std::unique_ptr<ServerSocket> makeServerSocket()
@@ -184,12 +200,12 @@ TEST(CloudServerSocketBaseTcpTest, OpenTunnelOnIndication)
         stun::cc::indications::connectionRequested, ::testing::_)).Times(1);
 
     std::vector<CloudServerSocket::AcceptorMaker> acceptorMakers;
-    acceptorMakers.push_back([](
-        const String&, hpm::api::ConnectionRequestedEvent& event)
-    {
-        return std::make_unique<FakeTcpTunnelAcceptor>(
-            event.originatingPeerID, network::test::kServerAddress);
-    });
+    acceptorMakers.push_back(
+        [](const String&, hpm::api::ConnectionRequestedEvent& event)
+        {
+            return std::make_unique<FakeTcpTunnelAcceptor>(
+                event.originatingPeerID, network::test::kServerAddress);
+        });
 
     auto server = std::make_unique<CloudServerSocket>(
         std::make_shared<hpm::api::MediatorServerTcpConnection>(
@@ -201,11 +217,13 @@ TEST(CloudServerSocketBaseTcpTest, OpenTunnelOnIndication)
     // there is not tunnel yet
     testTunnelConnect(SystemError::connectionRefused);
 
-    stun::Message message(stun::Header(
-        stun::MessageClass::indication, stun::cc::indications::connectionRequested));
     hpm::api::ConnectionRequestedEvent event;
     event.connectSessionId = String("someSessionId");
     event.originatingPeerID = String("somePeerId");
+
+    stun::Message message(stun::Header(
+        stun::MessageClass::indication,
+        stun::cc::indications::connectionRequested));
     event.serialize(&message);
     stunAsyncClient->emulateIndication(message);
 
@@ -217,8 +235,12 @@ TEST(CloudServerSocketBaseTcpTest, OpenTunnelOnIndication)
 static size_t kThreadCount = 5;
 static size_t kClientCount = 15;
 static size_t kPeerCount = 3;
+static size_t kQueueLimit = 10;
+static size_t kRetryDelay = 100;
 
-class CloudServerSocketStressTcpTest: public ::testing::Test
+class CloudServerSocketStressTcpTest
+:
+    public ::testing::Test
 {
 protected:
     void SetUp() override
@@ -234,57 +256,74 @@ protected:
             stun::cc::indications::connectionRequested, ::testing::_)).Times(1);
 
         std::vector<CloudServerSocket::AcceptorMaker> acceptorMakers;
-        acceptorMakers.push_back([this](
-            const String&, hpm::api::ConnectionRequestedEvent& event)
-        {
-            const auto it = m_peerAddresses.find(event.originatingPeerID);
-            assert(it != m_peerAddresses.end());
+        acceptorMakers.push_back(
+            [this](const String&, hpm::api::ConnectionRequestedEvent& event)
+            {
+                const auto it = m_peerAddresses.find(event.originatingPeerID);
+                assert(it != m_peerAddresses.end());
 
-            return std::make_unique<FakeTcpTunnelAcceptor>(
-                it->first, it->second, kClientCount);
-        });
+                return std::make_unique<FakeTcpTunnelAcceptor>(
+                    it->first, it->second, kClientCount);
+            });
 
         m_server = std::make_unique<CloudServerSocket>(
             std::make_shared<hpm::api::MediatorServerTcpConnection>(
                 m_stunClient, nullptr),
             std::move(acceptorMakers));
         ASSERT_TRUE(m_server->setNonBlockingMode(true));
-        ASSERT_TRUE(m_server->listen(100));
+        ASSERT_TRUE(m_server->listen(10));
         acceptServerForever();
     }
 
     void acceptServerForever()
     {
-        m_server->acceptAsync([this](
-            SystemError::ErrorCode c, AbstractStreamSocket* s)
-        {
+        m_server->acceptAsync(
+            [this](SystemError::ErrorCode code, AbstractStreamSocket* socket)
             {
-                QnMutexLocker lock(&m_mutex);
-                m_acceptedSockets.push_back(
-                    std::unique_ptr<AbstractStreamSocket>(s));
-            }
+                ASSERT_EQ(code, SystemError::noError);
+                acceptServerForever();
+                socket->sendAsync(
+                    network::test::kTestMessage,
+                    [](SystemError::ErrorCode code, size_t size)
+                    {
+                        ASSERT_EQ(code, SystemError::noError);
+                        ASSERT_EQ(size, network::test::kTestMessage.size());
+                    });
 
-            ASSERT_EQ(c, SystemError::noError);
-            acceptServerForever();
-        });
+                {
+                    QnMutexLocker lock(&m_mutex);
+                    m_acceptedSockets.push_back(
+                        std::unique_ptr<AbstractStreamSocket>(socket));
+                }
+            });
     }
 
     void startClientThread(size_t clientCount)
     {
-        std::thread thread([this, clientCount]()
-        {
-            for (int i = 0; i < clientCount; ++i)
-                for (auto it = m_peerAddresses.begin();
-                    it != m_peerAddresses.end(); ++it)
-                        connectClient(it);
-        });
+        std::thread thread(
+            [this, clientCount]()
+            {
+                for (int i = 0; i < clientCount; ++i)
+                    for (auto it = m_peerAddresses.begin();
+                        it != m_peerAddresses.end(); ++it)
+                            connectClient(it);
+            });
 
         m_threads.push_back(std::move(thread));
     }
 
+    struct Counters
+    {
+        size_t indications, retries;
+
+        Counters(size_t i = 0, size_t r = 0): indications(i), retries(r) {}
+        Counters addIndication() const { return {indications + 1, retries }; }
+        Counters addRetry() const { return {indications, retries + 1 }; }
+    };
+
     void connectClient(
         std::map<String, SocketAddress>::iterator peer,
-        std::shared_ptr<size_t> counter = std::make_shared<size_t>(0))
+        Counters counter = Counters())
     {
         AbstractStreamSocket* socket;
         {
@@ -298,21 +337,21 @@ protected:
             m_connectSockets.emplace(socket, std::move(socketPtr));
         }
 
-        socket->connectAsync(peer->second, [this, socket, peer, counter](
-            SystemError::ErrorCode c)
-        {
+        socket->connectAsync(
+            peer->second,
+            [this, socket, peer, counter](SystemError::ErrorCode code)
             {
-                QnMutexLocker lock(&m_mutex);
-                m_connectSockets.erase(socket);
-            }
+                if (code != SystemError::noError)
+                    return retryOnClient(
+                        socket,
+                        [this, peer, counter]()
+                        {
+                            requestAcceptor(peer->first);
+                            connectClient(peer, counter.addIndication());
+                        });
 
-            if (c == SystemError::noError)
-                return m_connectedResutls.push(*counter);
-
-            ++(*counter);
-            requestAcceptor(peer->first);
-            connectClient(peer, counter);
-        });
+                readOnClient(socket, peer, counter);
+            });
     }
 
     void requestAcceptor(String peerId)
@@ -331,23 +370,80 @@ protected:
         m_stunClient->emulateIndication(message);
     }
 
+    void readOnClient(
+        AbstractStreamSocket* socket,
+        std::map<String, SocketAddress>::iterator peer,
+        Counters counter)
+    {
+        auto buffer = std::make_shared<Buffer>();
+        buffer->reserve(network::test::kTestMessage.size() + 1);
+        socket->readSomeAsync(
+            buffer.get(),
+            [this, socket, peer, counter, buffer](
+                SystemError::ErrorCode code, size_t size)
+            {
+                if (code != SystemError::noError ||
+                    size != network::test::kTestMessage.size())
+                {
+                    return retryOnClient(
+                        socket,
+                        [this, peer, counter]()
+                        {
+                            connectClient(peer, counter.addRetry());
+                        });
+                }
+
+                {
+                    QnMutexLocker lock(&m_mutex);
+                    m_connectSockets.erase(socket);
+                }
+
+                EXPECT_EQ(*buffer, network::test::kTestMessage);
+                m_connectedResutls.push(counter);
+            });
+    }
+
+    void retryOnClient(AbstractStreamSocket* socket, std::function<void()> action)
+    {
+        socket->registerTimer(
+            300, [this, socket, action = std::move(action)]()
+            {
+                {
+                    QnMutexLocker lock(&m_mutex);
+                    m_connectSockets.erase(socket);
+                }
+
+                action();
+            });
+    }
+
     void TearDown() override
     {
         const auto kTotalConnects = kThreadCount * kClientCount * kPeerCount;
-        size_t indicationsCount = 0;
+        Counters totalCounters;
         for (int i = 0; i < kTotalConnects; ++i)
-            indicationsCount += m_connectedResutls.pop();
+        {
+            const auto c = m_connectedResutls.pop();
+            totalCounters.indications += c.indications;
+            totalCounters.retries += c.retries;
+        }
 
-        EXPECT_GE(indicationsCount, kPeerCount);
+        EXPECT_GE(totalCounters.indications, kPeerCount * kThreadCount);
+        EXPECT_GE(totalCounters.retries, kTotalConnects / kQueueLimit);
+
         m_server->pleaseStopSync();
         for (auto& thread : m_threads)
             thread.join();
+
+        EXPECT_GE(m_acceptedSockets.size(), kTotalConnects);
+        for (auto& socket : m_acceptedSockets)
+            socket->pleaseStopSync();
     }
 
     std::map<String, SocketAddress> m_peerAddresses;
     std::shared_ptr<network::test::StunAsyncClientMock> m_stunClient;
     std::unique_ptr<AbstractStreamServerSocket> m_server;
-    SyncQueue<size_t> m_connectedResutls;
+    SyncQueue<Counters> m_connectedResutls;
     std::vector<std::thread> m_threads;
 
     QnMutex m_mutex;
