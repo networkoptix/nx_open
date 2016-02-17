e5c9044dc597ea4c5e255344c202be3434c455c3
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-02-17 15:06:00 +0300

    added unit test project for nx_utils
    <==== Mercurial data ====>
    Branch: x_CLOUD-213_move_only_func

---
+++ unit_tests/utils_ut/src/move_only_func_ut.cpp
@@ -0,0 +1,27 @@
+
+#include <gtest/gtest.h>
+
+#include <future>
+
+#include <boost/optional.hpp>
+
+#include <nx/utils/move_only_func.h>
+
+
+TEST(MoveOnlyFunc, common)
+{
+    for (int i = 0; i < 100; ++i)
+    {
+        nx::utils::MoveOnlyFunc< void() > handler;
+        boost::optional< std::promise< bool > > promise;
+        int y = rand();
+        {
+            promise = std::promise< bool >();
+            handler = [&, y]() { promise->set_value(true); };
+            int x = 0;
+        }
+
+        handler();
+        ASSERT_TRUE(promise->get_future().get());
+    }
+}
