f969723505be952b3669bd5fea0f44fe93d8fb03
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-02-17 15:42:37 +0300

    added ability to pass -T
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- build-server-deb.sh
+++ build-server-deb.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-mvn package -e --pl=build_variables,nx_sdk,nx_storage_sdk,mediaserver,mediaserver_core,common,appserver2,common_libs/nx_email,common_libs/nx_streaming,common_libs/nx_network,common_libs/nx_utils,nx_cloud/cloud_db_client,debsetup/mediaserver-deb
+set -x
+if [ -n "$1" ]; then
+    threads="-T $1"
+else
+    threads=""
+fi
+projects=build_variables,nx_sdk,nx_storage_sdk,common,appserver2,common_libs/nx_email,common_libs/nx_streaming,common_libs/nx_network,common_libs/nx_utils,nx_cloud/cloud_db_client,mediaserver_core,mediaserver,debsetup/mediaserver-deb
+mvn package -e $threads --pl=$projects
