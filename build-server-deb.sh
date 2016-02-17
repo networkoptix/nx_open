72e87652be9d8d43435bc8ecb603c9ec58b23a8b
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-02-17 15:11:44 +0300

    unnecessary commend removed
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- build-server-deb.sh
+++ build-server-deb.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#mvn package -e --pl=build_environment,build_variables,nx_sdk,nx_storage_sdk,mediaserver,mediaserver_core,common,appserver2,common_libs/nx_email,common_libs/nx_streaming,common_libs/nx_network,common_libs/nx_utils,nx_cloud/cloud_db_client
 mvn package -e --pl=build_variables,nx_sdk,nx_storage_sdk,mediaserver,mediaserver_core,common,appserver2,common_libs/nx_email,common_libs/nx_streaming,common_libs/nx_network,common_libs/nx_utils,nx_cloud/cloud_db_client,debsetup/mediaserver-deb
