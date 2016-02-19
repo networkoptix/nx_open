a46b6af445212d1b04492e9e70171472941baf20
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2016-02-19 19:53:36 +0300

    VMS-2085: Client version and HW statistics
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- client/src/ui/statistics/modules/graphics_statistics_module.cpp
+++ client/src/ui/statistics/modules/graphics_statistics_module.cpp
@@ -1,14 +1,16 @@
 
 #include "graphics_statistics_module.h"
 
-#include <ui/workbench/workbench_context.h>
+#include <utils/common/app_info.h>
 
+#include <statistics/base/functor_metric.h>
 #include <statistics/base/metrics_container.h>
+#include <ui/graphics/opengl/gl_functions.h>
+#include <ui/workbench/workbench_context.h>
 #include <ui/statistics/modules/private/avg_tabs_count_metric.h>
 #include <ui/statistics/modules/private/camera_fullscreen_metric.h>
 #include <ui/statistics/modules/private/preview_search_duration_metric.h>
 #include <ui/statistics/modules/private/motion_search_duration_metric.h>
-#include <ui/statistics/modules/private/version_metric.h>
 
 QnGraphicsStatisticsModule::QnGraphicsStatisticsModule(QObject *parent)
     : base_type(parent)
@@ -17,23 +19,74 @@ QnGraphicsStatisticsModule::QnGraphicsStatisticsModule(QObject *parent)
 {
     const auto avgTabsCount = QnAbstractMetricPtr(
         new AvgTabsCountMetric(context()->workbench()));
+
     const auto psearchDuration = QnAbstractMetricPtr(
         new PreviewSearchDurationMetric(context()->workbench()));
 
-    m_metrics->addMetric(lit("avg_tabs_cnt"), avgTabsCount);
-    m_metrics->addMetric(lit("psearch_duration_ms"), psearchDuration);
-
     const auto cameraFullscreenMetric = QnAbstractMetricPtr(
         new CameraFullscreenMetric(context()->display()));
-    m_metrics->addMetric(lit("camera_fullscreen_duration_ms"), cameraFullscreenMetric);
 
     const auto msearchDuration = QnAbstractMetricPtr(
         new MotionSearchDurationMetric(context()));
-    m_metrics->addMetric(lit("msearch_duration_ms"), msearchDuration);
 
-    const auto versionMetric = QnAbstractMetricPtr(
-        new VersionMetric());
+    const auto versionMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::applicationVersion();
+    });
+
+    const auto archMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::applicationArch();
+    });
+
+    const auto platformMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::applicationPlatform();
+    });
+
+    const auto platformModificationMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::applicationPlatformModification();
+    });
+
+    const auto revisionMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::applicationRevision();
+    });
+
+    const auto customizationMetric = QnFunctorMetric::create([]()
+    {
+        return QnAppInfo::customizationName();
+    });
+
+    const auto glVersionMetric = QnFunctorMetric::create([]()
+    {
+        return QnGlFunctions::openGLCachedInfo().version;
+    });
+
+    const auto glRendererMetric = QnFunctorMetric::create([]()
+    {
+        return QnGlFunctions::openGLCachedInfo().renderer;
+    });
+
+    const auto glVendorMetric = QnFunctorMetric::create([]()
+    {
+        return QnGlFunctions::openGLCachedInfo().vendor;
+    });
+
+    m_metrics->addMetric(lit("avg_tabs_cnt"), avgTabsCount);
+    m_metrics->addMetric(lit("psearch_duration_ms"), psearchDuration);
+    m_metrics->addMetric(lit("camera_fullscreen_duration_ms"), cameraFullscreenMetric);
+    m_metrics->addMetric(lit("msearch_duration_ms"), msearchDuration);
     m_metrics->addMetric(lit("version"), versionMetric);
+    m_metrics->addMetric(lit("arch"), archMetric);
+    m_metrics->addMetric(lit("platform"), platformMetric);
+    m_metrics->addMetric(lit("platform_modification"), platformModificationMetric);
+    m_metrics->addMetric(lit("revision"), revisionMetric);
+    m_metrics->addMetric(lit("customization"), customizationMetric);
+    m_metrics->addMetric(lit("gl_version"), glVersionMetric);
+    m_metrics->addMetric(lit("gl_renderer"), glRendererMetric);
+    m_metrics->addMetric(lit("gl_vendor"), glVendorMetric);
 }
 
 QnGraphicsStatisticsModule::~QnGraphicsStatisticsModule()
@@ -41,7 +94,6 @@ QnGraphicsStatisticsModule::~QnGraphicsStatisticsModule()
 
 QnStatisticValuesHash QnGraphicsStatisticsModule::values() const
 {
-
     return m_metrics->values();
 }
 
