6531c3f2b62ba2af1d977f0d7269ecd3b53739c4
Denis Klychkov <dklychkov@networkoptix.com>
2016-03-01 12:44:16 +0300

    VMS-2122: Limit known server URLs.
    <==== Mercurial data ====>
    Branch: dev_2.5.0_gui

--- client/src/ui/workbench/watchers/workbench_server_address_watcher.h
+++ client/src/ui/workbench/watchers/workbench_server_address_watcher.h
@@ -1,23 +1,12 @@
-#ifndef SERVER_ADDRESS_WATCHER_H
-#define SERVER_ADDRESS_WATCHER_H
-
 #include <QtCore/QObject>
 #include <ui/workbench/workbench_context_aware.h>
 
-struct QnModuleInformation;
-class SocketAddress;
-
-class QnWorkbenchServerAddressWatcher : public QObject, public QnWorkbenchContextAware {
+class QnWorkbenchServerAddressWatcher : public QObject, public QnWorkbenchContextAware
+{
     Q_OBJECT
 public:
-    explicit QnWorkbenchServerAddressWatcher(QObject *parent = 0);
-
-private slots:
-    void at_moduleFinder_moduleUrlFound(const QnModuleInformation &moduleInformation, const SocketAddress &address);
-    void at_ec_connection_established();
+    explicit QnWorkbenchServerAddressWatcher(QObject* parent = nullptr);
 
 private:
-    QSet<QUrl> m_urls;
+    QList<QUrl> m_urls;
 };
-
-#endif // SERVER_ADDRESS_WATCHER_H
