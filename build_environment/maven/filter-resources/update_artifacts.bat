2086e7172b470a2c4f27c7c5ccacf54bee272af0
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-03-01 16:24:06 +0300

    fixed mswin build
    <==== Mercurial data ====>
    Branch: x_CLOUD-223_same_aio_thread

--- build_environment/maven/filter-resources/update_artifacts.bat
+++ build_environment/maven/filter-resources/update_artifacts.bat
@@ -9,17 +9,23 @@ set QT_PATH=%SOURCE%/buildenv/${qt.path}
 set BUILDENVDIR=%cd%
 set RSYNC=%BUILDENVDIR%\rsync-win32\rsync -rtvL --chmod=ugo=rwx
 
-mkdir %BUILDENVDIR%\${qt.path}
-cd %BUILDENVDIR%\${qt.path}
+set qtdir=%BUILDENVDIR%\${qt.path}
+rem replacing / with \
+set qtdir=%qtdir:/=\%
+
+mkdir %qtdir%
+cd %qtdir%
 
 @setlocal enableextensions enabledelayedexpansion
 @echo off
 set debug_mode="${qt.debug}"
+cd ..
 if not x%debug_mode:pdb=%==x%debug_mode% (
     %RSYNC% --delete rsync://%QT_PATH% .
 ) else (
     %RSYNC% --delete --exclude '*.pdb' rsync://%QT_PATH% .
 )
+cd %qtdir%
 
 if not x%debug_mode:src=%==x%debug_mode% (
     cd %BUILDENVDIR%\artifacts\qt\${qt.version}
