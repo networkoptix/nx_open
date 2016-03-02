22bf58f5cd037f1daf977c26eb11620769ee4d01
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-03-01 18:23:55 -0800

    building common with cmake
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

---
+++ common/resources/filter/common-specifics.pro
@@ -0,0 +1,20 @@
+exists( ${libdir}/libcreateprocess.pri ) {
+  include( ${libdir}/libcreateprocess.pri )
+}
+
+mac:!ios {
+  OBJECTIVE_SOURCES += ${basedir}/src/utils/mac_utils.mm
+  LIBS += -lobjc -framework Foundation -framework AppKit
+}
+
+!win32 {
+  ext_debug2.target  = $(DESTDIR)$(TARGET).debug
+  ext_debug2.depends = $(DESTDIR)$(TARGET)
+  ext_debug2.commands = $$QMAKE_OBJCOPY --only-keep-debug $(DESTDIR)/$(TARGET) $(DESTDIR)/$(TARGET).debug; $(STRIP) -g $(DESTDIR)/$(TARGET); $$QMAKE_OBJCOPY --add-gnu-debuglink=$(DESTDIR)/$(TARGET).debug $(DESTDIR)/$(TARGET); touch $(DESTDIR)/$(TARGET).debug
+
+  ext_debug.depends = $(DESTDIR)$(TARGET).debug
+
+  QMAKE_EXTRA_TARGETS += ext_debug ext_debug2
+}
+
+SOURCES += ${project.build.directory}/app_info_impl.cpp
---
+++ cpp/resources/filter/version.h
@@ -0,0 +1,95 @@
+//
+// This file is generated. Go to pom.xml.
+//
+#pragma once
+
+/* 
+ * Some defines from the build system. Using them directly strongly not recommended.
+ * 
+ * DO NOT USE THEM IN YOUR CODE. 
+ * DO NOT ADD NEW CONSTANTS HERE.
+ *
+ * All projects that are depend on "common" should use <utils/common/app_info.h> instead.
+ * The QN_APPLICATION_NAME should be read from QCoreApplication::applicationName().
+ *
+ * No new variables should be added here. The major part of the file will be wiped out asap.
+ */
+#define QN_BETA                         "${beta}"
+#define QN_ORGANIZATION_NAME            "${company.name}"
+#define QN_APPLICATION_NAME             "${PROJECT_LONGNAME}"
+#define QN_APPLICATION_DISPLAY_NAME     "${PROJECT_DISPLAY_LONGNAME}"
+#define QN_APPLICATION_VERSION          "${release.version}"
+#ifdef _WIN32
+#   define QN_PRODUCT_NAME              "${product.name}"
+#else
+#   define QN_PRODUCT_NAME              "${namespace.additional}"
+#endif
+#define QN_PRODUCT_NAME_SHORT           "${product.name.short}"
+#define QN_ENGINE_VERSION               "${release.version}"
+#define QN_APPLICATION_REVISION         "${changeSet}"
+#define QN_APPLICATION_PLATFORM         "${platform}"
+#define QN_APPLICATION_ARCH             "${arch}"
+#define QN_APPLICATION_COMPILER         "${additional.compiler}"
+#define QN_APPLICATION_MODIFICATION     "${modification}"
+#define QN_FFMPEG_VERSION               "${ffmpeg.version}"
+#define QN_SIGAR_VERSION                "${sigar.version}"
+#define QN_BOOST_VERSION                "${boost.version}"
+#define QN_CUSTOMIZATION_NAME           "${customization}"
+#define QN_MEDIA_FOLDER_NAME            "${client.mediafolder.name}"
+#ifdef _WIN32
+#   define QN_CLIENT_EXECUTABLE_NAME    "${product.name}.exe"
+#   define QN_APPLAUNCHER_EXECUTABLE_NAME "${product.name} Launcher.exe" // TODO: #Boris probably there exists a variable for this?
+#else
+#   define QN_CLIENT_EXECUTABLE_NAME    "client-bin"
+#   define QN_APPLAUNCHER_EXECUTABLE_NAME "applauncher-bin"
+#endif
+#define QN_LICENSING_MAIL_ADDRESS       "${company.license.address}"
+#define QN_COMPANY_URL                  "${company.url}"
+#define QN_SUPPORT_MAIL_ADDRESS         "${company.support.address}"
+#define QN_SUPPORT_LINK                 "${company.support.link}"
+#define QN_FREE_LICENSE_COUNT           ${freeLicenseCount}
+#define QN_FREE_LICENSE_KEY             "${freeLicenseKey}"
+#define QN_FREE_LICENSE_IS_TRIAL        ${freeLicenseIsTrial}
+#define QN_SHOWCASE_URL                 "${showcase.url}/${customization}"
+#define QN_SETTINGS_URL                 "${settings.url}/${customization}.json"
+#define QN_PRODUCT_NAME_LONG            "${display.product.name}"
+#define QN_BUILDENV_PATH                "${environment.dir}"
+#define QN_MIRRORLIST_URL               "${mirrorListUrl}"
+#define QN_HELP_URL                     "${helpUrl}/${customization}/${majorVersion}/${minorVersion}/url"
+#define QN_ARM_BOX                      "${box}"
+#define QN_IOS_PLAYBUTTON_TINT          "${ios.playButton.tint}"
+#define QN_IOS_SHARED_GROUP_ID          "${ios.group_identifier}"
+#define NX_EC_EC2_PROTO_VERSION         ${nxec.ec2ProtoVersion}
+
+#define QN_AX_CLASS_ID                  "${ax.classId}"
+#define QN_AX_INTERFACE_ID              "${ax.interfaceId}"
+#define QN_AX_EVENTS_ID                 "${ax.eventsId}"
+#define QN_AX_TYPELIB_ID                "${ax.typeLibId}"
+#define QN_AX_APP_ID                    "${ax.appId}"
+
+#define QN_REALM                        "${deb.customization.company.name}"
+
+/* 
+ * These constants are here for windows resource file.
+ *
+ * DO NOT USE THEM IN YOUR CODE. 
+ * DO NOT ADD NEW CONSTANTS HERE.
+ */
+#define VER_LINUX_ORGANIZATION_NAME     "${deb.customization.company.name}" // TODO: #Elric move up
+#define VER_FILEVERSION                 ${majorVersion},${minorVersion},${incrementalVersion},${buildNumber}
+#define VER_FILEVERSION_STR             "${release.version}.${buildNumber}"
+#define VER_PRODUCTVERSION              ${majorVersion},${minorVersion},${incrementalVersion}
+#define VER_PRODUCTVERSION_STR          "${full.version}"
+#define VER_COMPANYNAME_STR             "${company.name}"
+#define VER_FILEDESCRIPTION_STR         "${PROJECT_LONGNAME}"
+#define VER_INTERNALNAME_STR            "${PROJECT_DISPLAY_LONGNAME}"
+#define VER_LEGALCOPYRIGHT_STR          "Copyright (c) 2011-2016 Network Optix"
+#define VER_LEGALTRADEMARKS1_STR        "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR        VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR        "${project.artifactId}.exe"
+#define VER_PRODUCTNAME_STR             "${project.artifactId}"
+#define VER_COMPANYDOMAIN_STR           "${company.url}"
+
+#define IDI_ICON_APPLICATION            101
+#define IDI_ICON_VIDEOWALL              102
+/* Dear Developer. Please add new constants to the code block above (QN_* definitions). Thank you. */
