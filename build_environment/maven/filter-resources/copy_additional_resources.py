70766f5f1eda63e91df339664d11240e728de3db
Sergey Ivanov <sivanov@networkoptix.com>
2016-03-02 17:16:22 +0300

    Cleanup qt copying
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

--- build_environment/maven/filter-resources/copy_additional_resources.py
+++ build_environment/maven/filter-resources/copy_additional_resources.py
@@ -1,44 +1,4 @@
-import io, sys, os, errno, platform, shutil, fnmatch, tarfile, subprocess
-from os.path import dirname, join, exists, isfile
-
-basedir = join(dirname(os.path.abspath(__file__)))
-sys.path.append(basedir + '/' + '../..')
-
-from main import get_environment_variable, cd
-
-def make_list(string):
-    return [v.strip() for v in string.split(',') if v]
-
-qtlibs = make_list("""${qtlibs}""") + make_list("""${additional.qtlibs}""")
-
-qtplugins = make_list("""${qtplugins}""") + make_list("""${additional.qtplugins}""")
-
-qtbasedir = '${qt.dir}'
-
-def get_platform():
-    if sys.platform == 'win32':
-        return 'windows'
-    elif sys.platform == 'linux2':
-        return 'linux'
-    elif sys.platform == 'darwin':
-        return 'macosx'   
-
-def get_build_arch():
-    if '${arch}'.startswith('arm'):
-        return '${arch}'
-    else:
-        if platform.architecture()[0] == '64bit':
-            return 'x64'
-        else:
-            return 'x86'            
-        
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+import sys, os, tarfile, subprocess
 
 def extract(tar_url, extract_path='.'):
     print 'Extracting Archive: %s' % tar_url
@@ -58,11 +18,11 @@ def extract(tar_url, extract_path='.'):
 
 if __name__ == '__main__':        
 
-    buildenv = join('${project.build.directory}', 'donotrebuild')
+    buildenv = os.path.join('${project.build.directory}', 'donotrebuild')
     if os.path.exists(buildenv): 
         os.unlink(buildenv)
 
-    buildvar = join('${root.dir}/build_variables/target', 'donotrebuild')
+    buildvar = os.path.join('${root.dir}/build_variables/target', 'donotrebuild')
     if os.path.exists(buildvar): 
         os.unlink(buildvar)
 
@@ -74,119 +34,6 @@ if __name__ == '__main__':
         if file.endswith('tar.gz'):
             print(file)
             extract(file)
-
-    print '+++++++++++++++++++++ COPYING QT LIBS +++++++++++++++++++++'
-            
-    if get_platform() == 'windows':        
-        arch = '${arch}'
-        plugin_source_dir = '%s/plugins' % qtbasedir
-        lib_source_dir = '%s/bin' % qtbasedir
-        target_dir = join('${project.build.directory}', arch, 'bin')
-        lib_target_dir = join('${project.build.directory}', arch, 'lib')
-        help_dir = join('${project.build.directory}', arch, 'bin/help')
-            
-        #if os.path.exists(help_dir):
-        #    shutil.rmtree(help_dir)
-        #    shutil.copytree(join(basedir, 'help'), help_dir)
-
-        if not os.path.exists(target_dir):
-            mkdir_p(target_dir)        
-        
-        for qtlib in qtlibs:
-            if qtlib != '':
-                for file in os.listdir(lib_source_dir):
-                    if fnmatch.fnmatch(file, 'qt5%sd.dll' % qtlib) or fnmatch.fnmatch(file, 'qt5%sd.pdb' % qtlib):
-                        print (join(lib_source_dir, file))
-                        shutil.copy2(join(lib_source_dir, file), join(target_dir, 'debug'))
-                    elif fnmatch.fnmatch(file, 'qt5%s.dll' % qtlib) or fnmatch.fnmatch(file, 'qt5%s.pdb' % qtlib):
-                        print (join(lib_source_dir, file))
-                        shutil.copy2(join(lib_source_dir, file), join(target_dir, 'release'))    
-        
-        for qtplugin in qtplugins:
-            for config in ('debug', 'release'):
-                if not os.path.exists(join(target_dir, config, qtplugin)):
-                    os.makedirs (join(target_dir, config, qtplugin))    
-            
-            if qtplugin != '':
-                print join(plugin_source_dir, qtplugin)
-                plugin_dir = join(plugin_source_dir, qtplugin)
-                if not os.path.isdir(plugin_dir):
-                    continue
-                for file in os.listdir(plugin_dir):
-                    if fnmatch.fnmatch(file, 'q*d.*'):
-                        shutil.copy2(join(plugin_source_dir, qtplugin, file), join(target_dir, 'debug', qtplugin))
-                    else:
-                        shutil.copy2(join(plugin_source_dir, qtplugin, file), join(target_dir, 'release', qtplugin))
-                        
-        for config in ('debug', 'release'):
-            target_plugins = join(target_dir, config, 'plugins')
-            if not os.path.exists(target_plugins):
-                os.makedirs(join(target_dir, config, 'plugins'))
-            for file in os.listdir(lib_source_dir):
-                if fnmatch.fnmatch(file, 'icu*.dll') or fnmatch.fnmatch(file, 'lib*.dll'):
-                    print (join(lib_source_dir, file))
-                    shutil.copy2(join(lib_source_dir, file), join(target_dir, config))                    
-    else:     
-        lib_source_dir = '${qt.dir}/lib'
-        lib_target_dir = join('${project.build.directory}', 'lib')
-        plugin_source_dir = '${qt.dir}/plugins'	
-        target_dir = join('${project.build.directory}', 'bin')
-        help_dir = join('${project.build.directory}', 'bin/help')
-        
-    #    if os.path.exists(help_dir):
-    #        shutil.rmtree(help_dir)
-    #        shutil.copytree('help', help_dir)    
-        
-        if get_platform() == 'linux':
-            for qtlib in qtlibs:
-                if qtlib != '': 
-                    for file in os.listdir(lib_source_dir):            
-                        for config in ('debug', 'release'):
-                            if fnmatch.fnmatch(file, 'libQt5%s.so.*' % qtlib) or fnmatch.fnmatch(file, 'libicu*.so*'):
-                                print (join(lib_source_dir, file))
-                                srcname = join(lib_source_dir, file)
-                                dstname = join(lib_target_dir, config, file)
-                                if os.path.exists(dstname):
-                                    os.unlink(dstname)
-                                if os.path.islink(srcname):
-                                    linkto = os.readlink(srcname)
-                                    os.symlink(linkto, dstname)
-                                else:
-                                    shutil.copy2(srcname, dstname)       
-        elif get_platform() == 'macosx':
-            for qtlib in qtlibs:
-                if qtlib != '': 
-                    for root, dirs, files in os.walk(lib_source_dir):
-                        for dir in dirs:
-                            if dir.endswith('Qt%s.framework' % qtlib):
-                                print (join(dir))
-                                shutil.copy2(join(lib_source_dir, dir, 'Versions/5', 'Qt%s_debug' % qtlib), join(lib_target_dir, 'debug'))
-                                shutil.copy2(join(lib_source_dir, dir, 'Versions/5', 'Qt%s' % qtlib), join(lib_target_dir, 'release'))                                      
-        else: print '+++++++++++++++++++++++ Could not recognize platform +++++++++++++++++++++++'
-                                  
-        for config in ('debug', 'release'):
-            for qtplugin in qtplugins:
-                if qtplugin != '':
-                    source = join(plugin_source_dir, qtplugin)
-                    target = join(target_dir, config, qtplugin)
-                    print target
-                    if os.path.exists(target):
-                        shutil.rmtree(target)
-                    if os.path.exists(source):
-                        shutil.copytree(source, target)                          
-
-    for config in ('debug', 'release'):
-        print(join(target_dir, 'qml'))
-        if os.path.exists(join(target_dir, config, 'qml')):
-            shutil.rmtree(join(target_dir, config, 'qml'))
-        shutil.copytree(join(qtbasedir, 'qml'), join(target_dir, config, 'qml'))
-
-    if platform == "windows":
-        for root, dirs, files in os.walk(join(join(target_dir, 'release', 'qml'))):
-            for file in files:
-                if fnmatch.fnmatch(file, '*.pdb') or fnmatch.fnmatch(file, '*d.dll'):
-                    print("Deleting %s" % join(root, file))
-                    os.unlink(join(root, file))
 
     if not os.path.exists(buildenv): 
         open(buildenv, 'w').close() 
