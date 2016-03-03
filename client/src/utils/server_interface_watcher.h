b1558eec4fce0999902be8b19c10b3d0e4833217
Denis Klychkov <dklychkov@networkoptix.com>
2016-03-03 17:57:45 +0300

    Backed out changeset d10233179731
    <==== Mercurial data ====>
    Branch: prod_2.5

--- client/src/utils/server_interface_watcher.h
+++ client/src/utils/server_interface_watcher.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SERVER_INTERFACE_WATCHER_H
+#define SERVER_INTERFACE_WATCHER_H
 
 #include <QtCore/QObject>
 
@@ -15,8 +16,9 @@ public:
 private slots:
     void at_connectionChanged(const QnModuleInformation &moduleInformation);
     void at_resourcePool_statusChanged(const QnResourcePtr &resource);
-    void at_initialResourcesReceived();
 
 private:
     void updatePriaryInterface(const QnMediaServerResourcePtr &server);
 };
+
+#endif // SERVER_INTERFACE_WATCHER_H
