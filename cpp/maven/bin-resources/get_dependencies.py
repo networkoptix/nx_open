33496a4b0c60244a092bc1a564ebeda7a9e446b9
Denis Klychkov <dklychkov@networkoptix.com>
2016-03-04 14:45:31 +0300

    Don't fetch packages in rdpack file exists in the build dir.
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

--- cpp/maven/bin-resources/get_dependencies.py
+++ cpp/maven/bin-resources/get_dependencies.py
@@ -12,11 +12,6 @@ sys.path.pop(0)
 
 debug = "debug" in dependencies.BUILD_CONFIGURATION
 
-def fetch_packages(packages):
-    versioned = [dependencies.get_versioned_package_name(package) for package in packages]
-    return rdep.fetch_packages(dependencies.TARGET, versioned, debug)
-
-
 #Supports templates such as bin/*.dll
 def copy_recursive(src, dst):
     if "*" in src:
@@ -64,22 +59,6 @@ def install_dependency(dependency_dir, target_dir, debug):
         print "Copying {0} to {1}".format(src, bin_dst)
         copy_recursive(src, bin_dst)
 
-def copy_package_for_configuration(package, path, debug):
-    target_dir = dependencies.TARGET_DIRECTORY
-
-    installation_marker = (package if not debug else package + "-debug") + rdep.PACKAGE_CONFIG_NAME
-
-    description_file = os.path.join(target_dir, installation_marker)
-
-    if os.path.isfile(description_file):
-        return True
-
-    print "Copying package {0} into {1}".format(package, target_dir)
-    install_dependency(path, target_dir, debug)
-
-    shutil.copy(os.path.join(path, rdep.PACKAGE_CONFIG_NAME), description_file)
-    return True
-
 def get_deps_pri_file(debug = False):
     return "dependencies-debug.pri" if debug else "dependencies.pri"
 
@@ -87,40 +66,42 @@ def append_pri(pri_file, debug = False):
     with open(get_deps_pri_file(debug), "a") as file:
         file.write("include({0})\n".format(pri_file))
 
-def copy_package(package):
+def get_package_for_configuration(package, debug):
+    target_dir = dependencies.TARGET_DIRECTORY
     versioned = dependencies.get_versioned_package_name(package)
-    release_path = rdep.locate_package(dependencies.TARGET, versioned)
-    if not release_path:
+
+    installation_marker = (package if not debug else package + "-debug") + rdep.PACKAGE_CONFIG_NAME
+    description_file = os.path.join(target_dir, installation_marker)
+    installed = os.path.isfile(description_file)
+
+    if not installed:
+        print "Fetching package {0}".format(package)
+        rdep.fetch_packages(dependencies.TARGET, [ versioned ], debug)
+
+    location = rdep.locate_package(dependencies.TARGET, versioned)
+    if not location:
         print "Could not locate {0}".format(versioned)
         return False
 
-    copy_package_for_configuration(versioned, release_path, False)
+    if not installed:
+        print "Copying package {0} into {1}".format(package, target_dir)
+        install_dependency(location, target_dir, debug)
 
-    pri_file = os.path.join(release_path, package + ".pri")
-    if os.path.isfile(pri_file):
-        append_pri(pri_file)
+        pri_file = os.path.join(location, package + ".pri")
+        if os.path.isfile(pri_file):
+            append_pri(pri_file, debug)
+
+        shutil.copy(os.path.join(location, rdep.PACKAGE_CONFIG_NAME), description_file)
+
+    return True
+
+def get_package(package):
+    if not get_package_for_configuration(package, False):
+        return False
 
     if debug:
-        debug_path = rdep.locate_package(dependencies.TARGET, versioned, debug)
-        if not debug_path:
-            print "Could not locate {0}".format(versioned)
+        if not get_package_for_configuration(package, True):
             return False
-
-        if debug_path == release_path:
-            print "No debug package found. Using release package {0}".format(versioned)
-
-            copy_package_for_configuration(versioned, release_path, True)
-
-            if os.path.isfile(pri_file):
-                append_pri(pri_file, True)
-
-            return True
-
-        copy_package_for_configuration(versioned, debug_path, True)
-
-        debug_pri_file = os.path.join(debug_path, package + "-debug.pri")
-        if os.path.isfile(debug_pri_file):
-            append_pri(debug_pri_file, True)
 
     return True
 
@@ -140,14 +121,10 @@ def get_dependencies():
 
     dependencies.print_configuration()
 
-    if not fetch_packages(packages):
-        print "Cannot fetch packages"
-        sys.exit(1)
-
-    if not copy_packages(packages):
-        print "Cannot copy packages"
-        sys.exit(1)
-
+    for package in packages:
+        if not get_package(package):
+            print "Cannot get package {0}".format(package)
+            sys.exit(1)
 
 if __name__ == '__main__':
     get_dependencies()
