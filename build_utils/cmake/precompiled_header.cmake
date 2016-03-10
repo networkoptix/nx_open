d9b3d76e071c8ab8941e15297149cfd1dcf64998
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-03-09 23:18:03 -0800

    CMake:Using custom precompiled headers in all projects. No more cotire
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

--- build_utils/cmake/precompiled_header.cmake
+++ build_utils/cmake/precompiled_header.cmake
@@ -1,22 +1,16 @@
-function(ADD_MSVC_PRECOMPILED_HEADER file_name result_name)
+MACRO(ADD_MSVC_PRECOMPILED_HEADER PrecompiledHeader PrecompiledSource SourcesVar)
   IF(MSVC)
     GET_FILENAME_COMPONENT(PrecompiledBasename ${PrecompiledHeader} NAME_WE)
-    SET(PrecompiledBinary "$(IntDir)${PrecompiledBasename}.pch")
+    SET(PrecompiledBinary "$(IntDir)$(TargetName).pch")
     SET(Sources ${${SourcesVar}})
-
-    SET_SOURCE_FILES_PROPERTIES(${PrecompiledSource}
-                                PROPERTIES COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
-                                           OBJECT_OUTPUTS "${PrecompiledBinary}")
+    
     SET_SOURCE_FILES_PROPERTIES(${Sources}
                                 PROPERTIES COMPILE_FLAGS "/Yu\"${PrecompiledHeader}\" /FI\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
                                            OBJECT_DEPENDS "${PrecompiledBinary}")  
+    SET_SOURCE_FILES_PROPERTIES(${PrecompiledSource}
+                                PROPERTIES COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
+                                           OBJECT_OUTPUTS "${PrecompiledBinary}")                                           
     # Add precompiled header to SourcesVar
     LIST(APPEND ${SourcesVar} ${PrecompiledSource})
   ENDIF(MSVC)
-endfunction()
-
-function(precompiled_header)
-  if(MSVC)
-    ADD_MSVC_PRECOMPILED_HEADER("src/StdAfx.h" "src/StdAfx.cpp" CPP_FILES)
-  endif()
-endfunction()
+ENDMACRO(ADD_MSVC_PRECOMPILED_HEADER)
