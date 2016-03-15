b696b3b72adcc2bbd440be597ccc3c8e62a849d2
Denis Klychkov <dklychkov@networkoptix.com>
2016-03-15 14:36:23 +0300

    RDep to cmake integration
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

---
+++ build_utils/cmake/external_dependencies.cmake
@@ -0,0 +1,34 @@
+function(get_package_version package result)
+    set(${result} ${${package}.version} PARENT_SCOPE)
+endfunction()
+
+function(get_external_dependencies)
+    set(target "linux-x64")
+
+    set(args --target ${target}
+             --target-dir ${CMAKE_BINARY_DIR}/target
+             --deps-file cmake)
+    if(NOT CMAKE_BUILD_TYPE MATCHES Release)
+        list(APPEND args --debug)
+    endif()
+
+    foreach(package ${ARGN})
+        set(version ${${package}.version})
+        if(version)
+            list(APPEND args ${package}-${version})
+        else()
+            list(APPEND args ${package})
+        endif()
+    endforeach()
+
+    execute_process(
+            COMMAND ${PYTHON_EXECUTABLE}
+                    ${PYTHON_MODULES_DIR}/rdep_dependencies.py
+                    ${args}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            RESULT_VARIABLE result)
+
+    if(NOT ${result} EQUAL 0)
+        message(FATAL_ERROR "Could not get dependencies.")
+    endif()
+endfunction()
