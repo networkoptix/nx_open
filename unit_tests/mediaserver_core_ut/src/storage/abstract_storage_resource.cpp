b41caa242d2c12caf2fd4512de0c16cfac6242bf
Mikhail Uskov <muskov@networkoptix.com>
2016-03-16 14:52:38 +0300

    mediaserver_core_ut compile fixes
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- unit_tests/mediaserver_core_ut/src/storage/abstract_storage_resource.cpp
+++ unit_tests/mediaserver_core_ut/src/storage/abstract_storage_resource.cpp
@@ -418,7 +418,7 @@ TEST(Storage_load_balancing_algorithm_test, Main) {
     if (!qnCommon) {
         commonModule = std::unique_ptr<QnCommonModule>(new QnCommonModule);
     }
-    commonModule->setModuleGUID("{A680980C-70D1-4545-A5E5-72D89E33648B}");
+    commonModule->setModuleGUID(QnUuid("{A680980C-70D1-4545-A5E5-72D89E33648B}"));
 
     std::unique_ptr<QnStorageManager> storageManager;
     if (!qnNormalStorageMan) {
@@ -446,17 +446,17 @@ TEST(Storage_load_balancing_algorithm_test, Main) {
 
     QnStorageResourcePtr storage1 = QnStorageResourcePtr(new MockStorageResource1); 
     storage1->setUrl("url1");
-    storage1->setId("{45FF0AD9-649B-4EDC-B032-13603EA37077}"); 
+    storage1->setId(QnUuid("{45FF0AD9-649B-4EDC-B032-13603EA37077}"));
     storage1->setUsedForWriting(true);
 
     QnStorageResourcePtr storage2 = QnStorageResourcePtr(new MockStorageResource2);
     storage2->setUrl("url2");
-    storage2->setId("{22E3AD7E-F4E7-4AE5-AD70-0790B05B4566}"); 
+    storage2->setId(QnUuid("{22E3AD7E-F4E7-4AE5-AD70-0790B05B4566}"));
     storage2->setUsedForWriting(true);
 
     QnStorageResourcePtr storage3 = QnStorageResourcePtr(new MockStorageResource3);
     storage3->setUrl("url3");
-    storage3->setId("{30E7F3EA-F4DB-403F-B9DD-66A38DA784CF}"); 
+    storage3->setId(QnUuid("{30E7F3EA-F4DB-403F-B9DD-66A38DA784CF}"));
     storage3->setUsedForWriting(true);
 
     qnNormalStorageMan->addStorage(storage1);
