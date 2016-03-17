acd5146bf01a1dd63535c820856557aa117e87de
Alexander Kulikov <akulikov@networkoptix.com>
2016-03-17 20:43:11 +0300

    #vms-2194. Transcoders initialization fixed.
    <==== Mercurial data ====>
    Branch: dev_2.5

--- common/src/transcoding/transcoder.cpp
+++ common/src/transcoding/transcoder.cpp
@@ -181,7 +181,9 @@ QnTranscoder::QnTranscoder():
     m_initialized(false),
     m_eofCounter(0),
     m_packetizedMode(false),
-    m_useRealTimeOptimization(false)
+    m_useRealTimeOptimization(false),
+    m_initializedAudio(false),
+    m_initializedVideo(false)
 {
     QThread::currentThread()->setPriority(QThread::LowPriority); 
 }
@@ -398,7 +400,34 @@ int QnTranscoder::transcodePacket(const QnConstAbstractMediaDataPtr& media, QnBy
                        m_delayedAudioQueue.isEmpty() ? QnConstCompressedAudioDataPtr() : m_delayedAudioQueue.first());
         if (rez != 0)
             return rez;
+
         m_initialized = true;
+
+        if (!m_delayedVideoQueue.isEmpty())
+            m_initializedVideo = true;
+
+        if (!m_delayedAudioQueue.isEmpty())
+            m_initializedAudio = true;
+    }
+
+    if (media->dataType == QnAbstractMediaData::AUDIO && !m_initializedAudio)
+    {
+        int rez = open(QnCompressedVideoDataPtr(),
+                       std::dynamic_pointer_cast<const QnCompressedAudioData>(media));
+        if (rez != 0)
+            return rez;
+
+        m_initializedAudio = true;
+    }
+
+    if (media->dataType == QnAbstractMediaData::VIDEO && !m_initializedVideo)
+    {
+        int rez = open(std::dynamic_pointer_cast<const QnCompressedVideoData>(media),
+                       QnCompressedAudioDataPtr());
+        if (rez != 0)
+            return rez;
+
+        m_initializedVideo = true;
     }
 
     if( result )
