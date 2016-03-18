2058b424c81e8c3fd97347c4a4ec0c978069409e
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-03-18 13:34:59 +0300

    AIOThread class implementation moved to cpp file
    <==== Mercurial data ====>
    Branch: dev_2.6.0

---
+++ common_libs/nx_network/src/nx/network/aio/event_type.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+
+namespace nx {
+namespace network {
+namespace aio {
+
+enum EventType
+{
+    etNone = 0,
+    etRead = 1,
+    etWrite = 2,
+    //!Error occured on socket. Output only event. To get socket error code use \a Socket::getLastError
+    etError = 4,
+    //!Used for periodic operations and for socket timers
+    etTimedOut = 8,
+    etReadTimedOut = etRead | etTimedOut,
+    etWriteTimedOut = etWrite | etTimedOut,
+    etMax = 9
+};
+
+const char* toString(EventType eventType);
+
+}   //aio
+}   //network
+}   //nx
