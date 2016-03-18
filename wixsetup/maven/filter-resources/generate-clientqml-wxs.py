2ea8b4cbc18c792b305b638e95b725255e4be6ba
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-03-17 17:26:17 -0700

    installer. Fixed. Works
    <==== Mercurial data ====>
    Branch: dev_2.6.0

--- wixsetup/maven/filter-resources/generate-clientqml-wxs.py
+++ wixsetup/maven/filter-resources/generate-clientqml-wxs.py
@@ -1,8 +1,9 @@
 import os, sys, subprocess
 from subprocess import Popen, PIPE
 
+#TODO: use qtbinpatcher to minimize the amount of QML files
 if __name__ == '__main__':
-    p = subprocess.Popen(r'heat dir ${ClientQmlDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientQml.wxs -cg ClientQmlDir -dr ClientQml -var var.ClientQmlDir', shell=True, stdout=PIPE)
+    p = subprocess.Popen(r'heat dir ${ClientQmlDir} -wixvar -nologo -sfrag -suid -sreg -ag -srd -dir WebHelp -out ClientQml.wxs -cg ClientQmlComponent -dr ClientQml -var var.ClientQmlDir', shell=True, stdout=PIPE)
     out, err = p.communicate()
     print out
     p.wait()
