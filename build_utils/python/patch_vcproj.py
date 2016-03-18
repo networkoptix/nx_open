adf99744388fdc4baf02397bb57c2af088829ce7
Sergey Ivanov <sivanov@networkoptix.com>
2016-03-18 13:52:37 +0300

    Cleanup mocables from vcproj files
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

--- build_utils/python/patch_vcproj.py
+++ build_utils/python/patch_vcproj.py
@@ -9,19 +9,38 @@ ms_namespace = "http://schemas.microsoft.com/developer/msbuild/2003"
 ET.register_namespace('', ms_namespace)
 namespaces_dict = {'ms' : ms_namespace}
 
+parent_map = {}
+
+def fix_mocables(root):
+    """Disabling moc preprocessor steps, since we do it with jom."""
+    #xpath = "./Project/ItemGroup/CustomBuild[contains(@Include,'.h')]"
+
+    xpath = "./ms:ItemGroup/ms:CustomBuild"
+    customBuildNodes = root.findall(xpath, namespaces_dict)
+
+    nodes = [node for node in customBuildNodes if node.get('Include').endswith('.h')]
+    if len(nodes) < 1:
+        print "Mocable nodes were not found"
+        return
+
+    print "Removing moc custom build steps"
+    nodesToRemove = [parent_map[node] for node in nodes]
+    for itemGroupNode in nodesToRemove:
+        root.remove(itemGroupNode)        
+
 def fix_qrc(root):
+    """Removing additional inputs from qrc, since we rebuild it manually."""
     #xpath = "./Project/ItemGroup/CustomBuild[contains(@Include,'.qrc')]/AdditionalInputs"
 
     xpath = "./ms:ItemGroup/ms:CustomBuild"
     customBuildNodes = root.findall(xpath, namespaces_dict)
 
-    nodes = [node for node in customBuildNodes if '.qrc' in node.get('Include')]
-    for node in nodes:
-        print node.get('Include')
-
+    nodes = [node for node in customBuildNodes if node.get('Include').endswith('.qrc')]
     if len(nodes) < 1:
         print "Qrc node was not found"
+        return
 
+    print "Removing AdditionalInputs from qrc custom build step"
     for node in nodes:
         inputsNodes = node.findall("./ms:AdditionalInputs", namespaces_dict)
         for inputNode in inputsNodes:
@@ -29,10 +48,14 @@ def fix_qrc(root):
 
 def patch_project(project):
     print "Patching {0}...".format(project)
-
     tree = ET.parse(project)
     root = tree.getroot()
+    
+    global parent_map
+    parent_map = {c:p for p in tree.iter() for c in p}
+    
     fix_qrc(root)
+    fix_mocables(root)
     tree.write(project, encoding="utf-8", xml_declaration=True)
 
 def main():
