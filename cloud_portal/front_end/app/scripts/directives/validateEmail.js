947dc36b8e3021f047b319c7c9b43335b5c187bf
Evgeny Balashov <ebalashov@networkoptix.com>
2016-03-23 19:46:50 +0300

    CLOUD-268: refactoring - remove console.log
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/front_end/app/scripts/directives/validateEmail.js
+++ cloud_portal/front_end/app/scripts/directives/validateEmail.js
@@ -6,15 +6,10 @@ angular.module('cloudApp').directive('validateEmail', function() {
     return {
         require: '?ngModel',
         link: function(scope, elm, attrs, ctrl) {
-            console.log("validate email");
             // only apply the validator if ngModel is present and Angular has added the email validator
             if (ctrl && ctrl.$validators.email) {
-
                 // this will overwrite the default Angular email validator
                 ctrl.$validators.email = function(modelValue) {
-
-                    console.log("validate email", modelValue);
-
                     return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
                 };
             }
