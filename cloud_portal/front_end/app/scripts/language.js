3003bd12c9747b6f3aca82676cc14c8306cf0926
Evgeny Balashov <ebalashov@networkoptix.com>
2016-03-23 17:43:55 +0300

    Forbid leading and trailing spaces in passwords
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/front_end/app/scripts/language.js
+++ cloud_portal/front_end/app/scripts/language.js
@@ -64,7 +64,7 @@ var L = {
     passwordRequirements:
     {
         minLengthMessage:'Password must contain at least 6 characters',
-        requiredMessage: 'Use only latin letters, numbers and keyboard symbols',
+        requiredMessage: 'Use only latin letters, numbers and keyboard symbols, avoid leading and trailing spaces.',
         weakMessage: 'Use numbers, symbols in different case and special symbols to make your password stronger',
         strongMessage: 'Strong password!',
         commonMessage: 'This password is in top most popular passwords in the world'
