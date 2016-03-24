457b2987f5bfc6795198845576288e9e8ea0a0ae
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2016-03-24 19:30:06 +0300

    CLOUD-274: Settings for mail notifier
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/front_end/protractor-conf.js
+++ cloud_portal/front_end/protractor-conf.js
@@ -1,30 +1,50 @@
 // An example configuration file.
 exports.config = {
-  // Do not start a Selenium Standalone sever - only run this using chrome.
+    // Do not start a Selenium Standalone sever - only run this using chrome.
 
-  //chromeOnly: true,
-  //chromeDriver: 'node_modules/protractor/selenium/chromedriver',
+    //chromeOnly: true,
+    //chromeDriver: 'node_modules/protractor/selenium/chromedriver',
 
-  baseUrl: 'http://127.0.0.1:9000',
+    baseUrl: 'http://127.0.0.1:9000',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-      'browserName': 'chrome',
-      'chromeOptions': {
-          'args': ['--test-type']
-      }
-  },
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['--test-type']
+        }
+    },
+    seleniumAddress: 'http://localhost:4444/wd/hub',
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['test/e2e/**/*spec.js'],
-  //specs: ['test/e2e/account/*spec.js'],
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['test/e2e/**/*spec.js'],
+    //specs: ['test/e2e/register/*spec.js'],
 
     // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  },
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    },
+
+    onPrepare: function () {
+
+        var notifier = require('mail-notifier');
+
+        var imap = {
+            user: "ekorneeva@networkoptix.com",
+            password: "qweasd!@#",
+            host: "imap.gmail.com",
+            port: 993,
+            tls: true,
+            tlsOptions: { rejectUnauthorized: true }
+        };
+
+        global.notifier = notifier(imap);
+    }
+    //
+    //onCleanUp: function () {
+    //    notifier.stop();
+    //}
 
 };
