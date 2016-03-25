fa2cc0ac2c69d70438927df65c80a99522ae492c
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-03-25 22:45:27 +0300

    #CLOUD-222 lowering max udt packet size to support l2tp
    <==== Mercurial data ====>
    Branch: x_CLOUD_222_nat_traversal

--- common_libs/udt/src/udt/core.cpp
+++ common_libs/udt/src/udt/core.cpp
@@ -99,7 +99,7 @@ CUDT::CUDT()
    initSynch();
 
    // Default UDT configurations
-   m_iMSS = 1500;
+   m_iMSS = kDefaultMtuSize;
    m_bSynSending = true;
    m_bSynRecving = true;
    m_iFlightFlagSize = 25600;
