9e05be1840312a64e1c8d9aa4ceed33fce8513bb
Denis Klychkov <dklychkov@networkoptix.com>
2016-03-25 14:03:08 +0300

    Place arpa/inet.h include back to stdafx
    <==== Mercurial data ====>
    Branch: dev_2.6.0_gui

--- common/src/StdAfx.h
+++ common/src/StdAfx.h
@@ -6,6 +6,8 @@
 #   define FD_SETSIZE 2048
 #   include <winsock2.h>
 #   include <windows.h> /* You HAVE to include winsock2.h BEFORE windows.h */
+#else
+#   include <arpa/inet.h>
 #endif
 
 #ifdef __cplusplus
