9f2a1b6579a57680728617f891eb37a2066fc554
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2016-04-05 14:26:08 +0300

    CLOUD-246: Autotests for systems list page: check open in Nx button
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/front_end/test/e2e/systems/po.js
+++ cloud_portal/front_end/test/e2e/systems/po.js
@@ -4,19 +4,13 @@ var SystemsListPage = function () {
     var Helper = require('../helper.js');
     this.helper = new Helper();
 
-    this.getHomePage = function () {
-        browser.get('/');
-        browser.waitForAngular();
-    };
-
-    this.get = function () {
-        browser.get('/#/systems');
-        browser.waitForAngular();
-    };
+    this.url = '/#/systems';
 
     this.htmlBody = element(by.css('body'));
-
     this.systemsList = element.all(by.repeater('system in systems'));
+    this.openInNxButton = element(by.buttonText('Open in Nx Witness'));
+    this.notActivatedLabel = element(by.cssContainingText('.label-danger', 'not activated'));
+
 };
 
 module.exports = SystemsListPage;
--- cloud_portal/front_end/test/e2e/systems/spec.js
+++ cloud_portal/front_end/test/e2e/systems/spec.js
@@ -5,7 +5,7 @@ describe('Systems list suite', function () {
     var p = new SystemsListPage();
 
     beforeAll(function() {
-        p.getHomePage();
+        p.helper.get('/');
         p.helper.login(p.helper.userEmail, p.helper.userPassword);
         console.log("Systems start");
     });
@@ -15,9 +15,11 @@ describe('Systems list suite', function () {
         console.log("Systems finish");
     });
 
-    it("should show list of Systems", function () {
-        p.get();
+    beforeEach(function() {
+        //p.helper.get(p.url);
+    });
 
+    it("should show list of Systems", function () {
         expect(browser.getCurrentUrl()).toContain('systems');
         expect(p.htmlBody.getText()).toContain('Systems');
 
@@ -25,39 +27,38 @@ describe('Systems list suite', function () {
     });
 
     it("should show Open in NX client button for every online system", function () {
-        p.get();
-        expect("test").toBe("written");
+        p.systemsList.each(function (element, index) {
+            browser.sleep(5000);
+            element.getInnerHtml().then(function(content) {
+                if(content.indexOf('not activated') <= -1) {
+                    expect(element.element(by.buttonText('Open in Nx Witness')).isDisplayed()).toBe(true);
+                }
+                console.log(index);
+            });
+        });
     });
-    xit("should оpen in NX client, when clicking 'Open in NX client' button", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should not open system page, when clicking 'Open in NX client'", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should not show Open in NX client button for offline system", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should show system's state for systems (activated, not activated, online, offline)", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should show system's owner for every system in the list, 'Your system' for user's systems", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should show email for every system, if system owner's name is empty", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should opens system page (users list) when clicked on system", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
-    xit("should update owner name in systems list, if it's changed", function () {
-        p.get();
-        expect("test").toBe("written");
-    });
+    //xit("should оpen in NX client, when clicking 'Open in NX client' button", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should not open system page, when clicking 'Open in NX client'", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should not show Open in NX client button for offline system", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should show system's state for systems (activated, not activated, online, offline)", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should show system's owner for every system in the list, 'Your system' for user's systems", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should show email for every system, if system owner's name is empty", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should opens system page (users list) when clicked on system", function () {
+    //    expect("test").toBe("written");
+    //});
+    //xit("should update owner name in systems list, if it's changed", function () {
+    //    expect("test").toBe("written");
+    //});
 });
