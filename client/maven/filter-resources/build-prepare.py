b12038bf185fefb561236905ec82aebf855cda6a
Sergey Ivanov <sivanov@networkoptix.com>
2016-04-06 18:50:44 +0300

    Help moved to RDep
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/maven/filter-resources/build-prepare.py
+++ client/maven/filter-resources/build-prepare.py
@@ -1,16 +1,10 @@
 import os, sys, posixpath, shutil
 sys.path.insert(0, '${basedir}/../common')
-#sys.path.insert(0, os.path.join('..', '..', 'common'))
 
-help_source_dir = '${ClientHelpSourceDir}'
 target_dir = '${libdir}'
 
 if '${platform}' == 'windows':
     for arch in ('x86', 'x64'):
-        help_dir = os.path.join('${libdir}', arch, 'bin/help')
-        if os.path.exists(help_dir):
-            shutil.rmtree(help_dir)
-        shutil.copytree(help_source_dir, help_dir)
         for config in ('debug', 'release'):
             target_plugins = os.path.join(target_dir, arch, 'bin', config, 'plugins')
             if os.path.exists(os.path.join(target_plugins, 'hw_decoding_conf.xml')):
@@ -19,9 +13,3 @@ if '${platform}' == 'windows':
                 if not os.path.exists(target_plugins):
                     os.makedirs(target_plugins)
                 shutil.copy2('${root.dir}/plugins/quicksyncdecoder/hw_decoding_conf.xml', target_plugins)
-else:     
-    help_dir = os.path.join('${libdir}', 'bin/help')
-    if os.path.exists(help_dir):
-        shutil.rmtree(help_dir)
-    shutil.copytree(help_source_dir, help_dir)
-    
