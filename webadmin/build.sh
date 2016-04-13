1e01fc0bee464e0a042891d5318fc5cf3114b5bf
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-04-12 17:23:52 -0700

    Webadmin - builds on Jenkins
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- webadmin/build.sh
+++ webadmin/build.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+#rvm install ruby-2.3.0
 rvm use 2.3.0 --default
+
+export PATH=$PATH:~/.rvm/bin:~/.rvm/rubies/ruby-2.3.0/bin:~/.rvm/gems/ruby-2.3.0/bin
+#sleep 2
+#echo $PATH
+#sleep 2
+echo "+++++++++++++++++++++++++ Running NPM... +++++++++++++++++++++++++"
+#gem install compass
+#sleep 2
 npm install
+if [ $? -gt 0 ]; then 
+   echo "+++++++++++++++++++++++++ NPM errorlevel:" $? "+++++++++++++++++++++++++"
+   exit $?
+fi
+#sleep 2
+echo "+++++++++++++++++++++++++ Running BOWER... +++++++++++++++++++++++++"
 bower install
+if [ $? -gt 0 ]; then
+   echo "+++++++++++++++++++++++++ BOWER errorlevel:" $? "+++++++++++++++++++++++++"
+   exit $?
+fi
+#sleep 2
+echo "+++++++++++++++++++++++++ Running GRUNT... +++++++++++++++++++++++++"
+cp -f ./target/version_maven.txt ./static
 grunt pub
+if [ $? -gt 0 ]; then
+   echo "+++++++++++++++++++++++++ GRUNT errorlevel:" $? "+++++++++++++++++++++++++"
+   exit $?
+fi
