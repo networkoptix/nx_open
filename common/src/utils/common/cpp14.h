6821739efae1fd9a5d02fdcbbf319d99aeb38ea7
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-04-14 19:30:59 +0300

    std::make_unique re-written using variadic template
    <==== Mercurial data ====>
    Branch: x_CLOUD-306_verify_connection_id

--- common/src/utils/common/cpp14.h
+++ common/src/utils/common/cpp14.h
@@ -34,57 +34,11 @@ namespace std
 
 #ifdef USE_OWN_MAKE_UNIQUE
 template<
-    typename T>
-    std::unique_ptr<T> make_unique()
-{
-    return std::unique_ptr<T>( new T );
-}
-
-template<
     typename T,
-    typename Arg1>
-    std::unique_ptr<T> make_unique( Arg1&& arg1 )
+    typename... Args
+> std::unique_ptr<T> make_unique(Args&&... args)
 {
-    return std::unique_ptr<T>( new T( std::forward<Arg1>( arg1 ) ) );
-}
-
-template<
-    typename T,
-    typename Arg1,
-    typename Arg2>
-    std::unique_ptr<T> make_unique( Arg1&& arg1, Arg2&& arg2 )
-{
-    return std::unique_ptr<T>( new T(
-        std::forward<Arg1>( arg1 ),
-        std::forward<Arg2>( arg2 ) ) );
-}
-
-template<
-    typename T,
-    typename Arg1,
-    typename Arg2,
-    typename Arg3>
-    std::unique_ptr<T> make_unique( Arg1&& arg1, Arg2&& arg2, Arg3&& arg3 )
-{
-    return std::unique_ptr<T>( new T(
-        std::forward<Arg1>( arg1 ),
-        std::forward<Arg2>( arg2 ),
-        std::forward<Arg3>( arg3 ) ) );
-}
-
-template<
-    typename T,
-    typename Arg1,
-    typename Arg2,
-    typename Arg3,
-    typename Arg4>
-    std::unique_ptr<T> make_unique( Arg1&& arg1, Arg2&& arg2, Arg3&& arg3, Arg4&& arg4 )
-{
-    return std::unique_ptr<T>( new T(
-        std::forward<Arg1>( arg1 ),
-        std::forward<Arg2>( arg2 ),
-        std::forward<Arg3>( arg3 ),
-        std::forward<Arg4>( arg4 ) ) );
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 #endif  //USE_OWN_MAKE_UNIQUE
 }   //std
