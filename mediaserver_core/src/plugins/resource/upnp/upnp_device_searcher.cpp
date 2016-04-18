0565fb3faa7ec47279f4866e1c8528b79404200c
Roman Vasilenko <rvasilenko@networkoptix.com>
2016-04-18 20:01:01 +0300

    (VMS-2369) - server crash at stop
    <==== Mercurial data ====>
    Branch: dev_2.5

--- mediaserver_core/src/plugins/resource/upnp/upnp_device_searcher.cpp
+++ mediaserver_core/src/plugins/resource/upnp/upnp_device_searcher.cpp
@@ -129,6 +129,9 @@ void UPNPDeviceSearcher::pleaseStop()
     }
     m_socketList.clear();
 
+    if(m_receiveSocket)
+        m_receiveSocket->terminateAsyncIO(true);
+
     //cancelling ongoing http requests
     //NOTE m_httpClients cannot be modified by other threads, since UDP socket processing is over and m_terminated == true
     for( auto it = m_httpClients.begin();
