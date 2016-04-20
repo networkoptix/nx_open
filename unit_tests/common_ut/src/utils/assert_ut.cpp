1a57a4161a44c44ff0506a068384cc2fe007525e
Mikhail Uskov <muskov@networkoptix.com>
2016-04-20 18:48:58 +0300

    minor unit test fixes
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/common_ut/src/utils/assert_ut.cpp
+++ unit_tests/common_ut/src/utils/assert_ut.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 #include <nx/utils/log/assert.h>
 
+#ifdef _DEBUG
 TEST(NxAssert, All3)
+#else
+TEST(NxAssert, DISABLED_All3)
+#endif
 {
     EXPECT_DEATH(NX_ASSERT(false), "");
     EXPECT_DEATH(NX_ASSERT(false, "oops"), "");
