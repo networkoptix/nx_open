126ff0ebf170e2b68d7ad2e7821faef9845b6cf8
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-04-21 19:32:14 +0300

    (VMS-2403) new file added to use it from python, some more variables added to current_config
    <==== Mercurial data ====>
    Branch: dev_3.0.0

---
+++ build_variables/maven/filter-resources/current_config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Some build dependent variables, usable for auto.py and other python scripts 
+ARCH="{arch}"
+BOX="{box}"
+CONFIGURATION="{build.configuration}"
+TARGET="{rdep.target}"
+PACKAGES_DIR="{packages.dir}"
+TARGET_DIR="{libdir}"
+QT_DIR="{qt.dir}"
+QT_LIB="{qt.dir}/lib"
+# for win - add QT_LIB to PATH
+# for linux - add QT_LIB to LD_LIBRARY_PATH
+
+LIB_PATH="${libdir}/lib/${build.configuration}"
+BIN_PATH="${libdir}/bin/${build.configuration}"
+WIN_PATH="${libdir}/${arch}/bin/${build.configuration}"
+
+from os import pathsep
+
+def _add_path(env, var, path)
+    if env.get(var,'') not in ('', None):
+        env[var] += pathsep + path
+    else:
+        env[var] = path
+
+def add_lib_path(env):
+    target = TARGET.lower()
+    if target.startswith('windows'):
+         _add_path(Env, 'PATH', QT_LIB)
+    else:
+        libs = pathsep.join((LIB_PATH, QT_LIB)
+        if target.startswith('macosx'):
+            _add_path(Env, 'DYLD_LIBRARY_PATH', libs)
+            _add_path(Env, 'DYLD_FRAMEWORK_PATH', libs)
+        else:
+            _add_path(Env, 'LD_LIBRARY_PATH', libs)
+
