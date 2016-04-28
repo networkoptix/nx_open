fedb9b15675202b1961c2d13c540b10a36ac3cae
Ivan Vigasin <ivigasin@networkoptix.com>
2016-04-28 16:43:30 -0700

    add dev configuration
    <==== Mercurial data ====>
    Branch: cloud_portal_default

--- cloud_portal/cloud/install.sh
+++ cloud_portal/cloud/install.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 
 echo "CREATE DATABASE nx_cloud;" | mysql
 python manage.py migrate
+
+# Add the following line to your .profile
+export CLOUD_PORTAL_CONF_DIR=$HOME/develop/cloud_portal/etc
