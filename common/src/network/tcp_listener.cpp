6aef461edeb2aa0f7ca15de10b095b904890ca8b
Alexander Kulikov <akulikov@networkoptix.com>
2016-05-11 14:05:48 +0300

    #vms-2494. Tcp Listener fix.
    <==== Mercurial data ====>
    Branch: dev_2.5

--- common/src/network/tcp_listener.cpp
+++ common/src/network/tcp_listener.cpp
@@ -284,13 +284,13 @@ void QnTcpListener::run()
             }
             doPeriodicTasks();
         }
-        NX_LOG( lit("TCPListener (%1:%2). Removing all connections before stop").arg(d->serverAddress.toString()).arg(d->localPort), cl_logWARNING );
     }
     catch( const std::exception& e )
     {
         NX_LOG( lit("Exception in TCPListener (%1:%2). %3").
             arg(d->serverAddress.toString()).arg(d->localPort).arg(QString::fromLatin1(e.what())), cl_logWARNING );
     }
+    NX_LOG( lit("TCPListener (%1:%2). Removing all connections before stop").arg(d->serverAddress.toString()).arg(d->localPort), cl_logWARNING );
     removeAllConnections();
     NX_LOG( lit("Exiting QnTcpListener::run. %1:%2").arg(d->serverAddress.toString()).arg(d->localPort), cl_logWARNING );
 }
