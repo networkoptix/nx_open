1f53793bb1645444b6668a8e9ae123d1e3256e20
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-05-13 16:51:24 -0700

    Unit-tests run on Maven (need to pass -Punit-tests)
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-nx_vms_dir=`pwd`
-mkdir -p ../devtools 
-cd ../devtools 
-hg update || hg clone ssh://hg@hdw.mx/devtools .
-if [[ $? -ne 0 ]]; then exit $?; fi
-cd testing
-python auto.py -t -o -p $nx_vms_dir
-if [[ $? -ne 0 ]]; then exit $?; fi
+branch=$(hg branch)
+current_changeset=$(hg id -i)
+nx_vms_dir=$(pwd)
+latest_success_changeset=
+run_tests=true
+if [ -f $nx_vms_dir/.test/$branch ]; then 
+    latest_success_changeset=$(cat $nx_vms_dir/.test/$branch)
+    if [ $current_changeset == $latest_success_changeset ]; then run_tests=false; fi
+fi
+
+if [ $run_tests == "true" ]; then
+    mkdir -p ../devtools 
+    cd ../devtools 
+    hg update || hg clone ssh://hg@hdw.mx/devtools .
+    ERRORLEVEL=$?
+    if [[ $ERRORLEVEL -ne 0 ]]; then echo $ERRORLEVEL && exit $ERRORLEVEL; fi
+    cd testing
+    python auto.py -t -o -p $nx_vms_dir
+    ERRORLEVEL=$?
+    echo "Tests Finished with code " $ERRORLEVEL
+    if [[ $ERRORLEVEL -ne 0 ]]; then echo $ERRORLEVEL && exit $ERRORLEVEL; fi
+    mkdir -p $nx_vms_dir/.test
+    echo $current_changeset >> $nx_vms_dir/.test/$branch
+else
+    echo "This changeset has been already tested." $ERRORLEVEL
+    exit 0
+fi
