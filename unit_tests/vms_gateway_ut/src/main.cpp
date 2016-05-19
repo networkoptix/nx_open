b097ee1d4572a7aabd4df51aeba1f1986ba45e8f
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-05-19 22:14:44 +0300

    #CLOUD-358 added unit test project for vms_gateway
    <==== Mercurial data ====>
    Branch: dev_3.0.0

---
+++ unit_tests/vms_gateway_ut/src/main.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+* 8 sep 2014
+* a.kolesnikov
+***********************************************************/
+
+#include <gtest/gtest.h>
+
+#include <QCoreApplication>
+
+#include <nx/utils/log/log.h>
+#include <nx/network/socket_global.h>
+
+
+int main( int argc, char **argv )
+{
+    nx::network::SocketGlobalsHolder socketGlobalsInstance;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    for (int i = 0; i < argc; ++i)
+    {
+        std::string arg(argv[i]);
+        if (arg.find("--log=") == 0)
+            QnLog::initLog(QString::fromStdString(arg.substr(6)));
+    }
+
+    const int result = RUN_ALL_TESTS();
+    return result;
+}
