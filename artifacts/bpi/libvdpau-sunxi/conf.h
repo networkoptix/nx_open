da811c671a1e5e7ee87c62beba7deb5ba2209f22
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-05-20 21:14:00 +0300

    Tiny config flag description fix.
    <==== Mercurial data ====>
    Branch: master

--- artifacts/bpi/libvdpau-sunxi/conf.h
+++ artifacts/bpi/libvdpau-sunxi/conf.h
@@ -4,7 +4,7 @@
 #define CONFIG_BODY \
 { \
     NX_FLAG(0, enableOutput, "Log all calls to VDPAU interface functions."); \
-    NX_FLAG(0, enableColorKey, "Enable disp call DISP_CMD_SET_COLORKEY...)."); \
+    NX_FLAG(0, enableColorKey, "Enable disp call DISP_CMD_SET_COLORKEY..."); \
     NX_FLAG(0, enableIoctlOutput, "Log all disp calls: ioctl(/dev/disp, ...)."); \
     NX_FLAG(0, enableSetFb, "Use disp call DISP_CMD_LAYER_SET_FB instead of DISP_CMD_SET_PARA and DISP_CMD_LAYER_OPEN."); \
     NX_FLAG(0, enableXClearWindow, "Enable call to XClearWindow (was originally enabled)."); \
