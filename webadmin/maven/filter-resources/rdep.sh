29fd3b3a83a53fb9d1cc833b504f4389b40b9e25
Denis Klychkov <dklychkov@networkoptix.com>
2016-05-20 15:52:32 +0300

    Add force flag to webadmin upload script
    <==== Mercurial data ====>
    Branch: prod_3.0.0

--- webadmin/maven/filter-resources/rdep.sh
+++ webadmin/maven/filter-resources/rdep.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-export NX_REPOSITORY=$environment/packages
 mkdir -p ${environment.dir}/packages/any/server-external-${branch}-${release.version}/bin
 cp -f ${basedir}/external.dat ${environment.dir}/packages/any/server-external-${branch}-${release.version}/bin
 if [[ $? -ne 0 ]]; then exit $?; fi
-python ${root.dir}/build_utils/python/rdep.py -u -t any server-external-${branch}-${release.version} --root ${environment.dir}/packages
+python ${root.dir}/build_utils/python/rdep.py -u -f -t any server-external-${branch}-${release.version} --root ${environment.dir}/packages
 if [[ $? -ne 0 ]]; then exit $?; fi
 if [[ $branch == prod_${release.version} ]]; then 
     mkdir -p ${environment.dir}/packages/any/server-external-${release.version}/bin
     cp -f ${basedir}/external.dat ${environment.dir}/packages/any/server-external-${release.version}/bin
-    python ${root.dir}/build_utils/python/rdep.py -u -t any server-external-${release.version} --root ${environment.dir}/packages
+    python ${root.dir}/build_utils/python/rdep.py -u -f -t any server-external-${release.version} --root ${environment.dir}/packages
     if [[ $? -ne 0 ]]; then exit $?; fi
 fi
