b2f04e9b69b68a9c570599d84e6835be43fd16cd
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-05-20 17:24:31 +0300

    Updated flag_config hard-coded default to more suitable values; descriptions added.
    <==== Mercurial data ====>
    Branch: master

--- artifacts/bpi/libvdpau-sunxi/conf.h
+++ artifacts/bpi/libvdpau-sunxi/conf.h
@@ -3,12 +3,12 @@
 #define CONFIG_MODULE_NAME "vdpau_sunxi"
 #define CONFIG_BODY \
 { \
-    NX_FLAG(1, enableOutput, ""); \
-    NX_FLAG(0, enableColorKey, ""); \
-    NX_FLAG(1, enableIoctlOutput, ""); \
-    NX_FLAG(0, enableSetFb, ""); \
-    NX_FLAG(0, enableXClearWindow, ""); \
-    NX_FLAG(0, disableCscMatrix, ""); \
+    NX_FLAG(0, enableOutput, "Log all calls to VDPAU interface functions."); \
+    NX_FLAG(0, enableColorKey, "Enable disp call DISP_CMD_SET_COLORKEY...)."); \
+    NX_FLAG(0, enableIoctlOutput, "Log all disp calls: ioctl(/dev/disp, ...)."); \
+    NX_FLAG(0, enableSetFb, "Use disp call DISP_CMD_LAYER_SET_FB instead of DISP_CMD_SET_PARA and DISP_CMD_LAYER_OPEN."); \
+    NX_FLAG(0, enableXClearWindow, "Enable call to XClearWindow (was originally enabled)."); \
+    NX_FLAG(0, disableCscMatrix, "Disable disp calls for CSC Matrix params which also turn on Enhancement."); \
 }
 
 #include "flag_config_c.h"
