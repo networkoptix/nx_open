2fb472fca58c34a9649dcb555c804b80e85e24a6
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2016-05-23 18:04:04 +0300

    WebAdmin autotests: Navigation specs
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/test/e2e/navigation_spec.js
+++ webadmin/test/e2e/navigation_spec.js
@@ -1,29 +1,26 @@
 'use strict';
 
 describe('Navigation Menu', function() {
-
-
-    //it("should stop test",function(){expect("other test").toBe("uncommented");});return;
-
+    var navbarElems = element(by.css('.navbar-right')).all(by.css('a'));
 
     it('changes active link depending on route', function() {
-    browser.get('/');
+        var linkTexts = ['Web client','Settings', 'Information', 'For Developers', 'Help'];
+        browser.get('/#/settings');
+        browser.waitForAngular();
 
-    expect(element.all(by.css('.active')).first().getText()).toEqual('Web client');
-
-    var linkTexts = ['Web client','Settings', 'Information', 'For Developers', 'Help'];
-    for (var i = 0; i < linkTexts.length; i++) {
-      var linkText = linkTexts[i];
-
-      var link = element(by.linkText(linkText));
-      link.click();
-
-      expect(element(by.css('nav[role=navigation]')).element(by.css('.active')).getText()).toEqual(linkText);
-    }
-  });
+        navbarElems.each (function(element, index){
+            element.getText().then( function(text) {
+                if (text.length > 0) {
+                    expect(text).toContain(linkTexts[index]);
+                    element.click();
+                    expect(element.element(by.xpath('..')).getAttribute('class')).toContain('active');
+                }
+            });
+        });
+    });
 
     it('shows essential server info in title', function() {
-        var servertitle = element(by.css(".navbar-brand"));
-        expect(servertitle.getAttribute("title")).toMatch(/Server\sname\:\s[\d\w\s*].*\nVersion\:\s\d+\.\d+\.\d+\.\d+\s*[\d\w\s*].*\nIP\:\s(\d+\.\d+\.\d+\.\d+\s*)/); // add Version regex
+        var serverTitle = element(by.css(".navbar-brand"));
+        expect(serverTitle.getAttribute("title")).toMatch(/Server\sname\:\s[\d\w\s*].*\nVersion\:\s\d+\.\d+\.\d+\.\d+\s*[\d\w\s*].*\nIP\:\s(\d+\.\d+\.\d+\.\d+\s*)/); // add Version regex
     });
 });
