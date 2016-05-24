847079ed4136b31bee18b76c3924692f1f960b70
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-05-25 00:08:21 +0300

    Tiny config param description fix.
    <==== Mercurial data ====>
    Branch: master

--- artifacts/bpi/libvdpau-sunxi/conf.h
+++ artifacts/bpi/libvdpau-sunxi/conf.h
@@ -6,7 +6,7 @@
     NX_FLAG(0, enableOutput, "Log all calls to VDPAU interface functions."); \
     NX_FLAG(0, enableColorKey, "Enable disp call DISP_CMD_SET_COLORKEY..."); \
     NX_FLAG(0, enableIoctlOutput, "Log all disp calls: ioctl(/dev/disp, ...)."); \
-    NX_FLAG(0, enableSetFb, "Use disp call DISP_CMD_LAYER_SET_FB instead of DISP_CMD_SET_PARA and DISP_CMD_LAYER_OPEN."); \
+    NX_FLAG(0, enableSetFb, "Use disp call ...LAYER_SET_FB instead of ...SET_PARA and ...LAYER_OPEN."); \
     NX_FLAG(0, enableXClearWindow, "Enable call to XClearWindow (was originally enabled)."); \
     NX_FLAG(0, disableCscMatrix, "Disable disp calls for CSC Matrix params which also turn on Enhancement."); \
     NX_INT_PARAM(0, surfaceSleepUs, "Delay before switching to next surface, 0.."); \
