8cf9f6b2f3c2e22c3b1b44375a3381595b54687c
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-05-27 21:35:20 +0300

    flag_config updated to match nx_utils (fixed compiling on Android).
    <==== Mercurial data ====>
    Branch: master

--- artifacts/bpi/libvdpau-sunxi/flag_config.cpp
+++ artifacts/bpi/libvdpau-sunxi/flag_config.cpp
@@ -1,5 +1,7 @@
 #include "flag_config.h"
 
+#if !(defined(ANDROID) || defined(__ANDROID__))
+
 #include <string>
 #include <vector>
 #include <memory>
@@ -427,10 +429,8 @@ bool FlagConfig::Impl::parseIniFile()
         {
             for (const auto& param: m_params)
             {
-                std::string descr = param->descr;
-                if (descr.size() > 0)
-                    descr += " ";
-                file << "# " << descr << "Default: " << param->defaultValueStr() << "\n";
+                if (strlen(param->descr) > 0)
+                    file << "# " << param->descr << "\n";
                 file << param->name << "=" << param->defaultValueStr() << "\n";
                 file << "\n";
             }
@@ -553,3 +553,70 @@ void FlagConfig::skipNextReload()
 
 } // namespace utils
 } // namespace nx
+
+//-------------------------------------------------------------------------------------------------
+#else // ANDROID || __ANDROID__
+
+// Stub implementation which never changes hard-coded defaults.
+// NOTE: Android NDK does not support std::to_string
+
+namespace nx {
+namespace utils {
+
+struct FlagConfig::Impl
+{
+    const char* moduleName;
+};
+
+FlagConfig::FlagConfig(const char* moduleName)
+:
+    d(new Impl())
+{
+    d->moduleName = moduleName;
+}
+
+FlagConfig::~FlagConfig()
+{
+    delete d;
+}
+
+const char* FlagConfig::tempPath() const
+{
+    return "/tmp";
+}
+
+const char* FlagConfig::moduleName() const
+{
+    return d->moduleName;
+}
+
+bool FlagConfig::regFlagParam(
+    bool* pValue, bool defaultValue, const char* paramName, const char* descr)
+{
+    return defaultValue;
+}
+
+int FlagConfig::regIntParam(
+    int* pValue, int defaultValue, const char* paramName, const char* descr)
+{
+    return defaultValue;
+}
+
+const char* FlagConfig::regStringParam(
+    const char** pValue, const char* defaultValue, const char* paramName, const char* descr)
+{
+    return defaultValue;
+}
+
+void FlagConfig::reload()
+{
+}
+
+void FlagConfig::skipNextReload()
+{
+}
+
+} // namespace utils
+} // namespace nx
+
+#endif // ANDROID || __ANDROID__
