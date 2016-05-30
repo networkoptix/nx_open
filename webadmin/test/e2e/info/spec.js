2b327aa21c8294ae20324f2d7b2bbb65fe21eb17
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2016-05-30 17:32:28 +0300

    WEB-297: Web admin Info page autotests
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/test/e2e/info/spec.js
+++ webadmin/test/e2e/info/spec.js
@@ -2,55 +2,50 @@
 var Page = require('./po.js');
 describe('Information Page', function () {
 
-    //it("should stop test",function(){expect("other test").toBe("uncommented");});return;
+    var p = new Page();
 
-    var p = new Page(); 
-
-    browser.ignoreSynchronization = true;
+    beforeEach(function(){
+        p.get();
+    });
 
     it("should show software info",function(){
-
-        p.get();
-
-        browser.ignoreSynchronization = true;
         expect(p.versionNode.getText()).toMatch(/\d+\.\d+\.\d+.\d+/);//2.3.0.1234
-        expect(p.archNode.getText()).toMatch(/[\w\d]+/);;//x86,arm,x64
+        expect(p.archNode.getText()).toMatch(/[\w\d]+/);//x86,arm,x64
         expect(p.platformNode.getText()).toMatch(/\w+/); //linux,mac,windows
     });
 
-
     it("should show storage info",function(){
-
-        browser.ignoreSynchronization = true;
         expect(p.storagesNodes.count()).toBeGreaterThan(0); // storages exist
         expect(p.storagesNodes.first().element(by.css(".storage-url")).getText()).toMatch(/[\w\d/\\]+/); // Except one good url
         expect(p.storagesNodes.first().element(by.css(".storage-total-space")).getText()).toMatch(/[\d+\.?\d*\s*\wB]+/);// GB,  TB  - it's size
         expect(p.storagesNodes.first().element(by.css(".storage-indicators")).element(by.css("[title~=Internal]")).isDisplayed()).toBe(true); // At least one indicator
-
     });
 
     it("health monitoring: should display legend",function(){
+        //var EC = protractor.ExpectedConditions;
+        //var legend = $('.legend-checkbox');
+        //var isPresent = EC.presenceOf(legend);
+        browser.sleep(2000);
+
         browser.ignoreSynchronization = true;
 
-        var EC = protractor.ExpectedConditions;
-        // Waits for the .legend-checkbox to be present on the dom.
-        browser.wait(EC.presenceOf($(".legend-checkbox")), 5000);
-
+        //browser.wait(isPresent, 5000); //wait for an element to become present
+        expect(element(by.css('.legend-checkbox')).isPresent()).toBe(true);
         expect(p.hmLegendNodes.count()).toBeGreaterThan(1); //legend exists, except 2+ elements
+
+        browser.ignoreSynchronization = false;
     });
 
-    // it("health monitoring: should update graphics, should show message when server is offline, should enable-disable indicators using checkboxes",function(){
-    //     expect("health monitoring").toBe("tested manually");
-    // });
+     xit("health monitoring: should update graphics, should show message when server is offline, should enable-disable indicators using checkboxes",function(){
+         expect("health monitoring").toBe("tested manually");
+     });
 
     it("log: should show iframe",function(){
-        browser.ignoreSynchronization = true;
         expect(p.logIframe.isDisplayed()).toBe(true);
         expect(p.logIframe.getAttribute("src")).toMatch("/api/showLog");
     });
 
     it("log: should show refresh link to update iframe",function(){
-        browser.ignoreSynchronization = true;
         expect(p.refreshLogButton.isDisplayed()).toBe(true);
         expect(p.refreshLogButton.getText()).toEqual("Refresh");
         expect(p.refreshLogButton.getAttribute("target")).toEqual(p.logIframe.getAttribute("name"));
@@ -58,12 +53,9 @@ describe('Information Page', function () {
     });
 
     it("log: should have a link to open log in new window",function(){
-        browser.ignoreSynchronization = true;
         expect(p.openLogButton.isDisplayed()).toBe(true);
         expect(p.openLogButton.getText()).toEqual("Open in new window");
         expect(p.openLogButton.getAttribute("target")).toEqual("_blank");
         expect(p.openLogButton.getAttribute("href")).toMatch("/api/showLog");
     });
-
-    browser.ignoreSynchronization = false;
 });
