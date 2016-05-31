01c1838fefa6e7383a5a6b1bfaa4992568762c9d
Alexander Kulikov <akulikov@networkoptix.com>
2016-05-31 20:15:52 +0300

    #vms-2638. add --tmp option to mediaserver_core_ut unit test.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/mediaserver_core_ut/src/recording/buffered_file_ut.cpp
+++ unit_tests/mediaserver_core_ut/src/recording/buffered_file_ut.cpp
@@ -9,17 +9,18 @@
 #include <nx/utils/uuid.h>
 #include <utils/fs/file.h>
 #include <utils/common/buffered_file.h>
+#include "../utils.h"
 
 void doTestInternal(int systemFlags)
 {
-    const QByteArray kTestFileName("test_data1.bin");
+    nx::ut::utils::WorkDirResource workDir;
+    ASSERT_TRUE((bool)workDir.getDirName());
+
+    const QString kTestFileName = QDir(workDir.getDirName()).absoluteFilePath("test_data1.bin");
     const QByteArray kTestPattern("1234567890");
     const int kPatternRepCnt = 2000 * 499;
 
     // write file
-
-    QFile::remove(kTestFileName);
-
     std::unique_ptr<QBufferedFile> testFile(
         new QBufferedFile(
         std::shared_ptr<IQnFile>(new QnFile(kTestFileName)),
