4266bc92a22b12376c1db9f5f92b121560ae53b6
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2016-06-06 17:31:54 +0300

    WEB-303: Web admin autotests: help tab
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/test/e2e/help/po.js
+++ webadmin/test/e2e/help/po.js
@@ -7,6 +7,11 @@ var Page = function () {
     };
 
     this.links = element.all(by.repeater("(key, link) in config.helpLinks"));
+    this.supportButton = element(by.linkText('get support'));
+    this.calculatorButton = element(by.linkText('calculate'));
+    this.androidStoreButton = element(by.linkText('Android Client'));
+    this.appleStoreButton = element(by.linkText('iOS Client'));
+    this.body = element(by.css('body'));
 };
 
 module.exports = Page;
--- webadmin/test/e2e/help/spec.js
+++ webadmin/test/e2e/help/spec.js
@@ -1,8 +1,6 @@
 'use strict';
 var Page = require('./po.js');
 describe('Help Page', function () {
-    //it("should stop test",function(){expect("other test").toBe("uncommented");});return;
-
     var p = new Page();
 
     it("should show some links on the left",function(){
@@ -10,5 +8,82 @@ describe('Help Page', function () {
         expect (p.links.count()).toBeGreaterThan(0);
         var firstLink = p.links.first().element(by.tagName("a"));
         expect(firstLink.isPresent()).toBe(true);
+    });
+
+    it("Get Support button opens external support page",function(){
+        expect(p.supportButton.isPresent()).toBe(true);
+        p.supportButton.click();
+        // Switch to just opened new tab
+        browser.getAllWindowHandles().then(function (handles) {
+            var oldWindowHandle = handles[0];
+            var newWindowHandle = handles[1];
+            browser.switchTo().window(newWindowHandle);
+
+            browser.ignoreSynchronization = true;
+            expect(browser.getCurrentUrl()).toContain('/hc/');
+            expect(p.body.getText()).toContain('KNOWN ISSUES');
+            browser.ignoreSynchronization = false;
+
+            //Switch back
+            browser.close();
+            browser.switchTo().window(oldWindowHandle);
+        });
+    });
+
+    it("Calculate button opens external hardware calculator",function(){
+        expect(p.calculatorButton.isPresent()).toBe(true);
+        p.calculatorButton.click();
+        // Switch to just opened new tab
+        browser.getAllWindowHandles().then(function (handles) {
+            var oldWindowHandle = handles[0];
+            var newWindowHandle = handles[1];
+            browser.switchTo().window(newWindowHandle);
+
+            browser.ignoreSynchronization = true;
+            expect(browser.getCurrentUrl()).toContain('networkoptix.com/calculator');
+            browser.ignoreSynchronization = false;
+
+            //Switch back
+            browser.close();
+            browser.switchTo().window(oldWindowHandle);
+        });
+    });
+
+    it("Android moblie application link opens google play app page",function(){
+        expect(p.androidStoreButton.isPresent()).toBe(true);
+        p.androidStoreButton.click();
+        // Switch to just opened new tab
+        browser.getAllWindowHandles().then(function (handles) {
+            var oldWindowHandle = handles[0];
+            var newWindowHandle = handles[1];
+            browser.switchTo().window(newWindowHandle);
+
+            browser.ignoreSynchronization = true;
+            expect(browser.getCurrentUrl()).toContain('play.google.com/store/apps/details?id=com.networkoptix.hdwitness');
+            browser.ignoreSynchronization = false;
+
+            //Switch back
+            browser.close();
+            browser.switchTo().window(oldWindowHandle);
+        });
+    });
+
+    it("Ios moblie application link opens itunes app page",function(){
+        expect(p.appleStoreButton.isPresent()).toBe(true);
+        p.appleStoreButton.click();
+        // Switch to just opened new tab
+        browser.getAllWindowHandles().then(function (handles) {
+            var oldWindowHandle = handles[0];
+            var newWindowHandle = handles[1];
+            browser.switchTo().window(newWindowHandle);
+
+            browser.ignoreSynchronization = true;
+            expect(browser.getCurrentUrl()).toContain('itunes.apple.com/eg/app/hd-witness/');
+            browser.ignoreSynchronization = false;
+
+            //Switch back
+            browser.close();
+            browser.switchTo().window(oldWindowHandle);
+        });
     });
 });
