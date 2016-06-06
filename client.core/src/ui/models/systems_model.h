87426cb615a8a6de5c804539da35630bc89582b7
Denis Klychkov <dklychkov@networkoptix.com>
2016-06-06 16:52:19 +0300

    VMS-2166: Minimal version check for systems model + refactor to pimpl.
    <==== Mercurial data ====>
    Branch: mobile_client_ui_refactoring

--- client.core/src/ui/models/systems_model.h
+++ client.core/src/ui/models/systems_model.h
@@ -3,52 +3,32 @@
 #include <QtCore/QStringListModel>
 
 #include <utils/common/connective.h>
-#include <network/system_description.h>
-#include <nx/utils/disconnect_helper.h>
+
+class QnSystemsModelPrivate;
 
 class QnSystemsModel : public Connective<QAbstractListModel>
 {
     Q_OBJECT
-    typedef Connective<QAbstractListModel> base_type;
+    Q_PROPERTY(QString minimalVersion READ minimalVersion WRITE setMinimalVersion NOTIFY minimalVersionChanged)
+
+    using base_type = Connective<QAbstractListModel>;
 
 public:
-    QnSystemsModel(QObject *parent = nullptr);
-
+    QnSystemsModel(QObject* parent = nullptr);
     virtual ~QnSystemsModel();
 
+    QString minimalVersion() const;
+    void setMinimalVersion(const QString& minimalVersion);
+
 public: // overrides
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
     QHash<int, QByteArray> roleNames() const override;
 
-private:
-    void updateOwnerDescription();
-
-    void addSystem(const QnSystemDescriptionPtr &systemDescription);
-
-    void removeSystem(const QString &systemId);
-
-    struct InternalSystemData;
-    typedef QSharedPointer<InternalSystemData> InternalSystemDataPtr;
-    typedef QVector<InternalSystemDataPtr> InternalList;
-
-    InternalList::iterator getInternalDataIt(
-        const QnSystemDescriptionPtr &systemDescription);
-
-    void serverChanged(const QnSystemDescriptionPtr &systemDescription
-        , const QnUuid &serverId
-        , QnServerFields fields);
-
-    QStringListModel *createStringListModel(const QStringList &data) const;
+signals:
+    void minimalVersionChanged();
 
 private:
-    typedef std::function<bool(const InternalSystemDataPtr &first
-        , const InternalSystemDataPtr &second)> LessPred;
-
-    const int m_maxCount;
-    const LessPred m_lessPred;
-    QnDisconnectHelper m_connections;
-    InternalList m_internalData;
+    QScopedPointer<QnSystemsModelPrivate> d_ptr;
+    Q_DECLARE_PRIVATE(QnSystemsModel)
 };
