da513c067b79f6c27a5efd40437d669efba58996
Sergey Ivanov <sivanov@networkoptix.com>
2016-06-07 14:16:42 +0300

    VMS-2506 - Client should register himself as protocol handler for nx-vms://
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- common_libs/nx_utils/src/nx/utils/platform/protocol_handler_win.cpp
+++ common_libs/nx_utils/src/nx/utils/platform/protocol_handler_win.cpp
@@ -1,6 +1,7 @@
 #include "protocol_handler.h"
 
-#include <QtCore/QDebug>
+#include <QtCore/QSettings>
+#include <QtCore/QDir>
 
 namespace nx
 {
@@ -8,7 +9,30 @@ namespace nx
     {
         bool registerSystemUriProtocolHandler(const QString& protocol, const QString& applicationBinaryPath)
         {
-            qDebug() << "Register protocol" << protocol << applicationBinaryPath;
+            static const QString kClassesRootPath = "HKEY_CLASSES_ROOT\\";
+            static const QString kDefaultValueKey = ".";
+
+            QString protocolPath = kClassesRootPath + protocol;
+            QString applicationNativePath = QDir::toNativeSeparators(applicationBinaryPath);
+
+            QSettings registryEditor(protocolPath, QSettings::NativeFormat);
+            registryEditor.setValue(kDefaultValueKey, "URL:NxWitness Client Protocol");
+            registryEditor.setValue("URL Protocol", "");
+
+            registryEditor.beginGroup("DefaultIcon");
+            registryEditor.setValue(kDefaultValueKey, QString("%1 ,0").arg(applicationNativePath));
+            registryEditor.endGroup();
+
+            registryEditor.beginGroup("shell");
+            registryEditor.setValue(kDefaultValueKey, "open");
+            registryEditor.beginGroup("open");
+            registryEditor.beginGroup("command");
+            registryEditor.setValue(kDefaultValueKey, QString("\"%1\" -- \"%2\"").arg(applicationNativePath).arg("%1"));
+            registryEditor.endGroup();
+            registryEditor.endGroup();
+            registryEditor.endGroup();
+            registryEditor.sync();
+
             return true;
         }
     }
