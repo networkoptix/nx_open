f6690d4c50bad84499159d6f5eeb8061a50eff24
Alexander Kulikov <akulikov@networkoptix.com>
2016-06-14 16:39:17 +0300

    #vms-2618. Graceful windows service restart.
    <==== Mercurial data ====>
    Branch: dev_2.5

--- mediaserver_core/src/media_server_process.cpp
+++ mediaserver_core/src/media_server_process.cpp
@@ -1836,6 +1836,7 @@ void MediaServerProcess::run()
 #ifdef Q_OS_WIN
         HANDLE hProcess = GetCurrentProcess();
         TerminateProcess(hProcess, ERROR_SERVICE_SPECIFIC_ERROR);
+        WaitForSingleObject(hProcess, 10*1000);
 #endif
         abort();
         return;
