0afc354d5826d0e29233c53be22f84a5a7bdd256
Alexander Kulikov <akulikov@networkoptix.com>
2016-06-23 21:12:55 +0300

    #vms-2830. Get rid of apiUrl. Fixes.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common/src/core/resource/media_server_resource.h
+++ common/src/core/resource/media_server_resource.h
@@ -46,6 +46,7 @@ public:
     void setIgnoredUrls(const QList<QUrl> &urls);
     QList<QUrl> getIgnoredUrls() const;
 
+    virtual void setUrl(const QString& url) override;
     quint16 getPort() const;
 
     /** Get list of all available server addresses. */
