64a37218d6633a3a9866e920e1e15928bce3c1c3
Sergey Ivanov <sivanov@networkoptix.com>
2016-06-27 14:13:21 +0300

    Codestyle fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- appserver2/src/StdAfx.h
+++ appserver2/src/StdAfx.h
@@ -2,15 +2,15 @@
 
 /* Windows headers. */
 #ifdef _WIN32
-#   include <winsock2.h>
-#   include <windows.h> /* You HAVE to include winsock2.h BEFORE windows.h */
-#   include <ws2tcpip.h>
-#   include <iphlpapi.h>
+    #include <winsock2.h>
+    #include <windows.h> /*< You HAVE to include winsock2.h BEFORE windows.h */
+    #include <ws2tcpip.h>
+    #include <iphlpapi.h>
 #endif
 
 #ifdef __cplusplus
-/* This file should be included only main in vms projects. */
-#include <common/common_globals.h>
+    /* This file should be included only main in vms projects. */
+    #include <common/common_globals.h>
 #endif
 
 /* Boost headers. */
