b6481666899492a0a3f8b41819b64b0f69c3706e
Sergey Ivanov <sivanov@networkoptix.com>
2016-07-12 14:54:26 +0300

    Codestyle fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/camera/camera_bookmarks_query.cpp
+++ client/libclient/src/camera/camera_bookmarks_query.cpp
@@ -10,51 +10,60 @@ QnCameraBookmarksQuery::QnCameraBookmarksQuery(const QnVirtualCameraResourceSet 
     , m_filter(filter)
 {}
 
-QnCameraBookmarksQuery::~QnCameraBookmarksQuery() {
-}
+QnCameraBookmarksQuery::~QnCameraBookmarksQuery()
+{}
 
-QUuid QnCameraBookmarksQuery::id() const {
+QUuid QnCameraBookmarksQuery::id() const
+{
     return m_id;
 }
 
-bool QnCameraBookmarksQuery::isValid() const {
+bool QnCameraBookmarksQuery::isValid() const
+{
     return !m_cameras.isEmpty() && m_filter.isValid();
 }
 
-QnVirtualCameraResourceSet QnCameraBookmarksQuery::cameras() const {
+QnVirtualCameraResourceSet QnCameraBookmarksQuery::cameras() const
+{
     return m_cameras;
 }
 
-void QnCameraBookmarksQuery::setCameras(const QnVirtualCameraResourceSet &value) {
+void QnCameraBookmarksQuery::setCameras(const QnVirtualCameraResourceSet &value)
+{
     if (m_cameras == value)
         return;
     m_cameras = value;
     emit queryChanged();
 }
 
-void QnCameraBookmarksQuery::setCamera(const QnVirtualCameraResourcePtr &value) {
+void QnCameraBookmarksQuery::setCamera(const QnVirtualCameraResourcePtr &value)
+{
     QnVirtualCameraResourceSet cameras;
     if (value)
         cameras << value;
     setCameras(cameras);
 }
 
-QnCameraBookmarkSearchFilter QnCameraBookmarksQuery::filter() const {
+QnCameraBookmarkSearchFilter QnCameraBookmarksQuery::filter() const
+{
     return m_filter;
 }
 
-void QnCameraBookmarksQuery::setFilter(const QnCameraBookmarkSearchFilter &value) {
+void QnCameraBookmarksQuery::setFilter(const QnCameraBookmarkSearchFilter &value)
+{
     if (m_filter == value)
         return;
     m_filter = value;
     emit queryChanged();
 }
 
-QnCameraBookmarkList QnCameraBookmarksQuery::cachedBookmarks() const {
+QnCameraBookmarkList QnCameraBookmarksQuery::cachedBookmarks() const
+{
     return qnCameraBookmarksManager->cachedBookmarks(toSharedPointer());
 }
 
-void QnCameraBookmarksQuery::executeRemoteAsync(BookmarksCallbackType callback) {
+void QnCameraBookmarksQuery::executeRemoteAsync(BookmarksCallbackType callback)
+{
     qnCameraBookmarksManager->executeQueryRemoteAsync(toSharedPointer(), callback);
 }
 
@@ -63,7 +72,8 @@ void QnCameraBookmarksQuery::refresh()
     emit queryChanged();
 }
 
-QnCameraBookmarksQueryPtr QnCameraBookmarksQuery::toSharedPointer() const {
+QnCameraBookmarksQueryPtr QnCameraBookmarksQuery::toSharedPointer() const
+{
     return QnFromThisToShared<QnCameraBookmarksQuery>::toSharedPointer();
 }
 
