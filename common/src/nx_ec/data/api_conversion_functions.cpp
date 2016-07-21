75143a74ada5b01d25471d076d0cb8fd7550a716
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-07-21 17:48:46 +0300

    fromApiToResource: Assert has been made informative. Earlier it said
    nothing about why did it happend.
    
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- common/src/nx_ec/data/api_conversion_functions.cpp
+++ common/src/nx_ec/data/api_conversion_functions.cpp
@@ -163,7 +163,13 @@ void fromApiToResource(const ApiCameraData& src, QnVirtualCameraResourcePtr& dst
     dst->setStatusFlags(src.statusFlags);
 
     dst->setVendor(src.vendor);
-    NX_ASSERT(dst->getId() == QnVirtualCameraResource::uniqueIdToId(dst->getUniqueId()));
+
+    Q_ASSERT_X(dId == uidToId, "fromApiToResource()",
+        (QString::fromLatin1("Camera id != unique id: dst->getId() = " + dId.toString()
+        + QString::fromLatin1("; dst->getUniqueId() = ") + dUid
+        + QString::fromLatin1("; QnVirtualCameraResource::uniqueIdToId(dst->getUniqueId()) = ") 
+        + uidToId.toString()
+        ).toStdString().c_str());
 }
 
 void fromResourceToApi(const QnVirtualCameraResourcePtr& src, ApiCameraData& dst)
