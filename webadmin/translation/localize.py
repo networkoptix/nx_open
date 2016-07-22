b7ae1a892a25d747cf24d2442ee452c7c6e40ecf
Evgeny Balashov <ebalashov@networkoptix.com>
2016-07-22 15:12:18 -0700

    WEB-335:  remove branding reference for now
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/translation/localize.py
+++ webadmin/translation/localize.py
@@ -1,7 +1,6 @@
 import os
 import re
 import xml.etree.ElementTree as eTree
-import yaml
 from os.path import join
 
 
@@ -14,46 +13,12 @@ root_directory = ''
 branding_messages = {}
 
 
-def get_config():
-    conf_dir = os.getenv('CLOUD_PORTAL_CONF_DIR')
-    if not conf_dir:
-        raise RuntimeError('CLOUD_PORTAL_CONF_DIR environment variable is undefined')
-
-    return yaml.safe_load(open(join(conf_dir, 'cloud_portal.yaml')))
-
-def read_branding():
-    print "read_branding"
-    conf = get_config()
-    customization = conf['customization']
-    branding_file = join('..', '..', 'customizations', customization, 'branding.ts')
-    tree = eTree.parse(branding_file)
-    root = tree.getroot()
-
-    for context in root.iter('context'):
-        name = context.find('name').text
-        if name != 'global':
-            continue
-        for message in context.iter('message'):
-            source = message.find('source').text
-            translation = message.find('translation').text
-            branding_messages[source] = translation
-
-
-def process_branding(content):
-    for src, trans in branding_messages.iteritems():
-        content = content.replace(src, trans)
-    return content
-
-
 def save_content(filename, content):
     if filename:
-        # proceed with branding
-        active_content = process_branding(content)
-
         # save old file
         with open(filename, "w") as file_descriptor:
             print "save: " + filename
-            file_descriptor.write(active_content)
+            file_descriptor.write(content)
 
 
 def process_files():
@@ -87,6 +52,4 @@ def process_files():
                         active_content = active_content.replace(source, translation)
         save_content(active_filename, active_content)
 
-
-# read_branding()
 process_files()
