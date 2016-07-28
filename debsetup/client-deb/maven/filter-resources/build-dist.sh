825ee9b25ac4cc3af658a07d7ada996b172376d1
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-07-27 17:42:08 -0700

    fix build
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- debsetup/client-deb/maven/filter-resources/build-dist.sh
+++ debsetup/client-deb/maven/filter-resources/build-dist.sh
@@ -97,7 +97,12 @@ cp -r $CLIENT_PLATFORMS_PATH $BINSTAGE
 rm -f $LIBSTAGE/*.debug
 
 #copying qt libs
-QTLIBS="Core Gui Widgets WebKit WebChannel WebKitWidgets OpenGL Multimedia MultimediaWidgets Qml Quick QuickWidgets LabsTemplates X11Extras XcbQpa DBus Xml XmlPatterns Concurrent Network Sql PrintSupport Sensors Positioning"
+QTLIBS="Core Gui Widgets WebKit WebChannel WebKitWidgets OpenGL Multimedia MultimediaWidgets Qml Quick QuickWidgets LabsTemplates X11Extras XcbQpa DBus Xml XmlPatterns Concurrent Network Sql PrintSupport Positioning"
+if [ '${arch}' == 'arm' ]
+then
+  QTLIBS+=( Sensors )
+fi
+
 for var in $QTLIBS
 do
     qtlib=libQt5$var.so
