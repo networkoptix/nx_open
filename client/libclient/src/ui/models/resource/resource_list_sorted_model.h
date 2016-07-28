6bf449ecadb4adb47328c71c1c7323075bca2602
Sergey Ivanov <sivanov@networkoptix.com>
2016-07-28 21:22:41 +0300

    VMS-3342 - Tree views for different user types
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/models/resource/resource_list_sorted_model.h
+++ client/libclient/src/ui/models/resource/resource_list_sorted_model.h
@@ -3,15 +3,14 @@
 #include <QtCore/QSortFilterProxyModel>
 
 #include <core/resource/resource_fwd.h>
+#include <ui/models/resource/resource_compare_helper.h>
 
-class QnResourceListSortedModel: public QSortFilterProxyModel
+class QnResourceListSortedModel: public QSortFilterProxyModel, protected QnResourceCompareHelper
 {
     using base_type = QSortFilterProxyModel;
 public:
     explicit QnResourceListSortedModel(QObject *parent = 0);
     virtual ~QnResourceListSortedModel();
-
-    QnResourcePtr resource(const QModelIndex &index) const;
 
 protected:
     virtual bool lessThan(const QModelIndex &left, const QModelIndex &right) const override;
