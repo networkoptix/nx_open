192594c17c4e4f468cec9becd2488e7827fdd5be
Mikhail Uskov <muskov@networkoptix.com>
2016-07-29 22:33:01 +0300

    nx::utils ArgumentParser::get altNames compile fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common_libs/nx_utils/src/nx/utils/argument_parser.h
+++ common_libs/nx_utils/src/nx/utils/argument_parser.h
@@ -6,14 +6,13 @@
 #include <functional>
 #include <QString>
 
-
 namespace nx {
 namespace utils {
 
-//!Stand-alone & simple function for parsing arguments
-/*!
-    Supports both "-a value" and "--arg=value" syntax. "-" and "--" are ommitted
-*/
+/**
+ * Stand-alone & simple function for parsing arguments.
+ * Supports both "-a value" and "--arg=value" syntax. "-" and "--" are ommitted.
+ */
 class NX_UTILS_API ArgumentParser
 {
 public:
@@ -23,12 +22,6 @@ public:
     bool read(const QString& name, QString* const value) const;
     bool read(const QString& name, int* const value) const;
     bool read(const QString& name, size_t* const value) const;
-
-    template<typename ValueType = QString>
-    boost::optional<ValueType> get(const QString& name) const;
-
-    template<typename ValueType = QString>
-    boost::optional<ValueType> get(const char* name) const;
 
     template<typename ValueType = QString, typename MainName, typename ... AltNames>
     boost::optional<ValueType> get(MainName mainName, AltNames ... altNames) const;
@@ -40,31 +33,19 @@ public:
     void forEach(const QString& name, const Handler& handler) const;
 
 private:
+    template<typename ValueType = QString>
+    boost::optional<ValueType> getImpl(const QString& name) const;
+
+    template<typename ValueType = QString>
+    boost::optional<ValueType> getImpl(const char* name) const;
+
     std::multimap<QString, QString> m_args;
 };
-
-// ---
-
-template<typename ValueType>
-boost::optional<ValueType> ArgumentParser::get(const QString& name) const
-{
-    ValueType value;
-    if (read(name, &value))
-        return value;
-    else
-        return boost::none;
-}
-
-template<typename ValueType>
-boost::optional<ValueType> ArgumentParser::get(const char* name) const
-{
-    return get<ValueType>(QLatin1String(name));
-}
 
 template<typename ValueType, typename MainName, typename ... AltNames>
 boost::optional<ValueType> ArgumentParser::get(MainName mainName, AltNames ... altNames) const
 {
-    if (const auto value = get<ValueType>(std::forward<MainName>(mainName)))
+    if (const auto value = getImpl<ValueType>(std::forward<MainName>(mainName)))
         return value;
 
     return get<ValueType>(std::forward<AltNames>(altNames) ...);
@@ -76,6 +57,22 @@ void ArgumentParser::forEach(const QString& name, const Handler& handler) const
     const auto range = m_args.equal_range(name);
     for (auto it = range.first; it != range.second; it++)
         handler(it->second);
+}
+
+template<typename ValueType>
+boost::optional<ValueType> ArgumentParser::getImpl(const QString& name) const
+{
+    ValueType value;
+    if (read(name, &value))
+        return value;
+    else
+        return boost::none;
+}
+
+template<typename ValueType>
+boost::optional<ValueType> ArgumentParser::getImpl(const char* name) const
+{
+    return get<ValueType>(QLatin1String(name));
 }
 
 } // namespace utils
