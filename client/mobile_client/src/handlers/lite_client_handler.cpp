1577a66069690ecc91998a7b9073fe9ad71c7a33
Denis Klychkov <dklychkov@networkoptix.com>
2016-08-03 13:47:44 +0300

    MOBILE-231: Open the proper layout in the lite client at startup.
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/mobile_client/src/handlers/lite_client_handler.cpp
+++ client/mobile_client/src/handlers/lite_client_handler.cpp
@@ -2,8 +2,12 @@
 
 #include <api/runtime_info_manager.h>
 #include <nx_ec/data/api_conversion_functions.h>
+#include <core/resource_management/resource_pool.h>
+#include <core/resource/layout_resource.h>
+#include <common/common_module.h>
 #include <mobile_client/mobile_client_message_processor.h>
 #include <mobile_client/mobile_client_ui_controller.h>
+#include <helpers/lite_client_layout_helper.h>
 
 class QnLiteClientHandlerPrivate: public QObject
 {
@@ -14,9 +18,13 @@ public:
     QnLiteClientHandlerPrivate(QnLiteClientHandler* parent);
 
     void at_videowallControlMessageReceived(const ec2::ApiVideowallControlMessageData& message);
+    void at_initialResourcesReceived();
+    void at_resourceAdded(const QnResourcePtr& resource);
 
 public:
     QnMobileClientUiController* uiController = nullptr;
+    QnUuid videowallGuid;
+    QnLayoutResourcePtr layout;
 };
 
 QnLiteClientHandler::QnLiteClientHandler(QObject* parent):
@@ -33,24 +41,26 @@ void QnLiteClientHandler::setUiController(QnMobileClientUiController* controller
 {
     Q_D(QnLiteClientHandler);
     d->uiController = controller;
+    if (d->uiController && d->layout)
+        d->uiController->setLayoutId(d->layout->getId().toString());
 }
 
 QnLiteClientHandlerPrivate::QnLiteClientHandlerPrivate(QnLiteClientHandler* parent):
     QObject(parent),
-    q_ptr(parent)
+    q_ptr(parent),
+    videowallGuid(QnRuntimeInfoManager::instance()->localInfo().data.videoWallInstanceGuid)
 {
     connect(qnClientMessageProcessor, &QnClientMessageProcessor::videowallControlMessageReceived,
         this, &QnLiteClientHandlerPrivate::at_videowallControlMessageReceived);
+    connect(qnClientMessageProcessor, &QnClientMessageProcessor::initialResourcesReceived,
+            this, &QnLiteClientHandlerPrivate::at_initialResourcesReceived);
 }
 
 void QnLiteClientHandlerPrivate::at_videowallControlMessageReceived(
     const ec2::ApiVideowallControlMessageData& message)
 {
-    if (message.videowallGuid !=
-        QnRuntimeInfoManager::instance()->localInfo().data.videoWallInstanceGuid)
-    {
+    if (message.videowallGuid != videowallGuid)
         return;
-    }
 
     switch (message.operation)
     {
@@ -61,4 +71,14 @@ void QnLiteClientHandlerPrivate::at_videowallControlMessageReceived(
         default:
             break;
     }
+}
+
+void QnLiteClientHandlerPrivate::at_initialResourcesReceived()
+{
+    layout = QnLiteClientLayoutHelper::findLayoutForServer(videowallGuid);
+    if (!layout)
+        return;
+
+    if (uiController)
+        uiController->setLayoutId(layout->getId().toString());
 }
