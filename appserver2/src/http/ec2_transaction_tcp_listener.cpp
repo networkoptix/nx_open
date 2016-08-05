25d4ebeb4fdbfe387059c20dba9cf5561a937e3f
Roman Vasilenko <rvasilenko@networkoptix.com>
2016-08-05 18:07:44 +0300

    VMS-2986
    <==== Mercurial data ====>
    Branch: x_VMS-2986_merge

--- appserver2/src/http/ec2_transaction_tcp_listener.cpp
+++ appserver2/src/http/ec2_transaction_tcp_listener.cpp
@@ -261,11 +261,14 @@ void QnTransactionTcpProcessor::run()
             // since server-to-server order of transactions is unpredictable and access check for resource attribute
             // may come before resource itself is added to the resource pool and this may be restricted by the access
             // checking mechanics.
-            auto user = qnResPool->getResourceById<QnUserResource>(d->authUserId);
-            bool authAsOwner = qnResourceAccessManager->userRole(user) == Qn::UserRole::Owner;
-            NX_ASSERT(authAsOwner, "Server must always be authorised as owner");
-            if (authAsOwner)
-                access = Qn::kSystemAccess;
+            if (access != Qn::kSystemAccess)
+            {
+                auto user = qnResPool->getResourceById<QnUserResource>(d->authUserId);
+                bool authAsOwner = qnResourceAccessManager->userRole(user) == Qn::UserRole::Owner;
+                NX_ASSERT(authAsOwner, "Server must always be authorised as owner");
+                if (authAsOwner)
+                    access = Qn::kSystemAccess;
+            }
         }
 
         QnTransactionMessageBus::instance()->gotConnectionFromRemotePeer(
