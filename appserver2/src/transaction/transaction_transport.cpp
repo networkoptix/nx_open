e2163bdb10db4f3757ebc03c9eeda70559bf76f2
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-08-09 18:41:24 +0300

    #CLOUD-484 common logic from QnTransactionTransport moved to QnTransactionTransportBase. Needed to use it in libcloud_db
    <==== Mercurial data ====>
    Branch: x_VMS-3408_cloud_sync

---
+++ appserver2/src/transaction/transaction_transport.cpp
@@ -0,0 +1,77 @@
+
+#include "transaction_transport.h"
+
+#include "core/resource/media_server_resource.h"
+#include "api/app_server_connection.h"
+#include "database/db_manager.h"
+
+
+namespace ec2 {
+
+QnTransactionTransport::QnTransactionTransport(
+    const QnUuid& connectionGuid,
+    const ApiPeerData& localPeer,
+    const ApiPeerData& remotePeer,
+    QSharedPointer<AbstractStreamSocket> socket,
+    ConnectionType::Type connectionType,
+    const nx_http::Request& request,
+    const QByteArray& contentEncoding,
+    const Qn::UserAccessData &userAccessData)
+:
+    QnTransactionTransportBase(
+        connectionGuid,
+        localPeer,
+        remotePeer,
+        std::move(socket),
+        connectionType,
+        request,
+        contentEncoding,
+        userAccessData)
+{
+}
+
+QnTransactionTransport::QnTransactionTransport(const ApiPeerData& localPeer)
+:
+    QnTransactionTransportBase(localPeer)
+{
+}
+
+void QnTransactionTransport::fillAuthInfo(const nx_http::AsyncHttpClientPtr& httpClient, bool authByKey)
+{
+    if (!QnAppServerConnectionFactory::videowallGuid().isNull())
+    {
+        httpClient->addAdditionalHeader(
+            "X-NetworkOptix-VideoWall",
+            QnAppServerConnectionFactory::videowallGuid().toString().toUtf8());
+        return;
+    }
+
+    QnMediaServerResourcePtr ownServer = 
+        qnResPool->getResourceById<QnMediaServerResource>(qnCommon->moduleGUID());
+    if (ownServer && authByKey)
+    {
+        httpClient->setUserName(ownServer->getId().toString().toLower());
+        httpClient->setUserPassword(ownServer->getAuthKey());
+    }
+    else
+    {
+        QUrl url = QnAppServerConnectionFactory::url();
+        httpClient->setUserName(url.userName().toLower());
+        if (detail::QnDbManager::instance() && detail::QnDbManager::instance()->isInitialized())
+        {
+            // try auth by admin user if allowed
+            QnUserResourcePtr adminUser = qnResPool->getAdministrator();
+            if (adminUser)
+            {
+                httpClient->setUserPassword(adminUser->getDigest());
+                httpClient->setAuthType(nx_http::AsyncHttpClient::authDigestWithPasswordHash);
+            }
+        }
+        else
+        {
+            httpClient->setUserPassword(url.password());
+        }
+    }
+}
+
+}   // namespace ec2
---
+++ appserver2/src/transaction/transaction_transport.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+#include "transaction_transport_base.h"
+
+
+namespace ec2 {
+
+class QnTransactionTransport
+:
+    public QnTransactionTransportBase
+{
+public:
+    /** Initializer for incoming connection */
+    QnTransactionTransport(
+        const QnUuid& connectionGuid,
+        const ApiPeerData& localPeer,
+        const ApiPeerData& remotePeer,
+        QSharedPointer<AbstractStreamSocket> socket,
+        ConnectionType::Type connectionType,
+        const nx_http::Request& request,
+        const QByteArray& contentEncoding,
+        const Qn::UserAccessData &userAccessData);
+    /** Initializer for outgoing connection */
+    QnTransactionTransport(const ApiPeerData& localPeer);
+
+protected:
+    virtual void fillAuthInfo(const nx_http::AsyncHttpClientPtr& httpClient, bool authByKey) override;
+};
+
+}   // namespace ec2
