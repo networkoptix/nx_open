224e0f489a6c4d7ec75761df4aa432cec6c061a9
Mikhail Uskov <muskov@networkoptix.com>
2016-08-17 23:37:25 +0300

    some post merge compile fixes
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common/src/plugins/resource/avi/filetypesupport.cpp
+++ common/src/plugins/resource/avi/filetypesupport.cpp
@@ -6,8 +6,17 @@ extern "C"
     #include <libavutil/avutil.h>
 }
 
-#include "filetypes.h"
 #include "utils/common/util.h"
+
+static const char* VIDEO_FILETYPES[] = {
+    "3g2", "3gp", "3gp2", "3gpp", "amv", "asf", "avi", "divx", "dv",
+    "flv", "gxf", "m1v", "m2t", "m2v", "m2ts", "m4v", "mkv", "mov",
+    "mp2", "mp2v", "mp4", "mp4v", "mpa", "mpe", "mpeg", "mpeg1",
+    "mpeg2", "mpeg4", "mpg", "mpv2", "mts", "mxf", "nsv", "nuv",
+    "ogg", "ogm", "ogx", "ogv", "rec", "rm", "rmvb", "tod", "ts",
+    "tts", "vob", "vro", "webm", "wmv"};
+
+static const char* IMAGE_FILETYPES[] = {"jpg", "jpeg", "png", "bmp", "gif", "tif", "tiff"};
 
 bool FileTypeSupport::isMovieFileExt(const QString &filename)
 {
