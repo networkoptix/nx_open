6251d9f0d105d035525b694580c7099f8a4c8b0d
Sergey Ivanov <sivanov@networkoptix.com>
2016-08-18 19:58:27 +0300

    Mobile client build fixes
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/mobile_client/src/mobile_client/mobile_client_roles.cpp
+++ client/mobile_client/src/mobile_client/mobile_client_roles.cpp
@@ -2,22 +2,22 @@
 
 #include <nx/utils/log/assert.h>
 
-QByteArray Qn::roleName(int role) {
-    switch (role) {
-    case Qn::ResourceNameRole:
-        return "resourceName";
-    case Qn::ResourceStatusRole:
-        return "resourceStatus";
-    case NodeTypeRole:
-        return "nodeType";
-    case ThumbnailRole:
-        return "thumbnail";
-    case UuidRole:
-        return "uuid";
-    case IpAddressRole:
-        return "ipAddress";
-    default:
-        NX_ASSERT(0, "Unsupported role", Q_FUNC_INFO);
+QByteArray Qn::roleName(int role)
+{
+    switch (role)
+    {
+        case ResourceNameRole:
+            return "resourceName";
+        case ResourceStatusRole:
+            return "resourceStatus";
+        case ThumbnailRole:
+            return "thumbnail";
+        case UuidRole:
+            return "uuid";
+        case IpAddressRole:
+            return "ipAddress";
+        default:
+            NX_ASSERT(0, "Unsupported role", Q_FUNC_INFO);
     }
     return QByteArray();
 }
