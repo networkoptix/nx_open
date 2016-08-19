355311bbb351f182d6026a2d5d24cc35fb4c2fa1
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-08-19 21:15:23 +0300

    #CLOUD-511 RangeMatchContainer::emplace now returns (it,false) if same elements already exists
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common/src/plugins/videodecoder/stree/range_match_container.h
+++ common/src/plugins/videodecoder/stree/range_match_container.h
@@ -36,7 +36,6 @@ public:
     {
     }
 
-
     bool operator<(const Range& rhs) const
     {
         return left < rhs.left;
@@ -111,7 +110,15 @@ public:
     template<typename KeyTypeRef, typename MappedTypeRef>
     std::pair<iterator, bool> emplace(KeyTypeRef&& key, MappedTypeRef&& mapped)
     {
-        const auto it = m_container.emplace(
+        const auto iterRange = m_container.equal_range(key);
+        for (auto it = iterRange.first; it != iterRange.second; ++it)
+        {
+            if (it->first.right == key.right)
+                return std::make_pair(it, false);
+        }
+
+        const auto it = m_container.emplace_hint(
+            iterRange.first,
             std::forward<KeyTypeRef>(key),
             std::forward<MappedTypeRef>(mapped));
         return std::make_pair(it, true);
