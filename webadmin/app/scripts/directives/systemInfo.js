8fe8aebd78f6799c2f95cd9750a937a22c44c002
Evgeny Balashov <ebalashov@networkoptix.com>
2016-08-22 14:26:40 -0700

    WEB-344: Info block at the right side
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

---
+++ webadmin/app/scripts/directives/systemInfo.js
@@ -0,0 +1,91 @@
+'use strict';
+
+//http://stackoverflow.com/questions/28045427/angularjs-custom-directive-with-input-element-pass-validator-from-outside
+angular.module('webadminApp')
+    .directive('systemInfo', ['mediaserver','$timeout',function (mediaserver,$timeout) {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/components/systemInfo.html',
+            link:function($scope, element, attrs, ngModel){
+                $scope.Config = Config;
+
+                function checkServersIp(server,i){
+                    var ips = server.networkAddresses.split(';');
+
+                    var port = '';
+                    if(ips[i].indexOf(':') < 0){
+                        port = server.apiUrl.substring(server.apiUrl.lastIndexOf(':'));
+                    }
+
+                    server.apiUrl = window.location.protocol + '//' + ips[i] + port;
+
+                    mediaserver.getModuleInformation(server.apiUrl).catch(function(error){
+                        if(i < ips.length-1) {
+                            checkServersIp(server, i + 1);
+                        }
+                        else {
+                            server.status = L.settings.unavailable;
+
+                            $scope.mediaServers = _.sortBy($scope.mediaServers,function(server){
+                                return (server.status==='Online'?'0':'1') + server.Name + server.id;
+                                // Сортировка: online->name->id
+                            });
+
+                        }
+                        return false;
+                    });
+                }
+
+                function pingServers() {
+                    return mediaserver.getMediaServers().then(function (data) {
+                        $scope.mediaServers = _.sortBy(data.data, function (server) {
+                            // Set active state for server
+                            server.active = $scope.settings.id.replace('{', '').replace('}', '') === server.id.replace('{', '').replace('}', '');
+
+                            var ips = server.networkAddresses.split(';');
+                            var port = '';
+                            if(ips[0].indexOf(':') < 0){
+                                port = server.apiUrl.substring(server.apiUrl.lastIndexOf(':'));
+                            }
+                            server.apiUrl = window.location.protocol + '//' + ips[0] + port;
+
+
+                            return (server.status === 'Online' ? '0' : '1') + server.Name + server.id;
+                            // Sorting: online->name->id
+                        });
+                        $timeout(function () {
+                            _.each($scope.mediaServers, function (server) {
+                                checkServersIp(server, 0);
+                            });
+                        }, 1000);
+                    });
+                }
+
+                mediaserver.getModuleInformation().then(function (r) {
+                    $scope.settings = r.data.reply;
+
+                    $scope.settings.remoteAddresses = _.filter($scope.settings.remoteAddresses,function(addr){
+                        return addr.indexOf('-')==-1;
+                    });
+                    $scope.settings.remoteAddresses = $scope.settings.remoteAddresses.join('\n');
+
+                    mediaserver.resolveNewSystemAndUser().then(function(user){
+                        if(user === null){
+                            return;
+                        }
+                        $scope.user = {
+                            isAdmin: user.isAdmin,
+                            name: user.name
+                        };
+                    },function(error){
+                        if(error.status !== 401 && error.status !== 403) {
+                            alert(L.navigaion.cannotGetUser);
+                        }
+                    });
+
+                    pingServers();
+                });
+
+            }
+        };
+    }]);
