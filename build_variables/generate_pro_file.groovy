757710d2967052e8bbf3a11c2fbd83895385c27f
Denis Klychkov <dklychkov@networkoptix.com>
2016-08-23 19:09:14 +0300

    ENV-21: Root pro-file generation.
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

---
+++ build_variables/generate_pro_file.groovy
@@ -0,0 +1,36 @@
+def getProjectType(String artifactId)
+{
+    for (project in session.getProjects())
+    {
+        if (project.artifactId == artifactId)
+            return project.properties.projectType
+    }
+    return "generic"
+}
+
+def proFilePath = project.properties["root.dir"] + "/vms-${project.properties.arch}.pro"
+
+def file = new File(proFilePath)
+
+file.write("TEMPLATE = subdirs\n\n")
+
+for (project in session.getProjects())
+{
+    if (project.properties.projectType != "cpp")
+        continue
+
+    file << "SUBDIRS += ${project.artifactId}\n"
+    file << "${project.artifactId}.file = ${project.build.outputDirectory}/${project.artifactId}.pro\n"
+
+    def deps = []
+    println project
+    for (dependency in project.dependencies)
+    {
+        if (getProjectType(dependency.artifactId) == "cpp")
+            deps.add(dependency.artifactId)
+    }
+    if (deps.size() > 0)
+        file << "${project.artifactId}.depends = " + deps.join(" ") + "\n"
+
+    file << "\n"
+}
