2e9d5efd12ed9f29c95c8cde1a3d1b5ab85641c4
Sergey Ivanov <sivanov@networkoptix.com>
2016-08-23 12:56:36 +0300

    VMS-3307 - Standartize all dialogs with resources list
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/workbench/handlers/workbench_debug_handler.cpp
+++ client/libclient/src/ui/workbench/handlers/workbench_debug_handler.cpp
@@ -9,13 +9,14 @@
 #include <ui/actions/action_manager.h>
 #include <ui/widgets/palette_widget.h>
 #include <ui/dialogs/common/dialog.h>
-#include <ui/dialogs/resource_list_dialog.h>
+#include <ui/widgets/views/resource_list_view.h>
 #include <ui/workaround/qtbug_workaround.h>
 
 // -------------------------------------------------------------------------- //
 // QnDebugControlDialog
 // -------------------------------------------------------------------------- //
-class QnDebugControlDialog: public QnDialog, public QnWorkbenchContextAware {
+class QnDebugControlDialog: public QnDialog, public QnWorkbenchContextAware
+{
     typedef QnDialog base_type;
 
 public:
@@ -31,7 +32,8 @@ public:
     }
 
 private:
-    QToolButton *newActionButton(QnActions::IDType actionId, QWidget *parent = NULL) {
+    QToolButton *newActionButton(QnActions::IDType actionId, QWidget *parent = NULL)
+    {
         QToolButton *button = new QToolButton(parent);
         button->setDefaultAction(menu()->action(actionId));
         button->setToolButtonStyle(Qt::ToolButtonTextOnly);
@@ -48,39 +50,47 @@ QnWorkbenchDebugHandler::QnWorkbenchDebugHandler(QObject *parent):
     QnWorkbenchContextAware(parent)
 {
 #ifdef _DEBUG
-    connect(action(QnActions::DebugControlPanelAction),                &QAction::triggered,    this,   &QnWorkbenchDebugHandler::at_debugControlPanelAction_triggered);
-    connect(action(QnActions::DebugIncrementCounterAction),            &QAction::triggered,    this,   &QnWorkbenchDebugHandler::at_debugIncrementCounterAction_triggered);
-    connect(action(QnActions::DebugDecrementCounterAction),            &QAction::triggered,    this,   &QnWorkbenchDebugHandler::at_debugDecrementCounterAction_triggered);
-    connect(action(QnActions::DebugShowResourcePoolAction),            &QAction::triggered,    this,   &QnWorkbenchDebugHandler::at_debugShowResourcePoolAction_triggered);
+    connect(action(QnActions::DebugControlPanelAction), &QAction::triggered, this,
+        &QnWorkbenchDebugHandler::at_debugControlPanelAction_triggered);
+    connect(action(QnActions::DebugIncrementCounterAction), &QAction::triggered, this,
+        &QnWorkbenchDebugHandler::at_debugIncrementCounterAction_triggered);
+    connect(action(QnActions::DebugDecrementCounterAction), &QAction::triggered, this,
+        &QnWorkbenchDebugHandler::at_debugDecrementCounterAction_triggered);
+    connect(action(QnActions::DebugShowResourcePoolAction), &QAction::triggered, this,
+        &QnWorkbenchDebugHandler::at_debugShowResourcePoolAction_triggered);
 #endif
 }
 
-void QnWorkbenchDebugHandler::at_debugControlPanelAction_triggered() {
+void QnWorkbenchDebugHandler::at_debugControlPanelAction_triggered()
+{
     QScopedPointer<QnDebugControlDialog> dialog(new QnDebugControlDialog(mainWindow()));
     dialog->exec();
 }
 
-void QnWorkbenchDebugHandler::at_debugIncrementCounterAction_triggered() {
+void QnWorkbenchDebugHandler::at_debugIncrementCounterAction_triggered()
+{
     qnRuntime->setDebugCounter(qnRuntime->debugCounter() + 1);
     qDebug() << qnRuntime->debugCounter();
 
-    auto showPalette = [this] {
+    auto showPalette = [this]
+    {
         QnPaletteWidget *w = new QnPaletteWidget();
         w->setPalette(qApp->palette());
         w->show();
     };
 
-    // the palette widget code maight still be required
+    // the palette widget code might still be required
     Q_UNUSED(showPalette);
 }
 
 void QnWorkbenchDebugHandler::at_debugDecrementCounterAction_triggered()
 {
-    //menu()->trigger(QnActions::UserGroupsAction);
+    //at_debugShowResourcePoolAction_triggered();
 }
 
-void QnWorkbenchDebugHandler::at_debugShowResourcePoolAction_triggered() {
-    QScopedPointer<QnResourceListDialog> dialog(new QnResourceListDialog(mainWindow()));
-    dialog->setResources(qnResPool->getResources());
-    dialog->exec();
+void QnWorkbenchDebugHandler::at_debugShowResourcePoolAction_triggered()
+{
+    QnMessageBox messageBox(mainWindow());
+    messageBox.addCustomWidget(new QnResourceListView(qnResPool->getResources()));
+    messageBox.exec();
 }
