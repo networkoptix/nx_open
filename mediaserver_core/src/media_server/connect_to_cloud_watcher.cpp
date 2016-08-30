3cf667fdf5f3de04f4a2a098133d04cca36ee106
Roman Vasilenko <rvasilenko@networkoptix.com>
2016-08-30 14:52:55 +0300

    (VMS-3672) - QnConnectToCloudWatcher should take cloud url from CdbEndpointFetcher
    <==== Mercurial data ====>
    Branch: x_VMS-3408_cloud_sync

--- mediaserver_core/src/media_server/connect_to_cloud_watcher.cpp
+++ mediaserver_core/src/media_server/connect_to_cloud_watcher.cpp
@@ -3,41 +3,61 @@
 #include <api/global_settings.h>
 #include <common/common_module.h>
 #include <transaction/transaction_message_bus.h>
-#include <utils/common/app_info.h>
 
+namespace {
+    static const int kUpdateIfFailIntervalMs = 1000 * 60;
+}
 
-QnConnectToCloudWatcher::QnConnectToCloudWatcher()
+using namespace nx::network::cloud;
+
+QnConnectToCloudWatcher::QnConnectToCloudWatcher():
+    m_cdbEndPointFetcher(new CloudModuleEndPointFetcher(
+        "cdb",
+        std::make_unique<RandomEndpointSelector>()))
 {
+    m_timer.setSingleShot(true);
+    m_timer.setInterval(kUpdateIfFailIntervalMs);
+
+    connect(&m_timer, &QTimer::timeout, this, &QnConnectToCloudWatcher::at_updateConnection);
     connect(qnGlobalSettings, &QnGlobalSettings::cloudSettingsChanged, this, &QnConnectToCloudWatcher::at_updateConnection);
     connect(QnRuntimeInfoManager::instance(), &QnRuntimeInfoManager::runtimeInfoAdded, this, &QnConnectToCloudWatcher::at_updateConnection);
     connect(QnRuntimeInfoManager::instance(), &QnRuntimeInfoManager::runtimeInfoChanged, this, &QnConnectToCloudWatcher::at_updateConnection);
     connect(QnRuntimeInfoManager::instance(), &QnRuntimeInfoManager::runtimeInfoRemoved, this, &QnConnectToCloudWatcher::at_updateConnection);
 }
 
+QnConnectToCloudWatcher::~QnConnectToCloudWatcher()
+{
+    m_cdbEndPointFetcher->pleaseStopSync();
+}
+
 void QnConnectToCloudWatcher::at_updateConnection()
 {
-    //TODO #ak should take url from CdbEndpointFetcher
+    m_timer.stop();
 
     QnPeerRuntimeInfo localInfo = QnRuntimeInfoManager::instance()->localInfo();
-    QUrl cloudUrl = QnAppInfo::defaultCloudPortalUrl();
     bool needCloudConnect =
         localInfo.data.flags.testFlag(ec2::RF_MasterCloudSync) &&
-        !cloudUrl.isEmpty() &&
         !qnGlobalSettings->cloudSystemID().isEmpty();
-
     if (!needCloudConnect)
     {
-        if (!m_lastCloudUrl.isEmpty())
-            qnTransactionBus->removeConnectionFromPeer(m_lastCloudUrl);
-        m_lastCloudUrl.clear();
+        if (!m_cloudUrl.isEmpty())
+            qnTransactionBus->removeConnectionFromPeer(m_cloudUrl);
         return;
     }
 
-    if (cloudUrl != m_lastCloudUrl)
-    {
-        if (!m_lastCloudUrl.isEmpty())
-            qnTransactionBus->removeConnectionFromPeer(m_lastCloudUrl);
-        qnTransactionBus->addConnectionToPeer(cloudUrl);
-        m_lastCloudUrl = cloudUrl;
-    }
+    m_cdbEndPointFetcher->get(
+        nx_http::AuthInfo(),
+        [this](int statusCode, boost::optional<SocketAddress> endpoint)
+        {
+            if (statusCode != nx_http::StatusCode::ok || endpoint->isNull())
+            {
+                m_timer.start(); //< try once more later
+                return;
+            }
+
+            m_cloudUrl = QUrl(lit("http://%1:%2").
+                arg(endpoint->address.toString()).
+                arg(endpoint->port));
+            qnTransactionBus->addConnectionToPeer(m_cloudUrl);
+        });
 }
--- mediaserver_core/src/media_server/connect_to_cloud_watcher.h
+++ mediaserver_core/src/media_server/connect_to_cloud_watcher.h
@@ -5,6 +5,7 @@
 
 #include <core/resource/resource_fwd.h>
 #include <nx/utils/uuid.h>
+#include <nx/network/cloud/cdb_endpoint_fetcher.h>
 
 /**
  * Monitor runtime flags RF_CloudSync. Only one server at once should has it
@@ -15,9 +16,11 @@ class QnConnectToCloudWatcher: public QObject
     Q_OBJECT
 public:
     QnConnectToCloudWatcher();
-    virtual ~QnConnectToCloudWatcher() {}
+    virtual ~QnConnectToCloudWatcher();
 private slots:
     void at_updateConnection();
 private:
-    QUrl m_lastCloudUrl;
+    QUrl m_cloudUrl;
+    QTimer m_timer;
+    std::unique_ptr<nx::network::cloud::CloudModuleEndPointFetcher> m_cdbEndPointFetcher;
 };
