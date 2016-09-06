37ebe051ac4b4e044bfa159d97e5f368bbc91eb8
Sergey Ivanov <sivanov@networkoptix.com>
2016-09-06 12:55:31 +0300

    ##Code Style
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- common/src/network/multicast_module_finder.cpp
+++ common/src/network/multicast_module_finder.cpp
@@ -23,13 +23,13 @@ static const int MAX_CACHE_SIZE_BYTES = 1024 * 64;
 
 namespace {
 
-    const unsigned defaultPingTimeoutMs = 1000 * 5;
-    const unsigned defaultKeepAliveMultiply = 5;
-    const unsigned errorWaitTimeoutMs = 1000;
-    const unsigned checkInterfacesTimeoutMs = 60 * 1000;
+const unsigned defaultPingTimeoutMs = 1000 * 5;
+const unsigned defaultKeepAliveMultiply = 5;
+const unsigned errorWaitTimeoutMs = 1000;
+const unsigned checkInterfacesTimeoutMs = 60 * 1000;
 
-    const QHostAddress defaultModuleRevealMulticastGroup = QHostAddress(lit("239.255.11.11"));
-    const quint16 defaultModuleRevealMulticastGroupPort = 5007;
+const QHostAddress defaultModuleRevealMulticastGroup = QHostAddress(lit("239.255.11.11"));
+const quint16 defaultModuleRevealMulticastGroupPort = 5007;
 
 
 } // anonymous namespace
@@ -42,7 +42,7 @@ QnMulticastModuleFinder::QnMulticastModuleFinder(
     const quint16 multicastGroupPort,
     const unsigned int pingTimeoutMillis,
     const unsigned int keepAliveMultiply)
-:
+    :
     m_clientMode(clientOnly),
     m_serverSocket(nullptr),
     m_pingTimeoutMillis(pingTimeoutMillis == 0 ? defaultPingTimeoutMs : pingTimeoutMillis),
@@ -58,13 +58,15 @@ QnMulticastModuleFinder::QnMulticastModuleFinder(
     connect(qnCommon, &QnCommonModule::moduleInformationChanged, this, &QnMulticastModuleFinder::at_moduleInformationChanged, Qt::DirectConnection);
 }
 
-QnMulticastModuleFinder::~QnMulticastModuleFinder() {
+QnMulticastModuleFinder::~QnMulticastModuleFinder()
+{
     stop();
     clearInterfaces();
 }
 
-bool QnMulticastModuleFinder::isValid() const {
-    QnMutexLocker lk( &m_mutex );
+bool QnMulticastModuleFinder::isValid() const
+{
+    QnMutexLocker lk(&m_mutex);
     return !m_clientSockets.empty();
 }
 
@@ -78,17 +80,20 @@ void QnMulticastModuleFinder::setCompatibilityMode(bool compatibilityMode) {
 
 bool QnMulticastModuleFinder::isDisabled(false);
 
-void QnMulticastModuleFinder::setCheckInterfacesTimeout(unsigned int checkInterfacesTimeoutMs) {
+void QnMulticastModuleFinder::setCheckInterfacesTimeout(unsigned int checkInterfacesTimeoutMs)
+{
     m_checkInterfacesTimeoutMs = checkInterfacesTimeoutMs;
 }
 
-void QnMulticastModuleFinder::updateInterfaces() {
-    QnMutexLocker lk( &m_mutex );
+void QnMulticastModuleFinder::updateInterfaces()
+{
+    QnMutexLocker lk(&m_mutex);
 
     QList<QHostAddress> addressesToRemove = m_clientSockets.keys();
 
     /* This function only adds interfaces to the list. */
-    for (const QHostAddress &address: QNetworkInterface::allAddresses()) {
+    for (const QHostAddress &address : QNetworkInterface::allAddresses())
+    {
         addressesToRemove.removeOne(address);
 
         if (address.protocol() != QAbstractSocket::IPv4Protocol)
@@ -97,7 +102,8 @@ void QnMulticastModuleFinder::updateInterfaces() {
         if (m_clientSockets.contains(address))
             continue;
 
-        try {
+        try
+        {
             //if( addressToUse == QHostAddress(lit("127.0.0.1")) )
             //    continue;
             auto sock = std::make_unique<nx::network::UDPSocket>(AF_INET);
@@ -108,16 +114,23 @@ void QnMulticastModuleFinder::updateInterfaces() {
             if (m_serverSocket)
                 m_serverSocket->joinGroup(m_multicastGroupAddress.toString(), address.toString());
 
-            if (!m_pollSet.add(it.value(), aio::etRead, it.value())) {
+            if (!m_pollSet.add(it.value(), aio::etRead, it.value()))
+            {
                 const auto error = SystemError::getLastOSErrorText();
                 NX_ASSERT(false, Q_FUNC_INFO, error.toUtf8().data());
             }
-        } catch(const std::exception &e) {
-            NX_LOG(lit("Failed to create socket on local address %1. %2").arg(address.toString()).arg(QString::fromLatin1(e.what())), cl_logERROR);
+        }
+        catch (const std::exception &e)
+        {
+            NX_LOGX(lit("Failed to create socket on local address %1. %2")
+                .arg(address.toString())
+                .arg(QString::fromLatin1(e.what())),
+                cl_logERROR);
         }
     }
 
-    for (const QHostAddress &address: addressesToRemove) {
+    for (const QHostAddress &address : addressesToRemove)
+    {
         UDPSocket *socket = m_clientSockets.take(address);
         m_pollSet.remove(socket, aio::etRead);
         if (m_serverSocket)
@@ -125,10 +138,12 @@ void QnMulticastModuleFinder::updateInterfaces() {
     }
 }
 
-void QnMulticastModuleFinder::clearInterfaces() {
+void QnMulticastModuleFinder::clearInterfaces()
+{
     QnMutexLocker lk(&m_mutex);
 
-    if (m_serverSocket) {
+    if (m_serverSocket)
+    {
         for (auto it = m_clientSockets.begin(); it != m_clientSockets.end(); ++it)
             m_serverSocket->leaveGroup(m_multicastGroupAddress.toString(), it.key().toString());
 
@@ -136,25 +151,29 @@ void QnMulticastModuleFinder::clearInterfaces() {
         m_serverSocket.reset();
     }
 
-    for (UDPSocket *socket: m_clientSockets) {
+    for (UDPSocket *socket : m_clientSockets)
+    {
         m_pollSet.remove(socket, aio::etRead);
         delete socket;
     }
     m_clientSockets.clear();
 }
 
-void QnMulticastModuleFinder::pleaseStop() {
+void QnMulticastModuleFinder::pleaseStop()
+{
     QnLongRunnable::pleaseStop();
     m_pollSet.interrupt();
 }
 
-bool QnMulticastModuleFinder::processDiscoveryRequest(UDPSocket *udpSocket) {
+bool QnMulticastModuleFinder::processDiscoveryRequest(UDPSocket *udpSocket)
+{
     static const size_t READ_BUFFER_SIZE = UDPSocket::MAX_PACKET_SIZE;
     quint8 readBuffer[READ_BUFFER_SIZE];
 
     SocketAddress remoteEndpoint;
     int bytesRead = udpSocket->recvFrom(readBuffer, READ_BUFFER_SIZE, &remoteEndpoint);
-    if (bytesRead == -1) {
+    if (bytesRead == -1)
+    {
         SystemError::ErrorCode prevErrorCode = SystemError::getLastOSErrorCode();
         NX_LOGX(lit("Failed to read socket on local address (%1). %2").
             arg(udpSocket->getLocalAddress().toString()).arg(SystemError::toString(prevErrorCode)), cl_logERROR);
@@ -162,7 +181,8 @@ bool QnMulticastModuleFinder::processDiscoveryRequest(UDPSocket *udpSocket) {
     }
 
     //parsing received request
-    if (!RevealRequest::isValid(readBuffer, readBuffer + bytesRead)) {
+    if (!RevealRequest::isValid(readBuffer, readBuffer + bytesRead))
+    {
         //invalid response
         NX_LOGX(lit("Received invalid response from (%1) on local address %2").
             arg(remoteEndpoint.toString()).arg(udpSocket->getLocalAddress().toString()), cl_logDEBUG1);
@@ -175,7 +195,8 @@ bool QnMulticastModuleFinder::processDiscoveryRequest(UDPSocket *udpSocket) {
         if (m_serializedModuleInfo.isEmpty())
             m_serializedModuleInfo = RevealResponse(qnCommon->moduleInformation()).serialize();
     }
-    if (!udpSocket->sendTo(m_serializedModuleInfo.data(), m_serializedModuleInfo.size(), remoteEndpoint)) {
+    if (!udpSocket->sendTo(m_serializedModuleInfo.data(), m_serializedModuleInfo.size(), remoteEndpoint))
+    {
         NX_LOGX(lit("Can't send response to address (%1)").
             arg(remoteEndpoint.toString()), cl_logDEBUG1);
         return false;
@@ -196,9 +217,11 @@ RevealResponse *QnMulticastModuleFinder::getCachedValue(const quint8* buffer, co
     m_mdctx.addData(reinterpret_cast<const char*>(buffer), bufferEnd - buffer);
     QByteArray result = m_mdctx.result();
     RevealResponse* response = m_cachedResponse[result];
-    if (!response) {
+    if (!response)
+    {
         response = new RevealResponse();
-        if (!response->deserialize(buffer, bufferEnd)) {
+        if (!response->deserialize(buffer, bufferEnd))
+        {
             delete response;
             return 0;
         }
@@ -207,28 +230,36 @@ RevealResponse *QnMulticastModuleFinder::getCachedValue(const quint8* buffer, co
     return response;
 }
 
-bool QnMulticastModuleFinder::processDiscoveryResponse(UDPSocket *udpSocket) {
+bool QnMulticastModuleFinder::processDiscoveryResponse(UDPSocket *udpSocket)
+{
     const size_t readBufferSize = UDPSocket::MAX_PACKET_SIZE;
     quint8 readBuffer[readBufferSize];
 
     SocketAddress remoteEndpoint;
     int bytesRead = udpSocket->recvFrom(readBuffer, readBufferSize, &remoteEndpoint);
-    if (bytesRead == -1) {
+    if (bytesRead == -1)
+    {
         SystemError::ErrorCode prevErrorCode = SystemError::getLastOSErrorCode();
         NX_LOGX(lit("Failed to read socket on local address (%1). %2")
-               .arg(udpSocket->getLocalAddress().toString()).arg(SystemError::toString(prevErrorCode)), cl_logERROR);
+            .arg(udpSocket->getLocalAddress().toString())
+            .arg(SystemError::toString(prevErrorCode)),
+            cl_logERROR);
         return false;
     }
 
     RevealResponse *response = getCachedValue(readBuffer, readBuffer + bytesRead);
-    if (!response) {
+    if (!response)
+    {
         //invalid response
-        NX_LOGX(lit("Received invalid response from (%1) on local address %2").
-            arg(remoteEndpoint.toString()).arg(udpSocket->getLocalAddress().toString()), cl_logDEBUG1);
+        NX_LOGX(lit("Received invalid response from (%1) on local address %2")
+            .arg(remoteEndpoint.toString())
+            .arg(udpSocket->getLocalAddress().toString()),
+            cl_logDEBUG1);
         return false;
     }
 
-    if (response->type != QnModuleInformation::nxMediaServerId() && response->type != QnModuleInformation::nxECId())
+    if (response->type != QnModuleInformation::nxMediaServerId()
+        && response->type != QnModuleInformation::nxECId())
         return true;
 
     //TODO: #GDM #isCompatibleCustomization VMS-2163
@@ -256,7 +287,8 @@ bool QnMulticastModuleFinder::processDiscoveryResponse(UDPSocket *udpSocket) {
     return true;
 }
 
-void QnMulticastModuleFinder::run() {
+void QnMulticastModuleFinder::run()
+{
     if (isDisabled)
         return;
 
@@ -265,7 +297,8 @@ void QnMulticastModuleFinder::run() {
 
     QByteArray revealRequest = RevealRequest::serialize();
 
-    if (!m_clientMode) {
+    if (!m_clientMode)
+    {
         QnMutexLocker lk(&m_mutex);
 
         m_serverSocket.reset(new UDPSocket(AF_INET));
@@ -280,25 +313,32 @@ void QnMulticastModuleFinder::run() {
         }
     }
 
-    while (!needToStop()) {
+    while (!needToStop())
+    {
         quint64 currentClock = QDateTime::currentMSecsSinceEpoch();
 
-        if (currentClock - m_lastInterfacesCheckMs >= m_checkInterfacesTimeoutMs) {
+        if (currentClock - m_lastInterfacesCheckMs >= m_checkInterfacesTimeoutMs)
+        {
             updateInterfaces();
             m_lastInterfacesCheckMs = currentClock;
         }
 
         currentClock = QDateTime::currentMSecsSinceEpoch();
 
-        if (currentClock - m_prevPingClock >= m_pingTimeoutMillis) {
-            QnMutexLocker lk( &m_mutex );
+        if (currentClock - m_prevPingClock >= m_pingTimeoutMillis)
+        {
+            QnMutexLocker lk(&m_mutex);
 
-            for (UDPSocket *socket: m_clientSockets) {
-                if (!socket->send(revealRequest.data(), revealRequest.size())) {
+            for (UDPSocket *socket : m_clientSockets)
+            {
+                if (!socket->send(revealRequest.data(), revealRequest.size()))
+                {
                     //failed to send packet ???
                     SystemError::ErrorCode prevErrorCode = SystemError::getLastOSErrorCode();
-                    NX_LOGX(lit("Failed to send packet to %1. %2").
-                        arg(socket->getForeignAddress().toString()).arg(SystemError::toString(prevErrorCode)), cl_logDEBUG1);
+                    NX_LOGX(lit("Failed to send packet to %1. %2")
+                        .arg(socket->getForeignAddress().toString())
+                        .arg(SystemError::toString(prevErrorCode)),
+                        cl_logDEBUG1);
                     //TODO #ak if corresponding interface is down, should remove socket from set
                 }
             }
@@ -310,7 +350,8 @@ void QnMulticastModuleFinder::run() {
         if (socketCount == 0)
             continue;    //timeout
 
-        if (socketCount < 0) {
+        if (socketCount < 0)
+        {
             SystemError::ErrorCode prevErrorCode = SystemError::getLastOSErrorCode();
             if (prevErrorCode == SystemError::interrupted)
                 continue;
@@ -323,7 +364,8 @@ void QnMulticastModuleFinder::run() {
         //currentClock = QDateTime::currentMSecsSinceEpoch();
 
         /* some sockets changed state */
-        for (auto it = m_pollSet.begin(); it != m_pollSet.end(); ++it) {
+        for (auto it = m_pollSet.begin(); it != m_pollSet.end(); ++it)
+        {
             if (!(it.eventType() & aio::etRead))
                 continue;
 
@@ -338,5 +380,5 @@ void QnMulticastModuleFinder::run() {
 
     clearInterfaces();
 
-    NX_LOG(lit("QnMulticastModuleFinder stopped"), cl_logDEBUG1);
+    NX_LOGX(lit("stopped"), cl_logDEBUG1);
 }
