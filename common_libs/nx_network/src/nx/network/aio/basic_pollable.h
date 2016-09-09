f6a64ad2b2e03bee56a85d148562aab5cebac4de
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-09-09 21:41:44 +0300

    abstract method BasicPollable::stopWhileInAioThread moved to protected section
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common_libs/nx_network/src/nx/network/aio/basic_pollable.h
+++ common_libs/nx_network/src/nx/network/aio/basic_pollable.h
@@ -9,22 +9,22 @@
 
 #include "timer.h"
 
-
 namespace nx {
 namespace network {
 namespace aio {
 
-/** TODO #ak conflicts with Pollable. Introduce proper names for Pollable, AbstractPollable, BasicPollable.
-    This class implements \a AbstractPollable and simplifies async operation cancellation 
-        by introducing \a BasicPollable::stopWhileInAioThread method.
-
-    Successor to this class MUST support safe object deletion while in object's aio thread.
-    \a QnStoppableAsync::pleaseStop and \a BasicPollable::stopWhileInAioThread 
-        are not called in this case. 
-    So, it is recommended that
-    - \a BasicPollable::stopWhileInAioThread implementation can be safely called multiple times
-    - \a BasicPollable::stopWhileInAioThread call is added to the destructor
-*/
+/**
+ * This class implements \a AbstractPollable and simplifies async operation cancellation 
+ *     by introducing \a BasicPollable::stopWhileInAioThread method.
+ *
+ * TODO #ak conflicts with Pollable. Introduce proper names for Pollable, AbstractPollable, BasicPollable.
+ * Successor to this class MUST support safe object deletion while in object's aio thread.
+ * \a QnStoppableAsync::pleaseStop and \a BasicPollable::stopWhileInAioThread 
+ *     are not called in this case. 
+ * So, it is recommended that
+ * - \a BasicPollable::stopWhileInAioThread implementation can be safely called multiple times
+ * - \a BasicPollable::stopWhileInAioThread call is added to the destructor
+ */
 class NX_NETWORK_API BasicPollable
 :
     public AbstractPollable
@@ -36,24 +36,27 @@ public:
     void pleaseStopSync(bool doNotCheckForLocks = false) override;
 
     virtual aio::AbstractAioThread* getAioThread() const override;
-    /** Generally, binding to aio thread can be done just after 
-            object creation before any usage.
-        Some implementation may allow more (e.g., binding if no async 
-            operations are scheduled at the moment)
-        \note Re-binding is allowed
-    */
+    /**
+     * Generally, binding to aio thread can be done just after 
+     *     object creation before any usage.
+     * Some implementation may allow more (e.g., binding if no async 
+     *     operations are scheduled at the moment)
+     * @note Re-binding is allowed
+     */
     virtual void bindToAioThread(aio::AbstractAioThread* aioThread) override;
     virtual void post(nx::utils::MoveOnlyFunc<void()> func) override;
     virtual void dispatch(nx::utils::MoveOnlyFunc<void()> func) override;
-    virtual void stopWhileInAioThread() = 0;
 
 protected:
     Timer* timer();
+
+    /** Cancel your asynchronous operations here. */
+    virtual void stopWhileInAioThread() = 0;
 
 private:
     Timer m_timer;
 };
 
-}   //namespace aio
-}   //namespace network
-}   //namespace nx
+} // namespace aio
+} // namespace network
+} // namespace nx
