d4422105edb6ba93dbd7838b521e8e2ca9795e6e
Alexander Kulikov <akulikov@networkoptix.com>
2016-09-12 19:12:09 +0400

    vms-3725. Do not show 'Remove' for the system drive.
    <==== Mercurial data ====>
    Branch: dev_2.6

--- client/src/ui/models/storage_list_model.cpp
+++ client/src/ui/models/storage_list_model.cpp
@@ -440,7 +440,7 @@ bool QnStorageListModel::canRemoveStorage( const QnStorageModelInfo &data ) cons
     if (isStoragePoolInRebuild(data))
         return false;
 
-    return data.isExternal;
+    return data.isExternal || !data.isOnline;
 }
 
 bool QnStorageListModel::storageIsActive(const QnStorageModelInfo &data) const
