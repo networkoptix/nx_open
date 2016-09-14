340cb426ae645971b17a71beab1562626937f44a
Denis Klychkov <dklychkov@networkoptix.com>
2016-09-14 11:56:04 +0300

    Compilation fix.
    <==== Mercurial data ====>
    Branch: dev_2.6

--- client/src/client/client_model_functions.cpp
+++ client/src/client/client_model_functions.cpp
@@ -5,23 +5,26 @@
 #include <client/client_meta_types.h>
 #include <client/client_model_types.h>
 
-QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn::ClientSkin,
+QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
+    Qn, ClientSkin,
     (Qn::LightSkin, "LightSkin")
     (Qn::DarkSkin, "DarkSkin")
-);
+)
 
-QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn::BackgroundAnimationMode,
-                                          (Qn::DefaultAnimation,    "DefaultAnimation")
-                                          (Qn::RainbowAnimation,    "RainbowAnimation")
-                                          (Qn::CustomAnimation,     "CustomAnimation")
-                                          );
+QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
+    Qn, BackgroundAnimationMode,
+    (Qn::DefaultAnimation,    "DefaultAnimation")
+    (Qn::RainbowAnimation,    "RainbowAnimation")
+    (Qn::CustomAnimation,     "CustomAnimation")
+)
 
-QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn::ImageBehaviour,
-                                          (Qn::StretchImage,        "StretchImage")
-                                          (Qn::CropImage,           "CropImage")
-                                          (Qn::FitImage,            "FitImage")
-                                          (Qn::TileImage,           "TileImage")
-                                          );
+QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
+    Qn, ImageBehaviour,
+    (Qn::StretchImage,        "StretchImage")
+    (Qn::CropImage,           "CropImage")
+    (Qn::FitImage,            "FitImage")
+    (Qn::TileImage,           "TileImage")
+)
 
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnWorkbenchState, (datastream), (currentLayoutIndex)(layoutUuids));
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnServerStorageKey, (datastream)(eq)(hash), (serverUuid)(storagePath));
@@ -29,8 +32,8 @@ QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnLicenseWarningState, (datastream), (lastWarni
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnClientBackground, (datastream), QnClientBackground_Fields);
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnPtzHotkey, (json), (id)(hotkey))
 
-    QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
-    Qn::Permissions,
+QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
+    Qn, Permissions,
     (Qn::ReadPermission,                            "ReadPermission")
     (Qn::WritePermission,                           "WritePermission")
     (Qn::SavePermission,                            "SavePermission")
@@ -57,10 +60,10 @@ QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnPtzHotkey, (json), (id)(hotkey))
     (Qn::GlobalEditVideoWallPermission,             "GlobalEditVideoWallPermission")
     (Qn::DeprecatedEditCamerasPermission,           "DeprecatedEditCamerasPermission")
     (Qn::DeprecatedViewExportArchivePermission,     "DeprecatedViewExportArchivePermission")
-    );
+)
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
-    Qn::Permission,
+    Qn, Permission,
     (Qn::ReadPermission,                            "ReadPermission")
     (Qn::WritePermission,                           "WritePermission")
     (Qn::SavePermission,                            "SavePermission")
@@ -87,4 +90,4 @@ QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(
     (Qn::GlobalEditVideoWallPermission,             "GlobalEditVideoWallPermission")
     (Qn::DeprecatedEditCamerasPermission,           "DeprecatedEditCamerasPermission")
     (Qn::DeprecatedViewExportArchivePermission,     "DeprecatedViewExportArchivePermission")
-    );
+)
