a7d0196a771db603ac75d22b9d1da45cd4c025e0
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-09-16 13:31:35 +0300

    removed unused code
    <==== Mercurial data ====>
    Branch: dev_2.6

--- common/src/utils/network/system_socket.h
+++ common/src/utils/network/system_socket.h
@@ -371,13 +371,6 @@ public:
     {
     }
 
-    template<class Param1Type, class Param2Type, class Param3Type, class Param4Type, class Param5Type>
-    SocketImplementationDelegate(const Param1Type& param1, const Param2Type& param2, const Param3Type& param3, const Param4Type& param4, const Param5Type& param5)
-        :
-        base_type(this, param1, param2, param3, param4, param5)
-    {
-    }
-
     //!Implementation of AbstractSocket::terminateAsyncIO
     virtual void terminateAsyncIO( bool waitForRunningHandlerCompletion ) override {
         this->m_implDelegate.terminateAsyncIO( waitForRunningHandlerCompletion );
