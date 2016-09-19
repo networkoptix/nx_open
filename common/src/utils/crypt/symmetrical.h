9e38322c3c941c49842899739f1a736ee5ea0e73
Alexander Kulikov <akulikov@networkoptix.com>
2016-09-19 17:41:03 +0300

    vms-3429. Encrypting or filtering some public data.
    <==== Mercurial data ====>
    Branch: x_vms_3429_filtering_or_encrypting_public_data

--- common/src/utils/crypt/symmetrical.h
+++ common/src/utils/crypt/symmetrical.h
@@ -9,6 +9,6 @@ namespace nx {
 namespace utils {
 QByteArray encodeSimple(const QByteArray& data);
 QByteArray encodeAES128CBC(const QByteArray& data, const std::array<uint8_t, 16>& key);
-QByteArray decodeAES128CBC(const QByteArray& data, const std::array<uint8_t, 16>& key)
+QByteArray decodeAES128CBC(const QByteArray& data, const std::array<uint8_t, 16>& key);
 }
 }
