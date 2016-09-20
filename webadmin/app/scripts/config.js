23f1e3977a4c4f804010d15b458a3446bfa1ca97
Evgeny Balashov <ebalashov@networkoptix.com>
2016-09-20 12:31:02 -0700

    VMS-3526: Changing of "Cloud portal url" parameter leads to server blocking
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/app/scripts/config.js
+++ webadmin/app/scripts/config.js
@@ -104,10 +104,10 @@ var Config = {
         backupQualities: {label: 'Backup qualities', type: 'text'},
         serverDiscoveryPingTimeoutSec: {label: 'Server discovery timeout', type: 'number'},
 
-
         cloudAccountName: {label: 'Cloud owner account', type: 'static'},
+        cloudHost: {label: 'Cloud host', type: 'static'},
         cloudAuthKey: {label: 'Cloud auth key', type: 'static'},
-        cloudSystemID: {label: 'Cloud portal url', type: 'static'},
+        cloudSystemID: {label: 'Cloud system id', type: 'static'},
 
         newSystem: {label: 'Server is "New"', type: 'static'},
         proxyConnectTimeoutSec: {label: 'Proxy connection timeout (seconds)', type: 'number'},
