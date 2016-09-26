741c35e60bd83f5b4ea55772ea845c54927e6146
Vitaly Kutin <vkutin@networkoptix.com>
2016-09-26 18:14:36 +0300

    ## Title bar menu buttons state fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/widgets/main_window_title_bar_widget.cpp
+++ client/libclient/src/ui/widgets/main_window_title_bar_widget.cpp
@@ -102,8 +102,9 @@ QnMainWindowTitleBarWidget::QnMainWindowTitleBarWidget(
             QnHiDpiWorkarounds::showMenu(menu.data(),
                 QnHiDpiWorkarounds::safeMapToGlobal(d->mainMenuButton,
                     d->mainMenuButton->rect().bottomLeft()));
-    });
 
+            d->mainMenuButton->setDown(false);
+    });
 
     d->tabBar = new QnLayoutTabBar(this);
     d->tabBar->setFocusPolicy(Qt::NoFocus);
@@ -153,6 +154,8 @@ QnMainWindowTitleBarWidget::QnMainWindowTitleBarWidget(
             QnHiDpiWorkarounds::showMenu(menu.data(),
                 QnHiDpiWorkarounds::safeMapToGlobal(d->currentLayoutsButton,
                     d->currentLayoutsButton->rect().bottomLeft()));
+
+            d->currentLayoutsButton->setDown(false);
         });
 
     layout->addWidget(d->newTabButton);
