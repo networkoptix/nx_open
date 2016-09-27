2a2a1530bf9ee8d523d17d16f900e6ee2273f8f6
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-09-27 18:19:42 +0300

    #CLOUD-568 review fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common/src/utils/db/types.cpp
+++ common/src/utils/db/types.cpp
@@ -5,9 +5,9 @@
 // Using Qt plugin names here.
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(nx::db, RdbmsDriverType,
-(nx::db::RdbmsDriverType::unknown, "bad_driver_name")
-(nx::db::RdbmsDriverType::sqlite, "QSQLITE")
-(nx::db::RdbmsDriverType::mysql, "QMYSQL")
-(nx::db::RdbmsDriverType::postgresql, "QPSQL")
-(nx::db::RdbmsDriverType::oracle, "QOCI")
+    (nx::db::RdbmsDriverType::unknown, "bad_driver_name")
+    (nx::db::RdbmsDriverType::sqlite, "QSQLITE")
+    (nx::db::RdbmsDriverType::mysql, "QMYSQL")
+    (nx::db::RdbmsDriverType::postgresql, "QPSQL")
+    (nx::db::RdbmsDriverType::oracle, "QOCI")
 )
