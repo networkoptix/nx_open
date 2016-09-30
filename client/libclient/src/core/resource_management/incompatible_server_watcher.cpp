2ee1cdefec7c13e1ba9fcb96169057ccf732ccd8
Roman Vasilenko <rvasilenko@networkoptix.com>
2016-09-30 22:43:18 +0300

    VMS-3987: System name refactoring
    compile fix
    
    <==== Mercurial data ====>
    Branch: x_vms_3987_system_name

--- client/libclient/src/core/resource_management/incompatible_server_watcher.cpp
+++ client/libclient/src/core/resource_management/incompatible_server_watcher.cpp
@@ -248,7 +248,7 @@ void QnIncompatibleServerWatcherPrivate::addResource(const ec2::ApiDiscoveredSer
             fakeUuidByServerUuid[serverData.id] = server->getId();
             serverUuidByFakeUuid[server->getId()] = serverData.id;
         }
-        qnResPool->addIncopatibleResource(server);
+        qnResPool->addIncompatibleResource(server);
 
         NX_LOG(lit("QnIncompatibleServerWatcher: Add incompatible server %1 at %2 [%3]")
             .arg(serverData.id.toString())
