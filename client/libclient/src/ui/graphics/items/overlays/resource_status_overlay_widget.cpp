855e039158f4b78c9fec4b582478169702259e2e
Denis Klychkov <dklychkov@networkoptix.com>
2016-10-03 20:25:28 +0300

    VMS-4109: Item placeholder disappears on high scene zoom levels.
    In the previous implementation the whole status title (icon + caption)
    was emplaced in a single proxy widget wich occupied all the available space.
    It led to infinite growth of that proxy widget with growth of the scene scale.
    proxy widget uses grabWidget to get a pixmap, thus it tried to get
    too big pixmap to render.
    Now this caption cannot be bigger than it's sufficient maximum size and
    we don't get such big pixmaps.
    
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/graphics/items/overlays/resource_status_overlay_widget.cpp
+++ client/libclient/src/ui/graphics/items/overlays/resource_status_overlay_widget.cpp
@@ -230,16 +230,16 @@ void QnStatusOverlayWidget::setupCentralControls()
     const auto layout = new QVBoxLayout(container);
     layout->setSpacing(0);
 
-    layout->addStretch(1);
     layout->addWidget(m_centralAreaImage, 0, Qt::AlignHCenter);
     layout->addWidget(m_caption, 0, Qt::AlignHCenter);
-    layout->addStretch(1);
 
-    layout->setSizeConstraint(QLayout::SetMinimumSize);
+    layout->setSizeConstraint(QLayout::SetMinAndMaxSize);
 
-    const auto holderLayout = new QGraphicsLinearLayout(m_centralHolder);
+    const auto holderLayout = new QGraphicsLinearLayout(Qt::Vertical, m_centralHolder);
     holderLayout->setContentsMargins(16, 60, 16, 60);
+    holderLayout->addStretch(1);
     holderLayout->addItem(makeMaskedProxy(container, m_centralHolder));
+    holderLayout->addStretch(1);
 
     m_centralHolder->setOpacity(0.7);
     m_centralHolder->setAcceptedMouseButtons(Qt::NoButton);
