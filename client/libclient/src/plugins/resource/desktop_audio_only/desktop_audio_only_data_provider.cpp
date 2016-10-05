a64ef379ca6cfe89b80854175afd66272b87d2df
Dmitry Mishin <dmishin@networkoptix.com>
2016-10-05 10:15:12 +0100

    ## review fixes
    <==== Mercurial data ====>
    Branch: x_VMS_transcoding_issues

--- client/libclient/src/plugins/resource/desktop_audio_only/desktop_audio_only_data_provider.cpp
+++ client/libclient/src/plugins/resource/desktop_audio_only/desktop_audio_only_data_provider.cpp
@@ -163,8 +163,8 @@ bool QnDesktopAudioOnlyDataProvider::initAudioEncoder()
 
     encoderCtx->codec_id = audioCodec->id;
     encoderCtx->codec_type = AVMEDIA_TYPE_AUDIO;
-    encoderCtx->sample_fmt =
-        QnFfmpegAudioDecoder::audioFormatQtToFfmpeg(
+    encoderCtx->sample_fmt = 
+        QnFfmpegHelper::fromQtAudioFormatToFfmpegSampleType(
             fromQtAudioFormat(format));
 
     encoderCtx->channels = m_audioSourcesInfo.size() > 1 ?
