7d16bb453e105c677eb6e6839cc360a07bc7986b
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-10-10 16:46:03 +0300

    #VMS-3992 review fixes
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common_libs/nx_network/src/nx/network/time/abstract_accurate_time_fetcher.h
+++ common_libs/nx_network/src/nx/network/time/abstract_accurate_time_fetcher.h
@@ -7,25 +7,27 @@
 #include "../aio/basic_pollable.h"
 
 /**
- * Abstract interface for class performing fetching accurate current time from some public server
+ * Abstract interface for class performing fetching accurate current time from some public server.
  */
 class NX_NETWORK_API AbstractAccurateTimeFetcher
 :
     public nx::network::aio::BasicPollable
 {
 public:
+    /**
+     * Functor arguments:
+     * - \a qint64 on success UTC millis from epoch. On failure -1.
+     * - \a SystemError::ErrorCode last system error code. \a SystemError::noError in case of success.
+     */
     typedef nx::utils::MoveOnlyFunc<void(qint64, SystemError::ErrorCode)>
         CompletionHandler;
 
     virtual ~AbstractAccurateTimeFetcher() = default;
 
     /**
-     * Initiates asynchronous time request operation
-     * Upon completion \a handlerFunc will be called. Following parameters are passed:\n
-     * - \a qint64 on success UTC millis from epoch. On failure -1
-     * - \a SystemError::ErrorCode last system error code. \a SystemError::noError in case of success
-     * \note Implementation is NOT REQUIRED to support performing multiple simultaneous operations
-     * @return \a true if request issued successfully, otherwise \a false
+     * Initiates asynchronous time request operation.
+     * @note Implementation is NOT REQUIRED to support performing multiple simultaneous operations.
+     * @return \a true if request issued successfully, otherwise \a false.
      */
     virtual void getTimeAsync(CompletionHandler handlerFunc) = 0;
 };
