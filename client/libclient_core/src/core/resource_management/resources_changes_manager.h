8e54555993ab559dfc3535cdb066de4b998dba65
Sergey Ivanov <sivanov@networkoptix.com>
2016-10-19 11:07:46 +0300

    VMS-4268 - Do not allow to save User Role if role with such name already exist
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient_core/src/core/resource_management/resources_changes_manager.h
+++ client/libclient_core/src/core/resource_management/resources_changes_manager.h
@@ -61,8 +61,8 @@ public:
     /** Save accessible resources for the given user */
     void saveAccessibleResources(const QnResourceAccessSubject& subject, const QSet<QnUuid>& accessibleResources);
 
-    void saveUserGroup(const ec2::ApiUserGroupData& userGroup);
-    void removeUserGroup(const QnUuid& groupId);
+    void saveUserRole(const ec2::ApiUserGroupData& role);
+    void removeUserRole(const QnUuid& id);
 
     /** Apply changes to the given videoWall. */
     void saveVideoWall(const QnVideoWallResourcePtr &videoWall, VideoWallChangesFunction applyChanges);
