1a062b28f9884d80eb6997ac0acdc31f1b4446c8
Denis Klychkov <dklychkov@networkoptix.com>
2016-10-26 13:16:43 +0300

    CMake: working on common build...
    <==== Mercurial data ====>
    Branch: cmake_experiments

---
+++ cmake/ec_protocol.cmake
@@ -0,0 +1,6 @@
+# YOU MUST FIX THE LINES BELOW to make sure it will generate a merge conflict
+# if somebody also updates the protocol version.
+# Prefer to use issue number and title as a comment.
+
+# Changed localSystem
+set(nxec.ec2ProtoVersion "3022")
--- customization/default-values.cmake
+++ customization/default-values.cmake
@@ -1,34 +1,34 @@
-set (updatefeed.auto "false")
-set (mac.skip.sign "false")
-set (windows.skip.sign "false")
-#skip.sign false
-set (help.language "english")
-set (quicksync "false")
-set (dynamic.customization "false")
-set (display.mobile.name "${display.product.name} Mobile")
-# if true, you can use --customization <path> to specify customization
-# Note: config json files should be there
+set(updatefeed.auto "false")
+set(mac.skip.sign "false")
+set(windows.skip.sign "false")
 
-set (uri.protocol "nx-vms")
+set(help.language "english")
+set(quicksync "false")
+set(dynamic.customization "false")
+set(display.mobile.name "${display.product.name} Mobile")
 
-set (nxtoolUpgradeCode "cc740987-5070-4750-b853-327417176031")
-set (short.company.name "${company.name}")
-set (company.support.link "${company.support.address}")
+set(uri.protocol "nx-vms")
+
+set(nxtoolUpgradeCode "cc740987-5070-4750-b853-327417176031")
+set(short.company.name "${company.name}")
+set(company.support.link "${company.support.address}")
+set(backgroundImage "{}")
 
 # Help and settings URLs
-set (helpUrl "http://networkoptix.com/files/help")
-set (settings.url "http://networkoptix.com/files/settings")
-set (showcase.url "http://networkoptix.com/files/showcase")
-set (update.generator.url "http://updates.hdw.mx/upcombiner/upcombine")
+set(helpUrl "http://networkoptix.com/files/help")
+set(settings.url "http://networkoptix.com/files/settings")
+set(showcase.url "http://networkoptix.com/files/showcase")
+set(update.generator.url "http://updates.hdw.mx/upcombiner/upcombine")
 
 # Update parameters
-set (mirrorListUrl "http://downloads.hdwitness.com/clients/mirror.xml")
-set (prodUpdateFeedUrl "http://updates.hdwitness.com/updates.json")
-set (releaseNotesUrl "http://updates.hdwitness.com/releasenotes.json")
-set (testUpdateFeedUrl "http://updates.hdwitness.com/updates.json")
-# Additional Features
-set (vmax "false")
-#(true or false)
+set(mirrorListUrl "http://downloads.hdwitness.com/clients/mirror.xml")
+set(prodUpdateFeedUrl "http://updates.hdwitness.com/updates.json")
+set(releaseNotesUrl "http://updates.hdwitness.com/releasenotes.json")
+set(testUpdateFeedUrl "http://updates.hdwitness.com/updates.json")
 
-set (old.android.packagename "${namespace.major}.${namespace.minor}.${namespace.additional}")
-set (new.android.packagename "${namespace.major}.${namespace.minor}.${new.namespace.additional}")
+# Additional Features
+set(vmax "false")
+
+# Cloud parameters
+set(cloudHost "cloud-test.hdw.mx")
+set(cloudName "Nx Cloud")
