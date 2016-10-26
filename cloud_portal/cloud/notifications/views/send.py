a7a7d61e6d3313a2ed015b5628b507fa8650eb61
Ivan Vigasin <ivigasin@networkoptix.com>
2016-10-25 18:36:04 -0700

    Undo authorization by secret
    <==== Mercurial data ====>
    Branch: cloud_portal_dev_3.0.0

--- cloud_portal/cloud/notifications/views/send.py
+++ cloud_portal/cloud/notifications/views/send.py
@@ -7,26 +7,9 @@ from django.core.exceptions import ValidationError
 from cloud import settings
 
 
-def check_secret(func):
-    def handler(*args, **kwargs):
-        request = args[0]
-
-        if 'secret' not in request.data or not request.data['secret']:
-            raise APINotAuthorisedException('No secret provided', ErrorCodes.forbidden)
-
-        key = request.data['secret']
-
-        if key != settings.CLOUD_CONNECT['secret']:
-            raise APINotAuthorisedException('Wrong secret provided', ErrorCodes.forbidden)
-
-        return func(*args, **kwargs)
-    return handler
-
-
 @api_view(['POST'])
 @permission_classes((AllowAny, ))
 @handle_exceptions
-@check_secret  # @ip_allow_only('local')
 def send_notification(request):
     try:
         validation_error = False
