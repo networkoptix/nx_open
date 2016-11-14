388d596f0ad11ed62e5535b4c7309aa89395aa01
Alexander Kulikov <akulikov@networkoptix.com>
2016-11-14 16:54:58 +0300

    vms-4372. in progress.
    <==== Mercurial data ====>
    Branch: x_vms_4372_test_storage_plugin

--- storage_plugins/teststorageplugin/src/json.h
+++ storage_plugins/teststorageplugin/src/json.h
@@ -1,43 +1,91 @@
-#include <stdint.h>
+#pragma once
+
+#include <stddef.h>
 
 enum JsonType 
 {
-    jsonStringT = 1,
-    jsonNumberT = 2,
-    jsonObjectT = 3,
-    jsonArrayT = 4,
-    jsonTrueT = 5,
-    jsonFalseT = 6,
-    jsonNullT = 7,
+    jsonStringT,
+    jsonNumberT,
+    jsonObjectT,
+    jsonArrayT,
+    jsonTrueT,
+    jsonFalseT,
+    jsonNullT,
 };
 
 struct JsonVal 
 {
-    JsonType type;
+    enum JsonType type;
     union
     {
         char *string;
         long double number;
         struct {
-            const char **keys;
-            JsonVal *values;
-            size_t len;
+            char **keys;
+            struct JsonVal *values;
+            int len;
         } object;
         struct {
-            JsonVal *values;
-            size_t len;
+            struct JsonVal *values;
+            int len;
         } array;
     } u;
 };
 
-struct JsonVal jsonParseString(const char *str, char **errorBuf, int errorBufSize); 
+
+struct JsonVal jsonParseString(const char *str, char *errorBuf, int errorBufSize);
+
+/*creates empty (top-level) object*/
+struct JsonVal jsonCreateObject();
+
+/*check type functions*/
+int JsonVal_isString(const struct JsonVal *val);
+int JsonVal_isNumber(const struct JsonVal *val);
+int JsonVal_isObject(const struct JsonVal *val);
+int JsonVal_isArray(const struct JsonVal *val);
+int JsonVal_isTrue(const struct JsonVal *val);
+int JsonVal_isFalse(const struct JsonVal *val);
+int JsonVal_isNull(const struct JsonVal *val);
+
+/*add subvalues to existing values. return pointers to the newly created values.*/
+struct JsonVal *JsonVal_objectAddString(struct JsonVal *val, const char *key, const char *value);
+struct JsonVal *JsonVal_objectAddNumber(struct JsonVal *val, const char *key, long double number);
+struct JsonVal *JsonVal_objectAddObject(struct JsonVal *val, const char *key);
+struct JsonVal *JsonVal_objectAddArray(struct JsonVal *val, const char *key);
+struct JsonVal *JsonVal_objectAddTrue(struct JsonVal *val, const char *key);
+struct JsonVal *JsonVal_objectAddFalse(struct JsonVal *val, const char *key);
+struct JsonVal *JsonVal_objectAddNull(struct JsonVal *val, const char *key);
+
+struct JsonVal *JsonVal_arrayAddString(struct JsonVal *val, const char *value);
+struct JsonVal *JsonVal_arrayAddNumber(struct JsonVal *val, long double number);
+struct JsonVal *JsonVal_arrayAddObject(struct JsonVal *val);
+struct JsonVal *JsonVal_arrayAddArray(struct JsonVal *val);
+struct JsonVal *JsonVal_arrayAddTrue(struct JsonVal *val);
+struct JsonVal *JsonVal_arrayAddFalse(struct JsonVal *val);
+struct JsonVal *JsonVal_arrayAddNull(struct JsonVal *val);
+
+/*access functions*/
+struct JsonVal *JsonVal_getObjectValueByKey(const struct JsonVal *val, const char *key);
+
+void JsonVal_forEachArrayElement(
+        const struct JsonVal *val,
+        void *ctx,
+        void (*action)(void *, const struct JsonVal *));
+
+void JsonVal_forEachObjectElement(
+        const struct JsonVal *val,
+        void *ctx,
+        void (*action)(void *, const char *key, const struct JsonVal *));
+
+/*write functions*/
+int JsonVal_write(
+        const struct JsonVal *val,
+        /*User context. Will be passed to the writeFunc callback.*/
+        void *ctx,
+        /*user supplied write callback function.*/
+        int (*writeFunc)(void *ctx, void *buf, int len));
+
+/*returns total bytes required*/
+int JsonVal_writeString(const struct JsonVal *val, char *buf, int len);
 
 void JsonVal_destroy(struct JsonVal *val);
-
-int JsonVal_isString(const struct JsonVal *val); 
-int JsonVal_isNumber(const struct JsonVal *val); 
-int JsonVal_isObject(const struct JsonVal *val); 
-int JsonVal_isArray(const struct JsonVal *val); 
-int JsonVal_isTrue((const struct JsonVal *val); 
-int JsonVal_isFalse(const struct JsonVal *val); 
-int JsonVal_isNull(const struct JsonVal *val); 
