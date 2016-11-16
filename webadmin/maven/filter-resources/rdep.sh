6a84a9da031e552ca58178d7009cdb77a3e68439
Denis Klychkov <dklychkov@networkoptix.com>
2016-11-16 10:44:22 +0300

    Clear webadmin package directory before uploading a new version.
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- webadmin/maven/filter-resources/rdep.sh
+++ webadmin/maven/filter-resources/rdep.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-mkdir -p ${environment.dir}/packages/any/server-external-${branch}-${release.version}/bin
-cp -f ${basedir}/external.dat ${environment.dir}/packages/any/server-external-${branch}-${release.version}/bin
-if [[ $? -ne 0 ]]; then exit $?; fi
-python ${root.dir}/build_utils/python/rdep.py -u -f -t any server-external-${branch}-${release.version} --root ${environment.dir}/packages
-if [[ $? -ne 0 ]]; then exit $?; fi
-if [[ $branch == prod_${release.version} ]]; then 
-    mkdir -p ${environment.dir}/packages/any/server-external-${release.version}/bin
-    cp -f ${basedir}/external.dat ${environment.dir}/packages/any/server-external-${release.version}/bin
-    python ${root.dir}/build_utils/python/rdep.py -u -f -t any server-external-${release.version} --root ${environment.dir}/packages
-    if [[ $? -ne 0 ]]; then exit $?; fi
-fi
+
+set -e
+
+ROOT="$environment/packages"
+
+function upload() {
+    PACKAGE=$1
+    PACKAGE_DIR="$ROOT/any/$PACKAGE"
+
+    [ -d "$PACKAGE_DIR" ] && rm -rf "$PACKAGE_DIR"
+
+    mkdir -p "$PACKAGE_DIR"/bin
+    cp -f ${basedir}/external.dat "$PACKAGE_DIR"/bin
+    ${root.dir}/build_utils/python/rdep.py -u -f -t any $PACKAGE --root $ROOT
+}
+
+upload "server-external-${branch}"
+[[ "${branch}" == "prod_${release.version}" ]] && upload "server-external-${release.version}"
