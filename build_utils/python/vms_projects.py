3370b5839b547e92d542694fa77a775753520526
Sergey Ivanov <sivanov@networkoptix.com>
2016-11-21 20:58:10 +0300

    VMS-4586 - Check customizations for 3.0.0 Beta
    Extended script support
    
    <==== Mercurial data ====>
    Branch: release_3.0

--- build_utils/python/vms_projects.py
+++ build_utils/python/vms_projects.py
@@ -24,7 +24,7 @@ class TranslatableProject():
             self.name, self.path, self.extensions, self.locations, self.sources)
             
     def __str__(self):
-        return "Project %s (at %s): %s, %s at %s>" % (
+        return "Project %s (at %s): %s, %s at %s" % (
             self.name, self.path, self.extensions, self.locations, self.sources)            
 
 translatableProjects = [   
@@ -41,5 +41,38 @@ translatableProjects = [
 def getTranslatableProjects():
     return translatableProjects
 
+class CustomizableProject():
+    name = ""
+    sources = None
+
+    def __init__(self, name, sources):
+        self.name = name
+        self.sources = sources
+               
+    def __repr__(self):
+        return "<CustomizableProject name:%s sources:%s>" % (
+            self.name, self.sources)
+            
+    def __str__(self):
+        return "Project %s %s" % (
+            self.name, "at {0}".format(self.sources) if self.sources else "")
+
+customizableProjects = [   
+    CustomizableProject("common", ["common/src"]),
+    CustomizableProject("icons", None),
+    CustomizableProject("client", ["client/libclient/src", "client/libclient/static-resources/src"]),
+    CustomizableProject("mobile_client", ["client/mobile_client/src", "client/mobile_client/static-resources/qml"])
+]
+
+def getCustomizableProjects():
+    return customizableProjects
+    
+    
 if __name__ == "__main__":
-    print getTranslatableProjects()
+    print "Translatable:"
+    for p in getTranslatableProjects():
+        print p
+        
+    print "Customizable:"
+    for p in getCustomizableProjects():
+        print p
