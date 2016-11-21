7f3dcb2c6048492caa03020e8f993f5d19d4caed
Sergey Ivanov <sivanov@networkoptix.com>
2016-11-21 15:48:21 +0300

    VMS-4572 - Update translations for 3.0.0 beta
    Improved project list description file
    
    <==== Mercurial data ====>
    Branch: x_vms_4572_translations

--- build_utils/python/vms_projects.py
+++ build_utils/python/vms_projects.py
@@ -3,6 +3,7 @@ class TranslatableProject():
     path = ""
     extensions = "cpp,h"
     locations = "none"
+    sources = "src"
 
     def __init__(self, name, path = None):
         self.name = name
@@ -15,10 +16,16 @@ class TranslatableProject():
       
     def qml(self):
         self.extensions = "qml"
+        self.sources = "static-resources"
         return self
         
     def __repr__(self):
-        return "<TranslatableProject name:%s path:%s extensions:%s locations:%s>" % (self.name, self.path, self.extensions, self.locations)
+        return "<TranslatableProject name:%s path:%s extensions:%s locations:%s sources:%s>" % (
+            self.name, self.path, self.extensions, self.locations, self.sources)
+            
+    def __str__(self):
+        return "Project %s (at %s): %s, %s at %s>" % (
+            self.name, self.path, self.extensions, self.locations, self.sources)            
 
 translatableProjects = [   
     TranslatableProject("common"),
@@ -32,3 +39,6 @@ translatableProjects = [
 
 def getTranslatableProjects():
     return translatableProjects
+
+if __name__ == "__main__":
+    print getTranslatableProjects()
