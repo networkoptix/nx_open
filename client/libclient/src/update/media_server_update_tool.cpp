5647d2eba80a7f54a58a171e46d52898051d944c
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2016-11-22 16:10:49 +0300

    VMS-4581 - After clicking Cancel during servers update, progress bar remains in System Administration window
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/update/media_server_update_tool.cpp
+++ client/libclient/src/update/media_server_update_tool.cpp
@@ -335,16 +335,16 @@ void QnMediaServerUpdateTool::startUpdate(const QnUpdateTarget& target)
         this, &QnMediaServerUpdateTool::setPeerStage);
     connect(m_updateProcess, &QnUpdateProcess::peerStageProgressChanged,
         this, &QnMediaServerUpdateTool::setPeerStageProgress);
-    connect(m_updateProcess, &QnUpdateProcess::updateFinished,
-        this, &QnMediaServerUpdateTool::finishUpdate);
     connect(m_updateProcess, &QnUpdateProcess::targetsChanged,
         this, &QnMediaServerUpdateTool::targetsChanged);
     connect(m_updateProcess, &QnUpdateProcess::lowFreeSpaceWarning,
         this, &QnMediaServerUpdateTool::lowFreeSpaceWarning, Qt::BlockingQueuedConnection);
 
-    connect(m_updateProcess, &QThread::finished, this,
-        [this, incompatibleTargets, clearTargetsWhenFinished]()
+    connect(m_updateProcess, &QnUpdateProcess::updateFinished, this,
+        [this, incompatibleTargets, clearTargetsWhenFinished](const QnUpdateResult& result)
         {
+            emit updateFinished(result);
+
             const auto watcher = qnDesktopClientMessageProcessor->incompatibleServerWatcher();
 
             m_updateProcess->deleteLater();
