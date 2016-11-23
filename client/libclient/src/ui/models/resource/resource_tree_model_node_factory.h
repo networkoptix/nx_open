f397fef3930271ba5faed25fe307d72888553648
Sergey Ivanov <sivanov@networkoptix.com>
2016-11-23 19:27:58 +0300

    ## Tree nodes factory
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

---
+++ client/libclient/src/ui/models/resource/resource_tree_model_node_factory.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <ui/models/resource/resource_tree_model_fwd.h>
+
+class QnResourceTreeModelNodeFactory
+{
+public:
+    static QnResourceTreeModelNodePtr createNode(Qn::NodeType nodeType, QnResourceTreeModel* model);
+};
