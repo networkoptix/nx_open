4704005b2826deb39c9da9317440c2fe3cfffaeb
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2016-11-29 18:46:21 +0300

    #VMS-4662 One more test
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/nx_network_ut/src/dns_resolver_ut.cpp
+++ unit_tests/nx_network_ut/src/dns_resolver_ut.cpp
@@ -43,7 +43,8 @@ class DnsResolver:
 {
 public:
     DnsResolver():
-        m_prevResolveRequestId(0)
+        m_prevResolveRequestId(0),
+        m_expectedResponseCount(0)
     {
         using namespace std::placeholders;
 
@@ -74,13 +75,33 @@ protected:
         ASSERT_EQ(SystemError::timedOut, m_hostNameToResolveResult[easyToResolveHost]);
     }
 
+    void pipelineRequests()
+    {
+        m_pipelinedRequests.push_back(easyToResolveHost + "1");
+        m_pipelinedRequests.push_back(easyToResolveHost + "2");
+        m_pipelinedRequests.push_back(easyToResolveHost + "3");
+        m_expectedResponseCount = m_pipelinedRequests.size();
+        startNextPipelinedTask();
+    }
+
+    void assertIfNotAllRequestsHaveBeenCompleted()
+    {
+        QnMutexLocker lock(&m_mutex);
+        waitForResolveResult(&lock, m_expectedResponseCount);
+        ASSERT_EQ(m_expectedResponseCount, m_hostNameToResolveResult.size());
+        ASSERT_EQ(m_expectedResponseCount, m_requestIdToResolveResult.size());
+    }
+
 private:
     network::DnsResolver m_dnsResolver;
-    std::size_t m_prevResolveRequestId;
+    std::atomic<std::size_t> m_prevResolveRequestId;
+    std::size_t m_expectedResponseCount;
     QnMutex m_mutex;
     std::map<QString, SystemError::ErrorCode> m_hostNameToResolveResult;
+    std::map<std::size_t, SystemError::ErrorCode> m_requestIdToResolveResult;
     QnWaitCondition m_cond;
     std::list<nx::utils::test::ScopedTimeShift> m_shiftedTime;
+    std::list<QString> m_pipelinedRequests;
 
     std::deque<HostAddress> testResolve(const QString& hostName, int /*ipVersion*/)
     {
@@ -93,33 +114,53 @@ private:
                 ClockType::steady,
                 m_dnsResolver.resolveTimeout() + std::chrono::seconds(1)));
         }
+
+        if (!m_pipelinedRequests.empty())
+            startNextPipelinedTask();
+
         return std::deque<HostAddress>( {HostAddress::localhost} );
+    }
+
+    void startNextPipelinedTask()
+    {
+        auto hostname = std::move(m_pipelinedRequests.front());
+        m_pipelinedRequests.pop_front();
+        startResolveAsync(hostname);
+    }
+
+    void onHostResolved(
+        const QString& hostName,
+        std::size_t requestId,
+        SystemError::ErrorCode errorCode,
+        std::deque<HostAddress> resolvedAddresses)
+    {
+        QnMutexLocker lock(&m_mutex);
+        m_hostNameToResolveResult.emplace(hostName, errorCode);
+        m_requestIdToResolveResult.emplace(requestId, errorCode);
+        m_cond.wakeAll();
     }
 
     void startResolveAsync(const QString& hostName)
     {
         using namespace std::placeholders;
 
+        const auto requestId = ++m_prevResolveRequestId;
         m_dnsResolver.resolveAsync(
             hostName,
-            std::bind(&DnsResolver::onHostResolved, this, hostName, _1, _2),
+            std::bind(&DnsResolver::onHostResolved, this, hostName, requestId, _1, _2),
             AF_INET,
-            reinterpret_cast<network::DnsResolver::RequestId>(++m_prevResolveRequestId));
-    }
-
-    void onHostResolved(
-        const QString& hostName,
-        SystemError::ErrorCode errorCode,
-        std::deque<HostAddress> resolvedAddresses)
-    {
-        QnMutexLocker lock(&m_mutex);
-        m_hostNameToResolveResult.emplace(hostName, errorCode);
-        m_cond.wakeAll();
+            reinterpret_cast<network::DnsResolver::RequestId>(requestId));
     }
 
     void waitForResolveResult(QnMutexLockerBase* const lock, const QString& hostName)
     {
         while (m_hostNameToResolveResult.find(hostName) == m_hostNameToResolveResult.end())
+            m_cond.wait(lock->mutex());
+    }
+
+    void waitForResolveResult(QnMutexLockerBase* const lock, std::size_t requestId)
+    {
+        while (m_requestIdToResolveResult.find(requestId) == m_requestIdToResolveResult.end())
             m_cond.wait(lock->mutex());
     }
 };
@@ -129,6 +170,12 @@ TEST_F(DnsResolver, resolve_fails_with_timeout)
     addTaskThatTakesMoreThanResolveTimeout();
     addRegularTask();
     assertIfRegularTaskDidNotTimedOut();
+}
+
+TEST_F(DnsResolver, pipelining_resolve_request)
+{
+    pipelineRequests();
+    assertIfNotAllRequestsHaveBeenCompleted();
 }
 
 } // namespace test
