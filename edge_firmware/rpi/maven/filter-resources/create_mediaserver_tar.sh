dd624c249107ab9f7eff2bdd9dfe04225e9918d9
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-11-29 11:32:21 -0800

    Fixing bpi build for customizations
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- edge_firmware/rpi/maven/filter-resources/create_mediaserver_tar.sh
+++ edge_firmware/rpi/maven/filter-resources/create_mediaserver_tar.sh
@@ -123,6 +123,7 @@ if [ -e "$LIBS_DIR/libvpx.so.1.2.0" ]; then
   LIBS_TO_COPY+=( libvpx.so )
 fi
 
+if [ -d 'opt/networkoptix' ]; then mv -f opt/networkoptix opt/$CUSTOMIZATION; fi
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR/$PREFIX_DIR
 echo "$VERSION" > $BUILD_DIR/$PREFIX_DIR/version.txt
@@ -166,7 +167,7 @@ fi
 
 #conf
 mkdir -p $BUILD_DIR/$PREFIX_DIR/mediaserver/etc/
-cp opt/networkoptix/mediaserver/etc/mediaserver.conf.template $BUILD_DIR/$PREFIX_DIR/mediaserver/etc
+cp opt/$CUSTOMIZATION/mediaserver/etc/mediaserver.conf.template $BUILD_DIR/$PREFIX_DIR/mediaserver/etc
 
 #start script and platform specific scripts
 cp -R ./etc $BUILD_DIR
@@ -216,8 +217,8 @@ if [[ "${box}" == "bpi" ]] && [[ ! -z "$WITH_CLIENT" ]]; then
   cp -f ${qt.dir}/resources/* $BUILD_DIR/$PREFIX_DIR/lite_client/bin/libexec
   cp -R ./root $BUILD_DIR
   mkdir -p $BUILD_DIR/root/tools/nx
-  cp opt/networkoptix/mediaserver/etc/mediaserver.conf.template $BUILD_DIR/root/tools/nx
-  chmod -R 755 $BUILD_DIR/$PREFIX_DIR/mediaserver/var/scripts
+  cp opt/$CUSTOMIZAION/mediaserver/etc/mediaserver.conf.template $BUILD_DIR/root/tools/nx
+  if [[ "${box}" == "bpi" ]]; then chmod -R 755 $BUILD_DIR/$PREFIX_DIR/mediaserver/var/scripts; fi
 fi
 
 #copying plugins
@@ -257,8 +258,6 @@ cp -Rf $VOX_SOURCE_DIR/* $VOX_TARGET_DIR
 if [ ! "$CUSTOMIZATION" == "networkoptix" ]; then
     mv -f $BUILD_DIR/etc/init.d/networkoptix-mediaserver $BUILD_DIR/etc/init.d/$CUSTOMIZATION-mediaserver
     mv -f $BUILD_DIR/etc/init.d/networkoptix-lite_client $BUILD_DIR/etc/init.d/$CUSTOMIZATION-lite_client
-    cp -Rf $BUILD_DIR/opt/networkoptix/* $BUILD_DIR/opt/$CUSTOMIZATION
-    rm -Rf $BUILD_DIR/opt/networkoptix/
 fi
 
 if [[ "${box}" == "bpi" || "${box}" == "bananapi" ]]; then
@@ -302,4 +301,5 @@ zip ./$UPDATE_NAME.zip ./*
 mv ./* ../
 cd ..
 rm -Rf zip
+rm -Rf $BUILD_DIR
 rm -Rf $TEMP_DIR
