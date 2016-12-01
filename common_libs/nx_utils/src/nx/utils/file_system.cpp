3318e1f48d7544d4a1c95cdcb08fa47e519d35ad
Denis Klychkov <dklychkov@networkoptix.com>
2016-12-01 18:53:31 +0300

    ## VMS: Implement a recursive copy function
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

---
+++ common_libs/nx_utils/src/nx/utils/file_system.cpp
@@ -0,0 +1,109 @@
+#include "file_system.h"
+
+#if defined(Q_OS_UNIX)
+#include <unistd.h>
+#endif
+
+#include <QtCore/QFileInfo>
+#include <QtCore/QFile>
+#include <QtCore/QDir>
+
+namespace nx {
+namespace utils {
+namespace file_system {
+
+Result copy(const QString& sourcePath, const QString& targetPath, Options options)
+{
+    const QFileInfo srcFileInfo(sourcePath);
+    if (!srcFileInfo.exists())
+        return Result(Result::sourceDoesNotExist, sourcePath);
+
+    QFileInfo tgtFileInfo(targetPath);
+
+    {
+        auto fullTargetPath = targetPath;
+        if (tgtFileInfo.exists() && tgtFileInfo.isDir())
+            fullTargetPath = QDir(targetPath).absoluteFilePath(srcFileInfo.fileName());
+
+        tgtFileInfo = QFileInfo(fullTargetPath);
+    }
+
+    {
+        auto targetDir = tgtFileInfo.dir();
+        if (!targetDir.exists())
+        {
+            if (!options.testFlag(CreateTargetPath) || !targetDir.mkpath("."))
+                return Result(Result::cannotCreateDirectory, targetDir.path());
+        }
+    }
+
+    if (srcFileInfo.isDir() && (!srcFileInfo.isSymLink() || options.testFlag(FollowSymLinks)))
+    {
+        if (!tgtFileInfo.exists())
+        {
+            auto targetDir = tgtFileInfo.dir();
+            if (!targetDir.mkdir(tgtFileInfo.fileName()))
+                return Result(Result::cannotCreateDirectory, targetPath);
+        }
+
+        const auto sourceDir = QDir(sourcePath);
+
+        const auto entries = sourceDir.entryList(
+            QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System);
+        foreach (const auto& entry, entries)
+        {
+            const auto newSrcFilePath = sourceDir.absoluteFilePath(entry);
+
+            const auto result = copy(newSrcFilePath, tgtFileInfo.absoluteFilePath(), options);
+            if (!result.isOk())
+                return result;
+        }
+    }
+    else
+    {
+        const auto targetFilePath = tgtFileInfo.absoluteFilePath();
+        if (tgtFileInfo.exists())
+        {
+            if (options.testFlag(SkipExisting))
+                return Result(Result::ok);
+
+            if (!options.testFlag(OverwriteExisting) || !QFile::remove(targetFilePath))
+                return Result(Result::alreadyExists, targetFilePath);
+        }
+
+        if (srcFileInfo.isSymLink() && !options.testFlag(FollowSymLinks))
+        {
+            if (!QFile::link(symLinkTarget(srcFileInfo.absoluteFilePath()), targetFilePath))
+                return Result(Result::cannotCopy, sourcePath);
+        }
+        else
+        {
+            if (!QFile::copy(sourcePath, targetFilePath))
+                return Result(Result::cannotCopy, sourcePath);
+        }
+    }
+
+    return Result(Result::ok);
+}
+
+QString symLinkTarget(const QString& linkPath)
+{
+    #if defined(Q_OS_UNIX)
+        if (!QFileInfo(linkPath).isSymLink())
+            return QString();
+
+        char target[PATH_MAX + 1];
+        const auto path = QFile::encodeName(linkPath);
+        int len = readlink(path, target, PATH_MAX);
+        if (len <= 0)
+            return QString();
+        target[len] = '\0';
+        return QFile::decodeName(QByteArray(target));
+    #else
+        return QString();
+    #endif
+}
+
+} // namespace file_system
+} // namespace utils
+} // namespace nx
---
+++ common_libs/nx_utils/src/nx/utils/file_system.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <QtCore/QString>
+#include <QtCore/QFlags>
+
+namespace nx {
+namespace utils {
+namespace file_system {
+
+struct Result
+{
+    enum ResultCode
+    {
+        ok,
+        sourceDoesNotExist,
+        alreadyExists,
+        cannotCopy,
+        cannotCreateDirectory
+    };
+
+    ResultCode code;
+    QString path;
+
+    Result(ResultCode code, const QString& path = QString()): code(code), path(path) {}
+
+    bool isOk() const { return code == ok; }
+};
+
+enum Option
+{
+    NoOption = 0,
+    OverwriteExisting = 0x01,
+    SkipExisting = 0x02,
+    CreateTargetPath = 0x04,
+    FollowSymLinks = 0x08
+};
+Q_DECLARE_FLAGS(Options, Option)
+
+QString symLinkTarget(const QString& linkPath);
+
+Result copy(const QString& sourcePath, const QString& targetPath,
+    Options options = NoOption);
+
+} // namespace file_system
+} // namespace utils
+} // namespace nx
