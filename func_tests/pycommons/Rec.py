9b3312785385496b94f92de763dc6764a41092ba
Artem Nikitin <anikitin@networkoptix.com>
2016-12-01 08:44:18 +0300

    TEST-177: review
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- func_tests/pycommons/Rec.py
+++ func_tests/pycommons/Rec.py
@@ -2,39 +2,36 @@
 # Artem V. Nikitin
 # Record - string-key dictionary replacer
 
-from Utils import *
+class Rec(object):
 
+    def __init__(self, init = {}, **kw):
+        if isinstance(init, Rec): init = init.get()
+        for name, val in init.items() + kw.items():
+            setattr(self, name, val)
 
-class Rec:
+    def get(self, name = None):
+        if name is None:
+            return self.__dict__
+        else:
+            return getattr(self, name)
 
-  def __init__( self, init = {}, **kw ):
-    if isinstance(init, Rec): init = init.get()
-    for name, val in init.items() + kw.items():
-      setattr(self, name, val)
+    def set(self, name, val):
+        setattr(self, name, val)
 
-  def get( self, name = None ):
-    if name is None:
-      return self.__dict__
-    else:
-      return getattr(self, name)
+    def asString(self):
+        s = ', '.join(
+          '%s=%s' % (str(key), str(val))
+          for key, val in self.get().iteritems()
+        )
+        return '<%s>' % s
 
-  def set( self, name, val ):
-    setattr(self, name, val)
+    def __repr__(self):
+        return self.asString()
 
-  def asString( self ):
-    s = ', '.join(
-      '%s=%s' % (str(key), str(val))
-      for key, val in self.get().iteritems()
-    )
-    return '<%s>' % s
+    def __str__(self):
+        return self.asString()
 
-  def __repr__( self ):
-    return self.asString()
-
-  def __str__( self ):
-    return self.asString()
-
-  def __cmp__( self, other ):
-    if isinstance(other, Rec):
-      return cmp(self.get(), other.get())
-    return cmp(self.get(), other)
+    def __cmp__(self, other):
+        if isinstance(other, Rec):
+            return cmp(self.get(), other.get())
+        return cmp(self.get(), other)
