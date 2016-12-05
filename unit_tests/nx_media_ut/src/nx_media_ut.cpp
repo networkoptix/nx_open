5b1aae03fd169a9a2c735b2baa0bf9ecc55afc22
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-12-05 18:26:26 +0300

    ## Clang compiling fixed.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/nx_media_ut/src/nx_media_ut.cpp
+++ unit_tests/nx_media_ut/src/nx_media_ut.cpp
@@ -23,7 +23,7 @@ static const struct
     const bool enableHangOnFinish = false;
     const bool forceLog = false;
     const bool logMockCameraStreams = true;
-} conf;
+} conf{};
 #include <nx/utils/test_support/test_utils.h>
 
 namespace nx {
