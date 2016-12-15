6afb838267811d0a27b8b1814ec6013eb17bb06e
Vitaly Kutin <vkutin@networkoptix.com>
2016-12-15 20:41:50 +0300

    ## Linux compile fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/graphics/items/overlays/io_module_form_overlay_contents_p.cpp
+++ client/libclient/src/ui/graphics/items/overlays/io_module_form_overlay_contents_p.cpp
@@ -207,7 +207,7 @@ void QnIoModuleFormOverlayContentsPrivate::Layout::setGeometry(const QRectF& rec
         return;
 
     /* Calculate column widths: */
-    std::array<qreal, kColumnCount> columnWidths{ 0.0, 0.0 };
+    std::array<qreal, kColumnCount> columnWidths { 0.0 };
 
     if (m_rowCounts[kRightColumn] == 0)
     {
@@ -263,8 +263,15 @@ void QnIoModuleFormOverlayContentsPrivate::Layout::setGeometry(const QRectF& rec
     }
 
     qreal rowStride = kItemHeight + kVerticalSpacing;
-    const std::array<qreal, kColumnCount> columnPositions { 0.0,
-        columnWidths[kLeftColumn] + kHorizontalSpacing };
+
+    std::array<qreal, kColumnCount> columnPositions;
+
+    qreal columnPos = 0.0;
+    for (int i = 0; i < kColumnCount; ++i)
+    {
+        columnPositions[i] = columnPos;
+        columnPos += columnWidths[i] + kHorizontalSpacing;
+    }
 
     /* Calculate item geometries: */
     for (int i = 0; i < totalItems; ++i)
