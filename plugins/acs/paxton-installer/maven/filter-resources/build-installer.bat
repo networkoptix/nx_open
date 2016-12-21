20fd3e6362b72d82364a60d5185e0db82e85ce85
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2016-12-21 13:40:42 -0800

    arm: arch fixed, paxton installer name changed
    <==== Mercurial data ====>
    Branch: unify_installer_names

--- plugins/acs/paxton-installer/maven/filter-resources/build-installer.bat
+++ plugins/acs/paxton-installer/maven/filter-resources/build-installer.bat
@@ -1,3 +1,3 @@
 candle -dAxSourceDir=${libdir}\bin\Release Product.wxs AxClient.wxs
-light -ext WixUIExtension.dll -cultures:en-us Product.wixobj AxClient.wixobj -out ${final.artifact.name}-paxton-plugin.msi
-signtool.exe sign /a /v /p ${sign.password} /d "${display.product.name} Paxton Plugin" /f ${sign.cer} ${final.artifact.name}-paxton-plugin.msi
+light -ext WixUIExtension.dll -cultures:en-us Product.wixobj AxClient.wixobj -out ${final.artifact.name}-paxton.msi
+signtool.exe sign /a /v /p ${sign.password} /d "${display.product.name} Paxton Plugin" /f ${sign.cer} ${final.artifact.name}-paxton.msi
