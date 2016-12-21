4b0366cfe6b9d64cb15029f3f32b5a7f9fdf0884
Alexander Kulikov <akulikov@networkoptix.com>
2016-12-21 15:14:01 +0300

    vms-4873. Set admin enabled while restoring DB.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- appserver2/src/database/db_manager.cpp
+++ appserver2/src/database/db_manager.cpp
@@ -560,6 +560,7 @@ bool QnDbManager::init(const QUrl& dbUrl)
             userResource->setDigest(beforeRestoreDbData.digest);
             userResource->setCryptSha512Hash(beforeRestoreDbData.cryptSha512Hash);
             userResource->setRealm(beforeRestoreDbData.realm);
+            userResource->setEmail(true);
             updateUserResource = true;
         }
         if (updateUserResource)
