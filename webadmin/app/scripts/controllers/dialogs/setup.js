9db35ec9fff67f4d1132e28b76c8a3c3722a212e
Evgeny Balashov <ebalashov@networkoptix.com>
2016-12-27 11:41:34 -0800

    WEB-443: Add "Save password" checkbox to local and cloud login in setup wizard
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/app/scripts/controllers/dialogs/setup.js
+++ webadmin/app/scripts/controllers/dialogs/setup.js
@@ -19,6 +19,7 @@ angular.module('webadminApp')
         // Common model
         $scope.settings = {
             chooseCloudSystem: false,
+            savePassword: true,
             systemName: '',
 
             cloudEmail: '',
@@ -69,7 +70,7 @@ angular.module('webadminApp')
 
         function sendCredentialsToNativeClient(){
             $log.log("Send credentials to client app: " + $scope.activeLogin);
-            return nativeClient.updateCredentials($scope.activeLogin, $scope.activePassword, $scope.cloudCreds);
+            return nativeClient.updateCredentials($scope.activeLogin, $scope.activePassword, $scope.cloudCreds, $scope.savePassword);
         }
 
         function checkInternetOnServer(reload){
