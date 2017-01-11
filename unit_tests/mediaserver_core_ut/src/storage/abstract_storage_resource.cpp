ac24848dc26e751edaa779f5c24c0b8c6014343d
Alexander Kulikov <akulikov@networkoptix.com>
2017-01-11 20:21:57 +0300

    vms-5095. Crash fix in storage load balancing algorithm test.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/mediaserver_core_ut/src/storage/abstract_storage_resource.cpp
+++ unit_tests/mediaserver_core_ut/src/storage/abstract_storage_resource.cpp
@@ -505,6 +505,7 @@ struct OccupiedSpaceAccess
 
 TEST(Storage_load_balancing_algorithm_test, Main)
 {
+    MSSettings::initializeROSettings();
     std::unique_ptr<QnCommonModule> commonModule;
     if (!qnCommon) {
         commonModule = std::unique_ptr<QnCommonModule>(new QnCommonModule);
