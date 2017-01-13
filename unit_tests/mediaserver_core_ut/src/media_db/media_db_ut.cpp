2ad256a62b30290099161013225d5fa1ecc52fb6
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-01-13 17:04:36 +0300

    #UT fixes
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/mediaserver_core_ut/src/media_db/media_db_ut.cpp
+++ unit_tests/mediaserver_core_ut/src/media_db/media_db_ut.cpp
@@ -29,6 +29,7 @@
 #endif
 
 #include "../utils.h"
+#include <media_server/settings.h>
 
 template<qint64 From, qint64 To>
 qint64 genRandomNumber()
@@ -464,6 +465,7 @@ void reopenDbFile(QFile *dbFile, const QString& fileName)
 
 TEST(MediaDbTest, SimpleFileWriteTest)
 {
+    MSSettings::initializeROSettings();
     nx::ut::utils::WorkDirResource workDirResource;
     ASSERT_TRUE((bool)workDirResource.getDirName());
 
@@ -566,6 +568,7 @@ TEST(MediaDbTest, BitsTwiddling)
 
 TEST(MediaDbTest, ReadWrite_Simple)
 {
+    MSSettings::initializeROSettings();
     nx::ut::utils::WorkDirResource workDirResource;
     ASSERT_TRUE((bool)workDirResource.getDirName());
 
@@ -620,6 +623,7 @@ TEST(MediaDbTest, ReadWrite_Simple)
 
 TEST(MediaDbTest, DbFileTruncate)
 {
+    MSSettings::initializeROSettings();
     nx::ut::utils::WorkDirResource workDirResource;
     ASSERT_TRUE((bool)workDirResource.getDirName());
 
@@ -690,6 +694,7 @@ TEST(MediaDbTest, DbFileTruncate)
 
 TEST(MediaDbTest, ReadWrite_MT)
 {
+    MSSettings::initializeROSettings();
     nx::ut::utils::WorkDirResource workDirResource;
     ASSERT_TRUE((bool)workDirResource.getDirName());
 
@@ -807,6 +812,7 @@ TEST(MediaDbTest, ReadWrite_MT)
 
 TEST(MediaDbTest, StorageDB)
 {
+    MSSettings::initializeROSettings();
     nx::ut::utils::WorkDirResource workDirResource;
     ASSERT_TRUE((bool)workDirResource.getDirName());
 
@@ -954,6 +960,8 @@ TEST(MediaDbTest, StorageDB)
 
 TEST(MediaDbTest, Migration_from_sqlite)
 {
+    MSSettings::initializeROSettings();
+
     QnWriterPool writerPool;
     std::unique_ptr<QnCommonModule> commonModule;
     if (!qnCommon) {
