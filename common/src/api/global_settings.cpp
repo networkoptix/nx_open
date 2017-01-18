d5fb96873b3dc9ca279910be5177f0e0f70adda8
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-01-18 15:18:14 +0300

    VMS-5141: Email Server settings are not saved to Server
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common/src/api/global_settings.cpp
+++ common/src/api/global_settings.cpp
@@ -477,7 +477,6 @@ void QnGlobalSettings::setEmailSettings(const QnEmailSettings &settings)
     m_supportLinkAdaptor->setValue(settings.supportEmail);
     m_simpleAdaptor->setValue(settings.simple);
     m_timeoutAdaptor->setValue(settings.timeout);
-    qnGlobalSettings->synchronizeNow();
 }
 
 void QnGlobalSettings::synchronizeNow()
