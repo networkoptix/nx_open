5437cca6535b8fc9c1e1e863ca55ef7e8430ead2
Denis Klychkov <dklychkov@networkoptix.com>
2017-01-24 18:17:52 +0300

    ## Compilation fix
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient_core/src/ui/models/model_data_accessor.h
+++ client/libclient_core/src/ui/models/model_data_accessor.h
@@ -1,14 +1,15 @@
 #pragma once
 
-#include <QtCore/QPointer>
 #include <QtCore/QAbstractItemModel>
+
+#include <utils/common/connective.h>
 
 namespace nx {
 namespace client {
 
-class ModelDataAccessor: public QObject
+class ModelDataAccessor: public Connective<QObject>
 {
-    using base_type = QObject;
+    using base_type = Connective<QObject>;
 
     Q_OBJECT
     Q_PROPERTY(QVariant model READ model WRITE setModel NOTIFY modelChanged)
