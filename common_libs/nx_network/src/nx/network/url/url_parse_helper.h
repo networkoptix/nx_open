47da1f0e8ebda73c89e3b1079896b0bb09aa85c1
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-01-31 13:08:24 +0300

    #CLOUD-829 nx::network::url::getEndpoint takes into account default port for protocol
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- common_libs/nx_network/src/nx/network/url/url_parse_helper.h
+++ common_libs/nx_network/src/nx/network/url/url_parse_helper.h
@@ -9,6 +9,11 @@ namespace nx {
 namespace network {
 namespace url {
 
+/**
+ * @param scheme Comparison is done in low-case.
+ * @return 0 for unknown scheme
+ */
+NX_NETWORK_API quint16 getDefaultPortForScheme(const QString& scheme);
 NX_NETWORK_API SocketAddress getEndpoint(const QUrl&);
 NX_NETWORK_API QString normalizePath(const QString&);
 
