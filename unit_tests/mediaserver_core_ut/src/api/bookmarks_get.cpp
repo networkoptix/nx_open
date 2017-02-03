dbab328da35c81497dd7841eed904367ae0d1fc2
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-02-03 18:47:01 +0300

    ## Review fixes.
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- unit_tests/mediaserver_core_ut/src/api/bookmarks_get.cpp
+++ unit_tests/mediaserver_core_ut/src/api/bookmarks_get.cpp
@@ -23,12 +23,12 @@ TEST(BookmarksGet, noBookmarks)
     cameraData.id = ec2::ApiCameraData::physicalIdToId(cameraData.physicalId);
 
     NX_LOG("[TEST] Create a camera.", cl_logINFO);
-    ASSERT_NO_FATAL_FAILURE(testApiPost(launcher, "/ec2/saveCamera", cameraData));
+    NX_TEST_API_POST(launcher, "/ec2/saveCamera", cameraData);
 
     NX_LOG("[TEST] Check that there are no bookmarks for the camera.", cl_logINFO);
     QnCameraBookmarkList bookmarks;
-    ASSERT_NO_FATAL_FAILURE(testApiGet(launcher,
-        lit("/ec2/bookmarks?cameraId=%1").arg(cameraData.id.toString()), &bookmarks));
+    NX_TEST_API_GET(launcher,
+        lit("/ec2/bookmarks?cameraId=%1").arg(cameraData.id.toString()), &bookmarks);
     ASSERT_TRUE(bookmarks.isEmpty());
 }
 
