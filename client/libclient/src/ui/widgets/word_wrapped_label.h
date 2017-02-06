798d6f464a71022f786d953aefcfc3b142639ba9
Sergey Ivanov <sivanov@networkoptix.com>
2017-02-06 13:34:49 +0300

    VMS-5346 - License dialog looks incorrect sometimes
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/widgets/word_wrapped_label.h
+++ client/libclient/src/ui/widgets/word_wrapped_label.h
@@ -7,6 +7,8 @@ class QnWordWrappedLabel: public QWidget
     Q_OBJECT
     Q_PROPERTY(QString text READ text WRITE setText)
     Q_PROPERTY(Qt::TextFormat textFormat READ textFormat WRITE setTextFormat)
+
+    using base_type = QWidget;
 public:
     QnWordWrappedLabel(QWidget* parent = 0);
 
@@ -21,11 +23,21 @@ public:
     Qt::TextFormat textFormat() const;
     void setTextFormat(Qt::TextFormat value);
 
+    int approximateLines() const;
+    void setApproximateLines(int value);
+
 signals:
     void linkActivated(const QString& value);
 
 protected:
+    virtual void showEvent(QShowEvent* event) override;
     virtual void resizeEvent(QResizeEvent* event) override;
+
+private:
+    void ensureInitialized();
+
 private:
     QLabel* m_label = nullptr;
+    bool m_initialized = false;
+    int m_approximateLines = 2;
 };
