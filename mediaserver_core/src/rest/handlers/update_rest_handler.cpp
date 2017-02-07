dbd42f10be14fa869bbb78e10d46ca2ada7ec02a
Denis Klychkov <dklychkov@networkoptix.com>
2017-02-07 14:10:10 +0300

    VMS-5335: Disable user permissions check for normal update installation
    This covers only case when updateId is given and the request body is empty.
    
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- mediaserver_core/src/rest/handlers/update_rest_handler.cpp
+++ mediaserver_core/src/rest/handlers/update_rest_handler.cpp
@@ -29,6 +29,32 @@ int QnUpdateRestHandler::executePost(
 {
     Q_UNUSED(path)
 
+    qint64 offset = params.value(lit("offset")).toLongLong();
+    QString updateId = params.value(lit("updateId"));
+    bool delayed = params.value(lit("delayed"), lit("false")) != lit("false");
+
+    if (!updateId.isEmpty() && body.isEmpty())
+    {
+        /* TODO: #3.1 Check user rights for this case too
+           when offline cloud authentication is implemented.
+           Also see "/api/installUpdates" registration in media_server_process.cpp
+        */
+
+        QnUploadUpdateReply reply;
+        bool res = true;
+
+        if (delayed)
+            QnServerUpdateTool::instance()->installUpdateDelayed(updateId);
+        else
+            res = QnServerUpdateTool::instance()->installUpdate(updateId);
+
+        reply.offset = res
+            ? ec2::AbstractUpdatesManager::NoError
+            : ec2::AbstractUpdatesManager::UnknownError;
+        result.setReply(reply);
+        return nx_http::StatusCode::ok;
+    }
+
     bool remotePeerHasAdminRights = qnResourceAccessManager->hasGlobalPermission(
         processor->accessRights(),
         Qn::GlobalAdminPermission);
@@ -42,57 +68,42 @@ int QnUpdateRestHandler::executePost(
         return nx_http::StatusCode::forbidden;
     }
 
-
-    qint64 offset = params.value(lit("offset")).toLongLong();
-    QString updateId = params.value(lit("updateId"));
-    bool delayed = params.value(lit("delayed"), lit("false")) != lit("false");
-
-    if (!updateId.isEmpty()) {
-        if (params.contains(lit("offset"))) {
-            return handlePartialUpdate(updateId, body, offset, result);
-        } else if (body.isEmpty()) {
-            QnUploadUpdateReply reply;
-            bool res = true;
-
-            if (delayed)
-                QnServerUpdateTool::instance()->installUpdateDelayed(updateId);
-            else
-                res = QnServerUpdateTool::instance()->installUpdate(updateId);
-
-            reply.offset = res ? ec2::AbstractUpdatesManager::NoError : ec2::AbstractUpdatesManager::UnknownError;
-            result.setReply(reply);
-            return nx_http::StatusCode::ok;
-        }
-    }
+    if (!updateId.isEmpty() && params.contains(lit("offset")))
+        return handlePartialUpdate(updateId, body, offset, result);
 
     QnSoftwareVersion version;
     QnSystemInformation sysInfo;
 
     {
         QBuffer buffer(const_cast<QByteArray*>(&body)); // we're going to read data, so const_cast is safe
-        if (!verifyUpdatePackage(&buffer, &version, &sysInfo)) {
+        if (!verifyUpdatePackage(&buffer, &version, &sysInfo))
+        {
             cl_log.log("An upload has been received but not veryfied", cl_logWARNING);
             result.setError(QnJsonRestResult::CantProcessRequest, lit("INVALID_FILE"));
             return nx_http::StatusCode::ok;
         }
     }
 
-    if (version == qnCommon->engineVersion()) {
+    if (version == qnCommon->engineVersion())
+    {
         result.setError(QnJsonRestResult::NoError, lit("UP_TO_DATE"));
         return nx_http::StatusCode::ok;
     }
 
-    if (sysInfo != QnSystemInformation::currentSystemInformation()) {
+    if (sysInfo != QnSystemInformation::currentSystemInformation())
+    {
         result.setError(QnJsonRestResult::CantProcessRequest, lit("INCOMPATIBLE_SYSTEM"));
         return nx_http::StatusCode::ok;
     }
 
-    if (!QnServerUpdateTool::instance()->addUpdateFile(version.toString(), body)) {
+    if (!QnServerUpdateTool::instance()->addUpdateFile(version.toString(), body))
+    {
         result.setError(QnJsonRestResult::CantProcessRequest, lit("EXTRACTION_ERROR"));
         return nx_http::StatusCode::internalServerError;
     }
 
-    if (!QnServerUpdateTool::instance()->installUpdate(version.toString())) {
+    if (!QnServerUpdateTool::instance()->installUpdate(version.toString()))
+    {
         result.setError(QnJsonRestResult::CantProcessRequest, lit("INSTALLATION_ERROR"));
         return nx_http::StatusCode::ok;
     }
