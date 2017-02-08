bb7c68511588cd64f4ad60906112844c5a521b52
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2017-02-08 14:45:24 +0300

    CLOUD-862: Check that language is changed
    <==== Mercurial data ====>
    Branch: cloud_portal_dev_3.0.0_test

--- cloud_portal/front_end/test/e2e/customize/customize_spec.js
+++ cloud_portal/front_end/test/e2e/customize/customize_spec.js
@@ -8,11 +8,13 @@ describe('Customized confirmation email', function () {
         p.helper.createUserCustom(p.brandText, p.brandText+'Name', p.brandText+'LastName', userEmail);
     });
 
-    fit("should have header with text that matches specific portal if language is changed", function () {
+    it("should have header with text that matches specific portal if language is changed", function () {
         var userEmail = p.helper.getRandomEmail();
         p.helper.get();
         p.languageDropdown.click();
         p.russianOption.click();
+        browser.sleep(2000); // browser.wait and other fancy promises don't help http://docsplendid.com/tags/wait-for-element
+        expect(p.languageDropdown.getText()).toContain("Russia");
 
         p.helper.createUserCustom(p.brandText, p.brandText+'Name', p.brandText+'LastName', userEmail);
     });
