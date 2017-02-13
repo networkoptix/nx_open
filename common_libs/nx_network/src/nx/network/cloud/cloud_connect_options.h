08ff7e6eeab269d580e6895d5c4c8a76ad8588c7
Mikhail Uskov <muskov@networkoptix.com>
2017-02-13 19:50:50 +0300

    # CLOUD-824 review fix and test
    <==== Mercurial data ====>
    Branch: dev_3.0.0_cloud

---
+++ common_libs/nx_network/src/nx/network/cloud/cloud_connect_options.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/fusion/fusion/fusion_fwd.h>
+
+namespace nx {
+namespace hpm {
+namespace api {
+
+/**
+ * Enables or disables cloud connect features, enforsed by mediator.
+ */
+enum CloudConnectOption
+{
+    emptyCloudConnectOptions = 0,
+
+    /** #CLOUD-824 */
+    serverChecksConnectionState = 1,
+};
+
+Q_DECLARE_FLAGS(CloudConnectOptions, CloudConnectOption)
+Q_DECLARE_OPERATORS_FOR_FLAGS(CloudConnectOptions)
+
+} // namespace api
+} // namespace hpm
+} // namespace nx
+
+QN_FUSION_DECLARE_FUNCTIONS_FOR_TYPES((nx::hpm::api::CloudConnectOptions), (lexical))
