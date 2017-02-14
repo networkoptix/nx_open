6460fc7c9286ef31608ce1322306bacf9d832a89
Denis Klychkov <dklychkov@networkoptix.com>
2017-02-14 13:50:06 +0300

    ## CMake: Use local copy of rdep
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- build_utils/cmake/rdep.cmake
+++ build_utils/cmake/rdep.cmake
@@ -1,5 +1,5 @@
-set(RDEP_DIR $ENV{HOME}/develop/devtools/rdep CACHE PATH "Path to rdep scripts")
-set(PACKAGES_DIR $ENV{environment}/packages CACHE STRING "Path to local rdep repository")
+set(RDEP_DIR "${PROJECT_SOURCE_DIR}/build_utils/python" CACHE PATH "Path to rdep scripts")
+set(PACKAGES_DIR "$ENV{environment}/packages" CACHE STRING "Path to local rdep repository")
 
 function(rdep_configure)
     execute_process(COMMAND ${PYTHON_EXECUTABLE} ${RDEP_DIR}/rdep_configure.py)
