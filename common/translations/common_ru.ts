066fd40fba3c971e91a9144244a5af7f01f76c93
Sergey Ivanov <sivanov@networkoptix.com>
2017-02-15 12:35:40 +0300

    New russian translations
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- common/translations/common_ru.ts
+++ common/translations/common_ru.ts
@@ -723,7 +723,7 @@
     </message>
     <message>
         <source>Make sure port %1 is accessible (e.g. forwarded).</source>
-        <translation type="unfinished">Убедитесь, что порт %2 доступен (например, проброшен). {1 ?}</translation>
+        <translation type="unfinished">Убедитесь, что порт %1 доступен (например, проброшен).</translation>
     </message>
 </context>
 <context>
