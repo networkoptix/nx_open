82403b20fdfb4c61ea2847a0e825f47ffb179666
Sergey Ivanov <sivanov@networkoptix.com>
2017-02-22 11:54:18 +0300

    VMS-5379 - "Clear Screen" option for VideoWall doesn't remove icon at User Settings until relogin
    Fixed database migration. Existing layouts will be cloned correctly, unused layouts will be removed
    
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- appserver2/src/database/migrations/reparent_videowall_layouts.h
+++ appserver2/src/database/migrations/reparent_videowall_layouts.h
@@ -9,7 +9,7 @@ namespace migrations {
 /**
  * In 3.0 normal layouts cannot be placed on the videowall. Special layouts with parent id set to
  * videowall id are used. Therefore we must clone users' layouts and re-parent existing layouts
- * (if auto-generated) to videowall.
+ * (if auto-generated) to videowall. Unused auto-generated layouts must be deleted.
  */
 bool reparentVideoWallLayouts(const QSqlDatabase& database);
 
