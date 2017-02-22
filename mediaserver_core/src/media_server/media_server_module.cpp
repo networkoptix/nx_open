8622123ee9cd3b7c8f856b9cb61e9389ea864a0c
Sergey Ivanov <sivanov@networkoptix.com>
2017-02-22 15:22:52 +0300

    VMS-5219 - Change text in "motion on camera" email
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- mediaserver_core/src/media_server/media_server_module.cpp
+++ mediaserver_core/src/media_server/media_server_module.cpp
@@ -1,5 +1,7 @@
 #include "media_server_module.h"
 
+#include <QtCore/QCoreApplication>
+
 #include <common/common_globals.h>
 #include <common/common_module.h>
 
@@ -14,11 +16,28 @@
 
 #include <nx/network/socket_global.h>
 
+#include <translation/translation_manager.h>
+
 #include <utils/media/ffmpeg_initializer.h>
 #include <utils/common/buffered_file.h>
 #include <utils/common/writer_pool.h>
 #include "master_server_status_watcher.h"
 #include "settings.h"
+
+#include <utils/common/delayed.h>
+
+namespace {
+
+void installTranslations()
+{
+    static const QString kDefaultPath(":/translations/common_en_US.qm");
+
+    QnTranslationManager translationManager;
+    QnTranslation defaultTranslation = translationManager.loadTranslation(kDefaultPath);
+    QnTranslationManager::installTranslation(defaultTranslation);
+}
+
+}
 
 QnMediaServerModule::QnMediaServerModule(const QString& enforcedMediatorEndpoint, QObject *parent):
     QObject(parent)
@@ -51,6 +70,9 @@ QnMediaServerModule::QnMediaServerModule(const QString& enforcedMediatorEndpoint
     nx::network::SocketGlobals::mediatorConnector().enable(true);
 
     m_common->store(new QnNewSystemServerFlagWatcher());
+
+    // Translations must be installed from the main applicaition thread.
+    executeDelayed(&installTranslations, kDefaultDelay, qApp->thread());
 }
 
 QnMediaServerModule::~QnMediaServerModule() {
