93f37dbcc0ecfc98d3d58a250482aac087eb32e0
Vitaly Kutin <vkutin@networkoptix.com>
2017-03-01 13:36:54 +0300

    VMS-5574: Playback "Speed slider" isn't disabled for camera without archive
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/src/ui/graphics/items/controls/navigation_item.cpp
+++ client/libclient/src/ui/graphics/items/controls/navigation_item.cpp
@@ -153,6 +153,7 @@ QnNavigationItem::QnNavigationItem(QGraphicsItem *parent):
             timelinePlaceholder->setVisible(!isRelevant);
             m_separators->setFrameColor(palette().color(isRelevant ? QPalette::Shadow : QPalette::Midlight));
             m_separators->setFrameWidth(isRelevant ? 2.0 : 1.0);
+            updatePlaybackButtonsEnabled();
             if (reset)
                 m_timeSlider->invalidateWindow();
         });
@@ -485,7 +486,7 @@ void QnNavigationItem::updatePlaybackButtonsEnabled()
      * client will be in strange state: speed slider allows only 0x and 1x, but current speed is -16x.
      * So we making the slider enabled for I/O module to do not make the situation even stranger.
      */
-    m_speedSlider->setEnabled(playable);
+    m_speedSlider->setEnabled(playable && m_timeSlider->isVisible());
 }
 
 void QnNavigationItem::updateVolumeButtonsEnabled()
