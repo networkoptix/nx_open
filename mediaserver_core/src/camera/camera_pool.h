4400d01a76dc643917d71288c0693db6b4cb26e7
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-03-01 18:31:54 +0300

    #VMS-5536 Fixed server crash when exporting through use of hls
    <==== Mercurial data ====>
    Branch: x_VMS-5536

--- mediaserver_core/src/camera/camera_pool.h
+++ mediaserver_core/src/camera/camera_pool.h
@@ -1,13 +1,30 @@
 #ifndef __CAMERA_POOL_H__
 #define __CAMERA_POOL_H__
 
+#include <memory>
+
 #include <QtCore/QMap>
+
 #include <utils/thread/mutex.h>
 
 #include <core/resource/resource_fwd.h>
+
 #include "camera/video_camera.h"
 
 #define qnCameraPool QnVideoCameraPool::instance()
+
+class VideoCameraLocker
+{
+public:
+    VideoCameraLocker(QnVideoCameraPtr);
+    virtual ~VideoCameraLocker();
+
+private:
+    QnVideoCameraPtr m_camera;
+
+    VideoCameraLocker(const VideoCameraLocker&);
+    VideoCameraLocker& operator=(const VideoCameraLocker&);
+};
 
 class QnVideoCameraPool
 {
@@ -25,6 +42,9 @@ public:
     QnVideoCameraPtr getVideoCamera(const QnResourcePtr& res);
     void removeVideoCamera(const QnResourcePtr& res);
     void updateActivity();
+
+    std::unique_ptr<VideoCameraLocker> getVideoCameraLockerByResourceId(const QnUuid& id);
+
 private:
     typedef QMap<QnResourcePtr, QnVideoCameraPtr> CameraMap;
     CameraMap m_cameras;
