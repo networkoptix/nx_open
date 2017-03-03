f1f4fbf63699b344dfd3b6852e21d84e912f256d
Nick Hartleb <nhartleb@networkoptix.com>
2017-03-02 18:02:42 -0800

    Cloud-943 Email stability
    Added max_retires to all customizations' cloud_portal.yaml
    Removed @handle_exceptions from email_engine.py
    Added try except to send_email task. Purpose is to requeue task until successful or hits max_retries
    
    <==== Mercurial data ====>
    Branch: x_cloud-943_email-stability

--- cloud_portal/cloud/notifications/tasks.py
+++ cloud_portal/cloud/notifications/tasks.py
@@ -1,20 +1,41 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+from celery.exceptions import Ignore
 
 from .engines import email_engine
 
+@shared_task
+def send_email(user_email, type, message, customization, attempt=1):
+    from smtplib import SMTPException
+    from util.config import get_config
+
+    error = ''
+    try:
+        email_engine.send(user_email, type, message, customization)
+    except SMTPException as error:
+        print(error)
+        print("Attempt %s failed" % str(attempt))
+        if attempt < get_config()['max_retries']:
+            send_email.delay(user_email, type, message, customization, attempt+1)
+            send_email.update_state(state="FAILURE", meta={'error': str(error),
+                                                           'user_email': user_email,
+                                                           'type': type,
+                                                           'message': message,
+                                                           'customization': customization,
+                                                           'attempt': attempt})
+        raise Ignore()
+    else:
+        return {'user_email': user_email, 'type': type, 'message': message,
+                'customization': customization, 'attempt': attempt, 'error': str(error)}
+
 
 @shared_task
-def send_email(user_email, type, message, customization):
-    return email_engine.send(user_email, type, message, customization)
-
-@shared_task
-def test_task(x,y):
+def test_task(x, y):
     from time import sleep
     print("x: %i\ty:%i" % (x, y))
     sleep(y * 60)
     print("total: %i" % (x * y))
     with open('task.log', 'a+') as f:
-        f.write("Task Done: %i * %i = %i" %(x,x,x*y))
+        f.write("Task Done: %i * %i = %i" % (x, x, x*y))
     return x * y
