3bd04fe55d78be4ea2175861c20d9677024e7d3a
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-03-09 10:56:05 -0800

    Translations: ja, de, ko, fr
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient/translations/client_qml_ja.ts
+++ client/libclient/translations/client_qml_ja.ts
@@ -1,102 +1,102 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>EmptyTilesPlaceholder</name>
     <message>
-        <source>Searching servers in your local network...</source>
-        <translation type="unfinished"></translation>
+      <source>Searching servers in your local network...</source>
+      <translation>ローカル ネットワーク上のサーバーを検索しています...</translation>
     </message>
     <message>
-        <source>No servers found</source>
-        <translation type="unfinished"></translation>
+      <source>No servers found</source>
+      <translation>サーバーは見つかりませんでした</translation>
     </message>
     <message>
-        <source>Check your network connection or press &quot;Connect to Server&quot; button to enter known server address</source>
-        <translation type="unfinished"></translation>
+      <source>Check your network connection or press &quot;Connect to Server&quot; button to enter known server address</source>
+      <translation>ネットワーク接続を確認するか既知のサーバーアドレスを入力して &quot;サーバーに接続 &quot; ボタンを押す</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>FactorySystemTileArea</name>
     <message>
-        <source>Click to setup</source>
-        <translation type="unfinished"></translation>
+      <source>Click to setup</source>
+      <translation>クリックしてセットアップ</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>LocalSystemTileExpandedAreaPart</name>
     <message>
-        <source>Login</source>
-        <translation type="unfinished">ログイン</translation>
+      <source>Login</source>
+      <translation>ログイン</translation>
     </message>
     <message>
-        <source>Password</source>
-        <translation type="unfinished">パスワード</translation>
+      <source>Password</source>
+      <translation>パスワード</translation>
     </message>
     <message>
-        <source>Save password</source>
-        <translation type="unfinished">パスワードを保存する</translation>
+      <source>Save password</source>
+      <translation>パスワードを保存する</translation>
     </message>
     <message>
-        <source>Auto-login</source>
-        <translation type="unfinished"></translation>
+      <source>Auto-login</source>
+      <translation>自動ログイン</translation>
     </message>
     <message>
-        <source>Connect</source>
-        <translation type="unfinished"></translation>
+      <source>Connect</source>
+      <translation>接続</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>NxSearchEdit</name>
     <message>
-        <source>Search</source>
-        <translation type="unfinished">検索</translation>
+      <source>Search</source>
+      <translation>検索</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SystemTile</name>
     <message>
-        <source>SAFE MODE</source>
-        <translation type="unfinished"></translation>
+      <source>SAFE MODE</source>
+      <translation>セーフ モード</translation>
     </message>
     <message>
-        <source>INCOMPATIBLE</source>
-        <translation type="unfinished"></translation>
+      <source>INCOMPATIBLE</source>
+      <translation>互換性なし</translation>
     </message>
     <message>
-        <source>OFFLINE</source>
-        <translation type="unfinished"></translation>
+      <source>OFFLINE</source>
+      <translation>オフライン</translation>
     </message>
     <message>
-        <source>UNREACHABLE</source>
-        <translation type="unfinished"></translation>
+      <source>UNREACHABLE</source>
+      <translation>到達不可</translation>
     </message>
     <message>
-        <source>New Server</source>
-        <translation type="unfinished"></translation>
+      <source>New Server</source>
+      <translation>新しいサーバー</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>WelcomeScreen</name>
     <message>
-        <source>Nothing found</source>
-        <translation type="unfinished"></translation>
+      <source>Nothing found</source>
+      <translation>何も見つかりませんでした</translation>
     </message>
     <message>
-        <source>You have no access to %1. Some features could be unavailable.</source>
-        <translation type="unfinished"></translation>
+      <source>You have no access to %1. Some features could be unavailable.</source>
+      <translation>%1 へのアクセスがありません　いくつかの機能が使用できない可能性があります</translation>
     </message>
     <message>
-        <source>Loading...</source>
-        <translation type="unfinished">ロード中...</translation>
+      <source>Loading...</source>
+      <translation>ロード中...</translation>
     </message>
     <message>
-        <source>Connect to Another Server...</source>
-        <translation type="unfinished"></translation>
+      <source>Connect to Another Server...</source>
+      <translation>他のサーバーに接続...</translation>
     </message>
     <message>
-        <source>Connect to Server...</source>
-        <translation type="unfinished"></translation>
+      <source>Connect to Server...</source>
+      <translation>サーバーに接続....</translation>
     </message>
-</context>
+  </context>
 </TS>
