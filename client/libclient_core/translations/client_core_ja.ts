f24edbe015576acbaf7e6637e36be90623852af7
Sergey Ivanov <sivanov@networkoptix.com>
2017-03-10 12:29:46 +0300

    Translations: ja, de, ko, fr
    <==== Mercurial data ====>
    Branch: dev_3.0.0_gui

--- client/libclient_core/translations/client_core_ja.ts
+++ client/libclient_core/translations/client_core_ja.ts
@@ -1,122 +1,122 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-  <context>
+<context>
     <name>QnCloudResultInfo</name>
     <message>
-      <source>Successful.</source>
-      <translation>成功</translation>
+        <source>Successful.</source>
+        <translation>成功</translation>
     </message>
     <message>
-      <source>Invalid login or password.</source>
-      <translation>ログインIDまたはパスワードが無効です</translation>
+        <source>Invalid login or password.</source>
+        <translation>ログインIDまたはパスワードが無効です</translation>
     </message>
     <message>
-      <source>Requested operation is not allowed with provided credentials.</source>
-      <translation>提供された資格情報では要求された操作は許可されません</translation>
+        <source>Requested operation is not allowed with provided credentials.</source>
+        <translation>提供された資格情報では要求された操作は許可されません</translation>
     </message>
     <message>
-      <source>This account is not activated. Please check your email.</source>
-      <translation>このアカウントはアクティベートされていません　メール アドレスを確認してください</translation>
+        <source>This account is not activated. Please check your email.</source>
+        <translation>このアカウントはアクティベートされていません　メール アドレスを確認してください</translation>
     </message>
     <message>
-      <source>This account is blocked.</source>
-      <translation>アカウントがブロックされています</translation>
+        <source>This account is blocked.</source>
+        <translation>アカウントがブロックされています</translation>
     </message>
     <message>
-      <source>Requested feature is not implemented.</source>
-      <translation>要求された機能は実装されていません</translation>
+        <source>Requested feature is not implemented.</source>
+        <translation>要求された機能は実装されていません</translation>
     </message>
     <message>
-      <source>Unknown realm.</source>
-      <translation>未知の領域</translation>
+        <source>Unknown realm.</source>
+        <translation>未知の領域</translation>
     </message>
     <message>
-      <source>Bad request.</source>
-      <translation>不正な要求</translation>
+        <source>Bad request.</source>
+        <translation>不正な要求</translation>
     </message>
     <message>
-      <source>Invalid nonce.</source>
-      <translation>無効な nonce</translation>
+        <source>Invalid nonce.</source>
+        <translation>無効な nonce</translation>
     </message>
     <message>
-      <source>Credentials are no longer valid.</source>
-      <translation>資格情報が有効ではなりました</translation>
+        <source>Credentials are no longer valid.</source>
+        <translation>資格情報が有効ではなりました</translation>
     </message>
     <message>
-      <source>Invalid data received.</source>
-      <translation>無効なデータを受信しました</translation>
+        <source>Invalid data received.</source>
+        <translation>無効なデータを受信しました</translation>
     </message>
     <message>
-      <source>Unknown error.</source>
-      <translation>不明なエラー</translation>
+        <source>Unknown error.</source>
+        <translation>不明なエラー</translation>
     </message>
     <message>
-      <source>Unexpected network error. Please check your Internet connection and try again.</source>
-      <translation>予期しないネットワーク エラーです　インターネット接続を確認してやり直してください</translation>
+        <source>Unexpected network error. Please check your Internet connection and try again.</source>
+        <translation>予期しないネットワーク エラーです　インターネット接続を確認してやり直してください</translation>
     </message>
     <message>
-      <source>Invalid login.</source>
-      <translation>無効なログイン</translation>
+        <source>Invalid login.</source>
+        <translation>無効なログイン</translation>
     </message>
     <message>
-      <source>Requested object is not found.</source>
-      <translation>要求されたオブジェクトが見つかりません</translation>
+        <source>Requested object is not found.</source>
+        <translation>要求されたオブジェクトが見つかりません</translation>
     </message>
     <message>
-      <source>Object already exists.</source>
-      <translation>オブジェクトは既に存在します</translation>
+        <source>Object already exists.</source>
+        <translation>オブジェクトは既に存在します</translation>
     </message>
     <message>
-      <source>Internal %1 error. Please contact support team.</source>
-      <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
-      <translation>内部 %1 エラーです　サポート チームまでご連絡ください</translation>
+        <source>Internal %1 error. Please contact support team.</source>
+        <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
+        <translation>内部 %1 エラーです　サポート チームまでご連絡ください</translation>
     </message>
     <message>
-      <source>Sorry, %1 Service is temporary unavailable. We are doing our best to restore it. Please try again later.</source>
-      <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
-      <translation>申し訳ありませんが%1 サービスが一時的に利用不可　復元の為に最善を尽くしています　後でもう一度試してください</translation>
+        <source>Sorry, %1 Service is temporary unavailable. We are doing our best to restore it. Please try again later.</source>
+        <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
+        <translation>申し訳ありませんが%1 サービスが一時的に利用不可　復元の為に最善を尽くしています　後でもう一度試してください</translation>
     </message>
     <message>
-      <source>Sorry, %1 Service could not process your request. Please try again in a few moments.</source>
-      <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
-      <translation>申し訳ありませんが %1 サービスはリクエストを処理できませんでした　数分後にもう一度やり直してください</translation>
+        <source>Sorry, %1 Service could not process your request. Please try again in a few moments.</source>
+        <comment>%1 is the cloud name (like &apos;Nx Cloud&apos;)</comment>
+        <translation>申し訳ありませんが %1 サービスはリクエストを処理できませんでした　数分後にもう一度やり直してください</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>QnCloudSystemInformationWatcher</name>
     <message>
-      <source>Owner: %1</source>
-      <comment>%1 is a user name</comment>
-      <translation>所有者: %1</translation>
+        <source>Owner: %1</source>
+        <comment>%1 is a user name</comment>
+        <translation>所有者: %1</translation>
     </message>
     <message>
-      <source>Your System</source>
-      <translation>お使いのシステム</translation>
+        <source>Your System</source>
+        <translation>お使いのシステム</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>QnDirectSystemsFinder</name>
     <message>
-      <source>System</source>
-      <translation>システム</translation>
+        <source>System</source>
+        <translation>システム</translation>
     </message>
     <message>
-      <source>System (%1)</source>
-      <comment>%1 is ip and port of system</comment>
-      <translation>システム (%1)</translation>
+        <source>System (%1)</source>
+        <comment>%1 is ip and port of system</comment>
+        <translation>システム (%1)</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>QnSystemsModel</name>
     <message>
-      <source>Owner: %1</source>
-      <comment>%1 is a user name</comment>
-      <translation>所有者: %1</translation>
+        <source>Owner: %1</source>
+        <comment>%1 is a user name</comment>
+        <translation>所有者: %1</translation>
     </message>
     <message>
-      <source>Your System</source>
-      <translation>お使いのシステム</translation>
+        <source>Your System</source>
+        <translation>お使いのシステム</translation>
     </message>
-  </context>
+</context>
 </TS>
