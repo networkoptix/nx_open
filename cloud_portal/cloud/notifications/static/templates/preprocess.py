c28847916a3c53fdfc15df7f2e6b58aae42d73f7
Evgeny Balashov <ebalashov@networkoptix.com>
2017-03-13 13:55:07 -0700

    CLOUD-963: Email on Cloud sharing contains the same text twice
    text templates for notifications
    
    <==== Mercurial data ====>
    Branch: cloud_portal_dev_3.0.0

--- cloud_portal/cloud/notifications/static/templates/preprocess.py
+++ cloud_portal/cloud/notifications/static/templates/preprocess.py
@@ -12,7 +12,8 @@ assert inlinestyler.VERSION == (0, 2, 1), "Setup correct version of inlinestyler
 
 cssutils.log.setLevel(logging.ERROR)
 
-container_text = open('container.mustache.template').read()
+container_text = open('container.txt.mustache.template').read()
+container_html = open('container.mustache.template').read()
 css_content = open('styles.css').read()
 
 
@@ -29,14 +30,21 @@ css_content = read_branding(css_content)
 
 # Replace variables in css style, using _custom_palette.scss
 
-container_text = container_text.replace('{{>style_css}}', css_content)
+container_html = container_html.replace('{{>style_css}}', css_content)
 
 for file in os.listdir("."):
     if file.endswith(".mustache.html"):
         template_name = file.replace('.mustache.html', '')
         new_file = open('../' + template_name + '.mustache', 'w')
-        text = container_text.replace('{{>body}}', open(template_name + '.mustache.html').read())
+        text = container_html.replace('{{>body}}', open(template_name + '.mustache.html').read())
         print >> new_file, htmlmin.minify(inline_css(text), remove_comments=True, remove_empty_space=True, remove_optional_attribute_quotes=False).encode('utf-8', 'replace')
+
+    if file.endswith(".mustache.txt"):
+        template_name = file.replace('.mustache.txt', '')
+        new_file = open('../' + template_name + '.txt.mustache', 'w')
+        text = container_text.replace('{{>body}}', open(template_name + '.mustache.txt').read())
+        print >> new_file, text
+
 
 shutil.copyfile("notifications-language.json", "../notifications-language.json")
 
