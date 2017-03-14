4e977b3beb9adc4c5f631ec6b9f7320250ad2dd5
Evgeny Balashov <ebalashov@networkoptix.com>
2017-03-14 14:21:19 -0700

    Language pack for Boris
    <==== Mercurial data ====>
    Branch: dev_3.0.0_web

--- webadmin/translation/language_pack.sh
+++ webadmin/translation/language_pack.sh
@@ -1,1 +1,14 @@
-echo "implement me"
+#!/bin/bash
+set -e
+
+DIRECTORY=language_pack
+
+rm -rf $DIRECTORY || true
+mkdir $DIRECTORY
+
+echo "Copy views"
+mkdir $DIRECTORY/views
+cp -rf ../app/views/*  $DIRECTORY/views
+
+echo "Copy language.json"
+cp -rf ../app/language.json  $DIRECTORY/
