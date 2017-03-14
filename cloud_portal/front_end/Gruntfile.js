59ce40025e6cc8f18f49101811c0061beee41f63
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2017-03-14 19:25:57 +0300

    Merged dev_3.0.0_test into dev_3.0.0
    <==== Mercurial data ====>
    Branch: cloud_portal_dev_3.0.0

--- cloud_portal/front_end/Gruntfile.js
+++ cloud_portal/front_end/Gruntfile.js
@@ -663,6 +663,7 @@ module.exports = function (grunt) {
         }
     });
 
+    // Example: grunt testportal:login:default
     grunt.registerTask('testportal', function (specsuit, brand) {
         var specsuit = specsuit || 'all';
         var customizationsArr = JSON.parse(grunt.file.read('./test-customizations.json'));
