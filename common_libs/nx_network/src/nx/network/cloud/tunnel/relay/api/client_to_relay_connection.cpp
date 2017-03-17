5ed917321fff7f56f446780ec7276ec8d4ca845a
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-03-17 11:39:02 +0300

    #CLOUD-987 Delevoping connecting peer class hierarchy.
    <==== Mercurial data ====>
    Branch: x_CLOUD-906_relay

---
+++ common_libs/nx_network/src/nx/network/cloud/tunnel/relay/api/client_to_relay_connection.cpp
@@ -0,0 +1,49 @@
+#include "client_to_relay_connection.h"
+
+namespace nx {
+namespace network {
+namespace cloud {
+namespace relay {
+namespace api {
+
+ClientToRelayConnection::ClientToRelayConnection(const SocketAddress& relayEndpoint):
+    m_relayEndpoint(relayEndpoint)
+{
+}
+
+ClientToRelayConnection::~ClientToRelayConnection()
+{
+    if (isInSelfAioThread())
+        stopWhileInAioThread();
+}
+
+void ClientToRelayConnection::startSession(
+    const nx::String& desiredSessionId,
+    const nx::String& targetPeerName,
+    nx::utils::MoveOnlyFunc<void(ResultCode, nx::String /*sessionId*/)> handler)
+{
+    // TODO
+}
+
+void ClientToRelayConnection::upgradeToRelayConnection(
+    nx::String& sessionId,
+    nx::utils::MoveOnlyFunc<void(ResultCode, std::unique_ptr<AbstractStreamSocket>)> handler)
+{
+    // TODO
+}
+
+SystemError::ErrorCode ClientToRelayConnection::prevRequestSysErrorCode() const
+{
+    return SystemError::noError;
+}
+
+void ClientToRelayConnection::stopWhileInAioThread()
+{
+    // TODO
+}
+
+} // namespace api
+} // namespace relay
+} // namespace cloud
+} // namespace network
+} // namespace nx
