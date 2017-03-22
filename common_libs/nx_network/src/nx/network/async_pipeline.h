00f6b085c6dcb236d9bc1c30e625eed256b116a3
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-03-22 11:46:19 +0300

    #CLOUD-1009 AsynchronousChannel: fixed bugs, added more tests
    <==== Mercurial data ====>
    Branch: x_CLOUD-1009_async_channel

--- common_libs/nx_network/src/nx/network/async_pipeline.h
+++ common_libs/nx_network/src/nx/network/async_pipeline.h
@@ -3,37 +3,43 @@
 #include <nx/utils/move_only_func.h>
 #include <nx/utils/std/cpp14.h>
 
-#include "abstract_socket.h"
 #include "aio/basic_pollable.h"
 
 namespace nx {
 namespace network {
 
-class AbstractAsyncPipeline:
+class AsynchronousChannel:
     public aio::BasicPollable
 {
 public:
-    virtual ~AbstractAsyncPipeline() override = default;
+    static constexpr std::size_t kDefaultReadBufferSize = 16 * 1024;
+    static constexpr std::size_t kDefaultSendQueueSizeBytes = 128 * 1024;
+
+    virtual ~AsynchronousChannel() override = default;
 
     virtual void start() = 0;
-    virtual void setOnDone(nx::utils::MoveOnlyFunc<void()> handler) = 0;
+    virtual void setOnDone(nx::utils::MoveOnlyFunc<void(SystemError::ErrorCode)> handler) = 0;
 };
 
 template<typename LeftFile, typename RightFile>
-class AsyncPipeline:
-    public AbstractAsyncPipeline
+class AsyncChannelImpl:
+    public AsynchronousChannel
 {
-    using base_type = AbstractAsyncPipeline;
+    using base_type = AsynchronousChannel;
 
 public:
-    AsyncPipeline(LeftFile leftFile, RightFile rightFile):
-        m_leftFile(std::move(leftFile)),
-        m_rightFile(std::move(rightFile))
+    AsyncChannelImpl(LeftFile leftFile, RightFile rightFile)
     {
+        m_leftFileContext.file = std::move(leftFile);
+        m_leftFileContext.readBuffer.reserve(kDefaultReadBufferSize);
+
+        m_rightFileContext.file = std::move(rightFile);
+        m_rightFileContext.readBuffer.reserve(kDefaultReadBufferSize);
+
         bindToAioThread(getAioThread());
     }
 
-    virtual ~AsyncPipeline() override
+    virtual ~AsyncChannelImpl() override
     {
         if (isInSelfAioThread())
             stopWhileInAioThread();
@@ -42,37 +48,158 @@ public:
     virtual void bindToAioThread(aio::AbstractAioThread* aioThread) override
     {
         base_type::bindToAioThread(aioThread);
-        m_leftFile->bindToAioThread(aioThread);
-        m_rightFile->bindToAioThread(aioThread);
+        m_leftFileContext.file->bindToAioThread(aioThread);
+        m_rightFileContext.file->bindToAioThread(aioThread);
     }
 
     virtual void start() override
     {
-        // TODO
+        scheduleRead(&m_leftFileContext, &m_rightFileContext);
+        scheduleRead(&m_rightFileContext, &m_leftFileContext);
     }
 
-    virtual void setOnDone(nx::utils::MoveOnlyFunc<void()> handler) override
+    virtual void setOnDone(nx::utils::MoveOnlyFunc<void(SystemError::ErrorCode)> handler) override
     {
         m_onDoneHandler = std::move(handler);
     }
 
 private:
-    LeftFile m_leftFile;
-    RightFile m_rightFile;
-    nx::utils::MoveOnlyFunc<void()> m_onDoneHandler;
+    template<typename FileType>
+    struct FileContext
+    {
+        FileType file;
+        nx::Buffer readBuffer;
+        std::list<nx::Buffer> sendQueue;
+        bool isReading = false;
+    };
+
+    FileContext<LeftFile> m_leftFileContext;
+    FileContext<RightFile> m_rightFileContext;
+    nx::utils::MoveOnlyFunc<void(SystemError::ErrorCode)> m_onDoneHandler;
 
     virtual void stopWhileInAioThread() override
     {
-        m_leftFile = nullptr;
-        m_rightFile = nullptr;
+        if (m_leftFileContext.file)
+        {
+            m_leftFileContext.file->pleaseStopSync();
+            m_leftFileContext.file = nullptr;
+        }
+        if (m_rightFileContext.file)
+        {
+            m_rightFileContext.file->pleaseStopSync();
+            m_rightFileContext.file = nullptr;
+        }
+    }
+
+    template<typename FileTypeOne, typename FileTypeTwo>
+    void scheduleRead(FileContext<FileTypeOne>* ctx, FileContext<FileTypeTwo>* anotherCtx)
+    {
+        using namespace std::placeholders;
+
+        ctx->file->readSomeAsync(
+            &ctx->readBuffer,
+            [this, ctx, anotherCtx](
+                SystemError::ErrorCode sysErrorCode, std::size_t bytesRead)
+            {
+                onDataRead(ctx, anotherCtx, sysErrorCode, bytesRead);
+            });
+        ctx->isReading = true;
+    }
+
+    template<typename FileTypeOne, typename FileTypeTwo>
+    void onDataRead(
+        FileContext<FileTypeOne>* ctx,
+        FileContext<FileTypeTwo>* anotherCtx,
+        SystemError::ErrorCode sysErrorCode,
+        std::size_t /*bytesRead*/)
+    {
+        ctx->isReading = false;
+
+        if (sysErrorCode != SystemError::noError)
+            return reportFailure(sysErrorCode);
+
+        nx::Buffer tmp;
+        ctx->readBuffer.swap(tmp);
+        scheduleSend(std::move(tmp), anotherCtx, ctx);
+        ctx->readBuffer.reserve(kDefaultReadBufferSize);
+        if (!readyToAcceptMoreData(anotherCtx))
+            return;
+        scheduleRead(ctx, anotherCtx);
+    }
+
+    template<typename FileTypeOne, typename FileTypeTwo>
+    void scheduleSend(
+        nx::Buffer data,
+        FileContext<FileTypeOne>* ctx,
+        FileContext<FileTypeTwo>* anotherCtx)
+    {
+        ctx->sendQueue.push_back(std::move(data));
+        if (ctx->sendQueue.size() > 1) //< Send already in progress?
+            return;
+        sendNextData(ctx, anotherCtx);
+    }
+
+    template<typename FileType>
+    bool readyToAcceptMoreData(FileContext<FileType>* /*fileContext*/)
+    {
+        // TODO
+        return true;
+    }
+
+    template<typename FileTypeOne, typename FileTypeTwo>
+    void sendNextData(FileContext<FileTypeOne>* ctx, FileContext<FileTypeTwo>* anotherCtx)
+    {
+        using namespace std::placeholders;
+
+        ctx->file->sendAsync(
+            ctx->sendQueue.front(),
+            [this, ctx, anotherCtx](
+                SystemError::ErrorCode sysErrorCode, std::size_t bytesRead)
+            {
+                onDataSent(ctx, anotherCtx, sysErrorCode, bytesRead);
+            });
+    }
+
+    template<typename FileTypeOne, typename FileTypeTwo>
+    void onDataSent(
+        FileContext<FileTypeOne>* ctx,
+        FileContext<FileTypeTwo>* anotherCtx,
+        SystemError::ErrorCode sysErrorCode,
+        std::size_t /*bytesRead*/)
+    {
+        if (sysErrorCode != SystemError::noError)
+            return reportFailure(sysErrorCode);
+        ctx->sendQueue.pop_front();
+
+        if (!ctx->sendQueue.empty())
+            sendNextData(ctx, anotherCtx);
+
+        if (!readyToAcceptMoreData(ctx))
+        {
+            NX_ASSERT(!ctx->sendQueue.empty());
+            return;
+        }
+
+        // Resuming read, if needed.
+        if (!anotherCtx->isReading)
+            scheduleRead(anotherCtx, ctx);
+    }
+    
+    void reportFailure(SystemError::ErrorCode sysErrorCode)
+    {
+        m_leftFileContext.file->cancelIOSync(aio::EventType::etNone);
+        m_rightFileContext.file->cancelIOSync(aio::EventType::etNone);
+
+        if (m_onDoneHandler)
+            m_onDoneHandler(sysErrorCode);
     }
 };
 
 template<typename LeftFile, typename RightFile>
-std::unique_ptr<AsyncPipeline<LeftFile, RightFile>> makeAsyncPipeline(
+std::unique_ptr<AsyncChannelImpl<LeftFile, RightFile>> makeAsyncChannel(
     LeftFile leftFile, RightFile rightFile)
 {
-    return std::make_unique<AsyncPipeline<LeftFile, RightFile>>(
+    return std::make_unique<AsyncChannelImpl<LeftFile, RightFile>>(
         std::move(leftFile),
         std::move(rightFile));
 }
--- unit_tests/nx_network_ut/src/async_pipeline_ut.cpp
+++ unit_tests/nx_network_ut/src/async_pipeline_ut.cpp
@@ -2,6 +2,9 @@
 
 #include <nx/network/async_pipeline.h>
 #include <nx/utils/pipeline.h>
+#include <nx/utils/random.h>
+#include <nx/utils/thread/mutex.h>
+#include <nx/utils/thread/wait_condition.h>
 
 namespace nx {
 namespace network {
@@ -11,12 +14,21 @@ class AsyncBufferReader:
     public aio::BasicPollable
 {
 public:
+    enum class InputDepletionPolicy
+    {
+        sendConnectionReset,
+        ignore
+    };
+
     AsyncBufferReader(
         utils::pipeline::AbstractInput* input,
-        utils::pipeline::AbstractOutput* output)
+        utils::pipeline::AbstractOutput* output,
+        InputDepletionPolicy inputDepletionPolicy)
         :
         m_input(input),
-        m_output(output)
+        m_output(output),
+        m_inputDepletionPolicy(inputDepletionPolicy),
+        m_errorState(false)
     {
     }
 
@@ -29,14 +41,23 @@ public:
         post(
             [this, buffer, handler = std::move(handler)]()
             {
+                if (m_errorState)
+                {
+                    handler(SystemError::connectionReset, (size_t)-1);
+                    return;
+                }
+
                 int bytesRead = m_input->read(
                     buffer->data() + buffer->size(),
                     buffer->capacity());
                 if (bytesRead > 0)
+                {
                     buffer->resize(buffer->size() + bytesRead);
-                handler(
-                    bytesRead >= 0 ? SystemError::noError : SystemError::connectionReset,
-                    bytesRead);
+                    handler(SystemError::noError, bytesRead);
+                    return;
+                }
+
+                handleInputDepletion(std::move(handler));
             });
     }
 
@@ -47,6 +68,12 @@ public:
         post(
             [this, &buffer, handler = std::move(handler)]()
             {
+                if (m_errorState)
+                {
+                    handler(SystemError::connectionReset, (size_t)-1);
+                    return;
+                }
+
                 int bytesWritten = m_output->write(buffer.constData(), buffer.size());
                 handler(
                     bytesWritten >= 0 ? SystemError::noError : SystemError::connectionReset,
@@ -54,71 +81,228 @@ public:
             });
     }
 
+    void cancelIOSync(nx::network::aio::EventType /*eventType*/)
+    {
+        pleaseStopSync();
+    }
+
+    void setErrorState()
+    {
+        m_errorState = true;
+    }
+
 private:
+    InputDepletionPolicy m_inputDepletionPolicy;
     utils::pipeline::AbstractInput* m_input;
     utils::pipeline::AbstractOutput* m_output;
+    std::atomic<bool> m_errorState;
+
+    void handleInputDepletion(
+        std::function<void(SystemError::ErrorCode, size_t)> handler)
+    {
+        switch (m_inputDepletionPolicy)
+        {
+            case InputDepletionPolicy::sendConnectionReset:
+                handler(SystemError::connectionReset, (size_t)-1);
+                break;
+            case InputDepletionPolicy::ignore:
+                break;
+        }
+    }
+};
+
+class NotifyingOutput:
+    public utils::pipeline::AbstractOutput
+{
+public:
+    virtual int write(const void* data, size_t count) override
+    {
+        QnMutexLocker lock(&m_mutex);
+        m_receivedData.append(static_cast<const char*>(data), count);
+        m_cond.wakeAll();
+        return count;
+    }
+
+    void waitForReceivedDataToMatch(const QByteArray& referenceData)
+    {
+        waitFor([this, &referenceData]() { return m_receivedData.startsWith(referenceData); });
+    }
+
+    void waitForSomeDataToBeReceived()
+    {
+        waitFor([this]() { return !m_receivedData.isEmpty(); });
+    }
+
+    const QByteArray& internalBuffer() const
+    {
+        return m_receivedData;
+    }
+
+private:
+    QByteArray m_receivedData;
+    QnMutex m_mutex;
+    QnWaitCondition m_cond;
+
+    template<typename ConditionFunc>
+    void waitFor(ConditionFunc func)
+    {
+        QnMutexLocker lock(&m_mutex);
+        while (!func())
+            m_cond.wait(lock.mutex());
+    }
 };
 
 //-------------------------------------------------------------------------------------------------
 // Test fixture
 
-const QByteArray kLeftData = "Hello, world";
-const QByteArray kRightData = "Bye, cruel world";
-
-class AsyncPipeline:
+class AsynchronousChannel:
     public ::testing::Test
 {
 public:
-    ~AsyncPipeline()
+    AsynchronousChannel()
     {
-        if (m_pipeline)
-            m_pipeline->pleaseStopSync();
+        m_originalData.resize(nx::utils::random::number<int>(1, 1024*1024));
+        std::generate(
+            m_originalData.data(),
+            m_originalData.data() + m_originalData.size(),
+            rand);
+    }
+
+    ~AsynchronousChannel()
+    {
+        if (m_channel)
+            m_channel->pleaseStopSync();
     }
 
 protected:
-    void establishPipeline()
+    void initializeFixedDataInput()
     {
-        m_leftSource.write(kLeftData.constData(), kLeftData.size());
-        m_rightSource.write(kRightData.constData(), kRightData.size());
+        m_leftSource = std::make_unique<utils::pipeline::ReflectingPipeline>(m_originalData);
+        m_rightSource = std::make_unique<utils::pipeline::ReflectingPipeline>(m_originalData);
+    }
 
-        m_leftFile = std::make_unique<AsyncBufferReader>(&m_leftSource, &m_leftDest);
-        m_rightFile = std::make_unique<AsyncBufferReader>(&m_rightSource, &m_rightDest);
+    void initializeInfiniteDataInput()
+    {
+        m_leftSource = std::make_unique<utils::pipeline::RandomDataSource>();
+        m_rightSource = std::make_unique<utils::pipeline::RandomDataSource>();
+    }
 
-        m_pipeline = makeAsyncPipeline(m_leftFile.get(), m_rightFile.get());
+    void createChannel()
+    {
+        m_leftFile = std::make_unique<AsyncBufferReader>(
+            m_leftSource.get(), &m_leftDest, AsyncBufferReader::InputDepletionPolicy::ignore);
+        m_rightFile = std::make_unique<AsyncBufferReader>(
+            m_rightSource.get(), &m_rightDest, AsyncBufferReader::InputDepletionPolicy::ignore);
+
+        m_channel = makeAsyncChannel(m_leftFile.get(), m_rightFile.get());
+        m_channel->setOnDone(
+            std::bind(&AsynchronousChannel::onChannelDone, this, std::placeholders::_1));
     }
 
     void exchangeData()
     {
-        utils::promise<void> done;
-        m_pipeline->setOnDone([&done]() { done.set_value(); });
-        m_pipeline->start();
-        done.get_future().wait();
+        if (!m_channel)
+            createChannel();
+
+        //utils::promise<SystemError::ErrorCode> done;
+        //m_channel->setOnDone(
+        //    [&done](SystemError::ErrorCode sysErrorCode)
+        //    {
+        //        done.set_value(sysErrorCode);
+        //    });
+        m_channel->start();
+        //ASSERT_EQ(SystemError::noError, done.get_future().get());
+    }
+
+    void startExchangingInfiniteData()
+    {
+        initializeInfiniteDataInput();
+        createChannel();
+        m_channel->start();
+    }
+    
+    void waitForSomeExchangeToHappen()
+    {
+        m_rightDest.waitForSomeDataToBeReceived();
+        m_leftDest.waitForSomeDataToBeReceived();
+    }
+
+    void stopChannel()
+    {
+        m_channel->pleaseStopSync();
+    }
+
+    void emulateErrorOnAnySource()
+    {
+        m_leftFile->setErrorState();
+    }
+    
+    void assertErrorHasBeenReported()
+    {
+        ASSERT_NE(SystemError::noError, m_channelDone.get_future().get());
     }
 
     void assertDataExchangeWasSuccessful()
     {
-        ASSERT_EQ(kLeftData, m_rightDest.internalBuffer());
-        ASSERT_EQ(kRightData, m_leftDest.internalBuffer());
+        m_rightDest.waitForReceivedDataToMatch(m_originalData);
+        m_leftDest.waitForReceivedDataToMatch(m_originalData);
     }
 
 private:
-    utils::pipeline::ReflectingPipeline m_leftSource;
-    utils::pipeline::ReflectingPipeline m_leftDest;
-    utils::pipeline::ReflectingPipeline m_rightSource;
-    utils::pipeline::ReflectingPipeline m_rightDest;
-    std::unique_ptr<network::AbstractAsyncPipeline> m_pipeline;
+    QByteArray m_originalData;
+    std::unique_ptr<utils::pipeline::AbstractInput> m_leftSource;
+    NotifyingOutput m_leftDest;
+    std::unique_ptr<utils::pipeline::AbstractInput> m_rightSource;
+    NotifyingOutput m_rightDest;
+    std::unique_ptr<network::AsynchronousChannel> m_channel;
     std::unique_ptr<AsyncBufferReader> m_leftFile;
     std::unique_ptr<AsyncBufferReader> m_rightFile;
+    nx::utils::promise<SystemError::ErrorCode> m_channelDone;
+
+    void onChannelDone(SystemError::ErrorCode sysErrorCode)
+    {
+        m_channelDone.set_value(sysErrorCode);
+    }
 };
 
-//TEST_F(AsyncPipeline, data_is_passed_through)
-//{
-//    establishPipeline();
-//    exchangeData();
-//    assertDataExchangeWasSuccessful();
-//}
+//-------------------------------------------------------------------------------------------------
+// Test cases
 
-//TEST_F(AsyncPipeline, cancellation)
+TEST_F(AsynchronousChannel, data_is_passed_through)
+{
+    initializeFixedDataInput();
+    exchangeData();
+    assertDataExchangeWasSuccessful();
+}
+
+TEST_F(AsynchronousChannel, cancellation)
+{
+    startExchangingInfiniteData();
+    waitForSomeExchangeToHappen();
+    stopChannel();
+}
+
+TEST_F(AsynchronousChannel, reports_error_from_source)
+{
+    startExchangingInfiniteData();
+    waitForSomeExchangeToHappen();
+    emulateErrorOnAnySource();
+    assertErrorHasBeenReported();
+}
+
+TEST_F(AsynchronousChannel, forwards_all_received_data_after_channel_closure)
+{
+    startExchangingInfiniteData();
+    pauseRightDestination();
+    waitForSomeDataToBeReadFromLeftSource();
+    closeLeftSource();
+    resumeRightDestination();
+    assertAllDataHasBeenPassed();
+}
+
+//TEST_F(AsynchronousChannel, read_cache)
+//TEST_F(AsynchronousChannel, read_saturation)
+//TEST_F(AsynchronousChannel, inactivity_timeout)
 
 } // namespace test
 } // namespace network
