130a490c5b57dadab180d69c79901aa54f95cf31
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-03-24 20:49:32 +0300

    VMS-5882: Can't configure local SMTP Server as Email Server if there's no Internet connection
    merge issue fixed
    
    <==== Mercurial data ====>
    Branch: dev_3.0.0

--- client/libclient/src/ui/widgets/system_settings/smtp/smtp_test_connection_widget.cpp
+++ client/libclient/src/ui/widgets/system_settings/smtp/smtp_test_connection_widget.cpp
@@ -8,6 +8,7 @@
 #include <core/resource/media_server_resource.h>
 
 #include <utils/email/email.h>
+#include <common/common_module.h>
 
 namespace {
     const int testSmtpTimeoutMSec = 20 * 1000;
@@ -98,6 +99,12 @@ bool QnSmtpTestConnectionWidget::testSettings(const QnEmailSettings &value)
 
         serverConnection = server->apiConnection();
         break;
+    }
+
+    if (!serverConnection)
+    {
+        if (auto server = qnCommon->currentServer())
+            serverConnection = server->apiConnection();
     }
 
     if (!serverConnection)
