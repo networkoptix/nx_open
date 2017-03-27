f36f063d82ff311b7eb8a77ae3c7812438ce97a3
Vitaly Kutin <vkutin@networkoptix.com>
2017-03-27 13:36:27 +0300

    VMS-1820: Create and edit resource interfaces
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/libclient/src/ui/workbench/handlers/workbench_webpage_handler.cpp
+++ client/libclient/src/ui/workbench/handlers/workbench_webpage_handler.cpp
@@ -9,56 +9,40 @@
 #include <ui/actions/action.h>
 #include <ui/actions/action_manager.h>
 
-#include <ui/dialogs/common/input_dialog.h>
+#include <ui/dialogs/webpage_dialog.h>
 
-namespace
+QnWorkbenchWebPageHandler::QnWorkbenchWebPageHandler(QObject* parent /*= nullptr*/):
+    base_type(parent),
+    QnWorkbenchContextAware(parent)
 {
-    const static QString kHttp(lit("http"));
-    const static QString kHttps(lit("https"));
+    connect(qnClientMessageProcessor, &QnClientMessageProcessor::initialResourcesReceived, this,
+        []()
+        {
+            /* Online status is set by default, page will go offline if will be unreachable on opening. */
+            for (const auto& webPage: qnResPool->getResources<QnWebPageResource>())
+                webPage->setStatus(Qn::Online);
+        });
 
-    bool isValidUrl(const QUrl &url) {
-        if (!url.isValid())
-            return false;
-
-        return url.scheme() == kHttp || url.scheme() == kHttps;
-    }
-}
-
-QnWorkbenchWebPageHandler::QnWorkbenchWebPageHandler(QObject *parent /*= 0*/)
-    : base_type(parent)
-    , QnWorkbenchContextAware(parent)
-{
-
-    connect(qnClientMessageProcessor, &QnClientMessageProcessor::initialResourcesReceived, this, []()
-    {
-        /* Online status is set by default, page will go offline if will be unreachable on opening. */
-        for (const QnWebPageResourcePtr &webPage: qnResPool->getResources<QnWebPageResource>())
-            webPage->setStatus(Qn::Online);
-    });
-
-    connect(action(QnActions::NewWebPageAction),   &QAction::triggered,        this,   &QnWorkbenchWebPageHandler::at_newWebPageAction_triggered);
+    connect(action(QnActions::NewWebPageAction), &QAction::triggered,
+        this, &QnWorkbenchWebPageHandler::at_newWebPageAction_triggered);
 }
 
 QnWorkbenchWebPageHandler::~QnWorkbenchWebPageHandler()
 {
-
 }
 
 void QnWorkbenchWebPageHandler::at_newWebPageAction_triggered()
 {
-    QScopedPointer<QnInputDialog> dialog(new QnInputDialog(mainWindow()));
-    dialog->setWindowTitle(tr("New Web Page..."));
-    dialog->setCaption(tr("Enter URL of Web Page to add:"));
-    dialog->setPlaceholderText(lit("example.org"));
+    QScopedPointer<QnWebpageDialog> dialog(new QnWebpageDialog(mainWindow()));
+    dialog->setWindowTitle(tr("New Web Page"));
     dialog->setWindowModality(Qt::ApplicationModal);
 
-    while (true) {
-         if(!dialog->exec())
+    while (true)
+    {
+         if (!dialog->exec())
              return;
 
-        QUrl url = QUrl::fromUserInput(dialog->value());
-        if (!isValidUrl(url))
-            continue;
+        QUrl url = QUrl::fromUserInput(dialog->url());
 
         QnWebPageResourcePtr webPage(new QnWebPageResource(url));
         if (qnResPool->getResourceById(webPage->getId()))
@@ -67,11 +51,7 @@ void QnWorkbenchWebPageHandler::at_newWebPageAction_triggered()
             continue;
         }
 
-        qnResourcesChangesManager->saveWebPage(webPage, [](const QnWebPageResourcePtr &webPage)
-        {
-            Q_UNUSED(webPage);
-        });
-
+        qnResourcesChangesManager->saveWebPage(webPage, [](const QnWebPageResourcePtr& /*webPage*/) {});
         break;
     };
 }
