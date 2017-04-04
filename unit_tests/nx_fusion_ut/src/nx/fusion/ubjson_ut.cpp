38095a644f812f277d60698376827f60c59bdbe3
Sergey Ivanov <sivanov@networkoptix.com>
2017-04-04 17:36:02 +0300

    ## Ubjson unit tests
    <==== Mercurial data ====>
    Branch: 3.1_singletone_refactor

--- unit_tests/nx_fusion_ut/src/nx/fusion/ubjson_ut.cpp
+++ unit_tests/nx_fusion_ut/src/nx/fusion/ubjson_ut.cpp
@@ -10,6 +10,14 @@ TEST(UbJsonRectTest, float)
     ASSERT_TRUE(qFuzzyEquals(value, result));
 }
 
+TEST(UbJsonRectTest, double)
+{
+    const double value = 17.2;
+    QByteArray data = QnUbjson::serialized(value);
+    const double result = QnUbjson::deserialized<double>(data);
+    ASSERT_EQ(value, result);
+}
+
 TEST(UbJsonRectTest, QRect)
 {
     QRect value(1, 2, 5, 17);
@@ -20,8 +28,8 @@ TEST(UbJsonRectTest, QRect)
 
 TEST(UbJsonRectTest, QRectF)
 {
-    QRectF value(1, 2, 5.5, 17);
+    QRectF value(1, 2, 5.5, 17.2);
     QByteArray data = QnUbjson::serialized(value);
     QRectF result = QnUbjson::deserialized<QRectF>(data);
-    ASSERT_TRUE(qFuzzyEquals(value, result));
+    ASSERT_EQ(value, result);
 }
