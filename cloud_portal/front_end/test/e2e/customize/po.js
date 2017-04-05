88a6b968d25d5477fe20fffd3e9506a0c7e46a55
Ekaterina Korneeva <ekorneeva@networkoptix.com>
2017-04-05 17:37:38 +0300

    Multiple language and customizations autotests update
    <==== Mercurial data ====>
    Branch: cloud_portal_dev_3.0.0_test

--- cloud_portal/front_end/test/e2e/customize/po.js
+++ cloud_portal/front_end/test/e2e/customize/po.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Customization = function () {
+    var p = this;
 
     var PasswordFieldSuit = require('../password_check.js');
     this.passwordField = new PasswordFieldSuit();
@@ -15,8 +16,17 @@ var Customization = function () {
     this.brandText = brandObj.text;
 
     this.languageDropdown = element(by.id('language-dropdown'));
-    this.russianOption = element(by.css('li[language-select]')).element(by.cssContainingText('a', 'Russia'));
 
+    this.languageOption = element(by.css('li[language-select]')).all(by.repeater('lang in languages'));
+    this.russianOption = element(by.css('li[language-select]')).element(by.css('span[lang=ru]'));
+
+    this.welcomeElem = element(by.css(".welcome-caption"));
+
+    this.selectNextLang = function (index) {
+        p.languageDropdown.click();
+        p.languageOption.get(index).click();
+        browser.sleep(2000);
+    }
 };
 
 module.exports = Customization;
