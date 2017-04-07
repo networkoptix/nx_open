d11e5ea04d1b416d90868007115d2b7bd05bacca
Sergey Ivanov <sivanov@networkoptix.com>
2017-04-07 16:03:51 +0300

    VMS-5763 - Layout tours - simple fullscreen tour mode
    <==== Mercurial data ====>
    Branch: x_vms_5760_layout_tours

--- client/libclient/src/nx/client/ui/workbench/handlers/layout_tours_handler.cpp
+++ client/libclient/src/nx/client/ui/workbench/handlers/layout_tours_handler.cpp
@@ -12,7 +12,18 @@
 #include <ui/workbench/workbench.h>
 #include <ui/style/skin.h>
 
+#include <ui/workbench/workbench_layout.h>
+#include <nx/client/ui/workbench/layouts/layout_factory.h>
+
 #include <nx/utils/string.h>
+
+#include <utils/math/math.h>
+
+namespace {
+
+static const int kTimerPrecisionMs = 500;
+
+}
 
 namespace nx {
 namespace client {
@@ -81,8 +92,28 @@ LayoutToursHandler::LayoutToursHandler(QObject* parent):
             qnLayoutTourManager->saveTour(tour);
         });
 
+    connect(action(QnActions::StartLayoutTourAction), &QAction::triggered, this,
+        [this]()
+        {
+            QnActionParameters parameters = menu()->currentParameters(sender());
+            auto id = parameters.argument<QnUuid>(Qn::UuidRole);
+            auto tour = qnLayoutTourManager->tour(id);
+            if (!tour.isValid())
+                return;
+
+            startTour(tour);
+        });
+
     connect(action(QnActions::OpenLayoutTourAction), &QAction::triggered,
         this, &LayoutToursHandler::openToursLayout);
+}
+
+void LayoutToursHandler::timerEvent(QTimerEvent* event)
+{
+    if (event->timerId() != m_timerId)
+        base_type::timerEvent(event);
+
+    processTourStep();
 }
 
 void LayoutToursHandler::openToursLayout()
@@ -127,6 +158,105 @@ void LayoutToursHandler::openToursLayout()
     resource->setId(QnUuid::createUuid());
     qnResPool->addResource(resource);
     menu()->trigger(QnActions::OpenSingleLayoutAction, resource);
+}
+
+void LayoutToursHandler::startTour(const ec2::ApiLayoutTourData& tour)
+{
+    const auto items = qnLayoutTourManager->tourItems(tour);
+    if (items.empty())
+        return;
+
+    m_runningTourId = tour.id;
+    m_lastState = QnWorkbenchState();
+    workbench()->submit(m_lastState);
+    workbench()->clear();
+
+    QnWorkbenchLayout* firstLayout = nullptr;
+    //TODO #GDM #3.1 move logic to a separate controller
+    for (const auto& item: items)
+    {
+        auto layout = QnWorkbenchLayout::instance(item.layout);
+        if (!layout)
+        {
+            layout = qnWorkbenchLayoutsFactory->create(item.layout, workbench());
+            workbench()->addLayout(layout);
+        }
+        if (!firstLayout)
+            firstLayout = layout;
+    }
+    if (m_timerId == 0)
+        m_timerId = startTimer(kTimerPrecisionMs);
+    m_currentIndex = 0;
+    m_elapsed.start();
+    workbench()->setCurrentLayout(firstLayout);
+}
+
+void LayoutToursHandler::processTourStep()
+{
+    auto tour = qnLayoutTourManager->tour(m_runningTourId);
+    NX_ASSERT(tour.isValid());
+    if (!tour.isValid())
+    {
+        stopTour();
+        return;
+    }
+
+    const bool hasItem = qBetween(0, m_currentIndex, (int)tour.items.size());
+    NX_ASSERT(hasItem);
+    if (!hasItem)
+    {
+        stopTour();
+        return;
+    }
+
+    // No need to switch the only item.
+    if (tour.items.size() < 2)
+        return;
+
+    NX_ASSERT(m_elapsed.isValid());
+    if (!m_elapsed.isValid())
+        m_elapsed.start();
+
+    const auto& item = tour.items[m_currentIndex];
+    if (!m_elapsed.hasExpired(item.delayMs))
+        return;
+
+    m_currentIndex = (m_currentIndex + 1) % tour.items.size();
+    const auto& next = tour.items[m_currentIndex];
+
+    auto layout = qnResPool->getResourceById<QnLayoutResource>(next.layoutId);
+    if (!layout)
+    {
+        stopTour();
+        return;
+    }
+
+    auto wbLayout = QnWorkbenchLayout::instance(layout);
+    if (!wbLayout)
+    {
+        wbLayout = qnWorkbenchLayoutsFactory->create(layout, workbench());
+        workbench()->addLayout(wbLayout);
+    }
+    m_elapsed.restart();
+    workbench()->setCurrentLayout(wbLayout);
+}
+
+void LayoutToursHandler::stopTour()
+{
+    if (m_timerId != 0)
+    {
+        killTimer(m_timerId);
+        m_timerId = 0;
+    }
+    m_elapsed.invalidate();
+    m_currentIndex = 0;
+    m_runningTourId = QnUuid();
+
+    workbench()->clear();
+    workbench()->update(m_lastState);
+
+    if (workbench()->layouts().empty() || !workbench()->currentLayout()->resource())
+        menu()->trigger(QnActions::OpenNewTabAction);
 }
 
 } // namespace workbench
