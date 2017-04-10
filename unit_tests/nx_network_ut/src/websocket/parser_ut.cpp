d9b3e58e1c020d8c58e62a78a5348951d83c5ff8
Alexander Kulikov <akulikov@networkoptix.com>
2017-04-10 20:02:57 +0300

    vms-5922. Parser UT. In progress.
    <==== Mercurial data ====>
    Branch: x_vms_5922_websocket

--- unit_tests/nx_network_ut/src/websocket/parser_ut.cpp
+++ unit_tests/nx_network_ut/src/websocket/parser_ut.cpp
@@ -29,3 +29,22 @@ TEST(WebsocketParser, SimpleTextMessage_OneBuffer)
     Parser p(Role::client, &ph);
     p.consume((char*)kShortTextMessageFinNoMask, sizeof(kShortTextMessageFinNoMask));
 }
+
+TEST(WebsocketParser, SimpleTestMessage_SeveralParts)
+{
+    TestParserHandler ph;
+    EXPECT_CALL(ph, frameStarted(FrameType::text, true)).Times(1);
+    EXPECT_CALL(ph, framePayload((const char*)kShortTextMessageFinNoMask + 2, 1)).Times(1);
+    EXPECT_CALL(ph, framePayload((const char*)kShortTextMessageFinNoMask + 3, 1)).Times(1);
+    EXPECT_CALL(ph, framePayload((const char*)kShortTextMessageFinNoMask + 4, 1)).Times(1);
+    EXPECT_CALL(ph, framePayload((const char*)kShortTextMessageFinNoMask + 5, 1)).Times(1);
+    EXPECT_CALL(ph, framePayload((const char*)kShortTextMessageFinNoMask + 6, 1)).Times(1);
+
+    EXPECT_CALL(ph, frameEnded()).Times(1);
+    EXPECT_CALL(ph, messageEnded()).Times(1);
+    EXPECT_CALL(ph, handleError(_)).Times(0);
+
+    Parser p(Role::client, &ph);
+    for (int i = 0; i < sizeof(kShortTextMessageFinNoMask); ++i)
+        p.consume((char*)kShortTextMessageFinNoMask + i, 1);
+}
