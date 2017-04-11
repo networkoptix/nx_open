2d18b5a27c7017dae9bdf6ae360cd5896aef74d5
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-04-11 11:05:37 +0300

    #VMS-6000 Added reflecting asynchronous channel
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ unit_tests/nx_network_ut/src/aio/async_channel_reflector_ut.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include <nx/network/aio/async_channel_reflector.h>
+
+namespace nx {
+namespace network {
+namespace aio {
+namespace test {
+
+class AsyncChannelReflector:
+    public ::testing::Test
+{
+};
+
+//TEST_F(AsyncChannelReflector, actually_reflects_data)
+//TEST_F(AsyncChannelReflector, reports_underlying_channel_io_error)
+
+} // namespace test
+} // namespace aio
+} // namespace network
+} // namespace nx
