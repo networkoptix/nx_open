8731a927de3c25060a152c93aea4a5c72313e7c6
Nick Hartleb <nhartleb@networkoptix.com>
2017-04-11 18:22:17 -0700

    WEB-503: Webclient: refactor controls according to spec
    Added mute to volume controls. Volume level is now saved in local storage.
    Slider is in the correct position when it is initialized now.
    
    <==== Mercurial data ====>
    Branch: dev_3.1_web

--- webadmin/app/scripts/directives/volumeControl.js
+++ webadmin/app/scripts/directives/volumeControl.js
@@ -10,11 +10,27 @@ angular.module('webadminApp')
         	templateUrl: Config.viewsDir + 'components/volumeControl.html',
         	link: function(scope, element){
 		        var selector = '[data-rangeslider]';
+
+		        scope.currentVolume = scope.volumeLevel;
+		        scope.mute = false;
+
+		        scope.toggleMute = function(){
+		   			scope.mute = !scope.mute;
+		        	if(scope.mute){
+		        		scope.currentVolume = scope.volumeLevel;
+		        		scope.volumeLevel = 0;
+		        	}
+		        	else
+		        		scope.volumeLevel = scope.currentVolume;
+		        	
+		        };
+
         		$('volume-control>.bar').rangeslider({
 		        	// Deactivate the feature detection
 		        	polyfill: false,
 		        	// Callback function
 		            onInit: function() {
+		            	this.setPosition(scope.volumeLevel,false);
 		            	this.$element[0].value = scope.volumeLevel;
 		            },
 		            // Callback function
@@ -31,6 +47,7 @@ angular.module('webadminApp')
 		            		return;
 		                //console.log("In Volume Control", new Date());
 		                scope.volumeLevel = value;
+	                	scope.mute = value > 0 ? false : true;
 		                scope.$apply();
 		            }
 		        });
