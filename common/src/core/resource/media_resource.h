158f0a94fe14bae076cdd28d06c1f79ac11af3da
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-04-13 22:05:20 +0300

    # Fix server crash on reading chunks
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- common/src/core/resource/media_resource.h
+++ common/src/core/resource/media_resource.h
@@ -48,11 +48,11 @@ public:
     virtual const QnResourcePtr toResourcePtr() const = 0;
     virtual QnResourcePtr toResourcePtr() = 0;
 
-    QnMediaDewarpingParams getDewarpingParams() const;
+    virtual QnMediaDewarpingParams getDewarpingParams() const;
     void setDewarpingParams(const QnMediaDewarpingParams& params);
 
     //TODO: #dklychkov change to QnAspectRatio in 2.4
-    qreal customAspectRatio() const;
+    virtual qreal customAspectRatio() const;
     void setCustomAspectRatio(qreal value);
     void clearCustomAspectRatio();
 
