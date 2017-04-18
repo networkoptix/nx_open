16d1858d66448e68f70db634ec547a4341e56f63
Alexander Kulikov <akulikov@networkoptix.com>
2017-04-18 17:25:28 +0300

    vms-5922. Multibuffer class introduced.
    <==== Mercurial data ====>
    Branch: x_vms_5922_websocket

---
+++ common_libs/nx_network/src/nx/network/websocket/websocket_multibuffer.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <deque>
+#include <nx/network/buffer.h>
+
+namespace nx {
+namespace network {
+namespace websocket {
+
+/**
+ * Multi-frame(message) buffer. Represents a deque (queue) of buffers.
+ * Appends are allowed only to the front buffer. After front buffer is full it
+ * can be locked. Only locked buffers are allowed for pop.
+ */
+class NX_NETWORK_API MultiBuffer
+{
+    struct LockableBuffer
+    {
+        nx::Buffer buffer;
+        bool locked;
+
+        LockableBuffer() : locked(false) {}
+    };
+
+    using BuffersType = std::deque<LockableBuffer>;
+
+public:
+    int readySize() const;
+
+    void append(const nx::Buffer& buf);
+    void append(const char* data, int size);
+
+    void lock();
+    bool locked() const;
+
+    nx::Buffer pop();
+
+    void clear();
+    void clearLast();
+
+private:
+    BuffersType m_buffers;
+};
+
+} // namespace websocket
+} // namespace network
+} // namespace nx
