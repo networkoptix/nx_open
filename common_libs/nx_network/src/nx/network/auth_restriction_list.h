502f12abf3fac03406730b2bedfa8bcaa3c1bf00
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-04-21 13:11:45 +0300

    Coding style fixes.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/auth_restriction_list.h
+++ common_libs/nx_network/src/nx/network/auth_restriction_list.h
@@ -1,10 +1,4 @@
-/**********************************************************
-* Aug 27, 2015
-* a.kolesnikov
-***********************************************************/
-
-#ifndef AUTH_RESTRICTION_LIST_H
-#define AUTH_RESTRICTION_LIST_H
+#pragma once 
 
 #include <map>
 
@@ -12,61 +6,67 @@
 
 #include <nx/network/http/httptypes.h>
 
+namespace AuthMethod {
 
-namespace AuthMethod
+enum Value
 {
-    enum Value
-    {
-        NotDefined      = 0x00,
-        noAuth          = 0x01,
-        //!authentication method described in rfc2617
-        httpBasic       = 0x02,
-        httpDigest      = 0x04,
-        http            = httpBasic | httpDigest,
-        //!base on \a authinfo cookie
-        cookie          = 0x08,
-        //!authentication by X-NetworkOptix-VideoWall header
-        //TODO #ak remove this value from here
-        videowall       = 0x10,
-        //!Authentication by url query parameters \a auth and \a proxy_auth
-        /*!
-            params has following format: BASE64-URL( UTF8(username) ":" MD5( LATIN1(username) ":NetworkOptix:" LATIN1(password) ) )
-            TODO #ak this method is too poor, have to introduce some salt
-        */
-        urlQueryParam   = 0x20,
-        tempUrlQueryParam   = 0x40
-    };
-    Q_DECLARE_FLAGS(Values, Value);
-    Q_DECLARE_OPERATORS_FOR_FLAGS(Values);
-}
+    NotDefined      = 0x00,
+    noAuth          = 0x01,
+    /**
+     * Authentication method described in rfc2617.
+     */
+    httpBasic       = 0x02,
+    httpDigest      = 0x04,
+    http            = httpBasic | httpDigest,
+    /**
+     * Based on authinfo cookie.
+     */
+    cookie          = 0x08,
+    /**
+     * Authentication by X-NetworkOptix-VideoWall header.
+     * TODO: #ak Does not look appropriate here.
+     */
+    videowall       = 0x10,
+    /**
+     * Authentication by url query parameters auth and proxy_auth.
+     * params has following format: BASE64-URL( UTF8(username) ":" MD5( LATIN1(username) ":NetworkOptix:" LATIN1(password) ) )
+     * TODO #ak this method is too poor, have to introduce some salt.
+     */
+    urlQueryParam   = 0x20,
+    tempUrlQueryParam   = 0x40
+};
 
-/*!
-    \note By default, \a AuthMethod::http, \a AuthMethod::cookie and \a AuthMethod::videowall authorization methods are allowed fo every url
-*/
+Q_DECLARE_FLAGS(Values, Value);
+Q_DECLARE_OPERATORS_FOR_FLAGS(Values);
+
+} // namespace AuthMethod
+
+/**
+ * @note By default, AuthMethod::http, AuthMethod::cookie and AuthMethod::videowall 
+ * authorization methods are allowed fo every url.
+ */
 class NX_NETWORK_API QnAuthMethodRestrictionList
 {
 public:
     QnAuthMethodRestrictionList();
 
-    /*!
-        \return bit mask of auth methods (\a AuthMethod::Value enumeration)
-    */
-    unsigned int getAllowedAuthMethods( const nx_http::Request& request ) const;
+    /**
+     * @return bit mask of auth methods (AuthMethod::Value enumeration).
+     */
+    unsigned int getAllowedAuthMethods(const nx_http::Request& request) const;
 
-    /*!
-        \param pathMask wildcard-mask of path
-    */
-    void allow( const QString& pathMask, AuthMethod::Value method );
-    /*!
-        \param pathMask wildcard-mask of path
-    */
-    void deny( const QString& pathMask, AuthMethod::Value method );
+    /**
+     * @param pathMask wildcard-mask of path.
+     */
+    void allow(const QString& pathMask, AuthMethod::Value method);
+    /**
+     * @param pathMask wildcard-mask of path.
+     */
+    void deny(const QString& pathMask, AuthMethod::Value method);
 
 private:
-    //!map<path mask, allowed auth bitmask>
+    /** map<path mask, allowed auth bitmask>. */
     std::map<QString, unsigned int> m_allowed;
-    //!map<path mask, denied auth bitmask>
+    /** map<path mask, denied auth bitmask>. */
     std::map<QString, unsigned int> m_denied;
 };
-
-#endif  //AUTH_RESTRICTION_LIST_H
