f7e5aa0152d0b127836e708c4679ec0dc4b4859f
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-04-24 15:05:54 +0300

    #CLOUD-1066 Added dictionary that can match paths like /account/{accountId}/systems, also returning matched parameter value
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ common_libs/nx_network/src/nx/network/http/server/rest/http_server_rest_path_matcher.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <map>
+#include <regex>
+#include <string>
+
+namespace nx_http {
+namespace server {
+namespace rest {
+
+/**
+ * Given registered path: "/account/{accountId}/systems".
+ * When matching path "/account/vpupkin/systems".
+ * Then "/account/{accountId}/systems" is found and pathParams contains "vpupkin".
+ */
+template<typename Mapped>
+class RestPathMatcher
+{
+public:
+    bool add(const std::string& pathTemplate, Mapped mapped)
+    {
+        MatchContext matchContext;
+        matchContext.regex = convertToRegex(pathTemplate);
+        matchContext.mapped = std::move(mapped);
+
+        return m_restPathToMatchContext.emplace(
+            pathTemplate,
+            std::move(matchContext)).second;
+    }
+
+    boost::optional<const Mapped&> match(
+        const std::string& path,
+        std::vector<std::string>* pathParams) const
+    {
+        for (const auto& matchContext: m_restPathToMatchContext)
+        {
+            std::smatch matchResult;
+            if (std::regex_search(path, matchResult, matchContext.second.regex))
+            {
+                for (size_t i = 1; i < matchResult.size(); ++i)
+                    pathParams->push_back(matchResult[i]);
+                return boost::optional<const Mapped&>(matchContext.second.mapped);
+            }
+        }
+
+        return boost::none;
+    }
+
+private:
+    struct MatchContext
+    {
+        std::regex regex;
+        Mapped mapped;
+    };
+
+    /** REST path template, context */
+    std::map<std::string, MatchContext> m_restPathToMatchContext;
+
+    std::regex convertToRegex(const std::string& pathTemplate)
+    {
+        const std::regex replaceRestParams("\\{[\\w\\d]+\\}");
+
+        std::string restPathMatchRegex;
+        restPathMatchRegex += "^";
+        restPathMatchRegex +=
+            std::regex_replace(pathTemplate, replaceRestParams, "([\\w\\d\\-_.]+)");
+        restPathMatchRegex += "$";
+
+        return std::regex(std::move(restPathMatchRegex), std::regex::icase);
+    }
+};
+
+} // namespace rest
+} // namespace server
+} // namespace nx_http
---
+++ unit_tests/nx_network_ut/src/http/server/rest/http_server_rest_path_matcher_ut.cpp
@@ -0,0 +1,87 @@
+#include <gtest/gtest.h>
+
+#include <nx/network/http/server/rest/http_server_rest_path_matcher.h>
+
+namespace nx_http {
+namespace server {
+namespace rest {
+namespace test {
+
+class RestPathMatcher:
+    public ::testing::Test
+{
+protected:
+    void assertPathRegistered(const std::string& restPath, int val)
+    {
+        ASSERT_TRUE(m_dictionary.add(restPath, val));
+    }
+
+    void assertPathNotRegistered(const std::string& restPath, int val)
+    {
+        ASSERT_FALSE(m_dictionary.add(restPath, val));
+    }
+
+    void assertPathMatches(
+        const std::string& path,
+        int expectedValue,
+        std::vector<std::string> expectedParams)
+    {
+        std::vector<std::string> params;
+        const auto& result = m_dictionary.match(path, &params);
+
+        ASSERT_TRUE(result);
+        ASSERT_EQ(expectedValue, *result);
+        ASSERT_EQ(expectedParams.size(), params.size());
+        for (std::size_t i = 0; i < expectedParams.size(); ++i)
+        {
+            ASSERT_EQ(expectedParams[i], params[i]);
+        }
+    }
+
+    void assertPathNotMatched(const std::string& path)
+    {
+        std::vector<std::string> params;
+        ASSERT_FALSE(m_dictionary.match(path, &params));
+    }
+
+private:
+    rest::RestPathMatcher<int> m_dictionary;
+};
+
+TEST_F(RestPathMatcher, find_handler)
+{
+    assertPathRegistered("/account/{accountId}/systems", 1);
+    assertPathRegistered("/systems/{systemId}/users", 2);
+
+    assertPathMatches("/account/vpupkin/systems", 1, { "vpupkin" });
+    assertPathMatches("/account/vpu-pk.i_n/systems", 1, { "vpu-pk.i_n" });
+    assertPathMatches("/systems/sys1/users", 2, { "sys1" });
+}
+
+TEST_F(RestPathMatcher, find_case_insensitive)
+{
+    assertPathRegistered("/account/{accountId}/systems", 1);
+    assertPathMatches("/accOUNT/vpupkIN/SYSTems", 1, { "vpupkIN" });
+}
+
+TEST_F(RestPathMatcher, no_suitable_handler)
+{
+    assertPathRegistered("/account/{accountId}/systems", 1);
+
+    assertPathNotMatched("/account/vpupkin/info");
+    assertPathNotMatched("/account/vpupkin/systems/");
+    assertPathNotMatched("account/vpupkin/systems/");
+}
+
+TEST_F(RestPathMatcher, registering_conflicting_handler)
+{
+    assertPathRegistered("/account/{accountId}/systems", 1);
+    assertPathNotRegistered("/account/{accountId}/systems", 2);
+
+    assertPathMatches("/account/vpupkin/systems", 1, {"vpupkin"});
+}
+
+} // namespace test
+} // namespace rest
+} // namespace server
+} // namespace nx_http
