01cba4c5b598976f1834afee5ff01b4b5acf0938
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-04-25 16:17:23 +0300

    Coding style fixes
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/http/auth_restriction_list.h
+++ common_libs/nx_network/src/nx/network/http/auth_restriction_list.h
@@ -12,30 +12,27 @@ namespace AuthMethod {
 
 enum Value
 {
-    NotDefined      = 0x00,
-    noAuth          = 0x01,
-    /**
-     * Authentication method described in rfc2617.
-     */
-    httpBasic       = 0x02,
-    httpDigest      = 0x04,
-    http            = httpBasic | httpDigest,
-    /**
-     * Based on authinfo cookie.
-     */
-    cookie          = 0x08,
+    NotDefined = 0x00,
+    noAuth = 0x01,
+    /** Authentication method described in rfc2617. */
+    httpBasic = 0x02,
+    /** Authentication method described in rfc2617. */
+    httpDigest = 0x04,
+    http = httpBasic | httpDigest,
+    /** Based on authinfo cookie. */
+    cookie = 0x08,
     /**
      * Authentication by X-NetworkOptix-VideoWall header.
      * TODO: #ak Does not look appropriate here.
      */
-    videowall       = 0x10,
+    videowall = 0x10,
     /**
      * Authentication by url query parameters auth and proxy_auth.
-     * params has following format: BASE64-URL( UTF8(username) ":" MD5( LATIN1(username) ":NetworkOptix:" LATIN1(password) ) )
+     * Params have the following format: BASE64-URL( UTF8(username) ":" MD5( LATIN1(username) ":NetworkOptix:" LATIN1(password) ) )
      * TODO #ak this method is too poor, have to introduce some salt.
      */
-    urlQueryParam   = 0x20,
-    tempUrlQueryParam   = 0x40
+    urlQueryParam = 0x20,
+    tempUrlQueryParam = 0x40,
 };
 
 Q_DECLARE_FLAGS(Values, Value);
@@ -51,16 +48,16 @@ class NX_NETWORK_API AuthMethodRestrictionList
 {
 public:
     /**
-     * @return bit mask of auth methods (AuthMethod::Value enumeration).
+     * @return Bit mask of auth methods (AuthMethod::Value enumeration).
      */
     unsigned int getAllowedAuthMethods(const nx_http::Request& request) const;
 
     /**
-     * @param pathMask wildcard-mask of path.
+     * @param pathMask Wildcard-mask of path.
      */
     void allow(const QString& pathMask, AuthMethod::Value method);
     /**
-     * @param pathMask wildcard-mask of path.
+     * @param pathMask Wildcard-mask of path.
      */
     void deny(const QString& pathMask, AuthMethod::Value method);
 
