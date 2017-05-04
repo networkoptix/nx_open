d74035ad6b54917b90c7917d9bba847c352e1707
Evgeny Balashov <ebalashov@networkoptix.com>
2017-05-03 17:13:59 -0700

    CLOUD-1116: Make user model compatible with django admin
    add users to admin interface
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1056_cms

--- cloud_portal/cloud/api/admin.py
+++ cloud_portal/cloud/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+
+
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Account, AccountAdmin)
