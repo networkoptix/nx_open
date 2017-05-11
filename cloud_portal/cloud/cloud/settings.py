456cac11505b0ed3e8c65633e460c750afa4ffe9
Evgeny Balashov <ebalashov@networkoptix.com>
2017-05-10 17:39:17 -0700

    fix after merge
    <==== Mercurial data ====>
    Branch: x_CLOUD-1056_cms

--- cloud_portal/cloud/cloud/settings.py
+++ cloud_portal/cloud/cloud/settings.py
@@ -189,8 +189,6 @@ LOGGING = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = '/app/app/static/common/static'
-
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
