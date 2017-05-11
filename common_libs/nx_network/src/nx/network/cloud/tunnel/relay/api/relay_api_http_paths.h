6dff33d2fa5e9ca5e98598ce9f31892dcc9c5a56
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-05-11 17:02:53 +0300

    #CLOUD-984 Implemented relaying connection activation with notification.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_http_paths.h
+++ common_libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_http_paths.h
@@ -10,6 +10,7 @@ NX_NETWORK_API extern const char* const kRelayProtocolName;
 NX_NETWORK_API extern const char* const kServerIncomingConnectionsPath;
 NX_NETWORK_API extern const char* const kServerClientSessionsPath;
 NX_NETWORK_API extern const char* const kClientSessionConnectionsPath;
+NX_NETWORK_API extern const char* const kRelayClientPath;
 
 } // namespace api
 } // namespace relay
