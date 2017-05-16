9dcec3996896606b77476dea0ca0fa6799122256
Sergey Ivanov <sivanov@networkoptix.com>
2017-05-16 14:49:33 +0300

    VMS-6144 - Client crashes after offline server is deleted
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- common/src/core/resource/media_server_resource.cpp
+++ common/src/core/resource/media_server_resource.cpp
@@ -527,8 +527,11 @@ void QnMediaServerResource::setSystemInfo(const QnSystemInformation &systemInfo)
 }
 QnModuleInformation QnMediaServerResource::getModuleInformation() const
 {
-    if (getId() == resourcePool()->commonModule()->moduleGUID())
-        return resourcePool()->commonModule()->moduleInformation();
+    if (auto module = commonModule())
+    {
+        if (getId() == module->moduleGUID())
+            return module->moduleInformation();
+    }
 
     // build module information for other server
 
