63a80285cda7049f2e82c2c06b78915001cde874
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-05-16 19:21:26 +0300

    CLOUD-998 Added nat traversal methods delay to "Connect" response from mediator
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/cloud/data/connection_parameters.cpp
+++ common_libs/nx_network/src/nx/network/cloud/data/connection_parameters.cpp
@@ -24,7 +24,10 @@ bool ConnectionParameters::operator==(const ConnectionParameters& rhs) const
         && udpTunnelKeepAliveRetries == rhs.udpTunnelKeepAliveRetries
         && tunnelInactivityTimeout == rhs.tunnelInactivityTimeout
         && tcpReverseRetryPolicy == rhs.tcpReverseRetryPolicy
-        && tcpReverseHttpTimeouts == rhs.tcpReverseHttpTimeouts;
+        && tcpReverseHttpTimeouts == rhs.tcpReverseHttpTimeouts
+        && udpHolePunchingStartDelay == rhs.udpHolePunchingStartDelay
+        && trafficRelayingStartDelay == rhs.trafficRelayingStartDelay
+        && directTcpConnectStartDelay == rhs.directTcpConnectStartDelay;
 }
 
 void ConnectionParameters::serializeAttributes(nx::stun::Message* const message)
@@ -64,6 +67,18 @@ void ConnectionParameters::serializeAttributes(nx::stun::Message* const message)
     message->addAttribute(
         stun::extension::attrs::tcpReverseHttpMsgBodyTimeout,
         tcpReverseHttpTimeouts.messageBodyReadTimeout);
+
+    // Start delays.
+
+    message->addAttribute(
+        stun::extension::attrs::udpHolePunchingStartDelay,
+        udpHolePunchingStartDelay);
+    message->addAttribute(
+        stun::extension::attrs::trafficRelayingStartDelay,
+        trafficRelayingStartDelay);
+    message->addAttribute(
+        stun::extension::attrs::directTcpConnectStartDelay,
+        directTcpConnectStartDelay);
 }
 
 bool ConnectionParameters::parseAttributes(const nx::stun::Message& message)
@@ -118,6 +133,21 @@ bool ConnectionParameters::parseAttributes(const nx::stun::Message& message)
         message, stun::extension::attrs::tcpReverseHttpMsgBodyTimeout,
         &tcpReverseHttpTimeouts.messageBodyReadTimeout,
         (std::chrono::milliseconds)nx_http::AsyncHttpClient::Timeouts::kDefaultMessageBodyReadTimeout);
+
+    // Start delays.
+
+    readAttributeValue(
+        message, stun::extension::attrs::udpHolePunchingStartDelay,
+        &udpHolePunchingStartDelay,
+        std::chrono::milliseconds::zero());
+    readAttributeValue(
+        message, stun::extension::attrs::trafficRelayingStartDelay,
+        &trafficRelayingStartDelay,
+        std::chrono::milliseconds::zero());
+    readAttributeValue(
+        message, stun::extension::attrs::directTcpConnectStartDelay,
+        &directTcpConnectStartDelay,
+        std::chrono::milliseconds::zero());
 
     return true;
 }
