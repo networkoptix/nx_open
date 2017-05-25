c8f0bba6d0b27e0eb424236e17c84e6cb8c42b75
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-05-25 20:04:51 +0300

    Coding style fixes.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/http/http_parser.h
+++ common_libs/nx_network/src/nx/network/http/http_parser.h
@@ -8,29 +8,28 @@
 namespace nx_http {
 
 /**
- * This class is just a wrapper for use with \a nx::network::server::BaseStreamProtocolConnection class. 
+ * This class is just a wrapper for use with nx::network::server::BaseStreamProtocolConnection class. 
  */
 class NX_NETWORK_API MessageParser
 {
 public:
     MessageParser();
 
-    void setMessage( Message* const msg );
-    //!Returns current parse state
-    /*!
-        Methods returns if:\n
-            - end of message found
-            - source data depleted
-
-        \param buf
-        \param bytesProcessed Number of bytes from \a buf which were read and parsed is stored here
-        \note \a *buf MAY NOT contain whole message, but any part of it (it can be as little as 1 byte)
-        \note Reads whole message even if parse error occured
-    */
-    nx::network::server::ParserState parse( const nx::Buffer& buf, size_t* bytesProcessed );
+    void setMessage(Message* const msg);
+    /**
+     * Methods returns if:
+     *   - end of message found
+     *   - source data depleted
+     *
+     * @param bytesProcessed Number of bytes from buf which were read and parsed is stored here.
+     * Returns current parse state.
+     * NOTE: *buf MAY NOT contain whole message, but any part of it (it can be as little as 1 byte).
+     * NOTE: Reads whole message even if parse error occured.
+     */
+    nx::network::server::ParserState parse(const nx::Buffer& buf, size_t* bytesProcessed);
     nx::network::server::ParserState processEof();
 
-    //!Resets parse state and prepares for parsing different data
+    /** Resets parse state and prepares for parsing different data. */
     void reset();
 
 private:
