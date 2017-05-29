a032d541e3582a79886d251b29359470fc4480a5
Sergey Ivanov <sivanov@networkoptix.com>
2017-05-29 10:35:05 +0300

    VMS-6325 - Client crash in I/O monitor
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/camera/iomodule/iomodule_monitor.h
+++ client/nx_client_desktop/src/camera/iomodule/iomodule_monitor.h
@@ -14,6 +14,7 @@ class QnIOModuleMonitor: public QObject, public QnConnectionContextAware
     Q_OBJECT
 public:
     QnIOModuleMonitor(const QnSecurityCamResourcePtr &camera);
+    virtual ~QnIOModuleMonitor();
     bool open();
 signals:
     void connectionClosed();
