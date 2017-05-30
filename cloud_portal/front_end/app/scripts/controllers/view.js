2718731a3a4e692af66d5f0880221991e5a0543b
Evgeny Balashov <ebalashov@networkoptix.com>
2017-05-30 13:17:48 -0700

    WEB-546: Embed webclient to cloud portal
    Do not cache system key in localstorage, force update system auth key in view
    
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- cloud_portal/front_end/app/scripts/controllers/view.js
+++ cloud_portal/front_end/app/scripts/controllers/view.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('cloudApp')
-    .controller('ViewPageCtrl', ['$scope', 'account',
-    function ($scope, account) {
-        account.requireLogin();
+    .controller('ViewPageCtrl', ['$scope', 'account', 'system', '$routeParams',
+    function ($scope, account, system, $routeParams) {
+        account.requireLogin().then(function(account){
+            $scope.account = account;
+            var currentSystem = system($routeParams.systemId, account.email);
+            currentSystem.updateSystemAuth().then(function(){
+                $scope.system = currentSystem;
+            }); // Make system login
+        });
 
         $("body").addClass("webclient-page");
         $scope.$on('$destroy', function( event ) {
