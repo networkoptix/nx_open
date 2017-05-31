ab59ae2f5ad870d3a96d6cb9b732d2b16137ea2b
Ivan Vigasin <ivigasin@networkoptix.com>
2017-05-30 21:39:46 -0700

    Fix run_targets
    <==== Mercurial data ====>
    Branch: master

--- nx_cloud_deploy/make.sh
+++ nx_cloud_deploy/make.sh
@@ -8,7 +8,7 @@ function run_targets()
     local modules=$1
     local targets=$2
 
-    for module in $modules
+    for module in ${modules[@]}
     do
         echo "Running targets $targets for module $module.."
         (cd $module; ./make.sh $targets)
@@ -17,17 +17,17 @@ function run_targets()
 
 function pack_all()
 {
-    run_targets "$MODULES" "stage pack"
+    run_targets "${MODULES[@]}" "stage pack"
 }
 
 function push_all()
 {
-    run_targets "$MODULES" "push"
+    run_targets "${MODULES[@]}" "push"
 }
 
 function clean_all()
 {
-    run_targets "$MODULES" "clean"
+    run_targets "${MODULES[@]}" "clean"
 }
 
 main $@
