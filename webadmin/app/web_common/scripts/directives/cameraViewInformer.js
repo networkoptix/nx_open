d24634f000a41820295732852299ba0e84ca50a2
Nick Hartleb <nhartleb@networkoptix.com>
2017-05-31 15:34:18 -0700

    Web-532: Refactor camera states
    removed excess logic from cameraViewInformer
    
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- webadmin/app/web_common/scripts/directives/cameraViewInformer.js
+++ webadmin/app/web_common/scripts/directives/cameraViewInformer.js
@@ -5,78 +5,37 @@ angular.module('nxCommon')
 		return{
 			restrict: 'E',
         	scope:{
-        		viewFlags: "=",
-        		videoFlags: "="
+        	    flags: "="
         	},
         	templateUrl: Config.viewsDirCommon + 'components/cameraViewInformer.html',
         	link: function(scope){
         		scope.cameraStates = L.cameraStates;
-        		scope.showView = false;
-        		scope.showFlags = false;
                 scope.message = "";
 
-        		scope.$watch('viewFlags', function(){
+                scope.$watch('flags', function(){
                     scope.alertType = null;
-                    //if status is offline or unauthorized and no position is selected or ios video is too large show the cameraViewinformer directive
-        			if((scope.viewFlags.status == "Offline" || scope.viewFlags.status == 'Unauthorized') &&
-        			   !scope.viewFlags.positionSelected || scope.viewFlags.iosVideoTooLarge)
-        			{
-        				scope.showView = true;
-        			}
-        			else{
-        				scope.showView = false;
-        			}
 
-
-                    //find message to display
-                    for(var key in scope.viewFlags){
-                        if(scope.viewFlags[key]){
+                    //check for flag
+                    for(var key in scope.flags){
+                        if(scope.flags[key]){
                             scope.alertType = key;
                             break;
                         }
                     }
-
+                    
+                    //if non flag break
                     if(!scope.alertType){
                         return;
                     }
-                    
 
-                    //If status turn on html and load specific status messages
-                    if( scope.alertType == 'status'){
-                        if(scope.viewFlags[scope.alertType] == "Offline"){
-                            scope.message = scope.cameraStates.offline;
-                        }
-                        else if(scope.viewFlags[scope.alertType] == "Unauthorized"){
-                            scope.message = scope.cameraStates.unauthorized;
-                        }
+                    //offline and unauthorized are special cases. All others can be set without editing
+                    if(scope.alertType == 'status' && scope.flags[scope.alertType] != "Online"){
+                        scope.message = scope.cameraStates[(scope.flags[scope.alertType]).toLowerCase()];
                     }
-                    //Otherwise load remaining message
                     else{
                         scope.message = scope.cameraStates[scope.alertType];
                     }
-        		}, true);
-        		
-
-
-        		scope.$watch('videoFlags', function(){
-                    scope.alertType = null;
-
-                    //find message to display
-                    for(var key in scope.videoFlags){
-                        if(scope.videoFlags[key]){
-                            scope.alertType = key;
-                            break;
-                        }
-                    }
-                    scope.showVideo = scope.alertType != null;
-                    
-                    if(!scope.alertType){
-                        return;
-                    }
-
-                    //load message to display
-                    scope.message = scope.cameraStates[scope.alertType];
-        		}, true);
+                }, true);
         	}
 		};
 	});
