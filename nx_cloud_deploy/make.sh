63221fc3ef46a94a0fe15a7ba65f38a6f81e5eb9
Ivan Vigasin <ivigasin@networkoptix.com>
2017-05-30 21:47:33 -0700

    Fix run_targets
    <==== Mercurial data ====>
    Branch: master

--- nx_cloud_deploy/make.sh
+++ nx_cloud_deploy/make.sh
@@ -17,17 +17,17 @@ function run_targets()
 
 function pack_all()
 {
-    run_targets "${MODULES[@]}" "stage pack"
+    run_targets "${MODULES[*]}" "stage pack"
 }
 
 function push_all()
 {
-    run_targets "${MODULES[@]}" "push"
+    run_targets "${MODULES[*]}" "push"
 }
 
 function clean_all()
 {
-    run_targets "${MODULES[@]}" "clean"
+    run_targets "${MODULES[*]}" "clean"
 }
 
 main $@
