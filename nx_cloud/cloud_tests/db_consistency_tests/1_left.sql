8850c4f1c93abf9ad7f8e0260765b7deb0b10157
Ivan Vigasin <ivigasin@networkoptix.com>
2017-06-05 12:14:23 -0700

    Move monitoring and db_consistency_tests
    <==== Mercurial data ====>
    Branch: master

---
+++ nx_cloud/cloud_tests/db_consistency_tests/1_left.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) 
+	FROM clouddb.account
+LEFT JOIN cloudportal.api_account 
+	ON clouddb.account.email = cloudportal.api_account.email
+WHERE cloudportal.api_account.email IS NULL AND clouddb.account.status_code<>4
