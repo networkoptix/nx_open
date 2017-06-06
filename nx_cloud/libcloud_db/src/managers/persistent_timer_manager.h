806c4a1b7de36f2a4eb0a8c625f91e86c676eecd
Alexander Kulikov <akulikov@networkoptix.com>
2017-06-06 14:52:09 +0300

    cloud-303. In progress.
    <==== Mercurial data ====>
    Branch: x_cloud_303_PersistentTimerManager

--- nx_cloud/libcloud_db/src/managers/persistent_timer_manager.h
+++ nx_cloud/libcloud_db/src/managers/persistent_timer_manager.h
@@ -1,37 +1,52 @@
 #pragma once
 
 #include <string>
+#include <chrono>
 #include <unordered_map>
 #include <nx/utils/uuid.h>
 #include <nx/utils/singleton.h>
 
 namespace nx {
+namespace db {
+
+class QueryContext;
+
+}
+}
+
+namespace nx {
 namespace cdb {
 
 class PersistentTimerManager;
+using PersistentParamsMap = std::unordered_map<std::string, std::string>;
 
-using PersistentParamsMap =
-    std::unordered_map<std::string, std::unordered_map<std::string, std::string>>;
-
-class AbstractPersistentTimerAcceptor
+class AbstractPersistentTimerEventReceiver
 {
 public:
-    virtual void persistentTimerFired(
-        PersistentTimerManager* manager,
-        PersistentParamsMap& params) = 0;
+    virtual void persistentTimerFired(PersistentParamsMap& params) = 0;
 
-    virtual ~AbstractPersistentTimerAcceptor() {}
+    virtual ~AbstractPersistentTimerEventReceiver() {}
 };
 
-namespace detail {
-
-using GuidToHandlerMap = std::unordered_map<QnUuid, std::function<void(AbstractPersistentTimerAcceptor*, PersistentTimerManager*, const PersistentParamsMap&)>>;
-
-}
 
 class PersistentTimerManager: public Singleton<PersistentTimerManager>
 {
 public:
+    void registerEventReceiver(
+        const QnUuid& taskId,
+        AbstractPersistentTimerEventReceiver* receiver);
+
+    void subscribe(
+        const QnUuid& taskId,
+        std::chrono::milliseconds timeout,
+        const PersistentParamsMap& params,
+        nx::db::QueryContext* queryContext = nullptr);
+
+    void unsubscribe(const QnUuid& taskId, nx::db::QueryContext* = nullptr);
+
+private:
+    std::unordered_map<QnUuid, AbstractPersistentTimerEventReceiver*> m_receiverMap;
+    std::unordered_map<QnUuid, PersistentParamsMap> m_paramsMap;
 };
 
 }
--- unit_tests/cloud_db_ut/src/managers/persistent_timer_manager_ut.cpp
+++ unit_tests/cloud_db_ut/src/managers/persistent_timer_manager_ut.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <nx/utils/std/future.h>
+#include <chrono>
 #include <managers/persistent_timer_manager.h>
 
 namespace nx {
@@ -8,31 +10,71 @@ namespace test {
 class PersistentTimerManager : public ::testing::Test
 {
 protected:
+    virtual void SetUp() override
+    {
+        readyFuture = readyPromise.get_future();
+    }
+
     nx::cdb::PersistentTimerManager manager;
+    nx::utils::future<void> readyFuture;
+    nx::utils::promise<void> readyPromise;
 };
 
 
-class ManagerUser: public nx::cdb::AbstractPersistentTimerAcceptor
+class ManagerUser: public nx::cdb::AbstractPersistentTimerEventReceiver
 {
 public:
     static const QnUuid userTaskGuid;
 
-    ManagerUser()
+    ManagerUser(nx::cdb::PersistentTimerManager* manager, nx::utils::promise* readyPromise):
+        m_manager(manager)
     {
-        NX_REGISTER_PERSISTENT_TASK(this, userTaskGuid, &ManagerUser::onPersistentTimer);
+        manager->registerEventReceiver(userTaskGuid, this);
     }
 
-    virtual void persistentTimerFired(
-        PersistentTimerManager* manager,
-        const PersistentParamsMap& params) override
+    virtual void persistentTimerFired(const PersistentParamsMap& params) override
     {
+        ASSERT_NE(params.find("key1"), params.cend());
+        ASSERT_NE(params.find("key2"), params.cend());
 
+        ++m_fired;
+        if (m_shouldUnsubscribe)
+        {
+            m_manager->unsubscribe(userTaskGuid);
+            m_readyPromise->set_value();
+        }
     }
+
+    void subscribe(std::chrono::milliseconds timeout)
+    {
+        nx::cdb::PersistentParamsMap params;
+        params["key1"] = "value1";
+        params["key2"] = "value2";
+
+        m_manager->subscribe(userTaskGuid, timeout, params);
+    }
+
+    void setShouldUnsubscribe() { m_shouldUnsubscribe = true; }
+    int fired() const { return m_fired; }
+
+private:
+    nx::cdb::PersistentTimerManager* m_manager;
+    nx::utils::promise* m_readyPromise;
+    std::atomic<bool> m_shouldUnsubscribe = false;
+    int m_fired = 0;
 };
 
-NX_REGISTER_PERSISTENT_USER(ManagerUser);
-
 const QnUuid ManagerUser::userTaskGuid = QnUuid::fromStringSafe("{EC05F182-9380-48E3-9C76-AD6C10295136}");
+
+TEST_F(PersistentTimerManager, subscribeUnsubscribe_simple)
+{
+    ManagerUser user(&manager, &readyPromise);
+    manager.subscribe(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    readyFuture.wait();
+    ASSERT_GE(manager.fired(), 0);
+}
 
 }
 }
