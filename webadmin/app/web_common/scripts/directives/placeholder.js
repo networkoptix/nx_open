0fddd8dde97a908af4803c0b78642e53ce2407c0
Evgeny Balashov <ebalashov@networkoptix.com>
2017-06-05 19:37:43 -0700

    WEB-563: Show preloaders while loading view interfaces
    moving placaholder and preloader to a separate reusable directive
    
    <==== Mercurial data ====>
    Branch: vms_3.1_web

---
+++ webadmin/app/web_common/scripts/directives/placeholder.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('nxCommon')
+	.directive('placeholder',function(){
+		return{
+			restrict: 'E',
+        	scope:{
+        	    iconClass:"=",
+        	    title: "=",
+        	    message: "=",
+        	    preloader: "="
+        	},
+        	templateUrl: Config.viewsDirCommon + 'components/placeholder.html'
+		};
+	});
