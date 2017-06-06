d51ce0a746c0152bc1ccdb76c05c120a0e409d2e
Evgeny Balashov <ebalashov@networkoptix.com>
2017-06-06 14:51:19 -0700

    WEB-566: "Server Offline" placeholder for view interfaces
    better logic for preloader and system-offline state
    
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- cloud_portal/front_end/app/scripts/controllers/view.js
+++ cloud_portal/front_end/app/scripts/controllers/view.js
@@ -7,18 +7,18 @@ angular.module('cloudApp')
         var currentSystem;
         account.requireLogin().then(function(account){
             $scope.currentSystem = system($routeParams.systemId, account.email);
-            $scope.currentSystem.getInfo().then(delayedUpdateSystemInfo);
-            $scope.currentSystem.updateSystemAuth().then(function(){
-                $scope.systemReady = true;
-                $scope.system = $scope.currentSystem.mediaserver;
-            },function(error){
-                // cannot read the system
-                dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}", currentSystem.name || L.errorCodes.thisSystem), 'warning');
-                $location.path("/systems");
+            $scope.currentSystem.getInfo().then(function(){
+                return $scope.currentSystem.updateSystemAuth().then(function(){
+                    $scope.systemReady = true;
+                    $scope.system = $scope.currentSystem.mediaserver;
+                    delayedUpdateSystemInfo();
+                },function(error){
+                    // cannot read the system
+                    dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}", currentSystem.name || L.errorCodes.thisSystem), 'warning');
+                    $location.path("/systems");
+                });
             });
         });
-
-
 
         function delayedUpdateSystemInfo(){
             var pollingSystemUpdate = $poll(function(){
