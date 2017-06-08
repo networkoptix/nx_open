1ac0808912817a3d5516362ad7f064d8597f37e4
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-06-08 16:57:36 +0300

    VMS-5037: Add new tracked event under "Server Issue" topic
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- mediaserver_core/src/recorder/storage_manager.cpp
+++ mediaserver_core/src/recorder/storage_manager.cpp
@@ -61,6 +61,11 @@ static const qint64 MOTION_CLEANUP_INTERVAL = 1000ll * 3600;
 static const qint64 BOOKMARK_CLEANUP_INTERVAL = 1000ll * 60;
 
 const qint64 kMinStorageFreeSpace = 150 * 1024 * 1024LL;
+#ifdef __arm__
+const qint64 kMinSystemStorageFreeSpace = 1000000000LL;
+#else
+const qint64 kMinSystemStorageFreeSpace = 5000000000LL;
+#endif
 
 static const QString SCAN_ARCHIVE_FROM(lit("SCAN_ARCHIVE_FROM"));
 
@@ -430,7 +435,8 @@ QnStorageManager::QnStorageManager(
     m_firstStoragesTestDone(false),
     m_isRenameDisabled(qnServerModule->roSettings()->value("disableRename").toInt()),
     m_camInfoWriterHandler(this, commonModule->resourcePool()),
-    m_camInfoWriter(&m_camInfoWriterHandler)
+    m_camInfoWriter(&m_camInfoWriterHandler),
+    m_lowSysStorageSpaceWarnShown(false)
 {
     NX_ASSERT(m_role == QnServer::StoragePool::Normal || m_role == QnServer::StoragePool::Backup);
     m_storageWarnTimer.restart();
@@ -1517,9 +1523,34 @@ void QnStorageManager::updateCameraHistory() const
     return;
 }
 
+void QnStorageManager::checkSystemStorageSpace()
+{
+    QnStorageManager::StorageMap storageRoots = getAllStorages();
+    qint64 bigStorageThreshold = 0;
+    for (const auto& storage: getAllStorages())
+    {
+        if (storage->getStatus() == Qn::Online && storage->isSystem())
+        {
+            if (!m_lowSysStorageSpaceWarnShown
+                && storage->getFreeSpace() < kMinSystemStorageFreeSpace)
+            {
+                m_lowSysStorageSpaceWarnShown = true;
+                emit storageFailure(storage, QnBusiness::SystemStorageFullReason);
+            }
+            else if (storage->getFreeSpace() > kMinSystemStorageFreeSpace * 2)
+            {
+                m_lowSysStorageSpaceWarnShown = false;
+            }
+        }
+    }
+}
+
 void QnStorageManager::clearSpace(bool forced)
 {
     QnMutexLocker lk(&m_clearSpaceMutex);
+
+    checkSystemStorageSpace();
+
     // if Backup on Schedule (or on demand) synchronization routine is
     // running at the moment, dont run clearSpace() if it's been triggered by
     // the timer.
