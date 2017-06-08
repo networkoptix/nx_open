10ac859055a81dd64505abe470c996afb018f7d4
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-06-08 13:50:09 +0300

    #CLOUD-1175 Added cloud user offline login test.
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ tests/vms_cloud_integration_tests/src/cloud_user_offline_login.cpp
@@ -0,0 +1,85 @@
+#include <chrono>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <cloud_db_client/src/data/auth_data.h>
+
+#include "mediaserver_cloud_integration_test_setup.h"
+
+namespace {
+
+static bool resourceParamPresent(
+    const ec2::ApiResourceParamDataList& params,
+    const QString& name)
+{
+    for (const auto& param: params)
+    {
+        if (param.name == name)
+            return true;
+    }
+
+    return false;
+}
+
+} // namespace
+
+class CloudUserOfflineLogin:
+    public MediaServerCloudIntegrationTest,
+    public ::testing::Test
+{
+protected:
+    void givenCloudSystem()
+    {
+        waitUserCloudAuthInfoToBeSynchronized();
+    }
+
+    void whenSystemWentOffline()
+    {
+        cdb()->stop();
+    }
+
+    void whenMediaServerRestarted()
+    {
+        mediaServer().stop();
+        ASSERT_TRUE(mediaServer().start());
+    }
+
+    void thenUserCanStillLogin()
+    {
+        auto mediaServerClient = prepareMediaServerClientFromCloudOwner();
+        ec2::ApiResourceParamDataList vmsSettings;
+        ASSERT_EQ(ec2::ErrorCode::ok, mediaServerClient.ec2GetSettings(&vmsSettings));
+    }
+
+private:
+    virtual void SetUp() override
+    {
+        connectSystemToCloud();
+    }
+
+    void waitUserCloudAuthInfoToBeSynchronized()
+    {
+        auto mediaServerClient = prepareMediaServerClient();
+        for (;;)
+        {
+            ec2::ApiResourceParamDataList params;
+            ASSERT_EQ(
+                ec2::ErrorCode::ok,
+                mediaServerClient.ec2GetResourceParams(
+                    QnUuid::fromStringSafe(cloudOwnerVmsUserId()),
+                    &params));
+            if (resourceParamPresent(params, nx::cdb::api::kVmsUserAuthInfoAttributeName))
+                break;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+};
+
+TEST_F(CloudUserOfflineLogin, DISABLED_login_works_on_offline_server_after_restart)
+{
+    givenCloudSystem();
+    whenSystemWentOffline();
+    whenMediaServerRestarted();
+    thenUserCanStillLogin();
+}
