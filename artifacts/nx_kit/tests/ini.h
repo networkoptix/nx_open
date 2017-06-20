73fb4816ff970947d636613914a5ab1fb88edd7a
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-06-20 16:53:38 +0300

    # nx_kit: Added copyright notice of LGPLv3 to every source file.
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- artifacts/nx_kit/tests/ini.h
+++ artifacts/nx_kit/tests/ini.h
@@ -1,3 +1,4 @@
+// Copyright 2017 Network Optix, Inc. Licensed under GNU Lesser General Public License version 3.
 #pragma once
 
 #include <stdio.h>
--- artifacts/nx_kit/tests/ini_config_test.cpp
+++ artifacts/nx_kit/tests/ini_config_test.cpp
@@ -1,3 +1,5 @@
+// Copyright 2017 Network Optix, Inc. Licensed under GNU Lesser General Public License version 3.
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
