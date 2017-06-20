c028ce8bfffd9d55917c83ba16a0bd0edd26b88a
Evgeny Balashov <ebalashov@networkoptix.com>
2017-06-20 16:26:03 -0700

    CLOUD-1061: Preview site (local on admin instance)
    <==== Mercurial data ====>
    Branch: x_CLOUD-1056_cms

--- cloud_portal/cloud/cms/controllers/filldata.py
+++ cloud_portal/cloud/cms/controllers/filldata.py
@@ -6,6 +6,16 @@ import re
 STATIC_DIR = 'static/{{customization}}/source/'
 
 
+def make_dir(filename):
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+
 def customizable_file(filename):
     supported_format = filename.endswith('.json') or filename.endswith('.html') or filename.endswith('.mustache')
     supported_directory = "lang_" not in filename or "lang_en_US" in filename
@@ -37,19 +47,19 @@ def process_context_structure(customization, context, content, language):
         # try to get translated content
         if language:
             content_record = DataRecord.objects.filter(language_id=language.id,
-                                                      data_structure_id=record.id,
-                                                      customization_id=customization.id)
+                                                       data_structure_id=record.id,
+                                                       customization_id=customization.id)
         # if not - get default language
         if not content_record.exists():
             content_record = DataRecord.objects.filter(language_id=customization.default_language_id,
-                                                      data_structure_id=record.id,
-                                                      customization_id=customization.id)
+                                                       data_structure_id=record.id,
+                                                       customization_id=customization.id)
 
         # if not - get record without language
         if not content_record.exists():
             content_record = DataRecord.objects.filter(language_id=None,
-                                                      data_structure_id=record.id,
-                                                      customization_id=customization.id)
+                                                       data_structure_id=record.id,
+                                                       customization_id=customization.id)
 
         if content_record and content_record.exists():
             content_value = content_record.first().value
@@ -61,7 +71,7 @@ def process_context_structure(customization, context, content, language):
     return content
 
 
-def process_file(source_file, customization, product_id):
+def process_file(source_file, customization, product_id, preview):
     context_name, language_code = context_for_file(source_file, customization.name)
 
     branding_context = Context.objects.filter(name='branding')
@@ -73,28 +83,30 @@ def process_file(source_file, customization, product_id):
         content = file.read()
 
     if branding_context.exists():
-        print 'branding', source_file
         content = process_context_structure(customization, branding_context.first(), content, None)
     if context.exists() and language:
-        print 'context', source_file
         content = process_context_structure(customization, context.first(), content, language)
 
     filename = context_name
     if language_code:
         filename = filename.replace("{{language}}", language_code)
     # write content to target place
-    target_file = os.path.join('static', customization.name, filename)
+    if not preview:
+        target_file = os.path.join('static', customization.name, filename)
+    else:
+        target_file = os.path.join('static', customization.name, 'preview', filename)
+    make_dir(target_file)
     with open(target_file, 'w') as file:
         file.write(content)
 
 
-def fill_content(customization_name='default', product='cloud_portal'):
+def fill_content(customization_name='default', product='cloud_portal', preview=True):
     product_id = Product.objects.get(name=product).id
     customization = Customization.objects.get(name=customization_name)
 
     # iterate all files (same way we fill structure)
     for source_file in iterate_cms_files(customization_name):
-        process_file(source_file, customization, product_id)
+        process_file(source_file, customization, product_id, preview)
 
 
 # from cms.controllers.filldata import *
--- cloud_portal/front_end/app/scripts/bootstrap.js
+++ cloud_portal/front_end/app/scripts/bootstrap.js
@@ -13,6 +13,12 @@ $.ajax({
     success: function (response) {
         L = response;// Fill global L variable
         Config.viewsDir = 'static/lang_' + L.language + '/views/', //'static/lang_' + lang + '/views/';
+
+        // detect preview mode
+        var preview = window.location.href.indexOf('preview')>=0;
+        if(preview){
+            Config.viewsDir = 'preview/' + Config.viewsDir;
+        }
         angular.bootstrap(document, ['cloudApp']);
     },
     error:function(error){
