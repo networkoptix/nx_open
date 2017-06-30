9db5fb5bb0af1de5333a7312a3e187aec903aa88
Sergey Ivanov <sivanov@networkoptix.com>
2017-06-30 15:52:46 +0300

    # A bit of simplification
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- common/src/common/common_globals.h
+++ common/src/common/common_globals.h
@@ -243,8 +243,8 @@ QN_DECLARE_METAOBJECT_HEADER(Qn,
         local_live_cam = live_cam | local | network,
         server_live_cam = live_cam | remote,// | network,
         server_archive = remote | media | video | audio | streamprovider,
-        local_video = url | local | media | video | audio | streamprovider,     /**< Local media file. */
-        local_image = url | local | media | still_image | streamprovider,    /**< Local still image file. */
+        local_video = local_media | video | audio | streamprovider,     /**< Local media file. */
+        local_image = local_media | still_image | streamprovider,    /**< Local still image file. */
 
         web_page = url | remote,   /**< Web-page resource */
         fake_server = remote_server | fake
