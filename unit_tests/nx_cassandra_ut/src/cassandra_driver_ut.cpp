4f64ce8887d638fff1475d5b1fc04e7913e568bb
Alexander Kulikov <akulikov@networkoptix.com>
2017-06-30 17:43:51 +0300

    cloud-1222. In progress.
    <==== Mercurial data ====>
    Branch: x_cloud_1222_cassandra_driver

--- unit_tests/nx_cassandra_ut/src/cassandra_driver_ut.cpp
+++ unit_tests/nx_cassandra_ut/src/cassandra_driver_ut.cpp
@@ -1,6 +1,68 @@
 #include <gtest/gtest.h>
 #include <nx/casssandra/cassandra_connection.h>
 
-TEST(dummy, dummy)
+namespace nx {
+namespace cassandra {
+namespace test {
+
+using namespace nx::utils;
+
+class Connection: public ::testing::Test
 {
+protected:
+    Connection(): m_connection("127.0.0.1") {}
+
+    void whenConnectionIsEstablished()
+    {
+        m_connection.initAsync([this](CassError result) { m_connectCb(result); });
+    }
+
+    void givenQueringChainOfCreateInsertRequestsSetAsConnectCb()
+    {
+        m_connectCb =
+            [this](CassError result)
+            {
+                ASSERT_EQ(CASS_OK, result);
+                createKeySpace();
+            };
+
+        m_createKeySpaceCb =
+            [this](CassError result)
+            {
+                ASSERT_EQ(CASS_OK, result);
+                createTable();
+            };
+    }
+
+    void givenSelectRequestSetAsInsertCb()
+    {
+
+    }
+
+    void thenInsertedDataShouldBeEqualToSelected()
+    {
+
+    }
+
+private:
+    nx::cassandra::Connection m_connection;
+    MoveOnlyFunc<void(CassError)> m_connectCb;
+    MoveOnlyFunc<void(CassError)> m_createKeySpaceCb;
+
+    void createKeySpace() {}
+    void createTable() {}
+
+};
+
+TEST_F(Connection, InsertSelect_Async)
+{
+    givenQueringChainOfCreateInsertRequestsSetAsConnectCb();
+    givenSelectRequestSetAsInsertCb();
+    whenConnectionIsEstablished();
+
+    thenInsertedDataShouldBeEqualToSelected();
 }
+
+} // namespace test
+} // namespace cassandra
+} // namespace nx
