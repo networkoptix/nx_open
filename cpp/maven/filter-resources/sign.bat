e3dbdcc2aa46c83c459fa1a6bb766593d45fa4c1
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-06-30 13:35:05 -0700

    Backed out changeset: 8b4ddda2722e
    <==== Mercurial data ====>
    Branch: vms_3.0.2

--- cpp/maven/filter-resources/sign.bat
+++ cpp/maven/filter-resources/sign.bat
@@ -6,21 +6,14 @@ SET TIMESTAMP=
 IF NOT ["${beta}"] == ["true"] (
     SET TIMESTAMP=/tr http://tsa.startssl.com/rfc3161
 )
-ELSE (
-    SET TIMESTAMP=/tr http://timestamp.digicert.com
-)
 
 IF NOT ["${windows.skip.sign}"] == ["true"] (
-    IF ["${beta}"] == ["false"] IF ["${installer.token}"] == ["true"] (
-        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /d "${company.name} ${display.product.name}" %*
-        exit 0
-    )
     IF NOT [%PASSWORD%] == [] (
         IF NOT [%CERTIFICATE%] == [] (
             set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
         ) ELSE (
             set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
-        )
+        )    
     ) ELSE set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
 )
 echo signing %*
