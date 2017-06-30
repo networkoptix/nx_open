f0e54fd4c80b9d41b5db6da3bbbce7ed01d84186
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-06-30 13:04:32 -0700

    Class 4 Certificate (USB Token) support for Jenkins
    <==== Mercurial data ====>
    Branch: vms_3.0.2

--- wixsetup/maven/filter-resources/sign.bat
+++ wixsetup/maven/filter-resources/sign.bat
@@ -2,10 +2,15 @@
 SET PASSWORD=${sign.password}
 SET CERTIFICATE=${sign.intermediate.cer}
 
+IF ["${beta}"] == ["false"] IF ["${installer.token}"] == ["true"] (
+    call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 http://timestamp.digicert.com /d "${company.name} ${display.product.name}" %*
+    exit 0
+)
+
 IF NOT [%PASSWORD%] == [] (
     IF NOT [%CERTIFICATE%] == [] (
         call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
     ) ELSE (
         call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
-    )    
+    )
 ) ELSE call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
