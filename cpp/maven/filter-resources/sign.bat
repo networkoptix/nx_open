c0aa6630c12f8283574f66d5708083ebc2f11946
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-06-30 13:21:55 -0700

    Class 4 Certificate (USB Token) support for Jenkins - fixes
    <==== Mercurial data ====>
    Branch: vms_3.0.2

--- cpp/maven/filter-resources/sign.bat
+++ cpp/maven/filter-resources/sign.bat
@@ -5,8 +5,7 @@ SET SIGNCER=${static.customization.dir}/wixsetup/${sign.cer}
 SET TIMESTAMP=
 IF NOT ["${beta}"] == ["true"] (
     SET TIMESTAMP=/tr http://tsa.startssl.com/rfc3161
-)
-ELSE (
+) ELSE (
     SET TIMESTAMP=/tr http://timestamp.digicert.com
 )
 
