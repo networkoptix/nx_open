279ca3d5ce22d7e8da3f3b851a263cee4af52622
Alexander Kulikov <akulikov@networkoptix.com>
2017-06-30 15:06:27 +0300

    cloud-1222. In progress.
    <==== Mercurial data ====>
    Branch: x_cloud_1222_cassandra_driver

---
+++ unit_tests/nx_cassandra_ut/src/cassandra_driver_ut.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <nx/casssandra/cassandra_connection.h>
+
+TEST(dummy, dummy)
+{
+}
---
+++ unit_tests/nx_cassandra_ut/src/main.cpp
@@ -0,0 +1,8 @@
+#define USE_GMOCK
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
