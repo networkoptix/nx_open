c81b8d514e0f5ffd1d2613e5c918adf5991a8e58
Sergey Ivanov <sivanov@networkoptix.com>
2017-07-03 17:37:52 +0300

    VMS-5363: Client may hang if second Client window is opened
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/client/client_module.cpp
+++ client/nx_client_desktop/src/client/client_module.cpp
@@ -455,15 +455,23 @@ void QnClientModule::initLog(const QnStartupParameters& startupParams)
     logSettings.maxBackupCount = 5;
 
     nx::utils::log::initialize(
-        logSettings, dataLocation, qApp->applicationName(), qApp->applicationFilePath());
+        logSettings,
+        dataLocation,
+        qApp->applicationName(),
+        qApp->applicationFilePath(),
+        lit("log_file") + logFileNameSuffix);
 
     const auto ec2logger = nx::utils::log::addLogger({QnLog::EC2_TRAN_LOG});
     if (ec2TranLogLevel != lit("none"))
     {
         logSettings.level = nx::utils::log::levelFromString(ec2TranLogLevel);
         nx::utils::log::initialize(
-            logSettings, dataLocation, qApp->applicationName(), qApp->applicationFilePath(),
-            QLatin1String("ec2_tran"), ec2logger);
+            logSettings,
+            dataLocation,
+            qApp->applicationName(),
+            qApp->applicationFilePath(),
+            lit("ec2_tran") + logFileNameSuffix,
+            ec2logger);
     }
 
     defaultMsgHandler = qInstallMessageHandler(myMsgHandler);
