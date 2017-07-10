6213b5c224301227e6eb161e2fd516179cbb8187
Evgeny Balashov <ebalashov@networkoptix.com>
2017-07-10 18:16:25 +0300

    CLOUD-1180: User is not logged in if Cloud Portal was opened from thick Client
    trying to solve auth problem
    
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- cloud_portal/cloud/cloud/settings.py
+++ cloud_portal/cloud/cloud/settings.py
@@ -194,6 +194,10 @@ STATIC_ROOT = '/app/app/static/common/static'
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
        'rest_framework.permissions.AllowAny',
     )
