6afabe21c7716e88d9a01f0990575a2ad398e5d2
Evgeny Balashov <ebalashov@networkoptix.com>
2017-07-13 19:00:25 +0300

    CLOUD-1200: Permissions model for CMS
    add Django Admin View Permission
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1200_permissions

--- cloud_portal/cloud/cloud/settings.py
+++ cloud_portal/cloud/cloud/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
+    'admin_view_permission',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
