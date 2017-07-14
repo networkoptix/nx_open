b56815a0bcc40bbe9c58595908c4d64dbc330f3c
Alexander Kulikov <akulikov@networkoptix.com>
2017-07-14 13:17:03 +0300

    cloud-1229. Cf update.
    <==== Mercurial data ====>
    Branch: x_cloud_1222_cassandra_driver

--- common_libs/nx_utils/src/nx/utils/thread/cf/time_watcher.h
+++ common_libs/nx_utils/src/nx/utils/thread/cf/time_watcher.h
@@ -24,8 +24,38 @@ class time_watcher {
   }
 
 public:
-  time_watcher();
-  ~time_watcher();
+  time_watcher()
+    : wakeup_time_(year_forward()) {
+    watcher_thread_ = std::thread([this] {
+      while (!need_stop_) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        if (!record_set_.empty())
+          wakeup_time_ = record_set_.begin()->time;
+        while (!need_stop_ && !time_has_come()) {
+          cond_.wait_until(lock, wakeup_time_);
+          if (!time_has_come() && !record_set_.empty()) {
+            wakeup_time_ = record_set_.begin()->time;
+          }
+        }
+        if (need_stop_)
+          return;
+        while (time_has_come()) {
+          record_set_.begin()->task();
+          record_set_.erase(record_set_.begin());
+        }
+        wakeup_time_ = record_set_.empty() ?
+          year_forward() :
+          record_set_.begin()->time;
+      }
+    });
+  }
+
+  ~time_watcher() {
+    need_stop_ = true;
+    cond_.notify_one();
+    if (watcher_thread_.joinable())
+      watcher_thread_.join();
+  }
 
   void add(const detail::task_type& task, std::chrono::milliseconds timeout) {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -34,8 +64,15 @@ public:
   }
 
 private:
-  bool time_has_come() const;
-  std::chrono::time_point<std::chrono::steady_clock> year_forward();
+  bool time_has_come() const {
+    if (record_set_.empty())
+      return false;
+    return record_set_.cbegin()->time <= std::chrono::steady_clock::now();
+  }
+
+  std::chrono::time_point<std::chrono::steady_clock> year_forward() {
+    return std::chrono::steady_clock::now() + std::chrono::hours(365 * 24);
+  }
 
 private:
   std::condition_variable cond_;
