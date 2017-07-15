3cb9fa89bb781280e68fab58bde07756a82899c3
Ivan Vigasin <ivigasin@networkoptix.com>
2017-07-14 20:50:01 -0700

    Update update_wheels.sh. Set pillow version.
    <==== Mercurial data ====>
    Branch: master

--- nx_cloud_deploy/cloud_portal/update_wheels.sh
+++ nx_cloud_deploy/cloud_portal/update_wheels.sh
@@ -1,1 +1,6 @@
-pip wheel -w wheelhouse -r ~/workspace/cloud_portal/cloud/requirements.txt
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run --rm -i 009544449203.dkr.ecr.us-east-1.amazonaws.com/devtools/wheel_uploader:1.0-py2 < $DIR/../../cloud_portal/cloud/requirements.txt
