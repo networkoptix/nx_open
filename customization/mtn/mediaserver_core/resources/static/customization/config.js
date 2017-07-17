9d6dd92640482ff5328f6834fc3b843369ad5326
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-07-17 12:41:14 -0700

    ionetworks-mtn customization
    <==== Mercurial data ====>
    Branch: vms_3.0

---
+++ customization/mtn/mediaserver_core/resources/static/customization/config.js
@@ -0,0 +1,12 @@
+Config.helpLinks.push({
+    url: "https://play.google.com/store/apps/details?id=com.ioezio.ezpromobile",
+    title: "Android Client",
+    target: "new" // new|frame
+});
+
+Config.allowDebugMode = false;
+//Config.webclientEnabled = false; // Uncomment this string to disable web client
+Config.productName = 'EZ Pro';
+Config.cloud.productName = 'EZ Pro Cloud';
+
+Config.supportedLanguages = ['en_US', 'en_GB', 'fr', 'de', 'ru', 'es', 'ja', 'ko', 'tr', 'zh_CN', 'zh_TW', 'hu', 'he'];
