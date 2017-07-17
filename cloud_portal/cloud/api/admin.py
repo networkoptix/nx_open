958f599612dcb429a4241b28b9c577418338b972
Evgeny Balashov <ebalashov@networkoptix.com>
2017-07-17 13:12:20 +0300

    CLOUD-1200: Permissions model for CMS
    make models readonly for not superusers
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1200_permissions

--- cloud_portal/cloud/api/admin.py
+++ cloud_portal/cloud/api/admin.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from models import *
 # Register your models here.
 from cloud import settings
+from cloud.cms.admin import CMSAdmin
 
 
-class AccountAdmin(admin.ModelAdmin):
+class AccountAdmin(CMSAdmin):
     list_display = ('email', 'first_name', 'last_name', 'created_date', 'activated_date', 'last_login',
                     'subscribe', 'is_staff', 'is_superuser', 'language')
-
-    def get_queryset(self, request):
-        qs = super(AccountAdmin, self).get_queryset(request)
-        return qs.filter(customization=settings.CUSTOMIZATION)
-    	
 
 admin.site.register(Account, AccountAdmin)
