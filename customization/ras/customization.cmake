7c8004c354b23772a964cd0eff61bf6a34a485a7
Sergey Ivanov <sivanov@networkoptix.com>
2017-07-24 13:49:49 +0300

    RAS customization
    <==== Mercurial data ====>
    Branch: mobile_17.2

---
+++ customization/ras/customization.cmake
@@ -0,0 +1,73 @@
+[basic]
+set(defaultSkin "dark_blue")
+set(parent.customization "default")
+
+# Components Namings
+set(display.product.name "Sefica ProBox")
+set(display.mobile.name "Sefica ProBox Mobile")
+set(product.name "Sefica ProBox")
+set(product.name.short "ras")
+set(product.appName "probox")
+set(company.name "RAS SECURITY")
+set(short.company.name "RAS")
+set(uri.protocol "ras-vms")
+set(cloudName "Pbx Cloud")
+
+# Default settings
+set(defaultWebPages "{\"Home Page\": \"https://www.rassecurity.com\"}")
+
+# Support section
+set(companyUrl "https://www.rassecurity.com")
+set(productUrl "https://www.rassecurity.com/nl/producten/camerabewaking/recorders/product/pbx-09-lnx-2tb")
+set(supportUrl "")
+set(supportEmail "ras.support@rassecurity.com")
+set(supportPhone "")
+set(licenseEmail "ras.contact@rassecurity.com")
+
+set(installer.name "probox")
+set(android.packageName "com.rassecurity.probox")
+set(android.oldPackageName "")
+set(android.alias "probox")
+set(android.storepass "5B4M2Gn5")
+set(android.keypass "S6rtgPMk")
+
+# Custom Installer (if needed). If not leave "default"
+set(customization.upgradeCode "{E10C4B91-CC3A-442F-A017-266BB23A44E4}")
+set(customization.clientUpgradeCode "{6E9B3B5B-0AC9-4718-A1B1-EA59F706A72F}")
+set(customization.serverUpgradeCode "{B85E5E85-963A-477A-9D04-F122576E2364}")
+
+set(customization.clientBundleUpgradeCode "{4A83450D-1F57-4314-9638-6C66AB1C8E06}")
+set(customization.serverBundleUpgradeCode "{76AEB816-5F97-4C06-96E4-2D5603F29111}")
+set(customization.fullBundleUpgradeCode "{164BDBC7-B4AE-463D-9D9E-D2731D5E0E54}")
+set(#customization.nxtoolBundleUpgradeCode "")
+set(#customization.axBundleUpgradeCode "")
+
+set(deb.customization.company.name "ras")
+
+set(ios.skip.sign "false")
+set(ios.bundle_identifier "com.rassecurity.probox")
+set(ios.new_bundle_identifier "com.rassecurity.proboxmobile")
+set(ios.group_identifier "group.com.rassecurity.probox")
+set(ios.sign.identity "iPhone Distribution: RAS Bvba (CJ7G58V6NE)")
+set(ios.old_app_appstore_id "")
+
+set(mac.skip.sign "false")
+set(mac.sign.identity "Developer ID Application: RAS Bvba (CJ7G58V6NE)")
+set(mac.app.sign.identity "3rd Party Mac Developer Application: RAS Bvba (CJ7G58V6NE)")
+set(mac.pkg.sign.identity "3rd Party Mac Developer Installer: RAS Bvba (CJ7G58V6NE)")
+set(mac.bundle.identifier "com.rassecurity.probox2")
+set(mac.protocol_handler_bundle.identifier "com.probox.protocol_handler")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0027")
+
+# Installer Customizations
+set(sign.password "qweasd123")
+set(sign.cer "app.p12")
+set(sign.intermediate.cer "")
+
+# Localization
+set(defaultTranslation en_US)
+set(additionalTranslations en_GB fr_FR de_DE ru_RU es_ES ja_JP ko_KR tr_TR zh_CN zh_TW he_IL hu_HU)
