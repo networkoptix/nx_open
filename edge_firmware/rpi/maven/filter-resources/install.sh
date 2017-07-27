dac9dd00035bf68d1fb404e395c5bd10d41f8932
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-07-27 20:22:25 +0300

    VMS-6774: Nx1 3.0+ update fails in the field
    <==== Mercurial data ====>
    Branch: vms_3.0.2

--- edge_firmware/rpi/maven/filter-resources/install.sh
+++ edge_firmware/rpi/maven/filter-resources/install.sh
@@ -1,119 +1,164 @@
 #!/bin/bash
-set -x
+set -x #< Log each command.
 
-export LOG=/var/log/bpi-upgrade.log
+# Redirect all output of this script to the log file.
+LOG_FILE="/var/log/bpi-upgrade.log"
 exec 1<&- #< Close stdout fd.
 exec 2<&- #< Close stderr fd.
-exec 1<>$LOG #< Open stdout as $LOG file for reading and writing.
+exec 1<>"$LOG_FILE" #< Open stdout as $LOG_FILE for reading and writing.
 exec 2>&1 #< Redirect stderr to stdout.
 
-echo "Starting upgrade ..."
-COMPANY_NAME="${deb.customization.company.name}"
-MEDIASERVER_DIR="/opt/$COMPANY_NAME/mediaserver"
-export DISTRIB="${artifact.name.server}"
+FAILED_FLAG="/var/log/bpi-upgrade-failed.flag"
+CUSTOMIZATION="${deb.customization.company.name}"
+INSTALL_PATH="opt/$CUSTOMIZATION"
+MEDIASERVER_PATH="$INSTALL_PATH/mediaserver"
+DISTRIB="${artifact.name.server}"
+STARTUP_SCRIPT="/etc/init.d/$CUSTOMIZATION-mediaserver"
+TAR_FILE="./$DISTRIB.tar.gz"
 
-update()
+copyFilesToBootPartition()
 {
-    cp "$DISTRIB.tar.gz" /tmp/
-    rm -rf "$MEDIASERVER_DIR/lib" "$MEDIASERVER_DIR"/bin/core*
-    mkdir -p "./$DISTRIB"
-    tar xfv "$DISTRIB.tar.gz" -C "./$DISTRIB"
-    cp -rf "./$DISTRIB"/* /
+    local DEV="/dev/mmcblk0p1"
+    local MNT="/mnt/boot"
+    mkdir -p "$MNT" || return $?
+    umount "$DEV" #< Unmount in case it was mounted; ignore errors.
+    mount -t vfat "$DEV" "$MNT" || return $?
+    cp -f "/root/tools/uboot"/* "$MNT" #< When cp omits dirs, it produces an error.
+    umount "$DEV" || return $?
+    rm -rf "$MNT" || return $?
+}
+
+copyFilesToDataPartition()
+{
+    local DEV="/dev/mmcblk0p2"
+    local MNT="/mnt/data"
+    mkdir -p "$MNT" || return $?
+    umount "$DEV" #< Unmount in case it was mounted; ignore errors.
+    mount -t ext4 "$DEV" "$MNT" || return $?
+    cp -f "/$MEDIASERVER_PATH/var/ecs_static.sqlite" "/root/tools/nx/" || return $?
+    rm -rf "$MNT/$INSTALL_PATH" || return $?
+
+    # Unpack the distro to sdcard.
+    tar xfv "$TAR_FILE" -C "$MNT/" || return $?
+
+    mkdir -p "$MNT/$MEDIASERVER_PATH/var" || return $?
+    cp -f "/root/tools/nx/ecs_static.sqlite" "$MNT/$MEDIASERVER_PATH/var/" || return $?
+    local CONF_FILE="$MNT/$MEDIASERVER_PATH/etc/mediaserver.conf"
+    local CONF_CONTENT="statisticsReportAllowed=true"
+    grep -q "$CONF_CONTENT" "$CONF_FILE" \
+        && { echo "$CONF_CONTENT" >>"$CONF_FILE" || return $?; }
+    umount "$DEV" || return $?
+}
+
+installDeb() # package version
+{
+    local PACKAGE="$1"
+    local VERSION="$2"
+
+    local PRESENT=$(dpkg -l |grep "$PACKAGE" |grep "$VERSION" |awk '{print $3}')
+    if [ -z "$PRESENT" ]; then
+        dpkg -i "/opt/deb/$PACKAGE"/*.deb
+    fi
+}
+
+upgradeVms()
+{
+    rm -rf "../$DISTRIB.zip" #< Already unzipped, so remove .zip to save space in "/tmp".
+    rm -rf "/$MEDIASERVER_PATH/lib" "/$MEDIASERVER_PATH/bin"/core* #< Ignore errors.
+    tar xfv "$TAR_FILE" -C / || return $? #< Extract the distro to the root.
 
     if [[ "${box}" == "bpi" ]]; then
         # Avoid grabbing libstdc++ from mediaserver lib folder.
-        export LD_LIBRARY_PATH=
+        export LD_LIBRARY_PATH=""
 
-        export DATAPART=/dev/mmcblk0p2
-        export MNTDATAPART=/mnt/data
-        export BOOTPART=/dev/mmcblk0p1
-        export MNTBOOTPART=/mnt/boot
+        copyFilesToBootPartition || return $?
 
-        mkdir -p $MNTBOOTPART
-        mount -t vfat $BOOTPART $MNTBOOTPART
-        cp -f /root/tools/uboot/* $MNTBOOTPART
-        umount $BOOTPART
-        rm -rf $MNTBOOTPART
+        installDeb libvdpau 0.4.1 || return $?
+        installDeb fontconfig 2.11 || return $?
+        installDeb fonts-takao-mincho "" || return $?
 
-        LIBVDPAU=$(dpkg -l |grep libvdpau |grep 0.4.1 |awk '{print $3}')
-        [ -z "$LIBVDPAU" ] && dpkg -i /opt/deb/libvdpau/*.deb
+        touch "/dev/cedar_dev" || return $?
+        chmod 777 "/dev/disp" || return $?
+        chmod 777 "/dev/cedar_dev" || return $?
+        usermod -aG video root || return $?
 
-        FONTCONFIG=$(dpkg -l |grep fontconfig |grep 2.11 |awk '{print $3}')
-        [ -z "$FONTCONFIG" ] && dpkg -i /opt/deb/fontconfig/*.deb
+        copyFilesToDataPartition || return $?
 
-        FONTS=$(dpkg -l |grep fonts-takao-mincho |awk '{print $3}')
-        [ -z "$FONTS" ] && dpkg -i /opt/deb/fonts-takao-mincho/*.deb
-
-        touch /dev/cedar_dev
-        chmod 777 /dev/disp
-        chmod 777 /dev/cedar_dev
-        usermod -aG video root
-
-        mkdir -p $MNTDATAPART
-        mount -t ext4 $DATAPART $MNTDATAPART
-        cp -f /opt/$COMPANY_NAME/mediaserver/var/ecs_static.sqlite /root/tools/nx/ecs_static.sqlite
-        rm -rf $MNTDATAPART/opt/$COMPANY_NAME
-        cp -rf ./$DISTRIB/* $MNTDATAPART
-        mkdir -p $MNTDATAPART/opt/$COMPANY_NAME/mediaserver/var
-        cp -f /root/tools/nx/ecs_static.sqlite $MNTDATAPART/opt/$COMPANY_NAME/mediaserver/var
-        CONF_FILE="$MNTDATAPART/opt/$COMPANY_NAME/mediaserver/etc/mediaserver.conf"
-        grep -q "statisticsReportAllowed=true" $CONF_FILE \
-            && echo "statisticsReportAllowed=true" >> $CONF_FILE
-        umount $DATAPART
-        /etc/init.d/nx1boot upgrade
-        rm -rf "./$DISTRIB"
-        sync
+        /etc/init.d/nx1boot upgrade || return $?
     fi
-    # TODO: Add errorlevel handling.
-    rm "/tmp/$DISTRIB.tar.gz"
 }
 
-get_port_pid() # port
+getPidWhichUsesPort() # port
 {
     local PORT="$1"
     netstat -tpan |grep "$PORT" |grep 'LISTEN' |awk '{print $NF}' |grep -o '[0-9]\+'
 }
 
-# Output nothing if not found.
-get_another_server_pid() # port_pid
+# Output nothing if the specified pid does not belong to a mediaserver; otherwise, output the pid.
+getMediaserverPid() # pid
 {
-    local PORT_PID="$1"
-    local RESULT=$(ps "$PORT_PID" |grep "/opt/$COMPANY_NAME/mediaserver")
-    # $RESULT may contain spaces or \n, in this case we need to convert it to an empty string.
-    echo "${RESULT// }"
+    local PID="$1"
+
+    if [ ! -z $(ps "$PID" |grep "/$MEDIASERVER_PATH") ]; then
+        echo "$PID"
+    fi
 }
 
-/etc/init.d/cron stop
-/etc/init.d/$COMPANY_NAME-mediaserver stop
-update
-if [ "${box}" = "bpi" ]; then
-    reboot
-    exit 0
-fi
+upgradeVmsAndRestart()
+{
+    echo "Starting VMS upgrade..."
 
-/etc/init.d/$COMPANY_NAME-mediaserver start
+    "$STARTUP_SCRIPT" stop #< Ignore errors - if not stopped, try upgrading as is.
 
-# TODO: #mshevchenko: Gone in 3.1.
-/etc/init.d/cron start
+    upgradeVms
+    local RESULT=$?
+    if [ $RESULT != 0 ]; then
+        echo "VMS upgrade failed with exit status $RESULT"
+        return $RESULT;
+    fi
+    echo "VMS upgrade succeeded"
 
-sleep 3
+    sync
 
-# Determining Server port
-SERVER_PORT=$(cat /opt/$COMPANY_NAME/mediaserver/etc/mediaserver.conf \
-    |grep port= |grep -o '[0-9]\+')
+    if [ "${box}" = "bpi" ]; then
+        reboot
+        exit 0
+    fi
 
-# Determining the process that occupies Server port.
-PORT_PID=$(get_port_process "$SERVER_PORT")
+    "$STARTUP_SCRIPT" start #< Ignore errors - if not started, try to restart below.
 
-# Checking if the process belongs to our server. Netstat always shows process name like
-# "xxxxx/mediaserver", but it may be the mediaserver of a different customization.
-SERVER_PID=$(get_another_server_pid "$PORT_PID")
-while [ -z "$SERVER_PID" ]; do
-    echo "Restarting $COMPANY_NAME-mediaserver" >>"/opt/$COMPANY_NAME/mediaserver/var/log/update.log"
-    /etc/init.d/$COMPANY_NAME-mediaserver stop
-    kill -9 "$PORT_PID"
-    /etc/init.d/$COMPANY_NAME-mediaserver start
     sleep 3
-    PORT_PID=$(get_port_process "$SERVER_PORT")
-    SERVER_PID=$(get_another_server_pid "$PORT_PID")
-done
+
+    local SERVER_PORT=$(cat "/$MEDIASERVER_PATH/etc/mediaserver.conf" \
+        |grep 'port=' |grep -o '[0-9]\+')
+
+    # If the server cannot start because another process uses its port, kill it and restart.
+    while true; do
+        local PID_WHICH_USES_PORT=$(getPidWhichUsesPort "$SERVER_PORT")
+        local SERVER_PID=$(getMediaserverPid "$PID_WHICH_USES_PORT")
+        if [ ! -z "$SERVER_PID" ]; then
+            echo "Upgraded mediaserver is up and running with pid $SERVER_PID at port $SERVER_PORT"
+            break
+        fi
+        echo "Another process (pid $PID_WHICH_USES_PORT) uses port $SERVER_PORT:" \
+            "killing it and restarting $CUSTOMIZATION-mediaserver"
+        "$STARTUP_SCRIPT" stop #< Just in case - the server should not be running at this time.
+        kill -9 "$PID_WHICH_USES_PORT"
+        "$STARTUP_SCRIPT" start #< Ignore errors - if not started, the loop will try again.
+        sleep 3
+    done
+}
+
+main()
+{
+    rm -rf "$FAILED_FLAG"
+
+    upgradeVmsAndRestart
+    local RESULT=$?
+    if [ $RESULT != 0 ]; then
+        touch "$FAILED_FLAG"
+    fi
+    return $RESULT
+}
+
+main "$@"
