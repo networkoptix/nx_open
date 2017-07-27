de537fb10df35a691bb5816b6163907b99c7f8e2
Sergey Ivanov <sivanov@networkoptix.com>
2017-07-27 17:42:25 +0300

    VMS-6772: Add Ctrl+Enter hotkey for "Open containing folder"
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/nx/client/desktop/ui/actions/actions.cpp
+++ client/nx_client_desktop/src/nx/client/desktop/ui/actions/actions.cpp
@@ -814,6 +814,8 @@ void initialize(Manager* manager, Action* root)
     factory(OpenInFolderAction)
         .flags(Scene | Tree | SingleTarget | ResourceTarget | LayoutItemTarget)
         .text(ContextMenu::tr("Open Containing Folder"))
+        .shortcut(lit("Ctrl+Enter"))
+        .shortcut(lit("Ctrl+Return"))
         .autoRepeat(false)
         .condition(new OpenInFolderCondition());
 
