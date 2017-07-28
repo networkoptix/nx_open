9c8b4623da4890dd0fd45cc99a43cf65d923ec73
Evgeny Balashov <ebalashov@networkoptix.com>
2017-07-28 15:04:27 -0700

    CLOUD-1282: Rename "django administration" to something relevant
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- cloud_portal/cloud/cms/admin.py
+++ cloud_portal/cloud/cms/admin.py
@@ -5,6 +5,21 @@ from django.utils.html import format_html
 from models import *
 from cloud import settings
 
+from django.contrib.admin import AdminSite
+
+
+class CloudAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = 'Cloud Portal admin'
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = 'Cloud Portal administration'
+
+    # Text to put at the top of the admin index page.
+    index_title = 'Cloud Portal administration'
+
+admin_site = CloudAdminSite()
+
 
 class CMSAdmin(admin.ModelAdmin):
     # this class protects us from user error:
