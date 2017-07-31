a9dcf85ea62eb0dd63a849acd822f0221cce4f28
Sergey Ivanov <sivanov@networkoptix.com>
2017-07-31 16:54:08 +0300

    VMS-6824: Custom text on update notification
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/update/update_info.cpp
+++ client/nx_client_desktop/src/update/update_info.cpp
@@ -2,13 +2,4 @@
 
 #include <nx/fusion/model_functions.h>
 
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnUpdateInfo, (datastream), (currentRelease)(releaseNotesUrl)(releaseDateMs)(releaseDeliveryDays))
-
-QnUpdateInfo::QnUpdateInfo()
-  : currentRelease()
-  , releaseNotesUrl()
-  , releaseDateMs(0)
-  , releaseDeliveryDays(0)
-{
-
-}
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(QnUpdateInfo, (datastream), QnUpdateInfo_Fields)
