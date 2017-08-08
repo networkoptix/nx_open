9f2439a635e617b6410374c6c69ebb1bbd0bcc1e
Alexander Kulikov <akulikov@networkoptix.com>
2017-08-08 17:25:07 +0300

    cloud-1313. Redirect test.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- nx_cloud/tests/cloud_connect_integration_tests/src/relay_to_relay_redirect.cpp
+++ nx_cloud/tests/cloud_connect_integration_tests/src/relay_to_relay_redirect.cpp
@@ -1,11 +1,46 @@
 #include <gtest/gtest.h>
 
 #include "basic_test_fixture.h"
+#include "model/abstract_remote_relay_peer_pool.h"
+#include <controller/connect_session_manager.h>
+#include <controller/../settings.h>
 
 namespace nx {
 namespace network {
 namespace cloud {
 namespace test {
+
+using namespace nx::cloud::relay;
+
+class MemoryRemoteRelayPeerPool: public model::AbstractRemoteRelayPeerPool
+{
+public:
+    MemoryRemoteRelayPeerPool(const std::string& redirectToRelay):
+        m_redirectToRelay(redirectToRelay)
+    {}
+
+    virtual cf::future<std::string> findRelayByDomain(const std::string& domainName) const override
+    {
+        auto redirectToRelayCopy = m_redirectToRelay;
+        qWarning() << "findRelayByDomain: returning" << QString::fromStdString(redirectToRelayCopy);
+        return cf::make_ready_future<std::string>(std::move(redirectToRelayCopy));
+    }
+
+    virtual cf::future<bool> addPeer(
+        const std::string& domainName,
+        const std::string& relayHost) override
+    {
+        return cf::make_ready_future(true);
+    }
+
+    virtual cf::future<bool> removePeer(const std::string& domainName) override
+    {
+        return cf::make_ready_future(false);
+    }
+
+private:
+    std::string m_redirectToRelay;
+};
 
 class RelayToRelayRedirect: public BasicTestFixture
 {
@@ -16,11 +51,31 @@ public:
 
     virtual void SetUp() override
     {
+        setUpConnectSessionManagerFactoryFunc();
         BasicTestFixture::SetUp();
-
         ConnectorFactory::setEnabledCloudConnectMask((int)CloudConnectType::proxy);
-
         startServer();
+    }
+
+    void setUpConnectSessionManagerFactoryFunc()
+    {
+        controller::ConnectSessionManagerFactory::setFactoryFunc(
+            [](
+                const conf::Settings& settings,
+                model::ClientSessionPool* clientSessionPool,
+                model::ListeningPeerPool* listeningPeerPool,
+                controller::AbstractTrafficRelay* trafficRelay)
+        {
+            static int relayPort = 20000;
+
+            return std::make_unique<controller::ConnectSessionManager>(
+                settings,
+                clientSessionPool,
+                listeningPeerPool,
+                trafficRelay,
+                std::make_unique<MemoryRemoteRelayPeerPool>("127.0.0.1:20000"),
+                std::move("127.0.0.1:" + std::to_string(relayPort++)));
+        });
     }
 
     void assertServerIsAccessibleFromAnotherRelay()
