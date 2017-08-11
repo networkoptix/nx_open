80e241396c1f75e726708c6d64b9a7baaa32fbdf
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2017-08-11 11:12:44 +0300

    VMS-6097 - TImeline on mac moves without animation: global fix
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/ui/workbench/workbench_navigator.cpp
+++ client/nx_client_desktop/src/ui/workbench/workbench_navigator.cpp
@@ -1531,7 +1531,12 @@ void QnWorkbenchNavigator::updateSliderFromReader(UpdateSliderMode mode)
             if (m_previousMediaPosition != timeMSec || isTimelineCatchingUp())
             {
                 qint64 delta = timeMSec - m_animatedPosition;
-                if (qAbs(delta) < m_timeSlider->msecsPerPixel() || delta * speed() < 0)
+
+                const bool isLocalFileSource =
+                    m_currentWidget && m_currentWidget->resource()->flags().testFlag(Qn::local);
+
+                if (qAbs(delta) < m_timeSlider->msecsPerPixel() ||
+                    (isLocalFileSource && delta * speed() < 0))
                 {
                     /* If distance is less than 1 pixel or we catch up backwards, do it instantly: */
                     m_animatedPosition = timeMSec;
