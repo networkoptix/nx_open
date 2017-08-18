fb8c5e2948f45990fbe693d391cd95cb912164f9
Vitaly Kutin <vkutin@networkoptix.com>
2017-08-18 20:28:14 +0300

    # Working on overlays
    <==== Mercurial data ====>
    Branch: x_VMS-6861_universal_export_dialog

--- client/nx_client_desktop/src/nx/client/desktop/export/widgets/overlay_label_widget.h
+++ client/nx_client_desktop/src/nx/client/desktop/export/widgets/overlay_label_widget.h
@@ -16,8 +16,37 @@ class OverlayLabelWidget: public QWidget
 public:
     OverlayLabelWidget(QWidget* parent = nullptr);
 
+    QString text() const;
+    void setText(const QString& value);
+
+    bool wordWrap() const;
+    void setWordWrap(bool value);
+
+    QPixmap pixmap() const;
+    void setPixmap(const QPixmap& value);
+
+    bool borderVisible() const;
+    void setBorderVisible(bool value);
+
+    qreal roundingRadius() const;
+    void setRoundingRadius(qreal value);
+
+protected:
+    virtual void paintEvent(QPaintEvent* event) override;
+    virtual void mouseMoveEvent(QMouseEvent* event) override;
+    virtual void mousePressEvent(QMouseEvent* event) override;
+    virtual void mouseReleaseEvent(QMouseEvent* event) override;
+    virtual bool eventFilter(QObject* watched, QEvent* event) override;
+    virtual bool event(QEvent* event) override;
+
+private:
+    void updateLayout();
+    void updateCursor();
+    void updatePosition(const QPoint& pos);
+
 private:
     struct Private;
+    class FrameWidget;
     QScopedPointer<Private> d;
 };
 
