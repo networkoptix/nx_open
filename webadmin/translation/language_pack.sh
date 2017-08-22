b8b218c59e40878bcac72de56b0d1b7973056646
Evgeny Balashov <ebalashov@networkoptix.com>
2017-08-21 18:31:01 -0700

    WEB-679: Deal with translations for web_common/commonLanguage.json
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- webadmin/translation/language_pack.sh
+++ webadmin/translation/language_pack.sh
@@ -6,18 +6,15 @@ CROWDIN_OPERATION=$1
 DIRECTORY=language_pack
 
 rm -rf $DIRECTORY || true
-mkdir $DIRECTORY
 
 echo "Copy views"
-mkdir $DIRECTORY/views
+mkdir -p $DIRECTORY/views
 cp -rf ../app/views/*  $DIRECTORY/views
 
 echo "Copy language.json"
 cp -rf ../app/language.json  $DIRECTORY/
 
-
 echo "Copy web_common"
-mkdir $DIRECTORY/web_common
-mkdir $DIRECTORY/web_common/views
+mkdir -p $DIRECTORY/web_common/views
 cp -rf ../app/web_common/views/*  $DIRECTORY/web_common/views || true
 cp -rf ../app/web_common/commonLanguage.json  $DIRECTORY/web_common || true
