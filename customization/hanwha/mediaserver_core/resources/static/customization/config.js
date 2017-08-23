4e6c6527b6550bfa30ca3fcfaf9eb2363daa57d9
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-08-23 13:45:25 -0700

    hanwha customization. No graphics so far
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

---
+++ customization/hanwha/mediaserver_core/resources/static/customization/config.js
@@ -0,0 +1,41 @@
+Config.helpLinks.push({
+    url: "http://support.networkoptix.com",
+    title: "Support",
+    target: "new" // new|frame
+});
+
+Config.helpLinks.push({
+    url: "http://networkoptix.com/calculator/#/",
+    title: "System calculator",
+    target: "new" // new|frame
+});
+
+Config.helpLinks.push({
+    url: "https://itunes.apple.com/eg/app/hd-witness/id648369716?mt=8",
+    title: "iOS Client",
+    target: "new" // new|frame
+});
+
+Config.helpLinks.push({
+    urls: [
+        {
+            url: "https://itunes.apple.com/eg/app/hd-witness/id1050899754?mt=8",
+            button: "iOS Client",
+            class:'appstore'
+        },
+        {
+            url: "https://play.google.com/store/apps/details?id=com.networkoptix.nxwitness",
+            class:'googleplay',
+            button: "Android Client"
+        }
+    ],
+    title: "Mobile Apps",
+    target: "new" // new|frame
+});
+
+Config.allowDebugMode = false;
+//Config.webclientEnabled = false; // Uncomment this string to disable web client
+Config.productName = 'VMS Demo';
+Config.cloud.productName = 'VMS Demo Cloud';
+
+Config.supportedLanguages = ['en_US', 'en_GB', 'fr_FR', 'de_DE', 'ru_RU', 'es_ES', 'ja_JP', 'ko_KR', 'tr_TR', 'zh_CN', 'zh_TW', 'hu_HU', 'he_IL'];
