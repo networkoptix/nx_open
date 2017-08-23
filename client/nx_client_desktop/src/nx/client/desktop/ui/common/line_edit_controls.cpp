3bb5debe393f52c81758e40abae6e95ecddee26a
Sergey Ivanov <sivanov@networkoptix.com>
2017-08-23 15:38:28 +0300

    VMS-6876: Fix crash on user password change
    <==== Mercurial data ====>
    Branch: x_VMS-6876_crash_on_password_change

--- client/nx_client_desktop/src/nx/client/desktop/ui/common/line_edit_controls.cpp
+++ client/nx_client_desktop/src/nx/client/desktop/ui/common/line_edit_controls.cpp
@@ -9,6 +9,9 @@
 #include <ui/common/widget_anchor.h>
 #include <utils/common/event_processors.h>
 
+#include <nx/utils/disconnect_helper.h>
+#include <nx/utils/log/assert.h>
+
 namespace {
 
 static const QMargins kDefaultMargins(6, 1, 6, 2);
@@ -38,6 +41,11 @@ struct LineEditControls::Private
 
         installEventHandler(contents, QEvent::LayoutRequest, contents,
             [this]() { updateLayout(); });
+    }
+
+    ~Private()
+    {
+        disconnectHelper.clear();
     }
 
     void setEmpty()
@@ -196,16 +204,22 @@ struct LineEditControls::Private
             if (!control || controls.contains(control))
                 continue;
 
-            control->setParent(contents);
+            control->setParent(contents.data());
             controls << control;
 
             // Remove if deleted.
-            QObject::connect(control, &QWidget::destroyed,
-                [this](QObject* what) { removeControl(static_cast<QWidget*>(what), true); });
+            *disconnectHelper << QObject::connect(control, &QWidget::destroyed,
+                [this](QObject* what)
+                {
+                    removeControl(static_cast<QWidget*>(what), true);
+                });
 
             // Remove if reparented.
             installEventHandler(control, QEvent::ParentChange, contents,
-                [this](QObject* what) { removeControl(static_cast<QWidget*>(what), false); });
+                [this](QObject* what)
+                {
+                    removeControl(static_cast<QWidget*>(what), false);
+                });
         }
 
         if (oldCount != controls.size())
@@ -216,9 +230,11 @@ struct LineEditControls::Private
     QnWidgetAnchor* const anchor = nullptr;
     int spacing = kDefaultSpacing;
 
-    const QWidget* contents;
+    //QWidget* const contents;
+    const QScopedPointer<QWidget> contents;
     const QMargins initialMargins; //< Initial contents margins of line edit.
     QList<QWidget*> controls;
+    QnDisconnectHelperPtr disconnectHelper = QnDisconnectHelper::create();
 };
 
 // ------------------------------------------------------------------------------------------------
