f3781054185e736165f395f32422eda3dbf8d886
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-08-24 18:58:17 -0700

    Hanwha - calculator link changed
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- customization/hanwha/mediaserver_core/resources/static/customization/config.js
+++ customization/hanwha/mediaserver_core/resources/static/customization/config.js
@@ -5,7 +5,7 @@ Config.helpLinks.push({
 });
 
 Config.helpLinks.push({
-    url: "https://www.hanwhasecurity.com/calculator/",
+    url: "https://www.hanwhasecurity.com/wavecalculator/",
     title: "System calculator",
     target: "new" // new|frame
 });
