91680a617ee629c5a6ceaedb1e76cfa610271fa9
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-09-06 15:18:31 +0300

    Fixed lanuching Lite Client on Nx1 for cmake: liteMode is now on.
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- cmake/properties.cmake
+++ cmake/properties.cmake
@@ -24,7 +24,12 @@ endif()
 
 set(client.mediafolder.name "${product.name} Media")
 
-set(liteMode "false")
+if(targetDevice STREQUAL "bpi|bananapi|rpi|edge1")
+    set(liteMode "true")
+else()
+    set(liteMode "false")
+endif()
+
 set(launcher.version.file "launcher.version")
 set(installation.root "/opt/${deb.customization.company.name}")
 
