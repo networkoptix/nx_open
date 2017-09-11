40c9776870261761d4bab559793936bc57111e67
Sergey Ivanov <sivanov@networkoptix.com>
2017-09-11 15:30:44 +0300

    # Merge compile fix
    <==== Mercurial data ====>
    Branch: vms_3.1.2

--- client/nx_client_desktop/src/camera/cam_display.cpp
+++ client/nx_client_desktop/src/camera/cam_display.cpp
@@ -10,18 +10,17 @@
 #include <client/client_settings.h>
 #include <client/client_module.h>
 
+#include <analytics/metadata_analytics_controller.h>
 #include <nx/client/desktop/radass/radass_controller.h>
 
 #include "core/resource/camera_resource.h"
 #include "nx/streaming/media_data_packet.h"
 #include <nx/streaming/config.h>
 
-#include "decoders/audio/ffmpeg_audio_decoder.h"
 #include "nx/streaming/archive_stream_reader.h"
 
 #include "video_stream_display.h"
 #include "audio_stream_display.h"
-
 
 #if defined(Q_OS_MAC)
 #include <CoreServices/CoreServices.h>
