f90f529dcad25593bd6dddb5ccb551f53387e630
Vitaly Kutin <vkutin@networkoptix.com>
2017-09-11 15:30:37 +0300

    VMS-7078: Specific error message when user tries to login with not-activated cloud account
    <==== Mercurial data ====>
    Branch: vms_3.1_gui

--- client/nx_client_desktop/src/ui/dialogs/cloud/private/cloud_result_messages_p.h
+++ client/nx_client_desktop/src/ui/dialogs/cloud/private/cloud_result_messages_p.h
@@ -2,10 +2,6 @@
 
 #include <QtCore/QCoreApplication>
 
-#include <helpers/cloud_url_helper.h>
-#include <nx/network/app_info.h>
-#include <utils/common/html.h>
-
 class QnCloudResultMessages
 {
     Q_DECLARE_TR_FUNCTIONS(QnCloudResultMessages)
@@ -18,16 +14,6 @@ public:
 
     static QString accountNotActivated()
     {
-        using nx::vms::utils::SystemUri;
-        QnCloudUrlHelper urlHelper(
-            SystemUri::ReferralSource::DesktopClient,
-            SystemUri::ReferralContext::SettingsDialog);
-
-        const auto portalText = tr("%1 Portal", "%1 is the cloud name (like 'Nx Cloud')")
-            .arg(nx::network::AppInfo::cloudName()).replace(QChar(L' '), lit("&nbsp;"));
-
-        const auto portalLink = makeHref(portalText, urlHelper.mainUrl());
-        return tr("Account isn't activated. Please log in to %1 and follow provided instructions",
-            "%1 is the cloud portal name (like 'Nx Cloud Portal'").arg(portalLink);
-   }
+        return tr("Account isn't activated. Please check your email and follow provided instructions");
+    }
 };
