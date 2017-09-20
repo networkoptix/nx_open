13e129222e63b823858d03db76f553e271b532f7
Vitaly Kutin <vkutin@networkoptix.com>
2017-09-20 18:20:22 +0300

    # Code style: converted tabs to spaces
    <==== Mercurial data ====>
    Branch: vms_3.1.2_bitrate_control

--- client/nx_client_desktop/src/ui/widgets/properties/camera_advanced_param_widget_factory.cpp
+++ client/nx_client_desktop/src/ui/widgets/properties/camera_advanced_param_widget_factory.cpp
@@ -19,55 +19,55 @@
 #include <nx/utils/math/fuzzy.h>
 
 QnAbstractCameraAdvancedParamWidget::QnAbstractCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-	QWidget(parent),
-	m_id(parameter.id),
-	m_layout(new QHBoxLayout(this))
+    QWidget(parent),
+    m_id(parameter.id),
+    m_layout(new QHBoxLayout(this))
 {
-	m_layout->setContentsMargins(0, 0, 0, 0);
-	setLayout(m_layout);
+    m_layout->setContentsMargins(0, 0, 0, 0);
+    setLayout(m_layout);
 
-	if (parameter.readOnly) {
-		QLabel* readOnlyLabel = new QLabel(tr("Read-Only"));
-		setWarningStyle(readOnlyLabel);
-		m_layout->addWidget(readOnlyLabel);
-	}
+    if (parameter.readOnly) {
+        QLabel* readOnlyLabel = new QLabel(tr("Read-Only"));
+        setWarningStyle(readOnlyLabel);
+        m_layout->addWidget(readOnlyLabel);
+    }
 
 }
 
 class QnBoolCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget {
 public:
-	QnBoolCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-		QnAbstractCameraAdvancedParamWidget(parameter, parent),
-		m_checkBox(new QCheckBox(this))
-	{
-		m_checkBox->setToolTip(parameter.description);
-		setReadOnly(m_checkBox, parameter.readOnly);
+    QnBoolCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
+        QnAbstractCameraAdvancedParamWidget(parameter, parent),
+        m_checkBox(new QCheckBox(this))
+    {
+        m_checkBox->setToolTip(parameter.description);
+        setReadOnly(m_checkBox, parameter.readOnly);
 
-		m_layout->insertStretch(0);
-		m_layout->insertWidget(0, m_checkBox);
-		connect(m_checkBox, &QCheckBox::stateChanged, this, [this] {
-			emit valueChanged(m_id, value());
-		});
-	}
+        m_layout->insertStretch(0);
+        m_layout->insertWidget(0, m_checkBox);
+        connect(m_checkBox, &QCheckBox::stateChanged, this, [this] {
+            emit valueChanged(m_id, value());
+        });
+    }
 
-	virtual QString value() const override	{
-		return m_checkBox->isChecked() ? lit("true") : lit("false");
-	}
+    virtual QString value() const override	{
+        return m_checkBox->isChecked() ? lit("true") : lit("false");
+    }
 
-	virtual void setValue(const QString &newValue) override	{
-		m_checkBox->setChecked(newValue == lit("true") || newValue == lit("1"));
-	}
+    virtual void setValue(const QString &newValue) override	{
+        m_checkBox->setChecked(newValue == lit("true") || newValue == lit("1"));
+    }
 
 private:
-	QCheckBox* m_checkBox;
+    QCheckBox* m_checkBox;
 };
 
 class QnMinMaxStepCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget {
 public:
-	QnMinMaxStepCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-		QnAbstractCameraAdvancedParamWidget(parameter, parent),
-		m_spinBox(new QSpinBox(this))
-	{
+    QnMinMaxStepCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
+        QnAbstractCameraAdvancedParamWidget(parameter, parent),
+        m_spinBox(new QSpinBox(this))
+    {
         double min = 0;
         double max = 0;
         parameter.getRange(min, max);
@@ -80,29 +80,29 @@ public:
             m_spinBox->setMaximum(qRound(max * 100));
         }
 
-		m_spinBox->setToolTip(parameter.description);
-		setReadOnly(m_spinBox, parameter.readOnly);
+        m_spinBox->setToolTip(parameter.description);
+        setReadOnly(m_spinBox, parameter.readOnly);
 
-		m_layout->insertStretch(0);
-		m_layout->insertWidget(0, m_spinBox);
-		connect(m_spinBox, QnSpinboxIntValueChanged, this, [this] {
-			emit valueChanged(m_id, value());
-		});
-	}
+        m_layout->insertStretch(0);
+        m_layout->insertWidget(0, m_spinBox);
+        connect(m_spinBox, QnSpinboxIntValueChanged, this, [this] {
+            emit valueChanged(m_id, value());
+        });
+    }
 
-	virtual QString value() const override	{
-		int innerValue = m_spinBox->value();
+    virtual QString value() const override	{
+        int innerValue = m_spinBox->value();
         if (m_isInteger)
             return QString::number(innerValue);
         return QString::number(0.01 * innerValue);
-	}
+    }
 
-	virtual void setValue(const QString &newValue) override	{
+    virtual void setValue(const QString &newValue) override	{
         if (m_isInteger)
             m_spinBox->setValue(newValue.toInt());
         else
             m_spinBox->setValue(qRound(newValue.toDouble() * 100));
-	}
+    }
 
     virtual void setRange(const QString& range) override
     {
@@ -133,26 +133,26 @@ public:
     }
 
 private:
-	QSpinBox* m_spinBox;
+    QSpinBox* m_spinBox;
     bool m_isInteger;
 };
 
 class QnEnumerationCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget {
 public:
-	QnEnumerationCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-		QnAbstractCameraAdvancedParamWidget(parameter, parent),
-		m_comboBox(new QComboBox(this))
-	{
+    QnEnumerationCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
+        QnAbstractCameraAdvancedParamWidget(parameter, parent),
+        m_comboBox(new QComboBox(this))
+    {
         m_comboBox->addItems(parameter.getRange());
-		m_comboBox->setToolTip(parameter.description);
-		setReadOnly(m_comboBox, parameter.readOnly);
+        m_comboBox->setToolTip(parameter.description);
+        setReadOnly(m_comboBox, parameter.readOnly);
 
-		m_layout->insertStretch(0);
-		m_layout->insertWidget(0, m_comboBox);
-		connect(m_comboBox, &QComboBox::currentTextChanged, this, [this] {
-			emit valueChanged(m_id, value());
-		});
-	}
+        m_layout->insertStretch(0);
+        m_layout->insertWidget(0, m_comboBox);
+        connect(m_comboBox, &QComboBox::currentTextChanged, this, [this] {
+            emit valueChanged(m_id, value());
+        });
+    }
 
     virtual void setRange(const QString& range)
     {
@@ -166,62 +166,62 @@ public:
         m_comboBox->addItems(range);
     }
 
-	virtual QString value() const override	{
-		return m_comboBox->currentText();
-	}
+    virtual QString value() const override	{
+        return m_comboBox->currentText();
+    }
 
-	virtual void setValue(const QString &newValue) override	{
-		m_comboBox->setCurrentText(newValue);
-	}
+    virtual void setValue(const QString &newValue) override	{
+        m_comboBox->setCurrentText(newValue);
+    }
 
 private:
-	QComboBox* m_comboBox;
+    QComboBox* m_comboBox;
 };
 
 class QnButtonCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget {
 public:
-	QnButtonCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-		QnAbstractCameraAdvancedParamWidget(parameter, parent)
-	{
-		QPushButton *button = new QPushButton(this);
-		button->setText(parameter.name);
-		button->setToolTip(parameter.description);
-		setReadOnly(button, parameter.readOnly);
+    QnButtonCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
+        QnAbstractCameraAdvancedParamWidget(parameter, parent)
+    {
+        QPushButton *button = new QPushButton(this);
+        button->setText(parameter.name);
+        button->setToolTip(parameter.description);
+        setReadOnly(button, parameter.readOnly);
 
-		m_layout->insertStretch(0);
-		m_layout->insertWidget(0, button);
-		connect(button, &QPushButton::clicked, this, [this] {
-			emit valueChanged(m_id, QString());
-		});
-	}
+        m_layout->insertStretch(0);
+        m_layout->insertWidget(0, button);
+        connect(button, &QPushButton::clicked, this, [this] {
+            emit valueChanged(m_id, QString());
+        });
+    }
 
-	virtual QString value() const override	{ return QString(); }
-	virtual void setValue(const QString &newValue) override	{ Q_UNUSED(newValue); }
+    virtual QString value() const override	{ return QString(); }
+    virtual void setValue(const QString &newValue) override	{ Q_UNUSED(newValue); }
 };
 
 class QnStringCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget {
 public:
-	QnStringCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
-		QnAbstractCameraAdvancedParamWidget(parameter, parent),
-		m_lineEdit(new QLineEdit(this))
-	{
-		m_lineEdit->setToolTip(parameter.description);
-		setReadOnly(m_lineEdit, parameter.readOnly);
+    QnStringCameraAdvancedParamWidget(const QnCameraAdvancedParameter &parameter, QWidget* parent):
+        QnAbstractCameraAdvancedParamWidget(parameter, parent),
+        m_lineEdit(new QLineEdit(this))
+    {
+        m_lineEdit->setToolTip(parameter.description);
+        setReadOnly(m_lineEdit, parameter.readOnly);
 
-		m_layout->insertWidget(0, m_lineEdit, 1);
+        m_layout->insertWidget(0, m_lineEdit, 1);
 
-		connect(m_lineEdit, &QLineEdit::textEdited, this, [this] {
-			emit valueChanged(m_id, value());
-		});
-	}
+        connect(m_lineEdit, &QLineEdit::textEdited, this, [this] {
+            emit valueChanged(m_id, value());
+        });
+    }
 
-	virtual QString value() const override	{
-		return m_lineEdit->text();
-	}
+    virtual QString value() const override	{
+        return m_lineEdit->text();
+    }
 
-	virtual void setValue(const QString &newValue) override	{
-		m_lineEdit->setText(newValue);
-	}
+    virtual void setValue(const QString &newValue) override	{
+        m_lineEdit->setText(newValue);
+    }
 
     virtual QSize sizeHint() const override
     {
@@ -230,7 +230,7 @@ public:
     }
 
 private:
-	QLineEdit* m_lineEdit;
+    QLineEdit* m_lineEdit;
 };
 
 class QnSeparatorCameraAdvancedParamWidget: public QnAbstractCameraAdvancedParamWidget
@@ -255,38 +255,38 @@ private:
 QnAbstractCameraAdvancedParamWidget* QnCameraAdvancedParamWidgetFactory::createWidget(
     const QnCameraAdvancedParameter& parameter, QWidget* parent)
 {
-	if (!parameter.isValid())
-		return nullptr;
+    if (!parameter.isValid())
+        return nullptr;
 
-	switch (parameter.dataType)
+    switch (parameter.dataType)
     {
-	    /* CheckBox */
-	    case QnCameraAdvancedParameter::DataType::Bool:
-		    return new QnBoolCameraAdvancedParamWidget(parameter, parent);
+        /* CheckBox */
+        case QnCameraAdvancedParameter::DataType::Bool:
+            return new QnBoolCameraAdvancedParamWidget(parameter, parent);
 
-	    /* Slider */
-	    case QnCameraAdvancedParameter::DataType::Number:
-		    return new QnMinMaxStepCameraAdvancedParamWidget(parameter, parent);
+        /* Slider */
+        case QnCameraAdvancedParameter::DataType::Number:
+            return new QnMinMaxStepCameraAdvancedParamWidget(parameter, parent);
 
-	    /* Drop-down box. */
-	    case QnCameraAdvancedParameter::DataType::Enumeration:
-		    return new QnEnumerationCameraAdvancedParamWidget(parameter, parent);
+        /* Drop-down box. */
+        case QnCameraAdvancedParameter::DataType::Enumeration:
+            return new QnEnumerationCameraAdvancedParamWidget(parameter, parent);
 
-	    /* Button */
-	    case QnCameraAdvancedParameter::DataType::Button:
-		    return new QnButtonCameraAdvancedParamWidget(parameter, parent);
+        /* Button */
+        case QnCameraAdvancedParameter::DataType::Button:
+            return new QnButtonCameraAdvancedParamWidget(parameter, parent);
 
-	    /* LineEdit  */
-	    case QnCameraAdvancedParameter::DataType::String:
-		    return new QnStringCameraAdvancedParamWidget(parameter, parent);
+        /* LineEdit  */
+        case QnCameraAdvancedParameter::DataType::String:
+            return new QnStringCameraAdvancedParamWidget(parameter, parent);
 
         /* Separator */
         case QnCameraAdvancedParameter::DataType::Separator:
             return new QnSeparatorCameraAdvancedParamWidget(parameter, parent);
 
-	    default:
-		    return nullptr;
-	}
+        default:
+            return nullptr;
+    }
 }
 
 
