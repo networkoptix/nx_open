29df5f00db4d93931e5b9f66c9e7690dd7a888c2
Mikhail Uskov <muskov@networkoptix.com>
2017-09-20 15:00:04 +0300

    # VMS-7224 patch error fix
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- edge_firmware/maven/bin-resources/create_arm_installer.sh
+++ edge_firmware/maven/bin-resources/create_arm_installer.sh
@@ -536,9 +536,6 @@ main()
     echo "$VERSION" >"$INSTALL_DIR/version.txt"
 
     echo "Copying build_info.txt"
-    cp -r "$BUILD_DIR/common/build_info.txt" "$INSTALL_DIR/"
-
-    echo "Copying build_info.txt"
     cp "$BUILD_DIR/common/build_info.txt" "$INSTALL_DIR/" || {
         # This is an ugly workaround for maven, remove as soon as maven is not in use.
         cp "$BUILD_DIR/../../common/arm-$BOX/build_info.txt" "$INSTALL_DIR"
