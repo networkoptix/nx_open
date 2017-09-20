e76b525a4d1df02450cea133c11727693f548d89
Denis Klychkov <dklychkov@networkoptix.com>
2017-09-20 15:58:11 +0300

    CMake: Generate a dummy .pro file which can be used to deploy and debug on Android
    <==== Mercurial data ====>
    Branch: mobile

--- build_utils/cmake/android-apk.cmake
+++ build_utils/cmake/android-apk.cmake
@@ -1,3 +1,7 @@
+option(buildApk
+    "Build Android APK automatically. Uncheck if you want to use Qt Creator to debug the application."
+    ON)
+
 find_program(ANDROIDDEPLOYQT_EXECUTABLE androiddeployqt HINTS "${QT_DIR}/bin")
 
 function(find_buildtools sdk_path result_variable)
@@ -16,7 +20,7 @@ endfunction()
 
 function(add_android_apk target)
     set(options)
-    set(oneValueArgs TARGET APK_NAME PACKAGE_SOURCE QML_ROOT_PATH
+    set(oneValueArgs TARGET APK_NAME PACKAGE_SOURCE QML_ROOT_PATH VERSION
         KEYSTORE_FILE KEYSTORE_ALIAS KEYSTORE_PASSWORD KEYSTORE_KEY_PASSWORD)
     set(multiValueArgs QML_IMPORT_PATHS EXTRA_LIBS)
 
@@ -46,10 +50,9 @@ function(add_android_apk target)
         "    \"android-extra-libs\": \"${extra_libs}\",\n"
         "    \"qml-import-paths\": \"${APK_QML_IMPORT_PATHS}\",\n"
         "    \"qml-root-path\": \"${APK_QML_ROOT_PATH}\",\n"
+        "    \"application-binary\": \"$<TARGET_FILE:${APK_TARGET}>\"\n"
+        "}\n"
     )
-
-    list(APPEND settings "    \"application-binary\": \"$<TARGET_FILE:${APK_TARGET}>\"\n")
-    list(APPEND settings "}\n")
 
     string(CONCAT settings ${settings})
 
@@ -90,5 +93,31 @@ function(add_android_apk target)
             "${apk_dir}/build/outputs/apk/${APK_TARGET}_apk-${apk_suffix}.apk"
             "${CMAKE_BINARY_DIR}/${APK_APK_NAME}"
     )
-add_custom_target(${target} ALL DEPENDS ${APK_APK_NAME})
+
+    set(target_args)
+    if(buildApk)
+        set(target_args ALL)
+    endif()
+
+    add_custom_target(${target} ${target_args} DEPENDS ${APK_APK_NAME})
+
+    string(REPLACE ";" " " extra_libs "${APK_EXTRA_LIBS}")
+
+    set(pro_file_content
+        "#This file is generated and should not be modified by hand.\n"
+        "TEMPLATE = app\n"
+        "TARGET = ${APK_TARGET}\n"
+        "VERSION = ${APK_VERSION}\n"
+        "CONFIG(debug, debug|release): CONFIG += qml_debug\n"
+        "QML_IMPORT_PATH = ${APK_QML_IMPORT_PATHS}\n"
+        "QML_ROOT_PATH = ${APK_QML_ROOT_PATH}\n"
+        "ANDROID_EXTRA_LIBS += ${extra_libs}\n"
+        "DESTDIR = $<TARGET_FILE_DIR:${APK_TARGET}>\n"
+        "ANDROID_PACKAGE_SOURCE_DIR = $$_PRO_FILE_PWD_/android\n"
+        "OTHER_FILES += $$ANDROID_PACKAGE_SOURCE_DIR/AndroidManifest.xml\n"
+    )
+    string(CONCAT pro_file_content ${pro_file_content})
+
+    set(pro_file "${CMAKE_CURRENT_BINARY_DIR}/${APK_TARGET}.pro")
+    file(GENERATE OUTPUT "${pro_file}" CONTENT "${pro_file_content}")
 endfunction()
