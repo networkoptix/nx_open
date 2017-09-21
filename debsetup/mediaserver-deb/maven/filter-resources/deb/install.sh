30495f9548a4cf8ea00ca561b8b5c726dc885b64
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-09-21 16:15:41 -0700

    VMS-6181: Add cifs-utils to server distrib package. Ugly huck for ubuntu14
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- debsetup/mediaserver-deb/maven/filter-resources/deb/install.sh
+++ debsetup/mediaserver-deb/maven/filter-resources/deb/install.sh
@@ -2,11 +2,15 @@
 
 DISTRIB=${artifact.name.server}.deb
 
+UBUNTU_DISTRIB=$(lsb_release -a | grep Codename | awk {'print $2'})
+
 update () {
     export DEBIAN_FRONTEND=noninteractive
     CIFSUTILS=$(dpkg -l | grep cifs-utils | grep ii | awk '{print $2}')
     if [ -z "$CIFSUTILS" ]; then
-        dpkg -i cifs-utils/*.deb
+        #TODO: check when newer Ubuntu distrib is out and add another line
+        if [ "$UBUNTU_DISTRIB" == "trusty" ]; then dpkg -i ubuntu14/cifs-utils/*.deb; fi
+        if [ "$UBUNTU_DISTRIB" == "xenial" ]; then dpkg -i ubuntu16/cifs-utils/*.deb; fi
     fi
     dpkg -i $DISTRIB
 }
