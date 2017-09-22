707ef6fda7e31deb7fcb6253b5a3672e1a587446
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-09-22 15:47:58 -0700

    customization details
    <==== Mercurial data ====>
    Branch: vms_3.1.2_asis

--- customization/hanwha/mediaserver_core/resources/static/customization/config.js
+++ customization/hanwha/mediaserver_core/resources/static/customization/config.js
@@ -1,5 +1,5 @@
 Config.helpLinks.push({
-    url: "support@hanwhasecurity.com",
+    url: "mailto:support@hanwhasecurity.com",
     title: "Support",
     target: "new" // new|frame
 });
@@ -10,22 +10,22 @@ Config.helpLinks.push({
     target: "new" // new|frame
 });
 
-/*Config.helpLinks.push({
+Config.helpLinks.push({
     urls: [
         {
-            url: "https://itunes.apple.com/eg/app/hd-witness/id1050899754?mt=8",
+            url: "https://itunes.apple.com/us/app/wave-mobile/id1284037424?ls=1&mt=8",
             button: "iOS Client",
             class:'appstore'
         },
         {
-            url: "https://play.google.com/store/apps/details?id=com.networkoptix.nxwitness",
+            url: "https://play.google.com/store/apps/details?id=com.hanwhasecurity.wavemobile",
             class:'googleplay',
             button: "Android Client"
         }
     ],
     title: "Mobile Apps",
     target: "new" // new|frame
-});*/
+});
 
 Config.allowDebugMode = false;
 //Config.webclientEnabled = false; // Uncomment this string to disable web client
