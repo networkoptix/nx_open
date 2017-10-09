04ca2c469d1bec0abd9aac61907d646ed3544ae0
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-10-09 11:22:17 -0700

    #Coding style fixes.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_utils/src/nx/utils/stree/resourcecontainer.h
+++ common_libs/nx_utils/src/nx/utils/stree/resourcecontainer.h
@@ -16,21 +16,24 @@ namespace nx {
 namespace utils {
 namespace stree {
 
-//!Implement this interface to allow reading resource values from object
+/**
+ * Implement this interface to allow reading resource values from object.
+ */
 class NX_UTILS_API AbstractResourceReader
 {
 public:
     virtual ~AbstractResourceReader() = default;
 
-    /*!
-        \return true, if resource \a resID found, false otherwise
-    */
-    virtual bool getAsVariant( int resID, QVariant* const value ) const = 0;
-    //!Helper function to get typed value
-    /*!
-        If stored value cannot be cast to type \a T, false is returned
-    */
-    template<typename T> bool get( int resID, T* const value ) const
+    /**
+     * @return true, if resource resID found, false otherwise.
+     */
+    virtual bool getAsVariant(int resID, QVariant* const value) const = 0;
+
+    /**
+     * Helper function to get typed value.
+     * If stored value cannot be cast to type T, false is returned.
+     */
+    template<typename T> bool get(int resID, T* const value) const
     {
         QVariant untypedValue;
         if (!get(resID, &untypedValue))
@@ -40,7 +43,8 @@ public:
         *value = untypedValue.value<T>();
         return true;
     }
-    bool get( int resID, QnUuid* const value ) const
+
+    bool get(int resID, QnUuid* const value) const
     {
         QVariant untypedValue;
         if (!get(resID, &untypedValue))
@@ -57,6 +61,7 @@ public:
         }
         return false;
     }
+
     bool get(int resID, std::string* const value) const
     {
         QVariant untypedValue;
@@ -74,36 +79,42 @@ public:
         }
         return false;
     }
-    template<typename T> boost::optional<T> get( int resID ) const
+
+    template<typename T> boost::optional<T> get(int resID) const
     {
         T val;
-        if( !get( resID, &val ) )
+        if (!get(resID, &val))
             return boost::none;
         return val;
     }
-    bool get( int resID, QVariant* const value ) const
+
+    bool get(int resID, QVariant* const value) const
     {
-        return getAsVariant( resID, value );
+        return getAsVariant(resID, value);
     }
-    boost::optional<QVariant> get( int resID ) const
+
+    boost::optional<QVariant> get(int resID) const
     {
         QVariant val;
-        if( !get( resID, &val ) )
+        if (!get(resID, &val))
             return boost::none;
         return val;
     }
 };
 
-//!Implement this interface to allow writing resource values to object
+/**
+ * Implement this interface to allow writing resource values to object.
+ */
 class NX_UTILS_API AbstractResourceWriter
 {
 public:
     virtual ~AbstractResourceWriter() {}
 
-    /*!
-        If resource \a resID already exists, its value is overwritten with \a value. Otherwise, new resource added
-    */
-    virtual void put( int resID, const QVariant& value ) = 0;
+    /**
+     * If resource resID already exists, its value is overwritten with value. 
+     * Otherwise, new resource added.
+     */
+    virtual void put(int resID, const QVariant& value) = 0;
 };
 
 class NX_UTILS_API AbstractConstIterator
@@ -111,7 +122,9 @@ class NX_UTILS_API AbstractConstIterator
 public:
     virtual ~AbstractConstIterator() {}
 
-    //!Returns \a false if already at the end
+    /**
+     * @return false if already at the end.
+     */
     virtual bool next() = 0;
     virtual bool atEnd() const = 0;
     virtual int resID() const = 0;
@@ -119,57 +132,55 @@ public:
     template<typename T> T value() { return value().value<T>(); }
 };
 
-//!Implement this interface to allow walk through container data
-/*!
-    Usage sample:
-    AbstractIteratableContainer* cont = ...;
-    for( auto it = cont->begin(); !it->atEnd(); it->next() )
-    {
-        //accessing data through iterator
-    }
-*/
+/**
+ * Implement this interface to allow walk through container data.
+ * Usage sample:
+ * AbstractIteratableContainer* cont = ...;
+ * for( auto it = cont->begin(); !it->atEnd(); it->next() )
+ * {
+ *   //accessing data through iterator
+ * }
+ */
 class NX_UTILS_API AbstractIteratableContainer
 {
 public:
     virtual ~AbstractIteratableContainer() {}
 
-    //!Returns iterator set to the first element
+    /**
+     * @return Iterator set to the first element.
+     */
     virtual std::unique_ptr<nx::utils::stree::AbstractConstIterator> begin() const = 0;
 };
 
-
-//!Allows to add/get resources. Represents associative container
-class NX_UTILS_API ResourceContainer
-:
+/**
+ * Allows to add/get resources. Represents associative container.
+ */
+class NX_UTILS_API ResourceContainer:
     public AbstractResourceReader,
     public AbstractResourceWriter,
     public AbstractIteratableContainer
 {
 public:
-    ResourceContainer();
-    ResourceContainer(ResourceContainer&&);
-    ResourceContainer(const ResourceContainer&);
+    ResourceContainer() = default;
+    ResourceContainer(ResourceContainer&&) = default;
+    ResourceContainer(const ResourceContainer&) = default;
 
-    ResourceContainer& operator=(ResourceContainer&&);
-    ResourceContainer& operator=(const ResourceContainer&);
+    ResourceContainer& operator=(ResourceContainer&&) = default;
+    ResourceContainer& operator=(const ResourceContainer&) = default;
 
-    //!Implementation of AbstractResourceReader::get
-    virtual bool getAsVariant( int resID, QVariant* const value ) const override;
-    //!Implementation of AbstractResourceReader::put
-    virtual void put( int resID, const QVariant& value ) override;
+    virtual bool getAsVariant(int resID, QVariant* const value) const override;
+    virtual void put(int resID, const QVariant& value) override;
 
-    //!Implementation of AbstractIteratableContainer::begin
     virtual std::unique_ptr<nx::utils::stree::AbstractConstIterator> begin() const override;
 
-    QString toString( const nx::utils::stree::ResourceNameSet& rns ) const;
+    QString toString(const nx::utils::stree::ResourceNameSet& rns) const;
     bool empty() const;
 
 private:
     std::map<int, QVariant> m_mediaStreamPameters;
 };
 
-class NX_UTILS_API SingleResourceContainer
-:
+class NX_UTILS_API SingleResourceContainer:
     public AbstractResourceReader,
     public AbstractResourceWriter
 {
@@ -177,25 +188,24 @@ public:
     SingleResourceContainer();
     SingleResourceContainer(
         int resID,
-        QVariant value );
+        QVariant value);
 
-    //!Implementation of AbstractResourceReader::get
-    virtual bool getAsVariant( int resID, QVariant* const value ) const;
-    //!Implementation of AbstractResourceReader::put
-    virtual void put( int resID, const QVariant& value );
+    virtual bool getAsVariant(int resID, QVariant* const value) const;
+    virtual void put(int resID, const QVariant& value);
 
 private:
     int m_resID;
     QVariant m_value;
 };
 
-//!Reads from two AbstractResourceReader
-class NX_UTILS_API MultiSourceResourceReader
-:
+/**
+ * Reads from two AbstractResourceReader.
+ */
+class NX_UTILS_API MultiSourceResourceReader:
     public AbstractResourceReader
 {
 public:
-    //TODO #ak make initializer a variadic template func
+    // TODO: #ak Make initializer a variadic template func.
     MultiSourceResourceReader(
         const AbstractResourceReader& rc1,
         const AbstractResourceReader& rc2);
@@ -209,22 +219,19 @@ public:
         const AbstractResourceReader& rc3,
         const AbstractResourceReader& rc4);
 
-    //!Implementation of AbstractResourceReader::get
-    /*!
-        Reads first reader. If no value found, reads second reader
-    */
-    virtual bool getAsVariant( int resID, QVariant* const value ) const;
+    /**
+     * Reads first reader. If no value found, reads second reader.
+     */
+    virtual bool getAsVariant(int resID, QVariant* const value) const;
 
 private:
     const size_t m_elementCount;
     std::array<const AbstractResourceReader*, 4> m_readers;
 };
 
-//TODO #ak using variadic template add method makeMultiSourceResourceReader that accepts any number of aguments
+// TODO: #ak using variadic template add method makeMultiSourceResourceReader that accepts any number of aguments.
 
-
-class NX_UTILS_API MultiIteratableResourceReader
-:
+class NX_UTILS_API MultiIteratableResourceReader:
     public AbstractIteratableContainer
 {
 public:
@@ -232,7 +239,6 @@ public:
         const AbstractIteratableContainer& rc1,
         const AbstractIteratableContainer& rc2);
 
-    //!Implementation of AbstractIteratableContainer::begin
     virtual std::unique_ptr<nx::utils::stree::AbstractConstIterator> begin() const override;
 
 private:
@@ -244,17 +250,16 @@ private:
 };
 
 /**
-    * Writes to \a target and \a additionalOutput.
-    */
-class NX_UTILS_API ResourceWriterProxy
-:
+ * Writes to target and additionalOutput.
+ */
+class NX_UTILS_API ResourceWriterProxy:
     public nx::utils::stree::AbstractResourceWriter
 {
 public:
     ResourceWriterProxy(
         nx::utils::stree::AbstractResourceWriter* target,
         nx::utils::stree::AbstractResourceWriter* additionalOutput)
-    :
+        :
         m_target(target),
         m_additionalOutput(additionalOutput)
     {
