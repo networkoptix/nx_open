b7db52ad3990e163fd320de57c575e4d9d87c527
Sergey Ivanov <sivanov@networkoptix.com>
2017-10-09 12:22:49 +0300

    VMS-7340: Improve event loop check
    <==== Mercurial data ====>
    Branch: vms_3.1.2

--- common/src/utils/common/event_loop.cpp
+++ common/src/utils/common/event_loop.cpp
@@ -2,6 +2,7 @@
 
 #include <QtCore/QThread>
 #if !defined(Q_OS_ANDROID)
+    #include <QtGui/QGuiApplication>
     #include <QtCore/private/qthread_p.h>
 #endif
 
@@ -9,7 +10,14 @@ bool qnHasEventLoop(QThread* thread)
 {
 #if !defined(Q_OS_ANDROID)
     int loopLevel = QThreadData::get2(thread)->loopLevel;
-    return loopLevel > 0;
+    if (loopLevel > 0)
+        return true;
+
+    // Main thread of the GUI application will have event loop for sure.
+    if (thread == QCoreApplication::instance()->thread())
+        return dynamic_cast<QGuiApplication*>(QCoreApplication::instance());
+
+    return false;
 #else
     return true;
 #endif
