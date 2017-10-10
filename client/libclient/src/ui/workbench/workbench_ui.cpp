4e7f9a995ad0d396c9ec05cbca6c92e33c7cc004
Sergey Ivanov <sivanov@networkoptix.com>
2017-10-10 15:06:12 +0300

    Profiler mode for the client
    <==== Mercurial data ====>
    Branch: vms_3.0

--- client/libclient/src/ui/workbench/workbench_ui.cpp
+++ client/libclient/src/ui/workbench/workbench_ui.cpp
@@ -1338,6 +1338,8 @@ void QnWorkbenchUi::createFpsWidget()
     m_fpsItem = new QnProxyLabel(m_controlsWidget);
     m_fpsItem->setAcceptedMouseButtons(0);
     m_fpsItem->setAcceptHoverEvents(false);
+    m_fpsItem->setText(lit("...."));
+    updateFpsGeometry();
     setPaletteColor(m_fpsItem, QPalette::Window, Qt::transparent);
     setPaletteColor(m_fpsItem, QPalette::WindowText, QColor(63, 159, 216));
 
