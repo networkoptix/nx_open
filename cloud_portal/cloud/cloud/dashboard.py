03fe8c3d91095fda6824f57534f334063dfd5b6c
Nick Hartleb <nhartleb@networkoptix.com>
2017-10-12 18:28:00 -0700

    CLOUD-1444: Disable "Django news", "Support", "Quick links" modules in django-admin-tools
    removed dead code
    
    <==== Mercurial data ====>
    Branch: cloud_17.1.2

--- cloud_portal/cloud/cloud/dashboard.py
+++ cloud_portal/cloud/cloud/dashboard.py
@@ -16,7 +16,7 @@ try:
 except ImportError:
     from django.core.urlresolvers import reverse
 
-from admin_tools.dashboard import modules, Dashboard, AppIndexDashboard
+from admin_tools.dashboard import modules, Dashboard
 from admin_tools.utils import get_admin_site_name
 
 
@@ -50,31 +50,3 @@ class CustomIndexDashboard(Dashboard):
                 deletable=False,
                 collapsible=False,
             ))
-
-
-class CustomAppIndexDashboard(AppIndexDashboard):
-    """
-    Custom app index dashboard for cloud.
-    """
-
-    # we disable title because its redundant with the model list module
-    title = ''
-
-    def __init__(self, *args, **kwargs):
-        AppIndexDashboard.__init__(self, *args, **kwargs)
-
-        # append a model list module and a recent actions module
-        self.children += [
-            modules.ModelList(self.app_title, self.models),
-            modules.RecentActions(
-                _('Recent Actions'),
-                include_list=self.get_app_content_types(),
-                limit=5
-            )
-        ]
-
-    def init_with_context(self, context):
-        """
-        Use this method if you need to access the request context.
-        """
-        return super(CustomAppIndexDashboard, self).init_with_context(context)
