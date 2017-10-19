4dc323ba15418caaaefcb5e85a1ffedd1f9f6dfb
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-10-18 17:35:43 -0700

    WEB-679: Deal with translations for web_common/commonLanguage.json
    <==== Mercurial data ====>
    Branch: vms_3.1_web

--- translation/vms_up.bat
+++ translation/vms_up.bat
@@ -1,2 +1,5 @@
-python webadmin/generate_language_json.py
+set CURRENTDIR=%cd%
+cd ..\webadmin
+python generate_language_json.py
+cd %CURRENTDIR%
 crowdin upload sources -b vms_3.1 --config crowdin-vms.yaml
