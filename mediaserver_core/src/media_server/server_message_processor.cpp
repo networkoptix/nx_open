bb609a5cf3667f48ebe770034dfc35e24291359a
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-10-19 23:40:20 +0300

    VMS-7476: Deadlock in mediaserver
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- mediaserver_core/src/media_server/server_message_processor.cpp
+++ mediaserver_core/src/media_server/server_message_processor.cpp
@@ -273,13 +273,19 @@ void QnServerMessageProcessor::removeResourceIgnored(const QnUuid& resourceId)
         ec2::ApiMediaServerData apiServer;
         ec2::fromResourceToApi(mServer, apiServer);
         auto connection = commonModule()->ec2Connection();
-        connection->getMediaServerManager(Qn::kSystemAccess)->saveSync(apiServer);
+        connection->getMediaServerManager(Qn::kSystemAccess)->save(
+            apiServer,
+            ec2::DummyHandler::instance(),
+            &ec2::DummyHandler::onRequestDone);
     }
     else if (isOwnStorage && !storage->isExternal() && storage->isWritable())
     {
         ec2::ApiStorageDataList apiStorages;
         fromResourceListToApi(QnStorageResourceList() << storage, apiStorages);
-        commonModule()->ec2Connection()->getMediaServerManager(Qn::kSystemAccess)->saveStoragesSync(apiStorages);
+        commonModule()->ec2Connection()->getMediaServerManager(Qn::kSystemAccess)->saveStorages(
+            apiStorages,
+            ec2::DummyHandler::instance(),
+            &ec2::DummyHandler::onRequestDone);
     }
 }
 
