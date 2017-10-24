1b7bd8ad31ed1534e64e28008ea78baf527de76f
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-10-24 11:29:56 -0700

    #Coding style fixes.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- mediaserver_core/src/network/auth/abstract_nonce_provider.h
+++ mediaserver_core/src/network/auth/abstract_nonce_provider.h
@@ -1,21 +1,12 @@
-/**********************************************************
-* Oct 2, 2015
-* akolesnikov
-***********************************************************/
-
-#ifndef NX_AUTH_ABSTRACT_NONCE_PROVIDER_H
-#define NX_AUTH_ABSTRACT_NONCE_PROVIDER_H
+#pragma once
 
 #include <QtCore/QByteArray>
-
 
 class AbstractNonceProvider
 {
 public:
-    virtual ~AbstractNonceProvider() {}
+    virtual ~AbstractNonceProvider() = default;
 
     virtual QByteArray generateNonce() = 0;
     virtual bool isNonceValid(const QByteArray& nonce) const = 0;
 };
-
-#endif  //NX_AUTH_ABSTRACT_NONCE_PROVIDER_H
