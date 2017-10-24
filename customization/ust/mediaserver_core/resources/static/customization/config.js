d8e349e2bae9e0888d2f19c4a83d9442645865fe
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2017-10-24 12:38:56 -0700

    Added mobile apps links on webadmin
    <==== Mercurial data ====>
    Branch: vms_3.1

--- customization/ust/mediaserver_core/resources/static/customization/config.js
+++ customization/ust/mediaserver_core/resources/static/customization/config.js
@@ -6,6 +6,23 @@ Config.helpLinks.push({
     target: "new" // new|frame
 });
 
+Config.helpLinks.push({
+    urls: [
+        {
+            url: "https://itunes.apple.com/us/app/ust-vms-viewer/id1120424275?ls=1&mt=8",
+            button: "iOS Client",
+            class:'appstore'
+        },
+        {
+            url: "https://play.google.com/store/apps/details?id=com.networkoptix.nxwitness",
+            class:'googleplay',
+            button: "Android Client"
+        }
+    ],
+    title: "Mobile Apps",
+    target: "new" // new|frame
+});
+
 Config.allowDebugMode = false;
 //Config.webclientEnabled = false; // Uncomment this string to disable web client
 Config.productName = 'VMS DTV';
