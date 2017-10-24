d63bbe6c1dbe9ddca3badadca1e0f71786e373bf
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-10-23 17:47:47 -0700

    #Coding style fixes.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- mediaserver_core/src/server/host_system_password_synchronizer.cpp
+++ mediaserver_core/src/server/host_system_password_synchronizer.cpp
@@ -9,11 +9,12 @@
 #include <stdlib.h>
 #endif
 
+#include <nx/utils/crypt/linux_passwd_crypt.h>
+
 #include <core/resource/resource.h>
 #include <core/resource/user_resource.h>
 #include <core/resource_management/resource_pool.h>
 #include <utils/common/app_info.h>
-#include <nx/utils/crypt/linux_passwd_crypt.h>
 
 #include "platform/platform_abstraction.h"
 #include "platform/monitoring/platform_monitor.h"
@@ -34,45 +35,46 @@ HostSystemPasswordSynchronizer::~HostSystemPasswordSynchronizer()
     directDisconnectAll();
 }
 
-void HostSystemPasswordSynchronizer::syncLocalHostRootPasswordWithAdminIfNeeded( const QnUserResourcePtr& user )
+void HostSystemPasswordSynchronizer::syncLocalHostRootPasswordWithAdminIfNeeded(
+    const QnUserResourcePtr& user)
 {
 #ifdef __linux__
-    QnMutexLocker lk( &m_mutex );
-    if( QnAppInfo::isBpi() || QnAppInfo::isNx1() )
+    QnMutexLocker lk(&m_mutex);
+    if (QnAppInfo::isBpi() || QnAppInfo::isNx1())
     {
         //#5785 changing root password on nx1 only if DB is located on HDD
         QList<QnPlatformMonitor::PartitionSpace> partitions =
             qnPlatform->monitor()->QnPlatformMonitor::totalPartitionSpaceInfo(
-                QnPlatformMonitor::LocalDiskPartition );
+                QnPlatformMonitor::LocalDiskPartition);
 
-        for( int i = 0; i < partitions.size(); ++i )
+        for (int i = 0; i < partitions.size(); ++i)
         {
-            if( partitions[i].path == "/" )
+            if (partitions[i].path == "/")
             {
-                if( partitions[i].devName.indexOf( lit( "rootfs" ) ) != -1 ||
-                    partitions[i].devName == lit( "/dev/root" ) )
+                if (partitions[i].devName.indexOf(lit("rootfs")) != -1 ||
+                    partitions[i].devName == lit("/dev/root"))
                 {
                     struct stat st;
-                    memset( &st, 0, sizeof( st ) );
-                    if( stat( "/dev/root", &st ) == -1 )
+                    memset(&st, 0, sizeof(st));
+                    if (stat("/dev/root", &st) == -1)
                     {
                         //failure
                         return;
                     }
 
-                    if( !(st.st_mode & S_IFLNK) )
+                    if (!(st.st_mode & S_IFLNK))
                         return; //OS is on SD card or some internal memory
 
-                    //checking that root is really on SD card
+                                //checking that root is really on SD card
                     std::array<char, PATH_MAX> realPath;
-                    const auto realPathLen = readlink( "/dev/root", realPath.data(), realPath.size() );
-                    if( realPathLen == -1 )
+                    const auto realPathLen = readlink("/dev/root", realPath.data(), realPath.size());
+                    if (realPathLen == -1)
                     {
                         //failure
                         return;
                     }
 
-                    if( QString::fromLocal8Bit( realPath.data(), realPathLen ).indexOf( lit("sd") ) != -1 )
+                    if (QString::fromLocal8Bit(realPath.data(), realPathLen).indexOf(lit("sd")) != -1)
                         break;  //OS is on regular drive. Can change root password
 
                     return; //OS is on SD card or some internal memory
@@ -85,12 +87,12 @@ void HostSystemPasswordSynchronizer::syncLocalHostRootPasswordWithAdminIfNeeded(
         }
 
         //changing root password in system
-        if( !setRootPasswordDigest( "root", user->getCryptSha512Hash() ) )
+        if (!setRootPasswordDigest("root", user->getCryptSha512Hash()))
         {
             qWarning() << "Failed to set root password on current system";
         }
     }
-#else
+else
     Q_UNUSED( user );
 #endif
 }
--- mediaserver_core/src/server/host_system_password_synchronizer.h
+++ mediaserver_core/src/server/host_system_password_synchronizer.h
@@ -8,19 +8,18 @@
 #include <nx/utils/safe_direct_connection.h>
 #include <common/common_module_aware.h>
 
-
-//!Sets host system password to admin password if appropriate
-/*!
-    Currently, password is changed only on NX1/bananapi and only if OS is installed on HDD drive
-*/
-class HostSystemPasswordSynchronizer
-:
+/**
+ * Sets host system password to admin password if appropriate.
+ * Currently, password is changed only on NX1/bananapi and only if OS is installed on HDD drive.
+ */
+class HostSystemPasswordSynchronizer:
     public QObject,
     public Singleton<HostSystemPasswordSynchronizer>,
     public QnCommonModuleAware,
     public Qn::EnableSafeDirectConnection
 {
     Q_OBJECT
+
 public:
     HostSystemPasswordSynchronizer(QnCommonModule* commonModule);
     virtual ~HostSystemPasswordSynchronizer() override;
@@ -30,7 +29,6 @@ public:
 private:
     QnMutex m_mutex;
 
-private:
     void setAdmin(QnUserResourcePtr admin);
 
 private slots:
