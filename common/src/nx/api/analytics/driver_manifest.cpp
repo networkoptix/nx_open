6d39ce817b2981aec9e20647048bc09a9b1d0216
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-10-31 21:30:58 +0300

    # Compile fixed.
    <==== Mercurial data ====>
    Branch: object_analytics_plugin

--- common/src/nx/api/analytics/driver_manifest.cpp
+++ common/src/nx/api/analytics/driver_manifest.cpp
@@ -2,10 +2,12 @@
 
 #include <nx/fusion/model_functions.h>
 
+QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(nx::api::AnalyticsDriverManifestBase, Capability)
+QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(nx::api::AnalyticsDriverManifestBase, Capabilities)
+
 namespace nx {
 namespace api {
 
-QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(AnalyticsDriverManifestBase, Capabilities)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(AnalyticsDriverManifest, (json), AnalyticsDriverManifest_Fields,
     (brief, true))
 
