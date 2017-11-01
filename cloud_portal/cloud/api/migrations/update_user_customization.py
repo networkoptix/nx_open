9162244c92abe474d9c765b17be1dd7de1afc073
Nick Hartleb <nhartleb@networkoptix.com>
2017-11-01 15:29:39 -0700

    CLOUD-1488: migratedb shouldn't assume cloud_db database exists at the moment
    added return uncommented all of the code
    
    <==== Mercurial data ====>
    Branch: cloud_17.1.1

--- cloud_portal/cloud/api/migrations/update_user_customization.py
+++ cloud_portal/cloud/api/migrations/update_user_customization.py
@@ -7,16 +7,17 @@ from django.db import connection, migrations
 #currently not in use do nothing
 
 
-#def update_customizations_from_clouddb(apps, schema_editor):
-#    with connection.cursor() as cursor:
-#        cursor.execute('UPDATE cloudportal.api_account db1 JOIN clouddb.account db2 ON db1.email=db2.email SET db1.customization=db2.customization')
+def update_customizations_from_clouddb(apps, schema_editor):
+    return
+    with connection.cursor() as cursor:
+        cursor.execute('UPDATE cloudportal.api_account db1 JOIN clouddb.account db2 ON db1.email=db2.email SET db1.customization=db2.customization')
 
-#class Migration(migrations.Migration):
+class Migration(migrations.Migration):
 
-#    dependencies = [
-#        ('api', '0012_account_customization'),
-#    ]
+    dependencies = [
+        ('api', '0012_account_customization'),
+    ]
 
-#    operations = [
-#        migrations.RunPython(update_customizations_from_clouddb),
-#    ]
+    operations = [
+        migrations.RunPython(update_customizations_from_clouddb),
+    ]
