5816b2a09f396b4a3aebe1c3453092b27ea33f4f
Mikhail Uskov <muskov@networkoptix.com>
2017-11-03 16:59:50 +0300

    # VMS-7507 fix for /api/getRemoteNonce?url=...
    <==== Mercurial data ====>
    Branch: vms_3.1.2_dev

--- mediaserver_core/src/media_server_process.cpp
+++ mediaserver_core/src/media_server_process.cpp
@@ -1824,8 +1824,8 @@ void MediaServerProcess::registerRestHandlers(
     reg(kGetTimePath, new QnTimeRestHandler());
     reg("ec2/getTimeOfServers", new QnMultiserverTimeRestHandler(QLatin1String("/") + kGetTimePath));
     reg("api/getTimeZones", new QnGetTimeZonesRestHandler());
-    reg("api/getNonce", new QnGetNonceRestHandler(/*isUrlSupported*/ false));
-    reg("api/getRemoteNonce", new QnGetNonceRestHandler(/*isUrlSupported*/ true));
+    reg("api/getNonce", new QnGetNonceRestHandler());
+    reg("api/getRemoteNonce", new QnGetNonceRestHandler(lit("/api/getNonce")));
     reg("api/cookieLogin", new QnCookieLoginRestHandler());
     reg("api/cookieLogout", new QnCookieLogoutRestHandler());
     reg("api/getCurrentUser", new QnCurrentUserRestHandler());
