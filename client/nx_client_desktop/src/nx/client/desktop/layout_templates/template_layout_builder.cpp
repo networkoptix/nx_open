1c3a1caf260f1cf32340e7ae511a2dd506145156
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-11-03 22:20:36 +0300

    Merge with vms_3.1.2
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- client/nx_client_desktop/src/nx/client/desktop/layout_templates/template_layout_builder.cpp
+++ client/nx_client_desktop/src/nx/client/desktop/layout_templates/template_layout_builder.cpp
@@ -8,7 +8,7 @@
 #include <client/client_globals.h>
 #include <client_core/connection_context_aware.h>
 #include <core/resource/avi/filetypesupport.h>
-#include <plugins/resource/avi/avi_resource.h>
+#include <core/resource/avi/avi_resource.h>
 #include <core/resource/resource_directory_browser.h>
 #include <ui/graphics/items/resource/resource_widget.h>
 #include <nx/client/desktop/utils/resource_widget_pixmap_cache.h>
