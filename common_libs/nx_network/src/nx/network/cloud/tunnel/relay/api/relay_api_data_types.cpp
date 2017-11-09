7384c9fdb978ee97e2a43ebd18caa725186398e5
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-11-08 17:24:56 -0800

    #VMS-7656 Added tcp-keep-alive settings to "begin listening" response from traffic relay service.
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_data_types.cpp
+++ common_libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_data_types.cpp
@@ -9,16 +9,35 @@ namespace api {
 
 namespace {
 
-static constexpr const char* preemptiveConnectionCountHeaderName =
+static constexpr const char* kPreemptiveConnectionCountHeaderName =
     "Nx-Relay-Preemptive-Connection-Count";
+static constexpr int kPreemptiveConnectionCountDefault = 7;
+
+static constexpr const char* kTcpKeepAliveHeaderName =
+    "Nx-Relay-Tcp-Connection-Keep-Alive";
 
 } // namespace
+
+BeginListeningResponse::BeginListeningResponse():
+    preemptiveConnectionCount(kPreemptiveConnectionCountDefault)
+{
+}
+
+bool BeginListeningResponse::operator==(const BeginListeningResponse& right) const
+{
+    return preemptiveConnectionCount == right.preemptiveConnectionCount
+        && keepAliveOptions == right.keepAliveOptions;
+}
 
 bool serializeToHeaders(nx_http::HttpHeaders* where, const BeginListeningResponse& what)
 {
     where->emplace(
-        preemptiveConnectionCountHeaderName,
+        kPreemptiveConnectionCountHeaderName,
         nx_http::StringType::number(what.preemptiveConnectionCount));
+
+    if (what.keepAliveOptions)
+        where->emplace(kTcpKeepAliveHeaderName, what.keepAliveOptions->toString().toUtf8());
+
     return true;
 }
 
@@ -30,10 +49,13 @@ bool serializeToHeaders(nx_http::HttpHeaders* where, const CreateClientSessionRe
 
 bool deserializeFromHeaders(const nx_http::HttpHeaders& from, BeginListeningResponse* what)
 {
-    auto it = from.find(preemptiveConnectionCountHeaderName);
-    if (it == from.end())
-        return false;
-    what->preemptiveConnectionCount = it->second.toInt();
+    auto it = from.find(kPreemptiveConnectionCountHeaderName);
+    if (it != from.end())
+        what->preemptiveConnectionCount = it->second.toInt();
+
+    it = from.find(kTcpKeepAliveHeaderName);
+    if (it != from.end())
+        what->keepAliveOptions = KeepAliveOptions::fromString(QString::fromUtf8(it->second));
 
     return true;
 }
