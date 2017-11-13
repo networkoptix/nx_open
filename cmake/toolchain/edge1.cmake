78000d3d881ff81bca9dcb5f7e05c46565f890d8
Denis Klychkov <dklychkov@networkoptix.com>
2017-11-13 15:18:13 +0300

    Build QuaZip with the project
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- cmake/toolchain/edge1.cmake
+++ cmake/toolchain/edge1.cmake
@@ -5,13 +5,18 @@ set(box "edge1")
 set(CMAKE_C_COMPILER "${PACKAGES_DIR}/linux-arm/gcc-7.2.0/bin/arm-unknown-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "${PACKAGES_DIR}/linux-arm/gcc-7.2.0/bin/arm-unknown-linux-gnueabihf-g++")
 
-set(_glib_dir "${PACKAGES_DIR}/linux-arm/glib-2.0")
-set(common_link_flags "-Wl,-rpath-link,${_glib_dir}/lib -latomic")
+set(glib_dir "${PACKAGES_DIR}/linux-arm/glib-2.0")
+set(zlib_dir "${PACKAGES_DIR}/linux-arm/zlib-1.2")
+set(common_link_flags "-Wl,-rpath-link,${glib_dir}/lib")
+string(APPEND common_link_flags " -L${zlib_dir}/lib -Wl,-rpath-link,${zlib_dir}/lib")
+string(APPEND common_link_flags " -latomic")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${common_link_flags}")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "${common_link_flags}")
-set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-a9 -mfpu=vfpv3 -mfpu=neon")
-set(CMAKE_CXX_FLAGS_INIT ${CMAKE_C_FLAGS_INIT})
-unset(_glib_dir)
+
+set(common_c_flags "-mcpu=cortex-a9 -mfpu=vfpv3 -mfpu=neon")
+string(APPEND common_c_flags " -I${zlib_dir}/include")
+set(CMAKE_C_FLAGS_INIT "${common_c_flags}")
+set(CMAKE_CXX_FLAGS_INIT "${common_c_flags}")
 
 # This is required by FindThreads CMake module.
 set(THREADS_PTHREAD_ARG "2" CACHE STRING "Forcibly set by edge1.cmake." FORCE)
