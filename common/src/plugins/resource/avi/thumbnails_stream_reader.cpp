6cca64a90d9d687ef2c2649ef6591998f4f395d1
Alexander Kulikov <akulikov@networkoptix.com>
2017-11-13 18:42:13 +0300

    hnw-5. Review fixes.
    <==== Mercurial data ====>
    Branch: x_hnw_5_missing_modified_archive_files_check

--- common/src/plugins/resource/avi/thumbnails_stream_reader.cpp
+++ common/src/plugins/resource/avi/thumbnails_stream_reader.cpp
@@ -71,7 +71,7 @@ void QnThumbnailsStreamReader::run()
     NX_INFO(this, "Started");
     beforeRun();
 
-    m_delegate->open(getResource(), nullptr);
+    m_delegate->open(getResource(), nullptr /*archiveInterityWatcher*/);
 
     while(!needToStop())
     {
