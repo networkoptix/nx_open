b6e6ea6f1c3495b907e8192d08d7e8300d128d05
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-11-16 22:20:01 +0300

    # nx_kit: Improved cmake files. Unit test files renamed/moved.
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- open/artifacts/nx_kit/unit_tests/disabled_ini_config_ut.h
+++ open/artifacts/nx_kit/unit_tests/disabled_ini_config_ut.h
@@ -1,4 +1,4 @@
 // Copyright 2017 Network Optix, Inc. Licensed under GNU Lesser General Public License version 3.
 #pragma once
 
-int test_ini_config_c();
+DISABLED_INI_CONFIG_UT_API int disabled_ini_config_ut();
--- open/artifacts/nx_kit/unit_tests/ini_config_c_ut.h
+++ open/artifacts/nx_kit/unit_tests/ini_config_c_ut.h
@@ -1,4 +1,4 @@
 // Copyright 2017 Network Optix, Inc. Licensed under GNU Lesser General Public License version 3.
 #pragma once
 
-TEST_DISABLED_INI_CONFIG_API int test_disabled_ini_config();
+int ini_config_c_ut();
