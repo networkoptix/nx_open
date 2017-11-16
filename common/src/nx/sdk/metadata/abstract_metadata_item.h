33336323532130ff2041616a7b0fa86592af5584
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-11-16 22:45:36 +0300

    # Coding style fixes.
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- common/src/nx/sdk/metadata/abstract_metadata_item.h
+++ common/src/nx/sdk/metadata/abstract_metadata_item.h
@@ -8,19 +8,20 @@ namespace sdk {
 namespace metadata {
 
 /**
- * Each class that implements AbstractMetadataItem interface
- * should properly handle this GUID in its queryInterface method.
+ * Each class that implements AbstractMetadataItem interface should properly handle this GUID in
+ * its queryInterface() method.
  */
 static const nxpl::NX_GUID IID_MetadataItem
     = {{0xb3, 0x23, 0x89, 0x1d, 0x19, 0x62, 0x44, 0x3c, 0x84, 0x2a, 0x07, 0x50, 0x7d, 0x87, 0xab, 0x4e}};
 
 /**
- * @brief The AbstractMetadataItem class is an interface for particular
- * item of metadata (e.g. event, detected object) which is contained in a metadata packet.
+ * Interface for particular item of metadata (e.g. event, detected object) which is contained in a 
+ * metadata packet.
  */
 class AbstractMetadataItem: public nxpl::PluginInterface
 {
 public:
+    // TODO: #mike: Rename.
     /**
     * @brief Human readable object type (line crossing | human detected | etc)
     */
