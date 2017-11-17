afc1ec735f101d2c0f882192f0e57bb3a566338d
Nick Hartleb <nhartleb@networkoptix.com>
2017-11-17 14:32:41 -0800

    CLOUD-1514: Refactor - move highlight and escape directives to web_common
    removed log messages
    
    <==== Mercurial data ====>
    Branch: cloud_17.1.1

--- webadmin/app/web_common/scripts/filters/highlight.js
+++ webadmin/app/web_common/scripts/filters/highlight.js
@@ -3,10 +3,8 @@
 angular.module('nxCommon')
   .filter('highlight', ['$sce',function($sce) {
     return function(text, phrase) {
-      console.log(text, phrase);
       if (phrase && phrase!==''){
         text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>')
-        console.log(text);
       }
 
       return $sce.trustAsHtml(text)
