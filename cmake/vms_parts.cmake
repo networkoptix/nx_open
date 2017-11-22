9bffca5f71b15596374ee89f84525746546bea50
Mikhail Uskov <muskov@networkoptix.com>
2017-11-22 21:18:51 +0300

    # withPluginStabs review fix
    <==== Mercurial data ====>
    Branch: vms_3.1.1_dev

--- cmake/vms_parts.cmake
+++ cmake/vms_parts.cmake
@@ -53,7 +53,7 @@ option(withMobileClient "Enable mobile client" ${_withMobileClient})
 option(withClouds "Enable cloud components" ${_withClouds})
 option(withTestCamera "Enable test camera" ${_withTestCamera})
 option(withTests "Enable unit tests" ${_withTests})
-option(withPluginStabs "Enable plugin stabs" ON)
+option(withPluginStubs "Enable plugin stubs" ON)
 
 unset(_withMediaServer)
 unset(_withTrayTool)
