f6cd5e3db4366438ae4038bcba9b953b6ae18711
Sergey Ivanov <sivanov@networkoptix.com>
2017-11-24 16:44:23 +0300

    # Remove bunch of unneeded header includes
    <==== Mercurial data ====>
    Branch: vms

--- common/src/api/app_server_connection.h
+++ common/src/api/app_server_connection.h
@@ -1,25 +1,12 @@
 #pragma once
 
-#include <nx/utils/thread/mutex.h>
-
-#include <utils/common/request_param.h>
-
-#include <core/resource/resource_fwd.h>
-#include <core/misc/schedule_task.h>
-
-#include <licensing/license.h>
-#include <nx_ec/ec_api.h>
-
-#include <api/model/servers_reply.h>
-#include <api/model/connection_info.h>
-
-class QnAppServerConnectionFactory;
-class QnApiSerializer;
+#include <nx_ec/ec_api_fwd.h>
 
 class QN_EXPORT QnAppServerConnectionFactory
 {
 public:
     static void setEc2Connection(const ec2::AbstractECConnectionPtr &connection );
-    static ec2::AbstractECConnectionPtr ec2Connection(); // renamed to show compile errors. use commonModule->ec2Connection() instead
 
+    // Renamed to show compile errors. use commonModule->ec2Connection() instead.
+    static ec2::AbstractECConnectionPtr ec2Connection();
 };
