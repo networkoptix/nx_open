90b3b778041bbfbcdd8767220755fa6da1bf245d
Denis Klychkov <dklychkov@networkoptix.com>
2017-11-28 11:18:58 +0300

    VMS-7692: Add a base class for analytics providers
    <==== Mercurial data ====>
    Branch: vms

---
+++ client/nx_client_core/src/nx/client/core/media/abstract_analytics_metadata_provider.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <analytics/common/object_detection_metadata.h>
+#include "analytics_fwd.h"
+
+namespace nx {
+namespace client {
+namespace core {
+
+class AbstractAnalyticsMetadataProvider
+{
+public:
+    AbstractAnalyticsMetadataProvider();
+    virtual ~AbstractAnalyticsMetadataProvider();
+
+    virtual common::metadata::DetectionMetadataPacketPtr metadata(
+        qint64 timestamp,
+        int channel) = 0;
+};
+
+} // namespace core
+} // namespace client
+} // namespace nx
