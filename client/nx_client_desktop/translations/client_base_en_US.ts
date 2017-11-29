4ffafc9b2a088a2a75c3ab7c29d2bc91d1b7ff0d
Sergey Ivanov <sivanov@networkoptix.com>
2017-11-29 12:26:21 +0300

    Update translatable strings
    <==== Mercurial data ====>
    Branch: vms_3.1

--- client/nx_client_desktop/translations/client_base_en_US.ts
+++ client/nx_client_desktop/translations/client_base_en_US.ts
@@ -6233,10 +6233,6 @@ to %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Storage is full</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Storage is not configured</source>
         <translation type="unfinished"></translation>
     </message>
@@ -6278,10 +6274,6 @@ to %1</source>
     </message>
     <message>
         <source>Rebuilding archive index is canceled by user on the following Server:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Storage is full on the following Server:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
