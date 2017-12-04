8eb18785233dec17b6f2783a5f77e746e68c5c5b
Mikhail Uskov <muskov@networkoptix.com>
2017-12-04 19:17:41 +0300

    VMS-7700 2way audio for DW
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- common/src/core/resource/camera_resource.h
+++ common/src/core/resource/camera_resource.h
@@ -100,6 +100,7 @@ public:
 
 protected:
     virtual CameraDiagnostics::Result initInternal() override;
+    bool initializeBasicTwoWayAudio();
 private:
     void saveResolutionList( const CameraMediaStreams& supportedNativeStreams );
 private:
