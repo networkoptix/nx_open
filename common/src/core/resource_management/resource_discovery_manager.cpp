64ecd2aaa93b57ed151cffb2d51ec7423cee71e3
Mikhail Uskov <muskov@networkoptix.com>
2017-12-05 16:44:55 +0300

    # VMS-7574 review fix
    <==== Mercurial data ====>
    Branch: vms_3.1_dev

--- common/src/core/resource_management/resource_discovery_manager.cpp
+++ common/src/core/resource_management/resource_discovery_manager.cpp
@@ -365,8 +365,8 @@ void QnResourceDiscoveryManager::appendManualDiscoveredResources(QnResourceList&
     QMap<QString, QnManualCameraInfo> camerasByUrlForSearch;
     for (const auto& manualCamera: manualCameraByUniqueId)
     {
-        const auto camera = qSharedPointerDynamicCast<QnSecurityCamResource>(
-            commonModule()->resourcePool()->getResourceByUniqueId(manualCamera.uniqueId));
+        const auto camera = commonModule()->resourcePool()->getResourceByUniqueId(manualCamera.uniqueId)
+            .dynamicCast<QnSecurityCamResource>();
 
         if (!camera || !camera->hasFlags(Qn::foreigner) || canTakeForeignCamera(camera, 0))
             camerasByUrlForSearch.insert(manualCamera.url.toString(), manualCamera);
@@ -605,7 +605,7 @@ int QnResourceDiscoveryManager::registerManualCameras(const std::vector<QnManual
     int addedCount = 0;
     for (const auto& camera: cameras)
     {
-        for (const auto& searcher : m_searchersList)
+        for (const auto& searcher: m_searchersList)
         {
             if (!camera.resType || !searcher->isResourceTypeSupported(camera.resType->getId()))
                 continue;
