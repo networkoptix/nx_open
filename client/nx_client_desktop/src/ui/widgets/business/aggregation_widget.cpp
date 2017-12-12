b98f0a868a5aa4fdef7da2ea7dc3a7524663acc2
Dmitry Kargin <dkargin@networkoptix.com>
2017-12-12 14:19:05 +0300

    [VMS-7888] Implementing a widget for editing time durations
    Added QnTimeDurationWidget. UI is generated from time_duration_widget.ui
    
    Widget is used as part of:
     - AggregationWidget
     - QnCameraOutputBusinessActionWidget
    
    <==== Mercurial data ====>
    Branch: vms

--- client/nx_client_desktop/src/ui/widgets/business/aggregation_widget.cpp
+++ client/nx_client_desktop/src/ui/widgets/business/aggregation_widget.cpp
@@ -7,13 +7,6 @@
 
 namespace {
     const int defaultOptimalWidth = 300;
-
-    enum {
-        second = 1,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24
-    };
 }
 
 QnAggregationWidget::QnAggregationWidget(QWidget *parent) :
@@ -26,42 +19,17 @@ QnAggregationWidget::QnAggregationWidget(QWidget *parent) :
     minSize.setWidth(defaultOptimalWidth);
     setMinimumSize(minSize);
 
-    ui->periodComboBox->addItem(QnTimeStrings::longSuffix(QnTimeStrings::Suffix::Seconds), second);
-    ui->periodComboBox->addItem(QnTimeStrings::longSuffix(QnTimeStrings::Suffix::Minutes), minute);
-    ui->periodComboBox->addItem(QnTimeStrings::longSuffix(QnTimeStrings::Suffix::Hours), hour);
-    ui->periodComboBox->addItem(QnTimeStrings::longSuffix(QnTimeStrings::Suffix::Days), day);
-
     // initial state: checkbox is cleared
     ui->periodWidget->setVisible(false);
+    ui->periodWidget->addDurationSuffix(QnTimeStrings::Suffix::Minutes);
+    ui->periodWidget->addDurationSuffix(QnTimeStrings::Suffix::Hours);
+    ui->periodWidget->addDurationSuffix(QnTimeStrings::Suffix::Days);
 
     connect(ui->enabledCheckBox,    &QCheckBox::toggled,    ui->periodWidget,   &QWidget::setVisible);
     connect(ui->enabledCheckBox,    &QCheckBox::toggled,    ui->instantLabel,   &QLabel::setHidden);
     connect(ui->enabledCheckBox,    &QCheckBox::toggled,    this,               &QnAggregationWidget::valueChanged);
 
-    connect(ui->periodComboBox,     QnComboboxCurrentIndexChanged, this, [this]() {
-        updateMinimumValue();
-        emit valueChanged();
-    });
-
-    connect(ui->valueSpinBox,       QnSpinboxIntValueChanged, this, [this](int value) {
-        if (value == 0) {
-            int index = ui->periodComboBox->currentIndex();
-            NX_ASSERT(index > 0, Q_FUNC_INFO, "Invalid minimum value for the minimal period.");
-            if (index == 0) {
-                ui->valueSpinBox->setValue(1);
-            } else {
-                ui->periodComboBox->blockSignals(true);
-                ui->periodComboBox->setCurrentIndex(index - 1);
-                ui->periodComboBox->blockSignals(false);
-                updateMinimumValue();
-
-                int intervalSec = ui->periodComboBox->itemData(index).toInt();
-                int value = intervalSec / ui->periodComboBox->itemData(index - 1).toInt();
-                ui->valueSpinBox->setValue(value - 1);
-            }
-        }
-        emit valueChanged();
-    });
+    connect(ui->periodWidget, SIGNAL(valueChanged()), this, SIGNAL(valueChanged()));
 }
 
 QnAggregationWidget::~QnAggregationWidget()
@@ -70,29 +38,18 @@ QnAggregationWidget::~QnAggregationWidget()
 
 void QnAggregationWidget::setValue(int secs) {
     ui->enabledCheckBox->setChecked(secs > 0);
-    if (secs > 0) {
-        int idx = 0;
-        while (idx < ui->periodComboBox->count() - 1
-               && secs >= ui->periodComboBox->itemData(idx + 1).toInt()
-               && secs  % ui->periodComboBox->itemData(idx + 1).toInt() == 0)
-        {
-            idx++;
-        }
-
-        ui->periodComboBox->setCurrentIndex(idx);
-        ui->valueSpinBox->setValue(secs / ui->periodComboBox->itemData(idx).toInt());
-    }
+    ui->periodWidget->setValue(secs);
 }
 
 int QnAggregationWidget::value() const {
     if (!ui->enabledCheckBox->isChecked())
         return 0;
-    return  ui->valueSpinBox->value() * ui->periodComboBox->itemData(ui->periodComboBox->currentIndex()).toInt();
+    return ui->periodWidget->value();
 }
 
 QWidget *QnAggregationWidget::lastTabItem()
 {
-    return ui->periodComboBox;
+    return ui->periodWidget->lastTabItem();
 }
 
 void QnAggregationWidget::setShort(bool value) {
@@ -101,8 +58,4 @@ void QnAggregationWidget::setShort(bool value) {
 
 int QnAggregationWidget::optimalWidth() {
     return defaultOptimalWidth;
-}
-
-void QnAggregationWidget::updateMinimumValue() {
-    ui->valueSpinBox->setMinimum(ui->periodComboBox->currentIndex() == 0 ? 1 : 0);
 }
