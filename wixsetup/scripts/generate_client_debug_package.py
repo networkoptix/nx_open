867728c257272c4a42c29cabf1945ac41b9df5d1
Sergey Ivanov <sivanov@networkoptix.com>
2017-12-13 16:32:06 +0300

    VMS-7867: Generate client debug package with CMake
    <==== Mercurial data ====>
    Branch: vms_3.2

---
+++ wixsetup/scripts/generate_client_debug_package.py
@@ -0,0 +1,37 @@
+#!/bin/python2
+
+import argparse
+import os
+import zipfile
+import yaml
+
+
+def zip_files(zip, files, rel_path, target_path='.'):
+    for file in files:
+        zip.write(file, os.path.join(target_path, os.path.relpath(file, rel_path)))
+
+
+def create_client_debug_file(binaries_dir, output_filename):
+    pdb_filename = 'desktop_client.pdb'
+    with zipfile.ZipFile(output_filename, "w", zipfile.ZIP_DEFLATED) as zip:
+        zip.write(os.path.join(binaries_dir, pdb_filename), pdb_filename)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help="Config file", required=True)
+    parser.add_argument('--output', help="Output directory", required=True)
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as f:
+        config = yaml.load(f)
+
+    output_filename = os.path.join(
+        args.output, config['client_debug_distribution_name']) + '.zip'
+    create_client_debug_file(
+        binaries_dir=config['bin_source_dir'],
+        output_filename=output_filename)
+
+
+if __name__ == '__main__':
+    main()
