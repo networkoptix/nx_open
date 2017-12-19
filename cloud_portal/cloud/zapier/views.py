7d2355bc5c2e76f6b9e02f76f4244e5702a9a7aa
Nick Hartleb <nhartleb@networkoptix.com>
2017-12-19 15:11:25 -0800

    ##hotfix for zapier added rest_framework.status
    <==== Mercurial data ====>
    Branch: cloud_17.1

--- cloud_portal/cloud/zapier/views.py
+++ cloud_portal/cloud/zapier/views.py
@@ -1,5 +1,6 @@
 import django, json, base64, urllib, uuid
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
