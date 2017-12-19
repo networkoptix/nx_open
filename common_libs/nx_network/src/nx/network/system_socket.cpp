b86918941809554c05a044c4513f94d2238b0c6a
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2017-12-19 11:15:24 -0800

    Fixed link error on clang-900
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/system_socket.cpp
+++ common_libs/nx_network/src/nx/network/system_socket.cpp
@@ -1859,6 +1859,7 @@ int UDPSocket::recvFrom(
 
 template class Socket<AbstractStreamServerSocket>;
 template class Socket<AbstractStreamSocket>;
+template class Socket<AbstractDatagramSocket>;
 template class CommunicatingSocket<AbstractStreamSocket>;
 template class CommunicatingSocket<AbstractDatagramSocket>;
 
