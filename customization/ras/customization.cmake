f25b9774498ce0d1204447e8327b78b8f1eb7419
Denis Klychkov <dklychkov@networkoptix.com>
2017-12-21 14:20:14 +0300

    Possible fix for iOS application name
    For some reason iOS 11 removes spaces from the bundle name.
    My guess it removes spaces from CFBundleName and leaves CFDisplayBundleName untouched. However
    these values were equal and referenced the same value in the Info.plist. Probably
    CFDisplayBundleName is affected by mistake. This commit separates these two values.
        CFBundleName should contain no spaces and must be not longer that 16 characters.
        CFDisplayBundleName is the actual displayed name.
    
    <==== Mercurial data ====>
    Branch: mobile_17.3.1

--- customization/ras/customization.cmake
+++ customization/ras/customization.cmake
@@ -3,7 +3,8 @@ set(parent.customization "default")
 
 # Components Namings
 set(display.product.name "Sefica ProBox")
-set(display.mobile.name "Sefica ProBox Mobile")
+set(display_mobile_bundle_name "Sefica ProBox Mobile")
+set(mobile_bundle_name "ProBox")
 set(product.name "Sefica ProBox")
 set(product.name.short "ras")
 set(product.appName "probox")
