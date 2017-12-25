f135c03a8e9c512a2160dda962eacdbf29893686
Alexander Kulikov <akulikov@networkoptix.com>
2017-12-25 19:51:10 +0300

    VMS-7973: Implement check for update rest handler
    <==== Mercurial data ====>
    Branch: x_vms_7771_getting_updates_info

--- unit_tests/mediaserver_core_ut/src/nx/mediaserver/rest/updates2_rest_handler_ut.cpp
+++ unit_tests/mediaserver_core_ut/src/nx/mediaserver/rest/updates2_rest_handler_ut.cpp
@@ -5,6 +5,7 @@
 #include <nx/update/info/update_request_data.h>
 #include <test_support/mediaserver_launcher.h>
 #include <nx/network/http/http_client.h>
+#include <nx/api/updates2/available_update_info_data.h>
 
 namespace nx {
 namespace mediaserver {
@@ -13,6 +14,11 @@ namespace test {
 
 using namespace update::info::detail::data_provider;
 static QString urlPattern = "http://admin:admin@127.0.0.1/api/updates2";
+
+static const QString kDefaultCustomization = "default";
+static const QString kNonExistentCustomization = "non-existent";
+static const QString kDefaultCloudHost = "nxvms.com";
+static const QnSoftwareVersion kDefaultVersion = QnSoftwareVersion(2, 0);
 
 class Updates2RestHandler: public ::testing::Test
 {
@@ -41,28 +47,38 @@ protected:
             });
     }
 
-    void whenGetRequestIssued()
+    void whenGetRequestIssuedAndResponseReveived()
     {
         nx::utils::Url getUrl(urlPattern);
         getUrl.setPort(m_mediaServerLauncher->port());
         m_requestResult = m_httpClient.doGet(getUrl);
     }
 
-    void thenItShouldBeSuccessfull()
+    void andWhenResponseBodyParsed()
+    {
+        const auto optionalBody = m_httpClient.fetchEntireMessageBody();
+        ASSERT_TRUE(static_cast<bool>(optionalBody));
+        ASSERT_FALSE(optionalBody->isEmpty());
+        parseBody(*optionalBody);
+    }
+
+    void thenItShouldBeSuccessfull() const
     {
         ASSERT_TRUE(m_requestResult);
         const nx_http::Response* response = m_httpClient.response();
         ASSERT_TRUE(response);
         ASSERT_EQ(nx_http::StatusCode::ok ,response->statusLine.statusCode);
+    }
 
-        ASSERT_TRUE(static_cast<bool>(m_httpClient.fetchEntireMessageBody()));
-        const auto body = *m_httpClient.fetchEntireMessageBody();
-        qWarning() << body;
+    void thenContentShouldHaveVersion(const QnSoftwareVersion& version) const
+    {
+        ASSERT_EQ(version, QnSoftwareVersion(m_responseUpdatesInfo.version));
     }
 private:
     std::unique_ptr<MediaServerLauncher> m_mediaServerLauncher = nullptr;
     nx_http::HttpClient m_httpClient;
     bool m_requestResult = false;
+    api::AvailableUpdatesInfo m_responseUpdatesInfo;
 
     void setUpMediaServer()
     {
@@ -78,12 +94,35 @@ private:
                 return std::make_unique<test_support::AsyncJsonProviderMockup>(handler);
             });
     }
+
+    void parseBody(const QByteArray& rawData)
+    {
+        QnJsonRestResult restResult;
+        ASSERT_TRUE(QJson::deserialize(rawData, &restResult));
+        ASSERT_EQ(QnRestResult::Error::NoError, restResult.error);
+        ASSERT_TRUE(
+            QJson::deserialize<api::AvailableUpdatesInfo>(
+                restResult.reply,
+                &m_responseUpdatesInfo));
+    }
 };
 
-TEST_F(Updates2RestHandler, updateRequestProcessedByServer)
+TEST_F(Updates2RestHandler, checkResults_Ok)
 {
-    whenGetRequestIssued();
+    whenRequestDataMockedUpWith(kDefaultCloudHost, kDefaultCustomization, kDefaultVersion);
+    whenGetRequestIssuedAndResponseReveived();
     thenItShouldBeSuccessfull();
+    andWhenResponseBodyParsed();
+    thenContentShouldHaveVersion(QnSoftwareVersion(3, 1, 0, 16975));
+}
+
+TEST_F(Updates2RestHandler, checkResults_Null_WrongCustomization)
+{
+    whenRequestDataMockedUpWith(kDefaultCloudHost, kNonExistentCustomization, kDefaultVersion);
+    whenGetRequestIssuedAndResponseReveived();
+    thenItShouldBeSuccessfull();
+    andWhenResponseBodyParsed();
+    thenContentShouldHaveVersion(QnSoftwareVersion());
 }
 
 } // namespace test
