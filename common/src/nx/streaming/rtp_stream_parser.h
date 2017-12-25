6d2498e0ccd38719017678fe7b395c02f8db0985
Sergey Ivanov <sivanov@networkoptix.com>
2017-12-25 11:53:56 +0300

    Compile fix
    <==== Mercurial data ====>
    Branch: vms_3.1

--- common/src/nx/streaming/rtp_stream_parser.h
+++ common/src/nx/streaming/rtp_stream_parser.h
@@ -117,7 +117,7 @@ protected:
         quint8* bufferStart = nullptr;
         int bufferOffset = 0;
         quint16 len = 0;
-        bool nalStart = nullptr;
+        bool nalStart = false;
     };
 
 protected:
