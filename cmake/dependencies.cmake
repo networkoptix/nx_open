5b7be304e5e1eeb5127ea4d6085e5cd717918ada
Mike Shevchenko <mshevchenko@networkoptix.com>
2017-12-28 01:09:34 +0300

    edge1 build fixed
    <==== Mercurial data ====>
    Branch: vms

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -59,7 +59,7 @@ function(nx_detect_package_versions)
 
     if(box STREQUAL "edge1")
         _set_version(ffmpeg "3.1.1")
-        _set_version(qt "5.6.1")
+        _set_version(qt "5.6.3")
         _set_version(openssl "1.0.1f")
     endif()
 
