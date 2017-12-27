77a5806eaf8125ab148970210bcd847fa660c21d
Denis Klychkov <dklychkov@networkoptix.com>
2017-12-27 12:36:24 +0300

    VMS-CR-8222: Fix a typo in a comment
    <==== Mercurial data ====>
    Branch: vms

--- client/nx_client_desktop/src/ini.h
+++ client/nx_client_desktop/src/ini.h
@@ -26,7 +26,7 @@ struct Ini: public nx::kit::IniConfig
     NX_INI_FLAG(0, hideEnhancedVideo, "Hide enhanced video from the scene.");
     NX_INI_FLAG(0, redesignedCameraSettingsDialog, "Enable redesigned camera settings dialog.");
     NX_INI_FLAG(1, enableDetectedObjectsInterpolation, "Allow interpolation of detected objects between frames.");
-    NX_INI_FLAG(0, displayAnalyticsDelay, "Add delay lable to detected object description.");
+    NX_INI_FLAG(0, displayAnalyticsDelay, "Add delay label to detected object description.");
     NX_INI_FLAG(0, enableProgressInformers, "Enable global operation progress informers in the notification panel.");
     NX_INI_FLAG(0, enableDeviceSearch, "Enable reworked device search dialog");
 };
