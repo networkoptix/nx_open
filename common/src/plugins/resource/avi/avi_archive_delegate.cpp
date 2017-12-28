cfd4ffca0034abc4d4bc4f13906bb50b10e715a7
Roman Vasilenko <rvasilenko@networkoptix.com>
2017-12-28 20:00:24 +0300

    VMS-7906: Support G.726 playback
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common/src/plugins/resource/avi/avi_archive_delegate.cpp
+++ common/src/plugins/resource/avi/avi_archive_delegate.cpp
@@ -235,8 +235,11 @@ QnAbstractMediaDataPtr QnAviArchiveDelegate::getNextData()
                     stream->codec->bits_per_coded_sample == 16)
                 {
                     // Workaround for ffmpeg bug. It losts 'bits_per_coded_sample' field when saves G726 to the MKV.
+                    // Valid range for this field is [2..5]. Try to restore value from field 'block_align' if possible.
+                    // Otherwise use default value 4. Value 2 can't be restored.
                     // https://ffmpeg.org/pipermail/ffmpeg-devel/2014-January/153139.html
-                    stream->codec->bits_per_coded_sample = packet.size / 100;
+                    stream->codec->bits_per_coded_sample =
+                        stream->codec->block_align > 1 ? stream->codec->block_align : 4;
                 }
 
 
