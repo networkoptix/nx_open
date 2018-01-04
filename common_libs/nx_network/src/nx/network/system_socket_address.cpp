debc479dc506c12a2318c5e750f8fa1541d86542
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-01-04 15:59:18 -0800

    VMS-8085 Fixed strict aliasing rule violation
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common_libs/nx_network/src/nx/network/system_socket_address.cpp
+++ common_libs/nx_network/src/nx/network/system_socket_address.cpp
@@ -9,8 +9,9 @@ namespace nx {
 namespace network {
 
 SystemSocketAddress::SystemSocketAddress():
-    size(0)
+    m_sockaddrLen(0)
 {
+    memset(&m_sockaddr, 0, sizeof(m_sockaddr));
 }
 
 SystemSocketAddress::SystemSocketAddress(int ipVersion):
@@ -21,6 +22,7 @@ SystemSocketAddress::SystemSocketAddress(int ipVersion):
 SystemSocketAddress::SystemSocketAddress(SocketAddress endpoint, int ipVersion):
     SystemSocketAddress()
 {
+    // TODO: #ak Following check does not belong here.
     if (SocketGlobals::ini().isHostDisabled(endpoint.address))
     {
         SystemError::setLastErrorCode(SystemError::noPermission);
@@ -31,14 +33,11 @@ SystemSocketAddress::SystemSocketAddress(SocketAddress endpoint, int ipVersion):
     {
         if (const auto ip = endpoint.address.ipV4())
         {
-            const auto a = new sockaddr_in;
-            memset(a, 0, sizeof(*a));
-            ptr.reset((sockaddr*)a);
-            size = sizeof(sockaddr_in);
+            m_sockaddrLen = sizeof(m_sockaddr.v4);
 
-            a->sin_family = AF_INET;
-            a->sin_addr = *ip;
-            a->sin_port = htons(endpoint.port);
+            m_sockaddr.v4.sin_family = AF_INET;
+            m_sockaddr.v4.sin_addr = *ip;
+            m_sockaddr.v4.sin_port = htons(endpoint.port);
             return;
         }
     }
@@ -46,14 +45,11 @@ SystemSocketAddress::SystemSocketAddress(SocketAddress endpoint, int ipVersion):
     {
         if (const auto ip = endpoint.address.ipV6())
         {
-            const auto a = new sockaddr_in6;
-            memset(a, 0, sizeof(*a));
-            ptr.reset((sockaddr*)a);
-            size = sizeof(sockaddr_in6);
+            m_sockaddrLen = sizeof(m_sockaddr.v6);
 
-            a->sin6_family = AF_INET6;
-            a->sin6_addr = *ip;
-            a->sin6_port = htons(endpoint.port);
+            m_sockaddr.v6.sin6_family = AF_INET6;
+            m_sockaddr.v6.sin6_addr = *ip;
+            m_sockaddr.v6.sin6_port = htons(endpoint.port);
             return;
         }
     }
@@ -63,22 +59,36 @@ SystemSocketAddress::SystemSocketAddress(SocketAddress endpoint, int ipVersion):
 
 SocketAddress SystemSocketAddress::toSocketAddress() const
 {
-    if (!ptr)
+    if (m_sockaddrLen == 0)
         return SocketAddress();
 
-    if (ptr->sa_family == AF_INET)
-    {
-        const auto a = reinterpret_cast<const sockaddr_in*>(ptr.get());
-        return SocketAddress(a->sin_addr, ntohs(a->sin_port));
-    }
-    else if (ptr->sa_family == AF_INET6)
-    {
-        const auto a = reinterpret_cast<const sockaddr_in6*>(ptr.get());
-        return SocketAddress(a->sin6_addr, ntohs(a->sin6_port));
-    }
+    if (m_sockaddr.untyped.sa_family == AF_INET)
+        return SocketAddress(m_sockaddr.v4.sin_addr, ntohs(m_sockaddr.v4.sin_port));
+    else if (m_sockaddr.untyped.sa_family == AF_INET6)
+        return SocketAddress(m_sockaddr.v6.sin6_addr, ntohs(m_sockaddr.v6.sin6_port));
 
-    NX_ASSERT(false, lm("Corupt family: %1").arg(ptr->sa_family));
+    NX_ASSERT(false, lm("Unknown socket family type: %1").arg(m_sockaddr.untyped.sa_family));
     return SocketAddress();
+}
+
+const struct sockaddr* SystemSocketAddress::addr() const
+{
+    return &m_sockaddr.untyped;
+}
+
+struct sockaddr* SystemSocketAddress::addr()
+{
+    return &m_sockaddr.untyped;
+}
+
+const socklen_t& SystemSocketAddress::addrLen() const
+{
+    return m_sockaddrLen;
+}
+
+socklen_t& SystemSocketAddress::addrLen()
+{
+    return m_sockaddrLen;
 }
 
 } // namespace network
