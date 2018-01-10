38480fbb40894e3c3a54b57a41e3be9daf28e53e
Alexander Kulikov <akulikov@networkoptix.com>
2018-01-10 19:58:38 +0300

    VMS-8090. Fix storage manager related crash on abnormal server shutdown
    <==== Mercurial data ====>
    Branch: vms_3.1.1_release

--- mediaserver_core/src/recorder/storage_manager.cpp
+++ mediaserver_core/src/recorder/storage_manager.cpp
@@ -1206,7 +1206,7 @@ void QnStorageManager::removeAbsentStorages(const QnStorageResourceList &newStor
 
 QnStorageManager::~QnStorageManager()
 {
-    // these threads below should've been stopped and destroyed manually by this moment
+    // These threads below should've been stopped and destroyed manually by this moment.
     {
         QnMutexLocker lock(&m_testStorageThreadMutex);
         NX_ASSERT(!m_testStorageThread);
