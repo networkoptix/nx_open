d0a1821e17c3adb926b9d00ae0b15d1d8e171760
Denis Klychkov <dklychkov@networkoptix.com>
2018-01-15 17:43:36 +0300

    CMake: Create and use a temporary keychain for codesigning Client in MacOS
    Since this commit MacOS distribution building script is able to use p12 certificates
    automatically whitout a need to import them into Login keychain by hand.
    To acieve this a special script creates a temporary keychain nx_build and imports
    the required certificate into it. DMG building script is modified to use this keychain
    when it is available.
    
    Certificates are searched in RDEP packages "any/certificates-${customization}" in the "macosx"
    directory.
    
    <==== Mercurial data ====>
    Branch: vms_3.2

---
+++ cmake/macosx_keychain.cmake
@@ -0,0 +1,6 @@
+cmake_dependent_option(useLoginKeychain "Use Login keychain and do not create a temporary one"
+    ON "developerBuild"
+    OFF
+)
+set(codeSigningKeychainName "nx_build" CACHE INTERNAL "")
+set(codeSigningKeychainPassword "qweasd123" CACHE INTERNAL "")
