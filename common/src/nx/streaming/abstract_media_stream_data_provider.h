a5ea3e2b897b95a085c19397ecf4554683bab2ce
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-01-16 18:20:10 +0300

    Coding style fixed
    <==== Mercurial data ====>
    Branch: vms

--- common/src/nx/streaming/abstract_media_stream_data_provider.h
+++ common/src/nx/streaming/abstract_media_stream_data_provider.h
@@ -1,5 +1,4 @@
-#ifndef ABSTRACT_MEDIA_STREAM_DATA_PROVIDER
-#define ABSTRACT_MEDIA_STREAM_DATA_PROVIDER
+#pragma once
 
 #include <QtCore/QSharedPointer>
 #include <nx/streaming/media_stream_statistics.h>
@@ -10,16 +9,15 @@
 class QnResourceVideoLayout;
 class QnResourceAudioLayout;
 
-
 #define MAX_LIVE_FPS 10000000.0
 #define MAX_LOST_FRAME 2
 
-class QnAbstractMediaStreamDataProvider : public QnAbstractStreamDataProvider
+class QnAbstractMediaStreamDataProvider: public QnAbstractStreamDataProvider
 {
     Q_OBJECT;
 
 public:
-    explicit QnAbstractMediaStreamDataProvider( const QnResourcePtr& res );
+    explicit QnAbstractMediaStreamDataProvider(const QnResourcePtr& res);
     virtual ~QnAbstractMediaStreamDataProvider();
 
     const QnMediaStreamStatistics* getStatistics(int channel) const;
@@ -35,11 +33,10 @@ public:
 
     virtual QnConstMediaContextPtr getCodecContext() const;
 
-    //!Tests connection to media stream
-    /*!
-        Blocks for media stream open attempt.
-        Default implementation returns notImplemented result
-    */
+    /**
+     * Tests connection to a media stream. Blocks for media stream open attempt. Default
+     * implementation returns notImplemented.
+     */
     virtual CameraDiagnostics::Result diagnoseMediaStreamConnection();
 
     virtual bool hasThread() const { return true; }
@@ -51,11 +48,13 @@ protected:
     virtual void afterRun();
 
     virtual bool beforeGetData() { return true; }
-    // if function returns false we do not put result into the queues
+
+    // If the function returns false, we do not put the result into the queues.
     virtual bool afterGetData(const QnAbstractDataPacketPtr& data);
-    
-    void checkTime( const QnAbstractMediaDataPtr& data );
+
+    void checkTime(const QnAbstractMediaDataPtr& data);
     void resetTimeCheck();
+
 protected:
     QnMediaStreamStatistics m_stat[CL_MAX_CHANNEL_NUMBER];
     int m_gotKeyFrame[CL_MAX_CHANNEL_NUMBER];
@@ -65,10 +64,8 @@ protected:
 
 private:
     mutable int m_numberOfchannels;
-    qint64 m_lastMediaTime[CL_MAX_CHANNELS+1]; // max video channels + audio channel
+    qint64 m_lastMediaTime[CL_MAX_CHANNELS + 1]; //< max video channels + audio channel
     bool m_isCamera;
 };
 
 typedef QSharedPointer<QnAbstractMediaStreamDataProvider> QnAbstractMediaStreamDataProviderPtr;
-
-#endif // ABSTRACT_MEDIA_STREAM_DATA_PROVIDER
