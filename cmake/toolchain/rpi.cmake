1a8dc277e0715bee54c3c09e09491046528c6539
Mikhail Uskov <muskov@networkoptix.com>
2018-01-17 16:34:12 +0300

    VMS-7926: Fix cloud connect for ARM devices by openssl update.
    <==== Mercurial data ====>
    Branch: vms_3.1

--- cmake/toolchain/rpi.cmake
+++ cmake/toolchain/rpi.cmake
@@ -4,3 +4,10 @@ set(box "rpi")
 
 set(CMAKE_C_COMPILER "${PACKAGES_DIR}/${box}/gcc-4.9.3/bin/arm-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "${PACKAGES_DIR}/${box}/gcc-4.9.3/bin/arm-linux-gnueabihf-g++")
+
+include_directories(SYSTEM "${PACKAGES_DIR}/${box}/sysroot/usr/include")
+
+set(_sysroot_libs_dir "${PACKAGES_DIR}/${box}/sysroot/usr/lib/arm-linux-gnueabihf")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-rpath-link,${_sysroot_libs_dir} -L${_sysroot_libs_dir}")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-Wl,-rpath-link,${_sysroot_libs_dir} -L${_sysroot_libs_dir}")
+unset(_sysroot_libs_dir)
