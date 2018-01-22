9dd348fabc81890d0d08e80fa026a7154084a47b
Kyle Epperson <kepperson@networkoptix.com>
2018-01-22 15:59:07 -0800

    CLOUD-1614: Added custom library file for custom keywords
    <==== Mercurial data ====>
    Branch: cloud_17.1.1

---
+++ cloud_portal/robot_tests/NoptixLibrary/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+import time
+import types
+
+from robot.utils import NormalizedDict
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from SeleniumLibrary.base import keyword, LibraryComponent
+from SeleniumLibrary.locators import WindowManager
+from SeleniumLibrary.utils import (is_falsy, is_truthy, secs_to_timestr,
+                                   timestr_to_secs, SELENIUM_VERSION)
+
+from random import *
+
+class NoptixLibrary(object):
+
+    def copy_text(self, locator):
+        locator.send_keys(Keys.CONTROL+'a')
+        locator.send_keys(Keys.CONTROL+'c')
+
+    def paste_text(self, locator):
+        locator.send_keys(Keys.CONTROL+'v')
+
+    def get_random_email(self):
+        email = "noptixqa+" + str(time.time()) + "@gmail.com"
+        return email
