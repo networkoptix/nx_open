3ea62c2afd2d0f608aef14ba1bfafb8c6ba8ae63
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-01-22 15:57:52 -0800

    CLOUD-1618 Fixed: non-unique temporary credentials could be generated. Guid is now used to make it unique.
    <==== Mercurial data ====>
    Branch: cloud_17.1

--- nx_cloud/libcloud_db/src/nx/cloud/cdb/managers/temporary_account_password_manager.cpp
+++ nx_cloud/libcloud_db/src/nx/cloud/cdb/managers/temporary_account_password_manager.cpp
@@ -12,11 +12,11 @@
 #include <nx/network/http/auth_tools.h>
 #include <nx/utils/cryptographic_random_device.h>
 #include <nx/utils/log/log.h>
-#include <nx/utils/random.h>
 #include <nx/utils/std/future.h>
 #include <nx/utils/string.h>
 #include <nx/utils/time.h>
 #include <nx/utils/scope_guard.h>
+#include <nx/utils/uuid.h>
 
 #include "../stree/cdb_ns.h"
 
@@ -221,18 +221,9 @@ boost::optional<TemporaryAccountCredentialsEx>
 
 std::string TemporaryAccountPasswordManager::generateRandomPassword() const
 {
-    std::string password(
-        nx::utils::random::number(
-            nx::utils::random::CryptographicRandomDevice::instance(), 10U, 20U),
-        'a');
-    std::generate(
-        password.begin(), password.end(),
-        [this]()
-        {
-            return nx::utils::random::number(
-                nx::utils::random::CryptographicRandomDevice::instance(), (int)'a', (int)'z');
-        });
-    return password;
+    auto str = QnUuid::createUuid().toSimpleByteArray();
+    str.replace('-', "");
+    return str.toStdString();
 }
 
 bool TemporaryAccountPasswordManager::isTemporaryPasswordExpired(
