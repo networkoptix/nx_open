7206ccdd4fc2518767df1ecbef01b6560f5792fc
Denis Klychkov <dklychkov@networkoptix.com>
2018-01-23 11:24:12 +0300

    CMake: Move include(utils) back to the common includes list
    utils.cmake sets some variables related to CMake multi-configuration mode. The related CMake
    variables are initialized by the root project() call, so utils.cmake must be included after it.
    
    The script was moved to the beginning of the root CMakeLists.txt because of nx_option function.
    This function is now moved to a separate script.
    
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -29,20 +29,6 @@ function(nx_set_variable_if_empty variable value)
     endif()
 endfunction()
 
-function(nx_to_cpp_compatible_bool var)
-    if(${var})
-        set(${var} "true" PARENT_SCOPE)
-    else()
-        set(${var} "false" PARENT_SCOPE)
-    endif()
-endfunction()
-
-function(nx_option option_name)
-    option(${option_name} ${ARGN})
-    nx_to_cpp_compatible_bool(${option_name})
-    set(${option_name} ${${option_name}} PARENT_SCOPE)
-endfunction()
-
 function(nx_init_known_files_list)
     get_property(property_set GLOBAL PROPERTY known_files SET)
 
