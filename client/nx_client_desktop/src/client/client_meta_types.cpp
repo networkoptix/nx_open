b666d4e2797bd7c2e84caa1b28ff29d56d9c668b
Alexander Fokin <afokin@networkoptix.com>
2018-01-23 18:17:36 +0300

    VMS-8070 Proper include for FileUpload.
    <==== Mercurial data ====>
    Branch: vms_3.1

--- client/nx_client_desktop/src/client/client_meta_types.cpp
+++ client/nx_client_desktop/src/client/client_meta_types.cpp
@@ -23,7 +23,7 @@
 #include <nx/fusion/serialization/json_functions.h>
 #include <utils/ping_utility.h>
 #include <nx/client/desktop/utils/server_file_cache.h>
-#include <nx/client/desktop/utils/upload_worker.h>
+#include <nx/client/desktop/utils/file_upload.h>
 
 #include <nx/cloud/cdb/api/result_code.h>
 #include <nx/cloud/cdb/api/system_data.h>
