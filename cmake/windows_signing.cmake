6465cc9776a57b859b0754ccd4adc599d837f0c0
Sergey Ivanov <sivanov@networkoptix.com>
2018-01-23 17:37:37 +0300

    VMS-8131: Sign windows binaries after build
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

---
+++ cmake/windows_signing.cmake
@@ -0,0 +1,24 @@
+find_program(SIGNTOOL_EXECUTABLE signtool.exe ${signtool_directory})
+if(NOT SIGNTOOL_EXECUTABLE)
+    message(FATAL_ERROR "Cannot find signtool.exe")
+endif()
+
+function(nx_sign_windows_executable target)
+    nx_find_first_matching_file(certificate_file "${certificates_path}/wixsetup/*.p12")
+    if(NOT certificate_file)
+        message(FATAL_ERROR "Cannot find certificate file in ${certificates_path}/wixsetup/")
+    endif()
+
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND ${SIGNTOOL_EXECUTABLE} sign
+            /td sha256
+            /fd sha256
+            /tr "http://tsa.startssl.com/rfc3161"
+            /v
+            /a
+            /f ${certificate_file}
+            /p ${sign.password}
+            /d "${company.name} ${display.product.name}"
+            $<TARGET_FILE:${target}>
+    )
+endfunction()
