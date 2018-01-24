755b145cd2502cd1054161bbdbc85bb23e3ed779
Denis Klychkov <dklychkov@networkoptix.com>
2018-01-24 14:24:35 +0300

    CMake: Allow edge1 only for digitalwatchdog customization
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- cmake/parameters_validation.cmake
+++ cmake/parameters_validation.cmake
@@ -1,5 +1,5 @@
 function(nx_fail_with_invalid_parameters)
-    cmake_parse_arguments(FAIL "" "MESSAGE" "" ${ARGN})
+    cmake_parse_arguments(FAIL "" "MESSAGE;DESCRIPTION" "" ${ARGN})
 
     if(NOT FAIL_MESSAGE)
         set(FAIL_MESSAGE "Incompatible parameters:")
@@ -9,9 +9,14 @@ function(nx_fail_with_invalid_parameters)
         string(APPEND FAIL_MESSAGE "\n  ${var} = ${${var}}")
     endforeach()
 
+    if(FAIL_DESCRIPTION)
+        string(APPEND FAIL_MESSAGE "\n${FAIL_DESCRIPTION}")
+    endif()
+
     message(FATAL_ERROR ${FAIL_MESSAGE})
 endfunction()
 
-if(targetDevice STREQUAL edge1 AND customization STREQUAL "hanwha")
-    nx_fail_with_invalid_parameters(targetDevice customization)
+if(targetDevice STREQUAL edge1 AND NOT customization STREQUAL "digitalwatchdog")
+    nx_fail_with_invalid_parameters(targetDevice customization
+        DESCRIPTION "edge1 can be used only with digitalwatchdog customization.")
 endif()
