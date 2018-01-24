5ea674e120271ddd011ff65ccc9fa4cd9d5e4256
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2018-01-23 16:54:19 -0800

    Mac signing certificates changed for Hanwha and RAS customizations
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- customization/ras/customization.cmake
+++ customization/ras/customization.cmake
@@ -51,7 +51,7 @@ set(ios.sign.identity "iPhone Distribution: RAS Bvba (CJ7G58V6NE)")
 set(ios.old_app_appstore_id "")
 
 set(mac.skip.sign "false")
-set(mac.sign.identity "Developer ID Application: RAS Bvba (CJ7G58V6NE)")
+set(mac.sign.identity "3rd Party Mac Developer Application: RAS Bvba (CJ7G58V6NE)")
 set(mac.app.sign.identity "3rd Party Mac Developer Application: RAS Bvba (CJ7G58V6NE)")
 set(mac.pkg.sign.identity "3rd Party Mac Developer Installer: RAS Bvba (CJ7G58V6NE)")
 set(mac.bundle.identifier "com.rassecurity.probox2")
