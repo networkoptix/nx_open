64d1925789a9df341eec9084f863fc6173eef78d
Denis Klychkov <dklychkov@networkoptix.com>
2018-01-25 19:23:33 +0300

    Simplify addition of new camera vendors
    Now the workflow is as follows:
      1. Add vendor name to camera_vendors list in camera_vendors.cmake.
      2. Enable vendor for the certain platforms with nx_enable_camera_vendor(vendor_name).
      3. Use camera_vendors.h in the cpp sources where you need to check if vendor is enabled.
      4. Don't use ifdefs in src/plugins/resource/vendor_name because these files will be excluded from
      build automatically.
    
    nx_enable_camera_vendor_old is a legacy function which will be removed.
    
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- mediaserver_core/camera_vendors.cmake
+++ mediaserver_core/camera_vendors.cmake
@@ -1,76 +1,98 @@
+set(_enabled_camera_vendors)
+set(_enabled_camera_vendors_old)
+
+function(nx_enable_camera_vendor vendor)
+    set(_enabled_camera_vendors ${_enabled_camera_vendors} ${vendor} PARENT_SCOPE)
+endfunction()
+
+# TODO: Refactor vendors to use new approach, then remove this function.
+function(nx_enable_camera_vendor_old vendor)
+    set(_enabled_camera_vendors_old ${_enabled_camera_vendors_old} ${vendor} PARENT_SCOPE)
+endfunction()
+
+function(nx_add_vendor_macros target)
+    foreach(vendor ${camera_vendors})
+        string(TOUPPER ${vendor} VENDOR)
+        if(vendor IN_LIST _enabled_camera_vendors_old)
+            target_compile_definitions(${target} PUBLIC ENABLE_${VENDOR})
+        endif()
+    endforeach()
+endfunction()
+
+#--------------------------------------------------------------------------------------------------
+# Registering vendors
+
+set(vendor_source_exclusions)
+
+set(camera_vendors
+    onvif
+    axis
+    acti
+    arecont
+    dlink
+    droid
+    test_camera
+    stardot
+    iqe
+    isd
+    pulse_camera
+    flir
+    advantech
+    wearable
+    hanwha
+    third_party
+    mdns
+    vmax
+    desktop_camera
+)
+
 if(NOT targetDevice MATCHES "android|ios|edge1")
-    set(ENABLE_ONVIF true)
-    set(ENABLE_AXIS true)
-    set(ENABLE_ACTI true)
-    set(ENABLE_ARECONT true)
-    set(ENABLE_DLINK true)
-    set(ENABLE_DROID true)
-    set(ENABLE_TEST_CAMERA true)
-    set(ENABLE_STARDOT true)
-    set(ENABLE_IQE true)
-    set(ENABLE_ISD true)
-    set(ENABLE_PULSE_CAMERA true)
-    set(ENABLE_FLIR true)
-    set(ENABLE_ADVANTECH true)
-    set(ENABLE_WEARABLE true)
+    nx_enable_camera_vendor_old(onvif)
+    nx_enable_camera_vendor_old(axis)
+    nx_enable_camera_vendor_old(acti)
+    nx_enable_camera_vendor_old(arecont)
+    nx_enable_camera_vendor_old(dlink)
+    nx_enable_camera_vendor_old(droid)
+    nx_enable_camera_vendor_old(test_camera)
+    nx_enable_camera_vendor_old(stardot)
+    nx_enable_camera_vendor_old(iqe)
+    nx_enable_camera_vendor_old(isd)
+    nx_enable_camera_vendor_old(pulse_camera)
+    nx_enable_camera_vendor_old(flir)
+    nx_enable_camera_vendor_old(advantech)
+    nx_enable_camera_vendor_old(wearable)
 
     if(customization STREQUAL "hanwha")
-        set(ENABLE_HANWHA true)
+        nx_enable_camera_vendor(hanwha)
     endif()
 endif()
 
 # TODO: mediaserver_core uses MDNS unconditionally, so disabling this macro always leads to build
 # failure.
 if(NOT targetDevice MATCHES "android|ios")
-    set(ENABLE_THIRD_PARTY true)
-    set(ENABLE_MDNS true)
+    nx_enable_camera_vendor_old(third_party)
+    nx_enable_camera_vendor_old(mdns)
 endif()
 
 if(WINDOWS)
-    set(ENABLE_VMAX true)
-    set(ENABLE_DESKTOP_CAMERA true)
+    nx_enable_camera_vendor_old(vmax)
+    nx_enable_camera_vendor_old(desktop_camera)
 endif()
+
+#--------------------------------------------------------------------------------------------------
+
+set(camera_vendors_macros)
+
+foreach(vendor ${camera_vendors})
+    string(TOUPPER ${vendor} VENDOR)
+
+    if(vendor IN_LIST _enabled_camera_vendors)
+        string(APPEND camera_vendor_macros "#define ENABLE_${VENDOR}\n")
+    elseif(vendor IN_LIST _enabled_camera_vendors_old)
+        continue()
+    else()
+        list(APPEND vendor_source_exclusions src/plugins/resource/${vendor}/)
+    endif()
+endforeach()
 
 nx_configure_file(camera_vendors.h.in ${CMAKE_CURRENT_BINARY_DIR}/camera_vendors.h)
-
-set(vendor_source_exclusions)
-
-if(NOT ENABLE_HANWHA)
-    list(APPEND vendor_source_exclusions src/plugins/resource/hanwha/)
-endif()
-
-# TODO: Remove this function and use camera_vendors.h where needed.
-function(nx_add_vendor_macros target)
-    if(NOT targetDevice MATCHES "android|ios|edge1")
-        target_compile_definitions(${target} PUBLIC
-            ENABLE_ONVIF
-            ENABLE_AXIS
-            ENABLE_ACTI
-            ENABLE_ARECONT
-            ENABLE_DLINK
-            ENABLE_DROID
-            ENABLE_TEST_CAMERA
-            ENABLE_STARDOT
-            ENABLE_IQE
-            ENABLE_ISD
-            ENABLE_PULSE_CAMERA
-            ENABLE_FLIR
-            ENABLE_ADVANTECH
-            ENABLE_WEARABLE
-        )
-    endif()
-
-    if(NOT targetDevice MATCHES "android|ios")
-        target_compile_definitions(${target} PUBLIC
-            ENABLE_THIRD_PARTY
-            ENABLE_MDNS
-        )
-    endif()
-
-    if(WINDOWS)
-        target_compile_definitions(${target} PUBLIC
-            ENABLE_VMAX
-            ENABLE_DESKTOP_CAMERA
-        )
-    endif()
-endfunction()
