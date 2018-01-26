473d14557ef49d8735c019981c0b760796d6c4f5
Nick Hartleb <nhartleb@networkoptix.com>
2018-01-25 17:44:51 -0800

    CLOUD-1524: Cloud merge - interface
    Completed steps 4-6 of mockup for front end
    working on changes to systemsProvider to handle merging in the system controller
    
    <==== Mercurial data ====>
    Branch: cloud_17.1.2

--- cloud_portal/front_end/app/scripts/controllers/merge.js
+++ cloud_portal/front_end/app/scripts/controllers/merge.js
@@ -6,23 +6,29 @@ angular.module('cloudApp')
         var dialogSettings = dialogs.getSettings($scope);
         $scope.system = dialogSettings.params.system;
 
-
-        $scope.mergeSystemTextTop = L.system.mergeSystemTextTop;
-        $scope.mergeSystemTextBottom = L.system.mergeSystemTextBottom;
         $scope.user = null;
         $scope.systems = null;
         console.log($scope.system);
+
+        //Add system can merge where added to systems form api call
+        function checkMergeAbility(system){
+            if(system.stateOfHealth == 'offline')
+                return 'offline'
+            return ''
+        }
 
         account.get().then(function(user){
             $scope.user = user;
             $scope.systems = systemsProvider.getMySystems(user.email, $scope.system.id);
             $scope.canMerge = $scope.system.accessRole.toLowerCase() === "owner" && $scope.systems.length > 0;
             $scope.targetSystem = $scope.systems[0];
+            $scope.systemStatus = checkMergeAbility($scope.targetSystem);
             console.log($scope.systems);
         });
 
         $scope.setTargetSystem = function(system){
             $scope.targetSystem = system;
+            $scope.systemStatus = checkMergeAbility(system);
         };
 
         $scope.close = function(){
