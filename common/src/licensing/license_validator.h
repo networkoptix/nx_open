7301b824462c5ca71c75ea983ea6e5ef7971bcd3
Sergey Ivanov <sivanov@networkoptix.com>
2018-01-31 16:40:21 +0300

    Unit tests compile fix
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- common/src/licensing/license_validator.h
+++ common/src/licensing/license_validator.h
@@ -56,6 +56,7 @@ public:
 protected:
     QnLicenseErrorCode isValidEdgeLicense(const QnLicensePtr& license,
         ValidationMode mode = VM_Regular) const;
-    QnLicenseErrorCode isValidStartLicense(const QnLicensePtr& license, ValidationMode mode) const;
+    QnLicenseErrorCode isValidStartLicense(const QnLicensePtr& license,
+        ValidationMode mode = VM_Regular) const;
     bool isAllowedForArm(const QnLicensePtr& license) const;
 };
