d2cf60db0ed35f6dcfc9405decc89a469c3a299f
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2018-02-07 14:15:41 -0800

    WEB-816: Wrong customization name in Sefica ProBox setup wizard
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- customization/ras/mediaserver_core/resources/static/customization/config.js
+++ customization/ras/mediaserver_core/resources/static/customization/config.js
@@ -1,6 +1,7 @@
 Config.helpLinks.push({
-    url: "https://play.google.com/store/apps/details?id=com.ioezio.ezpromobile",
-    title: "Android Client",
+    url: "ras.support@rassecurity.com",
+    description:"Have a question about specific features of your system?",
+    button:"get support",
     target: "new" // new|frame
 });
 
@@ -23,7 +24,7 @@ Config.helpLinks.push({
 
 Config.allowDebugMode = false;
 //Config.webclientEnabled = false; // Uncomment this string to disable web client
-Config.productName = 'EZ Pro';
-Config.cloud.productName = 'EZ Pro Cloud';
+Config.productName = 'Sefica ProBox';
+Config.cloud.productName = 'Pbx Cloud';
 Config.defaultLanguage = 'en_US';
 Config.supportedLanguages = ['en_US', 'en_GB', 'fr_FR', 'de_DE', 'ru_RU', 'es_ES', 'ja_JP', 'ko_KR', 'tr_TR', 'zh_CN', 'zh_TW', 'hu_HU', 'he_IL', 'nl_NL', 'pl_PL', 'vi_VN', 'th_TH'];
