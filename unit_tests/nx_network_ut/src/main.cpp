8897909df83819574861847d42f3b5be3ae25a44
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-02-08 17:44:21 -0800

    nx_network_ut: added unconditional --gtest_throw_on_failure to prevent bunch of tests hanging in case of failure
    <==== Mercurial data ====>
    Branch: cloud_dev

--- unit_tests/nx_network_ut/src/main.cpp
+++ unit_tests/nx_network_ut/src/main.cpp
@@ -5,8 +5,15 @@
 
 int main(int argc, char** argv)
 {
+    std::vector<char*> extendedArgs;
+    for (int i = 0; i < argc; ++i)
+        extendedArgs.push_back(argv[i]);
+    extendedArgs.push_back("--gtest_throw_on_failure");
+
+    argc = (int)extendedArgs.size();
     return nx::network::test::runTest(
-        argc, argv,
+        //argc, argv,
+        argc, extendedArgs.data(),
         [](const nx::utils::ArgumentParser&)
         {
             nx::network::ssl::Engine::useRandomCertificate("nx_network_ut");
