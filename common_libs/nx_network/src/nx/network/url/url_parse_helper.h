c5ce24cb83c404e4f61e548d199ddff9a9df15ec
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-02-12 15:35:29 -0800

    VMS-8271: Introduced nx::network::url::joinPath function
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- common_libs/nx_network/src/nx/network/url/url_parse_helper.h
+++ common_libs/nx_network/src/nx/network/url/url_parse_helper.h
@@ -16,6 +16,7 @@ namespace url {
 NX_NETWORK_API quint16 getDefaultPortForScheme(const QString& scheme);
 NX_NETWORK_API SocketAddress getEndpoint(const QUrl&);
 NX_NETWORK_API QString normalizePath(const QString&);
+NX_NETWORK_API QString joinPath(const QString& left, const QString& right);
 
 } // namespace url
 } // namespace network
