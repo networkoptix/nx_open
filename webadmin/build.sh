92a68c820e0a0597fe09492597935e9498e842ca
Evgeny Balashov <ebalashov@networkoptix.com>
2018-02-25 17:05:36 -0800

    ## print to error log to make it visible
    <==== Mercurial data ====>
    Branch: vms_3.2_web

--- webadmin/build.sh
+++ webadmin/build.sh
@@ -13,41 +13,41 @@ then
 fi
 
 # Update sources.
-echo "Update sources"
+echo "Update sources" >&2
 for entry in $(ls -A "$SOURCE_DIR")
 do
     [ -e "$entry" ] && rm -r "$entry"
     cp -pr "$SOURCE_DIR/$entry" "$entry"
 done
 
-echo "Clean old directories"
+echo "Clean old directories" >&2
 [ -e static ] && rm -r static
 [ -e server-external ] && rm -r server-external
 [ -e external.dat ] && rm external.dat
 
 # Save the repository info.
-echo "Create version.txt"
+echo "Create version.txt" >&2
 hg log -r . --repository "$SOURCE_DIR/.." > version.txt
 
 # Install dependencies.
-echo "Install node dependencies"
+echo "Install node dependencies" >&2
 npm install
 
 # Build webadmin.
-echo "Build webadmin"
+echo "Build webadmin" >&2
 npm run build
 mv dist static
 
 # Make translations
-echo "Create translations"
+echo "Create translations" >&2
 pushd translation
     ./localize.sh
 popd
 
 #Pack
-echo "Pack external.dat"
+echo "Pack external.dat" >&2
 tar -czvf external.dat ./static
 mkdir -p ./server-external/bin
 mv external.dat server-external/bin/extemvrnal.dat
 
-echo "Webadmin build done"
+echo "Webadmin build done" >&2
