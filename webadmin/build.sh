103037a6e5b7c3c608b63720d6dec51f49af4428
Evgeny Balashov <ebalashov@networkoptix.com>
2018-02-25 16:21:13 -0800

    ## do not touch node_modules
    Add debug output
    
    <==== Mercurial data ====>
    Branch: vms_3.2_web

--- webadmin/build.sh
+++ webadmin/build.sh
@@ -13,32 +13,38 @@ then
 fi
 
 # Update sources.
-
+echo "Update sources"
 for entry in $(ls -A "$SOURCE_DIR")
 do
     [ -e "$entry" ] && rm -r "$entry"
     cp -pr "$SOURCE_DIR/$entry" "$entry"
 done
 
+echo "Clean old directories"
 [ -e static ] && rm -r static
-[ -e node_modules] && rm -r node_modules
+[ -e dist ] && rm -r dist
 [ -e server-external ] && rm -r server-external
+[ -e external.dat ] && rm external.dat
 
 # Save the repository info.
-
+echo "Create version.txt"
 hg log -r . --repository "$SOURCE_DIR/.." > version.txt
 
 # Install dependencies.
+echo "Install node dependencies"
 npm install
 
 # Build webadmin.
+echo "Build webadmin"
 npm run build
 mv dist static
 
 # Make translations
+echo "Create translations"
 pushd translation
     ./localize.sh
 popd
 
 #Pack
+echo "Pack external.dat"
 tar -czvf external.dat ./static
