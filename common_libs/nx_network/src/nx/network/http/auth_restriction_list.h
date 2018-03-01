bb70b16a3143ed49f5d8db78d8dd115a78b94826
Mikhail Uskov <muskov@networkoptix.com>
2018-03-01 20:55:04 +0300

    WEB-837, WEB-840: Allow some GET requests without CSRF.
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- common_libs/nx_network/src/nx/network/http/auth_restriction_list.h
+++ common_libs/nx_network/src/nx/network/http/auth_restriction_list.h
@@ -14,27 +14,35 @@ namespace AuthMethod {
 
 enum Value
 {
-    NotDefined = 0x00,
-    noAuth = 0x01,
-    /** Authentication method described in rfc2617. */
-    httpBasic = 0x02,
-    /** Authentication method described in rfc2617. */
-    httpDigest = 0x04,
+    NotDefined = 0,
+    noAuth = 1 << 0,
+
+    httpBasic = 1 << 1, //< Authentication method described in rfc2617.
+    httpDigest = 1 << 2, //< Authentication method described in rfc2617.
     http = httpBasic | httpDigest,
-    /** Based on authinfo cookie. */
-    cookie = 0x08,
+
+    cookie = 1 << 3, //< Based on authinfo cookie.
+
     /**
      * Authentication by X-NetworkOptix-VideoWall header.
      * TODO: #ak Does not look appropriate here.
      */
-    videowall = 0x10,
+    videowall = 1 << 4,
+
     /**
      * Authentication by url query parameters auth and proxy_auth.
      * Params have the following format: BASE64-URL( UTF8(username) ":" MD5( LATIN1(username) ":NetworkOptix:" LATIN1(password) ) )
-     * TODO #ak this method is too poor, have to introduce some salt.
+     * TODO: #ak This method is too poor, have to introduce some salt.
      */
-    urlQueryParam = 0x20,
-    tempUrlQueryParam = 0x40,
+    urlQueryParam = 1 << 5,
+    tempUrlQueryParam = 1 << 6,
+
+    /**
+     * Normally all GET requests do not requres SCRF token, but we could not allow that, because of
+     * poorly designed requests changing information (including passwords).
+     * TODO: Remove this propery as soon as all these requests are terminated.
+     */
+    allowWithourCsrf = 1 << 30,
 };
 
 Q_DECLARE_FLAGS(Values, Value);
@@ -42,40 +50,27 @@ Q_DECLARE_OPERATORS_FOR_FLAGS(Values);
 
 } // namespace AuthMethod
 
-/**
- * @note By default, AuthMethod::http, AuthMethod::cookie and AuthMethod::videowall 
- * authorization methods are allowed fo every url.
- */
 class NX_NETWORK_API AuthMethodRestrictionList
 {
 public:
-    static const unsigned int kDefaults;
+    static const AuthMethod::Values kDefaults;
 
-    /**
-     * @return Bit mask of auth methods (AuthMethod::Value enumeration).
-     */
-    unsigned int getAllowedAuthMethods(const nx_http::Request& request) const;
-
-    /**
-     * @param pathMask Wildcard-mask of path.
-     */
-    void allow(const QString& pathMask, AuthMethod::Value method);
-    /**
-     * @param pathMask Wildcard-mask of path.
-     */
-    void deny(const QString& pathMask, AuthMethod::Value method);
+    AuthMethod::Values getAllowedAuthMethods(const nx_http::Request& request) const;
+    void allow(const QString& pathMask, AuthMethod::Values method);
+    void deny(const QString& pathMask, AuthMethod::Values method);
 
 private:
     struct Rule
     {
         QRegExp expression;
-        unsigned int method;
-        Rule(const QString& expression, unsigned int method);
+        AuthMethod::Values methods;
+        Rule(const QString& expression, AuthMethod::Values methods);
     };
 
     mutable QnMutex m_mutex;
     std::map<QString, Rule> m_allowed;
     std::map<QString, Rule> m_denied;
+    std::set<QString> m_allowCookieWithotScrf;
 };
 
 } // namespace nx_http
