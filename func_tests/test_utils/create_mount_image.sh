ceabb9a92411bdf090f8024b54dc2d2fc796c732
Vsevolod Fedorov <vfedorov@networkoptix.com>
2018-03-01 14:06:07 +0300

    Functional tests: add second drive for mediaserver by mounting file: use fallocate instead of dd; use ext4
    <==== Mercurial data ====>
    Branch: vms_3.1

--- func_tests/test_utils/create_mount_image.sh
+++ func_tests/test_utils/create_mount_image.sh
@@ -5,8 +5,8 @@ IMAGE=/disk2.image
 MOUNT_POINT=/mnt/disk2
 
 if [ ! -f "$IMAGE" ]; then
-	dd if=/dev/zero of="$IMAGE" bs=$((1024 * 1024)) count=$((1024 * $SIZE))
-	/sbin/mke2fs -F -t ext3 "$IMAGE"
+	fallocate -l $((1024*1024*1024 * $SIZE)) "$IMAGE"
+	/sbin/mke2fs -F -t ext4 "$IMAGE"
 fi
 
 if ! grep "$MOUNT_POINT" /proc/mounts; then
