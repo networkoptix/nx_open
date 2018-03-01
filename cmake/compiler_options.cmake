0e6b23c8bda0c1dd6b70e03d57a08dfa168cd87a
Denis Klychkov <dklychkov@networkoptix.com>
2018-03-01 17:44:58 +0300

    CMake: Move -fno-devirtualize flag out of if(LINUX) condition
    Because this is not Linux-specific...
    (grafted from 35fca4d3df14ab1ffacf24c5aca36abd6649f445)
    
    <==== Mercurial data ====>
    Branch: vms_4.0_nvidia_demo

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -22,6 +22,12 @@ if(MSVC)
     endif()
 endif()
 
+if(CMAKE_BUILD_TYPE MATCHES "Release|RelWithDebInfo")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        add_compile_options(-fno-devirtualize)
+    endif()
+endif()
+
 add_definitions(
     -DUSE_NX_HTTP
     -D__STDC_CONSTANT_MACROS
@@ -139,10 +145,6 @@ if(LINUX)
     # TODO: Use CMake defaults in the next release version (remove the following two lines).
     string(REPLACE "-O3" "-O2" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
     string(REPLACE "-O3" "-O2" CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
-
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        add_compile_options(-fno-devirtualize)
-    endif()
 
     if(NOT "${arch}" MATCHES "arm|aarch64")
         add_compile_options(-msse2)
