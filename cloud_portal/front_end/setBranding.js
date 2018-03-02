76da3647dd10208604b19576df8349d911dcade2
Nick Hartleb <nhartleb@networkoptix.com>
2018-03-01 20:07:27 -0800

    CLOUD-1688: Move Cloud Portal to Webpack and npm
    us fs-extra to copy styles dir into app/styles/custom
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1688_move_cloud_portal_to_webpack_and_npm

--- cloud_portal/front_end/setBranding.js
+++ cloud_portal/front_end/setBranding.js
@@ -1,14 +1,12 @@
 const fsmv = require('fs-extra');
-const fs = require('fs');
 const path = require('path');
 const copyCustomization = (customization) => {
-	var customizationStyle = path.join('../customizations', customization, 'front_end/styles/_custom_palette.scss')
-	var source = fs.createReadStream(customizationStyle);
-	var dest = fs.createWriteStream('app/styles/custom/_custom_palette.scss');
-	source.pipe(dest);
+	var source = path.resolve(__dirname, '../customizations', customization, 'front_end/styles');
+	var dest = path.resolve(__dirname, 'app/styles/custom');
+	fsmv.copy(source, dest, {mkdirp:true}, error => error ? console.log(error) : null);
 
-	source = path.resolve('../customizations', customization, 'front_end/images');
-	dest = 'app/images';
+	source = path.resolve(__dirname, '../customizations', customization, 'front_end/images');
+	dest = path.resolve(__dirname, 'app/images');
 	fsmv.copy(source, dest, {mkdirp:true}, error => error ? console.log(error) : null);
 }
 
