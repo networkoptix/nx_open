9a5e30f444f4c48298c31a65a737f460e4d35fd3
Nick Hartleb <nhartleb@networkoptix.com>
2018-03-08 20:14:39 -0800

    CLOUD-1715: Emails not sending
    <==== Mercurial data ====>
    Branch: cloud_17.1.2

--- cloud_portal/cloud/notifications/engines/email_engine.py
+++ cloud_portal/cloud/notifications/engines/email_engine.py
@@ -74,7 +74,7 @@ def send(email, msg_type, message, customization_name):
     # msg.attach_alternative(email_txt_body, "text/plain")
 
     msg.mixed_subtype = 'related'
-    msg_img = MIMEImage(read_file(customization_name, 'templates/email_logo.png'))
+    msg_img = MIMEImage(read_file(customization_name, 'templates/email_logo.png'), _subtype="png")
     msg_img.add_header('Content-ID', '<logo>')
     msg.attach(msg_img)
     return msg.send()
@@ -85,7 +85,7 @@ def get_email_title(customization_name, language_code, event):
     if language_code not in titles_cache:
         filename = "templates/lang_{{language}}/notifications-language.json"
         data = read_file(customization_name, filename, language_code)
-        titles_cache[language_code] = json.load(data)
+        titles_cache[language_code] = json.loads(data)
         email_cache(customization_name, 'email_titles', titles_cache)
     return titles_cache[language_code][event]["emailSubject"]
 
@@ -98,7 +98,7 @@ def read_template(customization_name, name, language_code, html):
     return read_file(customization_name, filename, language_code)
     
 
-def read_file(customization_name, filename, language_code=None):
+def read_file(customization_name, filename, language_code=""):
     files_cache = email_cache(customization_name, 'files')
     translated_name = filename.replace("{{language}}", language_code)
     if translated_name not in files_cache:
