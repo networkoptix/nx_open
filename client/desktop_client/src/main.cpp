8300648bf0ed8ce69139719772e64fd08625beb0
Sergey Ivanov <sivanov@networkoptix.com>
2018-03-14 16:40:23 +0300

    Cleanup debug screen geometry code as it is moved to a separate project
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- client/desktop_client/src/main.cpp
+++ client/desktop_client/src/main.cpp
@@ -199,20 +199,6 @@ int runApplication(QtSingleApplication* application, const QnStartupParameters& 
             mainWindow->move(targetPosition);
         }
     }
-    bool debugScreensGeometry = startupParams.screen != QnStartupParameters::kInvalidScreen
-        && startupParams.screen >= desktop->screenCount();
-    if (debugScreensGeometry)
-    {
-        NX_INFO("MainWindow") "System screen configuration:";
-        for (int i = 0; i < desktop->screenCount(); ++i)
-        {
-            const auto screen = QGuiApplication::screens().value(i);
-            NX_INFO("MainWindow") "Screen" << i << "geometry is" << screen->geometry();
-            if (desktop->screenGeometry(i) != screen->geometry())
-                NX_INFO("MainWindow") "Alternative screen" << i << "geometry is" << desktop->screenGeometry(i);
-        }
-    }
-
 
     mainWindow->show();
     joystickManager->start();
