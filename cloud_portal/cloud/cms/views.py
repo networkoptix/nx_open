8beb11a81ca08897909776cbeff1f550b46dc670
Nick Hartleb <nhartleb@networkoptix.com>
2018-03-15 19:10:48 -0700

    CLOUD-1726: Handle image error when generating structure.json
    added error handling for images
    
    <==== Mercurial data ====>
    Branch: cloud_17.1.2

--- cloud_portal/cloud/cms/views.py
+++ cloud_portal/cloud/cms/views.py
@@ -258,8 +258,10 @@ def product_settings(request, product_id):
             if not file.name.endswith('zip'):
                 return HttpResponseBadRequest('zip archive is expected')
             if generate_json:
-                data = generate_structure.from_zip(file, product)
+                data, log_messages = generate_structure.from_zip(file, product)
                 content = json.dumps(data, ensure_ascii=False, indent=4, separators=(',', ': '))
+                for error in log_messages:
+                    messages.error(request, "Error with {} problem with {}".format(error['file'], error['extension']))
                 return response_attachment(content, 'structure.json', 'application/json')
             log_messages = structure.process_zip(file, request.user, update_structure, update_content)
             for item in log_messages:
