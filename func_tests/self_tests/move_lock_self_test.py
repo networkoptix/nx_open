cda00952e78aa7cbdc92ab1e945559e0c4e62862
George Sovetov <gsovetov@networkoptix.com>
2018-03-21 19:33:48 +0300

    CI-96: Actualize MoveLock test
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/move_lock_self_test.py
+++ func_tests/self_tests/move_lock_self_test.py
@@ -11,12 +11,9 @@ def os_access():
 
 
 @pytest.fixture()
-def path(acquired, os_access):
+def path(os_access):
     path = Path.home() / '.func_tests_dummy' / 'oi.lock'
-    if acquired:
-        os_access.run_command(['touch', path])
-    else:
-        os_access.run_command(['rm', '-f', path])
+    os_access.run_command(['rm', '-f', path])
     return path
 
 
@@ -25,23 +22,24 @@ def lock(os_access, path):
     return MoveLock(os_access, path)
 
 
-@pytest.mark.parametrize('acquired', [False])
-def test_acquire_free(lock):
-    lock.acquire()
+@pytest.fixture()
+def same_path_lock(os_access, path):
+    return MoveLock(os_access, path)
 
 
-@pytest.mark.parametrize('acquired', [True])
-def test_acquire_acquired(lock):
-    with pytest.raises(MoveLock.AlreadyAcquired):
-        lock.acquire()
+def test_already_acquired(lock, same_path_lock):
+    with lock:
+        with pytest.raises(MoveLock.AlreadyAcquired):
+            with same_path_lock:
+                pass
 
 
-@pytest.mark.parametrize('acquired', [True])
-def test_release_acquired(lock):
-    lock.release()
+def test_file_present(lock, os_access, path):
+    with lock:
+        assert os_access.file_exists(path)
 
 
-@pytest.mark.parametrize('acquired', [False])
-def test_release_free(lock):
+def test_not_acquired(lock, os_access, path):
     with pytest.raises(MoveLock.NotAcquired):
-        lock.release()
+        with lock:
+            os_access.run_command(['rm', path])
