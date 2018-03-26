8ee5359285790523d73a14505059ddd9322ec3bc
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-03-26 18:34:35 +0300

    VMS-8778: Hanwha cgi attributes parser fail on empty XML elements
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- unit_tests/mediaserver_core_ut/src/nx/mediaserver_core/plugins/hanwha_cgi_parser_ut.cpp
+++ unit_tests/mediaserver_core_ut/src/nx/mediaserver_core/plugins/hanwha_cgi_parser_ut.cpp
@@ -16,28 +16,41 @@ struct HanwhaCgiParser: ::testing::Test
         ASSERT_TRUE(parameters.isValid());
 
         auto stringParam = parameters.parameter("system/deviceinfo/view/stringParam");
+        ASSERT_TRUE(stringParam);
         auto intParam = parameters.parameter("system/deviceinfo/view/intParam");
+        ASSERT_TRUE(intParam);
         auto floatParam = parameters.parameter("system/deviceinfo/view/floatParam");
+        ASSERT_TRUE(floatParam);
         auto boolParam = parameters.parameter("system/deviceinfo/view/boolParam");
+        ASSERT_TRUE(boolParam);
         auto enumParam = parameters.parameter("system/deviceinfo/view/enumParam");
+        ASSERT_TRUE(enumParam);
 
         ASSERT_EQ(9, stringParam->maxLength());
+        ASSERT_EQ(HanwhaCgiParameterType::string, stringParam->type());
 
         ASSERT_EQ(5, intParam->min());
         ASSERT_EQ(15, intParam->max());
+        ASSERT_EQ(HanwhaCgiParameterType::integer, intParam->type());
 
         ASSERT_FLOAT_EQ( 0.1, floatParam->floatMin());
         ASSERT_FLOAT_EQ(176.0, floatParam->floatMax());
+        ASSERT_EQ(HanwhaCgiParameterType::floating, floatParam->type());
 
         ASSERT_EQ("True", boolParam->trueValue());
         ASSERT_EQ("False", boolParam->falseValue());
         ASSERT_EQ("False", boolParam->falseValue());
+        ASSERT_EQ(HanwhaCgiParameterType::boolean, boolParam->type());
 
         ASSERT_TRUE(enumParam->possibleValues().contains("entry0"));
         ASSERT_TRUE(enumParam->possibleValues().contains("entry1"));
         ASSERT_TRUE(enumParam->possibleValues().contains("entry2"));
         ASSERT_TRUE(enumParam->possibleValues().contains("entry3"));
         ASSERT_FALSE(enumParam->possibleValues().contains("entry4"));
+
+        ASSERT_FALSE(enumParam->isRequestParameter());
+        ASSERT_TRUE(enumParam->isResponseParameter());
+        ASSERT_EQ(HanwhaCgiParameterType::enumeration, enumParam->type());
 
         auto stringParam2 = parameters.parameter("system2/deviceinfo2/view2/stringParam2");
         ASSERT_EQ(10, stringParam2->maxLength());
