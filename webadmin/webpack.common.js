eb4d039471f2a28b8c49cbcd73783f454a705e84
Nick Hartleb <nhartleb@networkoptix.com>
2018-04-02 15:51:52 -0700

    WEB-858: Empty pages on webclient in Edge, IE and FF
    fixed order of webpack packages
    
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- webadmin/webpack.common.js
+++ webadmin/webpack.common.js
@@ -22,19 +22,22 @@ module.exports = {
         //Plugins used for making templates
         new webpack.NamedModulesPlugin(),
         new HtmlWebpackPlugin({
-            chunks: ['commons', 'app', 'webcommonApp'],
+            chunks: ['commons', 'webcommonApp', 'app'],
+            chunksSortMode: 'manual',
             template: 'index-template.html',
             filename: 'index.html',
             inject:false
         }),
         new HtmlWebpackPlugin({
-            chunks: ['commons', 'inline', 'webcommonPartial'],
+            chunks: ['commons', 'webcommonPartial', 'inline'],
+            chunksSortMode: 'manual',
             template: 'inline-template.html',
             filename: 'inline.html',
             inject:false
         }),
         new HtmlWebpackPlugin({
-            chunks: ['commons', 'login', 'webcommonPartial'],
+            chunks: ['commons', 'webcommonPartial', 'login'],
+            chunksSortMode: 'manual',
             template: 'login-template.html',
             filename: 'login.html',
             inject:false
