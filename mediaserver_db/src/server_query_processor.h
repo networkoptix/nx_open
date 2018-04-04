54cf89c94fdb6ff98d94e5bc63eaede977a05bae
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-04-05 00:49:12 +0300

    mediaserver UT fixed
    <==== Mercurial data ====>
    Branch: x_VMS-4535

--- mediaserver_db/src/server_query_processor.h
+++ mediaserver_db/src/server_query_processor.h
@@ -37,6 +37,11 @@ struct ServerQueryProcessorAccess
     {
     }
 
+    virtual ~ServerQueryProcessorAccess()
+    {
+        waitForAsyncTasks();
+    }
+
     void waitForAsyncTasks()
     {
         QnMutexLocker lock(&m_runnigAsyncOperationsMutex);
