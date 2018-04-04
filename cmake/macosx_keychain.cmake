c140a419d62bed0d306f4c671b7980e9382989b3
Denis Klychkov <dklychkov@networkoptix.com>
2018-04-04 12:10:15 +0300

    MOBILE: Use a temporary keychain when building the iOS app
    <==== Mercurial data ====>
    Branch: mobile

--- cmake/macosx_keychain.cmake
+++ cmake/macosx_keychain.cmake
@@ -8,3 +8,24 @@ set(codeSigningKeychainPassword "qweasd123" CACHE INTERNAL "")
 if(useLoginKeychain)
     set(codeSigningKeychainName)
 endif()
+
+if(NOT useLoginKeychain AND NOT (mac.skip.sign AND ios.skip.sign))
+    nx_find_first_matching_file(certificate "${certificates_path}/macosx/*.p12")
+    if(NOT certificate)
+        message(FATAL_ERROR "Cannot find any certificates in ${certificates_path}/macosx")
+    endif()
+
+    add_custom_target(prepare_mac_keychain
+        ${CMAKE_SOURCE_DIR}/build_utils/macos/prepare_build_keychain.sh
+            --keychain ${codeSigningKeychainName}
+            --keychain-password ${codeSigningKeychainPassword}
+            --certificate ${certificate}
+            --certificate-password ${mac_certificate_file_password}
+    )
+endif()
+
+function(prepare_mac_keychain target)
+    if(TARGET prepare_mac_keychain)
+        add_dependencies(${target} prepare_mac_keychain)
+    endif()
+endfunction()
