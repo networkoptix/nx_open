9b3ebcdc816d188a187471a5f26a0deb4d191488
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2018-04-18 12:54:12 -0700

    Changed timestamp server and signing logic: if beta - no timestamps
    <==== Mercurial data ====>
    Branch: vms_3.1.1

--- wixsetup/maven/filter-resources/sign.bat
+++ wixsetup/maven/filter-resources/sign.bat
@@ -1,11 +1,15 @@
 @echo off
 SET PASSWORD=${sign.password}
 SET CERTIFICATE=${sign.intermediate.cer}
+SET TIMESTAMP=
+IF NOT ["${beta}"] == ["true"] (
+    SET TIMESTAMP=/tr http://timestamp.digicert.com/
+)
 
 IF NOT [%PASSWORD%] == [] (
     IF NOT [%CERTIFICATE%] == [] (
-        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${certificates_path}/wixsetup/${sign.cer} %*
+        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
     ) ELSE (
-        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${certificates_path}/wixsetup/${sign.cer} %*
+        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
     )
-) ELSE call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 /tr http://tsa.startssl.com/rfc3161 /d "${company.name} ${display.product.name}" /f ${certificates_path}/wixsetup/${sign.cer} %*
+) ELSE call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
