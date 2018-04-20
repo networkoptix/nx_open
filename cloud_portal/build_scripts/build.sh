806e7272eb67751a1c80abafe25b095ca3fdbde1
Nick Hartleb <nhartleb@networkoptix.com>
2018-04-19 20:09:05 -0700

    CLOUD-1770: Make script build cmake-compatible
    needs more testing
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1770_Make_script_build_cmake-compatible

--- cloud_portal/build_scripts/build.sh
+++ cloud_portal/build_scripts/build.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 
-if [ "$DIR" != "$PWD" ]
+if [[ "$PWD" != */nx_vms/cloud_portal* ]]
 then
+    echo "Cleaning current directory"
+    rm -rf *
+
+    echo "Copying source files"
+    cp -pr $DIR/.. cloud_portal
+    mkdir -p webadmin/app/web_common && cp -pr $DIR/../../webadmin/app/web_common/* "$_"
+    mkdir -p webadmin/app/styles && cp -pr $DIR/../../webadmin/app/styles/* "$_"
+    cp $DIR/../../webadmin/package.json webadmin
+
+    cd cloud_portal
     echo "pip install requirements"
     virtualenv env
     . ./env/bin/activate
-    pip install -r requirements.txt
+    pip install -r build_scripts/requirements.txt
 
 
     echo "npm install"
-    pushd ../front_end
+    echo $PWD
+    pushd front_end
     npm install
     popd
 
-    pushd ../../webadmin
+    pushd ../webadmin
     npm install
     popd
+
+    cd build_scripts
+else
+    cd $DIR
 fi
 
 TARGET_DIR="../cloud/static"
@@ -26,7 +40,7 @@ TARGET_DIR="../cloud/static"
 echo "Clear $TARGET_DIR"
 rm -rf $TARGET_DIR
 echo "Create $TARGET_DIR"
-mkdir $TARGET_DIR
+mkdir -p $TARGET_DIR
 
 
 echo "Iterate all customizations"
@@ -34,7 +48,7 @@ for dir in ../customizations/*/
 do
     dir=${dir%*/}
     CUSTOMIZATION=${dir/..\/customizations\//}
-    ./build_customization.sh $CUSTOMIZATION
+    ./build_customization.sh $CUSTOMIZATION $DIR
 done
 
 echo "Done!"
