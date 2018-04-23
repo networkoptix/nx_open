5bb1107d2c508cddd8d3920c50718446b017c091
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-04-23 15:51:24 -0700

    cloud/mediator: Refactored StatisticsApi.http_and_stun_server_statistics test
    <==== Mercurial data ====>
    Branch: cloud_17.1.1_backend

--- unit_tests/connection_mediator_ut/src/functional_tests/statistics_ut.cpp
+++ unit_tests/connection_mediator_ut/src/functional_tests/statistics_ut.cpp
@@ -1,3 +1,5 @@
+#include <future>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -23,12 +25,30 @@ class StatisticsApi:
 {
     using base_type = MediatorFunctionalTest;
 
+public:
+    ~StatisticsApi()
+    {
+        if (m_clientConnection)
+            m_clientConnection->pleaseStopSync();
+    }
+
 protected:
     virtual void SetUp() override
     {
         base_type::SetUp();
 
         ASSERT_TRUE(startAndWaitUntilStarted());
+
+        m_system = addRandomSystem();
+        m_server = addServer(m_system, QnUuid::createUuid().toSimpleString().toUtf8());
+    }
+
+    void whenRequestListeningPeerList()
+    {
+        nx::network::http::StatusCode::Value statusCode =
+            nx::network::http::StatusCode::ok;
+        std::tie(statusCode, m_prevListeningPeersResponse) = getListeningPeers();
+        ASSERT_EQ(nx::network::http::StatusCode::ok, statusCode);
     }
 
     void whenRequestServerStatistics()
@@ -44,7 +64,45 @@ protected:
         m_responseBody = httpClient.fetchEntireMessageBody();
     }
 
-    void thenStatisticsIsProvided()
+    void establishClientConnection()
+    {
+        m_clientConnection = clientConnection();
+
+        hpm::api::ClientBindRequest request;
+        request.originatingPeerID = "someClient";
+        request.tcpReverseEndpoints.push_back(
+            nx::network::SocketAddress("12.34.56.78:1234"));
+        std::promise<void> bindPromise;
+        m_clientConnection->send(
+            request,
+            [&](nx::hpm::api::ResultCode code)
+            {
+                ASSERT_EQ(code, nx::hpm::api::ResultCode::ok);
+                bindPromise.set_value();
+            });
+        bindPromise.get_future().wait();
+    }
+
+    void thenExpectedConnectionsHaveBeenReported()
+    {
+        ASSERT_EQ(1U, m_prevListeningPeersResponse.systems.size());
+        const auto systemIter = m_prevListeningPeersResponse.systems.find(m_system.id);
+        ASSERT_NE(m_prevListeningPeersResponse.systems.end(), systemIter);
+
+        ASSERT_EQ(1U, systemIter->second.size());
+        const auto serverIter = systemIter->second.find(m_server->serverId());
+        ASSERT_NE(systemIter->second.end(), serverIter);
+
+        ASSERT_EQ(1U, m_prevListeningPeersResponse.clients.size());
+        const auto& boundClient = *m_prevListeningPeersResponse.clients.begin();
+        ASSERT_EQ("someClient", boundClient.first);
+        ASSERT_EQ(1U, boundClient.second.tcpReverseEndpoints.size());
+        ASSERT_EQ(
+            nx::network::SocketAddress("12.34.56.78:1234"),
+            boundClient.second.tcpReverseEndpoints.front());
+    }
+
+    void thenServerStatisticsIsProvided()
     {
         ASSERT_TRUE(static_cast<bool>(m_responseBody));
 
@@ -57,53 +115,23 @@ protected:
 private:
     boost::optional<nx::network::http::StringType> m_responseBody;
     stats::Statistics m_serverStatistics;
+    api::ListeningPeers m_prevListeningPeersResponse;
+    AbstractCloudDataProvider::System m_system;
+    std::unique_ptr<MediaServerEmulator> m_server;
+    std::unique_ptr<api::MediatorClientTcpConnection> m_clientConnection;
 };
 
 TEST_F(StatisticsApi, listening_peer_list)
 {
-    const auto client = clientConnection();
-    auto clientGuard = makeScopeGuard([&client]() { client->pleaseStopSync(); });
-    const auto system1 = addRandomSystem();
-    auto server1 = addServer(system1, QnUuid::createUuid().toSimpleString().toUtf8());
-
-    hpm::api::ClientBindRequest request;
-    request.originatingPeerID = "someClient";
-    request.tcpReverseEndpoints.push_back(nx::network::SocketAddress("12.34.56.78:1234"));
-    nx::utils::promise<void> bindPromise;
-    client->send(
-        request,
-        [&](nx::hpm::api::ResultCode code)
-        {
-            ASSERT_EQ(code, nx::hpm::api::ResultCode::ok);
-            bindPromise.set_value();
-        });
-    bindPromise.get_future().wait();
-
-    nx::network::http::StatusCode::Value statusCode = nx::network::http::StatusCode::ok;
-    api::ListeningPeers listeningPeers;
-    std::tie(statusCode, listeningPeers) = getListeningPeers();
-    ASSERT_EQ(nx::network::http::StatusCode::ok, statusCode);
-
-    ASSERT_EQ(1U, listeningPeers.systems.size());
-    const auto systemIter = listeningPeers.systems.find(system1.id);
-    ASSERT_NE(listeningPeers.systems.end(), systemIter);
-
-    ASSERT_EQ(1U, systemIter->second.size());
-    const auto serverIter = systemIter->second.find(server1->serverId());
-    ASSERT_NE(systemIter->second.end(), serverIter);
-
-    ASSERT_EQ(1U, listeningPeers.clients.size());
-    const auto& boundClient = *listeningPeers.clients.begin();
-    ASSERT_EQ("someClient", boundClient.first);
-    ASSERT_EQ(1U, boundClient.second.tcpReverseEndpoints.size());
-    ASSERT_EQ(nx::network::SocketAddress("12.34.56.78:1234"), boundClient.second.tcpReverseEndpoints.front());
+    establishClientConnection();
+    whenRequestListeningPeerList();
+    thenExpectedConnectionsHaveBeenReported();
 }
 
 TEST_F(StatisticsApi, http_and_stun_server_statistics)
 {
     whenRequestServerStatistics();
-
-    thenStatisticsIsProvided();
+    thenServerStatisticsIsProvided();
 }
 
 } // namespace test
