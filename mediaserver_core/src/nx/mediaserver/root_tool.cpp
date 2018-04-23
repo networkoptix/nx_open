052ece24057438d7c6cfdd81b74716417b550d34
Alexander Kulikov <akulikov@networkoptix.com>
2018-04-23 16:43:30 +0300

    VMS-9338: Fix review issues
    <==== Mercurial data ====>
    Branch: vms_3.2_dev

--- mediaserver_core/src/nx/mediaserver/root_tool.cpp
+++ mediaserver_core/src/nx/mediaserver/root_tool.cpp
@@ -74,16 +74,16 @@ Qn::StorageInitResult RootTool::mount(const QUrl& url, const QString& path)
             {
             case SystemCommands::MountCode::ok:
                 NX_DEBUG(
-                    this, lm("[mount] Successfully mounted %1 to %2 %3").args(url, path, viaString));
+                    this, lm("[mount] Successfully mounted '%1' to '%2' %3").args(url, path, viaString));
                 break;
             case SystemCommands::MountCode::otherError:
                 NX_WARNING(
-                    this, lm("[mount] Failed to mount %1 to %2 %3").args(url, path, viaString));
+                    this, lm("[mount] Failed to mount '%1' to '%2' %3").args(url, path, viaString));
                 break;
             case SystemCommands::MountCode::wrongCredentials:
                 NX_WARNING(
                     this,
-                    lm("[mount] Failed to mount %1 to %2 %3 due to WRONG credentials")
+                    lm("[mount] Failed to mount '%1' to '%2' %3 due to WRONG credentials")
                         .args(url, path, viaString));
                 break;
             }
