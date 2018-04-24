726ca32c1860dd264b6edf29e6cdd781de0a81ca
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-04-24 19:14:12 +0300

    VMS-8576: Axis horn speaker stopped working after upgrade 3.0 -> 3.1(SpaceX)
    <==== Mercurial data ====>
    Branch: vms_3.1

--- common/src/nx/streaming/archive_stream_reader.cpp
+++ common/src/nx/streaming/archive_stream_reader.cpp
@@ -468,7 +468,8 @@ begin_label:
                     setNeedKeyData();
                 m_outOfPlaybackMask = false;
                 internalJumpTo(displayTime);
-                setSkipFramesToTime(displayTime, false);
+                if (displayTime != DATETIME_NOW)
+                    setSkipFramesToTime(displayTime, false);
 
                 emit jumpOccured(displayTime, m_delegate->getSequence());
                 m_BOF = true;
