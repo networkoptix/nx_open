79221e7adfdeaf3b8b1d78683d33320f9a7bb213
Nick Hartleb <nhartleb@networkoptix.com>
2018-04-23 20:07:47 -0700

    CLOUD-1770: Make script build cmake-compatible
    major updates more testing
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1770_Make_script_build_cmake-compatible

--- cloud_portal/build_scripts/build.sh
+++ cloud_portal/build_scripts/build.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CLEANUP=False
 
+#If we are not using the repository we should update necessary files
 if [[ "$PWD" != */nx_vms/cloud_portal* ]]
 then
-    CLEANUP=True
-    echo "Cleaning current directory"
-    rm -rf *
+    echo "Updating Cloud Portal Content"
+    if [ -e "cloud_portal" ]
+    then
+        pushd cloud_portal
+            for entry in $(ls -A .)
+            do
+                [ -e "$entry" ] && rm -rf "$entry"
+                cp -pr "$DIR/../$entry" "$entry"
+                echo "Updated $entry"
+            done
+        popd
+    else
+        cp -pr $DIR/.. cloud_portal
+    fi
 
-    echo "Copying source files"
-    cp -pr $DIR/.. cloud_portal
-    mkdir -p webadmin/app/web_common && cp -pr $DIR/../../webadmin/app/web_common/* "$_"
-    mkdir -p webadmin/app/styles && cp -pr $DIR/../../webadmin/app/styles/* "$_"
-    cp $DIR/../../webadmin/package.json webadmin
+    ls $PWD
+    mkdir -p webadmin
+    pushd webadmin
+        [ -d 'app/web_common' ] && rm -rf app/web_common
+        mkdir -p app/web_common && cp -pr $DIR/../../webadmin/app/web_common/* "$_"
 
+        [ -d 'app/styles' ] && rm -rf app/styles
+        mkdir -p app/styles && cp -pr $DIR/../../webadmin/app/styles/* "$_"
+
+        [ -e 'package.json' ] && rm -rf 'package.json'
+        cp $DIR/../../webadmin/package.json ./
+    popd
     cd cloud_portal
-    echo "pip install requirements"
-    virtualenv env
-    . ./env/bin/activate
-    pip install -r build_scripts/requirements.txt
-
-
-    echo "npm install"
-    echo $PWD
-    pushd front_end
-        npm install
-    popd
-
-    pushd ../webadmin
-        npm install
-    popd
-
-    cd build_scripts
 else
-    cd $DIR
-    . ../env/bin/activate
+    cd $DIR/..
 fi
+
+echo "pip install requirements"
+[ -z "env" ] && virtualenv env
+. ./env/bin/activate
+pip install -r build_scripts/requirements.txt
+
+pushd front_end
+    echo "npm install cloud portal"
+    npm install
+popd
+pushd ../webadmin
+    echo "npm install webadmin"
+    npm install
+popd
+
+cd build_scripts
 
 TARGET_DIR="../cloud/static"
 
@@ -53,23 +68,6 @@ do
     CUSTOMIZATION=${dir/..\/customizations\//}
     ./build_customization.sh $CUSTOMIZATION $DIR
 done
-
-echo "Done!"
-
-if $CLEANUP
-then
-    echo "Clean build dir"
-    cd ..
-    for dir in $(ls $PWD)
-    do
-        dir=${dir%*/}
-        [[ "$dir" = "cloud" ]] && continue
-        rm -rf ./$dir
-    done
-
-    rm -rf ../webadmin
-    echo $PWD
-fi
 
 echo "Cloud portal build is finished"
 # say "Cloud portal build is finished"
