81ec37a686b8a33b4c8f37dc21a3143a6111c3dd
Nick Hartleb <nhartleb@networkoptix.com>
2018-04-24 14:52:26 -0700

    CLOUD-1770: Make script build cmake-compatible
    check for cloud_portal instead
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1770_Make_script_build_cmake-compatible

--- cloud_portal/build_scripts/build.sh
+++ cloud_portal/build_scripts/build.sh
@@ -8,7 +8,7 @@ set -e
 NX_VMS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
 
 #If we are not using the repository we should update necessary files
-if [[ "$PWD" != */nx_vms/* ]]
+if [[ "$PWD" != */cloud_portal/* ]]
 then
     echo "Updating Cloud Portal Content"
     if [ -e "cloud_portal" ]
