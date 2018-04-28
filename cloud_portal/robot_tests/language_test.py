00fa16ffa1e2523e684eb4f67a752b0beb49e359
Kyle Epperson <kepperson@networkoptix.com>
2018-04-27 17:19:15 -0700

    CLOUD-1805:Update to langauge_test to check for server up
    <==== Mercurial data ====>
    Branch: cloud_18.1

--- cloud_portal/robot_tests/language_test.py
+++ cloud_portal/robot_tests/language_test.py
@@ -1,13 +1,25 @@
 from os import system
 import json
 import codecs
+import time
+from check_server import ping
 
 with codecs.open("language_list.json", 'r', encoding='utf-8-sig') as languages_list:
         langList = json.load(languages_list)
 
 def allLanguages():
     for name in langList:
-        system('robot -N ' + name + ' -v headless:true -d outputs -e not-ready -V getvars.py:' + langList[name] + ' --timestampoutputs test-cases')
+        print ping().status_code
+        if ping().ok:
+            system('robot -N ' + name + ' -v headless:true -d outputs\\' + langList[name] + ' -e not-ready -V getvars.py:' + langList[name] + ' test-cases')
+        else:
+            while not ping().ok:
+                time.sleep(300)
+                if ping().ok:
+                    system('robot -N ' + name + ' -v headless:true -d outputs\\' + langList[name] + ' -e not-ready -V getvars.py:' + langList[name] + ' test-cases')
+                    break
+        
+
 
 if __name__ == "__main__":
     allLanguages()
