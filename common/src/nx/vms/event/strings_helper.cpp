7b3479a1cf38e2bf88157c8358e1793d34524bb0
Stepan Andreenko <sandreenko@networkoptix.com>
2018-04-28 14:26:53 +0300

    VMS-9475 Replaced "Storage Failure" with "Storage Issue" for storageFailureEvent text
    <==== Mercurial data ====>
    Branch: vms

--- common/src/nx/vms/event/strings_helper.cpp
+++ common/src/nx/vms/event/strings_helper.cpp
@@ -131,7 +131,7 @@ QString StringsHelper::eventName(EventType value, int count) const
     switch (value)
     {
         case EventType::cameraMotionEvent:    return tr("Motion on Cameras", "", count);
-        case EventType::storageFailureEvent:  return tr("Storage Failure");
+        case EventType::storageFailureEvent:  return tr("Storage Issue");
         case EventType::networkIssueEvent:    return tr("Network Issue");
         case EventType::serverFailureEvent:   return tr("Server Failure");
         case EventType::serverConflictEvent:  return tr("Server Conflict");
@@ -200,7 +200,7 @@ QString StringsHelper::eventAtResource(const EventParameters& params,
             return tr("Motion on %1").arg(resourceName);
 
         case EventType::storageFailureEvent:
-            return tr("Storage Failure at %1").arg(resourceName);
+            return tr("Storage Issue at %1").arg(resourceName);
 
         case EventType::networkIssueEvent:
             return tr("Network Issue at %1").arg(resourceName);
