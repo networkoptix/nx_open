1199f31bf8f30d45bd183e3d487f1a50a364318b
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-04-30 16:43:49 -0700

    Cleanup
    <==== Mercurial data ====>
    Branch: cloud_17.1.2_bootstrap4

--- cloud_portal/front_end/app/src/download-history/release/release.component.ts
+++ cloud_portal/front_end/app/src/download-history/release/release.component.ts
@@ -8,8 +8,6 @@ import {
 @Component({
     selector: 'nx-release',
     templateUrl: 'release.component.html',
-    // TODO: Load language tailored template
-    // templateUrl: this.CONFIG.viewsDir + 'components/downloadRelease.html'
 })
 export class ReleaseComponent implements OnInit {
     @Input() release: any;
