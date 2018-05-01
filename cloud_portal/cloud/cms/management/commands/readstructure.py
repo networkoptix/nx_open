48513a0e9ea4b439b363e6a16d4e54e5705077ab
Evgeny Balashov <ebalashov@networkoptix.com>
2018-05-01 12:57:43 -0700

    CLOUD-1816: All CMS managed content is not translatable
    small fixes
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1816_translate_templates

--- cloud_portal/cloud/cms/management/commands/readstructure.py
+++ cloud_portal/cloud/cms/management/commands/readstructure.py
@@ -6,7 +6,7 @@
 import os
 import re
 from ...controllers import structure
-from ...models import Product, Context, ContextTemplate, DataStructure
+from ...models import Product, Context, Language, ContextTemplate, DataStructure
 from django.core.management.base import BaseCommand
 
 
@@ -58,7 +58,7 @@ def find_or_add_context_by_file(file_path, product_id, has_language):
 def find_or_add_context_template(context, language_code):
     if ContextTemplate.objects.filter(context__id=context.id, language__code=language_code).exists():
         return ContextTemplate.objects.get(context__id=context.id, language__code=language_code)
-    context_template = ContextTemplate(context=context, language = Language.get(code=language_code))
+    context_template = ContextTemplate(context=context, language=Language.get(code=language_code))
     context_template.save()
     return context_template
 
