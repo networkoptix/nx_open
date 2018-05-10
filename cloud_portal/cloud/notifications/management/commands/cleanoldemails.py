7e98b34ee355e9cf46483a624629330d204f8141
Nick Hartleb <nhartleb@networkoptix.com>
2018-05-09 17:46:34 -0700

    CLOUD-1843: python manage.py cleanmessages
    update var name
    
    <==== Mercurial data ====>
    Branch: cloud_18.1

--- cloud_portal/cloud/notifications/management/commands/cleanoldemails.py
+++ cloud_portal/cloud/notifications/management/commands/cleanoldemails.py
@@ -7,11 +7,12 @@ from datetime import datetime, timedelta
 
 
 class Command(BaseCommand):
-    help = 'Cleans out django tasks results and messages older than {} days'.format(settings.CLEAR_OLDER_THAN_X_DAYS)
+    help = 'Cleans out django tasks results and messages older than {} days'\
+        .format(settings.CLEAN_TASKS_AND_MSGS_OLDER_THAN_X_DAYS)
 
     def handle(self, *args, **options):
-        cutoff_date = datetime.now() - timedelta(days=settings.CLEAR_OLDER_THAN_X_DAYS)
+        cutoff_date = datetime.now() - timedelta(days=settings.CLEAN_TASKS_AND_MSGS_OLDER_THAN_X_DAYS)
         TaskResult.objects.filter(date_done__lt=cutoff_date).delete()
         Message.objects.filter(send_date__lt=cutoff_date).delete()
         self.stdout.write(self.style.SUCCESS('Successfully deleted task results and messages older than {} days'\
-                                             .format(settings.CLEAR_OLDER_THAN_X_DAYS)))
+                                             .format(settings.CLEAN_TASKS_AND_MSGS_OLDER_THAN_X_DAYS)))
