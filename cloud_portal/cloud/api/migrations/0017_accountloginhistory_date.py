06e861865f22ebbfb6fe8c778aa985712f0931e8
Evgeny Balashov <ebalashov@networkoptix.com>
2018-05-11 15:02:31 -0700

    CLOUD-1567: (#330) Missing Access Control Logging
    migration witout default value
    
    <==== Mercurial data ====>
    Branch: x_CLOUD-1567_access_log

--- cloud_portal/cloud/api/migrations/0017_accountloginhistory_date.py
+++ cloud_portal/cloud/api/migrations/0017_accountloginhistory_date.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='accountloginhistory',
             name='date',
-            field=models.DateTimeField(auto_now_add=True, default=datetime.now),
+            field=models.DateTimeField(auto_now_add=True),
             preserve_default=False,
         ),
     ]
