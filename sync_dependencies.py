1fe7d680c9bd6a8515e015f8e0f4e22bbc301caf
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-05-14 23:36:15 +0300

    Graft fix
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- sync_dependencies.py
+++ sync_dependencies.py
@@ -181,6 +181,7 @@ def sync_dependencies(syncher):
         #sync("libpixman-0.34.0-deb7")
         #sync("libcedrus-1.0-deb7")
         #sync("uboot-2014.04-10733-gbb5691c-dirty-vanilla")
+        pass
 
     sync("any/certificates-" + customization, path_variable="certificates_path")
 
