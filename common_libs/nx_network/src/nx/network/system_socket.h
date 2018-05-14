0f0956b73a2d52a4f15502eeb62ec6d44a14d148
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-05-14 11:45:37 -0700

    Fixed comment
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/system_socket.h
+++ common_libs/nx_network/src/nx/network/system_socket.h
@@ -229,7 +229,7 @@ public:
      */
     static int accept(int sockDesc);
 
-    virtual bool listen(int queueLen = 128) override;
+    virtual bool listen(int queueLen = AbstractStreamServerSocket::kDefaultBacklogSize) override;
     virtual std::unique_ptr<AbstractStreamSocket> accept() override;
     virtual void pleaseStop(nx::utils::MoveOnlyFunc< void() > handler) override;
     virtual void pleaseStopSync(bool assertIfCalledUnderLock = true) override;
