fa0975e4bf34f3a55190a98ef2100f8e54400a98
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-05-15 18:38:41 -0700

    CLOUD-1854: Removed debug code
    <==== Mercurial data ====>
    Branch: cloud_dev

--- nx_cloud_deploy/deployment_verification_tests/src/module_discovery_test_suite.go
+++ nx_cloud_deploy/deployment_verification_tests/src/module_discovery_test_suite.go
@@ -9,38 +9,6 @@ import (
 	"net/url"
 )
 
-/*
-var personXML = []byte(`
-  <person>
-	<name>Luann Van Houten</name>
-	<addresses>
-		<address type="secondary">
-			<street>321 MadeUp Lane</street>
-			<city>Shelbyville</city>
-		</address>
-		<address type="primary">
-			<street>123 Fake St</street>
-			<city>Springfield</city>
-		</address>
-	</addresses>
-  </person>`)
-
-type Person struct {
-	Name      string `xml:"name"`
-	Addresses []struct {
-		Street string `xml:"street"`
-		City   string `xml:"city"`
-		Type   string `xml:"type,attr"`
-	} `xml:"addresses>address"`
-}
-
-func main() {
-	var luann Person
-	xml.Unmarshal(personXML, &luann)
-	fmt.Println(luann)
-}
-*/
-
 type CloudModules struct {
 	Modules []struct {
 		Name string `xml:"resName,attr"`
