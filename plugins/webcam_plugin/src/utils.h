91e646bcfc36b8f2791e3ef825d7b6071ed57b11
Nate Wilson <nwilson@networkoptix.com>
2018-05-17 14:17:11 -0700

    VMS-9818: Cleaned up utils.h
    <==== Mercurial data ====>
    Branch: webcam_plugin

--- plugins/webcam_plugin/src/utils.h
+++ plugins/webcam_plugin/src/utils.h
@@ -1,10 +1,5 @@
 #pragma once
-#include "device_info.h"
-#ifdef _WIN32
-#include "dshow_utils.h"
-#elif __linux__
-#elif __APPLE__
-#endif
+#include "device_data.h"
 
 namespace nx {
 namespace webcam_plugin {
