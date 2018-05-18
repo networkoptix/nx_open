3bd84800a04f9253816d30504985a99b6975a407
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-05-18 19:33:18 +0300

    NX1-42: Fixed: removed SF_HasLiteClient server flag and Lite Client scripts
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- mediaserver_core/src/rest/handlers/start_lite_client_rest_handler.h
+++ mediaserver_core/src/rest/handlers/start_lite_client_rest_handler.h
@@ -2,13 +2,18 @@
 
 #include "rest/server/json_rest_handler.h"
 
-class QnStartLiteClientRestHandler
-:
+class QnStartLiteClientRestHandler:
     public QnJsonRestHandler
 {
     Q_OBJECT
 
 public:
+    /**
+     * @return Whether Lite Client is present in the installation. The exact semantics of the check
+     * is encapsulated inside the implementation of this method.
+     */
+    static bool isLiteClientPresent();
+
     virtual int executeGet(
         const QString& path,
         const QnRequestParams& params,
