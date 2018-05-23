8a3c7344f5752ac5478d2741cd10e131aa183371
George Sovetov <gsovetov@networkoptix.com>
2018-05-23 18:26:08 +0300

    CI: Reformat self_tests/test_os_access_script.py
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/test_os_access_script.py
+++ func_tests/self_tests/test_os_access_script.py
@@ -1,13 +1,23 @@
-# test script for test_os_access.test_unclosed_stdout: finishes, but not closes stdout
+"""Forked child outlives parent
 
-import os, time, sys
+Parent process forks and exits.
+Child inherits and holds its stdout and stderr streams.
 
-sleep_time_sec = int(sys.argv[1])
+Script used in for test_os_access.test_unclosed_stdout.
+"""
+from __future__ import print_function
 
-if os.fork():
-    print 'parent process, finished.'
-else:
-    print 'child process, sleeping for %d seconds...' % sleep_time_sec
-    sys.stdout.flush()
-    time.sleep(sleep_time_sec)
-    print 'child process, finished.'
+import os
+import sys
+import time
+
+if __name__ == '__main__':
+    sleep_time_sec = int(sys.argv[1])
+
+    if os.fork():
+        print('Parent process, finished.')
+    else:
+        print('Child process, sleeping for {:d} seconds...'.format(sleep_time_sec))
+        sys.stdout.flush()
+        time.sleep(sleep_time_sec)
+        print('Child process, finished.')
