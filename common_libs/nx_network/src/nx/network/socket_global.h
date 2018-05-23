98f03c17e97a9935a79dda72652d58e860683afd
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-05-23 14:43:47 -0700

    CLOUD-1846: New ssl socket implementation can now be enabled through flag config nx_network.ini/enableNewSslSocket. Off by default
    <==== Mercurial data ====>
    Branch: cloud_dev

--- common_libs/nx_network/src/nx/network/socket_global.h
+++ common_libs/nx_network/src/nx/network/socket_global.h
@@ -27,6 +27,7 @@ struct NX_NETWORK_API Ini:
 
     NX_INI_FLAG(0, httpClientTraffic, "Trace HTTP traffic for nx::network::http::AsyncHttpClient");
     NX_INI_STRING("", disableHosts, "Comma-separated list of forbidden IPs and domains");
+    NX_INI_FLAG(0, enableNewSslSocket, "Enable new SSL socket implementation");
 
     bool isHostDisabled(const HostAddress& address) const;
 };
