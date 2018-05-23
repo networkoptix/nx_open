8f5716e7d800f6382981121a888ecdaef90745c1
George Sovetov <gsovetov@networkoptix.com>
2018-05-23 20:12:10 +0300

    CI: Adapt unclosed streams test for SSH, move them to test_posix_shell
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/test_os_access.py
+++ func_tests/self_tests/test_os_access.py
@@ -1,13 +1,7 @@
 import datetime
-import sys
-import time
 
 import pytest
 import pytz
-from pathlib2 import Path
-
-from framework.os_access.exceptions import Timeout
-from framework.os_access.posix_shell import local_shell
 
 
 @pytest.fixture(scope='session')
@@ -18,16 +12,3 @@ def os_access(one_vm):
 def test_get_set_time(os_access):
     os_access.get_time()
     os_access.set_time(datetime.datetime.now(pytz.utc) - datetime.timedelta(days=100))
-
-
-def test_timeout():
-    with pytest.raises(Timeout):
-        local_shell.run_command(['sleep', 60], timeout_sec=1)
-
-
-def test_unclosed_stdout():
-    sleep_time_sec = 60
-    script_path = Path(__file__).with_name('test_os_access_script.py')
-    start_time = time.time()
-    local_shell.run_command([sys.executable, script_path, str(sleep_time_sec)])
-    assert time.time() - start_time < sleep_time_sec
--- func_tests/self_tests/test_posix_shell.py
+++ func_tests/self_tests/test_posix_shell.py
@@ -1,6 +1,9 @@
+import timeit
+
 import pytest
 from pathlib2 import PurePath
 
+from framework.os_access.exceptions import Timeout
 from framework.os_access.posix_shell import local_shell
 from framework.os_access.ssh_path import make_ssh_path_cls
 
@@ -36,3 +39,36 @@ def test_run_script(posix_shell):
 
 def test_create_path(posix_shell):
     assert isinstance(make_ssh_path_cls(posix_shell)('/tmp'), PurePath)
+
+
+def test_timeout(posix_shell):
+    with pytest.raises(Timeout):
+        posix_shell.run_command(['sleep', 5], timeout_sec=1)
+
+
+# language=Python
+_forked_child_script = '''
+from __future__ import print_function
+
+import os
+import sys
+import time
+
+if __name__ == '__main__':
+    sleep_time_sec = int(sys.argv[1])
+
+    if os.fork():
+        print('Parent process, finished.')
+    else:
+        print('Child process, sleeping for {:d} seconds...'.format(sleep_time_sec))
+        sys.stdout.flush()
+        time.sleep(sleep_time_sec)
+        print('Child process, finished.')
+'''
+
+
+def test_streams_left_open(posix_shell):
+    sleep_time_sec = 10
+    start_time = timeit.default_timer()
+    posix_shell.run_command(['python', '-c', _forked_child_script, sleep_time_sec], timeout_sec=5)
+    assert timeit.default_timer() - start_time < sleep_time_sec
