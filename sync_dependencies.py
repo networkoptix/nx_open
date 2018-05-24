e65c3b83d9e0d410ce4c3f0f02eccc83e90aa7a5
Sergey Ivanov <sivanov@networkoptix.com>
2018-05-24 12:44:39 +0300

    VMS-10330: Make EULA stored in a single place and format, convert it on demand
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- sync_dependencies.py
+++ sync_dependencies.py
@@ -128,6 +128,7 @@ def sync_dependencies(syncher):
         sync("vmaxproxy-2.1")
         sync("windows/wix-3.11", path_variable="wix_directory")
         sync("windows/signtool", path_variable="signtool_directory")
+        sync("windows/pandoc-2.2.1", path_variable="pandoc_directory")
 
     if box == "edge1":
         sync("cpro-1.0.0-2")
@@ -172,7 +173,7 @@ def sync_dependencies(syncher):
         #sync("additional-fonts")
         #sync("read-edid-3.0.2")
         #sync("a10-display")
-        
+
         # Hardware video decoding in Lite Client on Debian 7; kernel upgrade.
         #sync("libvdpau-sunxi-1.0-deb7")
         #sync("proxy-decoder-deb7")
