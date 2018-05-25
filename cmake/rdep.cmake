70d0033db991c242039d085190b4c11893299ced
Denis Klychkov <dklychkov@networkoptix.com>
2018-05-25 15:21:22 +0300

    CMake: Remove RDEP_DIR cache variable; it was actually never used as a setting
    <==== Mercurial data ====>
    Branch: vms_3.2

--- cmake/rdep.cmake
+++ cmake/rdep.cmake
@@ -1,5 +1,4 @@
-set(RDEP_DIR "${CMAKE_SOURCE_DIR}/build_utils/python" CACHE PATH "Path to rdep scripts")
-mark_as_advanced(RDEP_DIR)
+set(rdep_dir "${CMAKE_SOURCE_DIR}/build_utils/python")
 set(PACKAGES_DIR "$ENV{environment}/packages" CACHE STRING "Path to local rdep repository")
 mark_as_advanced(PACKAGES_DIR)
 file(TO_CMAKE_PATH "${PACKAGES_DIR}" PACKAGES_DIR)
@@ -9,10 +8,10 @@ option(rdepSync
     ON)
 
 function(nx_rdep_configure)
-    execute_process(COMMAND ${PYTHON_EXECUTABLE} ${RDEP_DIR}/rdep_configure.py)
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} ${rdep_dir}/rdep_configure.py)
     if(rdepSync)
         execute_process(COMMAND
-            ${PYTHON_EXECUTABLE} ${RDEP_DIR}/rdep.py --sync-timestamps --root ${PACKAGES_DIR})
+            ${PYTHON_EXECUTABLE} ${rdep_dir}/rdep.py --sync-timestamps --root ${PACKAGES_DIR})
     endif()
 endfunction()
 
@@ -41,7 +40,7 @@ function(nx_rdep_sync_package package)
     execute_process(
         COMMAND
             ${PYTHON_EXECUTABLE}
-            ${RDEP_DIR}/sync.py
+            ${rdep_dir}/sync.py
             ${package}
         RESULT_VARIABLE result)
 
