78de3c72f27d4eedee0f43c8202f54f33a334c63
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-05-25 15:01:20 -0700

    CLOUD-1886 Pressing enter on the change pass form from the new password field requires two presses
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/scripts/controllers/account.js
+++ cloud_portal/front_end/app/scripts/controllers/account.js
@@ -12,13 +12,11 @@
     function AccountCtrl($scope, cloudApi, process, $routeParams, account, languageService,
                          systemsProvider, authorizationCheckService, $localStorage, dialogs) {
 
-        var lang = languageService.lang;
-
-        $scope.lang = lang;
+        $scope.lang = languageService.lang;
 
         if ($localStorage && $localStorage.langChanged) {
             $localStorage.langChanged = false;
-            dialogs.notify(lang.account.accountSavedSuccess, 'success', false);
+            dialogs.notify(languageService.lang.account.accountSavedSuccess, 'success', false);
         }
 
         authorizationCheckService
@@ -39,7 +37,7 @@
             return cloudApi.accountPost($scope.account)
                 .then(function (result) {
                     systemsProvider.forceUpdateSystems();
-                    if (lang.language != $scope.account.language) {
+                    if (languageService.lang.language != $scope.account.language) {
                         $localStorage.langChanged = true;
                         //Need to reload page
                         window.location.reload(true); // reload window to catch new language
@@ -48,8 +46,8 @@
                     return result;
                 });
         }, {
-            successMessage : lang.account.accountSavedSuccess,
-            errorPrefix    : lang.errorCodes.cantChangeAccountPrefix,
+            successMessage : languageService.lang.account.accountSavedSuccess,
+            errorPrefix    : languageService.lang.errorCodes.cantChangeAccountPrefix,
             logoutForbidden: true
         });
 
@@ -57,11 +55,11 @@
             return cloudApi.changePassword($scope.pass.newPassword, $scope.pass.password);
         }, {
             errorCodes        : {
-                notAuthorized   : lang.errorCodes.oldPasswordMistmatch,
-                wrongOldPassword: lang.errorCodes.oldPasswordMistmatch
+                notAuthorized   : languageService.lang.errorCodes.oldPasswordMistmatch,
+                wrongOldPassword: languageService.lang.errorCodes.oldPasswordMistmatch
             },
-            successMessage    : lang.account.passwordChangedSuccess,
-            errorPrefix       : lang.errorCodes.cantChangePasswordPrefix,
+            successMessage    : languageService.lang.account.passwordChangedSuccess,
+            errorPrefix       : languageService.lang.errorCodes.cantChangePasswordPrefix,
             ignoreUnauthorized: true
         });
     }
