a3d15c506c120a957e179111391b43e5d93d6d76
Denis Klychkov <dklychkov@networkoptix.com>
2018-05-25 13:15:20 +0300

    CMake: Dependecies synching requires much less incoming data to work
    Now sync_dependencies.py requires only basic parameters to perform synching. This is the
    preparation to toolchain synching refactor. Since the amount of incoming parameters reduced it no
    longer needs a configuration file.
    
    <==== Mercurial data ====>
    Branch: vms

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,58 +1,36 @@
-set(customWebAdminVersion "" CACHE STRING
-    "Custom version for server-external package")
-mark_as_advanced(customWebAdminVersion)
+set(rdepOverrides "" CACHE STRING "RDep package version or location overrides")
+mark_as_advanced(rdepOverrides)
+
+# TODO: Remove this variable because its goal can be achieved with rdepOverrides.
 set(customWebAdminPackageDirectory "" CACHE STRING
     "Custom location of server-external package")
 mark_as_advanced(customWebAdminPackageDirectory)
 
-function(_nx_create_vms_configuration)
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(prefer_debug_packages "True")
-    else()
-        set(prefer_debug_packages "False")
-    endif()
+set(cmake_include_file ${CMAKE_BINARY_DIR}/dependencies.cmake)
 
-    set(content "def cmake_to_bool(value): return value.lower() in ('on', 'true')\n\n")
-    string(APPEND content "PACKAGES_DIR = '${PACKAGES_DIR}'\n")
-    string(APPEND content "rdepSync = cmake_to_bool('${rdepSync}')\n")
-    string(APPEND content "prefer_debug_packages = ${prefer_debug_packages}\n")
-    string(APPEND content "cmake_include_file = '${CMAKE_BINARY_DIR}/dependencies.cmake'\n")
-    string(APPEND content "platform = '${platform}'\n")
-    string(APPEND content "arch = '${arch}'\n")
-    string(APPEND content "rdep_target = '${rdep_target}'\n")
-    string(APPEND content "box = '${box}'\n")
-    string(APPEND content "branch = '${branch}'\n")
-    string(APPEND content "customization = '${customization}'\n")
-    string(APPEND content "releaseVersion = '${releaseVersion}'\n")
-    string(APPEND content "shortReleaseVersion = '${releaseVersion.short}'\n")
-    string(APPEND content "customWebAdminVersion = '${customWebAdminVersion}'\n")
-    string(APPEND content "customWebAdminPackageDirectory = '${customWebAdminPackageDirectory}'\n")
-    string(APPEND content "withMediaServer = cmake_to_bool('${withMediaServer}')\n")
-    string(APPEND content "withTrayTool = cmake_to_bool('${withTrayTool}')\n")
-    string(APPEND content "withNxTool = cmake_to_bool('${withNxTool}')\n")
-    string(APPEND content "withDesktopClient = cmake_to_bool('${withDesktopClient}')\n")
-    string(APPEND content "withMobileClient = cmake_to_bool('${withMobileClient}')\n")
-    string(APPEND content "withClouds = cmake_to_bool('${withClouds}')\n")
-    string(APPEND content "withTestCamera = cmake_to_bool('${withTestCamera}')\n")
-    string(APPEND content "withTests = cmake_to_bool('${withTests}')\n")
+set(sync_command ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/sync_dependencies.py
+    --packages-dir=${PACKAGES_DIR}
+    --target=${rdep_target}
+    --release-version=${releaseVersion.short}
+    --cmake-include-file=${cmake_include_file}
+)
 
-    file(WRITE ${CMAKE_BINARY_DIR}/vms_configuration.py ${content})
-    nx_store_known_file(${CMAKE_BINARY_DIR}/vms_configuration.py)
-endfunction()
-
-_nx_create_vms_configuration()
-
-if(WIN32)
-    set(_sep ";")
-else()
-    set(_sep ":")
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    list(APPEND sync_command "--debug")
 endif()
 
-set(ENV{PYTHONPATH} "${CMAKE_SOURCE_DIR}/build_utils/python${_sep}${CMAKE_BINARY_DIR}")
-execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/sync_dependencies.py
-    RESULT_VARIABLE sync_result
-)
+if(customWebAdminPackageDirectory)
+    list(APPEND rdepOverrides "server-external=${customWebAdminPackageDirectory}")
+else()
+    list(APPEND rdepOverrides "server-external=${branch}")
+endif()
+
+if(rdepOverrides)
+    list(APPEND sync_command --overrides ${rdepOverrides})
+endif()
+
+set(ENV{PYTHONPATH} "${CMAKE_SOURCE_DIR}/build_utils/python")
+execute_process(COMMAND ${sync_command} RESULT_VARIABLE sync_result)
 
 if(NOT sync_result STREQUAL "0")
     message(FATAL_ERROR "error: Packages sync failed")
@@ -66,7 +44,7 @@ if(customWebAdminPackageDirectory)
     nx_copy_package(${customWebAdminPackageDirectory})
 endif()
 
-include(${CMAKE_BINARY_DIR}/dependencies.cmake)
+include(${cmake_include_file})
 
 foreach(package_dir ${synched_package_dirs})
     if(NOT EXISTS ${package_dir}/.nocopy)
--- sync_dependencies.py
+++ sync_dependencies.py
@@ -2,12 +2,19 @@
 
 
 from __future__ import print_function
+import sys
+import argparse
 from rdep import Rdep
 from rdep_cmake import RdepSyncher
-from vms_configuration import *
 
 
-def determine_package_versions():
+def determine_package_versions(
+    platform,
+    box,
+    release_version,
+    customization="default",
+    debug=False
+):
     v = {
         "qt": "5.6.2",
         "boost": "1.66.0",
@@ -24,7 +31,10 @@ def determine_package_versions():
         "doxygen": "1.8.14",
         "gstreamer": "1.0",
         "glib": "2.0",
-        "deepstream": "0.1"
+        "deepstream": "0.1",
+        "help": customization + "-" + release_version,
+        "server-external": release_version,
+        "certificates": customization,
     }
 
     if platform == "windows":
@@ -83,12 +93,20 @@ def determine_package_versions():
     if not "festival-vox" in v:
         v["festival-vox"] = v["festival"]
 
+    if platform == "windows" and debug:
+        for package in ("festival", "openal", "openssl", "sigar"):
+            v[package] += "-debug"
+
     return v
 
 
-def sync_dependencies(syncher):
-    def sync(package, **kwargs):
-        return syncher.sync(package, use_local=not rdepSync, **kwargs)
+def sync_dependencies(syncher, platform, arch, box, release_version):
+    have_mediaserver = platform not in ("android", "ios")
+    have_desktop_client = platform in ("windows", "macosx") \
+        or (platform == "linux" and box in ("none", "tx1"))
+    have_mobile_client = have_desktop_client or platform in ("android", "ios")
+
+    sync = syncher.sync
 
     sync("qt", path_variable="QT_DIR")
     sync("any/boost")
@@ -139,13 +157,13 @@ def sync_dependencies(syncher):
     if arch == "x64":
         sync("cassandra")
 
-    if withDesktopClient:
-        sync("any/help-{}-3.1".format(customization), path_variable="help_directory")
+    if have_desktop_client:
+        sync("any/help", path_variable="help_directory")
 
-    if withDesktopClient or withMobileClient:
+    if have_desktop_client or have_mobile_client:
         sync("any/roboto-fonts", path_variable="fonts_directory")
 
-    if (withMediaServer or withDesktopClient) and box != "edge1":
+    if (have_mediaserver or have_desktop_client) and box != "edge1":
         sync("festival")
         if syncher.versions["festival-vox"] != "system":
             sync("any/festival-vox", path_variable="festival_vox_directory")
@@ -153,29 +171,20 @@ def sync_dependencies(syncher):
     if platform in ("android", "ios"):
         sync("libjpeg-turbo")
 
-    if withMediaServer:
+    if have_mediaserver:
         sync("any/nx_sdk-1.7.1")
         sync("any/nx_storage_sdk-1.7.1")
         sync("sigar")
-
         sync("any/apidoctool-2.0", path_variable="APIDOCTOOL_PATH")
-
-        if customWebAdminPackageDirectory:
-            pass
-        elif customWebAdminVersion:
-            sync("any/server-external-" + customWebAdminVersion)
-        else:
-            if not sync("any/server-external-" + branch, optional=True):
-                sync("any/server-external-" + releaseVersion)
+        if not sync("any/server-external", optional=True):
+            sync("any/server-external-" + release_version)
 
         if box in ("tx1", "edge1"):
             sync("openldap")
             sync("sasl2")
 
-    if platform == "windows":
-        sync("windows/doxygen", path_variable="doxygen_directory")
-    elif platform == "linux":
-        sync("linux/doxygen", path_variable="doxygen_directory")
+    if platform in ("linux", "windows"):
+        sync("%s/doxygen" % platform, path_variable="doxygen_directory")
 
     if box == "bpi":
         # Lite Client dependencies.
@@ -183,7 +192,7 @@ def sync_dependencies(syncher):
         #sync("additional-fonts")
         #sync("read-edid-3.0.2")
         #sync("a10-display")
-        
+
         # Hardware video decoding in Lite Client on Debian 7; kernel upgrade.
         #sync("libvdpau-sunxi-1.0-deb7")
         #sync("ldpreloadhook-1.0-deb7")
@@ -197,19 +206,95 @@ def sync_dependencies(syncher):
         # Required for ffmpeg.
         sync("libvdpau-1.0.4.1")
 
-    sync("any/certificates-" + customization, path_variable="certificates_path")
+    sync("any/certificates", path_variable="certificates_path")
     sync("any/root-certificates", path_variable="root_certificates_path")
 
 
+def parse_target(target):
+    components = target.split("-")
+
+    platform = None
+    arch = None
+    box = "none"
+
+    if len(components) > 1:
+        platform, arch = components
+
+        if platform == "android":
+            box = platform
+    else:
+        box = target
+
+        if box == "ios":
+            platform = "ios"
+        else:
+            platform = "linux"
+
+            if box == "tx1":
+                arch = "aarch64"
+            else:
+                arch = "arm"
+
+    return platform, arch, box
+
+
+def parse_overrides(overrides_list):
+    versions = {}
+    locations = {}
+
+    for item in overrides_list:
+        try:
+            package, value = item.split("=")
+            if "/" in value or "\\" in value:
+                locations[package] = value
+            else:
+                versions[package] = value
+        except ValueError:
+            print("Invalid override item \"%s\"" % item, file=sys.stderr)
+            raise
+
+    return versions, locations
+
+
 def main():
-    syncher = RdepSyncher(PACKAGES_DIR)
-    syncher.versions = determine_package_versions()
-    syncher.rdep_target = rdep_target
-    syncher.prefer_debug_packages = prefer_debug_packages
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--packages-dir", required=True, help="Packages directory")
+    parser.add_argument("--target", required=True, help="Build target (e.g. linux-x64)")
+    parser.add_argument("--debug", action="store_true", help="Prepare for debug build")
+    parser.add_argument("--cmake-include-file", default="dependencies.cmake",
+        help="CMake include file name")
+    parser.add_argument("--use-local", action="store_true",
+        help="Don't sync package if a local copy is found")
+    parser.add_argument("--release-version", required=True, help="VMS release version")
+    parser.add_argument("--customization", default="default", help="VMS customization")
+    parser.add_argument("-o", "--overrides", nargs="*", default=[],
+        help="Package version or location overrides (e.g. -o ffmpeg=4.0)")
 
-    sync_dependencies(syncher)
+    args = parser.parse_args()
 
-    syncher.generate_cmake_include(cmake_include_file)
+    platform, arch, box = parse_target(args.target)
+
+    try:
+        version_overrides, location_overrides = parse_overrides(args.overrides)
+    except ValueError:
+        exit(1)
+
+    syncher = RdepSyncher(args.packages_dir)
+    syncher.rdep_target = args.target
+    syncher.versions = determine_package_versions(
+        platform,
+        box,
+        args.release_version,
+        customization=args.customization,
+        debug=args.debug
+    )
+    syncher.versions.update(version_overrides)
+    syncher.locations = location_overrides
+    syncher.use_local = args.use_local
+
+    sync_dependencies(syncher, platform, arch, box, args.release_version)
+
+    syncher.generate_cmake_include(args.cmake_include_file)
 
 
 if __name__ == "__main__":
