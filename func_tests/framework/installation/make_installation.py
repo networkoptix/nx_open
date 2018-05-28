53a2e29eb6a580bb2e2127b295f62e15e540f154
Vsevolod Fedorov <vfedorov@networkoptix.com>
2018-05-28 17:00:33 +0300

    Funtest: skip test if proper mediaserver installer is not provided
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/installation/make_installation.py
+++ func_tests/framework/installation/make_installation.py
@@ -5,13 +5,15 @@ from framework.installation.windows_installation import WindowsInstallation
 
 
 def make_installation(mediaserver_installers, vm_type, os_access):
-    if vm_type not in mediaserver_installers:
-        pytest.skip("Mediaserver installer %r is not provided for tests" % vm_type)
     if vm_type == 'linux':
+        if 'linux64' not in mediaserver_installers:
+            pytest.skip("Mediaserver installer %r is not provided for tests" % vm_type)
         local_deb = mediaserver_installers['linux64']
         linux_installation = DebInstallation(os_access, local_deb)
         return linux_installation
     if vm_type == 'windows':
+        if 'win64' not in mediaserver_installers:
+            pytest.skip("Mediaserver installer %r is not provided for tests" % vm_type)
         local_msi = mediaserver_installers['win64']
         windows_installation = WindowsInstallation(os_access, local_msi)
         return windows_installation
