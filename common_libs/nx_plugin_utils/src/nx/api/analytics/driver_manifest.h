adc623d44b261d21e370ad5882a3140f2716c55e
Sergei Zaitsev <szaitsev@networkoptix.com>
2018-05-30 22:10:28 +0300

    VMS-9536: SSC RS232 analytics plugin integration
    Now the plugin is available for all cameras.
    
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common_libs/nx_plugin_utils/src/nx/api/analytics/driver_manifest.h
+++ common_libs/nx_plugin_utils/src/nx/api/analytics/driver_manifest.h
@@ -16,15 +16,38 @@ namespace api {
 */
 struct /*NX_PLUGIN_UTILS_API*/ AnalyticsDriverManifestBase
 {
+    Q_GADGET
+    Q_ENUMS(Capability)
+    Q_FLAGS(Capabilities)
+
+public:
+    enum Capability
+    {
+        noCapabilities = 0,
+        needUncompressedVideoFrames_yuv420 = 1 << 0,
+        needUncompressedVideoFrames_argb = 1 << 1,
+        needUncompressedVideoFrames_abgr = 1 << 2,
+        needUncompressedVideoFrames_rgba = 1 << 3,
+        needUncompressedVideoFrames_bgra = 1 << 4,
+        needUncompressedVideoFrames_rgb = 1 << 5,
+        needUncompressedVideoFrames_bgr = 1 << 6,
+        cameraModelIndependent = 1 << 7,
+    };
+    Q_DECLARE_FLAGS(Capabilities, Capability)
+
     QnUuid driverId;
     TranslatableString driverName;
     QStringList acceptedDataTypes;
     QStringList supportedCodecs;
     QStringList supportedHandleTypes;
     QStringList supportedPixelFormats;
+    Capabilities capabilities;
 };
-#define AnalyticsDriverManifestBase_Fields (driverId)(driverName)(acceptedDataTypes)(supportedCodecs)\
-    (supportedHandleTypes)(supportedPixelFormats)
+Q_DECLARE_OPERATORS_FOR_FLAGS(AnalyticsDriverManifestBase::Capabilities)
+QN_ENABLE_ENUM_NUMERIC_SERIALIZATION(AnalyticsDriverManifestBase::Capability)
+
+#define AnalyticsDriverManifestBase_Fields (driverId)(driverName)(acceptedDataTypes)\
+    (supportedCodecs)(supportedHandleTypes)(supportedPixelFormats)(capabilities)
 
 struct /*NX_PLUGIN_UTILS_API*/ AnalyticsDriverManifest: AnalyticsDriverManifestBase
 {
@@ -38,3 +61,9 @@ QN_FUSION_DECLARE_FUNCTIONS(AnalyticsDriverManifest, (json))
 
 } // namespace api
 } // namespace nx
+
+QN_FUSION_DECLARE_FUNCTIONS_FOR_TYPES(
+(nx::api::AnalyticsDriverManifestBase::Capability)
+(nx::api::AnalyticsDriverManifestBase::Capabilities),
+(metatype)(numeric)(lexical)
+)
