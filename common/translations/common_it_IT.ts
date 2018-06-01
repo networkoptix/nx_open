6d06f63da22e8350a9a584f08ba96e5d31327a9f
Sergey Ivanov <sivanov@networkoptix.com>
2018-06-01 14:27:36 +0300

    Update translation files
    <==== Mercurial data ====>
    Branch: mobile_18.1

--- common/translations/common_it_IT.ts
+++ common/translations/common_it_IT.ts
@@ -328,7 +328,7 @@
     </message>
     <message>
       <source>First, try to turn on recording (if it is off) and decrease fps in camera settings (error &quot;%1&quot;).</source>
-      <translation>Innanzitutto, provare ad attivare la registrazione (se è disattivata) e ridurre l'impostazione fps del dispositivo) (errore &quot;%1&quot;).</translation>
+      <translation>Innanzitutto, provare ad attivare la registrazione (se è disattivata) e ridurre i valori fps nelle impostazioni della telecamera (errore &quot;%1&quot;).</translation>
     </message>
     <message>
       <source>First, try to turn on recording (if it is off) and decrease fps in I/O module settings (error &quot;%1&quot;).</source>
