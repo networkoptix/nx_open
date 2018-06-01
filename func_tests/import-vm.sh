5d2818208340e172f9b4bb55a1afedcfb036b42f
George Sovetov <gsovetov@networkoptix.com>
2018-06-01 20:09:00 +0300

    Functional Tests: Fix error messages in import-vm.sh
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/import-vm.sh
+++ func_tests/import-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
-: ${1:?"First parameter is existing VM name without '-template'."}
-: ${2:?"Second parameter is dir to put exported VM to."}
+: ${1:?"First parameter is existing exported VM file name without '-template.ova'."}
+: ${2:?"Second parameter is dir to get exported VM from."}
 NAME="$1-template"
 EXPORTED="$2/$NAME.ova"
 VBoxManage modifyvm "$NAME" --name "$NAME backup" || >&2 echo "OK if not exists."
