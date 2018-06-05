e94139c0fb3c0698a37152f7cec93367b0618eaa
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-06-05 14:30:13 -0700

    NCD-14: Cloud HM Page NCD-20: Create data structure json file
    <==== Mercurial data ====>
    Branch: cloud_dev

--- nx_cloud/cloud_tests/health_site/src/app/info-tile/info-tile.component.ts
+++ nx_cloud/cloud_tests/health_site/src/app/info-tile/info-tile.component.ts
@@ -1,12 +1,13 @@
-import { Input, Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { Input, Component, OnInit, OnChanges } from '@angular/core';
+import * as moment                             from 'moment';
 
 @Component({
     selector: 'app-info-tile',
     templateUrl: './info-tile.component.html',
     styleUrls: ['./info-tile.component.scss']
 })
-export class InfoTileComponent implements OnInit {
+export class InfoTileComponent implements OnInit, OnChanges {
+    @Input() tiles;
     @Input() items;
 
     constructor() {
@@ -15,7 +16,32 @@ export class InfoTileComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngOnChanges() {
+        this.tile_items = this.items || [];
+    }
+
     getClassFor(item) {
         return (item) ? 'healthy' : 'error';
+    }
+
+    getTileItemsFor(tile) {
+        if (!this.items) {
+            return;
+        }
+
+        let tile_items = [];
+
+        tile.sections.forEach((section) => {
+            let item = this.items.filter(item => {
+                if (item.name === section.key) {
+                    return item;
+                }
+            })[0];
+
+            tile_items.push({...section, ...item});
+
+        });
+
+        return tile_items;
     }
 }
--- nx_cloud/cloud_tests/health_site/src/app/services/api.service.ts
+++ nx_cloud/cloud_tests/health_site/src/app/services/api.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getHealthStatus() {
-    return this.http
-               .get('https://api.status.nxvms.com/health');
+    getHealthStatus() {
+        return this.http
+                   .get('https://api.status.nxvms.com/health');
 
-  }
+    }
+
+    getJSON(): Observable<any> {
+        return this.http.get('../assets/layout/tiles.json');
+    }
 }
