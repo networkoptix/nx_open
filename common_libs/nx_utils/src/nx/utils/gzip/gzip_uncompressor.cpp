904f92d12e3279eb2d9765268f904d11e4fdfe91
Denis Klychkov <dklychkov@networkoptix.com>
2018-06-07 12:46:35 +0300

    Refactor usages of zlib.h: remove inclusions from headers
    <==== Mercurial data ====>
    Branch: vms

--- common_libs/nx_utils/src/nx/utils/gzip/gzip_uncompressor.cpp
+++ common_libs/nx_utils/src/nx/utils/gzip/gzip_uncompressor.cpp
@@ -1,26 +1,38 @@
 #include "gzip_uncompressor.h"
 
 #include <nx/utils/log/assert.h>
+#include <nx/utils/zlib.h>
 
-namespace nx {
-namespace utils {
-namespace bstream {
-namespace gzip {
+namespace nx::utils::bstream::gzip {
 
-static const int OUTPUT_BUFFER_SIZE = 16 * 1024;
+static constexpr int kOutputBufferSize = 16 * 1024;
+
+class Uncompressor::Private
+{
+public:
+    enum class State
+    {
+        init,
+        inProgress,
+        done,
+        failed
+    };
+
+    State state = State::init;
+    z_stream zStream;
+    QByteArray outputBuffer;
+};
 
 Uncompressor::Uncompressor(const std::shared_ptr<AbstractByteStreamFilter>& nextFilter):
-    AbstractByteStreamFilter(nextFilter),
-    m_state(State::init)
+    AbstractByteStreamFilter(nextFilter)
 {
-    memset(&m_zStream, 0, sizeof(m_zStream));
-
-    m_outputBuffer.resize(OUTPUT_BUFFER_SIZE);
+    memset(&d->zStream, 0, sizeof(d->zStream));
+    d->outputBuffer.resize(kOutputBufferSize);
 }
 
 Uncompressor::~Uncompressor()
 {
-    inflateEnd(&m_zStream);
+    inflateEnd(&d->zStream);
 }
 
 bool Uncompressor::processData(const QnByteArrayConstRef& data)
@@ -30,65 +42,75 @@ bool Uncompressor::processData(const QnByteArrayConstRef& data)
 
     int zFlushMode = Z_NO_FLUSH;
 
-    m_zStream.next_in = (Bytef*)data.constData();
-    m_zStream.avail_in = (uInt)data.size();
-    m_zStream.next_out = (Bytef*)m_outputBuffer.data();
-    m_zStream.avail_out = (uInt)m_outputBuffer.size();
+    d->zStream.next_in = (Bytef*) data.constData();
+    d->zStream.avail_in = (uInt) data.size();
+    d->zStream.next_out = (Bytef*) d->outputBuffer.data();
+    d->zStream.avail_out = (uInt) d->outputBuffer.size();
 
-    for (;; )
+    for (;;)
     {
         int zResult = Z_OK;
-        switch (m_state)
+        switch (d->state)
         {
-            case State::init:
-            case State::done:   //to support stream of gzipped files
-                zResult = inflateInit2(&m_zStream, 16 + MAX_WBITS);
+            case Private::State::init:
+            case Private::State::done: //< To support stream of gzipped files.
+                zResult = inflateInit2(&d->zStream, 16 + MAX_WBITS);
                 if (zResult != Z_OK)
                 {
                     NX_ASSERT(false);
                 }
-                m_state = State::inProgress;
+                d->state = Private::State::inProgress;
                 continue;
 
-            case State::inProgress:
+            case Private::State::inProgress:
             {
-                //NOTE assuming that inflate always eat all input stream if output buffer is available
-                const uInt availInBak = m_zStream.avail_in;
-                zResult = inflate(&m_zStream, zFlushMode);
-                const uInt inBytesConsumed = availInBak - m_zStream.avail_in;
+                // Note, assuming that inflate always eat all input stream if output buffer is
+                // available.
+                const uInt availInBak = d->zStream.avail_in;
+                zResult = inflate(&d->zStream, zFlushMode);
+                const uInt inBytesConsumed = availInBak - d->zStream.avail_in;
+
                 switch (zResult)
                 {
                     case Z_OK:
                     case Z_STREAM_END:
                     {
                         if (zResult == Z_STREAM_END)
-                            m_state = State::done;
+                            d->state = Private::State::done;
 
-                        if ((m_zStream.avail_out == 0) && (m_zStream.avail_in > 0))
+                        if (d->zStream.avail_out == 0 && d->zStream.avail_in > 0)
                         {
-                            //setting new output buffer and calling inflate once again
-                            m_nextFilter->processData(m_outputBuffer);
-                            m_zStream.next_out = (Bytef*)m_outputBuffer.data();
-                            m_zStream.avail_out = (uInt)m_outputBuffer.size();
+                            // Setting new output buffer and calling inflate once again.
+                            m_nextFilter->processData(d->outputBuffer);
+                            d->zStream.next_out = (Bytef*) d->outputBuffer.data();
+                            d->zStream.avail_out = (uInt) d->outputBuffer.size();
                             continue;
                         }
-                        else if (m_zStream.avail_in == 0)
+                        else if (d->zStream.avail_in == 0)
                         {
-                            //input depleted
-                            return m_nextFilter->processData(QnByteArrayConstRef(m_outputBuffer, 0, m_outputBuffer.size() - m_zStream.avail_out));
+                            // Input depleted.
+                            return m_nextFilter->processData(QnByteArrayConstRef(
+                                d->outputBuffer,
+                                0,
+                                (uInt) d->outputBuffer.size() - d->zStream.avail_out));
                         }
-                        else    //m_zStream.avail_out > 0 && m_zStream.avail_in > 0
+                        else //< d->zStream.avail_out > 0 && d->zStream.avail_in > 0
                         {
-                            if (m_zStream.avail_out < (unsigned int)m_outputBuffer.size())
+                            if (d->zStream.avail_out < (uInt) d->outputBuffer.size())
                             {
-                                m_nextFilter->processData(QnByteArrayConstRef(m_outputBuffer, 0, m_outputBuffer.size() - m_zStream.avail_out));
-                                m_zStream.next_out = (Bytef*)m_outputBuffer.data();
-                                m_zStream.avail_out = (uInt)m_outputBuffer.size();
+                                m_nextFilter->processData(QnByteArrayConstRef(
+                                    d->outputBuffer,
+                                    0,
+                                    (uInt) d->outputBuffer.size() - d->zStream.avail_out));
+                                d->zStream.next_out = (Bytef*) d->outputBuffer.data();
+                                d->zStream.avail_out = (uInt) d->outputBuffer.size();
                             }
-                            else if (inBytesConsumed == 0) //&& m_zStream.avail_out == m_outputBuffer.size()
+                            else if (inBytesConsumed == 0)
+                                //< && d->zStream.avail_out == d->outputBuffer.size()
                             {
-                                //zlib does not consume any input data and does not write anything to output
-                                m_state = State::failed;    //to avoid inifinite loop
+                                // Zlib does not consume any input data and does not write anything
+                                // to output.
+                                d->state = Private::State::failed; //< To avoid inifinite loop.
                             }
                             continue;
                         }
@@ -97,46 +119,54 @@ bool Uncompressor::processData(const QnByteArrayConstRef& data)
                     }
 
                     case Z_BUF_ERROR:
-                        //this error is recoverable
-                        if (m_zStream.avail_in > 0)
+                        // This error is recoverable.
+                        if (d->zStream.avail_in > 0)
                         {
-                            //may be some more out buf is required?
-                            if (m_zStream.avail_out < (unsigned int)m_outputBuffer.size())
+                            // May be some more out buf is required?
+                            if (d->zStream.avail_out < (uInt) d->outputBuffer.size())
                             {
-                                m_nextFilter->processData(QnByteArrayConstRef(m_outputBuffer, 0, m_outputBuffer.size() - m_zStream.avail_out));
-                                m_zStream.next_out = (Bytef*)m_outputBuffer.data();
-                                m_zStream.avail_out = (uInt)m_outputBuffer.size();
-                                continue;   //trying with more output buffer
+                                m_nextFilter->processData(QnByteArrayConstRef(
+                                    d->outputBuffer,
+                                    0,
+                                    (uInt) d->outputBuffer.size() - d->zStream.avail_out));
+                                d->zStream.next_out = (Bytef*) d->outputBuffer.data();
+                                d->zStream.avail_out = (uInt) d->outputBuffer.size();
+                                continue; //< Trying with more output buffer.
                             }
                             else if (zFlushMode == Z_NO_FLUSH)
                             {
-                                //trying inflate with Z_SYNC_FLUSH
+                                // Trying inflate with Z_SYNC_FLUSH.
                                 zFlushMode = Z_SYNC_FLUSH;
                                 continue;
                             }
                             else
                             {
-                                //TODO/IMPL we SHOULD cache left input data and give it to inflate along with next input data portion
+                                // TODO: We should cache left input data and give it to inflate
+                                // along with next input data portion.
                             }
                         }
-                        else    //m_zStream.avail_in == 0
+                        else //< d->zStream.avail_in == 0
                         {
-                            if (m_zStream.avail_out > 0)
-                                m_nextFilter->processData(QnByteArrayConstRef(m_outputBuffer, 0, m_outputBuffer.size() - m_zStream.avail_out));
+                            if (d->zStream.avail_out > 0)
+                            {
+                                m_nextFilter->processData(QnByteArrayConstRef(
+                                    d->outputBuffer,
+                                    0,
+                                    (uInt) d->outputBuffer.size() - d->zStream.avail_out));
+                            }
                             return true;
                         }
-                        m_state = State::failed;
+                        d->state = Private::State::failed;
                         break;
 
                     default:
-                        m_state = State::failed;
+                        d->state = Private::State::failed;
                         break;
                 }
                 break;
             }
 
-            case State::failed:
-                //case State::done:
+            case Private::State::failed:
                 break;
 
             default:
@@ -151,21 +181,20 @@ bool Uncompressor::processData(const QnByteArrayConstRef& data)
 
 size_t Uncompressor::flush()
 {
-    m_zStream.next_in = 0;
-    m_zStream.avail_in = 0;
-    m_zStream.next_out = (Bytef*)m_outputBuffer.data();
-    m_zStream.avail_out = (uInt)m_outputBuffer.size();
+    d->zStream.next_in = nullptr;
+    d->zStream.avail_in = 0;
+    d->zStream.next_out = (Bytef*) d->outputBuffer.data();
+    d->zStream.avail_out = (uInt) d->outputBuffer.size();
 
-    int zResult = inflate(&m_zStream, Z_SYNC_FLUSH);
-    if ((zResult == Z_OK || zResult == Z_STREAM_END) && ((unsigned int)m_outputBuffer.size() > m_zStream.avail_out))
+    int zResult = inflate(&d->zStream, Z_SYNC_FLUSH);
+    if ((zResult == Z_OK || zResult == Z_STREAM_END)
+        && (uInt) d->outputBuffer.size() > d->zStream.avail_out)
     {
-        m_nextFilter->processData(QnByteArrayConstRef(m_outputBuffer, 0, m_outputBuffer.size() - m_zStream.avail_out));
-        return m_outputBuffer.size() - m_zStream.avail_out;
+        m_nextFilter->processData(QnByteArrayConstRef(
+            d->outputBuffer, 0, (uInt) d->outputBuffer.size() - d->zStream.avail_out));
+        return (uInt) d->outputBuffer.size() - d->zStream.avail_out;
     }
     return 0;
 }
 
-} // namespace gzip
-} // namespace bstream
-} // namespace utils
-} // namespace nx
+} // namespace nx::utils::bstream::gzip
