68df9c878982d1f98ad5b2d545b0daaa42a35a78
George Sovetov <gsovetov@networkoptix.com>
2018-06-07 12:06:02 +0300

    Functional Tests: Hyphen in backup VM name import-vm.sh
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/import-vm.sh
+++ func_tests/import-vm.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 : ${2:?"Second parameter is dir to get exported VM from."}
 NAME="$1-template"
 EXPORTED="$2/$NAME.ova"
-VBoxManage modifyvm "$NAME" --name "$NAME backup" || >&2 echo "OK if not exists."
+VBoxManage modifyvm "$NAME" --name "$NAME-backup" || >&2 echo "OK if not exists."
 VBoxManage import "$EXPORTED"  --vsys 0 --vmname "$NAME"
 VBoxManage snapshot "$NAME" take "template"
-VBoxManage unregistervm "$NAME backup" --delete || >&2 echo "OK if not exists."
+VBoxManage unregistervm "$NAME-backup" --delete || >&2 echo "OK if not exists."
