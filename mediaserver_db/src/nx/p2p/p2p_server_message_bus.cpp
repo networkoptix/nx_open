eb0114503aef3383b6377e867d273a340abe10aa
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-06-13 18:30:31 +0300

    VMS-CR-11132 fixes
    <==== Mercurial data ====>
    Branch: vms

--- mediaserver_db/src/nx/p2p/p2p_server_message_bus.cpp
+++ mediaserver_db/src/nx/p2p/p2p_server_message_bus.cpp
@@ -45,7 +45,7 @@ struct SendTransactionToTransportFuction
     {
         ApiPersistentIdData tranId(transaction.peerID, transaction.persistentInfo.dbID);
         NX_ASSERT(bus->context(connection)->isRemotePeerSubscribedTo(tranId));
-        NX_ASSERT(ApiPersistentIdData(connection->remotePeer()) != tranId, "Loop detected");
+        NX_ASSERT(!(ApiPersistentIdData(connection->remotePeer()) == tranId), "Loop detected");
 
         switch (connection->remotePeer().dataFormat)
         {
