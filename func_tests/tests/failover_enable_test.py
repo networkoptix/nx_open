1c9212f8317210514caaf1426418669a85234674
Artem Nikitin <anikitin@networkoptix.com>
2018-06-14 19:59:16 +0300

    Funtest: fix failover test
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/tests/failover_enable_test.py
+++ func_tests/tests/failover_enable_test.py
@@ -14,9 +14,10 @@ import pytest
 import server_api_data_generators as generator
 from framework.api_shortcuts import get_server_id, get_system_settings
 from framework.installation.mediaserver import MEDIASERVER_MERGE_TIMEOUT
-from framework.merging import merge_systems
+from framework.merging import merge_systems, setup_local_system
 from framework.utils import bool_to_str, datetime_utc_now, str_to_bool
 from framework.waiting import wait_for_true
+from framework.pool import ClosingPool
 
 CAMERA_SWITCHING_PERIOD_SEC = 4*60
 
@@ -33,9 +34,38 @@ class Counter(object):
         return self._value
 
 
+
+
+
 @pytest.fixture
 def counter():
     return Counter()
+
+
+@pytest.fixture
+def layout():
+    return {
+        'networks':
+        {
+            '10.254.0.0/28':
+            {
+                'one': None,
+                'two': None,
+                'three': None
+            }
+        }
+    }
+
+
+@pytest.fixture
+def three_mediaservers(network, mediaserver_factory):
+    allocated_mediaservers = []
+    with ClosingPool(mediaserver_factory.allocated_mediaserver, network, None) as mediaservers:
+        for name in ['one', 'two', 'three']:
+            mediaserver = mediaservers.get(name)
+            mediaserver.start()
+            allocated_mediaservers.append(mediaserver)
+        yield allocated_mediaservers
 
 
 ServerRec = namedtuple('ServerRec', 'server camera_mac_set')
@@ -44,31 +74,27 @@ ServerRec = namedtuple('ServerRec', 'server camera_mac_set')
 # To catch exact moment when servers merge complete, we first wait until each server discover all cameras.
 # After that we merge servers and wait until preferred cameras settle down on destined servers.
 
-
-def create_cameras_and_servers(linux_mediaservers_pool, camera_factory, counter, server_name_list):
-    server_count = len(server_name_list)
+def create_cameras_and_setup_servers(servers, camera_factory, counter):
+    server_count = len(servers)
     # we always use 2 cameras per server
     all_camera_mac_set = set(create_cameras(camera_factory, counter, count=server_count*2))
-    server_list = [
-        create_server(linux_mediaservers_pool, server_name, all_camera_mac_set)
-        for server_name in server_name_list]
-    for server in server_list[1:]:
-        server_list[0].merge_systems(server)
-    wait_until_servers_are_online(server_list)
+    for server in servers:
+        setup_server(server, all_camera_mac_set)
+    for server in servers[1:]:
+        merge_systems(servers[0], server)
+    wait_until_servers_are_online(servers)
     server_rec_list = [
         ServerRec(server, set(sorted(all_camera_mac_set)[idx * 2: idx * 2 + 2]))
-        for idx, server in enumerate(server_list)]
+        for idx, server in enumerate(servers)]
     for rec in server_rec_list:
         attach_cameras_to_server(rec.server, rec.camera_mac_set)
     for rec in server_rec_list:
         wait_until_cameras_on_server_reduced_to(rec.server, rec.camera_mac_set)
-    return server_rec_list
 
 
-def create_server(linux_mediaservers_pool, name, all_camera_mac_set, setup_settings=None):
-    server = linux_mediaservers_pool.get(name, setup_settings=setup_settings)
+def setup_server(server, all_camera_mac_set, system_settings=None):
+    setup_local_system(server, system_settings or {})
     wait_until_cameras_are_online(server, all_camera_mac_set)
-    return server
 
 
 def wait_until_servers_are_online(servers):
@@ -152,10 +178,9 @@ def get_server_camera_macs(server):
 # Based on testrail C744 Check "Enable failover" for server
 # https://networkoptix.testrail.net/index.php?/cases/view/744
 @pytest.mark.testcam
-def test_enable_failover_on_one_server(linux_mediaservers_pool, camera_factory, counter):
-    one, two, three = create_cameras_and_servers(
-        linux_mediaservers_pool, camera_factory, counter,
-        ['one', 'two', 'three'])
+def test_enable_failover_on_one_server(three_mediaservers, camera_factory, counter):
+    one, two, three = three_mediaservers
+    create_cameras_and_setup_servers([one, two, three], camera_factory, counter)
     two.server.api.ec2.saveMediaServerUserAttributes.POST(
         serverId=get_server_id(two.server.api),
         maxCameras=4,
@@ -174,10 +199,9 @@ def test_enable_failover_on_one_server(linux_mediaservers_pool, camera_factory, 
 
 
 @pytest.mark.testcam
-def test_enable_failover_on_two_servers(linux_mediaservers_pool, camera_factory, counter):
-    one, two, three = create_cameras_and_servers(
-        linux_mediaservers_pool, camera_factory, counter,
-        ['one', 'two', 'three'])
+def test_enable_failover_on_two_servers(three_mediaservers, camera_factory, counter):
+    one, two, three = three_mediaservers
+    create_cameras_and_setup_servers([one, two, three], camera_factory, counter)
     one.server.api.ec2.saveMediaServerUserAttributes.POST(
         serverId=get_server_id(one.server.api),
         maxCameras=3,
@@ -211,10 +235,11 @@ def discovery(request):
 # Based on testrail C745 Check "Enable failover" for server
 # https://networkoptix.testrail.net/index.php?/cases/view/745
 @pytest.mark.testcam
-def test_failover_and_auto_discovery(linux_mediaservers_pool, camera_factory, counter, discovery):
+def test_failover_and_auto_discovery(two_clean_mediaservers, camera_factory, counter, discovery):
     camera_mac_set = set(create_cameras(camera_factory, counter, count=2))
-    one = create_server(linux_mediaservers_pool, 'one', camera_mac_set)
-    two = create_server(linux_mediaservers_pool, 'two', set(), setup_settings=dict(
+    one, two = two_clean_mediaservers
+    setup_server(one, camera_mac_set)
+    setup_server(two, set(), system_settings=dict(
         systemSettings=dict(autoDiscoveryEnabled=bool_to_str(discovery))))
     assert str_to_bool(get_system_settings(two.api)['autoDiscoveryEnabled']) == discovery
     attach_cameras_to_server(one, camera_mac_set)
@@ -239,8 +264,9 @@ def test_failover_and_auto_discovery(linux_mediaservers_pool, camera_factory, co
 # Based on testrail C747 Check "Enable failover" for server
 # https://networkoptix.testrail.net/index.php?/cases/view/747
 @pytest.mark.testcam
-def test_max_camera_settings(linux_mediaservers_pool, camera_factory, counter):
-    one, two = create_cameras_and_servers(linux_mediaservers_pool, camera_factory, counter, ['one', 'two'])
+def test_max_camera_settings(two_clean_mediaservers, camera_factory, counter):
+    one, two = two_clean_mediaservers
+    create_cameras_and_setup_servers([one, two], camera_factory, counter)
     one.server.api.ec2.saveMediaServerUserAttributes.POST(
         serverId=get_server_id(one.server.api),
         maxCameras=3,
