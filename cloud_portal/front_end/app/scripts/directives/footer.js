d78c24e5bfba677c1ae897754124ad398e18adf6
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-06-15 17:09:18 -0700

    CLOUD-1945: If user has permissions to see /downloads/history - he need to see a link for that. Either in footer, on in top right menu
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/scripts/directives/footer.js
+++ cloud_portal/front_end/app/scripts/directives/footer.js
@@ -6,17 +6,25 @@
         .module('cloudApp.directives')
         .directive('nxFooter', NxFooter);
 
-    NxFooter.$inject = [ 'configService' ];
+    NxFooter.$inject = [ 'configService', 'account' ];
 
-    function NxFooter(configService) {
+    function NxFooter(configService, account) {
 
-        var CONFIG = configService.config;
+        var CONFIG = configService.config,
+            user = {};
 
         return {
             restrict   : 'E',
             templateUrl: CONFIG.viewsDir + 'components/footer.html',
             link       : function (scope, element, attrs) {
+                scope.canViewHistory = false;
 
+                account.get().then(function (account) {
+                    user = account;
+                    if (user.permissions.indexOf('can_view_release') > -1) {
+                        scope.canViewHistory = true;
+                    }
+                });
             }
         }
     }
