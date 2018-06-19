9678e9b986ae891bd59495ba8ae20ef8cb2fef5c
Dmitry Mishin <dmishin@networkoptix.com>
2018-06-19 21:48:27 +0300

    VMS-10380: Relative movement via continuous movement support
    In progress.
    
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common/src/nx/core/ptz/realtive/relative_move_workaround_controller.cpp
+++ common/src/nx/core/ptz/realtive/relative_move_workaround_controller.cpp
@@ -78,7 +78,10 @@ RelativeMoveWorkaroundController::RelativeMoveWorkaroundController(
     const QnPtzControllerPtr& controller)
     :
     base_type(controller),
-    m_continuousMoveEngine(std::make_unique<RelativeContinuousMoveEngine>()),
+    m_continuousMoveEngine(
+        std::make_unique<RelativeContinuousMoveEngine>(
+            controller.data(),
+            RelativeContinuousMoveMapping())), //< TODO: #dmishin pass mapping to controller.
     m_absoluteMoveEngine(std::make_unique<RelativeAbsoluteMoveEngine>(controller.data()))
 {
 }
