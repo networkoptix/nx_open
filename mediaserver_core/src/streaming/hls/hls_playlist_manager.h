0f4fef2ca5ec72a988628a2f504c6cf7e324dcbe
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-06-20 15:11:27 +0300

    Coding style fixes: indents
    <==== Mercurial data ====>
    Branch: vms_3.2

--- mediaserver_core/src/streaming/hls/hls_playlist_manager.h
+++ mediaserver_core/src/streaming/hls/hls_playlist_manager.h
@@ -9,48 +9,48 @@
 
 namespace nx_hls
 {
-    class AbstractPlaylistManager
+class AbstractPlaylistManager
+{
+public:
+    class ChunkData
     {
     public:
-        class ChunkData
-        {
-        public:
-            boost::optional<QString> alias;
-            //!internal timestamp (micros). Not a calendar time
-            quint64 startTimestamp;
-            //!in micros
-            quint64 duration;
-            //!sequential number of this chunk
-            unsigned int mediaSequence;
-            //!if true, there is discontinuity between this chunk and preceding one
-            bool discontinuity;
+        boost::optional<QString> alias;
+        //!internal timestamp (micros). Not a calendar time
+        quint64 startTimestamp;
+        //!in micros
+        quint64 duration;
+        //!sequential number of this chunk
+        unsigned int mediaSequence;
+        //!if true, there is discontinuity between this chunk and preceding one
+        bool discontinuity;
 
-            ChunkData();
-            ChunkData(
-                quint64 _startTimestamp,
-                quint64 _duration,
-                unsigned int _mediaSequence,
-                bool _discontinuity = false );
-            ChunkData(
-                const QString& _alias,
-                unsigned int _mediaSequence,
-                bool _discontinuity = false );
-        };
-
-        virtual ~AbstractPlaylistManager() = default;
-
-        //!Generates chunks and appends them to \a chunkList
-        /*!
-            \param endOfStreamReached Can be NULL
-            \return Number of chunks generated
-        */
-        virtual size_t generateChunkList(
-            std::vector<ChunkData>* const chunkList,
-            bool* const endOfStreamReached ) const = 0;
-        //!Returns maximum stream bitrate in bps
-        virtual int getMaxBitrate() const = 0;
+        ChunkData();
+        ChunkData(
+            quint64 _startTimestamp,
+            quint64 _duration,
+            unsigned int _mediaSequence,
+            bool _discontinuity = false );
+        ChunkData(
+            const QString& _alias,
+            unsigned int _mediaSequence,
+            bool _discontinuity = false );
     };
 
-    //!Using std::shared_ptr for \a std::shared_ptr::unique()
-    typedef std::shared_ptr<AbstractPlaylistManager> AbstractPlaylistManagerPtr;
+    virtual ~AbstractPlaylistManager() = default;
+
+    //!Generates chunks and appends them to \a chunkList
+    /*!
+        \param endOfStreamReached Can be NULL
+        \return Number of chunks generated
+    */
+    virtual size_t generateChunkList(
+        std::vector<ChunkData>* const chunkList,
+        bool* const endOfStreamReached ) const = 0;
+    //!Returns maximum stream bitrate in bps
+    virtual int getMaxBitrate() const = 0;
+};
+
+//!Using std::shared_ptr for \a std::shared_ptr::unique()
+typedef std::shared_ptr<AbstractPlaylistManager> AbstractPlaylistManagerPtr;
 }
