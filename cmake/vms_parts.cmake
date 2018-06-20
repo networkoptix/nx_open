54717644ad7d6e78bf31af15e2a189de84d74a75
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-06-20 19:42:54 +0300

    VMS-10672: Moved certain plugins to plugins_optional dir, excluding them from distribution
    Removed cmake configuration variable withPluginStubs: now stub_metadata_plugin is always compiled.
    
    Plugins made optional:
    - stub_metadata_plugin
    - image_library_plugin
    
    NOTE: This feature was back-ported from the next vms version. The ability to load plugins from the
    plugins_optional dir via plugins.ini was not back-ported.
    
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- cmake/vms_parts.cmake
+++ cmake/vms_parts.cmake
@@ -65,7 +65,6 @@ option(withMobileClient "Enable mobile client" ${_withMobileClient})
 option(withClouds "Enable cloud components" ${_withClouds})
 option(withTestCamera "Enable test camera" ${_withTestCamera})
 option(withTests "Enable unit tests" ${_withTests})
-option(withPluginStubs "Enable plugin stubs" OFF)
 option(withMiniLauncher "Enable minilauncher" ${_withMiniLauncher})
 option(withScreenChecker "Enable screen checker" OFF)
 
