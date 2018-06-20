956e230f3af3703ee3555da3d63b94dfec352724
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-06-19 17:18:33 -0700

    CLOUD-1857: Footer disappears after opening "View" tab for an offline system
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/scripts/controllers/view.js
+++ cloud_portal/front_end/app/scripts/controllers/view.js
@@ -1,41 +1,52 @@
-'use strict';
+(function () {
 
-angular.module('cloudApp')
-    .controller('ViewPageCtrl', ['$scope', 'account', 'system', '$routeParams', 'systemAPI', 'dialogs',
-    '$location', '$q', '$poll', 'authorizationCheckService',
-    function ($scope, account, system, $routeParams, systemAPI, dialogs, $location, $q, $poll, authorizationCheckService) {
-        authorizationCheckService.requireLogin().then(function(account){
-            $scope.currentSystem = system($routeParams.systemId, account.email);
-            var systemInfoRequest = $scope.currentSystem.getInfo();
-            var systemAuthRequest = $scope.currentSystem.updateSystemAuth();
-            $q.all([systemInfoRequest,systemAuthRequest]).then(function(){
-                $scope.systemReady = true;
-                $scope.system = $scope.currentSystem.mediaserver;
-                delayedUpdateSystemInfo();
-            },function(){
-                dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}",
-                               $scope.currentSystem.name || L.errorCodes.thisSystem), 'warning');
-                $location.path("/systems");
-            });
-        });
+    'use strict';
 
-        function delayedUpdateSystemInfo(){
-            var pollingSystemUpdate = $poll(function(){
-                return $scope.currentSystem.update();
-            },Config.updateInterval);
+    angular
+        .module('cloudApp')
+        .controller('ViewPageCtrl', [ '$rootScope', '$scope', 'account', 'system', '$routeParams', 'systemAPI', 'dialogs',
+            '$location', '$q', '$poll', 'authorizationCheckService',
 
-            $scope.$on('$destroy', function( event ) {
-                $poll.cancel(pollingSystemUpdate);
-            });
-        }
+            function ($rootScope, $scope, account, system, $routeParams, systemAPI, dialogs, $location, $q, $poll, authorizationCheckService) {
+                authorizationCheckService
+                    .requireLogin()
+                    .then(function (account) {
+                        $scope.currentSystem = system($routeParams.systemId, account.email);
+                        var systemInfoRequest = $scope.currentSystem.getInfo();
+                        var systemAuthRequest = $scope.currentSystem.updateSystemAuth();
+                        $q.all([ systemInfoRequest, systemAuthRequest ]).then(function () {
+                            $scope.systemReady = true;
+                            $scope.system = $scope.currentSystem.mediaserver;
+                            delayedUpdateSystemInfo();
 
-        var cancelSubscription = $scope.$on("unauthorized_" + $routeParams.systemId,function(event,data){
-            dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}",
-                           $scope.currentSystem.info.name || L.errorCodes.thisSystem), 'warning');
-            $location.path("/systems");
-        });
+                            $rootScope.$emit('nx.system.online', $scope.currentSystem.isOnline);
 
-        $scope.$on('$destroy', function( event ) {
-            cancelSubscription();
-        });
-    }]);
+                        }, function () {
+                            dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}",
+                                $scope.currentSystem.name || L.errorCodes.thisSystem), 'warning');
+                            $location.path("/systems");
+                        });
+                    });
+
+                function delayedUpdateSystemInfo() {
+                    var pollingSystemUpdate = $poll(function () {
+                        return $scope.currentSystem.update();
+                    }, Config.updateInterval);
+
+                    $scope.$on('$destroy', function (event) {
+                        $poll.cancel(pollingSystemUpdate);
+                    });
+                }
+
+                var cancelSubscription = $scope.$on("unauthorized_" + $routeParams.systemId, function (event, data) {
+                    dialogs.notify(L.errorCodes.lostConnection.replace("{{systemName}}",
+                        $scope.currentSystem.info.name || L.errorCodes.thisSystem), 'warning');
+                    $location.path("/systems");
+                });
+
+                $scope.$on('$destroy', function (event) {
+                    cancelSubscription();
+                    $rootScope.$emit('nx.system.online', false);
+                });
+            } ]);
+})();
--- cloud_portal/front_end/app/scripts/directives/footer.js
+++ cloud_portal/front_end/app/scripts/directives/footer.js
@@ -6,17 +6,18 @@
         .module('cloudApp.directives')
         .directive('nxFooter', NxFooter);
 
-    NxFooter.$inject = [ 'configService', 'account' ];
+    NxFooter.$inject = [ '$rootScope', 'configService', 'account' ];
 
-    function NxFooter(configService, account) {
+    function NxFooter($rootScope, configService, account) {
 
         var CONFIG = configService.config,
-            user = {};
+            user   = {};
 
         return {
             restrict   : 'E',
             templateUrl: CONFIG.viewsDir + 'components/footer.html',
             link       : function (scope, element, attrs) {
+                scope.viewFooter = true;
                 scope.canViewHistory = false;
 
                 account.get().then(function (account) {
@@ -24,6 +25,10 @@
                     if (user.permissions.indexOf('can_view_release') > -1) {
                         scope.canViewHistory = true;
                     }
+                });
+
+                $rootScope.$on('nx.system.online', function (evt, state) {
+                    scope.viewFooter = !state;
                 });
             }
         }
