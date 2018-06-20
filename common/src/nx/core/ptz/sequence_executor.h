ec26dbdd99ffa86f6d3fa3cd6b6c2ababe80a8e5
Dmitry Mishin <dmishin@networkoptix.com>
2018-06-20 20:56:31 +0300

    VMS-10380: Implemented ptz command sequence executor
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common/src/nx/core/ptz/sequence_executor.h
+++ common/src/nx/core/ptz/sequence_executor.h
@@ -1,21 +1,46 @@
 #pragma once
 
+#include <memory>
+
+#include <QtCore/QThreadPool>
+
 #include <core/ptz/abstract_ptz_controller.h>
+
+#include <nx/utils/thread/mutex.h>
 #include <nx/core/ptz/timed_command.h>
+#include <nx/network/aio/timer.h>
 
 namespace nx {
 namespace core {
 namespace ptz {
 
+using TimedCommandDoneCallback = nx::utils::MoveOnlyFunc<void()>;
+
 class SequenceExecutor
 {
 public:
-    SequenceExecutor(QnAbstractPtzController* controller);
+    SequenceExecutor(
+        QnAbstractPtzController* controller,
+        QThreadPool* threadPool);
+
+    ~SequenceExecutor();
 
     bool executeSequence(const CommandSequence& sequence);
 
 private:
+    bool executeSequenceInternal();
+
+    bool executeTimedCommand(
+        const TimedCommand& timedCommand,
+        TimedCommandDoneCallback commandDoneCallback);
+
+private:
+    mutable QnMutex m_mutex;
     QnAbstractPtzController* m_controller;
+    std::unique_ptr<nx::network::aio::Timer> m_timer;
+    QThreadPool* m_threadPool;
+    CommandSequence m_sequence;
+    bool m_terminated = false;
 };
 
 } // namespace ptz
