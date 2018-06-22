6006d53918b441a359acb05b83e8286d2aa5a279
Denis Klychkov <dklychkov@networkoptix.com>
2018-06-22 11:55:41 +0300

    Windows installer is now signed by certificates taken from RDep
    <==== Mercurial data ====>
    Branch: vms_3.1

--- wixsetup/maven/filter-resources/sign.bat
+++ wixsetup/maven/filter-resources/sign.bat
@@ -1,15 +1,21 @@
 @echo off
 SET PASSWORD=${sign.password}
 SET CERTIFICATE=${sign.intermediate.cer}
+SET SIGNCER=${certificates_dir}/wixsetup/${sign.cer}
 SET TIMESTAMP=
 IF NOT ["${beta}"] == ["true"] (
     SET TIMESTAMP=/tr http://timestamp.digicert.com/
 )
 
-IF NOT [%PASSWORD%] == [] (
-    IF NOT [%CERTIFICATE%] == [] (
-        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
-    ) ELSE (
-        call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
-    )
-) ELSE call ${environment.dir}\bin\signtool.exe sign /td sha256 /fd sha256 %TIMESTAMP% /d "${company.name} ${display.product.name}" /f ${sign.cer} %*
+IF NOT ["${windows.skip.sign}"] == ["true"] (
+    IF NOT [%PASSWORD%] == [] (
+        IF NOT [%CERTIFICATE%] == [] (
+            set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /ac ${sign.intermediate.cer} /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
+        ) ELSE (
+            set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /a /v /p ${sign.password} /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
+        )
+    ) ELSE set command="${environment.dir}\bin\signtool.exe" sign /td sha256 /fd sha256 %TIMESTAMP% /d "${company.name} ${display.product.name}" /f %SIGNCER% %*
+)
+echo signing %*
+echo %command%
+call %command%
