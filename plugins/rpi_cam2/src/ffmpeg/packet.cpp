3c6ba8d1a9cf4fe8644b0de8c1c3ae81c9a817b7
Nate Wilson <nwilson@networkoptix.com>
2018-06-27 16:36:37 -0700

    VMS-927: Move Packet implementation from header file to cpp file.
    <==== Mercurial data ====>
    Branch: webcam_plugin

---
+++ plugins/rpi_cam2/src/ffmpeg/packet.cpp
@@ -0,0 +1,53 @@
+#include "packet.h"
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+} // extern "C"
+
+#include "error.h"
+
+namespace nx {
+namespace webcam_plugin {
+namespace ffmpeg {
+
+Packet::Packet():
+    m_packet(av_packet_alloc())
+{
+    if(m_packet)
+        initialize();
+    else
+        error::setLastError(AVERROR(ENOMEM));
+}
+
+Packet::~Packet()
+{
+    av_packet_free(&m_packet);
+}
+
+AVPacket * Packet::packet() const
+{
+    return m_packet;
+}
+
+void Packet::initialize()
+{
+    av_init_packet(m_packet);
+    m_packet->data = nullptr;
+    m_packet->size = 0;
+}
+
+void Packet::unreference()
+{
+    av_packet_unref(m_packet);
+}
+
+int Packet::copy(Packet * outPacket) const
+{
+    int copyCode = av_copy_packet(outPacket->m_packet, m_packet);
+    error::updateIfError(copyCode);
+    return copyCode;
+}
+
+} // namespace ffmpeg
+} // namespace webcam_plugin
+} // namespace nx
--- plugins/rpi_cam2/src/ffmpeg/packet.h
+++ plugins/rpi_cam2/src/ffmpeg/packet.h
@@ -6,6 +6,8 @@ extern "C"{
 
 #include "error.h"
 
+class AVPacket;
+
 namespace nx {
 namespace webcam_plugin {
 namespace ffmpeg {
@@ -13,43 +15,15 @@ namespace ffmpeg {
 class Packet
 {
 public:
-    Packet():
-        m_packet(av_packet_alloc())
-    {
-        if(m_packet)
-            initialize();
-        else
-            error::setLastError(AVERROR(ENOMEM));
-    }
+    Packet();
+    ~Packet();
 
-    ~Packet()
-    {
-        av_packet_free(&m_packet);
-    }
-
-    AVPacket * packet() const
-    {
-        return m_packet;
-    }
-
-    void initialize()
-    {
-        av_init_packet(m_packet);
-        m_packet->data = nullptr;
-        m_packet->size = 0;
-    }
-
-    void unreference()
-    {
-        av_packet_unref(m_packet);
-    }
-
-    int copy(Packet * outPacket) const
-    {
-        int copyCode = av_copy_packet(outPacket->m_packet, m_packet);
-        error::updateIfError(copyCode);
-        return copyCode;
-    }
+    AVPacket * packet() const;
+    
+    void initialize();
+    void unreference();
+    
+    int copy(Packet * outPacket) const;
 
 private:
     AVPacket* m_packet;
