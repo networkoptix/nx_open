398c8208fcd5c4cfc931ae911e011a416ed55bf2
George Sovetov <gsovetov@networkoptix.com>
2018-07-04 21:00:24 +0300

    FT: windows_scripts: Don't remove Appx packages
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/windows_scripts/PrepareAndShutdown.cmd
+++ func_tests/windows_scripts/PrepareAndShutdown.cmd
@@ -3,12 +3,6 @@
 @rem Disable WinRM rules, enable them in SetupComplete.cmd via quickconfig.
 powershell -NonInteractive -ExecutionPolicy Unrestricted -Command "Disable-NetFirewallRule -DisplayGroup 'Windows Remote Management'"
 
-@echo Remove Appx packages that can be removed.
-@echo They can cause SysPrep to fail.
-@echo See: https://social.technet.microsoft.com/Forums/windows/en-US/c18af9ed-bd5b-4eac-bf03-bec89aca1ae3
-@echo Expect many error messages about packages that cannot be removed.
-powershell -NonInteractive -ExecutionPolicy Unrestricted -Command "Get-AppxPackage | Remove-AppxPackage" -ErrorAction SilentlyContinue
-
 @echo Defragment, consolidate free space on disk.
 @echo Zero-fill free space to make virtual disk size less.
 defrag C: /X /H /U /V
