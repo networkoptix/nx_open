24aad2db5605ad329d973035423c23476fa8ba48
George Sovetov <gsovetov@networkoptix.com>
2018-07-04 17:17:15 +0300

    FT: export-vm.sh: Don't create temporary to back up in case of error
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/export-vm.sh
+++ func_tests/export-vm.sh
@@ -4,8 +4,5 @@ set -euxo pipefail
 : ${2:?"Second parameter is path to new VM image."}
 NAME="$1"
 EXPORTED="$2"
-TEMP="$2.bu"
-mkdir -p "$(dirname "$TEMP")"
-VBoxManage export "$NAME" -o "$TEMP" --options nomacs
 mkdir -p "$(dirname "$EXPORTED")"
-mv "$TEMP" "$EXPORTED"
+VBoxManage export "$NAME" -o "$EXPORTED" --options nomacs
