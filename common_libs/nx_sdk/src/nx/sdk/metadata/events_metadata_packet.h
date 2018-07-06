17002bf025368777a92b8f344a10572c4597fe99
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-07-06 16:01:21 +0300

    Minor coding style fixes
    <==== Mercurial data ====>
    Branch: meta

--- common_libs/nx_sdk/src/nx/sdk/metadata/events_metadata_packet.h
+++ common_libs/nx_sdk/src/nx/sdk/metadata/events_metadata_packet.h
@@ -7,12 +7,9 @@ namespace nx {
 namespace sdk {
 namespace metadata {
 
-/**
- * Each class that implements Event interface
- * should properly handle this GUID in its queryInterface method
- */
-static const nxpl::NX_GUID IID_Event
-    = {{0xd5, 0xe1, 0x49, 0x96, 0x63, 0x33, 0x42, 0x5a, 0x8f, 0xee, 0xbc, 0x23, 0x50, 0x03, 0xc8, 0x0f}};
+/** Each class implementing Event should handle this GUID in queryInterface(). */
+static const nxpl::NX_GUID IID_Event =
+  {{0xd5,0xe1,0x49,0x96,0x63,0x33,0x42,0x5a,0x8f,0xee,0xbc,0x23,0x50,0x03,0xc8,0x0f}};
 
 class Event: public MetadataItem
 {
@@ -38,16 +35,12 @@ public:
     virtual bool isActive() const = 0;
 };
 
-/**
-* Each class that implements EventsMetadataPacket interface
-* should properly handle this GUID in its queryInterface method
-*/
-static const nxpl::NX_GUID IID_EventsMetadataPacket
-    = {{0x20, 0xfc, 0xa8, 0x08, 0x17, 0x6b, 0x48, 0xa6, 0x92, 0xfd, 0xba, 0xb5, 0x9d, 0x37, 0xd7, 0xc0}};
+/** Each class implementing EventsMetadataPacket should handle this GUID in queryInterface(). */
+static const nxpl::NX_GUID IID_EventsMetadataPacket =
+  {{0x20,0xfc,0xa8,0x08,0x17,0x6b,0x48,0xa6,0x92,0xfd,0xba,0xb5,0x9d,0x37,0xd7,0xc0}};
 
 /**
- * @brief The EventsMetadataPacket class is an interface for metadata packet
- * containing information about some event.
+ * Interface for metadata packet containing information about some event.
  */
 class EventsMetadataPacket: public IterableMetadataPacket
 {
