3b6940eb1c1e0ae1b7771aac08d932905115fb0b
George Sovetov <gsovetov@networkoptix.com>
2018-07-12 22:12:00 +0300

    FT-63: Minor fix in DpkgInstallation
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/installation/dpkg_installation.py
+++ func_tests/framework/installation/dpkg_installation.py
@@ -28,7 +28,7 @@ class DpkgInstallation(DebInstallation):
         remote_path = self.os_access.Path.tmp() / installer.path.name
         remote_path.parent.mkdir(parents=True, exist_ok=True)
         copy_file(installer.path, remote_path)
-        self.posix_access.ssh.run_sh_script(
+        self.posix_access.shell.run_sh_script(
             # language=Bash
             '''
                 # Commands and dependencies for trusty template.
