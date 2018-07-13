a2b21d8b5f42ab9d03fbfdc0ed4ad1e65ff6f1ff
Evgeny Balashov <ebalashov@networkoptix.com>
2018-07-13 15:08:55 -0700

    CLOUD-2079: Incorrect image breakes instance
    log and ignore all exceptions while filldata
    
    <==== Mercurial data ====>
    Branch: cloud_18.1

--- cloud_portal/cloud/cloud/settings.py
+++ cloud_portal/cloud/cloud/settings.py
@@ -211,42 +211,47 @@ LOGGING = {
         '': {  # default settings for all django loggers
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.views.utils': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.helpers.exceptions': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.controllers.cloud_gateway': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'notifications.tasks': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.account_backend': {  # explicitly mention all modules with loogers
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.controller.cloud_api': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
         },
         'api.views.account': {
             'level': 'DEBUG',
             'propagate': True,
-            'handlers': ['console', 'mail_admins']
+            'handlers': ['console']
+        },
+        'cms.controllers.filldata': {
+            'level': 'DEBUG',
+            'propagate': True,
+            'handlers': ['console']
         }
     }
 }
