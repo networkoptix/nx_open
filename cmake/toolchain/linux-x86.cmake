7d25d40207577fe38fdf61b3b54f9b5bb224152e
Denis Klychkov <dklychkov@networkoptix.com>
2018-07-16 17:26:38 +0300

    Fix Linux-x86 toolchain path
    <==== Mercurial data ====>
    Branch: x_VMS-8375_qt_5.11_migration

--- cmake/toolchain/linux-x86.cmake
+++ cmake/toolchain/linux-x86.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR i686)
 
-set(cross_prefix ${PACKAGES_DIR}/linux-x64/gcc-8.1/bin/i686-pc-linux-gnu)
+set(cross_prefix ${PACKAGES_DIR}/linux-x86/gcc-8.1/bin/i686-pc-linux-gnu)
 set(CMAKE_C_COMPILER "${cross_prefix}-gcc")
 set(CMAKE_CXX_COMPILER "${cross_prefix}-g++")
 
