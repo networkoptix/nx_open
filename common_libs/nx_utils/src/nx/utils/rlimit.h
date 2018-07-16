ecbc1f0300486722ee18171943f6fa5e4af9886f
Denis Klychkov <dklychkov@networkoptix.com>
2018-07-16 17:41:13 +0300

    Add files forgotten in the previous commit
    <==== Mercurial data ====>
    Branch: vms

---
+++ common_libs/nx_utils/src/nx/utils/rlimit.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace nx {
+namespace utils {
+namespace rlimit {
+
+unsigned long getNoFile();
+unsigned long setNoFile(unsigned long value);
+
+void setDefaultNoFile()
+{
+    constexpr unsigned long kDefaultNoFile = 8192;
+    setNoFile(kDefaultNoFile);
+}
+
+} // namespace rlimit
+} // namespace utils
+} // namespace nx
