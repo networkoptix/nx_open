70a8f304240bf9fe2c4a4e174294331a3b2a08d1
George Sovetov <gsovetov@networkoptix.com>
2018-07-20 21:53:56 +0300

    FT-61, FT-84: Use framework.mediaserver_api.MediaserverApi#set_local_system_id in tests.change_local_system_id_test.test_change_local_system_id
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/tests/change_local_system_id_test.py
+++ func_tests/tests/change_local_system_id_test.py
@@ -14,7 +14,7 @@ def test_change_local_system_id(two_merged_mediaservers):
     wait_for_true(one.api.servers_is_online, timeout_sec=10)
     wait_for_true(two.api.servers_is_online, timeout_sec=10)
     new_local_system_id = generator.generate_server_guid(1, True)
-    one.api.generic.post('api/configure', dict(localSystemId=new_local_system_id))
+    one.api.set_local_system_id(new_local_system_id)
     assert one.api.get_local_system_id() == uuid.UUID(new_local_system_id)
     wait_for_true(one.api.neighbor_is_offline, timeout_sec=10)
     wait_for_true(two.api.neighbor_is_offline, timeout_sec=10)
