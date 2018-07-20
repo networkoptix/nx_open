94b976e57a81d1670336d82c5e2d599658acbaeb
Nick Hartleb <nhartleb@networkoptix.com>
2018-07-19 18:30:32 -0700

    CLOUD-2098: If user has permissions to see /downloads/history - he need to see a link for that. Either in footer, on in top right menu
    removed from footer
    
    <==== Mercurial data ====>
    Branch: cloud_18.2

--- cloud_portal/front_end/app/scripts/directives/footer.js
+++ cloud_portal/front_end/app/scripts/directives/footer.js
@@ -1,16 +1,11 @@
 'use strict';
 
 angular.module('cloudApp')
-    .directive('footer', ['account', function (account) {
+    .directive('footer', function () {
         return {
             restrict: 'E',
             templateUrl: Config.viewsDir + 'components/footer.html',
             link: function (scope) {
-                account.requireLogin().then(function (account) {
-                    scope.account = account;
-                    scope.downloadsHistory = scope.account.permissions.indexOf(Config.permission.canViewRelease) > -1;
-                });
-
             }
         }
-    }]);
+    });
