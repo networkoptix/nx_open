10ced0c5c7e8d58726ac70b3fed1f99c8c010131
Sergey Ivanov <sivanov@networkoptix.com>
2018-07-23 19:46:09 +0300

    Fix ipv6 addresses handling in module discovery process
    <==== Mercurial data ====>
    Branch: vms

--- unit_tests/nx_network_ut/src/url/url_builder_ut.cpp
+++ unit_tests/nx_network_ut/src/url/url_builder_ut.cpp
@@ -7,16 +7,47 @@ namespace network {
 namespace url {
 namespace test {
 
-TEST(UrlBuilder, buildIpV6Url)
+class UrlBuilderTest: public ::testing::Test
 {
-    const SocketAddress address("2001::9d38:6ab8:384e:1c3e:aaa2", 7001);
-
-    const auto url = nx::network::url::Builder()
+public:
+    static void testHost(const QString& host)
+    {
+        const SocketAddress address(host, 7001);
+        const auto url = nx::network::url::Builder()
             .setScheme(nx::network::http::urlSheme(true))
             .setEndpoint(address).toUrl();
-    ASSERT_FALSE(url.host().isEmpty());
-    ASSERT_TRUE(url.isValid());
+        ASSERT_TRUE(url.isValid());
+        ASSERT_FALSE(url.host().isEmpty());
+    }
+};
+
+TEST_F(UrlBuilderTest, buildIpV6UrlFull)
+{
+    testHost("2001:0db8:11a3:09d7:1f34:8a2e:07a0:765d");
+    testHost("[2001:0db8:11a3:09d7:1f34:8a2e:07a0:765d]");
 }
+
+TEST_F(UrlBuilderTest, buildIpV6UrlCollapsed)
+{
+    testHost("[2001::9d38:6ab8:384e:1c3e:aaa2]");
+    testHost("2001::9d38:6ab8:384e:1c3e:aaa2");
+    testHost("::");
+    testHost("::1");
+    testHost("2001::");
+}
+
+TEST_F(UrlBuilderTest, buildIpV6UrlWithZeroes)
+{
+    testHost("[2001:0000:11a3:09d7:1f34:8a2e:07a0:765d]");
+    testHost("2001:0000:11a3:09d7:1f34:8a2e:07a0:765d");
+}
+
+TEST_F(UrlBuilderTest, buildIpV6UrlWithSingleZero)
+{
+    testHost("[2001:0:11a3:09d7:1f34:8a2e:07a0:765d]");
+    testHost("2001:0:11a3:09d7:1f34:8a2e:07a0:765d");
+}
+
 
 } // namespace test
 } // namespace url
