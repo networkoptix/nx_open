eb26e88221ef2681fb7d1d3e971ae78676bd80d3
Artem Nikitin <anikitin@networkoptix.com>
2018-07-24 09:31:41 +0300

    FT: fix logging test
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/test_logging_self.py
+++ func_tests/self_tests/test_logging_self.py
@@ -4,9 +4,9 @@ import pytest
 
 
 @pytest.mark.self
-def test_logging(work_dir):
+def test_logging(run_dir):
     _logger = logging.getLogger('test_logger')
     _logger.info('Test info log entry')
-    assert work_dir.joinpath('info.log').exists()
+    assert run_dir.joinpath('info.log').exists()
     _logger.debug('Test debug log entry')
-    assert work_dir.joinpath('debug.log').exists()
+    assert run_dir.joinpath('debug.log').exists()
