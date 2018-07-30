f647279c327db4e38b46fca2a04596d804e34010
Sergey Ivanov <sivanov@networkoptix.com>
2018-07-30 16:20:17 +0300

    VMS-CR-11689: MetaVMS server branding
    <==== Mercurial data ====>
    Branch: meta

--- customization/metavms/customization.cmake
+++ customization/metavms/customization.cmake
@@ -9,8 +9,8 @@ set(product.appName "metavms")
 set(company.name "Network Optix")
 set(cloudName "Nx Meta Cloud")
 set(uri.protocol "nx-metavms")
-set(mediaserver_application_name "Network Optix Nx Meta Media Server")
-set(mediaserver_service_name "Network Optix Nx Meta Media Server")
+set(mediaserver_application_name "Network Optix MetaVMS Media Server")
+set(mediaserver_service_name "Network Optix MetaVMS Media Server")
 
 # Default settings
 set(backgroundImage
