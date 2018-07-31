82e4ba7236abb0ca9fe4259867cefd2aacfa5fbb
Alexander Fokin <afokin@networkoptix.com>
2018-08-01 00:54:30 +0300

    VMS-10789 Better
    <==== Mercurial data ====>
    Branch: vms

--- client/nx_client_desktop/src/ui/workbench/handlers/workbench_wearable_handler.cpp
+++ client/nx_client_desktop/src/ui/workbench/handlers/workbench_wearable_handler.cpp
@@ -230,9 +230,6 @@ void QnWorkbenchWearableHandler::at_uploadWearableCameraFolderAction_triggered()
         return;
     }
 
-    for (QString& file : files)
-        file = path + QDir::separator() + file;
-
     qnClientModule->wearableManager()->prepareUploads(camera, files, this,
         [this, path, camera](WearableUpload upload)
         {
