57d48486a554fac6321650935fbc131c5a9cf9f6
George Sovetov <gsovetov@networkoptix.com>
2018-07-31 22:21:45 +0300

    FT-69: Reorder imports
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/os_access/ssh_access.py
+++ func_tests/framework/os_access/ssh_access.py
@@ -9,8 +9,8 @@ from framework.networking.linux import LinuxNetworking
 from framework.networking.prohibited import ProhibitedNetworking
 from framework.os_access.os_access_interface import OneWayPortMap, ReciprocalPortMap
 from framework.os_access.posix_access import PosixAccess
-from framework.os_access.remote_access import RemoteAccess
 from framework.os_access.posix_shell_path import PosixShellPath
+from framework.os_access.remote_access import RemoteAccess
 from framework.os_access.ssh_shell import SSH
 from framework.os_access.ssh_traffic_capture import SSHTrafficCapture
 from framework.utils import RunningTime
