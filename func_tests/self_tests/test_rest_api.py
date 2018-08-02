7a126f60d2f1901a57e77f434cfde19fe54e7869
George Sovetov <gsovetov@networkoptix.com>
2018-08-02 14:40:05 +0300

    FT-17, VMS-CR-11901: Better tests
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/test_rest_api.py
+++ func_tests/self_tests/test_rest_api.py
@@ -1,10 +1,14 @@
 import base64
+import logging
+import time
 
 import pytest
 import requests
 
 from framework import serving
 from framework.mediaserver_api import GenericMediaserverApi, MediaserverApiRequestError
+
+_logger = logging.getLogger(__name__)
 
 
 def test_auth_key(one_running_mediaserver):
@@ -33,8 +37,23 @@ def test_no_auth(one_running_mediaserver):
     assert response.status_code == 401
 
 
-def test_timeout(service_ports):
+def test_connection_error(service_ports):
     with serving.reserved_port(service_ports[30:35]) as port:
         api = GenericMediaserverApi.new('not a server', '127.0.0.1', port)
-        with pytest.raises(MediaserverApiRequestError):
-            api.get('oi')
+        exception_info = pytest.raises(MediaserverApiRequestError, api.get, 'oi')
+        _logger.info(exception_info)
+
+
+def test_timeout(service_ports):
+    def app(_, start_response):
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        yield b'Started.\n'
+        time.sleep(2)
+        yield b'Done.\n'
+
+    server = serving.WsgiServer(app, service_ports[30:35])
+    with server.serving():
+        api = GenericMediaserverApi.new('not a server', '127.0.0.1', server.port)
+        exception_info = pytest.raises(MediaserverApiRequestError, api.get, '', timeout=1)
+        _logger.info(exception_info)
+
