7655a5ef32568e47defc70b6f1d254759a61fad7
Denis Klychkov <dklychkov@networkoptix.com>
2018-08-02 17:04:04 +0300

    VMS-10957: Change vms products directory names and service names in Linux for meta customization
    <==== Mercurial data ====>
    Branch: meta

--- customization/metavms/customization.cmake
+++ customization/metavms/customization.cmake
@@ -37,7 +37,7 @@ set(customization.clientBundleUpgradeCode "{3401937b-5715-40f7-bf3d-d34b67b3b317
 set(customization.serverBundleUpgradeCode "{b0f6cba5-66f5-4ecd-bba5-87064c7618a6}")
 set(customization.fullBundleUpgradeCode "{b98a8e7a-2e21-472c-b951-e16f82a43798}")
 
-set(deb.customization.company.name "networkoptix")
+set(deb.customization.company.name "networkoptix-metavms")
 
 set(mac.skip.sign false)
 set(mac.sign.identity "Developer ID Application: Network Optix, Inc. (L6FE34GJWM)")
