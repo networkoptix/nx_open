0489d1add0e8ec7ff131cc4f9b41f06bb3e66836
Dmitry Mishin <dmishin@networkoptix.com>
2018-08-06 19:45:54 +0300

    VMS-9066: Fixed wrong parentheses positioning
    <==== Mercurial data ====>
    Branch: vms

--- common/src/core/resource/security_cam_resource.cpp
+++ common/src/core/resource/security_cam_resource.cpp
@@ -504,7 +504,7 @@ bool QnSecurityCamResource::isSharingLicenseInGroup() const
         return false; //< Don't allow sharing for encoders e.t.c
 
     const auto resourceData = qnStaticCommon->dataPool()->data(toSharedPointer(this));
-    if (resourceData.value<bool>(Qn::kCanShareLicenseGroup), false)
+    if (resourceData.value<bool>(Qn::kCanShareLicenseGroup, false))
         return true;
 
     QnResourceTypePtr resType = qnResTypePool->getResourceType(getTypeId());
