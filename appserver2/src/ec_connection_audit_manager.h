be4f97f183a19b407a3bc55d1b198009e8c55c77
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-08-07 14:35:40 +0300

    VMS-11128: The name of the removed server is wrong in audit trail
    <==== Mercurial data ====>
    Branch: vms_3.2

--- appserver2/src/ec_connection_audit_manager.h
+++ appserver2/src/ec_connection_audit_manager.h
@@ -3,8 +3,7 @@
 * a.kolesnikov
 ***********************************************************/
 
-#ifndef EC_CONNECTION_AUDIT_MANAGER_H
-#define EC_CONNECTION_AUDIT_MANAGER_H
+#pragma once
 
 #include "nx_ec/ec_api.h"
 
@@ -14,46 +13,51 @@
 
 namespace ec2
 {
-    class AbstractECConnection;
 
-    class ECConnectionAuditManager
+class AbstractECConnection;
+
+class ECConnectionAuditManager : public QObject
+{
+    Q_OBJECT
+public:
+    ECConnectionAuditManager(AbstractECConnection* ecConnection);
+    virtual ~ECConnectionAuditManager() override;
+
+    template <class T>
+    void addAuditRecord(ApiCommand::Value command, const T& params, const QnAuthSession& authInfo)
     {
-    public:
-        ECConnectionAuditManager(AbstractECConnection* ecConnection);
-        template <class T>
-        void addAuditRecord(ApiCommand::Value command, const T& params, const QnAuthSession& authInfo)
-        {
-            // nothing to do by default
-            Q_UNUSED(command);
-            Q_UNUSED(params);
-            Q_UNUSED(authInfo);
-        }
+        // nothing to do by default
+        Q_UNUSED(command);
+        Q_UNUSED(params);
+        Q_UNUSED(authInfo);
+    }
 
-        void addAuditRecord(ApiCommand::Value command,  const ApiCameraAttributesData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiCameraAttributesDataList& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiMediaServerUserAttributesData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiMediaServerUserAttributesDataList& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiStorageData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiStorageDataList& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiUserData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiUserDataList& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiBusinessRuleData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiBusinessRuleDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiCameraAttributesData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiCameraAttributesDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiMediaServerUserAttributesData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiMediaServerUserAttributesDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiStorageData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiStorageDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiUserData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiUserDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiBusinessRuleData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiBusinessRuleDataList& params, const QnAuthSession& authInfo);
 
-        void addAuditRecord(ApiCommand::Value command,  const ApiResourceParamWithRefData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiResourceParamWithRefDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiResourceParamWithRefData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiResourceParamWithRefDataList& params, const QnAuthSession& authInfo);
 
-        void addAuditRecord(ApiCommand::Value command,  const ApiIdData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiIdDataList& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiIdData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiIdDataList& params, const QnAuthSession& authInfo);
 
-        void addAuditRecord(ApiCommand::Value command,  const ApiResetBusinessRuleData& params, const QnAuthSession& authInfo);
-        void addAuditRecord(ApiCommand::Value command,  const ApiDatabaseDumpData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiResetBusinessRuleData& params, const QnAuthSession& authInfo);
+    void addAuditRecord(ApiCommand::Value command,  const ApiDatabaseDumpData& params, const QnAuthSession& authInfo);
 
-        AbstractECConnection* ec2Connection() const;
-    private:
-        AbstractECConnection* m_connection;
-    };
+    AbstractECConnection* ec2Connection() const;
+private:
+    void at_resourceAboutToRemoved(const QnUuid& id);
+private:
+    AbstractECConnection * m_connection;
+    QMap<QnUuid, QString> m_remvedResourceNames;
+};
 
 }
-
-#endif  //EC_CONNECTION_AUDIT_MANAGER_H
