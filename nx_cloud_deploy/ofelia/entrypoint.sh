20e6eb0d04e54c004d3012ac8ba130c67b649761
Ivan Vigasin <ivigasin@networkoptix.com>
2018-08-07 20:15:29 -0700

    NCD-24 Automate SSL certificates obtaining for traffic relay
    Add ofelia
    
    <==== Mercurial data ====>
    Branch: cloud_dev

--- nx_cloud_deploy/ofelia/entrypoint.sh
+++ nx_cloud_deploy/ofelia/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /etc/ofelia
+
 eval echo -e $(echo "$MODULE_CONFIGURATION" | base64 -d) > /etc/ofelia/config.ini
 
 /usr/local/bin/ofelia daemon --config /etc/ofelia/config.ini
