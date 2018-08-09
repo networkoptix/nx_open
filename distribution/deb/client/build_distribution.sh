bfa1c2ba37faa1be1c83c1c86ef1183599972c1f
Denis Klychkov <dklychkov@networkoptix.com>
2018-08-09 16:25:03 +0300

    VMS-11229: Bundle GStreamer libraries with the client
    These libraries are required to run the client in Ubuntu 14.04 which has older incompatible
    GStreamer.
    
    <==== Mercurial data ====>
    Branch: vms_4.0_alpha

--- distribution/deb/client/build_distribution.sh
+++ distribution/deb/client/build_distribution.sh
@@ -108,6 +108,15 @@ copyLibs()
         distrib_copySystemLibs "$STAGE_LIB" libpng12.so.0 \
             || distrib_copySystemLibs "$STAGE_LIB" libpng.so
         distrib_copySystemLibs "$STAGE_LIB" "${ICU_RUNTIME_LIBS[@]}"
+        distrib_copySystemLibs "$STAGE_LIB" \
+            libgstapp-1.0.so.0 \
+            libgstbase-1.0.so.0 \
+            libgstreamer-1.0.so.0 \
+            libgstpbutils-1.0.so.0 \
+            libgstaudio-1.0.so.0 \
+            libgsttag-1.0.so.0 \
+            libgstvideo-1.0.so.0 \
+            libgstfft-1.0.so.0
     fi
 }
 
