582e60242ae8677550d2198c8cece4352af9f896
Sergey Ivanov <sivanov@networkoptix.com>
2018-08-09 12:51:00 +0300

    Backed out changeset: 12d7800e898a
    <==== Mercurial data ====>
    Branch: vms

--- client/nx_client_core/src/nx/client/core/settings/secure_settings.cpp
+++ client/nx_client_core/src/nx/client/core/settings/secure_settings.cpp
@@ -2,14 +2,13 @@
 
 #include <QtCore/QCoreApplication>
 
-#include <nx/fusion/model_functions.h>
-
 #include "keychain_property_storage_backend.h"
+#include <nx/utils/property_storage/qsettings_backend.h>
 
 namespace nx::client::core {
 
 SecureSettings::SecureSettings():
-    Storage(new KeychainBackend(QCoreApplication::applicationName()))
+    nx::utils::property_storage::Storage(new KeychainBackend(QCoreApplication::applicationName()))
 {
     load();
 }
