a1f28e1a8c75a8952f189ad4362635291e2fd043
Leonid Busygin <lbusygin@networkoptix.com>
2018-08-14 13:29:49 +0300

    VMS-CR-11991: Review fixes
    <==== Mercurial data ====>
    Branch: vms

--- common/src/rtsp/abstract_rtsp_encoder.h
+++ common/src/rtsp/abstract_rtsp_encoder.h
@@ -6,8 +6,8 @@
 #include "utils/common/byte_array.h"
 
 /*
-* Base class for RTSP codec. Used for encode data only
-*/
+ * Base class for RTSP codec. Used for encode data only
+ */
 
 class AbstractRtspEncoder
 {
@@ -17,15 +17,15 @@ public:
     virtual QString getSdpMedia(bool isVideo, int trackId) = 0;
 
     /*
-    * Set media packet to encode
-    */
+     * Set media packet to encode
+     */
     virtual void setDataPacket(QnConstAbstractMediaDataPtr media) = 0;
 
     /*
-    * Function MUST write encoded data packet to sendBuffer. sendBuffer may contain some data before function call (TCP RTSP header)
-    * So, function MUST add data only without clearing buffer
-    * return true if some data are writed or false if no more data
-    */
+     * Function MUST write encoded data packet to sendBuffer. sendBuffer may contain some data
+     * before function call (TCP RTSP header). So, function MUST add data only without clearing
+     * buffer return true if some data are writed or false if no more data.
+     */
     virtual bool getNextPacket(QnByteArray& sendBuffer) = 0;
 
     virtual void init() = 0;
