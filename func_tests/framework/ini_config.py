53530a8b77ae3723bd73eff505ed358b1b90db31
George Sovetov <gsovetov@networkoptix.com>
2018-08-15 20:30:10 +0300

    FT-113, VMS-CR-12000: Parse IniConfig without ConfigParser
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/ini_config.py
+++ func_tests/framework/ini_config.py
@@ -1,15 +1,13 @@
-from io import BytesIO
+import logging
+from collections import OrderedDict
+
+from typing import MutableMapping
 
 from framework.os_access.exceptions import DoesNotExist
 from framework.os_access.posix_access import PosixAccess
 from framework.os_access.windows_access import WindowsAccess
 
-try:
-    # noinspection PyCompatibility
-    from ConfigParser import SafeConfigParser as ConfigParser, NoOptionError, DEFAULTSECT
-except ImportError:
-    # noinspection PyCompatibility
-    from configparser import ConfigParser, NoOptionError, DEFAULTSECT
+_logger = logging.getLogger(__name__)
 
 
 class NameNotFound(Exception):
@@ -18,6 +16,8 @@ class NameNotFound(Exception):
 
 class IniConfig(object):
     """See: https://networkoptix.atlassian.net/wiki/spaces/SD/pages/83895081."""
+
+    _dict = OrderedDict
 
     def __init__(self, os_access, name):
         env_vars = os_access.env_vars()
@@ -31,7 +31,7 @@ class IniConfig(object):
             else:
                 raise ValueError("Unknown type of os_access: {}".format(os_access.__class__))
         self._path = os_access.Path(ini_dir_raw, name + '.ini')
-        self._parser = None  # type: ConfigParser
+        self._cached_settings = None  # type: MutableMapping
         self.reload()
 
     def __repr__(self):
@@ -39,34 +39,32 @@ class IniConfig(object):
 
     def reload(self):
         try:
-            data = self._path.read_bytes()
+            data = self._path.read_text()
         except DoesNotExist:
-            self._parser = ConfigParser()
+            self._cached_settings = self._dict()
         else:
-            buffer = BytesIO(data)
-            self._parser = ConfigParser()
-            self._parser.readfp(buffer)
+            # Config here is parsed by hand, because IniConfig from C++ doesn't
+            # understand sections, which are always created by ConfigParser.
+            self._cached_settings = self._dict(
+                line.split(u'=', 1) for line in data.splitlines())
 
     def _write(self):
-        buffer = BytesIO()
-        self._parser.write(buffer)
-        data = buffer.getvalue()
-        self._path.parent.mkdir(parents=True, exist_ok=True)
-        self._path.write_bytes(data)
+        self._path.parent.mkdir(exist_ok=True, parents=True)
+        data = u'\n'.join(
+            u'{}={}'.format(name, value)
+            for name, value in self._cached_settings.items())
+        self._path.write_text(data)
 
     def get(self, name):
         try:
-            return self._parser.get(DEFAULTSECT, name)
-        except NoOptionError:
-            raise NameNotFound("No {} in {}".format(name, self._path))
+            return self._cached_settings[name]
+        except KeyError:
+            raise NameNotFound('{!r}: no name {!r}'.format(self, name))
 
     def set(self, name, value):
-        self._parser.set(DEFAULTSECT, name, value)
+        self._cached_settings[name] = value
         self._write()
 
     def delete(self, name):
-        try:
-            self._parser.remove_option(DEFAULTSECT, name)
-        except NoOptionError:
-            raise NameNotFound("No {} in {}".format(name, self._path))
+        del self._cached_settings[name]
         self._write()
