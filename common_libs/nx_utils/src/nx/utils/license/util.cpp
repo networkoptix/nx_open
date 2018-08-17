f05d9c0d86cd2d59ba8a40a4dbf688233c16baa0
Ivan Vigasin <ivigasin@networkoptix.com>
2018-08-16 19:24:40 -0700

    Fix bad commit 12105c5828e7
    <==== Mercurial data ====>
    Branch: vms_4.0_alpha

--- common_libs/nx_utils/src/nx/utils/license/util.cpp
+++ common_libs/nx_utils/src/nx/utils/license/util.cpp
@@ -14,7 +14,7 @@ QString changedGuidByteOrder(const QString& guid)
     int replace[] = {6, 7, 4, 5, 2, 3, 0, 1, 8, 11, 12, 9, 10, 13, 16, 17, 14, 15};
     
     for (size_t i = 0; i < sizeof(replace) / sizeof(replace[0]); i++)
-        result += guid[replace[i]];
+        result[i] = guid[replace[i]];
 
     return result;
 }
