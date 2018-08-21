ce78885774641346d1c14cf8eec1f523cec989b2
Mikhail Uskov <muskov@networkoptix.com>
2018-08-21 22:00:07 +0300

    After merge compilation fix
    <==== Mercurial data ====>
    Branch: vms

--- common_libs/nx_utils/src/nx/utils/license/util.h
+++ common_libs/nx_utils/src/nx/utils/license/util.h
@@ -6,6 +6,7 @@ namespace nx {
 namespace utils {
 namespace license {
 
+QString NX_UTILS_API changedGuidByteOrder(const QString& guid);
 int NX_UTILS_API hardwareIdVersion(const QString& hardwareId);
 
 } // namespace license
