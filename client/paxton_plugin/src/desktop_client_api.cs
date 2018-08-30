7a2d9a0b27269fdf6c53b6e4f0f3dcc416c0ea8c
Sergey Ivanov <sivanov@networkoptix.com>
2018-08-30 16:13:17 +0300

    Find minilauncher if applauncher is not found
    <==== Mercurial data ====>
    Branch: vms_3.2_paxton

--- client/paxton_plugin/src/desktop_client_api.cs
+++ client/paxton_plugin/src/desktop_client_api.cs
@@ -6,8 +6,9 @@ namespace nx.desktop_client_api {
 
 internal class Launcher
 {
-    public static Process startClient(string parameters)
+    private static string findLauncherBinary()
     {
+        // Check latest applauncher first.
         var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         var applauncherBinary = Path.Combine(
             appData,
@@ -15,7 +16,46 @@ internal class Launcher
             "applauncher",
             AppInfo.customization,
             "applauncher.exe");
-        return Process.Start(applauncherBinary, parameters);
+        if (File.Exists(applauncherBinary))
+            return applauncherBinary;
+
+        foreach (var directory in new[]
+            {
+                Environment.GetEnvironmentVariable("ProgramW6432"),
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86)
+            })
+        {
+            try
+            {
+                var clientDirectory = Path.Combine(
+                    directory,
+                    AppInfo.companyName,
+                    AppInfo.productName,
+                    "Client");
+
+                foreach (var versionDirectory in Directory.EnumerateDirectories(clientDirectory))
+                {
+                    var minilauncherBinary = Path.Combine(
+                        versionDirectory,
+                        AppInfo.minilauncherBinaryName);
+                    if (File.Exists(minilauncherBinary))
+                        return minilauncherBinary;
+                }
+            }
+            catch (Exception)
+            {
+                // Ignore different file search problems.
+            }
+        }
+
+        return null;
+    }
+
+    public static Process startClient(string parameters)
+    {
+        var launcherBinary = findLauncherBinary();
+        return launcherBinary != null ? Process.Start(launcherBinary, parameters) : null;
     }
 }
 
