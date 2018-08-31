d721d66873ef805576899a80492b50b36f9156b7
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-08-31 23:04:26 +0300

    VMS-11470: media_server_core_ut doesn't display google test help
    <==== Mercurial data ====>
    Branch: vms_3.2

--- unit_tests/mediaserver_core_ut/src/main.cpp
+++ unit_tests/mediaserver_core_ut/src/main.cpp
@@ -5,54 +5,56 @@
 
 #include <test_support/utils.h>
 
-static void fillConfig(const QStringList& arguments)
+namespace {
+
+std::unique_ptr<QCommandLineParser> fillConfig(const QStringList& arguments)
 {
-    QCommandLineParser parser;
-    parser.addOptions({ {{"t", "tmp"}, "Temporary working directory path. Default: 'tmp'", "tmp"},
+    auto parser = std::make_unique<QCommandLineParser>();
+    parser->addOptions({ {{"t", "tmp"}, "Temporary working directory path. Default: 'tmp'", "tmp"},
                        {"ftp-storage-url", "Ftp storage url"},
                        {"smb-storage-url", "Smb storage url"} });
-    parser.addHelpOption();
-    parser.parse(arguments);
+    parser->addHelpOption();
+    parser->parse(arguments);
 
-    if (parser.isSet("help"))
-    {
-        parser.showHelp();
-        QCoreApplication::exit(0);
-        return;
-    }
-
-    if (parser.isSet("tmp") && nx::ut::utils::validateAndOrCreatePath(parser.value("tmp")))
-        nx::ut::cfg::configInstance().tmpDir = parser.value("tmp");
+    if (parser->isSet("tmp") && nx::ut::utils::validateAndOrCreatePath(parser->value("tmp")))
+        nx::ut::cfg::configInstance().tmpDir = parser->value("tmp");
     else
         nx::ut::cfg::configInstance().tmpDir = QDir(QDir::tempPath()).absoluteFilePath("nx_ut");
 
-    if (parser.isSet("ftp-storage-url"))
-        nx::ut::cfg::configInstance().ftpUrl = parser.value("ftp-storage-url");
-    if (parser.isSet("smb-storage-url"))
-        nx::ut::cfg::configInstance().smbUrl = parser.value("smb-storage-url");
+    if (parser->isSet("ftp-storage-url"))
+        nx::ut::cfg::configInstance().ftpUrl = parser->value("ftp-storage-url");
+    if (parser->isSet("smb-storage-url"))
+        nx::ut::cfg::configInstance().smbUrl = parser->value("smb-storage-url");
+
+    return parser;
 }
 
-static void fillConfig(QCoreApplication& app)
+std::unique_ptr<QCommandLineParser> fillConfig(QCoreApplication& app)
 {
-    fillConfig(app.arguments());
+    return fillConfig(app.arguments());
 }
+
+} // namespace
 
 int main(int argc, char** argv)
 {
 #ifndef ENABLE_CLOUD_TEST
     QCoreApplication app(argc, argv);
-    fillConfig(app);
+    auto parser = fillConfig(app);
 #else
     QStringList arguments;
     for (int i = 0; i < argc; ++i)
         arguments.push_back(QString::fromUtf8(argv[i]));
-    fillConfig(arguments);
+    auto parser = fillConfig(arguments);
 #endif
-    return nx::network::test::runTest(
+    int result = nx::network::test::runTest(
         argc, argv,
         [](const nx::utils::ArgumentParser& /*args*/)
         {
             return nx::utils::test::DeinitFunctions();
         },
         0);
+    if (parser->isSet("help"))
+        parser->showHelp();
+    return result;
 }
