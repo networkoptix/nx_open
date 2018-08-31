7188dd76a18edbbf20189a934f6e63c7b0b24c4f
George Sovetov <gsovetov@networkoptix.com>
2018-08-31 20:47:33 +0300

    FT: Remove unused fixture vm_address and corresponding parameter --vm-address
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/fixtures/media.py
+++ func_tests/fixtures/media.py
@@ -12,9 +12,6 @@ def pytest_addoption(parser):
         'media sample file path, default is %(default)s at binary directory'))
     parser.addoption('--media-stream-path', type=Path, default=defaults['media_stream_path'], help=(
         'media sample test camera stream, relative to bin dir [%(default)s]'))
-    parser.addoption('--vm-address', type=IPAddress, help=(
-        'IP address virtual machines bind to. '
-        'Test camera discovery will answer only to this address if this option is specified.'))
 
 
 @pytest.fixture(params=['rtsp', 'webm', 'hls', 'direct-hls'])
@@ -22,13 +19,8 @@ def stream_type(request):
     return request.param
 
 
-@pytest.fixture(scope='session')
-def vm_address(request):
-    return request.config.getoption('--vm-address')
-
-
 @pytest.fixture()
-def camera_pool(request, bin_dir, vm_address, sample_media_file, service_ports):
+def camera_pool(request, bin_dir, sample_media_file, service_ports):
     stream_path = bin_dir / request.config.getoption('--media-stream-path')
     assert stream_path.exists(), '%s is expected at %s' % (stream_path.name, stream_path.parent)
     duration = sample_media_file.duration.total_seconds()
