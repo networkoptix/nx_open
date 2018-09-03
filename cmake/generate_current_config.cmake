bdc676f330c5dd45a2876eb3bad66dfc0b606fee
Sergey Ivanov <sivanov@networkoptix.com>
2018-09-03 16:34:49 +0300

    Add build_info.txt into client distribution
    <==== Mercurial data ====>
    Branch: vms_3.2_paxton

--- cmake/generate_current_config.cmake
+++ cmake/generate_current_config.cmake
@@ -1,3 +1,7 @@
+# Use "take local" when will merge that in to 3.2. Backport.
+nx_configure_file(
+    "${PROJECT_SOURCE_DIR}/build_info.txt"
+    ${CMAKE_CURRENT_BINARY_DIR})
 nx_configure_file(
     "${PROJECT_SOURCE_DIR}/build_variables/maven/filter-resources/current_config.py"
     "${CMAKE_CURRENT_BINARY_DIR}")
