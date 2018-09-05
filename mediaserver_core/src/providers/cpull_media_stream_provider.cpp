5b7f323739899949a44c350f32f84f041765909d
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-09-05 21:48:13 +0300

    VMS-9323: deeper setStatus control refactor for streamReaders
    <==== Mercurial data ====>
    Branch: vms

--- mediaserver_core/src/providers/cpull_media_stream_provider.cpp
+++ mediaserver_core/src/providers/cpull_media_stream_provider.cpp
@@ -18,12 +18,6 @@ QnClientPullMediaStreamProvider::QnClientPullMediaStreamProvider(const nx::media
 {
 }
 
-bool QnClientPullMediaStreamProvider::canChangeStatus() const
-{
-    const QnLiveStreamProvider* liveProvider = dynamic_cast<const QnLiveStreamProvider*>(this);
-    return liveProvider && liveProvider->canChangeStatus();
-}
-
 void QnClientPullMediaStreamProvider::run()
 {
     initSystemThreadId();
@@ -62,28 +56,15 @@ void QnClientPullMediaStreamProvider::run()
 
         QnAbstractMediaDataPtr data = getNextData();
 
-        if (data==0)
+        if (data == nullptr)
         {
             setNeedKeyData();
-            m_framesLost++;
-            m_stat[0].onData(0, false);
-            m_stat[0].onEvent(CL_STAT_FRAME_LOST);
-
-            if (m_framesLost % MAX_LOST_FRAME == 0) // if we lost MAX_LOST_FRAME frames => connection is lost for sure
-            {
-                if (canChangeStatus() && getResource()->getStatus() != Qn::Unauthorized) // avoid offline->unauthorized->offline loop
-                    getResource()->setStatus(Qn::Offline);
-
-                m_stat[0].onLostConnection();
-            }
-
+            m_stat[0].onEvent(CL_STREAM_ISSUE);
             if (!needToStop())
                 QnSleep::msleep(30);
 
             continue;
         }
-
-
 
         if (getResource()->hasFlags(Qn::local_live_cam)) // for all local live cam add MediaFlags_LIVE flag;
         {
@@ -100,16 +81,6 @@ void QnClientPullMediaStreamProvider::run()
         }
 
         QnCompressedVideoDataPtr videoData = std::dynamic_pointer_cast<QnCompressedVideoData>(data);
-
-        if (m_framesLost>0) // we are alive again
-        {
-            if (m_framesLost >= MAX_LOST_FRAME)
-            {
-                m_stat[0].onEvent(CL_STAT_CAMRESETED);
-            }
-
-            m_framesLost = 0;
-        }
 
         if (videoData && needKeyData())
         {
