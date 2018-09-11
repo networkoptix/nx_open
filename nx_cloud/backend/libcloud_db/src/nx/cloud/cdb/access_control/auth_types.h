930d8cfdd24616b0bf6a9095a267bdd2a1ee678b
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-09-11 15:47:24 -0700

    Coding style fixes
    <==== Mercurial data ====>
    Branch: cloud_18.2_backend

--- nx_cloud/backend/libcloud_db/src/nx/cloud/cdb/access_control/auth_types.h
+++ nx_cloud/backend/libcloud_db/src/nx/cloud/cdb/access_control/auth_types.h
@@ -1,52 +1,47 @@
-/**********************************************************
-* 3 may 2015
-* a.kolesnikov
-***********************************************************/
-
-#ifndef CLOUD_DB_AUTH_TYPES_H
-#define CLOUD_DB_AUTH_TYPES_H
+#pragma once
 
 #include <nx/utils/stree/resourcecontainer.h>
 
-
-namespace nx {
-namespace cdb {
+namespace nx::cdb {
 
 enum class AccessRole
 {
-    //!anyone
     none,
-    //!System, binded to account can fetch some data too
+    /** System, binded to account can fetch some data too. */
     system,
     user,
-    //!Other nx_cloud module
+    /** Other nx_cloud module. */
     nxCloudModule,
-    //!Used by classes of this module to communicate to each other. Everything is allowed for this role
-    cloudDB
+    /**
+     * Used by classes of this module to communicate to each other.
+     * Everything is allowed for this role.
+     */
+    cloudDB,
 };
 
-//!Contains information about authenticated user
-/*!
- \note This information plus requested action type is enough to perform authorization
+/**
+ * Contains information about authenticated user.
+ * NOTE: This information plus requested action type is enough to perform authorization.
  */
 class AuthenticationInfo
 {
 public:
     AccessRole role;
-    //!Parameters, discovered during authentication (e.g., account id)
+    /**
+     * Parameters, discovered during authentication (e.g., account id).
+     */
     nx::utils::stree::ResourceContainer params;
 };
 
-class AuthorizationInfo
-:
+class AuthorizationInfo:
     public nx::utils::stree::AbstractResourceReader,
     public nx::utils::stree::AbstractIteratableContainer
 {
 public:
-    AuthorizationInfo();
-    AuthorizationInfo( nx::utils::stree::ResourceContainer&& rc );
+    AuthorizationInfo() = default;
+    AuthorizationInfo(nx::utils::stree::ResourceContainer rc);
 
-    virtual bool getAsVariant( int resID, QVariant* const value ) const override;
+    virtual bool getAsVariant(int resID, QVariant* const value) const override;
     virtual std::unique_ptr<nx::utils::stree::AbstractConstIterator> begin() const override;
 
     bool accessAllowedToOwnDataOnly() const;
@@ -55,7 +50,4 @@ private:
     nx::utils::stree::ResourceContainer m_rc;
 };
 
-}   //cdb
-}   //nx
-
-#endif  //CLOUD_DB_AUTH_TYPES_H
+} // namespace nx::cdb
