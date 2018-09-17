3fc9891b135e53380f6973cca49b7927ebb08fb7
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-09-17 11:11:12 -0700

    Http handler class moved to a proper location
    <==== Mercurial data ====>
    Branch: cloud_18.2_backend

--- nx_cloud/backend/libconnection_mediator/src/http/http_server.cpp
+++ nx_cloud/backend/libconnection_mediator/src/http/http_server.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include <nx/network/cloud/mediator/api/mediator_api_http_paths.h>
+#include <nx/network/http/server/handler/fusion_based_handlers.h>
 #include <nx/network/url/url_parse_helper.h>
 #include <nx/utils/log/log.h>
 #include <nx/utils/std/cpp14.h>
@@ -14,34 +15,6 @@
 namespace nx {
 namespace hpm {
 namespace http {
-
-// TODO: #ak Consider moving this class to some common location.
-template<typename ResultType>
-class GetHandler:
-    public nx::network::http::AbstractFusionRequestHandler<void, ResultType>
-{
-public:
-    using FunctorType = nx::utils::MoveOnlyFunc<ResultType()>;
-
-    GetHandler(FunctorType func):
-        m_func(std::move(func))
-    {
-    }
-
-private:
-    FunctorType m_func;
-
-    virtual void processRequest(
-        nx::network::http::HttpServerConnection* const /*connection*/,
-        const nx::network::http::Request& /*request*/,
-        nx::utils::stree::ResourceContainer /*authInfo*/) override
-    {
-        auto data = m_func();
-        this->requestCompleted(nx::network::http::FusionRequestResult(), std::move(data));
-    }
-};
-
-//-------------------------------------------------------------------------------------------------
 
 Server::Server(
     const conf::Settings& settings,
@@ -93,7 +66,8 @@ const Server::MultiAddressHttpServer& Server::server() const
 
 void Server::registerStatisticsApiHandlers(const stats::Provider& provider)
 {
-    using GetAllStatisticsHandler = GetHandler<stats::Statistics>;
+    using GetAllStatisticsHandler =
+        network::http::server::handler::GetHandler<stats::Statistics>;
 
     registerApiHandler<GetAllStatisticsHandler>(
         network::url::joinPath(api::kMediatorApiPrefix, api::kStatisticsMetricsPath).c_str(),
