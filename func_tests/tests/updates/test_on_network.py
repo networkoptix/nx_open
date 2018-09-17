072ba636109b29cb48f5aa7ecfc72017f9670512
George Sovetov <gsovetov@networkoptix.com>
2018-09-17 18:12:55 +0300

    FT-123, FT-124: More networks for updates test
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/tests/updates/test_on_network.py
+++ func_tests/tests/updates/test_on_network.py
@@ -4,13 +4,17 @@ from framework.installation.mediaserver import Mediaserver
 from framework.waiting import wait_for_equal, wait_for_truthy
 
 
-@pytest.fixture(params=['square.net.yaml'])
+@pytest.fixture(params=[
+    'triangle.net.yaml',
+    'square.net.yaml',
+    'three-proxy-three.net.yaml',
+    ])
 def layout_file(request):
     return request.param
 
 
 def test_update_installed(system, update_info, updates_set):
-    mediaserver = system['first']  # type: Mediaserver
+    mediaserver = next(iter(system.values()))  # type: Mediaserver  # Random mediaserver.
     mediaserver.api.start_update(update_info)
     wait_for_equal(mediaserver.api.get_update_information, update_info)
     wait_for_truthy(mediaserver.api.updates_are_ready_to_install, timeout_sec=120)
