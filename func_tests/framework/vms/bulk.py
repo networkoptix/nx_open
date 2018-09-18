505b0947db72f40aec3dd55b6ee119092b2de4c2
George Sovetov <gsovetov@networkoptix.com>
2018-09-18 21:50:20 +0300

    FT-129, FT-140: Rewrite disk space configurationa a bit
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/vms/bulk.py
+++ func_tests/framework/vms/bulk.py
@@ -14,8 +14,11 @@ def many_allocated(machine_types, machine_configurations):
             machine_type = machine_types[conf['type']]
             machine = stack.enter_context(machine_type.vm_ready(conf['alias']))  # type: VM
             machines[conf['alias']] = machine
-            if 'free_disk_space' in conf:
-                free_space_bytes = parse_size(conf['free_disk_space'])
+            try:
+                free_space_bytes = conf['free_disk_space']
+            except KeyError:
+                pass
+            else:
                 free_space_cm = machine.os_access.free_disk_space_limited(free_space_bytes)
                 stack.enter_context(free_space_cm)
         yield machines
