8ecd338ae524bf7f69a3593343cf969be69756ad
Nick Hartleb <nhartleb@networkoptix.com>
2018-09-18 19:47:20 -0700

    CLOUD-2291: Use  X-Forwarded-For to log IP
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/cloud/api/account_backend.py
+++ cloud_portal/cloud/api/account_backend.py
@@ -119,14 +119,14 @@ class AccountManager(db.models.Manager):
 
 @receiver(user_logged_in)
 def user_logged_in_callback(sender, request, user, **kwargs):
-    ip = request.META.get('REMOTE_ADDR')
+    ip = get_ip(request)
     logger.info('User logged in: {}, IP: {}'.format(user.email, ip))
     models.AccountLoginHistory.objects.create(action='user_logged_in', ip=ip, email=user.email)
 
 
 @receiver(user_logged_out)
 def user_logged_out_callback(sender, request, user, **kwargs):
-    ip = request.META.get('REMOTE_ADDR')
+    ip = get_ip(request)
     logger.info('User logged our: {}, IP: {}'.format(user.email, ip))
     models.AccountLoginHistory.objects.create(action='user_logged_out', ip=ip, email=user.email)
 
