778984894257315bbc2ae90d50b2e428aa28ceed
George Sovetov <gsovetov@networkoptix.com>
2018-09-19 19:34:50 +0300

    FT-129, FT-140: Move threading stuff to ThreadedCall.periodic
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/framework/threaded.py
+++ func_tests/framework/threaded.py
@@ -1,11 +1,14 @@
+import time
 from threading import Thread
+
+from framework.utils import with_traceback
 
 
 class ThreadedCall(object):
     def __init__(self, call, terminate, timeout_sec=10):
         self._terminate = terminate
         self._timeout_sec = timeout_sec
-        self.thread = Thread(target=call)
+        self.thread = Thread(target=with_traceback(call))
 
     def __enter__(self):
         self.thread.start()
@@ -13,3 +16,17 @@ class ThreadedCall(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._terminate()
         self.thread.join(timeout=self._timeout_sec)
+
+    @classmethod
+    def periodic(cls, iteration_call, sleep_between_sec=0, terminate_timeout_sec=10):
+        should_continue = [True]
+
+        def terminate():
+            should_continue[0] = False
+
+        def call():
+            while should_continue[0]:
+                iteration_call()
+                time.sleep(sleep_between_sec)
+
+        return cls(call, terminate, timeout_sec=terminate_timeout_sec)
