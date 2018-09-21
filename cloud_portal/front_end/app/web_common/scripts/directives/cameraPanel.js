c290141cb3ba23dc27b74efff3d866fd9f513ee9
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-09-21 12:29:09 -0700

    CLOUD-2327: Web client looses search results when camera list updated
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/web_common/scripts/directives/cameraPanel.js
+++ cloud_portal/front_end/app/web_common/scripts/directives/cameraPanel.js
@@ -42,7 +42,7 @@
                     // If none - open first server and select first camera
                     // else select last opened server and last selected camera
                     if (Object.keys(scope.storage.serverStates).length === 0) {
-                        if (Object.keys(scope.cameras).length !== 0 && scope.mediaServers !== null) {
+                        if (Object.keys(scope.cameras).length !== 0 && !!scope.mediaServers) {
                             scope.selectCamera(scope.cameras[ scope.mediaServers[ 0 ].id ][ 0 ]);
 
                             scope.toggleInfo = false;
