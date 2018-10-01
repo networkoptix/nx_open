557c4974c19f4112e702bf549940f495134a9e4f
Dmitry Mishin <dmishin@networkoptix.com>
2018-10-01 19:00:13 +0300

    VMS-CR-12703: Review fixes
    <==== Mercurial data ====>
    Branch: vms

--- client/nx_client_desktop/src/nx/client/desktop/resource_properties/camera/legacy/legacy_expert_settings_widget.cpp
+++ client/nx_client_desktop/src/nx/client/desktop/resource_properties/camera/legacy/legacy_expert_settings_widget.cpp
@@ -116,20 +116,27 @@ LegacyExpertSettingsWidget::LegacyExpertSettingsWidget(QWidget* parent):
     connect(ui->comboBoxTransport, SIGNAL(currentIndexChanged(int)), this, SLOT(at_dataChanged()));
 
     connect(
-        ui->preferredPtzPresetTypeComboBox, SIGNAL(currentIndexChanged(int)),
-        this, SLOT(at_dataChanged()));
+        ui->preferredPtzPresetTypeComboBox,
+        static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
+        this,
+        &LegacyExpertSettingsWidget::at_dataChanged);
 
     connect(
-        ui->preferredPtzPresetTypeComboBox, SIGNAL(currentIndexChanged(int)),
-        this, SLOT(at_preferredPresetTypeChanged(int)));
+        ui->preferredPtzPresetTypeComboBox,
+        static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
+        this, &LegacyExpertSettingsWidget::at_preferredPresetTypeChanged);
 
     connect(
-        ui->forcedPanTiltCheckBox, SIGNAL(toggled(bool)),
-        this, SLOT(at_dataChanged()));
+        ui->forcedPanTiltCheckBox,
+        static_cast<void(QCheckBox::*)(bool)>(&QCheckBox::toggled),
+        this,
+        &LegacyExpertSettingsWidget::at_dataChanged);
 
     connect(
-        ui->forcedZoomCheckBox, SIGNAL(toggled(bool)),
-        this, SLOT(at_dataChanged()));
+        ui->forcedZoomCheckBox,
+        static_cast<void(QCheckBox::*)(bool)>(&QCheckBox::toggled),
+        this,
+        &LegacyExpertSettingsWidget::at_dataChanged);
 
     connect(ui->secondStreamDisableCheckBox, &QCheckBox::stateChanged,
         this, &LegacyExpertSettingsWidget::at_dataChanged);
