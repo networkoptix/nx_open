9fc571cdc3f5fcff202d98c5b69f2e0286a66098
George Sovetov <gsovetov@networkoptix.com>
2018-10-01 20:02:46 +0300

    FT-229: INI Config: Support space stripping and quotes
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/self_tests/test_ini_config.py
+++ func_tests/self_tests/test_ini_config.py
@@ -14,3 +14,20 @@ def test_ini_config(os_access_temp_dir):
     assert ini.get('a') == '1'
     ini.delete('a')
     pytest.raises(NameNotFound, ini.get, 'a')
+
+
+def test_spaces(os_access_temp_dir):
+    path = os_access_temp_dir / 'dummy_with_spaces.ini'
+    path.write_text(u'  a  =  1  \n b = \nc=  2 3 4  ')
+    ini = IniConfig(path)
+    assert ini.get('a') == '1'
+    assert ini.get('b') == ''
+    assert ini.get('c') == '2 3 4'
+
+
+def test_quotes(os_access_temp_dir):
+    path = os_access_temp_dir / 'dummy_with_quotes.ini'
+    path.write_text(u'  a  =  "  1  "  \nb=  "  "  ')
+    ini = IniConfig(path)
+    assert ini.get('a') == '  1  '
+    assert ini.get('b') == '  '
