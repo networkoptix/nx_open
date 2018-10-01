4edae766557fc6b0469e4ba28efa19913fbd7aee
Dmitry Mishin <dmishin@networkoptix.com>
2018-10-01 20:49:58 +0300

    VMS-CR-6888: Review fixes
    <==== Mercurial data ====>
    Branch: vms

--- common/src/core/resource/security_cam_resource.h
+++ common/src/core/resource/security_cam_resource.h
@@ -308,6 +308,11 @@ public:
     virtual int suggestBitrateKbps(const QnLiveStreamParams& streamParams, Qn::ConnectionRole role) const;
     static float rawSuggestBitrateKbps(Qn::StreamQuality quality, QSize resolution, int fps);
 
+    /**
+     * All events emitted by analytics driver bound to the resource can be captured within
+     * this method.
+     * @return true if event has been captured, false otherwise.
+     */
     virtual bool captureEvent(const nx::vms::event::AbstractEventPtr& event);
     virtual bool isAnalyticsDriverEvent(nx::vms::api::EventType eventType) const;
 
