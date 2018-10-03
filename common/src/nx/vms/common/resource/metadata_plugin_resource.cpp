fe5a95292dfc8f6914f7653b3f7aad4a854094a3
Denis Klychkov <dklychkov@networkoptix.com>
2018-10-03 11:04:20 +0300

    META-42: Add dummy resources for metadata plugins and their instances
    <==== Mercurial data ====>
    Branch: meta

---
+++ common/src/nx/vms/common/resource/metadata_plugin_resource.cpp
@@ -0,0 +1,11 @@
+#include "metadata_plugin_resource.h"
+
+namespace nx::vms::common {
+
+MetadataPluginResource::MetadataPluginResource(QnCommonModule* commonModule)
+{
+}
+
+MetadataPluginResource::~MetadataPluginResource() = default;
+
+} // namespace nx::vms::common
