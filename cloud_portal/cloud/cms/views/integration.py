863ff17beb08331583f2772999aefd526559dbd5
Nick Hartleb <nhartleb@networkoptix.com>
2018-10-05 18:32:09 -0700

    CLOUD-2367: Pass identifier for an integration
    pass integration id into the make json function
    
    <==== Mercurial data ====>
    Branch: cloud_18.4

--- cloud_portal/cloud/cms/views/integration.py
+++ cloud_portal/cloud/cms/views/integration.py
@@ -22,43 +22,41 @@ def make_integrations_json(integrations, contexts=[]):
     global_contexts = Context.objects.filter(product_type__type=CLOUD_PORTAL, is_global=True)
     cloud_portal = Product.objects.filter(product_type__type=CLOUD_PORTAL,
                                           customizations__name__in=[settings.CUSTOMIZATION])
-    if not cloud_portal.exists():
-        return []
+    if cloud_portal.exists():
+        cloud_portal = cloud_portal.first()
 
-    cloud_portal = cloud_portal.first()
+        for integration in integrations:
+            integration_dict = {}
+            current_version = integration.version_id()
+            if current_version == 0:
+                continue
+            for context in contexts:
+                for datastructure in context.datastructure_set.all():
+                    ds_name = datastructure.name
+                    if not datastructure.public:
+                        continue
+                    integration_dict[ds_name] = datastructure.find_actual_value(product=integration,
+                                                                                version_id=current_version)
 
-    for integration in integrations:
-        integration_dict = {}
-        current_version = integration.version_id()
-        if current_version == 0:
-            continue
-        for context in contexts:
-            for datastructure in context.datastructure_set.all():
-                ds_name = datastructure.name
-                if not datastructure.public:
-                    continue
-                integration_dict[ds_name] = datastructure.find_actual_value(product=integration,
-                                                                            version_id=current_version)
+            for global_context in global_contexts:
+                process_context_structure(cloud_portal, global_context, integration_dict, None, current_version, False, False)
 
-        for global_context in global_contexts:
-            process_context_structure(cloud_portal, global_context, integration_dict, None, current_version, False, False)
+            integration_dict['id'] = integration.id
+            integrations_json.append(integration_dict)
 
-        integrations_json.append(integration_dict)
-
-    return integrations_json
+    return {'data': integrations_json}
 
 
-@api_view(("POST", ))
+@api_view(("GET", ))
 @permission_classes((IsAuthenticated, ))
 @handle_exceptions
-def get_integration(request):
-    if 'product_id' in request.POST:
-        products = Product.objects.filter(product_type__type=INTEGRATION,
-                                          customizations__name__in=[settings.CUSTOMIZATION])
-        try:
-            return api_success(make_integrations_json(products.get(id=request.POST['product_id'])))
-        except ObjectDoesNotExist:
-            pass
+def get_integration(request, product_id=None):
+    products = Product.objects.filter(product_type__type=INTEGRATION,
+                                      customizations__name__in=[settings.CUSTOMIZATION])
+    try:
+        return api_success(make_integrations_json([products.get(id=product_id)]))
+    except ObjectDoesNotExist:
+        pass
 
     raise APINotFoundException("Could not find integration", )
 
