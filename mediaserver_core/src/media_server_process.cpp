f10ffcbbf75873aaddebda30ea98070dfe9c05c5
Alexander Kulikov <akulikov@networkoptix.com>
2018-10-08 18:05:44 +0300

    VMS-11783: Add documentation for the /ec2/saveUserEx request
    <==== Mercurial data ====>
    Branch: x_vms_11783_save_user_ex2

--- mediaserver_core/src/media_server_process.cpp
+++ mediaserver_core/src/media_server_process.cpp
@@ -2425,6 +2425,41 @@ void MediaServerProcess::registerRestHandlers(
     reg("api/installUpdate", new QnInstallUpdateRestHandler(serverModule()));
     reg("ec2/cancelUpdate", new QnCancelUpdateRestHandler(serverModule()));
 
+    /**%apidoc POST /ec2/saveUserEx
+     * <p>
+     * Parameters should be passed as a JSON object in POST message body with
+     * content type "application/json".
+     * </p>
+     * %permissions Administrator.
+     * %param[opt] id User unique id. Can be omitted when creating a new object. If such object
+     *     exists, omitted fields will not be changed.
+     * %param name User name.
+     * %param permissions Combination (via "|") of the following flags:
+     *     %value GlobalAdminPermission Admin, can edit other non-admins.
+     *     %value GlobalEditCamerasPermission Can edit camera settings.
+     *     %value GlobalControlVideoWallPermission Can control video walls.
+     *     %value GlobalViewArchivePermission Can view archives of available cameras.
+     *     %value GlobalExportPermission Can export archives of available cameras.
+     *     %value GlobalViewBookmarksPermission Can view bookmarks of available cameras.
+     *     %value GlobalManageBookmarksPermission Can modify bookmarks of available cameras.
+     *     %value GlobalUserInputPermission Can change PTZ state of a camera, use 2-way audio, I/O
+     *         buttons.
+     *     %value GlobalAccessAllMediaPermission Has access to all media (cameras and web pages).
+     *     %value GlobalCustomUserPermission Flag: this user has custom permissions
+     * %param[opt] userRoleId User role unique id.
+     * %param email User's email.
+     * %param[opt] password User's password.
+     * %param[opt] isLdap Whether the user was imported from LDAP.
+     *     %value false Default value.
+     *     %value true
+     * %param[opt] isEnabled Whether the user is enabled.
+     *     %value false
+     *     %value true Default value.
+     * %param[opt] isCloud Whether the user is a cloud user, as opposed to a local one.
+     *     %value false Default value.
+     *     %value true
+     * %param fullName Full name of the user.
+     */
     reg("ec2/saveUserEx", new QnSaveUserExRestHandler(serverModule()));
 
     /**%apidoc GET /ec2/cameraThumbnail
