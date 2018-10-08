99c5833c0fbc1235e6aa71b81483de678368a1b1
Evgeny Balashov <ebalashov@networkoptix.com>
2018-10-08 13:36:28 -0700

    CLOUD-2052: CLONE - Require authorization to see downloads
    if releases are public - anyone can see specific build
    
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/cloud/api/views/utils.py
+++ cloud_portal/cloud/api/views/utils.py
@@ -99,7 +99,8 @@ def downloads_history(request):
 def download_build(request, build):
     # TODO: later we can check specific permissions
     customization = settings.CUSTOMIZATION
-    if not UserGroupsToCustomizationPermissions.check_permission(request.user, customization, 'api.can_view_release'):
+    if not UserGroupsToCustomizationPermissions.check_permission(request.user, customization, 'api.can_view_release') \
+            or not customization.public_release_history:
         raise APIForbiddenException("Not authorized", ErrorCodes.forbidden)
 
     if re.search(r'\D+', build):
