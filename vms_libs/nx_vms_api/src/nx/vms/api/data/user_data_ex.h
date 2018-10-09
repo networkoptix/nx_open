1eb50a0045abaffd09a27c35334e4c8f390f11da
Alexander Kulikov <akulikov@networkoptix.com>
2018-10-09 13:26:45 +0300

    VMS-11783: Fix review issues
    <==== Mercurial data ====>
    Branch: vms

--- vms_libs/nx_vms_api/src/nx/vms/api/data/user_data_ex.h
+++ vms_libs/nx_vms_api/src/nx/vms/api/data/user_data_ex.h
@@ -6,9 +6,7 @@
 
 #include <nx/vms/api/types/access_rights_types.h>
 
-namespace nx {
-namespace vms {
-namespace api {
+namespace nx::vms::api {
 
 struct NX_VMS_API UserDataEx
 {
@@ -27,8 +25,6 @@ struct NX_VMS_API UserDataEx
 #define UserDataEx_Fields (id)(name)(permissions)(email)(password)(isCloud)(isLdap)(isEnabled) \
     (userRoleId)(fullName)
 
-} // namespace api
-} // namespace vms
-} // namespace nx
+} // namespace nx::vms::api
 
 Q_DECLARE_METATYPE(nx::vms::api::UserDataEx)
