a463c1d55b6e98803c6341feb9a9d4fc938e3e1d
Vsevolod Fedorov <vfedorov@networkoptix.com>
2018-10-12 14:41:59 +0300

    FT-298: Scalability test: measure transaction propagation time
    <==== Mercurial data ====>
    Branch: vms

--- func_tests/tests/scalability_test.py
+++ func_tests/tests/scalability_test.py
@@ -3,17 +3,20 @@
 Measure system synchronization time.
 """
 
-import datetime
+from datetime import timedelta
 import json
 from collections import namedtuple
 from contextlib import contextmanager
 import itertools
 from functools import partial
+import time
+import threading
 
+from contextlib2 import ExitStack
+import dateutil.parser
 import pytest
 from netaddr import IPNetwork
 
-import framework.utils as utils
 from framework.data_differ import full_info_differ, transaction_log_differ
 from framework.installation.mediaserver import MEDIASERVER_MERGE_TIMEOUT
 from framework.mediaserver_api import MediaserverApiRequestError
@@ -22,14 +25,23 @@ from framework.mediaserver_sync_wait import (
     wait_for_api_path_match,
     wait_until_no_transactions_from_servers,
     )
+from framework.message_bus import SetResourceParamCommand, message_bus_running
 from framework.merging import merge_systems
 from framework.context_logger import ContextLogger, context_logger
+from framework.threaded import ThreadedCall
 from framework.utils import (
+    bool_to_str,
+    datetime_local_now,
+    datetime_utc_now,
+    imerge,
     make_threaded_async_calls,
-    MultiFunction,
     single,
     take_some,
+    str_to_bool,
+    threadsafe_generator,
     with_traceback,
+    FunctionWithDescription,
+    MultiFunction,
     )
 from system_load_metrics import load_host_memory_usage
 import server_api_data_generators as generator
@@ -38,12 +50,15 @@ pytest_plugins = ['system_load_metrics', 'fixtures.unpacked_mediaservers']
 
 _logger = ContextLogger(__name__)
 _create_test_data_logger = _logger.getChild('create_test_data')
+_post_stamp_logger = _logger.getChild('stamp')
 _post_check_logger = _logger.getChild('post_check')
 _dumper_logger = _logger.getChild('dumper')
 
 
 SET_RESOURCE_STATUS_CMD = '202'
 CREATE_DATA_THREAD_NUMBER = 16
+TRANSACTION_GENERATOR_THREAD_NUMBER = 16
+STALE_LIMIT_SEC = 10
 
 
 @pytest.fixture()
@@ -55,15 +70,16 @@ def config(test_config):
         STORAGES_PER_SERVER=1,
         USERS_PER_SERVER=1,
         PROPERTIES_PER_CAMERA=5,
+        TRANSACTIONS_PER_SERVER_RATE=10,
         MERGE_TIMEOUT=MEDIASERVER_MERGE_TIMEOUT,
-        MESSAGE_BUS_TIMEOUT=datetime.timedelta(seconds=10),
+        MESSAGE_BUS_TIMEOUT=timedelta(seconds=3),
         MESSAGE_BUS_SERVER_COUNT=5,
+        TRANSACTIONS_STAGE_DURATION=timedelta(seconds=10),
         HOST_LIST=None,  # use vm servers by default
         )
 
 
 # resource creation  ================================================================================
-
 
 def get_server_admin(server):
     admins = [(server, u) for u in server.api.generic.get('ec2/getUsers') if u['isAdmin']]
@@ -200,13 +216,20 @@ def api_path_getter(server, api_path):
         raise
 
 
+def pick_some_servers(server_list, required_count):
+    server_count = len(server_list)
+    if server_count <= required_count:
+        return server_list
+    else:
+        return [server_list[i] for i in range(0, server_count, server_count/required_count)]
+
+
 def wait_for_servers_synced(artifact_factory, config, env):
     wait_until_no_transactions_from_servers(
         env.real_server_list[:1], config.MESSAGE_BUS_TIMEOUT.total_seconds())
     real_server_count = len(env.real_server_list)
-    if real_server_count > config.MESSAGE_BUS_SERVER_COUNT:
-        server_list = [env.real_server_list[i]
-                           for i in range(1, real_server_count, real_server_count/config.MESSAGE_BUS_SERVER_COUNT)]
+    if len(env.real_server_list) > config.MESSAGE_BUS_SERVER_COUNT:
+        server_list = pick_some_servers(env.real_server_list[1:], config.MESSAGE_BUS_SERVER_COUNT)
         wait_until_no_transactions_from_servers(server_list, config.MESSAGE_BUS_TIMEOUT.total_seconds())
 
     def wait_for_match(api_path, differ):
@@ -220,6 +243,121 @@ def wait_for_servers_synced(artifact_factory, config, env):
         e.log_and_dump_results(artifact_factory)
         make_core_dumps(env)
         raise
+
+
+# transactions ======================================================================================
+
+@threadsafe_generator
+def transaction_generator(server_list):
+
+    def server_generator(server):
+        camera_list = server.api.camera_list
+        for idx in itertools.count():
+            iso_datetime = datetime_local_now().isoformat()
+            param_list = [dict(
+                resourceId=camera.camera_id,
+                name='scalability-stamp',
+                value='{}.{:02}.{}'.format(server.name, camera_idx, iso_datetime),
+                )
+                for camera_idx, camera in enumerate(camera_list)]
+            yield FunctionWithDescription(
+                partial(server.api.generic.post, 'ec2/setResourceParams', param_list),
+                'to %s: %s' % (server.name, iso_datetime),
+                )
+
+    return imerge(*(server_generator(server) for server in server_list))
+
+
+def parse_stamp(stamp):
+    server_name, camera_idx, iso_datetime = stamp.split('.', 2)
+    return dateutil.parser.parse(iso_datetime)
+
+
+class Pacer(object):
+
+    def __init__(self, required_rate):
+        assert required_rate, repr(required_rate)  # must not be zero
+        self._required_rate = required_rate  # transactions per second
+        self._lock = threading.Lock()
+        self._interval_start = time.time()  # interval is 1 second
+        self._transaction_count = 0  # during last interval (second)
+        self._sleep_time = 0
+        self.stale_time = 0
+
+    @contextmanager
+    def pace_call(self):
+        yield
+        time.sleep(self._sleep_time)
+        with self._lock:
+            self._transaction_count += 1
+            if self._transaction_count < self._required_rate:
+                return
+            delta = time.time() - self._interval_start
+            if delta < 1:
+                self._sleep_time = (1 - delta) / self._transaction_count
+                time.sleep(1 - delta)  # sleep rest of this second, blocking other threads
+            else:
+                _logger.warning(
+                    ('Servers unable to process transactions at required rate (%d/sec),'
+                     ' stalled for %s seconds') % (self._required_rate, delta - 1))
+                self.stale_time += delta - 1
+            self._interval_start = time.time()
+            self._transaction_count = 0
+
+
+@contextmanager
+def transactions_generated(call_generator, rate):
+    pacer = Pacer(rate)
+    issued_transactoins_count = itertools.count()
+
+    def issue_transaction():
+        fn = next(call_generator)
+        with pacer.pace_call():
+            _post_stamp_logger.info('Post stamp %s', fn.description)
+            with ExitStack() as stack:
+                stack.enter_context(context_logger(_post_stamp_logger, 'framework.http_api'))
+                stack.enter_context(context_logger(_post_stamp_logger, 'framework.mediaserver_api'))
+                fn()
+                next(issued_transactoins_count)
+
+    with ExitStack() as stack:
+        _logger.debug('Starting transaction generating threads.')
+        for idx in range(TRANSACTION_GENERATOR_THREAD_NUMBER):
+            stack.enter_context(ThreadedCall.periodic(issue_transaction, terminate_timeout_sec=30))
+
+        yield
+
+        _logger.debug('Stopping transaction generating threads:')
+    _logger.debug('Stopping transaction generating threads: done.')
+
+    _logger.info('Issued %d transactions', next(issued_transactoins_count))
+    assert pacer.stale_time < STALE_LIMIT_SEC, (
+        'Servers was unable to process transactions at required rate (%d/sec): stalled for %s seconds'
+        % (rate, pacer.stale_time))
+
+
+def run_transactions(metrics_saver, config, env):
+    transaction_gen = transaction_generator(env.all_server_list)
+    transaction_rate = config.TRANSACTIONS_PER_SERVER_RATE * len(env.all_server_list)
+    with transactions_generated(transaction_gen, transaction_rate):
+        watched_server_list = pick_some_servers(env.all_server_list, config.MESSAGE_BUS_SERVER_COUNT)
+        with message_bus_running(watched_server_list) as bus:
+            start = datetime_utc_now()
+            max_delay = timedelta()
+            while datetime_utc_now() - start < config.TRANSACTIONS_STAGE_DURATION:
+                transaction = bus.get_transaction(timeout_sec=0.2)
+                if not transaction:
+                    continue
+                if not isinstance(transaction.command, SetResourceParamCommand):
+                    continue
+                if not transaction.command.param.name == 'scalability-stamp':
+                    continue
+                stamp_dt = parse_stamp(transaction.command.param.value)
+                delay = datetime_utc_now() - stamp_dt
+                _logger.debug('Transaction delay: %s', delay)
+                max_delay = max(max_delay, delay)
+    _logger.info('Maximum transaction delay: %s', max_delay)
+    metrics_saver.save('max_transaction_delay', max_delay.total_seconds())
 
 
 # ===================================================================================================
@@ -239,8 +377,8 @@ def collect_additional_metrics(metrics_saver, os_access_set, lws):
 
 
 system_settings = dict(
-    autoDiscoveryEnabled=utils.bool_to_str(False),
-    synchronizeTimeWithInternet=utils.bool_to_str(False),
+    autoDiscoveryEnabled=bool_to_str(False),
+    synchronizeTimeWithInternet=bool_to_str(False),
     )
 
 server_config = dict(
@@ -262,7 +400,7 @@ def lws_env(config, groups):
                 USERS_PER_SERVER=config.USERS_PER_SERVER,
                 PROPERTIES_PER_CAMERA=config.PROPERTIES_PER_CAMERA,
                 ) as lws:
-            merge_start_time = utils.datetime_utc_now()
+            merge_start_time = datetime_utc_now()
             server.api.merge(lws[0].api, lws.server_bind_address, lws[0].port, take_remote_settings=True)
             yield Env(
                 all_server_list=[server] + lws.servers,
@@ -277,7 +415,7 @@ def lws_env(config, groups):
 def make_real_servers_env(config, server_list, common_net):
     # lightweight servers create data themselves
     create_test_data(config, server_list)
-    merge_start_time = utils.datetime_utc_now()
+    merge_start_time = datetime_utc_now()
     for server in server_list[1:]:
         merge_systems(server_list[0], server, accessible_ip_net=common_net)
     return Env(
@@ -328,7 +466,7 @@ def perform_post_checks(env):
     _logger.info('Perform test post checks:')
     if env.real_server_list[0].api.is_online():
         settings = env.real_server_list[0].api.get_system_settings()  # log final settings
-        assert utils.str_to_bool(settings['autoDiscoveryEnabled']) is False
+        assert str_to_bool(settings['autoDiscoveryEnabled']) is False
     _logger.info('Perform test post checks: done.')
 
 
@@ -341,12 +479,15 @@ def dump_full_info(artifacts_dir, env):
         json.dump(full_info, f, indent=2)
 
 def test_scalability(artifact_factory, artifacts_dir, metrics_saver, load_averge_collector, config, env):
-    assert isinstance(config.MERGE_TIMEOUT, datetime.timedelta)
+    assert isinstance(config.MERGE_TIMEOUT, timedelta)
 
     try:
         with load_averge_collector(env.os_access_set):
             wait_for_servers_synced(artifact_factory, config, env)
-        merge_duration = utils.datetime_utc_now() - env.merge_start_time
+        merge_duration = datetime_utc_now() - env.merge_start_time
+
+        run_transactions(metrics_saver, config, env)
+
         metrics_saver.save('merge_duration', merge_duration)
         collect_additional_metrics(metrics_saver, env.os_access_set, env.lws)
         dump_full_info(artifacts_dir, env)
