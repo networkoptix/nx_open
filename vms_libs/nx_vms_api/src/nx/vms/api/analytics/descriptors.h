e0d2aa4a6a587930520605ff7fa379639de4ddfa
Dmitry Mishin <dmishin@networkoptix.com>
2018-10-20 15:49:37 +0300

    META-48: Added analytics concepts descriptors
    <==== Mercurial data ====>
    Branch: meta_resources

---
+++ vms_libs/nx_vms_api/src/nx/vms/api/analytics/descriptors.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <set>
+
+#include <QtCore/QString>
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/vms/api/analytics/manifest_items.h>
+
+namespace nx::vms::api::analytics {
+
+struct PluginDescriptor
+{
+    QString id;
+    QString name;
+};
+#define nx_vms_api_analyitcs_PluginDescriptor_Fields (id)(name)
+
+struct GroupDescriptor
+{
+    QString id;
+    QString name;
+};
+#define nx_vms_api_analyitcs_GroupDescriptor_Fields (id)(name)
+
+struct EventTypeDescriptor
+{
+    std::set<QString> pluginIds;
+    std::set<QString> groupIds;
+    EventType item;
+};
+#define  nx_vms_api_analyitcs_EventTypeDescriptor_Fields (pluginIds)(groupIds)(item)
+
+struct ObjectTypeDescriptor
+{
+    std::set<QString> pluginIds;
+    std::set<QString> groupIds;
+    ObjectType item;
+};
+#define nx_vms_api_analyitcs_ObjectTypeDescriptor_Fields (pluginIds)(groupIds)(item)
+
+QN_FUSION_DECLARE_FUNCTIONS(PluginDescriptor, (json), NX_VMS_API)
+QN_FUSION_DECLARE_FUNCTIONS(GroupDescriptor, (json), NX_VMS_API)
+QN_FUSION_DECLARE_FUNCTIONS(EventTypeDescriptor, (json), NX_VMS_API)
+QN_FUSION_DECLARE_FUNCTIONS(ObjectTypeDescriptor, (json), NX_VMS_API)
+
+} // namespace nx::vms::api::analytics
