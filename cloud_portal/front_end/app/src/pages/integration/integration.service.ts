b8a294e8d3b9ececef92f367819dfae4b7bfd68b
Nick Hartleb <nhartleb@networkoptix.com>
2018-11-02 17:02:23 -0700

    CLOUD-2422: Integration store - downloads block
    <==== Mercurial data ====>
    Branch: cloud_18.4

--- cloud_portal/front_end/app/src/pages/integration/integration.service.ts
+++ cloud_portal/front_end/app/src/pages/integration/integration.service.ts
@@ -3,6 +3,13 @@ import { HttpClient }                   from '@angular/common/http';
 import { BehaviorSubject, Observable }  from 'rxjs';
 import { NxCloudApiService }            from '../../services/nx-cloud-api';
 
+
+interface Platform {
+    file: string;
+    name: string;
+    url: string;
+}
+
 @Injectable({
     providedIn: 'root'
 })
@@ -16,12 +23,39 @@ export class IntegrationService implements OnDestroy {
 
         this.getIntegrations().subscribe(result => {
             this.plugins = result.data;
+            this.formatDownloads();
             this.pluginsSubject.next(this.plugins);
         });
     }
 
     private getIntegrations(): Observable<any> {
         return this.api.getIntegrations();
+    }
+
+    private formatDownloads() {
+        this.plugins.forEach((plugin) => {
+            if (plugin.downloadFiles) {
+                let downloadPlatforms = plugin.downloadFiles;
+                plugin.downloadFiles = [];
+                Object.keys(downloadPlatforms).forEach((platformName) => {
+                    let platform: Platform = { file: '', name: '', url: '' };
+                    platform.name = platformName.replace('-file', '')
+                            .replace('-', ' ');
+                    platform.name = platform.name[0].toUpperCase() + platform.name.slice(1);
+                    platform.url = downloadPlatforms[platformName];
+                    platform.file = platform.url.slice(platform.url.lastIndexOf('/') + 1);
+                    plugin.downloadFiles.push(platform);
+                });
+                plugin.downloadFiles = plugin.downloadFiles.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return -1;
+                    } else if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                });
+            }
+        });
     }
 
     getPluginBy(id) {
