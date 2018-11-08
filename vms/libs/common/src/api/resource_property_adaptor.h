575b1a681019b6694fca7fd6fd65783f693041bf
Roman Vasilenko <rvasilenko@networkoptix.com>
2018-11-08 18:00:38 +0300

    VMS-11998: Server has crashed on Windows: c0000005 (Access violation)
    <==== Mercurial data ====>
    Branch: vms

--- vms/libs/common/src/api/resource_property_adaptor.h
+++ vms/libs/common/src/api/resource_property_adaptor.h
@@ -7,6 +7,7 @@
 
 #include <nx/fusion/serialization/json_functions.h>
 #include <nx/fusion/serialization/lexical_functions.h>
+#include <nx/utils/safe_direct_connection.h>
 
 #include <core/resource/resource_fwd.h>
 
@@ -96,7 +97,10 @@ public:
  *
  * This class is thread-safe.
  */
-class QnAbstractResourcePropertyAdaptor: public Connective<QObject> {
+class QnAbstractResourcePropertyAdaptor:
+    public Connective<QObject>,
+    public Qn::EnableSafeDirectConnection
+{
     Q_OBJECT
     typedef Connective<QObject> base_type;
 
