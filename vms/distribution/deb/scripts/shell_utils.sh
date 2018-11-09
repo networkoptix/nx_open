d0f9810d29635084a3d2e50b17471bfd2ee4cb04
Mikhail Uskov <muskov@networkoptix.com>
2018-11-09 21:32:09 +0300

    VMS-12101: Fix core dump analysis on all linux servers
    <==== Mercurial data ====>
    Branch: vms

--- vms/distribution/deb/scripts/shell_utils.sh
+++ vms/distribution/deb/scripts/shell_utils.sh
@@ -2,17 +2,20 @@
 
 function crash_gdb_bt() {
     BIN_PATH=$1
-    CRASH_DIR=${2:$HOME}
+    CRASH_DIR=${2:-$HOME}
+    FULL_VERSION=${3:-"no-version"}
     if [ ! "$BIN_PATH" ]; then
-        echo "Usage: crash_gdb_bt BINARY_PATH [CRASH_DIR]" > 2
+        echo "Usage: crash_gdb_bt BINARY_PATH [CRASH_DIR]" >2
         return 2
     fi
 
     if [ ! $(which gdb) ]; then
-        return 0 # no gdb on the host
+        echo GDB is not available on the host. >2
+        return 0
     fi
     if [ $(pidof gdb) ]; then
-        return 0 # gdb is alrady engaged
+        echo GDB is already engaged. >2
+        return 0
     fi
 
     CORE_ORIG=$(dirname $BIN_PATH)/core
@@ -20,9 +23,9 @@ function crash_gdb_bt() {
     CORE=$CORE_ORIG.$TIME
     mv $CORE_ORIG $CORE 2>/dev/null
     if [ $? -eq 0 ]; then
-        REPORT=$(basename $BIN_PATH)_$($BIN_PATH --version)_$TIME.gdb-bt
+        REPORT=$(basename $BIN_PATH)_${FULL_VERSION}_$TIME.gdb-bt
 
-        echo Generate crash report $REPORT...        BACKGROUND
+        echo Generate crash report $CRASH_DIR/$REPORT...   BACKGROUND
         echo "t apply all bt 25" | gdb $BIN_PATH $CORE >/tmp/$REPORT 2>&1 && \
             mkdir -p $CRASH_DIR && mv /tmp/$REPORT $CRASH_DIR/$REPORT &
 
