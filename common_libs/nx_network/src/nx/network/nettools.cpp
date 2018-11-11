b1abb32c9e2d28e208a49ded18285998d9ce4099
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-11-11 12:30:07 -0800

    Fixed compilation error on ios
    <==== Mercurial data ====>
    Branch: cloud_18.3_backend

--- common_libs/nx_network/src/nx/network/nettools.cpp
+++ common_libs/nx_network/src/nx/network/nettools.cpp
@@ -415,7 +415,7 @@ void removeARPrecord(const QHostAddress& /*ip*/) {}
 #if defined(Q_OS_IOS)
 utils::MacAddress getMacByIP(const QHostAddress& /*ip*/, bool /*net*/)
 {
-    return MacAddress();
+    return utils::MacAddress();
 }
 #else // defined(Q_OS_IOS)
 
