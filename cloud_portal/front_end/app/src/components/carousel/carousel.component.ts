feccc296e7fafaed8217355d87b4cec4d0bde619
Nick Hartleb <nhartleb@networkoptix.com>
2018-11-15 16:38:25 -0800

    CLOUD-2472: Add screenshots to cms_structure and details page
    added animations
    made carousel and images responsive
    refactored code
    
    <==== Mercurial data ====>
    Branch: cloud_18.4

--- cloud_portal/front_end/app/src/components/carousel/carousel.component.ts
+++ cloud_portal/front_end/app/src/components/carousel/carousel.component.ts
@@ -1,20 +1,40 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 
 @Component({
     selector: 'nx-carousel',
     templateUrl: 'carousel.component.html',
     styleUrls: ['carousel.component.scss'],
+    animations: [
+        trigger('visibilityChange', [
+            transition('enter => leave', [
+                style({
+                    opacity: 1,
+                    visibility: 'visible'
+                }),
+                animate('0.5s ease-out', style({ opacity: 0, visibility: 'hidden'}))
+            ]),
+            transition('* => enter', [
+                style({
+                    opacity: 0,
+                    visibility: 'hidden'
+                }),
+                animate('0.5s ease-in', style({ opacity: 1, visibility: 'visible'}))
+            ])
+        ])
+    ]
 })
 export class NxCarouselComponent implements OnInit {
     @Input() elements: any;
     keys: any;
 
-    currentIndex = 0;
+    private currentIndex = 0;
     constructor(){}
 
     ngOnInit(){
          this.keys = Object.keys(this.elements).filter((element) => {
-            return element.match(/screenshot/i);
+            return element.match(/screenshot/i) && this.elements[element];
          });
          this.keys = this.keys.sort();
     }
