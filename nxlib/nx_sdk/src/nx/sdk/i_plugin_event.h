9e23d7abdf4fb552291fc14e6fa2f5cb41507ced
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-11-19 20:22:13 +0300

    analytics_sdk: Minor comment improvements; todos added; improved readme.md with specifying x64
    <==== Mercurial data ====>
    Branch: meta

--- nxlib/nx_sdk/src/nx/sdk/i_plugin_event.h
+++ nxlib/nx_sdk/src/nx/sdk/i_plugin_event.h
@@ -12,6 +12,10 @@ namespace sdk {
 static const nxpl::NX_GUID IID_PluginEvent =
     {{0x34,0x2c,0x8e,0x8b,0xad,0x94,0x49,0x85,0x90,0xd8,0x60,0x47,0x0c,0xb9,0x4b,0x9c}};
 
+/**
+ * VMS Event which can be triggered by a Plugin to inform the System about some issue or status
+ * change.
+ */
 class IPluginEvent: public nxpl::PluginInterface
 {
 public:
