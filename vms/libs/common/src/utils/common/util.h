204e705e2545d255da2c8bdd7e35a4568397e75d
Sergey Ivanov <sivanov@networkoptix.com>
2018-11-20 18:50:44 +0300

    Code cleanup: arraysize helper moved to the only place where it is used
    <==== Mercurial data ====>
    Branch: vms_gui

--- vms/libs/common/src/utils/common/util.h
+++ vms/libs/common/src/utils/common/util.h
@@ -4,10 +4,6 @@
 
 #include <QtCore/QString>
 
-template <typename T, size_t N>
-char (&ArraySizeHelper(T (&array)[N]))[N];
-#define arraysize(array) (sizeof(ArraySizeHelper(array)))
-
 /**
  * Remove directory recursively.
  *
