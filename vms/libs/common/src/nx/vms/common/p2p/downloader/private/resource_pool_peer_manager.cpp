e92f6aa2438af1c00d675ef8c992683147f6f0b1
Denis Klychkov <dklychkov@networkoptix.com>
2018-11-29 17:07:15 +0300

    Fix request IDs returned from ResourcePoolPeerManager
    This class should always return its local request IDs, not IDs generated by the used connections.
    
    <==== Mercurial data ====>
    Branch: vms_gui

--- vms/libs/common/src/nx/vms/common/p2p/downloader/private/resource_pool_peer_manager.cpp
+++ vms/libs/common/src/nx/vms/common/p2p/downloader/private/resource_pool_peer_manager.cpp
@@ -212,7 +212,7 @@ rest::Handle ResourcePoolPeerManager::requestFileInfo(
                 return callback(success, handle, FileInformation());
 
             const auto& fileInfo = result.deserialized<FileInformation>();
-            callback(success, handle, fileInfo);
+            callback(success, localRequestId, fileInfo);
         };
 
     const auto handle = connection->fileDownloadStatus(fileName, handleReply, thread());
@@ -247,7 +247,7 @@ rest::Handle ResourcePoolPeerManager::requestChecksums(
                 return callback(success, handle, QVector<QByteArray>());
 
             const auto& checksums = result.deserialized<QVector<QByteArray>>();
-            callback(success, handle, checksums);
+            callback(success, localRequestId, checksums);
         };
 
     const auto handle = connection->fileChunkChecksums(fileName, handleReply, thread());
@@ -281,7 +281,7 @@ rest::Handle ResourcePoolPeerManager::downloadChunk(
             const nx::network::http::HttpHeaders& /*headers*/)
         {
             d->removeCanceller(localRequestId);
-            return callback(success, requestId, result);
+            return callback(success, localRequestId, result);
         };
 
 
@@ -318,7 +318,7 @@ rest::Handle ResourcePoolPeerManager::downloadChunkFromInternet(
                 const QByteArray& data)
             {
                 d->removeCanceller(localRequestId);
-                callback(result, handle, data);
+                callback(result, localRequestId, data);
             };
 
         handle = d->internetPeerManger->downloadChunkFromInternet(
@@ -341,7 +341,7 @@ rest::Handle ResourcePoolPeerManager::downloadChunkFromInternet(
                 const nx::network::http::HttpHeaders& /*headers*/)
             {
                 d->removeCanceller(localRequestId);
-                callback(success, requestId, success ? result : QByteArray());
+                callback(success, localRequestId, success ? result : QByteArray());
             };
 
         handle = connection->downloadFileChunkFromInternet(
