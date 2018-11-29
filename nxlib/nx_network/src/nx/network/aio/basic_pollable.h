dd534394d7251ec5f65c819f5a3faf027b33e378
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-11-28 17:33:54 -0800

    Improved description of nx::network::aio::BasicPollable
    <==== Mercurial data ====>
    Branch: vms_server

--- nxlib/nx_network/src/nx/network/aio/basic_pollable.h
+++ nxlib/nx_network/src/nx/network/aio/basic_pollable.h
@@ -23,7 +23,7 @@ class AIOService;
  * 
  * Sample implementation:
  * <pre><code>
- * class Sample:
+ * class SampleClassWithAsyncOperations:
  *     public nx::network::aio::BasicPollable
  * {
  *     using base_type = nx::network::aio::BasicPollable;
@@ -40,6 +40,7 @@ class AIOService;
  *     void doSomethingAsync(CompletionHandler handler)
  *     {
  *         m_connection = ...; //< Creating connection.
+ *         m_connection->bindToAioThread(getAioThread());
  *         m_connection->any_async_call(... handler);
  *     }
  *
@@ -59,46 +60,86 @@ class AIOService;
  * (usually, it is acheived automatically).
  * QnStoppableAsync::pleaseStop and BasicPollable::stopWhileInAioThread are not called in this case.
  *
- * TODO #ak conflicts with Pollable. Consider renaming Pollable, AbstractPollable, BasicPollable.
+ * TODO #ak Name conflicts with Pollable. Consider renaming Pollable, AbstractPollable, BasicPollable.
  */
 class NX_NETWORK_API BasicPollable:
     public AbstractPollable
 {
 public:
+    /**
+     * @param aioThread If nullptr, then object is bound to the current AIO thread 
+     * (if called in AIO thread) or to a random AIO thread (if current thread is not AIO).
+     */
     BasicPollable(aio::AbstractAioThread* aioThread = nullptr);
+    /**
+     * @param aioThread See description in the previous constructor.
+     */
     BasicPollable(
         aio::AIOService* aioService,
         aio::AbstractAioThread* aioThread);
     virtual ~BasicPollable() override;
 
-    virtual void pleaseStop(nx::utils::MoveOnlyFunc<void()> completionHandler) override;
     /**
-     * NOTE: Non blocking when called within object's aio thread.
+     * @param completionHandler Called in object's AIO thread (returned by getAioThread()) after 
+     * completion or cancellation of all scheduled operations.
+     * NOTE: In most cases, you don't need to override this.
+     * Override BasicPollable::stopWhileInAioThread instead.
+     */
+    virtual void pleaseStop(nx::utils::MoveOnlyFunc<void()> completionHandler) override;
+
+    /**
+     * If called within object's AIO thread (returned by getAioThread()) then cancells all 
+     * scheduled non-blocking operations and returns immediately.
+     * Otherwise, invokes BasicPollable::pleaseStop and waits for completion.
+     * NOTE: In most cases, you don't need to override this.
+     * Override BasicPollable::stopWhileInAioThread instead.
      */
     virtual void pleaseStopSync() override;
 
     virtual aio::AbstractAioThread* getAioThread() const override;
     /**
-     * Generally, binding to aio thread can be done just after
-     *     object creation before any usage.
-     * Some implementation may allow more (e.g., binding if no async
-     *     operations are scheduled at the moment)
-     * NOTE: Re-binding is allowed
+     * Generally, binding to aio thread can be done just after object creation before scheduling 
+     * any asynchronous operations.
+     * Some implementation may allow more. E.g., binding if all async operations have completed.
+     * NOTE: Calling this method while there are scheduled asynchronous operations is 
+     * undefined behavior.
+     * NOTE: Re-binding is allowed.
      */
     virtual void bindToAioThread(aio::AbstractAioThread* aioThread) override;
+
+    /**
+     * Posts func to be called in object's AIO thread ASAP.
+     * Returns immediately (without waiting for func to be executed).
+     */
     virtual void post(nx::utils::MoveOnlyFunc<void()> func) override;
+    
+    /**
+     * If called within AIO thread (same as returned by getAioThread()) then func is executed 
+     * right in this call and dispatch returns after that.
+     * Otherwse, invokes BasicPollable::post.
+     */
     virtual void dispatch(nx::utils::MoveOnlyFunc<void()> func) override;
 
+    /**
+     * @return true if called within same thread as reported by getAioThread().
+     */
     bool isInSelfAioThread() const;
 
-    void cancelPostedCalls(nx::utils::MoveOnlyFunc<void()> completionHandler);
     /**
-     * NOTE: Non blocking if called within object's aio thread.
+     * @param completionHandler Will be called in object's AIO thread (returned by getAioThread())
+     * after completion or cancellation of all posted calls.
+     */
+    void cancelPostedCalls(nx::utils::MoveOnlyFunc<void()> completionHandler);
+
+    /**
+     * When called from object's AIO thread, then cancells all posted calls immediately and returns.
+     * Otherwise, invokes BasicPollable::cancelPostedCalls and waits for its completion.
      */
     void cancelPostedCallsSync();
 
     /**
-     * NOTE: Non blocking when called within object's aio thread.
+     * If called within object's AIO thread then invokes func immediately and returns.
+     * Otherwise, posts func using BasicPollable::post and waits for completion.
      */
     template<typename Func>
     void executeInAioThreadSync(Func func)
@@ -120,11 +161,20 @@ public:
         }
     }
 
+    /**
+     * @return Object, that can be used directly with AIOThread::startMonitoring.
+     * NOTE: If you think you need it, consider it one more time.
+     * If you still think you need it, go and talk to someone about it.
+     */
     Pollable& pollable();
     const Pollable& pollable() const;
 
 protected:
-    /** Reimplement and cancel your asynchronous operations here. */
+    /**
+     * Reimplement and cancel your asynchronous operations here.
+     * NOTE: See class description for an example.
+     * NOTE: Always called in object's AIO thread.
+     */
     virtual void stopWhileInAioThread();
 
 private:
