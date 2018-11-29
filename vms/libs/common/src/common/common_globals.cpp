1525cc6efc663efb52b15b7ae9876f0c93d318a4
Mike Shevchenko <mshevchenko@networkoptix.com>
2018-11-29 21:00:32 +0300

    Minor coding style fixes
    <==== Mercurial data ====>
    Branch: meta

--- vms/libs/common/src/common/common_globals.cpp
+++ vms/libs/common/src/common/common_globals.cpp
@@ -1,8 +1,8 @@
 #include "common_globals.h"
 
-#include <core/ptz/ptz_constants.h>
-
 #include <nx/fusion/model_functions.h>
+
+#include <core/ptz/ptz_constants.h>
 
 QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(Qn, PtzCommand)
 QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(Qn, PtzCoordinateSpace)
@@ -22,56 +22,56 @@ QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(Qn, StatusChangeReason)
 QN_DEFINE_METAOBJECT_ENUM_LEXICAL_FUNCTIONS(Qn, StorageStatuses)
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, IOPortType,
-(Qn::PT_Unknown,  "Unknown")
-(Qn::PT_Disabled, "Disabled")
-(Qn::PT_Input,    "Input")
-(Qn::PT_Output,   "Output")
+    (Qn::PT_Unknown, "Unknown")
+    (Qn::PT_Disabled, "Disabled")
+    (Qn::PT_Input, "Input")
+    (Qn::PT_Output, "Output")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, IODefaultState,
-(Qn::IO_OpenCircuit,  "Open Circuit")
-(Qn::IO_GroundedCircuit, "Grounded circuit")
+    (Qn::IO_OpenCircuit, "Open Circuit")
+    (Qn::IO_GroundedCircuit, "Grounded circuit")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, StorageInitResult,
-    (Qn::StorageInit_Ok,            "Ok")
-    (Qn::StorageInit_CreateFailed,  "CreateFailed")
-    (Qn::StorageInit_WrongPath,     "InitFailed_WrongPath")
-    (Qn::StorageInit_WrongAuth,     "InitFailed_WrongAuth")
+    (Qn::StorageInit_Ok, "Ok")
+    (Qn::StorageInit_CreateFailed, "CreateFailed")
+    (Qn::StorageInit_WrongPath, "InitFailed_WrongPath")
+    (Qn::StorageInit_WrongAuth, "InitFailed_WrongAuth")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, MediaStreamEvent,
-    (Qn::NoEvent,                       "NoEvent")
-    (Qn::TooManyOpenedConnections,      "TooManyOpenedConnections")
-    (Qn::ForbiddenWithDefaultPassword,  "ForbiddenWithDefaultPassword")
-    (Qn::ForbiddenWithNoLicense,        "ForbiddenWithNoLicense")
-    (Qn::oldFirmware,                   "oldFirmare")
+    (Qn::NoEvent, "NoEvent")
+    (Qn::TooManyOpenedConnections, "TooManyOpenedConnections")
+    (Qn::ForbiddenWithDefaultPassword, "ForbiddenWithDefaultPassword")
+    (Qn::ForbiddenWithNoLicense, "ForbiddenWithNoLicense")
+    (Qn::oldFirmware, "oldFirmare")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, BookmarkSortField,
-    (Qn::BookmarkName,          "name")
-    (Qn::BookmarkStartTime,     "startTime")
-    (Qn::BookmarkDuration,      "duration")
-    (Qn::BookmarkCreationTime,  "created")
-    (Qn::BookmarkCreator,       "creator")
-    (Qn::BookmarkTags,          "tags")
-    (Qn::BookmarkCameraName,    "cameraName")
+    (Qn::BookmarkName, "name")
+    (Qn::BookmarkStartTime, "startTime")
+    (Qn::BookmarkDuration, "duration")
+    (Qn::BookmarkCreationTime, "created")
+    (Qn::BookmarkCreator, "creator")
+    (Qn::BookmarkTags, "tags")
+    (Qn::BookmarkCameraName, "cameraName")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qt, SortOrder,
-    (Qt::AscendingOrder,     "asc")
-    (Qt::DescendingOrder,    "desc")
+    (Qt::AscendingOrder, "asc")
+    (Qt::DescendingOrder, "desc")
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, RebuildAction,
-(Qn::RebuildAction_Start,   "start")
-(Qn::RebuildAction_Cancel,  "stop")
+(Qn::RebuildAction_Start, "start")
+(Qn::RebuildAction_Cancel, "stop")
 (Qn::RebuildAction_ShowProgress, QString())
 )
 
 QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(Qn, BackupAction,
-(Qn::BackupAction_Start,   "start")
-(Qn::BackupAction_Cancel,  "stop")
+(Qn::BackupAction_Start, "start")
+(Qn::BackupAction_Cancel, "stop")
 (Qn::BackupAction_ShowProgress, QString())
 )
 
