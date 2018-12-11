0210dadad3de99f5c18c6c6a4d901667c2972d20
Nate Wilson <nwilson@networkoptix.com>
2018-12-10 16:39:04 -0800

    VMS-12052: Use the USB serial number as unique id if it has one
    <==== Mercurial data ====>
    Branch: vms_4.0_server

---
+++ vms/server/plugins/device/usb_camera_plugin/src/device/video/windows/wdm_utils.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+namespace nx {
+namespace usb_cam {
+namespace device {
+namespace video {
+namespace detail {
+
+std::string getDeviceUniqueId(const std::string& devicePath);
+
+} //namespace detail
+} //namespace video
+} //namespace device
+} //namespace usb_cam
+} //namespace nx
