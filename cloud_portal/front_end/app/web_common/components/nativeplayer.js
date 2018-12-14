7d0047f8b034d9f32f038df38a806b6002729ab7
Tsanko Tsolov <ttsolov@networkoptix.com>
2018-12-14 14:12:53 -0800

    CLOUD-2552: "Unexpected error" on particular camera in Edge browser
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/web_common/components/nativeplayer.js
+++ cloud_portal/front_end/app/web_common/components/nativeplayer.js
@@ -13,6 +13,9 @@
     };
     
     window.NativePlayer.prototype.play = function () {
+        if (this.video === undefined) {
+            return;
+        }
         var playPromise = this.video.play();
         if (playPromise) {
             playPromise.catch(function (error) {
@@ -26,6 +29,9 @@
     };
     
     window.NativePlayer.prototype.pause = function () {
+        if (this.video === undefined) {
+            return;
+        }
         this.video.pause();
     };
     
--- cloud_portal/front_end/app/web_common/scripts/directives/cameraViewInformer.js
+++ cloud_portal/front_end/app/web_common/scripts/directives/cameraViewInformer.js
@@ -2,19 +2,20 @@
     
     'use strict';
     
-    function CameraViewInformer(configService) {
+    function CameraViewInformer(configService, languageService) {
     
         const CONFIG = configService.config;
+        const LANG = languageService.lang;
         
         function updateTpl(scope) {
             //offline and unauthorized are special cases. All others can be set without editing
             if (scope.alertType === 'status') {
-                scope.placeholderTitle = L.common.cameraStates[(scope.flags[scope.alertType]).toLowerCase()];
+                scope.placeholderTitle = LANG.common.cameraStates[(scope.flags[scope.alertType]).toLowerCase()];
                 scope.message = '';
                 scope.iconClass = (scope.flags.status === 'Offline') ? 'camera-view-offline' : 'camera-view-unauthorized';
             } else {
                 scope.iconClass = 'camera-view-error';
-                scope.placeholderTitle = L.common.cameraStates.error;
+                scope.placeholderTitle = LANG.common.cameraStates.error;
                 scope.message = scope.flags.errorDescription;
             }
         }
@@ -72,7 +73,7 @@
         };
     }
     
-    CameraViewInformer.$inject = ['configService'];
+    CameraViewInformer.$inject = ['configService', 'languageService'];
     
     angular
         .module('nxCommon')
