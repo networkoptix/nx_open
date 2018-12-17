6e280b8664da95b4db1cc586bdbd1426c0563a2f
Sergey Yuldashev <syuldashev@networkoptix.com>
2018-12-17 16:13:44 +0300

    VMS-7699: AV and Axis camera lists are updated. Autoscan script is updated
    <==== Mercurial data ====>
    Branch: vms_3.2

--- mediaserver/update_db_scripts/av_compare.py
+++ mediaserver/update_db_scripts/av_compare.py
@@ -4,35 +4,51 @@ file using online database.
 Must be run from the original file location.
 '''
 
-#script puts file with mismatches to your home directory
+# script puts file with mismatches to your home directory
 import requests
 import re
 import os
 from os.path import expanduser
+from xml.dom import minidom
+import unicodedata
 
 home_dir = expanduser("~")
 av_url = "http://www.arecontvision.com/productselector.php"
 
-site_list = set(map(lambda x:x[2:], re.findall('AV\d\d\d\d+',
+site_list_draft = set(map(lambda x: x[2:], re.findall('AV\d+\w+',
     requests.get(av_url).content)))
-    
+site_list = []
+for item in site_list_draft:
+    match_0 = re.match('\d\d[A-Za-z]+', item)
+    match_1 = re.match('\d\d\d[A-Za-z]+', item)
+    match_2 = re.match('\d\d\d\d+', item)
+    for match in [match_0, match_1, match_2]:
+        if match is not None:
+            site_list.append(match.group(0))
+
+site_set = set(site_list)
 f1_path = os.path.abspath(os.getcwd() + '/..' + '/..' +
     '/appserver2/static-resources/resources/camera_types/arecontvision.xml')
-    
-f1 = open(f1_path, 'r')
 
 f2_path = os.path.abspath(os.getcwd() + '/..' + '/..' +
     '/appserver2/static-resources/02_insert_all_vendors.sql')
-    
 f2 = open(f2_path, 'r')
 
-cur_list = set(map(lambda x:x[1:], re.findall('\"\d\d\d\d+', 
-    f1.read()) + re.findall('\'\d\d\d\d+', f2.read())))
-    
+xml_list = []
+arecont_xml = minidom.parse(f1_path)
+arecont_xml_models = arecont_xml.getElementsByTagName('resource')
+for model in arecont_xml_models:
+    match = re.match('\d+\w+', model.attributes['name'].value)
+    if match is not None:
+        xml_list.append(str(match.group(0)))
+
+sql_list = re.findall('\d\d\d\d+', f2.read())
+
+cur_set = set(xml_list + sql_list)
 result_file = open(os.path.join(home_dir, "new_av_models.txt"), 'w')
 
-for model in site_list:
-    if model not in cur_list:
+for model in site_set:
+    if model not in cur_set:
         result_file.write(model + '\n')
 
 result_file.close()
