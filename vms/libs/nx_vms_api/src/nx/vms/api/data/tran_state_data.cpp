b13060ab327acb6b8467d6fade9a9677ecbfb574
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2018-12-19 16:45:04 +0300

    VMS-CR-14335: Fixed comparing peer's transaction states
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/libs/nx_vms_api/src/nx/vms/api/data/tran_state_data.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/tran_state_data.cpp
@@ -8,26 +8,26 @@ namespace api {
 
 bool TranState::operator<(const TranState& right) const
 {
-    // This is less if:
-    //  - right has record that is missing in this
-    //  - right has greater sequence than this in corresponding record
+    // Comparing states as sequences of pairs of comparable values.
+    // So, using general string comparison rules: string str1 is considered less than str2 if it
+    // contains a lesser character on some position.
 
     auto thisIter = values.cbegin();
-    auto rightIter = values.cbegin();
+    auto rightIter = right.values.cbegin();
 
     for (;;)
     {
         if (thisIter == values.cend() && rightIter != right.values.cend())
-            return true;    //< right has record that is missing in this
+            return true; //< Right is longer.
         if (thisIter != values.cend() && rightIter == right.values.cend())
-            return false;    //< this has record that is missing in right
+            return false; //< this is longer.
         if (thisIter == values.cend() && rightIter == right.values.cend())
-            return false;    //< seems like transaction states are equal
+            return false;    //< Equal.
 
         if (thisIter.key() < rightIter.key())
-            return false;   //< this has record that is missing in right
+            return true;   //< this has a record with a lesser key on the some position.
         if (thisIter.key() > rightIter.key())
-            return true;    //< right has record that is missing in this
+            return false;  //< this has a record with a greater key on the some position.
         if (thisIter.value() != rightIter.value())
             return thisIter.value() < rightIter.value();
         //< right has greater sequence than this in corresponding record
