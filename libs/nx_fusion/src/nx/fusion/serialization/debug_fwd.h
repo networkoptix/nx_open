c8ced2e6b82b160aa1af6ae8650bcca4b5f76ca3
Sergey Ivanov <sivanov@networkoptix.com>
2018-12-25 17:24:17 +0300

    Added GTest support for the fusion macro (debug)
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- libs/nx_fusion/src/nx/fusion/serialization/debug_fwd.h
+++ libs/nx_fusion/src/nx/fusion/serialization/debug_fwd.h
@@ -1,9 +1,7 @@
-#ifndef QN_SERIALIZATION_DEBUG_FWD_H
-#define QN_SERIALIZATION_DEBUG_FWD_H
+#pragma once
 
 class QDebug;
 
-#define QN_FUSION_DECLARE_FUNCTIONS_debug(TYPE, ... /* PREFIX */)               \
-__VA_ARGS__ QDebug &operator<<(QDebug &stream, const TYPE &value);
-
-#endif // QN_SERIALIZATION_DEBUG_FWD_H
+#define QN_FUSION_DECLARE_FUNCTIONS_debug(TYPE, ... /* PREFIX */)   \
+__VA_ARGS__ QDebug &operator<<(QDebug &stream, const TYPE &value);  \
+__VA_ARGS__ void PrintTo(const TYPE& value, ::std::ostream* os);    \
