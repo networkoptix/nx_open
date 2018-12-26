8355f8c6a1c06cca5669fefd195c5b7e4f9e85ba
Dmitry Kargin <dkargin@networkoptix.com>
2018-12-26 17:07:09 +0300

    Fixed compile error for gcc
    Fixes `debug_fwd.h:5:69: error: backslash-newline at end of file [-Werror]`
    
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- libs/nx_fusion/src/nx/fusion/serialization/debug_fwd.h
+++ libs/nx_fusion/src/nx/fusion/serialization/debug_fwd.h
@@ -4,4 +4,4 @@ class QDebug;
 
 #define QN_FUSION_DECLARE_FUNCTIONS_debug(TYPE, ... /* PREFIX */)   \
 __VA_ARGS__ QDebug &operator<<(QDebug &stream, const TYPE &value);  \
-__VA_ARGS__ void PrintTo(const TYPE& value, ::std::ostream* os);    \
+__VA_ARGS__ void PrintTo(const TYPE& value, ::std::ostream* os);
