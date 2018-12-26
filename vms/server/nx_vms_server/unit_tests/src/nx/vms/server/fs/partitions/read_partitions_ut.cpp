70657e23638eba18e788d8f8cad908758a21e638
Alexander Kulikov <akulikov@networkoptix.com>
2018-12-26 14:38:48 +0300

    VMS-11801: More unit tests
    <==== Mercurial data ====>
    Branch: x_vms_11801_partitions_information_refactoring

--- vms/server/nx_vms_server/unit_tests/src/nx/vms/server/fs/partitions/read_partitions_ut.cpp
+++ vms/server/nx_vms_server/unit_tests/src/nx/vms/server/fs/partitions/read_partitions_ut.cpp
@@ -2,6 +2,7 @@
 #include <nx/vms/server/fs/partitions/abstract_partitions_information_provider_linux.h>
 #include <gtest/gtest.h>
 #include <string.h>
+#include <QtCore>
 
 namespace nx::vms::server::fs::test {
 
@@ -15,9 +16,9 @@ void assertDecodedStrings(QByteArray source, const QByteArray& desired)
 
 class TestPartitionsInformationProvider: public AbstractPartitionsInformationProvider
 {
-    virtual QByteArray mountsFileContent() const
-    {
-        return R"(sysfs /sys sysfs rw,nosuid,nodev,noexec,relatime 0 0
+public:
+    TestPartitionsInformationProvider():
+        m_initialMountsFileContent(R"(sysfs /sys sysfs rw,nosuid,nodev,noexec,relatime 0 0
 proc /proc proc rw,nosuid,nodev,noexec,relatime 0 0
 udev /dev devtmpfs rw,nosuid,relatime,size=6680584k,nr_inodes=1670146,mode=755 0 0
 devpts /dev/pts devpts rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000 0 0
@@ -50,7 +51,35 @@ binfmt_misc /proc/sys/fs/binfmt_misc binfmt_misc rw,relatime 0 0
 cgmfs /run/cgmanager/fs tmpfs rw,relatime,size=100k,mode=755 0 0
 tmpfs /run/user/1000 tmpfs rw,nosuid,nodev,relatime,size=1342172k,mode=700,uid=1000,gid=1000 0 0
 gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=1000,group_id=1000 0 0
-/dev/sda1 /etc/hosts ext4 rw,relatime,errors=remount-ro,data=ordered 0 0)";
+/dev/sda1 /etc/hosts ext4 rw,relatime,errors=remount-ro,data=ordered 0 0
+)")
+    {}
+
+    void returnInvalidSpaceForPath(const QList<QString>& subPaths)
+    {
+        m_invalidSpaceSubPaths = subPaths;
+    }
+
+    void replaceMountsLineByPattern(const QByteArray& pattern, const QByteArray& newLine)
+    {
+        QBuffer buf(&m_initialMountsFileContent);
+        buf.open(QIODevice::ReadOnly);
+        QByteArray newContent;
+
+        for (QByteArray line = buf.readLine(); !line.isNull(); line = buf.readLine())
+        {
+            if (line.contains(pattern))
+                newContent.append(newLine + '\n');
+            else
+                newContent.append(line);
+        }
+
+        m_initialMountsFileContent = newContent;
+    }
+
+    virtual QByteArray mountsFileContent() const
+    {
+        return m_initialMountsFileContent;
     }
 
     virtual bool isScanfLongPattern() const
@@ -58,13 +87,17 @@ gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=
         return false;
     }
 
-    virtual qint64 totalSpace(const QByteArray& /*fsPath*/) const
+    virtual qint64 totalSpace(const QByteArray& fsPath) const
     {
+        if (hasInvalidSpaceSubPathMatch(fsPath))
+            return -1;
         return 1;
     }
 
     virtual qint64 freeSpace(const QByteArray& fsPath) const
     {
+        if (hasInvalidSpaceSubPathMatch(fsPath))
+            return -1;
         return 1;
     }
 
@@ -75,6 +108,19 @@ gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=
         return true;
     }
 
+private:
+    QList<QString> m_invalidSpaceSubPaths;
+    QByteArray m_initialMountsFileContent;
+
+    bool hasInvalidSpaceSubPathMatch(const QString& fsPath) const
+    {
+        return std::any_of(m_invalidSpaceSubPaths.cbegin(),
+            m_invalidSpaceSubPaths.cend(),
+            [&fsPath](const QString& subPath)
+            {
+                return fsPath.contains(subPath);
+            });
+    }
 };
 
 } // namespace
@@ -85,7 +131,10 @@ TEST(decodeOctalEncodedPath, variousCases)
     assertDecodedStrings("ab\040cdef", "ab cdef");
     assertDecodedStrings("ab\40cdef", "ab\40cdef");
     assertDecodedStrings("abcdef\0", "abcdef\0");
-    assertDecodedStrings("ab\040", "ab ");
+    assertDecodedStrings("/ab\040cd/ab\040cd/ab", "/ab cd/ab cd/ab");
+    assertDecodedStrings(
+        "/a/long/long/very\040long/with\040spaces",
+        "/a/long/long/very long/with spaces");
 }
 
 class ReadPartitions: public ::testing::Test
@@ -105,9 +154,39 @@ protected:
             }));
 
         ASSERT_TRUE(std::none_of(m_partitionInfoList.cbegin(), m_partitionInfoList.cend(),
+            [](const auto& pInfo) { return pInfo.path.contains("etc"); }));
+    }
+
+    void whenImpossibleToObtainSpaceForSda1Paths()
+    {
+        m_partitionsInformationProvider.returnInvalidSpaceForPath({"hosts", "long/long"});
+    }
+
+    void thenThesePathsShouldBeExcludedFromTheResults()
+    {
+        ASSERT_TRUE(std::none_of(m_partitionInfoList.cbegin(), m_partitionInfoList.cend(),
+            [](const auto& pInfo) { return pInfo.devName == "/dev/sda1"; }));
+
+        ASSERT_TRUE(std::none_of(m_partitionInfoList.cbegin(), m_partitionInfoList.cend(),
             [](const auto& pInfo)
             {
-                return pInfo.path.contains("etc");
+                return pInfo.path.contains("hosts") || pInfo.path.contains("long");
+            }));
+    }
+
+    void whenThereIsASuitablePathWithSpacesOnSda1()
+    {
+        m_partitionsInformationProvider.replaceMountsLineByPattern("long/long",
+            R"(/dev/sda1 /a/long/long/very\040long/with\040spaces ext4 rw,relatime,errors=remount-ro,data=ordered 0 0)");
+    }
+
+    void thenPathWithSpacesShouldBeSelected()
+    {
+        ASSERT_TRUE(std::any_of(m_partitionInfoList.cbegin(), m_partitionInfoList.cend(),
+            [](const auto& pInfo)
+            {
+                return pInfo.devName == "/dev/sda1"
+                    && pInfo.path == "/a/long/long/very long/with spaces";
             }));
     }
 
@@ -124,12 +203,16 @@ TEST_F(ReadPartitions, FilterOutNonFolders)
 
 TEST_F(ReadPartitions, InvalidSpacePathsShouldNotBeReturned)
 {
-
+    whenImpossibleToObtainSpaceForSda1Paths();
+    whenProcMountsContainingBoundFilesRead();
+    thenThesePathsShouldBeExcludedFromTheResults();
 }
 
 TEST_F(ReadPartitions, PathsWithSpacesShouldBeReturned)
 {
-
+    whenThereIsASuitablePathWithSpacesOnSda1();
+    whenProcMountsContainingBoundFilesRead();
+    thenPathWithSpacesShouldBeSelected();
 }
 
 } // namespace nx::vms::server::fs::test
