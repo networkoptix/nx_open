c94fb64a44135d7ed06814f20904d9420a0f3dc2
Victor Breus <vbreus@networkoptix.com>
2019-01-09 20:32:19 +0300

    VMS-12673: Improved IP address range autofill in Device Add dialog
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/manual_device_addition/dialog/device_addition_dialog.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/manual_device_addition/dialog/device_addition_dialog.cpp
@@ -215,8 +215,12 @@ void DeviceAdditionDialog::handleStartAddressFieldTextChanged(const QString& val
 void DeviceAdditionDialog::handleStartAddressEditingFinished()
 {
     const auto startAddress = fixedAddress(ui->startAddressEdit).toIPv4Address();
-    if (startAddress % 256 == 0)
-        ui->endAddressEdit->setText(QHostAddress(startAddress + 255).toString());
+    const auto endAddress = fixedAddress(ui->endAddressEdit).toIPv4Address();
+    if (!ui->endAddressEdit->isModified() && (startAddress / 256) != (endAddress / 256))
+    {
+        ui->endAddressEdit->setText(
+            QHostAddress(startAddress - startAddress % 256 + 255).toString());
+    }
 }
 
 void DeviceAdditionDialog::handleEndAddressFieldTextChanged(const QString& value)
