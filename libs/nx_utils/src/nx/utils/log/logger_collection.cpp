571695a3f0f0bb419b2e028af18b511b6518e7e5
Nate Wilson <nwilson@networkoptix.com>
2019-01-16 17:18:06 -0800

    VMS-12792: Exposed logger collection
    VMS-12793: Added auto-generated id to every logger instance
    
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ libs/nx_utils/src/nx/utils/log/logger_collection.cpp
@@ -0,0 +1,172 @@
+#include "logger_collection.h"
+
+namespace nx {
+namespace utils {
+namespace log {
+
+LoggerCollection::LoggerCollection()
+{
+    m_mainLogger = std::make_unique<Logger>(std::set<Tag>(), kDefaultLevel);
+    m_mainLogger->setOnLevelChanged([this]() { onLevelChanged(); });
+    updateMaxLevel();
+}
+
+LoggerCollection * LoggerCollection::instance()
+{
+    static LoggerCollection collection;
+    return &collection;
+}
+
+std::shared_ptr<AbstractLogger> LoggerCollection::main()
+{
+    QnMutexLocker lock(&m_mutex);
+    return m_mainLogger;
+}
+
+void LoggerCollection::setMainLogger(std::unique_ptr<AbstractLogger> logger)
+{
+    if (!logger)
+        return;
+
+    logger->writeLogHeader();
+
+    QnMutexLocker lock(&m_mutex);
+    m_mainLogger = std::move(logger);
+    m_mainLogger->setOnLevelChanged([this]() { onLevelChanged(); });
+
+    updateMaxLevel();
+}
+
+int LoggerCollection::add(std::unique_ptr<AbstractLogger> logger)
+{
+    if (!logger)
+        return invalidId;
+
+    QnMutexLocker lock(&m_mutex);
+
+    std::shared_ptr<AbstractLogger> sharedLogger(std::move(logger));
+
+    sharedLogger->setOnLevelChanged([this]() { onLevelChanged(); });
+
+    Context loggerContext(m_loggerId, sharedLogger);
+
+    bool inserted = false;
+    for (const auto& tag : sharedLogger->tags())
+    {
+        if (m_loggersByTags.emplace(tag, loggerContext).second)
+            inserted = true;
+    }
+
+    updateMaxLevel();
+
+    if (inserted)
+    {
+        ++m_loggerId;
+        return loggerContext.id;
+    }
+
+    return invalidId;
+}
+
+std::shared_ptr<AbstractLogger> LoggerCollection::get(const Tag& tag, bool exactMatch) const
+{
+    QnMutexLocker lock(&m_mutex);
+    if (exactMatch)
+    {
+        const auto it = m_loggersByTags.find(tag);
+        return (it == m_loggersByTags.end()) ? nullptr : it->second.logger;
+    }
+
+    for (const auto& it : m_loggersByTags)
+    {
+        if (tag.matches(it.first))
+            return it.second.logger;
+    }
+
+    return m_mainLogger;
+}
+
+std::shared_ptr<AbstractLogger> LoggerCollection::get(int loggerId) const
+{
+    QnMutexLocker lock(&m_mutex);
+    return getInternal(loggerId);
+}
+
+std::set<Tag> LoggerCollection::getEffectiveTags(int loggerId) const
+{
+    QnMutexLocker lock(&m_mutex);
+    auto logger = getInternal(loggerId);
+    if (!logger)
+        return std::set<Tag>();
+
+    std::set<Tag> effectiveTags;
+
+    for (const auto& tag : logger->tags())
+    {
+        auto it = m_loggersByTags.find(tag);
+        if (it != m_loggersByTags.end() && it->second.logger == logger)
+            effectiveTags.emplace(tag);
+    }
+
+    return effectiveTags;
+}
+
+LoggerCollection::LoggersByTag LoggerCollection::allLoggers() const
+{
+    QnMutexLocker lock(&m_mutex);
+    return m_loggersByTags;
+}
+
+void LoggerCollection::remove(const std::set<Tag>& filters)
+{
+    QnMutexLocker lock(&m_mutex);
+    for (const auto& f: filters)
+        m_loggersByTags.erase(f);
+
+    updateMaxLevel();
+}
+
+void LoggerCollection::remove(int id)
+{
+    QnMutexLocker lock(&m_mutex);
+    for (auto it = m_loggersByTags.begin(); it != m_loggersByTags.end();)
+    {
+        if (it->second.id == id)
+            it = m_loggersByTags.erase(it);
+        else
+            ++it;
+    }
+}
+
+Level LoggerCollection::maxLevel() const
+{
+    return m_maxLevel;
+}
+
+void LoggerCollection::updateMaxLevel()
+{
+    m_maxLevel = m_mainLogger->maxLevel();
+    for (const auto& element: m_loggersByTags)
+        m_maxLevel = std::max(element.second.logger->maxLevel(), m_maxLevel.load());
+}
+
+void LoggerCollection::onLevelChanged()
+{
+    QnMutexLocker lock(&m_mutex);
+    updateMaxLevel();
+}
+
+std::shared_ptr<AbstractLogger> LoggerCollection::getInternal(int loggerId) const
+{
+    for (const auto& element : m_loggersByTags)
+    {
+        if (element.second.id == loggerId)
+            return element.second.logger;
+    }
+
+    return nullptr;
+}
+
+} // namespace nx
+} // namespace utils
+} // namespace log
---
+++ libs/nx_utils/src/nx/utils/log/logger_collection.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "log_logger.h"
+
+#include <nx/utils/system_error.h>
+
+namespace nx {
+namespace utils {
+namespace log {
+
+class NX_UTILS_API LoggerCollection
+{
+public:
+    struct Context
+    {
+        int id;
+        std::shared_ptr<AbstractLogger> logger;
+
+        Context(int id, const std::shared_ptr<AbstractLogger>& logger):
+            id(id),
+            logger(logger)
+        {
+        }
+
+        bool operator <(const Context& rhs) const
+        {
+            return id < rhs.id;
+        }
+    };
+
+    using LoggersByTag = std::map<Tag, Context>;
+
+    static constexpr int invalidId = -1;
+
+public:
+    LoggerCollection();
+    ~LoggerCollection() = default;
+
+    static LoggerCollection * instance();
+
+    std::shared_ptr<AbstractLogger> main();
+
+    void setMainLogger(std::unique_ptr<AbstractLogger> logger);
+
+    /**
+     * Add a logger to the collection. 
+     * If this logger has a tag that is already associated with another logger, the existing
+     *    logger/tag association is not overwritten and the new logger will not be associated with
+     *    the tag. To get the tags associated with a given logger, see getEffectiveTags().
+     *
+     * @return - The logger id if added successfully (at least one tag was not already taken), 
+     * -1 otherwise.
+     */
+    int add(std::unique_ptr<AbstractLogger> logger);
+
+    std::shared_ptr<AbstractLogger> get(const Tag& tag, bool exactMatch) const;
+
+    std::shared_ptr<AbstractLogger> get(int loggerId) const;
+
+    /**
+     * Get the effective tags for a logger with the given id.
+     * If multiple loggers are added with the same tag, only the first logger added is associated 
+     * with the tag.
+     */
+    std::set<Tag> getEffectiveTags(int loggerId) const;
+
+    /*
+    * Get all the loggers in this collection.
+    * If a logger has multiple tags associated with it, the data structure will contain the same 
+    * logger multiple times. there are as many loggers as there are tags associated with it.
+    */
+    LoggersByTag allLoggers() const;
+
+    void remove(const std::set<Tag>& filters);
+
+    void remove(int id);
+
+    Level maxLevel() const;
+
+private:
+    void updateMaxLevel();
+
+    void onLevelChanged();
+
+    std::shared_ptr<AbstractLogger> getInternal(int loggerId) const;
+
+private:
+    mutable QnMutex m_mutex;
+    std::shared_ptr<AbstractLogger> m_mainLogger;
+    //std::map<Tag, std::shared_ptr<AbstractLogger>> m_loggersByTags;
+    std::map<Tag, Context> m_loggersByTags;
+    std::atomic<Level> m_maxLevel{Level::none};
+    int m_loggerId = 0;
+};
+
+} // namespace log
+} // namespace utils
+} // namespace nx
