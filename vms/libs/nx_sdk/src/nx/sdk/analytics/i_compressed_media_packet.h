8d971b1fcda9ed4f726199a530cf8fa048043625
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-01-21 20:03:34 +0300

    analytics_sdk: Improved handling enum-based flags (currently used only in ICompressedMediaPacket)
    <==== Mercurial data ====>
    Branch: meta_4.0

--- vms/libs/nx_sdk/src/nx/sdk/analytics/i_compressed_media_packet.h
+++ vms/libs/nx_sdk/src/nx/sdk/analytics/i_compressed_media_packet.h
@@ -11,13 +11,6 @@ namespace nx {
 namespace sdk {
 namespace analytics {
 
-using MediaFlags = uint64_t;
-
-enum class MediaFlag: MediaFlags
-{
-    keyFrame = 1 << 0,
-};
-
 /** Each class implementing ICompressedMediaPacket should handle this GUID in queryInterface(). */
 static const nxpl::NX_GUID IID_CompressedMediaPacket =
     {{0xf9,0xa4,0x59,0x8b,0xd7,0x18,0x42,0x29,0x98,0xdd,0xff,0xe5,0x41,0x28,0xfa,0xf8}};
@@ -48,6 +41,13 @@ public:
      * @return Pointer to the codec context.
      */
     virtual const IMediaContext* context() const = 0;
+
+    enum class MediaFlags: uint32_t
+    {
+        none = 0,
+        keyFrame = 1 << 0,
+        all = UINT32_MAX
+    };
 
     /**
      * @return Packet media flags.
