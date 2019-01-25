c3eb28953d4cde606d187bbbe1e825a5a344647b
Nate Wilson <nwilson@networkoptix.com>
2019-01-25 15:23:51 -0800

    NXLIB-67: Http server: make it possible to associate authentication manager with some requests only
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ libs/nx_network/src/nx/network/http/server/authentication_dispatcher.cpp
@@ -0,0 +1,39 @@
+#include "authentication_dispatcher.h"
+
+namespace nx::network::http::server {
+
+void AuthenticationDispatcher::add(
+    std::regex& pathPattern,
+    AbstractAuthenticationManager* authenticator)
+{
+    QnMutexLocker lock(&m_mutex);
+    m_authenticatorsByRegex.emplace_back(pathPattern, authenticator);
+}
+
+void AuthenticationDispatcher::authenticate(
+    const nx::network::http::HttpServerConnection& connection,
+    const nx::network::http::Request& request,
+    AuthenticationCompletionHandler completionHandler)
+{
+    AbstractAuthenticationManager * manager = nullptr;
+    std::string path = request.requestLine.url.path().toStdString();
+
+    {
+        QnMutexLocker lock(&m_mutex);
+        for (const auto& element : m_authenticatorsByRegex)
+        {
+            if (std::regex_search(path, element.first))
+            {
+                manager = element.second;
+                break;
+            }
+        }
+    }
+
+    if (manager)
+        manager->authenticate(connection, request, std::move(completionHandler));
+    else
+        completionHandler(SuccessfulAuthenticationResult());
+}
+
+} // namespace nx::network::http::server
