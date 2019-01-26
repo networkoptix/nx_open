97361588caed904b7f44fdb8305be6bffbab6ffd
Nick Hartleb <nhartleb@networkoptix.com>
2019-01-25 17:28:20 -0800

    CLOUD-1524: Cloud merge - interface
    <==== Mercurial data ====>
    Branch: cloud_18.3

--- cloud_portal/front_end/app/scripts/controllers/system.js
+++ cloud_portal/front_end/app/scripts/controllers/system.js
@@ -39,7 +39,7 @@ angular.module('cloudApp')
             }
 
             function setMergeStatus(mergeInfo) {
-                if (Object.keys(mergeInfo).length === 0) {
+                if (!mergeInfo || Object.keys(mergeInfo).length === 0) {
                     return;
                 }
                 $scope.currentlyMerging = true;
