c620454c230a62702055508b864afcffb193579e
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-02-07 10:34:09 -0800

    Removed excessive #include
    <==== Mercurial data ====>
    Branch: cloud_backend_18.3.1

--- libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_client_factory.cpp
+++ libs/nx_network/src/nx/network/cloud/tunnel/relay/api/relay_api_client_factory.cpp
@@ -1,15 +1,9 @@
-#include <algorithm>
-
 #include "relay_api_client_factory.h"
 
-#include <nx/network/cloud/cloud_connect_controller.h>
-#include <nx/network/cloud/cloud_connect_settings.h>
-#include <nx/network/socket_global.h>
+#include <algorithm>
+
 #include <nx/utils/std/algorithm.h>
 
-#include "relay_api_client_over_http_connect.h"
-#include "relay_api_client_over_http_get_post_tunnel.h"
-#include "relay_api_client_over_http_upgrade.h"
 #include "relay_api_client_over_http_tunnel.h"
 
 namespace nx::cloud::relay::api {
