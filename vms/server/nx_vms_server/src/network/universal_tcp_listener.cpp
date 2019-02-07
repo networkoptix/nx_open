757f9dce06a818fb392bf5b2f4742cf3c488671d
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-02-07 17:12:34 +0300

    Blinking UT fix: server has crashed on stop.
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/server/nx_vms_server/src/network/universal_tcp_listener.cpp
+++ vms/server/nx_vms_server/src/network/universal_tcp_listener.cpp
@@ -67,7 +67,7 @@ QnUniversalTcpListener::~QnUniversalTcpListener()
 void QnUniversalTcpListener::stop()
 {
     directDisconnectAll();
-    stop();
+    QnHttpConnectionListener::stop();
 }
 
 QnTCPConnectionProcessor* QnUniversalTcpListener::createRequestProcessor(
