d2a38339d96abe74e51342f2574d35a5f13e9f00
Sergey Ivanov <sivanov@networkoptix.com>
2019-02-11 14:22:08 +0300

    VMS-11099: Show warning in backup schedule if "Finish" time is less that "Start"
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/src/ui/dialogs/backup_schedule_dialog.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/dialogs/backup_schedule_dialog.cpp
@@ -20,34 +20,43 @@
 using namespace nx::vms;
 
 namespace {
-    const int secsPerDay = 3600 * 24;
-    const int daysPerWeek = 7;
-    const int bitsPerMegabit = 1024*1024;
-    const int bitsPerByte = 8;
 
-    Qt::DayOfWeek nextDay(Qt::DayOfWeek day) {
-        static_assert(Qt::Sunday == 7, "Make sure Sunday is the last day");
-        if (day == Qt::Sunday)
-            return Qt::Monday;
-        return static_cast<Qt::DayOfWeek>(day + 1);
-    }
+const int kSecsPerDay = 3600 * 24;
+const int kDaysPerWeek = 7;
+const int kBitsPerMegabit = 1024 * 1024;
+const int kBitsPerByte = 8;
 
-    int bytesToMBits(int bytes) {
-        return bytes * bitsPerByte / bitsPerMegabit;
-    }
+const int kUntilFinished = -1;
 
-    int mBitsToBytes(int mBits) {
-        return mBits * bitsPerMegabit / bitsPerByte;
-    }
-
-    size_t dayToIndex(Qt::DayOfWeek day) {
-        return static_cast<size_t>(day) - 1;
-    }
-
-    Qt::DayOfWeek indexToDay(size_t index) {
-        return static_cast<Qt::DayOfWeek>(index + 1);
-    }
+Qt::DayOfWeek nextDay(Qt::DayOfWeek day)
+{
+    static_assert(Qt::Sunday == 7, "Make sure Sunday is the last day");
+    if (day == Qt::Sunday)
+        return Qt::Monday;
+    return static_cast<Qt::DayOfWeek>(day + 1);
 }
+
+int bytesToMBits(int bytes)
+{
+    return bytes * kBitsPerByte / kBitsPerMegabit;
+}
+
+int mBitsToBytes(int mBits)
+{
+    return mBits * kBitsPerMegabit / kBitsPerByte;
+}
+
+size_t dayToIndex(Qt::DayOfWeek day)
+{
+    return static_cast<size_t>(day) - 1;
+}
+
+Qt::DayOfWeek indexToDay(size_t index)
+{
+    return static_cast<Qt::DayOfWeek>(index + 1);
+}
+
+} // namespace
 
 QnBackupScheduleDialog::QnBackupScheduleDialog(QWidget *parent):
     base_type(parent),
@@ -57,7 +66,7 @@ QnBackupScheduleDialog::QnBackupScheduleDialog(QWidget *parent):
 
     initDayOfWeekCheckboxes();
 
-    ui->comboBoxTimeTo->addItem(tr("Until finished"), -1);
+    ui->comboBoxTimeTo->addItem(tr("Until finished"), kUntilFinished);
     for (int i = 0; i < 24; ++i)
     {
         QString hour = datetime::toString(QTime(i, 0, 0), datetime::Format::hh);
@@ -79,6 +88,12 @@ QnBackupScheduleDialog::QnBackupScheduleDialog(QWidget *parent):
     setWarningStyle(ui->warningLabel);
 
     connect(ui->limitBandwithCheckBox, &QCheckBox::stateChanged, this, updateLimitControls);
+
+    connect(ui->comboBoxTimeStart, QnComboboxCurrentIndexChanged, this,
+		&QnBackupScheduleDialog::updateNextDayLabel);
+    connect(ui->comboBoxTimeTo, QnComboboxCurrentIndexChanged, this,
+		&QnBackupScheduleDialog::updateNextDayLabel);
+
     updateLimitControls();
 }
 
@@ -105,7 +120,7 @@ void QnBackupScheduleDialog::initDayOfWeekCheckboxes() {
     nameFont.setCapitalization(QFont::Capitalize);
 
     Qt::DayOfWeek day = locale.firstDayOfWeek();
-    for (int i = 0; i < daysPerWeek; ++i) {
+    for (int i = 0; i < kDaysPerWeek; ++i) {
         QCheckBox* checkbox = new QCheckBox(this);
 
         /* Adding checkboxes to widget in the locale order */
@@ -136,6 +151,16 @@ void QnBackupScheduleDialog::updateDayOfWeekCheckboxes()
     }
 }
 
+void QnBackupScheduleDialog::updateNextDayLabel()
+{
+    const int backupStartSec =
+		ui->comboBoxTimeStart->itemData(ui->comboBoxTimeStart->currentIndex()).toInt();
+    const int backupEnd = ui->comboBoxTimeTo->itemData(ui->comboBoxTimeTo->currentIndex()).toInt();
+
+    const bool showHint = (backupEnd != kUntilFinished && backupEnd <= backupStartSec);
+    ui->finishNextDayLabel->setVisible(showHint);
+}
+
 void QnBackupScheduleDialog::setNearestValue(QComboBox* combobox, int time)
 {
     int bestIndex = -1;
@@ -162,14 +187,15 @@ void QnBackupScheduleDialog::updateFromSettings(const QnServerBackupSchedule& va
     }
 
     setNearestValue(ui->comboBoxTimeStart, value.backupStartSec);
-    if (value.backupDurationSec == -1)
+    if (value.backupDurationSec == kUntilFinished)
         setNearestValue(ui->comboBoxTimeTo, value.backupDurationSec);
     else
-        setNearestValue(ui->comboBoxTimeTo, (value.backupStartSec + value.backupDurationSec) % secsPerDay);
+        setNearestValue(ui->comboBoxTimeTo, (value.backupStartSec + value.backupDurationSec) % kSecsPerDay);
 
     ui->spinBoxBandwidth->setValue(qAbs(bytesToMBits(value.backupBitrate)));
 
     ui->limitBandwithCheckBox->setChecked(value.backupBitrate > 0);
+    updateNextDayLabel();
 }
 
 void QnBackupScheduleDialog::submitToSettings(QnServerBackupSchedule& value)
@@ -185,14 +211,17 @@ void QnBackupScheduleDialog::submitToSettings(QnServerBackupSchedule& value)
 
     value.backupDaysOfTheWeek = api::daysOfWeek(days);
 
-    value.backupStartSec = ui->comboBoxTimeStart->itemData(ui->comboBoxTimeStart->currentIndex()).toInt();
+    value.backupStartSec =
+		ui->comboBoxTimeStart->itemData(ui->comboBoxTimeStart->currentIndex()).toInt();
     int backupEnd = ui->comboBoxTimeTo->itemData(ui->comboBoxTimeTo->currentIndex()).toInt();
-    if (backupEnd == -1) {
+    if (backupEnd == kUntilFinished)
+    {
         value.backupDurationSec = backupEnd;
     }
-    else {
-        if (backupEnd < value.backupStartSec)
-            backupEnd += secsPerDay;
+    else
+    {
+        if (backupEnd <= value.backupStartSec)
+            backupEnd += kSecsPerDay;
         value.backupDurationSec = backupEnd - value.backupStartSec;
     }
 
