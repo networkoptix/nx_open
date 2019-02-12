595769e1c20f514ac8373c7d758df2502a722aaa
Sergey Ivanov <sivanov@networkoptix.com>
2019-02-12 16:10:12 +0300

    VMS-13165: Always use the current server while testing LDAP settings
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/src/ui/dialogs/ldap_settings_dialog.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/dialogs/ldap_settings_dialog.cpp
@@ -81,7 +81,10 @@ void QnLdapSettingsDialogPrivate::testSettings()
 
     const auto server = commonModule()->currentServer();
     if (!NX_ASSERT(server))
+    {
+        stopTesting(tr("Could not perform a test."));
         return;
+    }
 
     Q_Q(QnLdapSettingsDialog);
 
