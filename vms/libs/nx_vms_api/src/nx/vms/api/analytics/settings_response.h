8c6ae92c6e7c759d6c9abbc9e3d6504423a48b32
Dmitry Mishin <dmishin@networkoptix.com>
2019-02-12 20:39:22 +0300

    META-125: Introduced a structure for analytics settings REST handlers response
    <==== Mercurial data ====>
    Branch: meta_4.0

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/settings_response.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QtCore/QJsonObject>
+
+#include <nx/fusion/model_functions_fwd.h>
+
+namespace nx::vms::api::analytics {
+
+struct SettingsResponse
+{
+    QJsonObject values;
+    QJsonObject model;
+};
+
+#define nx_vms_api_analytics_SettingsResponse_Fields (values)(model)
+
+QN_FUSION_DECLARE_FUNCTIONS(SettingsResponse, (json)(eq), NX_VMS_API)
+
+} // namespace nx::vms::server::rest
