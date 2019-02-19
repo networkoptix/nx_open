654a19d0d2a11930ac8ca4441608d35a9c4f0626
Nate Wilson <nwilson@networkoptix.com>
2019-02-19 12:35:41 -0800

    CLOUD-2681: Implement DiscoveryClient class, unit tests to follow
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/discovery_api_client.cpp
+++ cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/discovery_api_client.cpp
@@ -1,15 +1,108 @@
 #include "discovery_api_client.h"
 
+#include <nx/utils/timer_holder.h>
 #include <nx/fusion/model_functions.h>
+#include <nx/network/url/url_builder.h>
+#include <nx/network/http/http_types.h>
+#include <nx/network/http/buffer_source.h>
+
+#include "request_paths.h"
 
 namespace nx::cloud::discovery {
 
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS_FOR_TYPES(
-    (Node),
+    (NodeInfo)(Node),
     (json),
     _Fields)
 
 //-------------------------------------------------------------------------------------------------
+// RequestContext
+
+namespace {
+
+std::set<Node> moveToSet(const std::vector<Node>& nodes)
+{
+    std::set<Node> nodeSet;
+    for (const auto& node : nodes)
+        nodeSet.emplace(std::move(node));
+    return nodeSet;
+}
+
+std::vector<Node> toVector(const std::set<Node>& nodes)
+{
+    std::vector<Node> nodeList;
+    for (const auto& node : nodes)
+        nodeList.emplace_back(node);
+    return nodeList;
+}
+
+} //namespace
+
+DiscoveryClient::RequestContext::RequestContext(
+    ResponseReceivedHandler responseReceived)
+{
+    m_httpClient.setOnSomeMessageBodyAvailable([this]() { updateMessageBody(); });
+
+    m_httpClient.setOnDone(
+        [this, responseReceived = std::move(responseReceived)]()
+        {
+            updateMessageBody();
+            responseReceived(m_messageBody);
+        });
+}
+
+void DiscoveryClient::RequestContext::bindToAioThread(
+    nx::network::aio::AbstractAioThread* aioThread)
+{
+    m_httpClient.bindToAioThread(aioThread);
+}
+
+void DiscoveryClient::RequestContext::stopWhileInAioThread()
+{
+    m_timer.cancelSync();
+    m_httpClient.pleaseStopSync();
+}
+
+void DiscoveryClient::RequestContext::clearMessageBody()
+{
+    m_messageBody.clear();
+}
+
+void DiscoveryClient::RequestContext::updateMessageBody()
+{
+    m_messageBody += m_httpClient.fetchMessageBodyBuffer();
+}
+
+void DiscoveryClient::RequestContext::start(
+    nx::network::aio::TimerEventHandler timerEvent)
+{
+    m_timer.start(std::chrono::milliseconds(1000), std::move(timerEvent));
+}
+
+void DiscoveryClient::RequestContext::stop()
+{
+    m_timer.cancelSync();
+}
+
+void DiscoveryClient::RequestContext::doGet(const nx::utils::Url & url)
+{
+    clearMessageBody();
+    m_httpClient.doGet(url);
+}
+
+void DiscoveryClient::RequestContext::doPost(
+    const nx::utils::Url& url,
+    const QByteArray& messageBody)
+{
+    clearMessageBody();
+
+    m_httpClient.setRequestBody(
+        std::make_unique<nx::network::http::BufferSource>("application/json", messageBody));
+    m_httpClient.doPost(url);
+}
+
+//-------------------------------------------------------------------------------------------------
+// DiscoveryClient
 
 DiscoveryClient::DiscoveryClient(
     const nx::utils::Url& discoveryServiceUrl,
@@ -17,30 +110,54 @@ DiscoveryClient::DiscoveryClient(
     const std::string& nodeId,
     const std::string& infoJson)
     :
-    m_discoveryServiceUrl(discoveryServiceUrl),
-    m_clusterId(clusterId)
+    m_discoveryServiceUrl(discoveryServiceUrl)
 {
-    m_node.nodeId = nodeId;
-    m_node.infoJson = infoJson;
+    m_thisNodeInfo.nodeId = nodeId;
+    m_thisNodeInfo.infoJson = infoJson;
+
+    setupDiscoveryServiceUrl(clusterId);
+}
+
+DiscoveryClient::~DiscoveryClient()
+{
+    stopWhileInAioThread();
+}
+
+void DiscoveryClient::bindToAioThread(nx::network::aio::AbstractAioThread * aioThread)
+{
+    BaseType::bindToAioThread(aioThread);
+
+    if (m_registerNodeRequest)
+        m_registerNodeRequest->bindToAioThread(getAioThread());
+
+    if (m_onlineNodesRequest)
+        m_onlineNodesRequest->bindToAioThread(getAioThread());
+}
+
+void DiscoveryClient::stopWhileInAioThread()
+{
+    BaseType::stopWhileInAioThread();
+    m_registerNodeRequest->stopWhileInAioThread();
+    m_onlineNodesRequest->stopWhileInAioThread();
 }
 
 void DiscoveryClient::start()
 {
-    m_httpClient.doPost(
-        m_discoveryServiceUrl,
-        "application/json",
-        QJson::serialized<Node>(m_node));
+    startRegisterNode();
+    startOnlineNodesRequest();
 }
 
 void DiscoveryClient::updateInformation(const std::string& infoJson)
 {
-    m_node.infoJson = infoJson;
-    start();
+    QnMutexLocker lock(&m_mutex);
+    m_thisNodeInfo.infoJson = infoJson;
+    startRegisterNode();
 }
 
 std::vector<Node> DiscoveryClient::onlineNodes() const
 {
-    return std::vector<Node>();
+    QnMutexLocker lock(&m_mutex);
+    return toVector(m_onlineNodes);
 }
 
 void DiscoveryClient::setOnNodeDiscovered(NodeDiscoveredHandler handler)
@@ -53,4 +170,123 @@ void DiscoveryClient::setOnNodeLost(NodeLostHandler handler)
     m_nodeLostHandler = std::move(handler);
 }
 
-} //namespace nx::cloud::discovery
+void DiscoveryClient::setupDiscoveryServiceUrl(const std::string& clusterId)
+{
+    QString nodeRegistryPath(kRegisterNodePath);
+    nodeRegistryPath.replace(kClusterIdTemplate, clusterId.c_str());
+    m_discoveryServiceUrl.setPath(nodeRegistryPath);
+}
+
+void DiscoveryClient::startRegisterNode()
+{
+    const auto startRegisterNodeRequest =
+        [this]()
+        {
+            m_registerNodeRequest->start(
+                [this]()
+                {
+                    m_registerNodeRequest->doPost(
+                        m_discoveryServiceUrl,
+                        QJson::serialized(m_thisNodeInfo));
+                });
+        };
+
+    if (!m_registerNodeRequest)
+    {
+        m_registerNodeRequest = std::make_unique<RequestContext>(
+            [this, startRegisterNodeRequest](QByteArray messageBody)
+            {
+                m_registerNodeRequest->stop();
+
+                bool ok = false;
+                Node node = QJson::deserialized(messageBody, Node(), &ok);
+                if (ok)
+                {
+                    QnMutexLocker lock(&m_mutex);
+                    m_thisNode = std::move(node);
+                }
+                else
+                {
+                    NX_ERROR(this, lm("Error deserializing register node response"));
+                }
+
+                startRegisterNodeRequest();
+            });
+        m_registerNodeRequest->bindToAioThread(getAioThread());
+    }
+
+    startRegisterNodeRequest();
+}
+
+void DiscoveryClient::startOnlineNodesRequest()
+{
+    const auto startOnlineNodesRequest =
+        [this]()
+        {
+            m_onlineNodesRequest->start(
+                [this]()
+                {
+                    m_onlineNodesRequest->doGet(m_discoveryServiceUrl);
+                });
+        };
+
+    if (!m_onlineNodesRequest)
+    {
+        m_onlineNodesRequest = std::make_unique<RequestContext>(
+            [this, startOnlineNodesRequest](QByteArray messageBody)
+            {
+                m_onlineNodesRequest->stop();
+
+                bool ok = false;
+                std::vector<Node> onlineNodes =
+                    QJson::deserialized(messageBody, std::vector<Node>(), &ok);
+                if (ok)
+                    updateOnlineNodes(onlineNodes);
+                else
+                    NX_ERROR(this, lm("Error deserializing online nodes response"));
+
+                startOnlineNodesRequest();
+            });
+        m_onlineNodesRequest->bindToAioThread(getAioThread());
+    }
+
+    startOnlineNodesRequest();
+}
+
+void DiscoveryClient::updateOnlineNodes(const std::vector<Node>& onlineNodes)
+{
+    auto onlineNodeSet = moveToSet(onlineNodes);
+
+    // Nodes that are not present in onlineNodeSet and not m_onlineNodes are newly online.
+    for (const auto& node : onlineNodeSet)
+    {
+        if (m_onlineNodes.find(node) == m_onlineNodes.end())
+            emitNodeDiscovered(node);
+    }
+
+    // Nodes that are present in m_onlineNodes and not in onlineNodeSet have been lost.
+    for (const auto& node : m_onlineNodes)
+    {
+        if (onlineNodeSet.find(node) == onlineNodeSet.end())
+            emitNodeLost(node.nodeId);
+    }
+
+    {
+        QnMutexLocker lock(&m_mutex);
+        m_onlineNodes = std::move(onlineNodeSet);
+    }
+}
+
+void DiscoveryClient::emitNodeDiscovered(const Node& node)
+{
+    if (m_nodeDiscoveredHandler)
+        m_nodeDiscoveredHandler(node);
+}
+
+void DiscoveryClient::emitNodeLost(const std::string& nodeId)
+{
+    if (m_nodeLostHandler)
+        m_nodeLostHandler(nodeId);
+}
+
+} // namespace nx::cloud::discovery
--- cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/discovery_api_client.h
+++ cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/discovery_api_client.h
@@ -2,43 +2,58 @@
 
 #include <nx/network/aio/basic_pollable.h>
 
+#include <set>
+#include <vector>
 #include <string>
 
 #include <nx/utils/url.h>
 #include <nx/utils/move_only_func.h>
 #include <nx/fusion/model_functions_fwd.h>
-#include <nx/network/http/http_client.h>
+#include <nx/network/http/http_async_client.h>
 
-#include <nx/utils/timer_holder.h>
-#include <nx/utils/timer_manager.h>
+#include <nx/network/aio/timer.h>
 
 namespace nx::cloud::discovery {
 
+struct NodeInfo
+{
+    std::string nodeId;
+    /* The object under "info" key */
+    std::string infoJson;
+};
+
+#define NodeInfo_Fields (nodeId)(infoJson)
+
 struct Node
 {
-    enum Status
-    {
-        online,
-        offline
-    };
-
     std::string nodeId;
     std::string host;
     /* http time format */
-    std::string expirationTime;
+    std::chrono::system_clock::time_point expirationTime;
     /* The object under "info" key */
     std::string infoJson;
+
+    bool operator==(const Node& right) const
+    {
+        // TODO do other fields need to be compared? they are volatile.
+        return nodeId == right.nodeId;
+    }
+
+    bool operator<(const Node& right)const
+    {
+        return nodeId < right.nodeId;
+    }
 };
 
 #define Node_Fields (nodeId)(host)(expirationTime)(infoJson)
 
 QN_FUSION_DECLARE_FUNCTIONS_FOR_TYPES(
-    (Node),
+    (NodeInfo)(Node),
     (json))
 
 //-------------------------------------------------------------------------------------------------
 
-using NodeDiscoveredHandler = nx::utils::MoveOnlyFunc<void(Node, Node::Status)>;
+using NodeDiscoveredHandler = nx::utils::MoveOnlyFunc<void(Node)>;
 
 using NodeLostHandler = nx::utils::MoveOnlyFunc<void(std::string /*nodeId*/)>;
 
@@ -47,15 +62,22 @@ using NodeLostHandler = nx::utils::MoveOnlyFunc<void(std::string /*nodeId*/)>;
   * Situations, where we can't reach the discovery service, are handled by this class too.
   * NOTE: Every method is non-blocking.
   */
-class DiscoveryClient:
+class NX_DISCOVERY_CLIENT_API DiscoveryClient:
     public nx::network::aio::BasicPollable
 {
+    using BaseType = nx::network::aio::BasicPollable;
+
 public:
     DiscoveryClient(
         const nx::utils::Url& discoveryServiceUrl,
         const std::string& clusterId,
         const std::string& nodeId,
         const std::string& infoJson);
+    ~DiscoveryClient();
+
+    virtual void bindToAioThread(nx::network::aio::AbstractAioThread* aioThread) override;
+    virtual void stopWhileInAioThread() override;
+
 
     /** Starts the process of registering node in the discovery service. */
     void start();
@@ -71,14 +93,55 @@ public:
     void setOnNodeLost(NodeLostHandler handler);
 
 private:
+    void setupDiscoveryServiceUrl(const std::string& clusterId);
+
+    void startRegisterNode();
+    void startOnlineNodesRequest();
+
+    void updateOnlineNodes(const std::vector<Node>& discoveredNodes);
+
+    void emitNodeDiscovered(const Node& node);
+    void emitNodeLost(const std::string& nodeId);
+
+private:
+    using ResponseReceivedHandler = nx::utils::MoveOnlyFunc<void(QByteArray messageBody)>;
+    struct RequestContext
+    {
+        RequestContext(ResponseReceivedHandler responseReceived);
+
+        void bindToAioThread(
+            nx::network::aio::AbstractAioThread* aioThread);
+        void stopWhileInAioThread();
+
+        void start(nx::network::aio::TimerEventHandler timerEvent);
+        void stop();
+
+        void doGet(const nx::utils::Url& url);
+        void doPost(const nx::utils::Url& url, const QByteArray& messagBody);
+
+    private:
+        void clearMessageBody();
+        void updateMessageBody();
+
+    private:
+        QByteArray m_messageBody;
+        nx::network::http::AsyncClient m_httpClient;
+        nx::network::aio::Timer m_timer;
+    };
+
     nx::utils::Url m_discoveryServiceUrl;
-    std::string m_clusterId;
-    Node m_node;
+    Node m_thisNode;
+    Node m_thisNodeInfo;
 
     NodeDiscoveredHandler m_nodeDiscoveredHandler;
     NodeLostHandler m_nodeLostHandler;
 
-    nx::network::http::HttpClient m_httpClient;
+    std::unique_ptr<RequestContext> m_registerNodeRequest;
+    std::unique_ptr<RequestContext> m_onlineNodesRequest;
+
+    std::set<Node> m_onlineNodes;
+
+    mutable QnMutex m_mutex;
 };
 
 } // namespace nx::cloud::discovery
