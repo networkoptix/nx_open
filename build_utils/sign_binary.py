02d514d255d95a4ad1a7df151e9e947f68f792aa
Sergey Ivanov <sivanov@networkoptix.com>
2019-02-20 21:32:35 +0300

    Backport windows executables signing script improvements
    <==== Mercurial data ====>
    Branch: vms_3.2

--- build_utils/sign_binary.py
+++ build_utils/sign_binary.py
@@ -2,15 +2,28 @@
 
 import argparse
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 chunk_size = 1024 * 1024
+
+DEFAULT_TIMEOUT = 120
+DEFAULT_RETRIES = 10
 
 
 def bool_to_str(value):
     return 'true' if value else 'false'
 
 
-def sign_binary(url, file, output, customization, trusted_timestamping):
+def sign_binary(
+    url,
+    file,
+    output,
+    customization,
+    trusted_timestamping,
+    timeout=DEFAULT_TIMEOUT,
+    max_retries=DEFAULT_RETRIES
+):
 
     params = {
         'customization': customization,
@@ -21,16 +34,32 @@ def sign_binary(url, file, output, customization, trusted_timestamping):
         'file': open(file, 'rb')
     }
 
-    r = requests.post(url, params=params, files=files)
-    if r.status_code != 200:
-        print('ERROR: {}'.format(r.text))
-        return r.status_code
+    retries = Retry(
+        total=max_retries,
+        backoff_factor=0.1)
 
-    with open(output, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
+    session = requests.Session()
+    session.mount(url, HTTPAdapter(max_retries=retries))
+    try:
+        r = session.post(url, params=params, files=files, timeout=timeout)
+        if r.status_code != 200:
+            print('ERROR: {}'.format(r.text))
+            return r.status_code
 
-    return 0
+        with open(output, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                fd.write(chunk)
+
+        return 0
+    except requests.exceptions.ReadTimeout as e:
+        print('ERROR: Connection to the signing server has timed out' +
+              ' ({} seconds, {} retries)'.format(timeout, max_retries))
+        print(e)
+        return 1
+    except requests.exceptions.ConnectionError as e:
+        print('ERROR: Connection to the signing server cannot be established.')
+        print(e)
+        return 2
 
 
 def main():
@@ -45,6 +74,16 @@ def main():
         '-t', '--trusted-timestamping',
         action='store_true',
         help='Trusted timestamping')
+    parser.add_argument(
+        '--retries',
+        help='Max retries count ({})'.format(DEFAULT_RETRIES),
+        type=int,
+        default=DEFAULT_RETRIES)
+    parser.add_argument(
+        '--timeout',
+        help='Request timeout in seconds ({})'.format(DEFAULT_TIMEOUT),
+        type=int,
+        default=DEFAULT_TIMEOUT)
     args = parser.parse_args()
 
     return sign_binary(
@@ -52,7 +91,9 @@ def main():
         file=args.file,
         output=args.output if args.output else args.file,
         customization=args.customization,
-        trusted_timestamping=args.trusted_timestamping)
+        trusted_timestamping=args.trusted_timestamping,
+        timeout=args.timeout,
+        max_retries=args.retries)
 
 
 if __name__ == '__main__':
