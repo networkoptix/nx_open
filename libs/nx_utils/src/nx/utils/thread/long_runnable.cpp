0ed2504debf2ab7d68e21770a18b828b73b7d180
Alexander Kulikov <akulikov@networkoptix.com>
2019-02-22 17:46:58 +0300

    VMS-13194: More logs
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- libs/nx_utils/src/nx/utils/thread/long_runnable.cpp
+++ libs/nx_utils/src/nx/utils/thread/long_runnable.cpp
@@ -60,15 +60,18 @@ public:
         NX_ASSERT(runnable && m_created.contains(runnable));
 
         m_created.remove(runnable);
-        if (m_created.isEmpty())
-            m_waitCondition.wakeAll();
+        m_waitCondition.wakeAll();
     }
 
     void waitForDestruction()
     {
         QnMutexLocker lock(&m_mutex);
         while (!m_created.isEmpty())
+        {
+            for (const auto created: m_created)
+                NX_DEBUG(this, lm("Still created: %1").args(created));
             m_waitCondition.wait(&m_mutex);
+        }
     }
 
 private:
