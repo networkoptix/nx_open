1bd0f7795dcf3c12f8ea3ae6504eef89bf1e8ad4
Nate Wilson <nwilson@networkoptix.com>
2019-03-04 11:12:23 -0800

    CLOUD-2682: clusterdb engine loads discovery api client settings
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/libs/nx_clusterdb_engine/src/nx/clusterdb/engine/service/settings.cpp
+++ cloud/backend/libs/nx_clusterdb_engine/src/nx/clusterdb/engine/service/settings.cpp
@@ -33,12 +33,18 @@ const nx::network::http::server::Settings& Settings::http() const
     return m_http;
 }
 
+const nx::cloud::discovery::Settings& Settings::discovery() const
+{
+    return m_discovery;
+}
+
 void Settings::loadSettings()
 {
     m_logging.load(settings());
     m_syncSettings.load(settings());
     m_db.loadFromSettings(settings());
     m_http.load(settings());
+    m_discovery.load(settings());
 
     if (m_http.endpoints.empty() && m_http.sslEndpoints.empty())
         m_http.endpoints.push_back(nx::network::SocketAddress::anyPrivateAddressV4);
--- cloud/backend/libs/nx_clusterdb_engine/src/nx/clusterdb/engine/service/settings.h
+++ cloud/backend/libs/nx_clusterdb_engine/src/nx/clusterdb/engine/service/settings.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <nx/network/http/server/settings.h>
 #include <nx/sql/types.h>
+#include <nx/cloud/discovery/settings.h>
+#include <nx/network/http/server/settings.h>
 #include <nx/utils/basic_service_settings.h>
 
 #include "../p2p_sync_settings.h"
@@ -24,15 +25,17 @@ public:
     const SynchronizationSettings& synchronization() const;
     const nx::sql::ConnectionOptions& db() const;
     const nx::network::http::server::Settings& http() const;
+    const nx::cloud::discovery::Settings& discovery() const;
 
 protected:
     virtual void loadSettings() override;
 
 private:
-    utils::log::Settings m_logging;
+    nx::utils::log::Settings m_logging;
     SynchronizationSettings m_syncSettings;
     nx::sql::ConnectionOptions m_db;
     nx::network::http::server::Settings m_http;
+    nx::cloud::discovery::Settings m_discovery;
 };
 
 } // namespace nx::clusterdb::engine
