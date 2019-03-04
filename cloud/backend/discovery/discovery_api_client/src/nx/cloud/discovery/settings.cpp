8471338fa3aa45399d4803c4079521bf351fdd63
Nate Wilson <nwilson@networkoptix.com>
2019-03-04 11:11:40 -0800

    CLOUD-2682: Add settings to discovery api client
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/settings.cpp
@@ -0,0 +1,32 @@
+#include "settings.h"
+
+#include <nx/utils/deprecated_settings.h>
+
+namespace nx::cloud::discovery {
+
+namespace {
+
+static const QLatin1String kRoundTripPadding("discovery/roundTripPadding");
+static const std::chrono::milliseconds kDefaultRoundTripPadding(0);
+
+static const QLatin1String kDiscoveryServiceUrl("discovery/discoveryServiceUrl");
+static const QLatin1String kDefaultDiscoveryServiceUrl("https://discovery.nxvms.com");
+
+} // namespace
+
+Settings::Settings():
+    roundTripPadding(kDefaultRoundTripPadding),
+    discoveryServiceUrl(kDefaultDiscoveryServiceUrl)
+{
+}
+
+void Settings::load(const QnSettings& settings)
+{
+    discoveryServiceUrl =
+        settings.value(kDiscoveryServiceUrl, kDefaultDiscoveryServiceUrl).toString();
+
+    roundTripPadding = std::chrono::milliseconds(
+        settings.value(kRoundTripPadding, kDefaultRoundTripPadding.count()).toInt());
+}
+
+} // namespace nx::cloud::discovery
---
+++ cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/settings.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <nx/utils/url.h>
+
+class QnSettings;
+
+namespace nx::cloud::discovery {
+
+class Settings
+{
+public:
+    /**
+     * The base url for the discovery service. The default value is https://discovery.nxvms.com
+     */
+    nx::utils::Url discoveryServiceUrl;
+
+    /**
+     * When calculating how long to wait before sending a registration request, the DiscoveryClient
+     * adds this value to the amount of time that a request takes to travel from
+     * client to server and back, effectively making the round trip time longer.
+     * The default value is 0.
+     */
+    std::chrono::milliseconds roundTripPadding;
+
+public:
+    Settings();
+
+    void load(const QnSettings& settings);
+};
+
+} // namespace nx::cloud::discovery
