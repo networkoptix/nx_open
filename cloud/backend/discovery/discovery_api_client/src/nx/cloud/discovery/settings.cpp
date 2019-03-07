19f7b66625423e38b6d4be03e760c7ea6728b49a
Nate Wilson <nwilson@networkoptix.com>
2019-03-07 10:05:24 -0800

    CLOUD-2682: Reorder settings
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/settings.cpp
+++ cloud/backend/discovery/discovery_api_client/src/nx/cloud/discovery/settings.cpp
@@ -6,12 +6,12 @@ namespace nx::cloud::discovery {
 
 namespace {
 
+static const QLatin1String kDiscoveryServiceUrl("discovery/discoveryServiceUrl");
+static const QLatin1String kDefaultDiscoveryServiceUrl("https://discovery.nxvms.com");
+
 static const QLatin1String kRoundTripPadding("discovery/roundTripPadding");
 static const std::chrono::milliseconds kDefaultRoundTripPadding =
     std::chrono::seconds(3);
-
-static const QLatin1String kDiscoveryServiceUrl("discovery/discoveryServiceUrl");
-static const QLatin1String kDefaultDiscoveryServiceUrl("https://discovery.nxvms.com");
 
 static const QLatin1String kRegistrationErrorDelay("discovery/registrationErrorDelay");
 static const std::chrono::milliseconds kDefaultRegistrationErrorDelay =
