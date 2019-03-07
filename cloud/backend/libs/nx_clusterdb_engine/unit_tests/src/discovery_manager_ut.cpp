cff6b958e31c9d136379773c85cb2cfadd7762f0
Nate Wilson <nwilson@networkoptix.com>
2019-03-07 10:42:02 -0800

    CLOUD-2682: Add DiscoveryManager unit tests
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ cloud/backend/libs/nx_clusterdb_engine/unit_tests/src/discovery_manager_ut.cpp
@@ -0,0 +1,318 @@
+#include <gtest/gtest.h>
+
+#include <nx/cloud/discovery/test_support/discovery_server.h>
+
+#include "cluster_test_fixture.h"
+
+namespace nx::clusterdb::engine::test {
+
+namespace {
+
+static constexpr char kClusterId[] = "DiscoveryManagerTestCluster";
+
+
+} // namespace
+
+//-------------------------------------------------------------------------------------------------
+// DiscoveryTestFixture
+
+class DiscoveryTestFixture
+{
+public:
+    class NodeContext
+    {
+    public:
+        NodeContext(Peer& peer, nx::cloud::discovery::NodeInfo nodeInfo):
+            m_peer(peer),
+            m_nodeInfo(std::move(nodeInfo))
+        {
+        }
+
+        SynchronizationEngine& syncEngine()
+        {
+            return m_peer.process().moduleInstance()->synchronizationEngine();
+        }
+
+        engine::DiscoveryManager& discoveryManager()
+        {
+            return syncEngine().discoveryManager();
+        }
+
+        const nx::cloud::discovery::DiscoveryClient* discoveryClient()
+        {
+            return discoveryManager().discoveryClient();
+        }
+
+        nx::network::SocketAddress endpoint() const
+        {
+            auto url = m_peer.baseApiUrl();
+            return nx::network::SocketAddress(url.host(), url.port());
+        }
+
+        nx::cloud::discovery::Node toNode()
+        {
+            nx::cloud::discovery::Node node;
+            node.nodeId = m_nodeInfo.nodeId;
+            return node;
+        }
+
+        nx::cloud::discovery::NodeInfo& nodeInfo()
+        {
+            return m_nodeInfo;
+        }
+
+    private:
+        Peer& m_peer;
+        nx::cloud::discovery::NodeInfo m_nodeInfo;
+    };
+
+public:
+    void addNodeContext(const nx::utils::Url& discoveryServiceUrl)
+    {
+        const auto discoveryServiceUrlArg = lm("--discovery/discoveryServiceUrl=%1")
+            .arg(discoveryServiceUrl.toString());
+
+        // 2 milliseconds padding for testing purposes
+        const auto roundTripPaddingArg = lm("--discovery/roundTripPadding=%1")
+            .arg(QString::number(2));
+
+        const auto registrationErrorDelayArg = lm("--discovery/registrationErrorDelay=%1")
+            .arg(100);
+
+        const auto onlineNodesRequestDelayArg = lm("--discovery/onlineNodesRequestDelay=%1")
+            .arg(QString::number(2));
+
+        m_fixture.addPeer(/*startAndWaitUntilStarted*/ false);
+        Peer& peer = m_fixture.peer(m_fixture.peerCount() - 1);
+
+        peer.process().addArg(discoveryServiceUrlArg.toUtf8().constData());
+        peer.process().addArg(roundTripPaddingArg.toUtf8().constData());
+        peer.process().addArg(registrationErrorDelayArg.toUtf8().constData());
+        peer.process().addArg(onlineNodesRequestDelayArg.toUtf8().constData());
+
+        ASSERT_TRUE(peer.process().startAndWaitUntilStarted());
+
+        std::string nodeId =
+            peer.process().moduleInstance()->synchronizationEngine().peerId()
+                .toSimpleString().toStdString();
+
+        std::string syncUrl = peer.baseApiUrl().toStdString();
+
+        m_nodes.emplace_back(
+            peer,
+            nx::cloud::discovery::NodeInfo{
+                nodeId,
+                {syncUrl},
+                nx::cloud::discovery::test::generateInfoJson(nodeId)});
+    }
+
+    NodeContext& nodeContext(size_t index)
+    {
+        return m_nodes[index];
+    }
+
+private:
+    ClusterTestFixture m_fixture;
+    std::vector<NodeContext> m_nodes;
+};
+
+//-------------------------------------------------------------------------------------------------
+// DiscoveryManager
+
+class DiscoveryManager: public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        m_server = std::make_unique<nx::cloud::discovery::test::DiscoveryServer>(kClusterId);
+        ASSERT_TRUE(m_server->bindAndListen());
+    }
+
+    void givenOneNode()
+    {
+        m_fixture.addNodeContext(m_server->url());
+    }
+
+    void givenTwoNodes()
+    {
+        for (size_t i = 0; i < 2; ++i)
+            m_fixture.addNodeContext(m_server->url());
+    }
+
+    void givenRegisteredNode(size_t index = 0)
+    {
+        givenOneNode();
+        whenNodeStartsDiscovery(index);
+        thenNodeIsRegistered(index);
+    }
+
+    void givenTwoConnectedNodes()
+    {
+        givenTwoNodes();
+        whenBothNodesStartDiscovery();
+        thenBothNodesAreDiscovered();
+        andBothSyncEnginesAreConnected();
+    }
+
+    void whenSecondNodeStopsDiscovery()
+    {
+        whenNodeStopsDiscovery(1);
+    }
+
+    void whenBothNodesStartDiscovery()
+    {
+        for (size_t i = 0; i < 2; ++i)
+            whenNodeStartsDiscovery(i);
+    }
+
+    void whenNodeStartsDiscovery(size_t index = 0)
+    {
+        auto& nodeContext = m_fixture.nodeContext(index);
+        nodeContext.discoveryManager().start(
+            kClusterId,
+            nodeContext.nodeInfo());
+    }
+
+    void whenNodeStopsDiscovery(size_t index = 0)
+    {
+        m_fixture.nodeContext(index).discoveryManager().stop();
+    }
+
+    void thenNodeIsRegistered(size_t index = 0)
+    {
+        auto nodeContext = m_fixture.nodeContext(index);
+
+        ASSERT_NE(nodeContext.discoveryClient(), nullptr);
+
+        nx::cloud::discovery::Node node = nodeContext.discoveryClient()->node();
+        while (node.nodeId != nodeContext.nodeInfo().nodeId)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            node = nodeContext.discoveryClient()->node();
+        }
+    }
+
+    void thenBothNodesAreDiscovered()
+    {
+        auto& a = m_fixture.nodeContext(0);
+        auto& b = m_fixture.nodeContext(1);
+
+        waitForDiscovery(a, b);
+        waitForDiscovery(b, a);
+    }
+
+    void thenNodesAreDisconnected()
+    {
+        auto& a = m_fixture.nodeContext(0);
+        auto& b = m_fixture.nodeContext(1);
+
+        // b.discoveryClient should be nullptr after DiscoveryManager::stop is called.
+        ASSERT_EQ(b.discoveryClient(), nullptr);
+
+        // the online nodes of "a" should no longer contain the node representing "b".
+        auto onlineNodes = a.discoveryClient()->onlineNodes();
+        while (std::find(onlineNodes.begin(), onlineNodes.end(), b.toNode()) != onlineNodes.end())
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            onlineNodes = a.discoveryClient()->onlineNodes();
+        }
+    }
+
+    void andSyncEnginesAreDisconnected()
+    {
+        auto& a = m_fixture.nodeContext(0);
+        auto& b = m_fixture.nodeContext(1);
+
+        // Verify that the sync engine of "b" is NOT in the list of connections of "a"
+        const auto bIsEqual =
+            [&b](const engine::SystemConnectionInfo& other)
+            {
+                return b.endpoint() == other.peerEndpoint;
+            };
+
+        auto connections = a.syncEngine().connectionManager().getConnections();
+        while (std::find_if(connections.begin(), connections.end(), bIsEqual) != connections.end())
+        {
+            qDebug() << "b is present, sleeping";
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            connections = a.syncEngine().connectionManager().getConnections();
+        }
+    }
+
+    void andBothSyncEnginesAreConnected()
+    {
+        auto& a = m_fixture.nodeContext(0);
+        auto& b = m_fixture.nodeContext(1);
+
+        waitForSyncEngineConnection(a, b);
+        waitForSyncEngineConnection(b, a);
+    }
+private:
+    void waitForDiscovery(
+        DiscoveryTestFixture::NodeContext& a,
+        DiscoveryTestFixture::NodeContext& b)
+    {
+        // Verify that the node represented by "b" is in the online nodes list of "a".
+        auto onlineNodes = a.discoveryClient()->onlineNodes();
+        nx::cloud::discovery::Node bNode = b.toNode();
+        while (std::find(onlineNodes.begin(), onlineNodes.end(), bNode) == onlineNodes.end())
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            onlineNodes = a.discoveryClient()->onlineNodes();
+        }
+    }
+
+    void waitForSyncEngineConnection(
+        DiscoveryTestFixture::NodeContext& a,
+        DiscoveryTestFixture::NodeContext& b)
+    {
+        // Verify that the sync engine of "b" is in the list of connections of "a"
+        const auto bIsEqual =
+            [&b](const engine::SystemConnectionInfo& other)
+            {
+                return b.endpoint() == other.peerEndpoint;
+            };
+
+        auto connections = a.syncEngine().connectionManager().getConnections();
+        while (std::find_if(connections.begin(), connections.end(), bIsEqual) == connections.end())
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            connections = a.syncEngine().connectionManager().getConnections();
+        }
+    }
+
+private:
+    std::unique_ptr<nx::cloud::discovery::test::DiscoveryServer> m_server;
+    DiscoveryTestFixture m_fixture;
+};
+
+TEST_F(DiscoveryManager, discovery_service_starts)
+{
+    givenOneNode();
+
+    whenNodeStartsDiscovery();
+
+    thenNodeIsRegistered();
+}
+
+TEST_F(DiscoveryManager, discovers_other_nodes)
+{
+    givenTwoNodes();
+
+    whenBothNodesStartDiscovery();
+
+    thenBothNodesAreDiscovered();
+
+    andBothSyncEnginesAreConnected();
+}
+
+TEST_F(DiscoveryManager, discovery_service_stops)
+{
+    givenTwoConnectedNodes();
+    whenSecondNodeStopsDiscovery();
+
+    thenNodesAreDisconnected();
+    andSyncEnginesAreDisconnected();
+}
+
+} // namespace nx::clusterdb::engine::test
