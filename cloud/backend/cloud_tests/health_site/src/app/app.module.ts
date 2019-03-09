15877a225d2e31979f1c0c98fa8ef1e4cee398dc
Ivan Vigasin <ivigasin@networkoptix.com>
2019-03-08 20:03:14 -0800

    Health Page SLA frontend
    <==== Mercurial data ====>
    Branch: master

--- cloud/backend/cloud_tests/health_site/src/app/app.module.ts
+++ cloud/backend/cloud_tests/health_site/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent }      from './app.component';
 import { InfoTileComponent } from './info-tile/info-tile.component';
 import { TimeCheckedPipe }   from './pipes/time-checked.pipe';
 import { TimeAgoPipe }       from './pipes/time-ago.pipe';
+import { SlaPipe }           from './pipes/sla.pipe';
 
 @NgModule({
     declarations: [
         AppComponent,
         InfoTileComponent,
         TimeCheckedPipe,
-        TimeAgoPipe
+        TimeAgoPipe,
+        SlaPipe
     ],
     imports: [
         BrowserModule,
