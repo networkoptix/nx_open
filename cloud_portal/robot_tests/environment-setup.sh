6e0f55b3f05ad5db3b3ebd078e7d4c4c3776782f
Kyle Epperson <kepperson@networkoptix.com>
2019-03-11 15:10:59 -0700

    CLOUD-2721:more outline updates
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- cloud_portal/robot_tests/environment-setup.sh
+++ cloud_portal/robot_tests/environment-setup.sh
@@ -4,10 +4,17 @@ SERVERVERSION=4.0.0.28057
 
 robot -v ENV:$TESTENV special-cases/qa-user-creation
 docker build -t mediaserver --build-arg mediaserver_deb=nxwitness-server-$SERVERVERSION*.deb .
-for i in {1..8}
+for i in {1..7}
 do
 	docker-compose up
-	robot addsystem.robot
+	robot -v sysname:system$i addsystem.robot
 	docker-compose down
-	sudo rm -r video/
-	sudo rm -r data/
+	rm -r video/
+	rm -r data/
+
+docker-compose up
+robot -v sysname:"Auto Tests 2" addsystem.robot
+
+docker-compose down
+rm -r video/
+rm -r data/
