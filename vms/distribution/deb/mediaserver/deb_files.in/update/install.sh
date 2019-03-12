65b857dc5e48e3c232e9e0063155787b1e826b7b
Alexander Kulikov <akulikov@networkoptix.com>
2019-03-12 14:10:35 +0300

    VMS-13480: Fix the install.sh script for the 3.2 --> 4.0 update
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/distribution/deb/mediaserver/deb_files.in/update/install.sh
+++ vms/distribution/deb/mediaserver/deb_files.in/update/install.sh
@@ -12,14 +12,21 @@ installDeb()
     local -r FORCE="$2"
     local ARGS="-i"
 
+    if [ $FORCE = "force-conflicts" ]
+    then
+        ARGS="$ARGS --auto-deconfigure --force-conflicts"
+    fi
+
     if [ "$WITH_ROOT_TOOL" = true ]
     then
-        "/opt/$COMPANY_NAME/mediaserver/bin/root-tool-bin" install "$DEB" "$FORCE"
-    else
-        if [ $FORCE = "force-conflicts" ]
-        then
-            ARGS="$ARGS --auto-deconfigure --force-conflicts"
+        local -r ROOT_TOOL_BINARY="/opt/$COMPANY_NAME/mediaserver/bin/root-tool-bin"
+        if [ -f "${ROOT_TOOL_BINARY}" ]
+        then #< It must be 4.0+ update.
+            ${ROOT_TOOL_BINARY} install "$DEB" "$FORCE"
+        else #< It must be 3.2 --> 4.0 update.
+            dpkg $ARGS "$DEB"
         fi
+    else
         dpkg $ARGS "$DEB"
     fi
 }
@@ -28,13 +35,15 @@ update()
 {
     export DEBIAN_FRONTEND=noninteractive
     CIFSUTILS=$(dpkg -l |grep cifs-utils |grep ii |awk '{print $2}')
-    if [ -z "$CIFSUTILS" ]; then
+    if [ -z "$CIFSUTILS" ]
+    then
         [ -d "ubuntu${RELEASE_YEAR}" ] && installDeb ubuntu${RELEASE_YEAR}/cifs-utils/*.deb
     fi
     installDeb "$DISTRIB" force-conflicts
 }
 
-if [ "$1" != "" ]; then
+if [ "$1" != "" ]
+then
     update >> $1 2>&1
 else
     update 2>&1
