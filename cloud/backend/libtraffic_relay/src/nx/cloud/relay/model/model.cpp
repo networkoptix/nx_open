993df7d1b460823a9b56241e475551076e4757af
Nate Wilson <nwilson@networkoptix.com>
2019-03-15 17:13:34 -0700

    CLOUD-2175: Expose View::MessageDispatcher for use by AbstractRemoteRelayPeerPool
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/libtraffic_relay/src/nx/cloud/relay/model/model.cpp
+++ cloud/backend/libtraffic_relay/src/nx/cloud/relay/model/model.cpp
@@ -43,8 +43,10 @@ bool Model::doMandatoryInitialization()
     // TODO: #ak Refactor here. E.g., if DB connection parameters are specified,
     // we can create some dummy RemoteRelayPeerPool so that we keep real RemoteRelayPeerPool
     // implementation clear (free of DB needed/not needed checks).
-    if (m_settings.cassandraConnection().host.empty())
-        return true;
+
+    //if (m_settings.cassandraConnection().host.empty())
+    //    return true;
+
     return m_remoteRelayPeerPool->connectToDb();
 }
 
