6f460b10442a933a3d22b6b61e27c0406e0819c8
Alexander Kulikov <akulikov@networkoptix.com>
2019-03-18 19:14:41 +0300

    VMS-13313: Refactor archive reindexing: add unit test
    <==== Mercurial data ====>
    Branch: x_vms_13313_refactor_archive_reindexing

---
+++ vms/server/nx_vms_server/unit_tests/src/archive/reindex_archive_ut.cpp
@@ -0,0 +1,86 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <memory>
+
+#include <test_support/mediaserver_launcher.h>
+#include <health/system_health.h>
+#include <recorder/storage_manager.h>
+
+namespace nx::vms::server::test {
+
+class Reindex: public QObject, public ::testing::Test
+{
+protected:
+    virtual void SetUp() override
+    {
+        MediaServerLauncher::DisabledFeatures startupFlags;
+        startupFlags.setFlag(MediaServerLauncher::noResourceDiscovery);
+        startupFlags.setFlag(MediaServerLauncher::noMonitorStatistics);
+        startupFlags.setFlag(MediaServerLauncher::noPlugins);
+
+        m_server.reset(new MediaServerLauncher(QString(), 0, startupFlags));
+        QObject::connect(
+            m_server->serverModule()->normalStorageManager(), &QnStorageManager::rebuildFinished,
+            this, &Reindex::onReindexFinished);
+    }
+
+    void whenServerStarted()
+    {
+        m_server->addSetting("minStorageSpace", 0);
+        ASSERT_TRUE(m_server->start());
+    }
+
+    void givenSomeArchiveOnHdd()
+    {
+        m_generatedArchive["camera1"] = generateCameraArchive(
+            "camera1", std::chrono::system_clock::now() - std::chrono::hours(24), 10);
+
+        m_generatedArchive["camera2"] = generateCameraArchive(
+            "camera2", std::chrono::system_clock::now() - std::chrono::hours(23), 5);
+    }
+
+    void thenAllArchiveShouldBeFastScannedCorreclty()
+    {
+    }
+
+private:
+    struct ChunkData
+    {
+        qint64 startTimeMs;
+        qint64 durationsMs;
+    };
+
+    using ChunkDataList = std::vector<ChunkData>;
+
+    struct Catalog
+    {
+        ChunkDataList lowQualityChunks;
+        ChunkDataList highQualityChunks;
+    };
+
+    using Archive = QMap<QString, Catalog>;
+
+    std::unique_ptr<MediaServerLauncher> m_server;
+    Archive m_generatedArchive;
+
+    void onReindexFinished(QnSystemHealth::MessageType message)
+    {
+        qDebug() << "REINDEX FINISHED" << message;
+    }
+
+    Catalog generateCameraArchive(
+        const QString& cameraName,
+        std::chrono::time_point<std::chrono::system_clock> startPoint,
+        int count)
+    {
+    }
+};
+
+TEST_F(Reindex, FastArchiveScan_AllDataRetrieved)
+{
+    givenSomeArchiveOnHdd();
+    whenServerStarted();
+    thenAllArchiveShouldBeFastScannedCorreclty();
+}
+
+} // nx::vms::server::test
