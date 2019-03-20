5126276715eec35825c7a5f3113ad57839e83ccc
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-03-20 12:06:06 -0700

    piko customization
    <==== Mercurial data ====>
    Branch: vms_3.2_release

---
+++ customization/piko/customization.cmake
@@ -0,0 +1,61 @@
+set(defaultSkin "dark_blue")
+
+# Components Namings
+set(display.product.name "Piko")
+set(display.mobile.name "Piko")
+set(product.name "Piko")
+set(product.name.short "piko")
+set(product.appName "piko")
+set(company.name "Cook Security Group")
+set(uri.protocol "piko-vms")
+set(cloudName "piko Cloud")
+
+# Default settings
+set(defaultWebPages "{\"Home Page\": \"https://www.cooksecuritygroup.com\"}")
+
+# Support section
+set(companyUrl "https://www.cooksecuritygroup.com")
+set(productUrl "www.pikovms.com")
+set(supportUrl "")
+set(supportEmail "support@pikovms.com")
+set(supportPhone "")
+set(licenseEmail "support@pikovms.com")
+
+set(installer.name "piko")
+set(android.packageName "com.pikovms.piko")
+set(android.oldPackageName "")
+set(android.alias "piko")
+set(android.storepass "5B4M2Gn5")
+set(android.keypass "S6rtgPMk")
+
+# Custom Installer (if needed). If not leave "default"
+set(customization.upgradeCode "{771EC0A4-ABF6-4C73-865F-977813F930E4}")
+set(customization.clientUpgradeCode "{1FEE12C1-F7D0-4284-9C09-04F2277960B0}")
+set(customization.serverUpgradeCode "{1722D050-5E63-47E7-8D10-E870FA3F7B70}")
+
+set(customization.clientBundleUpgradeCode "{97D9B41A-F8BF-40B5-A61B-8A7CBA9A95DF}")
+set(customization.serverBundleUpgradeCode "{E3606532-4537-4BB4-ACAB-614302FF61AD}")
+set(customization.fullBundleUpgradeCode "{486DA831-335F-4D02-B1A1-82D3C9868404}")
+
+set(deb.customization.company.name "piko")
+
+set(ios.new_bundle_identifier "com.pikovms.pikomobile")
+set(ios.group_identifier "com.pikovms.piko")
+set(ios.sign.identity "")
+set(ios.old_app_appstore_id "")
+
+set(mac.skip.sign "false")
+set(mac.sign.identity "")
+set(mac.app.sign.identity "")
+set(mac.pkg.sign.identity "")
+set(mac.bundle.identifier "com.pikovms.piko2")
+set(mac.protocol_handler_bundle.identifier "com.pikovms.protocol_handler")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0032")
+
+# Installer Customizations
+set(sign.password "qweasd123")
+set(sign.cer "app.p12")
