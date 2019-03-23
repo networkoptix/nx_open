050087ffb8a342557e573404738245489500096d
Nick Hartleb <nhartleb@networkoptix.com>
2019-03-22 17:09:39 -0700

    CLOUD-2751: If cloud portal tries to start with missing customization - create it
    Fixed typo. Added logger error for Boris to configure new cloud
    
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- cloud_portal/cloud/cms/management/commands/filldata.py
+++ cloud_portal/cloud/cms/management/commands/filldata.py
@@ -1,8 +1,11 @@
+import logging
 from django.core.management.base import BaseCommand
 from ...controllers import filldata, structure
 from ...models import Customization, Language, Product, ProductType, get_cloud_portal_product
 from cloud import settings
 from cloud.debug import timer
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -20,17 +23,19 @@ class Command(BaseCommand):
                 filldata.init_skin(product, options['preview'])
                 self.stdout.write(self.style.SUCCESS('Initiated static content for ' + product.__str__()))
         else:
-            customization = Customization.objects.filter(name=options['customization'])
-            if not customization.exists():
+            customization = Customization.objects.filter(name=options['customization']).first()
+            if not customization:
+                logger.error('Customization {0} was automatically generated. Ask Boris to configure cloud for {0}.'
+                             .format(options['customization']))
                 en_us = Language.objects.get(code="en_US")
-                customization = Customization(name=options['customizations'], default_language=en_us)
+                customization = Customization(name=options['customization'], default_language=en_us)
                 customization.save()
                 customization.languages = [en_us]
                 customization.save()
 
-            if not Product.objects.filter(customizations__in=[customization.first()],
+            if not Product.objects.filter(customizations__in=[customization],
                                           product_type__type=ProductType.PRODUCT_TYPES.cloud_portal).exists():
-                product = structure.find_or_add_product('Cloud Portal', customization.first())
+                product = structure.find_or_add_product('Cloud Portal', customization)
             else:
                 product = get_cloud_portal_product(options['customization'])
             filldata.init_skin(product, options['preview'])
