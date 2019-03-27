26c735fd55b7a343f934eabef2642fcd8ce46a66
Sergey Ivanov <sivanov@networkoptix.com>
2019-03-27 14:20:28 +0300

    VMS-CR-16010: Fix Piko customization: disable hanwha components, enable iOS signing
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- customization/piko/customization.cmake
+++ customization/piko/customization.cmake
@@ -1,6 +1,3 @@
-set(defaultSkin "dark_blue")
-set(enable_hanwha true)
-
 # Components Namings
 set(display.product.name "Piko")
 set(display.mobile.name "Piko")
@@ -19,9 +16,7 @@ set(defaultWebPages "{\"Home Page\": \"https://www.cooksecuritygroup.com\"}")
 # Support section
 set(companyUrl "https://www.cooksecuritygroup.com")
 set(productUrl "www.pikovms.com")
-set(supportUrl "")
 set(supportEmail "support@pikovms.com")
-set(supportPhone "")
 set(licenseEmail "support@pikovms.com")
 
 set(installer.name "piko")
@@ -31,11 +26,8 @@ set(android.alias "piko")
 set(android.storepass "5B4M2Gn5")
 set(android.keypass "S6rtgPMk")
 
-# Custom Installer (if needed). If not leave "default"
 set(customization.upgradeCode "{771EC0A4-ABF6-4C73-865F-977813F930E4}")
 set(customization.clientUpgradeCode "{1FEE12C1-F7D0-4284-9C09-04F2277960B0}")
-set(customization.serverUpgradeCode "{1722D050-5E63-47E7-8D10-E870FA3F7B70}")
-
 set(customization.clientBundleUpgradeCode "{97D9B41A-F8BF-40B5-A61B-8A7CBA9A95DF}")
 set(customization.serverBundleUpgradeCode "{E3606532-4537-4BB4-ACAB-614302FF61AD}")
 set(customization.fullBundleUpgradeCode "{486DA831-335F-4D02-B1A1-82D3C9868404}")
@@ -44,8 +36,7 @@ set(deb.customization.company.name "piko")
 
 set(ios.new_bundle_identifier "com.pikovms.pikomobile")
 set(ios.group_identifier "com.pikovms.piko")
-set(ios.sign.identity "")
-set(ios.old_app_appstore_id "")
+set(ios.sign.identity "iPhone Distribution: Cook Security Group, Inc (N52Z7JQP9L)")
 
 set(mac.skip.sign "false")
 set(mac.sign.identity "")
