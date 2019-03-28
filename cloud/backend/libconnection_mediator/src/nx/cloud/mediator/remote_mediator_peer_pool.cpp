456604fe19d9b69b8415fb721450d97ceea24a93
Nate Wilson <nwilson@networkoptix.com>
2019-03-28 11:02:42 -0700

    CLOUD19: Add RemoteMediatorPeerPool (using SynchronizationEngine) to share listening peer info between mediator instances
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ cloud/backend/libconnection_mediator/src/nx/cloud/mediator/remote_mediator_peer_pool.cpp
@@ -0,0 +1,220 @@
+#include "remote_mediator_peer_pool.h"
+
+#include <nx/utils/std/algorithm.h>
+#include <nx/clusterdb/engine/http/http_paths.h>
+#include <nx/network/http/rest/http_rest_client.h>
+#include <nx/network/http/server/rest/http_server_rest_message_dispatcher.h>
+#include <nx/network/url/url_builder.h>
+
+namespace nx::hpm {
+
+namespace {
+
+static std::string toLowerReversed(std::string domainName)
+{
+    nx::utils::to_lower(&domainName);
+    return nx::utils::reverseWords(domainName, ".");
+}
+
+int toInt(const std::string& integer)
+{
+    try
+    {
+        return std::stoi(integer);
+    }
+    catch (const std::exception& /*e*/)
+    {
+        NX_WARNING(
+            nx::utils::log::Tag(typeid(RemoteMediatorPeerPool)),
+            "error converting string to int: %1",
+            integer);
+        return -1;
+    }
+}
+
+} //namespace
+
+RemoteMediatorPeerPool::RemoteMediatorPeerPool(const conf::ClusterDbMap& settings)
+    :
+    m_settings(settings)
+{
+}
+
+bool RemoteMediatorPeerPool::initialize()
+{
+    if (m_map)
+        return true;
+
+    try
+    {
+        auto sqlExecutor = std::make_unique<nx::sql::AsyncSqlQueryExecutor>(m_settings.sql);
+        // Throws exception
+        auto map = std::make_unique<nx::clusterdb::map::EmbeddedDatabase>(
+            m_settings.map,
+            sqlExecutor.get());
+    }
+    catch (const std::exception& e)
+    {
+        NX_ERROR(this, "Error initializing EmbeddedDatabase: %1", e.what());
+    }
+
+    return m_map != nullptr;
+}
+
+void RemoteMediatorPeerPool::setEndpoint(const MediatorEndpoint& endpoint)
+{
+    m_mediatorEndpointString = toString(endpoint);
+
+    m_syncEngineUrl.setHost(endpoint.domainName.c_str());
+    if (endpoint.httpsPort.has_value())
+    {
+        m_syncEngineUrl.setPort(*endpoint.httpsPort);
+        m_syncEngineUrl.setScheme(nx::network::http::kSecureUrlSchemeName);
+    }
+    else if (endpoint.httpPort.has_value())
+    {
+        m_syncEngineUrl.setPort(*endpoint.httpPort);
+        m_syncEngineUrl.setScheme(nx::network::http::kUrlSchemeName);
+    }
+    m_syncEngineUrl.setPath(nx::network::http::rest::substituteParameters(
+        nx::clusterdb::engine::kBaseSynchronizationPath,
+        {m_settings.map.synchronizationSettings.clusterId}).c_str());
+}
+
+void RemoteMediatorPeerPool::addPeer(
+    const std::string& peerDomainName,
+    nx::utils::MoveOnlyFunc<void(bool)> handler)
+{
+    if (!m_map || m_mediatorEndpointString.empty())
+        return handler(false);
+
+    m_map->database().dataManager().insertOrUpdate(
+        toLowerReversed(peerDomainName),
+        m_mediatorEndpointString,
+        [this, peerDomainName, handler = std::move(handler)](
+            nx::clusterdb::map::ResultCode result)
+        {
+            if (result != nx::clusterdb::map::ResultCode::ok)
+            {
+                NX_WARNING(
+                    this,
+                    "insertOrUpdate peerDomainName(key): %1, mediatorDomainName(value): %2, "
+                        "failed with error: %3",
+                    peerDomainName, m_mediatorEndpointString, nx::clusterdb::map::toString(result));
+                return handler(false);
+            }
+
+            return handler(true);
+        });
+}
+
+void RemoteMediatorPeerPool::removePeer(
+    const std::string& peerDomainName,
+    nx::utils::MoveOnlyFunc<void(bool)> handler)
+{
+    if (!m_map)
+        return handler(false);
+
+    m_map->database().dataManager().remove(
+        toLowerReversed(peerDomainName),
+        [this, peerDomainName, handler = std::move(handler)](
+            nx::clusterdb::map::ResultCode result)
+        {
+            if (result != nx::clusterdb::map::ResultCode::ok)
+            {
+                NX_WARNING(
+                    this,
+                    "removing peerDomainName: %1 failed with error: %3",
+                    peerDomainName, nx::clusterdb::map::toString(result));
+                return handler(false);
+            }
+
+            return handler(true);
+        });
+}
+
+void RemoteMediatorPeerPool::findMediatorByPeerDomain(
+    const std::string& peerDomainName,
+    nx::utils::MoveOnlyFunc<void(MediatorEndpoint)> handler)
+{
+   if (!m_map)
+        return handler(MediatorEndpoint());
+
+    m_map->database().dataManager().getRangeWithPrefix(
+        toLowerReversed(peerDomainName),
+        [this, peerDomainName, handler = std::move(handler)](
+            nx::clusterdb::map::ResultCode result, std::map<std::string, std::string> map)
+        {
+            if (result != nx::clusterdb::map::ResultCode::ok)
+            {
+                NX_WARNING(
+                    this,
+                    "getRangeWithPrefix with peerDomainName: %1 failed error: %1 for",
+                    nx::clusterdb::map::toString(result), peerDomainName);
+                return handler(std::string());
+            }
+
+            NX_VERBOSE(
+                this,
+                "getRangeWithPrefix returned ResultCode: %1 and result set: %2 for peerDomainName: %3",
+                nx::clusterdb::map::toString(result), containerString(map), peerDomainName);
+
+            return map.empty()
+                ? handler(MediatorEndpoint{})
+                : handler(toMediatorEndpoint(map.begin()->second));
+        });
+}
+
+void RemoteMediatorPeerPool::startDiscovery(
+    nx::network::http::server::rest::MessageDispatcher* messageDispatcher)
+{
+    if (!m_map)
+        return;
+
+    m_map->synchronizationEngine().registerHttpApi(
+        nx::clusterdb::engine::kBaseSynchronizationPath,
+        messageDispatcher);
+
+    m_map->synchronizationEngine().discoveryManager().start(
+        m_settings.map.synchronizationSettings.clusterId,
+        m_syncEngineUrl);
+}
+
+std::string RemoteMediatorPeerPool::toString(
+    const MediatorEndpoint& endpoint) const
+{
+    std::string s = endpoint.domainName;
+    s += ";";
+    s += std::to_string(endpoint.httpPort.has_value() ? *endpoint.httpPort : -1);
+    s += ";";
+    s += std::to_string(endpoint.httpsPort.has_value() ? *endpoint.httpsPort : -1);
+    s += ";";
+    s += std::to_string(endpoint.stunUdpPort.has_value() ? *endpoint.stunUdpPort : -1);
+    return s;
+}
+
+
+std::optional<MediatorEndpoint> RemoteMediatorPeerPool::toMediatorEndpoint(
+    const std::string& endpointStr) const
+{
+    std::vector<std::string> values;
+    boost::split(values, endpointStr, boost::is_any_of(";"));
+    if (values.size() != 4)
+        return std::nullopt;
+
+    MediatorEndpoint endpoint;
+    endpoint.domainName = values[0];
+
+    if (int httpPort = toInt(values[1]); httpPort != -1)
+        endpoint.domainName = httpPort;
+
+    if (int httpsPort = toInt(values[2]); httpsPort != -1)
+        endpoint.httpsPort = httpsPort;
+
+    if (int stunUdpPort = toInt(values[3]); stunUdpPort != -1)
+        endpoint.stunUdpPort = stunUdpPort;
+
+    return endpoint;
+}
+
+} // namespace nx::hpm
