d4df28060d17d8eacb79eff0c6735c5a8ec61b70
Nate Wilson <nwilson@networkoptix.com>
2019-03-29 16:07:52 -0700

    CLOUD-2766: Add reusable OverridePublicIp class
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/libconnection_mediator/unit_tests/src/functional_tests/mediator_functional_test.h
+++ cloud/backend/libconnection_mediator/unit_tests/src/functional_tests/mediator_functional_test.h
@@ -5,25 +5,18 @@
 #include <nx/cloud/mediator/public_ip_discovery.h>
 #include <nx/cloud/mediator/test_support/mediator_functional_test.h>
 
+#include "../override_public_ip.h"
+
 namespace nx {
 namespace hpm {
 namespace test {
 
 class MediatorFunctionalTest:
     public nx::hpm::MediatorFunctionalTest,
-    public testing::Test
+    public testing::Test,
+    OverridePublicIp
 {
 public:
-    MediatorFunctionalTest()
-    {
-        PublicIpDiscovery::setDiscoverFunc(
-            []() { return nx::network::HostAddress("127.0.0.1"); });
-    }
-
-    ~MediatorFunctionalTest()
-    {
-        PublicIpDiscovery::setDiscoverFunc(nullptr);
-    }
 };
 
 } // namespace test
