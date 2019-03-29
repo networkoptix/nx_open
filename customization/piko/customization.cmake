3eb35784bee3903685c8399968e31a4c9de563e5
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-03-28 17:59:42 -0700

    Piko customization
    <==== Mercurial data ====>
    Branch: mobile_release

---
+++ customization/piko/customization.cmake
@@ -0,0 +1,53 @@
+set(enable_hanwha true)
+
+# Components Namings
+set(display.product.name "Piko")
+set(display.mobile.name "Piko")
+set(product.name "Piko")
+set(product.name.short "piko")
+set(product.appName "piko")
+set(company.name "Cook Security Group")
+set(uri.protocol "piko-vms")
+set(cloudName "Piko Cloud")
+
+# Default settings
+set(backgroundImage
+    "{\"enabled\": true, \"name\": \":/skin/background.png\", \"mode\": \"Crop\"}")
+set(defaultWebPages "{\"Home Page\": \"https://www.cooksecuritygroup.com\"}")
+
+# Support section
+set(companyUrl "https://www.cooksecuritygroup.com")
+set(productUrl "www.pikovms.com")
+set(supportEmail "support@pikovms.com")
+set(licenseEmail "support@pikovms.com")
+
+set(installer.name "piko")
+set(android.packageName "com.pikovms.piko")
+set(android.oldPackageName "")
+set(android.alias "piko")
+set(android.storepass "5B4M2Gn5")
+set(android.keypass "S6rtgPMk")
+
+set(customization.upgradeCode "{771EC0A4-ABF6-4C73-865F-977813F930E4}")
+set(customization.clientUpgradeCode "{1FEE12C1-F7D0-4284-9C09-04F2277960B0}")
+set(customization.clientBundleUpgradeCode "{97D9B41A-F8BF-40B5-A61B-8A7CBA9A95DF}")
+set(customization.serverBundleUpgradeCode "{E3606532-4537-4BB4-ACAB-614302FF61AD}")
+set(customization.fullBundleUpgradeCode "{486DA831-335F-4D02-B1A1-82D3C9868404}")
+
+set(deb.customization.company.name "piko")
+
+set(ios.new_bundle_identifier "com.pikovms.pikomobile")
+set(ios.group_identifier "com.pikovms.piko")
+set(ios.sign.identity "iPhone Distribution: Cook Security Group, Inc (N52Z7JQP9L)")
+
+set(mac.skip.sign false)
+set(mac.sign.identity "Developer ID Application: Cook Security Group, Inc (N52Z7JQP9L)")
+set(mac.app.sign.identity "3rd Party Mac Developer Application: Cook Security Group, Inc (N52Z7JQP9L)")
+set(mac.pkg.sign.identity "3rd Party Mac Developer Installer: Cook Security Group, Inc (N52Z7JQP9L)")
+set(mac.bundle.identifier "com.pikovms.piko")
+set(mac.protocol_handler_bundle.identifier "com.pikovms.protocol_handler")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0033")
