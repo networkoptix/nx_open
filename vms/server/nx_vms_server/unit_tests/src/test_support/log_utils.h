3b0e9762c0ce9be174961d0e0095e04a3f75257a
Alexander Kulikov <akulikov@networkoptix.com>
2019-04-02 17:30:21 +0300

    FT-205: Fix misplaced content of log_utils.h and log_utils.cpp
    <==== Mercurial data ====>
    Branch: vms

--- vms/server/nx_vms_server/unit_tests/src/test_support/log_utils.h
+++ vms/server/nx_vms_server/unit_tests/src/test_support/log_utils.h
@@ -1,26 +1,8 @@
-#include <nx/utils/log/log_initializer.h>
-#include <nx/utils/log/log_main.h>
-#include <nx/utils/log/log_settings.h>
+#pragma once
 
 namespace nx::vms::server::test::test_support {
 
-void createTestLogger(const QSet<QString>& filters, nx::utils::log::Buffer** outBuffer)
-{
-    namespace log = nx::utils::log;
-    log::unlockConfiguration();
-
-    log::mainLogger()->setDefaultLevel(log::Level::none);
-
-    log::Settings settings;
-    settings.loggers.resize(1);
-    settings.loggers.front().level.primary = log::levelFromString("VERBOSE");
-
-    auto logWriter = std::unique_ptr<log::AbstractWriter>(*outBuffer = new log::Buffer);
-    std::set<log::Filter> logFilters;
-    for (const auto& filterString: filters)
-        logFilters.insert(log::Filter(filterString));
-
-    addLogger(buildLogger(settings, QString("log_ut"), QString(), logFilters, std::move(logWriter)));
-}
+/** Makes logging system duplicate filtered messages to the user-provided log::Buffer. */
+void createTestLogger(const QSet<QString>& filters, nx::utils::log::Buffer* outBuffer);
 
 } // namespace nx::vms::server::test::test_support
