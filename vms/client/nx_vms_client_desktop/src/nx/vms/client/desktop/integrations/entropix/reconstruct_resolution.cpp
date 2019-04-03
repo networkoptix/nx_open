bec444b1128218bd52d2b07714ed82b37a143cdd
Sergey Panasenko <spanasenko@networkoptix.com>
2019-04-03 21:17:49 +0300

    VMS-13744: Whole frame is passed to Reconstruct Resolution when a camera is selected
    <==== Mercurial data ====>
    Branch: meta_4.0

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/integrations/entropix/reconstruct_resolution.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/integrations/entropix/reconstruct_resolution.cpp
@@ -34,6 +34,7 @@ static const QnUuid kVmsRuleId = QnUuid::fromArbitraryData(
     std::string("entropix::ReconstructResolutionIntegration"));
 static const QString kVmsEventSource("ReconstructResolution");
 static const QString kVmsEventCaption("Entropix: Reconstruct Resolution");
+static const QRectF kWholeFrame(0.0, 0.0, 1.0, 1.0);
 
 struct ZoomWindow
 {
@@ -66,14 +67,19 @@ struct Description
 
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS_FOR_TYPES((ZoomWindow)(Description), (json), _Fields)
 
-class ZoomWindowIsSelected: public ui::action::Condition
+class CameraIsSelected: public ui::action::Condition
 {
 public:
     ui::action::ActionVisibility check(
         const QnResourceWidgetList& widgets,
         QnWorkbenchContext* context)
     {
-        return widgets.size() == 1 && widgets.first()->isZoomWindow()
+        const bool selected = std::any_of(
+            widgets.cbegin(),
+            widgets.cend(),
+            [](const auto& widget) { return widget->isSelected(); });
+
+        return selected
             ? ui::action::EnabledAction
             : ui::action::InvisibleAction;
     }
@@ -154,7 +160,7 @@ void ReconstructResolutionIntegration::registerActions(ui::action::MenuFactory* 
         .condition(
             !condition::isSafeMode()
             && ConditionWrapper(new AddBookmarkCondition())
-            && ConditionWrapper(new ZoomWindowIsSelected())
+            && ConditionWrapper(new CameraIsSelected())
             && ConditionWrapper(
                 new ResourceCondition(
                     [this](const QnResourcePtr& resource)
@@ -182,13 +188,13 @@ void ReconstructResolutionIntegration::addReconstructResolutionBookmark(
     const auto period = parameters.argument<QnTimePeriod>(Qn::TimePeriodRole);
 
     const auto widgets = context->display()->widgets(camera);
-    std::vector<QnResourceWidget*> selectedZoomWindows;
+    std::vector<QnResourceWidget*> selectedWindows;
     std::copy_if(widgets.cbegin(), widgets.cend(),
-        std::back_inserter(selectedZoomWindows),
-        [](QnResourceWidget* widget) { return widget->isZoomWindow() && widget->isSelected(); });
+        std::back_inserter(selectedWindows),
+        [](QnResourceWidget* widget) { return widget->isSelected(); });
 
-    NX_ASSERT(!selectedZoomWindows.empty(), "Condition must not allow this");
-    if (selectedZoomWindows.empty())
+    NX_ASSERT(!selectedWindows.empty(), "Condition must not allow this");
+    if (selectedWindows.empty())
         return;
 
     QnCameraBookmark bookmark;
@@ -209,9 +215,12 @@ void ReconstructResolutionIntegration::addReconstructResolutionBookmark(
     description.bookmarkId = bookmark.guid;
     description.startTimeMs = bookmark.startTimeMs;
     description.durationMs = bookmark.durationMs;
-    std::transform(selectedZoomWindows.cbegin(), selectedZoomWindows.cend(),
+    std::transform(selectedWindows.cbegin(), selectedWindows.cend(),
         std::back_inserter(description.crops),
-        [](QnResourceWidget* widget) { return widget->zoomRect(); });
+        [](QnResourceWidget* widget)
+        {
+            return widget->isZoomWindow() ? widget->zoomRect() : kWholeFrame;
+        });
 
     nx::vms::event::EventMetaData metadata;
     metadata.cameraRefs.push_back(camera->getId().toString());
