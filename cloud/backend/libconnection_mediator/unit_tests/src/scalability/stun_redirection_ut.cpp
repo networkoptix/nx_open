95346ac1c7df0d3190fbff7d3bb1673e48c9bc2a
Nate Wilson <nwilson@networkoptix.com>
2019-04-08 13:51:07 -0700

    CLOUD-2765: Add Stun redirection unit test
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ cloud/backend/libconnection_mediator/unit_tests/src/scalability/stun_redirection_ut.cpp
@@ -0,0 +1,58 @@
+#include "mediator_scalability_test_fixture.h"
+
+namespace nx::hpm::test {
+
+class StunRedirection: public MediatorScalabilityTestFixture
+{
+public:
+    ~StunRedirection()
+    {
+        m_udpClient->pleaseStopSync();
+    }
+
+protected:
+    void whenTryToConnectToServerOnDifferentMediator()
+    {
+        auto& mediator = m_mediatorCluster.mediator(1);
+
+        m_udpClient =
+            std::make_unique<api::MediatorClientUdpConnection>(mediator.stunUdpEndpoint());
+
+        api::ConnectRequest request;
+        request.connectionMethods = api::ConnectionMethod::udpHolePunching;
+        request.connectSessionId = QnUuid::createUuid().toSimpleByteArray();
+        request.destinationHostName = m_mediaServerFullName.c_str();
+        request.originatingPeerId = QnUuid::createUuid().toSimpleByteArray();
+
+        m_udpClient->connect(
+            request,
+            [this](
+                nx::network::stun::TransportHeader /*stunTransportHeader*/,
+                nx::hpm::api::ResultCode resultCode,
+                nx::hpm::api::ConnectResponse response)
+            {
+                m_connectResponseQueue.push(std::make_tuple(resultCode, std::move(response)));
+            });
+    }
+
+    void andConnectionToDifferentMediatorIsEstablished()
+    {
+        // TODO
+    }
+
+private:
+    std::unique_ptr<api::MediatorClientUdpConnection> m_udpClient;
+};
+
+TEST_F(StunRedirection, stun_connect_request_redirected_to_correct_mediator)
+{
+    givenSynchronizedClusterWithListeningServer();
+
+    whenTryToConnectToServerOnDifferentMediator();
+
+    thenRequestIsRedirected();
+
+    andConnectionToDifferentMediatorIsEstablished();
+}
+
+} // namespace nx::hpm::test
