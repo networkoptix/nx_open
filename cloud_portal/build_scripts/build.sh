f3c93560b8b5d06a69da861d5c2bcb324f9a234d
Nick Hartleb <nhartleb@networkoptix.com>
2019-04-08 21:25:06 -0700

    CLOUD-2792: Automatically validate absence of reference to Nx anywhere in files during build process.
    stripped nx1
    
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- cloud_portal/build_scripts/build.sh
+++ cloud_portal/build_scripts/build.sh
@@ -87,9 +87,9 @@ done
 
 cp ../cloud/cloud/cloud_portal.yaml $TARGET_DIR/_source
 
-BAN_LIST="nx witness|nx witness client|nx platform|nx1|nxvms|nx cloud"
+BAN_LIST="nx witness|nx witness client|nx platform|nxvms|nx cloud"
 echo "Checking files for mentions of nx with the following patterns: ${BAN_LIST}"
-branding=$(grep -iE "$BAN_LIST" --exclude=fonts --include=*.html -rl ${TARGET_DIR}/_source) || true
+branding=$(grep -iE "$BAN_LIST" --exclude=fonts -rl ${TARGET_DIR}/_source) || true
 if [[ -z ${branding} ]]
 then
     echo "No mentions were found"
