6f4f3853a45712e62fb31ce018376f6be89b5512
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-04-10 14:50:47 -0700

    META-235: Analytics DB: time period manipulation routines moved to a separate class
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/libs/common/src/analytics/detected_objects_storage/analytics_events_storage.cpp
+++ vms/libs/common/src/analytics/detected_objects_storage/analytics_events_storage.cpp
@@ -17,8 +17,6 @@ namespace storage {
 static constexpr char kSaveEventQueryAggregationKey[] = "c119fb61-b7d3-42c5-b833-456437eaa7c7";
 static constexpr int kUsecPerMsec = 1000;
 
-static constexpr auto kMaxTimePeriodLength = std::chrono::minutes(10);
-static constexpr auto kTimePeriodPreemptiveLength = std::chrono::minutes(1);
 static constexpr int kMaxObjectLookupResultSet = 1000;
 
 // NOTE: Limiting filtered_events subquery to make query
@@ -35,37 +33,12 @@ static constexpr auto kMaxCachedObjectLifeTime = std::chrono::minutes(1);
 static constexpr auto kTrackSearchResolutionX = 44;
 static constexpr auto kTrackSearchResolutionY = 32;
 
-namespace detail {
-
-std::chrono::milliseconds TimePeriod::length() const
-{
-    return endTime - startTime;
-}
-
-bool TimePeriod::addPacketToPeriod(
-    std::chrono::milliseconds timestamp,
-    std::chrono::milliseconds duration)
-{
-    if (length() > kMaxTimePeriodLength ||
-        startTime > timestamp || //< Packet from the past?
-        (timestamp > endTime && timestamp - endTime > kMinTimePeriodAggregationPeriod))
-    {
-        return false;
-    }
-
-    if (endTime < timestamp + duration)
-        endTime = timestamp + duration;
-
-    return true;
-}
-
-} // namespace detail
-
 //-------------------------------------------------------------------------------------------------
 
 EventsStorage::EventsStorage(const Settings& settings):
     m_settings(settings),
     m_dbController(settings.dbConnectionOptions),
+    m_timePeriodDao(&m_deviceDao),
     m_objectCache(kTrackAggregationPeriod, kMaxCachedObjectLifeTime),
     m_trackAggregator(
         kTrackSearchResolutionX,
@@ -281,7 +254,7 @@ bool EventsStorage::loadDictionaries()
             {
                 m_objectTypeDao.loadObjectTypeDictionary(queryContext);
                 m_deviceDao.loadDeviceDictionary(queryContext);
-                fillCurrentTimePeriodsCache(queryContext);
+                m_timePeriodDao.fillCurrentTimePeriodsCache(queryContext);
                 // TODO: #ak Remove when executeSelectQuerySync supports void functors.
                 return true;
             });
@@ -307,7 +280,7 @@ sql::DBResult EventsStorage::savePacket(
             queryContext,
             detectedObject.labels);
 
-        const auto timePeriodId = insertOrUpdateTimePeriod(queryContext, *packet);
+        const auto timePeriodId = m_timePeriodDao.insertOrUpdateTimePeriod(queryContext, *packet);
 
         insertEvent(
             queryContext,
@@ -374,142 +347,6 @@ void EventsStorage::insertEvent(
         packCoordinate(detectedObject.boundingBox.bottomRight().y()));
 
     insertEventQuery.exec();
-}
-
-long long EventsStorage::insertOrUpdateTimePeriod(
-    nx::sql::QueryContext* queryContext,
-    const common::metadata::DetectionMetadataPacket& packet)
-{
-    using namespace std::chrono;
-
-    const auto packetTimestamp = duration_cast<milliseconds>(microseconds(packet.timestampUsec));
-    const auto packetDuration = duration_cast<milliseconds>(microseconds(packet.durationUsec));
-    const auto deviceId = m_deviceDao.deviceIdFromGuid(packet.deviceId);
-
-    auto currentPeriod = insertOrFetchCurrentTimePeriod(
-        queryContext, deviceId, packetTimestamp, packetDuration);
-
-    if (!currentPeriod->addPacketToPeriod(packetTimestamp, packetDuration))
-    {
-        closeCurrentTimePeriod(queryContext, deviceId);
-
-        // Adding new period.
-        currentPeriod = insertOrFetchCurrentTimePeriod(
-            queryContext, deviceId, packetTimestamp, packetDuration);
-    }
-    else if (currentPeriod->lastSavedEndTime < currentPeriod->endTime)
-    {
-        currentPeriod->lastSavedEndTime = currentPeriod->endTime + kTimePeriodPreemptiveLength;
-
-        saveTimePeriodEnd(
-            queryContext,
-            currentPeriod->id,
-            currentPeriod->lastSavedEndTime);
-    }
-
-    return currentPeriod->id;
-}
-
-detail::TimePeriod* EventsStorage::insertOrFetchCurrentTimePeriod(
-    nx::sql::QueryContext* queryContext,
-    long long deviceId,
-    std::chrono::milliseconds packetTimestamp,
-    std::chrono::milliseconds packetDuration)
-{
-    auto periodIter = m_deviceIdToCurrentTimePeriod.find(deviceId);
-    if (periodIter != m_deviceIdToCurrentTimePeriod.end())
-        return &periodIter->second;
-
-    detail::TimePeriod timePeriod;
-
-    timePeriod.deviceId = deviceId;
-    timePeriod.startTime = packetTimestamp;
-    timePeriod.endTime = packetTimestamp + packetDuration;
-
-    auto query = queryContext->connection()->createQuery();
-    query->prepare(R"sql(
-        INSERT INTO time_period_full(device_id, period_start_ms, period_end_ms)
-        VALUES (:deviceId, :periodStartMs, :periodEndMs)
-    )sql");
-    query->bindValue(":deviceId", deviceId);
-    query->bindValue(":periodStartMs", (long long) packetTimestamp.count());
-    // Filling period end so that this period is used in queries.
-    query->bindValue(":periodEndMs", (long long) (timePeriod.endTime + kTimePeriodPreemptiveLength).count());
-
-    query->exec();
-
-    timePeriod.id = query->lastInsertId().toLongLong();
-    timePeriod.lastSavedEndTime = timePeriod.endTime + kTimePeriodPreemptiveLength;
-
-    periodIter = m_deviceIdToCurrentTimePeriod.emplace(deviceId, timePeriod).first;
-    m_idToTimePeriod[periodIter->second.id] = periodIter;
-
-    NX_VERBOSE(this, "Added new time period %1 (device %2, start time %3)",
-        timePeriod.id, m_deviceDao.deviceGuidFromId(timePeriod.deviceId), timePeriod.startTime);
-
-    return &periodIter->second;
-}
-
-void EventsStorage::closeCurrentTimePeriod(
-    nx::sql::QueryContext* queryContext,
-    long long deviceId)
-{
-    auto periodIter = m_deviceIdToCurrentTimePeriod.find(deviceId);
-    NX_ASSERT(periodIter != m_deviceIdToCurrentTimePeriod.end());
-    if (periodIter == m_deviceIdToCurrentTimePeriod.end())
-        return;
-
-    saveTimePeriodEnd(
-        queryContext,
-        periodIter->second.id,
-        periodIter->second.endTime);
-
-    NX_VERBOSE(this, "Closed time period %1 (device %2)",
-        periodIter->second.id, m_deviceDao.deviceGuidFromId(deviceId));
-
-    m_idToTimePeriod.erase(periodIter->second.id);
-    m_deviceIdToCurrentTimePeriod.erase(periodIter);
-}
-
-void EventsStorage::saveTimePeriodEnd(
-    nx::sql::QueryContext* queryContext,
-    long long id,
-    std::chrono::milliseconds endTime)
-{
-    auto query = queryContext->connection()->createQuery();
-    query->prepare(
-        "UPDATE time_period_full SET period_end_ms=:periodEndMs WHERE id=:id");
-
-    query->bindValue(":id", id);
-    query->bindValue(":periodEndMs", (long long) endTime.count());
-    query->exec();
-
-    NX_VERBOSE(this, "Updated end of time period %1 to %2", id, endTime);
-}
-
-void EventsStorage::fillCurrentTimePeriodsCache(
-    nx::sql::QueryContext* queryContext)
-{
-    auto query = queryContext->connection()->createQuery();
-    query->prepare(R"sql(
-        SELECT id, device_id, period_start_ms, period_end_ms
-        FROM time_period_full
-        GROUP BY device_id
-        HAVING max(rowid)
-    )sql");
-
-    while (query->next())
-    {
-        detail::TimePeriod timePeriod;
-
-        timePeriod.id = query->value("id").toLongLong();
-        timePeriod.deviceId = query->value("device_id").toLongLong();
-        timePeriod.startTime = std::chrono::milliseconds(query->value("period_start_ms").toLongLong());
-        timePeriod.endTime = std::chrono::milliseconds(query->value("period_end_ms").toLongLong());
-
-        auto it = m_deviceIdToCurrentTimePeriod.emplace(timePeriod.deviceId, timePeriod).first;
-        m_idToTimePeriod[timePeriod.id] = it;
-    }
 }
 
 void EventsStorage::prepareCursorQuery(
@@ -977,8 +814,11 @@ void EventsStorage::loadTimePeriods(
 
         // Fixing time period end if needed.
         const auto id = query->value("id").toLongLong();
-        if (auto it = m_idToTimePeriod.find(id); it != m_idToTimePeriod.end())
-            timePeriod.setEndTime(it->second->second.endTime);
+        if (auto detailTimePeriod = m_timePeriodDao.getTimePeriodById(id);
+            detailTimePeriod)
+        {
+            timePeriod.setEndTime(detailTimePeriod->endTime);
+        }
 
         // Truncating time period by the filter
         if (!timePeriodFilter.isEmpty())
