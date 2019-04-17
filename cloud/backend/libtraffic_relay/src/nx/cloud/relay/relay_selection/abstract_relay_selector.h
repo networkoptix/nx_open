51354d146c06cb961392fb68ff2ac57f69cabb47
Nate Wilson <nwilson@networkoptix.com>
2019-04-17 16:02:01 -0700

    CLOUD-2849: Mac compilation Fix
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/backend/libtraffic_relay/src/nx/cloud/relay/relay_selection/abstract_relay_selector.h
+++ cloud/backend/libtraffic_relay/src/nx/cloud/relay/relay_selection/abstract_relay_selector.h
@@ -7,6 +7,7 @@ namespace nx::cloud::relay {
 class AbstractRelaySelector
 {
 public:
+    virtual ~AbstractRelaySelector() = default;
     virtual std::string selectRelay(const std::vector<std::string>& relays) const = 0;
 };
 
