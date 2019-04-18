44f1fb2e87964e18bfdefe50773076ebe5840d95
Sergey Ivanov <sivanov@networkoptix.com>
2019-04-18 15:10:37 +0300

    RDep update
    <==== Mercurial data ====>
    Branch: vms_3.2

--- build_utils/python/rdep_config.py
+++ build_utils/python/rdep_config.py
@@ -1,6 +1,11 @@
 import os
-import ConfigParser
 import time
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
 
 class ConfigHelper:
     FILE_NAME = None
@@ -10,10 +15,10 @@ class ConfigHelper:
             self.__file_name = os.path.join(path, self.FILE_NAME)
         else:
             self.__file_name = path
-        self.__config = ConfigParser.ConfigParser()
+        self.__config = configparser.ConfigParser()
         try:
             self.__config.read(self.__file_name)
-        except:
+        except Exception:
             pass
 
     def get_file_name(self):
@@ -31,7 +36,7 @@ class ConfigHelper:
     def get_items(self, section):
         return self.__config.items(section)
 
-    def get_value(self, section, option, default_value = None):
+    def get_value(self, section, option, default_value=None):
         if not self.__config.has_option(section, option):
             return default_value
 
@@ -54,18 +59,22 @@ class RdepConfig(ConfigHelper):
 
     def get_name(self):
         return self.get_value("General", "name", "").strip()
+
     def set_name(self, name):
         self.set_value("General", "name", name)
 
     def get_ssh(self):
         return self.get_value("General", "ssh")
+
     def set_ssh(self, ssh):
         self.set_value("General", "ssh", ssh)
 
-    def get_rsync(self, default_value = None):
+    def get_rsync(self, default_value=None):
         return self.get_value("General", "rsync", default_value)
+
     def set_rsync(self, rsync):
         self.set_value("General", "rsync", rsync)
+
 
 class RepositoryConfig(ConfigHelper):
     FILE_NAME = ".rdep"
@@ -75,18 +84,22 @@ class RepositoryConfig(ConfigHelper):
 
     def get_url(self):
         return self.get_value("General", "url")
+
     def set_url(self, url):
         self.set_value("General", "url", url)
 
-    def get_push_url(self, default_value = None):
+    def get_push_url(self, default_value=None):
         return self.get_value("General", "push_url", default_value)
+
     def set_push_url(self, push_url):
         self.set_value("General", "push_url", push_url)
 
     def get_ssh(self):
         return self.get_value("General", "ssh")
+
     def set_ssh(self, ssh):
         return self.set_value("General", "ssh", ssh)
+
 
 class PackageConfig(ConfigHelper):
     FILE_NAME = ".rdpack"
@@ -94,20 +107,22 @@ class PackageConfig(ConfigHelper):
     def __init__(self, file_name):
         ConfigHelper.__init__(self, file_name)
 
-    def get_timestamp(self, default_value = None):
+    def get_timestamp(self, default_value=None):
         timestamp = self.get_value("General", "time", default_value)
-        return None if timestamp == None else int(timestamp)
+        return int(timestamp) if timestamp else None
+
     def update_timestamp(self):
-        self.set_value("General", "time", int(time.time()))
+        self.set_value("General", "time", str(int(time.time())))
 
     def get_uploader(self):
         return self.get_value("General", "uploader")
+
     def set_uploader(self, name):
         self.set_value("General", "uploader", name)
 
     def get_copy_list(self):
         if not self.has_section("Copy"):
-            return { "bin": [ "bin/*" ], "lib": [ "lib/*.so*", "lib/*.dylib" ] }
+            return {"bin": ["bin/*"], "lib": ["lib/*.so*", "lib/*.dylib"]}
         result = {}
         for key, value in self.get_items("Copy"):
             result[key] = value.split()
