19f2a322ba9bd2114e1d2d9b51c729e7caebb3e7
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-04-19 10:22:47 -0700

    VMS-CR-16402 META-235: Review fix
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/libs/common/src/analytics/detected_objects_storage/device_dao.cpp
+++ vms/libs/common/src/analytics/detected_objects_storage/device_dao.cpp
@@ -4,16 +4,7 @@
 
 namespace nx::analytics::storage {
 
-void DeviceDao::addDeviceToDictionary(
-    long long id, const QnUuid& deviceGuid)
-{
-    QnMutexLocker locker(&m_mutex);
-
-    m_deviceGuidToId.emplace(deviceGuid, id);
-    m_idToDeviceGuid.emplace(id, deviceGuid);
-}
-
-long long DeviceDao::deviceIdFromGuid(const QnUuid& deviceGuid) const
+int DeviceDao::deviceIdFromGuid(const QnUuid& deviceGuid) const
 {
     QnMutexLocker locker(&m_mutex);
 
@@ -21,7 +12,7 @@ long long DeviceDao::deviceIdFromGuid(const QnUuid& deviceGuid) const
     return it != m_deviceGuidToId.end() ? it->second : -1;
 }
 
-QnUuid DeviceDao::deviceGuidFromId(long long id) const
+QnUuid DeviceDao::deviceGuidFromId(int id) const
 {
     QnMutexLocker locker(&m_mutex);
 
@@ -29,7 +20,7 @@ QnUuid DeviceDao::deviceGuidFromId(long long id) const
     return it != m_idToDeviceGuid.end() ? it->second : QnUuid();
 }
 
-long long DeviceDao::insertOrFetch(
+int DeviceDao::insertOrFetch(
     nx::sql::QueryContext* queryContext,
     const QnUuid& deviceGuid)
 {
@@ -61,6 +52,15 @@ void DeviceDao::loadDeviceDictionary(nx::sql::QueryContext* queryContext)
             query->value(0).toLongLong(),
             QnSql::deserialized_field<QnUuid>(query->value(1)));
     }
+}
+
+void DeviceDao::addDeviceToDictionary(
+    int id, const QnUuid& deviceGuid)
+{
+    QnMutexLocker locker(&m_mutex);
+
+    m_deviceGuidToId.emplace(deviceGuid, id);
+    m_idToDeviceGuid.emplace(id, deviceGuid);
 }
 
 } // namespace nx::analytics::storage
