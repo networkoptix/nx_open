5c5435177f54b39dd27716dad7523ebc4295d317
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-04-22 11:51:06 -0700

    META-235: Removed unused DB field
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/libs/common/src/analytics/detected_objects_storage/detection_data_saver.cpp
+++ vms/libs/common/src/analytics/detected_objects_storage/detection_data_saver.cpp
@@ -1,7 +1,6 @@
 #include "detection_data_saver.h"
 
 #include <nx/fusion/serialization/sql_functions.h>
-#include <nx/utils/compact_int.h>
 
 #include "attributes_dao.h"
 #include "config.h"
@@ -185,9 +184,8 @@ void DetectionDataSaver::saveObjectSearchData(nx::sql::QueryContext* queryContex
     auto insertObjectSearchCell = queryContext->connection()->createQuery();
     insertObjectSearchCell->prepare(R"sql(
         INSERT INTO object_search(timestamp_seconds_utc,
-            box_top_left_x, box_top_left_y, box_bottom_right_x, box_bottom_right_y,
-            object_id_list)
-        VALUES (?, ?, ?, ?, ?, ?)
+            box_top_left_x, box_top_left_y, box_bottom_right_x, box_bottom_right_y)
+        VALUES (?, ?, ?, ?, ?)
     )sql");
 
     auto insertObjectSearchToAttributesBinding = queryContext->connection()->createQuery();
@@ -205,9 +203,6 @@ void DetectionDataSaver::saveObjectSearchData(nx::sql::QueryContext* queryContex
         insertObjectSearchCell->bindValue(2, objectSearchGridCell.boundingBox.topLeft().y());
         insertObjectSearchCell->bindValue(3, objectSearchGridCell.boundingBox.bottomRight().x());
         insertObjectSearchCell->bindValue(4, objectSearchGridCell.boundingBox.bottomRight().y());
-
-        insertObjectSearchCell->bindValue(5,
-            nx::utils::compact_int::serialized(toDbIds(objectSearchGridCell.objectIds)));
 
         insertObjectSearchCell->exec();
         const auto objectSearchCellId = insertObjectSearchCell->lastInsertId().toLongLong();
