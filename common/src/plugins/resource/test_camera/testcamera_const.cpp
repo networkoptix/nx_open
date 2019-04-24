9a77aa032b9f9618529a9b8432ed3d796ccc7ee6
Dmitry Liman <dliman@networkoptix.com>
2019-04-24 15:07:56 +0300

    VMS-13699: Make 3.2 server compliant with 4.0 testcamera
    <==== Mercurial data ====>
    Branch: vms_3.2

--- common/src/plugins/resource/test_camera/testcamera_const.cpp
+++ common/src/plugins/resource/test_camera/testcamera_const.cpp
@@ -1,5 +1,5 @@
 #include "testcamera_const.h"
 
 const int TestCamConst::DISCOVERY_PORT = 4984;
-const QByteArray TestCamConst::TEST_CAMERA_FIND_MSG("Network optix camera emulator 3.0 discovery");
-const QByteArray TestCamConst::TEST_CAMERA_ID_MSG("Network optix camera emulator 3.0 responce");
+const QByteArray TestCamConst::TEST_CAMERA_FIND_MSG("Network Optix Camera Emulator 3.0 discovery\n");
+const QByteArray TestCamConst::TEST_CAMERA_ID_MSG("Network Optix Camera Emulator 3.0 discovery response\n");
