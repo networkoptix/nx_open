4e5b58cf4ed6dcfe2cc3bdc156d30efe7f1762fa
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-04-24 11:58:57 -0700

    ionetworks intro video uploaded and developer form disabled
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- customization/ionetworks/mediaserver_core/resources/static/customization/config.js
+++ customization/ionetworks/mediaserver_core/resources/static/customization/config.js
@@ -10,3 +10,4 @@ Config.productName = 'EZ Pro';
 Config.cloud.productName = 'EZ Pro Cloud';
 Config.defaultLanguage = 'en_US';
 Config.supportedLanguages = ['en_US', 'en_GB', 'fr_FR', 'de_DE', 'ru_RU', 'es_ES', 'ja_JP', 'ko_KR', 'tr_TR', 'zh_CN', 'zh_TW', 'hu_HU', 'he_IL', 'nl_NL', 'pl_PL', 'vi_VN', 'th_TH'];
+Config.developersFeedbackForm = false;
