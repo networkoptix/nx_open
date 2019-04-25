a4616c18a5b6776a4ee9146af48e213a5f96e63b
Nate Wilson <nwilson@networkoptix.com>
2019-04-25 14:17:01 -0700

    CLOUD-2850: Implement lookupCountry and lookupContinent
    <==== Mercurial data ====>
    Branch: mediator_relay_selection

--- libs/nx_maxminddb/nx_maxminddb/src/nx/maxmind/maxmind_db.cpp
+++ libs/nx_maxminddb/nx_maxminddb/src/nx/maxmind/maxmind_db.cpp
@@ -14,6 +14,31 @@ struct Mmdb: public MMDB_s
     }
 };
 
+void dump(MMDB_lookup_result_s& lookupResult, const std::string& ipAddress)
+{
+    MMDB_entry_data_list_s *entry_data_list = NULL;
+    int exit_code = 0;
+    if (lookupResult.found_entry)
+    {
+        int status = MMDB_get_entry_data_list(&lookupResult.entry, &entry_data_list);
+
+        if (MMDB_SUCCESS != status)
+        {
+            fprintf(stderr, "Got an error looking up the entry data - %s\n", MMDB_strerror(status));
+            return;
+        }
+
+        if (NULL != entry_data_list)
+        {
+            MMDB_dump_entry_data_list(stdout, entry_data_list, 2);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "\n  No entry for this IP address (%s) was found\n\n", ipAddress.c_str());
+    }
+}
+
 } // namespace
 
 //-------------------------------------------------------------------------------------------------
@@ -21,12 +46,15 @@ struct Mmdb: public MMDB_s
 
 bool MaxmindDb::open(const std::string& dbPath)
 {
-    if (m_db)
-        m_db.reset();
+#ifdef _WIN32
+    auto path = QString(dbPath.c_str()).toUtf8();
+#else
+    auto path = dpPath.c_str();
+#endif
 
     auto db = std::make_unique<Mmdb>();
 
-    int result = MMDB_open(dbPath.c_str(), MMDB_MODE_MMAP, db.get());
+    int result = MMDB_open(path, MMDB_MODE_MMAP, db.get());
     if (result != MMDB_SUCCESS)
     {
         NX_VERBOSE(this, "Failed to open maxmind db file: %1, error: %2", MMDB_strerror(result));
@@ -36,6 +64,70 @@ bool MaxmindDb::open(const std::string& dbPath)
     m_db = std::move(db);
 
     return true;
+}
+
+std::string MaxmindDb::lookupCountry(const std::string& ipAddress, const std::string& language)
+{
+    auto lookupResult = lookupIpAddress(ipAddress);
+    if (!lookupResult)
+        return {};
+
+    int mmdbError = MMDB_SUCCESS;
+    std::string value;
+    std::tie(mmdbError, value) =
+        detail::getStringValue(*lookupResult, "country", "names", language.c_str());
+
+    if (mmdbError != MMDB_SUCCESS)
+    {
+        NX_VERBOSE(this, "Error from MMDB_get_value: %1", MMDB_strerror(mmdbError));
+        return {};
+    }
+
+    return value;
+}
+
+std::string MaxmindDb::lookupContinent(const std::string& ipAddress, const std::string& language)
+{
+    auto lookupResult = lookupIpAddress(ipAddress);
+    if (!lookupResult)
+        return {};
+
+    int mmdbError = MMDB_SUCCESS;
+    std::string value;
+    std::tie(mmdbError, value) =
+        detail::getStringValue(*lookupResult, "continent", "names", language.c_str());
+
+    if (mmdbError != MMDB_SUCCESS)
+    {
+        NX_VERBOSE(this, "Error from MMDB_get_value: %1", MMDB_strerror(mmdbError));
+        return {};
+    }
+
+    return value;
+}
+
+std::optional<MMDB_lookup_result_s> MaxmindDb::lookupIpAddress(const std::string& ipAddress)
+{
+    if (!m_db)
+        return std::nullopt;
+
+    int gaiError = 0;
+    int mmdbError = MMDB_SUCCESS;
+    auto lookupResult = MMDB_lookup_string(m_db.get(), ipAddress.c_str(), &gaiError, &mmdbError);
+
+    if (gaiError != 0)
+    {
+        NX_VERBOSE(this, "Error from getaddrinfo: %1", gai_strerror(gaiError));
+        return std::nullopt;
+    }
+
+    if (mmdbError != MMDB_SUCCESS)
+    {
+        NX_VERBOSE(this, "Error from MMDB_lookup_string: %1", MMDB_strerror(mmdbError));
+        return std::nullopt;
+    }
+
+    return lookupResult;
 }
 
 } // namespace nx::maxmind
--- libs/nx_maxminddb/nx_maxminddb/src/nx/maxmind/maxmind_db.h
+++ libs/nx_maxminddb/nx_maxminddb/src/nx/maxmind/maxmind_db.h
@@ -2,15 +2,51 @@
 
 #include <string>
 #include <memory>
+#include <optional>
 
 #include <maxminddb.h>
 
 namespace nx::maxmind {
 
+namespace detail {
+
+template<typename ...ConstCharPtrs>
+std::pair<int /*MMDB status*/, std::string> getStringValue(
+    MMDB_lookup_result_s& lookupResult,
+    ConstCharPtrs... strings)
+{
+    if (!lookupResult.found_entry)
+        return {0, {}};
+
+    MMDB_entry_data_s entryData;
+    int result = MMDB_get_value(&lookupResult.entry, &entryData, strings..., nullptr);
+
+    if (result != MMDB_SUCCESS)
+        return {result, {}};
+
+    if (entryData.type != MMDB_DATA_TYPE_UTF8_STRING)
+        return {result, {}};
+
+    return std::make_pair(
+        result,
+        QString::fromUtf8(entryData.utf8_string, entryData.data_size).toStdString());
+}
+
+} // namespace detail
+
+//-------------------------------------------------------------------------------------------------
+// MaxmindDb
+
 class NX_MAXMINDDB_API MaxmindDb
 {
 public:
     bool open(const std::string& dbPath);
+
+    std::string lookupCountry(const std::string& ipAddress, const std::string& language = "en");
+    std::string lookupContinent(const std::string& ipAddress, const std::string& language = "en");
+
+private:
+    std::optional<MMDB_lookup_result_s> lookupIpAddress(const std::string& ipAddress);
 
 private:
     std::unique_ptr<MMDB_s> m_db;
