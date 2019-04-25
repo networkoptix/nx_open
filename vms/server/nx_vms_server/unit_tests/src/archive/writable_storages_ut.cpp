cbadd39bd54b53e01c05ab586b715eedfadcb156
Alexander Kulikov <akulikov@networkoptix.com>
2019-04-25 19:23:42 +0300

    VMS-13820: Start with functional tests
    <==== Mercurial data ====>
    Branch: x_vms_13820_WritableStoragesRefactoring_4.1

---
+++ vms/server/nx_vms_server/unit_tests/src/archive/writable_storages_ut.cpp
@@ -0,0 +1,14 @@
+#include <recorder/storage_manager.h>
+
+#include <gtest/gtest.h>
+
+namespace nx::vms::server::test {
+
+class WritableStorageManager: public ::testing::Test
+{
+protected:
+private:
+
+};
+
+} // namespace nx::vms::server::test
