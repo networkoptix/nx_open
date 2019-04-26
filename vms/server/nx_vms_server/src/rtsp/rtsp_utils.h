1d294e9c4187daf371db186707cb1dfc63768fdf
Leonid Busygin <lbusygin@networkoptix.com>
2019-04-26 20:10:59 +0300

    VMS-12235: Fixed parsing of rtsp url params
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/server/nx_vms_server/src/rtsp/rtsp_utils.h
+++ vms/server/nx_vms_server/src/rtsp/rtsp_utils.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <QtCore/QString>
+#include <QtCore/QSize>
+#include <QtCore/QUrlQuery>
+
+#include <optional>
+#include <nx/streaming/media_data_packet.h>
 
 extern "C"
 {
@@ -9,6 +14,25 @@ extern "C"
 
 namespace nx::rtsp {
 
+struct UrlParams
+{
+    bool parse(const QUrlQuery& query);
+    QString getParseError() { return error; }
+
+    std::optional<AVCodecID> codec;
+    std::optional<int64_t> position;
+    std::optional<QSize> resolution;
+    std::optional<MediaQuality> quality;
+    //TODO std::optional<int> rotation;
+    //TODO std::optional<QString> speed;
+    //TODO bool multiplePayloadTypes = false;
+
+private:
+    QString error;
+};
+
 AVCodecID findEncoderCodecId(const QString& codecName);
+
+QSize parseResolution(const QString& str);
 
 } // namespace nx::rtsp
