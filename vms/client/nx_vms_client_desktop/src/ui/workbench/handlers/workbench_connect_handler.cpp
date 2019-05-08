f38400ae87ca8b3c515afe942b30397eb484e747
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2019-05-08 14:34:14 +0300

    VMS-13766: Fix comments spelling
    <==== Mercurial data ====>
    Branch: x_VMS-13766-client-connect-timeout-v2

--- vms/client/nx_vms_client_desktop/src/ui/workbench/handlers/workbench_connect_handler.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/workbench/handlers/workbench_connect_handler.cpp
@@ -218,7 +218,7 @@ QnWorkbenchConnectHandler::QnWorkbenchConnectHandler(QObject* parent):
     connect(qnClientMessageProcessor, &QnClientMessageProcessor::initialResourcesReceived, this,
         &QnWorkbenchConnectHandler::at_messageProcessor_initialResourcesReceived);
 
-    // The initialResourcesReceived signal may be never emitted if the server has issues.
+    // The initialResourcesReceived signal may never be emitted if the server has issues.
     // Avoid infinite "UI loading" state by forcibly dropping the connections after a timeout.
     const auto connectTimeout = ini().connectTimeoutMs;
     if (connectTimeout > 0)
