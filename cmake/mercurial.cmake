2e34d5537a8865d1ac8d070a0d112de2593bf479
Dmitry Liman <dliman@networkoptix.com>
2019-05-15 21:34:14 +0300

    ENV-203: Support git in cmake scripts
    <==== Mercurial data ====>
    Branch: vms_4.0

--- cmake/mercurial.cmake
+++ cmake/mercurial.cmake
@@ -1,9 +1,32 @@
+function(vcs_changeset dir var)
+    if(EXISTS ${dir}/.git/)
+        git_changeset(${dir} ${var})
+    elseif(EXISTS ${dir}/.hg/)
+        hg_changeset(${dir} ${var})
+    else()
+        message(WARNING "Can't get changeset: not found any VCS")
+    endif()
+    set(${var} ${${var}} PARENT_SCOPE)
+endfunction()
+
+function(vcs_branch dir var)
+    if(EXISTS ${dir}/.git/)
+        git_branch(${dir} ${var})
+    elseif(EXISTS ${dir}/.hg/)
+        hg_branch(${dir} ${var})
+    else()
+        message(WARNING "Can't get current branch: not found any VCS")
+    endif()
+    set(${var} ${${var}} PARENT_SCOPE)
+endfunction()
+
 function(hg_changeset dir var)
     execute_process(
-        COMMAND hg --repository "${dir}" log --rev . --template "{node|short}"
+        COMMAND hg --repository "${dir}" id -i
         OUTPUT_VARIABLE changeset
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+    string(REPLACE "+" "" changeset "${changeset}")
     set(${var} ${changeset} PARENT_SCOPE)
 endfunction()
 
@@ -16,21 +39,39 @@ function(hg_branch dir var)
     set(${var} "${branch}" PARENT_SCOPE)
 endfunction()
 
-function(hg_last_commit_branch dir var)
+function(git_changeset dir var)
     execute_process(
-        COMMAND hg --repository "${dir}" log --template "{branch}" -r .
-        OUTPUT_VARIABLE parent_branch
+        COMMAND git -C "${dir}" rev-parse HEAD
+        OUTPUT_VARIABLE changeset
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    set(${var} "${parent_branch}" PARENT_SCOPE)
+    set(${var} ${changeset} PARENT_SCOPE)
 endfunction()
 
-function(hg_parent_branch dir branch var)
+function(git_branch dir var)
     execute_process(
-        COMMAND hg --repository "${dir}"
-            log --template "{branch}" -r "parents(min(branch(${branch})))"
-        OUTPUT_VARIABLE parent_branch
+        COMMAND git -C "${dir}" rev-parse --abbrev-ref HEAD
+        OUTPUT_VARIABLE branch
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    set(${var} "${parent_branch}" PARENT_SCOPE)
+    set(${var} "${branch}" PARENT_SCOPE)
 endfunction()
+
+#function(hg_last_commit_branch dir var)
+#    execute_process(
+#        COMMAND hg --repository "${dir}" log --template "{branch}" -r -1
+#        OUTPUT_VARIABLE parent_branch
+#        OUTPUT_STRIP_TRAILING_WHITESPACE
+#    )
+#    set(${var} "${parent_branch}" PARENT_SCOPE)
+#endfunction()
+
+#function(hg_parent_branch dir branch var)
+#    execute_process(
+#        COMMAND hg --repository "${dir}"
+#            log --template "{branch}" -r "parents(min(branch(${branch})))"
+#        OUTPUT_VARIABLE parent_branch
+#        OUTPUT_STRIP_TRAILING_WHITESPACE
+#    )
+#    set(${var} "${parent_branch}" PARENT_SCOPE)
+#endfunction()
