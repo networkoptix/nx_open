c17faa044cfb4ead796deb2959f6c505f2305743
Sergey Ivanov <sivanov@networkoptix.com>
2019-05-17 13:50:10 +0300

    Compilation fix after merge
    <==== Mercurial data ====>
    Branch: vms

--- vms/server/nx_vms_server/src/rest/handlers/cookie_login_rest_handler.cpp
+++ vms/server/nx_vms_server/src/rest/handlers/cookie_login_rest_handler.cpp
@@ -64,7 +64,7 @@ rest::Response QnCookieLoginRestHandler::executePost(const rest::Request& reques
         auto auditManager = request.owner->commonModule()->auditManager();
         auditManager->addAuditRecord(auditManager->prepareRecord(session, Qn::AR_UnauthorizedLogin));
         NX_DEBUG(this, "Failure to authenticate request %1 with error %2. userName: %3",
-            request.path,
+            request.path(),
             Qn::toErrorMessage(authResult),
             session.userName);
 
