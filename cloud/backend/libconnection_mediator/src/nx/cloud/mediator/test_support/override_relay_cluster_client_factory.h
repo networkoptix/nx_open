5e0e89c26a23ca0a17e455590029b09c3bfec468
Nate Wilson <nwilson@networkoptix.com>
2019-05-20 13:24:36 -0700

    CLOUD-3023: Fix bad indenting
    <==== Mercurial data ====>
    Branch: mediator_relay_selection

--- cloud/backend/libconnection_mediator/src/nx/cloud/mediator/test_support/override_relay_cluster_client_factory.h
+++ cloud/backend/libconnection_mediator/src/nx/cloud/mediator/test_support/override_relay_cluster_client_factory.h
@@ -5,36 +5,36 @@
 
 namespace nx::hpm::test {
 
-	/**
-	 * For test purposes, overrides OnlineRelaysClusterClient implementation of
-	 * AbstractRelayClusterClient with one
-	 * that does not throw an exception when there is no Geolite2-*.mmdb database file.
-	 */
-	class OverrideRelayClusterClientFactory
+/**
+* For test purposes, overrides OnlineRelaysClusterClient implementation of
+* AbstractRelayClusterClient with one
+* that does not throw an exception when there is no Geolite2-*.mmdb database file.
+*/
+class OverrideRelayClusterClientFactory
+{
+public:
+	OverrideRelayClusterClientFactory()
 	{
-	public:
-		OverrideRelayClusterClientFactory()
-		{
-			m_relayClusterClientFactoryFuncBak =
-				nx::hpm::RelayClusterClientFactory::instance().setCustomFunc(
-					[](const nx::hpm::conf::Settings& settings)
-					{
-						return std::make_unique<nx::hpm::RelayClusterClient>(settings);
-					});
-		}
+		m_relayClusterClientFactoryFuncBak =
+			nx::hpm::RelayClusterClientFactory::instance().setCustomFunc(
+				[](const nx::hpm::conf::Settings& settings)
+				{
+					return std::make_unique<nx::hpm::RelayClusterClient>(settings);
+				});
+	}
 
-		~OverrideRelayClusterClientFactory()
+	~OverrideRelayClusterClientFactory()
+	{
+		if (m_relayClusterClientFactoryFuncBak)
 		{
-			if (m_relayClusterClientFactoryFuncBak)
-			{
-				nx::hpm::RelayClusterClientFactory::instance().setCustomFunc(
-					std::move(m_relayClusterClientFactoryFuncBak));
-			}
+			nx::hpm::RelayClusterClientFactory::instance().setCustomFunc(
+				std::move(m_relayClusterClientFactoryFuncBak));
 		}
+	}
 
-	private:
-		nx::utils::MoveOnlyFunc<
-			nx::hpm::RelayClusterClientFactoryFunction> m_relayClusterClientFactoryFuncBak;
-	};
+private:
+	nx::utils::MoveOnlyFunc<
+		nx::hpm::RelayClusterClientFactoryFunction> m_relayClusterClientFactoryFuncBak;
+};
 
 } // namespace nx::hpm::test
