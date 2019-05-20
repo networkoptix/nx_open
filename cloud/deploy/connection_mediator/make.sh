491b9333e7c327f80623bdc3cb6c636143b3fba2
Nate Wilson <nwilson@networkoptix.com>
2019-05-20 15:20:59 -0700

    CLOUD-3025: more trouble shooting
    <==== Mercurial data ====>
    Branch: mediator_relay_selection

--- cloud/deploy/connection_mediator/make.sh
+++ cloud/deploy/connection_mediator/make.sh
@@ -9,6 +9,9 @@ function stage_cmake_extra()
     mkdir -p stage/${moduleName}/var
 	echo "TRYING cp -l $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/"
     cp -l $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/
+	echo "return code: $?"
+	echo "current dir: `pwd`
+	test -f "stage/${moduleName}/var/GeoLite2-City.mmdb" && echo "stage/${moduleName}/var/GeoLite2-City.mmdb exists"
 }
 
 function stage()
