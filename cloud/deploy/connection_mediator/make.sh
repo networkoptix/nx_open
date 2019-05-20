7ac6857adf5a779e0dba84242227333825735d3b
Nate Wilson <nwilson@networkoptix.com>
2019-05-20 15:12:19 -0700

    CLOUD-3025: Some trouble shooting code, needs to be undone
    <==== Mercurial data ====>
    Branch: mediator_relay_selection

--- cloud/deploy/connection_mediator/make.sh
+++ cloud/deploy/connection_mediator/make.sh
@@ -5,9 +5,15 @@
 MODULE=connection_mediator
 
 function stage_cmake_extra()
-{
+{                                                                                    	
     mkdir -p stage/${moduleName}/var
+	echo "TRYING cp -l $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/"
     cp -l $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/
+}
+
+function stage()
+{
+	stage_cpp
 }
 
 
