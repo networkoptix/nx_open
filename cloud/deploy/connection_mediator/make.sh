f9852856eb14b21eff046ea87a17d7a1998a7059
Nate Wilson <nwilson@networkoptix.com>
2019-05-20 11:35:52 -0700

    CLOUD-3025: Code cleanup
    <==== Mercurial data ====>
    Branch: mediator_relay_selection

--- cloud/deploy/connection_mediator/make.sh
+++ cloud/deploy/connection_mediator/make.sh
@@ -4,12 +4,16 @@
 
 MODULE=connection_mediator
 
+function copy_geolite_db()
+{
+    mkdir -p stage/${moduleName}/var
+    cp -rl $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/
+}
+
 function stage()
 {
     stage_cpp
-    
-    mkdir -p stage/${moduleName}/var
-    cp -rl $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/
+    copy_geolite_db
 }
 
 main $@
