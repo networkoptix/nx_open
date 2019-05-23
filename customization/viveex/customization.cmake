5c9f2cdfb9d441bd102946ae8813fbc532df6f3d
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-05-22 19:02:32 -0700

    viveex customization
    <==== Mercurial data ====>
    Branch: vms_3.2_release

---
+++ customization/viveex/customization.cmake
@@ -0,0 +1,49 @@
+# Components Namings
+set(product.name "VIVEex")
+set(display.product.name "VIVEex")
+set(product.name.short "viveex")
+set(product.appName "viveex")
+set(company.name "DAEKYO CNS")
+set(uri.protocol "viveex-vms")
+set(cloudName "VIVEex-Link")
+set(installer.name "viveex")
+set(android.alias "")
+set(android.storepass "")
+set(android.keypass "")
+set(android.packageName "com.daekyocns.viveex")
+
+# Support section
+set(companyUrl "http://www.daekyocns.com")
+set(productUrl "Http://www.daekyocns.com/viveex")
+set(supportUrl "")
+set(supportEmail "viveex@daekyo.com")
+set(supportPhone "")
+set(licenseEmail "license@daekyo.com")
+# end of Support section
+
+set(defaultWebPages "{\"Home Page\": \"https://www.daekyo.com\"}")
+
+# Custom Installer (if needed). If not leave "default"
+set(customization.upgradeCode "{e44375fd-1b2e-478a-ab72-5b0df3fa9114}")
+set(customization.clientUpgradeCode "{e31ac7eb-9e99-4b6f-8dc6-2c1056d5d2ff}")
+set(customization.serverUpgradeCode "{26a53ac7-8065-4539-b276-34e591810ad4}")
+set(customization.clientBundleUpgradeCode "{b49b8b46-33f8-42ad-84a0-44736b27456e}")
+set(customization.serverBundleUpgradeCode "{aa5ab6f1-6e1a-46a8-9587-731a502d535a}")
+set(customization.fullBundleUpgradeCode "{c6a7a801-e010-4fc6-976e-29f0e96a9ba9}")
+
+set(deb.customization.company.name "viveex")
+
+set(mac.sign.identity "Developer ID Application: Network Optix, Inc. (L6FE34GJWM)")
+set(mac.app.sign.identity "3rd Party Mac Developer Application: Network Optix, Inc. (L6FE34GJWM)")
+set(mac.pkg.sign.identity "3rd Party Mac Developer Installer: Network Optix, Inc. (L6FE34GJWM)")
+set(mac.bundle.identifier "com.daekyocns.viveex2")
+set(mac.protocol_handler_bundle.identifier "com.daekyocns.protocol_handler")
+
+set(ios.new_bundle_identifier "com.daekyocns.viveex")
+set(ios.group_identifier "com.daekyocns.viveex")
+set(ios.sign.identity "iPhone Developer: Ivan Vigasin (KZ74EE5FP2)")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0034")
---
+++ customization/viveex/mediaserver_core/resources/static/customization/config.js
@@ -0,0 +1,39 @@
+Config.helpLinks.push({
+    url: "http://digital-watchdog.com/support/contact-tech-support/",
+    title: "Support",
+    description:"Have a question about specific features of your VMS system?",
+    button:"get support",
+    target: "new" // new|frame
+});
+
+Config.helpLinks.push({
+    url: "http://digital-watchdog.com/support/tools/ip-configurator/",
+    title: "System calculator",
+    description:"Building a new VMS system or expanding your current system? Use the System Calculator to calculate suggested storage and network requirements.",
+    button: "Go to the calculator",
+    target: "new" // new|frame
+});
+
+Config.helpLinks.push({
+    urls: [
+        {
+            url: "https://itunes.apple.com/us/app/cox-business-security/id904940952?mt=8",
+            button: "iOS Client",
+            class:'appstore'
+        },
+        {
+            url: "https://play.google.com/store/apps/details?id=com.cox.hdsec",
+            class:'googleplay',
+            button: "Android Client"
+        }
+    ],
+    title: "Mobile Apps",
+    target: "new" // new|frame
+});
+
+Config.allowDebugMode = false;
+//Config.webclientEnabled = false; // Uncomment this string to disable web client
+Config.productName = 'Cox Business Security Solutions';
+Config.cloud.productName = 'Cox Cloud';
+Config.defaultLanguage = 'en_US';
+Config.supportedLanguages = ['en_US', 'en_GB', 'fr_FR', 'de_DE', 'ru_RU', 'es_ES', 'ja_JP', 'ko_KR', 'tr_TR', 'zh_CN', 'zh_TW', 'hu_HU', 'he_IL', 'nl_NL', 'pl_PL', 'vi_VN', 'th_TH'];
