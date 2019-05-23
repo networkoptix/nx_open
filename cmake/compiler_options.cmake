d6a7b52c376feb236be2bf989d538fdaa3f6029c
Denis Klychkov <dklychkov@networkoptix.com>
2019-05-23 14:10:50 +0300

    CMake: Add support for compiler sanitizers
    To enable sanitizers, pass a list to -DenabledSanitizers CMake variable.
    E.g.: -DenabledSanitizers=address;memory
    
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -37,6 +37,19 @@ add_definitions(
 set(enableSpeechSynthesizer "true")
 set(isEdgeServer "false")
 
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    set(enabledSanitizers "" CACHE STRING "A semicolon-separated list of enabled sanitizers")
+
+    foreach(sanitizer ${enabledSanitizers})
+        add_compile_options(-fsanitize=${sanitizer})
+
+        if(sanitizer STREQUAL "address")
+            string(APPEND CMAKE_SHARED_LINKER_FLAGS " -lasan")
+            string(APPEND CMAKE_EXE_LINKER_FLAGS " -lasan")
+        endif()
+    endforeach()
+endif()
+
 if(WINDOWS)
     add_definitions(
         -D_CRT_RAND_S
