f04708b9f9494d6d62dd8cfe1101e032a13675c6
Victor Breus <vbreus@networkoptix.com>
2019-05-26 01:54:07 +0300

    Qt widgets debug tool has been added
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

---
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/debug_utils/instruments/widget_profiler.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace nx::vms::client::desktop {
+
+class WidgetProfiler
+{
+public:
+    /**
+     * Saves human-readable report file with Qt widgets allocation statistics and Qt widgets
+     * hierarchy listed. May be used for detecting Qt widgets leaks, e.g. dangling menus.
+     * @param filePath Full path to report file that will be created.
+     * @param parent Pointer to root widget in generated hierarchy,
+     *     if nullptr, all allocated widgets will be listed.
+     */
+    static void saveWidgetsReport(const QString& filePath, const QWidget* parent = nullptr);
+
+    /**
+     * Generates human-readable text with widgets hierarchy listed.
+     * @param parent Pointer to root widget in generated hierarchy,
+     *     if nullptr, all allocated widgets will be listed.
+     */
+    static QString getWidgetHierarchy(const QWidget* parent = nullptr);
+
+    /**
+     * Generates human-readable text with count of allocated Qt widgets by type.
+     */
+    static QString getWidgetStatistics();
+};
+
+} // nx::vms::client::desktop
