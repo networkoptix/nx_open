5962b3ff8eec27aff16fb38356371510e845deb0
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-05-30 17:31:23 -0700

    Telstra customization in 3.2 release
    <==== Mercurial data ====>
    Branch: vms_3.2_release

---
+++ customization/telstra/customization.cmake
@@ -0,0 +1,47 @@
+# Components Namings
+set(product.name "Telstra Video Surveillance")
+set(display.product.name "Telstra Video Surveillance")
+set(product.name.short "telstra")
+set(product.appName "telstra")
+set(company.name "Telstra")
+set(uri.protocol "telstra-vms")
+set(cloudName "Telstra Video Surveillance")
+set(installer.name "telstra")
+set(android.packageName "com.telstra.telstra")
+set(android.alias "telstra")
+set(android.storepass "5B4M2Gn5")
+set(android.keypass "S6rtgPMk")
+
+# Support section
+set(companyUrl "https://www.telstra.com.au/")
+set(productUrl "https://www.telstra.com.au/business-enterprise/solutions/security/electronic-security/managed-video-surveillance")
+set(supportUrl "")
+set(supportEmail "managedvideosurveillance@team.telstra.com")
+set(supportPhone "")
+set(licenseEmail "")
+# end of Support section
+
+set(backgroundImage
+    "{\"enabled\": true, \"name\": \":/skin/background.png\", \"mode\": \"Crop\"}")
+set(defaultWebPages "{\"Home Page\": \"https://www.telstra.com.au\"}")
+
+set(customization.upgradeCode "{e216dc9d-b009-4461-82a3-ecc5d46eb528}")
+set(customization.clientUpgradeCode "{e706076f-2528-4bce-b366-d0a0f5da1dee}")
+set(customization.clientBundleUpgradeCode "{bb0ae6fe-f7cb-49ad-83f5-c24a9271e6a9}")
+set(customization.serverBundleUpgradeCode "{80210dbd-76da-420e-9f5e-2c0833ce23c0}")
+set(customization.fullBundleUpgradeCode "{7f96fd2c-fa49-4a32-807c-ed60c4a4e5d2}")
+
+set(deb.customization.company.name "telstra")
+
+set(ios.new_bundle_identifier "com.telstra.telstramobile")
+set(ios.group_identifier "group.com.telstra.telstra")
+set(ios.sign.identity "iPhone Distribution: Network Optix, Inc. (L6FE34GJWM)")
+
+set(mac.sign.identity "Developer ID Application: Network Optix, Inc. (L6FE34GJWM)")
+set(mac.bundle.identifier "com.telstra.telstra2")
+set(mac.protocol_handler_bundle.identifier "com.telstra.protocol_handler")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0031")
