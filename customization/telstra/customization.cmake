c82e0e796bb8109bd4b08b8a3cbba828a325d0b1
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-05-31 13:45:21 -0700

    coresmp, telstra, viveex customizations
    <==== Mercurial data ====>
    Branch: mobile_19.1

--- customization/telstra/customization.cmake
+++ customization/telstra/customization.cmake
@@ -12,32 +12,33 @@ set(android.alias "telstra")
 set(android.storepass "5B4M2Gn5")
 set(android.keypass "S6rtgPMk")
 
+set(display_mobile_name "Telstra Video Surveillance Mobile")
+set(mobile_bundle_name "TelstraMobile")
+
 # Support section
 set(companyUrl "https://www.telstra.com.au/")
 set(productUrl "https://www.telstra.com.au/business-enterprise/solutions/security/electronic-security/managed-video-surveillance")
 set(supportUrl "")
-set(supportEmail "")
+set(supportEmail "managedvideosurveillance@team.telstra.com")
 set(supportPhone "")
 set(licenseEmail "")
 # end of Support section
 
+set(backgroundImage
+    "{\"enabled\": true, \"name\": \":/skin/background.png\", \"mode\": \"Crop\"}")
 set(defaultWebPages "{\"Home Page\": \"https://www.telstra.com.au\"}")
 
-# Custom Installer (if needed). If not leave "default"
 set(customization.upgradeCode "{e216dc9d-b009-4461-82a3-ecc5d46eb528}")
 set(customization.clientUpgradeCode "{e706076f-2528-4bce-b366-d0a0f5da1dee}")
 set(customization.clientBundleUpgradeCode "{bb0ae6fe-f7cb-49ad-83f5-c24a9271e6a9}")
 set(customization.serverBundleUpgradeCode "{80210dbd-76da-420e-9f5e-2c0833ce23c0}")
 set(customization.fullBundleUpgradeCode "{7f96fd2c-fa49-4a32-807c-ed60c4a4e5d2}")
 
-#set(nxtool.company.name "")
-#set(nxtool.bundleUpgradeCode "{0d48f2d3-686e-49d4-ab66-7222e984296e}")
-
 set(deb.customization.company.name "telstra")
 
-set(ios.new_bundle_identifier "com.telstrabusinessandenterprise.videosurveillance")
-set(ios.group_identifier "group.com.telstrabusinessandenterprise")
-set(ios.sign.identity "iPhone Distribution: Telstra Software Group PTY LTD (P82Y83AYW9)")
+set(ios.new_bundle_identifier "com.telstra.telstramobile")
+set(ios.group_identifier "group.com.telstra.telstra")
+set(ios.sign.identity "iPhone Distribution: Network Optix, Inc. (L6FE34GJWM)")
 
 set(mac.sign.identity "Developer ID Application: Network Optix, Inc. (L6FE34GJWM)")
 set(mac.bundle.identifier "com.telstra.telstra2")
---
+++ customization/viveex/customization.cmake
@@ -0,0 +1,50 @@
+set(defaultSkin "dark_orange")
+
+# Components Namings
+set(product.name "VIVEex")
+set(display.product.name "VIVEex")
+set(product.name.short "viveex")
+set(product.appName "viveex")
+set(company.name "DAEKYO CNS")
+set(uri.protocol "viveex-vms")
+set(cloudName "VIVEex-Link")
+set(installer.name "viveex")
+set(android.alias "viveex")
+set(android.storepass "hYCmvTDu")
+set(android.keypass "31O2zNNy")
+set(android.packageName "com.daekyocns.viveex")
+
+set(display_mobile_name "VIVEex Mobile")
+set(mobile_bundle_name "VIVEexMobile")
+
+# Support section
+set(companyUrl "http://www.daekyocns.com")
+set(productUrl "http://www.daekyocns.com/viveex")
+set(supportUrl "")
+set(supportEmail "viveex@daekyo.com")
+set(supportPhone "")
+set(licenseEmail "license@daekyo.com")
+# end of Support section
+
+set(defaultWebPages "{\"Home Page\": \"https://www.daekyocns.com\"}")
+
+set(customization.upgradeCode "{e44375fd-1b2e-478a-ab72-5b0df3fa9114}")
+set(customization.clientUpgradeCode "{e31ac7eb-9e99-4b6f-8dc6-2c1056d5d2ff}")
+set(customization.clientBundleUpgradeCode "{b49b8b46-33f8-42ad-84a0-44736b27456e}")
+set(customization.serverBundleUpgradeCode "{aa5ab6f1-6e1a-46a8-9587-731a502d535a}")
+set(customization.fullBundleUpgradeCode "{c6a7a801-e010-4fc6-976e-29f0e96a9ba9}")
+
+set(deb.customization.company.name "viveex")
+
+set(mac.sign.identity "Developer ID Application: Network Optix, Inc. (L6FE34GJWM)")
+set(mac.bundle.identifier "com.daekyocns.viveex2")
+set(mac.protocol_handler_bundle.identifier "com.daekyocns.protocol_handler")
+
+set(ios.new_bundle_identifier "com.daekyocns.viveex")
+set(ios.group_identifier "group.com.daekyocns.viveex")
+set(ios.sign.identity "iPhone Developer: Ivan Vigasin (KZ74EE5FP2)")
+
+# Other customizations.
+set(freeLicenseIsTrial "true")
+set(freeLicenseCount "4")
+set(freeLicenseKey "0000-0000-0000-0034")
