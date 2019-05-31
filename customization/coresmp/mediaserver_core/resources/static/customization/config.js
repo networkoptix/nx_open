5ec98522fad8c0eb8ed51a252e373b2da9a8196d
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-05-31 11:09:23 -0700

    coresmp customization fixes
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- customization/coresmp/mediaserver_core/resources/static/customization/config.js
+++ customization/coresmp/mediaserver_core/resources/static/customization/config.js
@@ -14,7 +14,7 @@ Config.helpLinks.push({
             class:'appstore'
         },
         {
-            url: "https://play.google.com/store/apps/details?id=com.networkoptix.nxwitness",
+            url: "https://play.google.com/store/apps/details?id=com.comm-core.coresmp",
             class:'googleplay',
             button: "Android Client"
         }
