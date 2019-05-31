441726b68d4ce8887206f9e68c73480f007f9fcf
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-05-31 10:36:14 -0700

    viveex and ipera fixes
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- customization/viveex/mediaserver_core/resources/static/customization/config.js
+++ customization/viveex/mediaserver_core/resources/static/customization/config.js
@@ -1,39 +1,30 @@
 Config.helpLinks.push({
-    url: "http://digital-watchdog.com/support/contact-tech-support/",
+    url: "mailto:viveex@daekyo.com",
     title: "Support",
-    description:"Have a question about specific features of your VMS system?",
-    button:"get support",
+    target: "new" // new|frame
+});
+
+/*Config.helpLinks.push({
+    url: "http://networkoptix.com/calculator/#/",
+    title: "Hardware calculator",
+    target: "new" // new|frame
+});*/
+
+Config.helpLinks.push({
+    url: "https://itunes.apple.com/eg/app/id1090087835",
+    title: "iOS Client",
     target: "new" // new|frame
 });
 
 Config.helpLinks.push({
-    url: "http://digital-watchdog.com/support/tools/ip-configurator/",
-    title: "System calculator",
-    description:"Building a new VMS system or expanding your current system? Use the System Calculator to calculate suggested storage and network requirements.",
-    button: "Go to the calculator",
-    target: "new" // new|frame
-});
-
-Config.helpLinks.push({
-    urls: [
-        {
-            url: "https://itunes.apple.com/us/app/dw-spectrum/id1090087818?mt=8",
-            button: "iOS Client",
-            class:'appstore'
-        },
-        {
-            url: "https://play.google.com/store/apps/details?id=com.digitalwatchdog.dwmobile",
-            class:'googleplay',
-            button: "Android Client"
-        }
-    ],
-    title: "Mobile Apps",
+    url: "https://play.google.com/store/apps/details?id=com.daekyocns.viveex",
+    title: "Android Client",
     target: "new" // new|frame
 });
 
 Config.allowDebugMode = false;
 //Config.webclientEnabled = false; // Uncomment this string to disable web client
-Config.productName = 'DW Spectrum';
-Config.cloud.productName = 'DW Cloud';
+Config.productName = 'VIVEex';
+Config.cloud.productName = 'VIVEex-Link';
 Config.defaultLanguage = 'en_US';
-Config.supportedLanguages = ['en_US', 'fr_FR', 'es_ES'];
+Config.supportedLanguages = ['en_US', 'en_GB', 'fr_FR', 'de_DE', 'ru_RU', 'es_ES', 'ja_JP', 'ko_KR', 'tr_TR', 'zh_CN', 'zh_TW', 'hu_HU', 'he_IL', 'nl_NL', 'pl_PL', 'vi_VN', 'th_TH'];
