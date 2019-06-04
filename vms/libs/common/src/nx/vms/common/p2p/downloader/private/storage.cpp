69e4da53c7300c7af65137837647832e42ce0da1
Denis Klychkov <dklychkov@networkoptix.com>
2019-06-04 14:29:30 +0300

    Remove synctime dependency from p2p Downloader Storage
    Using system timestamps for Storage needs is sufficient.
    
    The reason to remove: synctime is a hidden dependency which causes code to crash if synctime
    instance was not created.
    
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/libs/common/src/nx/vms/common/p2p/downloader/private/storage.cpp
+++ vms/libs/common/src/nx/vms/common/p2p/downloader/private/storage.cpp
@@ -9,7 +9,6 @@
 #include <nx/fusion/serialization/json.h>
 #include <nx/utils/scope_guard.h>
 #include <nx/utils/file_system.h>
-#include <utils/common/synctime.h>
 
 using namespace std::chrono;
 
@@ -93,7 +92,7 @@ FileInformation Storage::fileInformation(const QString& fileName) const
 ResultCode Storage::addFile(FileInformation fileInformation, bool updateTouchTime)
 {
     if (updateTouchTime)
-        fileInformation.touchTime = qnSyncTime->currentMSecsSinceEpoch();
+        fileInformation.touchTime = QDateTime::currentMSecsSinceEpoch();
 
     if (fileInformation.status == FileInformation::Status::downloaded)
         return addDownloadedFile(fileInformation);
@@ -401,7 +400,7 @@ ResultCode Storage::writeFileChunk(const QString& fileName, int chunkIndex, cons
         });
 
     it->downloadedChunks.setBit(chunkIndex);
-    it->touchTime = qnSyncTime->currentMSecsSinceEpoch();
+    it->touchTime = QDateTime::currentMSecsSinceEpoch();
     checkDownloadCompleted(it.value());
     saveMetadata(it.value());
 
@@ -534,7 +533,7 @@ void Storage::cleanupExpiredFiles()
 {
     QnMutexLocker lock(&m_mutex);
 
-    qint64 currentTime = qnSyncTime->currentMSecsSinceEpoch();
+    qint64 currentTime = QDateTime::currentMSecsSinceEpoch();
 
     QSet<QString> expiredFiles;
     for (const FileMetadata& data: m_fileInformationByName)
