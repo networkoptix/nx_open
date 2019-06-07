e0f6561eacfa7226843d347b68e4850efee931ac
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-06-07 15:34:29 +0100

    META-298: nx_kit: Introduce fromString() in nx::kit::utils.h
    <==== Mercurial data ====>
    Branch: meta_4.0

--- open/artifacts/nx_kit/src/nx/kit/utils.h
+++ open/artifacts/nx_kit/src/nx/kit/utils.h
@@ -45,7 +45,7 @@ inline bool isAsciiPrintable(int c)
 NX_KIT_API std::string baseName(std::string path);
 
 /**
- * @return Process name, without .exe in Windows. 
+ * @return Process name, without .exe in Windows.
  */
 NX_KIT_API std::string getProcessName();
 
@@ -67,11 +67,16 @@ std::string format(const std::string& formatStr, Args... args)
     return result;
 }
 
+NX_KIT_API bool fromString(const std::string& s, int* value);
+NX_KIT_API bool fromString(const std::string& s, double* value);
+NX_KIT_API bool fromString(const std::string& s, float* value);
+NX_KIT_API bool fromString(const std::string& s, bool* value);
+
 //-------------------------------------------------------------------------------------------------
 // OS support.
 
 /**
- * @return Command line arguments of the proccess, cached after the first call. If arguments are
+ * @return Command line arguments of the process, cached after the first call. If arguments are
  *     not available, then returns a single empty string.
  */
 NX_KIT_API const std::vector<std::string>& getProcessCmdLineArgs();
