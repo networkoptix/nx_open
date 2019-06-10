45f4ad13f572b590663b180d7690e1a36736af2c
Sergey Ivanov <sivanov@networkoptix.com>
2019-06-10 19:30:43 +0300

    Update translatable strings
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/translations/client_qml_en_US.ts
+++ vms/client/nx_vms_client_desktop/translations/client_qml_en_US.ts
@@ -11,6 +11,10 @@
         <source>Camera analytics will work only when camera is being viewed. Enable recording to make it work all the time.</source>
         <translation></translation>
     </message>
+    <message>
+        <source>This is the default built-in functionality</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>EmptyTilesPlaceholder</name>
