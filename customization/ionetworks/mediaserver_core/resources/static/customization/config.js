7b60453f131bfaf3b11df93a4fefab21eb9a97e1
Boris Biryuchkov <bbiryuchkov@networkoptix.com>
2019-06-11 18:18:49 -0700

    WEB-1009: There isn't the support email and a link to the Ios app. Ez Pro/EZ Pro Chinese.
    <==== Mercurial data ====>
    Branch: vms_3.2_release

--- customization/ionetworks/mediaserver_core/resources/static/customization/config.js
+++ customization/ionetworks/mediaserver_core/resources/static/customization/config.js
@@ -1,6 +1,24 @@
 Config.helpLinks.push({
-    url: "https://play.google.com/store/apps/details?id=com.ioezio.ezpromobile",
-    title: "Android Client",
+    url: "mailto:support@ioezio.com",
+    title: "EZ Pro Support",
+    target: "new",
+    button: "support@ioezio.com
+});
+
+Config.helpLinks.push({
+    urls: [
+        {
+            url: "https://apps.apple.com/us/app/ez-pro-mobile/id1167886315?ls=1",
+            button: "iOS Client",
+            class:'appstore'
+        },
+        {
+			url: "https://play.google.com/store/apps/details?id=com.ioezio.ezpromobile",
+			title: "Android Client",
+			target: "new" // new|frame
+        }
+    ],
+    title: "Mobile Apps",
     target: "new" // new|frame
 });
 
