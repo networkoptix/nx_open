19dd9fa0937702e7441916867e68b4b8b3b55632
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-06-13 21:20:41 +0300

    FT-161: (C1594) Add merge layouts to test
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/server/nx_vms_server/unit_tests/src/api/merge_systems_ut.cpp
+++ vms/server/nx_vms_server/unit_tests/src/api/merge_systems_ut.cpp
@@ -12,6 +12,9 @@
 #include "test_api_requests.h"
 #include <utils/merge_systems_common.h>
 #include <core/resource_management/resource_pool.h>
+#include <nx/vms/api/data/full_info_data.h>
+#include <core/resource/user_resource.h>
+#include <nx/vms/api/data/merge_status_reply.h>
 
 using MergeStatus = utils::MergeSystemsStatus::Value;
 using LauncherPtr = std::unique_ptr<MediaServerLauncher>;
@@ -96,6 +99,57 @@ protected:
         NX_TEST_API_POST(server.get(), "api/configure", configureData);
     }
 
+    void whenServerHasData(const LauncherPtr& server)
+    {
+        const auto serverGuid = server->commonModule()->moduleGUID().toString();
+
+        nx::vms::api::CameraData cameraData;
+        cameraData.parentId = QnUuid::createUuid();
+        cameraData.typeId = qnResTypePool->getResourceTypeByName("Camera")->getId();
+        cameraData.vendor = "test vendor";
+        cameraData.physicalId =
+            lit("matching physicalId %1").arg(serverGuid);
+        cameraData.id = nx::vms::api::CameraData::physicalIdToId(cameraData.physicalId);
+        NX_TEST_API_POST(server.get(), "/ec2/saveCamera", cameraData);
+
+        vms::api::UserDataList users;
+        vms::api::UserData userData;
+        userData.id = QnUuid::createUuid();
+        userData.fullName = lit("user %1").arg(serverGuid);
+        userData.permissions = GlobalPermission::admin;
+        NX_TEST_API_POST(server.get(), "/ec2/saveUser", userData);
+
+        nx::vms::api::LayoutData layoutData;
+        layoutData.name = "fixed layout name";
+        layoutData.id = QnUuid::createUuid();
+        layoutData.parentId = userData.id;
+        nx::vms::api::LayoutItemData item;
+        item.resourceId = cameraData.id;
+        item.id = QnUuid::createUuid();
+        layoutData.items.push_back(item);
+        NX_TEST_API_POST(server.get(), "/ec2/saveLayout", layoutData);
+    }
+
+    void thenFullInfoEqual(const std::vector<LauncherPtr>& servers)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        vms::api::FullInfoData fullInfoData;
+        NX_TEST_API_GET(servers[0].get(), "/ec2/getFullInfo", &fullInfoData);
+
+        ASSERT_EQ(servers.size(), fullInfoData.servers.size());
+        ASSERT_EQ(servers.size(), fullInfoData.cameras.size());
+        ASSERT_EQ(servers.size() + 1, fullInfoData.users.size());
+        ASSERT_EQ(servers.size(), fullInfoData.layouts.size());
+
+        for (int i = 1; i < servers.size(); ++i)
+        {
+            vms::api::FullInfoData fullInfoData2;
+            NX_TEST_API_GET(servers[0].get(), "/ec2/getFullInfo", &fullInfoData2);
+            ASSERT_EQ(QJson::serialized(fullInfoData), QJson::serialized(fullInfoData2));
+        }
+    }
+
 private:
     template<typename ResponseData>
     void issueGetRequest(
@@ -146,32 +200,19 @@ TEST_F(MergeSystems, SafeMode_To)
 }
 
 void waitForMergeFinished(
-    const std::vector<LauncherPtr>& servers,
-    int serverCount,
-    const nx::vms::api::SystemMergeHistoryRecord& mergeResult)
+    const LauncherPtr& server)
 {
     int success = 0;
     do
     {
+        QnJsonRestResult jsonResult;
+        NX_TEST_API_GET(server.get(), "/ec2/mergeStatus", &jsonResult);
+        auto mergeStatus = jsonResult.deserialized<vms::api::MergeStatusReply>();
+        if (!mergeStatus.mergeId.isNull() && !mergeStatus.mergeInProgress)
+            break;
+
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        success = 0;
-        for (int i = 0; i < serverCount; ++i)
-        {
-            auto connection = servers[i]->serverModule()->ec2Connection();
-            nx::vms::api::SystemMergeHistoryRecordList mergeData;
-            connection->getMiscManager(Qn::kSystemAccess)->getSystemMergeHistorySync(&mergeData);
-
-            auto itr = std::find_if(
-                mergeData.begin(), mergeData.end(),
-                [&mergeResult](const nx::vms::api::SystemMergeHistoryRecord& record)
-                {
-                    return mergeResult == record;
-                });
-            if (itr != mergeData.end())
-                ++success;
-
-        }
-    } while (success != serverCount);
+    } while (true);
 }
 
 TEST_F(MergeSystems, DoubleMergeWithTakeLocalSettings)
@@ -192,7 +233,7 @@ TEST_F(MergeSystems, DoubleMergeWithTakeLocalSettings)
         /* expectedCode */ MergeStatus::ok,
         &mergeResult);
 
-    waitForMergeFinished(servers, 2, mergeResult);
+    waitForMergeFinished(servers[0]);
 
     assertMergeRequestReturn(
         /* requestTarget */ servers[2],
@@ -200,8 +241,33 @@ TEST_F(MergeSystems, DoubleMergeWithTakeLocalSettings)
         /* expectedCode */ MergeStatus::ok,
         &mergeResult);
 
-    waitForMergeFinished(servers, 3, mergeResult);
+    waitForMergeFinished(servers[0]);
+}
 
+TEST_F(MergeSystems, MergeServersWithData)
+{
+    const int kServerCount = 3;
+    std::vector<LauncherPtr> servers;
+    nx::vms::api::SystemMergeHistoryRecord mergeResult;
+    for (int i = 0; i < kServerCount; ++i)
+    {
+        auto server = givenServer();
+        whenServerLaunched(server, SafeMode::off);
+        whenServerHasData(server);
+        whenServerIsConfigured(server);
+        servers.push_back(std::move(server));
+    }
+
+    for (int i = 0; i < kServerCount - 1; ++i)
+    {
+        assertMergeRequestReturn(
+            /* requestTarget */ servers[i],
+            /* serverToMerge */ servers[i+1],
+            /* expectedCode */ MergeStatus::ok,
+            &mergeResult);
+    }
+    waitForMergeFinished(servers[0]);
+    thenFullInfoEqual(servers);
 }
 
 } // namespace test
