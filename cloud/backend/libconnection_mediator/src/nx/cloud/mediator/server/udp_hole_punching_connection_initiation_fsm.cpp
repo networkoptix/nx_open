d8cdbb9e9f30116549ce44541414c38099ce2895
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-06-21 11:01:11 -0700

    mediator: fixed UDPHolePunchingConnectionInitiationFsm::toString(State)
    <==== Mercurial data ====>
    Branch: cloud_backend_18.3.2

--- cloud/backend/libconnection_mediator/src/nx/cloud/mediator/server/udp_hole_punching_connection_initiation_fsm.cpp
+++ cloud/backend/libconnection_mediator/src/nx/cloud/mediator/server/udp_hole_punching_connection_initiation_fsm.cpp
@@ -566,6 +566,8 @@ const char* UDPHolePunchingConnectionInitiationFsm::toString(State state)
             return "init";
         case State::waitingServerPeerUDPAddress:
             return "waitingServerPeerUDPAddress";
+        case State::resolvingServersRelayInstance:
+            return "resolvingServersRelayInstance";
         case State::waitingConnectionResult:
             return "waitingConnectionResult";
         case State::fini:
