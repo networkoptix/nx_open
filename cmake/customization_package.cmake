1e770e656e6e3d0480bdfd197d49da21e49d8976
Sergey Ivanov <sivanov@networkoptix.com>
2019-06-24 19:06:53 +0300

    Added customization pack script
    <==== Mercurial data ====>
    Branch: customization_pack_3.2

---
+++ cmake/customization_package.cmake
@@ -0,0 +1,22 @@
+if(NOT build_utils_dir)
+    set(build_utils_dir "${CMAKE_SOURCE_DIR}/build_utils")
+endif()
+
+function(nx_unpack_customization_package source_directory target_directory)
+    message(STATUS "Unpacking customization package from ${source_directory} to ${target_directory}")
+
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE}
+            ${build_utils_dir}/customization/pack2.py unpack
+            ${source_directory}/package.zip
+            ${target_directory}
+    )
+endfunction()
+
+nx_rdep_add_package(any/customization_pack-${customization}
+    PATH_VARIABLE customization_package_directory)
+
+set(customization_dir "${CMAKE_BINARY_DIR}/customization/${customization}")
+nx_unpack_customization_package(
+    ${customization_package_directory}
+    ${customization_dir})
