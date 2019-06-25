bd7a6eb678a457c15b2c350d80f18c8cc203ee78
Victor Breus <vbreus@networkoptix.com>
2019-06-25 11:44:07 +0300

    VMS-14457: Basic item model state to JSON serializer
    <==== Mercurial data ====>
    Branch: vms_resource_tree_model_tests

---
+++ vms/client/nx_vms_client_desktop/src/ui/models/item_model_state_snapshot_heper.cpp
@@ -0,0 +1,138 @@
+#include "item_model_state_snapshot_heper.h"
+
+#include <QtCore/QFile>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QAbstractItemModel>
+
+using namespace nx::vms::client::desktop;
+
+ItemModelStateSnapshotHelper::ItemModelStateSnapshotHelper(QAbstractItemModel* model):
+    m_model(model)
+{
+    NX_ASSERT(model);
+}
+
+QJsonDocument ItemModelStateSnapshotHelper::getItemModelStateDocument()
+{
+    return QJsonDocument(createItemModelObject());
+}
+
+void ItemModelStateSnapshotHelper::saveItemModelStateJson(const QString& path)
+{
+    QFile file(path);
+    if (!file.open(QFile::WriteOnly | QFile::Truncate))
+    {
+        NX_ASSERT(false);
+        return;
+    }
+    file.write(getItemModelStateDocument().toJson(QJsonDocument::Indented));
+}
+
+bool ItemModelStateSnapshotHelper::compareItemModelState(const QString& referencePath,
+    const QJsonDocument& stateDocument)
+{
+    QFile file(referencePath);
+    if (!file.open(QFile::ReadOnly))
+    {
+        NX_ASSERT(false);
+        return false;
+    }
+    QJsonParseError parseError = {};
+    const auto referenceDocument = QJsonDocument::fromJson(file.readAll(), &parseError);
+    if (parseError.error != QJsonParseError::NoError)
+    {
+        NX_ASSERT(false);
+        return false;
+    }
+    return referenceDocument == stateDocument;
+}
+
+QJsonObject ItemModelStateSnapshotHelper::createItemModelObject() const
+{
+    QJsonObject itemModelObject;
+
+    if (m_model->columnCount() > 0)
+    {
+        itemModelObject.insert(kHorizontalHeaderDataArrayKey,
+            createHeaderDataArray(Qt::Horizontal));
+    }
+    if (m_model->rowCount() > 0)
+    {
+        itemModelObject.insert(kVerticalHeaderDataArrayKey,
+            createHeaderDataArray(Qt::Vertical));
+    }
+    if ((m_model->rowCount() > 0) && (m_model->columnCount() > 0))
+        itemModelObject.insert(kRowsArrayKey, createRowsArray(QModelIndex()));
+
+    return itemModelObject;
+}
+
+QJsonObject ItemModelStateSnapshotHelper::createItemObject(const QModelIndex& index) const
+{
+    QJsonObject itemObject;
+    if (!index.isValid())
+    {
+        NX_ASSERT(false);
+        return itemObject;
+    }
+
+    const auto indexData = m_model->itemData(index);
+    QVariantMap variantMap;
+    for (auto i = indexData.cbegin(); i != indexData.cend(); ++i)
+        variantMap.insert(getRoleName(i.key()), i.value());
+
+    if (!variantMap.empty())
+        itemObject.insert(kItemDataObjectKey, QJsonObject::fromVariantMap(variantMap));
+    itemObject.insert(kItemFlagsValueKey, QJsonValue(static_cast<Qt::ItemFlags::Int>(index.flags())));
+    if ((m_model->rowCount(index) > 0) && (m_model->columnCount(index) > 0))
+        itemObject.insert(kRowsArrayKey, createRowsArray(index));
+
+    return itemObject;
+}
+
+QJsonArray ItemModelStateSnapshotHelper::createHeaderDataArray(Qt::Orientation orientation) const
+{
+    QJsonArray headerDataArray;
+    const auto roles = m_model->roleNames().keys();
+    const auto sectionCount =
+        orientation == Qt::Horizontal ? m_model->columnCount() : m_model->rowCount();
+
+    for (int section = 0; section < sectionCount; ++section)
+    {
+        QVariantMap headerDataMap;
+        for (const auto& role : roles)
+        {
+            const auto headerData = m_model->headerData(section, orientation, role);
+            if (!headerData.isNull())
+                headerDataMap.insert(getRoleName(role), headerData);
+        }
+        headerDataArray.append(QJsonObject::fromVariantMap(headerDataMap));
+    }
+
+    return headerDataArray;
+}
+
+QJsonArray ItemModelStateSnapshotHelper::createRowsArray(const QModelIndex& parent) const
+{
+    QJsonArray rowsArray;
+    for (int row = 0; row < m_model->rowCount(parent); ++row)
+        rowsArray.append(createRowItemsArray(row, parent));
+    return rowsArray;
+}
+
+QJsonArray ItemModelStateSnapshotHelper::createRowItemsArray(int row,
+    const QModelIndex& parent) const
+{
+    QJsonArray rowItems;
+    for (int column = 0; column < m_model->columnCount(parent); ++column)
+        rowItems.append(createItemObject(m_model->index(row, column, parent)));
+    return rowItems;
+}
+
+QString ItemModelStateSnapshotHelper::getRoleName(int role) const
+{
+    const auto roleNames = m_model->roleNames();
+    if (roleNames.contains(role))
+        return roleNames.value(role);
+    return QString::number(role);
+}
---
+++ vms/client/nx_vms_client_desktop/src/ui/models/item_model_state_snapshot_heper.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonArray>
+
+class QAbstractItemModel;
+
+namespace nx::vms::client::desktop {
+
+class ItemModelStateSnapshotHelper
+{
+public:
+    ItemModelStateSnapshotHelper(QAbstractItemModel* model);
+    QJsonDocument getItemModelStateDocument();
+    void saveItemModelStateJson(const QString& path);
+    bool compareItemModelState(const QString& referencePath, const QJsonDocument& stateDocument);
+
+private:
+    QJsonObject createItemModelObject() const;
+    QJsonObject createItemObject(const QModelIndex& index) const;
+    QJsonArray createHeaderDataArray(Qt::Orientation orientation) const;
+    QJsonArray createRowsArray(const QModelIndex& parent) const;
+    QJsonArray createRowItemsArray(int row, const QModelIndex& parent) const;
+    QString getRoleName(int role) const;
+
+private:
+    static constexpr auto kItemDataObjectKey = "itemData";
+    static constexpr auto kItemFlagsValueKey = "itemFlags";
+    static constexpr auto kVerticalHeaderDataArrayKey = "verticalHeaderData";
+    static constexpr auto kHorizontalHeaderDataArrayKey = "horizontalHeaderData";
+    static constexpr auto kRowsArrayKey = "rows";
+
+    QAbstractItemModel* m_model = nullptr;
+};
+
+} // namespace nx::vms::client::desktop
