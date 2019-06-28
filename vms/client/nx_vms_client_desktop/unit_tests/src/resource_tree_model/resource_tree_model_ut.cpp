5290a0d9990a65699df27b1d735046dc6d23a1bd
Victor Breus <vbreus@networkoptix.com>
2019-06-28 19:32:48 +0300

    Some kind of basic resource tree model test
    <==== Mercurial data ====>
    Branch: vms_resource_tree_model_tests

---
+++ vms/client/nx_vms_client_desktop/unit_tests/src/resource_tree_model/resource_tree_model_ut.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+
+#include <common/common_module.h>
+
+#include <client_core/client_core_module.h>
+#include <client/client_module.h>
+
+#include <ui/workbench/workbench_access_controller.h>
+#include <ui/models/resource/resource_tree_model.h>
+#include <ui/models/resource_search_proxy_model.h>
+
+class ResourceTreeModelTest: public testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        m_clientModule.reset(new QnClientModule(QnStartupParameters(), nullptr));
+        m_accessController.reset(new QnWorkbenchAccessController(commonModule()));
+        m_resourceTreeModel.reset(new QnResourceTreeModel(QnResourceTreeModel::FullScope,
+            m_accessController.get(), nullptr, commonModule()));
+        m_resourceSearchProxyModel.reset(new QnResourceSearchProxyModel());
+        m_resourceSearchProxyModel->setSourceModel(m_resourceTreeModel.get());
+    }
+
+    virtual void TearDown()
+    {
+        m_resourceSearchProxyModel.clear();
+        m_resourceTreeModel.clear();
+        m_accessController.clear();
+        m_clientModule.clear();
+    }
+
+    QnCommonModule* commonModule() const
+    {
+        return m_clientModule->clientCoreModule()->commonModule();
+    }
+
+    QnResourcePool* resourcePool() const
+    {
+        return commonModule()->resourcePool();
+    }
+
+protected:
+    QSharedPointer<QnClientModule> m_clientModule;
+    QSharedPointer<QnWorkbenchAccessController> m_accessController;
+    QSharedPointer<QnResourceTreeModel> m_resourceTreeModel;
+    QSharedPointer<QnResourceSearchProxyModel> m_resourceSearchProxyModel;
+};
+
+TEST_F(ResourceTreeModelTest, shouldBeNotEmpty)
+{
+    ASSERT_FALSE(m_resourceSearchProxyModel->rowCount() == 0);
+}
