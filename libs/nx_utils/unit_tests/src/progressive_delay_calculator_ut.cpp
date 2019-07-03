e6649470c47fd588cf0b52fcee2851d500935026
Dmitry Liman <dliman@networkoptix.com>
2019-07-03 19:10:26 +0300

    VMS-14503: Fix style in ProgressiveDelayCalculator unit tests
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- libs/nx_utils/unit_tests/src/progressive_delay_calculator_ut.cpp
+++ libs/nx_utils/unit_tests/src/progressive_delay_calculator_ut.cpp
@@ -2,49 +2,52 @@
 
 #include <nx/utils/progressive_delay_calculator.h>
 
-constexpr std::chrono::milliseconds initialDelay(1000);
-constexpr std::chrono::milliseconds maxDelay(10000);
+namespace nx::utils::test {
+
+constexpr std::chrono::seconds kInitialDelay(1);
+constexpr std::chrono::seconds kMaxDelay(10);
 
 using ProgressiveDelayPolicy = nx::utils::ProgressiveDelayPolicy;
 using ProgressiveDelayCalculator = nx::utils::ProgressiveDelayCalculator;
 
 TEST(ProgressiveDelayCalculator, calculateNewDelay)
 {
-
-    ProgressiveDelayCalculator calculator({initialDelay, /*delayMultiplier*/ 2, maxDelay});
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay * 2);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay * 4);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay * 8);
-    ASSERT_EQ(calculator.calculateNewDelay(), maxDelay);
-    ASSERT_EQ(calculator.calculateNewDelay(), maxDelay);
+    ProgressiveDelayCalculator calculator({kInitialDelay, /*delayMultiplier*/ 2, kMaxDelay});
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay * 2);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay * 4);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay * 8);
+    ASSERT_EQ(calculator.calculateNewDelay(), kMaxDelay);
+    ASSERT_EQ(calculator.calculateNewDelay(), kMaxDelay);
 
     ProgressiveDelayCalculator calculator1 =
-        ProgressiveDelayCalculator({initialDelay, /*delayMultiplier*/ 0, maxDelay});
-    ASSERT_EQ(calculator1.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator1.calculateNewDelay(), initialDelay);
+        ProgressiveDelayCalculator({kInitialDelay, /*delayMultiplier*/ 0, kMaxDelay});
+    ASSERT_EQ(calculator1.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator1.calculateNewDelay(), kInitialDelay);
 
     ProgressiveDelayCalculator calculator2 =
-        ProgressiveDelayCalculator({initialDelay, /*delayMultiplier*/ 1, maxDelay});
-    ASSERT_EQ(calculator2.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator2.calculateNewDelay(), initialDelay);
+        ProgressiveDelayCalculator({kInitialDelay, /*delayMultiplier*/ 1, kMaxDelay});
+    ASSERT_EQ(calculator2.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator2.calculateNewDelay(), kInitialDelay);
 
     ProgressiveDelayCalculator calculator3 =
-        ProgressiveDelayCalculator({initialDelay, /*delayMultiplier*/ 10, initialDelay});
-    ASSERT_EQ(calculator3.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator3.calculateNewDelay(), initialDelay);
+        ProgressiveDelayCalculator({kInitialDelay, /*delayMultiplier*/ 10, kInitialDelay});
+    ASSERT_EQ(calculator3.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator3.calculateNewDelay(), kInitialDelay);
 }
 
 TEST(ProgressiveDelayCalculator, reset)
 {
-    ProgressiveDelayCalculator calculator({initialDelay, /*delayMultiplier*/ 2, maxDelay});
+    ProgressiveDelayCalculator calculator({kInitialDelay, /*delayMultiplier*/ 2, kMaxDelay});
 
     ASSERT_EQ(calculator.triesMade(), 0);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay * 2);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay * 2);
 
     calculator.reset();
     ASSERT_EQ(calculator.triesMade(), 0);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay);
-    ASSERT_EQ(calculator.calculateNewDelay(), initialDelay * 2);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay);
+    ASSERT_EQ(calculator.calculateNewDelay(), kInitialDelay * 2);
 }
+
+} // nx::utils::test
