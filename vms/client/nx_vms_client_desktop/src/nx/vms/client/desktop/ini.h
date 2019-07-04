0eab34e938cc7bba3f1f6a327bdcfd8640a1897b
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-07-04 19:35:39 +0100

    Fix comment in .ini
    <==== Mercurial data ====>
    Branch: meta_4.0

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ini.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ini.h
@@ -169,7 +169,8 @@ struct Ini: nx::kit::IniConfig
         "Global live video cache length, in seconds. Set to zero to use built-in value.");
     NX_INI_INT(180000, connectTimeoutMs,
         "Timeout (in milliseconds) for waiting initial resources message from the server.\n"
-        "If exceeded then connections is dropped to avoid infinite UI \"Loading...\" state. 0 means disabled.");
+        "If exceeded then connections is dropped to avoid infinite UI \"Loading...\" state.\n"
+        "0 means disabled.");
     NX_INI_STRING("", dumpGeneratedIconsTo,
         "Dump icons, generated from svg, to a given folder.");
     NX_INI_FLAG(0, enableVSyncWorkaround,
