61c987282c2afbb54848f61dc0c25c6a008e819b
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-07-05 18:28:22 -0700

    META-330: RectAggregator: fixed assert when have to split existing rectangle to two associated with different value sets
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/libs/common/unit_tests/src/utils/math/rect_aggregator_ut.cpp
+++ vms/libs/common/unit_tests/src/utils/math/rect_aggregator_ut.cpp
@@ -2,7 +2,7 @@
 
 #include <utils/math/rect_aggregator.h>
 
-namespace test {
+namespace nx::analytics::db::test {
 
 using AggregatedRect = RectAggregator<int>::AggregatedRect;
 
@@ -171,4 +171,64 @@ TEST_F(RectAggregator, produced_adjacent_rects_with_same_values_are_merged)
     assertEqual(expected, actual);
 }
 
-} // namespace test
+using AggregatedRect1 = ::RectAggregator<std::string>::AggregatedRect;
+
+static const AggregatedRect1 rects2[] = {
+    AggregatedRect1{ QRect(9,10, 7, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,10, 8, 4), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,9, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,8, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,7, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,6, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,5, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,4, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,3, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,2, 8, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(9,2, 8, 4), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(10,1, 7, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(10,0, 7, 5), {"29a86763-4800-422b-8357-45bc6dfd1359"}},
+    AggregatedRect1{ QRect(6,6, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(5,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(4,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(4,8, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(5,8, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(6,8, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(7,8, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(9,8, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(10,8, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(11,8, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(12,7, 12, 7), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(14,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(15,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(16,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(17,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(19,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(20,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(21,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(22,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(24,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(25,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(26,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(27,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(29,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(30,7, 11, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(31,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(32,7, 12, 6), {"25c66495-6fde-4fd5-9fb2-214747b106de"}},
+    AggregatedRect1{ QRect(25,11, 19, 11), {"09357097-1ce5-49cd-a210-5f0e6eaf241b"}},
+    AggregatedRect1{ QRect(25,10, 19, 11), {"09357097-1ce5-49cd-a210-5f0e6eaf241b"}},
+    AggregatedRect1{ QRect(17,12, 12, 8), {"31ba2512-130e-4342-8d9f-614d42e4596a"}},
+    AggregatedRect1{ QRect(17,12, 12, 8), {"31ba2512-130e-4342-8d9f-614d42e4596a"}},
+    AggregatedRect1{ QRect(16,13, 12, 8), {"31ba2512-130e-4342-8d9f-614d42e4596a"}},
+    AggregatedRect1{ QRect(15,13, 12, 8), {"31ba2512-130e-4342-8d9f-614d42e4596a"}},
+    AggregatedRect1{ QRect(15,13, 11, 8), {"31ba2512-130e-4342-8d9f-614d42e4596a"}}
+};
+
+TEST_F(RectAggregator, xxx)
+{
+    ::RectAggregator<std::string> aggregator;
+
+    for (const auto& item: rects2)
+        aggregator.add(item.rect, *item.values.begin());
+}
+
+} // namespace nx::analytics::db::test
