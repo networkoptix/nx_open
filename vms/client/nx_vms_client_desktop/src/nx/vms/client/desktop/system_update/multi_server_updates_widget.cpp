6ff93a1122fbb9605ab48a49866735e6e3af6c37
Dmitry Kargin <dkargin@networkoptix.com>
2019-07-09 18:26:20 +0300

    VMS-14599: Update conditions for 'Install Update' button to be disabled
    It should be disabled if some servers have 'not enough space' error when widget is in 'readyInstall' state
    
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_update/multi_server_updates_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_update/multi_server_updates_widget.cpp
@@ -2332,16 +2332,20 @@ void MultiServerUpdatesWidget::syncRemoteUpdateStateToUi()
     {
         if (readyAndOnline.empty() || !readyAndOffline.empty())
         {
-            errorTooltips << tr("Some servers have gone offline. "
-                                "Please wait until they become online to continue.");
-        }
-        else if (hasVerificationErrors)
-        {
-            errorTooltips << tr("Some servers have no update packages available.");
-        }
-        else if (hasStatusErrors)
-        {
-            errorTooltips << tr("Some servers have encountered an internal error.");
+            if (hasVerificationErrors)
+            {
+                errorTooltips << tr("Some servers have no update packages available.");
+            }
+            else if (hasStatusErrors)
+            {
+                errorTooltips << tr("Some servers have encountered an internal error.");
+                errorTooltips << tr("Please please contact Customer Support.");
+            }
+            else
+            {
+                errorTooltips << tr("Some servers have gone offline. "
+                                    "Please wait until they become online to continue.");
+            }
         }
     }
 
@@ -2353,7 +2357,6 @@ void MultiServerUpdatesWidget::syncRemoteUpdateStateToUi()
     else
     {
         ui->downloadButton->setEnabled(false);
-        errorTooltips << tr("Please please contact Customer Support.");
         ui->downloadButton->setToolTip(errorTooltips.join("\n"));
     }
 
