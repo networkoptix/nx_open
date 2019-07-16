df5479e7780487ad17f47dcacf1538d2eaea7c06
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-07-16 16:20:06 +0100

    META-342: SDK: In Windows, set and restore DllDirectory for each plugin's entry point function
    <==== Mercurial data ====>
    Branch: meta_4.0

--- vms/server/nx_vms_server/src/plugins/plugin_manager.cpp
+++ vms/server/nx_vms_server/src/plugins/plugin_manager.cpp
@@ -11,7 +11,7 @@
 
 #include <nx/utils/log/log.h>
 #include <nx/kit/utils.h>
-
+#include <nx/utils/system_error.h>
 #include <plugins/plugin_api.h>
 #include <nx/sdk/helpers/lib_context.h>
 #include <nx/vms/server/sdk_support/ref_countable_registry.h>
@@ -252,21 +252,6 @@ std::unique_ptr<QLibrary> PluginManager::loadPluginLibrary(
     const QString& libFilename,
     PluginInfoPtr pluginInfo)
 {
-    #if defined(_WIN32)
-        if (!pluginsIni().disablePluginLinkedDllLookup && !pluginHomeDir.isEmpty())
-        {
-            NX_DEBUG(this, "Calling SetDllDirectoryW(%1)",
-                nx::kit::utils::toString(pluginHomeDir));
-            if (!SetDllDirectoryW(pluginHomeDir.toStdWString().c_str()))
-            {
-                NX_ERROR(this, "SetDllDirectoryW(%1) failed",
-                    nx::kit::utils::toString(pluginHomeDir));
-            }
-        }
-    #else
-        nx::utils::unused(pluginHomeDir);
-    #endif
-
     auto lib = std::make_unique<QLibrary>(libFilename);
 
     // Flag DeepBindHint forces plugin (the loaded side) to use its functions instead of the same
@@ -275,18 +260,7 @@ std::unique_ptr<QLibrary> PluginManager::loadPluginLibrary(
     hints |= QLibrary::DeepBindHint;
     lib->setLoadHints(hints);
 
-    const bool libLoaded = lib->load();
-
-    #if defined(_WIN32)
-        if (!pluginsIni().disablePluginLinkedDllLookup && !pluginHomeDir.isEmpty())
-        {
-            NX_DEBUG(this, "Calling SetDllDirectoryW(nullptr)");
-            if (!SetDllDirectoryW(nullptr))
-                NX_ERROR(this, "SetDllDirectoryW(nullptr) failed");
-        }
-    #endif
-
-    if (libLoaded)
+    if (lib->load())
         return lib;
 
     storePluginInfo(
@@ -314,6 +288,125 @@ void PluginManager::storePluginInfo(
     m_pluginContexts.push_back({pluginInfo, /*plugin*/ nullptr});
 }
 
+namespace {
+
+/**
+ * Intended to perform required operations before loading a plugin (in constructor), and then
+ * restore the original state of the process after calling plugin's entry point function (in
+ * destructor).
+ *
+ * Currently, does the following, and only in Windows:
+ *
+ * If a plugin resides in its home dir, and .ini does not disable the feature, remembers the result
+ * of WinAPI call GetDllDirectoryW(), and temporarily (to be restored in the destructor) sets
+ * plugin's home dir with SetDllDirectoryW().
+ */
+class PluginLoadingContext
+{
+public:
+    /**
+     * @oaram pluginHomeDir If empty, the plugin is assumed to reside in a dir with other plugins.
+     */
+    PluginLoadingContext(
+        const PluginManager* pluginManager, QString pluginHomeDir, QString libName)
+        :
+        m_pluginManager(pluginManager),
+        m_pluginHomeDir(std::move(pluginHomeDir)),
+        m_libName(std::move(libName))
+    {
+        #if defined(_WIN32)
+            const QString originalDllDirectory = getDllDirectory("before loading");
+            // Can be null on error - ignoring the error and assuming an empty string.
+            m_originalDllDirectory = originalDllDirectory.isNull() ? "" : originalDllDirectory;
+
+            if (!pluginsIni().disablePluginLinkedDllLookup && !m_pluginHomeDir.isEmpty())
+            {
+                setDllDirectory(m_pluginHomeDir, "before loading");
+            }
+            else
+            {
+                // For plugins residing in a dir with other plugins, set the default DLL search
+                // order excluding the current dir.
+                setDllDirectory("", "before loading");
+            }
+        #endif
+    }
+
+    ~PluginLoadingContext()
+    {
+        #if defined(_WIN32)
+            // Always restore the default DLL search order (which includes the current dir).
+            setDllDirectory(
+                m_originalDllDirectory.isEmpty() ? QString::null : m_originalDllDirectory,
+                "after loading");
+        #endif
+    }
+
+private:
+    /**
+     * @return The current value, possibly empty, or null QString on error. Note: there seems to be
+     *     no way to learn whether the previous call to SetDllDirectoryW() has set null or empty
+     *     string.
+     */
+    QString getDllDirectory(const char* logNote) const
+    {
+        const QString logPrefix = lm("(%1 plugin %2)").args(logNote, m_libName);
+
+        const DWORD len = GetDllDirectoryW(0, nullptr);
+        const auto errorCode = SystemError::getLastOSErrorCode();
+        if (len == 0 && errorCode != SystemError::noError)
+        {
+            NX_ERROR(m_pluginManager, "%1 GetDllDirectoryW(0, nullptr) -> %2: Error %3: %4",
+                logPrefix, len, errorCode, SystemError::toString(errorCode));
+            return QString::null;
+        }
+        NX_DEBUG(m_pluginManager, "%1 GetDllDirectoryW(0, nullptr) -> %2", logPrefix, len);
+
+        std::vector<wchar_t> result(len);
+        const DWORD r = GetDllDirectoryW(len, result.data());
+        const auto e = SystemError::getLastOSErrorCode();
+        if (r == 0 && e != SystemError::noError)
+        {
+            NX_ERROR(m_pluginManager, "%1 GetDllDirectoryW(%2, result) -> %3: Error %4: %5",
+                logPrefix, len, r, errorCode, SystemError::toString(errorCode));
+            return QString::null;
+        }
+        NX_DEBUG(m_pluginManager, "%1 GetDllDirectoryW(%2, result) -> %3 %4",
+            logPrefix, len, r, nx::kit::utils::toString(result.data()));
+        return QString::fromUtf16((const char16_t*) result.data());
+    }
+
+    /**
+     * @param value Distinguishes null and empty string - null sets the default DLL search order,
+     *     and empty string sets the default DLL search order without the current dir.
+     */
+    void setDllDirectory(const QString& value, const char* logNote) const
+    {
+        const QString messageFmt = lm("(%1 plugin %2) SetDllDirectoryW(%3) %4").args(
+            logNote,
+            m_libName,
+            // Enquoted and escaped representation of the UTF-16 string for logging.
+            value.isNull() ? "null" : nx::kit::utils::toString((const wchar_t*) value.utf16()),
+            // Placeholder for the call result description.
+            "%1"
+        );
+
+        const LPCWSTR ptr = value.isNull() ? nullptr : ((LPCWSTR) value.utf16());
+        if (!SetDllDirectoryW(ptr))
+            NX_ERROR(m_pluginManager, messageFmt, "failed: " + SystemError::getLastOSErrorText());
+        else
+            NX_DEBUG(m_pluginManager, messageFmt, "succeeded");
+    }
+
+private:
+    const PluginManager* const m_pluginManager;
+    const QString m_pluginHomeDir;
+    const QString m_libName;
+    QString m_originalDllDirectory;
+};
+
+} // namespace
+
 bool PluginManager::loadNxPlugin(
     const nx::plugins::SettingsHolder& settingsHolder,
     const QString& pluginHomeDir,
@@ -325,6 +418,8 @@ bool PluginManager::loadNxPlugin(
     using namespace nx::vms::api;
     using namespace nx::vms::server;
     using nx::vms::api::analytics::PluginManifest;
+
+    const PluginLoadingContext pluginLoadingContext(this, pluginHomeDir, libName);
 
     const auto lib = loadPluginLibrary(pluginHomeDir, libFilename, pluginInfo);
     if (!lib)
