f6986a2c00500e05f3f3d827286be3e7c64bca17
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-07-24 21:00:32 +0300

    VMS-14829: Server has crashed on Windows: c0000005 (Access violation)
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/server/nx_vms_server/src/nx/vms/server/analytics/db/analytics_db.cpp
+++ vms/server/nx_vms_server/src/nx/vms/server/analytics/db/analytics_db.cpp
@@ -368,25 +368,16 @@ bool EventsStorage::readMinimumEventTimestamp(std::chrono::milliseconds* outResu
 
     try
     {
-        QnWaitCondition waitCondition;
-        m_dbController->queryExecutor().executeSelect(
-            [this, outResult](nx::sql::QueryContext* queryContext)
-            {
-                auto query = queryContext->connection()->createQuery();
-                query->prepare("SELECT min(track_start_ms) FROM track");
-                query->exec();
-                if (query->next())
-                    *outResult = std::chrono::milliseconds(query->value(0).toLongLong());
-                else
-                    *outResult = std::chrono::milliseconds::zero();
-                return nx::sql::DBResult::ok;
-            },
-                [&waitCondition, this](sql::DBResult resultCode)
-            {
-                QnMutexLocker dbLock(&m_dbControllerMutex);
-                waitCondition.wakeAll();
-            });
-        waitCondition.wait(&m_dbControllerMutex);
+        *outResult = m_dbController->queryExecutor().executeSelectQuerySync(
+            [](nx::sql::QueryContext* queryContext)
+        {
+            auto query = queryContext->connection()->createQuery();
+            query->prepare("SELECT min(track_start_ms) FROM track");
+            query->exec();
+            if (query->next())
+                return std::chrono::milliseconds(query->value(0).toLongLong());
+            return std::chrono::milliseconds::zero();
+        });
     }
     catch (const std::exception& e)
     {
