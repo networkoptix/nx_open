696574c9c63487c2711c042739513f16e5126d6a
Dmitry Liman <dliman@networkoptix.com>
2019-08-07 16:07:45 +0300

    VMS-14677: Remove CyclicAllocator from gop keeper
    <==== Mercurial data ====>
    Branch: x_VMS-14677_spacex_leak

--- vms/server/nx_vms_server/src/camera/video_camera.cpp
+++ vms/server/nx_vms_server/src/camera/video_camera.cpp
@@ -163,16 +163,6 @@ bool QnVideoCameraGopKeeper::canAcceptData() const
     return true;
 }
 
-static CyclicAllocator gopKeeperKeyFramesAllocator;
-
-static QnAbstractAllocator* getAllocator(size_t frameSize)
-{
-    const static size_t kMaxFrameSize = CyclicAllocator::DEFAULT_ARENA_SIZE / 2;
-    return frameSize < kMaxFrameSize
-        ? static_cast<QnAbstractAllocator*>(&gopKeeperKeyFramesAllocator)
-        : static_cast<QnAbstractAllocator*>(QnSystemAllocator::instance());
-}
-
 void QnVideoCameraGopKeeper::putData(const QnAbstractDataPacketPtr& nonConstData)
 {
     if (m_allChannelsMask == 0)
@@ -205,8 +195,7 @@ void QnVideoCameraGopKeeper::putData(const QnAbstractDataPacketPtr& nonConstData
                 || m_lastKeyFrames[ch].back()->timestamp <=
                     video->timestamp - KEEP_IFRAMES_DISTANCE)
             {
-                m_lastKeyFrames[ch].push_back(QnCompressedVideoDataPtr(
-                    video->clone(getAllocator(video->dataSize()))));
+                m_lastKeyFrames[ch].push_back(QnCompressedVideoDataPtr(video->clone()));
             }
 
             while ((!m_lastKeyFrames[ch].empty()
