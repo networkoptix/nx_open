34c679cf0fa2bdad23696771b39c2682029d1e1a
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-08-14 14:00:02 -0700

    CLOUD-3438: Added printHelp()
    <==== Mercurial data ====>
    Branch: cloud_backend_19.1

--- cloud/backend/cloud_db_test_util/src/main.cpp
+++ cloud/backend/cloud_db_test_util/src/main.cpp
@@ -5,43 +5,60 @@
 #include <nx/network/socket_global.h>
 #include <nx/utils/argument_parser.h>
 #include <nx/utils/timer_manager.h>
-
-#include <utils/common/command_line_parser.h>
+#include <nx/utils/log/log_initializer.h>
 
 #include "fetch_data.h"
 #include "generate_data.h"
 #include "load_tests.h"
 
-int main(int argc, char* argv[])
+void printHelp()
 {
+    std::cout << R"(
+Common arguments:
+-c, --cdb               CloudDb url. E.g., https://cloud-test.hdw.mx
+-u, --user              Account login
+-p, --password          Account password
+-h, --help              This help message
+
+Data generation mode:
+-s, --generate-systems= Number of random systems to generate
+Creates active systems with name load_test_system_{some_id}
+
+Load mode:
+-l, --load              Enable load mode. Opens multiple synchronization connections
+--connections=ddd       Test connection count (100 by default)
+--max-delay=ddd         Maximum delay before starting connection (milliseconds). By default, no delay
+Opens connections to random systems with names load_test_system_{something}
+
+--fetch                 Fetch data. Values: systems. TODO
+--api-requests          Make api requests. TODO
+
+)";
+}
+
+int main(int argc, const char* argv[])
+{
+    nx::utils::ArgumentParser args(argc, argv);
+    if (args.get("help"))
+    {
+        printHelp();
+        return 0;
+    }
+
+    QString cdbUrl = args.get("cdb", "c").value_or(QString());
+    QString accountEmail = args.get("user", "u").value_or(QString());
+    QString accountPassword = args.get("password", "p").value_or(QString());
+    bool loadMode = static_cast<bool>(args.get("load", "l"));
+    int connectionCount = args.get<int>("connections").value_or(100);
+    int testSystemsToGenerate = args.get<int>("generate-systems", "s").value_or(-1);
+    int testRequestCount = args.get<int>("api-requests").value_or(0);
+    QString fetchRequest = args.get("fetch").value_or(QString());
+    QString maxDelayBeforeConnectStr = args.get("max-delay").value_or(QString());
+
+    nx::utils::log::initializeGlobally(args);
+
     nx::network::SocketGlobals::InitGuard socketGlobals;
-
-    QString cdbUrl;
-    QString accountEmail;
-    QString accountPassword;
-    bool loadMode = false;
-    int connectionCount = 100;
-    int testSystemsToGenerate = -1;
-    int testRequestCount = 0;
-    QString fetchRequest;
-    QString maxDelayBeforeConnectStr;
-
-    QnCommandLineParser commandLineParser;
-    commandLineParser.addParameter(&cdbUrl, "--cdb", "-c", "Cloud db url");
-    commandLineParser.addParameter(&accountEmail, "--user", "-u", "Account login");
-    commandLineParser.addParameter(&accountPassword, "--password", "-p", "Account password");
-    commandLineParser.addParameter(&loadMode, "--load", "-l", "Establish many connections");
-    commandLineParser.addParameter(&connectionCount, "--connections", "", "Test connection count");
-    commandLineParser.addParameter(
-        &maxDelayBeforeConnectStr,
-        "--max-delay", "",
-        "Maximum delay before starting connection. By default, no delay");
-    commandLineParser.addParameter(
-        &testSystemsToGenerate, "--generate-systems", "-s", "Number of random systems to generate");
-    commandLineParser.addParameter(&fetchRequest, "--fetch", nullptr, "Fetch data. Values: systems");
-    commandLineParser.addParameter(&testRequestCount, "--api-requests", nullptr, "Make api requests");
-
-    commandLineParser.parse(argc, (const char**)argv, stderr);
+    nx::network::SocketGlobals::applyArguments(args);
 
     if (cdbUrl.isEmpty() || accountEmail.isEmpty() || accountPassword.isEmpty())
     {
@@ -89,6 +106,8 @@ int main(int argc, char* argv[])
             testRequestCount,
             maxDelayBeforeConnect);
     }
+
+    std::cerr << "Nothing to do. Specify some command" << std::endl;
 
     return 0;
 }
