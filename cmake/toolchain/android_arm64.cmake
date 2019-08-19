a227b9aeb30c04fd0a4bd3a15b5b70906303af57
Denis Klychkov <dklychkov@networkoptix.com>
2019-08-19 11:45:50 +0300

    MOBILE-874: ARM64 support for Android
    <==== Mercurial data ====>
    Branch: mobile_19.4

---
+++ cmake/toolchain/android_arm64.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Android)
+set(box "android")
+
+if(NOT "$ENV{ANDROID_NDK}" STREQUAL "")
+    set(CMAKE_ANDROID_NDK "$ENV{ANDROID_NDK}")
+else()
+    set(CMAKE_ANDROID_NDK "${PACKAGES_DIR}/android/android-ndk-r17")
+endif()
+
+set(CMAKE_SYSTEM_VERSION 21)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
+set(CMAKE_ANDROID_STL_TYPE c++_shared)
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION clang)
+
+# These macros are not required because they are declared by a compiler.
+# We declare them explicitly to improve code parsing by IDEs.
+add_definitions(-DANDROID -D__ANDROID__)
