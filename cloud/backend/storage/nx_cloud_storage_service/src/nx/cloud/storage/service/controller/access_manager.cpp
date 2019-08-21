a434ddeab5a303babdbdbdb221a13532b71ea344
Nate Wilson <nwilson@networkoptix.com>
2019-08-20 17:13:38 -0700

    CLOUD-3336: Added AccessManager
    <==== Mercurial data ====>
    Branch: cloud_dev

---
+++ cloud/backend/storage/nx_cloud_storage_service/src/nx/cloud/storage/service/controller/access_manager.cpp
@@ -0,0 +1,119 @@
+#include "access_manager.h"
+
+#include <nx/cloud/db/api/auth_provider.h>
+#include <nx/cloud/db/api/cdb_client.h>
+#include <nx/cloud/storage/service/api/storage.h>
+#include <nx/utils/stree/resourcecontainer.h>
+
+#include "nx/cloud/storage/service/settings.h"
+#include "cloud_db/resource.h"
+
+namespace nx::cloud::storage::service::controller {
+
+namespace {
+
+static std::string getAccountOwner(const nx::utils::stree::ResourceContainer& authInfo)
+{
+    std::string accountOwner;
+    authInfo.get(cloud_db::Resource::accountOwner, &accountOwner);
+    return accountOwner;
+}
+
+QString toString(const db::api::UserAuthorization& userAuth)
+{
+    return lm("{requestMethod:%1, requestAuthorization=%2}")
+        .args(userAuth.requestMethod, userAuth.requestAuthorization);
+}
+
+} // namespace
+
+AccessManager::AccessManager(const conf::CloudDb& settings):
+    m_settings(settings)
+{
+}
+
+AccessManager::~AccessManager() = default;
+
+std::pair<bool, std::string> AccessManager::addStorageAllowed(
+    const nx::utils::stree::ResourceContainer& authInfo) const
+{
+    auto accountOwner = getAccountOwner(authInfo);
+    return {!accountOwner.empty(), accountOwner};
+}
+void AccessManager::readStorageAllowed(
+    const nx::utils::stree::ResourceContainer& authInfo,
+    const api::Storage& storage,
+    nx::utils::MoveOnlyFunc<void(bool)> handler)
+{
+    auto accountOwner = getAccountOwner(authInfo);
+    if (accountOwner == storage.owner)
+        return handler(true);
+
+    // Non-owner trying to access storage, check by user's access to a system in the storage
+
+    if (storage.systems.empty())
+        return handler(false);
+
+    db::api::UserAuthorization userAuth;
+    authInfo.get(cloud_db::Resource::httpMethod, &userAuth.requestMethod);
+    authInfo.get(cloud_db::Resource::authorization, &userAuth.requestAuthorization);
+
+    auto cdbClient = createCdbClient(std::move(handler));
+
+    // TODO use batch request when it is implemented
+    cdbClient->authProvider()->getSystemAccessLevel(
+        storage.systems.front(),
+        userAuth,
+        [this, userAuth, cdbClient, systemId = storage.systems.front()](
+            auto cdbResult,
+            auto systemAccess)
+        {
+            auto context = removeCdbClient(cdbClient);
+            if (cdbResult != db::api::ResultCode::ok)
+            {
+                NX_ERROR(this, "cloud_db: getSystemAccess: systemId = %1, userAuth = %2"
+                    " failed with result code: %3",
+                    systemId, toString(userAuth), db::api::toString(cdbResult));
+            }
+
+            return context.handler(systemAccess.accessRole > db::api::SystemAccessRole::disabled);
+        });
+}
+
+bool AccessManager::removeStorageAllowed(
+    const nx::utils::stree::ResourceContainer& authInfo, const api::Storage& storage) const
+{
+    return getAccountOwner(authInfo) == storage.owner;
+}
+
+db::api::CdbClient* AccessManager::createCdbClient(
+    nx::utils::MoveOnlyFunc<void(bool)> handler)
+{
+    auto cdbClient = std::make_unique<db::api::CdbClient>();
+    cdbClient->setCloudUrl(m_settings.url.toStdString());
+    auto cdbClientPtr = cdbClient.get();
+
+    ReadStorageContext readStorageContext;
+    readStorageContext.cdbClient = std::move(cdbClient);
+    readStorageContext.handler = std::move(handler);
+
+    QnMutexLocker lock(&m_mutex);
+    m_cdbRequests.emplace(cdbClientPtr, std::move(readStorageContext));
+
+    return cdbClientPtr;
+}
+
+AccessManager::ReadStorageContext AccessManager::removeCdbClient(
+    db::api::CdbClient* cdbClient)
+{
+    QnMutexLocker lock(&m_mutex);
+
+    auto it = m_cdbRequests.find(cdbClient);
+    NX_ASSERT(it != m_cdbRequests.end());
+    auto readStorageContext = std::move(it->second);
+    m_cdbRequests.erase(it);
+
+    return readStorageContext;
+}
+
+} // namespace nx::cloud::storage::service::controller
---
+++ cloud/backend/storage/nx_cloud_storage_service/src/nx/cloud/storage/service/controller/access_manager.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <string>
+
+#include <nx/utils/move_only_func.h>
+#include <nx/utils/thread/mutex.h>
+
+namespace nx {
+
+namespace utils::stree { class ResourceContainer; }
+
+namespace cloud {
+
+namespace db::api { class CdbClient; }
+
+namespace storage::service {
+
+namespace conf { struct CloudDb; }
+namespace api { struct Storage; }
+
+namespace controller {
+
+class AccessManager
+{
+public:
+    AccessManager(const conf::CloudDb& settings);
+    ~AccessManager();
+
+    std::pair<bool, std::string/*accountOwner*/> addStorageAllowed(
+        const nx::utils::stree::ResourceContainer& authInfo) const;
+
+    void readStorageAllowed(
+        const nx::utils::stree::ResourceContainer& authInfo,
+        const api::Storage& storage,
+        nx::utils::MoveOnlyFunc<void(bool)> handler);
+
+    bool removeStorageAllowed(
+        const nx::utils::stree::ResourceContainer& authInfo,
+        const api::Storage& storage) const;
+
+private:
+    struct ReadStorageContext
+    {
+        std::unique_ptr<db::api::CdbClient> cdbClient;
+        nx::utils::MoveOnlyFunc<void(bool)> handler;
+    };
+
+    db::api::CdbClient* createCdbClient(
+        nx::utils::MoveOnlyFunc<void(bool)> handler);
+    ReadStorageContext removeCdbClient(db::api::CdbClient* cdbClient);
+
+private:
+    const conf::CloudDb& m_settings;
+    QnMutex m_mutex;
+    std::map<db::api::CdbClient*, ReadStorageContext> m_cdbRequests;
+};
+
+} // namespace controller
+} // namespace storage::service
+} // namespace cloud
+} // namespace nx
