66d04a06df637f94ab23d3bdd6700aeec4ea1bb3
Nate Wilson <nwilson@networkoptix.com>
2019-08-27 15:30:39 -0700

    CLOUD-3417: Added Some UrlQuery helper functions
    <==== Mercurial data ====>
    Branch: cloud_dev

--- libs/nx_utils/src/nx/utils/url_query.cpp
+++ libs/nx_utils/src/nx/utils/url_query.cpp
@@ -2,6 +2,34 @@
 
 namespace nx::utils {
 
+namespace detail {
+
+bool convert(const QString& value, QString* outObject)
+{
+    *outObject = value;
+    return true;
+}
+
+bool convert(const QString& value, std::string* outObject)
+{
+    *outObject = value.toStdString();
+    return true;
+}
+
+bool convert(const QString& value, int* outObject)
+{
+    bool ok = false;
+    *outObject = value.toInt(&ok);
+    return ok;
+}
+
+} // namespace detail
+
+UrlQuery::UrlQuery(const QString& query):
+    m_query(query)
+{
+}
+
 UrlQuery& UrlQuery::add(const QString& key, const QString& value)
 {
     m_query.addQueryItem(key, value);
@@ -16,6 +44,11 @@ UrlQuery& UrlQuery::add(const QString& key, const std::string& value)
 UrlQuery& UrlQuery::add(const char* key, const char* value)
 {
     return add(QString(key), QString(value));
+}
+
+bool UrlQuery::hasKey(const QString& key) const
+{
+    return m_query.hasQueryItem(key);
 }
 
 QString UrlQuery::toString(QUrl::ComponentFormattingOptions encoding) const
--- libs/nx_utils/src/nx/utils/url_query.h
+++ libs/nx_utils/src/nx/utils/url_query.h
@@ -1,12 +1,30 @@
 #pragma once
 
 #include <QUrlQuery>
+#include <QVariant>
 
 namespace nx::utils {
+
+namespace detail {
+
+bool NX_UTILS_API convert(const QString& value, QString* outObject);
+bool NX_UTILS_API convert(const QString& value, std::string* outObject);
+bool NX_UTILS_API convert(const QString& value, int* outObject);
+
+template<typename T>
+bool convert(const QString& value, T* outObject)
+{
+    return false;
+}
+
+} //namespace detail
 
 class NX_UTILS_API UrlQuery
 {
 public:
+    UrlQuery() = default;
+    UrlQuery(const QString& query);
+
     UrlQuery& add(const QString& key, const QString& value);
     UrlQuery& add(const QString& key, const std::string& value);
     UrlQuery& add(const char* key, const char* value);
@@ -17,6 +35,34 @@ public:
     UrlQuery& add(const QString& key, NumericType value)
     {
         return add(key, QString::number(value));
+    }
+
+    bool hasKey(const QString& key) const;
+
+    /**
+     * @return true if the value converted to the desired type successfully
+     * NOTE: requires template specialization for new types, see detail namespace.
+     */
+    template<typename ObjectType>
+    bool value(
+        const QString& key,
+        ObjectType* outObject,
+        QUrl::ComponentFormattingOptions encoding = QUrl::PrettyDecoded) const
+    {
+        return detail::convert(m_query.queryItemValue(key, encoding), outObject);
+    }
+
+    /**
+     * @return true if the key exists and the value converted to the desired type successfully
+     * NOTE: requires template specialization for new types, see detail namespace.
+     */
+    template<typename ObjectType>
+    bool valueIfExists(
+        const QString& key,
+        ObjectType* outObject,
+        QUrl::ComponentFormattingOptions encoding = QUrl::PrettyDecoded) const
+    {
+        return hasKey(key) && value(key, outObject, encoding);
     }
 
     QString toString(QUrl::ComponentFormattingOptions encoding = QUrl::PrettyDecoded) const;
