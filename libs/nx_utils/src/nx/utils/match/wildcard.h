ef4158ac15a76b09ca918bb611e9c15bce935554
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-08-28 11:21:55 -0700

    VMS-CR-18917: nx_utils/wildcard: added Unicode support (through use of QString)
    <==== Mercurial data ====>
    Branch: meta_4.0

--- libs/nx_utils/src/nx/utils/match/wildcard.h
+++ libs/nx_utils/src/nx/utils/match/wildcard.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <cctype>
+#include <iterator>
+#include <type_traits>
+
 #include <QString>
 
 enum class MatchMode
@@ -8,18 +12,112 @@ enum class MatchMode
     caseInsensitive,
 };
 
+namespace nx::utils::detail {
+
+template <typename CharType>
+struct CharTraits {};
+
+template <>
+struct CharTraits<char>
+{
+    using CharType = char;
+
+    static bool equal(CharType one, CharType two, MatchMode mode)
+    {
+        return mode == MatchMode::caseSensitive
+            ? one == two
+            : std::tolower(one) == std::tolower(two);
+    }
+};
+
+template <>
+struct CharTraits<QChar>
+{
+    using CharType = QChar;
+
+    static bool equal(CharType one, CharType two, MatchMode mode)
+    {
+        return mode == MatchMode::caseSensitive
+            ? one == two
+            : one.toLower() == two.toLower();
+    }
+};
+
+} // namespace nx::utils::detail
+
 /**
  * Check whether a string matches a wildcard expression.
  *
  * @param str String being validated to match the wildcard expression.
- * @param mask Wildcard expression. Allowed to contain special chars '?' (matches any char except
- * '.') and '*' (matches any number of any chars).
- * @return Whether str matches mask.
+ * @param mask Wildcard expression. Allowed to contain special characters:
+ * '?' (matches any character except '.')
+ * and '*' (matches any number of any characters).
+ * @return true if mask matches str.
  */
-NX_UTILS_API bool wildcardMatch(
-    const char* mask,
-    const char* str,
-    MatchMode mode = MatchMode::caseSensitive);
+template<typename Iter>
+// requires ForwardIter<Iter>
+bool wildcardMatch(
+    Iter mask, Iter maskEnd,
+    Iter str, Iter strEnd,
+    MatchMode mode)
+{
+    using CharType = std::decay_t<decltype(*mask)>;
+
+    while (str != strEnd && mask != maskEnd)
+    {
+        if (*mask == CharType('*'))
+        {
+            if (*(std::next(mask)) == CharType('\0'))
+                return true; //< We have '*' at the end.
+            for (auto str1 = str; str1 != strEnd; ++str1)
+            {
+                if (wildcardMatch(std::next(mask), maskEnd, str1, strEnd, mode))
+                    return true;
+            }
+            return false; //< Not matching.
+        }
+        else if (*mask == CharType('?'))
+        {
+            if (*str == CharType('.'))
+                return false;
+        }
+        else
+        {
+            if (!nx::utils::detail::CharTraits<CharType>::equal(*str, *mask, mode))
+                return false;
+        }
+
+        ++str;
+        ++mask;
+    }
+
+    while (mask != maskEnd)
+    {
+        if (*mask != CharType('*'))
+            return false;
+        ++mask;
+    }
+
+    if (str != strEnd)
+        return false;
+
+    return true;
+}
+
+/**
+ * Matches null-terminated strings.
+ */
+template <typename CharType>
+bool wildcardMatch(
+    const CharType* mask,
+    const CharType* str,
+    MatchMode mode)
+{
+    return wildcardMatch(
+        mask, mask + std::strlen(mask),
+        str, str + std::strlen(str),
+        mode);
+}
 
 inline bool wildcardMatch(
     const QString& mask,
@@ -27,7 +125,7 @@ inline bool wildcardMatch(
     MatchMode mode = MatchMode::caseSensitive)
 {
     return wildcardMatch(
-        mask.toLatin1().constData(),
-        str.toLatin1().constData(),
+        mask.begin(), mask.end(),
+        str.begin(), str.end(),
         mode);
 }
