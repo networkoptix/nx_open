26818671da0f11108b8700dba25401c7c924b8fe
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-08-29 12:46:30 +0300

    VMS-15388: remove assert if dbManager changes id after initialization
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/server/nx_vms_server_db/src/database/db_manager.cpp
+++ vms/server/nx_vms_server_db/src/database/db_manager.cpp
@@ -5397,7 +5397,6 @@ bool QnDbManager::updateId()
 
 void QnDbManager::setDbId(const QnUuid& value)
 {
-    NX_ASSERT(!m_initialized);
     m_dbInstanceId = value;
     const auto& runtimeInfoManager = commonModule()->runtimeInfoManager();
     QnPeerRuntimeInfo localInfo = runtimeInfoManager->localInfo();
