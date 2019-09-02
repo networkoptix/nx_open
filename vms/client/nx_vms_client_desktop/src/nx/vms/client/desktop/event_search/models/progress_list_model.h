c42df0a174e75ce16d68199c12bbd05c08703386
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2019-09-02 22:49:52 +0300

    VMS-13985: Enhance WorkbenchProgressManager with actions and ability to cancel operations
    <==== Mercurial data ====>
    Branch: vms_4.0_gui

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/models/progress_list_model.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/models/progress_list_model.h
@@ -27,6 +27,9 @@ public:
     virtual bool setData(const QModelIndex& index, const QVariant& value,
         int role = Qn::DefaultNotificationRole) override;
 
+    virtual bool removeRows(
+        int row, int count, const QModelIndex& parent = QModelIndex()) override;
+
 private:
     nx::utils::KeyList<QnUuid> m_activities;
 };
