062e53a7f50ab25fc0f8fa001e23c8248794463c
Nate Wilson <nwilson@networkoptix.com>
2019-09-05 13:02:43 -0700

    NCD-152: Compilation fix
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/deploy/storage_service/make.sh
+++ cloud/deploy/storage_service/make.sh
@@ -8,7 +8,7 @@ MODULE=storage_service
 function stage_cmake_extra()
 {
         mkdir -p stage/${moduleName}/var
-        cp -l $environment/packages/any/Geolite2-City.mmdb stage/${moduleName}/var/
+        cp -l $environment/packages/any/GeoLite2-City.mmdb stage/${moduleName}/var/
 }
 
 main $@
