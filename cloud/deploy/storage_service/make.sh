03e0941f74aa416ae4fdca26fa76926dd0c8e073
Nate Wilson <nwilson@networkoptix.com>
2019-09-05 14:33:28 -0700

    NCD-152: Fix incorrect path
    <==== Mercurial data ====>
    Branch: cloud_dev

--- cloud/deploy/storage_service/make.sh
+++ cloud/deploy/storage_service/make.sh
@@ -7,8 +7,8 @@ MODULE=storage_service
 
 function stage_cmake_extra()
 {
-        mkdir -p stage/${moduleName}/var
-        cp -l $environment/packages/any/GeoLite2-City.mmdb stage/${moduleName}/var/
+    mkdir -p stage/${moduleName}/var
+    cp -l $environment/packages/any/geolite-2/GeoLite2-City.mmdb stage/${moduleName}/var/
 }
 
 main $@
