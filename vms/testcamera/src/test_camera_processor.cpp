6cd673f0592dd268a385538f09a400f3f8d2b1e4
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-09-10 19:16:10 +0100

    VMS-15591: Testcamera: Add args for overriding FPS independently for primary and secondary stream
    Additionally, fixed incorrect error reporting in case of correct args; related improvements.
    
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/testcamera/src/test_camera_processor.cpp
+++ vms/testcamera/src/test_camera_processor.cpp
@@ -13,11 +13,13 @@ QnTestCameraProcessor::QnTestCameraProcessor(
     std::unique_ptr<nx::network::AbstractStreamSocket> socket,
     QnTcpListener* owner,
     bool noSecondaryStream,
-    int fps)
+    int fpsPrimary,
+    int fpsSecondary)
     :
     QnTCPConnectionProcessor(std::move(socket), owner),
     m_noSecondaryStream(noSecondaryStream),
-    m_fps(fps)
+    m_fpsPrimary(fpsPrimary),
+    m_fpsSecondary(fpsSecondary)
 {
 }
 
@@ -30,51 +32,54 @@ void QnTestCameraProcessor::run()
 {
     Q_D(QnTestCameraProcessor);
     quint8 buffer[128];
-    if (!m_needStop && d->socket->isConnected())
+    if (m_needStop || !d->socket->isConnected())
+        return;
+
+    const int bytesRead = d->socket->recv(buffer, sizeof(buffer));
+    if (bytesRead == 0)
     {
-        int readed = d->socket->recv(buffer, sizeof(buffer));
-        if (readed == 0)
+        qDebug() << "No MAC address specified";
+        return;
+    }
+    QByteArray cameraUrl((const char*) buffer, bytesRead);
+    if (!cameraUrl.isEmpty() && cameraUrl[0] == '/')
+        cameraUrl = cameraUrl.mid(1);
+
+    const int pos = cameraUrl.indexOf('?');
+    const QByteArray macAddress = cameraUrl.left(pos);
+    const QList<QByteArray> params = cameraUrl.mid(pos+1).split('&');
+
+    QnTestCamera* const camera = QnCameraPool::instance()->findCamera(macAddress);
+    if (camera == nullptr)
+    {
+        qDebug() << "No camera found with MAC address " << macAddress;
+        return;
+    }
+
+    // Try to receive the values from the Client.
+    bool isSecondary = false;
+    int fps = 10;
+    for (int i = 0; i < params.size(); ++ i)
+    {
+        QList<QByteArray> paramVal = params[i].split('=');
+        if (paramVal[0] == "primary" && paramVal.size() == 2)
         {
-            qDebug() << "No MAC address specified";
-            return;
+            if (paramVal[1] != "1")
+                isSecondary = true;
         }
-        QByteArray cameraUrl((const char*) buffer, readed);
-        if (!cameraUrl.isEmpty() && cameraUrl[0] == '/')
-            cameraUrl = cameraUrl.mid(1);
-
-        int pos = cameraUrl.indexOf('?');
-        QByteArray macAddress = cameraUrl.left(pos);
-        QList<QByteArray> params = cameraUrl.mid(pos+1).split('&');
-
-        QnTestCamera* camera = QnCameraPool::instance()->findCamera(macAddress);
-        if (camera == 0)
+        else if (paramVal[0] == "fps" && paramVal.size() == 2)
         {
-            qDebug() << "No camera found with MAC address " << macAddress;
-            return;
-        }
-        bool isSecondary = false;
-        int fps = 10;
-        for (int i = 0; i < params.size(); ++ i)
-        {
-            QList<QByteArray> paramVal = params[i].split('=');
-            if (paramVal[0] == "primary" && paramVal.size() == 2)
-            {
-                if (paramVal[1] != "1")
-                    isSecondary = true;
-            }
-            else if (paramVal[0] == "fps" && paramVal.size() == 2)
-            {
-                fps = paramVal[1].toInt();
-            }
-        }
-
-        if (m_fps != -1)
-            fps = m_fps;
-
-        if (!isSecondary || !m_noSecondaryStream)
-        {
-            qDebug() << "Start" << (isSecondary ? "secondary" : "primary") << "stream";
-            camera->startStreaming(d->socket.get(), isSecondary, fps, commonModule());
+            fps = paramVal[1].toInt();
         }
     }
+
+    if (isSecondary && m_noSecondaryStream)
+        return;
+
+    const int overridingFps = isSecondary ? m_fpsSecondary : m_fpsPrimary;
+    if (overridingFps != -1)
+        fps = overridingFps;
+
+    qDebug() << "Start" << (isSecondary ? "secondary" : "primary") << "stream at" << fps << "fps";
+    camera->startStreaming(d->socket.get(), isSecondary, fps, commonModule());
 }
