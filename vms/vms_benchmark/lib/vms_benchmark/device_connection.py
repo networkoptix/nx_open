502c1b206a0ae2f9868f203748139cb37a968d8a
George Sovetov <gsovetov@networkoptix.com>
2019-09-10 12:47:13 -0700

    VMS Benchmark: Execute SSH client on each command
    <==== Mercurial data ====>
    Branch: vms_4.0_server

--- vms/vms_benchmark/lib/vms_benchmark/device_connection.py
+++ vms/vms_benchmark/lib/vms_benchmark/device_connection.py
@@ -1,15 +1,10 @@
 from vms_benchmark import exceptions
-from os import pipe, fdopen, close, environ, dup2, execvpe, kill, waitpid
-import time
 import sys
 import platform
 import subprocess
 from io import StringIO
 
 if platform.system() == 'Linux':
-    from os import O_NONBLOCK, fork
-    from fcntl import fcntl, F_GETFL, F_SETFL
-
     class DeviceConnection:
         class DeviceConnectionResult:
             def __init__(self, return_code, message=None):
@@ -41,49 +36,7 @@ if platform.system() == 'Linux':
                 ]
             else:
                 self.ssh_command = 'ssh'
-                self.ssh_args = ['ssh', "-t", f"-p{port}", f"{login}@{host}" if login else host, "sh", "-i"]
-            self.fds = {
-                'stdout': pipe(),
-                'stderr': pipe(),
-                'stdin': pipe(),
-            }
-
-            for fd in ('stdout', 'stderr'):
-                fcntl(self.fds[fd][0], F_SETFL, fcntl(self.fds[fd][0], F_GETFL) | O_NONBLOCK)
-            for fd in ('stdout', 'stderr'):
-                setattr(self, fd, fdopen(self.fds[fd][0], 'r'))
-            self.stdin = fdopen(self.fds['stdin'][1], 'w', buffering=1)
-
-            pid = fork()
-
-            if pid == 0:
-                dup2(self.fds['stdin'][0], 0)
-                dup2(self.fds['stdout'][1], 1)
-                dup2(self.fds['stderr'][1], 2)
-                execvpe(self.ssh_command, self.ssh_args, environ)
-
-            self.pid = pid
-
-            time.sleep(0.1)
-
-            def is_process_running(pid):
-                try:
-                    kill(pid, 0)
-                    return True
-                except OSError:
-                    return False
-
-            self.stdin.write("PS1=$(printf '\\03')\n")
-            while True:
-                time.sleep(0.001)
-                ch = self.stderr.read(1)
-                if str(ch) == str(chr(3)):
-                    break
-                else:
-                    pass
-                if not is_process_running(pid):
-                    print("Worker process exited unexpectedly", file=sys.stderr)
-                    sys.exit(1)
+                self.ssh_args = ['ssh', f"-p{port}", f"{login}@{host}" if login else host]
 
             self.host_key = None
             self.ip = None
@@ -97,83 +50,29 @@ if platform.system() == 'Linux':
             self.local_ip = ssh_connection_info[0]
             self.is_root = self.eval('id -u') == '0'
 
-        def _wait_finish_marker(self, stdout_cb=None, stderr_cb=None, timeout=15.):
-            started_at = time.time()
-            right_sequence = [chr(3)]
-            while time.time() - started_at < timeout:
-                ch = self.stderr.read(1)
-                if ch == '':
-                    time.sleep(0.001)
-                    continue
-                if ch == right_sequence[0]:
-                    right_sequence.pop(0)
-                    if len(right_sequence) == 0:
-                        return True
-                    else:
-                        continue
-                else:
-                    if callable(stderr_cb) and not stderr_cb(ch):
-                        return False
-                if callable(stdout_cb):
-                    [stdout_cb(line) for line in self.stdout.readlines()]
-            return False
-
-        def _ping(self, timeout=1):
-            self.stdout.readlines()
-            self.stderr.readlines()
-            self.stdin.write("\n")
-            return self._wait_finish_marker(stderr_cb=lambda _d: False, timeout=timeout)
-
         def sh(self, command, timeout=3, su=False, exc=False, stdout=sys.stdout, stderr=sys.stderr):
             command_wrapped = command if self.is_root or not su else f'sudo -n {command}'
 
-            if not self._ping():
-                message = f'Remote executor is not responding before command `{command_wrapped}`.'
+            run = subprocess.run([*self.ssh_args, command_wrapped], timeout=timeout, capture_output=True)
+            if run.returncode == 255:
                 if exc:
-                    raise exceptions.DeviceCommandError(message=message)
+                    raise exceptions.DeviceCommandError(message=run.stderr.rstrip())
                 else:
-                    return self.DeviceConnectionResult(None, message)
+                    return self.DeviceConnectionResult(None, run.stderr.rstrip())
 
-            self.stdin.write(command_wrapped)
-            self.stdin.write("\n")
-
-            def out_print(d):
-                write_method = getattr(stdout, 'write', None)
-                if callable(write_method):
-                    stdout.write(d)
-                return True
-
-            def err_print(d):
-                write_method = getattr(stderr, 'write', None)
-                if callable(write_method):
-                    stderr.write(d)
-                return True
-
-            if not self._wait_finish_marker(stdout_cb=out_print, stderr_cb=err_print, timeout=timeout):
-                message = f'Remote executor is not responding after command `{command_wrapped}`.'
-                if exc:
-                    raise exceptions.DeviceCommandError(message=message)
-                else:
-                    return self.DeviceConnectionResult(None, message)
-
-            time.sleep(0.01)
-            [out_print(line) for line in self.stdout.readlines()]
-            self.stdin.write("echo $?\n")
-            if not self._wait_finish_marker(stderr_cb=err_print, timeout=0.1):
-                message = f"Can't obtain return code of the command {command_wrapped}"
-                if exc:
-                    raise exceptions.DeviceCommandError(message=message)
-                else:
-                    return self.DeviceConnectionResult(None, message)
-
-            return_code = int(''.join(self.stdout.readlines()))
-
-            if return_code != 0 and exc:
+            if run.returncode != 0 and exc:
                 raise exceptions.DeviceCommandError(
-                    message=f'Command `{command_wrapped}` failed with code {return_code}'
+                    message=f'Command `{command_wrapped}` failed with code {run.returncode}'
                 )
 
-            return self.DeviceConnectionResult(return_code)
+            if stdout:
+                stdout.write(run.stdout.decode())
+                stdout.flush()
+            if stderr:
+                stderr.write(run.stderr.decode())
+                stderr.flush()
+
+            return self.DeviceConnectionResult(run.returncode)
 
         def eval(self, cmd, timeout=3, su=False, stderr=sys.stderr):
             out = StringIO()
@@ -186,14 +85,6 @@ if platform.system() == 'Linux':
 
         def get_file_content(self, path, su=False, stderr=sys.stderr, timeout=15):
             return self.eval(f'cat "{path}"', su=su, stderr=stderr, timeout=timeout)
-
-        def __del__(self):
-            try:
-                kill(self.pid, 9)
-                waitpid(self.pid, 0)
-            except ChildProcessError:
-                pass
-            [close(self.fds[name][i]) for (name, i) in (('stdin', 1), ('stdout', 0), ('stderr', 0))]
 
 elif platform.system() == 'Windows' or platform.system().startswith('CYGWIN'):
     class DeviceConnection:
