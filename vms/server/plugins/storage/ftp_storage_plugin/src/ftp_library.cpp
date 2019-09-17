b294ed2a52679ad7f6631a3023d115180c36bb94
Alexander Kulikov <akulikov@networkoptix.com>
2019-09-16 20:16:23 -0700

    META-403: Fix windows build
    <==== Mercurial data ====>
    Branch: vms_4.0

--- vms/server/plugins/storage/ftp_storage_plugin/src/ftp_library.cpp
+++ vms/server/plugins/storage/ftp_storage_plugin/src/ftp_library.cpp
@@ -524,7 +524,8 @@ static FileNameAndPath localUniqueFilePath(const std::string& suffix)
     std::string tmpFolderPath;
     #if defined (_WIN32)
         tmpFolderPath.resize(MAX_PATH + 1);
-        DWORD result = GetTempPathA(static_cast<DWORD>(tmpFolderPath.size()), tmpFolderPath.data());
+        DWORD result = GetTempPathA(
+            static_cast<DWORD>(tmpFolderPath.size()), (LPSTR) tmpFolderPath.data());
         assert(result > 0);
         if (result == 0)
             std::cerr << "Failed to get a temporary folder path" << std::endl;
