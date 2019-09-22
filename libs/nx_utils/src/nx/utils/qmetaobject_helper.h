75bf51c98bc2ff3194312776ee3d0c30c2d3defe
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-09-22 20:49:16 +0300

    VMS-15717: codestyle
    <==== Mercurial data ====>
    Branch: vms_4.1

--- libs/nx_utils/src/nx/utils/qmetaobject_helper.h
+++ libs/nx_utils/src/nx/utils/qmetaobject_helper.h
@@ -6,7 +6,7 @@ namespace nx::utils {
  * Emit QT signal asynchronously via sender object event queue.
  */
 template <typename ... Args>
-void emitAsync(QObject* object, const char *signal, const Args&... args)
+void emitAsync(QObject* object, const char* signal, const Args&... args)
 {
     QMetaObject::invokeMethod(object, signal, Qt::QueuedConnection,
         QArgument<decltype(args)>(typeid(args).name(), args)...);
