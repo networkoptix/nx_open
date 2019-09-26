6023ceb92a13bb7672d9b0c8b3ebb33c0f233980
Nate Wilson <nwilson@networkoptix.com>
2019-09-25 17:12:35 -0700

    CLOUD-3656: Dont std::move() location as it is used in multiple arguments.
    <==== Mercurial data ====>
    Branch: cloud_backend_19.1

--- cloud/backend/libconnection_mediator/src/nx/cloud/mediator/relay/online_relays_cluster_client.cpp
+++ cloud/backend/libconnection_mediator/src/nx/cloud/mediator/relay/online_relays_cluster_client.cpp
@@ -121,7 +121,7 @@ void OnlineRelaysClusterClient::onRelayDiscovered(nx::cloud::discovery::Node tra
         QnMutexLocker lock(&m_mutex);
         m_resolvedRelays.emplace(
             location->continent,
-            RelayContext{std::move(*location), std::move(trafficRelayNode)});
+            RelayContext{*location, std::move(trafficRelayNode)});
     }
     else
     {
@@ -188,7 +188,7 @@ std::vector<nx::utils::Url> OnlineRelaysClusterClient::findClosestRelays(
         if (!relay.second.urls.empty())
             urls.emplace_back(baseUrl(relay.second.urls.front()));
 
-        if ((int)urls.size() >= m_settings.geoIp().resolveErrorUrlCount)
+        if ((int) urls.size() >= m_settings.geoIp().resolveErrorUrlCount)
             break;
     }
 
@@ -204,7 +204,7 @@ std::vector<nx::utils::Url> OnlineRelaysClusterClient::getUnresolvedRelays() con
         if (!relay.urls.empty())
             urls.emplace_back(baseUrl(relay.urls.front()));
 
-        if ((int)urls.size() >= m_settings.geoIp().resolveErrorUrlCount)
+        if ((int) urls.size() >= m_settings.geoIp().resolveErrorUrlCount)
             break;
     }
 
