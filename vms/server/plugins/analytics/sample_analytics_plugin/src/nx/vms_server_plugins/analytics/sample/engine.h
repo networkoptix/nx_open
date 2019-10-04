4eb3892073eaf5bdebbee8b4ee9999f7165f3910
Roman Inflianskas <rinflianskas@networkoptix.com>
2019-10-04 09:20:35 +0300

    VMS-15804: Add sample_analytics_plugin
    Including this plugin into Metadata SDK will be a separate commit.
    
    <==== Mercurial data ====>
    Branch: vms_4.0

---
+++ vms/server/plugins/analytics/sample_analytics_plugin/src/nx/vms_server_plugins/analytics/sample/engine.h
@@ -0,0 +1,33 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/sdk/analytics/helpers/plugin.h>
+#include <nx/sdk/analytics/helpers/engine.h>
+#include <nx/sdk/analytics/i_uncompressed_video_frame.h>
+
+namespace nx {
+namespace vms_server_plugins {
+namespace analytics {
+namespace sample {
+
+class Engine: public nx::sdk::analytics::Engine
+{
+public:
+    Engine(nx::sdk::analytics::Plugin* plugin);
+
+    virtual ~Engine() override;
+
+protected:
+    virtual std::string manifestString() const override;
+
+protected:
+    virtual void doObtainDeviceAgent(
+        nx::sdk::Result<nx::sdk::analytics::IDeviceAgent*>* outResult,
+        const nx::sdk::IDeviceInfo* deviceInfo) override;
+};
+
+} // namespace sample
+} // namespace analytics
+} // namespace vms_server_plugins
+} // namespace nx
