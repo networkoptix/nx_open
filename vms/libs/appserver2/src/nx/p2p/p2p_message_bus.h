8427f9c752d2195ae0cc1e498b4f8e6b84780c1c
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-10-09 13:07:58 +0300

    Move template functions implementation from p2p_message_bus header to the .cpp
    <==== Mercurial data ====>
    Branch: vms_4.1_server

--- vms/libs/appserver2/src/nx/p2p/p2p_message_bus.h
+++ vms/libs/appserver2/src/nx/p2p/p2p_message_bus.h
@@ -81,38 +81,14 @@ public:
     virtual void dropConnections() override;
     virtual ConnectionInfoList connectionsInfo() const override;
 
-    void sendTransaction(const ec2::QnTransaction<vms::api::RuntimeData>& tran)
-    {
-        QnMutexLocker lock(&m_mutex);
-        vms::api::PersistentIdData peerId(tran.params.peer);
-        m_lastRuntimeInfo[peerId] = tran.params;
-        for (const auto& connection : m_connections)
-            sendTransactionImpl(connection, tran, TransportHeader());
-    }
+    void sendTransaction(const ec2::QnTransaction<vms::api::RuntimeData>& tran);
+    template<class T> void sendTransaction(const ec2::QnTransaction<T>& tran);
 
     template<class T>
-    void sendTransaction(const ec2::QnTransaction<T>& tran)
-    {
-        QnMutexLocker lock(&m_mutex);
-        for (const auto& connection: m_connections)
-            sendTransactionImpl(connection, tran, TransportHeader());
-    }
+    void sendTransaction(const ec2::QnTransaction<T>& tran, const TransportHeader& header);
 
     template<class T>
-    void sendTransaction(const ec2::QnTransaction<T>& tran, const TransportHeader& header)
-    {
-        QnMutexLocker lock(&m_mutex);
-        for (const auto& connection : m_connections)
-            sendTransactionImpl(connection, tran, header);
-    }
-
-    template<class T>
-    void sendTransaction(const ec2::QnTransaction<T>& tran, const vms::api::PeerSet& dstPeers)
-    {
-        NX_ASSERT(tran.command != ApiCommand::NotDefined);
-        QnMutexLocker lock(&m_mutex);
-        sendUnicastTransaction(tran, dstPeers);
-    }
+    void sendTransaction(const ec2::QnTransaction<T>& tran, const vms::api::PeerSet& dstPeers);
 
     bool isSubscribedTo(const vms::api::PersistentIdData& peer) const;
     qint32 distanceTo(const vms::api::PersistentIdData& peer) const;
@@ -126,223 +102,15 @@ protected:
     void sendTransactionImpl(
         const P2pConnectionPtr& connection,
         const ec2::QnTransaction<T>& srcTran,
-        TransportHeader transportHeader)
-    {
-        NX_ASSERT(srcTran.command != ApiCommand::NotDefined);
-
-        const vms::api::PersistentIdData remotePeer(connection->remotePeer());
-        if (!connection->shouldTransactionBeSentToPeer(srcTran))
-        {
-            NX_VERBOSE(this, "Peer %1 does not handler transactions like %2", remotePeer.id, srcTran);
-            return;
-        }
-
-        if (transportHeader.via.find(remotePeer.id) != transportHeader.via.end())
-        {
-            NX_VERBOSE(this, "Peer %1 already handled transaction %2", remotePeer.id, srcTran);
-            return;
-        }
-
-        const auto& descriptor = ec2::getTransactionDescriptorByTransaction(srcTran);
-        auto remoteAccess = descriptor->checkRemotePeerAccessFunc(
-            commonModule(), connection.staticCast<Connection>()->userAccessData(), srcTran.params);
-        if (remoteAccess == RemotePeerAccess::Forbidden)
-        {
-            NX_VERBOSE(this, "Permission check failed while sending transaction %1 to peer %2",
-                srcTran, remotePeer.id);
-            return;
-        }
-
-        const vms::api::PersistentIdData peerId(srcTran.peerID, srcTran.persistentInfo.dbID);
-        const auto context = this->context(connection);
-
-        ec2::QnTransaction<T> modifiedTran;
-        if (connection->remotePeer().isClient())
-        {
-            modifiedTran = srcTran;
-            if (ec2::amendOutputDataIfNeeded(
-                    connection.staticCast<Connection>()->userAccessData(),
-                    commonModule()->resourceAccessManager(),
-                    &modifiedTran.params))
-            {
-                // Make persistent info null in case if data has been amended. We don't want such
-                // transactions be checked against serialized transactions cache.
-                modifiedTran.persistentInfo = ec2::QnAbstractTransaction::PersistentInfo();
-            }
-        }
-        const ec2::QnTransaction<T>& tran(connection->remotePeer().isClient() ? modifiedTran : srcTran);
-
-        if (connection->remotePeer().isServer())
-        {
-            if (descriptor->isPersistent)
-            {
-                if (context->sendDataInProgress)
-                {
-                    NX_VERBOSE(this, "Send to server %1 already in progress", remotePeer.id);
-                    return;
-                }
-
-                if (!context->updateSequence(tran))
-                {
-                    NX_VERBOSE(this, "Server %1 already knows transaction %2", remotePeer.id, tran);
-                    return;
-                }
-            }
-            else
-            {
-                if (!context->isRemotePeerSubscribedTo(tran.peerID))
-                {
-                    NX_VERBOSE(this, "Peer %1 is not subscribed for %2", remotePeer.id, tran.peerID);
-                    return;
-                }
-            }
-        }
-        else if (remotePeer == peerId)
-        {
-            NX_VERBOSE(this, "Peer %1 is myself");
-            return;
-        }
-        else if (connection->remotePeer().isCloudServer())
-        {
-            if (!descriptor->isPersistent)
-            {
-                NX_VERBOSE(this, "Cloud %1 is not iterested in non-persistent transactions", remotePeer.id);
-                return;
-            }
-
-            if (context->sendDataInProgress)
-            {
-                NX_VERBOSE(this, "Send to cloud %1 already in progress", remotePeer.id);
-                return;
-            }
-
-            if (!context->updateSequence(tran))
-            {
-                NX_VERBOSE(this, "Cloud %1 already knows transaction %2", remotePeer.id, tran);
-                return;
-            }
-        }
-
-        NX_ASSERT(!(remotePeer == peerId)); //< loop
-
-        if (nx::utils::log::isToBeLogged(nx::utils::log::Level::debug, this))
-            printTran(connection, tran, Connection::Direction::outgoing);
-
-        switch (connection->remotePeer().dataFormat)
-        {
-        case Qn::JsonFormat:
-            connection->sendMessage(
-                m_jsonTranSerializer->serializedTransactionWithoutHeader(tran) + QByteArray("\r\n"));
-            break;
-        case Qn::UbjsonFormat:
-            if (connection->remotePeer().isClient())
-            {
-                connection->sendMessage(
-                    m_ubjsonTranSerializer->serializedTransactionWithoutHeader(tran));
-            }
-            else if (descriptor->isPersistent)
-            {
-                connection->sendMessage(MessageType::pushTransactionData,
-                    m_ubjsonTranSerializer->serializedTransactionWithoutHeader(tran));
-            }
-            else
-            {
-                TransportHeader header(transportHeader);
-                header.via.insert(localPeer().id);
-                connection->sendMessage(MessageType::pushImpersistentBroadcastTransaction,
-                    m_ubjsonTranSerializer->serializedTransactionWithHeader(tran, header));
-            }
-            break;
-        default:
-            qWarning() << "Client has requested data in an unsupported format"
-                << connection->remotePeer().dataFormat;
-            break;
-        }
-    }
+        TransportHeader transportHeader);
 
     template<class T>
-    void sendUnicastTransaction(const QnTransaction<T>& tran, const vms::api::PeerSet& dstPeers)
-    {
-        QMap<P2pConnectionPtr, TransportHeader> dstByConnection;
-
-        // split dstPeers by connection
-        for (const auto& peer : dstPeers)
-        {
-            qint32 distance = kMaxDistance;
-            auto dstPeer = routeToPeerVia(peer, &distance, /*address*/ nullptr);
-            if (auto& connection = m_connections.value(dstPeer))
-                dstByConnection[connection].dstPeers.push_back(peer);
-        }
-        sendUnicastTransactionImpl(tran, dstByConnection);
-    }
+    void sendUnicastTransaction(const QnTransaction<T>& tran, const vms::api::PeerSet& dstPeers);
 
     template<class T>
     void sendUnicastTransactionImpl(
         const QnTransaction<T>& tran,
-        const QMap<P2pConnectionPtr, TransportHeader>& dstByConnection)
-    {
-        for (auto itr = dstByConnection.begin(); itr != dstByConnection.end(); ++itr)
-        {
-            const auto& connection = itr.key();
-            TransportHeader transportHeader = itr.value();
-
-            if (transportHeader.via.find(connection->remotePeer().id) != transportHeader.via.end())
-                continue; //< Already processed by remote peer
-
-            if (!connection->shouldTransactionBeSentToPeer(tran))
-            {
-                NX_ASSERT(false,
-                    lm("Transaction '%1' can't be send to peer %2 (%3)")
-                    .arg(toString(tran.command))
-                    .arg(connection->remotePeer().id.toString())
-                    .arg(QnLexical::serialized(connection->remotePeer().peerType)));
-                return; //< This peer doesn't handle transactions of such type.
-            }
-
-            if (connection->remotePeer().isClient())
-            {
-                if (transportHeader.dstPeers.size() != 1
-                    || transportHeader.dstPeers[0] != connection->remotePeer().id)
-                {
-                    NX_ASSERT(0, lm("Unicast transaction routing error. "
-                        "Transaction %1 skipped. remotePeer: %2")
-                        .args(tran.command, connection->remotePeer().id));
-                    return;
-                }
-                switch (connection->remotePeer().dataFormat)
-                {
-                    case Qn::JsonFormat:
-                            connection->sendMessage(
-                                m_jsonTranSerializer->serializedTransactionWithoutHeader(tran) + QByteArray("\r\n"));
-                        break;
-                    case Qn::UbjsonFormat:
-                        connection->sendMessage(
-                            m_ubjsonTranSerializer->serializedTransactionWithoutHeader(tran));
-                        break;
-                    default:
-                        NX_WARNING(this,
-                            lm("Client has requested data in an unsupported format %1")
-                            .arg(connection->remotePeer().dataFormat));
-                        break;
-                }
-            }
-            else
-            {
-                switch (connection->remotePeer().dataFormat)
-                {
-                    case Qn::UbjsonFormat:
-                        transportHeader.via.insert(localPeer().id);
-                        connection->sendMessage(MessageType::pushImpersistentUnicastTransaction,
-                            m_ubjsonTranSerializer->serializedTransactionWithHeader(tran, transportHeader));
-                        break;
-                    default:
-                        NX_WARNING(this, lm("Server has requested data in an unsupported format %1")
-                            .arg(connection->remotePeer().dataFormat));
-                        break;
-                    }
-            }
-        }
-    }
+        const QMap<P2pConnectionPtr, TransportHeader>& dstByConnection);
 
     void printTran(
         const P2pConnectionPtr& connection,
@@ -384,23 +152,7 @@ protected:
     bool processSpecialTransaction(
         const QnTransaction<T>& tran,
         const nx::p2p::P2pConnectionPtr& connection,
-        const nx::p2p::TransportHeader& transportHeader)
-    {
-        if (nx::utils::log::isToBeLogged(nx::utils::log::Level::verbose, this))
-            printTran(connection, tran, Connection::Direction::incoming);
-
-        vms::api::PersistentIdData peerId(tran.peerID, tran.persistentInfo.dbID);
-
-        // Process special cases.
-        switch (tran.command)
-        {
-            case ApiCommand::runtimeInfoChanged:
-                processRuntimeInfo(tran, connection, transportHeader);
-                return true;
-            default:
-                return false; //< Not a special case.
-        }
-    }
+        const nx::p2p::TransportHeader& transportHeader);
 
     void gotTransaction(
         const QnTransaction<vms::api::UpdateSequenceData> &tran,
