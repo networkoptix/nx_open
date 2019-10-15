aca8635ae6db9c1848fdf7bc22370eaa64d05847
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-10-15 20:53:48 +0300

    VMS-15964: storage metrics UT, 'activity' group
    <==== Mercurial data ====>
    Branch: vms_4.1_server

--- vms/server/nx_vms_server/unit_tests/src/api/metrics_cameras_ut.cpp
+++ vms/server/nx_vms_server/unit_tests/src/api/metrics_cameras_ut.cpp
@@ -17,6 +17,8 @@ using namespace std::chrono;
 
 static const int kMinDays = 5;
 
+namespace {
+
 class DataProviderStub : public resource::test::LiveStreamProviderMock
 {
 public:
@@ -37,6 +39,8 @@ public:
         m_stat[0].onData(data);
     }
 };
+
+} // namespace
 
 class MetricsCamerasApi: public ::testing::Test
 {
