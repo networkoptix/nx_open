6d1fe1b41bdd1c8119c7560c571dfdbbb31ac67d
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2019-10-15 16:31:26 -0700

    CLOUD-3768: traffic_relay: monitoring SSL certificate for changes enabled again
    <==== Mercurial data ====>
    Branch: cloud_backend_19.1

--- cloud/backend/libtraffic_relay/src/nx/cloud/relay/relay_service.cpp
+++ cloud/backend/libtraffic_relay/src/nx/cloud/relay/relay_service.cpp
@@ -48,7 +48,7 @@ int RelayService::serviceMain(const utils::AbstractServiceSettings& abstractSett
 {
     const conf::Settings& settings = static_cast<const conf::Settings&>(abstractSettings);
 
-	//watchSslCertificateFileIfNeeded(settings);
+	watchSslCertificateFileIfNeeded(settings);
 
     auto model = std::make_unique<Model>(settings);
     m_model = model.get();
