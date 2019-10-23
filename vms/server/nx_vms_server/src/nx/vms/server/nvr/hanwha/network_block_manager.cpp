39b37a9137ba2b79065ab6c747773ee84632cef8
Dmitry Mishin <dmishin@networkoptix.com>
2019-10-23 22:17:01 +0300

    VMS-16079: Added simple fake data generator for the NVR network block rest handler
    <==== Mercurial data ====>
    Branch: x_VMS_hanwha_nvr_integration

--- vms/server/nx_vms_server/src/nx/vms/server/nvr/hanwha/network_block_manager.cpp
+++ vms/server/nx_vms_server/src/nx/vms/server/nvr/hanwha/network_block_manager.cpp
@@ -2,9 +2,33 @@
 
 namespace nx::vms::server::nvr::hanwha {
 
+using namespace nx::vms::api;
+
+nx::vms::api::NetworkBlockData makeFakeData()
+{
+    NetworkBlockData result;
+    result.totalPowerLimitWatts = 100;
+    for (int i = 1; i < 9; ++i)
+    {
+        NetworkPortState state;
+        state.portNumber = i;
+        state.poweringMode = NetworkPortWithPoweringMode::PoweringMode::automatic;
+        state.deviceId = QnUuid::createUuid();
+        state.devicePowerConsumptionLimitWatts = i * 3;
+        state.devicePowerConsumptionLimitWatts =
+            state.devicePowerConsumptionWatts > 6 ? 30 : 7;
+
+        state.linkSpeedMbps = 100;
+        state.poweringStatus = NetworkPortState::PoweringStatus::powered;
+        result.portStates.push_back(std::move(state));
+    }
+
+    return result;
+}
+
 nx::vms::api::NetworkBlockData NetworkBlockManager::state() const
 {
-    return nx::vms::api::NetworkBlockData();
+    return makeFakeData();
 }
 
 bool NetworkBlockManager::setPortPoweringModes(const PoweringModeByPort& poweringModeByPort)
