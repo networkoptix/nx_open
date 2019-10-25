9d9f5055d271e826dc7d1edb608710d91f187e2a
Dmitry Mishin <dmishin@networkoptix.com>
2019-10-25 16:15:00 +0300

    VMS-16091: Hanwha PoE NVR searcher returns an IO module resource
    <==== Mercurial data ====>
    Branch: x_VMS_hanwha_nvr_integration

--- vms/server/nx_vms_server/src/nx/vms/server/nvr/hanwha/io_module_searcher.cpp
+++ vms/server/nx_vms_server/src/nx/vms/server/nvr/hanwha/io_module_searcher.cpp
@@ -1,10 +1,18 @@
 #include "io_module_searcher.h"
 
 #include <media_server/media_server_module.h>
+#include <core/resource/media_server_resource.h>
+#include <nx/vms/server/resource/resource_fwd.h>
+#include <nx/vms/server/nvr/hanwha/common.h>
+#include <nx/vms/server/nvr/hanwha/io_module_resource.h>
 
 namespace nx::vms::server::nvr::hanwha {
 
-static const QString kHanwhaNvrManufacturer = "Hanwha WAVE NVR";
+static const QString kDefaultNvrIoModuleName("NVR IO block");
+static const QString kDefaultNvrIoModuleModel("NVR-IO-module #dmishin take model from board id!");
+static const QString kDefaultNvrIoModuleFirmware("#dmishin take firmware from driver version!");
+static const QString kNvrIoModuleResourceTypeName("Hanwha_Wave_Nvr_Io_Module");
+
 
 IoModuleSearcher::IoModuleSearcher(QnMediaServerModule* serverModule):
     QnAbstractResourceSearcher(serverModule->commonModule()),
@@ -14,20 +22,51 @@ IoModuleSearcher::IoModuleSearcher(QnMediaServerModule* serverModule):
 
 QString IoModuleSearcher::manufacturer() const
 {
-    return kHanwhaNvrManufacturer;
+    return kHanwhaPoeNvrDriverName;
 }
 
 QnResourceList IoModuleSearcher::findResources()
 {
-    // TODO: #dmishin implement.
-    return {};
+    const QnResourceTypePtr resourceType =
+        qnResTypePool->getResourceTypeByName(kNvrIoModuleResourceTypeName);
+
+    if (resourceType.isNull())
+        return {};
+
+    IoModuleResourcePtr ioModule(new IoModuleResource(serverModule()));
+    ioModule->setTypeId(resourceType->getId());
+    ioModule->setName(kDefaultNvrIoModuleName);
+    ioModule->setModel(kDefaultNvrIoModuleModel);
+    ioModule->setFirmware(kDefaultNvrIoModuleFirmware);
+    ioModule->setPhysicalId(
+        serverModule()->commonModule()->moduleGUID().toString() + "_io_module");
+    ioModule->setVendor(kDefaultVendor);
+    ioModule->setUrl(serverModule()->commonModule()->currentServer()->getUrl());
+
+    QnResourceList result;
+    result.push_back(std::move(ioModule));
+    return result;
 }
 
 QnResourcePtr IoModuleSearcher::createResource(
     const QnUuid& resourceTypeId, const QnResourceParams& params)
 {
-    // TODO: #dmishin implement.
-    return nullptr;
+    const QnResourceTypePtr resourceType = qnResTypePool->getResourceType(resourceTypeId);
+    if (resourceType.isNull())
+    {
+        NX_DEBUG(this, "No resource type for ID %1", resourceTypeId);
+        return nullptr;
+    }
+
+    if (resourceType->getManufacturer() != manufacturer())
+        return nullptr;
+
+    QnResourcePtr result;
+    result.reset(new IoModuleResource(serverModule()));
+    result->setTypeId(resourceTypeId);
+
+    NX_DEBUG(this, "Created Hanwha WAVE NVR IO module resource");
+    return result;
 }
 
 } // namespace nx::vms::server::nvr::hanwha
