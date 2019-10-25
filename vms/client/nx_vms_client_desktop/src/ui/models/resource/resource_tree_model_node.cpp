73a3da6599bc2f8b700245184b15b9cac5489f28
Victor Breus <vbreus@networkoptix.com>
2019-10-25 18:34:06 +0300

    VMS-CR-19449: Unit tests fixed, as well as resource tree implementation
    <==== Mercurial data ====>
    Branch: vms_4.1

--- vms/client/nx_vms_client_desktop/src/ui/models/resource/resource_tree_model_node.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/models/resource/resource_tree_model_node.cpp
@@ -65,10 +65,10 @@ bool nodeRequiresChildren(
             if (node->resourceFlags().testFlag(Qn::server))
             {
                 const bool isLoggedIn = !accessController->user().isNull();
-                const bool isAdmin =
-                    accessController->hasGlobalPermission(GlobalPermission::admin);
+                const bool isCustomUser =
+                    accessController->hasGlobalPermission(GlobalPermission::customUser);
 
-                if (isLoggedIn && !isAdmin)
+                if (isLoggedIn && isCustomUser)
                 {
                     const auto accessibleVia =
                         accessProvider->accessibleVia(accessController->user(), node->resource());
