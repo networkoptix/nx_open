cb9bd1691efab409670f5bc8ed31b5da8a0734fc
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-10-28 13:32:41 +0300

    Fix some blinking UT
    <==== Mercurial data ====>
    Branch: vms_4.1_server

--- vms/server/nx_vms_server/unit_tests/src/nx/mediaserver/live_stream_provider_mock.cpp
+++ vms/server/nx_vms_server/unit_tests/src/nx/mediaserver/live_stream_provider_mock.cpp
@@ -6,6 +6,12 @@ namespace resource {
 namespace test {
 
 
+void LiveStreamProviderMock::stop()
+{
+    exit(); //< Leave QT event loop (it is QThread default behaviour is there is no custom run()).
+    QnLiveStreamProvider::stop();
+}
+
 } // namespace test
 } // namespace resource
 } // namespace vms::server
