eb8b677b5992d25c3cf5da3f76b12ce443a45709
Dmitry Mishin <dmishin@networkoptix.com>
2019-11-05 16:21:43 +0300

    VMS-16187: Move TimeGuard comment to the header
    <==== Mercurial data ====>
    Branch: vms_4.0

--- vms/server/nx_vms_server/src/nx/vms/server/sdk_support/timed_guard.h
+++ vms/server/nx_vms_server/src/nx/vms/server/sdk_support/timed_guard.h
@@ -4,6 +4,13 @@
 
 namespace nx::vms::server::sdk_support {
 
+/**
+ * This guard is supposed to be used in purposes of reacting to unusually long execution time of
+ *     third-party code that we're unable to control. If timeout exceeds before the destructor is
+ *     executed then `onExpired` callback is called. This is a pretty rough tool and thus should
+ *     be used carefully since many factors (such as multithreading environment, CPU load, etc.)
+ *     can affect execution time of a code block with such a guard.
+ */
 class TimedGuard
 {
 public:
