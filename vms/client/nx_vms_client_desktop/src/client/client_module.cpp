7337dd6b99874f06cea87c543c083d3230771097
Sergey Ivanov <sivanov@networkoptix.com>
2019-11-12 14:13:19 +0300

    Disable webkit NPAPI plugins in the webengine browser due to main thread corruption on windows
    <==== Mercurial data ====>
    Branch: vms_4.1

--- vms/client/nx_vms_client_desktop/src/client/client_module.cpp
+++ vms/client/nx_vms_client_desktop/src/client/client_module.cpp
@@ -712,8 +712,9 @@ void QnClientModule::initWebEngine()
     qputenv("QTWEBENGINE_DIALOG_SET", "QtQuickControls2");
     QtWebEngine::initialize();
 
-    const auto settings = QWebEngineSettings::defaultSettings();
-    settings->setAttribute(QWebEngineSettings::PluginsEnabled, ini().enableWebKitPlugins);
+    //const auto settings = QWebEngineSettings::defaultSettings();
+    //settings->setAttribute(QWebEngineSettings::PluginsEnabled, ini().enableWebKitPlugins);
+
     // TODO: Add ini parameters for WebEngine attributes
     //settings->setAttribute(QWebEngineSettings::AllowRunningInsecureContent, true);
     //settings->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, true);
