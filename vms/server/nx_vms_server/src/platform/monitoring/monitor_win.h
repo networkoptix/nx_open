382c387c2773b642f49c1dbf9fda025f2e6be254
Roman Vasilenko <rvasilenko@networkoptix.com>
2019-11-12 13:29:34 +0300

    VMS-16227: add comments to code
    <==== Mercurial data ====>
    Branch: vms_4.1_server

--- vms/server/nx_vms_server/src/platform/monitoring/monitor_win.h
+++ vms/server/nx_vms_server/src/platform/monitoring/monitor_win.h
@@ -15,6 +15,10 @@ public:
     virtual QList<HddLoad> totalHddLoad() override;
     virtual QList<NetworkLoad> totalNetworkLoad() override;
     virtual int thisProcessThreads() override;
+
+     /**
+      * Sigar implementation has a bug: it never updates first result for Windows.
+      */
     virtual quint64 thisProcessRamUsageBytes() override;
 
 private:
