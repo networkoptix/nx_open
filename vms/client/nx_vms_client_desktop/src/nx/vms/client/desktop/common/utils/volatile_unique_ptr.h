685172625dfdaee9ebc250869d1669abb50d4efa
Vitaly Kutin <vkutin@networkoptix.com>
2019-11-13 16:33:59 +0300

    Safer animations in Right Panel
    <==== Mercurial data ====>
    Branch: vms_4.0

---
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/common/utils/volatile_unique_ptr.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <QtCore/QAbstractAnimation>
+#include <QtCore/QPointer>
+
+namespace nx::vms::client::desktop {
+
+/**
+ * This is an unique pointer to a QObject that can be destroyed elsewhere.
+ * While this semantic is not the best in the sense of clean memory management,
+ * it's quite convenient for transient objects like animations or single-shot timers
+ * if we need to track them and probably delete early.
+ */
+template<typename T>
+class VolatileUniquePtr
+{
+public:
+    VolatileUniquePtr() = default;
+    VolatileUniquePtr(VolatileUniquePtr&) = delete;
+    VolatileUniquePtr(VolatileUniquePtr&&) = default;
+    VolatileUniquePtr& operator=(VolatileUniquePtr&) = delete;
+    VolatileUniquePtr& operator=(VolatileUniquePtr&&) = default;
+
+    VolatileUniquePtr(T* raw): m_ptr(raw) {}
+    ~VolatileUniquePtr() { reset(); }
+
+    T* get() const { return m_ptr; }
+    T* data() const { return get(); }
+    T* operator->() const { return get(); }
+
+    explicit operator bool() const { return !m_ptr.isNull(); }
+
+    T* release()
+    {
+        QPointer<T> result;
+        result.swap(m_ptr);
+        return result;
+    }
+
+    void reset(T* raw = nullptr)
+    {
+        auto old = m_ptr;
+        m_ptr = raw;
+        delete old;
+    }
+
+    void swap(VolatileUniquePtr& other)
+    {
+        m_ptr.swap(other.m_ptr);
+    }
+
+private:
+    QPointer<T> m_ptr;
+};
+
+} // namespace nx::vms::client::desktop
