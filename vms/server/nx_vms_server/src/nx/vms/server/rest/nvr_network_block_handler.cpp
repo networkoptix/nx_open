1d37048b9f39174168d16e854c379a1f44fbf859
Dmitry Mishin <dmishin@networkoptix.com>
2019-11-17 21:30:37 +0300

    VMS-16079: Minor changes in the NVR network block rest handler
    <==== Mercurial data ====>
    Branch: x_VMS_hanwha_nvr_integration

--- vms/server/nx_vms_server/src/nx/vms/server/rest/nvr_network_block_handler.cpp
+++ vms/server/nx_vms_server/src/nx/vms/server/rest/nvr_network_block_handler.cpp
@@ -2,18 +2,21 @@
 
 #include <media_server/media_server_module.h>
 #include <nx/vms/server/nvr/i_service.h>
+#include <nx/vms/server/nvr/types.h>
+#include <nx/fusion/model_functions.h>
+#include <nx/vms/api/data/network_block_data.h>
 
 namespace nx::vms::server::rest {
 
 using namespace nx::vms::server::nvr;
 
-INetworkBlockController* networkBlockController(QnMediaServerModule* serverModule)
+INetworkBlockManager* networkBlockManager(QnMediaServerModule* serverModule)
 {
     auto nvrService = serverModule->nvrService();
-    if (!nvrService)
+    if (!NX_ASSERT(nvrService))
         return nullptr;
 
-    return nvrService->networkBlockController();
+    return nvrService->networkBlockManager();
 }
 
 NvrNetworkBlockHandler::NvrNetworkBlockHandler(QnMediaServerModule* serverModule):
@@ -23,22 +26,24 @@ NvrNetworkBlockHandler::NvrNetworkBlockHandler(QnMediaServerModule* serverModule
 
 JsonRestResponse NvrNetworkBlockHandler::executeGet(const JsonRestRequest& request)
 {
-    if (const INetworkBlockController* const manager = networkBlockController(serverModule()))
+    if (const INetworkBlockManager* const manager = networkBlockManager(serverModule()))
         return manager->state();
 
+    NX_DEBUG(this,
+        "Unable to access network block manager, current server doesn't support PoE management");
     return nx::network::http::StatusCode::notImplemented;
 }
 
 JsonRestResponse NvrNetworkBlockHandler::executePost(
     const JsonRestRequest& request, const QByteArray& body)
 {
-    INetworkBlockController::PoweringModeByPort poweringModeByPort;
-    if (!QJson::deserialize(body, &poweringModeByPort))
+    std::vector<nx::vms::api::NetworkPortWithPoweringMode> portPoweringModes;
+    if (!QJson::deserialize(body, &portPoweringModes))
         return nx::network::http::StatusCode::badRequest;
 
-    if (INetworkBlockController* const manager = networkBlockController(serverModule()))
+    if (INetworkBlockManager* const manager = networkBlockManager(serverModule()))
     {
-        if (manager->setPortPoweringModes(poweringModeByPort))
+        if (manager->setPoeModes(portPoweringModes))
             return manager->state();
 
         return nx::network::http::StatusCode::internalServerError;
