4ee0606279081437308874c640aad92f5414c323
Denis Klychkov <dklychkov@networkoptix.com>
2019-12-03 12:09:07 +0300

    CMake: Put compiler path to cache to make some IDE CMake integrations happy
    <==== Mercurial data ====>
    Branch: vms_4.1

--- cmake/toolchain/bpi.cmake
+++ cmake/toolchain/bpi.cmake
@@ -3,8 +3,8 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(box "bpi")
 
 set(cross_prefix ${PACKAGES_DIR}/bpi/gcc-8.1/bin/arm-linux-gnueabihf)
-set(CMAKE_C_COMPILER "${cross_prefix}-gcc")
-set(CMAKE_CXX_COMPILER "${cross_prefix}-g++")
+set(CMAKE_C_COMPILER "${cross_prefix}-gcc" CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER "${cross_prefix}-g++" CACHE INTERNAL "")
 
 set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-a15 -mfpu=neon-vfpv4")
 set(CMAKE_CXX_FLAGS_INIT ${CMAKE_C_FLAGS_INIT})
--- cmake/toolchain/edge1.cmake
+++ cmake/toolchain/edge1.cmake
@@ -3,8 +3,8 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(box "edge1")
 
 set(cross_prefix ${PACKAGES_DIR}/linux-arm/gcc-8.1/bin/arm-linux-gnueabihf)
-set(CMAKE_C_COMPILER "${cross_prefix}-gcc")
-set(CMAKE_CXX_COMPILER "${cross_prefix}-g++")
+set(CMAKE_C_COMPILER "${cross_prefix}-gcc" CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER "${cross_prefix}-g++" CACHE INTERNAL "")
 
 set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-a9")
 set(CMAKE_CXX_FLAGS_INIT ${CMAKE_C_FLAGS_INIT})
