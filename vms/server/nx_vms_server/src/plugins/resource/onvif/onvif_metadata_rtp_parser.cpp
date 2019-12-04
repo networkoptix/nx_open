38b927b7035cf75d944386f6bcee3cabb2a6b39c
Dmitry Mishin <dmishin@networkoptix.com>
2019-12-04 16:10:25 +0300

    VMS-16528: Onvif metadata RTP parser fixes
    <==== Mercurial data ====>
    Branch: vms_4.1_server

--- vms/server/nx_vms_server/src/plugins/resource/onvif/onvif_metadata_rtp_parser.cpp
+++ vms/server/nx_vms_server/src/plugins/resource/onvif/onvif_metadata_rtp_parser.cpp
@@ -2,6 +2,7 @@
 
 #include <nx/utils/log/log.h>
 
+#include <nx/streaming/in_stream_compressed_metadata.h>
 #include <nx/streaming/rtp/rtp.h>
 
 using namespace nx::streaming::rtp;
@@ -16,13 +17,24 @@ const std::set<QString> OnvifMetadataRtpParser::kSupportedCodecs = {
 void OnvifMetadataRtpParser::setSdpInfo(const nx::streaming::Sdp::Media& sdp)
 {
     NX_DEBUG(this, "Got SDP information about the media track: %1", sdp);
+    if (sdp.rtpmap.clockRate > 0)
+        StreamParser::setFrequency(sdp.rtpmap.clockRate);
+
     m_sdp = sdp;
 }
 
 QnAbstractMediaDataPtr OnvifMetadataRtpParser::nextData()
 {
     NX_VERBOSE(this, "Next data has been requested");
-    return m_metadata;
+    QnAbstractMediaDataPtr result;
+    if (m_metadata)
+    {
+        NX_VERBOSE(this, "Got metadata");
+        result = m_metadata;
+        m_metadata.reset();
+    }
+
+    return result;
 }
 
 bool OnvifMetadataRtpParser::processData(
@@ -35,29 +47,6 @@ bool OnvifMetadataRtpParser::processData(
     NX_VERBOSE(this, "Processing next RTP packet");
 
     const quint8* const currentRtpPacketBuffer = rtpBufferBase + bufferOffset;
-    if (bytesRead < RtpHeader::kSize)
-    {
-        NX_VERBOSE(this,
-            "Provided buffer size (%1 bytes) is less than the mimimal RTP header size (%2 bytes)",
-            bytesRead, RtpHeader::kSize);
-
-        return cleanUpOnError();
-    }
-
-    const auto* const rtpHeader = (RtpHeader*)(currentRtpPacketBuffer);
-    const bool receivingNewDataChunk = m_currentDataChunkTimestamp != kInvalidTimestamp
-        && rtpHeader->timestamp != m_currentDataChunkTimestamp;
-
-    if (receivingNewDataChunk)
-    {
-        m_metadata = makeCompressedMetadata();
-        if (m_metadata)
-            gotData = true;
-
-        m_buffer.clear();
-        m_currentDataChunkTimestamp = rtpHeader->timestamp;
-    }
-
     const std::optional<int> fullRtpHeaderSize =
         calculateFullRtpHeaderSize(currentRtpPacketBuffer, bytesRead);
 
@@ -67,9 +56,29 @@ bool OnvifMetadataRtpParser::processData(
         return cleanUpOnError();
     }
 
+    if (bytesRead < fullRtpHeaderSize)
+    {
+        NX_VERBOSE(this,
+            "Provided buffer size (%1 bytes) is less than calculated RTP header size (%2 bytes)",
+            bytesRead, fullRtpHeaderSize);
+
+        return cleanUpOnError();
+    }
+
+    const auto* const rtpHeader = (RtpHeader*)(currentRtpPacketBuffer);
+    m_currentDataChunkTimestamp = rtpHeader->timestamp;
+
     m_buffer.append(
         (const char*) (currentRtpPacketBuffer + *fullRtpHeaderSize),
         bytesRead - *fullRtpHeaderSize);
+
+    if (rtpHeader->marker)
+    {
+        NX_VERBOSE(this, "Got an RTP packet with the marker bit set, creating metadata");
+        m_metadata = makeCompressedMetadata();
+        if (m_metadata)
+            gotData = true;
+    }
 
     return true;
 }
@@ -82,11 +91,13 @@ bool OnvifMetadataRtpParser::cleanUpOnError()
 
 QnCompressedMetadataPtr OnvifMetadataRtpParser::makeCompressedMetadata()
 {
-    const auto metadataPacket = std::make_shared<QnCompressedMetadata>(
-        MetadataType::Onvif,
-        m_buffer.size());
+    const auto metadataPacket = std::make_shared<nx::streaming::InStreamCompressedMetadata>(
+        m_sdp.rtpmap.codecName,
+        std::move(m_buffer));
 
-    metadataPacket->setData(m_buffer);
+    metadataPacket->timestamp = m_currentDataChunkTimestamp;
+
+    m_buffer.clear();
 
     return metadataPacket;
 }
