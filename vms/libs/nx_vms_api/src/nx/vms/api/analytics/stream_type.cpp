b76bfd5f0c08b47df6c77734237a19d58b3bda8c
Dmitry Mishin <dmishin@networkoptix.com>
2019-12-13 17:59:18 +0300

    VMS-15420: Fixed StreamType enum deserialization
    Branch: x_VMS_sdk_motion_and_stream_control
    Change-Id: If24d69e4c7bf0cd4bafa9c384a0b833b1f124dad

--- vms/libs/nx_vms_api/src/nx/vms/api/analytics/stream_type.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/stream_type.cpp
@@ -7,7 +7,7 @@ QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(nx::vms::api::analytics, StreamType,
     (nx::vms::api::analytics::StreamType::compressedVideo, "compressedVideo")
     (nx::vms::api::analytics::StreamType::uncompressedVideo, "uncompressedVideo")
     (nx::vms::api::analytics::StreamType::metadata, "metadata")
-    (nx::vms::api::analytics::StreamType::metadata, "motion")
+    (nx::vms::api::analytics::StreamType::motion, "motion")
 )
 QN_FUSION_DEFINE_FUNCTIONS(nx::vms::api::analytics::StreamType, (numeric)(debug))
 
@@ -16,6 +16,6 @@ QN_DEFINE_EXPLICIT_ENUM_LEXICAL_FUNCTIONS(nx::vms::api::analytics, StreamTypes,
     (nx::vms::api::analytics::StreamType::compressedVideo, "compressedVideo")
     (nx::vms::api::analytics::StreamType::uncompressedVideo, "uncompressedVideo")
     (nx::vms::api::analytics::StreamType::metadata, "metadata")
-    (nx::vms::api::analytics::StreamType::metadata, "motion")
+    (nx::vms::api::analytics::StreamType::motion, "motion")
 )
 QN_FUSION_DEFINE_FUNCTIONS(nx::vms::api::analytics::StreamTypes, (numeric)(debug))
