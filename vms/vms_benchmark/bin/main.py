7b0df10fd85ab9f48b72fcda0b7a4a4ef905bb28
Artem Levenkov <alevenkov@networkoptix.com>
2019-12-13 21:50:11 +0300

    VMS-16252: Fix error constraint condition
    Branch: vms_4.0

--- vms/vms_benchmark/bin/main.py
+++ vms/vms_benchmark/bin/main.py
@@ -1048,7 +1048,7 @@ def main(conf_file, ini_file, log_file):
 
     conf, ini = load_configs(conf_file, ini_file)
 
-    if ini['liveStreamsPerCameraRatio'] + ini['archiveStreamsPerCameraRatio'] == 0:
+    if ini['liveStreamsPerCameraRatio'] == 0 and ini['archiveStreamsPerCameraRatio'] == 0:
         raise exceptions.ConfigOptionsRestrictionError(
             'Config settings liveStreamsPerCameraRatio and archiveStreamsPerCameraRatio should ' +
             'not be zero simultaneously.'
