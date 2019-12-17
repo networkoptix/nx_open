a5f0ce8edbd4a8750b1d9e21857bbad05f12360d
Dmitry Mishin <dmishin@networkoptix.com>
2019-12-17 13:32:54 +0300

    VMS-15292: Renamed IStreamDataProvider -> StreamDataProvider, extended its interface
    Branch: x_VMS_sdk_motion_and_stream_control
    Change-Id: If8817c435868017a99d01bd97e84ce3a1db66aa5

--- vms/server/nx_vms_server/src/nx/vms/server/analytics/i_stream_data_receptor.h
+++ vms/server/nx_vms_server/src/nx/vms/server/analytics/i_stream_data_receptor.h
@@ -3,18 +3,21 @@
 #include <set>
 
 #include <nx/streaming/abstract_data_packet.h>
-#include <nx/vms/api/analytics/stream_type.h>
+#include <nx/vms/server/analytics/stream_requirements.h>
 
 namespace nx::vms::server::analytics {
 
-class IStreamDataReceptor
+class StreamDataReceptor
 {
 public:
-    virtual ~IStreamDataReceptor() = default;
+    virtual ~StreamDataReceptor() = default;
 
     virtual void putData(const QnAbstractDataPacketPtr& streamData) = 0;
 
-    virtual nx::vms::api::analytics::StreamTypes requiredStreamTypes() const = 0;
+    virtual StreamProviderRequirements providerRequirements(
+        nx::vms::api::StreamIndex streamIndex) const = 0;
+
+    virtual void registerStream(nx::vms::api::StreamIndex streamIndex) = 0;
 };
 
 } // namespace nx::vms::server::analytics
