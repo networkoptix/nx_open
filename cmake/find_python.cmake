afdfd7d41042c52921c0114bf0c1fcd09fe01ac4
Artem Levenkov <alevenkov@networkoptix.com>
2019-12-26 22:08:47 +0300

    Revert "VMS-16843: Enchance Python3 finding logic"
    This reverts commit e348fffde40ebf2f99815e9b7352af56898bb05f.
    
    Branch: vms_4.1_server

--- cmake/find_python.cmake
+++ cmake/find_python.cmake
@@ -1,13 +1,13 @@
-set(Python3_FIND_STRATEGY LOCATION)
-set(Python3_FIND_REGISTRY NEVER)
-set(Python3_FIND_FRAMEWORK NEVER)
-find_package(Python3 COMPONENTS Interpreter)
-
-if(Python3_FOUND)
-    set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
-    message(STATUS "Found Python 3 executable: ${PYTHON_EXECUTABLE}")
+if(WIN32)
+    find_program(PYTHON_EXECUTABLE python.exe)
 else()
-    message(FATAL_ERROR "Python 3 executable not found.")
+    find_program(PYTHON_EXECUTABLE python3)
+endif()
+
+if(PYTHON_EXECUTABLE)
+    message(STATUS "Found python executable: ${PYTHON_EXECUTABLE}")
+else()
+    message(FATAL_ERROR "Python executable not found.")
 endif()
 
 set(ENV{PYTHONDONTWRITEBYTECODE} True)
