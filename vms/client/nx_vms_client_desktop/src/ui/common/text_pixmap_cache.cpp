e5a0cd3c51bb433df7085ea25ff38ee6760de964
Victor Breus <vbreus@networkoptix.com>
2019-12-27 18:43:18 +0300

    Fixed macOS build
    Branch: vms_4.1

--- vms/client/nx_vms_client_desktop/src/ui/common/text_pixmap_cache.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/common/text_pixmap_cache.cpp
@@ -62,7 +62,7 @@ int calculateTextPixmapCost(const QnTextPixmap& textPixmap)
     static constexpr int kBitsPerByte = 8;
     const QPixmap& pixmap = textPixmap.pixmap;
 
-    int result = std::pow(pixmap.devicePixelRatio(), 2)
+    int result = pixmap.devicePixelRatio() * pixmap.devicePixelRatio()
         * pixmap.height() * pixmap.width()
         * (pixmap.depth() / kBitsPerByte);
 
