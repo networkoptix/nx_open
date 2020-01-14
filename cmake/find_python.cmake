71fb9b3352db076de9c88af7a83e1c3d0c608615
Artem Levenkov <alevenkov@networkoptix.com>
2020-01-14 15:53:55 +0300

    VMS-16843: Rewrite python detection
    Branch: vms_4.0

--- cmake/find_python.cmake
+++ cmake/find_python.cmake
@@ -1,14 +1,56 @@
-if(WIN32)
-    find_program(PYTHON_EXECUTABLE python.exe)
-    if(PYTHON_EXECUTABLE)
-        message(STATUS "Found python executable: ${PYTHON_EXECUTABLE}")
+function(check_python_at_least_37)
+    if (NOT "${PYTHON_VERSION}" MATCHES "Python 3\.(7|8)(\.*)?")
+        if ("${PYTHON_VERSION}" MATCHES "Python (.*)")
+              message(
+                  FATAL_ERROR
+                  "Executable ${PYTHON_EXECUTABLE} has invalid version: "
+                  "${CMAKE_MATCH_1} instead of 3.(7|8)*."
+              )
+         else()
+              message(FATAL_ERROR "Unexpected Python version: '${PYTHON_VERSION}'.")
+         endif()
     endif()
-else()
-    find_package(PythonInterp 2 REQUIRED)
-endif()
+endfunction()
 
-if(NOT PYTHON_EXECUTABLE)
-    message(FATAL_ERROR "Python executable not found.")
-endif()
+function(find_python_at_least_37)
+    if(${ARGC} GREATER 1)
+        list(SUBLIST ARGV 1 ${ARGC} ARGV_EXCEPT_FIRST)
+    endif()
+
+    set(PYTHON_EXECUTABLE_PATTERNS ${ARGV0})
+
+    if (CYGWIN)
+        set(PYTHON_EXECUTABLE_PATTERNS ${PYTHON_EXECUTABLE_PATTERNS} ${PYTHON_EXECUTABLE_PATTERNS}.exe)
+    elseif(WIN32)
+        set(PYTHON_EXECUTABLE_PATTERNS ${PYTHON_EXECUTABLE_PATTERNS}.exe)
+    endif()
+
+    find_program(
+        PYTHON_EXECUTABLE NAMES ${PYTHON_EXECUTABLE_PATTERNS} NO_CMAKE_SYSTEM_PATH NO_CMAKE_PATH
+    )
+
+    if (${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
+        if(${ARGC} EQUAL 1)
+            message(FATAL_ERROR "Python >=3.7 executable not found.")
+        endif()
+
+        find_python_at_least_37(${ARGV_EXCEPT_FIRST})
+    else()
+        message(STATUS "Found python executable: '${PYTHON_EXECUTABLE}'.")
+
+        execute_process(
+            COMMAND "${PYTHON_EXECUTABLE}" --version
+            OUTPUT_VARIABLE PYTHON_VERSION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        check_python_at_least_37()
+
+        return()
+    endif()
+endfunction()
+
+find_python_at_least_37(python3.8 python3.7 python3 python)
 
 set(ENV{PYTHONDONTWRITEBYTECODE} True)
+
