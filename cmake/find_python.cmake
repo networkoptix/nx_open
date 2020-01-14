9a0459f9f2ddf224bf355286c67ef8af58880dc7
Artem Levenkov <alevenkov@networkoptix.com>
2020-01-14 14:35:25 +0300

    VMS-16843: Rewrite python detection (2nd attempt)
    Branch: vms_4.0

--- cmake/find_python.cmake
+++ cmake/find_python.cmake
@@ -1,10 +1,18 @@
-function(check_python_at_least_37)
-    if (NOT "${PYTHON_VERSION}" MATCHES "Python 3\.(7|8)(\.*)?")
+function(check_python_version)
+    execute_process(
+        COMMAND "${PYTHON_EXECUTABLE}" --version
+        OUTPUT_VARIABLE PYTHON_VERSION
+        ERROR_VARIABLE PYTHON_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_STRIP_TRAILING_WHITESPACE
+    )
+
+    if (NOT "${PYTHON_VERSION}" MATCHES "Python 2\.7(\..*)?")
         if ("${PYTHON_VERSION}" MATCHES "Python (.*)")
               message(
                   FATAL_ERROR
                   "Executable ${PYTHON_EXECUTABLE} has invalid version: "
-                  "${CMAKE_MATCH_1} instead of 3.(7|8)*."
+                  "${CMAKE_MATCH_1} instead of 2.7*."
               )
          else()
               message(FATAL_ERROR "Unexpected Python version: '${PYTHON_VERSION}'.")
@@ -12,7 +20,7 @@ function(check_python_at_least_37)
     endif()
 endfunction()
 
-function(find_python_at_least_37)
+function(find_python_2)
     if(${ARGC} GREATER 1)
         list(SUBLIST ARGV 1 ${ARGC} ARGV_EXCEPT_FIRST)
     endif()
@@ -31,26 +39,20 @@ function(find_python_at_least_37)
 
     if (${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
         if(${ARGC} EQUAL 1)
-            message(FATAL_ERROR "Python >=3.7 executable not found.")
+            message(FATAL_ERROR "Python 2.7 executable not found.")
         endif()
 
-        find_python_at_least_37(${ARGV_EXCEPT_FIRST})
+        find_python_2(${ARGV_EXCEPT_FIRST})
     else()
         message(STATUS "Found python executable: '${PYTHON_EXECUTABLE}'.")
 
-        execute_process(
-            COMMAND "${PYTHON_EXECUTABLE}" --version
-            OUTPUT_VARIABLE PYTHON_VERSION
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-        check_python_at_least_37()
+        check_python_version()
 
         return()
     endif()
 endfunction()
 
-find_python_at_least_37(python3.8 python3.7 python3 python)
+find_python_2(python2 python)
 
 set(ENV{PYTHONDONTWRITEBYTECODE} True)
 
