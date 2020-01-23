b3507c5b3671b45f92467347f65dc06757b9c9d7
Sergei Ivanov <sivanov@networkoptix.com>
2020-01-23 11:32:39 +0300

    NX-CR-349: Added tests on license validation, tests reworded
    Branch: x/VMS-16365_videowall_control_licenses

--- vms/libs/common/unit_tests/src/utils/videowall_license_usage_helper_ut.cpp
+++ vms/libs/common/unit_tests/src/utils/videowall_license_usage_helper_ut.cpp
@@ -49,6 +49,43 @@ protected:
         m_licenses->addLicenses(Qn::LC_VideoWall, count);
     }
 
+    QnVideoWallResourcePtr addVideoWallWithItems(int itemCount)
+    {
+        auto videowall = createVideoWall();
+        for (int i = 0; i < itemCount; ++i)
+            addItem(videowall);
+        resourcePool()->addResource(videowall);
+        return videowall;
+    }
+
+    QnVideoWallItem addItem(const QnVideoWallResourcePtr& videowall)
+    {
+        QnVideoWallItem item;
+        item.uuid = QnUuid::createUuid();
+        item.runtimeStatus.online = true;
+        videowall->items()->addItem(item);
+        return item;
+    }
+
+    bool canStartControlSession(const QnUuid& instanceId) const
+    {
+        return m_helper->canStartControlSession(instanceId);
+    }
+
+    void startControlSession(
+        const QnVideoWallResourcePtr& videowall,
+        QnVideoWallItem item,
+        QnUuid instanceId)
+    {
+        item.runtimeStatus.controlledBy = instanceId;
+        videowall->items()->updateItem(item);
+    }
+
+    bool enoughLicenses() const
+    {
+        return m_helper->isValid(Qn::LC_VideoWall);
+    }
+
     // Declares the variables your tests want to use.
     QSharedPointer<QnCommonModule> m_module;
     QScopedPointer<QnLicensePoolScaffold> m_licenses;
@@ -56,36 +93,56 @@ protected:
     QScopedPointer<QLicenseStubValidator> m_validator;
 };
 
+/**
+ * Validate scenarios when user can and cannot start a control session.
+ */
 TEST_F(QnVideowallLicenseUsageHelperTest, proposeToStartVideowallControl)
 {
-    QnUuid localInstanceId = QnUuid::createUuid();
-
-    QnVideoWallItem item1;
-    item1.uuid = QnUuid::createUuid();
-
-    QnVideoWallItem item2;
-    item2.uuid = QnUuid::createUuid();
-
     auto videowall = addVideoWall();
-    videowall->items()->addItem(item1);
-    videowall->items()->addItem(item2);
+    auto item = addItem(videowall);
 
-    ASSERT_FALSE(m_helper->isValid(Qn::LC_VideoWall));
     addLicenses(1);
 
-    ASSERT_TRUE(m_helper->isValid(Qn::LC_VideoWall));
-
     // In the initial state we can start controlling.
-    ASSERT_TRUE(m_helper->canStartControlSession(localInstanceId));
+    const auto localInstanceId = QnUuid::createUuid();
+    ASSERT_TRUE(canStartControlSession(localInstanceId));
 
-    item1.runtimeStatus.online = true;
-    item1.runtimeStatus.controlledBy = localInstanceId;
-    videowall->items()->updateItem(item1);
+    startControlSession(videowall, item, localInstanceId);
 
     // Another client cannot start control.
     const auto anotherInstanceId = QnUuid::createUuid();
-    ASSERT_FALSE(m_helper->canStartControlSession(anotherInstanceId));
+    ASSERT_FALSE(canStartControlSession(anotherInstanceId));
 
     // We can switch our control to another item.
-    ASSERT_TRUE(m_helper->canStartControlSession(localInstanceId));
+    ASSERT_TRUE(canStartControlSession(localInstanceId));
+}
+
+/**
+ * License text should be based on the screen count only. All changes in the screens count must be
+ * correctly handled.
+ */
+TEST_F(QnVideowallLicenseUsageHelperTest, validityDependsOnScreensCount)
+{
+    // Licenses are not required initially.
+    ASSERT_TRUE(enoughLicenses());
+
+    // Empty videowall does not require licenses.
+    auto videowall = addVideoWall();
+    ASSERT_TRUE(enoughLicenses());
+
+    // Adding an item must lead to license recalculation.
+    auto item = addItem(videowall);
+    ASSERT_FALSE(enoughLicenses());
+
+    // Removing an item must lead to license recalculation.
+    videowall->items()->removeItem(item);
+    ASSERT_TRUE(enoughLicenses());
+
+    // Adding a videowall must lead to license recalculation.
+    auto videowall2 = addVideoWallWithItems(1);
+    ASSERT_FALSE(enoughLicenses());
+
+    // Removing a videowall must lead to license recalculation.
+    resourcePool()->removeResource(videowall2);
+    ASSERT_TRUE(enoughLicenses());
 }
