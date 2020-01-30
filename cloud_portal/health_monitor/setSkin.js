aace2d41a262c15aebe1c90079e2b5704e1ac424
Nick Hartleb <nhartleb@networkoptix.com>
2020-01-30 14:27:08 -0800

    Using set skin just in case styles/custom is missing.
    Branch: vms_4.1

--- cloud_portal/health_monitor/setSkin.js
+++ cloud_portal/health_monitor/setSkin.js
@@ -1,11 +1,11 @@
 const fsmv = require('fs-extra');
 const path = require('path');
 const copySkin = (color) => {
-	var source = path.resolve(__dirname, '../skins', color, 'health_monitor/styles');
+	var source = path.resolve(__dirname, '../skins', color, 'front_end/styles');
 	var dest = path.resolve(__dirname, 'app/styles/custom');
 	fsmv.copy(source, dest, {mkdirp:true}, error => error ? console.log(error) : null);
 
-	source = path.resolve(__dirname, '../skins', color, 'health_monitor/images');
+	source = path.resolve(__dirname, '../skins', color, 'front_end/images');
 	dest = path.resolve(__dirname, 'app/images');
 	fsmv.copy(source, dest, {mkdirp:true}, error => error ? console.log(error) : null);
 };
