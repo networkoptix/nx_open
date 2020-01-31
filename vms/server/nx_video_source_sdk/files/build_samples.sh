5d0fa3f120b2171adf0fe4dc0f8abf1abc47c1ec
Mike Shevchenko <mshevchenko@networkoptix.com>
2020-01-31 23:04:34 +0300

    SDKs: Fix and improve SDK building scripts, use all CPU cores on MSVC
    Windows .bat:
    - Removed passing script args to cmake generation command, because this is difficult to implement
        (cmd's `shift` does not influence `%*`, thus, the previous implementation was buggy).
    - Simplified error-handling code.
    
    Cygwin .sh: Removed warning about using Cygwin's GCC in order to simplify the script.
    
    Branch: vms_4.0

--- vms/server/nx_video_source_sdk/files/build_samples.sh
+++ vms/server/nx_video_source_sdk/files/build_samples.sh
@@ -19,26 +19,20 @@ else
     WITH_RPI_SAMPLES=0
 fi
 
-# Use Ninja for Linux and Cygwin, if it is available on PATH.
-if which ninja >/dev/null
-then
-    GEN_OPTIONS=( -GNinja ) #< Generate for Ninja and gcc; Ninja uses all available CPU cores.
-    BUILD_OPTIONS=()
-else
-    GEN_OPTIONS=() #< Generate for GNU make and gcc.
-    BUILD_OPTIONS=( -- -j ) #< Use all available CPU cores.
-fi
-
 case "$(uname -s)" in #< Check if running in Windows from Cygwin/MinGW.
     CYGWIN*|MINGW*)
-        if [[ $(which cmake) == /usr/bin/* ]] # Cygwin's cmake is on PATH.
+        GEN_OPTIONS=( -Ax64 -Tv140,host=x64 ) #< Generate for Visual Studio 2015 compiler.
+        BASE_DIR=$(cygpath -w "$BASE_DIR") #< Windows-native cmake requires Windows path.
+        BUILD_OPTIONS=()
+        ;;
+    *) # Assume Linux; use Ninja if it is available on PATH.
+        if which ninja >/dev/null
         then
-            echo "WARNING: In Cygwin/MinGW, gcc instead of MSVC may work, but is not supported."
-            echo ""
-        else # Assuming Windows-native cmake is on PATH.
-            GEN_OPTIONS=( -Ax64 -Tv140,host=x64 ) #< Generate for Visual Studio 2015 compiler.
-            BASE_DIR=$(cygpath -w "$BASE_DIR") #< Windows-native cmake requires Windows path.
+            GEN_OPTIONS=( -GNinja ) #< Generate for Ninja and gcc; Ninja uses all CPU cores.
             BUILD_OPTIONS=()
+        else
+            GEN_OPTIONS=() #< Generate for GNU make and gcc.
+            BUILD_OPTIONS=( -- -j ) #< Use all available CPU cores.
         fi
         ;;
 esac
@@ -47,10 +41,9 @@ esac
     rm -rf "$BUILD_DIR/"
 )
 
-for SAMPLE_DIR in "$BASE_DIR/samples"/*
+for SOURCE_DIR in "$BASE_DIR/samples"/*
 do
-    SOURCE_DIR="$SAMPLE_DIR/src"
-    SAMPLE=$(basename "$SAMPLE_DIR")
+    SAMPLE=$(basename "$SOURCE_DIR")
     if [[ $WITH_RPI_SAMPLES == 0 && $SAMPLE == rpi_* ]]
     then
         echo "ATTENTION: Skipping $SAMPLE because --with-rpi-samples is not specified."
@@ -61,7 +54,7 @@ do
         mkdir -p "$BUILD_DIR/$SAMPLE"
         cd "$BUILD_DIR/$SAMPLE"
 
-        cmake "$SOURCE_DIR" `# allow empty array #` ${GEN_OPTIONS[@]+"${GEN_OPTIONS[@]}"} "$@"
+        cmake "$SOURCE_DIR/src" `# allow empty array #` ${GEN_OPTIONS[@]+"${GEN_OPTIONS[@]}"} "$@"
         cmake --build . `# allow empty array #` ${BUILD_OPTIONS[@]+"${BUILD_OPTIONS[@]}"}
     )
     ARTIFACT=$(find "$BUILD_DIR" -name "$SAMPLE.dll" -o -name "lib$SAMPLE.so")
