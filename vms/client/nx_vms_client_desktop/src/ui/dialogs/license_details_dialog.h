30d60c9fd57e3e68c672b39564087dbae720d89c
Sergei Ivanov <sivanov@networkoptix.com>
2020-02-04 12:15:03 +0300

    VMS-17254: Display number of license deactivations in the license details form
    Branch: vms_4.1

--- vms/client/nx_vms_client_desktop/src/ui/dialogs/license_details_dialog.h
+++ vms/client/nx_vms_client_desktop/src/ui/dialogs/license_details_dialog.h
@@ -1,29 +1,22 @@
-#ifndef QN_LICENSE_DETAILS_DIALOG_H
-#define QN_LICENSE_DETAILS_DIALOG_H
+#pragma once
 
-#include <QtWidgets/QDialog>
 #include <QtCore/QScopedPointer>
 
 #include <core/resource/resource_fwd.h>
 
 #include <ui/dialogs/common/session_aware_dialog.h>
 
-namespace Ui {
-    class LicenseDetailsDialog;
-}
+namespace Ui { class LicenseDetailsDialog; }
 
-class QnLicenseDetailsDialog: public QnSessionAwareButtonBoxDialog {
+class QnLicenseDetailsDialog: public QnSessionAwareButtonBoxDialog
+{
     Q_OBJECT
+    using base_type = QnSessionAwareButtonBoxDialog;
 
-    typedef QnSessionAwareButtonBoxDialog base_type;
 public:
-    QnLicenseDetailsDialog(const QnLicensePtr &license, QWidget *parent);
-    virtual ~QnLicenseDetailsDialog();
+    QnLicenseDetailsDialog(const QnLicensePtr& license, QWidget* parent);
+    virtual ~QnLicenseDetailsDialog() override;
 
-private:
-    QString licenseDescription(const QnLicensePtr &license) const;
 private:
     QScopedPointer<Ui::LicenseDetailsDialog> ui;
 };
-
-#endif // QN_LICENSE_DETAILS_DIALOG_H
