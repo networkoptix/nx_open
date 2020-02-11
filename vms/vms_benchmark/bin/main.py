63982ad489a802ee97e3704a04531d1e9138cd83
Artem Levenkov <alevenkov@networkoptix.com>
2020-02-11 16:42:49 +0300

    VMS-16997: VMS Benchmark: remove mistaken added sshpass option
    Branch: vms_4.0

--- vms/vms_benchmark/bin/main.py
+++ vms/vms_benchmark/bin/main.py
@@ -100,7 +100,6 @@ ini_definition = {
     "testcameraBin": {"type": "str", "default": "./testcamera/testcamera"},
     "rtspPerfBin": {"type": "str", "default": "./testcamera/rtsp_perf"},
     "plinkBin": {"type": "str", "default": "./plink"},
-    "sshpassBin": {"type": "str", "default": "sshpass"},
     "testFileHighResolution": {"type": "str", "default": "./high.ts"},
     "testFileHighPeriodUs": {"type": "int", "range": [1, None], "default": 10033334},
     "testFileLowResolution": {"type": "str", "default": "./low.ts"},
@@ -166,7 +165,6 @@ def load_configs(conf_file, ini_file_if_passed, ini_file_default):
     stream_reader_runner.ini_rtsp_perf_bin = abspath(ini['rtspPerfBin'])
     stream_reader_runner.ini_rtsp_perf_stderr_file = ini['rtspPerfStderrFile']
     box_connection.ini_plink_bin = abspath(ini['plinkBin'])
-    box_connection.ini_sshpass_bin = abspath(ini['sshpassBin'])
     box_connection.ini_ssh_command_timeout_s = ini['sshCommandTimeoutS']
     box_connection.ini_ssh_get_file_content_timeout_s = ini['sshGetFileContentTimeoutS']
     vms_scanner.ini_ssh_service_command_timeout_s = ini['sshServiceCommandTimeoutS']
