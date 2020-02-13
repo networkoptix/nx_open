79c60ca935ac475c14c7c50cd60f220b5e8c33e9
Artem Levenkov <alevenkov@networkoptix.com>
2020-02-13 18:20:05 +0300

    VMS Benchmark: Move mistakenly pushed to vms_4.1 changes
    Branch: vms_4.0

--- vms/vms_benchmark/bin/main.py
+++ vms/vms_benchmark/bin/main.py
@@ -215,7 +215,10 @@ def _check_storages(api, ini, camera_count):
         * ini['archiveBitratePerCameraMbps'] * 1000 * 1000 // 8
     )
     for storage in _get_storages(api, ini):
-        if storage.free_space >= storage.reserved_space + space_for_recordings_bytes:
+        if (
+            storage.is_used_for_writing and
+                storage.free_space >= storage.reserved_space + space_for_recordings_bytes
+        ):
             break
     else:
         raise exceptions.BoxStateError(
