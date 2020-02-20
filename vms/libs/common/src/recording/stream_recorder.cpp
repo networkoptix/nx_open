f6f1210b31f03004026b89e44d6be29b94a66cbc
Alexander Kulikov <akulikov@networkoptix.com>
2020-02-19 16:37:33 -0800

    VMS-17448: Calculate file size via storage API. Fix bug in system commands
    Branch: vms_4.0

--- vms/libs/common/src/recording/stream_recorder.cpp
+++ vms/libs/common/src/recording/stream_recorder.cpp
@@ -221,7 +221,7 @@ void QnStreamRecorder::close()
         if (m_recordingContextVector[i].formatCtx)
         {
             if (m_startDateTimeUs != qint64(AV_NOPTS_VALUE))
-                fileSize = QnFfmpegHelper::getFileSizeByIOContext(m_recordingContextVector[i].formatCtx->pb);
+                fileSize = m_recordingContextVector[i].storage->getFileSize(m_recordingContextVector[i].fileName);
 
             QnFfmpegHelper::closeFfmpegIOContext(m_recordingContextVector[i].formatCtx->pb);
 #ifndef SIGN_FRAME_ENABLED
