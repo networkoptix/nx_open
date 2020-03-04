8fa37aae38ad7cffcd4e8502d301642f4d653f37
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2020-03-04 20:37:02 +0300

    Add string description for OpenGL enum errors
    Branch: x/VMS-17682_support_opengl_coreprofile_on_macos

--- vms/client/nx_vms_client_desktop/src/ui/graphics/opengl/gl_shortcuts.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/opengl/gl_shortcuts.cpp
@@ -13,6 +13,48 @@
  */
 //#define QN_GL_RENDERER_DEBUG
 
+namespace {
+
+#ifdef QN_GL_RENDERER_DEBUG
+const char* glErrorString(GLenum err)
+{
+    switch (err)
+    {
+        case GL_NO_ERROR:
+            return "GL_NO_ERROR";
+
+        case GL_INVALID_ENUM:
+            return "GL_INVALID_ENUM";
+
+        case GL_INVALID_VALUE:
+            return "GL_INVALID_VALUE";
+
+        case GL_INVALID_OPERATION:
+            return "GL_INVALID_OPERATION";
+
+        case GL_STACK_OVERFLOW:
+            return "GL_STACK_OVERFLOW";
+
+        case GL_STACK_UNDERFLOW:
+            return "GL_STACK_UNDERFLOW";
+
+        case GL_OUT_OF_MEMORY:
+            return "GL_OUT_OF_MEMORY";
+
+        case GL_TABLE_TOO_LARGE:
+            return "GL_TABLE_TOO_LARGE";
+
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            return "GL_INVALID_FRAMEBUFFER_OPERATION";
+
+        default:
+            return "UNKNOWN";
+    }
+}
+#endif // QN_GL_RENDERER_DEBUG
+
+} // namespace
+
 int glCheckError(const char *context)
 {
 #ifdef QN_GL_RENDERER_DEBUG
@@ -25,7 +67,7 @@ int glCheckError(const char *context)
 
     const auto error = currentContext->functions()->glGetError();
     if (error != GL_NO_ERROR)
-        qnWarning("OpenGL error in '%1': %2", context, error);
+        qnWarning("OpenGL error in '%1': %2", context, glErrorString(error));
 
     return error;
 #else
