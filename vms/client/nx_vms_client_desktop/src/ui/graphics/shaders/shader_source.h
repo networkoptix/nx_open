ca0c9dcf95a01274f5c28511f8754c5a0a6d8a66
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2020-03-04 20:49:51 +0300

    Automatically modernize shaders for GLSL 3.3 on macOS
    Branch: x/VMS-17682_support_opengl_coreprofile_on_macos

--- vms/client/nx_vms_client_desktop/src/ui/graphics/shaders/shader_source.h
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/shaders/shader_source.h
@@ -5,11 +5,49 @@
 #include <boost/preprocessor/stringize.hpp>
 #endif
 
+#include <nx/utils/app_info.h>
+
 #include <QtCore/QByteArray>
+#include <QtGui/QSurfaceFormat>
+#include <QDebug>
 
 inline QByteArray qnUnparenthesize(const char *parenthesized) {
-    QByteArray result(parenthesized);
-    return result.mid(1, result.size() - 2);
+    QByteArray result;
+
+    result.append(parenthesized + 1);
+    result.chop(1);
+
+    const auto profile = QSurfaceFormat::defaultFormat().profile();
+
+    if (profile == QSurfaceFormat::CoreProfile && nx::utils::AppInfo::isMacOsX())
+    {
+        // Modernize shared source.
+
+        const bool isFragment = result.contains("gl_FragColor");
+
+        if (isFragment)
+        {
+            result.prepend("layout(location = 0, index = 0) out vec4 FragColor;\n");
+            result.replace("gl_FragColor", "FragColor");
+
+            // GL_LUMINANCE_ALPHA -> GL_RG
+            result.replace(").a,", ").g,");
+
+            // GL_LUMINANCE -> GL_RED
+            result.replace(").p,", ").r,");
+            result.replace(").p;", ").r;");
+            result.replace(").p)", ").r)");
+            result.replace(").p-", ").r-");
+
+            result.replace("texture2D", "texture");
+        }
+
+        result.replace("attribute", "in");
+        result.replace("varying", isFragment ? "in" : "out");
+
+        result.prepend("#version 330\n");
+    }
+
     return result;
 }
 
