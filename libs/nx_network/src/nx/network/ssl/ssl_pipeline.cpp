c9f0ecc8f969ca9ebeb68c236effe8809e24821a
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2020-03-09 17:58:32 -0700

    NX-CR-1392: Reverted some recent SSL socket changes
    Branch: vms_4.1_server
    Change-Id: I7d6c2b75c3be55c13dd07d5f098ef0fb9f0b6dd6

--- libs/nx_network/src/nx/network/ssl/ssl_pipeline.cpp
+++ libs/nx_network/src/nx/network/ssl/ssl_pipeline.cpp
@@ -28,18 +28,16 @@ std::string Pipeline::serverNameFromClientHello() const
 
 int Pipeline::write(const void* data, size_t size)
 {
-    const auto resultCode = performSslIoOperation(&SSL_write, data, size);
-    if (m_state >= State::handshakeDone
-        && resultCode < 0
-        && resultCode != utils::bstream::StreamIoError::wouldBlock)
-    {
-        m_failed = true;
-    }
     if (m_failed)
     {
         SystemError::setLastErrorCode(SystemError::invalidData);
         return utils::bstream::StreamIoError::nonRecoverableError;
     }
+
+    const auto resultCode = performSslIoOperation(&SSL_write, data, size);
+    if (m_state >= State::handshakeDone && resultCode < 0)
+        m_failed = true;
+
     return resultCode;
 }
 
