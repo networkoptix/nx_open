4a18b421f100af3aab575d87f2ddb37310898531
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2020-03-12 17:16:24 +0300

    Refactor qnUnparenthesize to qnProcessShaderSource
    Branch: x/VMS-17682_support_opengl_coreprofile_on_macos

--- vms/client/nx_vms_client_desktop/src/ui/graphics/shaders/shader_source.h
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/shaders/shader_source.h
@@ -6,45 +6,51 @@
 #endif
 
 #include <nx/utils/app_info.h>
+#include <nx/utils/log/assert.h>
 
 #include <QtCore/QByteArray>
 #include <QtGui/QSurfaceFormat>
-#include <QDebug>
 
-inline QByteArray qnUnparenthesize(const char *parenthesized) {
+inline QByteArray qnProcessShaderSource(const char* parenthesized)
+{
     QByteArray result;
 
-    result.append(parenthesized + 1);
-    result.chop(1);
+    // Remove leading '('.
+    if (NX_ASSERT(parenthesized[0] == '('))
+        result.append(parenthesized + 1); 
+
+    // Remove trailing ')'.
+    if (NX_ASSERT(result.back() == ')'))
+        result.chop(1);
 
     const auto profile = QSurfaceFormat::defaultFormat().profile();
 
-    if (profile == QSurfaceFormat::CoreProfile)
+    if (profile != QSurfaceFormat::CoreProfile)
+        return result;
+
+    // Modernize shared source.
+
+    const bool isFragment = result.contains("gl_FragColor");
+
+    if (isFragment)
     {
-        // Modernize shared source.
+        result.prepend("layout(location = 0, index = 0) out vec4 FragColor;\n");
+        result.replace("gl_FragColor", "FragColor");
 
-        const bool isFragment = result.contains("gl_FragColor");
+        // GL_LUMINANCE_ALPHA -> GL_RG
+        result.replace(").q,", ").g,");
 
-        if (isFragment)
-        {
-            result.prepend("layout(location = 0, index = 0) out vec4 FragColor;\n");
-            result.replace("gl_FragColor", "FragColor");
-
-            // GL_LUMINANCE_ALPHA -> GL_RG
-            result.replace(").q,", ").g,");
-
-            result.replace("texture2D", "texture");
-        }
-
-        result.replace("attribute", "in");
-        result.replace("varying", isFragment ? "in" : "out");
-
-        result.prepend("#version 330\n");
+        result.replace("texture2D", "texture");
     }
+
+    result.replace("attribute", "in");
+    result.replace("varying", isFragment ? "in" : "out");
+
+    result.prepend("#version 330\n");
 
     return result;
 }
 
-#define QN_SHADER_SOURCE(...) qnUnparenthesize(BOOST_PP_STRINGIZE((__VA_ARGS__)))
+#define QN_SHADER_SOURCE(...) qnProcessShaderSource(BOOST_PP_STRINGIZE((__VA_ARGS__)))
 
 #endif // QN_SHADER_SOURCE_H
