b70c041883ca4509598fce4baacb3e3bcf301150
Sergey Panasenko <spanasenko@networkoptix.com>
2020-03-20 18:51:55 +0300

    NX-CR-1542: Renamed icon file
    Branch: vms_4.1

--- vms/client/nx_vms_client_desktop/src/ui/widgets/business/push_notification_business_action_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/widgets/business/push_notification_business_action_widget.cpp
@@ -72,7 +72,7 @@ PushNotificationBusinessActionWidget::PushNotificationBusinessActionWidget(QWidg
 
     setHelpTopic(this, Qn::EventsActions_SendMobileNotification_Help);
 
-    ui->languageButton->setIcon(qnSkin->pixmap("events/lang.svg"));
+    ui->languageButton->setIcon(qnSkin->pixmap("events/push_language.svg"));
     connect(ui->languageButton, &QPushButton::clicked, this,
         [this]
         {
