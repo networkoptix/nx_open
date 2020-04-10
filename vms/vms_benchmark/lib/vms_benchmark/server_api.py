aa3917a470e32a513064a7447f759a3a5a847cdd
Artem Levenkov <alevenkov@networkoptix.com>
2020-04-10 13:44:00 +0300

    VMS-16245: Enchance HTTP error displaying

--- vms/vms_benchmark/lib/vms_benchmark/server_api.py
+++ vms/vms_benchmark/lib/vms_benchmark/server_api.py
@@ -15,30 +15,6 @@ from vms_benchmark.license import License
 Response = namedtuple('Response', ['code', 'body'])
 
 
-def _catch_http_errors(action):
-    def result_func(*args, **kwargs):
-        try:
-            return action(*args, **kwargs)
-        except urllib.error.HTTPError as e:
-            if e.code == 401 or e.code == 403:
-                raise exceptions.ServerApiError(
-                    f"Server failed to execute API request: insufficient VMS user permissions (HTTP code {e.code}).",
-                    original_exception=e
-                )
-            if e.code == 400 or e.code == 404 or e.code == 406:
-                raise exceptions.ServerApiError(
-                    f"Server failed to execute API request: Server internal error (HTTP code {e.code}).",
-                    original_exception=e
-                )
-            if 500 <= e.code <= 599:
-                raise exceptions.ServerApiError(
-                    f"Server failed to execute API request: Server internal error (HTTP code {e.code}).",
-                    original_exception=e
-                )
-            raise exceptions.ServerApiError(e)
-    return result_func
-
-
 class ServerApi:
     class Response:
         def __init__(self, code):
@@ -85,7 +61,6 @@ class ServerApi:
             return None
         return result
 
-    @_catch_http_errors
     def get_module_information(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/moduleInformationAuthenticated")
         credentials = f"{self.user}:{self.password}"
@@ -102,7 +77,6 @@ class ServerApi:
 
             return result.payload['reply']
 
-    @_catch_http_errors
     def get_server_id(self):
         module_information = self.get_module_information()
 
@@ -114,7 +88,6 @@ class ServerApi:
 
         return server_id
 
-    @_catch_http_errors
     def check_authentication(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/moduleInformationAuthenticated")
         credentials = f"{self.user}:{self.password}"
@@ -122,7 +95,6 @@ class ServerApi:
         request.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
         self.get_request(request)
 
-    @_catch_http_errors
     def get_test_cameras_all(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/ec2/getCamerasEx")
         credentials = f"{self.user}:{self.password}"
@@ -149,7 +121,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_test_cameras(self) -> List[Camera]:
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/ec2/getCamerasEx")
         credentials = f"{self.user}:{self.password}"
@@ -176,7 +147,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_licenses(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/ec2/getLicenses")
         credentials = f"{self.user}:{self.password}"
@@ -195,7 +165,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_storage_spaces(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/storageSpace")
         credentials = f"{self.user}:{self.password}"
@@ -217,7 +186,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_events(self, ts_from):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/getEvents?from={ts_from}")
         credentials = f"{self.user}:{self.password}"
@@ -235,7 +203,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_statistics(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/statistics")
         credentials = f"{self.user}:{self.password}"
@@ -253,7 +220,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def get_time(self):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/api/getTime")
         credentials = f"{self.user}:{self.password}"
@@ -271,7 +237,6 @@ class ServerApi:
 
         return None
 
-    @_catch_http_errors
     def add_cameras(self, hostname, count):
         from pprint import pformat
         cameras = []
@@ -307,7 +272,6 @@ class ServerApi:
             ))
         return cameras
 
-    @_catch_http_errors
     def remove_camera(self, camera_id):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/ec2/removeResource")
         credentials = f"{self.user}:{self.password}"
@@ -324,7 +288,6 @@ class ServerApi:
 
         return 200 <= response.code < 300
 
-    @_catch_http_errors
     def activate_license(self, license):
         request = urllib.request.Request(f"http://{self.ip}:{self.port}/ec2/addLicense")
         credentials = f"{self.user}:{self.password}"
@@ -342,7 +305,6 @@ class ServerApi:
 
         return 200 <= response.code < 300
 
-    @_catch_http_errors
     def get_archive_start_time_ms(self, camera_id: str) -> int:
         request = urllib.request.Request(
             f"http://{self.ip}:{self.port}"
