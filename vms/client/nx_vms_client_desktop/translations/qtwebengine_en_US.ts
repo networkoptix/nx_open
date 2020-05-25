aa36863dbe35ae4eb02894704763073ae4af6f87
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2020-05-25 19:02:47 +0300

    VMS-18700: Add QtWebEngine en_US translations

---
+++ vms/client/nx_vms_client_desktop/translations/qtwebengine_en_US.ts
@@ -0,0 +1,426 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en">
+<context>
+    <name>DownloadInterruptReason</name>
+    <message>
+        <source>Unknown reason or not interrupted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General file operation failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file cannot be written locally, due to access restrictions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insufficient space on the target drive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The directory or file name is too long</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file size exceeds the file system limitation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file is infected with a virus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temporary problem (for example file in use, or too many open files)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file was blocked due to local policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Checking the safety of the download failed due to unexpected reasons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File seek past the end of a file (resuming previously interrupted download)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The partial file did not match the expected hash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General network failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The network operation has timed out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The network connection has been terminated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The server has gone down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The network request was invalid (for example, the URL or scheme is invalid)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General server failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The server does not have the requested data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The server did not authorize access to the resource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A problem with the server certificate occurred</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Access forbidden by the server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unexpected server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download canceled by the user</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QQuickWebEngineView</name>
+    <message>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reload and Bypass Cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open link in this window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle Play/Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unselect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Bold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Italic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Underline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Strikethrough</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Justified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Indent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Outdent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert &amp;Ordered List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert &amp;Unordered List</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWebEnginePage</name>
+    <message>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reload and Bypass Cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle Play/Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unselect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open link in this window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open link in new background tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Bold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Italic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Underline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Strikethrough</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Align &amp;Justified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Indent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Outdent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert &amp;Ordered List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert &amp;Unordered List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to leave this page? Changes that you made may not be saved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select folder to upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RenderViewContextMenuQt</name>
+    <message>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paste and match style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open link in new window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open link in new tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy link address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy image address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy media address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save media</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inspect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit full screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>View page source</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UIDelegatesManager</name>
+    <message>
+        <source>Connect to proxy &quot;%1&quot; using:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter username and password for &quot;%1&quot; at %2://%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Javascript Alert - %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Javascript Confirm - %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Javascript Prompt - %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to leave this page?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Changes that you made may not be saved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebContentsAdapter</name>
+    <message>
+        <source>HTTP-POST data can only be sent over HTTP(S) protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
