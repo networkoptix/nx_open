6be4f8ffcb6adb9728095479c713fff895f5fd53
Pavel Tolstov <ptolstov@networkoptix.com>
2020-06-29 09:49:15 +0000

    VMS-18987: nx_kit OutputRedirector: Improve logging and comments

--- artifacts/nx_kit/src/nx/kit/output_redirector.h
+++ artifacts/nx_kit/src/nx/kit/output_redirector.h
@@ -29,6 +29,13 @@ namespace kit {
  *
  * If needed, this mechanism can be disabled defining a macro at compiling output_redirector.cpp:
  * -DNX_OUTPUT_REDIRECTOR_DISABLED.
+ *
+ * ATTENTION: If all of the following conditions are met:
+ *   1. The shared library is built on Windows as "Debug" with MSVC.
+ *   2. The library is loaded dynamically in runtime.
+ *   3. The main executable runs as a Windows service.
+ * Then the redirected output doesn't go to the file and just disappears. This behavior is due to
+ * some unknown MSVC issue.
  */
 class NX_KIT_API OutputRedirector
 {
@@ -45,6 +52,15 @@ public:
      * This is needed to guarantee that the static initializer in this library (that performs
      * the redirection) is called, because if no symbols from this library are used in the
      * executable, the linker can optimize away any static initialization in this library.
+     *
+     * ATTENTION: This call should be done exactly once to prevent unexpected behavior in the case
+     * when more than one copy of the library is linked to the executable. This situation can
+     * occur, for example, when one copy of nx::kit library is linked to the executable itself, and
+     * another one is linked to the shared library. If ensureOutputRedirection() is called once
+     * (in the executable) this works perfectly well, because the linker leaves static
+     * initilization in the executable and optimizes it away from the shared library. Otherwise
+     * (ensureOutputRedirection() is called both in the executable and in the shared library) it
+     * can result in multiple initialization of the Redirector, and some output can be lost.
      */
      static void ensureOutputRedirection();
 
