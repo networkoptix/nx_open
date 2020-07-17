09d3c344721952d40d379d856529ec4fc1b4c09a
Denis Klychkov <dklychkov@networkoptix.com>
2020-07-17 15:47:05 +0300

    VMS-19388: Make it possible to run 4.1 or newer Client from 4.0 or older Applauncher
    Recently we've added libstdc++ conditional loading into the `client` script. This made it possible
    to run Client successfully in any of supported Ubuntu versions using this script. Applauncher was
    configured to use this script instead of calling the Client binary directly. However old
    Applauncher still tries to load the binary (client-bin). The solution is to make `client-bin` a
    symlink to `client` script for backwards compatibility and rename the binary to something else.
    The new binary name contains brand information and it is now `nxwitness_client` for the default
    customization.

--- vms/client/nx_vms_client_desktop/src/client/client_app_info.h
+++ vms/client/nx_vms_client_desktop/src/client/client_app_info.h
@@ -16,7 +16,7 @@ struct QnClientAppInfo
     /** Name of the new applauncher binary in the filesystem (applaucher-bin, applauncher.exe). */
     static QString applauncherBinaryName();
 
-    /** Name of the client binary in the filesystem (client-bin, HD Witness.exe). */
+    /** Name of the client binary in the filesystem (nxwitness-client, HD Witness.exe). */
     static QString clientBinaryName();
 
     /** Directory where all software is installed by default. */
