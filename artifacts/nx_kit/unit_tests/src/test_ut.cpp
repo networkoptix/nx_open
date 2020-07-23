bcf0594dbe882a2611b3f42646db80798fb10095
Mikhail Shevchenko <mshevchenko@networkoptix.com>
2020-07-23 11:51:07 +0000

    nx_kit: Fix ASSERT_STREQ() for strings with NUL chars inside

--- artifacts/nx_kit/unit_tests/src/test_ut.cpp
+++ artifacts/nx_kit/unit_tests/src/test_ut.cpp
@@ -44,6 +44,96 @@ TEST(test, createFile)
     ASSERT_EQ(contents, dataFromFile);
 }
 
+template<typename Expected, typename Actual>
+void assertStreqFails(int line, const Expected& expected, const Actual& actual)
+{
+    try
+    {
+        ASSERT_STREQ_AT_LINE(line, expected, actual);
+    }
+    catch (const std::exception& e)
+    {
+        ASSERT_STREQ(
+            "    Expected: [" + detail::UniversalString(expected).toString() + "] (expected)\n"
+            "    Actual:   [" + detail::UniversalString(actual).toString() + "] (actual)",
+            e.what());
+        return; //< The exception is expected.
+    }
+    ASSERT_TRUE(false); //< Fail the test if there was no exception.
+}
+
+TEST(test, assertStreqWithNulChars)
+{
+    const std::string strWithNulInside("a\0b", 3);
+
+    // Check that ASSERT_STREQ works with all combinations of a char array and std::string.
+    ASSERT_STREQ("a\0b", strWithNulInside);
+    ASSERT_STREQ("a\0b", "a\0b");
+    ASSERT_STREQ(strWithNulInside, strWithNulInside);
+    ASSERT_STREQ(strWithNulInside, "a\0b");
+
+    // If the checks above succeed, it may mean that '\0' in the middle is not supported at all,
+    // thus, we add a second check that must fail - the strings differ in the part after '\0'.
+
+    const std::string strWithNulInsideAndDifferentSuffix("a\0B", 3);
+
+    assertStreqFails(__LINE__, "a\0b", strWithNulInsideAndDifferentSuffix);
+    assertStreqFails(__LINE__, "a\0b", "a\0B");
+    assertStreqFails(__LINE__, strWithNulInside, strWithNulInsideAndDifferentSuffix);
+    assertStreqFails(__LINE__, strWithNulInside, "a\0B");
+}
+
+TEST(utils, universalString)
+{
+    using detail::UniversalString;
+
+    // NOTE: ASSERT_STREQ() itself uses UniversalString type for its arguments.
+
+    { // Copy constructor.
+        const UniversalString src("text");
+        const UniversalString dst(src);
+        ASSERT_STREQ("text", dst.s);
+    }
+    { // From null char*.
+        const UniversalString str((const char*) nullptr);
+        ASSERT_TRUE(str.isNull);
+        ASSERT_STREQ("", str.s);
+        ASSERT_STREQ("null", str.toString());
+    }
+    { // From char array, empty.
+        const UniversalString str("");
+        ASSERT_FALSE(str.isNull);
+        ASSERT_STREQ("", str);
+        ASSERT_STREQ("\"\"", str.toString());
+    }
+    { // From char array, with '\0' inside.
+        const UniversalString str("a\0b");
+        ASSERT_FALSE(str.isNull);
+        ASSERT_EQ(3, (int) str.s.size());
+        ASSERT_STREQ("a\000b", str);
+        ASSERT_STREQ("\"a\\000b\"", str.toString());
+    }
+    { // From std::string, empty.
+        const UniversalString str(std::string(""));
+        ASSERT_FALSE(str.isNull);
+        ASSERT_STREQ("", str);
+        ASSERT_STREQ("\"\"", str.toString());
+    }
+    { // From std::string, with '\0' inside.
+        const UniversalString str(std::string("a\0b", 3));
+        ASSERT_FALSE(str.isNull);
+        ASSERT_EQ(3, (int) str.s.size());
+        ASSERT_STREQ("a\000b", str);
+        ASSERT_STREQ("\"a\\000b\"", str.toString());
+    }
+    { // From char*.
+        const UniversalString str((const char*) "abc");
+        ASSERT_FALSE(str.isNull);
+        ASSERT_STREQ("abc", str);
+        ASSERT_STREQ("\"abc\"", str.toString());
+    }
+}
+
 } // namespace test
 } // namespace test
 } // namespace kit
