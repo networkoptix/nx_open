223d9b24cc61eb7cbcbaa36eb31cfc480f849ed5
Mike Shevchenko <mshevchenko@networkoptix.com>
2020-07-24 20:49:32 +0100

    SDK: Add Doxygen comment explaining interface evolution naming scheme

--- vms/libs/nx_sdk/src/nx/sdk/interface.h
+++ vms/libs/nx_sdk/src/nx/sdk/interface.h
@@ -23,6 +23,27 @@ namespace sdk {
  *     ... // pure virtual methods
  * };
  * ```
+ *
+ * NOTE: For binary compatibility, when an interface needs to be extended with new methods, instead
+ * of adding virtual methods to the interface class, define a new descendant class with the new
+ * methods, and use the following naming scheme for class names and interface ids:
+ * ```
+ * // Version 0 (the original):
+ * class ISomething("my_namespace::ISomething") { <original-virtual-methods> }
+ *
+ * // Version 1 (first evolution step):
+ * class ISomething0("my_namespace::ISomething") { <original-virtual-methods> }
+ * class ISomething("my_namespace::ISomething1") { <step1-added-virtual-methods> }
+ *
+ * // Version 2 (second evolution step):
+ * class ISomething0("my_namespace::ISomething") { <original-virtual-methods> }
+ * class ISomething1("my_namespace::ISomething1") { <step1-added-virtual-methods> }
+ * class ISomething("my_namespace::ISomething2") { <step2-added-virtual-methods> }
+ * ```
+ * Such naming scheme implies the following:
+ * - The interface id for the particular VMT never changes, though the C++ class may be renamed.
+ * - If both the interface id and the interface class have a number appended, the numbers coincide.
+ * - The class of the latest interface version always has the original name without a number.
  */
 template<class DerivedInterface, class BaseInterface = IRefCountable>
 class Interface: public BaseInterface
