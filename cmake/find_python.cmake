215ebe959aee9eff1b109695148820ac6c8cc4e4
Artem Levenkov <alevenkov@networkoptix.com>
2020-09-14 16:18:10 +0300

    VMS-20141: Rework find_python_at_least_37(): search patterns in PATH components sequentially

--- cmake/find_python.cmake
+++ cmake/find_python.cmake
@@ -1,56 +1,79 @@
-function(check_python_at_least_37)
+function(verify_python_at_least_37)
     execute_process(
         COMMAND "${PYTHON_EXECUTABLE}" --version
         OUTPUT_VARIABLE PYTHON_VERSION
+        ERROR_VARIABLE PYTHON_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_STRIP_TRAILING_WHITESPACE
     )
 
     if (NOT "${PYTHON_VERSION}" MATCHES "Python 3\.(7|8)(\..*)?")
-        if ("${PYTHON_VERSION}" MATCHES "Python (.*)")
-              message(
-                  FATAL_ERROR
-                  "Executable ${PYTHON_EXECUTABLE} has invalid version: "
-                  "${CMAKE_MATCH_1} instead of 3.(7|8)*."
-              )
-         else()
-              message(FATAL_ERROR "Unexpected Python version: '${PYTHON_VERSION}'.")
-         endif()
+        set(PYTHON_EXECUTABLE PYTHON_EXECUTABLE-NOTFOUND PARENT_SCOPE)
+        return()
     endif()
+
+    message(STATUS "Found python executable: '${PYTHON_EXECUTABLE}'.")
+endfunction()
+
+function(find_python_at_least_37_in_dir)
+    cmake_parse_arguments("" "" "SEARCH_PATH" "PATTERNS" "${ARGN}")
+
+    foreach(PATTERN IN LISTS _PATTERNS)
+        if(CYGWIN)
+            set(
+                PYTHON_EXECUTABLE_PATTERNS
+                ${PATTERN}
+                ${PATTERN}.exe
+            )
+        elseif(WIN32)
+            set(PYTHON_EXECUTABLE_PATTERNS ${PATTERN}.exe)
+        else()
+            set(PYTHON_EXECUTABLE_PATTERNS ${PATTERN})
+        endif()
+
+        find_program(
+            PYTHON_EXECUTABLE NAMES ${PYTHON_EXECUTABLE_PATTERNS}
+            PATHS ${_SEARCH_PATH}
+            NO_DEFAULT_PATH
+        )
+
+        if(NOT ${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
+            verify_python_at_least_37()
+        endif()
+
+        if(NOT ${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
+            return()
+        endif()
+    endforeach ()
 endfunction()
 
 function(find_python_at_least_37)
-    if(${ARGC} GREATER 1)
-        list(SUBLIST ARGV 1 ${ARGC} ARGV_EXCEPT_FIRST)
+    set(SEARCH_PATHS_LIST $ENV{PATH})
+
+    if(CYGWIN OR NOT WIN32)
+        string(REPLACE ":" ";" SEARCH_PATHS_LIST ${SEARCH_PATHS_LIST})
     endif()
 
-    set(PYTHON_EXECUTABLE_PATTERNS ${ARGV0})
+    foreach(SEARCH_PATH IN LISTS SEARCH_PATHS_LIST)
+        message("Searching Python in \"${SEARCH_PATH}\"...")
 
-    if (CYGWIN)
-        set(PYTHON_EXECUTABLE_PATTERNS ${PYTHON_EXECUTABLE_PATTERNS} ${PYTHON_EXECUTABLE_PATTERNS}.exe)
-    elseif(WIN32)
-        set(PYTHON_EXECUTABLE_PATTERNS ${PYTHON_EXECUTABLE_PATTERNS}.exe)
-    endif()
+        find_python_at_least_37_in_dir(PATTERNS ${ARGV} SEARCH_PATH ${SEARCH_PATH})
 
-    find_program(
-        PYTHON_EXECUTABLE NAMES ${PYTHON_EXECUTABLE_PATTERNS} NO_CMAKE_SYSTEM_PATH NO_CMAKE_PATH
-    )
-
-    if (${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
-        if(${ARGC} EQUAL 1)
-            message(FATAL_ERROR "Python >=3.7 executable not found.")
+        if(NOT ${PYTHON_EXECUTABLE} STREQUAL PYTHON_EXECUTABLE-NOTFOUND)
+            return()
         endif()
+    endforeach()
 
-        find_python_at_least_37(${ARGV_EXCEPT_FIRST})
-    else()
-        message(STATUS "Found python executable: '${PYTHON_EXECUTABLE}'.")
-
-        check_python_at_least_37()
-
-        return()
-    endif()
+    message(FATAL_ERROR "Python >=3.7 executable not found.")
 endfunction()
 
-find_python_at_least_37(python3.8 python3.7 python3 python)
+if("${PYTHON_EXECUTABLE}" STREQUAL PYTHON_EXECUTABLE-NOTFOUND
+    OR "${PYTHON_EXECUTABLE}" STREQUAL ""
+)
+    find_python_at_least_37(python3.8 python3.7 python3 python)
+else()
+    message(STATUS "Found python executable: '${PYTHON_EXECUTABLE}'.")
+endif()
 
 set(ENV{PYTHONDONTWRITEBYTECODE} True)
 
