networkoptix/nx_open

commit 3d62b3dad346d0945b984137ccf3d7aecd6c4f69

VMS-18605: Validate update packages before extraction or installation
After this commit Client and Server will check a digital signature of every update package that is
going to be installed. The signatures must be provided in the update information (packages.json).

For simplified development and testing, it is possible to disable verification by a corresopnding
option in the INI-configs.

vms/server/nx_vms_server/src/nx/vms/server/update/update_manager.cpp
@@ -134,8 +134,8 @@ void UpdateManager::retry(bool forceRedownload)
         case ErrorCode::osVersionNotSupported:
         case ErrorCode::internalError:
         case ErrorCode::unknownError:
-        case ErrorCode::applauncherError:
         case ErrorCode::invalidUpdateContents:
+        case ErrorCode::verificationError:
         case ErrorCode::installationError:
             // We can do nothing with these cases.
             break;
@@ -339,7 +339,7 @@ void UpdateManager::extract()
 
     NX_INFO(this, "Extracting update package...");
 
-    m_installer.prepareAsync(downloader()->filePath(package.file));
+    m_installer.prepareAsync(downloader()->filePath(package.file), package.signature);
     detectStartedInstallation();
 }
 
@@ -558,6 +558,13 @@ bool UpdateManager::installerState(update::Status* outUpdateStatus, const QnUuid
             update::Status::Code::error,
             update::Status::ErrorCode::extractionError);
             outUpdateStatus->message = "Installer is busy";
+        return true;
+    case UpdateInstaller::State::verificationFailed:
+        *outUpdateStatus = update::Status(
+            peerId,
+            update::Status::Code::error,
+            update::Status::ErrorCode::verificationError);
+        outUpdateStatus->message = "Update verification failed";
         return true;
     case UpdateInstaller::State::installationFailed:
         *outUpdateStatus = update::Status(

cmake/qrc_utils.cmake
@@ -1,3 +1,19 @@
+function(nx_set_qrc_source_files_properties)
+    cmake_parse_arguments(RESOURCE "" "BASE_DIR;ALIAS_PREFIX" "" ${ARGN})
+
+    set(properties)
+
+    if(RESOURCE_BASE_DIR)
+        list(APPEND properties RESOURCE_BASE_DIR ${RESOURCE_BASE_DIR})
+    endif()
+
+    if(RESOURCE_ALIAS_PREFIX)
+        list(APPEND properties RESOURCE_ALIAS_PREFIX ${RESOURCE_ALIAS_PREFIX})
+    endif()
+
+    set_source_files_properties(${RESOURCE_UNPARSED_ARGUMENTS} PROPERTIES ${properties})
+endfunction()
+
 function(nx_generate_qrc qrc_file)
     cmake_parse_arguments(QRC "" "" "USED_FILES_VARIABLE" ${ARGN})
 

sync_dependencies.py
@@ -229,6 +229,7 @@ def sync_dependencies(target, syncher, platform, arch, box, release_version, opt
 
     if have_mediaserver or have_desktop_client:
         sync("%s/doxygen" % platform, path_variable="doxygen_directory")
+        sync("any/update_verification_keys", path_variable="update_verification_keys_dir")
 
     sync("any/root-certificates", path_variable="root_certificates_path")
     sync("any/customization_pack", path_variable="customization_package_directory")

vms/client/nx_vms_client_desktop/CMakeLists.txt
@@ -38,6 +38,9 @@ if(WINDOWS)
     set(library_type LIBRARY_TYPE SHARED)
 endif()
 
+nx_set_qrc_source_files_properties(
+    ${update_verification_keys_dir}/keys ALIAS_PREFIX update_verification_keys/)
+
 nx_add_target(nx_vms_client_desktop LIBRARY ${library_type}
     ADDITIONAL_SOURCES
         "${CMAKE_CURRENT_BINARY_DIR}/client_app_info_impl.cpp"
@@ -48,6 +51,7 @@ nx_add_target(nx_vms_client_desktop LIBRARY ${library_type}
         "${customization_dir}/nx_client_desktop/resources"
         "${customization_dir}/icons/all/logo.png"
         "${customization_dir}/license.html"
+        "${update_verification_keys_dir}/keys"
         "${nov_file_launcher_executable}"
         "${client_font_roboto_regular}"
     ADDITIONAL_MOCABLES

vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ini.h
@@ -75,6 +75,8 @@ struct Ini: nx::kit::IniConfig
     NX_INI_FLAG(0, massSystemUpdateClearDownloads,
         "[Dev] Forces the Client to remove downloaded data for system updates.");
 
+    NX_INI_FLAG(true, skipUpdateFilesVerification, "[Dev] Skip update files signature check.");
+
     NX_INI_FLAG(0, forceCompatibilityMode,
         "[Dev] Forces the compatibility mode dialog to appear when connecting to the system.");
 

vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_update/client_update_tool.cpp
@@ -19,6 +19,7 @@
 #include <nx/vms/client/desktop/ini.h>
 
 #include "update_verification.h"
+#include "peer_state_tracker.h"
 
 namespace nx::vms::client::desktop {
 
@@ -122,27 +123,54 @@ void ClientUpdateTool::setState(State newState)
 
     m_state = newState;
     m_stateChanged = true;
-    m_lastError = QString();
+    m_error = update::Status::ErrorCode::noError;
     emit updateStateChanged((int)m_state, 0, {});
 }
 
-void ClientUpdateTool::setError(const QString& error)
+void ClientUpdateTool::setError(const Error& error)
 {
     m_state = State::error;
-    m_lastError = error;
-    emit updateStateChanged((int)m_state, 0, error);
+    m_error = error;
+    emit updateStateChanged((int) m_state, 0, error);
 }
 
-void ClientUpdateTool::setApplauncherError(const QString& error)
+void ClientUpdateTool::verifyUpdateFile()
 {
-    m_state = State::applauncherError;
-    m_lastError = error;
-    emit updateStateChanged((int)m_state, 0, error);
-}
+    if (m_verificationResult.valid())
+        return;
 
-QString ClientUpdateTool::getErrorText() const
-{
-    return m_lastError;
+    NX_VERBOSE(this, "Start verifying the update file %1", m_updateFile);
+
+    setState(State::verifying);
+
+    m_verificationResult = std::async(std::launch::async,
+        [this]()
+        {
+            if (ini().skipUpdateFilesVerification)
+                return common::FileSignature::Result::ok;
+
+            common::FileSignature::Result result = common::FileSignature::Result::failed;
+
+            const QDir keysDir(":/update_verification_keys");
+            for (const QString& file: keysDir.entryList({"*.pem"}, QDir::Files, QDir::Name))
+            {
+                const QString& fileName = keysDir.absoluteFilePath(file);
+                const QByteArray& key = common::FileSignature::readKey(fileName);
+                if (key.isEmpty())
+                    NX_WARNING(this, "Cannot load key from %1", fileName);
+
+                result = common::FileSignature::verify(
+                    m_updateFile, key, m_clientPackage.signature);
+
+                if (result == common::FileSignature::Result::ok)
+                    break;
+            }
+
+            QMetaObject::invokeMethod(
+                this, &ClientUpdateTool::atVerificationFinished, Qt::QueuedConnection);
+
+            return result;
+        });
 }
 
 std::future<nx::update::UpdateContents> ClientUpdateTool::requestInstalledUpdateInfo()
@@ -253,6 +281,14 @@ std::set<nx::utils::SoftwareVersion> ClientUpdateTool::getInstalledClientVersion
     return result;
 }
 
+QString ClientUpdateTool::errorString(const ClientUpdateTool::Error& error)
+{
+    if (const auto code = std::get_if<nx::update::Status::ErrorCode>(&error))
+        return PeerStateTracker::errorString(*code);
+    else
+        return std::get<QString>(error);
+}
+
 bool ClientUpdateTool::isVersionInstalled(const nx::utils::SoftwareVersion& version) const
 {
     auto versions = getInstalledClientVersions(/*includeCurrentVersion=*/true);
@@ -310,15 +346,14 @@ void ClientUpdateTool::setUpdateTarget(const UpdateContents& contents)
         QString path = contents.storageDir.filePath(m_clientPackage.localFile);
         if (!QFileInfo::exists(path))
         {
-            NX_ERROR(this)
-                << "setUpdateTarget(" << contents.info.version << ") the file"
-                << path << "does not exist!";
-            setError(QString("File %1 does not exist").arg(path));
+            NX_ERROR(this, "setUpdateTarget(%1) file does not exist: %2",
+                contents.info.version, path);
+            setError(tr("File %1 does not exist").arg(path));
         }
         else
         {
             m_updateFile = path;
-            setState(State::readyInstall);
+            verifyUpdateFile();
         }
     }
     else
@@ -333,7 +368,7 @@ void ClientUpdateTool::setUpdateTarget(const UpdateContents& contents)
 
         if (!info.isValid())
         {
-            setError("There is no valid client package to download");
+            setError(tr("There is no valid client package to download"));
             return;
         }
 
@@ -374,7 +409,7 @@ void ClientUpdateTool::atDownloaderStatusChanged(const FileInformation& fileInfo
     switch (fileInformation.status)
     {
         case FileInformation::Status::notFound:
-            setError(tr("Update file is not found"));
+            setError(update::Status::ErrorCode::updatePackageNotFound);
             break;
         case FileInformation::Status::uploading:
             break;
@@ -382,7 +417,7 @@ void ClientUpdateTool::atDownloaderStatusChanged(const FileInformation& fileInfo
             atDownloadFinished(fileInformation.name);
             break;
         case FileInformation::Status::corrupted:
-            setError(tr("Update package is corrupted"));
+            setError(update::Status::ErrorCode::corruptedArchive);
             break;
         case FileInformation::Status::downloading:
             emit updateStateChanged(int(State::downloading),
@@ -403,7 +438,8 @@ void ClientUpdateTool::atDownloadFinished(const QString& fileName)
 
     NX_VERBOSE(this, "atDownloadFinished(%1) - finally downloaded file to %2",
         fileName, m_updateFile);
-    setState(State::readyInstall);
+
+    verifyUpdateFile();
 }
 
 void ClientUpdateTool::atChunkDownloadFailed(const QString& /*fileName*/)
@@ -417,7 +453,7 @@ void ClientUpdateTool::atDownloadFailed(const QString& fileName)
     if (m_state == State::downloading)
     {
         NX_ERROR(this, "atDownloadFailed() failed to download file %1", fileName);
-        setError(tr("Failed to download update package: %1").arg(fileName));
+        setError(update::Status::ErrorCode::downloadFailed);
     }
 }
 
@@ -426,7 +462,7 @@ void ClientUpdateTool::atDownloadStallChanged(const QString& fileName, bool stal
     if (m_state == State::downloading && stalled)
     {
         NX_ERROR(this, "atDownloadFailed() download of %1 has stalled", fileName);
-        setError(tr("Failed to download update package: %1").arg(fileName));
+        setError(update::Status::ErrorCode::downloadFailed);
     }
 }
 
@@ -434,14 +470,28 @@ void ClientUpdateTool::atExtractFilesFinished(int code)
 {
     if (code != QnZipExtractor::Ok)
     {
-        QString error = QnZipExtractor::errorToString((QnZipExtractor::Error)code);
+        QString error = QnZipExtractor::errorToString((QnZipExtractor::Error) code);
         NX_ERROR(this, "atExtractFilesFinished() err=%1",  error);
-        setError(tr("Update package is corrupted: %1").arg(error));
+        setError(update::Status::ErrorCode::corruptedArchive);
         return;
     }
 
     NX_VERBOSE(this, "at_extractFilesFinished() done unpacking file");
-    setState(State::readyInstall);
+
+    verifyUpdateFile();
+}
+
+void ClientUpdateTool::atVerificationFinished()
+{
+    m_verificationResult.wait();
+    const common::FileSignature::Result result = m_verificationResult.get();
+
+    NX_VERBOSE(this, "Update file verification finished (%1). Result: %2", m_updateFile, result);
+
+    if (result == common::FileSignature::Result::ok)
+        setState(State::readyInstall);
+    else
+        setError(update::Status::ErrorCode::verificationError);
 }
 
 bool ClientUpdateTool::isDownloadComplete() const
@@ -482,7 +532,7 @@ void ClientUpdateTool::checkInternalState()
             {
                 QString error = applauncherErrorToString(result);
                 NX_ERROR(this) << "Failed check installation:" << error;
-                setApplauncherError(error);
+                setError(error);
                 break;
             }
             default:
@@ -498,9 +548,8 @@ bool ClientUpdateTool::installUpdateAsync()
     // Try to run applauncher if it is not running.
     if (!applauncher::api::checkOnline())
     {
-        NX_VERBOSE(this) << "installUpdate can not install update - applauncher is offline" << error;
-        auto errorText = applauncherErrorToString(ResultType::otherError);
-        setApplauncherError(errorText);
+        NX_VERBOSE(this, "installUpdate cannot install update: applauncher is offline");
+        setError(applauncherErrorToString(ResultType::otherError));
         return false;
     }
 
@@ -605,6 +654,7 @@ bool ClientUpdateTool::isInstallComplete() const
         case State::readyDownload:
         case State::readyInstall:
         case State::downloading:
+        case State::verifying:
             return false;
         case State::installing:
             // We need to check applauncher to get a proper result
@@ -615,7 +665,6 @@ bool ClientUpdateTool::isInstallComplete() const
         case State::exiting:
         // Though actual install is not successful, no further progress is possible.
         case State::error:
-        case State::applauncherError:
             return true;
     }
 
@@ -637,7 +686,6 @@ bool ClientUpdateTool::isInstallComplete() const
         {
             QString error = applauncherErrorToString(result);
             NX_ERROR(this) << "Failed to check installation:" << error;
-            //setApplauncherError(error);
             return false;
         }
         default:
@@ -695,7 +743,7 @@ void ClientUpdateTool::resetState()
     }
 
     m_state = State::initial;
-    m_lastError = "";
+    m_error = update::Status::ErrorCode::noError;
     m_updateFile = "";
     m_updateVersion = nx::utils::SoftwareVersion();
     m_clientPackage = nx::update::Package();
@@ -718,7 +766,6 @@ bool ClientUpdateTool::hasUpdate() const
 {
     return m_state != State::initial
         && m_state != State::error
-        && m_state != State::applauncherError
         && m_state != State::complete;
 }
 
@@ -734,6 +781,8 @@ QString ClientUpdateTool::toString(State state)
             return "ReadyDownload";
         case State::downloading:
             return "Downloading";
+        case State::verifying:
+            return "Verifying";
         case State::readyInstall:
             return "ReadyInstall";
         case State::installing:
@@ -746,8 +795,6 @@ QString ClientUpdateTool::toString(State state)
             return "Exiting";
         case State::error:
             return "Error";
-        case State::applauncherError:
-            return "applauncherError";
     }
     return QString();
 }

vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_update/client_update_tool.h
@@ -6,6 +6,7 @@
 
 #include <nx/vms/common/p2p/downloader/downloader.h>
 #include <nx/vms/common/p2p/downloader/private/abstract_peer_manager.h>
+#include <nx/vms/common/utils/file_signature.h>
 #include <nx/vms/api/data/software_version.h>
 #include <nx/update/update_information.h>
 #include <utils/common/connective.h>
@@ -41,6 +42,8 @@ public:
     ClientUpdateTool(QObject* parent = nullptr);
     virtual ~ClientUpdateTool() override;
 
+    using Error = std::variant<nx::update::Status::ErrorCode, QString>;
+
     enum State
     {
         /** Tool starts at this state. */
@@ -51,6 +54,8 @@ public:
         readyDownload,
         /** Downloading client package. */
         downloading,
+        /** Verifying client package. */
+        verifying,
         /** Ready to install client package. */
         readyInstall,
         /** Installing update using applauncher. */
@@ -63,8 +68,6 @@ public:
         exiting,
         /** Got some critical error and can not continue installation. */
         error,
-        /** Got an error during installation. */
-        applauncherError,
     };
 
     /**
@@ -158,22 +161,19 @@ public:
 
     static QString toString(State state);
 
-    /**
-     * Get readable error text.
-     */
-    QString getErrorText() const;
+    Error getError() const { return m_error; }
+    static QString errorString(const Error& error);
 
     void checkInternalState();
 
 signals:
     /**
-     * This event is emitted every time update state changes,
-     * or its internal state is updated.
-     * @param state - current state, corresponds to enum ClientUpdateTool::State;
-     * @param percentComplete - progress for this state.
-     * @param message - contains printable error message.
+     * Emitted every time update state changes, or its internal state is updated.
+     * @param state Current state, corresponds to enum ClientUpdateTool::State.
+     * @param percentComplete Progress for this state.
+     * @param error Error code or printable error message.
      */
-    void updateStateChanged(int state, int percentComplete, const QString& message);
+    void updateStateChanged(int state, int percentComplete, const Error& error);
 
 protected:
     // Callbacks
@@ -183,12 +183,13 @@ protected:
     void atDownloadFailed(const QString& fileName);
     void atDownloadStallChanged(const QString& fileName, bool stalled);
     void atExtractFilesFinished(int code);
+    void atVerificationFinished();
 
 private:
     void setState(State newState);
-    void setError(const QString& error);
-    void setApplauncherError(const QString& error);
+    void setError(const Error& error);
     void clearDownloadFolder();
+    void verifyUpdateFile();
     /**
      * Converts applauncher::api::ResultType to a readable string.
      */
@@ -214,11 +215,12 @@ private:
     rest::QnConnectionPtr m_serverConnection;
     /** Full path to update package. */
     QString m_updateFile;
-    QString m_lastError;
+    Error m_error;
 
     QnMutex m_mutex;
 
     std::future<nx::vms::applauncher::api::ResultType> m_applauncherTask;
+    std::future<nx::vms::common::FileSignature::Result> m_verificationResult;
 
     mutable std::future<std::set<nx::utils::SoftwareVersion>> m_installedVersionsFuture;
     mutable std::set<nx::utils::SoftwareVersion> m_installedVersions;

vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_update/compatibility_version_installation_dialog.cpp
@@ -6,7 +6,6 @@
 #include <QtWidgets/QPushButton>
 
 #include <nx/update/update_check.h>
-#include <nx/vms/client/desktop/system_update/client_update_tool.h>
 #include <nx/vms/client/desktop/system_update/server_update_tool.h>
 #include <client/client_settings.h>
 #include <nx/utils/log/log.h>
@@ -17,7 +16,9 @@
 #include "update_verification.h"
 
 using namespace std::chrono_literals;
-using ClientUpdateTool = nx::vms::client::desktop::ClientUpdateTool;
+
+using namespace nx::vms::client::desktop;
+
 using Clock = std::chrono::steady_clock;
 using TimePoint = std::chrono::time_point<Clock>;
 using Dialog = CompatibilityVersionInstallationDialog;
@@ -130,7 +131,7 @@ void CompatibilityVersionInstallationDialog::processUpdateContents(
 }
 
 void CompatibilityVersionInstallationDialog::atUpdateStateChanged(
-    int state, int progress, const QString& message)
+    int state, int progress, const ClientUpdateTool::Error& error)
 {
     NX_DEBUG(this, "CompatibilityVersionInstallationDialog::atUpdateStateChanged(%1, %2)",
         ClientUpdateTool::toString(ClientUpdateTool::State(state)), progress);
@@ -156,7 +157,12 @@ void CompatibilityVersionInstallationDialog::atUpdateStateChanged(
         case ClientUpdateTool::State::downloading:
             m_installationResult = InstallResult::downloading;
             setMessage(tr("Downloading update package"));
-            finalProgress = 20 + 70 * progress / 100;
+            finalProgress = 20 + 65 * progress / 100;
+            break;
+        case ClientUpdateTool::State::verifying:
+            m_installationResult = InstallResult::verifying;
+            setMessage(tr("Verifying update package"));
+            finalProgress = 85;
             break;
         case ClientUpdateTool::State::readyInstall:
             finalProgress = 90;
@@ -186,14 +192,7 @@ void CompatibilityVersionInstallationDialog::atUpdateStateChanged(
             else
                 m_installationResult = InstallResult::failedDownload;
             setMessage(tr("Installation failed"));
-            m_errorMessage = message;
-            finalProgress = 100;
-            done(QDialogButtonBox::StandardButton::Ok);
-            break;
-        case ClientUpdateTool::State::applauncherError:
-            m_installationResult = InstallResult::failedInstall;
-            setMessage(tr("Installation failed"));
-            m_errorMessage = message;
+            m_errorMessage = ClientUpdateTool::errorString(error);
             finalProgress = 100;
             done(QDialogButtonBox::StandardButton::Ok);
             break;

commit ef42fee348c85f9561f6fbbe16a60c71ea844980

Fix stuck offline update preparation after pushing
It was broken in 3d62b3dad346d0945b984137ccf3d7aecd6c4f69. UpdateManager should track a file status
instead of the Downloader signals, because the Downloader is not even used in the offline update
procedure.

vms/server/nx_vms_server/src/nx/vms/server/update/update_manager.cpp
@@ -66,8 +66,8 @@ void UpdateManager::connectToSignals()
         this, &UpdateManager::onDownloaderFailed, Qt::QueuedConnection);
 
     connect(
-        downloader(), &Downloader::downloadFinished,
-        this, &UpdateManager::onDownloaderFinished, Qt::QueuedConnection);
+        downloader(), &Downloader::fileStatusChanged,
+        this, &UpdateManager::onDownloaderFileStatusChanged, Qt::QueuedConnection);
 
     const discovery::Manager* discoveryManager =
         serverModule()->commonModule()->moduleDiscoveryManager();
@@ -243,16 +243,23 @@ void UpdateManager::onDownloaderFailed(const QString& fileName)
     downloader()->deleteFile(fileName);
 }
 
-void UpdateManager::onDownloaderFinished(const QString& fileName)
+void UpdateManager::onDownloaderFileStatusChanged(
+    const downloader::FileInformation& fileInformation)
 {
-    NX_DEBUG(this, "Download finished for file %1", fileName);
+    if (fileInformation.status != FileInformation::Status::downloaded)
+        return;
 
     if (!m_targetUpdateInfo.isValid())
         return;
 
     nx::update::Package package;
-    if (findPackage(&package) != update::FindPackageResult::ok || package.file != fileName)
+    if (findPackage(&package) != update::FindPackageResult::ok
+        || package.file != fileInformation.name)
+    {
         return;
+    }
+
+    NX_DEBUG(this, "Download finished for file %1", fileInformation.name);
 
     extract();
 }

vms/server/nx_vms_server/src/nx/vms/server/update/update_manager.h
@@ -47,7 +47,8 @@ public:
 private:
     void onGlobalUpdateSettingChanged();
     void onDownloaderFailed(const QString& fileName);
-    void onDownloaderFinished(const QString& fileName);
+    void onDownloaderFileStatusChanged(
+        const common::p2p::downloader::FileInformation& fileInformation);
     void processFoundEndpoint(const nx::vms::discovery::ModuleEndpoint& endpoint);
     update::FindPackageResult findPackage(
         nx::update::Package* outPackage,