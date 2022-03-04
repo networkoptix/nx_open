cbf338fe2e1bf017c4cc0b26523e015f0f728537
Dmitry Koshelev <dkoshelev@networkoptix.com>
2022-03-04 17:05:11 +0300

    VMS-30870: Add REST API functions for log archive
    Closes VMS-29616

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/log_archive_filter.h
@@ -0,0 +1,29 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include <QtCore/QString>
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/log/log.h>
+#include <nx/utils/uuid.h>
+#include <nx/vms/api/json/value_or_array.h>
+
+namespace nx::vms::api {
+
+struct NX_VMS_API LogArchiveFilter
+{
+    /**%apidoc Indicates if the archive includes (and how many) rotated files. */
+    std::optional<int> backupCount;
+
+    /**%apidoc:stringArray Logs included in the archive. */
+    std::optional<json::ValueOrArray<nx::log::LogName>> names;
+};
+#define LogArchiveFilter_Fields (backupCount)(names)
+QN_FUSION_DECLARE_FUNCTIONS(LogArchiveFilter, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(LogArchiveFilter, LogArchiveFilter_Fields)
+
+} // namespace nx::vms::api
