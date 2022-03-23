1033852fcf6b62ba563ffc1e9f880c48640e4513
Victor Breus <vbreus@networkoptix.com>
2022-03-23 14:26:24 +0000

    VMS-31485: Fix excessive 'Cannot create bitmap cursor' warnings if scene item displays info block
    (cherry picked from commit 9f0d7289e1f3e276f8853f3ef52ce698f798b81d)
    
    # Conflicts:
    #	open/vms/client/nx_vms_client_desktop/src/ui/graphics/items/controls/html_text_item.cpp

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/controls/html_text_item.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/controls/html_text_item.cpp
@@ -170,6 +170,12 @@ void QnHtmlTextItem::setHtml(const QString &html)
 void QnHtmlTextItem::setIcon(const QPixmap& icon)
 {
     Q_D(QnHtmlTextItem);
+    // No pixmap comparison here for the early return.
+    // Since QPixmap hasn't operator== defined, pixmaps will be implicitely converted to the
+    // QCursor instances to perform comparison. This is not what was expected in general and also
+    // may lead to a lot of warnings reported which even may cause significant performance drop.
+    if (d->icon.isNull() && icon.isNull())
+        return;
     d->icon = icon;
     d->updatePixmap();
 }
