0d262a8e79f4bcb83ecdc3338b01bf7bbcc7c4be
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2022-03-26 01:37:25 +0300

    VMS-36737: Simplify lit() macro for Qt6
    There seems to be a bug in MSVC when using out lit() macro whem the result variable is static.

--- libs/nx_utils/src/nx/utils/literal.h
+++ libs/nx_utils/src/nx/utils/literal.h
@@ -11,8 +11,7 @@
  */
 #define QN_USE_QT_STRING_LITERALS
 #ifdef QN_USE_QT_STRING_LITERALS
-namespace QnLitDetail { template<int N> void check_string_literal(const char (&)[N]) {} }
-#   define lit(s) (QnLitDetail::check_string_literal(s), QStringLiteral(s))
+#   define lit(s) QStringLiteral(s)
 #else
 #   define lit(s) QLatin1String(s)
 #endif
