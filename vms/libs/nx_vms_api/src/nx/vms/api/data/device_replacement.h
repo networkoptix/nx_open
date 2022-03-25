5ecf9021d8293f12c2cadb0829fee08deaf59c2b
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-03-25 18:26:16 +0000

    VMS-31513: Refactor camera replacement response. Introduce message severity level

--- vms/libs/nx_vms_api/src/nx/vms/api/data/device_replacement.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/device_replacement.h
@@ -36,21 +36,63 @@ struct NX_VMS_API DeviceReplacementRequest
 
 QN_FUSION_DECLARE_FUNCTIONS(DeviceReplacementRequest, (json), NX_VMS_API)
 
+struct NX_VMS_API DeviceReplacementInfo
+{
+    NX_REFLECTION_ENUM_CLASS_IN_CLASS(Level,
+        info,
+        warning,
+        critical,
+        error
+    );
+
+    QString name;
+
+    /**%apidoc Message severity level. */
+    Level level{};
+
+    /**%apidoc:stringArray */
+    std::set<QString> messages;
+};
+
 struct NX_VMS_API DeviceReplacementResponse
 {
-    /**%apidoc:object
-     * A list of warnings during camera replacement. Map key is the warning group name, map value
-     * is the warning message itself.
+    /**%apidoc
+     * A list of info/warning messages during camera replacement.
+     * Messages are grouped to sections with some severity level.
+     * It is allowed that some sections could contain empty message list.
+     * That means some block of data is transfered without any warnings.
      */
-    std::map<QString, std::set<QString>> report;
+    std::vector<DeviceReplacementInfo> report;
 
     /**%apidoc Indicates if it is possible to proceed with replacement. */
     bool compatible = false;
+
+    void info(const QString& section, const QString& message = QString());
+    void warning(const QString& section, const QString& message);
+    void error(const QString& section, const QString& message);
+    void critical(const QString& section, const QString& message);
+
+private:
+    /**
+     * Add new message to the section.
+     * Due to section contains messages with same severity level only the new message 
+     * with higher severity level will clear previous messages from the section.
+     * And vice versa: in case of new message has lower severity level than current level
+     * of the section then such message will be not added.
+     */
+    void add(
+        const QString& section,
+        DeviceReplacementInfo::Level level,
+        const QString& message);
+    std::vector<DeviceReplacementInfo>::iterator addSection(const QString& section);
 };
 
+#define DeviceReplacementInfo_Fields (name)(level)(messages)
 #define DeviceReplacementResponse_Fields (report)(compatible)
 
 QN_FUSION_DECLARE_FUNCTIONS(DeviceReplacementResponse, (json), NX_VMS_API)
+QN_FUSION_DECLARE_FUNCTIONS(DeviceReplacementInfo, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(DeviceReplacementInfo, DeviceReplacementInfo_Fields)
 NX_REFLECTION_INSTRUMENT(DeviceReplacementResponse, DeviceReplacementResponse_Fields)
 
 } // namespace nx::vms::api
