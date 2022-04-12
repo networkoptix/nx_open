9ade5d510de48474d9cb94d18fb3afcf81cdc7ec
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2022-04-13 02:29:44 +0300

    VMS-30359: Update stubs and add missing system libraries

---
+++ libs/libgl_stub/src/glx_dummy.c
@@ -0,0 +1,7 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+void glXChooseFBConfig() {}
+void glXChooseVisual() {}
+void glXGetConfig() {}
+void glXGetFBConfigAttrib() {}
+void glXGetVisualFromFBConfig() {}
