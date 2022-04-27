7559990d5553eb1b3397ddd8b1b2bae803e0e671
Jenkins <jenkins@networkoptix.com>
2022-04-27 03:52:28 +0300

    Updated translations (committed by Jenkins)
    Branch: mobile_22.1
    
    http://jenkins.stage.lan.hdw.mx/job/nightly.mobile_22_1.update-translation/3/

--- vms/client/nx_vms_client_core/translations/sv_SE/client_core.ts
+++ vms/client/nx_vms_client_core/translations/sv_SE/client_core.ts
@@ -203,47 +203,47 @@
     <name>nx::vms::client::core::CertificateWarning</name>
     <message>
       <source>Trust this server?</source>
-      <translation type="unfinished">Trust this server?</translation>
+      <translation>Lita på den här servern?</translation>
     </message>
     <message>
       <source>Cannot verify the identity of %1</source>
-      <translation type="unfinished">Cannot verify the identity of %1</translation>
+      <translation>Kan inte verifiera identiteten på %1</translation>
     </message>
     <message>
       <source>Someone may be impersonating %1 to steal your personal information.</source>
-      <translation type="unfinished">Someone may be impersonating %1 to steal your personal information.</translation>
+      <translation>Någon kanske utger sig för att vara %1 för att stjäla din personliga information.</translation>
     </message>
     <message>
       <source>System:</source>
-      <translation type="unfinished">System:</translation>
+      <translation>System:</translation>
     </message>
     <message>
       <source>Server:</source>
-      <translation type="unfinished">Server:</translation>
+      <translation>Server:</translation>
     </message>
     <message>
       <source>Server ID:</source>
-      <translation type="unfinished">Server ID:</translation>
+      <translation>Server ID:</translation>
     </message>
     <message>
       <source>Review the certificate&apos;s details to make sure you are connecting to the correct server.</source>
-      <translation type="unfinished">Review the certificate&apos;s details to make sure you are connecting to the correct server.</translation>
+      <translation>Granska certifikatets&apos;s detaljer för att se till att du ansluter till rätt server.</translation>
     </message>
     <message>
       <source>Do not connect to this server unless instructed by your VMS administrator.</source>
-      <translation type="unfinished">Do not connect to this server unless instructed by your VMS administrator.</translation>
+      <translation>Anslut inte till den här servern om du inte har instruerats av din VMS administratör.</translation>
     </message>
     <message>
       <source>Server certificate is invalid.</source>
-      <translation type="unfinished">Server certificate is invalid.</translation>
+      <translation>Serverns certifikat är ogiltigt.</translation>
     </message>
     <message>
       <source>You attempted to connect to: %1 but the Server presented a certificate that is unable to be automatically verified.</source>
-      <translation type="unfinished">You attempted to connect to: %1 but the Server presented a certificate that is unable to be automatically verified.</translation>
+      <translation>Du försökte ansluta till: %1 men servern presenterade ett certifikat som inte kan verifieras automatiskt.</translation>
     </message>
     <message>
       <source>You attempted to connect to: %1 but the Server&apos;s certificate has changed.</source>
-      <translation type="unfinished">You attempted to connect to: %1 but the Server&apos;s certificate has changed.</translation>
+      <translation>Du försökte ansluta till: %1 men Server&apos;:s certifikat har ändrats.</translation>
     </message>
   </context>
 </TS>
