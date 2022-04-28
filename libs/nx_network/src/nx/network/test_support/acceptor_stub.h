44655e6b0f78d2425224801f347ced39d5c2abdc
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2022-04-28 00:45:04 +0000

    VMS-31979: make nx::network::cloud::* unit tests not depend on cloud
    This will allow moving these tests to nx_network and out of `-DwithClouds=` cmake flag.
    
    (cherry-picked from commit 39ecdca58a01)

--- libs/nx_network/src/nx/network/test_support/acceptor_stub.h
+++ libs/nx_network/src/nx/network/test_support/acceptor_stub.h
@@ -4,6 +4,7 @@
 
 #include <deque>
 
+#include <nx/network/aio/repetitive_timer.h>
 #include <nx/network/cloud/cloud_abstract_connection_acceptor.h>
 #include <nx/utils/thread/mutex.h>
 #include <nx/utils/thread/sync_queue.h>
@@ -15,28 +16,35 @@ namespace test {
 class NX_NETWORK_API AcceptorStub:
     public cloud::AbstractConnectionAcceptor
 {
+    using base_type = cloud::AbstractConnectionAcceptor;
+
 public:
-    AcceptorStub();
+    AcceptorStub(
+        nx::utils::SyncQueue<std::unique_ptr<AbstractStreamSocket>>* readyConnections);
+
     ~AcceptorStub();
+
+    virtual void bindToAioThread(aio::AbstractAioThread* aioThread) override;
+    virtual void stopWhileInAioThread() override;
 
     virtual void acceptAsync(AcceptCompletionHandler handler) override;
     virtual void cancelIOSync() override;
     virtual std::unique_ptr<AbstractStreamSocket> getNextSocketIfAny() override;
 
-    void setRemovedAcceptorsQueue(
-        utils::SyncQueue<AcceptorStub*>* removedAcceptorsQueue);
+    void setRemovedAcceptorsQueue(utils::SyncQueue<AcceptorStub*>* removedAcceptorsQueue);
+
     bool isAsyncAcceptInProgress() const;
-    void addReadyConnection(std::unique_ptr<AbstractStreamSocket> connection);
 
     static std::atomic<int> instanceCount;
 
 private:
+    nx::utils::SyncQueue<std::unique_ptr<AbstractStreamSocket>>* m_readyConnections = nullptr;
     utils::SyncQueue<AcceptorStub*>* m_removedAcceptorsQueue;
-    std::deque<std::unique_ptr<AbstractStreamSocket>> m_acceptedConnections;
     AcceptCompletionHandler m_acceptHandler;
     mutable nx::Mutex m_mutex;
+    nx::network::aio::RepetitiveTimer m_repetitiveTimer;
 
-    void deliverConnection();
+    void deliverConnectionIfAvailable();
 };
 
 } // namespace test
