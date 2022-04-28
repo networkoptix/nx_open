2feef249736ba12a13393915a851ab71dd026d2d
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2022-04-28 00:45:04 +0000

    VMS-31979: make nx::network::cloud::* unit tests not depend on cloud
    This will allow moving these tests to nx_network and out of `-DwithClouds=` cmake flag.
    
    (cherry-picked from commit 39ecdca58a01)

--- libs/nx_network/src/nx/network/test_support/acceptor_stub.cpp
+++ libs/nx_network/src/nx/network/test_support/acceptor_stub.cpp
@@ -8,9 +8,14 @@ namespace test {
 
 std::atomic<int> AcceptorStub::instanceCount(0);
 
-AcceptorStub::AcceptorStub()
+AcceptorStub::AcceptorStub(
+    nx::utils::SyncQueue<std::unique_ptr<AbstractStreamSocket>>* readyConnections)
+    :
+    m_readyConnections(readyConnections)
 {
     ++instanceCount;
+
+    bindToAioThread(getAioThread());
 }
 
 AcceptorStub::~AcceptorStub()
@@ -19,9 +24,27 @@ AcceptorStub::~AcceptorStub()
     m_removedAcceptorsQueue->push(this);
 }
 
+void AcceptorStub::bindToAioThread(aio::AbstractAioThread* aioThread)
+{
+    base_type::bindToAioThread(aioThread);
+
+    m_repetitiveTimer.bindToAioThread(aioThread);
+}
+
+void AcceptorStub::stopWhileInAioThread()
+{
+    base_type::stopWhileInAioThread();
+
+    m_repetitiveTimer.pleaseStopSync();
+}
+
 void AcceptorStub::acceptAsync(AcceptCompletionHandler handler)
 {
     m_acceptHandler = std::move(handler);
+
+    m_repetitiveTimer.start(
+        std::chrono::milliseconds(10),
+        [this]() { deliverConnectionIfAvailable(); });
 }
 
 void AcceptorStub::cancelIOSync()
@@ -45,27 +68,15 @@ bool AcceptorStub::isAsyncAcceptInProgress() const
     return m_acceptHandler != nullptr;
 }
 
-void AcceptorStub::addReadyConnection(
-    std::unique_ptr<AbstractStreamSocket> connection)
+void AcceptorStub::deliverConnectionIfAvailable()
 {
-    NX_MUTEX_LOCKER lock(&m_mutex);
-    m_acceptedConnections.push_back(std::move(connection));
-    deliverConnection();
-}
+    std::optional<std::unique_ptr<AbstractStreamSocket>> connection =
+        m_readyConnections->pop(std::chrono::milliseconds::zero());
+    if (!connection)
+        return;
 
-void AcceptorStub::deliverConnection()
-{
-    auto connection = std::move(m_acceptedConnections.front());
-    m_acceptedConnections.pop_front();
-    decltype(m_acceptHandler) acceptHandler;
-    acceptHandler.swap(m_acceptHandler);
-
-    post(
-        [connection = std::move(connection),
-            acceptHandler = std::move(acceptHandler)]() mutable
-        {
-            acceptHandler(SystemError::noError, std::move(connection));
-        });
+    m_repetitiveTimer.cancelSync();
+    nx::utils::swapAndCall(m_acceptHandler, SystemError::noError, std::move(*connection));
 }
 
 } // namespace test
