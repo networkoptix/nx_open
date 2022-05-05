6a908a4638efa14467caa2d4381b1ae7522283c4
Sergei Ivanov <sivanov@networkoptix.com>
2022-05-05 14:53:50 +0300

    VMS-31864: Show ROI on items on scene by default

--- vms/client/nx_vms_client_desktop/src/ui/workbench/handlers/workbench_action_handler.h
+++ vms/client/nx_vms_client_desktop/src/ui/workbench/handlers/workbench_action_handler.h
@@ -109,7 +109,7 @@ protected:
         QnTimePeriod timelineSelection;
         nx::vms::client::core::MotionSelection motionSelection;
         QRectF analyticsSelection;
-        bool displayRoi = false;
+        bool displayRoi = true;
         bool displayAnalyticsObjects = false;
     };
 
