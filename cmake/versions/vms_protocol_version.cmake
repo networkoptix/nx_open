67df84c2880a503fc5ae9ec2289d3aad0f60278e
Alexander Kulikov <akulikov@networkoptix.com>
2022-05-16 20:51:19 +0000

    VMS-32026: Remove metadataStorageId from ServerAttributes

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 02) #< VMS-31885: Support server LocationID.
+set(_vmsInVersionProtocolNumber 03) #< VMS-32026: Remove metadataStorageId from MediaServerUserAttributesData.
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
