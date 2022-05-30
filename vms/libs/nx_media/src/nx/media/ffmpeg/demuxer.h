b5c9e04a311857048d2c3313cb77475b26084083
Leonid Busygin <lbusygin@networkoptix.com>
2022-05-30 18:45:39 +0300

    VMS-31793: Move ffmpeg io context to nx_media

---
+++ vms/libs/nx_media/src/nx/media/ffmpeg/demuxer.h
@@ -0,0 +1,25 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/streaming/audio_data_packet.h>
+#include <nx/streaming/video_data_packet.h>
+#include <utils/media/ffmpeg_helper.h>
+#include <nx/media/ffmpeg/io_context.h>
+
+namespace nx::media::ffmpeg {
+
+class Demuxer
+{
+public:
+    ~Demuxer();
+    bool open(IoContextPtr ioContext);
+    QnAbstractMediaDataPtr getNextData();
+    void close();
+
+private:
+    IoContextPtr m_ioContext;
+    AVFormatContext* m_formatContext = nullptr;
+};
+
+} // namespace nx::media::ffmpeg
