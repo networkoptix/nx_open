2b61da78bb3932e88d854b4966f2896b9b7d516f
Sergey Uvarov <suvarov@networkoptix.com>
2022-06-09 14:54:39 +0000

    VMS-29932: Server API: Add handlers to access and modify device advanced parameters

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/device_advanced_parameters_model.h
@@ -0,0 +1,36 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/uuid.h>
+#include <nx/vms/api/data/id_data.h>
+
+namespace nx::vms::api {
+
+struct NX_VMS_API DeviceAdvancedFilter
+{
+    /**%apidoc:string
+     * Device id (can be obtained from "id", "physicalId" or "logicalId" field via
+     * `GET /rest/v{1-}/devices`) or MAC address (not supported for certain cameras).
+     */
+    QnUuid deviceId;
+
+    /**%apidoc[opt] Parameter id to read. */
+    QString id;
+
+    const DeviceAdvancedFilter& getId() const { return *this; }
+    bool operator==(const DeviceAdvancedFilter&) const = default;
+    QString toString() const;
+};
+#define DeviceAdvancedFilter_Fields (deviceId)(id)
+QN_FUSION_DECLARE_FUNCTIONS(DeviceAdvancedFilter, (json), NX_VMS_API)
+
+struct NX_VMS_API AdvancedManifestFilter: IdData
+{
+    AdvancedManifestFilter() {}
+};
+#define AdvancedManifestFilter_Fields (id)
+QN_FUSION_DECLARE_FUNCTIONS(AdvancedManifestFilter, (json), NX_VMS_API)
+
+} // namespace nx::vms::api
