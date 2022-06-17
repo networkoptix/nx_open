db9c2f23bd83c03fd657fbec01ea7367c8d28db9
Evgeniy Karpov <ekarpov@networkoptix.com>
2022-06-18 00:57:25 +0700

    VMS-34469: Server: Add manifest for REST OpenAPI schemas

---
+++ build_utils/yaml2json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+import argparse
+import json
+import yaml
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="Input YAML file name")
+    parser.add_argument("output", type=str, help="Output JSON file name")
+
+    args = parser.parse_args()
+    with open(args.input, 'r') as input:
+        with open(args.output, 'w') as output:
+            json.dump(yaml.safe_load(input), output, indent=4)
+
+if __name__ == '__main__':
+    main()
