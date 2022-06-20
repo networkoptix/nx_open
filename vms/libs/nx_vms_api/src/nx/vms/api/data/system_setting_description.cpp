71c540d4fb48f545819cf3e3a609872dd7a81607
Evgeniy Karpov <ekarpov@networkoptix.com>
2022-06-20 20:32:37 +0700

    VMS-30278: Server API documentation: Use map in return values

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/system_setting_description.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "system_setting_description.h"
+
+#include <nx/fusion/model_functions.h>
+
+namespace nx::vms::api {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(SystemSettingDescription, (json), SystemSettingDescription_Fields)
+
+} // namespace nx::vms::api
