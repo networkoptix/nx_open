c366a89541b08f2816783c2c8296525adce548a9
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2022-07-05 14:58:20 -0700

    VMS-30359: Add zlib dependency on Windows

--- libs/nx_utils/src/nx/utils/zlib.h
+++ libs/nx_utils/src/nx/utils/zlib.h
@@ -2,10 +2,4 @@
 
 #pragma once
 
-#include <QtCore/private/qconfig_p.h>
-
-#if !defined(QT_FEATURE_system_zlib) || QT_FEATURE_system_zlib == -1
-    #include <QtZlib/zlib.h>
-#else
-    #include <zlib.h>
-#endif
+#include <zlib.h>
