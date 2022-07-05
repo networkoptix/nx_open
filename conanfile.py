54b1a6ba062e483483436591b7434ad7764c57d5
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2022-07-05 14:58:20 -0700

    VMS-30359: Add zlib dependency on Windows

--- conanfile.py
+++ conanfile.py
@@ -116,6 +116,7 @@ class NxOpenConan(ConanFile):
             self.requires("msvc-redist/14.29.30133" "#9f10aa59e4671ce0669d6181e6b0a269")
             self.requires("winsdk-redist/10.0.19041.0" "#acf550438d5c5d7c1be638c7ac9662e3")
             self.requires("icu-win/70.1" "#1c50009a7165b74563ba149c315737f1")
+            self.requires("zlib/1.2.11" "#64ef9c596106c733055d8551926b4f0c")
 
         if self.isArm32 or self.isArm64:
             self.requires("sse2neon/efcbd5" "#57aba3f11ce3af714a328146c08e5171")
