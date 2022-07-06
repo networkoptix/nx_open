95a89f25776aab189768af9f713918bce8b3f810
Sergei Ivanov <sivanov@networkoptix.com>
2022-07-06 20:19:32 +0300

    VMS-34719: Bump VMS version to 5.2

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 05) #< VMS-34488: Add ONVIF progress notifications
+set(_vmsInVersionProtocolNumber 00) #< VMS-34719: Bump VMS version to 5.2
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
