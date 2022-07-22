58f2962c20f05a8bad721d4b56f891f07f18900d
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-07-22 16:43:31 +0000

    VMS-34920: Merge status could stuck in 'Merge in progress' after api/mergeSystem call
    (cherry picked from commit a5a361b4eac96aeb501c9b9acb06af3dce684b0d)

--- vms/libs/nx_vms_common/src/nx/vms/common/system_settings.h
+++ vms/libs/nx_vms_common/src/nx/vms/common/system_settings.h
@@ -11,6 +11,7 @@
 
 #include <common/common_globals.h>
 #include <core/resource/resource_fwd.h>
+#include <nx/utils/move_only_func.h>
 #include <nx/utils/singleton.h>
 #include <nx/utils/thread/mutex.h>
 #include <nx/utils/url.h>
@@ -139,7 +140,8 @@ public:
     /**
      * Save all settings to database
      */
-    bool resynchronizeNowSync();
+    bool resynchronizeNowSync(nx::utils::MoveOnlyFunc<
+        bool(const QString& paramName, const QString& paramValue)> filter = nullptr);
 
     /**
     * Save modified settings to database
