fe34c06658d3671dc759d19daaf5b2b86793116b
Leonid Busygin <lbusygin@networkoptix.com>
2022-08-02 14:46:09 +0300

    VMS-31793: Nvidia decoder: Implement screen shot interface
    (cherry picked from commit f7e73e0e5b35abe95d3f7aafeae4f8c7c0863552)

--- vms/libs/nx_media/src/nx/media/nvidia/nvidia_video_frame.cpp
+++ vms/libs/nx_media/src/nx/media/nvidia/nvidia_video_frame.cpp
@@ -2,18 +2,44 @@
 
 #include "nvidia_video_frame.h"
 
+#include <cuda.h>
+#include <cuda_runtime.h>
+
 #include <nx/media/nvidia/nvidia_video_decoder.h>
+#include <nx/utils/log/log.h>
 
 namespace nx::media::nvidia {
 
 AVFrame NvidiaVideoFrame::lockFrame()
 {
     AVFrame result;
+    memset(&result, 0, sizeof(AVFrame));
+    auto decoderLock = decoder.lock();
+    if (!decoderLock)
+    {
+        NX_DEBUG(this, "Nvidia decoder already deleted, failed to copy frame to system memory");
+        return result;
+    }
+    systemMemory.resize(bufferSize);
+    cudaMemcpy(systemMemory.data(), frameData, bufferSize, cudaMemcpyDeviceToHost);
+
+    result.width = width;
+    result.height = height;
+    result.linesize[0] = pitch;
+    result.linesize[1] = pitch;
+    result.linesize[2] = 0;
+    result.linesize[3] = 0;
+    result.data[0] = systemMemory.data();
+    result.data[1] = systemMemory.data() + pitch * height;
+    result.data[2] = nullptr;
+    result.data[3] = nullptr;
+    result.format = AV_PIX_FMT_NV12;
     return result;
 }
 
 void NvidiaVideoFrame::unlockFrame()
 {
+    systemMemory.clear();
 }
 
 NvidiaVideoFrame::~NvidiaVideoFrame()
