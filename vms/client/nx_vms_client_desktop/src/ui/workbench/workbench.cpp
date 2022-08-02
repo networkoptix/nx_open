e7931b03c9b52de91f628c98192ccd02014c197e
Anton Ivashchenko <aivashchenko@networkoptix.com>
2022-08-02 19:22:40 +0300

    VMS-35032: Fix storing state on client window closing
    Closes VMS-35032
    
    It is necessary to store client state before all layouts will be deleted from workbench.
    
    (cherry picked from commit 6e6726c7e14d54067a66c97fbf5ad377bfce5c81)

--- vms/client/nx_vms_client_desktop/src/ui/workbench/workbench.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/workbench/workbench.cpp
@@ -667,6 +667,7 @@ void QnWorkbench::submit(QnWorkbenchState& state)
             // Ignore other service layouts, e.g. videowall control layouts.
             if (layout->hasFlags(Qn::local) || layout->isServiceLayout())
                 return false;
+
             return true;
         };
 
