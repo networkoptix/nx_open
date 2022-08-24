f5104bb1d7007a70874f60e7a8ee7696dd35deb5
Evgenij Sobolev <esobolev@networkoptix.com>
2022-08-24 19:01:37 +0300

    VMS-34534: Add filter for CSN
    Fixes VMS-34534.
    
    The notifications tab is not uniform with the other tabs (the specifics of the notifications themselves). Because for uniformity, we need to redo a lot in the other tabs.

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/widgets/private/notification_list_widget_p.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/widgets/private/notification_list_widget_p.h
@@ -4,17 +4,22 @@
 
 #include "../notification_list_widget.h"
 
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QTabWidget>
+#include <QtWidgets/QVBoxLayout>
 
-#include <ui/workbench/workbench_context_aware.h>
+#include <nx/vms/client/desktop/event_search/right_panel_globals.h>
 #include <nx/vms/client/desktop/ui/actions/actions.h>
+#include <ui/workbench/workbench_context_aware.h>
 
+class QSortFilterProxyModel;
 class QToolButton;
 
 namespace nx::vms::client::desktop {
 
 class EventRibbon;
 class NotificationTabModel;
+class SelectableTextButton;
 
 class NotificationListWidget::Private:
     public QObject,
@@ -27,10 +32,20 @@ public:
     virtual ~Private() override;
 
 private:
+    void setupFilterSystemsButton();
+
+private:
     NotificationListWidget* const q;
+    QVBoxLayout* const m_mainLayout;
+    QWidget* const m_headerWidget;
+    QLabel* const m_itemCounterLabel;
+    QWidget* const m_ribbonContainer;
+    SelectableTextButton* const m_filterSystemsButton;
     EventRibbon* const m_eventRibbon;
     QWidget* const m_placeholder;
     NotificationTabModel* const m_model;
+    QSortFilterProxyModel* const m_filterModel;
+    QHash<RightPanel::SystemSelection, QAction*> m_systemSelectionActions;
 };
 
 } // namespace nx::vms::client::desktop
