65b48c2310ad0b457574376cd367e26c05a209d7
Mikhail Shevchenko <mshevchenko@networkoptix.com>
2022-08-31 00:23:32 +0300

    VMS-34985: ARM-specific behavior: Introduce Server setting resolveLocalhost
    The new setting instructs nx_network to manually resolve the localhost address, as was previously
    hard-coded for the `edge1` platform. This is a workaround for operating systems where /etc/hosts
    does not work, thus localhost is inaccessible, like certain Busybox-based systems.
    
    The Server behavior did not change because this option with the value "true" was added to
    mediaserver.conf for the edge1 platform.

--- libs/nx_network/src/nx/network/resolve/system_resolver.h
+++ libs/nx_network/src/nx/network/resolve/system_resolver.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <atomic>
+
 #include "abstract_resolver.h"
 
 namespace nx::network {
@@ -13,12 +15,20 @@ class NX_NETWORK_API SystemResolver:
     public AbstractResolver
 {
 public:
+    /**
+     * This is a workaround for operating systems where /etc/hosts does not work, thus localhost is
+     * inaccessible, like certain Busybox-based systems.
+     */
+    static void setResolveLocalhost() { s_resolveLocalhost = true; }
+
     virtual SystemError::ErrorCode resolve(
         const std::string_view& hostName,
         int ipVersion,
         ResolveResult* resolveResult) override;
 
 private:
+    static std::atomic<bool> s_resolveLocalhost /*= false*/;
+
     SystemError::ErrorCode resolveStatusToErrno(int status);
 
     void ensureLocalHostCompatibility(
