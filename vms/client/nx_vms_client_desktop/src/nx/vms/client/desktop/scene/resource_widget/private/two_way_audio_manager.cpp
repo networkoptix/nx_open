5472ac985c965c9eeecd9380e2de4d883576f149
Dmitry Fisenko <dfisenko@networkoptix.com>
2022-08-30 17:02:38 +0000

    VMS-35000: Implement intercom media resource widget buttons

---
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/scene/resource_widget/private/two_way_audio_manager.cpp
@@ -0,0 +1,94 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "two_way_audio_manager.h"
+
+#include <client/client_settings.h>
+#include <client_core/client_core_module.h>
+#include <core/resource/camera_resource.h>
+#include <core/resource/media_server_resource.h>
+#include <core/resource_management/resource_pool.h>
+#include <nx/audio/audiodevice.h>
+#include <nx/vms/client/core/network/network_module.h>
+#include <nx/vms/client/core/network/remote_connection.h>
+#include <nx/vms/client/core/network/remote_session.h>
+
+namespace nx::vms::client::desktop {
+
+TwoWayAudioManager::TwoWayAudioManager(
+    const QString& sourceId,
+    const QnSecurityCamResourcePtr& camera,
+    QObject* parent)
+    :
+    QObject(parent),
+    m_camera(camera)
+{
+    m_controller.setSourceId(sourceId);
+    m_controller.setResourceId(m_camera->getId());
+
+    connect(&m_controller, &core::TwoWayAudioController::startedChanged,
+        [this]
+        {
+            emit streamingStateChanged(m_controller.started()
+                ? StreamingState::active
+                : StreamingState::disabled);
+        });
+}
+
+TwoWayAudioManager::~TwoWayAudioManager()
+{
+    stopStreaming();
+}
+
+void TwoWayAudioManager::startStreaming()
+{
+    if (!m_controller.available() || !m_camera || m_controller.started())
+        return;
+
+    const auto currentSession = qnClientCoreModule->networkModule()->session();
+    if (!currentSession)
+        return;
+
+    const auto server = m_camera->resourcePool()->getResourceById<QnMediaServerResource>(
+        currentSession->connection()->moduleInformation().id);
+
+    if (!server || server->getStatus() != nx::vms::api::ResourceStatus::online)
+        return;
+
+    const auto requestCallback =
+        [this](bool success)
+        {
+            if (success)
+            {
+                if (qnSettings->muteOnAudioTransmit()
+                    && !nx::audio::AudioDevice::instance()->isMute())
+                {
+                    m_unmuteAudioOnStreamingStop = true;
+                }
+            }
+            else
+            {
+                NX_WARNING(this, "Streaming is not ready yet");
+                stopStreaming();
+                streamingStateChanged(StreamingState::error);
+            }
+        };
+
+    if (!m_controller.start(requestCallback))
+    {
+        NX_WARNING(this, "Network error");
+        streamingStateChanged(StreamingState::error);
+    }
+}
+
+void TwoWayAudioManager::stopStreaming()
+{
+    if (!m_controller.available() || !m_controller.started())
+        return;
+
+    if (m_unmuteAudioOnStreamingStop)
+        m_unmuteAudioOnStreamingStop = false;
+
+    m_controller.stop();
+}
+
+} // namespace nx::vms::client::desktop
