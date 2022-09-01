ebdf97f40f8db4d494111f0b0b2a56f2038e8a12
Sergey Uvarov <suvarov@networkoptix.com>
2022-09-01 13:53:41 +0300

    VMS-35142: Raise VMS protocol version
    (cherry picked from commit 05f98dc3b7bc4b36445c1a9eea37ddf78be777fa)
    
    # Conflicts:
    #	open/cmake/versions/vms_protocol_version.cmake

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 05) #< VMS-34488: Add ONVIF progress notifications
+set(_vmsInVersionProtocolNumber 06) #< VMS-35142: Remove some not important data from public API
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
