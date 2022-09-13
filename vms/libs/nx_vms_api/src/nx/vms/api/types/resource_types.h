016c2fd2d4101751925b84f2d5ed538176eef3b7
Sergei Ivanov <sivanov@networkoptix.com>
2022-09-13 21:07:29 +0300

    VMS-35764: Fix desktop client json compatibility

--- vms/libs/nx_vms_api/src/nx/vms/api/types/resource_types.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/types/resource_types.h
@@ -241,7 +241,8 @@ NX_REFLECTION_ENUM(ServerFlag,
 
     SF_ArmServer = 0x040,
 
-    //SF_Has_HDD = 0x080, //< Removed in 5.1, was proprietary.
+    // Value cannot be simply removed from here as it causes json compatibility to break.
+    SF_Has_HDD = 0x080, //< Removed in 5.1, was proprietary.
 
     /**
      * System is just installed, it has default admin password and is not linked to the cloud.
@@ -252,7 +253,9 @@ NX_REFLECTION_ENUM(ServerFlag,
 
     SF_SupportsTranscoding = 0x200,
     SF_P2pSyncDone = 0x1000000, //< For UT purpose only
-    //SF_RequiresEdgeLicense = 0x2000000, //< Removed in 5.1 - Edge-only licenses are gone.
+
+    // Value cannot be simply removed from here as it causes json compatibility to break.
+    SF_RequiresEdgeLicense = 0x2000000, //< Removed in 5.1 - Edge-only licenses are gone.
 
     // Server can provide information about built-in PoE block.
     SF_HasPoeManagementCapability = 0x4000000,
