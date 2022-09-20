dc19f4c735b7dd8990da77ce790bb0037f430016
Mark Altera <maltera@networkoptix.com>
2022-09-20 13:25:01 +0300

    VMS-35362: Fix Webpage minimal interface mode
    (cherry picked from commit 02c0cfe5512f730200e03a619b96d30ac3ff0e88)

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/web_resource_widget.h
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/web_resource_widget.h
@@ -42,7 +42,6 @@ private:
     void initClientApiSupport();
     void setupOverlays();
     void setupWidget();
-    void updateInterfaceVisibility();
     bool verifyCertificate(const QString& pemString, const QUrl& url);
     bool askUserToAcceptCertificate(
         const nx::network::ssl::CertificateChain& chain, const nx::utils::Url& url);
