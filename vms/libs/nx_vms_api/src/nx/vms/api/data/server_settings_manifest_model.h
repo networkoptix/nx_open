bf8ad7547a708ea5b32297f3bbd793d874f41b44
Sergey Uvarov <suvarov@networkoptix.com>
2022-09-20 06:27:42 +0000

    VMS-35174: Implement rest/v2/servers/{id}/settingsManifest

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/server_settings_manifest_model.h
@@ -0,0 +1,23 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/settings.h>
+#include <nx/vms/api/data/map.h>
+
+namespace nx::utils {
+
+QN_FUSION_DECLARE_FUNCTIONS(SettingInfo, (json), NX_VMS_API)
+
+} // nx::utils
+
+namespace nx::vms::api {
+
+struct SettingsDescription: public std::map<QString, nx::utils::SettingInfo>
+{
+};
+
+using ServerSettingsManifestValues = Map<QnUuid, SettingsDescription>;
+
+} // nx::vms::api
