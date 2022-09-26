8ece6ee605da1c418e233635f4320b6702830c7d
Pavel Tolstov <ptolstov@networkoptix.com>
2022-09-26 16:03:08 +0000

    VMS-35876: ninja_tool: Increase timeout for parsing ninja dependencies and improve diagnostics
    (cherry picked from commit 1fc92acd379eb6611b6dc29ed3d47e4f1b64f31a)

--- build_utils/ninja/ninja_launcher.bat
+++ build_utils/ninja/ninja_launcher.bat
@@ -3,10 +3,11 @@
 @echo off
 
 if not "%DISABLE_NINJA_TOOL%"=="" GOTO SKIP_NINJA_TOOL_RUN
-python "%~dp0\ninja_tool.py" --log-output --stack-trace
+SET LOG_FILE_NAME=%CD%\build_logs\pre_build.log
+python "%~dp0\ninja_tool.py" --log-output "%LOG_FILE_NAME%" --stack-trace
 if %ERRORLEVEL% GTR 0 (
     echo Ninja tool exited with error.
-    echo For more details, see <build-dir>\build_logs\pre_build.log
+    echo For more details, see %LOG_FILE_NAME%
     exit 1
 )
 :SKIP_NINJA_TOOL_RUN
