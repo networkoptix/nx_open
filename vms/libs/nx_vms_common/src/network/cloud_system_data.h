7acd973d91284a2278eaaae14df24338b3d93898
Sergei Ivanov <sivanov@networkoptix.com>
2022-09-28 17:47:48 +0300

    VMS-35929: Display online cloud systems as available in the desktop client
    (cherry picked from commit 5004e6b7adfe6d4b7b1d0d82a0ec484ad8d9fabd)

--- vms/libs/nx_vms_common/src/network/cloud_system_data.h
+++ vms/libs/nx_vms_common/src/network/cloud_system_data.h
@@ -21,14 +21,17 @@ struct NX_VMS_COMMON_API QnCloudSystem
     bool system2faEnabled = false;
     std::string newestServerVersion;
 
+    /** The VMS version reported by the last connected VMS server. */
+    QString version;
+
     bool operator==(const QnCloudSystem &other) const;
 
     bool visuallyEqual(const QnCloudSystem& other) const;
 };
 
 #define QnCloudSystem_Fields (cloudId)(localId)(name)(ownerAccountEmail)(ownerFullName)(weight) \
-    (lastLoginTimeUtcMs)(authKey)
-QN_FUSION_DECLARE_FUNCTIONS(QnCloudSystem, (json)(metatype)(json), NX_VMS_COMMON_API)
+    (lastLoginTimeUtcMs)(authKey)(online)(system2faEnabled)(version)
+QN_FUSION_DECLARE_FUNCTIONS(QnCloudSystem, (metatype)(json), NX_VMS_COMMON_API)
 
 typedef QList<QnCloudSystem> QnCloudSystemList;
 Q_DECLARE_METATYPE(QnCloudSystemList);
