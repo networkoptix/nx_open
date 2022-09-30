70c09857d6f0361ca0bb897d25941c5f1cdffa66
Evgeniy Karpov <ekarpov@networkoptix.com>
2022-09-30 11:05:56 +0000

    VMS-35930: Add Let's Encrypt "ISRG Root X1" to Server trusted certificates
    (cherry picked from commit ffc7ec7e2089a0f1ea07382f173bbb72f5a521f1)

--- vms/libs/nx_vms_common/src/nx/vms/common/network/abstract_certificate_verifier.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/common/network/abstract_certificate_verifier.cpp
@@ -2,9 +2,66 @@
 
 #include "abstract_certificate_verifier.h"
 
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtNetwork/QSslConfiguration>
+
+#include <nx/network/ssl/certificate.h>
+
 namespace nx::vms::common {
 
-AbstractCertificateVerifier::AbstractCertificateVerifier(QObject* parent): QObject(parent) {}
+AbstractCertificateVerifier::AbstractCertificateVerifier(QObject* parent): QObject(parent)
+{
+    constexpr auto kCaLogLevel = nx::utils::log::Level::verbose;
+    if (nx::utils::log::isToBeLogged(kCaLogLevel))
+    {
+        NX_UTILS_LOG(kCaLogLevel, this, "OS CA for now %1", nx::utils::utcTime());
+        for (const auto& c: QSslConfiguration::defaultConfiguration().caCertificates())
+            NX_UTILS_LOG(kCaLogLevel, this, c);
+    }
+    const QDir folder(":/trusted_certificates");
+    for (const auto& fileName: folder.entryList({"*.pem"}, QDir::Files))
+    {
+        QFile f(folder.absoluteFilePath(fileName));
+        if (!f.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            NX_WARNING(this, "Cannot open file %1", folder.absoluteFilePath(fileName));
+            continue;
+        }
+        loadTrustedCertificate(f.readAll(), fileName);
+    }
+}
+
 AbstractCertificateVerifier::~AbstractCertificateVerifier() {}
+
+void AbstractCertificateVerifier::loadTrustedCertificate(
+    const QByteArray& data, const QString& name)
+{
+    const auto chain = nx::network::ssl::Certificate::parse(data.toStdString(),
+        /*assertOnFail*/ false);
+    if (chain.size() != 1)
+    {
+        NX_VERBOSE(this, "The file does not contain a single certificate: %1", name);
+        return;
+    }
+
+    const auto& cert = chain[0];
+    if (cert.issuer() != cert.subject())
+    {
+        NX_VERBOSE(this, "Certificate is not self-signed: %1", name);
+        return;
+    }
+
+    const auto now = nx::utils::utcTime();
+    if (now >= cert.notBefore() && now <= cert.notAfter())
+    {
+        NX_VERBOSE(this, "Adding certificate into the trusted CA list: %1", name);
+        nx::network::ssl::addTrustedRootCertificate(cert);
+    }
+    else
+    {
+        NX_VERBOSE(this, "Ignore expired certificate: %1", name);
+    }
+}
 
 } // nx::vms::common
