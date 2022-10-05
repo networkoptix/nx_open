4826c76bbb0238b03b1bcb207c9a59d3220bb31c
Sergei Ivanov <sivanov@networkoptix.com>
2022-10-05 20:23:34 +0300

    Raise VMS protocol version

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 03) #< VMS-34488: Add ONVIF progress notifications
+set(_vmsInVersionProtocolNumber 05) #< VMS-35038: Refactor min/max archive days in API.
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
