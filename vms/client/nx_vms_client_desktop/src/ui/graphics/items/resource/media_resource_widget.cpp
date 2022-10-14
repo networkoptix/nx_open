5b821541e49798c2bdd83bb70d372190905602cf
Maksim Malofeev <mmalofeev@networkoptix.com>
2022-10-14 14:53:46 +0300

    VMS-36145: Hide objects search button for any resource on the cross system layout

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
@@ -85,6 +85,7 @@
 #include <nx/vms/client/desktop/ui/graphics/items/resource/widget_analytics_controller.h>
 #include <nx/vms/client/desktop/watermark/watermark_painter.h>
 #include <nx/vms/client/desktop/window_context.h>
+#include <nx/vms/client/desktop/workbench/workbench.h>
 #include <nx/vms/common/html/html.h>
 #include <nx/vms/common/intercom/utils.h>
 #include <nx/vms/common/system_context.h>
@@ -2306,7 +2307,7 @@ int QnMediaResourceWidget::calculateButtonsVisibility() const
 
     if (d->hasVideo
         && d->camera
-        && !d->camera->hasFlags(Qn::cross_system)
+        && !workbench()->currentLayout()->resource()->hasFlags(Qn::cross_system)
         && d->taxonomyManager
         && accessController()->hasGlobalPermission(GlobalPermission::viewArchive)
         && !d->taxonomyManager->relevantEngines().empty()
