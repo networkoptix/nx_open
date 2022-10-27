9fe3e3c311a53b7255bf135d18f7d73820065760
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-10-27 16:30:57 +0300

    VMS-36376: Disable printStackTrace on Assert in Release by default
    (cherry picked from commit 054f0a242efc0fdc10adb2f61c6fb0e166f7f847)

--- libs/nx_utils/src/nx/utils/nx_utils_ini.h
+++ libs/nx_utils/src/nx/utils/nx_utils_ini.h
@@ -11,14 +11,15 @@ struct NX_UTILS_API Ini: nx::kit::IniConfig
     Ini(): IniConfig("nx_utils.ini") { reload(); }
 
     static constexpr int kShowPasswordsInLogs = 0;
-    static constexpr int kDefaultPrintStackTraceOnAssert = 1;
     #if defined(_DEBUG)
+        static constexpr int kDefaultPrintStackTraceOnAssert = 1;
         static constexpr const char* kDefaultMutexImplementation = "debug";
         static constexpr int kDefaultAssertCrash = 1;
         static constexpr int kDefaultAssertHeavyCondition = 1;
         static constexpr int kLogLevelReducerPassLimit = 100;
         static constexpr int kLogLevelReducerWindowSizeS = 20;
     #else
+        static constexpr int kDefaultPrintStackTraceOnAssert = 0;
         static constexpr const char* kDefaultMutexImplementation = "qt";
         static constexpr int kDefaultAssertCrash = 0;
         static constexpr int kDefaultAssertHeavyCondition = 0;
