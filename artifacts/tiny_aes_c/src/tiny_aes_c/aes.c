466da153b2d7150225eaee0d369c30fb5e4606d8
Mikhail Uskov <muskov@networkoptix.com>
2022-10-28 20:05:19 +0300

    VMS-36414: Fix warning in tiny_aes_c artifact

--- artifacts/tiny_aes_c/src/tiny_aes_c/aes.c
+++ artifacts/tiny_aes_c/src/tiny_aes_c/aes.c
@@ -421,7 +421,7 @@ static void InvCipher(void)
     AddRoundKey(0);
 }
 
-static void BlockCopy(uint8_t* output, uint8_t* input)
+static void BlockCopy(uint8_t* output, const uint8_t* input)
 {
     uint8_t i;
     for (i = 0; i < KEYLEN; ++i)
