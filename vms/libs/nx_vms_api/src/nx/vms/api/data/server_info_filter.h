18928fa0225cd07a6f6698978636ccf76aa39bd8
Dmitry Koshelev <dkoshelev@networkoptix.com>
2022-11-16 17:18:27 +0300

    VMS-36665: Support offline Servers in Server Info endpoint
    (cherry picked from commit 51af81f6d8b9edbefa9c4024a6e7656cfd5f51b7)

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/server_info_filter.h
@@ -0,0 +1,27 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/uuid.h>
+
+namespace nx::vms::api {
+
+struct NX_VMS_API ServerInfoFilter
+{
+    /**%apidoc Server id. Can be obtained from "id" field via `GET /rest/v{1-}/servers`,
+     *     or be "this" to refer to the current Server.
+     *     %example this
+     */
+    QnUuid id;
+
+    /**%apidoc[opt] Flag to filter out Server Info from offline Servers. Use this flag
+     *     together with "_local" flag to control request propagation and response details.
+     */
+    bool onlyFreshInfo = true;
+};
+#define ServerInfoFilter_Fields (id)(onlyFreshInfo)
+QN_FUSION_DECLARE_FUNCTIONS(ServerInfoFilter, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(ServerInfoFilter, ServerInfoFilter_Fields)
+
+} // namespace nx::vms::api
