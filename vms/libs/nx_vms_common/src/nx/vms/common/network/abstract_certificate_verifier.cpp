52e8b6abb31c2a2db6e26160cb52a53ea0371ea1
Evgeniy Karpov <ekarpov@networkoptix.com>
2022-11-16 13:53:51 +0000

    VMS-36479: Server: Add Amazon Root CA certificates to trusted ones by conf file option
    (cherry picked from commit eb7c18d13835588eccdf0da2e9adc53f34ea75e1)

--- vms/libs/nx_vms_common/src/nx/vms/common/network/abstract_certificate_verifier.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/common/network/abstract_certificate_verifier.cpp
@@ -12,11 +12,16 @@ namespace nx::vms::common {
 
 static std::once_flag trustedCertificatesLoaded;
 
-AbstractCertificateVerifier::AbstractCertificateVerifier(QObject* parent): QObject(parent)
+AbstractCertificateVerifier::AbstractCertificateVerifier(
+    const std::string& trustedCertificateFilterRegex,
+    QObject* parent)
+    :
+    QObject(parent)
 {
     std::call_once(trustedCertificatesLoaded,
-        [this]()
+        [this, trustedCertificateFilterRegex]()
         {
+            const std::regex regex(trustedCertificateFilterRegex);
             constexpr auto kCaLogLevel = nx::utils::log::Level::verbose;
             if (nx::utils::log::isToBeLogged(kCaLogLevel))
             {
@@ -25,8 +30,17 @@ AbstractCertificateVerifier::AbstractCertificateVerifier(QObject* parent): QObje
                     NX_UTILS_LOG(kCaLogLevel, this, c);
             }
             const QDir folder(":/trusted_certificates");
-            for (const auto& fileName: folder.entryList({"*.pem"}, QDir::Files))
+            for (const auto& fileInfo: folder.entryInfoList({"*.pem"}, QDir::Files))
             {
+                const QString fileName = fileInfo.fileName();
+                if (!std::regex_match(fileInfo.baseName().toStdString(), regex))
+                {
+                    NX_INFO(this,
+                        "Ignore trusted certificate %1 which does not match the filter %2",
+                        fileName, trustedCertificateFilterRegex);
+                    continue;
+                }
+
                 QFile f(folder.absoluteFilePath(fileName));
                 if (!f.open(QIODevice::ReadOnly | QIODevice::Text))
                 {
@@ -47,26 +61,27 @@ void AbstractCertificateVerifier::loadTrustedCertificate(
         /*assertOnFail*/ false);
     if (chain.size() != 1)
     {
-        NX_VERBOSE(this, "The file does not contain a single certificate: %1", name);
+        NX_WARNING(this,
+            "Ignore trusted certificate %1 with more that one certificate", name);
         return;
     }
 
     const auto& cert = chain[0];
     if (cert.issuer() != cert.subject())
     {
-        NX_VERBOSE(this, "Certificate is not self-signed: %1", name);
+        NX_WARNING(this, "Ignore not self-signed trusted certificate %1", name);
         return;
     }
 
     const auto now = nx::utils::utcTime();
     if (now >= cert.notBefore() && now <= cert.notAfter())
     {
-        NX_VERBOSE(this, "Adding certificate into the trusted CA list: %1", name);
+        NX_INFO(this, "Adding trusted certificate %1 into the CA list", name);
         nx::network::ssl::addTrustedRootCertificate(cert);
     }
     else
     {
-        NX_VERBOSE(this, "Ignore expired certificate: %1", name);
+        NX_WARNING(this, "Ignore expired trusted certificate %1", name);
     }
 }
 
