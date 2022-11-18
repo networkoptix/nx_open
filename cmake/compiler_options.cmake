5b5e7c599605bb3cf9b1f1d0004f0fb5d99461e3
Pavel Tolstov <ptolstov@networkoptix.com>
2022-11-18 18:20:28 +0300

    VMS-36250: Minimize debug info size in Release builds; do not strip Server on linux_x64
    Since the debug information has reasonable size now, we do not strip it by default from
    the server distribution packages anymore.
    
    (cherry picked from commit b38335f36fae7a7365b9f2a37085d7ff65b4e66e)

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -27,6 +27,8 @@ endfunction()
 
 if(LINUX AND NOT ANDROID)
     option(useLdGold "Use ld.gold to link binaries" OFF)
+    option(verboseDebugInfoInRelease
+        "Add more debug info when building Release version; effective only when -DuseClang=ON" OFF)
 endif()
 
 if(CMAKE_BUILD_TYPE MATCHES "Release|RelWithDebInfo")
@@ -349,7 +351,15 @@ if(LINUX)
     string(APPEND CMAKE_SHARED_LINKER_FLAGS ${link_flags})
 
     if(NOT ANDROID AND CMAKE_BUILD_TYPE STREQUAL "Release")
-        nx_add_c_cxx_compile_options(-ggdb1 -fno-omit-frame-pointer)
+        if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT verboseDebugInfoInRelease)
+            set(debug_level_options "-gline-directives-only")
+        else()
+            set(debug_level_options "-ggdb1")
+        endif()
+
+        nx_add_c_cxx_compile_options(-fno-omit-frame-pointer ${debug_level_options})
+        string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--compress-debug-sections=zlib")
+        string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,--compress-debug-sections=zlib")
     endif()
 endif()
 
@@ -424,5 +434,10 @@ if(${colorOutput})
     endif()
 endif()
 
-option(stripBinaries "Strip the resulting binaries" ${_stripBinaries})
-unset(_stripBinaries)
+if(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo" OR targetDevice STREQUAL "edge1")
+    set(_stripBinaries ON)
+else()
+    set(_stripBinaries OFF)
+endif()
+
+nx_option(stripBinaries "Strip the resulting binaries" ${_stripBinaries})
