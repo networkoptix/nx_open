869d7c9552dc0f648581379c2b0613c2bcaa5899
Anton Malov <amalov@networkoptix.com>
2022-11-23 14:37:40 +0300

    VMS-36769: Add event connector test
    (cherry picked from commit 1c4fe732c6bf48663d8d2ed8cfd8df5f934089b5)

---
+++ vms/libs/nx_vms_rules/unit_tests/src/nx/vms/rules/event_connector_ut.cpp
@@ -0,0 +1,51 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include <gtest/gtest.h>
+
+#include <future>
+
+#include <nx/vms/rules/engine.h>
+#include <nx/vms/rules/event_connector.h>
+
+#include "test_event.h"
+#include "test_router.h"
+
+namespace nx::vms::rules::test {
+
+class TestEventConnector: public nx::vms::rules::EventConnector
+{
+public:
+    void sendEventBatch(std::vector<nx::vms::rules::EventPtr> events)
+    {
+        emit analyticsEvents(events);
+    }
+
+    void sendEvent(nx::vms::rules::EventPtr event)
+    {
+        emit this->event(event);
+    }
+};
+
+TEST(EventConnector, eventSignals)
+{
+   auto engine = std::make_unique<Engine>(std::make_unique<TestRouter>());
+   engine->registerEvent(TestEvent::manifest(), [] { return new TestEvent(); });
+
+   auto connector = std::make_unique<TestEventConnector>();
+   engine->addEventConnector(connector.get());
+
+   // Test if all event types are properly registered to send signals between threads.
+
+   auto future = std::async(std::launch::async,
+       [&connector] {
+           std::vector<nx::vms::rules::EventPtr> events;
+           events.emplace_back(new TestEvent());
+           connector->sendEventBatch(events);
+
+           connector->sendEvent(EventPtr(new TestEvent()));
+       });
+
+   future.wait();
+}
+
+} // namespace nx::vms::rules::test
