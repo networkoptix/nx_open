25916efbf993c3aa3e5ca33ab3c48b21bcc3d7c7
Victor Breus <vbreus@networkoptix.com>
2022-12-01 12:04:39 +0300

    VMS-35726: Update hotspots on the scene item according to the resource pool state and resource access rights

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/graphics/items/overlays/camera_hotspots_overlay_widget.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/graphics/items/overlays/camera_hotspots_overlay_widget.h
@@ -2,12 +2,10 @@
 
 #pragma once
 
-#include <core/resource/resource_fwd.h>
 #include <nx/utils/impl_ptr.h>
-#include <nx/vms/common/resource/camera_hotspots_data.h>
 #include <ui/graphics/items/generic/viewport_bound_widget.h>
 
-class QnWorkbenchContext;
+class QnMediaResourceWidget;
 
 namespace nx::vms::client::desktop {
 
@@ -16,13 +14,8 @@ class CameraHotspotsOverlayWidget: public QnViewportBoundWidget
     using base_type = QnViewportBoundWidget;
 
 public:
-    CameraHotspotsOverlayWidget(QGraphicsItem* parent = nullptr);
+    CameraHotspotsOverlayWidget(QnMediaResourceWidget* parent);
     virtual ~CameraHotspotsOverlayWidget() override;
-
-    void initHotspots(
-        QnWorkbenchContext* context,
-        const QnVirtualCameraResourcePtr& camera,
-        const nx::vms::common::CameraHotspotDataList& hotspotsData);
 
 protected:
     virtual void resizeEvent(QGraphicsSceneResizeEvent* event) override;
