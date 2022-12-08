fbf47f4ac6946a0463d741d7abab098cc48c5dd5
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-12-08 12:52:12 +0000

    VMS-37038: Fix help for VMS tests
    (cherry picked from commit 05a9199f8876d0975337546f234fd8887909d35c)
    
    Closes VMS-37038

--- libs/nx_network/unit_tests/src/main.cpp
+++ libs/nx_network/unit_tests/src/main.cpp
@@ -1,5 +1,7 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
+#include <QtCore/QCoreApplication>
+
 #include <nx/network/ssl/certificate.h>
 
 #define USE_GMOCK
@@ -8,6 +10,7 @@
 
 int main(int argc, char** argv)
 {
+    QCoreApplication app(argc, argv);
     return nx::network::test::runTest(
         argc, argv,
         [](const nx::utils::ArgumentParser&)
