afd1456e23f8dc7356d23c2c0d70b29130a8d6fd
Evgenij Sobolev <esobolev@networkoptix.com>
2022-12-19 15:09:18 +0000

    VMS-37179: Fix default state "Do not archive secondary stream" expert settings checkbox
    Fixes VMS-37179
    
    (cherry picked from commit 02f8250b6fc4dca28332b708d1a07e557292ec47)

--- vms/client/nx_vms_client_desktop/unit_tests/src/nx/vms/client/desktop/resource_properties/camera/flux/camera_settings_expert_ut.cpp
+++ vms/client/nx_vms_client_desktop/unit_tests/src/nx/vms/client/desktop/resource_properties/camera/flux/camera_settings_expert_ut.cpp
@@ -30,6 +30,11 @@ protected:
     {
         EXPECT_EQ(state().expert.keepCameraTimeSettings, value);
     }
+
+    void thenSecondaryRecordingEnabled(bool value)
+    {
+        EXPECT_EQ(state().expert.secondaryRecordingDisabled, !value);
+    }
 };
 
 TEST_F(CameraSettingsDialogExpertTest, checkKeepCameraTimeSettings)
@@ -42,6 +47,20 @@ TEST_F(CameraSettingsDialogExpertTest, checkKeepCameraTimeSettings)
 
     whenKeepCameraTimeSettings(false);
     thenKeepCameraTimeSettings(false);
+}
+
+// When we turn on the second stream (even if the camera does not have an internal second stream),
+// then by default the recording from the second stream should be enabled.
+TEST_F(CameraSettingsDialogExpertTest, checkSecondaryRecordingEnabled)
+{
+    auto camera = givenCamera();
+    camera->setHasDualStreaming(false);
+    whenCamerasAreLoaded();
+
+    whenRecordingEnabled(true);
+    whenDualStreamingEnabled(true);
+
+    thenSecondaryRecordingEnabled(true);
 }
 
 } // namespace nx::vms::client::desktop::test
