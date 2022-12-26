259556f708089800a0b104ce09d7cadd9dabb51b
Mikhail Uskov <muskov@networkoptix.com>
2022-12-26 19:21:22 +0300

    VMS-37344: VMS: Increase proto version

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 04) #< VMS-31378: Server LDAP: Add support of multiple search filters
+set(_vmsInVersionProtocolNumber 05) #< VMS-37344: VMS: Increase proto version
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
