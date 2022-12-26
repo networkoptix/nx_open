d04ecf23da75801558f4aa82efc21ef469e92da1
Mikhail Uskov <muskov@networkoptix.com>
2022-12-26 19:21:22 +0300

    VMS-37344: VMS: Increase proto version
    (cherry picked from commit f3a6656494288ebd62b3da2b646b79f1c63a8b1f)

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -10,6 +10,6 @@
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
 # somebody concurrently updates the Protocol version. Prefer to use the Jira Issue id and title as
 # a comment.
-set(_vmsInVersionProtocolNumber 06) #< VMS-35142: Remove some not important data from public API
+set(_vmsInVersionProtocolNumber 07) #< VMS-37344: VMS: Increase proto version
 set(vmsProtocolVersion "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${_vmsInVersionProtocolNumber}")
 unset(_vmsInVersionProtocolNumber)
