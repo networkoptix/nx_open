bbdc9acd33a60d8e4ea0217ca5990a736d7b2808
Denis Klychkov <dklychkov@networkoptix.com>
2023-01-10 23:55:30 -0800

    VMS-37533: Generate a list of the used Conan dependency references
    The generated file is supposed to be saved by the CI and may be used to get certain copies of the
    used dependencies (e.g. to get the debug symbols).

--- cmake/generate_open_source_config.cmake
+++ cmake/generate_open_source_config.cmake
@@ -6,15 +6,10 @@ set(UPDATE_SERVER_URL "https://updates.networkoptix.com")
 set(SERVER_PLATFORMS windows_x64 linux_x64 linux_arm32 linux_arm64)
 set(SERVER_REFS_FILE_NAME "${PROJECT_BINARY_DIR}/compatible_servers.txt")
 
-nx_configure_file(
-    "${open_source_root}/build_info.txt"
-    ${CMAKE_CURRENT_BINARY_DIR}/distrib)
-nx_configure_file(
-    "${open_source_root}/build_info.json"
-    ${CMAKE_CURRENT_BINARY_DIR}/distrib)
-nx_configure_file(
-    "${open_source_root}/nx_log_viewer.html"
-    ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
+nx_configure_file("${open_source_root}/build_info.txt" ${CMAKE_BINARY_DIR}/distrib)
+nx_configure_file("${open_source_root}/build_info.json" ${CMAKE_BINARY_DIR}/distrib)
+nx_configure_file("${open_source_root}/nx_log_viewer.html" ${CMAKE_BINARY_DIR} COPYONLY)
+nx_configure_file(${CMAKE_BINARY_DIR}/conan_refs.txt ${CMAKE_BINARY_DIR}/distrib COPYONLY)
 
 function(nx_generate_compatible_servers_txt)
     if(buildNumber STREQUAL "0")
