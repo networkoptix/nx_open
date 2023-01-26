955f9a19cb1ee4264e1eeeb7cb4b7a0c692387cd
Denis Klychkov <dklychkov@networkoptix.com>
2023-01-26 00:43:04 -0800

    VMS-37809: Make it possible to override update related URLs in a customization package

--- vms/libs/nx_vms_common/src/nx/vms/common/update/nx_system_updates_ini.h
+++ vms/libs/nx_vms_common/src/nx/vms/common/update/nx_system_updates_ini.h
@@ -11,9 +11,9 @@ struct NX_VMS_COMMON_API Ini: nx::kit::IniConfig
     Ini();
 
     NX_INI_STRING("", releaseListUrl,
-        "[Dev] Overrides update server URL. Leave empty to use the built-in value.\n"
-        "Use substring `{customization}` to substitute current customization value, e.g.:\n"
-        "`https://updates.vmsproxy.com/{customization}/releases.json`");
+        "If non-empty, overrides the update server URL. Use substring \"{customization}\" to\n"
+        "substitute the current customization value, e.g.:\n"
+        "`http://localhost/{customization}/releases.json`");
 };
 
 NX_VMS_COMMON_API Ini& ini();
