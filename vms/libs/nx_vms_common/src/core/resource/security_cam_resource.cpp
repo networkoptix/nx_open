c0c600293b0bc7099dd407ec5f943553379d2c4d
Evgenij Sobolev <esobolev@networkoptix.com>
2023-01-31 13:00:32 +0000

    VMS-37735: Fix wrong device type in context menu for I/O module
    Fixes VMS-37735, VMS-37608

--- vms/libs/nx_vms_common/src/core/resource/security_cam_resource.cpp
+++ vms/libs/nx_vms_common/src/core/resource/security_cam_resource.cpp
@@ -94,6 +94,7 @@ void QnSecurityCamResource::setSystemContext(nx::vms::common::SystemContext* sys
     {
         connect(context->resourceDataPool(), &QnResourceDataPool::changed, this,
             &QnSecurityCamResource::resetCachedValues, Qt::DirectConnection);
+        resetCachedValues();
     }
 }
 
