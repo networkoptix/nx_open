66d9ea642cd48866839bdfcdda51b757990de8ba
Pavel Tolstov <ptolstov@networkoptix.com>
2023-02-01 19:41:12 +0300

    VMS-37535: Update WinSDK Redist version to 10.0.20348.0
    (cherry picked from commit b25aea842cb09fe43508b53476e0ff19f02d9585)

--- conanfile.py
+++ conanfile.py
@@ -116,7 +116,7 @@ class NxOpenConan(ConanFile):
             self.requires("directx/JUN2010" "#ca268f1b54e3874ad43524cd81447b01")
             self.requires("intel-media-sdk-bin/2019R1" "#0a123266dd20b84b16a4cc60b752fc4b")
             self.requires("msvc-redist/14.29.30133" "#9f10aa59e4671ce0669d6181e6b0a269")
-            self.requires("winsdk-redist/10.0.19041.0" "#acf550438d5c5d7c1be638c7ac9662e3")
+            self.requires("winsdk-redist/10.0.20348.0" "#bf8bf6131653c35ae84474f399fe5113")
 
         if self.isArm32 or self.isArm64:
             self.requires("sse2neon/7bd15ea" "#d5c087ce33dbf1425b29d6435284d2c7")
