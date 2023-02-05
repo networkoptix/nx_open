818e59ae38e8d147ecd82845e2c9fae987cb7f1c
Denis Klychkov <dklychkov@networkoptix.com>
2023-02-04 22:33:11 -0800

    VMS-37948: Raise target macOS version to 11.0

--- cmake/toolchain/macos_x64.cmake
+++ cmake/toolchain/macos_x64.cmake
@@ -1,5 +1,4 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_OSX_DEPLOYMENT_TARGET 10.14 CACHE INTERNAL "")
 set(CMAKE_OSX_ARCHITECTURES x86_64 CACHE INTERNAL "")
+include(${CMAKE_CURRENT_LIST_DIR}/common/macos.cmake)
--- conan_profiles/macos_arm64.profile
+++ conan_profiles/macos_arm64.profile
@@ -2,4 +2,3 @@ include(common/macos.profile)
 
 [settings]
 arch=armv8
-os.version=11.0
