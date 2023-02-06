fd5adc8fc8b56e2c4edb5165a7675c30cab2b450
Anton Malov <amalov@networkoptix.com>
2023-02-06 15:00:53 +0000

    VMS-37939: Implement alarm layout action

--- vms/libs/nx_vms_rules/src/nx/vms/rules/actions/show_on_alarm_layout_action.cpp
+++ vms/libs/nx_vms_rules/src/nx/vms/rules/actions/show_on_alarm_layout_action.cpp
@@ -2,6 +2,39 @@
 
 #include "show_on_alarm_layout_action.h"
 
+#include "../action_builder_fields/event_id_field.h"
+#include "../action_builder_fields/optional_time_field.h"
+#include "../action_builder_fields/target_device_field.h"
+#include "../action_builder_fields/target_user_field.h"
+#include "../action_builder_fields/event_devices_field.h"
+#include "../action_builder_fields/flag_field.h"
+#include "../utils/event_details.h"
+#include "../utils/field.h"
+#include "../utils/type.h"
+
 namespace nx::vms::rules {
+
+const ItemDescriptor& ShowOnAlarmLayoutAction::manifest()
+{
+    static const auto kDescriptor = ItemDescriptor{
+        .id = utils::type<ShowOnAlarmLayoutAction>(),
+        .displayName = tr("Show on Alarm Layout"),
+        .fields = {
+            makeFieldDescriptor<TargetDeviceField>(utils::kDeviceIdsFieldName, "Event devices"),
+            utils::makeIntervalFieldDescriptor("Interval of action"),
+            makeFieldDescriptor<OptionalTimeField>("playbackTime", tr("Playback time")),
+            makeFieldDescriptor<TargetUserField>(utils::kUsersFieldName, tr("For")),
+            makeFieldDescriptor<FlagField>("forceOpen", tr("Force Alarm Layout opening")),
+
+            makeFieldDescriptor<EventIdField>("id", "Event ID"),
+            makeFieldDescriptor<EventDevicesField>("eventDeviceIds", "Event devices"),
+            utils::makeTextFormatterFieldDescriptor("caption", tr("Alarm: %1").arg("{@EventCaption}")),
+            utils::makeTextFormatterFieldDescriptor("description", "{@EventDescription}"),
+            utils::makeTextFormatterFieldDescriptor("tooltip", "{@EventTooltip}"),
+            utils::makeExtractDetailFieldDescriptor("sourceName", utils::kSourceNameDetailName),
+        }
+    };
+    return kDescriptor;
+}
 
 } // namespace nx::vms::rules
