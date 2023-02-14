3d1b629dd0898f2834740ac6374a83e1b56425d7
Evgeniy Karpov <ekarpov@networkoptix.com>
2023-02-14 23:10:35 +0300

    VMS-37944: Stop cloud stream socket on shutdown
    Revert this behavior back like it was before 152c8149a80ea50333af6bff7daaf39e4e234279
    https://networkoptix.atlassian.net/browse/VMS-29613
    Allow to call CloudStreamSocket::shutdown from AIO timer
    
    (cherry picked from commit e907704ac6d7d738a47f7295926a5e6d6332d245)

--- libs/nx_network/src/nx/network/cloud/cloud_stream_socket.cpp
+++ libs/nx_network/src/nx/network/cloud/cloud_stream_socket.cpp
@@ -101,10 +101,13 @@ bool CloudStreamSocket::isClosed() const
 
 bool CloudStreamSocket::shutdown()
 {
-    m_isTerminated = true;
+    if (m_isTerminated.exchange(true))
+        return true;
 
     if (auto promisePtr = m_connectPromisePtr.exchange(nullptr))
         promisePtr->set_value(std::make_pair(SystemError::interrupted, 0));
+
+    m_aioThreadBinder.post([this]() { cancelIoInAioThread(aio::etNone); });
 
     if (m_socketDelegate)
         return m_socketDelegate->shutdown();
