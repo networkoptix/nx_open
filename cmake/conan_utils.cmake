7133ece480b39b6d7b26537bec40210b69759f4a
Denis Klychkov <dklychkov@networkoptix.com>
2023-02-15 04:49:58 +0000

    VMS-38096: Avoid extra Conan remotes when a custom Conan home is not used
    (cherry picked from commit 00a0863019bd5195e1be9b395f0480e1dab6fe8b)

--- cmake/conan_utils.cmake
+++ cmake/conan_utils.cmake
@@ -29,45 +29,48 @@ function(nx_init_conan)
     endif()
 
     _configure_conan()
-
-    if(DEFINED "ENV{NX_CONAN_DOWNLOAD_CACHE}")
-        message(STATUS "Conan download cache: $ENV{NX_CONAN_DOWNLOAD_CACHE}")
-        execute_process(
-            COMMAND ${CONAN_EXECUTABLE} config set
-                storage.download_cache=$ENV{NX_CONAN_DOWNLOAD_CACHE})
-    endif()
 endfunction()
 
 function(_configure_conan)
     if(NOT customConanHome)
         set(conan_installed_config_list_file "$ENV{CONAN_USER_HOME}/.conan/config_install.json")
         file(REMOVE ${conan_installed_config_list_file})
-        set(repo_conan_config "${open_source_root}/conan_config")
+
+        set(download_cache_dir None)
+        if(DEFINED "ENV{NX_CONAN_DOWNLOAD_CACHE}")
+            message(STATUS "Conan download cache: $ENV{NX_CONAN_DOWNLOAD_CACHE}")
+            set(download_cache_dir $ENV{NX_CONAN_DOWNLOAD_CACHE})
+        endif()
+
+        set(conan_config_dir_src "${open_source_root}/conan_config")
+        set(conan_config_dir "${CMAKE_BINARY_DIR}/conan_config")
+        nx_configure_directory(${conan_config_dir_src} ${conan_config_dir})
+
         nx_execute_process_or_fail(
-            COMMAND ${CONAN_EXECUTABLE} config install ${repo_conan_config}
-            ERROR_MESSAGE "Can not apply Conan configuration from ${repo_conan_config}."
+            COMMAND ${CONAN_EXECUTABLE} config install ${conan_config_dir}
+            ERROR_MESSAGE "Can not apply Conan configuration from ${conan_config_dir}"
+        )
+    else()
+        # Check if "nx" remote is already added.
+        set(conan_remotes_file "$ENV{CONAN_USER_HOME}/.conan/remotes.json")
+        if(EXISTS ${conan_remotes_file})
+            file(READ ${conan_remotes_file} conan_remotes)
+            string(JSON conan_remotes_count LENGTH ${conan_remotes} "remotes")
+            math(EXPR last_conan_remote_index "${conan_remotes_count} - 1")
+            foreach(remote_index RANGE 0 ${last_conan_remote_index})
+                string(JSON remote_url GET ${conan_remotes} "remotes" ${remote_index} "url")
+                string(JSON remote_name GET ${conan_remotes} "remotes" ${remote_index} "name")
+                if("${remote_name}" STREQUAL "nx" AND "${remote_url}" STREQUAL "${conanNxRemote}")
+                    return()
+                endif()
+            endforeach()
+        endif()
+
+        nx_execute_process_or_fail(
+            COMMAND ${CONAN_EXECUTABLE} remote add nx ${conanNxRemote} True --force
+            ERROR_MESSAGE "Can not add Nx Conan remote."
         )
     endif()
-
-    # Check if "nx" remote is already added.
-    set(conan_remotes_file "$ENV{CONAN_USER_HOME}/.conan/remotes.json")
-    if(EXISTS ${conan_remotes_file})
-        file(READ ${conan_remotes_file} conan_remotes)
-        string(JSON conan_remotes_count LENGTH ${conan_remotes} "remotes")
-        math(EXPR last_conan_remote_index "${conan_remotes_count} - 1")
-        foreach(remote_index RANGE 0 ${last_conan_remote_index})
-            string(JSON remote_url GET ${conan_remotes} "remotes" ${remote_index} "url")
-            string(JSON remote_name GET ${conan_remotes} "remotes" ${remote_index} "name")
-            if("${remote_name}" STREQUAL "nx" AND "${remote_url}" STREQUAL "${conanNxRemote}")
-                return()
-            endif()
-        endforeach()
-    endif()
-
-    nx_execute_process_or_fail(
-        COMMAND ${CONAN_EXECUTABLE} remote add nx ${conanNxRemote} True --force
-        ERROR_MESSAGE "Can not add Nx Conan remote."
-    )
 endfunction()
 
 function(nx_run_conan)
