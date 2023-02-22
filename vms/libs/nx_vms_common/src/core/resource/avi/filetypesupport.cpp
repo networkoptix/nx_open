fa7558aedd44389ea330273a2716c4b1973b944b
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2023-02-21 19:56:07 -0800

    VMS-35307: Speed up file extension check for resources

--- vms/libs/nx_vms_common/src/core/resource/avi/filetypesupport.cpp
+++ vms/libs/nx_vms_common/src/core/resource/avi/filetypesupport.cpp
@@ -2,13 +2,13 @@
 
 #include "filetypesupport.h"
 
-#include <vector>
+#include <QSet>
 
 #include <nx/core/layout/layout_file_info.h>
 
 bool FileTypeSupport::isMovieFileExt(const QString &filename)
 {
-    static const std::vector<QString> kVideoExtensions{
+    static const QSet<QString> kVideoExtensions{
         "3g2", "3gp", "3gp2", "3gpp", "amv", "asf", "avi", "divx", "dv", "flv", "gxf", "m1v",
         "m2t", "m2v", "m2ts", "m4v", "mkv", "mov", "mp2", "mp2v", "mp4", "mp4v", "mpa", "mpe",
         "mpeg", "mpeg1", "mpeg2", "mpeg4", "mpg", "mpv2", "mts", "mxf", "nsv", "nuv", "ogg", "ogm",
@@ -16,30 +16,28 @@ bool FileTypeSupport::isMovieFileExt(const QString &filename)
         "mpegts"
     };
 
-    const QString lowerFilename = filename.toLower();
+    const auto i = filename.lastIndexOf('.');
+    if (i == -1)
+        return false;
 
-    for (const auto& extension: kVideoExtensions)
-    {
-        if (lowerFilename.endsWith('.' + extension))
-            return true;
-    }
-    return false;
+    const QString lowerExt = filename.sliced(i + 1).toLower();
+
+    return kVideoExtensions.contains(lowerExt);
 }
 
 bool FileTypeSupport::isImageFileExt(const QString &filename)
 {
-    static const std::vector<QString> kImageExtensions{
+    static const QSet<QString> kImageExtensions{
         "jpg", "jpeg", "png", "bmp", "gif", "tif", "tiff"
     };
 
-    const QString lowerFilename = filename.toLower();
+    const auto i = filename.lastIndexOf('.');
+    if (i == -1)
+        return false;
 
-    for (const auto& extension: kImageExtensions)
-    {
-        if (lowerFilename.endsWith('.' + extension))
-            return true;
-    }
-    return false;
+    const QString lowerExt = filename.sliced(i + 1).toLower();
+
+    return kImageExtensions.contains(lowerExt);
 }
 
 bool FileTypeSupport::isValidLayoutFile(const QString &filename)
