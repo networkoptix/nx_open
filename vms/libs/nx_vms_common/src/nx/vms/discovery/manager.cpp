bb3b7be0887d81223a0a92614446a1a54c3c593d
Mikhail Uskov <muskov@networkoptix.com>
2023-03-02 13:28:17 +0300

    VMS-38354: Remove assert in nx::vms::discovery::Manager on empty socket remote address
    (cherry picked from commit a9326af3b9a8d2f7f83bd9efddf3ec04a28562a8)

--- vms/libs/nx_vms_common/src/nx/vms/discovery/manager.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/discovery/manager.cpp
@@ -115,8 +115,8 @@ struct Manager::Private
                 // Add fixed address only if not a cloud address used for connection to avoid loop.
                 if (!newCloudHost.isEmpty()
                     && !resolver.isCloudHostname(module.endpoint.address.toString())
-                    && NX_ASSERT(resolvedEndpoint.address.isIpAddress(), resolvedEndpoint)
-                    && NX_ASSERT(resolvedEndpoint.port > 0, resolvedEndpoint))
+                    && resolvedEndpoint.address.isIpAddress()
+                    && resolvedEndpoint.port > 0)
                 {
                     resolver.addFixedAddress(newCloudHost, resolvedEndpoint);
                 }
