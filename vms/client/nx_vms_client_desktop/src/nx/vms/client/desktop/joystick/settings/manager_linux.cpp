b0510b667861a84d97dad190d3a9d23492148d06
Dmitry Fisenko <dfisenko@networkoptix.com>
2023-03-03 15:06:01 +0300

    VMS-34735: Fix joystick model detection on Linux

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/joystick/settings/manager_linux.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/joystick/settings/manager_linux.cpp
@@ -68,6 +68,10 @@ void ManagerLinux::enumerateDevices()
             "Model: %1, path: %2",
             modelAndManufacturer, path);
 
+        const auto model = findDeviceModel(modelAndManufacturer);
+        if (model.isEmpty())
+            continue;
+
         const auto config = createDeviceDescription(findDeviceModel(modelAndManufacturer));
         DeviceLinux* deviceLinux = new DeviceLinux(config, path, pollTimer());
         DevicePtr device(deviceLinux);
@@ -104,7 +108,7 @@ QString ManagerLinux::findDeviceModel(const QString& modelAndManufacturer) const
             return model == config.model;
         });
 
-    return iter != knownConfigsList.end() ? iter->model : modelAndManufacturer;
+    return iter != knownConfigsList.end() ? iter->model : "";
 }
 
 } // namespace nx::vms::client::desktop::joystick
