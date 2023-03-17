ced82a71ca35184a1f8e02bce2e9ec2c50961b00
Evgenii Karpov <ekarpov@networkoptix.com>
2023-03-17 17:01:01 +0300

    VMS-38579: Server: Do not report about possible MitM attack on proxy connection to not VMS Server
    (cherry picked from commit 0d1e6bb46fca775b080fc62de20736084372065e)

--- libs/nx_network/src/nx/network/ssl/ssl_stream_socket.cpp
+++ libs/nx_network/src/nx/network/ssl/ssl_stream_socket.cpp
@@ -27,7 +27,7 @@ const std::function<
                 return true;
             }
 
-            NX_VERBOSE(socket,
+            NX_WARNING(socket,
                 "Default certificate verification for server `%1` is failed: %2",
                 serverName, message);
             return false;
