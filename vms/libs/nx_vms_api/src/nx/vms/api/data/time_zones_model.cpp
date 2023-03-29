9584bd3a9c0341b4d24958ab3495326b6b5407e9
Sergei Uvarov <suvarov@networkoptix.com>
2023-03-29 17:54:14 +0300

    VMS-38819: Implement REST API handlers related to system/server time

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/time_zones_model.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "time_zones_model.h"
+
+#include <nx/fusion/model_functions.h>
+
+namespace nx::vms::api {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(TimeZonesData, (json), TimeZonesData_Fields)
+
+} // namespace nx::vms::api
