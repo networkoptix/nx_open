564ae194da65f07fc75a90ad2be80da9dc704822
Jenkins <jenkins@networkoptix.com>
2023-04-06 00:53:27 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.0_patch
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_0_patch.update-translation/68/

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
@@ -167,7 +167,7 @@
     </message>
     <message>
       <source>Keep Archive For...</source>
-      <translation>アーカイブを保持...</translation>
+      <translation>アーカイブ保持時間</translation>
     </message>
     <message>
       <source>Max</source>
