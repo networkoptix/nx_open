578d8444e1683e72ffc7762a6328dfcdd324575e
Maksim Malofeev <mmalofeev@networkoptix.com>
2023-04-10 15:16:23 +0000

    VMS-38827: Fix wrong panic recording action duration

--- vms/libs/nx_vms_rules/unit_tests/src/nx/vms/rules/test_action.h
+++ vms/libs/nx_vms_rules/unit_tests/src/nx/vms/rules/test_action.h
@@ -16,6 +16,15 @@ class TestAction: public nx::vms::rules::BasicAction
 {
     Q_OBJECT
     Q_CLASSINFO("type", "nx.actions.test.instant")
+
+public:
+    static ItemDescriptor manifest()
+    {
+        return ItemDescriptor{
+            .id = utils::type<TestAction>(),
+            .displayName = "Test action",
+        };
+    }
 };
 
 class TestProlongedAction: public nx::vms::rules::BasicAction
