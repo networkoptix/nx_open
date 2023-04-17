28a878fcc8c11c74b2e161b38ca3a0885ea4813b
Denis Klychkov <dklychkov@networkoptix.com>
2023-04-17 14:41:06 -0700

    Add missing copyright comment

--- libs/nx_network/src/nx/network/system_network_headers.h
+++ libs/nx_network/src/nx/network/system_network_headers.h
@@ -1,3 +1,5 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
 #pragma once
 
 // Windows network headers mut be included in a certain order to function properly.
