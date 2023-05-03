bbf1df73740d02438694aa07948b7fab82dceda2
Sergei Ivanov <sivanov@networkoptix.com>
2023-05-03 15:52:48 +0300

    VMS-39330: Improve too quick client reconnect error handling
    Issue occurs when client manages to reconnect earlier than server notices connection ever was
    broken. Corresponding message bus signal was renamed for consistency, and client now just tries to
    reconnect again in this case.
    
    Fixes VMS-39330, fixes VMS-39390.
    
    (cherry picked from commit ed85247ed31c2d1628bff20dd2dd3dcc37252e7b)

--- vms/libs/appserver2/src/nx/p2p/p2p_message_bus.cpp
+++ vms/libs/appserver2/src/nx/p2p/p2p_message_bus.cpp
@@ -529,7 +529,7 @@ void MessageBus::removeConnectionUnsafe(QWeakPointer<ConnectionBase> weakRef)
     else if (connection->state() == Connection::State::forbidden)
     {
         emitAsync(this,
-            &MessageBus::remotePeerIncompatible,
+            &MessageBus::remotePeerForbidden,
             remotePeer.id,
             connection->lastErrorMessage());
     }
