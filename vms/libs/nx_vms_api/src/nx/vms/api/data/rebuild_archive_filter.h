bf37e9b08cffcf5eb2d6f8112cb36fe44505ae51
Alexander Kulikov <akulikov@networkoptix.com>
2023-05-09 15:36:49 -0700

    VMS-37653: Add from parameter to Reindex API

--- vms/libs/nx_vms_api/src/nx/vms/api/data/rebuild_archive_filter.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/rebuild_archive_filter.h
@@ -10,14 +10,21 @@ namespace nx::vms::api {
 
 struct NX_VMS_API RebuildArchiveFilter
 {
-    /**%apidoc Server id. */
+    /**%apidoc
+     * Server id. Can be obtained from "id" field via `GET /rest/v{1-}/servers`
+     * or be "this" to refer to the current Server.
+     * %example this
+     */
     QnUuid id;
 
-    /** %apidoc[opt] Storage location. */
+    /**%apidoc[opt] Storage location. */
     StorageLocation location = StorageLocation::both;
+
+    /**%apidoc Starting time point of the rebuild process, in milliseconds. */
+    std::optional<std::chrono::system_clock::time_point> startPointMs;
 };
 
-#define RebuildArchiveFilter_Fields (id)(location)
+#define RebuildArchiveFilter_Fields (id)(location)(startPointMs)
 
 QN_FUSION_DECLARE_FUNCTIONS(RebuildArchiveFilter, (json), NX_VMS_API)
 NX_REFLECTION_INSTRUMENT(RebuildArchiveFilter, RebuildArchiveFilter_Fields)
