5f21382c27add9937e3c739dd11346f4197e46ad
Petr Privalov <pprivalov@networkoptix.com>
2023-05-10 13:44:27 +0000

    VMS-39102: Pause and resume video on rewind

--- vms/client/nx_vms_client_desktop/src/ui/workbench/workbench_controller.h
+++ vms/client/nx_vms_client_desktop/src/ui/workbench/workbench_controller.h
@@ -286,4 +286,7 @@ private:
     };
     ShiftDirection m_rewindDirection = ShiftDirection::noShift;
     QTimer* const m_rewindTimer;
+
+    /** Whether the video was playing when user started to rewind or fast forward. */
+    bool m_isPlayingOnShift = false;
 };
