05dd278734e996a63f6ae8f43d06f78279080d7c
Mike Shevchenko <mshevchenko@networkoptix.com>
2023-05-23 16:16:49 +0300

    VMS-39133: Apidoc: Fix comment style and wording

--- vms/libs/nx_vms_api/src/nx/vms/api/data/media_stream_settings.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/media_stream_settings.h
@@ -30,35 +30,41 @@ struct NX_VMS_API MediaStreamSettings
         f4v
     );
 
-    /**%apidoc Device id (can be obtained from "id", "physicalId" or "logicalId" field
-     * via /rest/v{1-}/devices) or MAC address (not supported for certain Devices).
+    /**%apidoc
+     * Device id (can be obtained from "id", "physicalId" or "logicalId" field via
+     * /rest/v{1-}/devices) or MAC address (not supported for certain Devices).
      */
     QnUuid id;
 
-    /**%apidoc [opt] Stream format. */
+    /**%apidoc[opt] Stream format. */
     Format format = Format::mkv;
 
-    /**%apidoc [opt] Video quality. */
+    /**%apidoc[opt] Video quality. */
     StreamQuality quality = StreamQuality::normal;
 
-    /**%apidoc [opt]:string Turn on video transcoding to the specified resolution.
-     * Resolution string either may contain width and height (for instance 320x240) or height
-     * only (for instance 240p). By default, 640x480 is used.
+    /**%apidoc[opt]:string
+     * Turn on video transcoding to the specified resolution. Resolution string either may contain
+     * width and height (for instance, 320x240) or height only (for instance, 240p). By default,
+     * 640x480 is used.
      */
     std::optional<ResolutionData> resolution = std::nullopt;
 
-    /**%apidoc [opt] If present specifies archive stream start position.
-     * Otherwise, LIVE stream is provided.
+    /**%apidoc[opt]
+     * If present, specifies the Archive stream start position. Otherwise, the Live stream is
+     * provided.
      */
     std::optional<std::chrono::microseconds> positionUs = std::nullopt;
 
-    /**%apidoc [opt] If present, specifies archive stream end position.
-     * It is used only if `positionUs` parameter is present.
+    /**%apidoc[opt]
+     * If present, specifies the Archive stream end position. It is used only if the `positionUs`
+     * parameter is present.
      */
     std::optional<std::chrono::microseconds> endPositionUs = std::nullopt;
 
-    /**%apidoc [opt] Rotate item, in degrees. If the parameter is absent, video will be
-     * rotated to the default value defined in Device settings dialog.
+    /**%apidoc[opt]
+     * Item rotation, in degrees. If the parameter is absent, the video will be rotated to the
+     * default value defined in the Device Settings dialog.
+     *
      * %value 0
      * %value 90
      * %value 180
@@ -66,26 +72,28 @@ struct NX_VMS_API MediaStreamSettings
      */
     std::optional<int> rotation = std::nullopt;
 
-    /**%apidoc [opt] Drop Late Frames.
-     */
+    /**%apidoc[opt] Drop Late Frames. */
     std::optional<int> dropLateFrames = std::nullopt;
 
-    /**%apidoc [opt] Stand Frame Duration. If the parameter is present, video speed is
-     * limited by real time.
+    /**%apidoc[opt]
+     * Stand Frame Duration. If the parameter is present, the video speed is limited by the real
+     * time.
      */
     bool standFrameDuration = false;
 
-    /**%apidoc [opt] Turn on realtime optimization. It will drop some frames if not enough
-     * CPU for realtime transcoding.
+    /**%apidoc[opt]
+     * Turn on the realtime optimization. It will drop some frames if there is not enough CPU for
+     * the realtime transcoding.
      */
     bool realTimeOptimization = false;
 
-    /**%apidoc [opt] Send only audio stream. */
+    /**%apidoc[opt] Send only the audio stream. */
     bool audioOnly = false;
 
-    /**%apidoc [opt] Seek to exact time in the archive by the specified `pos`,
-     * otherwise seek to the nearest left to the `pos` keyframe (on timeline). Enabling causes
-     * the stream to be transcoded. Disabled by default.
+    /**%apidoc[opt]
+     * Seek to the exact time in the Archive by the specified `pos`, otherwise seek to the nearest
+     * left to the `pos` keyframe (on timeline). Enabling causes the stream to be transcoded.
+     * Disabled by default.
      */
     bool accurateSeek = false;
 
@@ -95,26 +103,26 @@ struct NX_VMS_API MediaStreamSettings
      */
     StreamIndex stream = nx::vms::api::StreamIndex::undefined;
 
-    /**%apidoc [opt] Fragment length in seconds. Can be used for both live and archive streams -
+    /**%apidoc[opt] Fragment length in seconds. Can be used for both live and archive streams -
      * for archive streams effectively it's another way to specify `endPositionUs`.
      */
     std::optional<std::chrono::seconds> durationS = std::nullopt;
 
-    /**%apidoc [opt] Add signature to exported media data, only mp4 and webm
+    /**%apidoc[opt] Add signature to exported media data, only mp4 and webm
      * formats are supported.
      */
     bool signature = false;
 
-    /**%apidoc [opt] Use absolute UTC timestamps in exported media data,
+    /**%apidoc[opt] Use absolute UTC timestamps in exported media data,
      * only mp4 format is supported.
      */
     bool utcTimestamps = false;
 
-    /**%apidoc [opt] Add continuous timestamps in exported media data.
+    /**%apidoc[opt] Add continuous timestamps in exported media data.
      */
     bool continuousTimestamps = false;
 
-    /**%apidoc [opt] Force to download file in browser instead of displaying it.
+    /**%apidoc[opt] Force to download file in browser instead of displaying it.
      */
     bool download = false;
 
