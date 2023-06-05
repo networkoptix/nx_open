baf30328766b0ace51487f4fbe57be5f58a001cf
Sergei Ivanov <sivanov@networkoptix.com>
2023-06-05 17:17:10 +0000

    VMS-40030, VMS-40037: Fix client crash on losing connection to cloud systems
    Fixes VMS-40030, fixes VMS-40037
    
    (cherry picked from commit 8cfd0d51abf5ab8e4af1471b0c802527e807b287)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/cross_system/cloud_cross_system_manager.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/cross_system/cloud_cross_system_manager.cpp
@@ -7,6 +7,7 @@
 #include <finders/systems_finder.h>
 #include <network/system_helpers.h>
 #include <nx/vms/client/desktop/application_context.h>
+#include <nx/vms/client/desktop/debug_utils/utils/debug_custom_actions.h>
 #include <watchers/cloud_status_watcher.h>
 
 #include "cloud_cross_system_context.h"
@@ -78,6 +79,19 @@ CloudCrossSystemManager::CloudCrossSystemManager(QObject* parent):
         });
 
     setCloudSystems(appContext()->cloudStatusWatcher()->cloudSystems());
+
+    registerDebugAction("Cross-system contexts reset",
+        [this, setCloudSystems](auto)
+        {
+            setCloudSystems({});
+        });
+
+    registerDebugAction("Cross-system contexts restore",
+        [this, setCloudSystems](auto)
+        {
+            setCloudSystems(appContext()->cloudStatusWatcher()->cloudSystems());
+        });
+
 
     NX_VERBOSE(this, "Initialized");
 }
