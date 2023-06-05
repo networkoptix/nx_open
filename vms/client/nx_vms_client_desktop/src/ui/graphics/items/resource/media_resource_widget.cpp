fcdc1a4ca1a4cd7385e0995949f4d4b878331618
Sergei Ivanov <sivanov@networkoptix.com>
2023-06-05 21:59:08 +0300

    VMS-40044: Stop client from sending a lot of ptz requests to the systems it is not connected to
    Client sent ptzData, ptzPresets and ptzTours requests to all systems, which are available through
    cloud (one of each request per camera).
    
    (cherry picked from commit cbb9995a1b2cb3a071ee5e59e50c5c5b5e948f69)

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
@@ -50,6 +50,7 @@
 #include <nx/utils/guarded_callback.h>
 #include <nx/utils/log/log.h>
 #include <nx/utils/trace/trace.h>
+#include <nx/vms/client/core/cross_system/cross_system_ptz_controller_pool.h>
 #include <nx/vms/client/core/media/consuming_motion_metadata_provider.h>
 #include <nx/vms/client/core/motion/motion_grid.h>
 #include <nx/vms/client/core/resource/data_loaders/caching_camera_data_loader.h> //< TODO: #sivanov Remove this dependency.
@@ -1072,7 +1073,18 @@ void QnMediaResourceWidget::updatePtzController()
     if (d->camera)
     {
         auto ptzPool = systemContext()->ptzControllerPool();
-        if (QnPtzControllerPtr serverController = ptzPool->controller(d->camera))
+        QnPtzControllerPtr serverController = ptzPool->controller(d->camera);
+
+        // Camera is opened for the first time, initialize controller.
+        if (d->camera->hasFlags(Qn::cross_system) && !serverController)
+        {
+            auto crossSystemControllerPool =
+                dynamic_cast<nx::vms::client::core::CrossSystemPtzControllerPool*>(ptzPool);
+            if (NX_ASSERT(crossSystemControllerPool))
+                serverController = crossSystemControllerPool->ensureControllerExists(d->camera);
+        }
+
+        if (serverController)
         {
             serverController.reset(new QnActivityPtzController(
                 QnActivityPtzController::Client,
