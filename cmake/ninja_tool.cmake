3a2536335faa69b235d6cd45a257232dbef18621
Denis Klychkov <dklychkov@networkoptix.com>
2023-06-30 13:57:42 -0700

    VMS-40452: Build: Remove RDep
    (cherry picked from commit e86326b984080761b4167d955a281fbb7baa020f)

--- cmake/ninja_tool.cmake
+++ cmake/ninja_tool.cmake
@@ -55,49 +55,9 @@ function(nx_add_targets_to_strengthened)
     endforeach()
 endfunction()
 
-function(nx_add_custom_pre_build_command command)
-    set_property(GLOBAL APPEND_STRING PROPERTY pre_build_commands "run ${command}\n\n")
-endfunction()
-
 function(nx_use_custom_verify_globs)
     set_property(GLOBAL APPEND_STRING
         PROPERTY pre_build_commands "substitute_verify_globs ${CONAN_VERIFY_GLOBS_ROOT}\n\n")
-endfunction()
-
-function(nx_add_pre_build_artifacts_check)
-    # TODO: Modify ninja_tool `run` to allow it run programs with custom environment variables set.
-    # The preffered syntax in "pre_build.ninja_tool" file would be the following:
-    # run [<ENV-VARIABLE1>=<VALUE1> [...<ENV-VARIABLEN>=<VALUEN>]] <COMMAND> [<ARG1> [...<ARGN>]]
-    nx_c_escape_string("${CMAKE_SOURCE_DIR}/build_utils/python/run_after_fetch.py"
-        run_after_fetch_escaped)
-    nx_c_escape_string("${packages_sync_flag_file}" packages_sync_flag_file_escaped)
-    nx_c_escape_string("${RDEP_DIR}" rdep_dir_escaped)
-    nx_c_escape_string("${CMAKE_SOURCE_DIR}" sync_dependencies_path_escaped)
-    nx_c_escape_string("${PYTHON_EXECUTABLE}" python_executable_escaped)
-    nx_c_escape_string("${PACKAGES_DIR}" package_dir_escaped)
-    nx_c_escape_string("${cmake_include_file}" cmake_include_file_escaped)
-    set(check_artifacts_command
-        "${python_executable_escaped} ${run_after_fetch_escaped}"
-        " --checker-source-dir=${CMAKE_SOURCE_DIR}"
-        " --checker-flag-file=${packages_sync_flag_file_escaped}"
-        " --checker-run-script=sync_dependencies"
-        " --checker-pythonpath ${rdep_dir_escaped} ${sync_dependencies_path_escaped}"
-        " --packages-dir=${package_dir_escaped}"
-        " --target=${rdep_target}"
-        " --release-version=${releaseVersion}"
-        " --customization=${customization}"
-        " --cmake-include-file=${cmake_include_file_escaped}"
-        " --options sync-timestamps=True"
-    )
-
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        string(APPEND check_artifacts_command " --debug")
-    endif()
-    if(rdepVerbose)
-        string(APPEND check_artifacts_command " --verbose")
-    endif()
-
-    nx_add_custom_pre_build_command("${check_artifacts_command}")
 endfunction()
 
 function(nx_save_ninja_preprocessor_script)
