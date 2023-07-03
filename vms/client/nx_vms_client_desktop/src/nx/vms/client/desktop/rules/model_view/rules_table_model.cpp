8940a6dcfdab6595cd686b36d8102a7b151568bb
Maksim Malofeev <mmalofeev@networkoptix.com>
2023-07-03 12:18:06 +0000

    VMS-40453: Fix icon absence for the disabled rule

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/rules/model_view/rules_table_model.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/rules/model_view/rules_table_model.cpp
@@ -229,7 +229,7 @@ QVariant RulesTableModel::stateColumnData(const ConstRulePtr& rule, int role) co
         // TODO: #mmalofeev check if the rule has warning.
 
         if (!rule->enabled())
-            return "icon://svg/skin/slider/navigation/pause_32.svg";
+            return "image://svg/skin/misc/disabled.svg";
     }
 
     return {};
