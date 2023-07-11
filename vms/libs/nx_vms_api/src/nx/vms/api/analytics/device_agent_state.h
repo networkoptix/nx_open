4e443253a8ecd4523ad065b6ff2bc74185be87c0
Maksim Tikhomirov <mtikhomirov@networkoptix.com>
2023-07-11 13:49:04 +0000

    VMS-40380: Create DeviceAgent API handler stub
    VMS-40380: Create DeviceAgent API handler stub(initial commit)

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/device_agent_state.h
@@ -0,0 +1,29 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/uuid.h>
+
+namespace nx::vms::api::analytics {
+
+struct NX_VMS_API DeviceAgentState
+{
+    /**%apidoc:string
+     * Device id (can be obtained from "id", "physicalId" or "logicalId" field via
+     * `GET /rest/v{1-}/devices`) or MAC address (not supported for certain cameras).
+     */
+    QnUuid id;
+    QnUuid engineId;
+    bool isEnabled = false;
+
+    DeviceAgentState() = default;
+    DeviceAgentState(const QnUuid& id): id(id) {}
+
+    QnUuid getId() const { return id; }
+};
+#define DeviceAgentState_Fields (id)(engineId)(isEnabled)
+QN_FUSION_DECLARE_FUNCTIONS(DeviceAgentState, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(DeviceAgentState, DeviceAgentState_Fields);
+
+} // namespace nx::vms::api::analytics
