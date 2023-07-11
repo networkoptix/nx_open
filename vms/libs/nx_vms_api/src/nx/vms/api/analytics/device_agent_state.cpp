6082f9f2dc704c27c164de06186f77257fc21091
Maksim Tikhomirov <mtikhomirov@networkoptix.com>
2023-07-11 13:49:04 +0000

    VMS-40380: Create DeviceAgent API handler stub
    VMS-40380: Create DeviceAgent API handler stub(initial commit)

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/device_agent_state.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "device_agent_state.h"
+
+#include <nx/fusion/model_functions.h>
+
+namespace nx::vms::api::analytics {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(DeviceAgentState, (json), DeviceAgentState_Fields)
+
+} // namespace nx::vms::api::analytics
