e6841e334e860d8ef296c6816c2d5ffaba33e6f0
Sergei Kolesnik <skolesnik@networkoptix.com>
2023-07-17 22:52:59 +0000

    VMS-39043: Server API: Add Virtual Device Handlers
    Closes VMS-39043
    
    - Implemented test suite and rest virtual device handlers in db and mediaserver modules.
    - Deprecated `/api/virtualCamera` legacy handler.
    - Partially refactored API for Test Server. 
        - Added possibility for populating URI parameters from a request data, if `Request` type is serializable. 
        - Removed `server::get` overload that accepts expected http status code. Refactored dependent code to use `expectGet` instead.

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/virtual_device_model.h
@@ -0,0 +1,166 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <chrono>
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/reflect/instrument.h>
+#include <nx/utils/uuid.h>
+
+namespace nx::vms::api {
+
+struct NX_VMS_API FlexibleId
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+};
+#define FlexibleId_Fields (id)
+QN_FUSION_DECLARE_FUNCTIONS(FlexibleId, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(FlexibleId, FlexibleId_Fields);
+
+struct NX_VMS_API VirtualDeviceCreate
+{
+    /**%apidoc Name of the virtual Device to be created. */
+    QString name;
+};
+#define VirtualDeviceCreate_Fields (name)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceCreate, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceCreate, VirtualDeviceCreate_Fields);
+
+struct NX_VMS_API VirtualDeviceLockInfo
+{
+    /**%apidoc User who owns the lock. */
+    QnUuid userId;
+
+    /**%apidoc Lock token. */
+    QnUuid token;
+
+    /**%apidoc Lock timeout in milliseconds. */
+    std::chrono::milliseconds ttlMs;
+
+    /**%apidoc Consume progress, if ongoing. */
+    std::optional<int> progress = 0;
+};
+#define VirtualDeviceLockInfo_Fields (userId)(token)(ttlMs)(progress)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceLockInfo, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceLockInfo, VirtualDeviceLockInfo_Fields);
+
+struct NX_VMS_API VirtualDeviceStatus
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+
+    /**%apidoc Lock state information, if the Device is locked. */
+    std::optional<VirtualDeviceLockInfo> lockInfo;
+};
+#define VirtualDeviceStatus_Fields (id)(lockInfo)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceStatus, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceStatus, VirtualDeviceStatus_Fields);
+
+struct NX_VMS_API VirtualDeviceFootageElement
+{
+    /**%apidoc:integer Footage element size in Bytes.
+     * %example 4096
+     */
+    double sizeB = 0;
+
+    /**%apidoc Start time in milliseconds.
+     * %example 0
+     */
+    std::chrono::milliseconds startTimeMs{0};
+
+    /**%apidoc
+     * Duration in milliseconds.
+     * %example 60000
+     */
+    std::chrono::milliseconds durationMs{0};
+};
+#define VirtualDeviceFootageElement_Fields (sizeB)(startTimeMs)(durationMs)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceFootageElement, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceFootageElement, VirtualDeviceFootageElement_Fields);
+
+struct NX_VMS_API VirtualDevicePrepare
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+
+    /**%apidoc[opt] Footage to upload. */
+    std::vector<VirtualDeviceFootageElement> footage;
+};
+#define VirtualDevicePrepare_Fields (id)(footage)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDevicePrepare, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDevicePrepare, VirtualDevicePrepare_Fields);
+
+struct VirtualDevicePrepareReply
+{
+    std::vector<VirtualDeviceFootageElement> footage;
+    bool storageCleanupNeeded = false;
+    bool storageFull = false;
+};
+#define VirtualDevicePrepareReply_Fields (footage)(storageCleanupNeeded)(storageFull)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDevicePrepareReply, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDevicePrepareReply, VirtualDevicePrepareReply_Fields);
+
+struct NX_VMS_API VirtualDeviceConsume
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+
+    /**%apidoc Token acquired when the virtual Device was locked. */
+    QnUuid token;
+
+    /**%apidoc Name of the previously uploaded file. */
+    QString uploadId;
+
+    /**%apidoc Starting time of the file in milliseconds since epoch.
+     * %example 0
+     */
+    std::chrono::milliseconds startTimeMs;
+};
+#define VirtualDeviceConsume_Fields (id)(token)(uploadId) (startTimeMs)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceConsume, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceConsume, VirtualDeviceConsume_Fields);
+
+struct NX_VMS_API VirtualDeviceLock
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+
+    // TODO: (?) add optional `token` to be able to extend the lock
+
+    /**%apidoc Id of a user to acquire the lock for. Should only be used by an admininstrator. */
+    std::optional<QnUuid> userId;
+
+    /**%apidoc Lock timeout in milliseconds.
+     * %example 60000
+     */
+    std::chrono::milliseconds ttlMs;
+};
+#define VirtualDeviceLock_Fields (id)(userId)(ttlMs)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceLock, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceLock, VirtualDeviceLock_Fields);
+
+// TODO: (?) this can be done using `lock`
+struct NX_VMS_API VirtualDeviceExtend: VirtualDeviceLock
+{
+    /**%apidoc Token acquired when the virtual Device was locked. */
+    QnUuid token;
+};
+#define VirtualDeviceExtend_Fields VirtualDeviceLock_Fields(token)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceExtend, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceExtend, VirtualDeviceExtend_Fields);
+
+struct NX_VMS_API VirtualDeviceRelease
+{
+    /**%apidoc Virtual Device id. */
+    QString id;
+
+    /**%apidoc Token acquired when the virtual Device was locked. */
+    QnUuid token;
+};
+#define VirtualDeviceRelease_Fields (id)(token)
+QN_FUSION_DECLARE_FUNCTIONS(VirtualDeviceRelease, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(VirtualDeviceRelease, VirtualDeviceRelease_Fields);
+
+} // namespace nx::vms::api
