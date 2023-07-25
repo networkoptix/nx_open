cb8fb101ef36b435d150c0f493904bb18d57d901
Mikhail Uskov <muskov@networkoptix.com>
2023-07-26 02:40:15 +0300

    VMS-40847: VMS Server API: Remove deprecated LDAP API
    (cherry picked from commit f085aaeaede81da935498d93de1247d023cf3056)

--- vms/libs/nx_vms_api/src/nx/vms/api/data/ldap.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/ldap.cpp
@@ -6,27 +6,17 @@
 
 namespace nx::vms::api {
 
-bool LdapSettingsBase::isValid(bool checkPassword) const
+bool LdapSettings::isValid(bool checkPassword) const
 {
     return !uri.isEmpty()
         && (!checkPassword || !(adminDn.isEmpty() ^ adminPassword.value_or(QString()).isEmpty()));
 }
 
-int LdapSettingsBase::defaultPort(bool useSsl)
+int LdapSettings::defaultPort(bool useSsl)
 {
     constexpr int kDefaultLdapPort = 389;
     constexpr int kDefaultLdapSslPort = 636;
     return useSsl ? kDefaultLdapSslPort : kDefaultLdapPort;
-}
-
-LdapSettingsDeprecated::LdapSettingsDeprecated(LdapSettings settings)
-{
-    static_cast<LdapSettingsBase&>(*this) = static_cast<LdapSettingsBase&&>(settings);
-    if (!settings.filters.empty())
-    {
-        searchBase = settings.filters[0].base;
-        searchFilter = settings.filters[0].filter;
-    }
 }
 
 QString LdapSettings::syncId() const
@@ -47,11 +37,9 @@ QString LdapSettings::syncId() const
     return QnUuid::fromArbitraryData(data.toUtf8()).toSimpleString();
 }
 
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapSettingsDeprecated, (json), LdapSettingsDeprecated_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapSettingSearchFilter, (json), LdapSettingSearchFilter_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapSettings, (json), LdapSettings_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapSettingsChange, (json), LdapSettingsChange_Fields)
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapUser, (json), LdapUser_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(LdapStatus, (json), LdapStatus_Fields)
 
 } // namespace nx::vms::api
