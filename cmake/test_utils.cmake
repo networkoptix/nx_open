4518ea9aba635e268b30b08371bc7184bd0357f1
Sergei Ivanov <sivanov@networkoptix.com>
2023-08-02 22:20:23 +0300

    VMS-40694: Add epic task key to unit tests mapping file
    (cherry picked from commit 316666dbc1279c26aef7b29005dfd0016c021a75)
    (cherry picked from commit 62f1da407e0b58a25f323e40ed055c91180a3870)

--- cmake/test_utils.cmake
+++ cmake/test_utils.cmake
@@ -6,40 +6,13 @@ add_custom_target(unit_tests)
 set_target_properties(unit_tests PROPERTIES FOLDER utils)
 
 set(testTempDirectory "${CMAKE_BINARY_DIR}" CACHE STRING "Temp directory for running tests.")
-set(testMetaInformationFile "${CMAKE_BINARY_DIR}/unit_tests_info.yml")
-file(WRITE ${testMetaInformationFile} "")
-nx_store_known_file(${testMetaInformationFile})
 
-function(nx_store_test_metainformation target)
-    set(oneValueArgs PROJECT COMPONENT)
-    cmake_parse_arguments(NX_TEST_INFO
-        #[[options]] ""
-        "${oneValueArgs}"
-        #[[multi_value_keywords]] ""
-        ${ARGN})
+include(test_metainformation)
 
-    if(NOT NX_TEST_INFO_PROJECT)
-        message(FATAL_ERROR
-            "Add PROJECT parameter with a related Jira project to target ${target}")
-    endif()
-
-    if(NX_TEST_INFO_COMPONENT)
-        file(APPEND ${testMetaInformationFile}
-            "${target}: { project: ${NX_TEST_INFO_PROJECT}, component: ${NX_TEST_INFO_COMPONENT} }\n")
-    else()
-        if(NX_TEST_INFO_PROJECT STREQUAL "VMS")
-            message(FATAL_ERROR
-                "Add COMPONENT parameter with a related Jira component to target ${target}")
-        endif()
-        file(APPEND ${testMetaInformationFile}
-            "${target}: { project: ${NX_TEST_INFO_PROJECT} }\n")
-    endif()
-
-endfunction()
 
 function(nx_add_test target) # [NO_GTEST] [NO_QT] [NO_NX_UTILS] ...
     set(options NO_GTEST NO_QT NO_NX_UTILS)
-    set(oneValueArgs FOLDER PROJECT COMPONENT)
+    set(oneValueArgs FOLDER PROJECT COMPONENT EPIC)
     cmake_parse_arguments(NX_ADD_TEST "${options}"
         "${oneValueArgs}" #[[multi_value_keywords]] "" ${ARGN})
 
@@ -87,7 +60,8 @@ function(nx_add_test target) # [NO_GTEST] [NO_QT] [NO_NX_UTILS] ...
     # Write unit test metainformation.
     nx_store_test_metainformation(${target}
         PROJECT ${NX_ADD_TEST_PROJECT}
-        COMPONENT ${NX_ADD_TEST_COMPONENT})
+        COMPONENT ${NX_ADD_TEST_COMPONENT}
+        EPIC ${NX_ADD_TEST_EPIC})
 
     if(WINDOWS)
         # Adding ${CMAKE_MSVCIDE_RUN_PATH} to PATH for running unit tests via CTest.
