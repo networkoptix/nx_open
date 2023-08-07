bad590efa9f9389d4946715f0417f989fef73635
Petr Privalov <pprivalov@networkoptix.com>
2023-08-07 15:03:05 +0000

    VMS-40172: Add verification for Email Username and Password
    (cherry picked from commit 29ad943388b38509702ca1d41fe7bd405e4bf014)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/widgets/outgoing_mail_settings_widget.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/widgets/outgoing_mail_settings_widget.h
@@ -19,6 +19,7 @@ public:
 
     virtual void loadDataToUi() override;
     virtual void applyChanges() override;
+    virtual bool canApplyChanges() const override;
     virtual bool hasChanges() const override;
 
 protected:
