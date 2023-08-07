7512791059b948d9ed88532336fde27d8354ab7b
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2023-08-06 18:04:21 -0700

    VMS-40931: Allow to use different suffixes in TimeDuration component
    (cherry picked from commit d85acc9d7ed87db203e43f25b065389e22ebcf9c)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/models/time_duration_suffix_model.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/models/time_duration_suffix_model.cpp
@@ -25,14 +25,16 @@ QVariant TimeDurationSuffixModel::data(const QModelIndex& index, int role) const
     if (index.row() < 0 || index.row() >= rowCount() || role != Qt::DisplayRole)
         return {};
 
-    switch ((Suffix) index.row())
+    switch ((Suffix) m_suffixes.at(index.row()))
     {
-        case Suffix::secondsIndex:
+        case Suffix::seconds:
             return QnTimeStrings::fullSuffix(QnTimeStrings::Suffix::Seconds, m_value);
-        case Suffix::minutesIndex:
+        case Suffix::minutes:
             return QnTimeStrings::fullSuffix(QnTimeStrings::Suffix::Minutes, m_value);
-        case Suffix::hoursIndex:
+        case Suffix::hours:
             return QnTimeStrings::fullSuffix(QnTimeStrings::Suffix::Hours, m_value);
+        case Suffix::days:
+            return QnTimeStrings::fullSuffix(QnTimeStrings::Suffix::Days, m_value);
 
         default:
             break;
@@ -43,23 +45,31 @@ QVariant TimeDurationSuffixModel::data(const QModelIndex& index, int role) const
 
 int TimeDurationSuffixModel::multiplierAt(int row)
 {
-    switch ((Suffix) row)
+    if (row < 0 || row >= m_suffixes.size())
+        return -1;
+
+    switch ((Suffix) m_suffixes.at(row))
     {
-        case Suffix::minutesIndex:
+        case Suffix::minutes:
             return 60;
 
-        case Suffix::hoursIndex:
+        case Suffix::hours:
             return 3600;
 
-        case Suffix::secondsIndex:
-        default:
+        case Suffix::days:
+            return 3600 * 24;
+
+        case Suffix::seconds:
             return 1;
+
+        default:
+            return -1;
     }
 }
 
 int TimeDurationSuffixModel::rowCount(const QModelIndex&) const
 {
-    return (int) Suffix::count;
+    return m_suffixes.size();
 }
 
 void TimeDurationSuffixModel::setValue(int value)
@@ -71,6 +81,44 @@ void TimeDurationSuffixModel::setValue(int value)
     emit valueChanged();
 
     emit dataChanged(index(0), index(rowCount() - 1), {Qt::DisplayRole});
+}
+
+QList<int> TimeDurationSuffixModel::suffixes() const
+{
+    return m_suffixes;
+}
+
+void TimeDurationSuffixModel::setSuffixes(const QList<int> &suffixes)
+{
+    if (m_suffixes == suffixes)
+        return;
+
+    const int diff = suffixes.size() - m_suffixes.size();
+    if (diff > 0)
+    {
+        beginInsertRows({}, m_suffixes.size(), m_suffixes.size() + diff - 1);
+        m_suffixes = suffixes;
+        endInsertRows();
+    }
+    else if (diff < 0)
+    {
+        beginRemoveRows({}, m_suffixes.size() + diff, m_suffixes.size() - 1);
+        m_suffixes = suffixes;
+        endRemoveRows();
+    }
+    else
+    {
+        m_suffixes = suffixes;
+    }
+
+    if (m_suffixes.size() > 0)
+    {
+        emit dataChanged(
+            this->index(0, 0),
+            this->index(m_suffixes.size() - 1, 0));
+    }
+
+    emit suffixesChanged();
 }
 
 } // namespace nx::vms::client::desktop
