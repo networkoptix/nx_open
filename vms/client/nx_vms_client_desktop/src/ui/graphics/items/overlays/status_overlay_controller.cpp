a5da1c1beca8b7c81f61a60ea64ace2343ec4b72
Victor Breus <vbreus@networkoptix.com>
2023-08-23 11:39:20 +0000

    VMS-41047: Do not check for Video Wall licenses for systems with SaaS licensing on the Client

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/overlays/status_overlay_controller.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/overlays/status_overlay_controller.cpp
@@ -268,6 +268,8 @@ QString QnStatusOverlayController::captionText(Qn::ResourceStatusOverlay overlay
         { Qn::OldFirmwareOverlay, tr("UNSUPPORTED FIRMWARE VERSION") },
         { Qn::CannotDecryptMediaOverlay, tr("ARCHIVE ENCRYPTED") },
         { Qn::InformationRequiredOverlay, tr("INFORMATION REQUIRED") },
+        { Qn::SaasSuspended, tr("SAAS SUSPENDED") },
+        { Qn::SaasShutDown, tr("SAAS SHUT DOWN") },
     };
     return extractValue(overlay, kCaptions);
 }
@@ -317,6 +319,8 @@ QString QnStatusOverlayController::statusIconPath(Qn::ResourceStatusOverlay over
         { Qn::PasswordRequiredOverlay, "item_placeholders/alert.png" },
         { Qn::CannotDecryptMediaOverlay, "item_placeholders/encrypted.svg" },
         { Qn::InformationRequiredOverlay, "item_placeholders/alert.png" },
+        { Qn::SaasSuspended, "item_placeholders/offline.svg" }, //< TODO: #vbreus Change icon if needed.
+        { Qn::SaasShutDown, "item_placeholders/offline.png" },
     };
 
     return extractValue(overlay, kIconPaths);
