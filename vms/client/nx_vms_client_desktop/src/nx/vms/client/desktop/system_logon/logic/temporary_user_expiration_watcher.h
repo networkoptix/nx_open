491fe8f22a60ac6953108528a1e99665efc1b4c3
Evgenij Sobolev <esobolev@networkoptix.com>
2023-08-24 16:00:20 +0000

    VMS-41143: Add informer in the right panel to Temporary User

---
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_logon/logic/temporary_user_expiration_watcher.h
@@ -0,0 +1,39 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <chrono>
+#include <memory>
+#include <optional>
+
+#include <QtCore/QObject>
+#include <QtCore/QPointer>
+#include <QtCore/QTimer>
+
+#include <nx/utils/uuid.h>
+
+namespace nx::vms::client::desktop::workbench {
+class LocalNotificationsManager;
+}
+
+namespace nx::vms::client::desktop {
+
+class TemporaryUserExpirationWatcher: public QObject
+{
+    Q_OBJECT
+
+public:
+    TemporaryUserExpirationWatcher(
+        QPointer<workbench::LocalNotificationsManager> notificationManager, QObject* parent);
+
+private:
+    void createNotification();
+    void updateNotification();
+
+private:
+    QPointer<workbench::LocalNotificationsManager> m_notificationManager;
+    std::optional<QnUuid> m_notification;
+    QTimer m_timer;
+};
+
+} // namespace nx::vms::client::desktop
