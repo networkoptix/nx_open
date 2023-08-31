fd4bedbf09787f5b54fe4a1e35d78672b91fe49d
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2023-08-31 04:21:52 +0300

    CB-2039: fix building go-written components

--- cmake/go/compiler.cmake
+++
@@ -1,7 +0,0 @@
-## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
-
-include_guard()
-
-if(NOT NX_GO_COMPILER)
-    set(NX_GO_COMPILER "${CONAN_GO_ROOT}/bin/go")
-endif()
