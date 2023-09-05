1b7a5c4c1e6bbe856b20159e76060bff8bdd1cfb
Roman Vasilenko <rvasilenko@networkoptix.com>
2023-09-05 18:08:20 +0000

    VMS-40925: Send request api/configure to the each server of the System during merge

--- vms/libs/nx_vms_api/src/nx/vms/api/data/merge_status_reply.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/merge_status_reply.h
@@ -5,6 +5,8 @@
 #include <nx/utils/uuid.h>
 #include <nx/vms/api/data/data_macros.h>
 
+#include "server_merge_data.h"
+
 namespace nx::vms::api {
 
 /**%apidoc Details about the last Merge operation. */
@@ -15,8 +17,14 @@ struct NX_VMS_API MergeStatusReply
 
     /**%apidoc Whether the last Merge operation is in progress. */
     bool mergeInProgress = false;
+
+    /**%apidoc List of unmerged servers.
+     * SystemMergeHistory record is not inserted if the list
+     * is not empty to prevent removing slave system on the cloud side.
+     */
+    std::vector<ServerMergeData> unmergedServers;
 };
-#define MergeStatusReply_Fields (mergeId)(mergeInProgress)
+#define MergeStatusReply_Fields (mergeId)(mergeInProgress)(unmergedServers)
 NX_VMS_API_DECLARE_STRUCT(MergeStatusReply)
 
 } // namespace nx::vms::api
