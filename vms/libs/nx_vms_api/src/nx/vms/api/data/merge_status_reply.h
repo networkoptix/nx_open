3d1e1a3a50882d63d977fc81a54eca0622c899b2
Mikhail Uskov <muskov@networkoptix.com>
2023-09-09 12:36:08 +0300

    VMS-40925: Revert Send request api/configure to the each server of the System during merge
    This reverts commit 2256dc5c3718f5d2346f33e76b5e410bc96cec9f.
    
    (cherry picked from commit bd901f2999af403a0ded5480f22d28a7aabe543e)

--- vms/libs/nx_vms_api/src/nx/vms/api/data/merge_status_reply.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/merge_status_reply.h
@@ -5,8 +5,6 @@
 #include <nx/utils/uuid.h>
 #include <nx/vms/api/data/data_macros.h>
 
-#include "server_merge_data.h"
-
 namespace nx::vms::api {
 
 /**%apidoc Details about the last Merge operation. */
@@ -17,14 +15,8 @@ struct NX_VMS_API MergeStatusReply
 
     /**%apidoc Whether the last Merge operation is in progress. */
     bool mergeInProgress = false;
-
-    /**%apidoc List of unmerged servers.
-     * SystemMergeHistory record is not inserted if the list
-     * is not empty to prevent removing slave system on the cloud side.
-     */
-    std::vector<ServerMergeData> unmergedServers;
 };
-#define MergeStatusReply_Fields (mergeId)(mergeInProgress)(unmergedServers)
+#define MergeStatusReply_Fields (mergeId)(mergeInProgress)
 NX_VMS_API_DECLARE_STRUCT(MergeStatusReply)
 
 } // namespace nx::vms::api
