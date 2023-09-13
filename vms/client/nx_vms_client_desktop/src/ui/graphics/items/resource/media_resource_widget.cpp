6659a1c5256353a1060911b70cdf8b9a3ef59f87
Anton Ivashchenko <aivashchenko@networkoptix.com>
2023-09-13 00:33:25 +0000

    VMS-41540: Fix required permissions for I/O modules

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
@@ -2379,6 +2379,9 @@ Qn::ResourceStatusOverlay QnMediaResourceWidget::calculateStatusOverlay() const
         const auto calculateRequiredPermissions =
             [this]() -> Qn::Permissions
             {
+                if (d->isIoModule)
+                    return Qn::ViewContentPermission;
+
                 Qn::Permissions requiredPermissions = d->isPlayingLive()
                     ? Qn::ViewLivePermission
                     : Qn::ViewFootagePermission;
@@ -2411,6 +2414,22 @@ Qn::ResourceStatusOverlay QnMediaResourceWidget::calculateStatusOverlay() const
     if (d->isUnauthorized())
         return Qn::UnauthorizedOverlay;
 
+    if (d->isIoModule)
+    {
+        if (!d->isPlayingLive())
+        {
+            return d->display()->camDisplay()->isLongWaiting()
+                ? Qn::NoDataOverlay
+                : Qn::NoVideoDataOverlay;
+        }
+
+        if (m_ioCouldBeShown) /// If widget could be shown then licenses Ok
+            return Qn::EmptyOverlay;
+
+        if (!NX_ASSERT(d->camera) || !d->camera->isScheduleEnabled())
+            return Qn::IoModuleDisabledOverlay;
+    }
+
     if (d->camera)
     {
         if (d->isPlayingLive()
@@ -2428,22 +2447,6 @@ Qn::ResourceStatusOverlay QnMediaResourceWidget::calculateStatusOverlay() const
             if (!ResourceAccessManager::hasPermissions(d->camera, requiredPermission))
                 return Qn::AnalogWithoutLicenseOverlay;
         }
-    }
-
-    if (d->isIoModule)
-    {
-        if (!d->isPlayingLive())
-        {
-            if (d->display()->camDisplay()->isLongWaiting())
-                return Qn::NoDataOverlay;
-            return Qn::NoVideoDataOverlay;
-        }
-
-        if (m_ioCouldBeShown) /// If widget could be shown then licenses Ok
-            return Qn::EmptyOverlay;
-
-        if (!NX_ASSERT(d->camera) || !d->camera->isScheduleEnabled())
-            return Qn::IoModuleDisabledOverlay;
     }
 
     const auto mediaEvent = d->display()->camDisplay()->lastMediaEvent();
