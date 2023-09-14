b065cf3e85070b62461a83db164a3b835f9e5f00
Sergei Ivanov <sivanov@networkoptix.com>
2023-09-14 13:21:28 +0000

    VMS-41642: Fix Advanced tab is not displayed in Camera Settings
    In some rare cases server failed to reply on camera manifest request. Client will try to re-request it.
    
    Added logging of such requests, implemented request cancelling on client disconnect.
    
    (cherry picked from commit f7a3d88302d52337e9674b364928f6adbbc83ffd)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/camera/camera_advanced_parameters_manifest_manager.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/camera/camera_advanced_parameters_manifest_manager.cpp
@@ -3,19 +3,18 @@
 #include "camera_advanced_parameters_manifest_manager.h"
 
 #include <api/server_rest_connection.h>
-
 #include <core/resource/camera_resource.h>
 #include <core/resource/media_server_resource.h>
-
 #include <nx/network/rest/params.h>
-
 #include <nx/utils/guarded_callback.h>
 #include <nx/utils/log/log.h>
+#include <nx/vms/client/desktop/system_context.h>
 
 namespace nx::vms::client::desktop {
 
 struct CameraAdvancedParametersManifestManager::Private
 {
+    // Store manifests based on camera id, knowing id will not differ between various systems.
     QHash<QnUuid, QnCameraAdvancedParams> manifestsByCameraId;
 };
 
@@ -42,28 +41,41 @@ std::optional<QnCameraAdvancedParams> CameraAdvancedParametersManifestManager::m
     return *iter;
 }
 
-void CameraAdvancedParametersManifestManager::loadManifestAsync(
+rest::Handle CameraAdvancedParametersManifestManager::loadManifestAsync(
     const QnVirtualCameraResourcePtr& camera)
 {
     if (!NX_ASSERT(camera))
-        return;
+        return 0;
 
-    if (!NX_ASSERT(connection()))
-        return;
+    auto systemContext = SystemContext::fromResource(camera);
+    if (!NX_ASSERT(systemContext))
+        return 0;
+
+    auto api = systemContext->connectedServerApi();
+    if (!NX_ASSERT(api))
+        return 0;
 
     auto callback = nx::utils::guarded(this,
-        [this, camera](bool success, auto /*handle*/, auto result, auto /*headers*/)
+        [this, camera](bool success, auto handle, auto result, auto /*headers*/)
         {
             if (!success)
+            {
+                NX_WARNING(this, "Manifest was not loaded for %1", camera);
+                emit manifestLoadingFailed(handle, camera);
                 return;
+            }
 
+            NX_VERBOSE(this, "Manifest loaded successfully for %1", camera);
             QnCameraAdvancedParams manifest;
             QJson::deserialize(result, &manifest);
             d->manifestsByCameraId[camera->getId()] = manifest;
-            emit manifestLoaded(camera, manifest);
+            NX_VERBOSE(this, "Groups count is %1", manifest.groups.size());
+
+            emit manifestLoaded(handle, camera, manifest);
         });
 
-    connectedServerApi()->getRawResult(
+    NX_VERBOSE(this, "Requesting analytics manifest for camera %1", camera);
+    return api->getRawResult(
         NX_FMT("/rest/v2/devices/%1/advanced/*/manifest", camera->getId()),
         /*params*/ {},
         callback,
