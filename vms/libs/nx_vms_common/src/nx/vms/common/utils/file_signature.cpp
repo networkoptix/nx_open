0a7dc6c31cc1a97759ad8b0af30ee3d5607cbf1d
Denis Klychkov <dklychkov@networkoptix.com>
2023-09-14 00:26:40 -0700

    VMS-41588: Verify update files using a signature from the ZIP file comment

--- vms/libs/nx_vms_common/src/nx/vms/common/utils/file_signature.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/common/utils/file_signature.cpp
@@ -6,12 +6,57 @@
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rsa.h>
+#include <quazip/quazip.h>
 
 #include <QtCore/QFile>
+#include <QtCore/QRegularExpression>
 
+#include <nx/utils/log/assert.h>
 #include <nx/utils/type_utils.h>
 
 namespace nx::vms::common {
+
+namespace {
+
+const QString kZipSignaturePrefix = "signature,digest,sha256,base64:";
+
+qint64 getZipCommentStart(const QString& fileName)
+{
+    constexpr int kMaxCommentSize = std::numeric_limits<quint16>::max();
+    const QByteArray kEocdPrefix = "\x50\x4b\x05\x06";
+    constexpr int kEocdSize = 22;
+    // We consider comment length bytes as a part of the comment.
+    constexpr int kCommentPosInEocd = 20;
+
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly))
+        return -1;
+
+    const auto size = file.size();
+
+    if (size < kEocdSize)
+        return -1;
+
+    qint64 pos = size - kEocdSize;
+    file.seek(pos);
+    QByteArray buf = file.read(kEocdPrefix.size());
+    while (pos >= 0 && size - pos < kMaxCommentSize + kEocdSize)
+    {
+        if (buf == kEocdPrefix)
+            return pos + kCommentPosInEocd;
+
+        for (int i = 3; i > 0; --i)
+            buf[i] = buf[i - 1];
+
+        --pos;
+        file.seek(pos);
+        file.read(buf.data(), 1);
+    }
+
+    return -1;
+}
+
+} // namespace
 
 QByteArray FileSignature::readKey(const QString& fileName)
 {
@@ -26,6 +71,23 @@ QByteArray FileSignature::readKey(const QString& fileName)
     return file.readAll();
 }
 
+std::optional<QByteArray> FileSignature::readSignatureFromZipFileComment(const QString& fileName)
+{
+    QuaZip zip(fileName);
+    if (!zip.open(QuaZip::mdUnzip))
+        return std::nullopt;
+
+    QRegularExpression regex(
+        nx::format("^%1(.*)?(\n|\r|$)", kZipSignaturePrefix), QRegularExpression::MultilineOption);
+
+    const QString comment = zip.getComment();
+    const QRegularExpressionMatch match = regex.match(comment);
+    if (!match.isValid())
+        return QByteArray();
+
+    return QByteArray::fromBase64(match.captured(1).toLatin1());
+}
+
 std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
     const QString& fileName, const QByteArray& privateKey)
 {
@@ -37,7 +99,7 @@ std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
 }
 
 std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
-    QIODevice& inputDevice, const QByteArray& privateKey)
+    QIODevice& inputDevice, const QByteArray& privateKey, qint64 size)
 {
     if (privateKey.isEmpty())
         return FileSignature::Result::failed;
@@ -59,9 +121,10 @@ std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
     constexpr int kBufferSize = 1024 * 1024;
     QByteArray buffer(kBufferSize, Qt::Uninitialized);
 
-    while (!inputDevice.atEnd())
+    while (!inputDevice.atEnd() && size != 0)
     {
-        const qint64 bytesRead = inputDevice.read(buffer.data(), kBufferSize);
+        qint64 bytesToRead = size >= 0 ? std::min<qint64>(kBufferSize, size) : kBufferSize;
+        const qint64 bytesRead = inputDevice.read(buffer.data(), bytesToRead);
         if (bytesRead < 0)
             return Result::ioError;
         if (bytesRead == 0)
@@ -69,6 +132,9 @@ std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
 
         if (EVP_DigestSignUpdate(ctx.get(), buffer, bytesRead) <= 0)
             return Result::internalError;
+
+        if (size > 0)
+            size -= bytesRead;
     }
 
     size_t signatureSize = 0;
@@ -84,6 +150,33 @@ std::variant<QByteArray, FileSignature::Result> FileSignature::sign(
     return signature;
 }
 
+FileSignature::Result FileSignature::signZipFile(
+    const QString& fileName, const QByteArray& privateKey)
+{
+    const qint64 commentPos = getZipCommentStart(fileName);
+    if (commentPos == -1)
+        return Result::ioError;
+
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly))
+        return Result::ioError;
+
+    const auto result = sign(file, privateKey, commentPos);
+
+    if (std::holds_alternative<Result>(result))
+        return std::get<Result>(result);
+
+    const QByteArray signature = std::get<QByteArray>(result);
+
+    QuaZip zip(fileName);
+    if (!zip.open(QuaZip::mdAdd))
+        return Result::ioError;
+
+    zip.setComment(kZipSignaturePrefix + signature.toBase64());
+
+    return Result::ok;
+}
+
 FileSignature::Result FileSignature::verify(
     const QString& fileName, const QByteArray& publicKey, const QByteArray& signature)
 {
@@ -95,7 +188,7 @@ FileSignature::Result FileSignature::verify(
 }
 
 FileSignature::Result FileSignature::verify(
-    QIODevice& inputDevice, const QByteArray& publicKey, const QByteArray& signature)
+    QIODevice& inputDevice, const QByteArray& publicKey, const QByteArray& signature, qint64 size)
 {
     if (signature.isEmpty() || publicKey.isEmpty())
         return FileSignature::Result::failed;
@@ -117,9 +210,10 @@ FileSignature::Result FileSignature::verify(
     constexpr int kBufferSize = 1024 * 1024;
     QByteArray buffer(kBufferSize, Qt::Uninitialized);
 
-    while (!inputDevice.atEnd())
+    while (!inputDevice.atEnd() && size != 0)
     {
-        const qint64 bytesRead = inputDevice.read(buffer.data(), kBufferSize);
+        qint64 bytesToRead = size >= 0 ? std::min<qint64>(kBufferSize, size) : kBufferSize;
+        const qint64 bytesRead = inputDevice.read(buffer.data(), bytesToRead);
         if (bytesRead < 0)
             return Result::ioError;
         if (bytesRead == 0)
@@ -127,12 +221,44 @@ FileSignature::Result FileSignature::verify(
 
         if (EVP_DigestVerifyUpdate(ctx.get(), buffer, bytesRead) <= 0)
             return Result::internalError;
+
+        if (size > 0)
+            size -= bytesRead;
     }
 
     const int result = EVP_DigestVerifyFinal(
         ctx.get(), (const unsigned char*) signature.constData(), signature.size());
 
     return result == 1 ? Result::ok : Result::failed;
+}
+
+FileSignature::Result FileSignature::verifyZipFile(
+    const QString& fileName, const QByteArray& publicKey, QByteArray signature)
+{
+    if (publicKey.isEmpty())
+        return Result::failed;
+
+    if (signature.isEmpty())
+    {
+        const auto result = readSignatureFromZipFileComment(fileName);
+        if (!result)
+            return Result::ioError;
+
+        signature = *result;
+    }
+
+    if (signature.isEmpty())
+        return Result::failed;
+
+    const qint64 commentPos = getZipCommentStart(fileName);
+    if (!NX_ASSERT(commentPos != -1))
+        return Result::failed;
+
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly))
+        return Result::ioError;
+
+    return verify(file, publicKey, signature, commentPos);
 }
 
 QString toString(FileSignature::Result value)
--- vms/libs/nx_vms_common/src/nx/vms/common/utils/file_signature.h
+++ vms/libs/nx_vms_common/src/nx/vms/common/utils/file_signature.h
@@ -24,21 +24,36 @@ public:
 
     static QByteArray readKey(const QString& fileName);
 
+    static std::optional<QByteArray> readSignatureFromZipFileComment(const QString& fileName);
+
     /** Sign the file using RSA private key. */
     static std::variant<QByteArray, Result> sign(
         const QString& fileName, const QByteArray& privateKey);
 
     /** Sign data using RSA private key. */
     static std::variant<QByteArray, Result> sign(
-        QIODevice& inputDevice, const QByteArray& privateKey);
+        QIODevice& inputDevice, const QByteArray& privateKey, qint64 size = -1);
+
+    /** Sign ZIP file using RSA private key. */
+    static Result signZipFile(const QString& fileName, const QByteArray& privateKey);
 
     /** Verify file signature using RSA public key. */
     static Result verify(
         const QString& fileName, const QByteArray& publicKey, const QByteArray& signature);
 
-    /** Verify data signature using RSA public key. */
+    /**
+     * Verify data signature using RSA public key.
+     * @param size Number of bytes to verify. If negative value is specified, verify all bytes.
+     */
     static Result verify(
-        QIODevice& inputDevice, const QByteArray& publicKey, const QByteArray& signature);
+        QIODevice& inputDevice,
+        const QByteArray& publicKey,
+        const QByteArray& signature,
+        qint64 size = -1);
+
+    /** Verify ZIP file signature using RSA public key. */
+    static Result verifyZipFile(
+        const QString& fileName, const QByteArray& publicKey, QByteArray signature = {});
 };
 
 NX_VMS_COMMON_API QString toString(FileSignature::Result value);
