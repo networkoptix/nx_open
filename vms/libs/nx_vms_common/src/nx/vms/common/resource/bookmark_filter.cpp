7a22fa0436fbd59b7f27793fc300fbbdb123bdcc
Anton Malov <amalov@networkoptix.com>
2023-09-17 20:34:20 +0000

    VMS-41284: Optimize bookmark processing
    * Eliminate redundant regex compilation on bookmark text filtering.
    * Move bookmark text filtering out of the UI thread.
    * Move bookmark response parsing out of the UI thread.
    
    (cherry picked from commit b43cf6c7eb9626bd7a0ea68e11ad31bdcc6d548d)

---
+++ vms/libs/nx_vms_common/src/nx/vms/common/resource/bookmark_filter.cpp
@@ -0,0 +1,51 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "bookmark_filter.h"
+
+#include <core/resource/camera_bookmark.h>
+
+namespace nx::vms::common {
+
+BookmarkTextFilter::BookmarkTextFilter(const QString& text)
+{
+    for (const QString& word: text.split(QRegularExpression("[\\W]"), Qt::SkipEmptyParts))
+    {
+        m_words.emplace_back(word, QRegularExpression(QString("\\b%1").arg(word),
+            QRegularExpression::CaseInsensitiveOption));
+    }
+};
+
+bool BookmarkTextFilter::operator()(const QnCameraBookmark& bookmark) const
+{
+    return match(bookmark);
+}
+
+bool BookmarkTextFilter::match(const QnCameraBookmark& bookmark) const
+{
+    for (const auto& [word, wordRegExp]: m_words)
+    {
+        bool tagFound = false;
+
+        for (const QString &tag: bookmark.tags)
+            if (tag.startsWith(word, Qt::CaseInsensitive))
+            {
+                tagFound = true;
+                break;
+            }
+
+        if (tagFound)
+            continue;
+
+        if (wordRegExp.match(bookmark.name).hasMatch() ||
+            wordRegExp.match(bookmark.description).hasMatch())
+        {
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+} //namespace nx::vms::common
