2f71f8ed145be068b8102b0d23ea71a587da9236
Alexander Kulikov <akulikov@networkoptix.com>
2023-10-05 14:05:22 -0700

    VMS-41942: Put remoteArchive motion detection checkbox under ini
    (cherry picked from commit 4559e15fa2984f6b5577a8d8d14e0a0380bf129b)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/camera/widgets/camera_expert_settings_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/camera/widgets/camera_expert_settings_widget.cpp
@@ -450,7 +450,8 @@ void CameraExpertSettingsWidget::loadState(const CameraSettingsDialogState& stat
     // Motion detection.
 
     const bool remoteArchiveMdSupported =
-        state.devicesDescription.hasRemoteArchiveCapability == CombinedValue::All;
+        state.devicesDescription.hasRemoteArchiveCapability == CombinedValue::All
+        && ini().enableRemoteArchiveSynchronization;
 
     ui->groupBoxMotionDetection->setVisible(remoteArchiveMdSupported);
 
