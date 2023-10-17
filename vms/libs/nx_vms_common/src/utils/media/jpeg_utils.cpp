9242a0db52c0c34167fdf78d17f976e99cf8d113
Leonid Busygin <lbusygin@networkoptix.com>
2023-10-17 18:03:51 +0300

    VMS-41978: Fix crash when parse JPEG on RTP
    (cherry picked from commit 2948b077e883b64b9685737d34968d33c564424d)
    
    # Conflicts:
    #	open/vms/libs/nx_vms_common/src/utils/media/jpeg_utils.h
    #	open/vms/libs/nx_vms_common/unit_tests/src/utils/media/jpeg_ut.cpp
    #	vms/server/nx_vms_server/src/nx/streaming/rtp/parsers/mjpeg_rtp_parser.cpp

--- vms/libs/nx_vms_common/src/utils/media/jpeg_utils.cpp
+++ vms/libs/nx_vms_common/src/utils/media/jpeg_utils.cpp
@@ -5,10 +5,11 @@
 #include <nx/utils/log/assert.h>
 #include <utils/media/bitStream.h>
 
-namespace nx_jpg
+namespace nx::media::jpeg
 {
-    static const int MARKER_HEADER_SIZE = 2;
-    static const int MARKER_LENGTH_SIZE = 2;
+
+static const int MARKER_HEADER_SIZE = 2;
+static const int MARKER_LENGTH_SIZE = 2;
 
 struct Component
 {
@@ -150,122 +151,122 @@ AVPixelFormat parsePixelFormatFromSof(const quint8* data, size_t size)
     return convertYuv(pixFmtMask, bits);
 }
 
-    QString toString(ResultCode resCode)
+QString toString(ResultCode resCode)
+{
+    switch (resCode)
     {
-        switch (resCode)
-        {
-            case succeeded:
-                return "succeeded";
-            case interrupted:
-                return "interrupted";
-            case badData:
-                return "badData";
-            case failed:
-                return "failed";
-            default:
-                return "unknownError";
-        }
-    }
-
-    static bool markerHasBody( int markerType )
-    {
-        return    !((markerType >= 0xD0 && markerType <= 0xD9)   //RSTn, SOI, EOI
-                 || (markerType == 0x01)                         //TEM
-                 );
-    }
-
-    JpegParser::JpegParser()
-    :
-        m_state( waitingStartOfMarker ),
-        m_currentMarkerCode( 0 )
-    {
-    }
-
-    ResultCode JpegParser::parse( const quint8* data, size_t size )
-    {
-        const quint8* dataEnd = data + size;
-        for( const quint8* pos = data; pos < dataEnd; )
-        {
-            switch( m_state )
-            {
-                case waitingStartOfMarker:
-                    if( *pos == 0xff )
-                        m_state = readingMarkerCode;
-                    ++pos;
-                    continue;
-
-                case readingMarkerCode:
-                    m_currentMarkerCode = *pos;
-                    ++pos;
-                    if( markerHasBody(m_currentMarkerCode) )
-                    {
-                        m_state = readingMarkerLength;
-                    }
-                    else
-                    {
-                        if( m_parseHandler )
-                            if( !m_parseHandler( m_currentMarkerCode, 0, pos - data - MARKER_HEADER_SIZE ) )
-                            {
-                                if( m_errorHandler )
-                                    m_errorHandler( interrupted, pos - data - MARKER_HEADER_SIZE );
-                                return interrupted;
-                            }
-                        m_state = waitingStartOfMarker;
-                    }
-                    continue;
-
-                case readingMarkerLength:
-                {
-                    if( pos + 1 >= dataEnd )
-                    {
-                        if( m_errorHandler )
-                            m_errorHandler( badData, pos - data - MARKER_HEADER_SIZE );
-                        return badData;   //error
-                    }
-                    const size_t markerLength = (*pos << 8) | *(pos+1);
-                    if( m_parseHandler )
-                        if( !m_parseHandler( m_currentMarkerCode, markerLength, pos - data - MARKER_HEADER_SIZE ) )
-                        {
-                            if( m_errorHandler )
-                                m_errorHandler( interrupted, pos - data - MARKER_HEADER_SIZE );
-                            return interrupted;
-                        }
-                    //skipping marker body
-                    pos += markerLength;
-                    m_state = waitingStartOfMarker;
-                    break;
-                }
-
-                default:
-                    return failed;
-            }
-        }
-        return succeeded;
-    }
-
-    bool readJpegImageInfo( const quint8* data, size_t size, ImageInfo* const imgInfo )
-    {
-        auto parseHandler = [data, size, imgInfo]( int markerType, size_t markerLength, size_t currentOffset ) -> bool
-        {
-            static const size_t MIN_SOF_SIZE = 5;
-            if( !(markerType == 0xC0 || markerType == 0xC2) )
-                return true;   //not SOF marker, continue parsing
-            if( (markerLength < MARKER_LENGTH_SIZE + MIN_SOF_SIZE) ||       //marker size not enough to read data. marker not recognized?
-                (currentOffset + MARKER_HEADER_SIZE + MARKER_LENGTH_SIZE + MIN_SOF_SIZE >= size) )  //not enough data in source buffer
-            {
-                return false;
-            }
-            const quint8* sofDataStart = data + currentOffset + MARKER_HEADER_SIZE + MARKER_LENGTH_SIZE;
-            imgInfo->precision = sofDataStart[0];
-            imgInfo->height = (sofDataStart[1] << 8) | sofDataStart[2];
-            imgInfo->width = (sofDataStart[3] << 8) | sofDataStart[4];
-            imgInfo->pixelFormat = parsePixelFormatFromSof(data + currentOffset, size);
-            return false;
-        };
-
-        JpegParser parser;
-        parser.setParseHandler(parseHandler);
-        ResultCode rc = parser.parse( data, size );
-        return rc == succeeded || rc == interrupted;
+        case succeeded:
+            return "succeeded";
+        case interrupted:
+            return "interrupted";
+        case badData:
+            return "badData";
+        case failed:
+            return "failed";
+        default:
+            return "unknownError";
     }
 }
+
+static bool markerHasBody(int markerType)
+{
+    return !((markerType >= 0xD0 && markerType <= 0xD9) //RSTn, SOI, EOI
+        || (markerType == 0x01)); //TEM
+}
+
+JpegParser::JpegParser():
+    m_state(waitingStartOfMarker),
+    m_currentMarkerCode(0)
+{}
+
+ResultCode JpegParser::parse(const quint8* data, size_t size)
+{
+    const quint8* dataEnd = data + size;
+    for (const quint8* pos = data; pos < dataEnd;)
+    {
+        switch(m_state)
+        {
+            case waitingStartOfMarker:
+                if (*pos == 0xff)
+                    m_state = readingMarkerCode;
+                ++pos;
+                continue;
+
+            case readingMarkerCode:
+                m_currentMarkerCode = *pos;
+                ++pos;
+                if (markerHasBody(m_currentMarkerCode))
+                {
+                    m_state = readingMarkerLength;
+                }
+                else
+                {
+                    if (m_parseHandler)
+                        if (!m_parseHandler(m_currentMarkerCode, 0, pos - data - MARKER_HEADER_SIZE))
+                        {
+                            if (m_errorHandler)
+                                m_errorHandler(interrupted, pos - data - MARKER_HEADER_SIZE);
+                            return interrupted;
+                        }
+                    m_state = waitingStartOfMarker;
+                }
+                continue;
+
+            case readingMarkerLength:
+            {
+                if (pos + 1 >= dataEnd)
+                {
+                    if (m_errorHandler)
+                        m_errorHandler(badData, pos - data - MARKER_HEADER_SIZE);
+                    return badData;   //error
+                }
+                const size_t markerLength = (*pos << 8) | *(pos+1);
+                if (m_parseHandler)
+                    if (!m_parseHandler(m_currentMarkerCode, markerLength, pos - data - MARKER_HEADER_SIZE))
+                    {
+                        if (m_errorHandler)
+                            m_errorHandler(interrupted, pos - data - MARKER_HEADER_SIZE);
+                        return interrupted;
+                    }
+                //skipping marker body
+                pos += markerLength;
+                m_state = waitingStartOfMarker;
+                break;
+            }
+
+            default:
+                return failed;
+        }
+    }
+    return succeeded;
+}
+
+bool readJpegImageInfo(const quint8* data, size_t size, ImageInfo* const imgInfo)
+{
+    bool parsed = false;
+    auto parseHandler = [&parsed, data, size, imgInfo]( int markerType, size_t markerLength, size_t currentOffset ) -> bool
+    {
+        static const size_t MIN_SOF_SIZE = 5;
+        if( !(markerType == 0xC0 || markerType == 0xC2) )
+            return true;   //not SOF marker, continue parsing
+        if( (markerLength < MARKER_LENGTH_SIZE + MIN_SOF_SIZE) ||       //marker size not enough to read data. marker not recognized?
+            (currentOffset + MARKER_HEADER_SIZE + MARKER_LENGTH_SIZE + MIN_SOF_SIZE >= size) )  //not enough data in source buffer
+        {
+            return false;
+        }
+        const quint8* sofDataStart = data + currentOffset + MARKER_HEADER_SIZE + MARKER_LENGTH_SIZE;
+        imgInfo->precision = sofDataStart[0];
+        imgInfo->height = (sofDataStart[1] << 8) | sofDataStart[2];
+        imgInfo->width = (sofDataStart[3] << 8) | sofDataStart[4];
+        imgInfo->pixelFormat = parsePixelFormatFromSof(data + currentOffset, size);
+        parsed = true;
+        return false;
+    };
+
+    JpegParser parser;
+    parser.setParseHandler(parseHandler);
+    parser.parse(data, size);
+    return parsed;
+}
+
+} // namespace nx::media::jpeg
