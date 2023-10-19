ea621134acda3b3fe481ea8cc3ee327dd27e0407
Maksim Malofeev <mmalofeev@networkoptix.com>
2023-10-19 18:33:15 +0300

    VMS-42075: Fix an inability to switch a storage type in the server settings dialog
    (cherry picked from commit 78c176cde56d1589cd86c79ab202e2abe3fe4880)

--- vms/client/nx_vms_client_desktop/src/ui/widgets/properties/storage_config_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/widgets/properties/storage_config_widget.cpp
@@ -948,12 +948,8 @@ void QnStorageConfigWidget::atStorageViewClicked(const QModelIndex& index)
 
     if (index.column() == QnStorageListModel::StoragePoolColumn)
     {
-        if (!m_model->canChangeStoragePool(record)
-            || !index.flags().testFlag(Qt::ItemIsEditable)
-            || !index.flags().testFlag(Qt::ItemIsUserCheckable))
-        {
+        if (!m_model->canChangeStoragePool(record) || !index.flags().testFlag(Qt::ItemIsEditable))
             return;
-        }
 
         auto findAction =
             [this](StoragePoolMenuItem menuItem) -> QAction*
