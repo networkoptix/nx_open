3bd2bbce2cfa3649d9dabcc7a99cc6cf968ae76d
Anton Malov <amalov@networkoptix.com>
2023-10-26 13:08:57 +0000

    VMS-42090: Fix client reconnection algorithm
    Message bus error signal may consume other servers from the reconnect list.
    
    (cherry picked from commit dec4c90d174f61de77793e09a4125a7df18c4fc9)

--- vms/client/nx_vms_client_core/src/nx/vms/client/core/utils/reconnect_helper.cpp
+++ vms/client/nx_vms_client_core/src/nx/vms/client/core/utils/reconnect_helper.cpp
@@ -13,9 +13,9 @@ namespace nx::vms::client::core {
 ReconnectHelper::ReconnectHelper(SystemContext* systemContext, bool stickyReconnect):
     SystemContextAware(systemContext),
     m_servers(systemContext->resourcePool()->servers()),
-    m_currentServerId(systemContext->currentServerId())
+    m_originalServerId(systemContext->currentServerId())
 {
-    auto currentServer = resourcePool()->getResourceById<QnMediaServerResource>(m_currentServerId);
+    auto currentServer = resourcePool()->getResourceById<QnMediaServerResource>(m_originalServerId);
     if (stickyReconnect && currentServer)
         m_servers = {currentServer};
 
@@ -53,7 +53,7 @@ std::optional<nx::network::SocketAddress> ReconnectHelper::currentAddress() cons
 
     // Skip current server since connection will be restored automatically.
     // Prevents racing when connecting to several m_servers simultaneously.
-    if (server->getId() == m_currentServerId)
+    if (server->getId() == m_originalServerId)
         return std::nullopt;
 
     const auto discoverManager = appContext()->moduleDiscoveryManager();
@@ -64,6 +64,13 @@ void ReconnectHelper::markCurrentServerAsInvalid()
 {
     if (auto server = currentServer())
         m_servers.removeAll(server);
+}
+
+void ReconnectHelper::markOriginalServerAsInvalid()
+{
+    auto it = std::remove_if(m_servers.begin(), m_servers.end(),
+        [this](const auto& server) { return server->getId() == m_originalServerId; });
+    m_servers.erase(it, m_servers.end());
 }
 
 void ReconnectHelper::next()
