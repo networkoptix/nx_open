bb1737a5fe56d5722c6f0bd11aace2a7743c4541
Mikhail Shevchenko <mshevchenko@networkoptix.com>
2023-10-27 16:13:55 +0200

    VMS-42296: Set 6.0 meta-version to R2

--- cmake/versions/meta_version.cmake
+++ cmake/versions/meta_version.cmake
@@ -1,6 +1,6 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-set(metaVersion R1)
+set(metaVersion R2)
 if(customization.advanced.useMetaVersion)
     # Set meta-version to be shown in MetaVMS builds - in the Client, etc.
     set(usedMetaVersion ${metaVersion})
