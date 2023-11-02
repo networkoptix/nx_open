7ff7b3c62005ad9fbdc999767d16a76ff5582650
Petr Privalov <pprivalov@networkoptix.com>
2023-11-02 12:01:53 +0000

    VMS-38198: Add analytics object to object list

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/models/private/analytics_search_list_model_p.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/models/private/analytics_search_list_model_p.cpp
@@ -15,6 +15,7 @@
 #include <analytics/db/analytics_db_types.h>
 #include <api/server_rest_connection.h>
 #include <client/client_module.h>
+#include <common/common_globals.h>
 #include <common/common_module.h>
 #include <core/resource/camera_history.h>
 #include <core/resource/camera_resource.h>
@@ -45,9 +46,12 @@
 #include <nx/vms/client/desktop/application_context.h>
 #include <nx/vms/client/desktop/help/help_topic.h>
 #include <nx/vms/client/desktop/ini.h>
+#include <nx/vms/client/desktop/menu/action_manager.h>
+#include <nx/vms/client/desktop/resource/resource_access_manager.h>
 #include <nx/vms/client/desktop/style/helper.h>
 #include <nx/vms/client/desktop/system_context.h>
 #include <nx/vms/client/desktop/utils/managed_camera_set.h>
+#include <nx/vms/common/lookup_lists/lookup_list_manager.h>
 #include <nx/vms/text/human_readable.h>
 #include <server/server_storage_manager.h>
 #include <ui/dialogs/common/message_box.h>
@@ -1333,6 +1337,33 @@ QString AnalyticsSearchListModel::Private::engineName(
     return QString();
 }
 
+void AnalyticsSearchListModel::Private::addCreateNewListAction(
+    QMenu* menu, const ObjectTrack& track) const
+{
+    menu->addAction<std::function<void()>>(tr("Create New List by object"),
+        nx::utils::guarded(this,
+            [&]()
+            {
+                std::vector<QString> attributeNames;
+                std::vector<std::map<QString, QString>> entries;
+                std::map<QString, QString> val;
+
+                for (const auto& attribute: track.attributes)
+                {
+                    attributeNames.push_back(attribute.name);
+                    val[attribute.name] = attribute.value;
+                }
+                entries.push_back(val);
+
+                nx::vms::api::LookupListData list{
+                    .name = track.objectTypeId, //< TODO: #pprivalov Add check for name duplication.
+                    .objectTypeId = track.objectTypeId,
+                    .attributeNames = attributeNames,
+                    .entries = entries};
+                q->system()->lookupListManager()->addOrUpdate(list);
+            }));
+}
+
 QSharedPointer<QMenu> AnalyticsSearchListModel::Private::contextMenu(
     const ObjectTrack& track) const
 {
@@ -1360,6 +1391,52 @@ QSharedPointer<QMenu> AnalyticsSearchListModel::Private::contextMenu(
                     emit q->pluginActionRequested(engineId, actionId, track, camera,
                         AnalyticsSearchListModel::QPrivateSignal());
                 }));
+        }
+    }
+
+    menu->addSeparator();
+
+    if (ResourceAccessManager::hasPermissions(camera, Qn::ReadWriteSavePermission))
+    {
+        auto& lists = q->system()->lookupListManager()->lookupLists();
+        if (lists.empty())
+        {
+            addCreateNewListAction(menu.get(), track);
+        }
+        else
+        {
+            auto addMenu = menu->addMenu(tr("Add To List"));
+            addCreateNewListAction(addMenu, track);
+
+            addMenu->addSeparator();
+            for (const auto& list: lists)
+            {
+                addMenu->addAction<std::function<void()>>(list.name,
+                    nx::utils::guarded(this,
+                        [&]()
+                        {
+                            auto lookupList =
+                                q->system()->lookupListManager()->lookupList(list.id);
+
+                            std::map<QString, QString> val;
+                            for (const auto& attribute: track.attributes)
+                            {
+                                for (const auto& name: lookupList.attributeNames)
+                                {
+                                    if (attribute.name == name)
+                                    {
+                                        val[name] = attribute.value;
+                                        continue;
+                                    }
+                                }
+                            }
+                            if (val.empty())
+                                return;
+
+                            lookupList.entries.push_back(val);
+                            q->system()->lookupListManager()->addOrUpdate(lookupList);
+                        }));
+            }
         }
     }
 
