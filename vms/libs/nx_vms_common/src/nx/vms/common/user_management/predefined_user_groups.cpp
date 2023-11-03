24b5c281f773d77307366c3535355997d9e03b47
Anton Ivashchenko <aivashchenko@networkoptix.com>
2023-11-03 01:11:43 +0000

    VMS-42038: Fix descriptions for predefined groups

--- vms/libs/nx_vms_common/src/nx/vms/common/user_management/predefined_user_groups.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/common/user_management/predefined_user_groups.cpp
@@ -43,29 +43,32 @@ struct PredefinedUserGroups::Private
     static QString description(const QnUuid& groupId)
     {
         if (groupId == kAdministratorsGroupId)
-            return tr("Members of this group have unlimited System privileges. Administrators can"
-                " create and modify Power Users, and merge Systems and link or unlink to %1"
-                " accounts.", "%1 is the cloud name (like Nx Cloud)")
+            return tr("Members of this group have unlimited System privileges. Administrators can "
+                "create and modify Power Users, merge Systems and connect or disconnect "
+                "System to  %1.", "%1 is the cloud name (like Nx Cloud)")
                 .arg(nx::branding::cloudName());
 
         if (groupId == kPowerUsersGroupId)
-            return tr("Members of this group have control of most of System configuration, but"
-                " are not allowed to change any Administrator settings, delete or change their"
-                " own groups and permissions, and cannot create or edit other Power Users.");
+            return tr("Members of this group can, in addition to the permissions granted by the "
+                "Advanced Viewers group, control most of the System configuration, but are not "
+                "allowed to change any Administrator related settings, like delete or change "
+                "their own groups and permissions, and cannot create or edit other Power Users.");
 
         if (groupId == kAdvancedViewersGroupId)
-            return tr("Members of this group can see and run PTZ positions and PTZ Tours, use"
-                " 2-way audio, operate I/O Module buttons, create and edit Bookmarks, and view"
-                " the Event Log.");
+            return tr("Members of this group can, in addition to the permissions granted by the "
+                "Viewers group, see and activate PTZ positions and PTZ tours, use 2-way audio, "
+                "operate I/O module buttons, create and edit bookmarks, and view the Event Log.");
 
         if (groupId == kViewersGroupId)
-            return tr("Members of this group can view and export archive and Bookmarks.");
+            return tr("Members of this group can, in addition to the permissions granted by the "
+                "Live Viewers group, view and export archive and Bookmarks.");
 
         if (groupId == kLiveViewersGroupId)
             return tr("Members of this group can view live videos, I/O modules and web pages.");
 
         if (groupId == kSystemHealthViewersGroupId)
-            return tr("Members of this group can view System Health Monitoring information.");
+            return tr("Members of this group can view System Health Monitoring information and "
+                "server processor load in real-time (Server Monitoring).");
 
         NX_ASSERT(false, "Not a predefined user group: %1", groupId);
         return {};
