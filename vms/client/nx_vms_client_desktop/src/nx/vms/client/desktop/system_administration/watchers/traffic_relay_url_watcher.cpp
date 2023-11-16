345cd1932391c891e2e7e39f59497fc7a99f3ea5
Evgeny Sobolev <esobolev@networkoptix.com>
2023-11-16 19:49:40 +0000

    VMS-42469: Fix loading link for temporary user without cloud connection
    (cherry picked from commit f2bc347a64729b8048234de026494502b8a0a952)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/watchers/traffic_relay_url_watcher.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/system_administration/watchers/traffic_relay_url_watcher.cpp
@@ -3,10 +3,13 @@
 #include "traffic_relay_url_watcher.h"
 
 #include <QtCore/QJsonObject>
+#include <QtCore/QTimer>
 
 #include <nx/network/http/buffer_source.h>
 #include <nx/network/http/http_async_client.h>
 #include <nx/network/url/url_builder.h>
+#include <nx/utils/guarded_callback.h>
+#include <nx/utils/pending_operation.h>
 #include <nx/utils/serialization/qjson.h>
 #include <nx/utils/url.h>
 #include <nx/vms/client/core/common/utils/cloud_url_helper.h>
@@ -29,24 +32,19 @@ static const QString kTrafficRelayUrl = "trafficRelayUrl";
 class TraffiRelayUrlWatcher::Private
 {
 public:
-    Private(TraffiRelayUrlWatcher* owner): q(owner) {}
-    ~Private()
+    Private(TraffiRelayUrlWatcher* owner): q(owner)
     {
-        nx::utils::promise<void> promise;
-        reconnectTimer.pleaseStop(
-            [this, &promise]()
-            {
-                httpClient.reset();
-                promise.set_value();
-            });
-
-        promise.get_future().wait();
+        requestTrafficRelayUrl();
     }
+
+    ~Private() {}
 
 public:
     TraffiRelayUrlWatcher* const q;
-    nx::network::aio::Timer reconnectTimer;
+    QTimer reconnectTimer;
     std::unique_ptr<nx::network::http::AsyncClient> httpClient;
+    nx::utils::PendingOperation delayedRequestTrafficRelayUrl{
+        [this]() { requestTrafficRelayUrl(); }, kRequestRetryDelay};
     QString trafficRelayUrl;
     mutable nx::Mutex mutex;
 
@@ -56,102 +54,82 @@ public:
         return trafficRelayUrl;
     }
 
-    void updateTrafficRelayUrl()
-    {
-        reconnectTimer.pleaseStop(
-            [this]()
-            {
-                if (httpClient)
-                    httpClient->pleaseStopSync();
-
-                const auto cloudSystemId = q->systemSettings()->cloudSystemId();
-                if (cloudSystemId.isEmpty())
-                    return;
-
-                const core::CloudUrlHelper cloudUrlHelper(
-                    nx::vms::utils::SystemUri::ReferralSource::DesktopClient,
-                    nx::vms::utils::SystemUri::ReferralContext::None);
-
-                const auto urlCloud = nx::utils::Url::fromQUrl(cloudUrlHelper.mainUrl());
-
-                const nx::network::SocketAddress address{urlCloud.host().toStdString(),
-                    (quint16) urlCloud.port(
-                        nx::network::http::defaultPortForScheme(urlCloud.scheme().toStdString()))};
-
-                httpClient = std::make_unique<nx::network::http::AsyncClient>(
-                    nx::network::ssl::kAcceptAnyCertificate);
-                httpClient->bindToAioThread(reconnectTimer.getAioThread());
-
-                const auto url = nx::network::url::Builder()
-                    .setScheme(urlCloud.scheme().toStdString())
-                    .setEndpoint(address)
-                    .setPath(nx::format(kCloudPathTrafficRelayInfo, cloudSystemId))
-                    .toUrl();
-
-                auto messageBody = std::make_unique<nx::network::http::BufferSource>(
-                    Qn::serializationFormatToHttpContentType(Qn::SerializationFormat::json),
-                    nx::format(kTrafficRelayUrlRequest, cloudSystemId).toStdString());
-
-                httpClient->doPost(url, std::move(messageBody),
-                    [this, url]()
-                    {
-                        NX_MUTEX_LOCKER lock(&mutex);
-                        trafficRelayUrl = {};
-                        if (httpClient->failed())
-                        {
-                            scheduleRequestRelayUrl();
-                            NX_VERBOSE(this, "POST request failed. Url: %1", url);
-                            return;
-                        }
-
-                        const auto result = httpClient->fetchMessageBodyBuffer();
-                        httpClient.reset();
-                        if (result.empty())
-                        {
-                            NX_VERBOSE(this, "POST body fetch failed. Url: %1", url);
-                            return;
-                        }
-
-                        QJsonObject response;
-                        if (nx::reflect::json::deserialize(result, &response))
-                        {
-                            trafficRelayUrl = response[kTrafficRelayUrl].toString();
-                        }
-                        else
-                        {
-                            NX_VERBOSE(this, "Can not deserialize POST response. Url: %1", url);
-                        }
-
-                        emit q->trafficRelayUrlReady();
-                    });
-            });
-    }
-
-private:
-
-    void stopSync()
+    void requestTrafficRelayUrl()
     {
         if (httpClient)
             httpClient->pleaseStopSync();
-    }
 
-    void scheduleRequestRelayUrl()
-    {
-        reconnectTimer.start(kRequestRetryDelay, [this]() { updateTrafficRelayUrl(); });
+        const auto cloudSystemId = q->systemSettings()->cloudSystemId();
+        if (cloudSystemId.isEmpty())
+            return;
+
+        const core::CloudUrlHelper cloudUrlHelper(
+            nx::vms::utils::SystemUri::ReferralSource::DesktopClient,
+            nx::vms::utils::SystemUri::ReferralContext::None);
+
+        const auto urlCloud = nx::utils::Url::fromQUrl(cloudUrlHelper.mainUrl());
+
+        const auto address = nx::network::SocketAddress::fromUrl(urlCloud, true);
+
+        httpClient = std::make_unique<nx::network::http::AsyncClient>(
+            nx::network::ssl::kAcceptAnyCertificate);
+
+        const auto url = nx::network::url::Builder()
+            .setScheme(urlCloud.scheme().toStdString())
+            .setEndpoint(address)
+            .setPath(nx::format(kCloudPathTrafficRelayInfo, cloudSystemId))
+            .toUrl();
+
+        auto messageBody = std::make_unique<nx::network::http::BufferSource>(
+            Qn::serializationFormatToHttpContentType(Qn::SerializationFormat::json),
+            nx::format(kTrafficRelayUrlRequest, cloudSystemId).toStdString());
+
+        httpClient->doPost(
+            url,
+            std::move(messageBody),
+            nx::utils::guarded(q,
+                [this, url]()
+                {
+                    NX_MUTEX_LOCKER lock(&mutex);
+                    trafficRelayUrl = {};
+                    if (httpClient->failed())
+                    {
+                        NX_VERBOSE(this, "POST request failed. Url: %1", url);
+                        delayedRequestTrafficRelayUrl.requestOperation();
+                        return;
+                    }
+
+                    const auto result = httpClient->fetchMessageBodyBuffer();
+                    httpClient.reset();
+                    if (result.empty())
+                    {
+                        NX_VERBOSE(this, "POST body fetch failed. Url: %1", url);
+                        delayedRequestTrafficRelayUrl.requestOperation();
+                        return;
+                    }
+
+                    QJsonObject response;
+                    if (nx::reflect::json::deserialize(result, &response))
+                    {
+                        trafficRelayUrl = response[kTrafficRelayUrl].toString();
+                    }
+                    else
+                    {
+                        NX_VERBOSE(this, "Can not deserialize POST response. Url: %1", url);
+                        delayedRequestTrafficRelayUrl.requestOperation();
+                        return;
+                    }
+
+                    emit q->trafficRelayUrlReady();
+                }));
     }
 };
 
 TraffiRelayUrlWatcher::TraffiRelayUrlWatcher(SystemContext* context, QObject* parent):
-    QObject(parent),
-    SystemContextAware(context),
-    d(new Private(this))
+    QObject(parent), SystemContextAware(context), d(new Private(this))
 {
-    connect(globalSettings(),
-        &common::SystemSettings::cloudSettingsChanged,
-        this,
-        [this]() { d->updateTrafficRelayUrl(); });
-
-    d->updateTrafficRelayUrl();
+    connect(globalSettings(), &common::SystemSettings::cloudSettingsChanged,
+        this, [this]() { d->delayedRequestTrafficRelayUrl.requestOperation(); });
 }
 
 QString TraffiRelayUrlWatcher::trafficRelayUrl() const
