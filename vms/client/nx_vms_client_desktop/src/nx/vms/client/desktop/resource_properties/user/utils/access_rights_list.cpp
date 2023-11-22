82f72e283cfb222c9688ec112ec1961dd5a49582
Vitaly Kutin <vkutin@networkoptix.com>
2023-11-22 01:57:40 +0000

    VMS-42557: Add captions to permission header buttons

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/user/utils/access_rights_list.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/resource_properties/user/utils/access_rights_list.cpp
@@ -24,40 +24,40 @@ AccessRightsList::AccessRightsList(QObject* parent):
             .name = tr("View Live"),
             .description = common::html::bold(tr("View Live.")) + " " +
                 tr("Allows users to access a resource and view live footage from a camera."),
-            .icon = QUrl(kIconsDir + "view_live.svg")},
+            .icon = QUrl(kIconsDir + "view_live_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::viewArchive,
             .name = tr("View Archive"),
             .description = common::html::bold(tr("View Archive")),
-            .icon = QUrl(kIconsDir + "view_archive.svg")},
+            .icon = QUrl(kIconsDir + "view_archive_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::exportArchive,
             .name = tr("Export Archive"),
             .description = common::html::bold(tr("Export Archive")),
-            .icon = QUrl(kIconsDir + "export_archive.svg")},
+            .icon = QUrl(kIconsDir + "export_archive_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::viewBookmarks,
             .name = tr("View Bookmarks"),
             .description = common::html::bold(tr("View Bookmarks")),
-            .icon = QUrl(kIconsDir + "view_bookmarks.svg")},
+            .icon = QUrl(kIconsDir + "view_bookmarks_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::manageBookmarks,
             .name = tr("Manage Bookmarks"),
             .description = common::html::bold(tr("Modify Bookmarks")),
-            .icon = QUrl(kIconsDir + "manage_bookmarks.svg")},
+            .icon = QUrl(kIconsDir + "manage_bookmarks_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::userInput,
             .name = tr("User Input"),
             .description = common::html::bold("User Input.") + " " +
                 tr("Allows user to control PTZ, use 2-Way Audio, Soft Triggers and I/O buttons."),
-            .icon = QUrl(kIconsDir + "user_input.svg")},
+            .icon = QUrl(kIconsDir + "user_input_24x20.svg")},
         AccessRightDescriptor{
             .accessRight = AccessRight::edit,
             .name = tr("Edit Settings"),
             .description = common::html::bold(tr("Edit Camera Settings.")) + " " +
                 tr("Depending on the resource type it either allows user to modify device settings"
                 " or to control video wall."),
-            .icon = QUrl(kIconsDir + "edit_settings.svg")}})
+            .icon = QUrl(kIconsDir + "edit_settings_24x20.svg")}})
 {
     QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
 }
