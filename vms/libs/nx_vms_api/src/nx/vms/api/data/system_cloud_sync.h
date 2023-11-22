6d4a5434e4d709ba1125cf8cb24dfa1d5fd6a124
Evgenii Karpov <ekarpov@networkoptix.com>
2023-11-22 16:42:29 +0100

    VMS-42460: Server API: Add Cloud sync handler

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/system_cloud_sync.h
@@ -0,0 +1,51 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <chrono>
+
+#include <QtCore/QString>
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/reflect/instrument.h>
+
+namespace nx::vms::api {
+
+NX_REFLECTION_ENUM_CLASS(SystemCloudService,
+
+    /**%apidoc[unused] */
+    both,
+
+    /**%apidoc
+     * Fetch information about Cloud users, System name, 2FA enabled.
+     */
+    cdb,
+
+    /**%apidoc
+     * Fetch information about Channel Partner, Organization, available services, send SaaS reports.
+     */
+    cps
+);
+
+struct NX_VMS_API SystemCloudSyncRequest
+{
+    SystemCloudService service = SystemCloudService::both;
+
+    /**%apidoc[opt] Wait until the data is processed. */
+    bool waitForDone = false;
+};
+#define SystemCloudSyncRequest_Fields (waitForDone)
+NX_REFLECTION_INSTRUMENT(SystemCloudSyncRequest, SystemCloudSyncRequest_Fields)
+QN_FUSION_DECLARE_FUNCTIONS(SystemCloudSyncRequest, (json), NX_VMS_API)
+
+struct NX_VMS_API CloudPullingStatus
+{
+    bool isRunning = false;
+    std::optional<std::chrono::milliseconds> timeSinceLastSyncMs{};
+    QString message;
+};
+#define CloudPullingStatus_Fields (isRunning)(timeSinceLastSyncMs)
+QN_FUSION_DECLARE_FUNCTIONS(CloudPullingStatus, (json), NX_VMS_API)
+NX_REFLECTION_INSTRUMENT(CloudPullingStatus, CloudPullingStatus_Fields)
+
+} // namespace nx::vms::api
