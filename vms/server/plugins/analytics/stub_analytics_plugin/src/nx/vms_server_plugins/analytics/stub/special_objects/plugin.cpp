2eefe6cb6f2743e0f68ec139368e84417470a04e
Maksim Tikhomirov <MTikhomirov@networkoptix.com>
2023-11-23 16:59:49 +0400

    Resolve merge conflict

--- vms/server/plugins/analytics/stub_analytics_plugin/src/nx/vms_server_plugins/analytics/stub/special_objects/plugin.cpp
+++ vms/server/plugins/analytics/stub_analytics_plugin/src/nx/vms_server_plugins/analytics/stub/special_objects/plugin.cpp
@@ -28,7 +28,7 @@ std::string Plugin::manifestString() const
     return /*suppress newline*/ 1 + (const char*) R"json(
 {
     "id": ")json" + instanceId() + R"json(",
-    "name": "Stub: Special Objects",
+    "name": "Stub, Special Objects",
     "description": "A plugin for testing and debugging special Objects.",
     "version": "1.0.0",
     "vendor": "Plugin vendor"
