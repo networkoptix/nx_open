3dd1cc12ba53a778ac05c666660e70300f288a4f
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2023-12-04 08:33:07 +0000

    CB-2204: cloud_db/oauth: introduce rfc7662 token introspect API

--- cloud/cloud_db_client/src/nx/cloud/db/client/data/oauth_data.h
+++ cloud/cloud_db_client/src/nx/cloud/db/client/data/oauth_data.h
@@ -29,10 +29,6 @@ NX_REFLECTION_INSTRUMENT(IssueTokenRequest, Token_request_Fields)
 #define Issue_token_response_Fields \
     (access_token)(refresh_token)(expires_in)(expires_at)(token_type)(scope)(error)
 
-#define Validate_token_response_Fields \
-    (access_token)(expires_in)(expires_at)(token_type)(scope)(username)(vms_user_id)( \
-        time_since_password)
-
 #define Issue_code_response_Fields (access_code)(code)(error)
 
 #define Issue_stun_token_request_Fields (server_name)
@@ -41,7 +37,8 @@ NX_REFLECTION_INSTRUMENT(IssueTokenRequest, Token_request_Fields)
 
 NX_REFLECTION_INSTRUMENT(IssueTokenResponse, Issue_token_response_Fields);
 
-NX_REFLECTION_INSTRUMENT(ValidateTokenResponse, Validate_token_response_Fields)
+NX_REFLECTION_INSTRUMENT(ValidateTokenResponse, (access_token)(expires_in)(expires_at)(token_type) \
+    (scope)(username)(vms_user_id)(time_since_password))
 
 NX_REFLECTION_INSTRUMENT(IssueCodeResponse, Issue_code_response_Fields);
 
