729d84088d029628ad7a94d5418dcaf16e32b10c
Jenkins <jenkins@networkoptix.com>
2023-12-06 01:08:30 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -1081,6 +1081,10 @@
         <source>User Manual...</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Convert to Shared Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Deactivator</name>
