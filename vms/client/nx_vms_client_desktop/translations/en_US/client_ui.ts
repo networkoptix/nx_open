77691a95accd5dbc69e66a1aee5a3337b3b89819
Jenkins <jenkins@networkoptix.com>
2023-12-08 01:15:32 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_ui.ts
@@ -685,6 +685,10 @@
         <source>Record audio</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Do not send Stop PTZ command</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>CameraInfoWidget</name>
