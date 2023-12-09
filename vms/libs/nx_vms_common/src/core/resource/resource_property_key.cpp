95dc256f9cf5bb831ad324fc63b674a11206d462
Denis Shkiria <dshkiria@networkoptix.com>
2023-12-09 01:52:34 +0000

    VMS-42477: Limit the use of remote archive synchronization to supported devices only
    (cherry picked from commit 737e5828bdf0968dd2a39b5e7f3696c66b4c8a17)

--- vms/libs/nx_vms_common/src/core/resource/resource_property_key.cpp
+++ vms/libs/nx_vms_common/src/core/resource/resource_property_key.cpp
@@ -233,6 +233,8 @@ const QString kDisableRtspMetadataStream("disableRtspMetadataStream");
 
 const QString kOnvifRemoteArchiveDisableFastDownload("onvifRemoteArchiveDisableFastDownload");
 
+const QString kOnvifRemoteArchiveEnabled("onvifRemoteArchiveEnabled");
+
 const QString kGsoapAdditionalFlags("gsoapAdditionalFlags");
 
 const QString kOnvifIgnoreOutdatedNotifications("onvifIgnoreOutdatedNotifications");
