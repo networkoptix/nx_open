feb860c607c2d23665a9e950cf3faa296f034711
Denis Klychkov <dklychkov@networkoptix.com>
2023-12-11 22:35:55 -0800

    VMS-42844: Fix race condition when client starts update and reads persistent stogare server list
    Persistent storage servers are selected at the server side in the `startUpdate` REST handler and
    may not be available in `systemSettings()` in the client immediately after receiving the reply.
    
    This commit adds the list into the REST handler reply.

---
+++ vms/libs/nx_vms_common/src/nx/vms/common/update/start_update_reply.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include <nx/fusion/model_functions.h>
+
+#include "start_update_reply.h"
+
+namespace nx::vms::common::update {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(StartUpdateReply, (json), StartUpdateReply_Fields)
+
+} // namespace nx::vms::common::update
