218db8c9cb36ff84c135d17bb77e9001b590b525
Jenkins <jenkins@networkoptix.com>
2023-12-13 00:07:20 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.1_patch
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_1_patch.download-translation/26/

--- vms/client/nx_vms_client_desktop/translations/vi_VN/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/vi_VN/client_ui.ts
@@ -683,7 +683,7 @@
     </message>
     <message>
       <source>Record audio</source>
-      <translation type="unfinished">Record audio</translation>
+      <translation>Ghi âm</translation>
     </message>
     <message>
       <source>Do not send Stop PTZ command</source>
