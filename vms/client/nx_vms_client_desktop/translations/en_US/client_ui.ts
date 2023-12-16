c1f7909a2575520ad9a5f833128190092f15a5ec
Jenkins <jenkins@networkoptix.com>
2023-12-16 01:08:30 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_ui.ts
@@ -2469,6 +2469,10 @@
         <source>Services Usage</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Services state</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>SayTextBusinessActionWidget</name>
