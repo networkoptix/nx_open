3d9ad692080859416ee9195500591e7500eea614
Sergei Ivanov <sivanov@networkoptix.com>
2023-12-20 14:51:41 +0100

    VMS-44993: Fix windows debug build with "vpl.lib was not found" error
    Conan recipe was updated to correctly define library name for linking
    
    (cherry picked from commit 9242dd99e9655d90af827f7fd4788b1ee6ff9059)

--- conanfile.py
+++ conanfile.py
@@ -164,7 +164,7 @@ class NxOpenConan(ConanFile):
         if self.isWindows or self.isLinux:
             if self.settings.arch == "x86_64":
                 self.requires("cuda-toolkit/11.7" "#85c06d4043d49e1fb06b75b5bf9bd20e")
-                self.requires("libvpl/2023.4.0" "#dbed8bf2164276de19adf7ca3f56055f")
+                self.requires("libvpl/2023.4.0" "#5c092e69a8d1ffd163d6db5a3b79c576")
                 self.requires("zlib/1.2.12" "#bb959a1d68d4c35d0fba4cc66f5bb25f")
 
         if self.isLinux:
