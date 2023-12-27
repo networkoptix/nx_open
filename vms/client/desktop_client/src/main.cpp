83633409a09914638ced2eeea219b7f99d33b032
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2023-12-26 16:20:53 -0800

    VMS-42701: Enable QML threaded rendering on macOS when Metal API is used
    (cherry picked from commit 0582ffa4c9fd9444d974383bceaf61384fc3aa7a)

--- vms/client/desktop_client/src/main.cpp
+++ vms/client/desktop_client/src/main.cpp
@@ -1,12 +1,13 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-#include <nx/vms/client/desktop/application.h>
-
 #include <nx/build_info.h>
+#include <nx/vms/client/desktop/application.h>
+#include <nx/vms/client/desktop/ini.h>
 
 int main(int argc, char** argv)
 {
-    if (nx::build_info::isMacOsX())
+    if (nx::build_info::isMacOsX()
+        && QString(nx::vms::client::desktop::ini().graphicsApi) == "opengl")
     {
         // We do not rely on Mac OS OpenGL implementation-related throttling.
         // Otherwise all animations go faster.
