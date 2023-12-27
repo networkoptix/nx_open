ab90dbe44bc80ff80618823541096629c0590622
Jenkins <jenkins@networkoptix.com>
2023-12-27 00:07:14 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.1_patch
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_1_patch.download-translation/40/

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
@@ -675,7 +675,7 @@
     </message>
     <message>
       <source>Web Page</source>
-      <translation>ウェブページ</translation>
+      <translation>Webページ</translation>
     </message>
     <message>
       <source>Web Page Port</source>
@@ -718,7 +718,7 @@
     </message>
     <message>
       <source>Web Page</source>
-      <translation>ウェブページ</translation>
+      <translation>Webページ</translation>
     </message>
     <message>
       <source>Primary Stream</source>
@@ -2005,7 +2005,7 @@
     </message>
     <message>
       <source>Advanced Update Settings</source>
-      <translation>詳細設定</translation>
+      <translation>更新の詳細設定</translation>
     </message>
     <message>
       <source>It looks like the update process is taking more time than usual. Please keep waiting.</source>
@@ -2740,11 +2740,11 @@
     </message>
     <message>
       <source>Display watermark with username over video</source>
-      <translation>ビデオの上にユーザー名で透かしを表示する</translation>
+      <translation>映像上にユーザー名のウォーターマークを表示する</translation>
     </message>
     <message>
       <source>Watermark Preview</source>
-      <translation>透かしのプレビュー</translation>
+      <translation>ウォーターマークのプレビュー</translation>
     </message>
     <message>
       <source>to</source>
@@ -2933,7 +2933,7 @@
     <name>SignDialog</name>
     <message>
       <source>Watermark Check</source>
-      <translation>ウォーターマーク チェック</translation>
+      <translation>ウォーターマーク確認</translation>
     </message>
     <message>
       <source>Checked File:</source>
@@ -3352,7 +3352,7 @@
     </message>
     <message>
       <source>Advanced</source>
-      <translation>詳細</translation>
+      <translation>上級設定</translation>
     </message>
     <message>
       <source>Allow using Client API</source>
@@ -3394,7 +3394,7 @@
     <name>nx::vms::client::desktop::WatermarkPreviewDialog</name>
     <message>
       <source>Watermark preview</source>
-      <translation>透かしのプレビュー</translation>
+      <translation>ウォーターマークのプレビュー</translation>
     </message>
     <message>
       <source>Opacity</source>
