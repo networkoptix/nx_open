66e6d174176ee0156039c9d93e550ca468c99621
Petr Privalov <pprivalov@networkoptix.com>
2024-01-08 11:55:55 +0000

    VMS-42847: Fix icons coloring
    (cherry picked from commit 30d768843d139ee556fd44ff1ac66bd9191e52f7)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/common/widgets/close_button.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/common/widgets/close_button.cpp
@@ -2,17 +2,23 @@
 
 #include "close_button.h"
 
+#include <QtGui/QPainter>
+#include <QtWidgets/QStyle>
+
 #include <nx/vms/client/core/skin/color_theme.h>
+#include <nx/vms/client/core/skin/icon.h>
 #include <nx/vms/client/core/skin/skin.h>
+#include <nx/vms/client/desktop/common/widgets/hover_button.h>
+#include <nx/vms/client/desktop/style/helper.h>
 
 namespace nx::vms::client::desktop {
 
-static const QColor kLight16Color = "#698796";
 static const QColor kLight10Color = "#A5B7C0";
 static const nx::vms::client::core::SvgIconColorer::IconSubstitutions kIconSubstitutions = {
-    {QIcon::Normal, {{kLight16Color, "light16"}, {kLight10Color, "light16"}}},
-    {QIcon::Active, {{kLight16Color, "light14"}, {kLight10Color, "light14"}}},
-    {QIcon::Selected, {{kLight16Color, "light17"}, {kLight10Color, "light17"}}},
+    {QIcon::Normal, {{kLight10Color, "light14"}}},
+    {QIcon::Active, {{kLight10Color, "light14"}}},
+    {QIcon::Selected, {{kLight10Color, "light14"}}},
+    {QnIcon::Pressed, {{kLight10Color, "light10"}}},
 };
 
 CloseButton::CloseButton(QWidget* parent):
@@ -20,5 +26,34 @@ CloseButton::CloseButton(QWidget* parent):
 {
     setFixedSize(HoverButton::sizeHint());
 }
+
+void CloseButton::setAccented(bool accented)
+{
+    m_accented = accented;
+}
+
+void CloseButton::paintEvent(QPaintEvent* event)
+{
+    if (!m_accented)
+    {
+        base_type::paintEvent(event);
+    }
+    else
+    {
+        // If button is accented then it uses another color scheme, which is stored into
+        // QnIcon::Pressed mode.
+        QPixmap pixmap = qnSkin->icon("text_buttons/cross_close_20.svg", kIconSubstitutions)
+            .pixmap(nx::style::Metrics::kDefaultIconSize, QnIcon::Pressed);
+        if (pixmap.isNull())
+            return;
+
+        const auto pixmapRect = QStyle::alignedRect(
+            Qt::LeftToRight, Qt::AlignCenter, pixmap.size() / pixmap.devicePixelRatio(), rect());
+
+        QPainter painter(this);
+        painter.drawPixmap(pixmapRect, pixmap);
+    }
+}
+
 
 } // namespace nx::vms::client::desktop
