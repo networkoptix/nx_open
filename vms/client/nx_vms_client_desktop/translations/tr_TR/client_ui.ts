c5582d7e9401ae82047a381b2961ca8656a24cc8
Jenkins <jenkins@networkoptix.com>
2024-01-10 00:07:14 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.1_patch
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_1_patch.download-translation/54/

--- vms/client/nx_vms_client_desktop/translations/tr_TR/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/tr_TR/client_ui.ts
@@ -1826,7 +1826,7 @@
     </message>
     <message>
       <source>Limit Max Volume</source>
-      <translation>Maksimum Sesi Sınırla</translation>
+      <translation>Maksimum Alanı Sınırla</translation>
     </message>
   </context>
   <context>
