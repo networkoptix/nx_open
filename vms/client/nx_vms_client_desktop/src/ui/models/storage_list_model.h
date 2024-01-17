b168d24ea16dddb7dc57d77d2db62e00d46dea73
Victor Breus <vbreus@networkoptix.com>
2024-01-17 13:38:38 +0000

    VMS-47306: Fix storages list flickering while displaying a storage with long URL in the 'Storage Management' tab

--- vms/client/nx_vms_client_desktop/src/ui/models/storage_list_model.h
+++ vms/client/nx_vms_client_desktop/src/ui/models/storage_list_model.h
@@ -29,7 +29,7 @@ public:
         StoragePoolColumn,
         TotalSpaceColumn,
         ActionsColumn,
-        SeparatorColumn,
+        SpacerColumn,
         CheckBoxColumn,
 
         ColumnCount
