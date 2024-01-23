1cae8ec9a870072e82d9d1b860eabeed1e827c0d
Andrey Kolesnikov <akolesnikov@networkoptix.com>
2024-01-23 15:32:03 -0800

    VMS-43752: udt: remove unexpected deinitialization order requirement

--- artifacts/udt/src/udt/unit_queue.cpp
+++ artifacts/udt/src/udt/unit_queue.cpp
@@ -4,7 +4,80 @@
 #include <cassert>
 #include <utility>
 
-Unit::Unit(UnitQueue* unitQueue, std::unique_ptr<CPacket> packet):
+class UDT_API UnitQueueImpl:
+    public std::enable_shared_from_this<UnitQueueImpl>
+{
+public:
+    UnitQueueImpl(int initialQueueSize, int bufferSize);
+    ~UnitQueueImpl();
+
+    UnitQueueImpl(const UnitQueueImpl&) = delete;
+    UnitQueueImpl& operator=(const UnitQueueImpl&) = delete;
+
+    Unit takeNextAvailUnit();
+
+    void putBack(std::unique_ptr<CPacket> packet);
+
+private:
+    int m_bufferSize = 0;
+    std::vector<std::unique_ptr<CPacket>> m_availablePackets;
+    int m_takenPackets = 0;
+    std::mutex m_mutex;
+};
+
+//-------------------------------------------------------------------------------------------------
+
+UnitQueueImpl::UnitQueueImpl(int initialQueueSize, int bufferSize):
+    m_bufferSize(bufferSize)
+{
+    m_availablePackets.resize(initialQueueSize);
+
+    std::for_each(
+        m_availablePackets.begin(), m_availablePackets.end(),
+        [this](auto& packet)
+        {
+            packet = std::make_unique<CPacket>();
+            packet->payload().resize(m_bufferSize);
+        });
+}
+
+UnitQueueImpl::~UnitQueueImpl()
+{
+    assert(m_takenPackets == 0);
+}
+
+Unit UnitQueueImpl::takeNextAvailUnit()
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    ++m_takenPackets;
+
+    if (!m_availablePackets.empty())
+    {
+        Unit unit(shared_from_this(), std::move(m_availablePackets.back()));
+        m_availablePackets.pop_back();
+        return unit;
+    }
+    else
+    {
+        Unit unit(shared_from_this(), std::make_unique<CPacket>());
+        unit.packet().payload().resize(m_bufferSize);
+        return unit;
+    }
+}
+
+void UnitQueueImpl::putBack(std::unique_ptr<CPacket> packet)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    m_availablePackets.push_back(std::move(packet));
+
+    --m_takenPackets;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+Unit::Unit(std::weak_ptr<UnitQueueImpl> unitQueue, std::unique_ptr<CPacket> packet):
     m_unitQueue(unitQueue),
     m_packet(std::move(packet))
 {
@@ -12,8 +85,11 @@ Unit::Unit(UnitQueue* unitQueue, std::unique_ptr<CPacket> packet):
 
 Unit::~Unit()
 {
-    if (m_packet)
-        m_unitQueue->putBack(std::exchange(m_packet, nullptr));
+    if (!m_packet)
+        return;
+
+    if (auto unitQueueStrong = m_unitQueue.lock(); unitQueueStrong)
+        unitQueueStrong->putBack(std::exchange(m_packet, nullptr));
 }
 
 CPacket& Unit::packet()
@@ -34,49 +110,18 @@ Unit::Flag Unit::flag() const
 //-------------------------------------------------------------------------------------------------
 
 UnitQueue::UnitQueue(int initialQueueSize, int bufferSize):
-    m_bufferSize(bufferSize)
+    m_impl(std::make_shared<UnitQueueImpl>(initialQueueSize, bufferSize))
 {
-    m_availablePackets.resize(initialQueueSize);
-
-    std::for_each(
-        m_availablePackets.begin(), m_availablePackets.end(),
-        [this](auto& packet)
-        {
-            packet = std::make_unique<CPacket>();
-            packet->payload().resize(m_bufferSize);
-        });
 }
 
-UnitQueue::~UnitQueue()
-{
-    assert(m_takenPackets == 0);
-}
+UnitQueue::~UnitQueue() = default;
 
 Unit UnitQueue::takeNextAvailUnit()
 {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    ++m_takenPackets;
-
-    if (!m_availablePackets.empty())
-    {
-        Unit unit(this, std::move(m_availablePackets.back()));
-        m_availablePackets.pop_back();
-        return unit;
-    }
-    else
-    {
-        Unit unit(this, std::make_unique<CPacket>());
-        unit.packet().payload().resize(m_bufferSize);
-        return unit;
-    }
+    return m_impl->takeNextAvailUnit();
 }
 
 void UnitQueue::putBack(std::unique_ptr<CPacket> packet)
 {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    m_availablePackets.push_back(std::move(packet));
-
-    --m_takenPackets;
+    m_impl->putBack(std::move(packet));
 }
