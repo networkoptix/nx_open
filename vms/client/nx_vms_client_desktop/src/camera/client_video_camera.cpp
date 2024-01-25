ba9c292788cfefa644f3b4c3c3a148fca2969c92
Leonid Busygin <lbusygin@networkoptix.com>
2024-01-25 17:54:57 +0300

    VMS-47500: Do not transcode when export multisensor video to nov file

--- vms/client/nx_vms_client_desktop/src/camera/client_video_camera.cpp
+++ vms/client/nx_vms_client_desktop/src/camera/client_video_camera.cpp
@@ -214,7 +214,7 @@ void QnClientVideoCamera::exportMediaPeriodToFile(
     m_exportRecorder->setContainer(format);
 
     nx::core::transcoding::FilterChain filters(
-        transcodingSettings, m_resource->getDewarpingParams(), m_resource->getVideoLayout());
+        transcodingSettings, nx::vms::api::dewarping::MediaData(), nullptr);
     m_exportRecorder->setTranscodeFilters(filters);
 
     m_exportReader->addDataProcessor(m_exportRecorder);
