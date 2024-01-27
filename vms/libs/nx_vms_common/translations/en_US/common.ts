ab8f3a46ee837f2d0edb2889338f5cde9f9399d2
Jenkins <jenkins@networkoptix.com>
2024-01-27 01:08:40 +0000

    Update translatable strings (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/en_US/common.ts
+++ vms/libs/nx_vms_common/translations/en_US/common.ts
@@ -462,83 +462,6 @@
     </message>
 </context>
 <context>
-    <name>QnFfmpegAudioTranscoder</name>
-    <message>
-        <source>Audio context was not specified.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not find encoder for codec %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not initialize audio encoder.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not initialize audio decoder.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not find decoder for codec %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not send audio frame to encoder, Error code: %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not receive audio packet from encoder, Error code: %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not receive audio frame from decoder, Error code: %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not allocate sample buffers</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
-    <name>QnFfmpegTranscoder</name>
-    <message>
-        <source>Container %1 was not found in FFMPEG library.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not create output context for format %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not allocate output stream for recording.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not perform direct stream copy because frame size is undefined.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not find codec %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Video or audio codec is incompatible with container %1.</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
-    <name>QnFfmpegVideoTranscoder</name>
-    <message>
-        <source>Could not find encoder for codec %1.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Could not initialize video encoder.</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
     <name>QnLicense</name>
     <message>
         <source>Analog</source>
@@ -1232,14 +1155,6 @@
 </context>
 <context>
     <name>QnTranscoder</name>
-    <message>
-        <source>OpenCL transcoding is not implemented.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Unknown transcoding method.</source>
-        <translation></translation>
-    </message>
     <message>
         <source>OpenCLTranscode is not implemented.</source>
         <translation></translation>
