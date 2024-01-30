f65e9d3624d23bc19b41821857e5fc8e88044599
Jenkins <jenkins@networkoptix.com>
2024-01-30 01:08:38 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -7271,6 +7271,14 @@ We recommend you not to open this integration. If you understand the risks, you 
         <source>Resolution</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Setting was changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&quot;All archive&quot; option was changed to &quot;Motion, Objects, Bookmarks&quot; due to cloud storage backup was enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>nx::vms::client::desktop::BackupSettingsPickerWidget</name>
