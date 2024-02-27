a1457433cdac5f0d47b2904d485b552735c9f482
Denis Klychkov <dklychkov@networkoptix.com>
2024-02-26 18:34:06 -0800

    VMS-47792: Fix warning in Qt Multimedia

--- conanfile.py
+++ conanfile.py
@@ -160,7 +160,7 @@ class NxOpenConan(ConanFile):
 
         self.requires(f"ffmpeg/{self.ffmpeg_version_and_revision}")
         self.requires("openssl/1.1.1q" "#a23bd98469b500b2d658a17351fa279c")
-        self.requires("qt/6.5.3" "#11dd688b8a075726c3bf92a1c67c3d33")
+        self.requires("qt/6.5.3" "#4a20c9e2d211d3aaaf8b04606bbbe643")
         self.requires("roboto-fonts/1.0" "#a1d64ec2d6a2e16f8f476b2b47162123")
         self.requires("boost/1.83.0" "#e0be85c6f8107d7e960246e31cbbf7ab")
         self.requires("rapidjson/cci.20230929" "#624c0094d741e6a3749d2e44d834b96c")
