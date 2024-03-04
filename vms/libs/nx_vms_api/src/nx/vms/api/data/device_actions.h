7da3018ab6d0b1dce634d8e7301fd22d6ad9097b
Sergei Kolesnik <skolesnik@networkoptix.com>
2024-03-04 13:31:00 +0000

    VMS-41582: Server Api: Deprecate '/ec2/recordedTimePeriods'

--- vms/libs/nx_vms_api/src/nx/vms/api/data/device_actions.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/device_actions.h
@@ -28,7 +28,7 @@ struct DevicePasswordRequest: IdData
     bool operator==(const DevicePasswordRequest& other) const = default;
 };
 
-#define DevicePasswordRequest_Fields IdData_Fields (user)(password)
+#define DevicePasswordRequest_Fields IdData_Fields(user)(password)
 QN_FUSION_DECLARE_FUNCTIONS(DevicePasswordRequest, (json), NX_VMS_API);
 NX_REFLECTION_INSTRUMENT(DevicePasswordRequest, DevicePasswordRequest_Fields)
 
@@ -74,21 +74,57 @@ struct DeviceFootageRequest: IdData
     StorageLocation storageLocation = StorageLocation::both;
 
     /**%apidoc[opt]
+     * %// Appeared starting from /rest/v2/devices/{id}/footage.
+     */
+    bool includeCloudData = false;
+
+    /**%apidoc[opt]
      * Chunk type.
      */
     TimePeriodContentType periodType = TimePeriodContentType::recording;
 
-    /**%apidoc[opt]
-     * %// Appeared starting from /rest/v2/devices/{id}/footage.
+    // TODO: #skolesnik Implement using
+    //     `std::optional<std::variant<std::vector<Region>, AnalyticsContentFilter>>`.
+    //     `Region` must be defined to be serializeable to `QRegion` and `AnalyticsContentFilter`
+    //     from `nx::analytics::db::Filter`.
+    /**%apidoc[opt]:arrayJson filter This parameter is used for Motion and Analytics Search
+     *     (if "periodType" is set to "motion" or "analytics"). Search motion or analytics event on a video
+     *     according to the specified attribute values.
+     *     <br/>Motion Search Format: string with a JSON array of <i>sensors</i>,
+     *     each <i>sensor</i> is a JSON array of <i>rectangles</i>, each <i>rectangle</i> is:
+     *     <br/>
+     *     <code>{"x": <i>x</i>, "y": <i>y</i>, "width": <i>width</i>,"height": <i>height</i>}</code>
+     *     <br/>All values are measured in relative portions of a video frame,
+     *     <i>x</i> and <i>width</i> in range [0..43], <i>y</i> and <i>height</i> in range [0..31],
+     *     zero is the left-top corner.
+     *     <br/>Example of a full-frame rectangle for a single-sensor camera:
+     *     <code>[[{"x":0,"y":0,"width":43,"height":31}]]</code>
+     *     <br/>Example of two rectangles for a single-sensor camera:
+     *     <code>[[{"x":0,"y":0,"width":5,"height":7},{"x":12,"y":10,"width":8,"height":6}]]</code>
+     *     <br/>Analytics Search Format: string with a JSON object that might contain the following
+     *     fields (keys):
+     *     <br/>
+     *     <ul>
+     *     <li>"boundingBox" key represents a <i>rectangle</i>. The value is a JSON object with the
+     *     same format as the Motion Search rectangle;</li>
+     *     <li>"freeText" key for full-text search over analytics data attributes. The value is
+     *     expected to be a string with search input;
+     *     </li>
+     *     </ul>
+     *     <br/>Example of the JSON object:
+     *     <code>{"boundingBox":{"height":0,"width":0.1,"x":0.0,"y":1.0},"freeText":"Test"}</code>
+     *     <br/>
+     *     <br/>ATTENTION: This field is an enquoted JSON string containing a JSON array. Example:
+     *     <code>"[\\"item1\\", \\"item2\\"]"</code>
      */
-    bool includeCloudData = false;
+    QString filter;
 
     bool operator==(const DeviceFootageRequest& other) const = default;
 };
 
 #define DeviceFootageRequest_Fields \
     (id)(startTimeMs)(endTimeMs)(detailLevelMs)(keepSmallChunks)(preciseBounds)(maxCount) \
-    (storageLocation)(periodType)(includeCloudData)
+    (storageLocation)(includeCloudData)(periodType)(filter)
 QN_FUSION_DECLARE_FUNCTIONS(DeviceFootageRequest, (json), NX_VMS_API);
 
 struct DeviceDiagnosis
