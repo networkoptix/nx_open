954d428b18e2d1aae80ed5405aa207b22217a1c8
Yuriy <ynikitenkov@networkoptix.com>
2024-03-11 15:49:55 -0700

    VMS-51733: Fixed build for Xcode 15.3 (15E204a)

--- cmake/toolchain/common/macos.cmake
+++ cmake/toolchain/common/macos.cmake
@@ -3,17 +3,18 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 
 execute_process(
-    COMMAND sh -c "xcodebuild -version | awk '/Xcode/ {print $2}'"
-    OUTPUT_VARIABLE xcode_version)
+    COMMAND sh -c "clang++ --version | awk -F '(clang-|\\\\))' '{print $2}'"
+    OUTPUT_VARIABLE clang_version)
 
-string(REGEX MATCH "([0-9]+)\\.([0-9]+)" has_xcode_version ${xcode_version})
-if(has_xcode_version)
+string(REGEX MATCH "([0-9]+)\\.([0-9]+)" has_clang_version ${clang_version})
+
+if(has_clang_version)
     set(major ${CMAKE_MATCH_1})
     set(minor ${CMAKE_MATCH_2})
 
-    # We have to set deployment target to 13.3 while using XCode 15.3+ since it uses features from
-    # libc++ which are only available in macOS 13.3+.
-    if(major LESS_EQUAL 15 AND minor LESS 3)
+    # We have to set deployment target to 13.3 while using clang from Xcode 15.3+ since it
+    # uses features from libc++ which are only available in macOS 13.3+.
+    if((major LESS 1500) OR (major EQUAL 1500 AND minor LESS 3))
         set(CMAKE_OSX_DEPLOYMENT_TARGET 11.0 CACHE INTERNAL "")
     else()
         set(CMAKE_OSX_DEPLOYMENT_TARGET 13.3 CACHE INTERNAL "")
