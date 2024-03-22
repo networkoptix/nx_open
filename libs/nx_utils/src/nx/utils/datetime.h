9d42b4c15ccfb606aceaacefe7458f890f0e4dbc
Yuri Kuznecov <ykuznecov@networkoptix.com>
2024-03-22 17:18:28 +0300

    VMS-51928: Move helper functions parseDateTime from nx::aws::client and nx::network to nx::utils
    (cherry picked from commit ee935f67d79baf6990f3c5e5f331a9ba5323f4ec)

--- libs/nx_utils/src/nx/utils/datetime.h
+++ libs/nx_utils/src/nx/utils/datetime.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include <chrono>
+#include <string>
 
+#include <QtCore/QDateTime>
 #include <QtCore/QString>
 
 /** Time value for 'now'. */
@@ -17,8 +19,7 @@ static const qint64 DATETIME_NOW = std::numeric_limits<qint64>::max();
 */
 static const qint64 DATETIME_INVALID = std::numeric_limits<qint64>::min();
 
-namespace nx {
-namespace utils {
+namespace nx::utils {
 
 QString NX_UTILS_API timestampToRfc2822(qint64 timestampMs);
 QString NX_UTILS_API timestampToRfc2822(std::chrono::milliseconds timestamp);
@@ -29,5 +30,26 @@ QString NX_UTILS_API timestampToDebugString(qint64 timestampMs,
 QString NX_UTILS_API timestampToDebugString(std::chrono::milliseconds timestamp,
     const QString& format = QString());
 
-} // namespace utils
-} // namespace nx
+/**
+ * Convert QDateTime to HTTP header date format according to RFC 1123#5.2.14.
+ */
+NX_UTILS_API std::string formatDateTime(const QDateTime& value);
+
+/**
+ * Parses the http Date according to RFC 2616#3.3 Date/Time Formats.
+ *
+ * NOTE: There are 3 different formats, though 2 and 3 are deprecated by HTTP 1.1 standard
+ * The order in which parsing is attempted is:
+ * 1. Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
+ * 2. Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850
+ * 3. Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+ */
+NX_UTILS_API QDateTime parseDateToQDateTime(const std::string_view& str);
+
+NX_UTILS_API std::string formatDateTime(std::chrono::system_clock::time_point tp);
+
+NX_UTILS_API std::chrono::system_clock::time_point parseDateTime(std::string_view datetime);
+
+NX_UTILS_API std::string formatDateTime(std::chrono::steady_clock::time_point tp);
+
+} // namespace nx::utils
