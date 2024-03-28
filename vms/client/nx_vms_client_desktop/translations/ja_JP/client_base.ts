a021388717aba0b1cec7850d1756bab3d92a0de3
Jenkins <jenkins@networkoptix.com>
2024-03-28 00:08:27 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_6.0
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_6_0.download-translation/135/

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
@@ -7566,7 +7566,7 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>Use License</source>
       <translation>
-        <numerusform>ライセンスの使用</numerusform>
+        <numerusform>ライセンスを使用</numerusform>
       </translation>
     </message>
   </context>
@@ -7574,11 +7574,11 @@ If you intended to move analytics data to another storage location, please conta
     <name>nx::vms::client::desktop::CameraMotionSettingsWidget</name>
     <message>
       <source>Motion detection will work only when camera is being viewed. Enable recording to make it work all the time.</source>
-      <translation>モーション検知は、カメラが表示されている場合にのみ動作します。常に動作させるためには録画を有効にしてください。</translation>
+      <translation>モーション検知は、カメラ映像が表示されている間のみ動作します。常に動作させるためには、録画を有効にしてください。</translation>
     </message>
     <message>
       <source>Maximum number of motion detection rectangles for current camera is reached</source>
-      <translation>現在のカメラのモーションエリアの最大数に達しました</translation>
+      <translation>現在のカメラのモーション検知エリアの最大数に達しました</translation>
     </message>
     <message>
       <source>Maximum number of ignore motion rectangles for current camera is reached</source>
@@ -7586,7 +7586,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Maximum number of detect motion rectangles for current camera is reached</source>
-      <translation>現在のカメラのモーションエリアの最大数に達しました</translation>
+      <translation>現在のカメラのモーション検知エリアの最大数に達しました</translation>
     </message>
     <message>
       <source>Reset motion regions to default?</source>
@@ -7594,11 +7594,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>This action cannot be undone.</source>
-      <translation>この操作は元に戻せません</translation>
+      <translation>この操作は元に戻せません。</translation>
     </message>
     <message>
       <source>Primary Stream</source>
-      <translation>プライマリ ストリーム</translation>
+      <translation>プライマリストリーム</translation>
     </message>
     <message>
       <source>Secondary Stream</source>
@@ -7907,7 +7907,7 @@ If you intended to move analytics data to another storage location, please conta
     <name>nx::vms::client::desktop::CameraStreamsDialog</name>
     <message>
       <source>Primary Stream</source>
-      <translation>プライマリ ストリーム</translation>
+      <translation>プライマリストリーム</translation>
     </message>
     <message>
       <source>Secondary Stream</source>
@@ -7915,7 +7915,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>No secondary stream</source>
-      <translation>セカンダリストリームなし</translation>
+      <translation>セカンダリストリームがありません</translation>
     </message>
     <message>
       <source>Invalid stream address</source>
@@ -7985,15 +7985,15 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Broken update package.</source>
-      <translation>更新プログラムパッケージが壊れています。</translation>
+      <translation>更新パッケージが壊れています。</translation>
     </message>
     <message>
       <source>Internal error.</source>
-      <translation>内部エラー</translation>
+      <translation>内部エラー。</translation>
     </message>
     <message>
       <source>Not enough space on disk to install the client update.</source>
-      <translation>クライアント更新プログラムをインストールするにはディスク容量が足りません。</translation>
+      <translation>クライアント更新プログラムをインストールするためのディスク領域が不足しています。</translation>
     </message>
     <message>
       <source>Installation package has been lost.</source>
@@ -8119,7 +8119,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Could not connect to server. Video Wall will be closed.</source>
-      <translation>サーバーに接続できませんでした。ビデオウォールを閉じます。</translation>
+      <translation>サーバーに接続できませんでした。ビデオウォールを終了します。</translation>
     </message>
     <message>
       <source>Could not connect to server. Application will be closed.</source>
@@ -8412,7 +8412,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Device search failed</source>
-      <translation>デバイス検索失敗</translation>
+      <translation>デバイス検索に失敗しました</translation>
     </message>
     <message numerus="yes">
       <source>Failed to add %n devices</source>
@@ -8422,7 +8422,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Scan</source>
-      <translation>検索</translation>
+      <translation>スキャン</translation>
     </message>
     <message>
       <source>Search</source>
@@ -8463,7 +8463,7 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>%n devices are being added. You can close this dialog or start a new search</source>
       <translation>
-        <numerusform>%n デバイスを追加しています。このダイアログボックスを閉じるか、新しい検索を開始することができます。</numerusform>
+        <numerusform>%n デバイスを追加しています。このダイアログを閉じることも、新しい検索を開始することもできます。</numerusform>
       </translation>
     </message>
     <message>
@@ -8504,7 +8504,7 @@ If you intended to move analytics data to another storage location, please conta
     <name>nx::vms::client::desktop::EulaDialog</name>
     <message>
       <source>Please review and agree to the %1 in order to proceed</source>
-      <translation>続行するには %1 に同意してください</translation>
+      <translation>続行するには、 %1 を確認して同意してください</translation>
     </message>
     <message>
       <source>To use the software you must agree with the end user license agreement</source>
@@ -8630,7 +8630,7 @@ If you intended to move analytics data to another storage location, please conta
     <name>nx::vms::client::desktop::EventSearchWidget</name>
     <message>
       <source>No events</source>
-      <translation>イベントなし</translation>
+      <translation>イベントはありません</translation>
     </message>
     <message numerus="yes">
       <source>%n events</source>
@@ -8782,11 +8782,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Exported .EXE file will have size over 4 GB and cannot be opened by double-click in Windows. It can be played only in %1 Client.</source>
-      <translation>エクスポートされた .EXE ファイルのサイズは4 GBを超えるため、Windowsではダブルクリックで開くことができません。%1 クライアントでのみ再生できます。</translation>
+      <translation>エクスポートされる .EXE ファイルのサイズが 4 GB を超えるため、Windows でのダブルクリックでは開くことができません。 %1 クライアントでのみ再生できます。</translation>
     </message>
     <message>
       <source>Settings are not available for .NOV and .EXE files.</source>
-      <translation>.NOVファイルおよび.EXEファイルでは設定を使用できません。</translation>
+      <translation>.NOV ファイルおよび .EXE ファイルでは設定を利用できません。</translation>
     </message>
     <message>
       <source>Local files, server monitor widgets and webpages will not be exported.</source>
@@ -9038,7 +9038,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Image cannot be loaded.</source>
-      <translation>画像を読み取ることができません。</translation>
+      <translation>画像を読み込むことができません。</translation>
     </message>
   </context>
   <context>
@@ -9049,11 +9049,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Servers from the other System will appear in the resource tree.</source>
-      <translation>他のシステムのサーバーはリソース ツリーに表示されます</translation>
+      <translation>他システムのサーバーがリソースツリーに表示されます。</translation>
     </message>
     <message>
       <source>Connecting to the current System...</source>
-      <translation>システムに接続...</translation>
+      <translation>現在のシステムに接続しています...</translation>
     </message>
     <message>
       <source>Server will be connected to System shortly</source>
@@ -9061,11 +9061,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>It will appear in the resource tree when the database synchronization is finished.</source>
-      <translation>データベースの同期が完了すると、リソースツリーに表示されます</translation>
+      <translation>データベースの同期が完了すると、サーバーがリソースツリーに表示されます。</translation>
     </message>
     <message>
       <source>Failed to merge %1 to our system.</source>
-      <translation>システムに %1 を統合できませんでした。</translation>
+      <translation>このシステムに %1 を統合することに失敗しました。</translation>
     </message>
   </context>
   <context>
@@ -9107,7 +9107,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Picture cannot be loaded</source>
-      <translation>画像を読み取ることができません</translation>
+      <translation>画像を読み込むことができません</translation>
     </message>
   </context>
   <context>
@@ -9118,7 +9118,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Custom number that can be assigned to a layout for quick identification and access</source>
-      <translation>迅速な識別とアクセスのためにレイアウトに割り当てることのできるカスタム番号</translation>
+      <translation>識別や制御を簡単にするための、レイアウトに割り当て可能なカスタム番号</translation>
     </message>
     <message>
       <source>This ID is already used in the System. Use Generate button to find a free ID.</source>
@@ -9341,7 +9341,7 @@ Remember to return the Logging Level to its default setting after you have colle
     <name>nx::vms::client::desktop::LoginDialog</name>
     <message>
       <source>Connect to Server...</source>
-      <translation>サーバーに接続....</translation>
+      <translation>サーバーに接続...</translation>
     </message>
     <message>
       <source>Version %1</source>
@@ -9553,11 +9553,11 @@ Remember to return the Logging Level to its default setting after you have colle
     </message>
     <message>
       <source>Use a specific hostname or IP address rather than %1.</source>
-      <translation>%1 よりも具体的なホスト名またはIPアドレスを使用してください。</translation>
+      <translation>%1ではなく、具体的なホスト名またはIPアドレスを使用してください。</translation>
     </message>
     <message>
       <source>This is the current System URL.</source>
-      <translation>既存のシステムのURL</translation>
+      <translation>これは現在のシステムの URL です。</translation>
     </message>
     <message>
       <source>Merge with %1</source>
@@ -9577,7 +9577,7 @@ Remember to return the Logging Level to its default setting after you have colle
     </message>
     <message>
       <source>Failed to merge Systems</source>
-      <translation>システム統合に失敗しました</translation>
+      <translation>システムの統合に失敗しました</translation>
     </message>
     <message>
       <source>support</source>
@@ -9719,7 +9719,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Download in External Browser</source>
-      <translation>外部ブラウザーでダウンロード</translation>
+      <translation>外部ブラウザでダウンロード</translation>
     </message>
     <message>
       <source>Copy Link to Clipboard</source>
@@ -9727,7 +9727,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>You have already installed this version.</source>
-      <translation>このバージョンは既にインストールされています</translation>
+      <translation>このバージョンはすでにインストールされています。</translation>
     </message>
     <message>
       <source>Build not found</source>
@@ -9747,7 +9747,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Update file is not found</source>
-      <translation>更新プログラム ファイルが見つかりませんでした</translation>
+      <translation>更新ファイルが見つかりません</translation>
     </message>
     <message>
       <source>Upgrade package is broken</source>
@@ -9760,7 +9760,7 @@ You can force it, but it may significantly increase CPU load.</source>
     <message>
       <source>Incompatible %1 instance. To update disconnect System from %1 first.</source>
       <comment>%1 here will be substituted with cloud name e.g. &apos;Nx Cloud&apos;.</comment>
-      <translation>%1 とは互換性がありません。更新するにはシステムを %1 から切断します。</translation>
+      <translation>互換性のない %1 インスタンスです。更新するには、まず %1 からシステムを切断してください。</translation>
     </message>
     <message>
       <source>No update package available</source>
@@ -9776,7 +9776,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Some servers are offline and will not be updated. Skip them?</source>
-      <translation>一部のサーバーはオフラインであり、更新されません。スキップしますか？</translation>
+      <translation>一部のサーバーはオフラインであるため、更新されません。それらをスキップしてよろしいですか?</translation>
     </message>
     <message numerus="yes">
       <source>Missing update package for the client and %n servers</source>
@@ -9790,7 +9790,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>There are no online servers to update.</source>
-      <translation>更新するオンラインサーバーはありません。</translation>
+      <translation>更新できるオンラインサーバーがありません。</translation>
     </message>
     <message>
       <source>Cancel update and delete all downloaded data?</source>
@@ -9826,7 +9826,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>If the problem persists, please contact Customer Support.</source>
-      <translation>問題が解決しない場合は、カスタマー サポートに連絡してください</translation>
+      <translation>問題が解決しない場合は、カスタマーサポートにお問い合わせください。</translation>
     </message>
     <message>
       <source>Try again</source>
@@ -9846,11 +9846,11 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Please update %1 manually using an installation package.</source>
-      <translation>インストール パッケージを使用して手動で %1 を更新してください</translation>
+      <translation>インストールパッケージを使用して、手動で %1 を更新してください。</translation>
     </message>
     <message>
       <source>%1 will be restarted to the updated version.</source>
-      <translation>%1 は、更新されたバージョンで再起動します</translation>
+      <translation>更新されたバージョンに %1 を再起動します。</translation>
     </message>
     <message>
       <source>OK</source>
@@ -9858,7 +9858,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>There was an error while installing updates:</source>
-      <translation>アップデートのインストール中にエラーが発生しました。</translation>
+      <translation>更新プログラムのインストール中にエラーが発生しました:</translation>
     </message>
     <message>
       <source>Downloading updates...</source>
@@ -9866,7 +9866,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Finish Update</source>
-      <translation>更新終了</translation>
+      <translation>更新を完了する</translation>
     </message>
     <message>
       <source>Cancel</source>
@@ -9874,11 +9874,11 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>The latest version is already installed</source>
-      <translation>お使いのソフトウェアは最新版です</translation>
+      <translation>最新バージョンがすでにインストールされています</translation>
     </message>
     <message>
       <source>This version is already installed</source>
-      <translation>このバージョンは既にインストールされています</translation>
+      <translation>このバージョンはすでにインストールされています</translation>
     </message>
     <message>
       <source>Install update</source>
@@ -9890,7 +9890,7 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Upload &amp;&amp; Install</source>
-      <translation>アップロードとインストール</translation>
+      <translation>アップロードしてインストール</translation>
     </message>
     <message>
       <source>Download</source>
@@ -9918,11 +9918,11 @@ You can force it, but it may significantly increase CPU load.</source>
     </message>
     <message>
       <source>Installing updates...</source>
-      <translation>更新をインストール中...</translation>
+      <translation>更新プログラムをインストールしています...</translation>
     </message>
     <message>
       <source>Installing client updates...</source>
-      <translation>クライアントアップデートをインストールしています...</translation>
+      <translation>クライアント用更新プログラムをインストールしています...</translation>
     </message>
     <message>
       <source>Starting update to ...</source>
@@ -11098,7 +11098,7 @@ Do not pin this certificate if you did not modify %2 Server SSL certificate.</so
     </message>
     <message>
       <source>library file cannot be loaded</source>
-      <translation>ライブラリファイルを読み込めません</translation>
+      <translation>ライブラリファイルを読み込むことができません</translation>
     </message>
     <message>
       <source>invalid or incompatible plugin library</source>
