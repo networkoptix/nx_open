4ee8323e95c28c0f8f11506a1e83acb586d0e59e
Jenkins <jenkins@networkoptix.com>
2024-04-02 01:08:41 +0000

    Update translatable strings (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/en_US/common.ts
+++ vms/libs/nx_vms_common/translations/en_US/common.ts
@@ -1791,6 +1791,10 @@
         <source>Local System ID, null means the System is not set up yet.</source>
         <translation></translation>
     </message>
+    <message>
+        <source>HTTP header: Server, supported variables: $vmsName, $vmsVersion, $company, $compatibility</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>nx::vms::event::StringsHelper</name>
