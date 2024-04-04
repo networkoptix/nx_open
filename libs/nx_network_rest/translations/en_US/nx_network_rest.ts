f1668c3e860dc7605f101c851531813dabd9c739
Sergei Ivanov <sivanov@networkoptix.com>
2024-04-04 19:06:14 +0200

    VMS-51952: Make nx_network_rest library translatable

---
+++ libs/nx_network_rest/translations/en_US/nx_network_rest.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en">
+<context>
+    <name>ApiErrorStrings</name>
+    <message>
+        <source>Invalid JSON content.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unsupported content type.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AuthResultTranslations</name>
+    <message>
+        <source>This user does not exist or is unable to login.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wrong password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The password is expired. Please contact your Site administrator.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The LDAP server is not accessible. Please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 is not accessible yet. Please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This user has been disabled by a Site administrator.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The user is locked out due to several failed attempts. Please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This authorization method is forbidden. Please contact your Site administrator.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The session key is invalid or expired.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The session key is to old for this Site.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The ticket key is invalid or expired.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>More than one user with the same login are presented.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to verify LDAP server SSL/TLS certificate.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal server error (%1). Please contact your Site administrator.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Result</name>
+    <message>
+        <source>Invalid parameter `%1`: %2.</source>
+        <comment>%1 is name, %2 is value.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Missing required parameter: %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to process request.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forbidden.</source>
+        <comment>Generic HTTP response</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conflict.</source>
+        <comment>Generic HTTP response</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bad request.</source>
+        <comment>Generic HTTP response</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not implemented.</source>
+        <comment>Generic HTTP response</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not found.</source>
+        <comment>Generic HTTP response</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unsupported media type.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Service unavailable.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unauthorized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Session expired.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Session authorization required.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Session is too old according to the Site config.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resource no longer present on server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
