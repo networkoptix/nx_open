3ad26e8741cd8c4e8ab4c9a82b856f3f54947b7d
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-04-11 12:27:48 -0700

    VMS-52209: Fix deployment to macOS 11 with Xcode 15.3
    ADL bumps into preprocessor error when looking for std::chrono stream operators
    with Xcode 15.3 because they are supported only for CMAKE_OSX_DEPLOYMENT_TARGET >= 13.3
    
    Define these operators to satisfy the compiler.

--- cmake/toolchain/common/macos.cmake
+++ cmake/toolchain/common/macos.cmake
@@ -2,23 +2,4 @@
 
 set(CMAKE_SYSTEM_NAME Darwin)
 
-execute_process(
-    COMMAND sh -c "clang++ --version | awk -F '(clang-|\\\\))' '{print $2}'"
-    OUTPUT_VARIABLE clang_version)
-
-string(REGEX MATCH "([0-9]+)\\.([0-9]+)" has_clang_version ${clang_version})
-
-if(has_clang_version)
-    set(major ${CMAKE_MATCH_1})
-    set(minor ${CMAKE_MATCH_2})
-
-    # We have to set deployment target to 13.3 while using clang from Xcode 15.3+ since it
-    # uses features from libc++ which are only available in macOS 13.3+.
-    if((major LESS 1500) OR (major EQUAL 1500 AND minor LESS 3))
-        set(CMAKE_OSX_DEPLOYMENT_TARGET 11.0 CACHE INTERNAL "")
-    else()
-        set(CMAKE_OSX_DEPLOYMENT_TARGET 13.3 CACHE INTERNAL "")
-    endif()
-else()
-    message(FATAL_ERROR "Can't get XCode version, generation failed")
-endif()
+set(CMAKE_OSX_DEPLOYMENT_TARGET 11.0 CACHE INTERNAL "")
