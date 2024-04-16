d8b64ab88a3b8915090928ab18e75ac2e94e9e16
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-04-15 17:54:59 -0700

    VMS-51692: Reduce the number of window updates when RHI-based rendering is used

--- vms/client/nx_vms_client_desktop/src/ui/graphics/view/quick_widget_container.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/view/quick_widget_container.cpp
@@ -2,6 +2,8 @@
 
 #include "quick_widget_container.h"
 
+#include <ui/graphics/view/graphics_view.h>
+
 namespace nx::vms::client::desktop {
 
 void QuickWidgetContainer::setQuickWidget(QQuickWidget* quickWidget)
@@ -9,11 +11,43 @@ void QuickWidgetContainer::setQuickWidget(QQuickWidget* quickWidget)
     m_quickWidget = quickWidget;
     m_quickWidget->setParent(this);
     m_quickWidget->setGeometry(0, 0, width(), height());
+
+    setAttribute(Qt::WA_OpaquePaintEvent);
+    setAttribute(Qt::WA_NoSystemBackground);
+    setAttribute(Qt::WA_DontShowOnScreen);
 }
 
 QQuickWidget* QuickWidgetContainer::quickWidget() const
 {
     return m_quickWidget;
+}
+
+void QuickWidgetContainer::setView(QWidget* view)
+{
+    m_view = view;
+}
+
+QWidget* QuickWidgetContainer::view() const
+{
+    return m_view;
+}
+
+bool QuickWidgetContainer::event(QEvent* event)
+{
+    if (event->type() == QEvent::Paint)
+    {
+        if (auto view = qobject_cast<QnGraphicsView*>(m_view))
+        {
+            view->paintRhi();
+            return true;
+        }
+    }
+    return base_type::event(event);
+}
+
+QPaintEngine* QuickWidgetContainer::paintEngine() const
+{
+    return nullptr;
 }
 
 void QuickWidgetContainer::resizeEvent(QResizeEvent* event)
