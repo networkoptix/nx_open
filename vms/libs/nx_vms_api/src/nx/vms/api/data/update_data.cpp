09d309ba521c9a19e6a926e9dc6451fe0eef67f9
Craig Hoedebeck <choedebeck@networkoptix.com>
2024-04-18 17:51:12 +0000

    VMS-37039: VMS Server API: Deprecate current update process and create rest updater

--- vms/libs/nx_vms_api/src/nx/vms/api/data/update_data.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/update_data.cpp
@@ -4,9 +4,22 @@
 
 #include <nx/fusion/model_functions.h>
 
-namespace nx {
-namespace vms {
-namespace api {
+namespace nx::vms::api {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ProductInfo, (json), ProductInfo_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdateInfoRequest, (json), UpdateInfoRequest_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdateVersions, (json), UpdateVersions_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdatePackage, (json), UpdatePackage_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdateInformation, (json), UpdateInformation_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(StartUpdateReply, (json), StartUpdateReply_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(DownloadStatus, (json), DownloadStatus_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdateStatusInfo, (json), UpdateStatusInfo_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(InstallUpdateRequest, (json), InstallUpdateRequest_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(FinishUpdateRequest, (json), FinishUpdateRequest_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(
+    PersistentUpdateStorageInfo, (json), PersistentUpdateStorageInfo_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(
+    PersistentUpdateStorageRequest, (json), PersistentUpdateStorageRequest_Fields)
 
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(
     UpdateUploadData, (ubjson)(xml)(json)(sql_record)(csv_record), UpdateUploadData_Fields)
@@ -15,6 +28,4 @@ QN_FUSION_ADAPT_STRUCT_FUNCTIONS(UpdateUploadResponseData,
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(
     UpdateInstallData, (ubjson)(xml)(json)(sql_record)(csv_record), UpdateInstallData_Fields)
 
-} // namespace api
-} // namespace vms
-} // namespace nx
+} // namespace nx::vms::api
