de08a013ea1b173c9083d4b3c21e967a4b9e60f1
Jenkins <jenkins@networkoptix.com>
2024-04-26 00:58:21 +0000

    Updated translations (committed by Jenkins)
    Branch: mobile_24.1
    
    https://jenkins.us.nxteam.dev/job/nightly.mobile.mobile_24_1.download-translation/114/

--- vms/client/nx_vms_client_core/translations/ja_JP/client_core.ts
+++ vms/client/nx_vms_client_core/translations/ja_JP/client_core.ts
@@ -59,7 +59,7 @@
     </message>
     <message>
       <source>Server has a different version:</source>
-      <translation>サーバーのバージョンが異なります：</translation>
+      <translation>サーバーのバージョンが異なります:</translation>
     </message>
     <message>
       <source>You will be asked to restart the client in compatibility mode.</source>
@@ -113,7 +113,7 @@
     </message>
     <message>
       <source>User is disabled</source>
-      <translation>ユーザーは無効になっています</translation>
+      <translation>ユーザーは無効な状態です</translation>
     </message>
     <message>
       <source>Too many attempts. Try again in a minute.</source>
