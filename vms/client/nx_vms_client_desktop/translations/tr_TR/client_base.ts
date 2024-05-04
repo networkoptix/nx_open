8102d81fa55ef796943fa2875e828a6569135007
Jenkins <jenkins@networkoptix.com>
2024-05-04 00:08:44 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_6.0
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_6_0.download-translation/172/

--- vms/client/nx_vms_client_desktop/translations/tr_TR/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/tr_TR/client_base.ts
@@ -1373,7 +1373,7 @@
     <message>
       <source>Language Name</source>
       <comment>Language name that will be displayed to the user.</comment>
-      <translation>Turkish</translation>
+      <translation>Dil Adı</translation>
     </message>
   </context>
   <context>
