78559b5ded9d064d5df4ba877f26988caea51a13
Armen Nunuparov <anunuparov@networkoptix.com>
2024-05-16 13:49:36 +0000

    VMS-52327: Add query and fragments to sendHttpRequest, improvements for load balancer url in Nx Analytics Plugin
    Add automatic load balancer url selection, based on current `cloudHost` value
    
    Rename old load balancer field, to custom load balancer field, which is now become optional
    
    Add possibility to pass query and fragment to http requests via UtilityProvider
    
    (cherry picked from commit 4f223098492902a434cd91ec5e799fea0c3face7)

--- vms/server/plugins/analytics/stub_analytics_plugin/src/nx/vms_server_plugins/analytics/stub/http_requests/settings.cpp
+++ vms/server/plugins/analytics/stub_analytics_plugin/src/nx/vms_server_plugins/analytics/stub/http_requests/settings.cpp
@@ -7,7 +7,7 @@ namespace nx::vms_server_plugins::analytics::stub::http_requests {
 extern const std::string kHttpDomainVar = "httpDomain";
 extern const std::string kHttpVmsDomain = "vms";
 extern const std::string kHttpCloudDomain = "cloud";
-extern const std::string kHttpPathVar = "httpPath";
+extern const std::string kHttpUrlVar = "httpUrl";
 extern const std::string kHttpMethodVar = "httpMethod";
 extern const std::string kHttpMimeTypeVar = "mimeType";
 extern const std::string kHttpRequestBodyVar = "requestBody";
