61f16ce39305d38400fd5af127ad85331421dec0
Sergei Kolesnik <skolesnik@networkoptix.com>
2024-05-23 14:47:51 +0000

    VMS-52169: Server API: Implement Analytics Engine Settings handler
    Removed `modelId` parameter from `AnalyticsEngineResource::setSettingsInternal` since it is never used by the compiled code.

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/analytics_engine_settings.h
@@ -0,0 +1,39 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <map>
+
+#include <QtCore/QJsonObject>
+#include <QtCore/QString>
+
+#include <nx/fusion/model_functions_fwd.h>
+#include <nx/utils/uuid.h>
+
+namespace nx::vms::api {
+
+struct NX_VMS_API AnalyticsEngineSettings
+{
+    /**%apidoc
+     * Id of an Analytics Engine.
+     */
+    nx::Uuid id;
+
+    /**%apidoc[opt]
+     * Name-value map with setting values, using json types corresponding to each setting type.
+     * If not specified, current setting values will be returned.
+     */
+    QJsonObject values;
+
+    /**%apidoc[readonly]
+     * Name-value map with errors that occurred while performing the current settings operation.
+     */
+    std::optional<std::map<QString, QString>> _error;
+
+    nx::Uuid getId() const { return id; }
+};
+#define AnalyticsEngineSettings_Fields (id)(values)(_error)
+QN_FUSION_DECLARE_FUNCTIONS(AnalyticsEngineSettings, (json), NX_VMS_API);
+NX_REFLECTION_INSTRUMENT(AnalyticsEngineSettings, AnalyticsEngineSettings_Fields);
+
+} // namespace nx::vms::api
