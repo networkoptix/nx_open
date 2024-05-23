2210054d46fc2fdea926bdb46baed93f6c2a9bb6
Craig Hoedebeck <choedebeck@networkoptix.com>
2024-05-23 23:50:06 +0300

    VMS-52330: VMS Server API:  Create /rest/v4/servers/{id}/settings

---
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/settings_data.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#include "settings_data.h"
+
+#include <nx/fusion/model_functions.h>
+
+namespace nx::vms::api {
+
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(SettingsInfo, (json), SettingsInfo_Fields)
+
+} // namespace nx::vms::api
