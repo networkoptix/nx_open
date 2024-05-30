8fdc16e147f6bc20f391abc8089a121a33fd127a
Leonid Busygin <lbusygin@networkoptix.com>
2024-05-30 16:51:08 +0300

    VMS-53024: add ini option to disable color conversion optimization

---
+++ vms/libs/nx_vms_common/src/utils/media/nx_image_ini.h
@@ -0,0 +1,15 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <nx/kit/ini_config.h>
+
+struct NxImageIniConfig: public nx::kit::IniConfig
+{
+    NxImageIniConfig(): nx::kit::IniConfig("nx_image.ini") { reload(); }
+
+    NX_INI_FLAG(1, enableSseColorConvertion,
+        "Enables SSE color convertion optimizations. If false Ffmpeg sws_scale will be used");
+};
+
+NX_VMS_COMMON_API NxImageIniConfig& nxImageIni();
