e8e15ad12660ed2aa20cb924a54b8ce245f0431c
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-06-06 19:58:46 -0700

    MOBILE-1287: Move toolchain files for iOS and Android to open source folder

---
+++ cmake/toolchain/android_arm32.cmake
@@ -0,0 +1,4 @@
+## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+set(ANDROID_ABI armeabi-v7a)
+include(${CMAKE_CURRENT_LIST_DIR}/common/android.cmake)
