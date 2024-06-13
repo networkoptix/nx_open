9ad89ef4e08c4352daf72af3b85cb59d8f1620c8
Maksim Tikhomirov <MTikhomirov@networkoptix.com>
2024-06-13 16:29:44 +0400

    VMS-52865: Metadata API: Add support for all types of Metadata sent by an Integration

--- vms/libs/nx_vms_api/src/nx/vms/api/analytics/types.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/types.cpp
@@ -6,42 +6,7 @@
 
 namespace nx::vms::api::analytics {
 
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(EngineIdentity, (json),
-    nx_vms_api_analytics_EngineIdentity_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(DeviceAgentIdentity, (json),
-    nx_vms_api_analytics_DeviceAgentIdentity_Fields, (brief, true))
-
-bool operator<(const DeviceAgentIdentity& first, const DeviceAgentIdentity& second)
-{
-    if (first.engineId != second.engineId)
-        return first.engineId < second.engineId;
-
-    if (first.deviceId != second.deviceId)
-        return first.deviceId < second.deviceId;
-
-    return false;
-}
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObtainEngineRequestParameters, (json),
-    nx_vms_api_analytics_ObtainEngineRequestParameters_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObtainDeviceAgentRequestParameters, (json),
-    nx_vms_api_analytics_ObtainDeviceAgentRequestParameters_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(RpcObjectMetadataAttribute, (json),
-    nx_vms_api_analytics_RpcObjectMetadataAttribute_Fields, (brief, true))
-
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(Rect, (json),
     nx_vms_api_analytics_Rect_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(RpcObjectMetadata, (json),
-    nx_vms_api_analytics_RpcObjectMetadata_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(RpcObjectMetadataPacket, (json),
-    nx_vms_api_analytics_RpcObjectMetadataPacket_Fields, (brief, true))
-
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(DataPacket, (json),
-    nx_vms_api_analytics_DataPacket_Fields, (brief, true))
 
 } // namespace nx::vms::api::analytics
--- vms/libs/nx_vms_api/src/nx/vms/api/analytics/types.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/analytics/types.h
@@ -11,45 +11,6 @@
 
 namespace nx::vms::api::analytics {
 
-struct NX_VMS_API EngineIdentity
-{
-    std::string engineId;
-};
-#define nx_vms_api_analytics_EngineIdentity_Fields (engineId)
-QN_FUSION_DECLARE_FUNCTIONS(EngineIdentity, (json), NX_VMS_API)
-
-struct NX_VMS_API DeviceAgentIdentity
-{
-    std::string engineId;
-    std::string deviceId;
-};
-#define nx_vms_api_analytics_DeviceAgentIdentity_Fields (engineId)(deviceId)
-QN_FUSION_DECLARE_FUNCTIONS(DeviceAgentIdentity, (json), NX_VMS_API)
-
-NX_VMS_API bool operator<(const DeviceAgentIdentity& first, const DeviceAgentIdentity& second);
-
-struct NX_VMS_API ObtainEngineRequestParameters
-{
-    std::string engineId;
-};
-#define nx_vms_api_analytics_ObtainEngineRequestParameters_Fields (engineId)
-QN_FUSION_DECLARE_FUNCTIONS(ObtainEngineRequestParameters, (json), NX_VMS_API)
-
-struct NX_VMS_API ObtainDeviceAgentRequestParameters
-{
-    std::string deviceId;
-};
-#define nx_vms_api_analytics_ObtainDeviceAgentRequestParameters_Fields (deviceId)
-QN_FUSION_DECLARE_FUNCTIONS(ObtainDeviceAgentRequestParameters, (json), NX_VMS_API)
-
-struct NX_VMS_API RpcObjectMetadataAttribute
-{
-    std::string name;
-    std::string value;
-};
-#define nx_vms_api_analytics_RpcObjectMetadataAttribute_Fields (name)(value)
-QN_FUSION_DECLARE_FUNCTIONS(RpcObjectMetadataAttribute, (json), NX_VMS_API)
-
 struct Rect
 {
     float x = 0;
@@ -59,32 +20,5 @@ struct Rect
 };
 #define nx_vms_api_analytics_Rect_Fields (x)(y)(width)(height)
 QN_FUSION_DECLARE_FUNCTIONS(Rect, (json), NX_VMS_API)
-
-struct NX_VMS_API RpcObjectMetadata
-{
-    nx::Uuid trackId;
-    std::string typeId;
-    Rect boundingBox;
-    std::vector<RpcObjectMetadataAttribute> attributes;
-};
-#define nx_vms_api_analytics_RpcObjectMetadata_Fields (trackId)(typeId)(boundingBox)(attributes)
-QN_FUSION_DECLARE_FUNCTIONS(RpcObjectMetadata, (json), NX_VMS_API)
-
-struct NX_VMS_API RpcObjectMetadataPacket
-{
-    std::chrono::microseconds timestampUs;
-    std::chrono::microseconds durationUs;
-    std::vector<RpcObjectMetadata> metadata;
-};
-#define nx_vms_api_analytics_RpcObjectMetadataPacket_Fields (timestampUs)(durationUs)(metadata)
-QN_FUSION_DECLARE_FUNCTIONS(RpcObjectMetadataPacket, (json), NX_VMS_API)
-
-struct NX_VMS_API DataPacket
-{
-    // TODO: #dmishin Either remove this struct or add other fields.
-    std::chrono::microseconds timestampUs;
-};
-#define nx_vms_api_analytics_DataPacket_Fields (timestampUs)
-QN_FUSION_DECLARE_FUNCTIONS(DataPacket, (json), NX_VMS_API)
 
 } // namespace nx::vms::api::analytics
