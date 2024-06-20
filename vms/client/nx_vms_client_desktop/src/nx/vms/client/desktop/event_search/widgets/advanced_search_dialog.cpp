f5daffe690267a78be3c6a6b1b267593e02c7b62
Denis Klychkov <dklychkov@networkoptix.com>
2024-06-19 22:03:29 -0700

    VMS-52734: Do not set parent window to the Advanced Search Dialog
    This dialog acts like a separate modeless window. Playing with its transien parent led to unwanted
    effects like unnecessary animations, unnecessary content reloading, z-order issues. This commit
    removes all the logic arount the transient parent. To make the dialog open near the main window,
    when it is created, it is placed to the same screen. It won't be centered inside the main window,
    but that's not really important.
    
    # Conflicts:
    #	open/vms/client/nx_vms_client_desktop/qml/Nx/Dialogs/Analytics/AnalyticsSearchDialog.qml
    #	open/vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/widgets/advanced_search_dialog.cpp

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/widgets/advanced_search_dialog.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/event_search/widgets/advanced_search_dialog.cpp
@@ -42,11 +42,10 @@ static const QString kScreenNumberKey = "screenNumber";
 // ------------------------------------------------------------------------------------------------
 // AdvancedSearchDialog
 
-AdvancedSearchDialog::AdvancedSearchDialog(QWidget* parent) :
+AdvancedSearchDialog::AdvancedSearchDialog(QObject* parent):
     QmlDialogWrapper(
         QUrl("Nx/LeftPanel/private/analytics/AnalyticsSearchDialog.qml"),
-        {},
-        parent),
+        {}),
     QnSessionAwareDelegate(parent)
 {
     rootObjectHolder()->object()->setProperty("workbenchContext", QVariant::fromValue(context()));
