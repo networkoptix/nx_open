681e6b0efe93e7193f2daf2362cca85218a2225d
Sergei Ivanov <sivanov@networkoptix.com>
2024-06-26 16:44:02 +0200

    VMS-53686: Use graphics software emulation in the desktop client unit tests

--- vms/client/nx_vms_client_desktop/unit_tests/src/main.cpp
+++ vms/client/nx_vms_client_desktop/unit_tests/src/main.cpp
@@ -1,5 +1,6 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
+#include <QtQuick/QQuickWindow>
 #include <QtWidgets/QApplication>
 
 #include <nx/utils/test_support/run_test.h>
@@ -9,6 +10,8 @@ int main(int argc, char** argv)
     // Unit tests should be able to run in headless mode.
     if (qgetenv("QT_QPA_PLATFORM").isEmpty())
         qputenv("QT_QPA_PLATFORM", "offscreen");
+
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::GraphicsApi::Software);
 
     QApplication application(argc, argv);
 
