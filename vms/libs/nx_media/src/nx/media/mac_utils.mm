15ea155f5f6ca88ba12edd0094b074e23594615f
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-06-28 12:59:51 -0700

    MOBILE-1948: Fix missing headers

--- vms/libs/nx_media/src/nx/media/mac_utils.mm
+++ vms/libs/nx_media/src/nx/media/mac_utils.mm
@@ -1,5 +1,8 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
+#include <unordered_map>
+#include <vector>
+
 #include <QtCore/QSize>
 
 #include <CoreMedia/CMFormatDescription.h>
