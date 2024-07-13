846f0cd1bbe36fb4d7872e9bd348c2e2fb643471
Jenkins <jenkins@networkoptix.com>
2024-07-13 01:08:35 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -7519,14 +7519,6 @@ If you intended to move analytics data to another storage location, please conta
 <context>
     <name>nx::vms::client::desktop::CameraButtonController</name>
     <message>
-        <source>Mute</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Unmute</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Heater</source>
         <translation></translation>
     </message>
