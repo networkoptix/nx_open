98b631270a2907137c1850df95cb828a26e300ad
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-07-25 17:29:48 -0700

    MOBILE-1991: Explicitly ask for recording permissions on iOS

--- vms/client/nx_vms_client_core/src/utils/mac_permissions.mm
+++ vms/client/nx_vms_client_core/src/utils/mac_permissions.mm
@@ -2,10 +2,32 @@
 
 #include "mac_permissions.h"
 
+#include <future>
+
 #include <AVFoundation/AVAudioSession.h>
 
 bool mac_ensureAudioRecordPermission()
 {
-    return [[AVAudioSession sharedInstance] recordPermission]
-        != AVAudioSessionRecordPermissionDenied;
+    // TODO: Switch to AVAudioApplication.requestRecordPermission for iOS 17.0+ and macOS 14.0+.
+    switch ([[AVAudioSession sharedInstance] recordPermission])
+    {
+        case AVAudioSessionRecordPermissionGranted:
+            return true;
+        case AVAudioSessionRecordPermissionDenied:
+            return false;
+        case AVAudioSessionRecordPermissionUndetermined:
+        {
+            __block std::promise<bool> promise;
+            std::future<bool> future = promise.get_future();
+            [[AVAudioSession sharedInstance] requestRecordPermission:
+                ^(BOOL granted)
+                {
+                    promise.set_value(granted);
+                }];
+
+            return future.get();
+        }
+        default:
+            return true; //< Let Qt handle the permission request.
+    }
 }
