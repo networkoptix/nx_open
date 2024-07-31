8e4b31c97e9e60c1b5757be5234f4505271ec1a6
Sergei Ivanov <sivanov@networkoptix.com>
2024-07-31 15:53:16 +0200

    VMS-54233: Fix unit tests assertion on absent QCoreApplication instance

--- vms/client/libvms_gateway_core/unit_tests/src/main.cpp
+++ vms/client/libvms_gateway_core/unit_tests/src/main.cpp
@@ -1,14 +1,16 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
 #include <gtest/gtest.h>
-#include <QCoreApplication>
 
+#include <QtCore/QCoreApplication>
+
+#include <nx/cloud/vms_gateway/test_support/vms_gateway_functional_test.h>
 #include <nx/network/ssl/certificate.h>
 #include <nx/network/test_support/run_test.h>
-#include <nx/cloud/vms_gateway/test_support/vms_gateway_functional_test.h>
 
 int main(int argc, char** argv)
 {
+    QCoreApplication app(argc, argv);
     return nx::network::test::runTest(
         argc, argv,
         [](const nx::ArgumentParser& /*args*/)
