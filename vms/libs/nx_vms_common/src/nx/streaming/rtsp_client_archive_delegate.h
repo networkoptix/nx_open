34757ee9bc63460b5779c0bc913a2eefa5b7c978
Roman Vasilenko <rvasilenko@networkoptix.com>
2024-08-06 18:20:54 +0000

    VMS-54381: Use reopenPosition for export video mode only
    (cherry picked from commit f3830c94ee7800b45e572c4dd8851e2d260f979c)

--- vms/libs/nx_vms_common/src/nx/streaming/rtsp_client_archive_delegate.h
+++ vms/libs/nx_vms_common/src/nx/streaming/rtsp_client_archive_delegate.h
@@ -214,6 +214,7 @@ private:
     const bool m_sleepIfEmptySocket;
     CameraDiagnostics::Result m_lastError = CameraDiagnostics::NoErrorResult();
     nx::utils::ScopedConnections m_cameraConnections;
+    PlaybackMode m_mode = PlaybackMode::default_;
 };
 
 typedef QSharedPointer<QnRtspClientArchiveDelegate> QnRtspClientArchiveDelegatePtr;
