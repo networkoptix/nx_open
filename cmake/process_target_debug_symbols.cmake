5af0fd43cf47e1a8d7b1438b69df92cba7f65aa4
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2024-08-15 03:24:41 +0300

    MOBILE-1776: Strip debug symbols before signing the iOS application
    (cherry picked from commit 6fc78881d7971740755e616c5aee6b50bfade9a0)

--- cmake/process_target_debug_symbols.cmake
+++ cmake/process_target_debug_symbols.cmake
@@ -10,8 +10,17 @@ function(nx_process_macos_target_debug_symbols target)
         message(FATAL_ERROR "strip is not found.")
     endif()
 
+    set(oneValueArgs OUTPUT_DIR)
+    cmake_parse_arguments(DSYM "" "${oneValueArgs}" "" ${ARGN})
+
+    if(DSYM_OUTPUT_DIR)
+        set(create_dsym_command COMMAND dsymutil $<TARGET_FILE:${target}> -o "${DSYM_OUTPUT_DIR}/${target}.dSYM")
+    else()
+        set(create_dsym_command COMMAND dsymutil $<TARGET_FILE:${target}> -o $<TARGET_FILE:${target}>.dSYM)
+    endif()
+
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND dsymutil $<TARGET_FILE:${target}> -o $<TARGET_FILE:${target}>.dSYM
+        ${create_dsym_command}
         COMMAND ${CMAKE_STRIP} -S $<TARGET_FILE:${target}>
         COMMENT "Stripping ${target}")
 endfunction()
