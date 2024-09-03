b87c70286799e9245a57ac74bfae84cf52110a98
Jenkins <jenkins@networkoptix.com>
2024-09-03 00:07:41 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_6.0
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_6_0.download-translation/296/

--- vms/client/nx_vms_client_desktop/translations/zh_TW/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/zh_TW/client_ui.ts
@@ -665,7 +665,7 @@
     </message>
     <message>
       <source>Import from Device</source>
-      <translation type="unfinished">Import from Device</translation>
+      <translation>從裝置匯入</translation>
     </message>
     <message>
       <source>Import video automatically</source>
