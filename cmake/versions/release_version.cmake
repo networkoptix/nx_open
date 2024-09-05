a6e3e0fd0e9d0c0965893555ef9d1d18b5eda921
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-05 17:08:04 +0200

    Fix trailing space in the file

--- cmake/versions/release_version.cmake
+++ cmake/versions/release_version.cmake
@@ -1,6 +1,6 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-# ATTENTION: When updating the major or minor digit in the VMS version, reset the Protocol 
+# ATTENTION: When updating the major or minor digit in the VMS version, reset the Protocol
 # version in the following file:
 # open/cmake/versions/vms_protocol_version.cmake
 # NOTE: Changing of the hotfix digit does not affect protocol version.
