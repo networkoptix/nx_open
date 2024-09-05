4e5fa49393e52dd5fcd279df0229a47633f18047
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-05 18:33:33 +0300

    VMS-55023: Improve wording in the protocol version raise instructions

--- cmake/versions/vms_protocol_version.cmake
+++ cmake/versions/vms_protocol_version.cmake
@@ -4,7 +4,7 @@
 # the VMS Server and the Cloud. Usually it should be increased when some API structures are changed
 # and logical or network-level compatibility is lost.
 
-# In-version Protocol number should be reset to 00 when updating the VMS version.
+# In-version Protocol number should be reset to 00 when updating the VMS major or minor version.
 # ATTENTION: When changing the Protocol version, the corresponding changes must be added to the
 # Cloud release branch. Inform the Team Lead of the Cloud team about the changes.
 # ATTENTION: You must change the comment below to make sure it will generate a merge conflict if
