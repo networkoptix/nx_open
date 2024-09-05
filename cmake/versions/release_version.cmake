81728bd5f9234aa66f945eb84808399a37ddf061
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-05 18:33:33 +0300

    VMS-55023: Improve wording in the protocol version raise instructions

--- cmake/versions/release_version.cmake
+++ cmake/versions/release_version.cmake
@@ -1,6 +1,8 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-# ATTENTION: When updating the VMS version, reset the Protocol version in the following file:
+# ATTENTION: When updating the major or minor digit in the VMS version, reset the Protocol
+# version in the following file:
 # open/cmake/versions/vms_protocol_version.cmake
+# NOTE: Changing of the hotfix digit does not affect the protocol version.
 set(releaseVersion 6.1.0)
 set(releaseVersion.full ${releaseVersion}.${buildNumber})
