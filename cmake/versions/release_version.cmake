d80e82903384979c4b38266fd00da9d4fbe0cc43
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-05 16:29:55 +0200

    Raise release version to 6.0.1

--- cmake/versions/release_version.cmake
+++ cmake/versions/release_version.cmake
@@ -1,6 +1,8 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-# ATTENTION: When updating the VMS version, reset the Protocol version in the following file:
+# ATTENTION: When updating the major or minor digit in the VMS version, reset the Protocol 
+# version in the following file:
 # open/cmake/versions/vms_protocol_version.cmake
-set(releaseVersion 6.0.0)
+# NOTE: Changing of the hotfix digit does not affect protocol version.
+set(releaseVersion 6.0.1)
 set(releaseVersion.full ${releaseVersion}.${buildNumber})
