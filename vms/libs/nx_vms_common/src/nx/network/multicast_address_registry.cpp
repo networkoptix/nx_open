b329f0f7fc552e0a88d727147bc5d674fe8c89ec
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-23 16:21:32 +0200

    VMS-55319: Fix opensource copyright notices in the newly added files

--- vms/libs/nx_vms_common/src/nx/network/multicast_address_registry.cpp
+++ vms/libs/nx_vms_common/src/nx/network/multicast_address_registry.cpp
@@ -1,3 +1,5 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
 #include "multicast_address_registry.h"
 
 #include <core/resource/camera_resource.h>
