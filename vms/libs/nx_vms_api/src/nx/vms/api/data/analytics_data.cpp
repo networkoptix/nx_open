68e48240902581bc183005cbaaacbb269013a68c
Sergei Kolesnik <skolesnik@networkoptix.com>
2024-09-24 07:34:06 +0000

    VMS-52168: Server API: Implement AnalyticsObjectTracksHandler

--- vms/libs/nx_vms_api/src/nx/vms/api/data/analytics_data.cpp
+++ vms/libs/nx_vms_api/src/nx/vms/api/data/analytics_data.cpp
@@ -24,41 +24,21 @@ QN_FUSION_ADAPT_STRUCT_FUNCTIONS(PluginResourceBindingInfo,
     (ubjson)(xml)(json)(sql_record)(csv_record), PluginResourceBindingInfo_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(PluginInfoEx,
     (ubjson)(xml)(json)(sql_record)(csv_record), PluginInfoEx_Fields)
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(BestShotV3,
-    (json), BestShotV3_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(BestShotV4,
+    (json), BestShotV4_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackVideoFrameImageInfo,
+    (json), ObjectTrackVideoFrameImageInfo_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackTitle,
     (json), ObjectTrackTitle_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(BaseObjectRegion,
     (json)(sql_record)(csv_record), BaseObjectRegion_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(AnalyticsAttribute,
     (json)(sql_record)(csv_record), AnalyticsAttribute_Fields)
-QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackV3,
-    (json), ObjectTrackV3_Fields)
+QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackV4,
+    (json), ObjectTrackV4_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackFilterFreeText,
     (json), ObjectTrackFilterFreeText_Fields)
 QN_FUSION_ADAPT_STRUCT_FUNCTIONS(ObjectTrackFilter,
     (json), ObjectTrackFilter_Fields)
-
-/*static*/ ObjectTrackV3 ObjectTrackV3::makeStub(nx::Uuid id)
-{
-    using namespace std::chrono_literals;
-
-    ObjectTrackV3 result;
-
-    result.id = id;
-
-    result.deviceId = nx::Uuid::createUuid();
-    result.objectTypeId = "nx.base.Vehicle";
-    result.firstAppearanceTimeMs = 1716840924000ms;
-    result.lastAppearanceTimeMs = 1716840924999ms;
-    result.objectRegion.boundingBoxGrid.resize(176, 0xFF);
-
-    ObjectTrackTitle title;
-    title.text = "Example Object Track title text.";
-    title.isImageAvailable = true;
-    result.title = title;
-
-    return result;
-}
 
 } // namespace nx::vms::api
