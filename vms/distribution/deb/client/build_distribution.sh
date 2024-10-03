77c300ac3a51e7aa2be00397748317077dbb9a52
Denis Klychkov <dklychkov@networkoptix.com>
2024-10-03 00:38:03 -0700

    VMS-53531, VMS-53534: Vix video playback in Qt Quick parts of the Client UI
    Certain GStreamer libraries were missing. That caused loading of the Qt Multimedia plugin to fail.

--- vms/distribution/deb/client/build_distribution.sh
+++ vms/distribution/deb/client/build_distribution.sh
@@ -217,14 +217,16 @@ copyLibs()
         distrib_copySystemLibs "$STAGE_LIB" libpng16.so.16 \
             || distrib_copySystemLibs "$STAGE_LIB" libpng.so
         distrib_copySystemLibs "$STAGE_LIB" \
+            libgstallocators-1.0.so.0 \
             libgstapp-1.0.so.0 \
-            libgstbase-1.0.so.0 \
-            libgstreamer-1.0.so.0 \
-            libgstpbutils-1.0.so.0 \
             libgstaudio-1.0.so.0 \
+            libgstbase-1.0.so.0 \
+            libgstfft-1.0.so.0 \
+            libgstgl-1.0.so.0 \
+            libgstpbutils-1.0.so.0 \
+            libgstreamer-1.0.so.0 \
             libgsttag-1.0.so.0 \
             libgstvideo-1.0.so.0 \
-            libgstfft-1.0.so.0 \
             libxkbcommon.so.0 \
             libxkbcommon-x11.so.0
 
