22e56ccd244f0f8f0d5d72a1ace69c4c9a4ad6f7
Jenkins <jenkins@networkoptix.com>
2024-10-07 18:03:52 -0700

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/no_NO/common.ts
+++ vms/libs/nx_vms_common/translations/no_NO/common.ts
@@ -1370,7 +1370,7 @@
     </message>
     <message>
       <source>File write error.</source>
-      <translation type="unfinished">File write error.</translation>
+      <translation>Filskrivefeil.</translation>
     </message>
   </context>
   <context>
