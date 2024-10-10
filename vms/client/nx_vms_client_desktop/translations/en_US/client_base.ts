2e9e2e4f2e2fae37bbea4f7e99b85ad1e4395c1b
Jenkins <jenkins@networkoptix.com>
2024-10-10 05:30:55 -0700

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -139,10 +139,6 @@
         <source>Verifying update package</source>
         <translation></translation>
     </message>
-    <message>
-        <source>Cannot connect to the System</source>
-        <translation></translation>
-    </message>
 </context>
 <context>
     <name>ContextMenu</name>
