48b74acedde47a244b2c1693bdb09c8d7828877b
Jenkins <jenkins@networkoptix.com>
2024-10-15 05:20:20 -0700

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
@@ -7332,7 +7332,7 @@ If you intended to move analytics data to another storage location, please conta
     <name>nx::vms::client::desktop::BackupStatusWidget</name>
     <message>
       <source>Backup process will ignore existing footage. Only further recording will be backed up.</source>
-      <translation>バックアップ処理は既存の録画データを無視します。適用ボタン押下後に録画されたデータのみがバックアップされます。</translation>
+      <translation>バックアップ処理で、既存の録画データを無視します。以後に録画されたデータのみがバックアップされます。</translation>
     </message>
     <message>
       <source>Skip backup for existing footage?</source>
