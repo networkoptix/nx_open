6ea594cc32879228d37e68ae96ead55199978283
Jenkins <jenkins@networkoptix.com>
2024-10-18 11:46:52 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/ko_KR/common.ts
+++ vms/libs/nx_vms_common/translations/ko_KR/common.ts
@@ -1217,7 +1217,7 @@
     <message>
       <source>Forbidden.</source>
       <comment>Generic HTTP response</comment>
-      <translation type="unfinished">Forbidden.</translation>
+      <translation>금지</translation>
     </message>
     <message>
       <source>Conflict.</source>
