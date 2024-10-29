a49b1a041684a15db91e3ad01ed7bb3d9d02bb52
Jenkins <jenkins@networkoptix.com>
2024-10-29 17:45:26 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ko_KR/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/ko_KR/client_qml.ts
@@ -505,7 +505,7 @@
     </message>
     <message>
       <source>Password</source>
-      <translation>비밀번호</translation>
+      <translation>암호</translation>
     </message>
     <message>
       <source>Ignore LDAP server certificate errors</source>
@@ -1487,7 +1487,7 @@
     </message>
     <message>
       <source>Password</source>
-      <translation>비밀번호</translation>
+      <translation>암호</translation>
     </message>
     <message>
       <source>Connect</source>
@@ -2427,7 +2427,7 @@ Alt 키를 누른 채 클릭하여 객체를 따라가세요.</translation>
     </message>
     <message>
       <source>Password</source>
-      <translation>비밀번호</translation>
+      <translation>암호</translation>
     </message>
     <message>
       <source>Confirm Password</source>
