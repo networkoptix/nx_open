bc2421031c3f00d763a85b02e320379daedbd272
Jenkins <jenkins@networkoptix.com>
2024-11-01 02:04:07 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_core/translations/pl_PL/client_core.ts
+++ vms/client/nx_vms_client_core/translations/pl_PL/client_core.ts
@@ -186,7 +186,7 @@
     <message>
       <source>Connection to %1 is not ready yet. Check the server&apos;s internet connection or try again later.</source>
       <comment>%1 is the cloud name (like Nx Cloud)</comment>
-      <translation type="unfinished">Connection to %1 is not ready yet. Check the server&apos;s internet connection or try again later.</translation>
+      <translation>Połączenie z %1 nie jest jeszcze gotowe. Sprawdź połączenie internetowe serwera lub spróbuj ponownie później.</translation>
     </message>
     <message>
       <source>Log in as a %1 user to the system in a shutdown state is forbidden. You can still connect as local user.</source>
@@ -201,7 +201,7 @@
     <message>
       <source>To log in to this System, disable &quot;Ask for a verification code on every login with your %1 account&quot; in your %2.</source>
       <comment>%1 is the cloud name (like Nx Cloud),%2 is link that leads to /account/security section of Nx Cloud</comment>
-      <translation type="unfinished">To log in to this System, disable &quot;Ask for a verification code on every login with your %1 account&quot; in your %2.</translation>
+      <translation>Aby zalogować się do tego Systemu, wyłącz opcję „Pytaj o kod weryfikacyjny przy każdym logowaniu przy użyciu konta %1 ” w swoim %2.</translation>
     </message>
     <message>
       <source>The server&apos;s certificate was rejected.</source>
--- vms/client/nx_vms_client_desktop/translations/pl_PL/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/pl_PL/client_base.ts
@@ -1113,7 +1113,7 @@
     <message numerus="yes">
       <source>Mute Items</source>
       <translation type="unfinished">
-        <numerusform>Mute Items</numerusform>
+        <numerusform>Wycisz elementy</numerusform>
         <numerusform>Mute Items</numerusform>
         <numerusform>Mute Items</numerusform>
         <numerusform>Mute Items</numerusform>
@@ -1122,7 +1122,7 @@
     <message numerus="yes">
       <source>Unmute Items</source>
       <translation type="unfinished">
-        <numerusform>Unmute Items</numerusform>
+        <numerusform>Wyłącz wyciszenie elementów</numerusform>
         <numerusform>Unmute Items</numerusform>
         <numerusform>Unmute Items</numerusform>
         <numerusform>Unmute Items</numerusform>
@@ -3283,7 +3283,7 @@ Klucz licencyjny: %2</translation>
     <message>
       <source>Logged in as %1</source>
       <comment>%1 is the cloud login name (like user@domain.com</comment>
-      <translation type="unfinished">Logged in as %1</translation>
+      <translation>Zalogowano jako %1</translation>
     </message>
   </context>
   <context>
@@ -5232,11 +5232,11 @@ Klucz licencyjny: %2</translation>
     </message>
     <message>
       <source>RESTRICTED</source>
-      <translation type="unfinished">RESTRICTED</translation>
+      <translation>OGRANICZONE</translation>
     </message>
     <message>
       <source>Authorize</source>
-      <translation type="unfinished">Authorize</translation>
+      <translation>Autoryzacja</translation>
     </message>
   </context>
   <context>
@@ -5382,11 +5382,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>If cloud storage is activated for backup, other backup storages will be deactivated and the &quot;All archive&quot; option for already configured devices will be changed to &quot;Motion, Object, Bookmarks&quot;</source>
-      <translation type="unfinished">If cloud storage is activated for backup, other backup storages will be deactivated and the &quot;All archive&quot; option for already configured devices will be changed to &quot;Motion, Object, Bookmarks&quot;</translation>
+      <translation>Jeśli w celu utworzenia kopii zapasowej aktywowano przechowywanie w chmurze, inne miejsca przechowywania kopii zapasowych zostaną wyłączone, a opcja &quot;Wszystkie archiwa&quot; dla już skonfigurowanych urządzeń zostanie zmieniona na &quot;Ruch, Obiekt, Zakładki&quot;</translation>
     </message>
     <message>
       <source>If cloud storage is activated for backup, the &quot;All archive&quot; option for already configured devices will be changed to &quot;Motion, Object, Bookmarks&quot;</source>
-      <translation type="unfinished">If cloud storage is activated for backup, the &quot;All archive&quot; option for already configured devices will be changed to &quot;Motion, Object, Bookmarks&quot;</translation>
+      <translation>Jeśli w celu utworzenia kopii zapasowej aktywowano przechowywanie w chmurze, opcja &quot;Wszystkie archiwa&quot; dla już skonfigurowanych urządzeń zostanie zmieniona na &quot;Ruch, Obiekt, Zakładki&quot;</translation>
     </message>
   </context>
   <context>
@@ -5485,7 +5485,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Path</source>
-      <translation type="unfinished">Path</translation>
+      <translation>Ścieżka</translation>
     </message>
     <message>
       <source>Type</source>
@@ -5493,7 +5493,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Purpose</source>
-      <translation type="unfinished">Purpose</translation>
+      <translation>Cel</translation>
     </message>
     <message>
       <source>Size</source>
@@ -5610,7 +5610,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Failed to save system settings</source>
-      <translation type="unfinished">Failed to save system settings</translation>
+      <translation>Nie udało się zapisać ustawień systemowych</translation>
     </message>
   </context>
   <context>
@@ -5775,11 +5775,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Intercom call</source>
-      <translation type="unfinished">Intercom call</translation>
+      <translation>Połączenie interkomowe</translation>
     </message>
     <message>
       <source>Intercom missed call</source>
-      <translation type="unfinished">Intercom missed call</translation>
+      <translation>Nieodebrane połączenie przez interkom</translation>
     </message>
   </context>
   <context>
@@ -6183,8 +6183,8 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>Failed to open %n devices on the video wall:</source>
       <translation type="unfinished">
-        <numerusform>Failed to open %n devices on the video wall:</numerusform>
-        <numerusform>Failed to open %n devices on the video wall:</numerusform>
+        <numerusform>Nie udało się otworzyć %n urządzeń na ścianie wideo:</numerusform>
+        <numerusform>Nie udało się otworzyć %n urządzeń na ścianie wideo:</numerusform>
         <numerusform>Failed to open %n devices on the video wall:</numerusform>
         <numerusform>Failed to open %n devices on the video wall:</numerusform>
       </translation>
@@ -6192,7 +6192,7 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>Failed to open %n cameras on the video wall:</source>
       <translation type="unfinished">
-        <numerusform>Failed to open %n cameras on the video wall:</numerusform>
+        <numerusform>Nie udało się otworzyć %n kamer na ścianie wideo:</numerusform>
         <numerusform>Failed to open %n cameras on the video wall:</numerusform>
         <numerusform>Failed to open %n cameras on the video wall:</numerusform>
         <numerusform>Failed to open %n cameras on the video wall:</numerusform>
@@ -6201,7 +6201,7 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>You cannot add to the video wall devices for which you do not have View Live permission.</source>
       <translation type="unfinished">
-        <numerusform>You cannot add to the video wall devices for which you do not have View Live permission.</numerusform>
+        <numerusform>Nie możesz dodać urządzeń do ściany wideo, dla których nie masz uprawnień widoku na żywo.</numerusform>
         <numerusform>You cannot add to the video wall devices for which you do not have View Live permission.</numerusform>
         <numerusform>You cannot add to the video wall devices for which you do not have View Live permission.</numerusform>
         <numerusform>You cannot add to the video wall devices for which you do not have View Live permission.</numerusform>
@@ -6210,7 +6210,7 @@ If you intended to move analytics data to another storage location, please conta
     <message numerus="yes">
       <source>You cannot add to the video wall cameras for which you do not have View Live permission.</source>
       <translation type="unfinished">
-        <numerusform>You cannot add to the video wall cameras for which you do not have View Live permission.</numerusform>
+        <numerusform>Nie możesz dodawać do ściany wideo kamer, do których nie masz uprawnień widoku na żywo.</numerusform>
         <numerusform>You cannot add to the video wall cameras for which you do not have View Live permission.</numerusform>
         <numerusform>You cannot add to the video wall cameras for which you do not have View Live permission.</numerusform>
         <numerusform>You cannot add to the video wall cameras for which you do not have View Live permission.</numerusform>
@@ -6222,11 +6222,11 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>To add a Video Wall, the System should be in active state. %1</source>
-      <translation type="unfinished">To add a Video Wall, the System should be in active state. %1</translation>
+      <translation>Aby dodać ścianę wideo, system powinien być w stanie aktywnym. %1</translation>
     </message>
     <message>
       <source>Screen is locked and cannot be changed</source>
-      <translation type="unfinished">Screen is locked and cannot be changed</translation>
+      <translation>Ekran jest zablokowany i nie można go zmienić</translation>
     </message>
   </context>
   <context>
@@ -6704,12 +6704,12 @@ If you intended to move analytics data to another storage location, please conta
     <message>
       <source>All members of %1 and %2 groups will get access to this layout</source>
       <comment>%1 and %2 will be substituted with user group names</comment>
-      <translation type="unfinished">All members of %1 and %2 groups will get access to this layout</translation>
+      <translation>Dostęp do tego układu otrzymają wszyscy członkowie grup %1 i %2</translation>
     </message>
     <message>
       <source>Convert</source>
       <comment>Converting layout to shared</comment>
-      <translation type="unfinished">Convert</translation>
+      <translation>Konwertuj</translation>
     </message>
   </context>
   <context>
@@ -6933,7 +6933,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Change search criteria or create a new group</source>
-      <translation type="unfinished">Change search criteria or create a new group</translation>
+      <translation>Zmień kryteria wyszukiwania lub utwórz nową grupę</translation>
     </message>
     <message>
       <source>Multiple groups share the same name, which can lead to confusion. To maintain a clear and organized structure, we suggest providing unique names for each group.</source>
