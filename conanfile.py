1da7d6e4d40e762b5c560248eeb7f0a20c5c8ee7
Mark Altera <maltera@networkoptix.com>
2024-11-15 17:13:43 +0100

    VMS-56005: Fix macOS detecting touch devices

--- conanfile.py
+++ conanfile.py
@@ -184,7 +184,7 @@ class NxOpenConan(ConanFile):
 
         self.requires(f"ffmpeg/{self.ffmpeg_version_and_revision}")
         self.requires("openssl/1.1.1q" "#bcc8c366b4291c68913eabe817fb15c7")
-        self.requires("qt/6.8.0" "#50349278f7fab33d5c83812698ae734c")
+        self.requires("qt/6.8.0" "#8e36855d51a96537f4423f657da99c96")
         self.requires("roboto-fonts/1.0" "#a1d64ec2d6a2e16f8f476b2b47162123")
         self.requires("boost/1.83.0" "#e0be85c6f8107d7e960246e31cbbf7ab")
         self.requires("concurrentqueue/1.0.4" "#957c470e9abc81ff3850bbe39fc11135")
