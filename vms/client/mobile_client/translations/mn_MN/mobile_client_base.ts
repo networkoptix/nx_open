c927f1bb5820d9231dd0a424bc3a41976dbc5bec
Jenkins <jenkins@networkoptix.com>
2024-11-26 05:54:29 -0800

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/mn_MN/mobile_client_base.ts
+++ vms/client/mobile_client/translations/mn_MN/mobile_client_base.ts
@@ -1,4 +1,102 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="mn_MN" sourcelanguage="en">
+  <context>
+    <name>ParametersVisualizationModel::Private</name>
+    <message numerus="yes">
+      <source>%n Object Types</source>
+      <comment>%n is number of types</comment>
+      <translation type="unfinished">
+        <numerusform>%n Object Types</numerusform>
+        <numerusform>%n Object Types</numerusform>
+      </translation>
+    </message>
+    <message numerus="yes">
+      <source>%n Object Attributes</source>
+      <comment>%n is number of attributes</comment>
+      <translation type="unfinished">
+        <numerusform>%n Object Attributes</numerusform>
+        <numerusform>%n Object Attributes</numerusform>
+      </translation>
+    </message>
+  </context>
+  <context>
+    <name>PtzCameraButtonController</name>
+    <message>
+      <source>Control PTZ</source>
+      <translation type="unfinished">Control PTZ</translation>
+    </message>
+  </context>
+  <context>
+    <name>QnCloudSystemInformationWatcher</name>
+    <message>
+      <source>Owner: %1</source>
+      <comment>%1 is a user name</comment>
+      <translation>Эзэмшигч: %1</translation>
+    </message>
+    <message>
+      <source>Your Site</source>
+      <translation type="unfinished">Your Site</translation>
+    </message>
+  </context>
+  <context>
+    <name>QnContext</name>
+    <message>
+      <source>Cannot connect to the Server</source>
+      <translation>Серверт холбогдох боломжгүй байна</translation>
+    </message>
+    <message>
+      <source>Cannot connect to the Site &quot;%1&quot;</source>
+      <comment>%1 is a site name</comment>
+      <translation type="unfinished">Cannot connect to the Site &quot;%1&quot;</translation>
+    </message>
+    <message>
+      <source>Unknown</source>
+      <translation>Тодорхойгүй</translation>
+    </message>
+  </context>
+  <context>
+    <name>QnLayoutsModelUnsorted</name>
+    <message>
+      <source>All Cameras</source>
+      <translation>Бүх камерууд</translation>
+    </message>
+  </context>
+  <context>
+    <name>QnMobileClientModule</name>
+    <message>
+      <source>Your session has expired</source>
+      <translation>Таны нэвтрэх хугацаа дууссан байна</translation>
+    </message>
+    <message>
+      <source>Session duration limit can be changed by the site administrators</source>
+      <translation type="unfinished">Session duration limit can be changed by the site administrators</translation>
+    </message>
+  </context>
+  <context>
+    <name>nx::vms::client::mobile::MediaDownloadBackend</name>
+    <message>
+      <source>Can&apos;t download video</source>
+      <translation type="unfinished">Can&apos;t download video</translation>
+    </message>
+    <message>
+      <source>Please check a network connection.</source>
+      <translation type="unfinished">Please check a network connection.</translation>
+    </message>
+  </context>
+  <context>
+    <name>nx::vms::client::mobile::PushNotificationManager</name>
+    <message>
+      <source>Cannot change push notifications settings</source>
+      <translation>Мэдэгдлийн тохиргоог өөрчлөх боломжгүй</translation>
+    </message>
+    <message>
+      <source>Cannot enable push notifications</source>
+      <translation>Мэдэгдлийг идэвхжүүлэх боломжгүй</translation>
+    </message>
+    <message>
+      <source>Please check your internet connection</source>
+      <translation type="unfinished">Please check your internet connection</translation>
+    </message>
+  </context>
 </TS>
