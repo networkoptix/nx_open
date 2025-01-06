e82a188d976ff7a8bd3677ca87fa9091df5bf067
Jenkins <jenkins@networkoptix.com>
2025-01-06 01:18:56 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/fr_FR/common.ts
+++ vms/libs/nx_vms_common/translations/fr_FR/common.ts
@@ -487,7 +487,7 @@
     </message>
     <message>
       <source>Start</source>
-      <translation>Démarrer</translation>
+      <translation>Démarrage</translation>
     </message>
     <message>
       <source>Analog Encoder</source>
@@ -523,7 +523,7 @@
     </message>
     <message>
       <source>Start Licenses</source>
-      <translation>Démarrer Licences</translation>
+      <translation>Licences de démarrage</translation>
     </message>
     <message>
       <source>Invalid</source>
