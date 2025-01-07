0c8facb4e2fafc8e2e2d030ba6de501ed2f3b28a
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2025-01-07 08:21:25 +0300

    VMS-56905: Update Qt version to e01173df8db41583ad916d3b7ea9b0f6

--- conanfile.py
+++ conanfile.py
@@ -186,7 +186,7 @@ class NxOpenConan(ConanFile):
         self.requires("libsrtp/2.6.0" "#248ee72d7d91db948f5651b7fe4905ea")
         self.requires(f"ffmpeg/{self.ffmpeg_version_and_revision}")
         self.requires("openssl/1.1.1q" "#bcc8c366b4291c68913eabe817fb15c7")
-        self.requires("qt/6.8.1" "#139adc221374b629f774118e234ed81e")
+        self.requires("qt/6.8.1" "#e01173df8db41583ad916d3b7ea9b0f6")
         self.requires("roboto-fonts/1.0" "#1bff09c31c4d334f27795653e0f4b2bb")
         self.requires("boost/1.83.0" "#d150c9edc8081c98965b05ea9c2df318")
         self.requires("concurrentqueue/1.0.4" "#957c470e9abc81ff3850bbe39fc11135")
