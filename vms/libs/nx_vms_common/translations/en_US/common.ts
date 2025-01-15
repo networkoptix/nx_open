e8647c7cec8e4ce366bb0bc04d56d1d973b31fba
Jenkins <jenkins@networkoptix.com>
2025-01-15 01:15:33 +0000

    Update translatable strings (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/en_US/common.ts
+++ vms/libs/nx_vms_common/translations/en_US/common.ts
@@ -81,6 +81,10 @@
         <source>Wrong password</source>
         <translation></translation>
     </message>
+    <message>
+        <source>No credentials provided when required by endpoint.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>BookmarkSort</name>
