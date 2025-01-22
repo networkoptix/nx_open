7ae59b0b9bf8a6b94f44fca4c8e4576cb7346edd
Jenkins <jenkins@networkoptix.com>
2025-01-22 01:15:43 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_qml.ts
@@ -1042,10 +1042,14 @@
     </message>
 </context>
 <context>
-    <name>InsecureWarning</name>
+    <name>InsecureBanner</name>
     <message>
-        <source>Account security is important. Do not enable this setting unless you have good reasons to do so.</source>
-        <translation></translation>
+        <source>Digest authentication is deprecated and will be disabled in the next release, and should only be used when default Bearer Authentication cannot be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Digest authentication is deprecated and will be disabled in the next release, and should only be used when default Bearer Authentication cannot be used. User logins must consist only of lowercase letters.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
