7e7ffe4df4e79ea653a0987bc274449af7cdc5af
Roman Vasilenko <rvasilenko@networkoptix.com>
2025-02-06 17:44:14 +0100

    VMS-55448: Limit HW decoders count by default to 16

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/settings/local_settings.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/settings/local_settings.h
@@ -55,7 +55,7 @@ public:
         "Whether hardware video decoding is used, if available."};
     Property<int> maxIntelHardwareDecoders{this, "maxIntelHardwareDecoders", 8,
         "Maximum number of hardware decoders for Intel QS."};
-    Property<int> maxNVidiaHardwareDecoders{ this, "maxNVidiaHardwareDecoders", 64,
+    Property<int> maxNVidiaHardwareDecoders{ this, "maxNVidiaHardwareDecoders", 16,
         "Maximum number of hardware decoders for NVidia." };
 
     Property<QString> locale{this, "locale", nx::branding::defaultLocale()};
