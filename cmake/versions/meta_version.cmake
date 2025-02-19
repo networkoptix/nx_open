39512e1e12e529e392d759b715363b09f271df0d
Tamás Szelei <tszelei@networkoptix.com>
2025-02-19 12:15:38 +0000

    VMS-57388: Reset MetaVMS version

--- cmake/versions/meta_version.cmake
+++ cmake/versions/meta_version.cmake
@@ -1,6 +1,7 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-set(metaVersion R7)
+# Version is reset on each minor release.
+set(metaVersion R1)
 if(customization.advanced.useMetaVersion)
     # Set meta-version to be shown in MetaVMS builds - in the Client, etc.
     set(usedMetaVersion ${metaVersion})
