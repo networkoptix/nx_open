7e5d13ffd5009fd695b85f093650b945258bf066
Tamás Szelei <tszelei@networkoptix.com>
2025-02-26 18:36:16 +0100

    VMS-57517: Set 6.1.0 MetaVMS version to R3

--- cmake/versions/meta_version.cmake
+++ cmake/versions/meta_version.cmake
@@ -1,6 +1,6 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-set(metaVersion R2)
+set(metaVersion R3)
 if(customization.advanced.useMetaVersion)
     # Set meta-version to be shown in MetaVMS builds - in the Client, etc.
     set(usedMetaVersion ${metaVersion})
