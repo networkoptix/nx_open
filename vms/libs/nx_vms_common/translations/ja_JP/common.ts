43f7d542ee9df113fe642845222e47cdc18d6c59
Jenkins <jenkins@networkoptix.com>
2025-03-24 01:19:27 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/ja_JP/common.ts
+++ vms/libs/nx_vms_common/translations/ja_JP/common.ts
@@ -1751,7 +1751,7 @@
     </message>
     <message>
       <source>SMTP settings. These settings are visible for Power Users only</source>
-      <translation type="unfinished">SMTP settings. These settings are visible for Power Users only</translation>
+      <translation>SMTP設定。これらの設定はパワーユーザーにのみ表示されます。</translation>
     </message>
   </context>
   <context>
