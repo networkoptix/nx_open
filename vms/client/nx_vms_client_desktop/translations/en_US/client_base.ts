34725f458d00610fb0667e8c7cef42e95657dff1
Jenkins <jenkins@networkoptix.com>
2025-05-08 01:15:02 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -11664,6 +11664,10 @@ Do not pin this certificate if you did not modify %2 Server SSL certificate.</so
         <source>This will instantly remove an access to the site for this user</source>
         <translation></translation>
     </message>
+    <message>
+        <source>The user with the specified email already has Administrator access via the Channel Partner</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>nx::vms::client::desktop::VersionSelectionDialog</name>
