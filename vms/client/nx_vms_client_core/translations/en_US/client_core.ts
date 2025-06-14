0bc9f833ec0c0350573bde66cf9127ccfb40b0ab
Jenkins <jenkins@networkoptix.com>
2025-06-14 01:15:14 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_core/translations/en_US/client_core.ts
+++ vms/client/nx_vms_client_core/translations/en_US/client_core.ts
@@ -16,10 +16,6 @@
         <translation></translation>
     </message>
     <message>
-        <source>Your Site</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Owner: %1</source>
         <comment>%1 is a user/company name</comment>
         <translation></translation>
