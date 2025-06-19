ad93b3a5bd114cd92a9459162b50059ddc57af16
Jenkins <jenkins@networkoptix.com>
2025-06-19 23:59:41 +0300

    Updated help package version (committed by Jenkins)

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ required_conan_version = ">=1.53.0"
 # Help packages are not required to be built from the same commit.
 VMS_HELP_VERSION = "6.1.0-77eebbb9ab9ebbe0d84ff125f6975460b35db147"
 QUICK_START_GUIDE_VERSION = "6.0.0-bfdf1ce0a6f533e97993e3b2e088696549d94f62"
-MOBILE_USER_MANUAL_VERSION = "21.2-bfdf1ce0a6f533e97993e3b2e088696549d94f62"
+MOBILE_USER_MANUAL_VERSION = "25.1-77eebbb9ab9ebbe0d84ff125f6975460b35db147"
 
 
 # Conan does not provide a generator which makes it possible to easily access package folders for
