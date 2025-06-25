f7d6cc880bd73ce51f722e1e3a494c0935f8a7f7
Mark Hermeling <mark@hermeling.org>
2025-06-25 11:36:19 -0400

    Update inclusions
    Found as I am compiling without precompiled headers

--- libs/nx_utils/unit_tests/src/reflect/type_utils_ut.cpp
+++ libs/nx_utils/unit_tests/src/reflect/type_utils_ut.cpp
@@ -1,6 +1,8 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
 #include <nx/reflect/type_utils.h>
+#include <QString>
+#include <QByteArray>
 
 namespace nx::reflect::test {
 
