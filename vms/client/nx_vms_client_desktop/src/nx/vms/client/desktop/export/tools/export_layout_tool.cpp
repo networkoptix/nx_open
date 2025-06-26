1877b15c99c250cf95398c15b8bb8edc949d8ce2
Dmitry Fisenko <dfisenko@networkoptix.com>
2025-06-26 17:07:51 +0200

    VMS-59315: Fix export layout crash

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/export/tools/export_layout_tool.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/export/tools/export_layout_tool.cpp
@@ -551,7 +551,7 @@ bool ExportLayoutTool::exportMediaResource(const QnMediaResourcePtr& resource)
 }
 
 void ExportLayoutTool::at_camera_exportFinished(const std::optional<nx::recording::Error>& status,
-    const QString& /*filename*/)
+    const QString& filename)
 {
     if (d->status == ExportProcessStatus::cancelled)
         return;
@@ -559,7 +559,7 @@ void ExportLayoutTool::at_camera_exportFinished(const std::optional<nx::recordin
     d->lastError = convertError(status);
     if (d->lastError == ExportProcessError::dataNotFound)
     {
-        NX_VERBOSE(this, "Data not found: %1", d->resources.head()->getName());
+        NX_VERBOSE(this, "Data not found: %1", filename);
 
         d->lastError = ExportProcessError::noError;
         exportNextCamera();
@@ -570,6 +570,8 @@ void ExportLayoutTool::at_camera_exportFinished(const std::optional<nx::recordin
 
     if (d->lastError != ExportProcessError::noError)
     {
+        NX_VERBOSE(this, "An error ocurred: %1", filename);
+
         finishExport(false);
         return;
     }
