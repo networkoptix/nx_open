200c1da8f1e225dd1acf5ee48cdca7d9f66adec4
Alexander Kulikov <akulikov@networkoptix.com>
2025-07-04 10:26:21 +0000

    VMS-59362: Optimize ResourceProperyDictionary shared access
    (cherry picked from commit afd51cc99acf1371217764c7626f05d501ea2f8c)

--- libs/nx_utils/src/nx/utils/range_adapters.h
+++ libs/nx_utils/src/nx/utils/range_adapters.h
@@ -36,10 +36,51 @@ auto rangeAdapter(const PairType& pair)
     return rangeAdapter(pair.first, pair.second);
 }
 
-template<typename KeyValueContainer>
-auto keyValueRange(KeyValueContainer& container)
+#if __cpp_concepts >= 201907L
+    template<typename T>
+    concept HasKeyValueBeginEnd = requires(T&& t)
+    {
+        t.keyValueBegin();
+        t.keyValueEnd();
+    };
+
+    template<typename T>
+    concept HasBeginEnd = requires(T&& t)
+    {
+        t.begin();
+        t.end();
+    };
+#else
+    template<typename, typename = void>
+    constexpr bool HasKeyValueBeginEnd = false;
+
+    template<typename T>
+    constexpr bool HasKeyValueBeginEnd<T,
+        std::void_t<
+            decltype(std::declval<T>().keyValueBegin()),
+            decltype(std::declval<T>().keyValueEnd())>> = true;
+
+    template<typename, typename = void>
+    constexpr bool HasBeginEnd = false;
+
+    template<typename T>
+    constexpr bool HasBeginEnd<T,
+        std::void_t<
+            decltype(std::declval<T>().begin()),
+            decltype(std::declval<T>().end())>> = true;
+#endif
+
+template<typename Container>
+auto keyValueRange(Container& container)
 {
-    return rangeAdapter(container.keyValueBegin(), container.keyValueEnd());
+    constexpr bool hasKVbe = HasKeyValueBeginEnd<Container>;
+    constexpr bool hasBe = HasBeginEnd<Container>;
+    if constexpr(hasKVbe)
+        return rangeAdapter(container.keyValueBegin(), container.keyValueEnd());
+    else if constexpr(hasBe)
+        return rangeAdapter(container.begin(), container.end());
+    else
+        static_assert(hasKVbe || hasBe, "Container not supported");
 }
 
 // Avoid constructing from temporaries.
