d1cc701db8531aa51c69420ce8f3a03485a35788
Jenkins <jenkins@networkoptix.com>
2025-07-10 01:14:44 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
@@ -792,10 +792,6 @@
         <translation></translation>
     </message>
     <message>
-        <source>We didn&apos;t find any organizations, try contacting support</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Log In</source>
         <translation></translation>
     </message>
@@ -825,6 +821,10 @@
     </message>
     <message>
         <source>No accessible sites were found. Log in into the cloud account or connect to a local server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create an organization in the Cloud Portal to access it here</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
