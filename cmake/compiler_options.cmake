5e1de059695c4f4bfa0654ccaca4980072771990
Pavel Tolstov <ptolstov@networkoptix.com>
2025-07-11 20:26:53 +0600

    VMS-59473: Fix using std::format on macOs

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -514,6 +514,7 @@ if(MACOSX)
         add_compile_options(-msse4.1)
     endif()
     add_compile_options(-Wno-unused-local-typedef)
+    add_compile_options(-mmacosx-version-min=13.4)
     list(APPEND CMAKE_INSTALL_RPATH @executable_path/../lib)
 endif()
 
