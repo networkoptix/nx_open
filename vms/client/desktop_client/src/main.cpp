f052f373d27ee5100a4a337c3f7e91a3036b1050
Vitaly Kutin <vkutin@networkoptix.com>
2025-07-17 21:12:53 +0000

    VMS-59407, VMS-59439, VMS-57809: Disable multithreaded rendering with Direct3D and with Metal
    Multithreaded rendering with these backends and enabled VSync causes prolonged locks of the GUI tread at the synchronization stage (for 1/60 ms, seems `QQuickWindow`s wait for the main window to swap frames or vice versa). It's either a Qt bug or some interaction between our low-level code and RHI, but I haven't found the reason, and at this point we should just disable multithreaded rendering with those backends.
    
    Also removed an old workaround which is no longer needed (with "We do not rely on Mac OS OpenGL implementation-related throttling" comment).
    
    (cherry picked from commit 13d67e35f495942633ab759e4c3580a6e6402e48)

--- vms/client/desktop_client/src/main.cpp
+++ vms/client/desktop_client/src/main.cpp
@@ -6,13 +6,5 @@
 
 int main(int argc, char** argv)
 {
-    if (nx::build_info::isMacOsX()
-        && QString(nx::vms::client::desktop::ini().graphicsApi) == "opengl")
-    {
-        // We do not rely on Mac OS OpenGL implementation-related throttling.
-        // Otherwise all animations go faster.
-        qputenv("QSG_RENDER_LOOP", "basic");
-    }
-
     return nx::vms::client::desktop::runApplication(argc, argv);
 }
