f7c3ef71d9a627edd00ee16b64f0ea859a2a8f9b
Anton Malov <amalov@networkoptix.com>
2025-07-25 11:24:59 +0000

    VMS-57284: Terminate pending requests on connection removal
    (cherry picked from commit df208070401e9974e0201d95783ddd9a866c1ad5)

--- vms/libs/nx_vms_common/src/api/server_rest_connection_fwd.h
+++ vms/libs/nx_vms_common/src/api/server_rest_connection_fwd.h
@@ -5,23 +5,17 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <vector>
 
 #include <nx/utils/move_only_func.h>
 #include <nx/vms/api/data/json_rpc.h>
-#include <nx/vms/event/event_fwd.h>
 
-namespace nx::network::http { class AsyncHttpClientPtr; }
 namespace nx::network::rest { struct JsonResult; }
 namespace nx::network::rest { class UbjsonResult; }
 
 namespace rest {
 
-#if defined(__arm__)
-    // Some devices lack kernel support for atomic int64.
-    typedef qint32 Handle;
-#else
-    typedef qint64 Handle;
-#endif
+using Handle = int; // TODO: Make dependend on nx::network::http::ClientPool handle type.
 
 template<typename ResultType>
 using Callback = nx::MoveOnlyFunc<void(bool success, Handle requestId, ResultType result)>;
