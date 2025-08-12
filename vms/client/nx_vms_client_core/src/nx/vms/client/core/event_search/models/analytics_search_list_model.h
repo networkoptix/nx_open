07d27d91d31bcf5ca0ea1dce7df9213a07c1fbb1
Yuriy Nikitenkov <ynikitenkov@networkoptix.com>
2025-08-11 21:13:48 -0700

    MOBILE-2512: Show only filter object type in filters panel

--- vms/client/nx_vms_client_core/src/nx/vms/client/core/event_search/models/analytics_search_list_model.h
+++ vms/client/nx_vms_client_core/src/nx/vms/client/core/event_search/models/analytics_search_list_model.h
@@ -44,6 +44,7 @@ public:
     nx::Uuid selectedEngine() const;
     void setSelectedEngine(const nx::Uuid& value);
 
+    std::set<QString> filteredObjectTypes() const;
     QStringList selectedObjectTypes() const;
     void setSelectedObjectTypes(const QStringList& value);
 
