dda960bb7ef2edef93fd1f03137174e33281f1e4
Jenkins <jenkins@networkoptix.com>
2025-08-27 01:19:10 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_core/translations/fr_FR/client_core.ts
+++ vms/client/nx_vms_client_core/translations/fr_FR/client_core.ts
@@ -177,11 +177,11 @@
     </message>
     <message>
       <source>Failed to access site</source>
-      <translation type="unfinished">Failed to access site</translation>
+      <translation>Impossible d'accéder au site</translation>
     </message>
     <message>
       <source>Failed to access site.</source>
-      <translation type="unfinished">Failed to access site.</translation>
+      <translation>Impossible d'accéder au site.</translation>
     </message>
   </context>
   <context>
