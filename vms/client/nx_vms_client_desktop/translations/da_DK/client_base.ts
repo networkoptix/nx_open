a2e2c1e41a73396d5877af214cabf526b3dfd795
Jenkins <jenkins@networkoptix.com>
2025-08-30 01:18:53 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/da_DK/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/da_DK/client_base.ts
@@ -4517,7 +4517,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Disabling cloud storage on one server will disable it on all servers. Local backup must be configured manually.</source>
-      <translation type="unfinished">Disabling cloud storage on one server will disable it on all servers. Local backup must be configured manually.</translation>
+      <translation>Deaktivering af cloudlagring på én server vil deaktivere det på alle servere. Lokal backup skal konfigureres manuelt.</translation>
     </message>
   </context>
   <context>
