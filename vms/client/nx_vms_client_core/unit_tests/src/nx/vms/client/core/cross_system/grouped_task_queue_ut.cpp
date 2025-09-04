1311ebd82cb516e28ddf96cb6654bb7b1543a04d
Mark Altera <maltera@networkoptix.com>
2025-09-04 11:05:47 +0000

    VMS-59806: Simplify GroupedTaskQueue test
    (cherry picked from commit 4e634e6ed6b46afd7e4654f23a51392aa3012ced)

--- vms/client/nx_vms_client_core/unit_tests/src/nx/vms/client/core/cross_system/grouped_task_queue_ut.cpp
+++ vms/client/nx_vms_client_core/unit_tests/src/nx/vms/client/core/cross_system/grouped_task_queue_ut.cpp
@@ -14,89 +14,74 @@
 namespace nx::vms::client::core::test {
 using namespace testing;
 
-class GroupRequestInvokeSequenceTest
+class GroupedTaskSequenceTest
 {
 public:
-    MOCK_METHOD(void, requestInvoked, (const QString& /*group*/, int /*id*/));
-    MOCK_METHOD(void, requestFinished, (const QString& /*group*/, int /*id*/));
+    MOCK_METHOD(void, taskStarted, (const QString& /*group*/, int /*id*/));
+    MOCK_METHOD(void, taskFinished, (const QString& /*group*/, int /*id*/));
 };
 
-class GroupQueueTest: public Test
+class GroupedTaskQueueTest: public Test
 {
 public:
-    static constexpr auto kTimeout = std::chrono::seconds(7);
-
-    virtual void SetUp() override;
-    void requestFunction(GroupedTaskQueue::PromisePtr promise, const QString& group, int id);
-    void addRequest(const QString& group, int id);
+    void taskFunction(GroupedTaskQueue::PromisePtr promise, const QString& group, int id);
+    void addTask(const QString& group, int id);
 
     GroupedTaskQueue m_queue;
-    GroupRequestInvokeSequenceTest m_test;
+    GroupedTaskSequenceTest m_test;
     QEventLoop m_eventLoop;
 };
 
-void GroupQueueTest::SetUp()
+void GroupedTaskQueueTest::taskFunction(
+    GroupedTaskQueue::PromisePtr promise,
+    const QString& group,
+    int id)
 {
-    QTimer::singleShot(kTimeout, &m_eventLoop,
-        [this]()
-        {
-            m_eventLoop.quit();
-            ASSERT_TRUE(false) << "Timeout";
-        });
+    m_test.taskStarted(group, id);
+    promise->finish();
+    m_test.taskFinished(group, id);
 }
 
-void GroupQueueTest::requestFunction(GroupedTaskQueue::PromisePtr promise, const QString& group, int id)
-{
-    m_test.requestInvoked(group, id);
-
-    std::thread(
-        [this, promise, group, id]()
-        {
-            m_test.requestFinished(group, id);
-            promise->finish();
-        }).detach();
-}
-
-void GroupQueueTest::addRequest(const QString& group, int id)
+void GroupedTaskQueueTest::addTask(const QString& group, int id)
 {
     m_queue.add(std::make_shared<GroupedTaskQueue::Task>(
-        [=, this](GroupedTaskQueue::PromisePtr promise) { requestFunction(promise, group, id); }),
+        [=, this](GroupedTaskQueue::PromisePtr promise) { taskFunction(promise, group, id); }),
         group);
 }
 
-TEST_F(GroupQueueTest, priorities)
+TEST_F(GroupedTaskQueueTest, priorities)
 {
     {
         InSequence sequence;
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group1"), /*id*/ 1));
-        EXPECT_CALL(m_test, requestFinished(QString("group1"), /*id*/ 1));
+        EXPECT_CALL(m_test, taskStarted(QString("group1"), /*id*/ 1));
+        EXPECT_CALL(m_test, taskFinished(QString("group1"), /*id*/ 1));
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group1"), /*id*/ 2));
-        EXPECT_CALL(m_test, requestFinished(QString("group1"), /*id*/ 2));
+        EXPECT_CALL(m_test, taskStarted(QString("group1"), /*id*/ 2));
+        EXPECT_CALL(m_test, taskFinished(QString("group1"), /*id*/ 2));
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group2"), /*id*/ 3));
-        EXPECT_CALL(m_test, requestFinished(QString("group2"), /*id*/ 3));
+        EXPECT_CALL(m_test, taskStarted(QString("group2"), /*id*/ 3));
+        EXPECT_CALL(m_test, taskFinished(QString("group2"), /*id*/ 3));
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group2"), /*id*/ 4));
-        EXPECT_CALL(m_test, requestFinished(QString("group2"), /*id*/ 4));
+        EXPECT_CALL(m_test, taskStarted(QString("group2"), /*id*/ 4));
+        EXPECT_CALL(m_test, taskFinished(QString("group2"), /*id*/ 4));
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group3"), /*id*/ 5));
-        EXPECT_CALL(m_test, requestFinished(QString("group3"), /*id*/ 5));
+        EXPECT_CALL(m_test, taskStarted(QString("group3"), /*id*/ 5));
+        EXPECT_CALL(m_test, taskFinished(QString("group3"), /*id*/ 5));
 
-        EXPECT_CALL(m_test, requestInvoked(QString("group3"), /*id*/ 6));
-        EXPECT_CALL(m_test, requestFinished(QString("group3"), /*id*/ 6))
+        EXPECT_CALL(m_test, taskStarted(QString("group3"), /*id*/ 6));
+        EXPECT_CALL(m_test, taskFinished(QString("group3"), /*id*/ 6))
             .WillOnce([this]() { m_eventLoop.quit(); });
     }
 
     m_queue.setPriority("group1", 3);
-    addRequest("group3", /*id*/ 5);
-    addRequest("group1", /*id*/ 1);
-    addRequest("group1", /*id*/ 2);
-    addRequest("group3", /*id*/ 6);
+    addTask("group3", /*id*/ 5);
+    addTask("group1", /*id*/ 1);
+    addTask("group1", /*id*/ 2);
+    addTask("group3", /*id*/ 6);
     m_queue.setPriority("group2", 2);
-    addRequest("group2", /*id*/ 3);
-    addRequest("group2", /*id*/ 4);
+    addTask("group2", /*id*/ 3);
+    addTask("group2", /*id*/ 4);
     m_queue.setPriority("group3", 1);
 
     m_eventLoop.exec();
