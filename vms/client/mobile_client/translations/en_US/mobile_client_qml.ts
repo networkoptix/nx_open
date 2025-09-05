918af7b7e534fd62508f55ec56b0d75ba39f70a2
Jenkins <jenkins@networkoptix.com>
2025-09-05 01:14:48 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
@@ -1099,25 +1099,6 @@
     </message>
 </context>
 <context>
-    <name>TextField</name>
-    <message>
-        <source>Cut</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Copy</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Paste</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Select All</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
     <name>TimeSelector</name>
     <message>
         <source>Period</source>
