f8675bb4632c377f1e0f453dc5e10404401301f3
Roman Vasilenko <rvasilenko@networkoptix.com>
2025-09-11 01:13:28 +0200

    VMS-59775: Correct process response code 'created' in new CP API

--- cloud/cloud_db_client/src/include/nx/cloud/db/api/result_code.h
+++ cloud/cloud_db_client/src/include/nx/cloud/db/api/result_code.h
@@ -70,6 +70,7 @@ NX_REFLECTION_ENUM_CLASS(ResultCode,
 );
 
 std::string toString(ResultCode resultCode);
+bool isSuccessCode(ResultCode code);
 
 struct Result
 {
@@ -86,6 +87,7 @@ struct Result
 
     bool ok() const { return code == ResultCode::ok; }
     bool isTechnicalError();
+    bool isSuccess() const { return isSuccessCode(code); }
     bool isBusinessLogicError();
 
     std::string toString() const
