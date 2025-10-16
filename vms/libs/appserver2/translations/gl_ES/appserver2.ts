99bf386e021705764ab51de5e3a64c17e2719045
Jenkins <jenkins@networkoptix.com>
2025-10-16 01:20:06 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/appserver2/translations/gl_ES/appserver2.ts
+++ vms/libs/appserver2/translations/gl_ES/appserver2.ts
@@ -4,10 +4,6 @@
   <context>
     <name>ServerApiErrors</name>
     <message>
-      <source>Maximum number of archive seconds must be provided in range up to %1.</source>
-      <translation>Débese proporcionar o número máximo de segundos de arquivo nun rango de ata %1.</translation>
-    </message>
-    <message>
       <source>User Group &apos;%1&apos; does not exist.</source>
       <translation>O grupo de usuarios &apos;%1&apos; non existe.</translation>
     </message>
