08ec63a77f76443c0bd6323f5568d70102f11cc4
Jenkins <jenkins@networkoptix.com>
2025-10-19 01:19:38 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/da_DK/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/da_DK/mobile_client_qml.ts
@@ -246,11 +246,11 @@
     </message>
     <message>
       <source>Copy Client ID</source>
-      <translation type="unfinished">Copy Client ID</translation>
+      <translation>Kopier Klient ID</translation>
     </message>
     <message>
       <source>Force Crash</source>
-      <translation type="unfinished">Force Crash</translation>
+      <translation>Gennemtving Nedbrud</translation>
     </message>
   </context>
   <context>
