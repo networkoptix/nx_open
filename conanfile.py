d5f92c4baf02085bdc8978c1a1e6bd1d75283d2f
Jenkins <jenkins@networkoptix.com>
2025-10-21 18:07:15 +0300

    Updated help package version (committed by Jenkins)

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ required_conan_version = ">=1.53.0"
 
 
 # Help packages are not required to be built from the same commit.
-VMS_HELP_VERSION = "6.1.0-7918e9cd6fb62ca872d18ca2a71459ab37fa1408"
+VMS_HELP_VERSION = "6.1.0-319cfa6eafb240d3072ac6fd466492c7da75b4b9"
 QUICK_START_GUIDE_VERSION = "6.1.0-c13297795d662c039fbae120562fa5809a3aacb0"
 MOBILE_USER_MANUAL_VERSION = "25.2-319cfa6eafb240d3072ac6fd466492c7da75b4b9"
 
