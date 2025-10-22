0a63aecf4bc27ace9fbbd5692049345a564511c5
Jenkins <jenkins@networkoptix.com>
2025-10-22 01:14:44 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/en_US/client_base.ts
@@ -1145,6 +1145,10 @@
             <numerusform>Unmute Items</numerusform>
         </translation>
     </message>
+    <message>
+        <source>Event log...</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Deactivator</name>
@@ -1190,6 +1194,34 @@
     <message>
         <source>LDAP group membership is managed on LDAP server</source>
         <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DurationPickerTranslationHelper</name>
+    <message>
+        <source>Also include</source>
+        <comment>Part of the text, action duration: Also include &lt;time&gt; Before Event</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Begin</source>
+        <comment>Part of the text, action duration: Begin &lt;time&gt; Before Event</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Before Event</source>
+        <comment>Part of the text, action duration: Begin &lt;time&gt; Before Event</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>End</source>
+        <comment>Part of the text, action duration: End &lt;time&gt; After Event</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>After Event</source>
+        <comment>Part of the text, action duration: End &lt;time&gt; After Event</comment>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -6000,34 +6032,6 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
         <source>Cross-Site features</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
-    <name>TimeDurationWidget</name>
-    <message>
-        <source>Also include</source>
-        <comment>Part of the text, action duration: Also include &lt;time&gt; Before Event</comment>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Begin</source>
-        <comment>Part of the text, action duration: Begin &lt;time&gt; Before Event</comment>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Before Event</source>
-        <comment>Part of the text, action duration: Begin &lt;time&gt; Before Event</comment>
-        <translation></translation>
-    </message>
-    <message>
-        <source>End</source>
-        <comment>Part of the text, action duration: End &lt;time&gt; After Event</comment>
-        <translation></translation>
-    </message>
-    <message>
-        <source>After Event</source>
-        <comment>Part of the text, action duration: End &lt;time&gt; After Event</comment>
         <translation></translation>
     </message>
 </context>
