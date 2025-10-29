41b322af64f0aefa97d90ab00fe7e7216e675116
Evgeny Sobolev <esobolev@networkoptix.com>
2025-10-29 21:43:50 +0300

    MOBILE-2619: Fix time format for Android
    Related to MOBILE-2619
    
    (cherry picked from commit dd99ebf40b8a7540e9013bb4f6c140952ec356c9)

--- vms/client/mobile_client/src/nx/vms/client/mobile/application_context.cpp
+++ vms/client/mobile_client/src/nx/vms/client/mobile/application_context.cpp
@@ -33,6 +33,7 @@
 #include <nx/vms/time/formatter.h>
 #include <settings/qml_settings_adaptor.h>
 #include <ui/camera_thumbnail_provider.h>
+#include <ui/window_utils.h>
 #include <utils/common/delayed.h>
 #include <utils/mobile_app_info.h>
 
@@ -224,7 +225,7 @@ void ApplicationContext::Private::initOsSpecificStuff()
     if (build_info::isAndroid())
     {
         // We have to use android-specific code to check if we use 24-hours time format.
-        time::Formatter::forceSystemTimeFormat(time::is24HoursTimeFormat());
+        time::Formatter::forceSystemTimeFormat(::is24HoursTimeFormat());
     }
 }
 
