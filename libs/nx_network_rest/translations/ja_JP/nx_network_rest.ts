68a1290f1a36d9c984f0e6d785759924ac0d22a9
Jenkins <jenkins@networkoptix.com>
2025-11-07 01:18:27 +0000

    Updated translations (committed by Jenkins)

--- libs/nx_network_rest/translations/ja_JP/nx_network_rest.ts
+++ libs/nx_network_rest/translations/ja_JP/nx_network_rest.ts
@@ -13,7 +13,7 @@
     </message>
     <message>
       <source>Unsupported charset.</source>
-      <translation type="unfinished">Unsupported charset.</translation>
+      <translation>サポートされていない文字セットです。</translation>
     </message>
   </context>
   <context>
