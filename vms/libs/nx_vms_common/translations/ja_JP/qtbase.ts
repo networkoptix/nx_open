d0555bcb3ef469652f3bb166102884f28259d40c
Jenkins <jenkins@networkoptix.com>
2025-11-21 01:20:34 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_common/translations/ja_JP/qtbase.ts
+++ vms/libs/nx_vms_common/translations/ja_JP/qtbase.ts
@@ -614,7 +614,7 @@ Do you want to delete it anyway?</source>
     <message>
       <source>Folder</source>
       <comment>All other platforms</comment>
-      <translation>フォルダ</translation>
+      <translation>フォルダー</translation>
     </message>
     <message>
       <source>Alias</source>
@@ -628,7 +628,7 @@ Do you want to delete it anyway?</source>
     </message>
     <message>
       <source>Unknown</source>
-      <translation>未知の値</translation>
+      <translation>不明</translation>
     </message>
     <message>
       <source>Sidebar</source>
