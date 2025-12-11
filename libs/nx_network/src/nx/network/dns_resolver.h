dc5cbaf4045001d6052b08488bf7f09b166dee60
Igor Kulaychuk <ikulaychuk@networkoptix.com>
2025-12-10 19:56:25 -0800

    MOBILE-2693: Fix TSan warnings in DnsResolver

--- libs/nx_network/src/nx/network/dns_resolver.h
+++ libs/nx_network/src/nx/network/dns_resolver.h
@@ -98,7 +98,7 @@ private:
             size_t sequence, int ipVersion);
     };
 
-    bool m_terminated = false;
+    std::atomic<bool> m_terminated = false;
     mutable nx::Mutex m_mutex;
     mutable nx::WaitCondition m_cond;
     std::deque<size_t /*sequence*/> m_taskQueue;
