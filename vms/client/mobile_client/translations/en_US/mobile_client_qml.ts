39e1179421a19b86c03b70331ec5cc81252e3f5f
Jenkins <jenkins@networkoptix.com>
2025-12-17 01:14:54 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
@@ -630,6 +630,10 @@
         <source>Show more</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>No data</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>OptionSelector</name>
