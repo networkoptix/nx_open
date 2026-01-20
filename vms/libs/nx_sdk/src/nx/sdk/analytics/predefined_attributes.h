34c88c889f4f6442a0807b144fc28b6f28496549
Maksim Tikhomirov <mtikhomirov@networkoptix.com>
2026-01-20 17:39:37 +0300

    VMS-60859: Extract nx::vms::server::analytics::kPortIdAttribute from nx_vms_server
    Related to VMS-60859

---
+++ vms/libs/nx_sdk/src/nx/sdk/analytics/predefined_attributes.h
@@ -0,0 +1,11 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <string>
+
+namespace nx::sdk::analytics {
+
+static const std::string kPortIdAttribute{"nx.sys.portId"};
+
+} // namespace nx::sdk::analytics
