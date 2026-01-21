dcdc82a3776c9ebdab462927eddca7c70aa17fea
Jenkins <jenkins@networkoptix.com>
2026-01-21 17:42:07 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
@@ -972,7 +972,7 @@
     </message>
     <message>
       <source>Notifications are turned off in the device settings</source>
-      <translation type="unfinished">Notifications are turned off in the device settings</translation>
+      <translation>Las notificaciones están desactivadas en los ajustes del dispositivo</translation>
     </message>
     <message>
       <source>Beta Features</source>
@@ -992,7 +992,7 @@
     </message>
     <message>
       <source>Log in to the cloud to receive notifications</source>
-      <translation type="unfinished">Log in to the cloud to receive notifications</translation>
+      <translation>Inicie sesión en la nube para recibir notificaciones</translation>
     </message>
     <message>
       <source>About</source>
