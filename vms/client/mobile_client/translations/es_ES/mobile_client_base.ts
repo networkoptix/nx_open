96e738db0c22b24a46bd63695776a1ee0b4735b2
Jenkins <jenkins@networkoptix.com>
2026-01-21 17:42:06 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/es_ES/mobile_client_base.ts
+++ vms/client/mobile_client/translations/es_ES/mobile_client_base.ts
@@ -119,11 +119,11 @@
     </message>
     <message>
       <source>Objects (&gt;%1)</source>
-      <translation type="unfinished">Objects (&gt;%1)</translation>
+      <translation>Objetos (&gt;%1)</translation>
     </message>
     <message>
       <source>Objects (%1)</source>
-      <translation type="unfinished">Objects (%1)</translation>
+      <translation>Objetos (%1)</translation>
     </message>
   </context>
   <context>
@@ -134,11 +134,11 @@
     </message>
     <message>
       <source>Bookmarks (&gt;%1)</source>
-      <translation type="unfinished">Bookmarks (&gt;%1)</translation>
+      <translation>Marcadores (&gt;%1)</translation>
     </message>
     <message>
       <source>Bookmarks (%1)</source>
-      <translation type="unfinished">Bookmarks (%1)</translation>
+      <translation>Marcadores (%1)</translation>
     </message>
   </context>
   <context>
@@ -149,11 +149,11 @@
     </message>
     <message>
       <source>Motion (&gt;%1)</source>
-      <translation type="unfinished">Motion (&gt;%1)</translation>
+      <translation>Movimiento (&gt;%1)</translation>
     </message>
     <message>
       <source>Motion (%1)</source>
-      <translation type="unfinished">Motion (%1)</translation>
+      <translation>Movimiento (%1)</translation>
     </message>
   </context>
 </TS>
--- vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
@@ -825,11 +825,11 @@
     </message>
     <message>
       <source>Log in to the cloud to receive notifications</source>
-      <translation type="unfinished">Log in to the cloud to receive notifications</translation>
+      <translation>Inicie sesión en la nube para recibir notificaciones</translation>
     </message>
     <message>
       <source>Notifications are turned off in the device settings</source>
-      <translation type="unfinished">Notifications are turned off in the device settings</translation>
+      <translation>Las notificaciones están desactivadas en los ajustes del dispositivo</translation>
     </message>
   </context>
   <context>
