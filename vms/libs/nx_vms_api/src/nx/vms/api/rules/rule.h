feefdf5ef433c836deadbbafb6cd4af688f6d3e4
Sergei Belov <sbelov@networkoptix.com>
2026-02-26 13:32:37 +0000

    VMS-61653: Server API: Add flag prolonged to rest/v4-/events/rules
    Related to VMS-61653
    
    (cherry picked from commit d65f4f098c4c15694acd5b077af545378785d2ee)

--- vms/libs/nx_vms_api/src/nx/vms/api/rules/rule.h
+++ vms/libs/nx_vms_api/src/nx/vms/api/rules/rule.h
@@ -53,12 +53,15 @@ struct NX_VMS_API RuleV4: public IdData
     /**%apidoc[opt] String comment explaining the rule. */
     QString comment;
 
+    /**%apidoc[readonly] Action requires an event to be prolonged. */
+    std::optional<bool> prolonged;
+
     /**%apidoc[proprietary] */
     std::optional<std::string> etag;
 };
 
 #define nx_vms_api_rules_RuleV4_Fields \
-    IdData_Fields(event)(action)(enabled)(schedule)(comment)(etag)
+    IdData_Fields(event)(action)(enabled)(schedule)(comment)(prolonged)(etag)
 NX_VMS_API_DECLARE_STRUCT_AND_LIST_EX(RuleV4, (json))
 NX_REFLECTION_INSTRUMENT(RuleV4, nx_vms_api_rules_RuleV4_Fields)
 
