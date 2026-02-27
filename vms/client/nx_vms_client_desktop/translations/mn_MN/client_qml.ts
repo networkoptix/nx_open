2f288c60ffe7394837205fe55c64fd868c2bd3a3
Jenkins <jenkins@networkoptix.com>
2026-02-27 01:21:50 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/mn_MN/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/mn_MN/client_qml.ts
@@ -3098,7 +3098,7 @@ Press Alt + Click to follow object</translation>
     </message>
     <message>
       <source>Duplicate</source>
-      <translation type="unfinished">Duplicate</translation>
+      <translation>Хуулбарлах</translation>
     </message>
     <message>
       <source>Delete</source>
