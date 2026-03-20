ad503f394a1f00d56eb91f22daba9dc1df6f5421
Jenkins <jenkins@networkoptix.com>
2026-03-20 01:19:29 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/es_ES/mobile_client_qml.ts
@@ -745,7 +745,7 @@
     <name>RangeSelector</name>
     <message>
       <source>From</source>
-      <translation type="unfinished">From</translation>
+      <translation>De</translation>
     </message>
     <message>
       <source>To</source>
