6e170114dda9b6cd2b1963cd545128f06cea47e2
Alexander Kulikov <akulikov@networkoptix.com>
2026-04-03 09:10:32 +0000

    VMS-61875: Set inheritHandles to false when starting win process
    Related to VMS-61875

--- libs/nx_utils/src/nx/utils/platform/process_unix.cpp
+++ libs/nx_utils/src/nx/utils/platform/process_unix.cpp
@@ -9,6 +9,23 @@
 #include <QtCore/QProcess>
 #include <QtCore/QFile>
 
+namespace {
+
+std::unique_ptr<QProcess> createProcess(
+    const QString& executablePath,
+    const QStringList& arguments,
+    const QString& workingDirectory)
+{
+    auto process = std::make_unique<QProcess>();
+    process->setProgram(executablePath);
+    process->setArguments(arguments);
+    if (!workingDirectory.isNull())
+        process->setWorkingDirectory(workingDirectory);
+    return process;
+}
+
+} // anonymous namespace
+
 namespace nx::utils {
 
 SystemError::ErrorCode killProcessByPid(qint64 pid)
@@ -24,12 +41,23 @@ bool startProcessDetached(
     const QString& workingDirectory,
     qint64* pid)
 {
-    return QProcess::startDetached(executablePath, arguments, workingDirectory, pid);
+    auto p = createProcess(executablePath, arguments, workingDirectory);
+    return p->startDetached(pid);
 }
 
 bool checkProcessExists(qint64 pid)
 {
     return kill(pid, 0) == 0;
+}
+
+std::unique_ptr<QProcess> startProcess(
+    const QString& executablePath,
+    const QStringList& arguments,
+    const QString& workingDirectory)
+{
+    auto p = createProcess(executablePath, arguments, workingDirectory);
+    p->start();
+    return p;
 }
 
 } // namespace nx::utils
