5c759c053b9a2132a697856dd3d2106e4248b50f
Jenkins <jenkins@networkoptix.com>
2026-04-23 01:20:09 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
@@ -2006,6 +2006,26 @@ License Key: %2</source>
       <source>Note: Image Enhancement is not available when Hardware Video Decoding is turned on.</source>
       <translation>注意: ハードウェアビデオデコーディングが有効になっている場合、画像補正は利用できません。</translation>
     </message>
+    <message>
+      <source>Cannot clear WebEngine cache</source>
+      <translation type="unfinished">Cannot clear WebEngine cache</translation>
+    </message>
+    <message>
+      <source>Please close all web pages and try again.</source>
+      <translation type="unfinished">Please close all web pages and try again.</translation>
+    </message>
+    <message>
+      <source>The files may be locked by another process.</source>
+      <translation type="unfinished">The files may be locked by another process.</translation>
+    </message>
+    <message>
+      <source>WebEngine cache partially cleared</source>
+      <translation type="unfinished">WebEngine cache partially cleared</translation>
+    </message>
+    <message>
+      <source>Some files may be locked by another process.</source>
+      <translation type="unfinished">Some files may be locked by another process.</translation>
+    </message>
   </context>
   <context>
     <name>QnAttachToVideowallDialog</name>
