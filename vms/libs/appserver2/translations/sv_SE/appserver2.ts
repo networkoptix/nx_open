a14f07fdf90fd47a2b3fe1c2e82db4cf6098d00a
Jenkins <jenkins@networkoptix.com>
2026-06-04 01:22:16 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/appserver2/translations/sv_SE/appserver2.ts
+++ vms/libs/appserver2/translations/sv_SE/appserver2.ts
@@ -341,7 +341,7 @@
     </message>
     <message>
       <source>`serverId` must be `*` or nil UUID</source>
-      <translation type="unfinished">`serverId` must be `*` or nil UUID</translation>
+      <translation>`serverId` måste vara `*` eller noll UUID</translation>
     </message>
   </context>
 </TS>
