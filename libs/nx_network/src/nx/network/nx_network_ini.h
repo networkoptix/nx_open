d493bbfefccc119b678db701691b6d679230ba26
Sergei Belov <sbelov@networkoptix.com>
2026-07-10 16:27:49 +0000

    VMS-62432: Server: Reuse http clients in cloud integrations call
    Related to VMS-62432

--- libs/nx_network/src/nx/network/nx_network_ini.h
+++ libs/nx_network/src/nx/network/nx_network_ini.h
@@ -43,6 +43,10 @@ struct NX_NETWORK_API Ini: nx::kit::IniConfig
 
     NX_INI_FLAG(false, traceIoObjectsLifetime,
         "Enables reporting creation stack traces of dangling HTTP clients during server shutdown");
+
+    NX_INI_INT(5, cloudDbIdleConnectionsLimit,
+        "Maximum number of idle connections to the Cloud kept open for reuse by subsequent "
+        "requests. 0 disables connection reuse.");
 };
 
 NX_NETWORK_API Ini& ini();
