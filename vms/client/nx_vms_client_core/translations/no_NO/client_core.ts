a9e6238ed8908e5e6e643fe0a1827d1971804359
Jenkins <jenkins@networkoptix.com>
2026-07-21 01:20:05 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_core/translations/no_NO/client_core.ts
+++ vms/client/nx_vms_client_core/translations/no_NO/client_core.ts
@@ -6,7 +6,7 @@
     <message>
       <source>Language Name</source>
       <comment>Language name that will be displayed to the user.</comment>
-      <translation>Språknavn</translation>
+      <translation>Norsk</translation>
     </message>
   </context>
   <context>
