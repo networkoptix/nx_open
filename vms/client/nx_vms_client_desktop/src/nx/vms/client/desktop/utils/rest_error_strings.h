ca1468c1821dbbcbaa7c05b78b8dc25786c691ae
Dmitry Fisenko <dfisenko@networkoptix.com>
2026-07-22 15:35:28 +0200

    VMS-62155: Revert error id displaying instead of error string from a server
    Related to VMS-62155
    
    (cherry picked from commit b805d7ff08b13084461a203b518a625f8c03761a)

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/utils/rest_error_strings.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/utils/rest_error_strings.h
@@ -3,11 +3,7 @@
 #pragma once
 
 #include <QtCore/QCoreApplication>
-#include <QLocale>
 
-#include <nx/branding.h>
-#include <nx/vms/client/core/settings/client_core_settings.h>
-#include <nx/vms/client/desktop/application_context.h>
 #include <nx/vms/client/desktop/ini.h>
 
 namespace nx::vms::client::desktop {
@@ -17,22 +13,14 @@ class RestErrorStrings
     Q_DECLARE_TR_FUNCTIONS(RestErrorStrings)
 
 public:
-    // We cannot guarantee that errorString is translated, so we show it only in the developer mode
-    // or for English locale.
     template<typename T>
     static QString description(const T& error)
     {
-        auto languageName = appContext()->coreSettings()->locale().split('_').first();
-        if (languageName.isEmpty())
-            languageName = nx::branding::defaultLocale().split('_').first();
+        if (!error.errorString.isEmpty())
+            return error.errorString;
 
-        return ini().developerMode || languageName == "en"
-            ? getPersistentStringPart((int)error.errorId) + ". " + error.errorString
-            : getPersistentStringPart((int)error.errorId);
+        return tr("Error code: %1").arg((int) error.errorId);
     }
-
-private:
-    static QString getPersistentStringPart(int errorId);
 };
 
 } // namespace nx::vms::client::desktop
