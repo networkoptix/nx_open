fbda358617f3420f70e231799300be7151ae4887
Jenkins <jenkins@networkoptix.com>
2026-07-25 01:20:34 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_core/translations/el_GR/client_core.ts
+++ vms/client/nx_vms_client_core/translations/el_GR/client_core.ts
@@ -1,19 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="lt_LT" sourcelanguage="en">
+<TS version="2.1" language="el_GR" sourcelanguage="en">
   <context>
     <name>Language</name>
     <message>
       <source>Language Name</source>
       <comment>Language name that will be displayed to the user.</comment>
-      <translation>Lietuvių</translation>
+      <translation>Ελληνικά</translation>
     </message>
   </context>
   <context>
     <name>QnSystemsModel</name>
     <message>
       <source>New Site</source>
-      <translation>Lietuvių</translation>
+      <translation type="unfinished">New Site</translation>
     </message>
   </context>
   <context>
@@ -226,8 +226,6 @@
       <translation type="unfinished">
         <numerusform>Cannot verify the identity of %n servers</numerusform>
         <numerusform>Cannot verify the identity of %n servers</numerusform>
-        <numerusform>Cannot verify the identity of %n servers</numerusform>
-        <numerusform>Cannot verify the identity of %n servers</numerusform>
       </translation>
     </message>
     <message>
@@ -330,14 +328,12 @@
     </message>
     <message>
       <source>Any</source>
-      <translation>Bet kuris</translation>
+      <translation type="unfinished">Any</translation>
     </message>
     <message numerus="yes">
       <source>%n Cameras</source>
       <comment>%n is a number of cameras</comment>
       <translation type="unfinished">
-        <numerusform>%n Cameras</numerusform>
-        <numerusform>%n Cameras</numerusform>
         <numerusform>%n Cameras</numerusform>
         <numerusform>%n Cameras</numerusform>
       </translation>
@@ -400,7 +396,7 @@
     </message>
     <message>
       <source>Cloud</source>
-      <translation>Debesis</translation>
+      <translation type="unfinished">Cloud</translation>
     </message>
     <message>
       <source>Local</source>
