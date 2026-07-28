15e875948905a1d4eda519abf3bc5f1a4b56279c
Jenkins <jenkins@networkoptix.com>
2026-07-28 01:20:15 +0000

    Updated translations (committed by Jenkins)

--- vms/libs/nx_vms_rules/translations/el_GR/nx_vms_rules.ts
+++ vms/libs/nx_vms_rules/translations/el_GR/nx_vms_rules.ts
@@ -153,7 +153,7 @@
     </message>
     <message>
       <source>State</source>
-      <translation type="unfinished">State</translation>
+      <translation>Κατάσταση</translation>
     </message>
     <message>
       <source>Volume</source>
@@ -470,7 +470,7 @@
     </message>
     <message>
       <source>Camera</source>
-      <translation type="unfinished">Camera</translation>
+      <translation>Κάμερα</translation>
     </message>
     <message>
       <source>Device disconnected</source>
@@ -569,7 +569,7 @@
     <name>nx::vms::rules::EnterFullscreenAction</name>
     <message>
       <source>Camera</source>
-      <translation type="unfinished">Camera</translation>
+      <translation>Κάμερα</translation>
     </message>
     <message>
       <source>Set to Fullscreen</source>
