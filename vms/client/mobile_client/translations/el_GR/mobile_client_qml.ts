2ba2ce6126309d8618478ea266bd3a9c939854ee
Jenkins <jenkins@networkoptix.com>
2026-08-13 01:20:18 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/el_GR/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/el_GR/mobile_client_qml.ts
@@ -219,7 +219,7 @@
     <name>DetailsScreen</name>
     <message>
       <source>Preview</source>
-      <translation>Προεπισκόπηση</translation>
+      <translation type="unfinished">Preview</translation>
     </message>
     <message>
       <source>Show on Camera</source>
