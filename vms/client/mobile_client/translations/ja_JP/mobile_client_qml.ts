dd876a8183120d30f1b528684ede0a75e8076a85
Jenkins <jenkins@networkoptix.com>
2026-08-24 01:20:24 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/ja_JP/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/ja_JP/mobile_client_qml.ts
@@ -1665,7 +1665,7 @@
     </message>
     <message>
       <source>Calendar</source>
-      <translation type="unfinished">Calendar</translation>
+      <translation>カレンダー</translation>
     </message>
     <message>
       <source>Actions</source>
