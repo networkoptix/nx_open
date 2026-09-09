3bcd982730efd59d813e85fde68182eb6235fe4e
Jenkins <jenkins@networkoptix.com>
2026-09-09 01:15:09 +0000

    Update translatable strings (committed by Jenkins)

--- vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/en_US/mobile_client_qml.ts
@@ -717,6 +717,10 @@
         <source>Select what to download</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Edit sharing</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>ObjectSelectionSheet</name>
