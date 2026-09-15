51fa28aabc4d6a36a4045c4100e0630400f8fe14
Jenkins <jenkins@networkoptix.com>
2026-09-15 01:21:21 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ar_SA/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/ar_SA/client_qml.ts
@@ -687,7 +687,7 @@ Select an Integration to begin configuring its parameters.</translation>
     </message>
     <message>
       <source>Value</source>
-      <translation type="unfinished">Value</translation>
+      <translation>القيمة</translation>
     </message>
   </context>
   <context>
@@ -795,7 +795,7 @@ Select an Integration to begin configuring its parameters.</translation>
     </message>
     <message>
       <source>Value</source>
-      <translation type="unfinished">Value</translation>
+      <translation>القيمة</translation>
     </message>
   </context>
   <context>
@@ -1314,7 +1314,7 @@ Select an Integration to begin configuring its parameters.</translation>
     <name>IntegrationPanel</name>
     <message>
       <source>Version</source>
-      <translation type="unfinished">Version</translation>
+      <translation>الإصدار</translation>
     </message>
     <message>
       <source>Vendor</source>
@@ -1330,7 +1330,7 @@ Select an Integration to begin configuring its parameters.</translation>
     </message>
     <message>
       <source>Usage</source>
-      <translation type="unfinished">Usage</translation>
+      <translation>الإستخدام</translation>
     </message>
   </context>
   <context>
@@ -1594,7 +1594,7 @@ Select an Integration to begin configuring its parameters.</translation>
     </message>
     <message>
       <source>value</source>
-      <translation type="unfinished">value</translation>
+      <translation>القيمة</translation>
     </message>
   </context>
   <context>
@@ -2383,7 +2383,7 @@ Press Alt + Click to follow object</translation>
     </message>
     <message>
       <source>Try searching for something else</source>
-      <translation type="unfinished">Try searching for something else</translation>
+      <translation>حاول البحث عن شيء آخر</translation>
     </message>
   </context>
   <context>
