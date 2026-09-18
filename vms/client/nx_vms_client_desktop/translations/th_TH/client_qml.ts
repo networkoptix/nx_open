8e9c3a520844902947a29d30c28ff18c7fd4ed1e
Jenkins <jenkins@networkoptix.com>
2026-09-18 12:48:09 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/th_TH/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/th_TH/client_qml.ts
@@ -212,6 +212,16 @@
       <source>In selected area</source>
       <translation>ในพื้นที่ที่เลือก</translation>
     </message>
+    <message numerus="yes">
+      <source>%n new results</source>
+      <translation type="unfinished">
+        <numerusform>%n new results</numerusform>
+      </translation>
+    </message>
+    <message>
+      <source>To the top</source>
+      <translation type="unfinished">To the top</translation>
+    </message>
     <message>
       <source>No objects</source>
       <translation>วัตถุ</translation>
