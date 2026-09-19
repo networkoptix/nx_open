cfbe5f10f5677a77cb84e6249668c9b23c7eaa51
Jenkins <jenkins@networkoptix.com>
2026-09-19 01:21:33 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/id_ID/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/id_ID/client_qml.ts
@@ -222,8 +222,8 @@
     </message>
     <message numerus="yes">
       <source>%n new results</source>
-      <translation type="unfinished">
-        <numerusform>%n new results</numerusform>
+      <translation>
+        <numerusform>%n hasil baru</numerusform>
       </translation>
     </message>
     <message>
