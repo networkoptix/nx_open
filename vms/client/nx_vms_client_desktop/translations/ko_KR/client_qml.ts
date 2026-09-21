fa371895fd206c3df8c7a5b20470950e00c02c9b
Jenkins <jenkins@networkoptix.com>
2026-09-21 11:56:01 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ko_KR/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/ko_KR/client_qml.ts
@@ -220,6 +220,16 @@
       <source>In selected area</source>
       <translation>선택된 영역에서</translation>
     </message>
+    <message numerus="yes">
+      <source>%n new results</source>
+      <translation type="unfinished">
+        <numerusform>%n new results</numerusform>
+      </translation>
+    </message>
+    <message>
+      <source>To the top</source>
+      <translation type="unfinished">To the top</translation>
+    </message>
     <message>
       <source>No objects</source>
       <translation>객체가 없습니다</translation>
