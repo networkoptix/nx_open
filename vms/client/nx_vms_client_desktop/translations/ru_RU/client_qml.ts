00c46982570fc33a9780cd761c6c884f0cce443c
Jenkins <jenkins@networkoptix.com>
2026-09-21 11:56:02 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ru_RU/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/ru_RU/client_qml.ts
@@ -220,6 +220,19 @@
       <source>In selected area</source>
       <translation>В выбранной области</translation>
     </message>
+    <message numerus="yes">
+      <source>%n new results</source>
+      <translation type="unfinished">
+        <numerusform>%n new results</numerusform>
+        <numerusform>%n new results</numerusform>
+        <numerusform>%n new results</numerusform>
+        <numerusform>%n new results</numerusform>
+      </translation>
+    </message>
+    <message>
+      <source>To the top</source>
+      <translation type="unfinished">To the top</translation>
+    </message>
     <message>
       <source>No objects</source>
       <translation>Нет объектов</translation>
--- vms/client/nx_vms_client_desktop/translations/vi_VN/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/vi_VN/client_qml.ts
@@ -220,6 +220,16 @@
       <source>In selected area</source>
       <translation>Trong khu vực được chọn</translation>
     </message>
+    <message numerus="yes">
+      <source>%n new results</source>
+      <translation type="unfinished">
+        <numerusform>%n new results</numerusform>
+      </translation>
+    </message>
+    <message>
+      <source>To the top</source>
+      <translation type="unfinished">To the top</translation>
+    </message>
     <message>
       <source>No objects</source>
       <translation>Không có đối tượng</translation>
