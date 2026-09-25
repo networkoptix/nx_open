d0e7e7d189d135e317967b8e925df46f4ff0c93e
Jenkins <jenkins@networkoptix.com>
2026-09-25 01:22:41 +0000

    Updated translations (committed by Jenkins)

--- vms/client/mobile_client/translations/da_DK/mobile_client_qml.ts
+++ vms/client/mobile_client/translations/da_DK/mobile_client_qml.ts
@@ -146,11 +146,11 @@
     <name>CameraSwitcherTutorial</name>
     <message>
       <source>Swipe Between Resources</source>
-      <translation type="unfinished">Swipe Between Resources</translation>
+      <translation>Stryg Mellem Ressourcer</translation>
     </message>
     <message>
       <source>Swipe right to switch to next Camera</source>
-      <translation type="unfinished">Swipe right to switch to next Camera</translation>
+      <translation>Stryg til højre for at skifte til næste kamera</translation>
     </message>
   </context>
   <context>
@@ -744,11 +744,11 @@
     <name>ObjectTypesTutorial</name>
     <message>
       <source>View Mode</source>
-      <translation type="unfinished">View Mode</translation>
+      <translation>Visningstilstand</translation>
     </message>
     <message>
       <source>Tap to choose desired data type</source>
-      <translation type="unfinished">Tap to choose desired data type</translation>
+      <translation>Tryk for at vælge den ønskede datatype</translation>
     </message>
   </context>
   <context>
@@ -785,11 +785,11 @@
     <name>PanelButtonTutorial</name>
     <message>
       <source>Floating Panel Button</source>
-      <translation type="unfinished">Floating Panel Button</translation>
+      <translation>Flydende Panel Knap</translation>
     </message>
     <message>
       <source>Tap the floating button to restore the panel</source>
-      <translation type="unfinished">Tap the floating button to restore the panel</translation>
+      <translation>Tryk på den flydende knap for at gendanne panelet</translation>
     </message>
   </context>
   <context>
@@ -1422,11 +1422,11 @@
     <name>SoftTriggersTutorial</name>
     <message>
       <source>Soft Triggers</source>
-      <translation type="unfinished">Soft Triggers</translation>
+      <translation>Soft udløsere</translation>
     </message>
     <message>
       <source>Tap to invoke soft triggers panel</source>
-      <translation type="unfinished">Tap to invoke soft triggers panel</translation>
+      <translation>Tryk for at aktivere panelet for soft udløsere</translation>
     </message>
   </context>
   <context>
@@ -1483,22 +1483,22 @@
     <name>TimelinePreviewTutorial</name>
     <message>
       <source>Timeline Cards and Player</source>
-      <translation type="unfinished">Timeline Cards and Player</translation>
+      <translation>Tidslinje kort og afspiller</translation>
     </message>
     <message>
       <source>Long tap to invoke preview with more details</source>
-      <translation type="unfinished">Long tap to invoke preview with more details</translation>
+      <translation>Langt tryk for at aktivere forhåndsvisning med flere detaljer</translation>
     </message>
   </context>
   <context>
     <name>TimelineZoomTutorial</name>
     <message>
       <source>Zoom Timeline In/Out</source>
-      <translation type="unfinished">Zoom Timeline In/Out</translation>
+      <translation>Zoom Tidslinje Ind/Ud</translation>
     </message>
     <message>
       <source>Pinch to Zoom Out and Spread to Zoom In</source>
-      <translation type="unfinished">Pinch to Zoom Out and Spread to Zoom In</translation>
+      <translation>Knib for at zoome ud og spred for at zoome ind</translation>
     </message>
   </context>
   <context>
