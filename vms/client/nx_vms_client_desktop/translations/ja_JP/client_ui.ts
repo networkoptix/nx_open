4eff2c5cce11ac898ac7a97871e506fc6a821f5c
Jenkins <jenkins@networkoptix.com>
2026-09-25 01:22:42 +0000

    Updated translations (committed by Jenkins)

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_ui.ts
@@ -480,7 +480,7 @@
     </message>
     <message>
       <source>Use camera native presets</source>
-      <translation>カメラのネイティブプリセットを使用</translation>
+      <translation>カメラのネイティブプリセットを使用する</translation>
     </message>
     <message>
       <source>Time Settings</source>
@@ -516,7 +516,7 @@
     </message>
     <message>
       <source>Web Page Port</source>
-      <translation>Webページポート</translation>
+      <translation>Webページのポート</translation>
     </message>
     <message>
       <source>Do not send Stop PTZ command</source>
@@ -528,15 +528,15 @@
     </message>
     <message>
       <source>Import from Device</source>
-      <translation>デバイスからインポート</translation>
+      <translation>デバイスからのインポート</translation>
     </message>
     <message>
       <source>Import video automatically</source>
-      <translation>映像を自動的にインポート</translation>
+      <translation>映像を自動的にインポートする</translation>
     </message>
     <message>
       <source>Use site presets</source>
-      <translation>サイトプリセットを使用</translation>
+      <translation>サイトのプリセットを使用する</translation>
     </message>
     <message>
       <source>Native camera presets will not be available.</source>
